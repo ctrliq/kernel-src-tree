064cac0e85883d89bb45f3cce00e2f1eb9e0b8df
Eugene Syromiatnikov <esyr@redhat.com>
2018-06-14 16:36:08 -0400

    bpf: Add tech preview taint for syscall
    Message-id: <394af3a9985d1ad9ea6741cb95ff145312d106b9.1528991396.git.esyr@redhat.com>
    Patchwork-id: 8251
    O-Subject: [kernel team] [RHEL8 PATCH v4 3/5] [bpf] bpf: Add tech preview taint for syscall
    Bugzilla: 1559877
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    
    Adding tech preview taint for using the eBPF syscall.
    
    Based on a RHEL 7 commit "bpf: Add tech preview taint for syscall" by
    Jiri Olsa.
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1559877
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=16716594
    
    Upstream Status: RHEL only
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Signed-off-by: Eugene Syromiatnikov <esyr@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -4390,10 +4390,16 @@ out_prog_put:
 SYSCALL_DEFINE3(bpf, int, cmd, union bpf_attr __user *, uattr, unsigned int, size)
 {
 	union bpf_attr attr;
+	static int marked;
 	int err;
 
 	if (sysctl_unprivileged_bpf_disabled && !bpf_capable())
 		return -EPERM;
+
+	if (!marked) {
+		mark_tech_preview("eBPF syscall", NULL);
+		marked = true;
+	}
 
 	err = bpf_check_uarg_tail_zero(uattr, sizeof(attr), size);
 	if (err)
