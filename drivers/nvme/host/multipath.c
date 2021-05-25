63efc9c5a8a71869e6a77a9d775ff78ad9d1b18b
Mike Snitzer <snitzer@redhat.com>
2021-05-25 12:36:06 -0400

    nvme: nvme_mpath_init remove multipath check
    BZ: 1948690
    Upstream Status: RHEL-only
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    
    rhel-8.git commit f027c2e4045d02d103c7a545181b6df0b6162ee7
    Author: David Milburn <dmilburn@redhat.com>
    Date:   Wed Jan 29 15:29:37 2020 -0500
    
        [nvme] nvme: nvme_mpath_init remove multipath check
    
        Message-id: <1580311777-9193-1-git-send-email-dmilburn@redhat.com>
        Patchwork-id: 294254
        Patchwork-instance: patchwork
        O-Subject: [RHEL8.2 PATCH] nvme: nvme_mpath_init remove multipath check
        Bugzilla: 1790958
        RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
        RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
        Marco Patalano found missing NVMe optimized/inaccessible paths when
        executing "nvme list-subsys" command with native multipathing disabled.
        He was able to git bisect this back to 6d0f426e ("nvme: fix multipath
        crash when ANA is deactivated"). The problem is the check for multipath,
        removing this is similar to RHEL commit 158eef2e ("nvme: allow ANA support
        to be independent of native multipathing"), I did leave the existing
        comment in place for future back ports and as a reminder to watch for
        these changes in the future.
    
        Bugzilla: 1790958
        Build info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=26061480
        Upstream: RHEL only
        Test: QE verified "nvme list-subsys" command and did sanity check with native
        multipathing disabled.
    
        Fixes: 6d0f426e ("nvme: fix multipath crash when ANA is deactivated")
        Signed-off-by: David Milburn <dmilburn@redhat.com>
        Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- drivers/nvme/host/multipath.c
+++ drivers/nvme/host/multipath.c
@@ -1006,8 +1006,7 @@ int nvme_mpath_init_identify(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 	int error = 0;
 
 	/* check if multipath is enabled and we have the capability */
-	if (!multipath || !ctrl->subsys ||
-	    !(ctrl->subsys->cmic & NVME_CTRL_CMIC_ANA))
+	if (!ctrl->subsys || !(ctrl->subsys->cmic & NVME_CTRL_CMIC_ANA))
 		return 0;
 
 	/* initialize this in the identify path to cover controller resets */
