4231684a55d9e62884ec542749c0efadaf4a94eb
Corinna Vinschen <vinschen@redhat.com>
2021-06-30 10:15:32 +0200

    ixgbevf: Add support for new mailbox communication between PF and VF
    Author: Radoslaw Tyl <radoslawx.tyl@intel.com>
    
    Provide improved mailbox communication, between PF and VF,
    which is defined as API version 1.5.
    
    Signed-off-by: Radoslaw Tyl <radoslawx.tyl@intel.com>
    Tested-by: Tony Brelinski <tony.brelinski@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit 339f28964147db9cb5c45d84a9b9d86a8ceb9260)
    JIRA: https://issues.redhat.com/browse/RHEL-97673
    Conflicts: This commit should have been applied back in 2022, before applying
               commit 9c9463c29d1b ("ixgbevf: Add legacy suffix to old API mailbox
    	   functions"), but wasn't for some reason.  This leads to a naming
    	   conflict ixgbevf_mbx_ops vs. ixgbevf_mbx_ops_legacy.
    Signed-off-by: Corinna Vinschen <vinschen@redhat.com>

--- drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
+++ drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
@@ -2275,6 +2275,7 @@ static void ixgbevf_negotiate_api(struct ixgbevf_adapter *adapter)
 {
 	struct ixgbe_hw *hw = &adapter->hw;
 	static const int api[] = {
+		ixgbe_mbox_api_15,
 		ixgbe_mbox_api_14,
 		ixgbe_mbox_api_13,
 		ixgbe_mbox_api_12,
@@ -2291,6 +2292,12 @@ static void ixgbevf_negotiate_api(struct ixgbevf_adapter *adapter)
 		if (!err)
 			break;
 		idx++;
+	}
+
+	if (hw->api_version >= ixgbe_mbox_api_15) {
+		hw->mbx.ops.init_params(hw);
+		memcpy(&hw->mbx.ops, &ixgbevf_mbx_ops_legacy,
+		       sizeof(struct ixgbe_mbx_operations));
 	}
 
 	spin_unlock_bh(&adapter->mbx_lock);
@@ -2643,6 +2650,7 @@ static void ixgbevf_set_num_queues(struct ixgbevf_adapter *adapter)
 		case ixgbe_mbox_api_12:
 		case ixgbe_mbox_api_13:
 		case ixgbe_mbox_api_14:
+		case ixgbe_mbox_api_15:
 			if (adapter->xdp_prog &&
 			    hw->mac.max_tx_queues == rss)
 				rss = rss > 3 ? 2 : 1;
@@ -4636,6 +4644,7 @@ static int ixgbevf_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	case ixgbe_mbox_api_12:
 	case ixgbe_mbox_api_13:
 	case ixgbe_mbox_api_14:
+	case ixgbe_mbox_api_15:
 		netdev->max_mtu = IXGBE_MAX_JUMBO_FRAME_SIZE -
 				  (ETH_HLEN + ETH_FCS_LEN);
 		break;
