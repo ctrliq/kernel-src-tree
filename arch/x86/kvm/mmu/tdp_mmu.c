8dfc2ee4c4895627216db5264b8cc6b9123af734
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-10-07 14:57:54 +0200

    KVM: const-ify all relevant uses of struct kvm_memory_slot
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2009338
    
    commit 269e9552d208179bc14ea7f80a9e3e8ae97795a2
    Author: Hamza Mahfooz <someguy@effective-light.com>
    Date:   Mon Jul 12 22:33:38 2021 -0400
    
        KVM: const-ify all relevant uses of struct kvm_memory_slot
    
        As alluded to in commit f36f3f2846b5 ("KVM: add "new" argument to
        kvm_arch_commit_memory_region"), a bunch of other places where struct
        kvm_memory_slot is used, needs to be refactored to preserve the
        "const"ness of struct kvm_memory_slot across-the-board.
    
        Signed-off-by: Hamza Mahfooz <someguy@effective-light.com>
        Message-Id: <20210713023338.57108-1-someguy@effective-light.com>
        [Do not touch body of slot_rmap_walk_init. - Paolo]
        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1259,8 +1259,8 @@ retry:
  * only affect leaf SPTEs down to min_level.
  * Returns true if an SPTE has been changed and the TLBs need to be flushed.
  */
-bool kvm_tdp_mmu_wrprot_slot(struct kvm *kvm, struct kvm_memory_slot *slot,
-			     int min_level)
+bool kvm_tdp_mmu_wrprot_slot(struct kvm *kvm,
+			     const struct kvm_memory_slot *slot, int min_level)
 {
 	struct kvm_mmu_page *root;
 	bool spte_set = false;
@@ -1330,7 +1330,8 @@ retry:
  * each SPTE. Returns true if an SPTE has been changed and the TLBs need to
  * be flushed.
  */
-bool kvm_tdp_mmu_clear_dirty_slot(struct kvm *kvm, struct kvm_memory_slot *slot)
+bool kvm_tdp_mmu_clear_dirty_slot(struct kvm *kvm,
+				  const struct kvm_memory_slot *slot)
 {
 	struct kvm_mmu_page *root;
 	bool spte_set = false;
