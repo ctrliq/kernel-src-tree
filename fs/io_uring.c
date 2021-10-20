15d7b0e26fc4ff6bb5d028f9e8b16f414c215c32
Jeff Moyer <jmoyer@redhat.com>
2021-10-20 09:53:02 +0100

    io_uring: fix ltimeout unprep
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2107656
    
    commit 4ea672ab694c23886b52e97cee10dea056e43e62
    Author: Pavel Begunkov <asml.silence@gmail.com>
    Date:   Wed Oct 20 09:53:02 2021 +0100
    
        io_uring: fix ltimeout unprep
        
        io_unprep_linked_timeout() is broken, first it needs to return back
        REQ_F_ARM_LTIMEOUT, so the linked timeout is enqueued and disarmed. But
        now we refcounted it, and linked timeouts may get not executed at all,
        leaking a request.
        
        Just kill the unprep optimisation.
        
        Fixes: 906c6caaf586 ("io_uring: optimise io_prep_linked_timeout()")
        Reported-by: Beld Zhang <beldzhang@gmail.com>
        Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
        Link: https://lore.kernel.org/r/51b8e2bfc4bea8ee625cf2ba62b2a350cc9be031.1634719585.git.asml.silence@gmail.com
        Link: https://github.com/axboe/liburing/issues/460
        Reported-by: Beld Zhang <beldzhang@gmail.com>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1403,11 +1403,6 @@ static void io_req_track_inflight(struct io_kiocb *req)
 	}
 }
 
-static inline void io_unprep_linked_timeout(struct io_kiocb *req)
-{
-	req->flags &= ~REQ_F_LINK_TIMEOUT;
-}
-
 static struct io_kiocb *__io_prep_linked_timeout(struct io_kiocb *req)
 {
 	if (WARN_ON_ONCE(!req->link))
@@ -7022,7 +7017,7 @@ issue_sqe:
 		switch (io_arm_poll_handler(req)) {
 		case IO_APOLL_READY:
 			if (linked_timeout)
-				io_unprep_linked_timeout(req);
+				io_queue_linked_timeout(linked_timeout);
 			goto issue_sqe;
 		case IO_APOLL_ABORTED:
 			/*
