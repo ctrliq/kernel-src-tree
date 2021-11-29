1b8a5ffdb912552b432f606204b17e8d10fe5eeb
Jeff Moyer <jmoyer@redhat.com>
2021-11-29 11:21:38 +0100

    dax: simplify the dax_device <-> gendisk association
    Bugzilla: https://bugzilla.redhat.com/2162211
    Conflicts: Minor context differences.
    
    commit fb08a1908cb119a4585611d91461ab6d27756b14
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Mon Nov 29 11:21:38 2021 +0100
    
        dax: simplify the dax_device <-> gendisk association
        
        Replace the dax_host_hash with an xarray indexed by the pointer value
        of the gendisk, and require explicitly calls from the block drivers that
        want to associate their gendisk with a dax_device.
        
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Acked-by: Mike Snitzer <snitzer@redhat.com>
        Reviewed-by: Darrick J. Wong <djwong@kernel.org>
        Link: https://lore.kernel.org/r/20211129102203.2243509-5-hch@lst.de
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -2008,6 +2008,7 @@ static void cleanup_mapped_device(struct mapped_device *md)
 	dm_free_md_mempools(md->mempools);
 
 	if (md->dax_dev) {
+		dax_remove_host(md->disk);
 		kill_dax(md->dax_dev);
 		put_dax(md->dax_dev);
 		md->dax_dev = NULL;
@@ -2116,12 +2117,13 @@ static struct mapped_device *alloc_dev(int minor)
 	sprintf(md->disk->disk_name, "dm-%d", minor);
 
 	if (IS_ENABLED(CONFIG_FS_DAX)) {
-		md->dax_dev = alloc_dax(md, md->disk->disk_name,
-					&dm_dax_ops, 0);
+		md->dax_dev = alloc_dax(md, &dm_dax_ops, 0);
 		if (IS_ERR(md->dax_dev)) {
 			md->dax_dev = NULL;
 			goto bad;
 		}
+		if (dax_add_host(md->dax_dev, md->disk))
+			goto bad;
 	}
 
 	format_dev_t(md->name, MKDEV(_major, minor));
