b702c0a5d3f639071d9cafdbe2d6ce4e9e7aa9bf
Rafael Aquini <aquini@redhat.com>
2021-11-29 11:37:11 -0500

    mm: ignore MAP_DENYWRITE in ksys_mmap_pgoff()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2028891
    
    This patch is a backport of the following upstream commit:
    commit 6128b3af2a5e42386aa7faf37609b57f39fb7d00
    Author: David Hildenbrand <david@redhat.com>
    Date:   Fri Apr 23 09:38:14 2021 +0200
    
        mm: ignore MAP_DENYWRITE in ksys_mmap_pgoff()
    
        Let's also remove masking off MAP_DENYWRITE from ksys_mmap_pgoff():
        the last in-tree occurrence of MAP_DENYWRITE is now in LEGACY_MAP_MASK,
        which accepts the flag e.g., for MAP_SHARED_VALIDATE; however, the flag
        is ignored throughout the kernel now.
    
        Add a comment to LEGACY_MAP_MASK stating that MAP_DENYWRITE is ignored.
    
        Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
        Acked-by: Christian König <christian.koenig@amd.com>
        Signed-off-by: David Hildenbrand <david@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1620,8 +1620,6 @@ unsigned long ksys_mmap_pgoff(unsigned long addr, unsigned long len,
 			return PTR_ERR(file);
 	}
 
-	flags &= ~MAP_DENYWRITE;
-
 	retval = vm_mmap_pgoff(file, addr, len, prot, flags, pgoff);
 out_fput:
 	if (file)
