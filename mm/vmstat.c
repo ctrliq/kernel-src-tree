1df11c0d0e7d1b1ec756c98ce4d294c507f93989
Rafael Aquini <aquini@redhat.com>
2021-11-29 11:38:10 -0500

    mm/vmstat: remove unneeded return value
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2028891
    
    This patch is a backport of the following upstream commit:
    commit 33090af97350cee9ea0e347301cef704bd5b0d8e
    Author: Miaohe Lin <linmiaohe@huawei.com>
    Date:   Thu Sep 2 15:01:08 2021 -0700
    
        mm/vmstat: remove unneeded return value
    
        The return value of pagetypeinfo_showfree and pagetypeinfo_showblockcount
        are unused now.  Remove them.
    
        Link: https://lkml.kernel.org/r/20210715122911.15700-4-linmiaohe@huawei.com
        Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
        Reviewed-by: David Hildenbrand <david@redhat.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1466,7 +1466,7 @@ static void pagetypeinfo_showfree_print(struct seq_file *m,
 }
 
 /* Print out the free pages at each order for each migatetype */
-static int pagetypeinfo_showfree(struct seq_file *m, void *arg)
+static void pagetypeinfo_showfree(struct seq_file *m, void *arg)
 {
 	int order;
 	pg_data_t *pgdat = (pg_data_t *)arg;
@@ -1478,8 +1478,6 @@ static int pagetypeinfo_showfree(struct seq_file *m, void *arg)
 	seq_putc(m, '\n');
 
 	walk_zones_in_node(m, pgdat, true, false, pagetypeinfo_showfree_print);
-
-	return 0;
 }
 
 static void pagetypeinfo_showblockcount_print(struct seq_file *m,
@@ -1515,7 +1513,7 @@ static void pagetypeinfo_showblockcount_print(struct seq_file *m,
 }
 
 /* Print out the number of pageblocks for each migratetype */
-static int pagetypeinfo_showblockcount(struct seq_file *m, void *arg)
+static void pagetypeinfo_showblockcount(struct seq_file *m, void *arg)
 {
 	int mtype;
 	pg_data_t *pgdat = (pg_data_t *)arg;
@@ -1526,8 +1524,6 @@ static int pagetypeinfo_showblockcount(struct seq_file *m, void *arg)
 	seq_putc(m, '\n');
 	walk_zones_in_node(m, pgdat, true, false,
 		pagetypeinfo_showblockcount_print);
-
-	return 0;
 }
 
 /*
