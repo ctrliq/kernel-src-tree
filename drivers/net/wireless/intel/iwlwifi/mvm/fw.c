69d09aa29c901c3858e076914054f63ecfccb9b5
Íñigo Huguet <ihuguet@redhat.com>
2021-12-22 13:46:09 +0100

    iwlwifi: mvm: read 6E enablement flags from DSM and pass to FW
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2033354
    Upstream Status: https://git.kernel.org/pub/scm/linux/kernel/git/kvalo/wireless-drivers.git
    
    commit 698b166ed3464e1604a0e6a3e23cc1b529a5adc1
    Author: Luca Coelho <luciano.coelho@intel.com>
    Date:   Sun Oct 24 18:20:28 2021 +0300
    
        iwlwifi: mvm: read 6E enablement flags from DSM and pass to FW
    
        We need to call a new DSM function and pass the values to the firmware
        in order to allow enablement of 6E support by the OEMs via ACPI.
    
        Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
        Link: https://lore.kernel.org/r/iwlwifi.20211024181719.2fa34d31383c.I6504005c60882c94e6e58f64cab4e42e6481ce08@changeid
        Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    
    Signed-off-by: Íñigo Huguet <ihuguet@redhat.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/fw.c
@@ -1141,7 +1141,6 @@ static void iwl_mvm_lari_cfg(struct iwl_mvm *mvm)
 				   &iwl_guid, &value);
 	if (!ret)
 		cmd.oem_11ax_allow_bitmap = cpu_to_le32(value);
-	/* apply more config masks here */
 
 	ret = iwl_acpi_get_dsm_u32((&mvm->fwrt)->dev, 0,
 				   DSM_FUNC_ENABLE_UNII4_CHAN,
@@ -1149,7 +1148,14 @@ static void iwl_mvm_lari_cfg(struct iwl_mvm *mvm)
 	if (!ret)
 		cmd.oem_unii4_allow_bitmap = cpu_to_le32(value);
 
+	ret = iwl_acpi_get_dsm_u32(mvm->fwrt.dev, 0,
+				   DSM_FUNC_ENABLE_6E,
+				   &iwl_guid, &value);
+	if (!ret)
+		cmd.oem_uhb_allow_bitmap = cpu_to_le32(value);
+
 	if (cmd.config_bitmap ||
+	    cmd.oem_uhb_allow_bitmap ||
 	    cmd.oem_11ax_allow_bitmap ||
 	    cmd.oem_unii4_allow_bitmap) {
 		size_t cmd_size;
@@ -1173,6 +1179,9 @@ static void iwl_mvm_lari_cfg(struct iwl_mvm *mvm)
 				"sending LARI_CONFIG_CHANGE, oem_unii4_allow_bitmap=0x%x, cmd_ver=%d\n",
 				le32_to_cpu(cmd.oem_unii4_allow_bitmap),
 				cmd_ver);
+		IWL_DEBUG_RADIO(mvm,
+				"sending LARI_CONFIG_CHANGE, oem_uhb_allow_bitmap=0x%x\n",
+				le32_to_cpu(cmd.oem_uhb_allow_bitmap));
 		ret = iwl_mvm_send_cmd_pdu(mvm,
 					   WIDE_ID(REGULATORY_AND_NVM_GROUP,
 						   LARI_CONFIG_CHANGE),
