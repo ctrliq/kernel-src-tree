c49f146c13c23ff00118069ddf0ee618f334cbec
Jonathan Toppins <jtoppins@redhat.com>
2022-01-10 11:16:54 -0500

    ice: remove ring_active from ice_ring
    This field is dead and driver is not making any use of it. Simply remove
    it.
    
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    Tested-by: Gurucharan G <gurucharanx.g@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit e93d1c37a85b7276506c99ec964a4094d9c1bcf6)
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2032582
    Tested: compile tested

--- drivers/net/ethernet/intel/ice/ice_lib.c
+++ drivers/net/ethernet/intel/ice/ice_lib.c
@@ -1296,7 +1296,6 @@ static int ice_vsi_alloc_rings(struct ice_vsi *vsi)
 
 		ring->q_index = i;
 		ring->reg_idx = vsi->txq_map[i];
-		ring->ring_active = false;
 		ring->vsi = vsi;
 		ring->tx_tstamps = &pf->ptp.port.tx;
 		ring->dev = dev;
@@ -1315,7 +1314,6 @@ static int ice_vsi_alloc_rings(struct ice_vsi *vsi)
 
 		ring->q_index = i;
 		ring->reg_idx = vsi->rxq_map[i];
-		ring->ring_active = false;
 		ring->vsi = vsi;
 		ring->netdev = vsi->netdev;
 		ring->dev = dev;
