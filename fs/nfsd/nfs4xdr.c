2a518bf59c9274d0ed165d38d40a1c9aa0789973
Scott Mayhew <smayhew@redhat.com>
2022-01-21 13:52:29 -0500

    SUNRPC: Change return value type of .pc_encode
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2049200
    
    commit 130e2054d4a652a2bd79fb1557ddcd19c053cb37
    Author: Chuck Lever <chuck.lever@oracle.com>
    Date:   Wed Oct 13 10:41:13 2021 -0400
    
        SUNRPC: Change return value type of .pc_encode
    
        Returning an undecorated integer is an age-old trope, but it's
        not clear (even to previous experts in this code) that the only
        valid return values are 1 and 0. These functions do not return
        a negative errno, rpc_stat value, or a positive length.
    
        Document there are only two valid return values by having
        .pc_encode return only true or false.
    
        Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
        Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    
    Signed-off-by: Scott Mayhew <smayhew@redhat.com>

--- fs/nfsd/nfs4xdr.c
+++ fs/nfsd/nfs4xdr.c
@@ -5429,7 +5429,7 @@ nfs4svc_decode_compoundargs(struct svc_rqst *rqstp, struct xdr_stream *xdr)
 	return nfsd4_decode_compound(args);
 }
 
-int
+bool
 nfs4svc_encode_compoundres(struct svc_rqst *rqstp, struct xdr_stream *xdr)
 {
 	struct nfsd4_compoundres *resp = rqstp->rq_resp;
@@ -5455,5 +5455,5 @@ nfs4svc_encode_compoundres(struct svc_rqst *rqstp, struct xdr_stream *xdr)
 	*p++ = htonl(resp->opcnt);
 
 	nfsd4_sequence_done(resp);
-	return 1;
+	return true;
 }
