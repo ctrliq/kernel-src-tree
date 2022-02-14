e20149756f9e6f9f7d15247ffdcdf6b4833c0b01
Nico Pache <npache@redhat.com>
2022-02-14 14:06:06 -0500

    kABI: Pad the mempolicy struct
    The mempolicy struct is effected by the kabi. Add 3 reserves for future
    kABI work.
    
    The final cacheline currently has 8 bytes being used. We could fit 7
    reserves, but pad with 3 to be conservative with memory.
    
    Upstream-status: RHEL-only
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2048328
    Signed-off-by: Nico Pache <npache@redhat.com>

--- include/linux/mempolicy.h
+++ include/linux/mempolicy.h
@@ -51,6 +51,9 @@ struct mempolicy {
 		nodemask_t cpuset_mems_allowed;	/* relative to these nodes */
 		nodemask_t user_nodemask;	/* nodemask passed by user */
 	} w;
+	RH_KABI_RESERVE(1)
+	RH_KABI_RESERVE(2)
+	RH_KABI_RESERVE(3)
 };
 
 /*
