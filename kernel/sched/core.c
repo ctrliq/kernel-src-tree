533662442c01839703e250406c326cd5fa312b61
Juri Lelli <juri.lelli@redhat.com>
2022-02-15 22:22:13 -0300

    sched: Redo delayed mm_struct & task struct deallocation.
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2023946
    
    commit 2f895b8835ea436c5dc2918849c13941b007ebce
    Author: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Date:   Fri Sep 24 19:04:03 2021 +0200
    
        sched: Redo delayed mm_struct & task struct deallocation.
    
        This is an all-on-commit containing the rewrite of the delayed
        mm_struct and stack deallocation.
    
        Rework by Thomask Gleixner.
    
        Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    cherry picked from commit 48ae0faa44fb27c486257b5679f8634b7223f1a7)
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4918,10 +4918,7 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 	 *   provided by mmdrop(),
 	 * - a sync_core for SYNC_CORE.
 	 */
-	/*
-	 * We use mmdrop_delayed() here so we don't have to do the
-	 * full __mmdrop() when we are the last user.
-	 */
+
 	if (mm) {
 		membarrier_mm_sync_core_before_usermode(mm);
 		mmdrop_sched(mm);
@@ -4929,6 +4926,13 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 	if (unlikely(prev_state == TASK_DEAD)) {
 		if (prev->sched_class->task_dead)
 			prev->sched_class->task_dead(prev);
+
+		/*
+		 * Release VMAP'ed task stack immediate for reuse. On RT
+		 * enabled kernels this is delayed for latency reasons.
+		 */
+		if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+			put_task_stack(prev);
 
 		put_task_struct_rcu_user(prev);
 	}
@@ -8835,7 +8839,6 @@ void sched_setnuma(struct task_struct *p, int nid)
 #endif /* CONFIG_NUMA_BALANCING */
 
 #ifdef CONFIG_HOTPLUG_CPU
-
 /*
  * Ensure that the idle task is using init_mm right before its CPU goes
  * offline.
