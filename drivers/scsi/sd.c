33c83fe95c73102108a1655b09b35e793f2cab41
Ewan D. Milne <emilne@redhat.com>
2022-04-05 16:11:03 -0400

    scsi: core: pm: Only runtime resume if necessary
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2071832
    Upstream Status: From upstream linux mainline
    
    The following query shows which drivers define callbacks that are called by
    the power management support code in the SCSI core (scsi_pm.c):
    
    $ git grep -nHEwA16 "$(echo $(git grep -h 'scsi_register_driver(&' |
          sed 's/.*&//;s/\..*//') | sed 's/ /|/g')" |
        grep '\.pm[[:blank:]]*=[[:blank:]]'
    drivers/scsi/sd.c-620-			.pm = &sd_pm_ops,
    drivers/scsi/sr.c-100-			.pm = &sr_pm_ops,
    drivers/scsi/ufs/ufshcd.c-9765-		.pm = &ufshcd_wl_pm_ops,
    
    Since unconditionally runtime resuming a device during system resume is not
    necessary, remove that code. Modify the SCSI disk (sd) driver such that it
    follows the same approach as the UFS driver, namely to skip system suspend
    and resume for devices that are runtime suspended. The CD-ROM code does not
    need to be updated since its PM callbacks do not affect the device power
    state.
    
    This patch has been tested as follows:
    
    [ shell 1 ]
    
    cd /sys/kernel/debug/tracing
    grep -E 'blk_(pre|post)_runtime|runtime_(suspend|resume)|autosuspend_delay|pm_runtime_(get|put)' available_filter_functions |
      while read a b; do echo "$a"; done |
      grep -v __pm_runtime_resume >set_ftrace_filter
    echo function > current_tracer
    echo 1 > tracing_on
    cat trace_pipe
    
    [ shell 2 ]
    
    cd /sys/block/sr0
     # Increase the event poll interval to make it easier to derive from the
     # tracing output whether runtime power actions are the result of sg_inq.
    echo 30000 > events_poll_msecs
    cd device/power
     # Enable runtime power management.
    echo auto > control
    echo 1000 > autosuspend_delay_ms
    sleep 1
     # Verify in shell 1 that sr0 has been runtime suspended
    sg_inq /dev/sr0
    eject /dev/sr0
    sg_inq /dev/sr0
     # Disable runtime power management.
    echo on > control
    
    cd /sys/block/sda/device/power
    echo auto > control
    echo 1000 > autosuspend_delay_ms
    sleep 1
     # Verify in shell 1 that sr0 has been runtime suspended
    sg_inq /dev/sda
    
    Link: https://lore.kernel.org/r/20211006215453.3318929-4-bvanassche@acm.org
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Hannes Reinecke <hare@suse.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Martin Kepplinger <martin.kepplinger@puri.sm>
    Tested-by: Martin Kepplinger <martin.kepplinger@puri.sm>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 9131bff6a9f1d4c6ab2d4673974a9659d8d24dc7)
    Signed-off-by: Ewan D. Milne <emilne@redhat.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3710,6 +3710,9 @@ static int sd_suspend_common(struct device *dev, bool ignore_stop_errors)
 
 static int sd_suspend_system(struct device *dev)
 {
+	if (pm_runtime_suspended(dev))
+		return 0;
+
 	return sd_suspend_common(dev, true);
 }
 
@@ -3738,6 +3741,9 @@ static int sd_resume(struct device *dev)
 
 static int sd_resume_system(struct device *dev)
 {
+	if (pm_runtime_suspended(dev))
+		return 0;
+
 	return sd_resume(dev);
 }
 
