785ffd41c12fad533a073956c02b6407e08b66cf
Aristeu Rozanski <arozansk@redhat.com>
2022-04-07 09:58:24 -0400

    mm: Add folio_raw_mapping()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2019485
    Tested: ran Rafael's set of sanity tests, other than known issues, seems ok
    
    commit 646010009d3541b8cb4f803dcb4b8d0da2f22579
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Fri May 7 11:17:34 2021 -0400
    
        mm: Add folio_raw_mapping()
    
        Convert __page_rmapping to folio_raw_mapping and move it to mm/internal.h.
        It's only a couple of instructions (load and mask), so it's definitely
        going to be cheaper to inline it than call it.  Leave page_rmapping
        out of line.  Change page_anon_vma() to not call folio_raw_mapping() --
        it's more efficient to do the subtraction than the mask.
    
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Reviewed-by: David Howells <dhowells@redhat.com>
        Acked-by: Vlastimil Babka <vbabka@suse.cz>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- mm/util.c
+++ mm/util.c
@@ -706,21 +706,10 @@ void *vcalloc(size_t n, size_t size)
 }
 EXPORT_SYMBOL(vcalloc);
 
-static inline void *__page_rmapping(struct page *page)
-{
-	unsigned long mapping;
-
-	mapping = (unsigned long)page->mapping;
-	mapping &= ~PAGE_MAPPING_FLAGS;
-
-	return (void *)mapping;
-}
-
 /* Neutral page->mapping pointer to address_space or anon_vma or other */
 void *page_rmapping(struct page *page)
 {
-	page = compound_head(page);
-	return __page_rmapping(page);
+	return folio_raw_mapping(page_folio(page));
 }
 
 /**
@@ -751,13 +740,12 @@ EXPORT_SYMBOL(folio_mapped);
 
 struct anon_vma *page_anon_vma(struct page *page)
 {
-	unsigned long mapping;
+	struct folio *folio = page_folio(page);
+	unsigned long mapping = (unsigned long)folio->mapping;
 
-	page = compound_head(page);
-	mapping = (unsigned long)page->mapping;
 	if ((mapping & PAGE_MAPPING_FLAGS) != PAGE_MAPPING_ANON)
 		return NULL;
-	return __page_rmapping(page);
+	return (void *)(mapping - PAGE_MAPPING_ANON);
 }
 
 /**
