9184eb4496bf5f7e59ee162fe81ce4a0247ba950
Tomas Henzl <thenzl@redhat.com>
2022-04-09 20:04:15 +0200

    ata: libata: use min() to make code cleaner
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2042790
    
    Use min() in order to make code cleaner.
    
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: Changcheng Deng <deng.changcheng@zte.com.cn>
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>
    (cherry picked from commit da29947057950232f4ad8e0e118d2d5002daaf2b)
    Signed-off-by: Tomas Henzl <thenzl@redhat.com>

--- drivers/ata/libata-scsi.c
+++ drivers/ata/libata-scsi.c
@@ -3582,10 +3582,7 @@ static int ata_mselect_caching(struct ata_queued_cmd *qc,
 	 */
 
 	if (len != CACHE_MPAGE_LEN - 2) {
-		if (len < CACHE_MPAGE_LEN - 2)
-			*fp = len;
-		else
-			*fp = CACHE_MPAGE_LEN - 2;
+		*fp = min(len, CACHE_MPAGE_LEN - 2);
 		return -EINVAL;
 	}
 
@@ -3638,10 +3635,7 @@ static int ata_mselect_control(struct ata_queued_cmd *qc,
 	 */
 
 	if (len != CONTROL_MPAGE_LEN - 2) {
-		if (len < CONTROL_MPAGE_LEN - 2)
-			*fp = len;
-		else
-			*fp = CONTROL_MPAGE_LEN - 2;
+		*fp = min(len, CONTROL_MPAGE_LEN - 2);
 		return -EINVAL;
 	}
 
