a5243e0995be5877a378cf4671ce8bfec9a55701
Ming Lei <ming.lei@redhat.com>
2022-04-11 11:44:11 +0800

    scsi: bsg: Move bsg_scsi_ops to drivers/scsi/
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2066297
    Conflicts: include header for avoiding to break building
    
    commit 78011042684dfbb50f7060f4623793f7a5c74a01
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Sat Jul 24 09:20:23 2021 +0200
    
        scsi: bsg: Move bsg_scsi_ops to drivers/scsi/
    
        Move the SCSI-specific bsg code in the SCSI midlayer instead of in the
        common bsg code.  This just keeps the common bsg code block/ and also
        allows building it as a module.
    
        Link: https://lore.kernel.org/r/20210724072033.1284840-15-hch@lst.de
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- include/linux/bsg.h
+++ include/linux/bsg.h
@@ -3,10 +3,12 @@
 #define _LINUX_BSG_H
 
 #include <uapi/linux/bsg.h>
+#include <linux/rh_kabi.h>
 
 struct request;
+struct request_queue;
 
-#ifdef CONFIG_BLK_DEV_BSG
+#ifdef CONFIG_BLK_DEV_BSG_COMMON
 struct bsg_ops {
 	int	(*check_proto)(struct sg_io_v4 *hdr);
 	int	(*fill_hdr)(struct request *rq, struct sg_io_v4 *hdr,
@@ -30,16 +32,10 @@ struct bsg_class_device {
 
 int bsg_register_queue(struct request_queue *q, struct device *parent,
 		const char *name, const struct bsg_ops *ops);
-int bsg_scsi_register_queue(struct request_queue *q, struct device *parent);
 void bsg_unregister_queue(struct request_queue *q);
 #else
-static inline int bsg_scsi_register_queue(struct request_queue *q,
-		struct device *parent)
-{
-	return 0;
-}
 static inline void bsg_unregister_queue(struct request_queue *q)
 {
 }
-#endif /* CONFIG_BLK_DEV_BSG */
+#endif /* CONFIG_BLK_DEV_BSG_COMMON */
 #endif /* _LINUX_BSG_H */
