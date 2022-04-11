c83fd12294271ccd4f0d39a49a0faaeefa7f226e
Ming Lei <ming.lei@redhat.com>
2022-04-11 11:16:43 +0800

    scsi: scsi_ioctl: Remove scsi_cmd_blk_ioctl()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2066297
    
    commit fb1ba406c451045f1063ace70086b4645d4e9d54
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Sat Jul 24 09:20:18 2021 +0200
    
        scsi: scsi_ioctl: Remove scsi_cmd_blk_ioctl()
    
        Open code scsi_cmd_blk_ioctl() in its two callers.
    
        Link: https://lore.kernel.org/r/20210724072033.1284840-10-hch@lst.de
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -622,8 +622,6 @@ void submit_bio_noacct(struct bio *bio);
 extern int blk_lld_busy(struct request_queue *q);
 extern void blk_queue_split(struct bio **);
 extern int scsi_verify_blk_ioctl(struct block_device *, unsigned int);
-extern int scsi_cmd_blk_ioctl(struct block_device *, fmode_t,
-			      unsigned int, void __user *);
 extern int scsi_cmd_ioctl(struct request_queue *, struct gendisk *, fmode_t,
 			  unsigned int, void __user *);
 extern int sg_scsi_ioctl(struct request_queue *, struct gendisk *, fmode_t,
