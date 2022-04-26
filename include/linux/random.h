6478c02d7905ea96d28730c47f78ce6a157089a9
Prarit Bhargava <prarit@redhat.com>
2022-04-26 19:33:11 -0400

    random: do not export add_vmfork_randomness() unless needed
    Bugzilla: https://bugzilla.redhat.com/2079030
    
    commit a4107d34f960df99ca07fa8eb022425a804f59f3
    Author: Jason A. Donenfeld <Jason@zx2c4.com>
    Date:   Tue Mar 1 15:14:04 2022 +0100
    
        random: do not export add_vmfork_randomness() unless needed
    
        Since add_vmfork_randomness() is only called from vmgenid.o, we can
        guard it in CONFIG_VMGENID, similarly to how we do with
        add_disk_randomness() and CONFIG_BLOCK. If we ever have multiple things
        calling into add_vmfork_randomness(), we can add another shared Kconfig
        symbol for that, but for now, this is good enough. Even though
        add_vmfork_randomess() is a pretty small function, removing it means
        that there are only calls to crng_reseed(false) and none to
        crng_reseed(true), which means the compiler can constant propagate the
        false, removing branches from crng_reseed() and its descendants.
    
        Additionally, we don't even need the symbol to be exported if
        CONFIG_VMGENID is not a module, so conditionalize that too.
    
        Cc: Dominik Brodowski <linux@dominikbrodowski.net>
        Cc: Theodore Ts'o <tytso@mit.edu>
        Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- include/linux/random.h
+++ include/linux/random.h
@@ -36,7 +36,9 @@ static inline void add_latent_entropy(void) {}
 extern void add_input_randomness(unsigned int type, unsigned int code,
 				 unsigned int value) __latent_entropy;
 extern void add_interrupt_randomness(int irq) __latent_entropy;
+#if IS_ENABLED(CONFIG_VMGENID)
 extern void add_vmfork_randomness(const void *unique_vm_id, size_t size);
+#endif
 
 extern void get_random_bytes(void *buf, size_t nbytes);
 extern int wait_for_random_bytes(void);
