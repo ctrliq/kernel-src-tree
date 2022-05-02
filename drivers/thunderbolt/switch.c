bb77985161fd60a2338e685bbf96a02c81f971e9
Torez Smith <torez@redhat.com>
2022-05-02 12:31:29 -0400

    thunderbolt: Add authorized value to the KOBJ_CHANGE uevent
    Bugzilla: http://bugzilla.redhat.com/2061784
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=45014530
    Tested: HP ZBook 15 G5/80D5 Laptop
     TGL-U(QS) Laptop
    
    commit 1651d9e7810e79500b4940122e192b8aaeb2d63c
    Author: Rajat Jain <rajatja@google.com>
    Date:   Fri Jul 30 16:53:04 2021 -0700
    
        thunderbolt: Add authorized value to the KOBJ_CHANGE uevent
    
        For security reasons, we would like to monitor and track when the
        Thunderbolt devices are authorized and deauthorized (i.e. when the
        Thunderbolt sysfs "authorized" attribute changes). Currently the
        userspace gets a udev change notification when there is a change, but
        the state may have changed (again) by the time we look at the authorized
        attribute in sysfs. So an authorization event may go unnoticed. Thus
        make it easier by informing the actual change (new value of authorized
        attribute) in the udev change notification.
    
        The change is included as a key value "authorized=<val>" where <val>
        is the new value of sysfs attribute "authorized", and is described at
        Documentation/ABI/testing/sysfs-bus-thunderbolt under
        /sys/bus/thunderbolt/devices/.../authorized.
    
        Signed-off-by: Rajat Jain <rajatja@google.com>
        Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    
    Signed-off-by: Torez Smith <torez@redhat.com>

--- drivers/thunderbolt/switch.c
+++ drivers/thunderbolt/switch.c
@@ -1501,6 +1501,7 @@ static ssize_t authorized_show(struct device *dev,
 
 static int disapprove_switch(struct device *dev, void *not_used)
 {
+	char *envp[] = { "AUTHORIZED=0", NULL };
 	struct tb_switch *sw;
 
 	sw = tb_to_switch(dev);
@@ -1517,7 +1518,7 @@ static int disapprove_switch(struct device *dev, void *not_used)
 			return ret;
 
 		sw->authorized = 0;
-		kobject_uevent(&sw->dev.kobj, KOBJ_CHANGE);
+		kobject_uevent_env(&sw->dev.kobj, KOBJ_CHANGE, envp);
 	}
 
 	return 0;
@@ -1525,7 +1526,9 @@ static int disapprove_switch(struct device *dev, void *not_used)
 
 static int tb_switch_set_authorized(struct tb_switch *sw, unsigned int val)
 {
+	char envp_string[13];
 	int ret = -EINVAL;
+	char *envp[] = { envp_string, NULL };
 
 	if (!mutex_trylock(&sw->tb->lock))
 		return restart_syscall();
@@ -1562,8 +1565,12 @@ static int tb_switch_set_authorized(struct tb_switch *sw, unsigned int val)
 
 	if (!ret) {
 		sw->authorized = val;
-		/* Notify status change to the userspace */
-		kobject_uevent(&sw->dev.kobj, KOBJ_CHANGE);
+		/*
+		 * Notify status change to the userspace, informing the new
+		 * value of /sys/bus/thunderbolt/devices/.../authorized.
+		 */
+		sprintf(envp_string, "AUTHORIZED=%u", sw->authorized);
+		kobject_uevent_env(&sw->dev.kobj, KOBJ_CHANGE, envp);
 	}
 
 unlock:
