e12fbbc61bac4bff4614bd9425fbc9e524989d62
Waiman Long <longman@redhat.com>
2022-05-18 13:39:16 -0400

    mm: unexport folio_memcg_{,un}lock
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2079537
    
    commit 913ffbdd99856184b4e8004f984d7432dcb990cd
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Wed, 10 Nov 2021 20:32:43 -0800
    
        mm: unexport folio_memcg_{,un}lock
    
        Patch series "unexport memcg locking helpers".
    
        Neither the old page-based nor the new folio-based memcg locking helpers
        are used in modular code at all, so drop the exports.
    
        This patch (of 2):
    
        folio_memcg_{,un}lock are only used in built-in core mm code.
    
        Link: https://lkml.kernel.org/r/20210820095815.445392-1-hch@lst.de
        Link: https://lkml.kernel.org/r/20210820095815.445392-2-hch@lst.de
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Cc: Johannes Weiner <hannes@cmpxchg.org>
        Cc: Michal Hocko <mhocko@kernel.org>
        Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2000,7 +2000,6 @@ again:
 	memcg->move_lock_task = current;
 	memcg->move_lock_flags = flags;
 }
-EXPORT_SYMBOL(folio_memcg_lock);
 
 void lock_page_memcg(struct page *page)
 {
@@ -2034,7 +2033,6 @@ void folio_memcg_unlock(struct folio *folio)
 {
 	__folio_memcg_unlock(folio_memcg(folio));
 }
-EXPORT_SYMBOL(folio_memcg_unlock);
 
 void unlock_page_memcg(struct page *page)
 {
