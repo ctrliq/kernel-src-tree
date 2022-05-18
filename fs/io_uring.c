9143d07c139066e60b0d9a2312f271038bfa7cef
Jeff Moyer <jmoyer@redhat.com>
2022-05-18 14:36:18 -0600

    io_uring: initialize io_buffer_list head when shared ring is unregistered
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2123490
    
    commit 1d0dbbfa282d9be57792e3b5827dc57b010181ee
    Author: Jens Axboe <axboe@kernel.dk>
    Date:   Wed May 18 14:36:18 2022 -0600
    
        io_uring: initialize io_buffer_list head when shared ring is unregistered
        
        We use ->buf_pages != 0 to tell if this is a shared buffer ring or a
        classic provided buffer group. If we unregister the shared ring and
        then attempt to use it, buf_pages is zero yet the classic list head
        isn't properly initialized. This causes io_buffer_select() to think
        that we have classic buffers available, but then we crash when we try
        and get one from the list.
        
        Just initialize the list if we unregister a shared buffer ring, leaving
        it in a sane state for either re-registration or for attempting to use
        it. And do the same for the initial setup from the classic path.
        
        Fixes: c7fb19428d67 ("io_uring: add support for ring mapped supplied buffers")
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -5592,6 +5592,8 @@ static int __io_remove_buffers(struct io_ring_ctx *ctx,
 		kvfree(bl->buf_pages);
 		bl->buf_pages = NULL;
 		bl->buf_nr_pages = 0;
+		/* make sure it's seen as empty */
+		INIT_LIST_HEAD(&bl->buf_list);
 		return i;
 	}
 
@@ -5779,6 +5781,7 @@ static int io_provide_buffers(struct io_kiocb *req, unsigned int issue_flags)
 			ret = -ENOMEM;
 			goto err;
 		}
+		INIT_LIST_HEAD(&bl->buf_list);
 		ret = io_buffer_add_list(ctx, bl, p->bgid);
 		if (ret) {
 			kfree(bl);
