ffe2973b7588bc86435c8d0b8a950ab1c7caea3b
Gopal Tiwari <gtiwari@redhat.com>
2022-06-08 14:03:41 +0530

    nvme: don't print verbose errors for internal passthrough requests
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2091823
    
    commit b42b6f4485e3f0970e11f73df6202eeaf9f53a3e
    Author: Chaitanya Kulkarni <kch@nvidia.com>
    Date:   Sun Apr 10 20:12:49 2022 -0700
    
        nvme: don't print verbose errors for internal passthrough requests
    
        Use the RQF_QUIET flag to skip the newly added verbose error reporting,
        and set the flag in __nvme_submit_sync_cmd, which is used for most
        internal passthrough requests where we do expect errors (e.g. due to
        probing for optional functionality).  This is similar to what the SCSI
        verbose error logging does.
    
        Signed-off-by: Chaitanya Kulkarni <kch@nvidia.com>
        Reviewed-by: Alan Adamson <alan.adamson@oracle.com>
        Reviewed-by: Keith Busch <kbusch@kernel.org>
        Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
        Tested-by: Alan Adamson <alan.adamson@oracle.com>
        Tested-by: Yi Zhang <yi.zhang@redhat.com>
        Signed-off-by: Christoph Hellwig <hch@lst.de>
    
    Signed-off-by: Gopal Tiwari <gtiwari@redhat.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -375,7 +375,7 @@ static inline void nvme_end_req(struct request *req)
 {
 	blk_status_t status = nvme_error_status(nvme_req(req)->status);
 
-	if (unlikely(nvme_req(req)->status != NVME_SC_SUCCESS))
+	if (unlikely(nvme_req(req)->status && !(req->rq_flags & RQF_QUIET)))
 		nvme_log_error(req);
 	nvme_end_req_zoned(req);
 	nvme_trace_bio_complete(req);
@@ -1140,6 +1140,7 @@ int __nvme_submit_sync_cmd(struct request_queue *q, struct nvme_command *cmd,
 			goto out;
 	}
 
+	req->rq_flags |= RQF_QUIET;
 	ret = nvme_execute_rq(NULL, req, at_head);
 	if (result && ret >= 0)
 		*result = nvme_req(req)->result;
