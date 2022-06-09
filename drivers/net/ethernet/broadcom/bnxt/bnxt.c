c8be8bcad0e83ddcbe0b0853027e24294531cb6a
Ken Cox <jkc@redhat.com>
2022-06-09 11:15:47 -0500

    bnxt_en: Fix incorrect multicast rx mask setting when not requested
    Bugzilla: http://bugzilla.redhat.com/2043783
    Bugzilla: http://bugzilla.redhat.com/2042819
    
    commit 8cdb15924252e27af16c4a8fe0fc606ce5fd04dc
    Author: Pavan Chebbi <pavan.chebbi@broadcom.com>
    Date:   Sun Feb 20 04:05:50 2022 -0500
    
        bnxt_en: Fix incorrect multicast rx mask setting when not requested
    
        We should setup multicast only when net_device flags explicitly
        has IFF_MULTICAST set. Otherwise we will incorrectly turn it on
        even when not asked.  Fix it by only passing the multicast table
        to the firmware if IFF_MULTICAST is set.
    
        Fixes: 7d2837dd7a32 ("bnxt_en: Setup multicast properly after resetting device.")
        Signed-off-by: Pavan Chebbi <pavan.chebbi@broadcom.com>
        Signed-off-by: Michael Chan <michael.chan@broadcom.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ken Cox <jkc@redhat.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -4746,8 +4746,10 @@ static int bnxt_hwrm_cfa_l2_set_rx_mask(struct bnxt *bp, u16 vnic_id)
 		return rc;
 
 	req->vnic_id = cpu_to_le32(vnic->fw_vnic_id);
-	req->num_mc_entries = cpu_to_le32(vnic->mc_list_count);
-	req->mc_tbl_addr = cpu_to_le64(vnic->mc_list_mapping);
+	if (vnic->rx_mask & CFA_L2_SET_RX_MASK_REQ_MASK_MCAST) {
+		req->num_mc_entries = cpu_to_le32(vnic->mc_list_count);
+		req->mc_tbl_addr = cpu_to_le64(vnic->mc_list_mapping);
+	}
 	req->mask = cpu_to_le32(vnic->rx_mask);
 	return hwrm_req_send_silent(bp, req);
 }
@@ -8650,7 +8652,7 @@ static int bnxt_init_chip(struct bnxt *bp, bool irq_re_init)
 	if (bp->dev->flags & IFF_ALLMULTI) {
 		vnic->rx_mask |= CFA_L2_SET_RX_MASK_REQ_MASK_ALL_MCAST;
 		vnic->mc_list_count = 0;
-	} else {
+	} else if (bp->dev->flags & IFF_MULTICAST) {
 		u32 mask = 0;
 
 		bnxt_mc_list_updated(bp, &mask);
@@ -10778,7 +10780,7 @@ static void bnxt_set_rx_mode(struct net_device *dev)
 	if (dev->flags & IFF_ALLMULTI) {
 		mask |= CFA_L2_SET_RX_MASK_REQ_MASK_ALL_MCAST;
 		vnic->mc_list_count = 0;
-	} else {
+	} else if (dev->flags & IFF_MULTICAST) {
 		mc_update = bnxt_mc_list_updated(bp, &mask);
 	}
 
@@ -10855,9 +10857,10 @@ skip_uc:
 	    !bnxt_promisc_ok(bp))
 		vnic->rx_mask &= ~CFA_L2_SET_RX_MASK_REQ_MASK_PROMISCUOUS;
 	rc = bnxt_hwrm_cfa_l2_set_rx_mask(bp, 0);
-	if (rc && vnic->mc_list_count) {
+	if (rc && (vnic->rx_mask & CFA_L2_SET_RX_MASK_REQ_MASK_MCAST)) {
 		netdev_info(bp->dev, "Failed setting MC filters rc: %d, turning on ALL_MCAST mode\n",
 			    rc);
+		vnic->rx_mask &= ~CFA_L2_SET_RX_MASK_REQ_MASK_MCAST;
 		vnic->rx_mask |= CFA_L2_SET_RX_MASK_REQ_MASK_ALL_MCAST;
 		vnic->mc_list_count = 0;
 		rc = bnxt_hwrm_cfa_l2_set_rx_mask(bp, 0);
