4230639d07dfd7e2599ce551fd94f604903511d0
Ming Lei <ming.lei@redhat.com>
2022-06-18 20:50:58 +0800

    dm: fix bio length of empty flush
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2083917
    
    commit 92b914e29af3e99589f2d2876616c0b534892ed4
    Author: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
    Date:   Fri Apr 15 17:45:13 2022 +0900
    
        dm: fix bio length of empty flush
    
        The commit 92986f6b4c8a ("dm: use bio_clone_fast in alloc_io/alloc_tio")
        removed bio_clone_fast() call from alloc_tio() when ci->io->tio is
        available. In this case, ci->bio is not copied to ci->io->tio.clone.
        This is fine since init_clone_info() sets same values to ci->bio and
        ci->io->tio.clone.
    
        However, when incoming bios have REQ_PREFLUSH flag, __send_empty_flush()
        prepares a zero length bio on stack and set it to ci->bio. At this time,
        ci->io->tio.clone still keeps non-zero length. When alloc_tio() chooses
        this ci->io->tio.clone as the bio to map, it is passed to targets as
        non-empty flush bio. It causes bio length check failure in dm-zoned and
        unexpected operation such as dm_accept_partial_bio() call.
    
        To avoid the non-empty flush bio, set zero length to ci->io->tio.clone
        in __send_empty_flush().
    
        Fixes: 92986f6b4c8a ("dm: use bio_clone_fast in alloc_io/alloc_tio")
        Signed-off-by: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
        Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1359,6 +1359,7 @@ static int __send_empty_flush(struct clone_info *ci)
 
 	ci->bio = &flush_bio;
 	ci->sector_count = 0;
+	ci->io->tio.clone.bi_iter.bi_size = 0;
 
 	BUG_ON(bio_has_data(ci->bio));
 	while ((ti = dm_table_get_target(ci->map, target_nr++)))
