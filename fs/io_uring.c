dcda81514cb5f8748b573dee119d9eb576419fa7
Jeff Moyer <jmoyer@redhat.com>
2022-06-21 13:25:06 +0100

    io_uring: fix req->apoll_events
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2123490
    
    commit aacf2f9f382c91df73f33317e28a4c34c8038986
    Author: Pavel Begunkov <asml.silence@gmail.com>
    Date:   Tue Jun 21 13:25:06 2022 +0100
    
        io_uring: fix req->apoll_events
        
        apoll_events should be set once in the beginning of poll arming just as
        poll->events and not change after. However, currently io_uring resets it
        on each __io_poll_execute() for no clear reason. There is also a place
        in __io_arm_poll_handler() where we add EPOLLONESHOT to downgrade a
        multishot, but forget to do the same thing with ->apoll_events, which is
        buggy.
        
        Fixes: 81459350d581e ("io_uring: cache req->apoll->events in req->cflags")
        Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
        Reviewed-by: Hao Xu <howeyxu@tencent.com>
        Link: https://lore.kernel.org/r/0aef40399ba75b1a4d2c2e85e6e8fd93c02fc6e4.1655814213.git.asml.silence@gmail.com
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -6951,7 +6951,8 @@ static void io_apoll_task_func(struct io_kiocb *req, bool *locked)
 		io_req_complete_failed(req, ret);
 }
 
-static void __io_poll_execute(struct io_kiocb *req, int mask, __poll_t events)
+static void __io_poll_execute(struct io_kiocb *req, int mask,
+			      __poll_t __maybe_unused events)
 {
 	req->cqe.res = mask;
 	/*
@@ -6960,7 +6961,6 @@ static void __io_poll_execute(struct io_kiocb *req, int mask, __poll_t events)
 	 * CPU. We want to avoid pulling in req->apoll->events for that
 	 * case.
 	 */
-	req->apoll_events = events;
 	if (req->opcode == IORING_OP_POLL_ADD)
 		req->io_task_work.func = io_poll_task_func;
 	else
@@ -7111,6 +7111,8 @@ static int __io_arm_poll_handler(struct io_kiocb *req,
 	io_init_poll_iocb(poll, mask, io_poll_wake);
 	poll->file = req->file;
 
+	req->apoll_events = poll->events;
+
 	ipt->pt._key = mask;
 	ipt->req = req;
 	ipt->error = 0;
@@ -7141,8 +7143,10 @@ static int __io_arm_poll_handler(struct io_kiocb *req,
 
 	if (mask) {
 		/* can't multishot if failed, just queue the event we've got */
-		if (unlikely(ipt->error || !ipt->nr_entries))
+		if (unlikely(ipt->error || !ipt->nr_entries)) {
 			poll->events |= EPOLLONESHOT;
+			req->apoll_events |= EPOLLONESHOT;
+		}
 		__io_poll_execute(req, mask, poll->events);
 		return 0;
 	}
@@ -7389,7 +7393,7 @@ static int io_poll_add_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe
 		return -EINVAL;
 
 	io_req_set_refcount(req);
-	req->apoll_events = poll->events = io_poll_parse_events(sqe, flags);
+	poll->events = io_poll_parse_events(sqe, flags);
 	return 0;
 }
 
