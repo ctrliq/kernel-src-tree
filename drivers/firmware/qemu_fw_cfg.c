459a47dcf5c390498f9ab290a1bcf4ecc5eba03e
Al Stone <ahs3@redhat.com>
2022-07-09 12:30:47 -0600

    qemu_fw_cfg: use default_groups in kobj_type
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2071830
    Depends: https://bugzilla.redhat.com/show_bug.cgi?id=2071840
    Tested: This is one of a series of patch sets to enable Arm SystemReady IR
     support in the kernel for NXP i.MX8 platforms.  At this stage, this
     has been tested by ensuring we can survive the CI/CD loop -- i.e.,
     that we have not broken anything else, and a simple boot test.  When
     sufficient drivers have been brought in for i.MX8M, we will be able
     to run further tests.
    
    commit ad8a5d1d2f577843dba14e4727c86e952ffbd5f1
    Author: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Date:   Wed Jan 5 19:31:33 2022 +0100
    
        qemu_fw_cfg: use default_groups in kobj_type
    
        There are currently 2 ways to create a set of sysfs files for a
        kobj_type, through the default_attrs field, and the default_groups
        field.  Move the firmware qemu_fw_cfg sysfs code to use default_groups
        field which has been the preferred way since aa30f47cf666 ("kobject: Add
        support for default attribute groups to kobj_type") so that we can soon
        get rid of the obsolete default_attrs field.
    
        Cc: Gabriel Somlo <somlo@cmu.edu>
        Cc: "Michael S. Tsirkin" <mst@redhat.com>
        Cc: qemu-devel@nongnu.org
        Acked-by: Michael S. Tsirkin <mst@redhat.com>
        Link: https://lore.kernel.org/r/20220105183133.2812848-1-gregkh@linuxfoundation.org
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        (cherry picked from commit ad8a5d1d2f577843dba14e4727c86e952ffbd5f1)
    
    Signed-off-by: Al Stone <ahs3@redhat.com>

--- drivers/firmware/qemu_fw_cfg.c
+++ drivers/firmware/qemu_fw_cfg.c
@@ -394,7 +394,7 @@ static void fw_cfg_sysfs_cache_cleanup(void)
 	}
 }
 
-/* default_attrs: per-entry attributes and show methods */
+/* per-entry attributes and show methods */
 
 #define FW_CFG_SYSFS_ATTR(_attr) \
 struct fw_cfg_sysfs_attribute fw_cfg_sysfs_attr_##_attr = { \
@@ -427,6 +427,7 @@ static struct attribute *fw_cfg_sysfs_entry_attrs[] = {
 	&fw_cfg_sysfs_attr_name.attr,
 	NULL,
 };
+ATTRIBUTE_GROUPS(fw_cfg_sysfs_entry);
 
 /* sysfs_ops: find fw_cfg_[entry, attribute] and call appropriate show method */
 static ssize_t fw_cfg_sysfs_attr_show(struct kobject *kobj, struct attribute *a,
@@ -452,7 +453,7 @@ static void fw_cfg_sysfs_release_entry(struct kobject *kobj)
 
 /* kobj_type: ties together all properties required to register an entry */
 static struct kobj_type fw_cfg_sysfs_entry_ktype = {
-	.default_attrs = fw_cfg_sysfs_entry_attrs,
+	.default_groups = fw_cfg_sysfs_entry_groups,
 	.sysfs_ops = &fw_cfg_sysfs_attr_ops,
 	.release = fw_cfg_sysfs_release_entry,
 };
