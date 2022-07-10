101cf0898698b047d3f05b8bbf7bf46b117211c8
Aristeu Rozanski <arozansk@redhat.com>
2022-07-10 10:44:06 -0400

    mm: Add DEFINE_PAGE_VMA_WALK and DEFINE_FOLIO_VMA_WALK
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2083861
    Tested: by me with multiple test suites
    
    commit eed05e54d275b3cfc5d8c79843c5276a5878e94a
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Thu Feb 3 09:06:08 2022 -0500
    
        mm: Add DEFINE_PAGE_VMA_WALK and DEFINE_FOLIO_VMA_WALK
    
        Instead of declaring a struct page_vma_mapped_walk directly,
        use these helpers to allow us to transition to a PFN approach in the
        following patches.
    
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -773,11 +773,7 @@ static bool page_referenced_one(struct page *page, struct vm_area_struct *vma,
 			unsigned long address, void *arg)
 {
 	struct page_referenced_arg *pra = arg;
-	struct page_vma_mapped_walk pvmw = {
-		.page = page,
-		.vma = vma,
-		.address = address,
-	};
+	DEFINE_PAGE_VMA_WALK(pvmw, page, vma, address, 0);
 	int referenced = 0;
 
 	while (page_vma_mapped_walk(&pvmw)) {
@@ -905,12 +901,7 @@ int page_referenced(struct page *page,
 static bool page_mkclean_one(struct page *page, struct vm_area_struct *vma,
 			    unsigned long address, void *arg)
 {
-	struct page_vma_mapped_walk pvmw = {
-		.page = page,
-		.vma = vma,
-		.address = address,
-		.flags = PVMW_SYNC,
-	};
+	DEFINE_PAGE_VMA_WALK(pvmw, page, vma, address, PVMW_SYNC);
 	struct mmu_notifier_range range;
 	int *cleaned = arg;
 
@@ -1412,11 +1403,7 @@ static bool try_to_unmap_one(struct page *page, struct vm_area_struct *vma,
 		     unsigned long address, void *arg)
 {
 	struct mm_struct *mm = vma->vm_mm;
-	struct page_vma_mapped_walk pvmw = {
-		.page = page,
-		.vma = vma,
-		.address = address,
-	};
+	DEFINE_PAGE_VMA_WALK(pvmw, page, vma, address, 0);
 	pte_t pteval;
 	struct page *subpage;
 	bool ret = true;
@@ -1707,11 +1694,7 @@ static bool try_to_migrate_one(struct page *page, struct vm_area_struct *vma,
 		     unsigned long address, void *arg)
 {
 	struct mm_struct *mm = vma->vm_mm;
-	struct page_vma_mapped_walk pvmw = {
-		.page = page,
-		.vma = vma,
-		.address = address,
-	};
+	DEFINE_PAGE_VMA_WALK(pvmw, page, vma, address, 0);
 	pte_t pteval;
 	struct page *subpage;
 	bool ret = true;
@@ -1994,11 +1977,7 @@ static bool page_make_device_exclusive_one(struct page *page,
 		struct vm_area_struct *vma, unsigned long address, void *priv)
 {
 	struct mm_struct *mm = vma->vm_mm;
-	struct page_vma_mapped_walk pvmw = {
-		.page = page,
-		.vma = vma,
-		.address = address,
-	};
+	DEFINE_PAGE_VMA_WALK(pvmw, page, vma, address, 0);
 	struct make_exclusive_args *args = priv;
 	pte_t pteval;
 	struct page *subpage;
