249b0cbb1313f7c4df6adf16ca170925de17cadb
Maurizio Lombardi <mlombard@redhat.com>
2022-07-12 09:05:13 +0200

    scsi: target: pscsi: Set SCF_TREAT_READ_AS_NORMAL flag only if there is valid data
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2080216
    
    With tape devices, the SCF_TREAT_READ_AS_NORMAL flag is used by the target
    subsystem to mark commands which have both data to return as well as sense
    data. But with pscsi, SCF_TREAT_READ_AS_NORMAL can be set even if there is
    no data to return. The SCF_TREAT_READ_AS_NORMAL flag causes the target core
    to call iscsit data-in callbacks even if there is no data, which iscsit
    does not support. This results in iscsit going into an error state
    requiring recovery and being unable to complete the command to the
    initiator.
    
    This issue can be resolved by fixing pscsi to only set
    SCF_TREAT_READ_AS_NORMAL if there is valid data to return alongside the
    sense data.
    
    Link: https://lore.kernel.org/r/20220427183250.291881-1-djeffery@redhat.com
    Fixes: bd81372065fa ("scsi: target: transport should handle st FM/EOM/ILI reads")
    Reported-by: Scott Hamilton <scott.hamilton@atos.net>
    Tested-by: Laurence Oberman <loberman@redhat.com>
    Reviewed-by: Laurence Oberman <loberman@redhat.com>
    Signed-off-by: David Jeffery <djeffery@redhat.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 8be70a842f70c0fe8e00fd488b1966344fa10ff4)
    Signed-off-by: Maurizio Lombardi <mlombard@redhat.com>
    
    # Conflicts:
    #	drivers/target/target_core_pscsi.c
    (In the upstream kernel, scsi_req(req)->sense has been replaced by
    scmd->sense_buffer)

--- drivers/target/target_core_pscsi.c
+++ drivers/target/target_core_pscsi.c
@@ -588,7 +588,7 @@ static void pscsi_destroy_device(struct se_device *dev)
 }
 
 static void pscsi_complete_cmd(struct se_cmd *cmd, u8 scsi_status,
-			       unsigned char *req_sense)
+			       unsigned char *req_sense, int valid_data)
 {
 	struct pscsi_dev_virt *pdv = PSCSI_DEV(cmd->se_dev);
 	struct scsi_device *sd = pdv->pdv_sd;
@@ -681,7 +681,7 @@ after_mode_select:
 		 * back despite framework assumption that a
 		 * check condition means there is no data
 		 */
-		if (sd->type == TYPE_TAPE &&
+		if (sd->type == TYPE_TAPE && valid_data &&
 		    cmd->data_direction == DMA_FROM_DEVICE) {
 			/*
 			 * is sense data valid, fixed format,
@@ -1003,6 +1003,7 @@ static void pscsi_req_done(struct request *req, blk_status_t status)
 	struct se_cmd *cmd = req->end_io_data;
 	int result = scsi_req(req)->result;
 	enum sam_status scsi_status = result & 0xff;
+	int valid_data = cmd->data_length - scsi_req(req)->resid_len;
 	u8 *cdb = cmd->priv;
 
 	if (scsi_status != SAM_STAT_GOOD) {
@@ -1010,12 +1011,12 @@ static void pscsi_req_done(struct request *req, blk_status_t status)
 			" 0x%02x Result: 0x%08x\n", cmd, cdb[0], result);
 	}
 
-	pscsi_complete_cmd(cmd, scsi_status, scsi_req(req)->sense);
+	pscsi_complete_cmd(cmd, scsi_status, scsi_req(req)->sense, valid_data);
 
 	switch (host_byte(result)) {
 	case DID_OK:
 		target_complete_cmd_with_length(cmd, scsi_status,
-			cmd->data_length - scsi_req(req)->resid_len);
+			valid_data);
 		break;
 	default:
 		pr_debug("PSCSI Host Byte exception at cmd: %p CDB:"
