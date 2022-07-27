41339011073aa44860feb755250357ad7c12f495
Vladis Dronov <vdronov@redhat.com>
2022-07-27 15:41:05 +0200

    crypto: testmgr - disallow plain cbcmac(aes) and ghash in FIPS mode
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2107596
    Upstream Status: RHEL only
    
    cbcmac(aes) and ghash may be used only as a part of the ccm(aes) and
    gcm(aes) construction in FIPS mode. Mark plain cbcmac(aes) and ghash
    as non-FIPS-approved.
    
    Signed-off-by: Vladis Dronov <vdronov@redhat.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -4442,7 +4442,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 #endif
 		.alg = "cbcmac(aes)",
-		.fips_allowed = 1,
 		.test = alg_test_hash,
 		.suite = {
 			.hash = __VECS(aes_cbcmac_tv_template)
@@ -4971,7 +4970,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "ghash",
 		.test = alg_test_hash,
-		.fips_allowed = 1,
 		.suite = {
 			.hash = __VECS(ghash_tv_template)
 		}
