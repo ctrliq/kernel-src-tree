1ca80672a18ae3713eea8a11ca9f899a9eac94d0
Waiman Long <longman@redhat.com>
2022-07-29 11:56:00 -0400

    x86/ibt,kvm: Add ENDBR to fastops
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2090231
    Conflicts: A fuzz due to the presence of a later uptream commit
    	   fe83f5eae432 ("Fix SETcc emulation function offsets with
    	   SLS").
    
    commit 6649fa876da4c505548b8e8945a6fc48e62e427c
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Tue, 8 Mar 2022 16:30:28 +0100
    
        x86/ibt,kvm: Add ENDBR to fastops
    
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
        Link: https://lore.kernel.org/r/20220308154318.168850084@infradead.org
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -189,7 +189,7 @@
 #define X16(x...) X8(x), X8(x)
 
 #define NR_FASTOP (ilog2(sizeof(ulong)) + 1)
-#define FASTOP_SIZE 8
+#define FASTOP_SIZE (8 * (1 + HAS_KERNEL_IBT))
 
 struct opcode {
 	u64 flags;
@@ -311,7 +311,8 @@ static int fastop(struct x86_emulate_ctxt *ctxt, fastop_t fop);
 #define __FOP_FUNC(name) \
 	".align " __stringify(FASTOP_SIZE) " \n\t" \
 	".type " name ", @function \n\t" \
-	name ":\n\t"
+	name ":\n\t" \
+	ASM_ENDBR
 
 #define FOP_FUNC(name) \
 	__FOP_FUNC(#name)
@@ -448,6 +449,7 @@ static_assert(SETCC_LENGTH <= SETCC_ALIGN);
 	".align " __stringify(SETCC_ALIGN) " \n\t" \
 	".type " #op ", @function \n\t" \
 	#op ": \n\t" \
+	ASM_ENDBR \
 	#op " %al \n\t" \
 	__FOP_RET(#op)
 
