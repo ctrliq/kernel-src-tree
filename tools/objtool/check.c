343f3e3880b923be849aa81d422ac512fd2df86f
Waiman Long <longman@redhat.com>
2022-07-29 11:56:14 -0400

    x86: Add magic AMD return-thunk
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2090231
    CVE: CVE-2022-23816
    CVE: CVE-2022-23825
    CVE: CVE-2022-29900
    CVE: CVE-2022-29901
    Conflicts:
     1) Merge conflict in the add_jump_destinations() hunk of
        tools/objtool/check.c due to missing upstream commit 34c861e80647
        ("objtool: Fix sibling call detection in alternatives") and commit
        26ff604102c9 ("objtool: Don't set 'jump_dest' for sibling calls").
     2) Merge conflicts in arch/x86/entry/entry_64.S and
        arch/x86/entry/entry_64_compat.S due to missing upstream commit
        e8d61bdf0fdf ("x86/ibt,sev: Annotations").
     3) Merge conflict in the __x86_return_thunk hunk of
        arch/x86/lib/retpoline.S due to missing upstream commit c8c301abeae
        ("x86/ibt: Add ANNOTATE_NOENDBR").
    
    commit a149180fbcf336e97ce4eb2cdc13672727feb94d
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Tue, 14 Jun 2022 23:15:48 +0200
    
        x86: Add magic AMD return-thunk
    
        Note: needs to be in a section distinct from Retpolines such that the
        Retpoline RET substitution cannot possibly use immediate jumps.
    
        ORC unwinding for zen_untrain_ret() and __x86_return_thunk() is a
        little tricky but works due to the fact that zen_untrain_ret() doesn't
        have any stack ops and as such will emit a single ORC entry at the
        start (+0x3f).
    
        Meanwhile, unwinding an IP, including the __x86_return_thunk() one
        (+0x40) will search for the largest ORC entry smaller or equal to the
        IP, these will find the one ORC entry (+0x3f) and all works.
    
          [ Alexandre: SVM part. ]
          [ bp: Build fix, massages. ]
    
        Suggested-by: Andrew Cooper <Andrew.Cooper3@citrix.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Reviewed-by: Josh Poimboeuf <jpoimboe@kernel.org>
        Signed-off-by: Borislav Petkov <bp@suse.de>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -1222,7 +1222,7 @@ static void add_retpoline_call(struct objtool_file *file, struct instruction *in
 	annotate_call_site(file, insn, false);
 }
 
-static void add_return_call(struct objtool_file *file, struct instruction *insn)
+static void add_return_call(struct objtool_file *file, struct instruction *insn, bool add)
 {
 	/*
 	 * Return thunk tail calls are really just returns in disguise,
@@ -1231,7 +1231,8 @@ static void add_return_call(struct objtool_file *file, struct instruction *insn)
 	insn->type = INSN_RETURN;
 	insn->retpoline_safe = true;
 
-	list_add_tail(&insn->call_node, &file->return_thunk_list);
+	if (add)
+		list_add_tail(&insn->call_node, &file->return_thunk_list);
 }
 
 /*
@@ -1259,7 +1260,7 @@ static int add_jump_destinations(struct objtool_file *file)
 			add_retpoline_call(file, insn);
 			continue;
 		} else if (reloc->sym->return_thunk) {
-			add_return_call(file, insn);
+			add_return_call(file, insn, true);
 			continue;
 		} else if (insn->func) {
 			/* internal or external sibling call (with reloc) */
@@ -1276,6 +1277,20 @@ static int add_jump_destinations(struct objtool_file *file)
 
 		insn->jump_dest = find_insn(file, dest_sec, dest_off);
 		if (!insn->jump_dest) {
+			struct symbol *sym = find_symbol_by_offset(dest_sec, dest_off);
+
+			/*
+			 * This is a special case for zen_untrain_ret().
+			 * It jumps to __x86_return_thunk(), but objtool
+			 * can't find the thunk's starting RET
+			 * instruction, because the RET is also in the
+			 * middle of another instruction.  Objtool only
+			 * knows about the outer instruction.
+			 */
+			if (sym && sym->return_thunk) {
+				add_return_call(file, insn, false);
+				continue;
+			}
 
 			/*
 			 * This is a special case where an alt instruction
