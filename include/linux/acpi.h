13d222724b29b3fb535f5f154c10e6bb3bda622a
Mark Langsdorf <mlangsdo@redhat.com>
2022-08-16 16:15:39 -0400

    PCI/ACPI: add a helper for retrieving _OSC Control DWORDs
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2122317
    
    commit cc10eee95204579fcd66fd5965073fdcbf629676
    Author: Vishal Verma <vishal.l.verma@intel.com>
    Date: Wed, 13 Apr 2022 01:36:16 -0600
    
    During _OSC negotiation, when the 'Control' DWORD is needed from the
    result buffer after running _OSC, a couple of places performed manual
    pointer arithmetic to offset into the right spot in the raw buffer.
    Add a acpi_osc_ctx_get_pci_control() helper to use the #define'd
    DWORD offsets to fetch the DWORDs needed from @acpi_osc_context, and
    replace the above instances of the open-coded arithmetic.
    
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Suggested-by: Davidlohr Bueso <dave@stgolabs.net>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Davidlohr Bueso <dave@stgolabs.net>
    Reviewed by: Adam Manzanares <a.manzanares@samsung.com>
    Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>
    Link: https://lore.kernel.org/r/20220413073618.291335-2-vishal.l.verma@intel.com
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -605,6 +605,13 @@ extern u32 osc_sb_native_usb4_control;
 #define OSC_PCI_EXPRESS_LTR_CONTROL		0x00000020
 #define OSC_PCI_EXPRESS_DPC_CONTROL		0x00000080
 
+static inline u32 acpi_osc_ctx_get_pci_control(struct acpi_osc_context *context)
+{
+	u32 *ret = context->ret.pointer;
+
+	return ret[OSC_CONTROL_DWORD];
+}
+
 #define ACPI_GSB_ACCESS_ATTRIB_QUICK		0x00000002
 #define ACPI_GSB_ACCESS_ATTRIB_SEND_RCV         0x00000004
 #define ACPI_GSB_ACCESS_ATTRIB_BYTE		0x00000006
@@ -990,6 +997,12 @@ static inline struct acpi_device *acpi_resource_consumer(struct resource *res)
 static inline int acpi_get_local_address(acpi_handle handle, u32 *addr)
 {
 	return -ENODEV;
+}
+
+struct acpi_osc_context;
+static inline u32 acpi_osc_ctx_get_pci_control(struct acpi_osc_context *context)
+{
+	return 0;
 }
 
 #endif	/* !CONFIG_ACPI */
