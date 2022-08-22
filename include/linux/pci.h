baa16b4be25ab9fb74c491ea9fc38f409ec95cae
Myron Stowe <mstowe@redhat.com>
2022-08-22 10:55:14 -0600

    PCI/MSI: Move msi_lock to struct pci_dev
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2151246
    Upstream Status: cd119b09a87d8beb50356d8c5c6aa42d89c44eb7
    
    commit cd119b09a87d8beb50356d8c5c6aa42d89c44eb7
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Mon Dec 6 23:27:56 2021 +0100
    
        PCI/MSI: Move msi_lock to struct pci_dev
    
        It's only required for PCI/MSI. So no point in having it in every struct
        device.
    
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
        Acked-by: Bjorn Helgaas <bhelgaas@google.com>
        Link: https://lore.kernel.org/r/20211206210224.925241961@linutronix.de
    
    Signed-off-by: Myron Stowe <mstowe@redhat.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -486,6 +486,7 @@ struct pci_dev {
 #endif
 #ifdef CONFIG_PCI_MSI
 	void __iomem	*msix_base;
+	raw_spinlock_t	msi_lock;
 	const struct attribute_group **msi_irq_groups;
 #endif
 	RH_KABI_EXCLUDE(struct pci_vpd	vpd);
