8ddbe037e14aa1003dbcf5896b74d0427acc0d1a
Prarit Bhargava <prarit@redhat.com>
2022-08-29 13:39:43 -0400

    Revert "redhat/scripts: Update merge-subtrees.sh with new subtree location"
    Bugzilla: INTERNAL
    Upstream Status: RHEL only.
    
    Backout all centos-stream-9 redhat/ commits.
    
    This reverts commit ab7632530cb06d18b3a9aa2fde7d9f1a023526d9.
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- redhat/scripts/merge-subtrees.sh
+++ redhat/scripts/merge-subtrees.sh
@@ -3,7 +3,7 @@
 # however, given the wide variance of git versions we need to include it.
 
 entries="
-	redhat/rhdocs git@gitlab.com:redhat/centos-stream/src/kernel/documentation.git
+	redhat/rhdocs git://git.host.prod.eng.bos.redhat.com/rhkernel-docs
 	"
 
 echo "$entries" | while read -r name url; do
