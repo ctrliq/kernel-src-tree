b2f9142dad8e4dbe5e5586a421b509d573169cc8
Vitaly Kuznetsov <vkuznets@redhat.com>
2022-09-08 11:58:46 +0200

    KVM: VMX: Enable Notify VM exit
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2119111
    
    commit 2f4073e08f4cc5a41e35d777c240aaadd0257051
    Author: Tao Xu <tao3.xu@intel.com>
    Date:   Tue May 24 21:56:24 2022 +0800
    
        KVM: VMX: Enable Notify VM exit
    
        There are cases that malicious virtual machines can cause CPU stuck (due
        to event windows don't open up), e.g., infinite loop in microcode when
        nested #AC (CVE-2015-5307). No event window means no event (NMI, SMI and
        IRQ) can be delivered. It leads the CPU to be unavailable to host or
        other VMs.
    
        VMM can enable notify VM exit that a VM exit generated if no event
        window occurs in VM non-root mode for a specified amount of time (notify
        window).
    
        Feature enabling:
        - The new vmcs field SECONDARY_EXEC_NOTIFY_VM_EXITING is introduced to
          enable this feature. VMM can set NOTIFY_WINDOW vmcs field to adjust
          the expected notify window.
        - Add a new KVM capability KVM_CAP_X86_NOTIFY_VMEXIT so that user space
          can query and enable this feature in per-VM scope. The argument is a
          64bit value: bits 63:32 are used for notify window, and bits 31:0 are
          for flags. Current supported flags:
          - KVM_X86_NOTIFY_VMEXIT_ENABLED: enable the feature with the notify
            window provided.
          - KVM_X86_NOTIFY_VMEXIT_USER: exit to userspace once the exits happen.
        - It's safe to even set notify window to zero since an internal hardware
          threshold is added to vmcs.notify_window.
    
        VM exit handling:
        - Introduce a vcpu state notify_window_exits to records the count of
          notify VM exits and expose it through the debugfs.
        - Notify VM exit can happen incident to delivery of a vector event.
          Allow it in KVM.
        - Exit to userspace unconditionally for handling when VM_CONTEXT_INVALID
          bit is set.
    
        Nested handling
        - Nested notify VM exits are not supported yet. Keep the same notify
          window control in vmcs02 as vmcs01, so that L1 can't escape the
          restriction of notify VM exits through launching L2 VM.
    
        Notify VM exit is defined in latest Intel Architecture Instruction Set
        Extensions Programming Reference, chapter 9.2.
    
        Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
        Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
        Signed-off-by: Tao Xu <tao3.xu@intel.com>
        Co-developed-by: Chenyi Qiang <chenyi.qiang@intel.com>
        Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>
        Message-Id: <20220524135624.22988-5-chenyi.qiang@intel.com>
        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Conflicts:
    	arch/x86/kvm/x86.c (context)
    	Documentation/virt/kvm/api.rst
    	include/uapi/linux/kvm.h (context, missing some ARM and s390
    	caps)
    	Use '37' for KVM_EXIT_NOTIFY (see upstream merge 63f4b210414b6)
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -269,6 +269,7 @@ struct kvm_xen_exit {
 #define KVM_EXIT_AP_RESET_HOLD    32
 #define KVM_EXIT_X86_BUS_LOCK     33
 #define KVM_EXIT_XEN              34
+#define KVM_EXIT_NOTIFY           37
 
 /* For KVM_EXIT_INTERNAL_ERROR */
 /* Emulate instruction failed. */
@@ -489,6 +490,11 @@ struct kvm_run {
 		} msr;
 		/* KVM_EXIT_XEN */
 		struct kvm_xen_exit xen;
+		/* KVM_EXIT_NOTIFY */
+		struct {
+#define KVM_NOTIFY_CONTEXT_INVALID	(1 << 0)
+			__u32 flags;
+		} notify;
 		/* Fix the size of the union. */
 		char padding[256];
 	};
@@ -1150,6 +1156,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_SYSTEM_EVENT_DATA 215
 #define KVM_CAP_ARM_SYSTEM_SUSPEND 216
 #define KVM_CAP_X86_TRIPLE_FAULT_EVENT 218
+#define KVM_CAP_X86_NOTIFY_VMEXIT 219
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
@@ -2110,5 +2117,9 @@ struct kvm_stats_desc {
 
 /* Available with KVM_CAP_XSAVE2 */
 #define KVM_GET_XSAVE2		  _IOR(KVMIO,  0xcf, struct kvm_xsave)
+
+/* Available with KVM_CAP_X86_NOTIFY_VMEXIT */
+#define KVM_X86_NOTIFY_VMEXIT_ENABLED		(1ULL << 0)
+#define KVM_X86_NOTIFY_VMEXIT_USER		(1ULL << 1)
 
 #endif /* __LINUX_KVM_H */
