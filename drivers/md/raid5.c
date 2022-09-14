c08811c875c469c34b2fe286d0c844e88ad29278
Ming Lei <ming.lei@redhat.com>
2022-09-14 20:59:01 +0800

    block: remove the per-bio/request write hint
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2118511
    
    commit c75e707fe1aab32f1dc8e09845533b6542d9aaa9
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Fri Mar 4 18:55:56 2022 +0100
    
        block: remove the per-bio/request write hint
    
        With the NVMe support for this gone, there are no consumers of these hints
        left, so remove them.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Link: https://lore.kernel.org/r/20220304175556.407719-2-hch@lst.de
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -1222,9 +1222,6 @@ again:
 			bi->bi_io_vec[0].bv_len = RAID5_STRIPE_SIZE(conf);
 			bi->bi_io_vec[0].bv_offset = sh->dev[i].offset;
 			bi->bi_iter.bi_size = RAID5_STRIPE_SIZE(conf);
-			bi->bi_write_hint = sh->dev[i].write_hint;
-			if (!rrdev)
-				sh->dev[i].write_hint = RWH_WRITE_LIFE_NOT_SET;
 			/*
 			 * If this is discard request, set bi_vcnt 0. We don't
 			 * want to confuse SCSI because SCSI will replace payload
@@ -1275,8 +1272,6 @@ again:
 			rbi->bi_io_vec[0].bv_len = RAID5_STRIPE_SIZE(conf);
 			rbi->bi_io_vec[0].bv_offset = sh->dev[i].offset;
 			rbi->bi_iter.bi_size = RAID5_STRIPE_SIZE(conf);
-			rbi->bi_write_hint = sh->dev[i].write_hint;
-			sh->dev[i].write_hint = RWH_WRITE_LIFE_NOT_SET;
 			/*
 			 * If this is discard request, set bi_vcnt 0. We don't
 			 * want to confuse SCSI because SCSI will replace payload
@@ -3434,7 +3429,6 @@ static int add_stripe_bio(struct stripe_head *sh, struct bio *bi, int dd_idx,
 		(unsigned long long)sh->sector);
 
 	spin_lock_irq(&sh->stripe_lock);
-	sh->dev[dd_idx].write_hint = bi->bi_write_hint;
 	/* Don't allow new IO added to stripes in batch list */
 	if (sh->batch_head)
 		goto overlap;
--- fs/iomap/direct-io.c
+++ fs/iomap/direct-io.c
@@ -302,7 +302,6 @@ iomap_dio_bio_actor(struct inode *inode, loff_t pos, loff_t length,
 
 		bio = bio_alloc(iomap->bdev, nr_pages, bio_opf, GFP_KERNEL);
 		bio->bi_iter.bi_sector = iomap_sector(iomap, pos);
-		bio->bi_write_hint = dio->iocb->ki_hint;
 		bio->bi_ioprio = dio->iocb->ki_ioprio;
 		bio->bi_private = dio;
 		bio->bi_end_io = iomap_dio_bio_end_io;
