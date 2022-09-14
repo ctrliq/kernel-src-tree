49eb481183d38729e627893e537cd8c49bdf784c
Ming Lei <ming.lei@redhat.com>
2022-09-14 20:59:08 +0800

    block: simplify disk shutdown
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2118511
    Conflicts: drop change on drivers/nvme/host/apple.c and
    drivers/scsi/ufs/, both don't exit on rhel9.
    
    commit 6f8191fdf41d3a53cc1d63fe2234e812c55a0092
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Sun Jun 19 08:05:51 2022 +0200
    
        block: simplify disk shutdown
    
        Set the queue dying flag and call blk_mq_exit_queue from del_gendisk for
        all disks that do not have separately allocated queues, and thus remove
        the need to call blk_cleanup_queue for them.
    
        Rename blk_cleanup_disk to blk_mq_destroy_queue to make it clear that
        this function is intended only for separately allocated blk-mq queues.
    
        This saves an extra queue freeze for devices without a separately
        allocated queue.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Hannes Reinecke <hare@suse.de>
        Link: https://lore.kernel.org/r/20220619060552.1850436-6-hch@lst.de
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/block/ataflop.c
+++ drivers/block/ataflop.c
@@ -2043,7 +2043,6 @@ static void atari_floppy_cleanup(void)
 			if (!unit[i].disk[type])
 				continue;
 			del_gendisk(unit[i].disk[type]);
-			blk_cleanup_queue(unit[i].disk[type]->queue);
 			put_disk(unit[i].disk[type]);
 		}
 		blk_mq_free_tag_set(&unit[i].tag_set);
