681c4122e9bf8836b1d54ce809ed690ce441184a
Ming Lei <ming.lei@redhat.com>
2022-09-14 20:59:10 +0800

    blk-mq: Drop blk_mq_ops.timeout 'reserved' arg
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2118511
    
    commit 9bdb4833dd399cbff82cc20893f52bdec66a9eca
    Author: John Garry <john.garry@huawei.com>
    Date:   Wed Jul 6 20:03:51 2022 +0800
    
        blk-mq: Drop blk_mq_ops.timeout 'reserved' arg
    
        With new API blk_mq_is_reserved_rq() we can tell if a request is from
        the reserved pool, so stop passing 'reserved' arg. There is actually
        only a single user of that arg for all the callback implementations, which
        can use blk_mq_is_reserved_rq() instead.
    
        This will also allow us to stop passing the same 'reserved' around the
        blk-mq iter functions next.
    
        Signed-off-by: John Garry <john.garry@huawei.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Bart Van Assche <bvanassche@acm.org>
        Reviewed-by: Hannes Reinecke <hare@suse.de>
        Acked-by: Ulf Hansson <ulf.hansson@linaro.org> # For MMC
        Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
        Link: https://lore.kernel.org/r/1657109034-206040-4-git-send-email-john.garry@huawei.com
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/nvme/host/tcp.c
+++ drivers/nvme/host/tcp.c
@@ -2303,8 +2303,7 @@ static void nvme_tcp_complete_timed_out(struct request *rq)
 	}
 }
 
-static enum blk_eh_timer_return
-nvme_tcp_timeout(struct request *rq, bool reserved)
+static enum blk_eh_timer_return nvme_tcp_timeout(struct request *rq)
 {
 	struct nvme_tcp_request *req = blk_mq_rq_to_pdu(rq);
 	struct nvme_ctrl *ctrl = &req->queue->ctrl->ctrl;
