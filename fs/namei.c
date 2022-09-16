49339e98bf51a24ca1f4fc9480ee9e89aca5126e
Andrey Albershteyn <aalbersh@redhat.com>
2022-09-16 19:38:33 +0200

    fsnotify: invalidate dcache before IN_DELETE event
    Bugzilla: http://bugzilla.redhat.com/2090079
    Tested: ltp inotify11
    
    commit a37d9a17f099072fe4d3a9048b0321978707a918
    Author: Amir Goldstein <amir73il@gmail.com>
    Date:   Thu Jan 20 23:53:04 2022 +0200
    
        fsnotify: invalidate dcache before IN_DELETE event
    
        Apparently, there are some applications that use IN_DELETE event as an
        invalidation mechanism and expect that if they try to open a file with
        the name reported with the delete event, that it should not contain the
        content of the deleted file.
    
        Commit 49246466a989 ("fsnotify: move fsnotify_nameremove() hook out of
        d_delete()") moved the fsnotify delete hook before d_delete() so fsnotify
        will have access to a positive dentry.
    
        This allowed a race where opening the deleted file via cached dentry
        is now possible after receiving the IN_DELETE event.
    
        To fix the regression, create a new hook fsnotify_delete() that takes
        the unlinked inode as an argument and use a helper d_delete_notify() to
        pin the inode, so we can pass it to fsnotify_delete() after d_delete().
    
        Backporting hint: this regression is from v5.3. Although patch will
        apply with only trivial conflicts to v5.4 and v5.10, it won't build,
        because fsnotify_delete() implementation is different in each of those
        versions (see fsnotify_link()).
    
        A follow up patch will fix the fsnotify_unlink/rmdir() calls in pseudo
        filesystem that do not need to call d_delete().
    
        Link: https://lore.kernel.org/r/20220120215305.282577-1-amir73il@gmail.com
        Reported-by: Ivan Delalande <colona@arista.com>
        Link: https://lore.kernel.org/linux-fsdevel/YeNyzoDM5hP5LtGW@visor/
        Fixes: 49246466a989 ("fsnotify: move fsnotify_nameremove() hook out of d_delete()")
        Cc: stable@vger.kernel.org # v5.3+
        Signed-off-by: Amir Goldstein <amir73il@gmail.com>
        Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>

--- fs/namei.c
+++ fs/namei.c
@@ -3909,13 +3909,12 @@ int vfs_rmdir(struct user_namespace *mnt_userns, struct inode *dir,
 	dentry->d_inode->i_flags |= S_DEAD;
 	dont_mount(dentry);
 	detach_mounts(dentry);
-	fsnotify_rmdir(dir, dentry);
 
 out:
 	inode_unlock(dentry->d_inode);
 	dput(dentry);
 	if (!error)
-		d_delete(dentry);
+		d_delete_notify(dir, dentry);
 	return error;
 }
 EXPORT_SYMBOL(vfs_rmdir);
@@ -4037,7 +4036,6 @@ int vfs_unlink(struct user_namespace *mnt_userns, struct inode *dir,
 			if (!error) {
 				dont_mount(dentry);
 				detach_mounts(dentry);
-				fsnotify_unlink(dir, dentry);
 			}
 		}
 	}
@@ -4045,9 +4043,11 @@ out:
 	inode_unlock(target);
 
 	/* We don't d_delete() NFS sillyrenamed files--they still exist. */
-	if (!error && !(dentry->d_flags & DCACHE_NFSFS_RENAMED)) {
+	if (!error && dentry->d_flags & DCACHE_NFSFS_RENAMED) {
+		fsnotify_unlink(dir, dentry);
+	} else if (!error) {
 		fsnotify_link_count(target);
-		d_delete(dentry);
+		d_delete_notify(dir, dentry);
 	}
 
 	return error;
