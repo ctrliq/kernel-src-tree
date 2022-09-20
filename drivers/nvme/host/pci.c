f8eb5ad8ec8c4d1ffacc7c771f5a12621fb2b248
Ming Lei <ming.lei@redhat.com>
2022-09-20 19:19:32 +0000

    nvme-pci: set min_align_mask before calculating max_hw_sectors
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2175212
    
    commit 61ce339f19fabbc3e51237148a7ef6f2270e44fa
    Author: Rishabh Bhatnagar <risbhat@amazon.com>
    Date:   Tue Sep 20 19:19:32 2022 +0000
    
        nvme-pci: set min_align_mask before calculating max_hw_sectors
    
        If swiotlb is force enabled dma_max_mapping_size ends up calling
        swiotlb_max_mapping_size which takes into account the min align mask for
        the device.  Set the min align mask for nvme driver before calling
        dma_max_mapping_size while calculating max hw sectors.
    
        Signed-off-by: Rishabh Bhatnagar <risbhat@amazon.com>
        Signed-off-by: Christoph Hellwig <hch@lst.de>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -2841,6 +2841,8 @@ static void nvme_reset_work(struct work_struct *work)
 		nvme_start_admin_queue(&dev->ctrl);
 	}
 
+	dma_set_min_align_mask(dev->dev, NVME_CTRL_PAGE_SIZE - 1);
+
 	/*
 	 * Limit the max command size to prevent iod->sg allocations going
 	 * over a single page.
@@ -2853,7 +2855,6 @@ static void nvme_reset_work(struct work_struct *work)
 	 * Don't limit the IOMMU merged segment size.
 	 */
 	dma_set_max_seg_size(dev->dev, 0xffffffff);
-	dma_set_min_align_mask(dev->dev, NVME_CTRL_PAGE_SIZE - 1);
 
 	mutex_unlock(&dev->shutdown_lock);
 
