9e52165f0bef5b48003387c525ebf3b1ed560fa3
Chris von Recklinghausen <crecklin@redhat.com>
2022-09-27 13:13:29 -0400

    Revert "s390/mm: do not trigger write fault when vma does not allow VM_WRITE"
    Bugzilla: https://bugzilla.redhat.com/2120352
    Upstream status: RHEL only
    
    This reverts commit dd32386192221c2bb6235b8a0f7521ded76c71b7.
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- arch/s390/mm/fault.c
+++ arch/s390/mm/fault.c
@@ -392,9 +392,7 @@ static inline vm_fault_t do_exception(struct pt_regs *regs, int access)
 	flags = FAULT_FLAG_DEFAULT;
 	if (user_mode(regs))
 		flags |= FAULT_FLAG_USER;
-	if ((trans_exc_code & store_indication) == 0x400)
-		access = VM_WRITE;
-	if (access == VM_WRITE)
+	if (access == VM_WRITE || (trans_exc_code & store_indication) == 0x400)
 		flags |= FAULT_FLAG_WRITE;
 	mmap_read_lock(mm);
 
