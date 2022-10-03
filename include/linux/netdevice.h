f05cf731a8e54edc7954ae5112d18ef8407375a8
Ivan Vecera <ivecera@redhat.com>
2022-10-03 17:05:10 +0200

    net: move netif_set_gso_max helpers
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2128180
    
    Conflicts:
    - adjusted due to missing eac1b93c14d6 ("gro: add ability to control
      gro max packet size")
    
    commit 744d49daf8bd3b17b345c836f2e6f97d49fa6ae8
    Author: Jakub Kicinski <kuba@kernel.org>
    Date:   Thu May 5 19:51:34 2022 -0700
    
        net: move netif_set_gso_max helpers
    
        These are now internal to the core, no need to expose them.
    
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -4749,20 +4749,6 @@ static inline bool netif_needs_gso(struct sk_buff *skb,
 			 (skb->ip_summed != CHECKSUM_UNNECESSARY)));
 }
 
-static inline void netif_set_gso_max_size(struct net_device *dev,
-					  unsigned int size)
-{
-	/* dev->gso_max_size is read locklessly from sk_setup_caps() */
-	WRITE_ONCE(dev->gso_max_size, size);
-}
-
-static inline void netif_set_gso_max_segs(struct net_device *dev,
-					  unsigned int segs)
-{
-	/* dev->gso_max_segs is read locklessly from sk_setup_caps() */
-	WRITE_ONCE(dev->gso_max_segs, segs);
-}
-
 void netif_set_tso_max_size(struct net_device *dev, unsigned int size);
 void netif_set_tso_max_segs(struct net_device *dev, unsigned int segs);
 void netif_inherit_tso_max(struct net_device *to,
