0d71a5d640317343d42aad14e4b5194c8af7368d
Antoine Tenart <atenart@redhat.com>
2022-10-10 15:16:38 +0200

    tcp: add drop reason support to tcp_validate_incoming()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2059161
    Upstream Status: linux.git
    
    commit da40b613f89c43c58986e6f30560ad6573a4d569
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Fri Apr 15 17:10:41 2022 -0700
    
        tcp: add drop reason support to tcp_validate_incoming()
    
        Creates four new drop reasons for the following cases:
    
        1) packet being rejected by RFC 7323 PAWS check
        2) packet being rejected by SEQUENCE check
        3) Invalid RST packet
        4) Invalid SYN packet
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -5629,6 +5629,7 @@ static bool tcp_validate_incoming(struct sock *sk, struct sk_buff *skb,
 				  const struct tcphdr *th, int syn_inerr)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
+	SKB_DR(reason);
 
 	/* RFC1323: H1. Apply PAWS check first. */
 	if (tcp_fast_parse_options(sock_net(sk), skb, th, tp) &&
@@ -5640,6 +5641,7 @@ static bool tcp_validate_incoming(struct sock *sk, struct sk_buff *skb,
 						  LINUX_MIB_TCPACKSKIPPEDPAWS,
 						  &tp->last_oow_ack_time))
 				tcp_send_dupack(sk, skb);
+			SKB_DR_SET(reason, TCP_RFC7323_PAWS);
 			goto discard;
 		}
 		/* Reset is accepted even if it did not pass PAWS. */
@@ -5663,6 +5665,7 @@ static bool tcp_validate_incoming(struct sock *sk, struct sk_buff *skb,
 		} else if (tcp_reset_check(sk, skb)) {
 			goto reset;
 		}
+		SKB_DR_SET(reason, TCP_INVALID_SEQUENCE);
 		goto discard;
 	}
 
@@ -5705,6 +5708,7 @@ static bool tcp_validate_incoming(struct sock *sk, struct sk_buff *skb,
 		    sk->sk_state == TCP_ESTABLISHED)
 			tcp_fastopen_active_disable(sk);
 		tcp_send_challenge_ack(sk);
+		SKB_DR_SET(reason, TCP_RESET);
 		goto discard;
 	}
 
@@ -5719,6 +5723,7 @@ syn_challenge:
 			TCP_INC_STATS(sock_net(sk), TCP_MIB_INERRS);
 		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPSYNCHALLENGE);
 		tcp_send_challenge_ack(sk);
+		SKB_DR_SET(reason, TCP_INVALID_SYN);
 		goto discard;
 	}
 
@@ -5727,7 +5732,7 @@ syn_challenge:
 	return true;
 
 discard:
-	tcp_drop(sk, skb);
+	tcp_drop_reason(sk, skb, reason);
 	return false;
 
 reset:
