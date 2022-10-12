d6859c78582af96b20f5a69694aa73c9227c2061
Chris von Recklinghausen <crecklin@redhat.com>
2022-10-12 07:13:05 -0400

    mm/hugetlb: handle uffd-wp during fork()
    Bugzilla: https://bugzilla.redhat.com/2120352
    
    commit bc70fbf269fdff410b0b6d75c3770b9f59117b90
    Author: Peter Xu <peterx@redhat.com>
    Date:   Thu May 12 20:22:55 2022 -0700
    
        mm/hugetlb: handle uffd-wp during fork()
    
        Firstly, we'll need to pass in dst_vma into copy_hugetlb_page_range()
        because for uffd-wp it's the dst vma that matters on deciding how we
        should treat uffd-wp protected ptes.
    
        We should recognize pte markers during fork and do the pte copy if needed.
    
        [lkp@intel.com: vma_needs_copy can be static]
          Link: https://lkml.kernel.org/r/Ylb0CGeFJlc4EzLk@7ec4ff11d4ae
        Link: https://lkml.kernel.org/r/20220405014918.14932-1-peterx@redhat.com
        Signed-off-by: Peter Xu <peterx@redhat.com>
        Cc: Alistair Popple <apopple@nvidia.com>
        Cc: Andrea Arcangeli <aarcange@redhat.com>
        Cc: Axel Rasmussen <axelrasmussen@google.com>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: Hugh Dickins <hughd@google.com>
        Cc: Jerome Glisse <jglisse@redhat.com>
        Cc: "Kirill A . Shutemov" <kirill@shutemov.name>
        Cc: Matthew Wilcox <willy@infradead.org>
        Cc: Mike Kravetz <mike.kravetz@oracle.com>
        Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
        Cc: Nadav Amit <nadav.amit@gmail.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1238,7 +1238,7 @@ copy_p4d_range(struct vm_area_struct *dst_vma, struct vm_area_struct *src_vma,
  * false when we can speed up fork() by allowing lazy page faults later until
  * when the child accesses the memory range.
  */
-bool
+static bool
 vma_needs_copy(struct vm_area_struct *dst_vma, struct vm_area_struct *src_vma)
 {
 	/*
@@ -1282,7 +1282,7 @@ copy_page_range(struct vm_area_struct *dst_vma, struct vm_area_struct *src_vma)
 		return 0;
 
 	if (is_vm_hugetlb_page(src_vma))
-		return copy_hugetlb_page_range(dst_mm, src_mm, src_vma);
+		return copy_hugetlb_page_range(dst_mm, src_mm, dst_vma, src_vma);
 
 	if (unlikely(src_vma->vm_flags & VM_PFNMAP)) {
 		/*
