fd0e205fe2ac816ab9c1198b5adf49922bdfcb3e
Chris von Recklinghausen <crecklin@redhat.com>
2022-10-12 06:41:56 -0400

    s390/kexec_file: move kernel image size check
    Bugzilla: https://bugzilla.redhat.com/2120352
    
    commit 277c8389386e2ccb8417afe4e36f67fc5dcd735d
    Author: Sven Schnelle <svens@linux.ibm.com>
    Date:   Thu Sep 30 09:34:13 2021 +0200
    
        s390/kexec_file: move kernel image size check
    
        In preparation of adding support for command lines with variable
        sizes on s390, the check whether the new kernel image is at least HEAD_END
        bytes long isn't correct. Move the check to kexec_file_add_components()
        so we can get the size of the parm area and check the size there.
    
        The '.org HEAD_END' directive can now also be removed from head.S. This
        was used in the past to reserve space for the early sccb buffer, but with
        commit 9a5131b87cac1 ("s390/boot: move sclp early buffer from fixed address
        in asm to C") this is no longer required.
    
        Signed-off-by: Sven Schnelle <svens@linux.ibm.com>
        Reviewed-by: Heiko Carstens <hca@linux.ibm.com>
        Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- arch/s390/kernel/machine_kexec_file.c
+++ arch/s390/kernel/machine_kexec_file.c
@@ -237,7 +237,8 @@ void *kexec_file_add_components(struct kimage *image,
 	if (ret)
 		goto out;
 
-	if (image->cmdline_buf_len >= ARCH_COMMAND_LINE_SIZE) {
+	if (image->kernel_buf_len < PARMAREA + sizeof(struct parmarea) ||
+	    image->cmdline_buf_len >= ARCH_COMMAND_LINE_SIZE) {
 		ret = -EINVAL;
 		goto out;
 	}
@@ -352,18 +353,4 @@ int arch_kimage_file_post_load_cleanup(struct kimage *image)
 	image->arch.ipl_buf = NULL;
 
 	return kexec_image_post_load_cleanup_default(image);
-}
-
-int arch_kexec_kernel_image_probe(struct kimage *image, void *buf,
-				  unsigned long buf_len)
-{
-	/* A kernel must be at least large enough to contain head.S. During
-	 * load memory in head.S will be accessed, e.g. to register the next
-	 * command line. If the next kernel were smaller the current kernel
-	 * will panic at load.
-	 */
-	if (buf_len < HEAD_END)
-		return -ENOEXEC;
-
-	return kexec_image_probe_default(image, buf, buf_len);
 }
