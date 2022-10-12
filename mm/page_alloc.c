63d42d92cdc23dd368a7976ba2f8a8d28c3a652e
Chris von Recklinghausen <crecklin@redhat.com>
2022-10-12 06:42:03 -0400

    mm: page table check
    Bugzilla: https://bugzilla.redhat.com/2120352
    
    commit df4e817b710809425d899340dbfa8504a3ca4ba5
    Author: Pasha Tatashin <pasha.tatashin@soleen.com>
    Date:   Fri Jan 14 14:06:37 2022 -0800
    
        mm: page table check
    
        Check user page table entries at the time they are added and removed.
    
        Allows to synchronously catch memory corruption issues related to double
        mapping.
    
        When a pte for an anonymous page is added into page table, we verify
        that this pte does not already point to a file backed page, and vice
        versa if this is a file backed page that is being added we verify that
        this page does not have an anonymous mapping
    
        We also enforce that read-only sharing for anonymous pages is allowed
        (i.e.  cow after fork).  All other sharing must be for file pages.
    
        Page table check allows to protect and debug cases where "struct page"
        metadata became corrupted for some reason.  For example, when refcnt or
        mapcount become invalid.
    
        Link: https://lkml.kernel.org/r/20211221154650.1047963-4-pasha.tatashin@soleen.com
        Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
        Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
        Cc: Dave Hansen <dave.hansen@linux.intel.com>
        Cc: David Rientjes <rientjes@google.com>
        Cc: Frederic Weisbecker <frederic@kernel.org>
        Cc: Greg Thelen <gthelen@google.com>
        Cc: "H. Peter Anvin" <hpa@zytor.com>
        Cc: Hugh Dickins <hughd@google.com>
        Cc: Ingo Molnar <mingo@redhat.com>
        Cc: Jiri Slaby <jirislaby@kernel.org>
        Cc: Jonathan Corbet <corbet@lwn.net>
        Cc: Kees Cook <keescook@chromium.org>
        Cc: Masahiro Yamada <masahiroy@kernel.org>
        Cc: Mike Rapoport <rppt@kernel.org>
        Cc: Muchun Song <songmuchun@bytedance.com>
        Cc: Paul Turner <pjt@google.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Sami Tolvanen <samitolvanen@google.com>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Wei Xu <weixugc@google.com>
        Cc: Will Deacon <will@kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -64,6 +64,7 @@
 #include <linux/sched/rt.h>
 #include <linux/sched/mm.h>
 #include <linux/page_owner.h>
+#include <linux/page_table_check.h>
 #include <linux/kthread.h>
 #include <linux/memcontrol.h>
 #include <linux/ftrace.h>
@@ -1299,6 +1300,7 @@ static __always_inline bool free_pages_prepare(struct page *page,
 		if (memcg_kmem_enabled() && PageMemcgKmem(page))
 			__memcg_kmem_uncharge_page(page, order);
 		reset_page_owner(page, order);
+		page_table_check_free(page, order);
 		return false;
 	}
 
@@ -1338,6 +1340,7 @@ static __always_inline bool free_pages_prepare(struct page *page,
 	page_cpupid_reset_last(page);
 	page->flags &= ~PAGE_FLAGS_CHECK_AT_PREP;
 	reset_page_owner(page, order);
+	page_table_check_free(page, order);
 
 	if (!PageHighMem(page)) {
 		debug_check_no_locks_freed(page_address(page),
@@ -2375,6 +2378,7 @@ inline void post_alloc_hook(struct page *page, unsigned int order,
 	}
 
 	set_page_owner(page, order, gfp_flags);
+	page_table_check_alloc(page, order);
 }
 
 static void prep_new_page(struct page *page, unsigned int order, gfp_t gfp_flags,
