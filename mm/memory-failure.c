281e153b89c84bb23ad7ba599417ae0622eaaee8
Chris von Recklinghausen <crecklin@redhat.com>
2022-10-12 07:09:15 -0400

    mm/hwpoison: avoid the impact of hwpoison_filter() return value on mce handler
    Bugzilla: https://bugzilla.redhat.com/2120352
    
    commit d1fe111fb62a1cf0446a2919f5effbb33ad0702c
    Author: luofei <luofei@unicloud.com>
    Date:   Tue Mar 22 14:44:38 2022 -0700
    
        mm/hwpoison: avoid the impact of hwpoison_filter() return value on mce handler
    
        When the hwpoison page meets the filter conditions, it should not be
        regarded as successful memory_failure() processing for mce handler, but
        should return a distinct value, otherwise mce handler regards the error
        page has been identified and isolated, which may lead to calling
        set_mce_nospec() to change page attribute, etc.
    
        Here memory_failure() return -EOPNOTSUPP to indicate that the error
        event is filtered, mce handler should not take any action for this
        situation and hwpoison injector should treat as correct.
    
        Link: https://lkml.kernel.org/r/20220223082135.2769649-1-luofei@unicloud.com
        Signed-off-by: luofei <luofei@unicloud.com>
        Acked-by: Borislav Petkov <bp@suse.de>
        Cc: Dave Hansen <dave.hansen@linux.intel.com>
        Cc: H. Peter Anvin <hpa@zytor.com>
        Cc: Ingo Molnar <mingo@redhat.com>
        Cc: Miaohe Lin <linmiaohe@huawei.com>
        Cc: Naoya Horiguchi <naoya.horiguchi@nec.com>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Tony Luck <tony.luck@intel.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1517,7 +1517,7 @@ static int memory_failure_hugetlb(unsigned long pfn, int flags)
 				if (TestClearPageHWPoison(head))
 					num_poisoned_pages_dec();
 				unlock_page(head);
-				return 0;
+				return -EOPNOTSUPP;
 			}
 			unlock_page(head);
 			res = MF_FAILED;
@@ -1604,7 +1604,7 @@ static int memory_failure_dev_pagemap(unsigned long pfn, int flags,
 		goto out;
 
 	if (hwpoison_filter(page)) {
-		rc = 0;
+		rc = -EOPNOTSUPP;
 		goto unlock;
 	}
 
@@ -1673,6 +1673,10 @@ static DEFINE_MUTEX(mf_mutex);
  *
  * Must run in process context (e.g. a work queue) with interrupts
  * enabled and no spinlocks hold.
+ *
+ * Return: 0 for successfully handled the memory error,
+ *         -EOPNOTSUPP for memory_filter() filtered the error event,
+ *         < 0(except -EOPNOTSUPP) on failure.
  */
 int memory_failure(unsigned long pfn, int flags)
 {
@@ -1838,6 +1842,7 @@ try_again:
 			num_poisoned_pages_dec();
 		unlock_page(p);
 		put_page(p);
+		res = -EOPNOTSUPP;
 		goto unlock_mutex;
 	}
 
