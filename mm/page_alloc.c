1419281dae893c3d9121b61cfb0e052290634c70
Chris von Recklinghausen <crecklin@redhat.com>
2022-10-12 07:08:22 -0400

    mm/page_alloc.c: show watermark_boost of zone in zoneinfo
    Bugzilla: https://bugzilla.redhat.com/2120352
    
    commit a6ea8b5b9f1ce3403a1c8516035d653006741e80
    Author: Liangcai Fan <liangcaifan19@gmail.com>
    Date:   Fri Nov 5 13:40:37 2021 -0700
    
        mm/page_alloc.c: show watermark_boost of zone in zoneinfo
    
        min/low/high_wmark_pages(z) is defined as
    
          (z->_watermark[WMARK_MIN/LOW/HIGH] + z->watermark_boost)
    
        If kswapd is frequently woken up due to the increase of
        min/low/high_wmark_pages, printing watermark_boost can quickly locate
        whether watermark_boost or _watermark[WMARK_MIN/LOW/HIGH] caused
        min/low/high_wmark_pages to increase.
    
        Link: https://lkml.kernel.org/r/1632472566-12246-1-git-send-email-liangcaifan19@gmail.com
        Signed-off-by: Liangcai Fan <liangcaifan19@gmail.com>
        Cc: Chunyan Zhang <zhang.lyra@gmail.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5990,6 +5990,7 @@ void show_free_areas(unsigned int filter, nodemask_t *nodemask)
 		printk(KERN_CONT
 			"%s"
 			" free:%lukB"
+			" boost:%lukB"
 			" min:%lukB"
 			" low:%lukB"
 			" high:%lukB"
@@ -6010,6 +6011,7 @@ void show_free_areas(unsigned int filter, nodemask_t *nodemask)
 			"\n",
 			zone->name,
 			K(zone_page_state(zone, NR_FREE_PAGES)),
+			K(zone->watermark_boost),
 			K(min_wmark_pages(zone)),
 			K(low_wmark_pages(zone)),
 			K(high_wmark_pages(zone)),
