2364d35ef6c4bd02a6935e7c7627d2b4b82be3b6
Marcin Wcisło <marcin.wcislo@conclusive.pl>
2022-10-21 12:06:22 +0800

    tcp: fix a signed-integer-overflow bug in tcp_add_backlog()
    jira VULN-172278
    cve CVE-2022-50865
    commit-author Lu Wei <luwei32@huawei.com>
    commit ec791d8149ff60c40ad2074af3b92a39c916a03f
    upstream-diff Adapted the change to suit the codebase missing commits
      d519f350967a60b85a574ad8aeac43f2b4384746 ("tcp: minor optimization in
      tcp_add_backlog()") and 8265792bf8871acc2d00fd03883d830e2249d395
      ("net: silence KCSAN warnings around sk_add_backlog() calls")
    
    The type of sk_rcvbuf and sk_sndbuf in struct sock is int, and
    in tcp_add_backlog(), the variable limit is caculated by adding
    sk_rcvbuf, sk_sndbuf and 64 * 1024, it may exceed the max value
    of int and overflow. This patch reduces the limit budget by
    halving the sndbuf to solve this issue since ACK packets are much
    smaller than the payload.
    
    Fixes: c9c3321257e1 ("tcp: add tcp_add_backlog()")
    	Signed-off-by: Lu Wei <luwei32@huawei.com>
    	Reviewed-by: Eric Dumazet <edumazet@google.com>
    	Acked-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ec791d8149ff60c40ad2074af3b92a39c916a03f)
    	Signed-off-by: Marcin Wcisło <marcin.wcislo@conclusive.pl>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1690,7 +1690,7 @@ int tcp_v4_early_demux(struct sk_buff *skb)
 
 bool tcp_add_backlog(struct sock *sk, struct sk_buff *skb)
 {
-	u32 limit = sk->sk_rcvbuf + sk->sk_sndbuf;
+	u32 limit = (u32)(sk->sk_rcvbuf) + (u32)(sk->sk_sndbuf >> 1);
 	u32 tail_gso_size, tail_gso_segs;
 	struct skb_shared_info *shinfo;
 	const struct tcphdr *th;
