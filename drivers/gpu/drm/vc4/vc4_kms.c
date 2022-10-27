ef5330b0b19a074932ca12bbfcb19f3e418d7555
Adrien Thierry <athierry@redhat.com>
2022-10-27 14:03:41 -0400

    drm/vc4: kms: Clear the HVS FIFO commit pointer once done
    Bugzilla: https://bugzilla.redhat.com/2138240
    
    commit d134c5ff71c7f2320fc7997f2fbbdedf0c76889a
    Author: Maxime Ripard <maxime@cerno.tech>
    Date:   Wed Nov 17 10:45:25 2021 +0100
    
        drm/vc4: kms: Clear the HVS FIFO commit pointer once done
    
        Commit 9ec03d7f1ed3 ("drm/vc4: kms: Wait on previous FIFO users before a
        commit") introduced a wait on the previous commit done on a given HVS
        FIFO.
    
        However, we never cleared that pointer once done. Since
        drm_crtc_commit_put can free the drm_crtc_commit structure directly if
        we were the last user, this means that it can lead to a use-after free
        if we were to duplicate the state, and that stale pointer would even be
        copied to the new state.
    
        Set the pointer to NULL once we're done with the wait so that we don't
        carry over a pointer to a free'd structure.
    
        Fixes: 9ec03d7f1ed3 ("drm/vc4: kms: Wait on previous FIFO users before a commit")
        Signed-off-by: Maxime Ripard <maxime@cerno.tech>
        Reviewed-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
        Tested-by: Jian-Hong Pan <jhp@endlessos.org>
        Link: https://lore.kernel.org/r/20211117094527.146275-5-maxime@cerno.tech
    
    Signed-off-by: Adrien Thierry <athierry@redhat.com>

--- drivers/gpu/drm/vc4/vc4_kms.c
+++ drivers/gpu/drm/vc4/vc4_kms.c
@@ -395,6 +395,7 @@ static void vc4_atomic_commit_tail(struct drm_atomic_state *state)
 			drm_err(dev, "Timed out waiting for commit\n");
 
 		drm_crtc_commit_put(commit);
+		old_hvs_state->fifo_state[channel].pending_commit = NULL;
 	}
 
 	drm_atomic_helper_commit_modeset_disables(dev, state);
