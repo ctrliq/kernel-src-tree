13bd7281324ae3d0a55200329052ec177b779153
Artem Savkov <asavkov@redhat.com>
2022-10-31 10:29:04 +0100

    uprobe: gate bpf call behind BPF_EVENTS
    Bugzilla: https://bugzilla.redhat.com/2137876
    
    commit aca80dd95e20f1fa0daa212afc83c9fa0ad239e5
    Author: Delyan Kratunov <delyank@fb.com>
    Date:   Mon Jun 20 21:47:55 2022 +0000
    
        uprobe: gate bpf call behind BPF_EVENTS
        
        The call into bpf from uprobes needs to be gated now that it doesn't use
        the trace_events.h helpers.
        
        Randy found this as a randconfig build failure on linux-next [1].
        
          [1]: https://lore.kernel.org/linux-next/2de99180-7d55-2fdf-134d-33198c27cc58@infradead.org/
        
        Reported-by: Randy Dunlap <rdunlap@infradead.org>
        Signed-off-by: Delyan Kratunov <delyank@fb.com>
        Tested-by: Randy Dunlap <rdunlap@infradead.org>
        Acked-by: Randy Dunlap <rdunlap@infradead.org>
        Link: https://lore.kernel.org/r/cb8bfbbcde87ed5d811227a393ef4925f2aadb7b.camel@fb.com
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Artem Savkov <asavkov@redhat.com>

--- kernel/trace/trace_uprobe.c
+++ kernel/trace/trace_uprobe.c
@@ -1353,6 +1353,7 @@ static void __uprobe_perf_func(struct trace_uprobe *tu,
 	int size, esize;
 	int rctx;
 
+#ifdef CONFIG_BPF_EVENTS
 	if (bpf_prog_array_valid(call)) {
 		u32 ret;
 
@@ -1360,6 +1361,7 @@ static void __uprobe_perf_func(struct trace_uprobe *tu,
 		if (!ret)
 			return;
 	}
+#endif /* CONFIG_BPF_EVENTS */
 
 	esize = SIZEOF_TRACE_ENTRY(is_ret_probe(tu));
 
