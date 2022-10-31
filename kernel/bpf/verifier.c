5cc4428bc926cd16777f059a4c1b8bccc4299acb
Artem Savkov <asavkov@redhat.com>
2022-10-31 13:05:09 +0100

    bpf: Warn on non-preallocated case for BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE
    Bugzilla: https://bugzilla.redhat.com/2137876
    
    commit 5002615a37b1e23a4b51c386ee22c8f90a70b4dd
    Author: Yafang Shao <laoar.shao@gmail.com>
    Date:   Wed Jul 13 16:09:36 2022 +0000
    
        bpf: Warn on non-preallocated case for BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE
        
        BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE is also tracing type, which may
        cause unexpected memory allocation if we set BPF_F_NO_PREALLOC. Let's
        also warn on it similar as we do in case of BPF_PROG_TYPE_RAW_TRACEPOINT.
        
        Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Acked-by: Yonghong Song <yhs@fb.com>
        Link: https://lore.kernel.org/bpf/20220713160936.57488-1-laoar.shao@gmail.com
    
    Signed-off-by: Artem Savkov <asavkov@redhat.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -12574,6 +12574,7 @@ static bool is_tracing_prog_type(enum bpf_prog_type type)
 	case BPF_PROG_TYPE_TRACEPOINT:
 	case BPF_PROG_TYPE_PERF_EVENT:
 	case BPF_PROG_TYPE_RAW_TRACEPOINT:
+	case BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE:
 		return true;
 	default:
 		return false;
