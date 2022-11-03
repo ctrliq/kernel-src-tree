b21ae58e03dd4e200136ed4edae0c4049bd1023d
Benjamin Marzinski <bmarzins@redhat.com>
2022-11-03 14:30:35 -0500

    provide arch_test_bit_acquire for architectures that define test_bit
    Bugzilla: https://bugzilla.redhat.com/2138462
    Upstream Status: kernel/git/torvalds/linux.git
    Conflicts: context and code changes due to code refactoring upstream
    
    commit d6ffe6067a54972564552ea45d320fb98db1ac5e
    Author: Mikulas Patocka <mpatocka@redhat.com>
    Date:   Fri Aug 26 16:43:51 2022 -0400
    
        provide arch_test_bit_acquire for architectures that define test_bit
    
        Some architectures define their own arch_test_bit and they also need
        arch_test_bit_acquire, otherwise they won't compile.  We also clean up
        the code by using the generic test_bit if that is equivalent to the
        arch-specific version.
    
        Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
        Cc: stable@vger.kernel.org
        Fixes: 8238b4579866 ("wait_on_bit: add an acquire memory barrier")
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>

--- arch/h8300/include/asm/bitops.h
+++ arch/h8300/include/asm/bitops.h
@@ -87,7 +87,8 @@ static inline int test_bit(int nr, const volatile unsigned long *addr)
 	return ret;
 }
 
-#define __test_bit(nr, addr) test_bit(nr, addr)
+#define __test_bit(nr, addr)		test_bit(nr, addr)
+#define test_bit_acquire(nr, addr)	test_bit(nr, addr)
 
 #define H8300_GEN_TEST_BITOP(FNNAME, OP)				\
 static inline int FNNAME(int nr, void *addr)				\
