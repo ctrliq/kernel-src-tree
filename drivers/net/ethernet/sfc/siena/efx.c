30235645137e6555f522c987a94e02478f15a26c
Íñigo Huguet <ihuguet@redhat.com>
2022-11-07 10:04:02 +0100

    sfc: move unmaintained message to sfc_siena
    Bugzilla: https://bugzilla.redhat.com/2112206
    Bugzilla: https://bugzilla.redhat.com/2139802
    Upstream-status: RHEL9 only
    
    Devices of Siena family were marked as unmaintained in commit
    6d38c2cf547c sfc: Mark siena driver unmaintained
    
    However, previous commits created a new driver for these devices,
    removing the support for them in the main sfc driver. The unmaintained
    messages need to be moved to the new sfc_siena driver.
    
    Signed-off-by: Íñigo Huguet <ihuguet@redhat.com>

--- drivers/net/ethernet/sfc/siena/efx.c
+++ drivers/net/ethernet/sfc/siena/efx.c
@@ -797,6 +797,12 @@ static const struct pci_device_id efx_pci_table[] = {
 	{0}			/* end of list */
 };
 
+static const struct pci_device_id efx_unmaintained_pci_table[] = {
+	{PCI_DEVICE(PCI_VENDOR_ID_SOLARFLARE, 0x0803)},	/* SFC9020 */
+	{PCI_DEVICE(PCI_VENDOR_ID_SOLARFLARE, 0x0813)},	/* SFL9021 */
+	{0}
+};
+
 /**************************************************************************
  *
  * Data housekeeping
@@ -1050,6 +1056,8 @@ static int efx_pci_probe(struct pci_dev *pci_dev,
 
 	if (!efx->type->is_vf)
 		efx_probe_vpd_strings(efx);
+
+	pci_hw_unmaintained(efx_unmaintained_pci_table, pci_dev);
 
 	/* Set up basic I/O (BAR mappings etc) */
 	rc = efx_siena_init_io(efx, efx->type->mem_bar(efx),
