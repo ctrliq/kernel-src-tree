39c65a01b036e89b21d72ba0e0e477d9a956a196
Cédric Le Goater <clg@redhat.com>
2022-11-08 15:56:20 +0100

    s390/ap: use the common device_driver pointer
    Bugzilla: https://bugzilla.redhat.com/1871126
    
    commit c8c68c5fca47add52f7830a4e791434e98ba69c7
    Author: Julian Wiedmann <jwi@linux.ibm.com>
    Date:   Mon Jun 7 11:18:43 2021 +0200
    
        s390/ap: use the common device_driver pointer
    
        The device struct itself already contains a pointer to its driver.
        Use this consistently, instead of duplicating it.
    
        Signed-off-by: Julian Wiedmann <jwi@linux.ibm.com>
        Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>
        Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- drivers/s390/crypto/ap_bus.c
+++ drivers/s390/crypto/ap_bus.c
@@ -707,7 +707,7 @@ static int __ap_calc_helper(struct device *dev, void *arg)
 
 	if (is_queue_dev(dev)) {
 		pctrs->apqns++;
-		if ((to_ap_dev(dev))->drv)
+		if (dev->driver)
 			pctrs->bound++;
 	}
 
@@ -887,7 +887,6 @@ static int ap_device_probe(struct device *dev)
 			 to_ap_queue(dev)->qid);
 	spin_unlock_bh(&ap_queues_lock);
 
-	ap_dev->drv = ap_drv;
 	rc = ap_drv->probe ? ap_drv->probe(ap_dev) : -ENODEV;
 
 	if (rc) {
@@ -895,7 +894,6 @@ static int ap_device_probe(struct device *dev)
 		if (is_queue_dev(dev))
 			hash_del(&to_ap_queue(dev)->hnode);
 		spin_unlock_bh(&ap_queues_lock);
-		ap_dev->drv = NULL;
 	} else
 		ap_check_bindings_complete();
 
@@ -908,7 +906,7 @@ out:
 static void ap_device_remove(struct device *dev)
 {
 	struct ap_device *ap_dev = to_ap_dev(dev);
-	struct ap_driver *ap_drv = ap_dev->drv;
+	struct ap_driver *ap_drv = to_ap_drv(dev->driver);
 
 	/* prepare ap queue device removal */
 	if (is_queue_dev(dev))
@@ -927,7 +925,6 @@ static void ap_device_remove(struct device *dev)
 	if (is_queue_dev(dev))
 		hash_del(&to_ap_queue(dev)->hnode);
 	spin_unlock_bh(&ap_queues_lock);
-	ap_dev->drv = NULL;
 
 	put_device(dev);
 }
