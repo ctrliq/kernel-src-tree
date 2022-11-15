bf49a1cbe4bcef62147ade7114000d4a5211717f
Cornelia Huck <cohuck@redhat.com>
2022-11-15 18:56:22 +0100

    KVM: arm64: Ignore 'kvm-arm.mode=protected' when using VHE
    Bugzilla: https://bugzilla.redhat.com/2128973
    
    commit cde5042adf11b0a30a6ce0ec3d071afcf8d2efaf
    Author: Will Deacon <will@kernel.org>
    Date:   Thu Jun 9 13:12:20 2022 +0100
    
        KVM: arm64: Ignore 'kvm-arm.mode=protected' when using VHE
    
        Ignore 'kvm-arm.mode=protected' when using VHE so that kvm_get_mode()
        only returns KVM_MODE_PROTECTED on systems where the feature is available.
    
        Cc: David Brazdil <dbrazdil@google.com>
        Acked-by: Mark Rutland <mark.rutland@arm.com>
        Signed-off-by: Will Deacon <will@kernel.org>
        Signed-off-by: Marc Zyngier <maz@kernel.org>
        Link: https://lore.kernel.org/r/20220609121223.2551-4-will@kernel.org
    
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -1984,15 +1984,7 @@ static void elf_hwcap_fixup(void)
 #ifdef CONFIG_KVM
 static bool is_kvm_protected_mode(const struct arm64_cpu_capabilities *entry, int __unused)
 {
-	if (kvm_get_mode() != KVM_MODE_PROTECTED)
-		return false;
-
-	if (is_kernel_in_hyp_mode()) {
-		pr_warn("Protected KVM not available with VHE\n");
-		return false;
-	}
-
-	return true;
+	return kvm_get_mode() == KVM_MODE_PROTECTED;
 }
 #endif /* CONFIG_KVM */
 
