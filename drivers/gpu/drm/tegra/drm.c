afa91b02bc22f40fe3f2be26f2ce60f626161a05
Mika Penttilä <mpenttil@redhat.com>
2022-11-17 13:49:07 +0200

    drm/tegra: Don't set struct drm_device.irq_enabled
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2154295
    Upstream Status: v5.15-rc1
    
    Conflicts:
            drivers/gpu/drm/tegra/drm.c
    
    commit 14746211570b4e368d2027a77ff8372722136a5c
    Author:     Thomas Zimmermann <tzimmermann@suse.de>
    AuthorDate: Fri Jun 25 10:22:16 2021 +0200
    Commit:     Thomas Zimmermann <tzimmermann@suse.de>
    CommitDate: Tue Jun 29 15:40:52 2021 +0200
    
        The field drm_device.irq_enabled is only used by legacy drivers
        with userspace modesetting. Don't set it in tegra.
    
        Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
        Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
        Acked-by: Thierry Reding <treding@nvidia.com>
        Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
        Link: https://patchwork.freedesktop.org/patch/msgid/20210625082222.3845-22-tzimmermann@suse.de
    
    Signed-off-by: Mika Penttilä <mpenttil@redhat.com>

--- drivers/gpu/drm/tegra/drm.c
+++ drivers/gpu/drm/tegra/drm.c
@@ -1187,18 +1187,7 @@ static int host1x_drm_probe(struct host1x_device *dev)
 		if (err < 0)
 			goto device;
 	}
-
-	/*
-	 * We don't use the drm_irq_install() helpers provided by the DRM
-	 * core, so we need to set this manually in order to allow the
-	 * DRM_IOCTL_WAIT_VBLANK to operate correctly.
-	 */
-
-	//mjp
-#if 0	
-	drm->irq_enabled = true;
-#endif	
-
+	
 	/* syncpoints are used for full 32-bit hardware VBLANK counters */
 	drm->max_vblank_count = 0xffffffff;
 
