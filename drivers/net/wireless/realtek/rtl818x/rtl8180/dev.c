e857172cbde8d027d0e1602d633ca3188016a274
Íñigo Huguet <ihuguet@redhat.com>
2022-11-28 12:57:04 +0100

    wifi: mac80211: return a beacon for a specific link
    Bugzilla: https://bugzilla.redhat.com/2136122
    
    Conflicts: removed chunks of unsupported drivers: plfxlc, wfx
    
    commit 6e8912a503759bb8f1f01c5b761d0d45815fa6de
    Author: Shaul Triebitz <shaul.triebitz@intel.com>
    Date:   Mon Jun 6 14:25:54 2022 +0300
    
        wifi: mac80211: return a beacon for a specific link
        
        Pass the link id through to the get_beacon and return
        the beacon for a specific link id.
        
        Signed-off-by: Shaul Triebitz <shaul.triebitz@intel.com>
        Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    Signed-off-by: Íñigo Huguet <ihuguet@redhat.com>

--- drivers/net/wireless/realtek/rtl818x/rtl8180/dev.c
+++ drivers/net/wireless/realtek/rtl818x/rtl8180/dev.c
@@ -1296,7 +1296,7 @@ static void rtl8180_beacon_work(struct work_struct *work)
 		goto resched;
 
 	/* grab a fresh beacon */
-	skb = ieee80211_beacon_get(dev, vif);
+	skb = ieee80211_beacon_get(dev, vif, 0);
 	if (!skb)
 		goto resched;
 
