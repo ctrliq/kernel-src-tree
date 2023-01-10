4b0de36b9e07dd12e8bcba962bac5f38124b0dc2
Waiman Long <longman@redhat.com>
2023-01-10 13:30:15 -0500

    blk-cgroup: Return -ENOMEM directly in blkcg_css_alloc() error path
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2077665
    
    commit b5a9adcbd5dc95d34d1f5fc84eff9af6fc60d284
    Author: Waiman Long <longman@redhat.com>
    Date:   Fri, 4 Nov 2022 20:59:00 -0400
    
        blk-cgroup: Return -ENOMEM directly in blkcg_css_alloc() error path
    
        For blkcg_css_alloc(), the only error that will be returned is -ENOMEM.
        Simplify error handling code by returning this error directly instead
        of setting an intermediate "ret" variable.
    
        Signed-off-by: Waiman Long <longman@redhat.com>
        Reviewed-by: Ming Lei <ming.lei@redhat.com>
        Acked-by: Tejun Heo <tj@kernel.org>
        Link: https://lore.kernel.org/r/20221105005902.407297-2-longman@redhat.com
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -1140,7 +1140,6 @@ static struct cgroup_subsys_state *
 blkcg_css_alloc(struct cgroup_subsys_state *parent_css)
 {
 	struct blkcg *blkcg;
-	struct cgroup_subsys_state *ret;
 	int i;
 
 	mutex_lock(&blkcg_pol_mutex);
@@ -1149,10 +1148,8 @@ blkcg_css_alloc(struct cgroup_subsys_state *parent_css)
 		blkcg = &blkcg_root;
 	} else {
 		blkcg = kzalloc(sizeof(*blkcg), GFP_KERNEL);
-		if (!blkcg) {
-			ret = ERR_PTR(-ENOMEM);
+		if (!blkcg)
 			goto unlock;
-		}
 	}
 
 	for (i = 0; i < BLKCG_MAX_POLS ; i++) {
@@ -1169,10 +1166,9 @@ blkcg_css_alloc(struct cgroup_subsys_state *parent_css)
 			continue;
 
 		cpd = pol->cpd_alloc_fn(GFP_KERNEL);
-		if (!cpd) {
-			ret = ERR_PTR(-ENOMEM);
+		if (!cpd)
 			goto free_pd_blkcg;
-		}
+
 		blkcg->cpd[i] = cpd;
 		cpd->blkcg = blkcg;
 		cpd->plid = i;
@@ -1201,7 +1197,7 @@ free_pd_blkcg:
 		kfree(blkcg);
 unlock:
 	mutex_unlock(&blkcg_pol_mutex);
-	return ret;
+	return ERR_PTR(-ENOMEM);
 }
 
 static int blkcg_css_online(struct cgroup_subsys_state *css)
