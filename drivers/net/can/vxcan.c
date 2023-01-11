e64679e8e598ad4846b21fc0d947c0bb0127b336
Al Stone <ahs3@redhat.com>
2023-01-10 17:00:11 -0700

    can: do not increase tx_bytes statistics for RTR frames
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2071839
    Tested: This is one of a series of patch sets to enable Arm SystemReady IR
     support.  This set updates the CAN driver subsystem.  This set has
     been tested on NXP imx8mm and NV Jetson Xavier NX via simple boot
     tests, using cansend/candump from can-utils to pass messages through
     a virtual CAN interface, and of course the CI loop.
    
    Conflicts:
        drivers/net/can/vxcan.c
    
        A previous commit renamed netif_rx_ni() to netif_rx() causing
        the merge to fail; applied this hunk by hand as it was a change
        to a single assignment statement.
    
    commit cc4b08c31b5c51352f258032cc65e884b3e61e6a
    Author: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Date:   Tue Dec 7 21:15:31 2021 +0900
    
        can: do not increase tx_bytes statistics for RTR frames
    
        The actual payload length of the CAN Remote Transmission Request (RTR)
        frames is always 0, i.e. no payload is transmitted on the wire.
        However, those RTR frames still use the DLC to indicate the length of
        the requested frame.
    
        As such, net_device_stats::tx_bytes should not be increased when
        sending RTR frames.
    
        The function can_get_echo_skb() already returns the correct length,
        even for RTR frames (c.f. [1]). However, for historical reasons, the
        drivers do not use can_get_echo_skb()'s return value and instead, most
        of them store a temporary length (or dlc) in some local structure or
        array. Using the return value of can_get_echo_skb() solves the
        issue. After doing this, such length/dlc fields become unused and so
        this patch does the adequate cleaning when needed.
    
        This patch fixes all the CAN drivers.
    
        Finally, can_get_echo_skb() is decorated with the __must_check
        attribute in order to force future drivers to correctly use its return
        value (else the compiler would emit a warning).
    
        [1] commit ed3320cec279 ("can: dev: __can_get_echo_skb():
        fix real payload length return value for RTR frames")
    
        Link: https://lore.kernel.org/all/20211207121531.42941-6-mailhol.vincent@wanadoo.fr
        Cc: Nicolas Ferre <nicolas.ferre@microchip.com>
        Cc: Alexandre Belloni <alexandre.belloni@bootlin.com>
        Cc: Ludovic Desroches <ludovic.desroches@microchip.com>
        Cc: Maxime Ripard <mripard@kernel.org>
        Cc: Chen-Yu Tsai <wens@csie.org>
        Cc: Jernej Skrabec <jernej.skrabec@gmail.com>
        Cc: Yasushi SHOJI <yashi@spacecubics.com>
        Cc: Oliver Hartkopp <socketcan@hartkopp.net>
        Cc: Stephane Grosjean <s.grosjean@peak-system.com>
        Cc: Andreas Larsson <andreas@gaisler.com>
        Tested-by: Jimmy Assarsson <extja@kvaser.com> # kvaser
        Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
        Acked-by: Stefan Mätje <stefan.maetje@esd.eu> # esd_usb2
        Tested-by: Stefan Mätje <stefan.maetje@esd.eu> # esd_usb2
        [mkl: add conversion for grcan]
        Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
        (cherry picked from commit cc4b08c31b5c51352f258032cc65e884b3e61e6a)
    
    Signed-off-by: Al Stone <ahs3@redhat.com>

--- drivers/net/can/vxcan.c
+++ drivers/net/can/vxcan.c
@@ -67,7 +67,7 @@ static netdev_tx_t vxcan_xmit(struct sk_buff *oskb, struct net_device *dev)
 	skb->dev        = peer;
 	skb->ip_summed  = CHECKSUM_UNNECESSARY;
 
-	len = cfd->len;
+	len = cfd->can_id & CAN_RTR_FLAG ? 0 : cfd->len;
 	if (netif_rx(skb) == NET_RX_SUCCESS) {
 		srcstats->tx_packets++;
 		srcstats->tx_bytes += len;
