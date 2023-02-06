f323bcdc29293212bf67379f093e236980e9ed04
Carlos Maiolino <cmaiolino@redhat.com>
2023-02-06 10:46:35 +0100

    xfs,iomap: move delalloc punching to iomap
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2155605
    Tested: With xfstests and bz reproducer
    
    Conflicts:
    	- Context conflict due the lack of iomap_read_folio()
    
    Because that's what Christoph wants for this error handling path
    only XFS uses.
    
    It requires a new iomap export for handling errors over delalloc
    ranges. This is basically the XFS code as is stands, but even though
    Christoph wants this as iomap funcitonality, we still have
    to call it from the filesystem specific ->iomap_end callback, and
    call into the iomap code with yet another filesystem specific
    callback to punch the delalloc extent within the defined ranges.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    (cherry picked from commit 9c7babf94a0d686b552e53aded8d4703d1b8b92b)
    Signed-off-by: Carlos Maiolino <cmaiolino@redhat.com>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -221,6 +221,10 @@ static inline const struct iomap *iomap_iter_srcmap(const struct iomap_iter *i)
 ssize_t iomap_file_buffered_write(struct kiocb *iocb, struct iov_iter *from,
 		const struct iomap_ops *ops);
 int iomap_readpage(struct page *page, const struct iomap_ops *ops);
+int iomap_file_buffered_write_punch_delalloc(struct inode *inode,
+		struct iomap *iomap, loff_t pos, loff_t length, ssize_t written,
+		int (*punch)(struct inode *inode, loff_t pos, loff_t length));
+
 void iomap_readahead(struct readahead_control *, const struct iomap_ops *ops);
 bool iomap_is_partially_uptodate(struct folio *, size_t from, size_t count);
 int iomap_releasepage(struct page *page, gfp_t gfp_mask);
