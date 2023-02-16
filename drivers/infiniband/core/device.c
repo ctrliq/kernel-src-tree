c3e1f75e9dfd7d8c5d8e41d75212dd4efc8e3b0d
Kamal Heib <kheib@redhat.com>
2023-02-16 14:31:18 -0500

    RDMA/core: Delete IPsec flow action logic from the core
    Bugzilla: https://bugzilla.redhat.com/2168933
    
    commit 32313c6ae622ed7aa553fa1ee22d8e5f24146f0e
    Author: Leon Romanovsky <leon@kernel.org>
    Date:   Wed Apr 6 11:25:43 2022 +0300
    
        RDMA/core: Delete IPsec flow action logic from the core
    
        The removal of mlx5 flow steering logic, left the kernel without any RDMA
        drivers that implements flow action callbacks supplied by RDMA/core. Any
        user access to them caused to EOPNOTSUPP error, which can be achieved by
        simply removing ioctl implementation.
    
        Link: https://lore.kernel.org/r/a638e376314a2eb1c66f597c0bbeeab2e5de7faf.1649232994.git.leonro@nvidia.com
        Reviewed-by: Raed Salem <raeds@nvidia.com>
        Acked-by: Jason Gunthorpe <jgg@nvidia.com>
        Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    
    Signed-off-by: Kamal Heib <kheib@redhat.com>

--- drivers/infiniband/core/device.c
+++ drivers/infiniband/core/device.c
@@ -2614,7 +2614,6 @@ void ib_set_device_ops(struct ib_device *dev, const struct ib_device_ops *ops)
 	SET_DEVICE_OP(dev_ops, create_counters);
 	SET_DEVICE_OP(dev_ops, create_cq);
 	SET_DEVICE_OP(dev_ops, create_flow);
-	SET_DEVICE_OP(dev_ops, create_flow_action_esp);
 	SET_DEVICE_OP(dev_ops, create_qp);
 	SET_DEVICE_OP(dev_ops, create_rwq_ind_table);
 	SET_DEVICE_OP(dev_ops, create_srq);
@@ -2677,7 +2676,6 @@ void ib_set_device_ops(struct ib_device *dev, const struct ib_device_ops *ops)
 	SET_DEVICE_OP(dev_ops, modify_ah);
 	SET_DEVICE_OP(dev_ops, modify_cq);
 	SET_DEVICE_OP(dev_ops, modify_device);
-	SET_DEVICE_OP(dev_ops, modify_flow_action_esp);
 	SET_DEVICE_OP(dev_ops, modify_hw_stat);
 	SET_DEVICE_OP(dev_ops, modify_port);
 	SET_DEVICE_OP(dev_ops, modify_qp);
