ae5d540bb146ff1c463ad1f573f540460f4f4e60
David Arcari <darcari@redhat.com>
2023-02-23 13:45:03 -0500

    Revert "include/linux/irq*.h: Pad irq structs for KABI"
    Upstream Status: RHEL only
    Bugzilla: https://bugzilla.redhat.com/2175165
    
    This reverts commit 879f2336902e89c0cfcfbe6f2fba811c9301f1ef
    which allows for easier application of upstream commits.
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- include/linux/irq_work.h
+++ include/linux/irq_work.h
@@ -5,8 +5,6 @@
 #include <linux/smp_types.h>
 #include <linux/rcuwait.h>
 
-#include <linux/rh_kabi.h>
-
 /*
  * An entry can be in one of four states:
  *
@@ -20,10 +18,6 @@ struct irq_work {
 	struct __call_single_node node;
 	void (*func)(struct irq_work *);
 	struct rcuwait irqwait;
-	RH_KABI_RESERVE(1)
-	RH_KABI_RESERVE(2)
-	RH_KABI_RESERVE(3)
-	RH_KABI_RESERVE(4)
 };
 
 #define __IRQ_WORK_INIT(_func, _flags) (struct irq_work){	\
--- include/linux/irqdesc.h
+++ include/linux/irqdesc.h
@@ -6,8 +6,6 @@
 #include <linux/kobject.h>
 #include <linux/mutex.h>
 
-#include <linux/rh_kabi.h>
-
 /*
  * Core internal functions to deal with irq descriptors
  */
@@ -104,10 +102,6 @@ struct irq_desc {
 	int			parent_irq;
 	struct module		*owner;
 	const char		*name;
-	RH_KABI_RESERVE(1)
-	RH_KABI_RESERVE(2)
-	RH_KABI_RESERVE(3)
-	RH_KABI_RESERVE(4)
 } ____cacheline_internodealigned_in_smp;
 
 #ifdef CONFIG_SPARSE_IRQ
