19d12a23a011bec41cc06535843fa4e58ff1a1be
David Arcari <darcari@redhat.com>
2023-03-01 13:39:19 -0500

    powerpc/pseries/pci: Add a msi_free() handler to clear XIVE data
    Bugzilla: https://bugzilla.redhat.com/2175165
    
    commit 9a014f456881e947bf8cdd8c984a207097e6c096
    Author: Cédric Le Goater <clg@kaod.org>
    Date:   Thu Jul 1 15:27:27 2021 +0200
    
        powerpc/pseries/pci: Add a msi_free() handler to clear XIVE data
    
        The MSI domain clears the IRQ with msi_domain_free(), which calls
        irq_domain_free_irqs_top(), which clears the handler data. This is a
        problem for the XIVE controller since we need to unmap MMIO pages and
        free a specific XIVE structure.
    
        The 'msi_free()' handler is called before irq_domain_free_irqs_top()
        when the handler data is still available. Use that to clear the XIVE
        controller data.
    
        Signed-off-by: Cédric Le Goater <clg@kaod.org>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
        Link: https://lore.kernel.org/r/20210701132750.1475580-10-clg@kaod.org
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- arch/powerpc/sysdev/xive/common.c
+++ arch/powerpc/sysdev/xive/common.c
@@ -989,6 +989,8 @@ EXPORT_SYMBOL_GPL(is_xive_irq);
 
 void xive_cleanup_irq_data(struct xive_irq_data *xd)
 {
+	pr_debug("%s for HW %x\n", __func__, xd->hw_irq);
+
 	if (xd->eoi_mmio) {
 		iounmap(xd->eoi_mmio);
 		if (xd->eoi_mmio == xd->trig_mmio)
@@ -1030,7 +1032,7 @@ static int xive_irq_alloc_data(unsigned int virq, irq_hw_number_t hw)
 	return 0;
 }
 
-static void xive_irq_free_data(unsigned int virq)
+void xive_irq_free_data(unsigned int virq)
 {
 	struct xive_irq_data *xd = irq_get_handler_data(virq);
 
@@ -1040,6 +1042,7 @@ static void xive_irq_free_data(unsigned int virq)
 	xive_cleanup_irq_data(xd);
 	kfree(xd);
 }
+EXPORT_SYMBOL_GPL(xive_irq_free_data);
 
 #ifdef CONFIG_SMP
 
