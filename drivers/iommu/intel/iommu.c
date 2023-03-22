2078a0d0a0e37b925df1c2abd8c091558f7627d3
Jerry Snitselaar <jsnitsel@redhat.com>
2023-03-22 20:34:21 +0800

    iommu: Use sysfs_emit() for sysfs show
    JIRA: https://issues.redhat.com/browse/RHEL-10094
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    commit c33fcc13ee0d02efe5729588e8fba8b899c8879a
    Author: Lu Baolu <baolu.lu@linux.intel.com>
    Date:   Wed Mar 22 20:34:21 2023 +0800
    
        iommu: Use sysfs_emit() for sysfs show
    
        Use sysfs_emit() instead of the sprintf() for sysfs entries. sysfs_emit()
        knows the maximum of the temporary buffer used for outputting sysfs
        content and avoids overrunning the buffer length.
    
        Prefer 'long long' over 'long long int' as suggested by checkpatch.pl.
    
        Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
        Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
        Link: https://lore.kernel.org/r/20230322123421.278852-1-baolu.lu@linux.intel.com
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    (cherry picked from commit c33fcc13ee0d02efe5729588e8fba8b899c8879a)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -3735,8 +3735,8 @@ static ssize_t version_show(struct device *dev,
 {
 	struct intel_iommu *iommu = dev_to_intel_iommu(dev);
 	u32 ver = readl(iommu->reg + DMAR_VER_REG);
-	return sprintf(buf, "%d:%d\n",
-		       DMAR_VER_MAJOR(ver), DMAR_VER_MINOR(ver));
+	return sysfs_emit(buf, "%d:%d\n",
+			  DMAR_VER_MAJOR(ver), DMAR_VER_MINOR(ver));
 }
 static DEVICE_ATTR_RO(version);
 
@@ -3744,7 +3744,7 @@ static ssize_t address_show(struct device *dev,
 			    struct device_attribute *attr, char *buf)
 {
 	struct intel_iommu *iommu = dev_to_intel_iommu(dev);
-	return sprintf(buf, "%llx\n", iommu->reg_phys);
+	return sysfs_emit(buf, "%llx\n", iommu->reg_phys);
 }
 static DEVICE_ATTR_RO(address);
 
@@ -3752,7 +3752,7 @@ static ssize_t cap_show(struct device *dev,
 			struct device_attribute *attr, char *buf)
 {
 	struct intel_iommu *iommu = dev_to_intel_iommu(dev);
-	return sprintf(buf, "%llx\n", iommu->cap);
+	return sysfs_emit(buf, "%llx\n", iommu->cap);
 }
 static DEVICE_ATTR_RO(cap);
 
@@ -3760,7 +3760,7 @@ static ssize_t ecap_show(struct device *dev,
 			 struct device_attribute *attr, char *buf)
 {
 	struct intel_iommu *iommu = dev_to_intel_iommu(dev);
-	return sprintf(buf, "%llx\n", iommu->ecap);
+	return sysfs_emit(buf, "%llx\n", iommu->ecap);
 }
 static DEVICE_ATTR_RO(ecap);
 
@@ -3768,7 +3768,7 @@ static ssize_t domains_supported_show(struct device *dev,
 				      struct device_attribute *attr, char *buf)
 {
 	struct intel_iommu *iommu = dev_to_intel_iommu(dev);
-	return sprintf(buf, "%ld\n", cap_ndoms(iommu->cap));
+	return sysfs_emit(buf, "%ld\n", cap_ndoms(iommu->cap));
 }
 static DEVICE_ATTR_RO(domains_supported);
 
@@ -3776,8 +3776,9 @@ static ssize_t domains_used_show(struct device *dev,
 				 struct device_attribute *attr, char *buf)
 {
 	struct intel_iommu *iommu = dev_to_intel_iommu(dev);
-	return sprintf(buf, "%d\n", bitmap_weight(iommu->domain_ids,
-						  cap_ndoms(iommu->cap)));
+	return sysfs_emit(buf, "%d\n",
+			  bitmap_weight(iommu->domain_ids,
+					cap_ndoms(iommu->cap)));
 }
 static DEVICE_ATTR_RO(domains_used);
 
