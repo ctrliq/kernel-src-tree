27a6e05247e1cccdd5a24d894ce23fddc897f702
Chris von Recklinghausen <crecklin@redhat.com>
2023-03-24 07:44:53 -0400

    arm64: mte: Lock a page for MTE tag initialisation
    Bugzilla: https://bugzilla.redhat.com/2160210
    
    commit d77e59a8fccde7fb5dd8c57594ed147b4291c970
    Author: Catalin Marinas <catalin.marinas@arm.com>
    Date:   Thu Nov 3 18:10:38 2022 -0700
    
        arm64: mte: Lock a page for MTE tag initialisation
    
        Initialising the tags and setting PG_mte_tagged flag for a page can race
        between multiple set_pte_at() on shared pages or setting the stage 2 pte
        via user_mem_abort(). Introduce a new PG_mte_lock flag as PG_arch_3 and
        set it before attempting page initialisation. Given that PG_mte_tagged
        is never cleared for a page, consider setting this flag to mean page
        unlocked and wait on this bit with acquire semantics if the page is
        locked:
    
        - try_page_mte_tagging() - lock the page for tagging, return true if it
          can be tagged, false if already tagged. No acquire semantics if it
          returns true (PG_mte_tagged not set) as there is no serialisation with
          a previous set_page_mte_tagged().
    
        - set_page_mte_tagged() - set PG_mte_tagged with release semantics.
    
        The two-bit locking is based on Peter Collingbourne's idea.
    
        Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
        Signed-off-by: Peter Collingbourne <pcc@google.com>
        Reviewed-by: Steven Price <steven.price@arm.com>
        Cc: Will Deacon <will@kernel.org>
        Cc: Marc Zyngier <maz@kernel.org>
        Cc: Peter Collingbourne <pcc@google.com>
        Reviewed-by: Cornelia Huck <cohuck@redhat.com>
        Signed-off-by: Marc Zyngier <maz@kernel.org>
        Link: https://lore.kernel.org/r/20221104011041.290951-6-pcc@google.com
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -1082,7 +1082,7 @@ static int sanitise_mte_tags(struct kvm *kvm, kvm_pfn_t pfn,
 		return -EFAULT;
 
 	for (i = 0; i < nr_pages; i++, page++) {
-		if (!page_mte_tagged(page)) {
+		if (try_page_mte_tagging(page)) {
 			mte_clear_page_tags(page_address(page));
 			set_page_mte_tagged(page);
 		}
