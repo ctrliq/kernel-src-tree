7fba27fbaffa2fbee99a0f940f5f46a78085d0e4
Chris von Recklinghausen <crecklin@redhat.com>
2023-03-24 07:43:41 -0400

    fs: Convert iomap_readpage to iomap_read_folio
    Conflicts:
    	Drop changes to fs/erofs/data.c - unsupported config
    	include/linux/iomap.h - We already have
    		9c7babf94a0d ("xfs,iomap: move delalloc punching to iomap")
    		so keep declaration of iomap_file_buffered_write_punch_delalloc
    
    Bugzilla: https://bugzilla.redhat.com/2160210
    
    commit 7479c505b4ab5ed5f81f35fdd68c44c58d6f0439
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Fri Apr 29 08:54:32 2022 -0400
    
        fs: Convert iomap_readpage to iomap_read_folio
    
        A straightforward conversion as iomap_readpage already worked in folios.
    
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -248,7 +248,7 @@ static inline const struct iomap *iomap_iter_srcmap(const struct iomap_iter *i)
 
 ssize_t iomap_file_buffered_write(struct kiocb *iocb, struct iov_iter *from,
 		const struct iomap_ops *ops);
-int iomap_readpage(struct page *page, const struct iomap_ops *ops);
+int iomap_read_folio(struct folio *folio, const struct iomap_ops *ops);
 int iomap_file_buffered_write_punch_delalloc(struct inode *inode,
 		struct iomap *iomap, loff_t pos, loff_t length, ssize_t written,
 		int (*punch)(struct inode *inode, loff_t pos, loff_t length));
