f4e01cb40d24bce14447a5d39ac739d7437915ae
Chris von Recklinghausen <crecklin@redhat.com>
2023-03-24 06:24:12 -0400

    sysctl: use SYSCTL_ZERO to replace some static int zero uses
    Bugzilla: https://bugzilla.redhat.com/2160210
    
    commit 2452dcb9f7f2bab5b47344148bc23a732be9195c
    Author: Xiaoming Ni <nixiaoming@huawei.com>
    Date:   Fri Jan 21 22:11:19 2022 -0800
    
        sysctl: use SYSCTL_ZERO to replace some static int zero uses
    
        Use the variable SYSCTL_ZERO to replace some static int boundary
        variables with a value of 0 (minolduid, min_extfrag_threshold,
        min_wakeup_granularity_ns).
    
        Link: https://lkml.kernel.org/r/20211123202347.818157-8-mcgrof@kernel.org
        Signed-off-by: Xiaoming Ni <nixiaoming@huawei.com>
        Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>
        Cc: Al Viro <viro@zeniv.linux.org.uk>
        Cc: Amir Goldstein <amir73il@gmail.com>
        Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
        Cc: Benjamin LaHaise <bcrl@kvack.org>
        Cc: "Eric W. Biederman" <ebiederm@xmission.com>
        Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Cc: Iurii Zaikin <yzaikin@google.com>
        Cc: Jan Kara <jack@suse.cz>
        Cc: Kees Cook <keescook@chromium.org>
        Cc: Paul Turner <pjt@google.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Petr Mladek <pmladek@suse.com>
        Cc: Qing Wang <wangqing@vivo.com>
        Cc: Sebastian Reichel <sre@kernel.org>
        Cc: Sergey Senozhatsky <senozhatsky@chromium.org>
        Cc: Stephen Kitt <steve@sk2.org>
        Cc: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
        Cc: Antti Palosaari <crope@iki.fi>
        Cc: Arnd Bergmann <arnd@arndb.de>
        Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
        Cc: Clemens Ladisch <clemens@ladisch.de>
        Cc: David Airlie <airlied@linux.ie>
        Cc: Jani Nikula <jani.nikula@linux.intel.com>
        Cc: Joel Becker <jlbec@evilplan.org>
        Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
        Cc: Joseph Qi <joseph.qi@linux.alibaba.com>
        Cc: Julia Lawall <julia.lawall@inria.fr>
        Cc: Lukas Middendorf <kernel@tuxforce.de>
        Cc: Mark Fasheh <mark@fasheh.com>
        Cc: Phillip Potter <phil@philpotter.co.uk>
        Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
        Cc: Douglas Gilbert <dgilbert@interlog.com>
        Cc: James E.J. Bottomley <jejb@linux.ibm.com>
        Cc: Jani Nikula <jani.nikula@intel.com>
        Cc: John Ogness <john.ogness@linutronix.de>
        Cc: Martin K. Petersen <martin.petersen@oracle.com>
        Cc: "Rafael J. Wysocki" <rafael@kernel.org>
        Cc: Steven Rostedt (VMware) <rostedt@goodmis.org>
        Cc: Suren Baghdasaryan <surenb@google.com>
        Cc: "Theodore Ts'o" <tytso@mit.edu>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -118,7 +118,7 @@ static const unsigned long dirty_bytes_min = 2 * PAGE_SIZE;
 
 /* this is needed for the proc_dointvec_minmax for [fs_]overflow UID and GID */
 static const int maxolduid = 65535;
-static const int minolduid;
+/* minolduid is SYSCTL_ZERO */
 
 static const int ngroups_max = NGROUPS_MAX;
 static const int cap_last_cap = CAP_LAST_CAP;
@@ -167,7 +167,7 @@ int sysctl_legacy_va_layout;
 #endif
 
 #ifdef CONFIG_COMPACTION
-static const int min_extfrag_threshold;
+/* min_extfrag_threshold is SYSCTL_ZERO */;
 static const int max_extfrag_threshold = 1000;
 #endif
 
@@ -1987,7 +1987,7 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= (void *)&minolduid,
+		.extra1		= SYSCTL_ZERO,
 		.extra2		= (void *)&maxolduid,
 	},
 	{
@@ -1996,7 +1996,7 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= (void *)&minolduid,
+		.extra1		= SYSCTL_ZERO,
 		.extra2		= (void *)&maxolduid,
 	},
 #ifdef CONFIG_S390
@@ -2495,7 +2495,7 @@ static struct ctl_table vm_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= (void *)&min_extfrag_threshold,
+		.extra1		= SYSCTL_ZERO,
 		.extra2		= (void *)&max_extfrag_threshold,
 	},
 	{
@@ -2807,7 +2807,7 @@ static struct ctl_table fs_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= (void *)&minolduid,
+		.extra1		= SYSCTL_ZERO,
 		.extra2		= (void *)&maxolduid,
 	},
 	{
@@ -2816,7 +2816,7 @@ static struct ctl_table fs_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= (void *)&minolduid,
+		.extra1		= SYSCTL_ZERO,
 		.extra2		= (void *)&maxolduid,
 	},
 #ifdef CONFIG_FILE_LOCKING
