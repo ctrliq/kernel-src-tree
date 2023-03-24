b7caefc21b23fd7ead13a67f873d398c940d4ebf
Chris von Recklinghausen <crecklin@redhat.com>
2023-03-24 07:43:12 -0400

    iomap: Convert __iomap_zero_iter to use a folio
    Conflicts: fs/iomap/buffered-io.c - We already have
    	d7b64041164c ("iomap: write iomap validity checks")
    	so keep check for IOMAP_F_STALE
    
    Bugzilla: https://bugzilla.redhat.com/2160210
    
    commit a25def1fe56858efa40a8490e875da4a711487f8
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Fri Nov 5 14:24:09 2021 -0400
    
        iomap: Convert __iomap_zero_iter to use a folio
    
        The zero iterator can work in folio-sized chunks instead of page-sized
        chunks.  This will save a lot of page cache lookups if the file is cached
        in large folios.
    
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -1143,9 +1143,10 @@ EXPORT_SYMBOL_GPL(iomap_file_unshare);
 
 static s64 __iomap_zero_iter(struct iomap_iter *iter, loff_t pos, u64 length)
 {
+	struct folio *folio;
 	struct page *page;
 	int status;
-	unsigned offset = offset_in_page(pos);
+	size_t offset;
 	unsigned bytes = min_t(u64, UINT_MAX, length);
 
 	status = iomap_write_begin(iter, pos, bytes, &page);
@@ -1153,11 +1154,14 @@ static s64 __iomap_zero_iter(struct iomap_iter *iter, loff_t pos, u64 length)
 		return status;
 	if (iter->iomap.flags & IOMAP_F_STALE)
 		return status;
-	if (bytes > PAGE_SIZE - offset)
-		bytes = PAGE_SIZE - offset;
+	folio = page_folio(page);
 
-	zero_user(page, offset, bytes);
-	mark_page_accessed(page);
+	offset = offset_in_folio(folio, pos);
+	if (bytes > folio_size(folio) - offset)
+		bytes = folio_size(folio) - offset;
+
+	folio_zero_range(folio, offset, bytes);
+	folio_mark_accessed(folio);
 
 	return iomap_write_end(iter, pos, bytes, bytes, page);
 }
