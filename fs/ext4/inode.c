3a3be8de749a6146fddec41fe8eb5f8fd343633f
Chris von Recklinghausen <crecklin@redhat.com>
2023-03-24 07:44:16 -0400

    ext4: Convert mpage_release_unused_pages() to use filemap_get_folios()
    Bugzilla: https://bugzilla.redhat.com/2160210
    
    commit fb5a5be05fb45ab6fb3d1036c53435cde3d420ab
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Fri Jun 3 15:36:49 2022 -0400
    
        ext4: Convert mpage_release_unused_pages() to use filemap_get_folios()
    
        If the folio is large, it may overlap the beginning or end of the
        unused range.  If it does, we need to avoid invalidating it.
    
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Acked-by: Christian Brauner (Microsoft) <brauner@kernel.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -1564,9 +1564,9 @@ struct mpage_da_data {
 static void mpage_release_unused_pages(struct mpage_da_data *mpd,
 				       bool invalidate)
 {
-	int nr_pages, i;
+	unsigned nr, i;
 	pgoff_t index, end;
-	struct pagevec pvec;
+	struct folio_batch fbatch;
 	struct inode *inode = mpd->inode;
 	struct address_space *mapping = inode->i_mapping;
 
@@ -1591,15 +1591,18 @@ static void mpage_release_unused_pages(struct mpage_da_data *mpd,
 		up_write(&EXT4_I(inode)->i_data_sem);
 	}
 
-	pagevec_init(&pvec);
+	folio_batch_init(&fbatch);
 	while (index <= end) {
-		nr_pages = pagevec_lookup_range(&pvec, mapping, &index, end);
-		if (nr_pages == 0)
+		nr = filemap_get_folios(mapping, &index, end, &fbatch);
+		if (nr == 0)
 			break;
-		for (i = 0; i < nr_pages; i++) {
-			struct page *page = pvec.pages[i];
-			struct folio *folio = page_folio(page);
+		for (i = 0; i < nr; i++) {
+			struct folio *folio = fbatch.folios[i];
 
+			if (folio->index < mpd->first_page)
+				continue;
+			if (folio->index + folio_nr_pages(folio) - 1 > end)
+				continue;
 			BUG_ON(!folio_test_locked(folio));
 			BUG_ON(folio_test_writeback(folio));
 			if (invalidate) {
@@ -1611,7 +1614,7 @@ static void mpage_release_unused_pages(struct mpage_da_data *mpd,
 			}
 			folio_unlock(folio);
 		}
-		pagevec_release(&pvec);
+		folio_batch_release(&fbatch);
 	}
 }
 
