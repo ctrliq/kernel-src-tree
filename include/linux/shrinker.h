8dced2b153abf1b58ceff2e25d0c4edf3edc9b2b
Chris von Recklinghausen <crecklin@redhat.com>
2023-03-24 07:44:19 -0400

    mm: shrinkers: provide shrinkers with names
    Bugzilla: https://bugzilla.redhat.com/2160210
    
    commit e33c267ab70de4249d22d7eab1cc7d68a889bac2
    Author: Roman Gushchin <roman.gushchin@linux.dev>
    Date:   Tue May 31 20:22:24 2022 -0700
    
        mm: shrinkers: provide shrinkers with names
    
        Currently shrinkers are anonymous objects.  For debugging purposes they
        can be identified by count/scan function names, but it's not always
        useful: e.g.  for superblock's shrinkers it's nice to have at least an
        idea of to which superblock the shrinker belongs.
    
        This commit adds names to shrinkers.  register_shrinker() and
        prealloc_shrinker() functions are extended to take a format and arguments
        to master a name.
    
        In some cases it's not possible to determine a good name at the time when
        a shrinker is allocated.  For such cases shrinker_debugfs_rename() is
        provided.
    
        The expected format is:
            <subsystem>-<shrinker_type>[:<instance>]-<id>
        For some shrinkers an instance can be encoded as (MAJOR:MINOR) pair.
    
        After this change the shrinker debugfs directory looks like:
          $ cd /sys/kernel/debug/shrinker/
          $ ls
            dquota-cache-16     sb-devpts-28     sb-proc-47       sb-tmpfs-42
            mm-shadow-18        sb-devtmpfs-5    sb-proc-48       sb-tmpfs-43
            mm-zspool:zram0-34  sb-hugetlbfs-17  sb-pstore-31     sb-tmpfs-44
            rcu-kfree-0         sb-hugetlbfs-33  sb-rootfs-2      sb-tmpfs-49
            sb-aio-20           sb-iomem-12      sb-securityfs-6  sb-tracefs-13
            sb-anon_inodefs-15  sb-mqueue-21     sb-selinuxfs-22  sb-xfs:vda1-36
            sb-bdev-3           sb-nsfs-4        sb-sockfs-8      sb-zsmalloc-19
            sb-bpf-32           sb-pipefs-14     sb-sysfs-26      thp-deferred_split-10
            sb-btrfs:vda2-24    sb-proc-25       sb-tmpfs-1       thp-zero-9
            sb-cgroup2-30       sb-proc-39       sb-tmpfs-27      xfs-buf:vda1-37
            sb-configfs-23      sb-proc-41       sb-tmpfs-29      xfs-inodegc:vda1-38
            sb-dax-11           sb-proc-45       sb-tmpfs-35
            sb-debugfs-7        sb-proc-46       sb-tmpfs-40
    
        [roman.gushchin@linux.dev: fix build warnings]
          Link: https://lkml.kernel.org/r/Yr+ZTnLb9lJk6fJO@castle
          Reported-by: kernel test robot <lkp@intel.com>
        Link: https://lkml.kernel.org/r/20220601032227.4076670-4-roman.gushchin@linux.dev
        Signed-off-by: Roman Gushchin <roman.gushchin@linux.dev>
        Cc: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
        Cc: Dave Chinner <dchinner@redhat.com>
        Cc: Hillf Danton <hdanton@sina.com>
        Cc: Kent Overstreet <kent.overstreet@gmail.com>
        Cc: Muchun Song <songmuchun@bytedance.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- include/linux/shrinker.h
+++ include/linux/shrinker.h
@@ -77,6 +77,7 @@ struct shrinker {
 #endif
 #ifdef CONFIG_SHRINKER_DEBUG
 	int debugfs_id;
+	const char *name;
 	struct dentry *debugfs_entry;
 #endif
 	/* objs pending delete, per node */
@@ -96,9 +97,11 @@ struct shrinker {
  */
 #define SHRINKER_NONSLAB	(1 << 3)
 
-extern int prealloc_shrinker(struct shrinker *shrinker);
+extern int __printf(2, 3) prealloc_shrinker(struct shrinker *shrinker,
+					    const char *fmt, ...);
 extern void register_shrinker_prepared(struct shrinker *shrinker);
-extern int register_shrinker(struct shrinker *shrinker);
+extern int __printf(2, 3) register_shrinker(struct shrinker *shrinker,
+					    const char *fmt, ...);
 extern void unregister_shrinker(struct shrinker *shrinker);
 extern void free_prealloced_shrinker(struct shrinker *shrinker);
 extern void synchronize_shrinkers(void);
@@ -106,6 +109,8 @@ extern void synchronize_shrinkers(void);
 #ifdef CONFIG_SHRINKER_DEBUG
 extern int shrinker_debugfs_add(struct shrinker *shrinker);
 extern void shrinker_debugfs_remove(struct shrinker *shrinker);
+extern int __printf(2, 3) shrinker_debugfs_rename(struct shrinker *shrinker,
+						  const char *fmt, ...);
 #else /* CONFIG_SHRINKER_DEBUG */
 static inline int shrinker_debugfs_add(struct shrinker *shrinker)
 {
@@ -113,6 +118,11 @@ static inline int shrinker_debugfs_add(struct shrinker *shrinker)
 }
 static inline void shrinker_debugfs_remove(struct shrinker *shrinker)
 {
+}
+static inline __printf(2, 3)
+int shrinker_debugfs_rename(struct shrinker *shrinker, const char *fmt, ...)
+{
+	return 0;
 }
 #endif /* CONFIG_SHRINKER_DEBUG */
 #endif /* _LINUX_SHRINKER_H */
