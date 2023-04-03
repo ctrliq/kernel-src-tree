313c1e66fedbb2531c7bc1aa1db9074dee746a4f
Waiman Long <longman@redhat.com>
2023-04-03 14:37:03 -0400

    tty: include list & lockdep from tty_ldisc.h
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2184097
    
    commit abca990183e933d8448a4791c2912b509a17e290
    Author: Jiri Slaby <jirislaby@kernel.org>
    Date:   Fri, 23 Jul 2021 12:31:43 +0200
    
        tty: include list & lockdep from tty_ldisc.h
    
        We use structs list_head and lockdep_map as non-pointers in tty_ldisc.h.
        So better have headers defining them explicitly included so that the
        structs are always defined. Not only implicitly via random include
        chains.
    
        Signed-off-by: Jiri Slaby <jslaby@suse.cz>
        Link: https://lore.kernel.org/r/20210723103147.18250-5-jslaby@suse.cz
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- include/linux/tty_ldisc.h
+++ include/linux/tty_ldisc.h
@@ -127,6 +127,8 @@ struct tty_struct;
 #include <linux/fs.h>
 #include <linux/wait.h>
 #include <linux/atomic.h>
+#include <linux/list.h>
+#include <linux/lockdep.h>
 #include <linux/seq_file.h>
 
 /*
