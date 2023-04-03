cad27a2931c55e1b12c645d9e16dc6f13380ae43
Mark Langsdorf <mlangsdo@redhat.com>
2023-04-03 11:33:55 -0400

    ACPI: x86: Introduce an acpi_quirk_skip_gpio_event_handlers() helper
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2176554
    Conflicts:
    	drivers/gpio/gpiolib-acpi.c - minor context differences
    
    commit 5adc409340b1fc82bc1175e602d14ac82ac685e3
    Author: Hans de Goede <hdegoede@redhat.com>
    Date: Wed, 1 Mar 2023 11:04:34 +0100
    
    x86 ACPI boards which ship with only Android as their factory image usually
    have pretty broken ACPI tables, relying on everything being hardcoded in
    the factory kernel image and often disabling parts of the ACPI enumeration
    kernel code to avoid the broken tables causing issues.
    
    Part of this broken ACPI code is that sometimes these boards have _AEI
    ACPI GPIO event handlers which are broken.
    
    So far this has been dealt with in the platform/x86/x86-android-tablets.c
    module, which contains various workarounds for these devices, by it calling
    acpi_gpiochip_free_interrupts() on gpiochip-s with troublesome handlers to
    disable the handlers.
    
    But in some cases this is too late, if the handlers are of the edge type
    then gpiolib-acpi.c's code will already have run them at boot.
    This can cause issues such as GPIOs ending up as owned by "ACPI:OpRegion",
    making them unavailable for drivers which actually need them.
    
    Boards with these broken ACPI tables are already listed in
    drivers/acpi/x86/utils.c for e.g. acpi_quirk_skip_i2c_client_enumeration().
    Extend the quirks mechanism for a new acpi_quirk_skip_gpio_event_handlers()
    helper, this re-uses the DMI-ids rather then having to duplicate the same
    DMI table in gpiolib-acpi.c .
    
    Also add the new ACPI_QUIRK_SKIP_GPIO_EVENT_HANDLERS quirk to existing
    boards with troublesome ACPI gpio event handlers, so that the current
    acpi_gpiochip_free_interrupts() hack can be removed from
    x86-android-tablets.c .
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Rafael J. Wysocki <rjw@rjwysocki.net>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -658,6 +658,7 @@ static inline bool acpi_quirk_skip_acpi_ac_and_battery(void)
 #if IS_ENABLED(CONFIG_X86_ANDROID_TABLETS)
 bool acpi_quirk_skip_i2c_client_enumeration(struct acpi_device *adev);
 int acpi_quirk_skip_serdev_enumeration(struct device *controller_parent, bool *skip);
+bool acpi_quirk_skip_gpio_event_handlers(void);
 #else
 static inline bool acpi_quirk_skip_i2c_client_enumeration(struct acpi_device *adev)
 {
@@ -668,6 +669,10 @@ acpi_quirk_skip_serdev_enumeration(struct device *controller_parent, bool *skip)
 {
 	*skip = false;
 	return 0;
+}
+static inline bool acpi_quirk_skip_gpio_event_handlers(void)
+{
+	return false;
 }
 #endif
 
