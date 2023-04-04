ca3ad9f7f3121395ca73d0cda330d5dd495a549d
Andrew Halaney <ahalaney@redhat.com>
2023-04-04 16:33:24 -0500

    scsi: ufs: Track system suspend / resume activity
    Bugzilla: https://bugzilla.redhat.com/2185234
    
    commit 1a547cbc6fdd07992f915a614a3f7ba3fccef8fb
    Author: Bart Van Assche <bvanassche@acm.org>
    Date:   Tue Oct 18 13:29:56 2022 -0700
    
        scsi: ufs: Track system suspend / resume activity
    
        Add a new boolean variable that tracks whether the system is suspending,
        suspended or resuming. This information will be used in a later commit to
        fix a deadlock between the SCSI error handler and the suspend code.
    
        Reviewed-by: Adrian Hunter <adrian.hunter@intel.com>
        Signed-off-by: Bart Van Assche <bvanassche@acm.org>
        Link: https://lore.kernel.org/r/20221018202958.1902564-9-bvanassche@acm.org
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Andrew Halaney <ahalaney@redhat.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -9253,6 +9253,7 @@ static int ufshcd_wl_suspend(struct device *dev)
 
 	hba = shost_priv(sdev->host);
 	down(&hba->host_sem);
+	hba->system_suspending = true;
 
 	if (pm_runtime_suspended(dev))
 		goto out;
@@ -9294,6 +9295,7 @@ out:
 		hba->curr_dev_pwr_mode, hba->uic_link_state);
 	if (!ret)
 		hba->is_sys_suspended = false;
+	hba->system_suspending = false;
 	up(&hba->host_sem);
 	return ret;
 }
