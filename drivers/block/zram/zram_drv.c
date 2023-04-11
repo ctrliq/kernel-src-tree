8ac0c377bdd7488ce7a6d00393999a086e56e0f7
Ming Lei <ming.lei@redhat.com>
2023-04-11 19:14:50 +0200

    zram: don't use highmem for the bounce buffer in zram_bvec_{read,write}
    JIRA: https://issues.redhat.com/browse/RHEL-29262
    
    commit f575a5add8a9a3ca593e58e218f2113e5bd3e50e
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Tue Apr 11 19:14:50 2023 +0200
    
        zram: don't use highmem for the bounce buffer in zram_bvec_{read,write}
    
        There is no point in allocation a highmem page when we instantly need to
        copy from it.
    
        Link: https://lkml.kernel.org/r/20230411171459.567614-9-hch@lst.de
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
        Acked-by: Minchan Kim <minchan@kernel.org>
        Cc: Jens Axboe <axboe@kernel.dk>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1442,7 +1442,7 @@ static int zram_bvec_read(struct zram *zram, struct bio_vec *bvec,
 	page = bvec->bv_page;
 	if (is_partial_io(bvec)) {
 		/* Use a temporary buffer to decompress the page */
-		page = alloc_page(GFP_NOIO|__GFP_HIGHMEM);
+		page = alloc_page(GFP_NOIO);
 		if (!page)
 			return -ENOMEM;
 	}
@@ -1451,12 +1451,8 @@ static int zram_bvec_read(struct zram *zram, struct bio_vec *bvec,
 	if (unlikely(ret))
 		goto out;
 
-	if (is_partial_io(bvec)) {
-		void *src = kmap_atomic(page);
-
-		memcpy_to_bvec(bvec, src + offset);
-		kunmap_atomic(src);
-	}
+	if (is_partial_io(bvec))
+		memcpy_to_bvec(bvec, page_address(page) + offset);
 out:
 	if (is_partial_io(bvec))
 		__free_page(page);
@@ -1600,12 +1596,11 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec,
 
 	vec = *bvec;
 	if (is_partial_io(bvec)) {
-		void *dst;
 		/*
 		 * This is a partial IO. We need to read the full page
 		 * before to write the changes.
 		 */
-		page = alloc_page(GFP_NOIO|__GFP_HIGHMEM);
+		page = alloc_page(GFP_NOIO);
 		if (!page)
 			return -ENOMEM;
 
@@ -1613,9 +1608,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec,
 		if (ret)
 			goto out;
 
-		dst = kmap_atomic(page);
-		memcpy_from_bvec(dst + offset, bvec);
-		kunmap_atomic(dst);
+		memcpy_from_bvec(page_address(page) + offset, bvec);
 
 		vec.bv_page = page;
 		vec.bv_len = PAGE_SIZE;
