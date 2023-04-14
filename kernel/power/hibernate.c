df4804af32ced1e933361ad343020246fd070905
Ming Lei <ming.lei@redhat.com>
2023-04-14 20:10:26 +0800

    PM: hibernate: Turn snapshot_test into global variable
    JIRA: https://issues.redhat.com/browse/RHEL-1516
    
    commit 08169a162f97819d3e5b4a342bb9cf5137787154
    Author: Chen Yu <yu.c.chen@intel.com>
    Date:   Fri Apr 14 20:10:26 2023 +0800
    
        PM: hibernate: Turn snapshot_test into global variable
    
        There is need to check snapshot_test and open block device
        in different mode, so as to avoid the race condition.
    
        No functional changes intended.
    
        Suggested-by: Pavankumar Kondeti <quic_pkondeti@quicinc.com>
        Signed-off-by: Chen Yu <yu.c.chen@intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -64,6 +64,7 @@ enum {
 static int hibernation_mode = HIBERNATION_SHUTDOWN;
 
 bool freezer_test_done;
+bool snapshot_test;
 
 static const struct platform_hibernation_ops *hibernation_ops;
 
@@ -712,7 +713,6 @@ static int load_image_and_restore(void)
  */
 int hibernate(void)
 {
-	bool snapshot_test = false;
 	int error;
 
 	if (!hibernation_available()) {
@@ -738,6 +738,9 @@ int hibernate(void)
 	error = freeze_processes();
 	if (error)
 		goto Exit;
+
+	/* protected by system_transition_mutex */
+	snapshot_test = false;
 
 	lock_device_hotplug();
 	/* Allocate memory management structures */
@@ -933,6 +936,8 @@ static int software_resume(void)
 	 * here to avoid lockdep complaining.
 	 */
 	mutex_lock_nested(&system_transition_mutex, SINGLE_DEPTH_NESTING);
+
+	snapshot_test = false;
 
 	if (swsusp_resume_device)
 		goto Check_image;
