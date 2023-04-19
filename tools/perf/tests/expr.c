27558c26d69c857e62b218dde56b9156acd9b300
Michael Petlan <mpetlan@redhat.com>
2023-04-19 13:44:47 +0200

    perf smt: Compute SMT from topology
    Bugzilla: https://bugzilla.redhat.com/2177180
    
    upstream
    ========
    commit 09b73fe9e3debfeed61c1395652aeff59bda6ae4
    Author: Ian Rogers <irogers@google.com>
    Date: Wed Aug 31 10:49:22 2022 -0700
    
    description
    ===========
    The topology records sibling threads. Rather than computing SMT using
    siblings in sysfs, reuse the values in topology. This only applies
    when the file smt/active isn't available.
    
        Signed-off-by: Ian Rogers <irogers@google.com>
        Cc: Ahmad Yasin <ahmad.yasin@intel.com>
        Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
        Cc: Andi Kleen <ak@linux.intel.com>
        Cc: Caleb Biggers <caleb.biggers@intel.com>
        Cc: Florian Fischer <florian.fischer@muhq.space>
        Cc: Ingo Molnar <mingo@redhat.com>
        Cc: James Clark <james.clark@arm.com>
        Cc: Jiri Olsa <jolsa@kernel.org>
        Cc: John Garry <john.garry@huawei.com>
        Cc: Kan Liang <kan.liang@linux.intel.com>
        Cc: Kshipra Bopardikar <kshipra.bopardikar@intel.com>
        Cc: Mark Rutland <mark.rutland@arm.com>
        Cc: Miaoqian Lin <linmq006@gmail.com>
        Cc: Namhyung Kim <namhyung@kernel.org>
        Cc: Perry Taylor <perry.taylor@intel.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Stephane Eranian <eranian@google.com>
        Cc: Thomas Richter <tmricht@linux.ibm.com>
        Cc: Xing Zhengjun <zhengjun.xing@linux.intel.com>
        Link: https://lore.kernel.org/r/20220831174926.579643-4-irogers@google.com
        Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    
    Conflicts:
    ==========
    Due to already backported c302378bc157 ("libbpf: Hashmap interface
    update to allow both long and void* keys/values"), some types changed.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- tools/perf/tests/expr.c
+++ tools/perf/tests/expr.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
+#include "util/cputopo.h"
 #include "util/debug.h"
 #include "util/expr.h"
 #include "util/header.h"
@@ -147,14 +148,20 @@ static int test__expr(struct test_suite *t __maybe_unused, int subtest __maybe_u
 	TEST_ASSERT_VAL("find ids", hashmap__find(ctx->ids, "dash-event2", &val_ptr));
 
 	/* Only EVENT1 or EVENT2 need be measured depending on the value of smt_on. */
-	expr__ctx_clear(ctx);
-	TEST_ASSERT_VAL("find ids",
-			expr__find_ids("EVENT1 if #smt_on else EVENT2",
-				NULL, ctx) == 0);
-	TEST_ASSERT_VAL("find ids", hashmap__size(ctx->ids) == 1);
-	TEST_ASSERT_VAL("find ids", hashmap__find(ctx->ids,
-						  smt_on() ? "EVENT1" : "EVENT2",
-						  &val_ptr));
+	{
+		struct cpu_topology *topology = cpu_topology__new();
+		bool smton = smt_on(topology);
+
+		cpu_topology__delete(topology);
+		expr__ctx_clear(ctx);
+		TEST_ASSERT_VAL("find ids",
+				expr__find_ids("EVENT1 if #smt_on else EVENT2",
+					NULL, ctx) == 0);
+		TEST_ASSERT_VAL("find ids", hashmap__size(ctx->ids) == 1);
+ 		TEST_ASSERT_VAL("find ids", hashmap__find(ctx->ids,
+							  smton ? "EVENT1" : "EVENT2",
+							  &val_ptr));
+	}
 
 	/* The expression is a constant 1.0 without needing to evaluate EVENT1. */
 	expr__ctx_clear(ctx);
