df3cfdab96ede869f3fa90213704d9dbe46717e0
Michael Petlan <mpetlan@redhat.com>
2023-04-19 13:43:19 +0200

    perf/core: Convert snprintf() to scnprintf()
    Bugzilla: https://bugzilla.redhat.com/2177180
    
    upstream
    ========
    commit dca6344d7a77dd0501a73745f4a9fb1ee2bc9d7c
    Author: Jules Irenge <jbi.octave@gmail.com>
    Date: Sun Sep 18 00:41:08 2022 +0100
    
    description
    ===========
    Coccinelle reports a warning:
    
        WARNING: use scnprintf or sprintf
    
    This LWN article explains the rationale for this change:
    
        https: //lwn.net/Articles/69419/
    
    Ie. snprintf() returns what *would* be the resulting length,
    while scnprintf() returns the actual length.
    
    Adding to that, there has also been some slow migration from snprintf to scnprintf,
    here's the shift in usage in the past 3.5 years, in all fs/ files:
    
                             v5.0    v6.0-rc6
       --------------------------------------
       snprintf() uses:        63         213
       scnprintf() uses:      374         186
    
    No intended change in behavior.
    
    [ mingo: Improved the changelog & reviewed the usage sites. ]
    
        Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -10967,7 +10967,7 @@ static ssize_t nr_addr_filters_show(struct device *dev,
 {
 	struct pmu *pmu = dev_get_drvdata(dev);
 
-	return snprintf(page, PAGE_SIZE - 1, "%d\n", pmu->nr_addr_filters);
+	return scnprintf(page, PAGE_SIZE - 1, "%d\n", pmu->nr_addr_filters);
 }
 DEVICE_ATTR_RO(nr_addr_filters);
 
@@ -10978,7 +10978,7 @@ type_show(struct device *dev, struct device_attribute *attr, char *page)
 {
 	struct pmu *pmu = dev_get_drvdata(dev);
 
-	return snprintf(page, PAGE_SIZE-1, "%d\n", pmu->type);
+	return scnprintf(page, PAGE_SIZE - 1, "%d\n", pmu->type);
 }
 static DEVICE_ATTR_RO(type);
 
@@ -10989,7 +10989,7 @@ perf_event_mux_interval_ms_show(struct device *dev,
 {
 	struct pmu *pmu = dev_get_drvdata(dev);
 
-	return snprintf(page, PAGE_SIZE-1, "%d\n", pmu->hrtimer_interval_ms);
+	return scnprintf(page, PAGE_SIZE - 1, "%d\n", pmu->hrtimer_interval_ms);
 }
 
 static DEFINE_MUTEX(mux_interval_mutex);
