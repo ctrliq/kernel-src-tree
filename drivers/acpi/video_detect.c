81495af2504cc37db564691023d7db3429a8a2bb
Mark Langsdorf <mlangsdo@redhat.com>
2023-04-25 14:16:31 -0400

    ACPI: video: Drop backlight_device_get_by_type() call from acpi_video_get_backlight_type()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2176554
    
    commit a2fc3c899bb091ec69cd74d4d69d71f8b2acdc11
    Author: Hans de Goede <hdegoede@redhat.com>
    Date:   Thu Apr 28 14:45:35 2022 +0200
    
    All x86/ACPI kms drivers which register native/BACKLIGHT_RAW type
    backlight devices call acpi_video_backlight_use_native() now. This sets
    __acpi_video_get_backlight_type()'s internal static native_available flag.
    
    This makes the backlight_device_get_by_type(BACKLIGHT_RAW) check
    unnecessary.
    
    Relying on the cached native_available value not only is simpler, it will
    also work correctly in cases where then native backlight registration was
    skipped because of acpi_video_backlight_use_native() returning false.
    
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/acpi/video_detect.c
+++ drivers/acpi/video_detect.c
@@ -666,8 +666,7 @@ enum acpi_backlight_type __acpi_video_get_backlight_type(bool native, bool *auto
 	if (!(video_caps & ACPI_VIDEO_BACKLIGHT))
 		return acpi_backlight_vendor;
 
-	if (acpi_osi_is_win8() &&
-	    (native_available || backlight_device_get_by_type(BACKLIGHT_RAW)))
+	if (acpi_osi_is_win8() && native_available)
 		return acpi_backlight_native;
 
 	return acpi_backlight_video;
