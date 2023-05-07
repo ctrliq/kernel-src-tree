ea18b379388963b5bf3ac378cc0f3f6c64f32f3b
Mohammad Kabat <mkabat@redhat.com>
2023-05-07 11:52:26 +0000

    net/mlx5e: Remove accesses to priv for low level IPsec FS code
    Bugzilla: http://bugzilla.redhat.com/2165320
    Upstream-status: v6.2-rc1
    
    commit c7049ca6213732e5d511a4b8d6fdabea4c3b4811
    Author: Leon Romanovsky <leon@kernel.org>
    Date:   Fri Dec 2 22:10:29 2022 +0200
    
        net/mlx5e: Remove accesses to priv for low level IPsec FS code
    
        mlx5 priv structure is driver main structure that holds high level data.
        That information is not needed for IPsec flow steering logic and the
        pointer to mlx5e_priv was not supposed to be passed in the first place.
    
        This change "cleans" the logic to rely on internal to IPsec structures
        without touching global mlx5e_priv.
    
        Reviewed-by: Raed Salem <raeds@nvidia.com>
        Reviewed-by: Saeed Mahameed <saeedm@nvidia.com>
        Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
        Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    
    Signed-off-by: Mohammad Kabat <mkabat@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_accel/ipsec.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_accel/ipsec.c
@@ -307,7 +307,7 @@ static int mlx5e_xfrm_add_state(struct xfrm_state *x,
 	if (err)
 		goto err_xfrm;
 
-	err = mlx5e_accel_ipsec_fs_add_rule(priv, sa_entry);
+	err = mlx5e_accel_ipsec_fs_add_rule(sa_entry);
 	if (err)
 		goto err_hw_ctx;
 
@@ -325,7 +325,7 @@ static int mlx5e_xfrm_add_state(struct xfrm_state *x,
 	goto out;
 
 err_add_rule:
-	mlx5e_accel_ipsec_fs_del_rule(priv, sa_entry);
+	mlx5e_accel_ipsec_fs_del_rule(sa_entry);
 err_hw_ctx:
 	mlx5_ipsec_free_sa_ctx(sa_entry);
 err_xfrm:
@@ -345,10 +345,9 @@ static void mlx5e_xfrm_del_state(struct xfrm_state *x)
 static void mlx5e_xfrm_free_state(struct xfrm_state *x)
 {
 	struct mlx5e_ipsec_sa_entry *sa_entry = to_ipsec_sa_entry(x);
-	struct mlx5e_priv *priv = netdev_priv(x->xso.dev);
 
 	cancel_work_sync(&sa_entry->modify_work.work);
-	mlx5e_accel_ipsec_fs_del_rule(priv, sa_entry);
+	mlx5e_accel_ipsec_fs_del_rule(sa_entry);
 	mlx5_ipsec_free_sa_ctx(sa_entry);
 	kfree(sa_entry);
 }
@@ -379,6 +378,7 @@ void mlx5e_ipsec_init(struct mlx5e_priv *priv)
 	if (ret)
 		goto err_fs_init;
 
+	ipsec->fs = priv->fs;
 	priv->ipsec = ipsec;
 	netdev_dbg(priv->netdev, "IPSec attached to netdevice\n");
 	return;
