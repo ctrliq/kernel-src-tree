a2739bb14d0ca402521ef285cf1039eff9af217e
Nico Pache <npache@redhat.com>
2023-05-08 17:26:29 -0600

    mm/slub: remove dead code for debug caches on deactivate_slab()
    commit a8e53869995b90609a798f9830d44086ab6025c4
    Author: Hyeonggon Yoo <42.hyeyoo@gmail.com>
    Date:   Fri Oct 14 20:43:22 2022 +0900
    
        mm/slub: remove dead code for debug caches on deactivate_slab()
    
        After commit c7323a5ad0786 ("mm/slub: restrict sysfs validation to debug
        caches and make it safe"), SLUB never installs percpu slab for debug caches
        and thus never deactivates percpu slab for them.
    
        Since only debug caches use the full list, SLUB no longer deactivates to
        full list. Remove dead code in deactivate_slab().
    
        Signed-off-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>
        Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2168372
    Signed-off-by: Nico Pache <npache@redhat.com>

--- mm/slub.c
+++ mm/slub.c
@@ -2358,7 +2358,7 @@ static void init_kmem_cache_cpus(struct kmem_cache *s)
 static void deactivate_slab(struct kmem_cache *s, struct slab *slab,
 			    void *freelist)
 {
-	enum slab_modes { M_NONE, M_PARTIAL, M_FULL, M_FREE, M_FULL_NOLIST };
+	enum slab_modes { M_NONE, M_PARTIAL, M_FREE, M_FULL_NOLIST };
 	struct kmem_cache_node *n = get_node(s, slab_nid(slab));
 	int free_delta = 0;
 	enum slab_modes mode = M_NONE;
@@ -2434,14 +2434,6 @@ redo:
 		 * acquire_slab() will see a slab that is frozen
 		 */
 		spin_lock_irqsave(&n->list_lock, flags);
-	} else if (kmem_cache_debug_flags(s, SLAB_STORE_USER)) {
-		mode = M_FULL;
-		/*
-		 * This also ensures that the scanning of full
-		 * slabs from diagnostic functions will not see
-		 * any frozen slabs.
-		 */
-		spin_lock_irqsave(&n->list_lock, flags);
 	} else {
 		mode = M_FULL_NOLIST;
 	}
@@ -2451,7 +2443,7 @@ redo:
 				old.freelist, old.counters,
 				new.freelist, new.counters,
 				"unfreezing slab")) {
-		if (mode == M_PARTIAL || mode == M_FULL)
+		if (mode == M_PARTIAL)
 			spin_unlock_irqrestore(&n->list_lock, flags);
 		goto redo;
 	}
@@ -2465,10 +2457,6 @@ redo:
 		stat(s, DEACTIVATE_EMPTY);
 		discard_slab(s, slab);
 		stat(s, FREE_SLAB);
-	} else if (mode == M_FULL) {
-		add_full(s, n, slab);
-		spin_unlock_irqrestore(&n->list_lock, flags);
-		stat(s, DEACTIVATE_FULL);
 	} else if (mode == M_FULL_NOLIST) {
 		stat(s, DEACTIVATE_FULL);
 	}
