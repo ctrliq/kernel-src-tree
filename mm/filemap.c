26e1647308c0865de66f03a1f068fa75747ac4ad
Nico Pache <npache@redhat.com>
2023-05-08 17:26:30 -0600

    mm/filemap: make folio_put_wait_locked static
    commit c195c3215741746b1eb7ab7980b926ddc37a4be3
    Author: Ke Sun <sunke@kylinos.cn>
    Date:   Wed Sep 14 10:17:38 2022 +0800
    
        mm/filemap: make folio_put_wait_locked static
    
        It's only used in mm/filemap.c, since commit <ffa65753c431>
        ("mm/migrate.c: rework migration_entry_wait() to not take a pageref").
    
        Make it static.
    
        Link: https://lkml.kernel.org/r/20220914021738.3228011-1-sunke@kylinos.cn
        Signed-off-by: Ke Sun <sunke@kylinos.cn>
        Reported-by: k2ci <kernel-bot@kylinos.cn>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2168372
    Signed-off-by: Nico Pache <npache@redhat.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1459,7 +1459,7 @@ EXPORT_SYMBOL(folio_wait_bit_killable);
  *
  * Return: 0 if the folio was unlocked or -EINTR if interrupted by a signal.
  */
-int folio_put_wait_locked(struct folio *folio, int state)
+static int folio_put_wait_locked(struct folio *folio, int state)
 {
 	return folio_wait_bit_common(folio, PG_locked, state, DROP);
 }
