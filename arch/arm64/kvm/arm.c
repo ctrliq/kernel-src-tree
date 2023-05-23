3d07d2fcb3d0a66e11fbc4fcd5fe126a8ac0b567
Maxim Levitsky <mlevitsk@redhat.com>
2023-05-23 13:08:11 +0300

    KVM: arm64: Unregister perf callbacks if hypervisor finalization fails
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2177720
    
    commit 78b3bf485d5cb5b9a11ff6e1d4b76cbf2f2360ef
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Wed Nov 30 23:09:02 2022 +0000
    
        KVM: arm64: Unregister perf callbacks if hypervisor finalization fails
    
        Undo everything done by init_subsystems() if a later initialization step
        fails, i.e. unregister perf callbacks in addition to unregistering the
        power management notifier.
    
        Fixes: bfa79a805454 ("KVM: arm64: Elevate hypervisor mappings creation at EL2")
        Signed-off-by: Sean Christopherson <seanjc@google.com>
        Message-Id: <20221130230934.1014142-19-seanjc@google.com>
        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1843,10 +1843,19 @@ static int init_subsystems(void)
 	kvm_register_perf_callbacks(NULL);
 
 out:
+	if (err)
+		hyp_cpu_pm_exit();
+
 	if (err || !is_protected_kvm_enabled())
 		on_each_cpu(_kvm_arch_hardware_disable, NULL, 1);
 
 	return err;
+}
+
+static void teardown_subsystems(void)
+{
+	kvm_unregister_perf_callbacks();
+	hyp_cpu_pm_exit();
 }
 
 static void teardown_hyp_mode(void)
@@ -2252,7 +2261,7 @@ int kvm_arch_init(void *opaque)
 
 	err = init_subsystems();
 	if (err)
-		goto out_subs;
+		goto out_hyp;
 
 	if (!in_hyp_mode) {
 		err = finalize_hyp_mode();
@@ -2273,7 +2282,7 @@ int kvm_arch_init(void *opaque)
 	return 0;
 
 out_subs:
-	hyp_cpu_pm_exit();
+	teardown_subsystems();
 out_hyp:
 	if (!in_hyp_mode)
 		teardown_hyp_mode();
