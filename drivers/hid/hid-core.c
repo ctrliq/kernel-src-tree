685f4f87de23434b0f7753f6751c82654de661e4
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2023-05-23 15:54:25 +0200

    HID: bpf: allocate data memory for device_event BPF programs
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2166610
    Upstream Status: since v6.3
    Tested: with the hid-tools test suite and some hardware
    
    commit 658ee5a64fcfbbf758447fa3af425729eaabb0dc
    Author: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Date:   Thu Nov 3 16:57:47 2022 +0100
    
        HID: bpf: allocate data memory for device_event BPF programs
    
        We need to also be able to change the size of the report.
        Reducing it is easy, because we already have the incoming buffer that is
        big enough, but extending it is harder.
    
        Pre-allocate a buffer that is big enough to handle all reports of the
        device, and use that as the primary buffer for BPF programs.
        To be able to change the size of the buffer, we change the device_event
        API and request it to return the size of the buffer.
    
        Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
        Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -2050,9 +2050,11 @@ int hid_input_report(struct hid_device *hid, enum hid_report_type type, u8 *data
 	report_enum = hid->report_enum + type;
 	hdrv = hid->driver;
 
-	ret = dispatch_hid_bpf_device_event(hid, type, data, size, interrupt);
-	if (ret)
+	data = dispatch_hid_bpf_device_event(hid, type, data, &size, interrupt);
+	if (IS_ERR(data)) {
+		ret = PTR_ERR(data);
 		goto unlock;
+	}
 
 	if (!size) {
 		dbg_hid("empty report\n");
@@ -2168,6 +2170,10 @@ int hid_connect(struct hid_device *hdev, unsigned int connect_mask)
 	int len;
 	int ret;
 
+	ret = hid_bpf_connect_device(hdev);
+	if (ret)
+		return ret;
+
 	if (hdev->quirks & HID_QUIRK_HIDDEV_FORCE)
 		connect_mask |= (HID_CONNECT_HIDDEV_FORCE | HID_CONNECT_HIDDEV);
 	if (hdev->quirks & HID_QUIRK_HIDINPUT_FORCE)
@@ -2269,6 +2275,8 @@ void hid_disconnect(struct hid_device *hdev)
 	if (hdev->claimed & HID_CLAIMED_HIDRAW)
 		hidraw_disconnect(hdev);
 	hdev->claimed = 0;
+
+	hid_bpf_disconnect_device(hdev);
 }
 EXPORT_SYMBOL_GPL(hid_disconnect);
 
