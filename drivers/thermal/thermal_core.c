05ece3c56cb3990325269ebad264e6bef3869d86
Eric Auger <eric.auger@redhat.com>
2023-05-23 08:55:17 -0400

    thermal/of: Make new code and old code co-exist
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2183350
    
    This transient change allows to use old and new OF together until all
    the drivers are converted to use the new OF API.
    
    This will go away when the old OF code will be removed.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linexp.org>
    Link: https://lore.kernel.org/r/20220804224349.1926752-3-daniel.lezcano@linexp.org
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    (cherry picked from commit 48ad3b104b9ec85de58c2b4e38fdad9a26446f99)
    Signed-off-by: Eric Auger <eric.auger@redhat.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1592,10 +1592,6 @@ static int __init thermal_init(void)
 	if (result)
 		goto unregister_governors;
 
-	result = of_parse_thermal_zones();
-	if (result)
-		goto unregister_class;
-
 	result = register_pm_notifier(&thermal_pm_nb);
 	if (result)
 		pr_warn("Thermal: Can not register suspend notifier, return %d\n",
@@ -1603,8 +1599,6 @@ static int __init thermal_init(void)
 
 	return 0;
 
-unregister_class:
-	class_unregister(&thermal_class);
 unregister_governors:
 	thermal_unregister_governors();
 error:
