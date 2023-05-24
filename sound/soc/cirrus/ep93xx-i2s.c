bf6cf85f9fb8ff3f21608530e5eb9b7fe062c424
Jaroslav Kysela <jkysela@redhat.com>
2023-05-24 09:51:21 +0200

    ASoC: ep93xx: Add OF support
    Bugzilla: https://bugzilla.redhat.com/2179848
    
    Omitted-fix: 13b1b95245a7 "ASoC: ep93xx: Add I2S description"
                 not supported in RHEL
    
    commit b599a4d7d905a8e8cf5c36ccae0b3dd97c956075
    Author: Alexander Sverdlin <alexander.sverdlin@gmail.com>
    Date: Tue Apr 11 18:59:51 2023 +0200
    
        ASoC: ep93xx: Add OF support
    
        Add the new cirrus,ep9301-ac97 and cirrus,ep9301-i2s compatibles.
    
        Signed-off-by: Alexander Sverdlin <alexander.sverdlin@gmail.com>
        Link: https://lore.kernel.org/r/20230411165951.2335899-2-alexander.sverdlin@gmail.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/soc/cirrus/ep93xx-i2s.c
+++ sound/soc/cirrus/ep93xx-i2s.c
@@ -15,6 +15,7 @@
 #include <linux/slab.h>
 #include <linux/clk.h>
 #include <linux/io.h>
+#include <linux/of.h>
 
 #include <sound/core.h>
 #include <sound/dmaengine_pcm.h>
@@ -513,11 +514,18 @@ static int ep93xx_i2s_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static const struct of_device_id ep93xx_i2s_of_ids[] = {
+	{ .compatible = "cirrus,ep9301-i2s" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, ep93xx_i2s_of_ids);
+
 static struct platform_driver ep93xx_i2s_driver = {
 	.probe	= ep93xx_i2s_probe,
 	.remove	= ep93xx_i2s_remove,
 	.driver	= {
 		.name	= "ep93xx-i2s",
+		.of_match_table = ep93xx_i2s_of_ids,
 	},
 };
 
