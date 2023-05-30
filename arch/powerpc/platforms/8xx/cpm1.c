5e8a6f1b625916777fb12d8e21cdb3bf215f705d
Jaroslav Kysela <jkysela@redhat.com>
2023-05-30 17:32:43 +0200

    powerpc/8xx: Use a larger CPM1 command check mask
    Bugzilla: https://bugzilla.redhat.com/2179848
    
    commit b38736ac01e34491f339e12f9883dc4a8cc3d358
    Author: Herve Codina <herve.codina@bootlin.com>
    Date: Fri Feb 17 15:56:39 2023 +0100
    
        powerpc/8xx: Use a larger CPM1 command check mask
    
        The CPM1 command mask is defined for use with the standard
        CPM1 command register as described in the user's manual:
          0  |1        3|4    7|8   11|12      14| 15|
          RST|    -     |OPCODE|CH_NUM|     -    |FLG|
    
        In the QMC extension the CPM1 command register is redefined
        (QMC supplement user's manuel) with the following mapping:
          0  |1        3|4    7|8           13|14| 15|
          RST|QMC OPCODE|  1110|CHANNEL_NUMBER| -|FLG|
    
        Extend the check command mask in order to support both the
        standard CH_NUM field and the QMC extension CHANNEL_NUMBER
        field.
    
        Signed-off-by: Herve Codina <herve.codina@bootlin.com>
        Acked-by: Christophe Leroy <christophe.leroy@csgroup.eu>
        Acked-by: Michael Ellerman <mpe@ellerman.id.au>
        Link: https://lore.kernel.org/r/20230217145645.1768659-5-herve.codina@bootlin.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- arch/powerpc/platforms/8xx/cpm1.c
+++ arch/powerpc/platforms/8xx/cpm1.c
@@ -233,7 +233,7 @@ int cpm_command(u32 command, u8 opcode)
 	int i, ret;
 	unsigned long flags;
 
-	if (command & 0xffffff0f)
+	if (command & 0xffffff03)
 		return -EINVAL;
 
 	spin_lock_irqsave(&cmd_lock, flags);
