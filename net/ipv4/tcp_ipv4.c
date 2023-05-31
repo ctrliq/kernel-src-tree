1cfc972fac24fe935bdc47b1ca216a42cbc7957e
Antoine Tenart <atenart@redhat.com>
2023-05-31 18:37:11 +0200

    net: ipv4: use consistent txhash in TIME_WAIT and SYN_RECV
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2214966
    Upstream Status: net-next.git
    Conflicts:\
    - Context difference due to missing upstream commit e22aa1486668 ("net:
      Find dst with sk's xfrm policy not ctl_sk") in c9s.
    
    commit c0a8966e2bc7d31f77a7246947ebc09c1ff06066
    Author: Antoine Tenart <atenart@kernel.org>
    Date:   Tue May 23 18:14:52 2023 +0200
    
        net: ipv4: use consistent txhash in TIME_WAIT and SYN_RECV
    
        When using IPv4/TCP, skb->hash comes from sk->sk_txhash except in
        TIME_WAIT and SYN_RECV where it's not set in the reply skb from
        ip_send_unicast_reply. Those packets will have a mismatched hash with
        others from the same flow as their hashes will be 0. IPv6 does not have
        the same issue as the hash is set from the socket txhash in those cases.
    
        This commits sets the hash in the reply skb from ip_send_unicast_reply,
        which makes the IPv4 code behaving like IPv6.
    
        Signed-off-by: Antoine Tenart <atenart@kernel.org>
        Reviewed-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -683,6 +683,7 @@ static void tcp_v4_send_reset(const struct sock *sk, struct sk_buff *skb)
 	u64 transmit_time = 0;
 	struct sock *ctl_sk;
 	struct net *net;
+	u32 txhash = 0;
 
 	/* Never send a reset in response to a reset. */
 	if (th->rst)
@@ -818,6 +819,8 @@ static void tcp_v4_send_reset(const struct sock *sk, struct sk_buff *skb)
 		ctl_sk->sk_priority = (sk->sk_state == TCP_TIME_WAIT) ?
 				   inet_twsk(sk)->tw_priority : sk->sk_priority;
 		transmit_time = tcp_transmit_time(sk);
+		txhash = (sk->sk_state == TCP_TIME_WAIT) ?
+			 inet_twsk(sk)->tw_txhash : sk->sk_txhash;
 	} else {
 		ctl_sk->sk_mark = 0;
 		ctl_sk->sk_priority = 0;
@@ -826,7 +829,7 @@ static void tcp_v4_send_reset(const struct sock *sk, struct sk_buff *skb)
 			      skb, &TCP_SKB_CB(skb)->header.h4.opt,
 			      ip_hdr(skb)->saddr, ip_hdr(skb)->daddr,
 			      &arg, arg.iov[0].iov_len,
-			      transmit_time);
+			      transmit_time, txhash);
 
 	__TCP_INC_STATS(net, TCP_MIB_OUTSEGS);
 	__TCP_INC_STATS(net, TCP_MIB_OUTRSTS);
@@ -846,7 +849,7 @@ static void tcp_v4_send_ack(const struct sock *sk,
 			    struct sk_buff *skb, u32 seq, u32 ack,
 			    u32 win, u32 tsval, u32 tsecr, int oif,
 			    struct tcp_md5sig_key *key,
-			    int reply_flags, u8 tos)
+			    int reply_flags, u8 tos, u32 txhash)
 {
 	const struct tcphdr *th = tcp_hdr(skb);
 	struct {
@@ -921,7 +924,7 @@ static void tcp_v4_send_ack(const struct sock *sk,
 			      skb, &TCP_SKB_CB(skb)->header.h4.opt,
 			      ip_hdr(skb)->saddr, ip_hdr(skb)->daddr,
 			      &arg, arg.iov[0].iov_len,
-			      transmit_time);
+			      transmit_time, txhash);
 
 	__TCP_INC_STATS(net, TCP_MIB_OUTSEGS);
 	local_bh_enable();
@@ -940,7 +943,8 @@ static void tcp_v4_timewait_ack(struct sock *sk, struct sk_buff *skb)
 			tw->tw_bound_dev_if,
 			tcp_twsk_md5_key(tcptw),
 			tw->tw_transparent ? IP_REPLY_ARG_NOSRCCHECK : 0,
-			tw->tw_tos
+			tw->tw_tos,
+			tw->tw_txhash
 			);
 
 	inet_twsk_put(tw);
@@ -973,7 +977,7 @@ static void tcp_v4_reqsk_send_ack(const struct sock *sk, struct sk_buff *skb,
 			0,
 			tcp_md5_do_lookup(sk, l3index, addr, AF_INET),
 			inet_rsk(req)->no_srccheck ? IP_REPLY_ARG_NOSRCCHECK : 0,
-			ip_hdr(skb)->tos);
+			ip_hdr(skb)->tos, tcp_rsk(req)->txhash);
 }
 
 /*
