14cf5a769cbb6a3fd6554e2b387b37b301713735
Ming Lei <ming.lei@redhat.com>
2023-06-01 11:44:53 +0200

    block: add a mark_dead holder operation
    JIRA: https://issues.redhat.com/browse/RHEL-1516
    
    commit f55e017c642051ddc01d77a89ab18f5ee71d6276
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Thu Jun 1 11:44:53 2023 +0200
    
        block: add a mark_dead holder operation
    
        Add a mark_dead method to blk_holder_ops that is called from blk_mark_disk_dead
        to notify the holder that the block device it is using has been marked dead.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Jan Kara <jack@suse.cz>
        Acked-by: Christian Brauner <brauner@kernel.org>
        Acked-by: Dave Chinner <dchinner@redhat.com>
        Reviewed-by: Dave Chinner <dchinner@redhat.com>
        Link: https://lore.kernel.org/r/20230601094459.1350643-11-hch@lst.de
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1484,6 +1484,7 @@ void blkdev_show(struct seq_file *seqf, off_t offset);
 #endif
 
 struct blk_holder_ops {
+	void (*mark_dead)(struct block_device *bdev);
 };
 
 struct block_device *blkdev_get_by_dev(dev_t dev, fmode_t mode, void *holder,
