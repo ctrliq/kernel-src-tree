b9318acc2959e85531bd747e5c227e930881e828
Jeff Moyer <jmoyer@redhat.com>
2023-06-02 08:41:46 -0600

    io_uring: avoid indirect function calls for the hottest task_work
    JIRA: https://issues.redhat.com/browse/RHEL-12076
    
    commit c92fcfc2bab54451c4f1481755ea244f413455cb
    Author: Jens Axboe <axboe@kernel.dk>
    Date:   Fri Jun 2 08:41:46 2023 -0600
    
        io_uring: avoid indirect function calls for the hottest task_work
        
        We use task_work for a variety of reasons, but doing completions or
        triggering rety after poll are by far the hottest two. Use the indirect
        funtion call wrappers to avoid the indirect function call if
        CONFIG_RETPOLINE is set.
        
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -96,6 +96,7 @@
 
 #include "timeout.h"
 #include "poll.h"
+#include "rw.h"
 #include "alloc_cache.h"
 
 #define IORING_MAX_ENTRIES	32768
@@ -1231,7 +1232,9 @@ static unsigned int handle_tw_list(struct llist_node *node,
 			ts->locked = mutex_trylock(&(*ctx)->uring_lock);
 			percpu_ref_get(&(*ctx)->refs);
 		}
-		req->io_task_work.func(req, ts);
+		INDIRECT_CALL_2(req->io_task_work.func,
+				io_poll_task_func, io_req_rw_complete,
+				req, ts);
 		node = next;
 		count++;
 		if (unlikely(need_resched())) {
@@ -1441,7 +1444,9 @@ again:
 		struct io_kiocb *req = container_of(node, struct io_kiocb,
 						    io_task_work.node);
 		prefetch(container_of(next, struct io_kiocb, io_task_work.node));
-		req->io_task_work.func(req, ts);
+		INDIRECT_CALL_2(req->io_task_work.func,
+				io_poll_task_func, io_req_rw_complete,
+				req, ts);
 		ret++;
 		node = next;
 	}
