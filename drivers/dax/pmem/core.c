d950665a169e4d6cb4aefcb9423884a6fd7f919b
Jeff Moyer <jmoyer@redhat.com>
2023-06-02 23:14:11 -0700

    dax: Cleanup extra dax_region references
    JIRA: https://issues.redhat.com/browse/RHEL-23824
    Conflicts: RHEL does not have commit 09d09e04d2fc ("cxl/dax: Create
    dax devices for CXL RAM regions"), so drop the changes to
    drivers/dax/cxl.c.  RHEL also does not have commit 83762cb5c7c4 ("dax:
    Kill DEV_DAX_PMEM_COMPAT"), so apply changes to
    drivers/dax/pmem/core.c instead of drivers/dax/pmem.c
    
    commit 2532f41607c4308733239dd43278f8a5540f3ec7
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Fri Jun 2 23:14:11 2023 -0700
    
        dax: Cleanup extra dax_region references
        
        Now that free_dev_dax_id() internally manages the references it needs
        the extra references taken by the dax_region drivers are not needed.
        
        Reported-by: Ira Weiny <ira.weiny@intel.com>
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
        Link: https://lore.kernel.org/r/168577285161.1672036.8111253437794419696.stgit@dwillia2-xfh.jf.intel.com
        Reviewed-by: Dave Jiang <dave.jiang@intel.com>
        Reviewed-by: Fan Ni <fan.ni@samsung.com>
        Reviewed-by: Ira Weiny <ira.weiny@intel.com>
        Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- drivers/dax/pmem/core.c
+++ drivers/dax/pmem/core.c
@@ -13,7 +13,6 @@ struct dev_dax *__dax_pmem_probe(struct device *dev, enum dev_dax_subsys subsys)
 	int rc, id, region_id;
 	resource_size_t offset;
 	struct nd_pfn_sb *pfn_sb;
-	struct dev_dax *dev_dax;
 	struct dev_dax_data data;
 	struct nd_namespace_io *nsio;
 	struct dax_region *dax_region;
@@ -66,12 +65,7 @@ struct dev_dax *__dax_pmem_probe(struct device *dev, enum dev_dax_subsys subsys)
 		.subsys = subsys,
 		.size = range_len(&range),
 	};
-	dev_dax = devm_create_dev_dax(&data);
-
-	/* child dev_dax instances now own the lifetime of the dax_region */
-	dax_region_put(dax_region);
-
-	return dev_dax;
+	return devm_create_dev_dax(&data);
 }
 EXPORT_SYMBOL_GPL(__dax_pmem_probe);
 
