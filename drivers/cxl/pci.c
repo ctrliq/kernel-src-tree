3951ce9bac3948abab8586ec845cdbb949ab42f4
John W. Linville <linville@redhat.com>
2023-06-12 11:10:35 -0700

    cxl/mem: Wire up Sanitization support
    JIRA: https://issues.redhat.com/browse/RHEL-23582
    
    Implement support for CXL 3.0 8.2.9.8.5.1 Sanitize. This is done by
    adding a security/sanitize' memdev sysfs file to trigger the operation
    and extend the status file to make it poll(2)-capable for completion.
    Unlike all other background commands, this is the only operation that
    is special and monopolizes the device for long periods of time.
    
    In addition to the traditional pmem security requirements, all regions
    must also be offline in order to perform the operation. This permits
    avoiding explicit global CPU cache management, relying instead on the
    implict cache management when a region transitions between
    CXL_CONFIG_ACTIVE and CXL_CONFIG_COMMIT.
    
    The expectation is that userspace can use it such as:
    
        cxl disable-memdev memX
        echo 1 > /sys/bus/cxl/devices/memX/security/sanitize
        cxl wait-sanitize memX
        cxl enable-memdev memX
    
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Davidlohr Bueso <dave@stgolabs.net>
    Link: https://lore.kernel.org/r/20230612181038.14421-5-dave@stgolabs.net
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    (cherry picked from commit 48dcdbb16e5dc0947f949ce17bc2d09a625a0d5c)
    Signed-off-by: John W. Linville <linville@redhat.com>

--- drivers/cxl/pci.c
+++ drivers/cxl/pci.c
@@ -128,6 +128,9 @@ static irqreturn_t cxl_pci_mbox_irq(int irq, void *id)
 	reg = readq(cxlds->regs.mbox + CXLDEV_MBOX_BG_CMD_STATUS_OFFSET);
 	opcode = FIELD_GET(CXLDEV_MBOX_BG_CMD_COMMAND_OPCODE_MASK, reg);
 	if (opcode == CXL_MBOX_OP_SANITIZE) {
+		if (mds->security.sanitize_node)
+			sysfs_notify_dirent(mds->security.sanitize_node);
+
 		dev_dbg(cxlds->dev, "Sanitization operation ended\n");
 	} else {
 		/* short-circuit the wait in __cxl_pci_mbox_send_cmd() */
@@ -150,6 +153,9 @@ static void cxl_mbox_sanitize_work(struct work_struct *work)
 	if (cxl_mbox_background_complete(cxlds)) {
 		mds->security.poll_tmo_secs = 0;
 		put_device(cxlds->dev);
+
+		if (mds->security.sanitize_node)
+			sysfs_notify_dirent(mds->security.sanitize_node);
 
 		dev_dbg(cxlds->dev, "Sanitization operation ended\n");
 	} else {
