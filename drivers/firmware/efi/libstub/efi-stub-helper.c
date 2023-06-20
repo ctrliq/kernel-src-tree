e08741b606630067346f03297a6d091ffbc94062
Sebastian Ott <sebott@redhat.com>
2023-06-20 12:51:02 -0400

    efi: libstub: install boot-time memory map as config table
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2183343
    
    Expose the EFI boot time memory map to the kernel via a configuration
    table. This is arch agnostic and enables future changes that remove the
    dependency on DT on architectures that don't otherwise rely on it.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    (cherry picked from commit 171539f5a90e3fdf7d17f5396fac79d7e44ad68e)
    Signed-off-by: Sebastian Ott <sebott@redhat.com>

--- drivers/firmware/efi/libstub/efi-stub-helper.c
+++ drivers/firmware/efi/libstub/efi-stub-helper.c
@@ -438,7 +438,7 @@ efi_status_t efi_exit_boot_services(void *handle, void *priv,
 	struct efi_boot_memmap *map;
 	efi_status_t status;
 
-	status = efi_get_memory_map(&map);
+	status = efi_get_memory_map(&map, true);
 	if (status != EFI_SUCCESS)
 		return status;
 
