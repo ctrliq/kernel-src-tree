a0ce32e48b47168d9229ad1ec72d4a65478106fa
Joel Slebodnick <jslebodn@redhat.com>
2023-06-21 19:06:30 -0400

    can: ucan: remove unused ucan_priv::intf
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2183338
    Tested: Kernel booted on NXP-IMX8mquad and passed sanity tests on virtual CAN interface.
     Packets were sent while the can-utils package was used to listen
     on the vcan interface.
    
    commit f54b101ddeaea68fe7e52e842deb8b7a081bf156
    Author: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Date:   Thu Dec 8 17:11:41 2022 +0900
    
        can: ucan: remove unused ucan_priv::intf
    
        Field intf of struct ucan_priv is set but never used. Remove it.
    
        Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
        Link: https://lore.kernel.org/all/20221208081142.16936-2-mailhol.vincent@wanadoo.fr
        Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
        (cherry picked from commit f54b101ddeaea68fe7e52e842deb8b7a081bf156)
    
    Signed-off-by: Joel Slebodnick <jslebodn@redhat.com>

--- drivers/net/can/usb/ucan.c
+++ drivers/net/can/usb/ucan.c
@@ -276,7 +276,6 @@ struct ucan_priv {
 
 	/* linux USB device structures */
 	struct usb_device *udev;
-	struct usb_interface *intf;
 	struct net_device *netdev;
 
 	/* lock for can->echo_skb (used around
@@ -1500,7 +1499,6 @@ static int ucan_probe(struct usb_interface *intf,
 
 	/* initialize data */
 	up->udev = udev;
-	up->intf = intf;
 	up->netdev = netdev;
 	up->intf_index = iface_desc->desc.bInterfaceNumber;
 	up->in_ep_addr = in_ep_addr;
