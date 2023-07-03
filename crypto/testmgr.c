380543ef5b2f828ddf11225fefaa69e4a3171668
Jeremy Allison <jallison@ciq.com>
2023-07-03 14:20:14 -0700

    Re-enable hmac(sha1) for fips. This is a dependency for instantiating authenc(hmac(sha1),cbc(aes) (which is allowed in fips).
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -3116,6 +3116,7 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "hmac(sha1)",
 		.test = alg_test_hash,
+		.fips_allowed = 1,
 		.suite = {
 			.hash = __VECS(hmac_sha1_tv_template)
 		}
