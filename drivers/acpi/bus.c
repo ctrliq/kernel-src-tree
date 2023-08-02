fc9615eea73d9b83c3f270813205c4d07dd6ff2a
Mark Langsdorf <mlangsdo@redhat.com>
2023-08-02 16:14:21 -0400

    ACPI: AGDI: Add driver for Arm Generic Diagnostic Dump and Reset device
    JIRA: https://issues.redhat.com/browse/RHEL-4625
    Conflicts:
        drivers/acpi/arm64/Makefile, drivers/acpi/bus.c -
    minor context differences due to the presence of AMTI commits
    
    commit a2a591fb76e6f5461dfd04715b69c317e50c43a5
    Author: Ilkka Koskinen <ilkka@os.amperecomputing.com>
    Date: Tue, 8 Mar 2022 18:07:50 -0800
    
    ACPI for Arm Components 1.1 Platform Design Document v1.1 [0] specifices
    Arm Generic Diagnostic Device Interface (AGDI). It allows an admin to
    issue diagnostic dump and reset via an SDEI event or an interrupt.
    This patch implements SDEI path.
    
    [0] https://developer.arm.com/documentation/den0093/latest/
    
    Signed-off-by: Ilkka Koskinen <ilkka@os.amperecomputing.com>
    Reviewed-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Acked-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/acpi/bus.c
+++ drivers/acpi/bus.c
@@ -26,6 +26,7 @@
 #include <asm/mpspec.h>
 #include <linux/dmi.h>
 #endif
+#include <linux/acpi_agdi.h>
 #include <linux/acpi_apmt.h>
 #include <linux/acpi_iort.h>
 #include <linux/acpi_viot.h>
@@ -1412,6 +1413,7 @@ static int __init acpi_init(void)
 	acpi_debugger_init();
 	acpi_setup_sb_notify_handler();
 	acpi_viot_init();
+	acpi_agdi_init();
 	acpi_apmt_init();
 	return 0;
 }
