967880358335e2d5b8d7540015b090605c0bb27f
Maxim Levitsky <mlevitsk@redhat.com>
2023-08-07 15:44:13 +0300

    KVM: x86/pmu: Rename global_ovf_ctrl_mask to global_status_mask
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2225079
    
    commit 53550b89220beda42934a76a61313ecf308b2b03
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Fri Jun 2 18:10:47 2023 -0700
    
        KVM: x86/pmu: Rename global_ovf_ctrl_mask to global_status_mask
    
        Rename global_ovf_ctrl_mask to global_status_mask to avoid confusion now
        that Intel has renamed GLOBAL_OVF_CTRL to GLOBAL_STATUS_RESET in PMU v4.
        GLOBAL_OVF_CTRL and GLOBAL_STATUS_RESET are the same MSR index, i.e. are
        just different names for the same thing, but the SDM provides different
        entries in the IA-32 Architectural MSRs table, which gets really confusing
        when looking at PMU v4 definitions since it *looks* like GLOBAL_STATUS has
        bits that don't exist in GLOBAL_OVF_CTRL, but in reality the bits are
        simply defined in the GLOBAL_STATUS_RESET entry.
    
        No functional change intended.
    
        Cc: Like Xu <like.xu.linux@gmail.com>
        Link: https://lore.kernel.org/r/20230603011058.1038821-2-seanjc@google.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -524,7 +524,7 @@ struct kvm_pmu {
 	u64 global_status;
 	u64 counter_bitmask[2];
 	u64 global_ctrl_mask;
-	u64 global_ovf_ctrl_mask;
+	u64 global_status_mask;
 	u64 reserved_bits;
 	u64 raw_event_mask;
 	u8 version;
