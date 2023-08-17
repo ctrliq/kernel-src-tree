d138228b80592bd7b83d2c12f3ce5b1a91543839
Adrien Thierry <athierry@redhat.com>
2023-08-17 15:16:42 -0400

    tracing: Fix return value of trace_pid_write()
    JIRA: https://issues.redhat.com/browse/RHEL-1491
    
    commit b27f266f74fbda4ee36c2b2b04d15992860cf23b
    Author: Wonhyuk Yang <vvghjk1234@gmail.com>
    Date:   Tue May 3 14:05:46 2022 +0900
    
        tracing: Fix return value of trace_pid_write()
    
        Setting set_event_pid with trailing whitespace lead to endless write
        system calls like below.
    
            $ strace echo "123 " > /sys/kernel/debug/tracing/set_event_pid
            execve("/usr/bin/echo", ["echo", "123 "], ...) = 0
            ...
            write(1, "123 \n", 5)                   = 4
            write(1, "\n", 1)                       = 0
            write(1, "\n", 1)                       = 0
            write(1, "\n", 1)                       = 0
            write(1, "\n", 1)                       = 0
            write(1, "\n", 1)                       = 0
            ....
    
        This is because, the result of trace_get_user's are not returned when it
        read at least one pid. To fix it, update read variable even if
        parser->idx == 0.
    
        The result of applied patch is below.
    
            $ strace echo "123 " > /sys/kernel/debug/tracing/set_event_pid
            execve("/usr/bin/echo", ["echo", "123 "], ...) = 0
            ...
            write(1, "123 \n", 5)                   = 5
            close(1)                                = 0
    
        Link: https://lkml.kernel.org/r/20220503050546.288911-1-vvghjk1234@gmail.com
    
        Cc: Ingo Molnar <mingo@redhat.com>
        Cc: Baik Song An <bsahn@etri.re.kr>
        Cc: Hong Yeon Kim <kimhy@etri.re.kr>
        Cc: Taeung Song <taeung@reallinux.co.kr>
        Cc: linuxgeek@linuxgeek.io
        Cc: stable@vger.kernel.org
        Fixes: 4909010788640 ("tracing: Add set_event_pid directory for future use")
        Signed-off-by: Wonhyuk Yang <vvghjk1234@gmail.com>
        Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    
    Signed-off-by: Adrien Thierry <athierry@redhat.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -736,12 +736,15 @@ int trace_pid_write(struct trace_pid_list *filtered_pids,
 		pos = 0;
 
 		ret = trace_get_user(&parser, ubuf, cnt, &pos);
-		if (ret < 0 || !trace_parser_loaded(&parser))
+		if (ret < 0)
 			break;
 
 		read += ret;
 		ubuf += ret;
 		cnt -= ret;
+
+		if (!trace_parser_loaded(&parser))
+			break;
 
 		ret = -EINVAL;
 		if (kstrtoul(parser.buffer, 0, &val))
@@ -767,7 +770,6 @@ int trace_pid_write(struct trace_pid_list *filtered_pids,
 	if (!nr_pids) {
 		/* Cleared the list of pids */
 		trace_free_pid_list(pid_list);
-		read = ret;
 		pid_list = NULL;
 	}
 
