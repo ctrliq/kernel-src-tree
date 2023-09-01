6f0542ca9742017128a4283c616ad1f564b1fa7f
Kamal Heib <kheib@redhat.com>
2023-09-01 10:23:29 -0400

    RDMA/rxe: Add workqueue support for rxe tasks
    JIRA: https://issues.redhat.com/browse/RHEL-1958
    Conflicts:
    Minor context diff due to the use of mark_tech_preview()
    
    commit 9b4b7c1f9f54120940e243251e2b1407767b3381
    Author: Bob Pearson <rpearsonhpe@gmail.com>
    Date:   Fri Apr 28 12:13:22 2023 -0500
    
        RDMA/rxe: Add workqueue support for rxe tasks
    
        Replace tasklets by work queues for the three main rxe tasklets:
        rxe_requester, rxe_completer and rxe_responder.
    
        work queues are a more modern way to process work from an IRQ and provide
        more control over how that work is run for future patches.
    
        Link: https://lore.kernel.org/r/20230428171321.5774-1-rpearsonhpe@gmail.com
        Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>
        Signed-off-by: Bob Pearson <rpearsonhpe@gmail.com>
        Reviewed-by: Daisuke Matsuda <matsuda-daisuke@fujitsu.com>
        Tested-by: Daisuke Matsuda <matsuda-daisuke@fujitsu.com>
        Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Signed-off-by: Kamal Heib <kheib@redhat.com>

--- drivers/infiniband/sw/rxe/rxe.c
+++ drivers/infiniband/sw/rxe/rxe.c
@@ -214,9 +214,15 @@ static int __init rxe_module_init(void)
 
 	mark_tech_preview("Soft-RoCE (rdma_rxe) Driver", THIS_MODULE);
 
-	err = rxe_net_init();
+	err = rxe_alloc_wq();
 	if (err)
 		return err;
+
+	err = rxe_net_init();
+	if (err) {
+		rxe_destroy_wq();
+		return err;
+	}
 
 	rdma_link_register(&rxe_link_ops);
 	pr_info("loaded\n");
@@ -228,6 +234,7 @@ static void __exit rxe_module_exit(void)
 	rdma_link_unregister(&rxe_link_ops);
 	ib_unregister_driver(RDMA_DRIVER_RXE);
 	rxe_net_exit();
+	rxe_destroy_wq();
 
 	pr_info("unloaded\n");
 }
