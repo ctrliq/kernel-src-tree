bafaf00118f8e3a6c2a2dff75d53e9756abb23ce
Phil Auld <pauld@redhat.com>
2023-09-05 11:38:00 -0400

    sched/fair: Move unused stub functions to header
    JIRA: https://issues.redhat.com/browse/RHEL-1536
    
    commit 7aa55f2a5902646a19db89dab9961867724b27b8
    Author: Arnd Bergmann <arnd@arndb.de>
    Date:   Mon May 22 21:50:21 2023 +0200
    
        sched/fair: Move unused stub functions to header
    
        These four functions have a normal definition for CONFIG_FAIR_GROUP_SCHED,
        and empty one that is only referenced when FAIR_GROUP_SCHED is disabled
        but CGROUP_SCHED is still enabled. If both are turned off, the functions
        are still defined but the misisng prototype causes a W=1 warning:
    
        kernel/sched/fair.c:12544:6: error: no previous prototype for 'free_fair_sched_group'
        kernel/sched/fair.c:12546:5: error: no previous prototype for 'alloc_fair_sched_group'
        kernel/sched/fair.c:12553:6: error: no previous prototype for 'online_fair_sched_group'
        kernel/sched/fair.c:12555:6: error: no previous prototype for 'unregister_fair_sched_group'
    
        Move the alternatives into the header as static inline functions with
        the correct combination of #ifdef checks to avoid the warning without
        adding even more complexity.
    
        Signed-off-by: Arnd Bergmann <arnd@arndb.de>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
        Link: https://lore.kernel.org/r/20230522195021.3456768-6-arnd@kernel.org
    
    Signed-off-by: Phil Auld <pauld@redhat.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -669,7 +669,7 @@ struct sched_entity *__pick_last_entity(struct cfs_rq *cfs_rq)
 /**************************************************************
  * Scheduling class statistics methods:
  */
-#ifdef CONFIG_SMP
+
 int sched_update_scaling(void)
 {
 	unsigned int factor = get_update_sysctl_factor();
@@ -686,7 +686,6 @@ int sched_update_scaling(void)
 
 	return 0;
 }
-#endif
 #endif
 
 /*
@@ -6068,8 +6067,9 @@ static inline int throttled_lb_pair(struct task_group *tg,
 	return 0;
 }
 
-#ifdef CONFIG_FAIR_GROUP_SCHED
 void init_cfs_bandwidth(struct cfs_bandwidth *cfs_b) {}
+
+#ifdef CONFIG_FAIR_GROUP_SCHED
 static void init_cfs_rq_runtime(struct cfs_rq *cfs_rq) {}
 #endif
 
