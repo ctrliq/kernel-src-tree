36266f140130f95efa080782fab9fe8242f65878
Mark Langsdorf <mlangsdo@redhat.com>
2023-09-11 16:09:19 -0400

    cpufreq: Abort show()/store() for half-initialized policies
    JIRA: https://issues.redhat.com/browse/RHEL-1026
    JIRA: https://issues.redhat.com/browse/RHELPLAN-167418
    
    commit d4627a287e251efed59f2b4bda82c5950768c963
    Author: Schspa Shi <schspa@gmail.com>
    Date:   Mon May 16 11:02:50 2022 +0800
    
    If policy initialization fails after the sysfs files are created,
    there is a possibility to end up running show()/store() callbacks
    for half-initialized policies, which may have unpredictable
    outcomes.
    
    Abort show()/store() in such a case by making sure the policy is active.
    
    Also dectivate the policy on such failures.
    
    Signed-off-by: Schspa Shi <schspa@gmail.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    [ rjw: Subject and changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -955,13 +955,14 @@ static ssize_t show(struct kobject *kobj, struct attribute *attr, char *buf)
 {
 	struct cpufreq_policy *policy = to_policy(kobj);
 	struct freq_attr *fattr = to_attr(attr);
-	ssize_t ret;
+	ssize_t ret = -EBUSY;
 
 	if (!fattr->show)
 		return -EIO;
 
 	down_read(&policy->rwsem);
-	ret = fattr->show(policy, buf);
+	if (likely(!policy_is_inactive(policy)))
+		ret = fattr->show(policy, buf);
 	up_read(&policy->rwsem);
 
 	return ret;
@@ -972,7 +973,7 @@ static ssize_t store(struct kobject *kobj, struct attribute *attr,
 {
 	struct cpufreq_policy *policy = to_policy(kobj);
 	struct freq_attr *fattr = to_attr(attr);
-	ssize_t ret = -EINVAL;
+	ssize_t ret = -EBUSY;
 
 	if (!fattr->store)
 		return -EIO;
@@ -986,7 +987,8 @@ static ssize_t store(struct kobject *kobj, struct attribute *attr,
 
 	if (cpu_online(policy->cpu)) {
 		down_write(&policy->rwsem);
-		ret = fattr->store(policy, buf, count);
+		if (likely(!policy_is_inactive(policy)))
+			ret = fattr->store(policy, buf, count);
 		up_write(&policy->rwsem);
 	}
 
@@ -1542,6 +1544,7 @@ out_destroy_policy:
 	for_each_cpu(j, policy->real_cpus)
 		remove_cpu_dev_symlink(policy, j, get_cpu_device(j));
 
+	cpumask_clear(policy->cpus);
 	up_write(&policy->rwsem);
 
 out_offline_policy:
