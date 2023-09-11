295a9a3983bf5f48eaa943311dbdf7d90267f494
Mika Penttilä <mpenttil@redhat.com>
2023-09-11 14:20:44 +0300

    Revert drm/i2c: Switch i2c drivers back to use .probe()
    NOTE: This commit does not compile due to missing RHEL-specific fix-ups,
    which are split out into separate Git commits following this one.
    
    If git bisect selected this commit, run "git bisect skip" and try again.
    Repeat this until it selects a commit which compiles.
    
    commit d13b5d2b2b45ab4912190c8f5d8352f22daebcf7
    Author: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Date:   Sun Jun 11 22:27:40 2023 +0200
    
        drm/i2c: Switch i2c drivers back to use .probe()
    
    JIRA: https://issues.redhat.com/browse/RHEL-1350
    
    Upstream Status: RHEL-only
    
    Signed-off-by: Mika Penttilä <mpenttil@redhat.com>

--- drivers/gpu/drm/i2c/ch7006_drv.c
+++ drivers/gpu/drm/i2c/ch7006_drv.c
@@ -498,7 +498,7 @@ static const struct dev_pm_ops ch7006_pm_ops = {
 
 static struct drm_i2c_encoder_driver ch7006_driver = {
 	.i2c_driver = {
-		.probe = ch7006_probe,
+		.probe_new = ch7006_probe,
 		.remove = ch7006_remove,
 
 		.driver = {
