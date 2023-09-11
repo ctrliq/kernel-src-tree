e116e6fe5c1c9400016a7b93053b3acdd0dde4f6
Artem Savkov <asavkov@redhat.com>
2023-09-11 11:03:46 +0200

    bpf: Fix an error around PTR_UNTRUSTED
    Bugzilla: https://bugzilla.redhat.com/2221599
    
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    commit 7ce4dc3e4a9d954c8a1fb483c7a527e9b060b860
    Author: Yafang Shao <laoar.shao@gmail.com>
    Date:   Thu Jul 13 02:56:39 2023 +0000
    
        bpf: Fix an error around PTR_UNTRUSTED
    
        Per discussion with Alexei, the PTR_UNTRUSTED flag should not been
        cleared when we start to walk a new struct, because the struct in
        question may be a struct nested in a union. We should also check and set
        this flag before we walk its each member, in case itself is a union.
        We will clear this flag if the field is BTF_TYPE_SAFE_RCU_OR_NULL.
    
        Fixes: 6fcd486b3a0a ("bpf: Refactor RCU enforcement in the verifier.")
        Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
        Link: https://lore.kernel.org/r/20230713025642.27477-2-laoar.shao@gmail.com
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Artem Savkov <asavkov@redhat.com>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -6125,7 +6125,6 @@ static int btf_struct_walk(struct bpf_verifier_log *log, const struct btf *btf,
 	const char *tname, *mname, *tag_value;
 	u32 vlen, elem_id, mid;
 
-	*flag = 0;
 again:
 	tname = __btf_name_by_offset(btf, t->name_off);
 	if (!btf_type_is_struct(t)) {
@@ -6134,6 +6133,14 @@ again:
 	}
 
 	vlen = btf_type_vlen(t);
+	if (BTF_INFO_KIND(t->info) == BTF_KIND_UNION && vlen != 1 && !(*flag & PTR_UNTRUSTED))
+		/*
+		 * walking unions yields untrusted pointers
+		 * with exception of __bpf_md_ptr and other
+		 * unions with a single member
+		 */
+		*flag |= PTR_UNTRUSTED;
+
 	if (off + size > t->size) {
 		/* If the last element is a variable size array, we may
 		 * need to relax the rule.
@@ -6294,15 +6301,6 @@ error:
 		 * of this field or inside of this struct
 		 */
 		if (btf_type_is_struct(mtype)) {
-			if (BTF_INFO_KIND(mtype->info) == BTF_KIND_UNION &&
-			    btf_type_vlen(mtype) != 1)
-				/*
-				 * walking unions yields untrusted pointers
-				 * with exception of __bpf_md_ptr and other
-				 * unions with a single member
-				 */
-				*flag |= PTR_UNTRUSTED;
-
 			/* our field must be inside that union or struct */
 			t = mtype;
 
@@ -6468,7 +6466,7 @@ bool btf_struct_ids_match(struct bpf_verifier_log *log,
 			  bool strict)
 {
 	const struct btf_type *type;
-	enum bpf_type_flag flag;
+	enum bpf_type_flag flag = 0;
 	int err;
 
 	/* Are we already done? */
