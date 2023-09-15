94bc7f715fba54c24ee35026f37c72a15195555c
Mika Penttilä <mpenttil@redhat.com>
2023-09-15 14:02:00 +0300

    kunit: Fix obsolete name in documentation headers (func->action)
    JIRA: https://issues.redhat.com/browse/RHEL-1349
    Upstream Status: v6.5-rc1
    
    commit c042030aa15e9265504a034243a8cae062e900a1
    Author:     David Gow <davidgow@google.com>
    AuthorDate: Tue May 30 15:55:57 2023 +0800
    Commit:     Shuah Khan <skhan@linuxfoundation.org>
    CommitDate: Thu Jun  1 13:04:40 2023 -0600
    
        The kunit_add_action() and related functions named the kunit_action_t
        parameter 'func' in early drafts, which was later renamed to 'action'
        However, the doc comments were not properly updated.
    
        Fix these to avoid confusion and 'make htmldocs' warnings.
    
        Fixes: b9dce8a1ed3e ("kunit: Add kunit_add_action() to defer a call until test exit")
        Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
        Closes: https://lore.kernel.org/lkml/20230530151840.16a56460@canb.auug.org.au/
        Signed-off-by: David Gow <davidgow@google.com>
        Reviewed-by: Maxime Ripard <mripard@kernel.org>
        Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>
    
    Signed-off-by: Mika Penttilä <mpenttil@redhat.com>

--- include/kunit/resource.h
+++ include/kunit/resource.h
@@ -409,7 +409,7 @@ typedef void (kunit_action_t)(void *);
 /**
  * kunit_add_action() - Call a function when the test ends.
  * @test: Test case to associate the action with.
- * @func: The function to run on test exit
+ * @action: The function to run on test exit
  * @ctx: Data passed into @func
  *
  * Defer the execution of a function until the test exits, either normally or
@@ -431,7 +431,7 @@ int kunit_add_action(struct kunit *test, kunit_action_t *action, void *ctx);
 /**
  * kunit_add_action_or_reset() - Call a function when the test ends.
  * @test: Test case to associate the action with.
- * @func: The function to run on test exit
+ * @action: The function to run on test exit
  * @ctx: Data passed into @func
  *
  * Defer the execution of a function until the test exits, either normally or
@@ -457,7 +457,7 @@ int kunit_add_action_or_reset(struct kunit *test, kunit_action_t *action,
 /**
  * kunit_remove_action() - Cancel a matching deferred action.
  * @test: Test case the action is associated with.
- * @func: The deferred function to cancel.
+ * @action: The deferred function to cancel.
  * @ctx: The context passed to the deferred function to trigger.
  *
  * Prevent an action deferred via kunit_add_action() from executing when the
@@ -475,7 +475,7 @@ void kunit_remove_action(struct kunit *test,
 /**
  * kunit_release_action() - Run a matching action call immediately.
  * @test: Test case the action is associated with.
- * @func: The deferred function to trigger.
+ * @action: The deferred function to trigger.
  * @ctx: The context passed to the deferred function to trigger.
  *
  * Execute a function deferred via kunit_add_action()) immediately, rather than
