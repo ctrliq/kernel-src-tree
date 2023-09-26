67afd4a87e7c12d37342af5c37f416fe99d14721
Mark Langsdorf <mlangsdo@redhat.com>
2023-09-26 17:49:01 -0400

    cpufreq: replace cpumask_weight with cpumask_empty where appropriate
    JIRA: https://issues.redhat.com/browse/RHEL-1026
    Conflicts:
           drivers/cpufreq/scmi-cpufreq.c - centos-9 uses
    opp_shared_cpus as a variable, not a member of a struct,
    so continue that usage
    
    commit b48cd0d12f8e3b5fc928ac84734e563eda8e430f
    Author: Yury Norov <yury.norov@gmail.com>
    Date:   Sun Jan 23 10:38:47 2022 -0800
    
    drivers/cpufreq calls cpumask_weight() to check if any bit of a given
    cpumask is set. We can do it more efficiently with cpumask_empty() because
    cpumask_empty() stops traversing the cpumask as soon as it finds first set
    bit, while cpumask_weight() counts all bits unconditionally.
    
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com> (for SCMI cpufreq driver)
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/cpufreq/scmi-cpufreq.c
+++ drivers/cpufreq/scmi-cpufreq.c
@@ -155,7 +155,7 @@ static int scmi_cpufreq_init(struct cpufreq_policy *policy)
 	 * table and opp-shared.
 	 */
 	ret = dev_pm_opp_of_get_sharing_cpus(cpu_dev, opp_shared_cpus);
-	if (ret || !cpumask_weight(opp_shared_cpus)) {
+	if (ret || cpumask_empty(opp_shared_cpus)) {
 		/*
 		 * Either opp-table is not set or no opp-shared was found.
 		 * Use the CPU mask from SCMI to designate CPUs sharing an OPP
