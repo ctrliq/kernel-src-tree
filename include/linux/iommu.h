3b7a41281b49229aabf0e8c0a283917551969f47
Jerry Snitselaar <jsnitsel@redhat.com>
2023-09-28 16:31:39 +0200

    iommu/dma: Allow a single FQ in addition to per-CPU FQs
    JIRA: https://issues.redhat.com/browse/RHEL-28780
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Conflicts: Context diff due to backport of PCI SAC address commit. Fixed ordering of
               bits to match upstream.
    
    commit 32d5bc8b09c7cc48c511809e7c3b1755c7ecc5fa
    Author: Niklas Schnelle <schnelle@linux.ibm.com>
    Date:   Thu Sep 28 16:31:39 2023 +0200
    
        iommu/dma: Allow a single FQ in addition to per-CPU FQs
    
        In some virtualized environments, including s390 paged memory guests,
        IOTLB flushes are used to update IOMMU shadow tables. Due to this, they
        are much more expensive than in typical bare metal environments or
        non-paged s390 guests. In addition they may parallelize poorly in
        virtualized environments. This changes the trade off for flushing IOVAs
        such that minimizing the number of IOTLB flushes trumps any benefit of
        cheaper queuing operations or increased paralellism.
    
        In this scenario per-CPU flush queues pose several problems. Firstly
        per-CPU memory is often quite limited prohibiting larger queues.
        Secondly collecting IOVAs per-CPU but flushing via a global timeout
        reduces the number of IOVAs flushed for each timeout especially on s390
        where PCI interrupts may not be bound to a specific CPU.
    
        Let's introduce a single flush queue mode that reuses the same queue
        logic but only allocates a single global queue. This mode is selected by
        dma-iommu if a newly introduced .shadow_on_flush flag is set in struct
        dev_iommu. As a first user the s390 IOMMU driver sets this flag during
        probe_device. With the unchanged small FQ size and timeouts this setting
        is worse than per-CPU queues but a follow up patch will make the FQ size
        and timeout variable. Together this allows the common IOVA flushing code
        to more closely resemble the global flush behavior used on s390's
        previous internal DMA API implementation.
    
        Link: https://lore.kernel.org/all/9a466109-01c5-96b0-bf03-304123f435ee@arm.com/
        Acked-by: Robin Murphy <robin.murphy@arm.com>
        Reviewed-by: Matthew Rosato <mjrosato@linux.ibm.com> #s390
        Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>
        Link: https://lore.kernel.org/r/20230928-dma_iommu-v13-5-9e5fc4dacc36@linux.ibm.com
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    (cherry picked from commit 32d5bc8b09c7cc48c511809e7c3b1755c7ecc5fa)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -533,8 +533,9 @@ struct iommu_fault_param {
  * @priv:	 IOMMU Driver private data
  * @max_pasids:  number of PASIDs this device can consume
  * @attach_deferred: the dma domain attachment is deferred
- * @require_direct: device requires IOMMU_RESV_DIRECT regions
  * @pci_32bit_workaround: Limit DMA allocations to 32-bit IOVAs
+ * @require_direct: device requires IOMMU_RESV_DIRECT regions
+ * @shadow_on_flush: IOTLB flushes are used to sync shadow tables
  *
  * TODO: migrate other per device data pointers under iommu_dev_data, e.g.
  *	struct iommu_group	*iommu_group;
@@ -548,8 +549,9 @@ struct dev_iommu {
 	void				*priv;
 	u32				max_pasids;
 	u32				attach_deferred:1;
-	u32				require_direct:1;
 	u32				pci_32bit_workaround:1;
+	u32				require_direct:1;
+	u32				shadow_on_flush:1;
 };
 
 int iommu_device_register(struct iommu_device *iommu,
