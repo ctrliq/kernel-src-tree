24dd0624bad21415447428f257bfedede2223105
John W. Linville <linville@redhat.com>
2023-09-29 12:44:20 -0700

    cxl/pci: Cleanup 'sanitize' to always poll
    JIRA: https://issues.redhat.com/browse/RHEL-54609
    
    In preparation for fixing the init/teardown of the 'sanitize' workqueue
    and sysfs notification mechanism, arrange for cxl_mbox_sanitize_work()
    to be the single location where the sysfs attribute is notified. With
    that change there is no distinction between polled mode and interrupt
    mode. All the interrupt does is accelerate the polling interval.
    
    The change to check for "mds->security.sanitize_node" under the lock is
    there to ensure that the interrupt, the work routine and the
    setup/teardown code can all have a consistent view of the registered
    notifier and the workqueue state. I.e. the expectation is that the
    interrupt is live past the point that the sanitize sysfs attribute is
    published, and it may race teardown, so it must be consulted under a
    lock. Given that new locking requirement, cxl_pci_mbox_irq() is moved
    from hard to thread irq context.
    
    Lastly, some opportunistic replacements of
    "queue_delayed_work(system_wq, ...)", which is just open coded
    schedule_delayed_work(), are included.
    
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: Davidlohr Bueso <dave@stgolabs.net>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    (cherry picked from commit e30a106558e7d1e06d1fcfd12466dc646673d03d)
    Signed-off-by: John W. Linville <linville@redhat.com>
    
    Conflicts:
            drivers/cxl/pci.c       (overlapping patch applied out-of-order)

--- drivers/cxl/cxlmem.h
+++ drivers/cxl/cxlmem.h
@@ -364,7 +364,6 @@ struct cxl_fw_state {
  *
  * @state: state of last security operation
  * @enabled_cmds: All security commands enabled in the CEL
- * @poll: polling for sanitization is enabled, device has no mbox irq support
  * @poll_tmo_secs: polling timeout
  * @sanitize_active: sanitize completion pending
  * @poll_dwork: polling work item
@@ -373,7 +372,6 @@ struct cxl_fw_state {
 struct cxl_security_state {
 	unsigned long state;
 	DECLARE_BITMAP(enabled_cmds, CXL_SEC_ENABLED_MAX);
-	bool poll;
 	int poll_tmo_secs;
 	bool sanitize_active;
 	struct delayed_work poll_dwork;
