9b4b0b8114fba9b58cfa03017f7c1fcaa0e1d63d
Waiman Long <longman@redhat.com>
2023-10-07 15:11:12 -0400

    x86/idle: Disable IBRS when CPU is offline to improve single-threaded performance
    JIRA: https://issues.redhat.com/browse/RHEL-8620
    Upstream Status: tip commit 2743fe89d4d41616ffbe1e7e96e443ae7a4b1cc6
    Conflicts: A minor context diff due to missing upstream commit
    	   82d89369141b ("x86/ACPI: CPPC: Move AMD maximum frequency
    	   ratio setting function into x86 CPPC").
    
    commit 2743fe89d4d41616ffbe1e7e96e443ae7a4b1cc6
    Author: Waiman Long <longman@redhat.com>
    Date:   Thu, 27 Jul 2023 14:45:58 -0400
    
        x86/idle: Disable IBRS when CPU is offline to improve single-threaded performance
    
        Commit bf5835bcdb96 ("intel_idle: Disable IBRS during long idle")
        disables IBRS when the CPU enters long idle. However, when a CPU
        becomes offline, the IBRS bit is still set when X86_FEATURE_KERNEL_IBRS
        is enabled. That will impact the performance of a sibling CPU. Mitigate
        this performance impact by clearing all the mitigation bits in SPEC_CTRL
        MSR when offline. When the CPU is online again, it will be re-initialized
        and so restoring the SPEC_CTRL value isn't needed.
    
        Add a comment to say that native_play_dead() is a __noreturn function,
        but it can't be marked as such to avoid confusion about the missing
        MSR restoration code.
    
        When DPDK is running on an isolated CPU thread processing network packets
        in user space while its sibling thread is idle. The performance of the
        busy DPDK thread with IBRS on and off in the sibling idle thread are:
    
                                        IBRS on         IBRS off
                                        -------         --------
          packets/second:                  7.8M           10.4M
          avg tsc cycles/packet:         282.26          209.86
    
        This is a 25% performance degradation. The test system is a Intel Xeon
        4114 CPU @ 2.20GHz.
    
        [ mingo: Extended the changelog with performance data from the 0/4 mail. ]
    
        Signed-off-by: Waiman Long <longman@redhat.com>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
        Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Link: https://lore.kernel.org/r/20230727184600.26768-3-longman@redhat.com
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -84,6 +84,7 @@
 #include <asm/hw_irq.h>
 #include <asm/stackprotector.h>
 #include <asm/sev.h>
+#include <asm/spec-ctrl.h>
 
 #ifdef CONFIG_ACPI_CPPC_LIB
 #include <acpi/cppc_acpi.h>
@@ -1839,8 +1840,15 @@ void hlt_play_dead(void)
 	}
 }
 
+/*
+ * native_play_dead() is essentially a __noreturn function, but it can't
+ * be marked as such as the compiler may complain about it.
+ */
 void native_play_dead(void)
 {
+	if (cpu_feature_enabled(X86_FEATURE_KERNEL_IBRS))
+		__update_spec_ctrl(0);
+
 	play_dead_common();
 	tboot_shutdown(TB_SHUTDOWN_WFS);
 
