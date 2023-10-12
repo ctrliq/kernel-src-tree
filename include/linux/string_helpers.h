3b9aaf98ce340e30fc9934ca009cde960e026991
David Arcari <darcari@redhat.com>
2023-10-12 10:39:56 -0400

    lib/string_helpers: Split out string_choices.h
    JIRA: https://issues.redhat.com/browse/RHEL-2037
    
    commit fca76071bab2304b379c35674d3b9e36a82e364a
    Author: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Date:   Mon Mar 6 21:55:42 2023 +0200
    
        lib/string_helpers: Split out string_choices.h
    
        Some users may only need the string choice APIs. Split
        the respective header, i.e. string_choices.h. Include
        it in the string_helpers.h for backward compatibility.
    
        Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- include/linux/string_helpers.h
+++ include/linux/string_helpers.h
@@ -4,6 +4,7 @@
 
 #include <linux/bits.h>
 #include <linux/ctype.h>
+#include <linux/string_choices.h>
 #include <linux/string.h>
 #include <linux/types.h>
 
@@ -109,30 +110,5 @@ char *kstrdup_quotable_file(struct file *file, gfp_t gfp);
 
 char **kasprintf_strarray(gfp_t gfp, const char *prefix, size_t n);
 void kfree_strarray(char **array, size_t n);
-
-static inline const char *str_yes_no(bool v)
-{
-	return v ? "yes" : "no";
-}
-
-static inline const char *str_on_off(bool v)
-{
-	return v ? "on" : "off";
-}
-
-static inline const char *str_enable_disable(bool v)
-{
-	return v ? "enable" : "disable";
-}
-
-static inline const char *str_enabled_disabled(bool v)
-{
-	return v ? "enabled" : "disabled";
-}
-
-static inline const char *str_read_write(bool v)
-{
-	return v ? "read" : "write";
-}
 
 #endif
