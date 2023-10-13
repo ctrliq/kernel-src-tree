2120ed1903fc7c0d4e08424f5575008fb8238a8f
Kate Hsuan <hpa@redhat.com>
2023-10-13 09:31:37 +0200

    media: v4l: subdev: Also return pads array information on stream functions
    JIRA: https://issues.redhat.com/browse/RHEL-67885
    
    commit 049fa16b81c276f25a09abaa4e8f743470c3cdc8
    Author: Sakari Ailus <sakari.ailus@linux.intel.com>
    Date: Fri, 13 Oct 2023 09:31:37 +0200
    
      There are two sets of functions that return information from sub-device
      state, one for stream-unaware users and another for stream-aware users.
      Add support for stream-aware functions to return format, crop and compose
      information from pad-based array that are functionally equivalent to the
      old, stream-unaware ones.
    
      Also check state is non-NULL, in order to guard against old drivers
      potentially calling this with NULL state for active formats or selection
      rectangles.
    
      Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
      Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
      Reviewed-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
      Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    
    Signed-off-by: Kate Hsuan <hpa@redhat.com>

--- include/media/v4l2-subdev.h
+++ include/media/v4l2-subdev.h
@@ -1536,7 +1536,8 @@ int v4l2_subdev_set_routing_with_fmt(struct v4l2_subdev *sd,
  * This returns a pointer to &struct v4l2_mbus_framefmt for the given pad +
  * stream in the subdev state.
  *
- * If the state does not contain the given pad + stream, NULL is returned.
+ * For stream-unaware drivers the format for the corresponding pad is returned.
+ * If the pad does not exist, NULL is returned.
  */
 struct v4l2_mbus_framefmt *
 v4l2_subdev_state_get_stream_format(struct v4l2_subdev_state *state,
@@ -1551,7 +1552,8 @@ v4l2_subdev_state_get_stream_format(struct v4l2_subdev_state *state,
  * This returns a pointer to crop rectangle for the given pad + stream in the
  * subdev state.
  *
- * If the state does not contain the given pad + stream, NULL is returned.
+ * For stream-unaware drivers the crop rectangle for the corresponding pad is
+ * returned. If the pad does not exist, NULL is returned.
  */
 struct v4l2_rect *
 v4l2_subdev_state_get_stream_crop(struct v4l2_subdev_state *state,
@@ -1567,7 +1569,8 @@ v4l2_subdev_state_get_stream_crop(struct v4l2_subdev_state *state,
  * This returns a pointer to compose rectangle for the given pad + stream in the
  * subdev state.
  *
- * If the state does not contain the given pad + stream, NULL is returned.
+ * For stream-unaware drivers the compose rectangle for the corresponding pad is
+ * returned. If the pad does not exist, NULL is returned.
  */
 struct v4l2_rect *
 v4l2_subdev_state_get_stream_compose(struct v4l2_subdev_state *state,
