4db8b2abcb7244274a5877776af27668fe140705
Jerry Snitselaar <jsnitsel@redhat.com>
2023-10-16 11:28:29 +0800

    iommu/vt-d: debugfs: Create/remove debugfs file per {device, pasid}
    JIRA: https://issues.redhat.com/browse/RHEL-28780
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    commit d87731f609318a27e9aa3d01cf97798599d32839
    Author: Jingqi Liu <Jingqi.liu@intel.com>
    Date:   Mon Oct 16 11:28:29 2023 +0800
    
        iommu/vt-d: debugfs: Create/remove debugfs file per {device, pasid}
    
        Add a debugfs directory per pair of {device, pasid} if the mappings of
        its page table are created and destroyed by the iommu_map/unmap()
        interfaces. i.e. /sys/kernel/debug/iommu/intel/<device source id>/<pasid>.
        Create a debugfs file in the directory for users to dump the page
        table corresponding to {device, pasid}. e.g.
        /sys/kernel/debug/iommu/intel/0000:00:02.0/1/domain_translation_struct.
        For the default domain without pasid, it creates a debugfs file in the
        debugfs device directory for users to dump its page table. e.g.
        /sys/kernel/debug/iommu/intel/0000:00:02.0/domain_translation_struct.
    
        When setting a domain to a PASID of device, create a debugfs file in
        the pasid debugfs directory for users to dump the page table of the
        specified pasid. Remove the debugfs device directory of the device
        when releasing a device. e.g.
        /sys/kernel/debug/iommu/intel/0000:00:01.0
    
        Signed-off-by: Jingqi Liu <Jingqi.liu@intel.com>
        Link: https://lore.kernel.org/r/20231013135811.73953-3-Jingqi.liu@intel.com
        Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    (cherry picked from commit d87731f609318a27e9aa3d01cf97798599d32839)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -4469,6 +4469,8 @@ static struct iommu_device *intel_iommu_probe_device(struct device *dev)
 		}
 	}
 
+	intel_iommu_debugfs_create_dev(info);
+
 	return &iommu->iommu;
 }
 
@@ -4478,6 +4480,7 @@ static void intel_iommu_release_device(struct device *dev)
 
 	dmar_remove_one_dev_info(dev);
 	intel_pasid_free_table(dev);
+	intel_iommu_debugfs_remove_dev(info);
 	dev_iommu_priv_set(dev, NULL);
 	kfree(info);
 	set_dma_ops(dev, NULL);
@@ -4770,6 +4773,7 @@ static void intel_iommu_remove_dev_pasid(struct device *dev, ioasid_t pasid)
 	spin_unlock_irqrestore(&dmar_domain->lock, flags);
 
 	domain_detach_iommu(dmar_domain, iommu);
+	intel_iommu_debugfs_remove_dev_pasid(dev_pasid);
 	kfree(dev_pasid);
 out_tear_down:
 	intel_pasid_tear_down_entry(iommu, dev, pasid, false);
@@ -4824,6 +4828,9 @@ static int intel_iommu_set_dev_pasid(struct iommu_domain *domain,
 	spin_lock_irqsave(&dmar_domain->lock, flags);
 	list_add(&dev_pasid->link_domain, &dmar_domain->dev_pasids);
 	spin_unlock_irqrestore(&dmar_domain->lock, flags);
+
+	if (domain->type & __IOMMU_DOMAIN_PAGING)
+		intel_iommu_debugfs_create_dev_pasid(dev_pasid);
 
 	return 0;
 out_detach_iommu:
