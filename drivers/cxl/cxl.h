88981b4f8015818484e0cb2d43c8f301fe6209c2
John W. Linville <linville@redhat.com>
2023-10-18 19:16:55 +0200

    cxl/core/regs: Rename @dev to @host in struct cxl_register_map
    JIRA: https://issues.redhat.com/browse/RHEL-10040
    
    The primary role of @dev is to host the mappings for devm operations.
    @dev is too ambiguous as a name. I.e. when does @dev refer to the
    'struct device *' instance that the registers belong, and when does
    @dev refer to the 'struct device *' instance hosting the mapping for
    devm operations?
    
    Clarify the role of @dev in cxl_register_map by renaming it to @host.
    Also, rename local variables to 'host' where map->host is used.
    
    Signed-off-by: Terry Bowman <terry.bowman@amd.com>
    Signed-off-by: Robert Richter <rrichter@amd.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Link: https://lore.kernel.org/r/20231018171713.1883517-3-rrichter@amd.com
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    (cherry picked from commit dd22581f89537163f065e8ef7c125ce0fddf62cc)
    Signed-off-by: John W. Linville <linville@redhat.com>

--- drivers/cxl/cxl.h
+++ drivers/cxl/cxl.h
@@ -214,7 +214,7 @@ struct cxl_device_reg_map {
 
 /**
  * struct cxl_register_map - DVSEC harvested register block mapping parameters
- * @dev: device for devm operations and logging
+ * @host: device for devm operations and logging
  * @base: virtual base of the register-block-BAR + @block_offset
  * @resource: physical resource base of the register block
  * @max_size: maximum mapping size to perform register search
@@ -223,7 +223,7 @@ struct cxl_device_reg_map {
  * @device_map: cxl_reg_maps for device registers
  */
 struct cxl_register_map {
-	struct device *dev;
+	struct device *host;
 	void __iomem *base;
 	resource_size_t resource;
 	resource_size_t max_size;
