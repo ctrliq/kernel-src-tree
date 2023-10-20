03db55932356c89c9e8782e3b305dfd60aaff46e
Chris von Recklinghausen <crecklin@redhat.com>
2023-10-20 06:16:09 -0400

    mm: multi-gen LRU: cleanup lru_gen_soft_reclaim()
    JIRA: https://issues.redhat.com/browse/RHEL-1848
    
    commit 5c7e7a0d79072eb02780a2c0dee730b23cde711d
    Author: T.J. Alumbaugh <talumbau@google.com>
    Date:   Mon May 22 11:20:56 2023 +0000
    
        mm: multi-gen LRU: cleanup lru_gen_soft_reclaim()
    
        lru_gen_soft_reclaim() gets the lruvec from the memcg and node ID to keep a
        cleaner interface on the caller side.
    
        Link: https://lkml.kernel.org/r/20230522112058.2965866-2-talumbau@google.com
        Signed-off-by: T.J. Alumbaugh <talumbau@google.com>
        Reviewed-by: Yuanchu Xie <yuanchu@google.com>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: Yu Zhao <yuzhao@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -544,7 +544,7 @@ void lru_gen_exit_memcg(struct mem_cgroup *memcg);
 void lru_gen_online_memcg(struct mem_cgroup *memcg);
 void lru_gen_offline_memcg(struct mem_cgroup *memcg);
 void lru_gen_release_memcg(struct mem_cgroup *memcg);
-void lru_gen_soft_reclaim(struct lruvec *lruvec);
+void lru_gen_soft_reclaim(struct mem_cgroup *memcg, int nid);
 
 #else /* !CONFIG_MEMCG */
 
@@ -595,7 +595,7 @@ static inline void lru_gen_release_memcg(struct mem_cgroup *memcg)
 {
 }
 
-static inline void lru_gen_soft_reclaim(struct lruvec *lruvec)
+static inline void lru_gen_soft_reclaim(struct mem_cgroup *memcg, int nid)
 {
 }
 
