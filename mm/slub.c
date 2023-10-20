a54b2a2fb0c7b3233cfaf57c45925ffffb924eaf
Chris von Recklinghausen <crecklin@redhat.com>
2023-10-20 06:13:19 -0400

    mm/slab_common: drop kmem_alloc & avoid dereferencing fields when not using
    JIRA: https://issues.redhat.com/browse/RHEL-1848
    
    commit 2c1d697fb8ba6d2d44f914d4268ae1ccdf025f1b
    Author: Hyeonggon Yoo <42.hyeyoo@gmail.com>
    Date:   Wed Aug 17 19:18:24 2022 +0900
    
        mm/slab_common: drop kmem_alloc & avoid dereferencing fields when not using
    
        Drop kmem_alloc event class, and define kmalloc and kmem_cache_alloc
        using TRACE_EVENT() macro.
    
        And then this patch does:
           - Do not pass pointer to struct kmem_cache to trace_kmalloc.
             gfp flag is enough to know if it's accounted or not.
           - Avoid dereferencing s->object_size and s->size when not using kmem_cache_alloc event.
           - Avoid dereferencing s->name in when not using kmem_cache_free event.
           - Adjust s->size to SLOB_UNITS(s->size) * SLOB_UNIT in SLOB
    
        Cc: Vasily Averin <vasily.averin@linux.dev>
        Suggested-by: Vlastimil Babka <vbabka@suse.cz>
        Signed-off-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>
        Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
        Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/slub.c
+++ mm/slub.c
@@ -3328,8 +3328,7 @@ void *__kmem_cache_alloc_lru(struct kmem_cache *s, struct list_lru *lru,
 {
 	void *ret = slab_alloc(s, lru, gfpflags, _RET_IP_, s->object_size);
 
-	trace_kmem_cache_alloc(_RET_IP_, ret, s, s->object_size,
-				s->size, gfpflags, NUMA_NO_NODE);
+	trace_kmem_cache_alloc(_RET_IP_, ret, s, gfpflags, NUMA_NO_NODE);
 
 	return ret;
 }
@@ -3359,8 +3358,7 @@ void *kmem_cache_alloc_node(struct kmem_cache *s, gfp_t gfpflags, int node)
 {
 	void *ret = slab_alloc_node(s, NULL, gfpflags, node, _RET_IP_, s->object_size);
 
-	trace_kmem_cache_alloc(_RET_IP_, ret, s, s->object_size,
-			       s->size, gfpflags, node);
+	trace_kmem_cache_alloc(_RET_IP_, ret, s, gfpflags, node);
 
 	return ret;
 }
@@ -3597,7 +3595,7 @@ void kmem_cache_free(struct kmem_cache *s, void *x)
 	s = cache_from_obj(s, x);
 	if (!s)
 		return;
-	trace_kmem_cache_free(_RET_IP_, x, s->name);
+	trace_kmem_cache_free(_RET_IP_, x, s);
 	slab_free(s, virt_to_slab(x), x, NULL, &x, 1, _RET_IP_);
 }
 EXPORT_SYMBOL(kmem_cache_free);
