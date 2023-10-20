f889afb2e1b7e2ebe451a4556d77441181aecad5
Chris von Recklinghausen <crecklin@redhat.com>
2023-10-20 06:16:07 -0400

    mm: compaction: only force pageblock scan completion when skip hints are obeyed
    JIRA: https://issues.redhat.com/browse/RHEL-1848
    
    commit 9ecc5fc50a9c75b0af252913163a9f1ac4206b17
    Author: Mel Gorman <mgorman@techsingularity.net>
    Date:   Mon May 15 12:33:42 2023 +0100
    
        mm: compaction: only force pageblock scan completion when skip hints are obeyed
    
        fast_find_migrateblock relies on skip hints to avoid rescanning a recently
        selected pageblock but compact_zone() only forces the pageblock scan
        completion to set the skip hint if in direct compaction.  While this
        prevents direct compaction repeatedly scanning a subset of blocks due to
        fast_find_migrateblock(), it does not prevent proactive compaction, node
        compaction and kcompactd encountering the same problem described in commit
        cfccd2e63e7e ("mm, compaction: finish pageblocks on complete migration
        failure").
    
        Force the scan completion of a pageblock to set the skip hint if skip
        hints are obeyed to prevent fast_find_migrateblock() repeatedly selecting
        a subset of pageblocks.
    
        Link: https://lkml.kernel.org/r/20230515113344.6869-3-mgorman@techsingularity.net
        Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
        Suggested-by: Vlastimil Babka <vbabka@suse.cz>
        Tested-by: Raghavendra K T <raghavendra.kt@amd.com>
        Acked-by: Vlastimil Babka <vbabka@suse.cz>
        Cc: Chuyi Zhou <zhouchuyi@bytedance.com>
        Cc: Jiri Slaby <jirislaby@kernel.org>
        Cc: Maxim Levitsky <mlevitsk@redhat.com>
        Cc: Michal Hocko <mhocko@kernel.org>
        Cc: Paolo Bonzini <pbonzini@redhat.com>
        Cc: Pedro Falcato <pedro.falcato@gmail.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -2439,7 +2439,8 @@ rescan:
 			}
 			/*
 			 * If an ASYNC or SYNC_LIGHT fails to migrate a page
-			 * within the current order-aligned block, scan the
+			 * within the current order-aligned block and
+			 * fast_find_migrateblock may be used then scan the
 			 * remainder of the pageblock. This will mark the
 			 * pageblock "skip" to avoid rescanning in the near
 			 * future. This will isolate more pages than necessary
@@ -2448,7 +2449,7 @@ rescan:
 			 * recently partially scanned.
 			 */
 			if (!pageblock_aligned(cc->migrate_pfn) &&
-			    cc->direct_compaction && !cc->finish_pageblock &&
+			    !cc->ignore_skip_hint && !cc->finish_pageblock &&
 			    (cc->mode < MIGRATE_SYNC)) {
 				cc->finish_pageblock = true;
 
