3e2b7d226adfdfb189dc4c3a08b48137b56a2fd4
Chris von Recklinghausen <crecklin@redhat.com>
2023-10-20 06:15:07 -0400

    mm: multi-gen LRU: move lru_gen_add_mm() out of IRQ-off region
    Conflicts: fs/exec.c - We don't have
    	7964cf8caa4d ("mm: remove vmacache")
    	so keep zeroing tsk->mm->vmacache_seqnum and calling vmacache_flush
    
    JIRA: https://issues.redhat.com/browse/RHEL-1848
    
    commit dda1c41a07b4a4c3f99b5b28c1e8c485205fe860
    Author: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Date:   Wed Oct 26 15:48:30 2022 +0200
    
        mm: multi-gen LRU: move lru_gen_add_mm() out of IRQ-off region
    
        lru_gen_add_mm() has been added within an IRQ-off region in the commit
        mentioned below.  The other invocations of lru_gen_add_mm() are not within
        an IRQ-off region.
    
        The invocation within IRQ-off region is problematic on PREEMPT_RT because
        the function is using a spin_lock_t which must not be used within
        IRQ-disabled regions.
    
        The other invocations of lru_gen_add_mm() occur while
        task_struct::alloc_lock is acquired.  Move lru_gen_add_mm() after
        interrupts are enabled and before task_unlock().
    
        Link: https://lkml.kernel.org/r/20221026134830.711887-1-bigeasy@linutronix.d
    e
        Fixes: bd74fdaea1460 ("mm: multi-gen LRU: support page table walks")
        Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
        Acked-by: Yu Zhao <yuzhao@google.com>
        Cc: Al Viro <viro@zeniv.linux.org.uk>
        Cc: "Eric W . Biederman" <ebiederm@xmission.com>
        Cc: Kees Cook <keescook@chromium.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1017,7 +1017,6 @@ static int exec_mmap(struct mm_struct *mm)
 	active_mm = tsk->active_mm;
 	tsk->active_mm = mm;
 	tsk->mm = mm;
-	lru_gen_add_mm(mm);
 	/*
 	 * This prevents preemption while active_mm is being loaded and
 	 * it and mm are being updated, which could cause problems for
