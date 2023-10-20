d93e59472643dd9b667708661eb4966bead2fde4
Chris von Recklinghausen <crecklin@redhat.com>
2023-10-20 06:15:28 -0400

    mm: multi-gen LRU: remove NULL checks on NODE_DATA()
    JIRA: https://issues.redhat.com/browse/RHEL-1848
    
    commit 931b6a8b36a2de3985eca27e758900e70cd99779
    Author: Yu Zhao <yuzhao@google.com>
    Date:   Tue Nov 15 18:38:08 2022 -0700
    
        mm: multi-gen LRU: remove NULL checks on NODE_DATA()
    
        NODE_DATA() is preallocated for all possible nodes after commit
        09f49dca570a ("mm: handle uninitialized numa nodes gracefully").  Checking
        its return value against NULL is now unnecessary.
    
        Link: https://lkml.kernel.org/r/20221116013808.3995280-2-yuzhao@google.com
        Signed-off-by: Yu Zhao <yuzhao@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3168,7 +3168,7 @@ static struct lruvec *get_lruvec(struct mem_cgroup *memcg, int nid)
 	if (memcg) {
 		struct lruvec *lruvec = &memcg->nodeinfo[nid]->lruvec;
 
-		/* for hotadd_new_pgdat() */
+		/* see the comment in mem_cgroup_lruvec() */
 		if (!lruvec->pgdat)
 			lruvec->pgdat = pgdat;
 
@@ -3177,7 +3177,7 @@ static struct lruvec *get_lruvec(struct mem_cgroup *memcg, int nid)
 #endif
 	VM_WARN_ON_ONCE(!mem_cgroup_disabled());
 
-	return pgdat ? &pgdat->__lruvec : NULL;
+	return &pgdat->__lruvec;
 }
 
 static int get_swappiness(struct lruvec *lruvec, struct scan_control *sc)
@@ -3241,9 +3241,6 @@ void lru_gen_add_mm(struct mm_struct *mm)
 	for_each_node_state(nid, N_MEMORY) {
 		struct lruvec *lruvec = get_lruvec(memcg, nid);
 
-		if (!lruvec)
-			continue;
-
 		/* the first addition since the last iteration */
 		if (lruvec->mm_state.tail == &mm_list->fifo)
 			lruvec->mm_state.tail = &mm->lru_gen.list;
@@ -3272,9 +3269,6 @@ void lru_gen_del_mm(struct mm_struct *mm)
 
 	for_each_node(nid) {
 		struct lruvec *lruvec = get_lruvec(memcg, nid);
-
-		if (!lruvec)
-			continue;
 
 		/* where the last iteration ended (exclusive) */
 		if (lruvec->mm_state.tail == &mm->lru_gen.list)
@@ -5355,9 +5349,6 @@ static void lru_gen_change_state(bool enabled)
 
 		for_each_node(nid) {
 			struct lruvec *lruvec = get_lruvec(memcg, nid);
-
-			if (!lruvec)
-				continue;
 
 			spin_lock_irq(&lruvec->lru_lock);
 
