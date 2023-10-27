025eafccec45cc81bb38f9601982e9f0b58c0615
Desnes Nunes <desnesn@redhat.com>
2023-10-27 10:39:56 -0300

    of: device: Ignore modalias of reused nodes
    JIRA: https://issues.redhat.com/browse/RHEL-13058
    Conflicts:
    * Considering already applied commit <a77ad4bf7926> ("of: device: make
      of_device_uevent_modalias() take a const device *")
    
    commit 553bd29700145e1849698985e9800f14e967da49
    Author: Alexander Stein <alexander.stein@ew.tq-group.com>
    Date: Tue, 7 Feb 2023 12:05:29 +0100
    
      If of_node is reused, do not use that node's modalias. This will hide
      the name of the actual device. This is rather prominent in USB glue
      drivers creating a platform device for the host controller.
    
      Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>
      Reviewed-by: Rob Herring <robh@kernel.org>
      Link: https://lore.kernel.org/r/20230207110531.1060252-2-alexander.stein@ew.tq-group.com
      Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Desnes Nunes <desnesn@redhat.com>

--- drivers/of/device.c
+++ drivers/of/device.c
@@ -246,7 +246,7 @@ static ssize_t of_device_get_modalias(const struct device *dev, char *str, ssize
 	ssize_t csize;
 	ssize_t tsize;
 
-	if ((!dev) || (!dev->of_node))
+	if ((!dev) || (!dev->of_node) || dev->of_node_reused)
 		return -ENODEV;
 
 	/* Name & Type */
@@ -369,7 +369,7 @@ int of_device_uevent_modalias(const struct device *dev, struct kobj_uevent_env *
 {
 	int sl;
 
-	if ((!dev) || (!dev->of_node))
+	if ((!dev) || (!dev->of_node) || dev->of_node_reused)
 		return -ENODEV;
 
 	/* Devicetree modalias is tricky, we add it in 2 steps */
