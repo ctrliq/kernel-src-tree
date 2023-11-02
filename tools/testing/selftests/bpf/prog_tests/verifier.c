77d33ff6df0f6419ad5809e1aefaaaa524aa5c7a
Viktor Malik <vmalik@redhat.com>
2023-11-02 07:55:49 +0100

    selftests/bpf: precision tracking test for BPF_NEG and BPF_END
    JIRA: https://issues.redhat.com/browse/RHEL-9957
    
    Conflicts: changed context due to missing upstream commit
               f02ec3ff3f09 ("selftests/bpf: Add unit tests for new sign-extension mov insns")
    
    commit 3c41971550f58f2e006c58aa71e8c23ad312110f
    Author: Shung-Hsi Yu <shung-hsi.yu@suse.com>
    Date:   Thu Nov 2 13:39:05 2023 +0800
    
        selftests/bpf: precision tracking test for BPF_NEG and BPF_END
    
        As seen from previous commit that fix backtracking for BPF_ALU | BPF_TO_BE
        | BPF_END, both BPF_NEG and BPF_END require special handling. Add tests
        written with inline assembly to check that the verifier does not incorrecly
        use the src_reg field of BPF_NEG and BPF_END (including bswap added in v4).
    
        Suggested-by: Eduard Zingerman <eddyz87@gmail.com>
        Signed-off-by: Shung-Hsi Yu <shung-hsi.yu@suse.com>
        Link: https://lore.kernel.org/r/20231102053913.12004-4-shung-hsi.yu@suse.com
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Viktor Malik <vmalik@redhat.com>

--- tools/testing/selftests/bpf/prog_tests/verifier.c
+++ tools/testing/selftests/bpf/prog_tests/verifier.c
@@ -42,6 +42,7 @@
 #include "verifier_meta_access.skel.h"
 #include "verifier_netfilter_ctx.skel.h"
 #include "verifier_netfilter_retcode.skel.h"
+#include "verifier_precision.skel.h"
 #include "verifier_prevent_map_lookup.skel.h"
 #include "verifier_raw_stack.skel.h"
 #include "verifier_raw_tp_writable.skel.h"
@@ -143,6 +144,7 @@ void test_verifier_masking(void)              { RUN(verifier_masking); }
 void test_verifier_meta_access(void)          { RUN(verifier_meta_access); }
 void test_verifier_netfilter_ctx(void)        { RUN(verifier_netfilter_ctx); }
 void test_verifier_netfilter_retcode(void)    { RUN(verifier_netfilter_retcode); }
+void test_verifier_precision(void)            { RUN(verifier_precision); }
 void test_verifier_prevent_map_lookup(void)   { RUN(verifier_prevent_map_lookup); }
 void test_verifier_raw_stack(void)            { RUN(verifier_raw_stack); }
 void test_verifier_raw_tp_writable(void)      { RUN(verifier_raw_tp_writable); }
