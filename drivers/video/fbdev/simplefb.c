f6166bea7192e68cbc2554b2f5050077876f9e0e
Robert Foss <rfoss@kernel.org>
2023-11-07 12:56:02 +0100

    fbdev/simplefb: Support memory-region property
    JIRA: https://issues.redhat.com/browse/RHEL-14708
    Upstream Status: git://anongit.freedesktop.org/drm/drm-misc
    
    commit 8ddfc01ace51c85a2333fb9a9cbea34d9f87885d
    Author:     Thierry Reding <treding@nvidia.com>
    AuthorDate: Wed Nov  1 18:20:16 2023 +0100
    Commit:     Hans de Goede <hdegoede@redhat.com>
    CommitDate: Thu Nov  2 11:17:31 2023 +0100
    
        The simple-framebuffer bindings specify that the "memory-region"
        property can be used as an alternative to the "reg" property to define
        the framebuffer memory used by the display hardware. Implement support
        for this in the simplefb driver.
    
        Reviewed-by: Hans de Goede <hdegoede@redhat.com>
        Signed-off-by: Thierry Reding <treding@nvidia.com>
        Signed-off-by: Hans de Goede <hdegoede@redhat.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20231101172017.3872242-2-thierry.reding@gmail.com
    
    Signed-off-by: Robert Foss <rfoss@kernel.org>

--- drivers/video/fbdev/simplefb.c
+++ drivers/video/fbdev/simplefb.c
@@ -20,6 +20,7 @@
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/of.h>
+#include <linux/of_address.h>
 #include <linux/of_clk.h>
 #include <linux/of_platform.h>
 #include <linux/parser.h>
@@ -120,12 +121,13 @@ struct simplefb_params {
 	u32 height;
 	u32 stride;
 	struct simplefb_format *format;
+	struct resource memory;
 };
 
 static int simplefb_parse_dt(struct platform_device *pdev,
 			   struct simplefb_params *params)
 {
-	struct device_node *np = pdev->dev.of_node;
+	struct device_node *np = pdev->dev.of_node, *mem;
 	int ret;
 	const char *format;
 	int i;
@@ -165,6 +167,23 @@ static int simplefb_parse_dt(struct platform_device *pdev,
 		return -EINVAL;
 	}
 
+	mem = of_parse_phandle(np, "memory-region", 0);
+	if (mem) {
+		ret = of_address_to_resource(mem, 0, &params->memory);
+		if (ret < 0) {
+			dev_err(&pdev->dev, "failed to parse memory-region\n");
+			of_node_put(mem);
+			return ret;
+		}
+
+		if (of_property_present(np, "reg"))
+			dev_warn(&pdev->dev, "preferring \"memory-region\" over \"reg\" property\n");
+
+		of_node_put(mem);
+	} else {
+		memset(&params->memory, 0, sizeof(params->memory));
+	}
+
 	return 0;
 }
 
@@ -191,6 +210,8 @@ static int simplefb_parse_pd(struct platform_device *pdev,
 		dev_err(&pdev->dev, "Invalid format value\n");
 		return -EINVAL;
 	}
+
+	memset(&params->memory, 0, sizeof(params->memory));
 
 	return 0;
 }
@@ -442,10 +463,14 @@ static int simplefb_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (!res) {
-		dev_err(&pdev->dev, "No memory resource\n");
-		return -EINVAL;
+	if (params.memory.start == 0 && params.memory.end == 0) {
+		res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+		if (!res) {
+			dev_err(&pdev->dev, "No memory resource\n");
+			return -EINVAL;
+		}
+	} else {
+		res = &params.memory;
 	}
 
 	mem = request_mem_region(res->start, resource_size(res), "simplefb");
