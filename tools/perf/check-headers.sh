f8f92b91d1583133daee591b698d72775306ddd5
Michael Petlan <mpetlan@redhat.com>
2023-11-08 15:25:19 +0100

    perf build: Fix shellcheck issue about quotes for check-headers.sh
    JIRA: https://issues.redhat.com/browse/RHEL-15009
    
    upstream
    ========
    commit e936584214b93929eb41ec598959469e3a1f6079
    Author: Kajol Jain <kjain@linux.ibm.com>
    Date: Sun Jul 9 23:57:49 2023 +0530
    
    description
    ===========
    Running shellcheck on check-headers.sh generates below warning:
    
    In check-headers.sh line 126:
      check_2 "tools/$file" "$file" $*
                                    ^-- SC2048 (warning): Use "$@" (with quotes) to prevent whitespace problems.
    
    In check-headers.sh line 134:
      check_2 "tools/perf/trace/beauty/$file" "$file" $*
                                                      ^-- SC2048 (warning): Use "$@" (with quotes) to prevent whitespace problems.
    
    In check-headers.sh line 186:
    cd tools/perf
    ^-----------^ SC2164 (warning): Use 'cd ... || exit' or 'cd ... || return' in case cd fails.
    
    Fixed the warnings by:
    - Using "$@" instead of $*
    - Adding exit condition with cd command
    
        Signed-off-by: Kajol Jain <kjain@linux.ibm.com>
        Acked-by: Ian Rogers <irogers@google.com>
        Cc: Disha Goel <disgoel@linux.vnet.ibm.com>
        Cc: Jiri Olsa <jolsa@kernel.org>
        Cc: Madhavan Srinivasan <maddy@linux.ibm.com>
        Cc: Namhyung Kim <namhyung@kernel.org>
        Cc: linuxppc-dev@lists.ozlabs.org
        Link: https://lore.kernel.org/r/20230709182800.53002-16-atrajeev@linux.vnet.ibm.com
        Signed-off-by: Athira Rajeev <atrajeev@linux.vnet.ibm.com>
        Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- tools/perf/check-headers.sh
+++ tools/perf/check-headers.sh
@@ -122,7 +122,7 @@ check () {
 
   shift
 
-  check_2 "tools/$file" "$file" $*
+  check_2 "tools/$file" "$file" "$@"
 }
 
 beauty_check () {
@@ -130,7 +130,7 @@ beauty_check () {
 
   shift
 
-  check_2 "tools/perf/trace/beauty/$file" "$file" $*
+  check_2 "tools/perf/trace/beauty/$file" "$file" "$@"
 }
 
 # Check if we have the kernel headers (tools/perf/../../include), else
@@ -182,7 +182,7 @@ done
 check_2 tools/perf/util/hashmap.h tools/lib/bpf/hashmap.h
 check_2 tools/perf/util/hashmap.c tools/lib/bpf/hashmap.c
 
-cd tools/perf
+cd tools/perf || exit
 
 if [ ${#FAILURES[@]} -gt 0 ]
 then
