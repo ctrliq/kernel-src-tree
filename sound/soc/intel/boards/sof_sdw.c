faa25f1ac8379114d1a845f54f26d72c11bc8beb
Jaroslav Kysela <jkysela@redhat.com>
2023-11-09 14:21:58 +0100

    ASoC: intel: sof_sdw: Use a module device table
    JIRA: https://issues.redhat.com/browse/RHEL-13724
    
    Conflicts: fix remove/remove_new clash in struct platform_driver
    
    commit 18c45cb362fcd6918548a4e11128304aa634acf3
    Author: Charles Keepax <ckeepax@opensource.cirrus.com>
    Date: Mon Jul 31 16:42:46 2023 -0500
    
        ASoC: intel: sof_sdw: Use a module device table
    
        A module device table is generally preferred over hard coding a
        MODULE_ALIAS.
    
        Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
        Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
        Reviewed-by: Rander Wang <rander.wang@intel.com>
        Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
        Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
        Link: https://lore.kernel.org/r/20230731214257.444605-13-pierre-louis.bossart@linux.intel.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/soc/intel/boards/sof_sdw.c
+++ sound/soc/intel/boards/sof_sdw.c
@@ -2047,6 +2047,12 @@ static int mc_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static const struct platform_device_id mc_id_table[] = {
+	{ "sof_sdw", },
+	{}
+};
+MODULE_DEVICE_TABLE(platform, mc_id_table);
+
 static struct platform_driver sof_sdw_driver = {
 	.driver = {
 		.name = "sof_sdw",
@@ -2054,6 +2060,7 @@ static struct platform_driver sof_sdw_driver = {
 	},
 	.probe = mc_probe,
 	.remove = mc_remove,
+	.id_table = mc_id_table,
 };
 
 module_platform_driver(sof_sdw_driver);
@@ -2063,6 +2070,5 @@ MODULE_AUTHOR("Bard Liao <yung-chuan.liao@linux.intel.com>");
 MODULE_AUTHOR("Rander Wang <rander.wang@linux.intel.com>");
 MODULE_AUTHOR("Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("platform:sof_sdw");
 MODULE_IMPORT_NS(SND_SOC_INTEL_HDA_DSP_COMMON);
 MODULE_IMPORT_NS(SND_SOC_INTEL_SOF_MAXIM_COMMON);
