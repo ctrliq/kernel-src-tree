d09aee95b412c08564aaa0ed553874f749bc964b
Jaroslav Kysela <jkysela@redhat.com>
2023-11-09 14:45:58 +0100

    ASoC: Intel: soc-acpi: Add entry for HDMI_In capture support in RPL match table
    JIRA: https://issues.redhat.com/browse/RHEL-13724
    
    Conflicts: Handle upstream code reshuffle (merge commit 63e041027669473ab7474c195771bc86af9685a6)
    
    commit fbc82c016d96aa4a2d99587cae2b78d5c5a59ea7
    Author: Balamurugan C <balamurugan.c@intel.com>
    Date: Mon Aug 14 18:13:56 2023 -0500
    
        ASoC: Intel: soc-acpi: Add entry for HDMI_In capture support in RPL match table
    
        Adding HDMI-In capture via I2S feature support in RPL platfroms.
    
        Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
        Signed-off-by: Balamurugan C <balamurugan.c@intel.com>
        Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
        Link: https://lore.kernel.org/r/20230814231358.78971-3-pierre-louis.bossart@linux.intel.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/soc/intel/common/soc-acpi-intel-rpl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-rpl-match.c
@@ -351,14 +351,19 @@ static const struct snd_soc_acpi_codecs rpl_max98373_amp = {
 	.codecs = {"MX98373"}
 };
 
-static const struct snd_soc_acpi_codecs rpl_rt1019p_amp = {
+static const struct snd_soc_acpi_codecs rpl_lt6911_hdmi = {
 	.num_codecs = 1,
-	.codecs = {"RTL1019"}
+	.codecs = {"INTC10B0"}
 };
 
 static const struct snd_soc_acpi_codecs rpl_nau8318_amp = {
 	.num_codecs = 1,
 	.codecs = {"NVTN2012"}
+};
+
+static const struct snd_soc_acpi_codecs rpl_rt1019p_amp = {
+	.num_codecs = 1,
+	.codecs = {"RTL1019"}
 };
 
 struct snd_soc_acpi_mach snd_soc_acpi_intel_rpl_machines[] = {
@@ -396,6 +401,13 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_rpl_machines[] = {
 		.machine_quirk = snd_soc_acpi_codec_list,
 		.quirk_data = &rpl_rt1019p_amp,
 		.sof_tplg_filename = "sof-rpl-rt1019-rt5682.tplg",
+	},
+	{
+		.comp_ids = &rpl_essx_83x6,
+		.drv_name = "rpl_es83x6_c1_h02",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &rpl_lt6911_hdmi,
+		.sof_tplg_filename = "sof-rpl-es83x6-ssp1-hdmi-ssp02.tplg",
 	},
 	{
 		.comp_ids = &rpl_essx_83x6,
