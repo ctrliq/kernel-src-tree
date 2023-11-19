14fa9c73d2c1b633308c61b75e30ff83e4880b99
Bill O'Donnell <bodonnel@redhat.com>
2023-11-19 16:21:08 -0600

    xfs: check that per-cpu inodegc workers actually run on that cpu
    JIRA: https://issues.redhat.com/browse/RHEL-15844
    
    Conflicts: diff in xfs_super.c due to previous out of order patch
    
    commit b37c4c8339cd394ea6b8b415026603320a185651
    Author: Darrick J. Wong <djwong@kernel.org>
    Date:   Tue May 2 09:16:12 2023 +1000
    
        xfs: check that per-cpu inodegc workers actually run on that cpu
    
        Now that we've allegedly worked out the problem of the per-cpu inodegc
        workers being scheduled on the wrong cpu, let's put in a debugging knob
        to let us know if a worker ever gets mis-scheduled again.
    
        Signed-off-by: Darrick J. Wong <djwong@kernel.org>
        Reviewed-by: Dave Chinner <dchinner@redhat.com>
        Signed-off-by: Dave Chinner <david@fromorbit.com>
    
    Signed-off-by: Bill O'Donnell <bodonnel@redhat.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1107,6 +1107,9 @@ xfs_inodegc_init_percpu(
 
 	for_each_possible_cpu(cpu) {
 		gc = per_cpu_ptr(mp->m_inodegc, cpu);
+#if defined(DEBUG) || defined(XFS_WARN)
+		gc->cpu = cpu;
+#endif
 		init_llist_head(&gc->list);
 		gc->items = 0;
 		gc->error = 0;
