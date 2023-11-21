61d807d6769acece0397d029b504eddbf2fd9dab
Alexander Larsson <alexl@redhat.com>
2023-11-21 16:05:42 +0100

    erofs: Add tech preview markers at mount
    This is added to the cs9 kernel in:
    https://gitlab.com/redhat/centos-stream/src/kernel/centos-stream-9/-/merge_requests/3317
    
    Copying it to ARK to not forget this.
    
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -581,6 +581,9 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 {
 	struct inode *inode;
 	struct erofs_sb_info *sbi = EROFS_SB(sb);
+#ifdef CONFIG_RHEL_DIFFERENCES
+	static bool printed = false;
+#endif
 	int err;
 
 	sb->s_magic = EROFS_SUPER_MAGIC;
@@ -687,6 +690,12 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 		return err;
 
 	erofs_info(sb, "mounted with root inode @ nid %llu.", sbi->root_nid);
+#ifdef CONFIG_RHEL_DIFFERENCES
+	if (!printed) {
+		mark_tech_preview("EROFS filesystem", NULL);
+		printed = true;
+       }
+#endif
 	return 0;
 }
 
