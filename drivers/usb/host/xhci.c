addd42fa8454bd32704a982f9582d6271df662ea
Jose Ignacio Tornos Martinez <jtornosm@redhat.com>
2023-11-27 15:02:19 +0100

    USB: Remove remnants of Wireless USB and UWB
    JIRA: https://issues.redhat.com/browse/RHEL-2907
    
    commit 1e4c574225cc5a0553115e5eb5787d1474db5b0f
    Author: Alan Stern <stern@rowland.harvard.edu>
    Date:   Tue Aug 8 20:44:18 2023 -0400
    
        USB: Remove remnants of Wireless USB and UWB
        
        Wireless USB has long been defunct, and kernel support for it was
        removed in 2020 by commit caa6772db4c1 ("Staging: remove wusbcore and
        UWB from the kernel tree.").
        
        Nevertheless, some vestiges of the old implementation still clutter up
        the USB subsystem and one or two other places.  Let's get rid of them
        once and for all.
        
        The only parts still left are the user-facing APIs in
        include/uapi/linux/usb/ch9.h.  (There are also a couple of misleading
        instances, such as the Sierra Wireless USB modem, which is a USB modem
        made by Sierra Wireless.)
        
        Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
        Link: https://lore.kernel.org/r/b4f2710f-a2de-4fb0-b50f-76776f3a961b@rowland.harvard.edu
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Jose Ignacio Tornos Martinez <jtornosm@redhat.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -2192,7 +2192,6 @@ static unsigned int xhci_get_block_size(struct usb_device *udev)
 	case USB_SPEED_SUPER_PLUS:
 		return SS_BLOCK;
 	case USB_SPEED_UNKNOWN:
-	case USB_SPEED_WIRELESS:
 	default:
 		/* Should never happen */
 		return 1;
@@ -2553,10 +2552,7 @@ static void xhci_drop_ep_from_interval_table(struct xhci_hcd *xhci,
 	case USB_SPEED_HIGH:
 		interval_bw->overhead[HS_OVERHEAD_TYPE] -= 1;
 		break;
-	case USB_SPEED_SUPER:
-	case USB_SPEED_SUPER_PLUS:
-	case USB_SPEED_UNKNOWN:
-	case USB_SPEED_WIRELESS:
+	default:
 		/* Should never happen because only LS/FS/HS endpoints will get
 		 * added to the endpoint list.
 		 */
@@ -2613,10 +2609,7 @@ static void xhci_add_ep_to_interval_table(struct xhci_hcd *xhci,
 	case USB_SPEED_HIGH:
 		interval_bw->overhead[HS_OVERHEAD_TYPE] += 1;
 		break;
-	case USB_SPEED_SUPER:
-	case USB_SPEED_SUPER_PLUS:
-	case USB_SPEED_UNKNOWN:
-	case USB_SPEED_WIRELESS:
+	default:
 		/* Should never happen because only LS/FS/HS endpoints will get
 		 * added to the endpoint list.
 		 */
