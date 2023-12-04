d9cfd418bbe7d7c3c58b98c93dd8a8c8bad572cb
Justin M. Forbes <jforbes@fedoraproject.org>
2023-12-04 13:09:40 -0600

    Add some initial Fedora stable branch script to redhat/scripts/fedora/
    These scripts are of less use to the os-build branch, but need to exist
    in the stable Fedora branches.  A future script will be created to do
    the right thing with them when creating a new stable Fedora branch.
    
    Signed-off-by: Justin M. Forbes <jforbes@fedoraproject.org>

---
+++ redhat/scripts/fedora/fedora-stable-release.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+releasenum=$1
+
+# Releasenum is really just the last digit as we have series based on Fedora Release
+# by default we assume 0, but if you need to do a rebuild with a fix before a new release
+# you can call the script with a 1 or whatever digit you wish for the releasenum.
+if [ -z "$releasenum" ]; then
+	releasenum="0"
+fi
+
+klist -s
+if [ ! $? -eq 0 ]; then
+	echo "klist couldn't read the credential cache."
+	echo "Do you need to fix your kerberos tokens?"
+	exit 1
+fi
+
+
+# Occasionally we have a patch which  needs to be applied only to a specific
+# Fedora release for whatever reason.  If this is the case, we can set
+# ApplyPatches="1" under that releases case switch and we will apply any
+# patches from redhat/patches for that release only.
+ApplyPatches="0"
+
+for release in $( cat redhat/release_targets );  do 
+	case "$release" in
+	40) build=30$releasenum
+	    ;;
+	39) build=20$releasenum
+	    ;;
+	38) build=10$releasenum
+	    ;;
+	esac
+	if [[ $ApplyPatches == "1" ]] ; then
+		for patch in redhat/patches/* ; do patch -p1 < $patch ; done
+	fi
+	make IS_FEDORA=1 DIST=".fc$release" BUILDID="" BUILD=$build RHDISTGIT_BRANCH=f$release dist-git;
+	sleep 60;
+	git checkout .
+done
