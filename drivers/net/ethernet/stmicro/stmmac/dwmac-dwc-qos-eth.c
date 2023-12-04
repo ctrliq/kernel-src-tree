7017320d90aebb68799b1893743e66d7d230eef8
Eric Chanudet <echanude@redhat.com>
2023-12-04 18:46:27 -0500

    net: stmmac: add new mode parameter for fix_mac_speed
    JIRA: https://issues.redhat.com/browse/RHEL-20494
    Conflicts: drop dwmac-starfive.c hunk, not supported CentOS.
    
    commit 1fc04a0b973392df5975901f56addc913d2c8f4d
    Author: Shenwei Wang <shenwei.wang@nxp.com>
    Date:   Mon Aug 7 11:07:15 2023 -0500
    
        net: stmmac: add new mode parameter for fix_mac_speed
    
        A mode parameter has been added to the callback function of fix_mac_speed
        to indicate the physical layer type.
    
        The mode can be one the following:
                MLO_AN_PHY      - Conventional PHY
                MLO_AN_FIXED    - Fixed-link mode
                MLO_AN_INBAND   - In-band protocol
    
        Signed-off-by: Shenwei Wang <shenwei.wang@nxp.com>
        Link: https://lore.kernel.org/r/20230807160716.259072-2-shenwei.wang@nxp.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Eric Chanudet <echanude@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-dwc-qos-eth.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-dwc-qos-eth.c
@@ -180,7 +180,7 @@ static int dwc_qos_remove(struct platform_device *pdev)
 #define AUTO_CAL_STATUS 0x880c
 #define  AUTO_CAL_STATUS_ACTIVE BIT(31)
 
-static void tegra_eqos_fix_speed(void *priv, unsigned int speed)
+static void tegra_eqos_fix_speed(void *priv, unsigned int speed, unsigned int mode)
 {
 	struct tegra_eqos *eqos = priv;
 	unsigned long rate = 125000000;
