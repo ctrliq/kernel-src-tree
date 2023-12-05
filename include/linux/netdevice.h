7000cca296deb9f2159a6cc5817bda73c9723c26
Ivan Vecera <ivecera@redhat.com>
2023-12-05 16:49:56 +0100

    net: add NDOs for configuring hardware timestamping
    JIRA: https://issues.redhat.com/browse/RHEL-18147
    
    Conflicts:
    - Context conflict due to missing commit 97dc7cd92ac6 ("ptp: Support
      late timestamp determination")
    
    commit 66f7223039c0cef81221e9779520479895995815
    Author: Maxim Georgiev <glipus@gmail.com>
    Date:   Tue Aug 1 17:28:13 2023 +0300
    
        net: add NDOs for configuring hardware timestamping
    
        Current hardware timestamping API for NICs requires implementing
        .ndo_eth_ioctl() for SIOCGHWTSTAMP and SIOCSHWTSTAMP.
    
        That API has some boilerplate such as request parameter translation
        between user and kernel address spaces, handling possible translation
        failures correctly, etc. Since it is the same all across the board, it
        would be desirable to handle it through generic code.
    
        Here we introduce .ndo_hwtstamp_get() and .ndo_hwtstamp_set(), which
        implement that boilerplate and allow drivers to just act upon requests.
    
        Suggested-by: Jakub Kicinski <kuba@kernel.org>
        Signed-off-by: Maxim Georgiev <glipus@gmail.com>
        Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
        Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
        Tested-by: Horatiu Vultur <horatiu.vultur@microchip.com>
        Link: https://lore.kernel.org/r/20230801142824.1772134-2-vladimir.oltean@nxp.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -56,6 +56,7 @@
 struct netpoll_info;
 struct device;
 struct ethtool_ops;
+struct kernel_hwtstamp_config;
 struct phy_device;
 struct dsa_port;
 struct ip_tunnel_parm;
@@ -1374,6 +1375,15 @@ struct netdev_net_notifier {
  *	The caller must be under RCU read context.
  * int (*ndo_fill_forward_path)(struct net_device_path_ctx *ctx, struct net_device_path *path);
  *     Get the forwarding path to reach the real device from the HW destination address
+ * int (*ndo_hwtstamp_get)(struct net_device *dev,
+ *			   struct kernel_hwtstamp_config *kernel_config);
+ *	Get the currently configured hardware timestamping parameters for the
+ *	NIC device.
+ *
+ * int (*ndo_hwtstamp_set)(struct net_device *dev,
+ *			   struct kernel_hwtstamp_config *kernel_config,
+ *			   struct netlink_ext_ack *extack);
+ *	Change the hardware timestamping parameters for NIC device.
  */
 struct net_device_ops {
 	int			(*ndo_init)(struct net_device *dev);
@@ -1596,6 +1606,11 @@ struct net_device_ops {
 	struct net_device *	(*ndo_get_peer_dev)(struct net_device *dev);
 	int                     (*ndo_fill_forward_path)(struct net_device_path_ctx *ctx,
                                                          struct net_device_path *path);
+	int			(*ndo_hwtstamp_get)(struct net_device *dev,
+						    struct kernel_hwtstamp_config *kernel_config);
+	int			(*ndo_hwtstamp_set)(struct net_device *dev,
+						    struct kernel_hwtstamp_config *kernel_config,
+						    struct netlink_ext_ack *extack);
 };
 
 struct xdp_metadata_ops {
