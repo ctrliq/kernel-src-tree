32f51b3565c6d5e1d24f5b03eb81e7536a619fc5
Michal Schmidt <mschmidt@redhat.com>
2023-12-06 16:04:31 +0100

    octeon_ep: ensure octep_get_link_status() successfully before octep_link_up()
    JIRA: https://issues.redhat.com/browse/RHEL-9308
    
    commit 9d3ff7131877fb092185c369fbb14b57ac4e7cec
    Author: Ziyang Xuan <william.xuanziyang@huawei.com>
    Date:   Fri Nov 11 15:08:47 2022 +0800
    
        octeon_ep: ensure octep_get_link_status() successfully before octep_link_up()
    
        octep_get_link_status() can fail because send mbox message failed, then
        octep_get_link_status() will return ret less than 0. Excute octep_link_up()
        as long as ret is not equal to 0 in octep_open() now. That is not correct.
    
        The value type of link.state is enum octep_ctrl_net_state. Positive value
        represents up. Excute octep_link_up() when ret is bigger than 0.
    
        Fixes: 862cd659a6fb ("octeon_ep: Add driver framework and device initialization")
        Signed-off-by: Ziyang Xuan <william.xuanziyang@huawei.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/marvell/octeon_ep/octep_main.c
+++ drivers/net/ethernet/marvell/octeon_ep/octep_main.c
@@ -522,7 +522,7 @@ static int octep_open(struct net_device *netdev)
 	octep_oq_dbell_init(oct);
 
 	ret = octep_get_link_status(oct);
-	if (ret)
+	if (ret > 0)
 		octep_link_up(netdev);
 
 	return 0;
