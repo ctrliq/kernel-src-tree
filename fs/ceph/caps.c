707d730cfd6d6f0583a39995ca1858e33f5d83d5
Xiubo Li <xiubli@redhat.com>
2023-12-12 12:42:26 +0800

    ceph: don't get the inline data for new creating files
    JIRA: https://issues.redhat.com/browse/RHEL-19813
    
    commit 4849077604f0126514d487836e7d87c3e53a753c
    Author: Xiubo Li <xiubli@redhat.com>
    Date:   Tue Jun 7 10:13:53 2022 +0800
    
        ceph: don't get the inline data for new creating files
    
        If the 'i_inline_version' is 1, that means the file is just new
        created and there shouldn't have any inline data in it, we should
        skip retrieving the inline data from MDS.
    
        This also could help reduce possiblity of dead lock issue introduce
        by the inline data and Fcr caps.
    
        Gradually we will remove the inline feature from kclient after ceph's
        scrub too have support to unline the inline data, currently this
        could help reduce the teuthology test failures.
    
        This is possiblly could also fix a bug that for some old clients if
        they couldn't explictly uninline the inline data when writing, the
        inline version will keep as 1 always. We may always reading non-exist
        data from inline data.
    
        Signed-off-by: Xiubo Li <xiubli@redhat.com>
        Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -3050,7 +3050,7 @@ int __ceph_get_caps(struct inode *inode, struct ceph_file_info *fi, int need,
 		}
 
 		if (S_ISREG(ci->netfs.inode.i_mode) &&
-		    ci->i_inline_version != CEPH_INLINE_NONE &&
+		    ceph_has_inline_data(ci) &&
 		    (_got & (CEPH_CAP_FILE_CACHE|CEPH_CAP_FILE_LAZYIO)) &&
 		    i_size_read(inode) > 0) {
 			struct page *page =
