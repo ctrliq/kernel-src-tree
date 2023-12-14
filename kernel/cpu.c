fc2db2d8a5a05c3cb7bd703ca95a1a4879e5dc81
David Arcari <darcari@redhat.com>
2023-12-14 14:23:37 -0500

    cpu/hotplug: Fix off by one in cpuhp_bringup_mask()
    JIRA: https://issues.redhat.com/browse/RHEL-19514
    
    commit 06c6796e0304234da65e70577f354cb194086521
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Tue May 23 01:12:26 2023 +0200
    
        cpu/hotplug: Fix off by one in cpuhp_bringup_mask()
    
        cpuhp_bringup_mask() iterates over a cpumask and starts all present CPUs up
        to a caller provided upper limit.
    
        The limit variable is decremented and checked for 0 before invoking
        cpu_up(), which is obviously off by one and prevents the bringup of the
        last CPU when the limit is equal to the number of present CPUs.
    
        Move the decrement and check after the cpu_up() invocation.
    
        Fixes: 18415f33e2ac ("cpu/hotplug: Allow "parallel" bringup up to CPUHP_BP_KICK_AP_STATE")
        Reported-by: Mark Brown <broonie@kernel.org>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Tested-by: Mark Brown <broonie@kernel.org>
        Link: https://lore.kernel.org/r/87wn10ufj9.ffs@tglx
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1788,9 +1788,6 @@ static void __init cpuhp_bringup_mask(const struct cpumask *mask, unsigned int n
 	for_each_cpu(cpu, mask) {
 		struct cpuhp_cpu_state *st = per_cpu_ptr(&cpuhp_state, cpu);
 
-		if (!--ncpus)
-			break;
-
 		if (cpu_up(cpu, target) && can_rollback_cpu(st)) {
 			/*
 			 * If this failed then cpu_up() might have only
@@ -1799,6 +1796,9 @@ static void __init cpuhp_bringup_mask(const struct cpumask *mask, unsigned int n
 			 */
 			WARN_ON(cpuhp_invoke_callback_range(false, cpu, st, CPUHP_OFFLINE));
 		}
+
+		if (!--ncpus)
+			break;
 	}
 }
 
