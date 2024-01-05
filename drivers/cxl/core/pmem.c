7dc15ec7673bee1310ca87a3fd66ee792798d5da
John W. Linville <linville@redhat.com>
2024-01-05 15:07:53 -0700

    cxl: Refactor to use __free() for cxl_root allocation in cxl_find_nvdimm_bridge()
    JIRA: https://issues.redhat.com/browse/RHEL-54609
    
    Use scope-based resource management __free() macro to drop the open coded
    put_device() in cxl_find_nvdimm_bridge().
    
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Link: https://lore.kernel.org/r/170449247353.3779673.5963704495491343135.stgit@djiang5-mobl3
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    (cherry picked from commit 66f11890d35a609012037cccfd9e63ed98474f99)
    
    Conflicts:	(context fixup required after patches merged out of order)
    	drivers/cxl/core/pmem.c
    Signed-off-by: John W. Linville <linville@redhat.com>

--- drivers/cxl/core/pmem.c
+++ drivers/cxl/core/pmem.c
@@ -69,14 +69,13 @@ static int match_nvdimm_bridge(struct device *dev, void *data)
  */
 struct cxl_nvdimm_bridge *cxl_find_nvdimm_bridge(struct cxl_port *port)
 {
-	struct cxl_root *cxl_root = find_cxl_root(port);
+	struct cxl_root *cxl_root __free(put_cxl_root) = find_cxl_root(port);
 	struct device *dev;
 
 	if (!cxl_root)
 		return NULL;
 
-	port = &cxl_root->port;
-	dev = device_find_child(&port->dev, NULL, match_nvdimm_bridge);
+	dev = device_find_child(&cxl_root->port.dev, NULL, match_nvdimm_bridge);
 
 	if (!dev)
 		return NULL;
