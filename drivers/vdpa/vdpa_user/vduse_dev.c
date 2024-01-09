90afa5973f41ed4b77ed1925bcc1cc056254474a
Cindy Lu <lulu@redhat.com>
2024-01-09 12:10:25 +0100

    vduse: enable Virtio-net device type
    JIRA: https://issues.redhat.com/browse/RHEL-23888
    
    This patch adds Virtio-net device type to the supported
    devices types.
    
    Initialization fails if the device does not support
    VIRTIO_F_VERSION_1 feature, in order to guarantee the
    configuration space is read-only. It also fails with
    -EPERM if the CAP_NET_ADMIN is missing.
    
    Acked-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Eugenio Pérez <eperezma@redhat.com>
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Message-Id: <20240109111025.1320976-4-maxime.coquelin@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Xie Yongji <xieyongji@bytedance.com>
    (cherry picked from commit 894452180d732413fd29fa95a4820560fa44ca4a)
    Signed-off-by: Cindy Lu <lulu@redhat.com>

--- drivers/vdpa/vdpa_user/vduse_dev.c
+++ drivers/vdpa/vdpa_user/vduse_dev.c
@@ -141,6 +141,7 @@ static struct workqueue_struct *vduse_irq_bound_wq;
 
 static u32 allowed_device_id[] = {
 	VIRTIO_ID_BLOCK,
+	VIRTIO_ID_NET,
 };
 
 static inline struct vduse_dev *vdpa_to_vduse(struct vdpa_device *vdpa)
@@ -1703,6 +1704,10 @@ static bool features_is_valid(struct vduse_dev_config *config)
 			(config->features & BIT_ULL(VIRTIO_BLK_F_CONFIG_WCE)))
 		return false;
 
+	if ((config->device_id == VIRTIO_ID_NET) &&
+			!(config->features & BIT_ULL(VIRTIO_F_VERSION_1)))
+		return false;
+
 	return true;
 }
 
@@ -1809,6 +1814,10 @@ static int vduse_create_dev(struct vduse_dev_config *config,
 {
 	int ret;
 	struct vduse_dev *dev;
+
+	ret = -EPERM;
+	if ((config->device_id == VIRTIO_ID_NET) && !capable(CAP_NET_ADMIN))
+		goto err;
 
 	ret = -EEXIST;
 	if (vduse_find_dev(config->name))
@@ -2053,6 +2062,7 @@ static const struct vdpa_mgmtdev_ops vdpa_dev_mgmtdev_ops = {
 
 static struct virtio_device_id id_table[] = {
 	{ VIRTIO_ID_BLOCK, VIRTIO_DEV_ANY_ID },
+	{ VIRTIO_ID_NET, VIRTIO_DEV_ANY_ID },
 	{ 0 },
 };
 
