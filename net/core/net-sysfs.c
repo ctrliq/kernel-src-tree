1a10ee13b574920e28250dbe5dd4c69ac3e273f9
Sabrina Dubroca <sdubroca@redhat.com>
2024-01-11 15:13:26 +0100

    net: add reserved fields to rtnl_link_stats*
    JIRA: https://issues.redhat.com/browse/RHEL-21356
    Upstream Status: RHEL-only
    
    rtnl_link_stats and rtnl_link_stats64 are protected by kABI, add 4
    reserved fields. We need to use a custom mechanism here, because those
    structures are part of uapi.
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -675,7 +675,7 @@ static ssize_t netstat_show(const struct device *d,
 	struct net_device *dev = to_net_dev(d);
 	ssize_t ret = -EINVAL;
 
-	WARN_ON(offset > sizeof(struct rtnl_link_stats64) ||
+	WARN_ON(offset > sizeof_rtnl_link_stats64 ||
 		offset % sizeof(u64) != 0);
 
 	read_lock(&dev_base_lock);
