2b51fbf27b1ca234893cd743b886f9c7b32d90cb
Vitaly Kuznetsov <vkuznets@redhat.com>
2024-01-23 17:43:02 +0100

    x86/sev: Move sev_setup_arch() to mem_encrypt.c
    JIRA: https://issues.redhat.com/browse/RHEL-25415
    
    commit 6e74b125155dc8c747d76fb45d8e6d20e9e4fb4d
    Author: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Date:   Tue Oct 10 17:52:19 2023 +0300
    
        x86/sev: Move sev_setup_arch() to mem_encrypt.c
    
        Since commit:
    
          4d96f9109109b ("x86/sev: Replace occurrences of sev_active() with cc_platform_has()")
    
        ... the SWIOTLB bounce buffer size adjustment and restricted virtio memory
        setting also inadvertently apply to TDX: the code is using
        cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT) as a gatekeeping condition,
        which is also true for TDX, and this is also what we want.
    
        To reflect this, move the corresponding code to generic mem_encrypt.c.
    
        No functional changes intended.
    
        Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
        Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
        Link: https://lore.kernel.org/r/20231010145220.3960055-2-alexander.shishkin@linux.intel.com
    
    [pbonzini: Conflict: RHEL uses arch_has_restricted_virtio_memory_access
     instead of virtio_set_mem_acc_cb]
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- arch/x86/mm/mem_encrypt.c
+++ arch/x86/mm/mem_encrypt.c
@@ -98,3 +98,33 @@ int arch_has_restricted_virtio_memory_access(void)
 	return cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT);
 }
 EXPORT_SYMBOL_GPL(arch_has_restricted_virtio_memory_access);
+
+void __init mem_encrypt_setup_arch(void)
+{
+	phys_addr_t total_mem = memblock_phys_mem_size();
+	unsigned long size;
+
+	if (!cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT))
+		return;
+
+	/*
+	 * For SEV and TDX, all DMA has to occur via shared/unencrypted pages.
+	 * Kernel uses SWIOTLB to make this happen without changing device
+	 * drivers. However, depending on the workload being run, the
+	 * default 64MB of SWIOTLB may not be enough and SWIOTLB may
+	 * run out of buffers for DMA, resulting in I/O errors and/or
+	 * performance degradation especially with high I/O workloads.
+	 *
+	 * Adjust the default size of SWIOTLB using a percentage of guest
+	 * memory for SWIOTLB buffers. Also, as the SWIOTLB bounce buffer
+	 * memory is allocated from low memory, ensure that the adjusted size
+	 * is within the limits of low available memory.
+	 *
+	 * The percentage of guest memory used here for SWIOTLB buffers
+	 * is more of an approximation of the static adjustment which
+	 * 64MB for <1G, and ~128M to 256M for 1G-to-4G, i.e., the 6%
+	 */
+	size = total_mem * 6 / 100;
+	size = clamp_val(size, IO_TLB_DEFAULT_SIZE, SZ_1G);
+	swiotlb_adjust_size(size);
+}
