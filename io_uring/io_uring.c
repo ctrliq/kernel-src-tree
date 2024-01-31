9b21e6036acee2828af10cd73e746517252f9932
Jeff Moyer <jmoyer@redhat.com>
2024-01-31 10:39:05 -0700

    io_uring: remove next io_kiocb fetch in task_work running
    JIRA: https://issues.redhat.com/browse/RHEL-64867
    
    commit 670d9d3df8808b39430ade7a04b38363971167f5
    Author: Jens Axboe <axboe@kernel.dk>
    Date:   Wed Jan 31 10:39:05 2024 -0700
    
        io_uring: remove next io_kiocb fetch in task_work running
        
        We just reversed the task_work list and that will have touched requests
        as well, just get rid of this optimization as it should not make a
        difference anymore.
        
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -1181,8 +1181,6 @@ static unsigned int handle_tw_list(struct llist_node *node,
 		struct io_kiocb *req = container_of(node, struct io_kiocb,
 						    io_task_work.node);
 
-		prefetch(container_of(next, struct io_kiocb, io_task_work.node));
-
 		if (req->ctx != *ctx) {
 			ctx_flush_and_put(*ctx, ts);
 			*ctx = req->ctx;
@@ -1418,7 +1416,6 @@ again:
 		struct llist_node *next = node->next;
 		struct io_kiocb *req = container_of(node, struct io_kiocb,
 						    io_task_work.node);
-		prefetch(container_of(next, struct io_kiocb, io_task_work.node));
 		INDIRECT_CALL_2(req->io_task_work.func,
 				io_poll_task_func, io_req_rw_complete,
 				req, ts);
