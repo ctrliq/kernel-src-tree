c8a912b06d05baaaaf5553be6247b31264aefca8
Prarit Bhargava <prarit@redhat.com>
2024-02-06 16:13:51 -0500

    locking: remove spin_lock_prefetch
    JIRA: https://issues.redhat.com/browse/RHEL-25415
    
    commit c8afaa1b0f8bc93d013ab2ea6b9649958af3f1d3
    Author: Mateusz Guzik <mjguzik@gmail.com>
    Date:   Sat Aug 12 18:15:54 2023 +0200
    
        locking: remove spin_lock_prefetch
    
        The only remaining consumer is new_inode, where it showed up in 2001 as
        commit c37fa164f793 ("v2.4.9.9 -> v2.4.9.10") in a historical repo [1]
        with a changelog which does not mention it.
    
        Since then the line got only touched up to keep compiling.
    
        While it may have been of benefit back in the day, it is guaranteed to
        at best not get in the way in the multicore setting -- as the code
        performs *a lot* of work between the prefetch and actual lock acquire,
        any contention means the cacheline is already invalid by the time the
        routine calls spin_lock().  It adds spurious traffic, for short.
    
        On top of it prefetch is notoriously tricky to use for single-threaded
        purposes, making it questionable from the get go.
    
        As such, remove it.
    
        I admit upfront I did not see value in benchmarking this change, but I
        can do it if that is deemed appropriate.
    
        Removal from new_inode and of the entire thing are in the same patch as
        requested by Linus, so whatever weird looks can be directed at that guy.
    
        Link: https://git.kernel.org/pub/scm/linux/kernel/git/tglx/history.git/commit/fs/inode.c?id=c37fa164f793735b32aa3f53154ff1a7659e6442 [1]
        Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- arch/powerpc/include/asm/processor.h
+++ arch/powerpc/include/asm/processor.h
@@ -365,7 +365,6 @@ int validate_sp(unsigned long sp, struct task_struct *p,
  */
 #define ARCH_HAS_PREFETCH
 #define ARCH_HAS_PREFETCHW
-#define ARCH_HAS_SPINLOCK_PREFETCH
 
 static inline void prefetch(const void *x)
 {
@@ -382,8 +381,6 @@ static inline void prefetchw(const void *x)
 
 	__asm__ __volatile__ ("dcbtst 0,%0" : : "r" (x));
 }
-
-#define spin_lock_prefetch(x)	prefetchw(x)
 
 /* asm stubs */
 extern unsigned long isa300_idle_stop_noloss(unsigned long psscr_val);
