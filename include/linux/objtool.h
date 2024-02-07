666775ce91b7fc884d2ca109a4abba922efe9ea9
Prarit Bhargava <prarit@redhat.com>
2024-02-07 11:40:30 -0500

    x86/speculation, objtool: Use absolute relocations for annotations
    JIRA: https://issues.redhat.com/browse/RHEL-25415
    
    commit b8ec60e1186cdcfce41e7db4c827cb107e459002
    Author: Fangrui Song <maskray@google.com>
    Date:   Tue Sep 19 17:17:28 2023 -0700
    
        x86/speculation, objtool: Use absolute relocations for annotations
    
        .discard.retpoline_safe sections do not have the SHF_ALLOC flag.  These
        sections referencing text sections' STT_SECTION symbols with PC-relative
        relocations like R_386_PC32 [0] is conceptually not suitable.  Newer
        LLD will report warnings for REL relocations even for relocatable links [1]:
    
            ld.lld: warning: vmlinux.a(drivers/i2c/busses/i2c-i801.o):(.discard.retpoline_safe+0x120): has non-ABS relocation R_386_PC32 against symbol ''
    
        Switch to absolute relocations instead, which indicate link-time
        addresses.  In a relocatable link, these addresses are also output
        section offsets, used by checks in tools/objtool/check.c.  When linking
        vmlinux, these .discard.* sections will be discarded, therefore it is
        not a problem that R_X86_64_32 cannot represent a kernel address.
    
        Alternatively, we could set the SHF_ALLOC flag for .discard.* sections,
        but I think non-SHF_ALLOC for sections to be discarded makes more sense.
    
        Note: if we decide to never support REL architectures (e.g. arm, i386),
        we can utilize R_*_NONE relocations (.reloc ., BFD_RELOC_NONE, sym),
        making .discard.* sections zero-sized.  That said, the section content
        waste is 4 bytes per entry, much smaller than sizeof(Elf{32,64}_Rel).
    
          [0] commit 1c0c1faf5692 ("objtool: Use relative pointers for annotations")
          [1] https://github.com/ClangBuiltLinux/linux/issues/1937
    
        Signed-off-by: Fangrui Song <maskray@google.com>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
        Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Josh Poimboeuf <jpoimboe@redhat.com>
        Link: https://lore.kernel.org/r/20230920001728.1439947-1-maskray@google.com
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- include/linux/objtool.h
+++ include/linux/objtool.h
@@ -49,13 +49,13 @@
 #define ANNOTATE_NOENDBR					\
 	"986: \n\t"						\
 	".pushsection .discard.noendbr\n\t"			\
-	".long 986b - .\n\t"					\
+	".long 986b\n\t"					\
 	".popsection\n\t"
 
 #define ASM_REACHABLE							\
 	"998:\n\t"							\
 	".pushsection .discard.reachable\n\t"				\
-	".long 998b - .\n\t"						\
+	".long 998b\n\t"						\
 	".popsection\n\t"
 
 #else /* __ASSEMBLY__ */
@@ -67,7 +67,7 @@
 #define ANNOTATE_INTRA_FUNCTION_CALL				\
 	999:							\
 	.pushsection .discard.intra_function_calls;		\
-	.long 999b - .;						\
+	.long 999b;						\
 	.popsection;
 
 /*
@@ -120,7 +120,7 @@
 .macro ANNOTATE_NOENDBR
 .Lhere_\@:
 	.pushsection .discard.noendbr
-	.long	.Lhere_\@ - .
+	.long	.Lhere_\@
 	.popsection
 .endm
 
@@ -144,7 +144,7 @@
 .macro REACHABLE
 .Lhere_\@:
 	.pushsection .discard.reachable
-	.long	.Lhere_\@ - .
+	.long	.Lhere_\@
 	.popsection
 .endm
 
