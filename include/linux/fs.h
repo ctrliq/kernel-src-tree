f332056770e7819a6ff067924e09ec8681c94811
Jeff Moyer <jmoyer@redhat.com>
2024-02-15 12:47:38 -0800

    fs/aio: Restrict kiocb_set_cancel_fn() to I/O submitted via libaio
    JIRA: https://issues.redhat.com/browse/RHEL-105612
    Conflicts: RHEL does not have commit 164f4064ca81e ("keep iocb_flags()
    result cached in struct file"), so keep the call to iocb_flags().
    
    commit b820de741ae48ccf50dd95e297889c286ff4f760
    Author: Bart Van Assche <bvanassche@acm.org>
    Date:   Thu Feb 15 12:47:38 2024 -0800
    
        fs/aio: Restrict kiocb_set_cancel_fn() to I/O submitted via libaio
        
        If kiocb_set_cancel_fn() is called for I/O submitted via io_uring, the
        following kernel warning appears:
        
        WARNING: CPU: 3 PID: 368 at fs/aio.c:598 kiocb_set_cancel_fn+0x9c/0xa8
        Call trace:
         kiocb_set_cancel_fn+0x9c/0xa8
         ffs_epfile_read_iter+0x144/0x1d0
         io_read+0x19c/0x498
         io_issue_sqe+0x118/0x27c
         io_submit_sqes+0x25c/0x5fc
         __arm64_sys_io_uring_enter+0x104/0xab0
         invoke_syscall+0x58/0x11c
         el0_svc_common+0xb4/0xf4
         do_el0_svc+0x2c/0xb0
         el0_svc+0x2c/0xa4
         el0t_64_sync_handler+0x68/0xb4
         el0t_64_sync+0x1a4/0x1a8
        
        Fix this by setting the IOCB_AIO_RW flag for read and write I/O that is
        submitted by libaio.
        
        Suggested-by: Jens Axboe <axboe@kernel.dk>
        Cc: Christoph Hellwig <hch@lst.de>
        Cc: Avi Kivity <avi@scylladb.com>
        Cc: Sandeep Dhavale <dhavale@google.com>
        Cc: Jens Axboe <axboe@kernel.dk>
        Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Cc: Kent Overstreet <kent.overstreet@linux.dev>
        Cc: stable@vger.kernel.org
        Signed-off-by: Bart Van Assche <bvanassche@acm.org>
        Link: https://lore.kernel.org/r/20240215204739.2677806-2-bvanassche@acm.org
        Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -332,6 +332,8 @@ struct readahead_control;
  * unrelated IO (like cache flushing, new IO generation, etc).
  */
 #define IOCB_DIO_CALLER_COMP	(1 << 22)
+/* kiocb is a read or write operation submitted by fs/aio.c. */
+#define IOCB_AIO_RW		(1 << 23)
 #define IOCB_HAS_METADATA	(1 << 24)
 
 /* for use in trace events */
