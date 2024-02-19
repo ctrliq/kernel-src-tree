e50e0c0630ee4ba051d99eeb8b8ab48455635f80
David Arcari <darcari@redhat.com>
2024-02-19 17:03:27 -0500

    pinctrl: Replace kernel.h by what is actually being used
    JIRA: https://issues.redhat.com/browse/RHEL-26354
    
    commit 82cc14c9930c7613da2fcb41a8d4f90c8b4cb048
    Author: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Date:   Tue Oct 3 15:10:11 2023 +0300
    
        pinctrl: Replace kernel.h by what is actually being used
    
        The kernel.h is a mess of unrelated things and we only used it
        as a proxy to array_size.h, hence switch from former to the latter.
    
        While at it, group and sort the headers where it makes sense.
    
        Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- drivers/pinctrl/pinctrl-utils.c
+++ drivers/pinctrl/pinctrl-utils.c
@@ -19,12 +19,14 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307, USA
  */
+#include <linux/array_size.h>
 #include <linux/device.h>
 #include <linux/export.h>
-#include <linux/kernel.h>
-#include <linux/pinctrl/pinctrl.h>
 #include <linux/of.h>
 #include <linux/slab.h>
+
+#include <linux/pinctrl/pinctrl.h>
+
 #include "core.h"
 #include "pinctrl-utils.h"
 
