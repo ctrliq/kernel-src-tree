008506f2a0d76c1831bc5550b3d1831d8f81f595
Ming Lei <ming.lei@redhat.com>
2024-02-20 10:32:48 +0100

    null_blk: pass queue_limits to blk_mq_alloc_disk
    JIRA: https://issues.redhat.com/browse/RHEL-56837
    Conflicts: context difference because we pulled in
    	"null_blk: fix null-ptr-dereference while configuring 'power' and 'submit_queues'"
    
    commit e440626b1caf3767eda2d78610dfdc0ae7fd5238
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Tue Feb 20 10:32:48 2024 +0100
    
        null_blk: pass queue_limits to blk_mq_alloc_disk
    
        Pass the queue limits directly to blk_mq_alloc_disk instead of
        setting them one at a time.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Damien Le Moal <dlemoal@kernel.org>
        Reviewed-by: Hannes Reinecke <hare@suse.de>
        Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
        Tested-by: Damien Le Moal <dlemoal@kernel.org>
        Link: https://lore.kernel.org/r/20240220093248.3290292-6-hch@lst.de
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/block/null_blk/main.c
+++ drivers/block/null_blk/main.c
@@ -1710,7 +1710,7 @@ static void null_del_dev(struct nullb *nullb)
 	dev->nullb = NULL;
 }
 
-static void null_config_discard(struct nullb *nullb)
+static void null_config_discard(struct nullb *nullb, struct queue_limits *lim)
 {
 	if (nullb->dev->discard == false)
 		return;
@@ -1727,7 +1727,7 @@ static void null_config_discard(struct nullb *nullb)
 		return;
 	}
 
-	blk_queue_max_discard_sectors(nullb->q, UINT_MAX >> 9);
+	lim->max_hw_discard_sectors = UINT_MAX >> 9;
 }
 
 static const struct block_device_operations null_ops = {
@@ -1886,6 +1886,12 @@ static bool null_setup_fault(void)
 
 static int null_add_dev(struct nullb_device *dev)
 {
+	struct queue_limits lim = {
+		.logical_block_size	= dev->blocksize,
+		.physical_block_size	= dev->blocksize,
+		.max_hw_sectors		= dev->max_sectors,
+	};
+
 	struct nullb *nullb;
 	int rv;
 
@@ -1911,10 +1917,19 @@ static int null_add_dev(struct nullb_device *dev)
 	if (rv)
 		goto out_cleanup_queues;
 
-	nullb->disk = blk_mq_alloc_disk(nullb->tag_set, NULL, nullb);
+	if (dev->virt_boundary)
+		lim.virt_boundary_mask = PAGE_SIZE - 1;
+	null_config_discard(nullb, &lim);
+	if (dev->zoned) {
+		rv = null_init_zoned_dev(dev, &lim);
+		if (rv)
+			goto out_cleanup_tags;
+	}
+
+	nullb->disk = blk_mq_alloc_disk(nullb->tag_set, &lim, nullb);
 	if (IS_ERR(nullb->disk)) {
 		rv = PTR_ERR(nullb->disk);
-		goto out_cleanup_tags;
+		goto out_cleanup_zone;
 	}
 	nullb->q = nullb->disk->queue;
 
@@ -1928,30 +1943,14 @@ static int null_add_dev(struct nullb_device *dev)
 		blk_queue_write_cache(nullb->q, true, true);
 	}
 
-	if (dev->zoned) {
-		rv = null_init_zoned_dev(dev, nullb->q);
-		if (rv)
-			goto out_cleanup_disk;
-	}
-
 	nullb->q->queuedata = nullb;
 	blk_queue_flag_set(QUEUE_FLAG_NONROT, nullb->q);
 
 	rv = ida_alloc(&nullb_indexes, GFP_KERNEL);
 	if (rv < 0)
-		goto out_cleanup_zone;
+		goto out_cleanup_disk;
 	nullb->index = rv;
 	dev->index = rv;
-
-	blk_queue_logical_block_size(nullb->q, dev->blocksize);
-	blk_queue_physical_block_size(nullb->q, dev->blocksize);
-	if (dev->max_sectors)
-		blk_queue_max_hw_sectors(nullb->q, dev->max_sectors);
-
-	if (dev->virt_boundary)
-		blk_queue_virt_boundary(nullb->q, PAGE_SIZE - 1);
-
-	null_config_discard(nullb);
 
 	if (config_item_name(&dev->group.cg_item)) {
 		/* Use configfs dir name as the device name */
