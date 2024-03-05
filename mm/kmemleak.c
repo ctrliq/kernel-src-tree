6e5d9b97ef5b99e5f71bb3e4f84f84c2f8185b09
Waiman Long <longman@redhat.com>
2024-03-05 12:04:09 -0500

    mm: use stack_depot_early_init for kmemleak
    JIRA: https://issues.redhat.com/browse/RHEL-28060
    
    commit 993f57e0277b78182eb9e4955d118cc8b086c83d
    Author: Zhaoyang Huang <zhaoyang.huang@unisoc.com>
    Date:   Thu, 19 Jan 2023 09:22:25 +0800
    
        mm: use stack_depot_early_init for kmemleak
    
        Mirsad report the below error which is caused by stack_depot_init()
        failure in kvcalloc.  Solve this by having stackdepot use
        stack_depot_early_init().
    
        On 1/4/23 17:08, Mirsad Goran Todorovac wrote:
        I hate to bring bad news again, but there seems to be a problem with the output of /sys/kernel/debug/kmemleak:
    
        [root@pc-mtodorov ~]# cat /sys/kernel/debug/kmemleak
        unreferenced object 0xffff951c118568b0 (size 16):
        comm "kworker/u12:2", pid 56, jiffies 4294893952 (age 4356.548s)
        hex dump (first 16 bytes):
            6d 65 6d 73 74 69 63 6b 30 00 00 00 00 00 00 00 memstick0.......
            backtrace:
        [root@pc-mtodorov ~]#
    
        Apparently, backtrace of called functions on the stack is no longer
        printed with the list of memory leaks.  This appeared on Lenovo desktop
        10TX000VCR, with AlmaLinux 8.7 and BIOS version M22KT49A (11/10/2022) and
        6.2-rc1 and 6.2-rc2 builds.  This worked on 6.1 with the same
        CONFIG_KMEMLEAK=y and MGLRU enabled on a vanilla mainstream kernel from
        Mr.  Torvalds' tree.  I don't know if this is deliberate feature for some
        reason or a bug.  Please find attached the config, lshw and kmemleak
        output.
    
        [vbabka@suse.cz: remove stack_depot_init() call]
        Link: https://lore.kernel.org/all/5272a819-ef74-65ff-be61-4d2d567337de@alu.unizg.hr/
        Link: https://lkml.kernel.org/r/1674091345-14799-2-git-send-email-zhaoyang.huang@unisoc.com
        Fixes: 56a61617dd22 ("mm: use stack_depot for recording kmemleak's backtrace")
        Reported-by: Mirsad Todorovac <mirsad.todorovac@alu.unizg.hr>
        Suggested-by: Vlastimil Babka <vbabka@suse.cz>
        Signed-off-by: Zhaoyang Huang <zhaoyang.huang@unisoc.com>
        Acked-by: Mike Rapoport (IBM) <rppt@kernel.org>
        Acked-by: Catalin Marinas <catalin.marinas@arm.com>
        Acked-by: Vlastimil Babka <vbabka@suse.cz>
        Tested-by: Borislav Petkov (AMD) <bp@alien8.de>
        Cc: ke.wang <ke.wang@unisoc.com>
        Cc: Nathan Chancellor <nathan@kernel.org>
        Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -2069,8 +2069,10 @@ static int __init kmemleak_boot_config(char *str)
 		return -EINVAL;
 	if (strcmp(str, "off") == 0)
 		kmemleak_disable();
-	else if (strcmp(str, "on") == 0)
+	else if (strcmp(str, "on") == 0) {
 		kmemleak_skip_disable = 1;
+		stack_depot_want_early_init();
+	}
 	else
 		return -EINVAL;
 	return 0;
@@ -2092,7 +2094,6 @@ void __init kmemleak_init(void)
 	if (kmemleak_error)
 		return;
 
-	stack_depot_init();
 	jiffies_min_age = msecs_to_jiffies(MSECS_MIN_AGE);
 	jiffies_scan_wait = msecs_to_jiffies(SECS_SCAN_WAIT * 1000);
 
