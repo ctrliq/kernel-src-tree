004f9c4e058336b04bca1700e6b2e98711d9b658
Audra Mitchell <audra@redhat.com>
2024-03-05 20:07:53 -0500

    swap: add a limit for readahead page-cluster value
    JIRA: https://issues.redhat.com/browse/RHEL-27739
    
    This patch is a backport of the following upstream commit:
    commit ea0ffd0c08d0fef1f6e93eb07badbeeabf6b43d6
    Author: Kairui Song <kasong@tencent.com>
    Date:   Mon Oct 24 00:25:33 2022 +0800
    
        swap: add a limit for readahead page-cluster value
    
        Currenty there is no upper limit for /proc/sys/vm/page-cluster, and it's a
        bit shift value, so it could result in overflow of the 32-bit integer.
        Add a reasonable upper limit for it, read-in at most 2**31 pages, which is
        a large enough value for readahead.
    
        Link: https://lkml.kernel.org/r/20221023162533.81561-1-ryncsn@gmail.com
        Signed-off-by: Kairui Song <kasong@tencent.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Audra Mitchell <audra@redhat.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2207,6 +2207,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
+		.extra2		= (void *)&page_cluster_max,
 	},
 	{
 		.procname	= "dirtytime_expire_seconds",
