c54c519daa444530d98a010f11977b70d128e6d3
Mark Langsdorf <mlangsdo@redhat.com>
2024-03-08 13:00:56 -0500

    ACPICA: actbl1: Replace 1-element arrays with flexible arrays
    JIRA: https://issues.redhat.com/browse/RHEL-26871
    
    commit 48ff467c338b184bc309ea2e0d2a9524f569d245
    Author: Kees Cook <keescook@chromium.org>
    Date:   Wed Apr 5 15:48:56 2023 +0200
    
    ACPICA commit 8c9bd5d151f77767b2fd937911848b7159dc8ee9
    
    Similar to "Replace one-element array with flexible-array", replace the
    1-element array with a proper flexible array member as defined by C99.
    
    This allows the code to operate without tripping compile-time and run-
    time bounds checkers (e.g. via __builtin_object_size(), -fsanitize=bounds,
    and/or -fstrict-flex-arrays=3).
    
    No .text nor .data differences result from this change.
    
    Link: https://github.com/acpica/acpica/commit/8c9bd5d1
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- include/acpi/actbl1.h
+++ include/acpi/actbl1.h
@@ -898,7 +898,7 @@ struct acpi_table_drtm {
 
 struct acpi_drtm_vtable_list {
 	u32 validated_table_count;
-	u64 validated_tables[1];
+	u64 validated_tables[];
 };
 
 /* 2) Resources List (of Resource Descriptors) */
@@ -913,7 +913,7 @@ struct acpi_drtm_resource {
 
 struct acpi_drtm_resource_list {
 	u32 resource_count;
-	struct acpi_drtm_resource resources[1];
+	struct acpi_drtm_resource resources[];
 };
 
 /* 3) Platform-specific Identifiers List */
@@ -936,7 +936,7 @@ struct acpi_table_ecdt {
 	struct acpi_generic_address data;	/* Address of EC data register */
 	u32 uid;		/* Unique ID - must be same as the EC _UID method */
 	u8 gpe;			/* The GPE for the EC */
-	u8 id[1];		/* Full namepath of the EC in the ACPI namespace */
+	u8 id[];		/* Full namepath of the EC in the ACPI namespace */
 };
 
 /*******************************************************************************
