0b80535f8ecbd4b6cb85ede0be540a23bad76b59
Dick Kennedy <dkennedy@redhat.com>
2024-03-20 18:50:23 +0000

    scsi: lpfc: Fix reftag generation sizing errors
    JIRA: https://issues.redhat.com/browse/RHEL-24508
    
    commit 68a6a66c5168f3995baed3fc5bee2d4515eb16d0
    Author: James Smart <jsmart2021@gmail.com>
    Date:   Mon Mar 1 09:18:02 2021 -0800
    
        scsi: lpfc: Fix reftag generation sizing errors
    
        An LBA is 8 bytes. The driver generates a reftag from the LBA but the
        reftag is 4 bytes. Thus scsi_get_lba() could return a value that exceeds
        our reftag size.
    
        Fix by converting all the code to calling the common routine
        t10_pi_ref_tag() which returns a u32, thus ensuring a consistent 4byte
        value.  Also correct a few code lines that access LBA directly and ensure
        64-bit data types are used.
    
        Link: https://lore.kernel.org/r/20210301171821.3427-4-jsmart2021@gmail.com
        Co-developed-by: Dick Kennedy <dick.kennedy@broadcom.com>
        Signed-off-by: Dick Kennedy <dick.kennedy@broadcom.com>
        Signed-off-by: James Smart <jsmart2021@gmail.com>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Dick Kennedy <dkennedy@redhat.com>

--- drivers/scsi/lpfc/lpfc_scsi.c
+++ drivers/scsi/lpfc/lpfc_scsi.c
@@ -133,6 +133,8 @@ lpfc_sli4_set_rsp_sgl_last(struct lpfc_hba *phba,
 	}
 }
 
+#define LPFC_INVALID_REFTAG ((u32)-1)
+
 /**
  * lpfc_rampdown_queue_depth - Post RAMP_DOWN_QUEUE event to worker thread
  * @phba: The Hba for which this call is being executed.
@@ -1000,6 +1002,8 @@ lpfc_bg_err_inject(struct lpfc_hba *phba, struct scsi_cmnd *sc,
 
 	sgpe = scsi_prot_sglist(sc);
 	lba = t10_pi_ref_tag(sc->request);
+	if (lba == LPFC_INVALID_REFTAG)
+		return 0;
 
 	/* First check if we need to match the LBA */
 	if (phba->lpfc_injerr_lba != LPFC_INJERR_LBA_OFF) {
@@ -1581,6 +1585,8 @@ lpfc_bg_setup_bpl(struct lpfc_hba *phba, struct scsi_cmnd *sc,
 
 	/* extract some info from the scsi command for pde*/
 	reftag = t10_pi_ref_tag(sc->request);
+	if (reftag == LPFC_INVALID_REFTAG)
+		goto out;
 
 #ifdef CONFIG_SCSI_LPFC_DEBUG_FS
 	rc = lpfc_bg_err_inject(phba, sc, &reftag, NULL, 1);
@@ -1742,6 +1748,8 @@ lpfc_bg_setup_bpl_prot(struct lpfc_hba *phba, struct scsi_cmnd *sc,
 	/* extract some info from the scsi command */
 	blksize = lpfc_cmd_blksize(sc);
 	reftag = t10_pi_ref_tag(sc->request);
+	if (reftag == LPFC_INVALID_REFTAG)
+		goto out;
 
 #ifdef CONFIG_SCSI_LPFC_DEBUG_FS
 	rc = lpfc_bg_err_inject(phba, sc, &reftag, NULL, 1);
@@ -1970,6 +1978,8 @@ lpfc_bg_setup_sgl(struct lpfc_hba *phba, struct scsi_cmnd *sc,
 
 	/* extract some info from the scsi command for pde*/
 	reftag = t10_pi_ref_tag(sc->request);
+	if (reftag == LPFC_INVALID_REFTAG)
+		goto out;
 
 #ifdef CONFIG_SCSI_LPFC_DEBUG_FS
 	rc = lpfc_bg_err_inject(phba, sc, &reftag, NULL, 1);
@@ -2169,6 +2179,8 @@ lpfc_bg_setup_sgl_prot(struct lpfc_hba *phba, struct scsi_cmnd *sc,
 	/* extract some info from the scsi command */
 	blksize = lpfc_cmd_blksize(sc);
 	reftag = t10_pi_ref_tag(sc->request);
+	if (reftag == LPFC_INVALID_REFTAG)
+		goto out;
 
 #ifdef CONFIG_SCSI_LPFC_DEBUG_FS
 	rc = lpfc_bg_err_inject(phba, sc, &reftag, NULL, 1);
@@ -2760,6 +2772,8 @@ lpfc_calc_bg_err(struct lpfc_hba *phba, struct lpfc_io_buf *lpfc_cmd)
 
 		src = (struct scsi_dif_tuple *)sg_virt(sgpe);
 		start_ref_tag = t10_pi_ref_tag(cmd->request);
+		if (start_ref_tag == LPFC_INVALID_REFTAG)
+			goto out;
 		start_app_tag = src->app_tag;
 		len = sgpe->length;
 		while (src && protsegcnt) {
