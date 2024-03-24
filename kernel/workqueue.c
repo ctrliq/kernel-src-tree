09bde2a4d31ec5cc4c83bb1c9c5ae398a897692e
Waiman Long <longman@redhat.com>
2024-03-24 19:58:44 -0400

    workqueue: Move wq_pod_init() below workqueue_init()
    JIRA: https://issues.redhat.com/browse/RHEL-25103
    Conflicts: A context diff due to the presence of a later upstream commit
    	   4a6c5607d450 ("workqueue: Make sure that wq_unbound_cpumask
    	   is never empty").
    
    commit a86feae6195ac2148097b063f7fdad8ee1f6dad4
    Author: Tejun Heo <tj@kernel.org>
    Date:   Mon, 7 Aug 2023 15:57:24 -1000
    
        workqueue: Move wq_pod_init() below workqueue_init()
    
        wq_pod_init() is called from workqueue_init() and responsible for
        initializing unbound CPU pods according to NUMA node. Workqueue is in the
        process of improving affinity awareness and wants to use other topology
        information to initialize unbound CPU pods; however, unlike NUMA nodes,
        other topology information isn't yet available in workqueue_init().
    
        The next patch will introduce a later stage init function for workqueue
        which will be responsible for initializing unbound CPU pods. Relocate
        wq_pod_init() below workqueue_init() where the new init function is going to
        be located so that the diff can show the content differences.
    
        Just a relocation. No functional changes.
    
        Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -6352,44 +6352,7 @@ static void __init restrict_unbound_cpumask(const char *name, const struct cpuma
 	cpumask_and(wq_unbound_cpumask, wq_unbound_cpumask, mask);
 }
 
-static void __init wq_pod_init(void)
-{
-	cpumask_var_t *tbl;
-	int node, cpu;
-
-	if (num_possible_nodes() <= 1)
-		return;
-
-	for_each_possible_cpu(cpu) {
-		if (WARN_ON(cpu_to_node(cpu) == NUMA_NO_NODE)) {
-			pr_warn("workqueue: NUMA node mapping not available for cpu%d, disabling NUMA support\n", cpu);
-			return;
-		}
-	}
-
-	wq_update_pod_attrs_buf = alloc_workqueue_attrs();
-	BUG_ON(!wq_update_pod_attrs_buf);
-
-	/*
-	 * We want masks of possible CPUs of each node which isn't readily
-	 * available.  Build one from cpu_to_node() which should have been
-	 * fully initialized by now.
-	 */
-	tbl = kcalloc(nr_node_ids, sizeof(tbl[0]), GFP_KERNEL);
-	BUG_ON(!tbl);
-
-	for_each_node(node)
-		BUG_ON(!zalloc_cpumask_var_node(&tbl[node], GFP_KERNEL,
-				node_online(node) ? node : NUMA_NO_NODE));
-
-	for_each_possible_cpu(cpu) {
-		node = cpu_to_node(cpu);
-		cpumask_set_cpu(cpu, tbl[node]);
-	}
-
-	wq_pod_cpus = tbl;
-	wq_pod_enabled = true;
-}
+static void wq_pod_init(void);
 
 /**
  * workqueue_init_early - early init for workqueue subsystem
@@ -6571,6 +6534,45 @@ void __init workqueue_init(void)
 
 	wq_online = true;
 	wq_watchdog_init();
+}
+
+static void __init wq_pod_init(void)
+{
+	cpumask_var_t *tbl;
+	int node, cpu;
+
+	if (num_possible_nodes() <= 1)
+		return;
+
+	for_each_possible_cpu(cpu) {
+		if (WARN_ON(cpu_to_node(cpu) == NUMA_NO_NODE)) {
+			pr_warn("workqueue: NUMA node mapping not available for cpu%d, disabling NUMA support\n", cpu);
+			return;
+		}
+	}
+
+	wq_update_pod_attrs_buf = alloc_workqueue_attrs();
+	BUG_ON(!wq_update_pod_attrs_buf);
+
+	/*
+	 * We want masks of possible CPUs of each node which isn't readily
+	 * available.  Build one from cpu_to_node() which should have been
+	 * fully initialized by now.
+	 */
+	tbl = kcalloc(nr_node_ids, sizeof(tbl[0]), GFP_KERNEL);
+	BUG_ON(!tbl);
+
+	for_each_node(node)
+		BUG_ON(!zalloc_cpumask_var_node(&tbl[node], GFP_KERNEL,
+				node_online(node) ? node : NUMA_NO_NODE));
+
+	for_each_possible_cpu(cpu) {
+		node = cpu_to_node(cpu);
+		cpumask_set_cpu(cpu, tbl[node]);
+	}
+
+	wq_pod_cpus = tbl;
+	wq_pod_enabled = true;
 }
 
 void __warn_flushing_systemwide_wq(void)
