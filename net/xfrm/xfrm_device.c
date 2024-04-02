30f23c21f7d1bcdfa4c54e3172222263ca7db95b
Michal Schmidt <mschmidt@redhat.com>
2024-04-02 11:47:32 +0200

    xfrm: Support UDP encapsulation in packet offload mode
    JIRA: https://issues.redhat.com/browse/RHEL-30141
    
    Conflicts:
    	net/xfrm/xfrm_device.c
    	- Context: RHEL has an additional tech-preview mark.
    
    commit 89edf40220be8e68922beb54a06fdfc66f743c39
    Author: Leon Romanovsky <leonro@nvidia.com>
    Date:   Wed Jul 19 12:26:56 2023 +0300
    
        xfrm: Support UDP encapsulation in packet offload mode
    
        Since mlx5 supports UDP encapsulation in packet offload, change the XFRM
        core to allow users to configure it.
    
        Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
        Acked-by: Steffen Klassert <steffen.klassert@secunet.com>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- net/xfrm/xfrm_device.c
+++ net/xfrm/xfrm_device.c
@@ -257,12 +257,6 @@ int xfrm_dev_state_add(struct net *net, struct xfrm_state *x,
 		return -EINVAL;
 	}
 
-	/* We don't yet support UDP encapsulation and TFC padding. */
-	if (x->encap || x->tfcpad) {
-		NL_SET_ERR_MSG(extack, "Encapsulation and TFC padding can't be offloaded");
-		return -EINVAL;
-	}
-
 	if (xuo->flags &
 	    ~(XFRM_OFFLOAD_IPV6 | XFRM_OFFLOAD_INBOUND | XFRM_OFFLOAD_PACKET)) {
 		NL_SET_ERR_MSG(extack, "Unrecognized flags in offload request");
@@ -272,6 +266,12 @@ int xfrm_dev_state_add(struct net *net, struct xfrm_state *x,
 	is_packet_offload = xuo->flags & XFRM_OFFLOAD_PACKET;
 	if (is_packet_offload)
 		xfrm_packet_offload_tech_preview();
+
+	/* We don't yet support UDP encapsulation and TFC padding. */
+	if ((!is_packet_offload && x->encap) || x->tfcpad) {
+		NL_SET_ERR_MSG(extack, "Encapsulation and TFC padding can't be offloaded");
+		return -EINVAL;
+	}
 
 	dev = dev_get_by_index(net, xuo->ifindex);
 	if (!dev) {
