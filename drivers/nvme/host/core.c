0d400e99ef0a297e2bd35986daffacd1c073e8ee
Ming Lei <ming.lei@redhat.com>
2024-04-02 16:47:54 +0200

    nvme: split nvme_update_zone_info
    JIRA: https://issues.redhat.com/browse/RHEL-56837
    
    commit c85c9ab926a592e2f59f7d9a6ca7d6562843d8fa
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Tue Apr 2 16:47:54 2024 +0200
    
        nvme: split nvme_update_zone_info
    
        nvme_update_zone_info does (admin queue) I/O to the device and can fail.
        We fail to abort the queue limits update if that happen, but really
        should avoid with the frozen I/O queue as much as possible anyway.
    
        Split the logic into a helper to query the information that can be
        called on an unfrozen queue and one to apply it to the queue limits.
    
        Fixes: 9b130d681443 ("nvme: use the atomic queue limits update API")
        Reported-by: Kanchan Joshi <joshi.k@samsung.com>
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Kanchan Joshi <joshi.k@samsung.com>
        Signed-off-by: Keith Busch <kbusch@kernel.org>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -2074,6 +2074,7 @@ static int nvme_update_ns_info_block(struct nvme_ns *ns,
 	bool vwc = ns->ctrl->vwc & NVME_CTRL_VWC_PRESENT;
 	struct queue_limits lim;
 	struct nvme_id_ns_nvm *nvm = NULL;
+	struct nvme_zone_info zi = {};
 	struct nvme_id_ns *id;
 	sector_t capacity;
 	unsigned lbaf;
@@ -2089,6 +2090,7 @@ static int nvme_update_ns_info_block(struct nvme_ns *ns,
 		ret = -ENODEV;
 		goto out;
 	}
+	lbaf = nvme_lbaf_index(id->flbas);
 
 	if (ns->ctrl->ctratt & NVME_CTRL_ATTR_ELBAS) {
 		ret = nvme_identify_ns_nvm(ns->ctrl, info->nsid, &nvm);
@@ -2096,8 +2098,14 @@ static int nvme_update_ns_info_block(struct nvme_ns *ns,
 			goto out;
 	}
 
+	if (IS_ENABLED(CONFIG_BLK_DEV_ZONED) &&
+	    ns->head->ids.csi == NVME_CSI_ZNS) {
+		ret = nvme_query_zone_info(ns, lbaf, &zi);
+		if (ret < 0)
+			goto out;
+	}
+
 	blk_mq_freeze_queue(ns->disk->queue);
-	lbaf = nvme_lbaf_index(id->flbas);
 	ns->head->lba_shift = id->lbaf[lbaf].ds;
 	ns->head->nuse = le64_to_cpu(id->nuse);
 	capacity = nvme_lba_to_sect(ns->head, le64_to_cpu(id->nsze));
@@ -2110,13 +2118,8 @@ static int nvme_update_ns_info_block(struct nvme_ns *ns,
 		capacity = 0;
 	nvme_config_discard(ns, &lim);
 	if (IS_ENABLED(CONFIG_BLK_DEV_ZONED) &&
-	    ns->head->ids.csi == NVME_CSI_ZNS) {
-		ret = nvme_update_zone_info(ns, lbaf, &lim);
-		if (ret) {
-			blk_mq_unfreeze_queue(ns->disk->queue);
-			goto out;
-		}
-	}
+	    ns->head->ids.csi == NVME_CSI_ZNS)
+		nvme_update_zone_info(ns, &lim, &zi);
 	ret = queue_limits_commit_update(ns->disk->queue, &lim);
 	if (ret) {
 		blk_mq_unfreeze_queue(ns->disk->queue);
