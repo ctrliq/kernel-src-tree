6d47aa9b303e436f32e4b76d7a43c460af03151a
Paulo Alcantara <paalcant@redhat.com>
2024-04-03 14:24:06 -0300

    smb: client: reduce stack usage in smb2_set_ea()
    JIRA: https://issues.redhat.com/browse/RHEL-31245
    
    commit f4e5ceb6c1a64f9f8c666877164d95d5eb46da5b
    Author: Paulo Alcantara <pc@manguebit.com>
    Date:   Thu Aug 17 12:34:13 2023 -0300
    
        smb: client: reduce stack usage in smb2_set_ea()
        
        Clang warns about exceeded stack frame size
        
          fs/smb/client/smb2ops.c:1080:1: warning: stack frame size (1432)
          exceeds limit (1024) in 'smb2_set_ea' [-Wframe-larger-than]
        
        Fix this by allocating a structure that will hold most of the large
        variables.
        
        Signed-off-by: Paulo Alcantara (SUSE) <pc@manguebit.com>
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/cifsglob.h
+++ fs/smb/client/cifsglob.h
@@ -2214,4 +2214,17 @@ static inline struct scatterlist *cifs_sg_set_buf(struct scatterlist *sg,
 	return sg;
 }
 
+struct smb2_compound_vars {
+	struct cifs_open_parms oparms;
+	struct kvec rsp_iov[3];
+	struct smb_rqst rqst[3];
+	struct kvec open_iov[SMB2_CREATE_IOV_SIZE];
+	struct kvec qi_iov;
+	struct kvec io_iov[SMB2_IOCTL_IOV_SIZE];
+	struct kvec si_iov[SMB2_SET_INFO_IOV_SIZE];
+	struct kvec close_iov;
+	struct smb2_file_rename_info rename_info;
+	struct smb2_file_link_info link_info;
+};
+
 #endif	/* _CIFS_GLOB_H */
