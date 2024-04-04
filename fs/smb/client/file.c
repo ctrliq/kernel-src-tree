8966bc53a091a1dff81d31c7758795ae026571fe
Paulo Alcantara <paalcant@redhat.com>
2024-04-03 22:14:48 -0300

    smb: client: do not defer close open handles to deleted files
    JIRA: https://issues.redhat.com/browse/RHEL-31245
    
    commit ffceb7640cbfe6ea60e7769e107451d63a2fe3d3
    Author: Meetakshi Setiya <msetiya@microsoft.com>
    Date:   Tue Mar 5 23:07:52 2024 -0600
    
        smb: client: do not defer close open handles to deleted files
        
        When a file/dentry has been deleted before closing all its open
        handles, currently, closing them can add them to the deferred
        close list. This can lead to problems in creating file with the
        same name when the file is re-created before the deferred close
        completes. This issue was seen while reusing a client's already
        existing lease on a file for compound operations and xfstest 591
        failed because of the deferred close handle that remained valid
        even after the file was deleted and was being reused to create a
        file with the same name. The server in this case returns an error
        on open with STATUS_DELETE_PENDING. Recreating the file would
        fail till the deferred handles are closed (duration specified in
        closetimeo).
        
        This patch fixes the issue by flagging all open handles for the
        deleted file (file path to be precise) by setting
        status_file_deleted to true in the cifsFileInfo structure. As per
        the information classes specified in MS-FSCC, SMB2 query info
        response from the server has a DeletePending field, set to true
        to indicate that deletion has been requested on that file. If
        this is the case, flag the open handles for this file too.
        
        When doing close in cifs_close for each of these handles, check the
        value of this boolean field and do not defer close these handles
        if the corresponding filepath has been deleted.
        
        Signed-off-by: Meetakshi Setiya <msetiya@microsoft.com>
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/file.c
+++ fs/smb/client/file.c
@@ -360,6 +360,7 @@ struct cifsFileInfo *cifs_new_fileinfo(struct cifs_fid *fid, struct file *file,
 	cfile->uid = current_fsuid();
 	cfile->dentry = dget(dentry);
 	cfile->f_flags = file->f_flags;
+	cfile->status_file_deleted = false;
 	cfile->invalidHandle = false;
 	cfile->deferred_close_scheduled = false;
 	cfile->tlink = cifs_get_tlink(tlink);
@@ -962,7 +963,7 @@ int cifs_close(struct inode *inode, struct file *file)
 		if ((cifs_sb->ctx->closetimeo && cinode->oplock == CIFS_CACHE_RHW_FLG)
 		    && cinode->lease_granted &&
 		    !test_bit(CIFS_INO_CLOSE_ON_LOCK, &cinode->flags) &&
-		    dclose) {
+		    dclose && !(cfile->status_file_deleted)) {
 			if (test_and_clear_bit(CIFS_INO_MODIFIED_ATTR, &cinode->flags)) {
 				inode->i_ctime = inode->i_mtime = current_time(inode);
 			}
