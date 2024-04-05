da991833271b45270e80baf3587e4ce0ea11d9a2
Ivan Vecera <ivecera@redhat.com>
2024-04-05 11:27:41 +0200

    net: Convert some ethtool_sprintf() to ethtool_puts()
    JIRA: https://issues.redhat.com/browse/RHEL-31886
    
    Conflicts:
    - hunks for missing files were skipped
    - drivers/net/ethernet/freescale/fec_main.c
      adjusted due to missing commit 6970ef27ff7fd ("net: fec: add xdp and
      page pool statistics")
    - drivers/net/ethernet/netronome/nfp/nfp_net_ethtool.c
      adjusted due to missing commit 15137daef7b0d ("nfp: add support for
      'ethtool -t DEVNAME' command")
    
    commit e403cffff1a46ab1a95d3bc72e92634445d68328
    Author: justinstitt@google.com <justinstitt@google.com>
    Date:   Wed Dec 6 23:16:12 2023 +0000
    
        net: Convert some ethtool_sprintf() to ethtool_puts()
    
        This patch converts some basic cases of ethtool_sprintf() to
        ethtool_puts().
    
        The conversions are used in cases where ethtool_sprintf() was being used
        with just two arguments:
        |       ethtool_sprintf(&data, buffer[i].name);
        or when it's used with format string: "%s"
        |       ethtool_sprintf(&data, "%s", buffer[i].name);
        which both now become:
        |       ethtool_puts(&data, buffer[i].name);
    
        Signed-off-by: Justin Stitt <justinstitt@google.com>
        Reviewed-by: Wei Fang <wei.fang@nxp.com>
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Reviewed-by: Louis Peens <louis.peens@corigine.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -2551,8 +2551,9 @@ static void fec_enet_get_strings(struct net_device *netdev,
 	int i;
 	switch (stringset) {
 	case ETH_SS_STATS:
-		for (i = 0; i < ARRAY_SIZE(fec_stats); i++)
-			ethtool_sprintf(&data, "%s", fec_stats[i].name);
+		for (i = 0; i < ARRAY_SIZE(fec_stats); i++) {
+			ethtool_puts(&data, fec_stats[i].name);
+		}
 		break;
 	case ETH_SS_TEST:
 		net_selftest_get_strings(data);
