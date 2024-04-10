4d9c68b1711edb6600c5f086e73654f0330735cb
Mark Salter <msalter@redhat.com>
2024-04-10 10:46:52 -0400

    coresight: etm4x: Drop iomem 'base' argument from etm4_probe()
    JIRA: https://issues.redhat.com/browse/RHEL-21456
    
    commit 4e3b9a6eae987c80330e5253754dab35acc2a63b
    Author: Anshuman Khandual <anshuman.khandual@arm.com>
    Date: Mon, 10 Jul 2023 11:54:56 +0530
    
        'struct etm4_drvdata' itself can carry the base address before etm4_probe()
        gets called. Just drop that redundant argument from etm4_probe().
    
        Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
        Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
        Cc: Mike Leach <mike.leach@linaro.org>
        Cc: Leo Yan <leo.yan@linaro.org>
        Cc: coresight@lists.linaro.org
        Cc: linux-arm-kernel@lists.infradead.org
        Cc: linux-kernel@vger.kernel.org
        Reviewed-by: James Clark <james.clark@arm.com>
        Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
        Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
        Link: https://lore.kernel.org/r/20230710062500.45147-3-anshuman.khandual@arm.com
    
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -2046,7 +2046,7 @@ static int etm4_add_coresight_dev(struct etm4_init_arg *init_arg)
 	return 0;
 }
 
-static int etm4_probe(struct device *dev, void __iomem *base, u32 etm_pid)
+static int etm4_probe(struct device *dev, u32 etm_pid)
 {
 	struct etmv4_drvdata *drvdata = dev_get_drvdata(dev);
 	struct csdev_access access = { 0 };
@@ -2066,8 +2066,6 @@ static int etm4_probe(struct device *dev, void __iomem *base, u32 etm_pid)
 		if (!drvdata->save_state)
 			return -ENOMEM;
 	}
-
-	drvdata->base = base;
 
 	spin_lock_init(&drvdata->spinlock);
 
@@ -2122,8 +2120,9 @@ static int etm4_probe_amba(struct amba_device *adev, const struct amba_id *id)
 	if (!drvdata)
 		return -ENOMEM;
 
+	drvdata->base = base;
 	dev_set_drvdata(dev, drvdata);
-	ret = etm4_probe(dev, base, id->id);
+	ret = etm4_probe(dev, id->id);
 	if (!ret)
 		pm_runtime_put(&adev->dev);
 
@@ -2139,6 +2138,7 @@ static int etm4_probe_platform_dev(struct platform_device *pdev)
 	if (!drvdata)
 		return -ENOMEM;
 
+	drvdata->base = NULL;
 	dev_set_drvdata(&pdev->dev, drvdata);
 	pm_runtime_get_noresume(&pdev->dev);
 	pm_runtime_set_active(&pdev->dev);
@@ -2149,7 +2149,7 @@ static int etm4_probe_platform_dev(struct platform_device *pdev)
 	 * HW by reading appropriate registers on the HW
 	 * and thus we could skip the PID.
 	 */
-	ret = etm4_probe(&pdev->dev, NULL, 0);
+	ret = etm4_probe(&pdev->dev, 0);
 
 	pm_runtime_put(&pdev->dev);
 	return ret;
