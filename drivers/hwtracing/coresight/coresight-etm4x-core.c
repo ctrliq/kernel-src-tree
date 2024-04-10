5976e72c83b68a9d59df343890de93542d592048
Mark Salter <msalter@redhat.com>
2024-04-10 10:46:52 -0400

    coresight: etm4x: Make etm4_remove_dev() return void
    JIRA: https://issues.redhat.com/browse/RHEL-21456
    
    commit c5f231f1a7e18d28e02b282d33541d31358360e4
    Author: =?UTF-8?q?Uwe=20Kleine-K=C3=B6nig?= <u.kleine-koenig@pengutronix.de>
    Date: Thu, 18 May 2023 22:16:29 +0200
    
        etm4_remove_dev() returned zero unconditionally. Make it return void
        instead, which makes it clear in the callers that there is no error to
        handle. Simplify etm4_remove_platform_dev() accordingly.
    
        Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
        Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
        Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
        Link: https://lore.kernel.org/r/20230518201629.260672-1-u.kleine-koenig@pengutronix.de
    
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -2192,7 +2192,7 @@ static void clear_etmdrvdata(void *info)
 	per_cpu(delayed_probe, cpu) = NULL;
 }
 
-static int __exit etm4_remove_dev(struct etmv4_drvdata *drvdata)
+static void __exit etm4_remove_dev(struct etmv4_drvdata *drvdata)
 {
 	bool had_delayed_probe;
 	/*
@@ -2219,8 +2219,6 @@ static int __exit etm4_remove_dev(struct etmv4_drvdata *drvdata)
 		cscfg_unregister_csdev(drvdata->csdev);
 		coresight_unregister(drvdata->csdev);
 	}
-
-	return 0;
 }
 
 static void __exit etm4_remove_amba(struct amba_device *adev)
@@ -2233,13 +2231,12 @@ static void __exit etm4_remove_amba(struct amba_device *adev)
 
 static int __exit etm4_remove_platform_dev(struct platform_device *pdev)
 {
-	int ret = 0;
 	struct etmv4_drvdata *drvdata = dev_get_drvdata(&pdev->dev);
 
 	if (drvdata)
-		ret = etm4_remove_dev(drvdata);
+		etm4_remove_dev(drvdata);
 	pm_runtime_disable(&pdev->dev);
-	return ret;
+	return 0;
 }
 
 static const struct amba_id etm4_ids[] = {
