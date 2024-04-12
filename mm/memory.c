c6121c7de254f943534c1aba4202223da8b21f6a
Chris von Recklinghausen <crecklin@redhat.com>
2024-04-12 15:16:35 -0400

    mm: introduce per-VMA lock statistics
    JIRA: https://issues.redhat.com/browse/RHEL-27741
    
    commit 52f238653e452e0fda61e880f263a173d219acd1
    Author: Suren Baghdasaryan <surenb@google.com>
    Date:   Mon Feb 27 09:36:27 2023 -0800
    
        mm: introduce per-VMA lock statistics
    
        Add a new CONFIG_PER_VMA_LOCK_STATS config option to dump extra statistics
        about handling page fault under VMA lock.
    
        Link: https://lkml.kernel.org/r/20230227173632.3292573-29-surenb@google.com
        Signed-off-by: Suren Baghdasaryan <surenb@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/memory.c
+++ mm/memory.c
@@ -5287,6 +5287,7 @@ retry:
 	/* Check if the VMA got isolated after we found it */
 	if (vma->detached) {
 		vma_end_read(vma);
+		count_vm_vma_lock_event(VMA_LOCK_MISS);
 		/* The area was replaced with another one */
 		goto retry;
 	}
@@ -5295,6 +5296,7 @@ retry:
 	return vma;
 inval:
 	rcu_read_unlock();
+	count_vm_vma_lock_event(VMA_LOCK_ABORT);
 	return NULL;
 }
 #endif /* CONFIG_PER_VMA_LOCK */
