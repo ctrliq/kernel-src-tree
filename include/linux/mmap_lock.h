06ea40f7dc41c3840a89e3424edc0efa0b23dca1
Chris von Recklinghausen <crecklin@redhat.com>
2024-04-12 15:16:31 -0400

    mm: add per-VMA lock and helper functions to control it
    Conflicts: include/linux/mm.h, include/linux/mmap_lock.h - fuzz
    
    JIRA: https://issues.redhat.com/browse/RHEL-27741
    
    commit 5e31275cc997f8ec5d9e8d65fe9840ebed89db19
    Author: Suren Baghdasaryan <surenb@google.com>
    Date:   Mon Feb 27 09:36:11 2023 -0800
    
        mm: add per-VMA lock and helper functions to control it
    
        Introduce per-VMA locking.  The lock implementation relies on a per-vma
        and per-mm sequence counters to note exclusive locking:
    
          - read lock - (implemented by vma_start_read) requires the vma
            (vm_lock_seq) and mm (mm_lock_seq) sequence counters to differ.
            If they match then there must be a vma exclusive lock held somewhere.
          - read unlock - (implemented by vma_end_read) is a trivial vma->lock
            unlock.
          - write lock - (vma_start_write) requires the mmap_lock to be held
            exclusively and the current mm counter is assigned to the vma counter.
            This will allow multiple vmas to be locked under a single mmap_lock
            write lock (e.g. during vma merging). The vma counter is modified
            under exclusive vma lock.
          - write unlock - (vma_end_write_all) is a batch release of all vma
            locks held. It doesn't pair with a specific vma_start_write! It is
            done before exclusive mmap_lock is released by incrementing mm
            sequence counter (mm_lock_seq).
          - write downgrade - if the mmap_lock is downgraded to the read lock, all
            vma write locks are released as well (effectivelly same as write
            unlock).
    
        Link: https://lkml.kernel.org/r/20230227173632.3292573-13-surenb@google.com
        Signed-off-by: Suren Baghdasaryan <surenb@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- include/linux/mmap_lock.h
+++ include/linux/mmap_lock.h
@@ -60,6 +60,17 @@ static inline void __mmap_lock_trace_released(struct mm_struct *mm, bool write)
 
 #endif /* CONFIG_TRACING */
 
+#ifdef CONFIG_PER_VMA_LOCK
+static inline void vma_end_write_all(struct mm_struct *mm)
+{
+	mmap_assert_write_locked(mm);
+	/* No races during update due to exclusive mmap_lock being held */
+	WRITE_ONCE(mm->mm_lock_seq, mm->mm_lock_seq + 1);
+}
+#else
+static inline void vma_end_write_all(struct mm_struct *mm) {}
+#endif
+
 static inline void mmap_init_lock(struct mm_struct *mm)
 {
 	init_rwsem(&mm->mmap_lock);
@@ -102,12 +113,14 @@ static inline bool mmap_write_trylock(struct mm_struct *mm)
 static inline void mmap_write_unlock(struct mm_struct *mm)
 {
 	__mmap_lock_trace_released(mm, true);
+	vma_end_write_all(mm);
 	up_write(&mm->mmap_lock);
 }
 
 static inline void mmap_write_downgrade(struct mm_struct *mm)
 {
 	__mmap_lock_trace_acquire_returned(mm, false, true);
+	vma_end_write_all(mm);
 	downgrade_write(&mm->mmap_lock);
 }
 
