e165932b0d2498154367a263aa8282de8e29e033
Aristeu Rozanski <arozansk@redhat.com>
2024-04-16 10:45:34 -0400

    Revert "Revert "Merge: EDAC: add initial support for El Capitan""
    JIRA: https://issues.redhat.com/browse/RHEL-29211
    Upstream Status: RHEL9 only
    
    This reverts commit e348fbc57a6580b4e9a888f1f44949a00b57219a.
    With the x86 rebase in place, merge back RHEL-10022
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/edac/amd64_edac.h
+++ drivers/edac/amd64_edac.h
@@ -16,6 +16,7 @@
 #include <linux/slab.h>
 #include <linux/mmzone.h>
 #include <linux/edac.h>
+#include <linux/bitfield.h>
 #include <asm/cpu_device_id.h>
 #include <asm/msr.h>
 #include "edac_module.h"
@@ -362,6 +363,7 @@ struct amd64_pvt {
 	u32 dct_sel_lo;		/* DRAM Controller Select Low */
 	u32 dct_sel_hi;		/* DRAM Controller Select High */
 	u32 online_spare;	/* On-Line spare Reg */
+	u32 gpu_umc_base;	/* Base address used for channel selection on GPUs */
 
 	/* x4, x8, or x16 syndromes in use */
 	u8 ecc_sym_sz;
@@ -469,6 +471,8 @@ struct low_ops {
 	int (*hw_info_get)(struct amd64_pvt *pvt);
 	bool (*ecc_enabled)(struct amd64_pvt *pvt);
 	void (*setup_mci_misc_attrs)(struct mem_ctl_info *mci);
+	void (*dump_misc_regs)(struct amd64_pvt *pvt);
+	void (*get_err_info)(struct mce *m, struct err_info *err);
 };
 
 int __amd64_read_pci_cfg_dword(struct pci_dev *pdev, int offset,
