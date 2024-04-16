fda3ca9780341550b9bc0c73ca7eb5995e2689d7
Sebastian Ott <sebott@redhat.com>
2024-04-16 08:06:25 -0400

    KVM: arm64: PMU: Add a helper to read a vCPU's PMCR_EL0
    JIRA: https://issues.redhat.com/browse/RHEL-31215
    
    Add a helper to read a vCPU's PMCR_EL0, and use it whenever KVM
    reads a vCPU's PMCR_EL0.
    
    Currently, the PMCR_EL0 value is tracked per vCPU. The following
    patches will make (only) PMCR_EL0.N track per guest. Having the
    new helper will be useful to combine the PMCR_EL0.N field
    (tracked per guest) and the other fields (tracked per vCPU)
    to provide the value of PMCR_EL0.
    
    No functional change intended.
    
    Reviewed-by: Sebastian Ott <sebott@redhat.com>
    Signed-off-by: Reiji Watanabe <reijiw@google.com>
    Signed-off-by: Raghavendra Rao Ananta <rananta@google.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20231020214053.2144305-4-rananta@google.com
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    (cherry picked from commit 57fc267f1b5caa56dfb46f60e20e673cbc4cc4a8)
    Signed-off-by: Sebastian Ott <sebott@redhat.com>

--- include/kvm/arm_pmu.h
+++ include/kvm/arm_pmu.h
@@ -104,6 +104,7 @@ u8 kvm_arm_pmu_get_pmuver_limit(void);
 u64 kvm_pmu_evtyper_mask(struct kvm *kvm);
 int kvm_arm_set_default_pmu(struct kvm *kvm);
 
+u64 kvm_vcpu_read_pmcr(struct kvm_vcpu *vcpu);
 #else
 struct kvm_pmu {
 };
@@ -183,6 +184,11 @@ static inline void kvm_vcpu_pmu_resync_el0(void) {}
 static inline int kvm_arm_set_default_pmu(struct kvm *kvm)
 {
 	return -ENODEV;
+}
+
+static inline u64 kvm_vcpu_read_pmcr(struct kvm_vcpu *vcpu)
+{
+	return 0;
 }
 
 #endif
