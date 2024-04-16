49dd7db0853db0af486c372063086b6e44870f1e
Nico Pache <npache@redhat.com>
2024-04-15 19:04:21 -0600

    kunit: kasan_test: disable fortify string checker on kmalloc_oob_memset
    commit b2325bf860faa2f304a7e188f00cf9f7dc9b5ee8
    Author: Nico Pache <npache@redhat.com>
    Date:   Tue Dec 12 16:26:59 2023 -0700
    
        kunit: kasan_test: disable fortify string checker on kmalloc_oob_memset
    
        Similar to commit 09c6304e38e4 ("kasan: test: fix compatibility with
        FORTIFY_SOURCE") the kernel is panicing in kmalloc_oob_memset_*.
    
        This is due to the `ptr` not being hidden from the optimizer which would
        disable the runtime fortify string checker.
    
        kernel BUG at lib/string_helpers.c:1048!
        Call Trace:
        [<00000000272502e2>] fortify_panic+0x2a/0x30
        ([<00000000272502de>] fortify_panic+0x26/0x30)
        [<001bffff817045c4>] kmalloc_oob_memset_2+0x22c/0x230 [kasan_test]
    
        Hide the `ptr` variable from the optimizer to fix the kernel panic.  Also
        define a memset_size variable and hide that as well.  This cleans up the
        code and follows the same convention as other tests.
    
        [npache@redhat.com: address review comments from Andrey]
          Link: https://lkml.kernel.org/r/20231214164423.6202-1-npache@redhat.com
        Link: https://lkml.kernel.org/r/20231212232659.18839-1-npache@redhat.com
        Signed-off-by: Nico Pache <npache@redhat.com>
        Reviewed-by: Andrey Konovalov <andreyknvl@gmail.com>
        Cc: Alexander Potapenko <glider@google.com>
        Cc: Andrey Konovalov <andreyknvl@gmail.com>
        Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
        Cc: Dmitry Vyukov <dvyukov@google.com>
        Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    JIRA: https://issues.redhat.com/browse/RHEL-5619
    Signed-off-by: Nico Pache <npache@redhat.com>

--- mm/kasan/kasan_test.c
+++ mm/kasan/kasan_test.c
@@ -492,14 +492,17 @@ static void kmalloc_oob_memset_2(struct kunit *test)
 {
 	char *ptr;
 	size_t size = 128 - KASAN_GRANULE_SIZE;
+	size_t memset_size = 2;
 
 	KASAN_TEST_NEEDS_CHECKED_MEMINTRINSICS(test);
 
 	ptr = kmalloc(size, GFP_KERNEL);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, ptr);
 
+	OPTIMIZER_HIDE_VAR(ptr);
 	OPTIMIZER_HIDE_VAR(size);
-	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 1, 0, 2));
+	OPTIMIZER_HIDE_VAR(memset_size);
+	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 1, 0, memset_size));
 	kfree(ptr);
 }
 
@@ -507,14 +510,17 @@ static void kmalloc_oob_memset_4(struct kunit *test)
 {
 	char *ptr;
 	size_t size = 128 - KASAN_GRANULE_SIZE;
+	size_t memset_size = 4;
 
 	KASAN_TEST_NEEDS_CHECKED_MEMINTRINSICS(test);
 
 	ptr = kmalloc(size, GFP_KERNEL);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, ptr);
 
+	OPTIMIZER_HIDE_VAR(ptr);
 	OPTIMIZER_HIDE_VAR(size);
-	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 3, 0, 4));
+	OPTIMIZER_HIDE_VAR(memset_size);
+	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 3, 0, memset_size));
 	kfree(ptr);
 }
 
@@ -522,14 +528,17 @@ static void kmalloc_oob_memset_8(struct kunit *test)
 {
 	char *ptr;
 	size_t size = 128 - KASAN_GRANULE_SIZE;
+	size_t memset_size = 8;
 
 	KASAN_TEST_NEEDS_CHECKED_MEMINTRINSICS(test);
 
 	ptr = kmalloc(size, GFP_KERNEL);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, ptr);
 
+	OPTIMIZER_HIDE_VAR(ptr);
 	OPTIMIZER_HIDE_VAR(size);
-	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 7, 0, 8));
+	OPTIMIZER_HIDE_VAR(memset_size);
+	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 7, 0, memset_size));
 	kfree(ptr);
 }
 
@@ -537,14 +546,17 @@ static void kmalloc_oob_memset_16(struct kunit *test)
 {
 	char *ptr;
 	size_t size = 128 - KASAN_GRANULE_SIZE;
+	size_t memset_size = 16;
 
 	KASAN_TEST_NEEDS_CHECKED_MEMINTRINSICS(test);
 
 	ptr = kmalloc(size, GFP_KERNEL);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, ptr);
 
+	OPTIMIZER_HIDE_VAR(ptr);
 	OPTIMIZER_HIDE_VAR(size);
-	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 15, 0, 16));
+	OPTIMIZER_HIDE_VAR(memset_size);
+	KUNIT_EXPECT_KASAN_FAIL(test, memset(ptr + size - 15, 0, memset_size));
 	kfree(ptr);
 }
 
