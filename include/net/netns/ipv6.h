69090f6b19cb3cace7b22173dfb3b9adc4ae8ba0
Davide Caratti <dcaratti@redhat.com>
2024-04-18 18:50:29 +0200

    net/ipv6: fix bool/int mismatch for skip_notify_on_dev_down
    JIRA: https://issues.redhat.com/browse/RHEL-33410
    Upstream Status: net.git commit edf2e1d2019b2730d6076dbe4c040d37d7c10bbe
    Conflicts:
     - include/net/netns/ipv6.h: mismatch because of missing commit 9ee11f0fff20 ("ipv6:
       ioam: Data plane support for Pre-allocated Trace")
    
    commit edf2e1d2019b2730d6076dbe4c040d37d7c10bbe
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Thu Jun 1 16:04:44 2023 +0000
    
        net/ipv6: fix bool/int mismatch for skip_notify_on_dev_down
    
        skip_notify_on_dev_down ctl table expects this field
        to be an int (4 bytes), not a bool (1 byte).
    
        Because proc_dou8vec_minmax() was added in 5.13,
        this patch converts skip_notify_on_dev_down to an int.
    
        Following patch then converts the field to u8 and use proc_dou8vec_minmax().
    
        Fixes: 7c6bb7d2faaf ("net/ipv6: Add knob to skip DELROUTE message on device down")
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reviewed-by: David Ahern <dsahern@kernel.org>
        Acked-by: Matthieu Baerts <matthieu.baerts@tessares.net>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>

--- include/net/netns/ipv6.h
+++ include/net/netns/ipv6.h
@@ -51,7 +51,7 @@ struct netns_sysctl_ipv6 {
 	int max_dst_opts_len;
 	int max_hbh_opts_len;
 	int seg6_flowlabel;
-	bool skip_notify_on_dev_down;
+	int skip_notify_on_dev_down;
 	u8 fib_notify_on_flag_change;
 };
 
