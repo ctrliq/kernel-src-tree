c930da6d1b2d6694d28b9767e2dab87ee6eec78d
Ewan D. Milne <emilne@redhat.com>
2024-04-22 16:26:19 -0400

    scsi: core: Make scsi_bus_type const
    JIRA: https://issues.redhat.com/browse/RHEL-33543
    Upstream Status: From upstream linux mainline
    
    Since commit d492cc2573a0 ("driver core: device.h: make struct bus_type a
    const *"), the driver core can properly handle constant struct bus_type,
    move the scsi_bus_type variable to be a constant structure as well, placing
    it into read-only memory which can not be modified at runtime.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Suggested-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>
    Link: https://lore.kernel.org/r/20240212-bus_cleanup-scsi2-v2-1-65004493ff09@marliere.net
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit a0bcad233fd6a4cdd14441d7cc27b28475721fe8)
    Signed-off-by: Ewan D. Milne <emilne@redhat.com>

--- drivers/scsi/scsi_priv.h
+++ drivers/scsi/scsi_priv.h
@@ -151,7 +151,7 @@ extern void scsi_sysfs_device_initialize(struct scsi_device *);
 extern struct scsi_transport_template blank_transport_template;
 extern void __scsi_remove_device(struct scsi_device *);
 
-extern struct bus_type scsi_bus_type;
+extern const struct bus_type scsi_bus_type;
 extern const struct attribute_group *scsi_sysfs_shost_attr_groups[];
 
 /* scsi_netlink.c */
