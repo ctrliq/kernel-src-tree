f3883363c5fc59f62d40126129e19ef7488033d7
Thomas Huth <thuth@redhat.com>
2024-04-23 13:36:19 +0200

    s390: simplify early program check handler
    JIRA: https://issues.redhat.com/browse/RHEL-29741
    
    commit 85806016acb0610e47ffcd5415d1575f18f58a33
    Author: Heiko Carstens <hca@linux.ibm.com>
    Date:   Fri May 20 19:23:48 2022 +0200
    
        s390: simplify early program check handler
    
        Due to historic reasons the base program check handler calls a
        configurable function. Given that there is only the early program
        check handler left, simplify the code by directly calling that
        function.
    
        The only other user was removed with commit d485235b0054 ("s390:
        assume diag308 set always works").
    
        Also rename all functions and the asm file to reflect this.
    
        Reviewed-by: Sven Schnelle <svens@linux.ibm.com>
        Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- arch/s390/include/asm/processor.h
+++ arch/s390/include/asm/processor.h
@@ -302,12 +302,6 @@ static __always_inline void __noreturn disabled_wait(void)
 	while (1);
 }
 
-/*
- * Basic Program Check Handler.
- */
-extern void s390_base_pgm_handler(void);
-extern void (*s390_base_pgm_handler_fn)(struct pt_regs *regs);
-
 #define ARCH_LOW_ADDRESS_LIMIT	0x7fffffffUL
 
 extern int s390_isolate_bp(void);
