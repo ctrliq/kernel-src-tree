ce562615a8acd97f5f94cd0a091a6b2b32fe7d34
Florian Westphal <fwestpha@redhat.com>
2024-04-25 17:31:41 +0200

    netfilter: nf_tables: restrict tunnel object to NFPROTO_NETDEV
    JIRA: https://issues.redhat.com/browse/RHEL-33985
    Upstream Status: commit 776d45164844
    
    commit 776d451648443f9884be4a1b4e38e8faf1c621f9
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Tue Jan 23 23:45:32 2024 +0100
    
        netfilter: nf_tables: restrict tunnel object to NFPROTO_NETDEV
    
        Bail out on using the tunnel dst template from other than netdev family.
        Add the infrastructure to check for the family in objects.
    
        Fixes: af308b94a2a4 ("netfilter: nf_tables: add tunnel support")
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -1259,6 +1259,7 @@ void nft_obj_notify(struct net *net, const struct nft_table *table,
  *	@type: stateful object numeric type
  *	@owner: module owner
  *	@maxattr: maximum netlink attribute
+ *	@family: address family for AF-specific object types
  *	@policy: netlink attribute policy
  */
 struct nft_object_type {
@@ -1268,6 +1269,7 @@ struct nft_object_type {
 	struct list_head		list;
 	u32				type;
 	unsigned int                    maxattr;
+	u8				family;
 	struct module			*owner;
 	const struct nla_policy		*policy;
 };
