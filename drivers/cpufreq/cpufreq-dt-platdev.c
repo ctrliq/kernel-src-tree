f1a2648288b46e1e250aae213a0db067e52a4e22
Mark Langsdorf <mlangsdo@redhat.com>
2024-04-25 11:50:09 -0400

    cpufreq: dt-platdev: Support building as module
    JIRA: https://issues.redhat.com/browse/RHEL-32446
    
    commit 3b062a086984d35a3c6d3a1c7841d0aa73aa76af
    Author: Zhipeng Wang <zhipeng.wang_1@nxp.com>
    Date: Mon, 05 Jun 2023 16:33:05 +0000
    
    Make the cpufreq platdev driver as tristate so that it can be built
    as loadable module.
    
    Also add MODULE_LICENSE to support building as module.
    
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>
    [ Viresh: Merged two commits, included module.h ]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -5,6 +5,7 @@
  */
 
 #include <linux/err.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 
@@ -220,3 +221,4 @@ create_pdev:
 			       sizeof(struct cpufreq_dt_platform_data)));
 }
 core_initcall(cpufreq_dt_platdev_init);
+MODULE_LICENSE("GPL");
