20dd56698ec35db199f9729a5a2eb693395d2a01
Aristeu Rozanski <arozansk@redhat.com>
2024-04-29 14:32:56 -0400

    mm: remove zap_page_range and create zap_vma_pages
    JIRA: https://issues.redhat.com/browse/RHEL-27740
    Tested: by me
    Conflicts: dropped RISCV changes, and due missing b59c9dc4d9d47b
    
    commit e9adcfecf572fcfaa9f8525904cf49c709974f73
    Author: Mike Kravetz <mike.kravetz@oracle.com>
    Date:   Tue Jan 3 16:27:32 2023 -0800
    
        mm: remove zap_page_range and create zap_vma_pages
    
        zap_page_range was originally designed to unmap pages within an address
        range that could span multiple vmas.  While working on [1], it was
        discovered that all callers of zap_page_range pass a range entirely within
        a single vma.  In addition, the mmu notification call within zap_page
        range does not correctly handle ranges that span multiple vmas.  When
        crossing a vma boundary, a new mmu_notifier_range_init/end call pair with
        the new vma should be made.
    
        Instead of fixing zap_page_range, do the following:
        - Create a new routine zap_vma_pages() that will remove all pages within
          the passed vma.  Most users of zap_page_range pass the entire vma and
          can use this new routine.
        - For callers of zap_page_range not passing the entire vma, instead call
          zap_page_range_single().
        - Remove zap_page_range.
    
        [1] https://lore.kernel.org/linux-mm/20221114235507.294320-2-mike.kravetz@oracle.com/
        Link: https://lkml.kernel.org/r/20230104002732.232573-1-mike.kravetz@oracle.com
        Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
        Suggested-by: Peter Xu <peterx@redhat.com>
        Acked-by: Michal Hocko <mhocko@suse.com>
        Acked-by: Peter Xu <peterx@redhat.com>
        Acked-by: Heiko Carstens <hca@linux.ibm.com>    [s390]
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Cc: Christian Borntraeger <borntraeger@linux.ibm.com>
        Cc: Christian Brauner <brauner@kernel.org>
        Cc: Dave Hansen <dave.hansen@linux.intel.com>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: Eric Dumazet <edumazet@google.com>
        Cc: Matthew Wilcox <willy@infradead.org>
        Cc: Michael Ellerman <mpe@ellerman.id.au>
        Cc: Nadav Amit <nadav.amit@gmail.com>
        Cc: Palmer Dabbelt <palmer@dabbelt.com>
        Cc: Rik van Riel <riel@surriel.com>
        Cc: Vlastimil Babka <vbabka@suse.cz>
        Cc: Will Deacon <will@kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- arch/powerpc/platforms/pseries/vas.c
+++ arch/powerpc/platforms/pseries/vas.c
@@ -782,8 +782,7 @@ static int reconfig_close_windows(struct vas_caps *vcap, int excess_creds,
 		 * is done before the original mmap() and after the ioctl.
 		 */
 		if (vma)
-			zap_page_range(vma, vma->vm_start,
-					vma->vm_end - vma->vm_start);
+			zap_vma_pages(vma);
 
 		mutex_unlock(&task_ref->mmap_mutex);
 		mmap_write_unlock(task_ref->mm);
