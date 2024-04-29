835641fe9528153668afb5a3a5ee90acaed0fd23
Aristeu Rozanski <arozansk@redhat.com>
2024-04-29 14:32:57 -0400

    mm/page_alloc: use deferred_pages_enabled() wherever applicable
    JIRA: https://issues.redhat.com/browse/RHEL-27740
    Tested: by me
    
    commit 076cf7ea67010d11a97912423f4cdbeff1bd1f5f
    Author: Anshuman Khandual <anshuman.khandual@arm.com>
    Date:   Thu Jan 5 13:55:06 2023 +0530
    
        mm/page_alloc: use deferred_pages_enabled() wherever applicable
    
        Instead of directly accessing static deferred_pages, replace such
        instances with the helper deferred_pages_enabled().  No functional change
        is intended.
    
        Link: https://lkml.kernel.org/r/20230105082506.241529-1-anshuman.khandual@arm.com
        Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
        Reviewed-by: Mike Rapoport (IBM) <rppt@kernel.org>
        Reviewed-by: David Hildenbrand <david@redhat.com>
        Acked-by: Mel Gorman <mgorman@suse.de>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4316,7 +4316,7 @@ retry:
 			 * Watermark failed for this zone, but see if we can
 			 * grow this zone if it contains deferred pages.
 			 */
-			if (static_branch_unlikely(&deferred_pages)) {
+			if (deferred_pages_enabled()) {
 				if (_deferred_grow_zone(zone, order))
 					goto try_this_zone;
 			}
@@ -4370,7 +4370,7 @@ try_this_zone:
 
 #ifdef CONFIG_DEFERRED_STRUCT_PAGE_INIT
 			/* Try again if zone has deferred pages */
-			if (static_branch_unlikely(&deferred_pages)) {
+			if (deferred_pages_enabled()) {
 				if (_deferred_grow_zone(zone, order))
 					goto try_this_zone;
 			}
