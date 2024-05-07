8815abf6f8d62b269c07881473152d5899c2008d
Andrew Halaney <ahalaney@redhat.com>
2024-05-07 15:52:25 -0500

    drm/fb: remove unused includes of drm_fb_cma_helper.h
    JIRA: https://issues.redhat.com/browse/RHEL-35606
    Conflicts: Only take the TI bits left out of RHEL's
               4cb818386e62 ("Merge DRM changes from upstream v6.0.8..v6.1")
    
    commit 5e8bf00ea915a0f290bf5ee544b91aad5eaab4bc
    Author: Danilo Krummrich <dakr@redhat.com>
    Date:   Tue Aug 2 02:04:01 2022 +0200
    
        drm/fb: remove unused includes of drm_fb_cma_helper.h
    
        Quite a lot of drivers include the drm_fb_cma_helper.h header file
        without actually making use of it's provided API, hence remove those
        includes.
    
        Suggested-by: Sam Ravnborg <sam@ravnborg.org>
        Signed-off-by: Danilo Krummrich <dakr@redhat.com>
        Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
        Acked-by: Sam Ravnborg <sam@ravnborg.org>
        Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
        Link: https://patchwork.freedesktop.org/patch/msgid/20220802000405.949236-2-dakr@redhat.com
    
    Signed-off-by: Andrew Halaney <ahalaney@redhat.com>

--- drivers/gpu/drm/tidss/tidss_plane.c
+++ drivers/gpu/drm/tidss/tidss_plane.c
@@ -10,7 +10,6 @@
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_fourcc.h>
 #include <drm/drm_framebuffer.h>
-#include <drm/drm_fb_cma_helper.h>
 #include <drm/drm_gem_atomic_helper.h>
 
 #include "tidss_crtc.h"
