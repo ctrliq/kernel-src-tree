f8e5ebbd21810d947b4da9853d4f1e4c65b9e0ad
Donald Dutile <ddutile@redhat.com>
2024-05-09 01:53:48 -0400

    modpost: reuse ARRAY_SIZE() macro for section_mismatch()
    JIRA: https://issues.redhat.com/browse/RHEL-28063
    
    commit c5c468dcc25efc0095361bb63b6255622e22f695
    Author: Masahiro Yamada <masahiroy@kernel.org>
    Date:   Tue May 24 01:46:25 2022 +0900
    
        modpost: reuse ARRAY_SIZE() macro for section_mismatch()
    
        Move ARRAY_SIZE() from file2alias.c to modpost.h to reuse it in
        section_mismatch().
    
        Also, move the variable 'check' inside the for-loop.
    
        Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
        Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1050,8 +1050,6 @@ static const struct sectioncheck *section_mismatch(
 		const char *fromsec, const char *tosec)
 {
 	int i;
-	int elems = sizeof(sectioncheck) / sizeof(struct sectioncheck);
-	const struct sectioncheck *check = &sectioncheck[0];
 
 	/*
 	 * The target section could be the SHT_NUL section when we're
@@ -1062,14 +1060,15 @@ static const struct sectioncheck *section_mismatch(
 	if (*tosec == '\0')
 		return NULL;
 
-	for (i = 0; i < elems; i++) {
+	for (i = 0; i < ARRAY_SIZE(sectioncheck); i++) {
+		const struct sectioncheck *check = &sectioncheck[i];
+
 		if (match(fromsec, check->fromsec)) {
 			if (check->bad_tosec[0] && match(tosec, check->bad_tosec))
 				return check;
 			if (check->good_tosec[0] && !match(tosec, check->good_tosec))
 				return check;
 		}
-		check++;
 	}
 	return NULL;
 }
