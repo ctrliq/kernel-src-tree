d92b090692ed0c65fb85f1cf08cd914745f6d556
Donald Dutile <ddutile@redhat.com>
2024-05-09 01:53:59 -0400

    dyndbg: use the module notifier callbacks
    JIRA: https://issues.redhat.com/browse/RHEL-28063
    
    commit 7deabd67498869640c937c9bd83472574b7dea0b
    Author: Jason Baron <jbaron@akamai.com>
    Date:   Fri Mar 3 11:50:56 2023 -0500
    
        dyndbg: use the module notifier callbacks
    
        Bring dynamic debug in line with other subsystems by using the module
        notifier callbacks. This results in a net decrease in core module
        code.
    
        Additionally, Jim Cromie has a new dynamic debug classmap feature,
        which requires that jump labels be initialized prior to dynamic debug.
        Specifically, the new feature toggles a jump label from the existing
        dynamic_debug_setup() function. However, this does not currently work
        properly, because jump labels are initialized via the
        'module_notify_list' notifier chain, which is invoked after the
        current call to dynamic_debug_setup(). Thus, this patch ensures that
        jump labels are initialized prior to dynamic debug by setting the
        dynamic debug notifier priority to 0, while jump labels have the
        higher priority of 1.
    
        Tested by Jim using his new test case, and I've verfied the correct
        printing via: # modprobe test_dynamic_debug dyndbg.
    
        Link: https://lore.kernel.org/lkml/20230113193016.749791-21-jim.cromie@gmail.com/
        Reported-by: kernel test robot <lkp@intel.com>
        Link: https://lore.kernel.org/oe-kbuild-all/202302190427.9iIK2NfJ-lkp@intel.com/
        Tested-by: Jim Cromie <jim.cromie@gmail.com>
        Reviewed-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        CC: Jim Cromie <jim.cromie@gmail.com>
        Cc: Luis Chamberlain <mcgrof@kernel.org>
        Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Signed-off-by: Jason Baron <jbaron@akamai.com>
        Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -1227,7 +1227,7 @@ static void ddebug_attach_module_classes(struct ddebug_table *dt,
  * Allocate a new ddebug_table for the given module
  * and add it to the global list.
  */
-static int __ddebug_add_module(struct _ddebug_info *di, const char *modname)
+static int ddebug_add_module(struct _ddebug_info *di, const char *modname)
 {
 	struct ddebug_table *dt;
 
@@ -1264,11 +1264,6 @@ static int __ddebug_add_module(struct _ddebug_info *di, const char *modname)
 
 	vpr_info("%3u debug prints in module %s\n", di->num_descs, modname);
 	return 0;
-}
-
-int ddebug_add_module(struct _ddebug_info *di, const char *modname)
-{
-	return __ddebug_add_module(di, modname);
 }
 
 /* helper for ddebug_dyndbg_(boot|module)_param_cb */
@@ -1317,11 +1312,13 @@ static void ddebug_table_free(struct ddebug_table *dt)
 	kfree(dt);
 }
 
+#ifdef CONFIG_MODULES
+
 /*
  * Called in response to a module being unloaded.  Removes
  * any ddebug_table's which point at the module.
  */
-int ddebug_remove_module(const char *mod_name)
+static int ddebug_remove_module(const char *mod_name)
 {
 	struct ddebug_table *dt, *nextdt;
 	int ret = -ENOENT;
@@ -1339,6 +1336,33 @@ int ddebug_remove_module(const char *mod_name)
 		v2pr_info("removed module \"%s\"\n", mod_name);
 	return ret;
 }
+
+static int ddebug_module_notify(struct notifier_block *self, unsigned long val,
+				void *data)
+{
+	struct module *mod = data;
+	int ret = 0;
+
+	switch (val) {
+	case MODULE_STATE_COMING:
+		ret = ddebug_add_module(&mod->dyndbg_info, mod->name);
+		if (ret)
+			WARN(1, "Failed to allocate memory: dyndbg may not work properly.\n");
+		break;
+	case MODULE_STATE_GOING:
+		ddebug_remove_module(mod->name);
+		break;
+	}
+
+	return notifier_from_errno(ret);
+}
+
+static struct notifier_block ddebug_module_nb = {
+	.notifier_call = ddebug_module_notify,
+	.priority = 0, /* dynamic debug depends on jump label */
+};
+
+#endif /* CONFIG_MODULES */
 
 static void ddebug_remove_all_tables(void)
 {
@@ -1391,6 +1415,14 @@ static int __init dynamic_debug_init(void)
 		.num_classes = __stop___dyndbg_classes - __start___dyndbg_classes,
 	};
 
+#ifdef CONFIG_MODULES
+	ret = register_module_notifier(&ddebug_module_nb);
+	if (ret) {
+		pr_warn("Failed to register dynamic debug module notifier\n");
+		return ret;
+	}
+#endif /* CONFIG_MODULES */
+
 	if (&__start___dyndbg == &__stop___dyndbg) {
 		if (IS_ENABLED(CONFIG_DYNAMIC_DEBUG)) {
 			pr_warn("_ddebug table is empty in a CONFIG_DYNAMIC_DEBUG build\n");
@@ -1411,7 +1443,7 @@ static int __init dynamic_debug_init(void)
 			mod_ct++;
 			di.num_descs = mod_sites;
 			di.descs = iter_mod_start;
-			ret = __ddebug_add_module(&di, modname);
+			ret = ddebug_add_module(&di, modname);
 			if (ret)
 				goto out_err;
 
@@ -1422,7 +1454,7 @@ static int __init dynamic_debug_init(void)
 	}
 	di.num_descs = mod_sites;
 	di.descs = iter_mod_start;
-	ret = __ddebug_add_module(&di, modname);
+	ret = ddebug_add_module(&di, modname);
 	if (ret)
 		goto out_err;
 
