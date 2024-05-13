6284997394d46d8b4bc6991be4b2e2eb07d5dc43
Paolo Bonzini <pbonzini@redhat.com>
2024-05-13 14:21:17 +0200

    x86/cpufeatures: Enable/unmask SEV-SNP CPU feature
    JIRA: https://issues.redhat.com/browse/RHEL-16745
    
    With all the required host changes in place, it should now be possible
    to initialize SNP-related MSR bits, set up RMP table enforcement, and
    initialize SNP support in firmware while maintaining legacy support for
    SEV/SEV-ES guests. Go ahead and enable the SNP feature now.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20240126041126.1927228-23-michael.roth@amd.com
    (cherry picked from commit c3b86e61b75645276aa2565649a6da5d6e77030f)
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Conflict: no DISABLE_IBT in RHEL

--- arch/x86/include/asm/disabled-features.h
+++ arch/x86/include/asm/disabled-features.h
@@ -111,7 +111,11 @@
 #define DISABLE_USER_SHSTK	(1 << (X86_FEATURE_USER_SHSTK & 31))
 #endif
 
+#ifdef CONFIG_KVM_AMD_SEV
+#define DISABLE_SEV_SNP		0
+#else
 #define DISABLE_SEV_SNP		(1 << (X86_FEATURE_SEV_SNP & 31))
+#endif
 
 /*
  * Make sure to add features to the correct mask
