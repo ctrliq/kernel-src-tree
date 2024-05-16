50157ec86f845675fe3fcb9b9d3e707ae883801a
Ivan Vecera <ivecera@redhat.com>
2024-05-16 11:54:32 +0200

    drivers: vxlan: fix returnvar.cocci warning
    JIRA: https://issues.redhat.com/browse/RHEL-36610
    
    commit e58bc864630f0eb5e7bff8ac3c2d5816591189de
    Author: Guo Zhengkui <guozhengkui@vivo.com>
    Date:   Tue Mar 8 21:43:09 2022 +0800
    
        drivers: vxlan: fix returnvar.cocci warning
    
        Fix the following coccicheck warning:
    
        drivers/net/vxlan/vxlan_core.c:2995:5-8:
        Unneeded variable: "ret". Return "0" on line 3004.
    
        Fixes: f9c4bb0b245c ("vxlan: vni filtering support on collect metadata device")
        Signed-off-by: Guo Zhengkui <guozhengkui@vivo.com>
        Acked-by: Roopa Prabhu <roopa@nvidia.com>
        Link: https://lore.kernel.org/r/20220308134321.29862-1-guozhengkui@vivo.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/vxlan/vxlan_core.c
+++ drivers/net/vxlan/vxlan_core.c
@@ -3020,7 +3020,6 @@ static void vxlan_flush(struct vxlan_dev *vxlan, bool do_all)
 static int vxlan_stop(struct net_device *dev)
 {
 	struct vxlan_dev *vxlan = netdev_priv(dev);
-	int ret = 0;
 
 	vxlan_multicast_leave(vxlan);
 
@@ -3029,7 +3028,7 @@ static int vxlan_stop(struct net_device *dev)
 	vxlan_flush(vxlan, false);
 	vxlan_sock_release(vxlan);
 
-	return ret;
+	return 0;
 }
 
 /* Stub, nothing needs to be done. */
