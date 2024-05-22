060dc0b240e94ab13011997488574479ec2563de
Ian Kent <ikent@redhat.com>
2024-05-22 13:39:20 +0800

    fs: port ->fileattr_set() to pass mnt_idmap
    JIRA: https://issues.redhat.com/browse/RHEL-33888
    Status: Linus
    
    Conflicts: For consistency drop btrfs hunks because it isn't supported in
    	CentOS Stream and other backports also drop such hunks.
    
    commit 8782a9aea3ab4d697ad67d1f8ebca38a4e1c24ab
    Author: Christian Brauner <brauner@kernel.org>
    Date:   Fri Jan 13 12:49:21 2023 +0100
    
        fs: port ->fileattr_set() to pass mnt_idmap
    
        Convert to struct mnt_idmap.
    
        Last cycle we merged the necessary infrastructure in
        256c8aed2b42 ("fs: introduce dedicated idmap type for mounts").
        This is just the conversion to struct mnt_idmap.
    
        Currently we still pass around the plain namespace that was attached to a
        mount. This is in general pretty convenient but it makes it easy to
        conflate namespaces that are relevant on the filesystem with namespaces
        that are relevent on the mount level. Especially for non-vfs developers
        without detailed knowledge in this area this can be a potential source for
        bugs.
    
        Once the conversion to struct mnt_idmap is done all helpers down to the
        really low-level helpers will take a struct mnt_idmap argument instead of
        two namespace arguments. This way it becomes impossible to conflate the two
        eliminating the possibility of any bugs. All of the vfs and all filesystems
        only operate on struct mnt_idmap.
    
        Acked-by: Dave Chinner <dchinner@redhat.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>
    
    Signed-off-by: Ian Kent <ikent@redhat.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -3056,7 +3056,7 @@ int f2fs_fileattr_get(struct dentry *dentry, struct fileattr *fa)
 	return 0;
 }
 
-int f2fs_fileattr_set(struct user_namespace *mnt_userns,
+int f2fs_fileattr_set(struct mnt_idmap *idmap,
 		      struct dentry *dentry, struct fileattr *fa)
 {
 	struct inode *inode = d_inode(dentry);
