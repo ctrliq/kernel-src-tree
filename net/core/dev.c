b0488605b02ecc1abd32046eb2a7c6620c21ce2e
Antoine Tenart <atenart@redhat.com>
2024-05-28 15:18:33 +0200

    net: remove default_device_exit()
    JIRA: https://issues.redhat.com/browse/RHEL-29681
    Upstream Status: linux.git
    
    commit ee403248fa6db5ca23031fc51b06284d6855cd02
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Mon Feb 7 20:50:38 2022 -0800
    
        net: remove default_device_exit()
    
        For some reason default_device_ops kept two exit method:
    
        1) default_device_exit() is called for each netns being dismantled in
        a cleanup_net() round. This acquires rtnl for each invocation.
    
        2) default_device_exit_batch() is called once with the list of all netns
        int the batch, allowing for a single rtnl invocation.
    
        Get rid of the .exit() method to handle the logic from
        default_device_exit_batch(), to decrease the number of rtnl acquisition
        to one.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reviewed-by: David Ahern <dsahern@kernel.org>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -11284,14 +11284,14 @@ static struct pernet_operations __net_initdata netdev_net_ops = {
 	.exit = netdev_exit,
 };
 
-static void __net_exit default_device_exit(struct net *net)
+static void __net_exit default_device_exit_net(struct net *net)
 {
 	struct net_device *dev, *aux;
 	/*
 	 * Push all migratable network devices back to the
 	 * initial network namespace
 	 */
-	rtnl_lock();
+	ASSERT_RTNL();
 	for_each_netdev_safe(net, dev, aux) {
 		int err;
 		char fb_name[IFNAMSIZ];
@@ -11315,22 +11315,22 @@ static void __net_exit default_device_exit(struct net *net)
 			BUG();
 		}
 	}
-	rtnl_unlock();
 }
 
 static void __net_exit rtnl_lock_unregistering(struct list_head *net_list)
 {
-	/* Return with the rtnl_lock held when there are no network
+	/* Return (with the rtnl_lock held) when there are no network
 	 * devices unregistering in any network namespace in net_list.
 	 */
-	struct net *net;
-	bool unregistering;
 	DEFINE_WAIT_FUNC(wait, woken_wake_function);
+	bool unregistering;
+	struct net *net;
 
+	ASSERT_RTNL();
 	add_wait_queue(&netdev_unregistering_wq, &wait);
 	for (;;) {
 		unregistering = false;
-		rtnl_lock();
+
 		list_for_each_entry(net, net_list, exit_list) {
 			if (atomic_read(&net->dev_unreg_count) > 0) {
 				unregistering = true;
@@ -11342,6 +11342,7 @@ static void __net_exit rtnl_lock_unregistering(struct list_head *net_list)
 		__rtnl_unlock();
 
 		wait_woken(&wait, TASK_UNINTERRUPTIBLE, MAX_SCHEDULE_TIMEOUT);
+		rtnl_lock();
 	}
 	remove_wait_queue(&netdev_unregistering_wq, &wait);
 }
@@ -11357,6 +11358,11 @@ static void __net_exit default_device_exit_batch(struct list_head *net_list)
 	struct net *net;
 	LIST_HEAD(dev_kill_list);
 
+	rtnl_lock();
+	list_for_each_entry(net, net_list, exit_list) {
+		default_device_exit_net(net);
+		cond_resched();
+	}
 	/* To prevent network device cleanup code from dereferencing
 	 * loopback devices or network devices that have been freed
 	 * wait here for all pending unregistrations to complete,
@@ -11369,6 +11375,7 @@ static void __net_exit default_device_exit_batch(struct list_head *net_list)
 	 * default_device_exit_batch.
 	 */
 	rtnl_lock_unregistering(net_list);
+
 	list_for_each_entry(net, net_list, exit_list) {
 		for_each_netdev_reverse(net, dev) {
 			if (dev->rtnl_link_ops && dev->rtnl_link_ops->dellink)
@@ -11382,7 +11389,6 @@ static void __net_exit default_device_exit_batch(struct list_head *net_list)
 }
 
 static struct pernet_operations __net_initdata default_device_ops = {
-	.exit = default_device_exit,
 	.exit_batch = default_device_exit_batch,
 };
 
