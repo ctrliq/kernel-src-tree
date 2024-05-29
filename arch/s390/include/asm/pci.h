3ad1e086dbf6320373eb5e5b1a3592d44a55344f
Thomas Huth <thuth@redhat.com>
2024-05-29 16:19:10 +0200

    s390/pci: remove unused bus_next field from struct zpci_dev
    JIRA: https://issues.redhat.com/browse/RHEL-54248
    
    commit 8fb65e05bd60058e15842e511b3ee5299ac51829
    Author: Niklas Schnelle <schnelle@linux.ibm.com>
    Date:   Fri Sep 9 14:34:41 2022 +0200
    
        s390/pci: remove unused bus_next field from struct zpci_dev
    
        This field was added in commit 44510d6fa0c0 ("s390/pci: Handling
        multifunctions") but is an unused remnant of an earlier version where
        the devices on the virtual bus were connected in a linked list instead
        of a fixed 256 entry array of pointers.
    
        It is also not used for the list of busses as that is threaded through
        struct zpci_bus not through struct zpci_dev.
    
        Reviewed-by: Pierre Morel <pmorel@linux.ibm.com>
        Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>
        Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    
    Conflicts:
        arch/s390/include/asm/pci.h
        (simple contextual conflict due to out-of-order backport of the
         "iommu/s390: Get rid of s390_domain_device" patch)
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- arch/s390/include/asm/pci.h
+++ arch/s390/include/asm/pci.h
@@ -117,7 +117,6 @@ struct zpci_bus {
 struct zpci_dev {
 	struct zpci_bus *zbus;
 	struct list_head entry;		/* list of all zpci_devices, needed for hotplug, etc. */
-	struct list_head bus_next;
 	struct list_head iommu_list;
 	struct kref kref;
 	struct rcu_head rcu;
