c7df211c4754227fc0b6d39da62e5ce629b5aca3
Ivan Vecera <ivecera@redhat.com>
2024-05-30 17:34:13 +0200

    net: Add NAPI IRQ support
    JIRA: https://issues.redhat.com/browse/RHEL-30139
    
    Conflicts:
    - context conflict due to RH KABI reservations for z-stream
    
    commit 26793bfb5d6072326d1465343e7cbf6156abca4f
    Author: Amritha Nambiar <amritha.nambiar@intel.com>
    Date:   Fri Dec 1 15:29:07 2023 -0800
    
        net: Add NAPI IRQ support
    
        Add support to associate the interrupt vector number for a
        NAPI instance.
    
        Signed-off-by: Amritha Nambiar <amritha.nambiar@intel.com>
        Reviewed-by: Sridhar Samudrala <sridhar.samudrala@intel.com>
        Link: https://lore.kernel.org/r/170147334728.5260.13221803396905901904.stgit@anambiarhost.jf.intel.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -367,6 +367,7 @@ struct napi_struct {
 	/* control-path-only fields follow */
 	struct list_head	dev_list;
 	struct hlist_node	napi_hash_node;
+	int			irq;
 
 	RH_KABI_RESERVE(1)
 	RH_KABI_RESERVE(2)
@@ -2630,6 +2631,11 @@ static inline void *netdev_priv(const struct net_device *dev)
 void netif_queue_set_napi(struct net_device *dev, unsigned int queue_index,
 			  enum netdev_queue_type type,
 			  struct napi_struct *napi);
+
+static inline void netif_napi_set_irq(struct napi_struct *napi, int irq)
+{
+	napi->irq = irq;
+}
 
 /* Default NAPI poll() weight
  * Device drivers are strongly advised to not use bigger value
