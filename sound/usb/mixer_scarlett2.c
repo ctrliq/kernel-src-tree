79d8a3ede557384dc286380c54a41eba0a453daf
Jaroslav Kysela <jkysela@redhat.com>
2024-05-30 12:38:15 +0200

    ALSA: scarlett2: Remove repeated elem->head.mixer references
    JIRA: https://issues.redhat.com/browse/RHEL-27515
    
    commit 4dedf7ca929a3f29fbed973e85372056d69a1848
    Author: Geoffrey D. Bennett <g@b4.vu>
    Date: Wed Dec 27 04:36:13 2023 +1030
    
        ALSA: scarlett2: Remove repeated elem->head.mixer references
    
        Use a local variable *mixer rather than repeating elem->header.mixer
        in scarlett2_direct_monitor_ctl_get() and scarlett2_meter_ctl_get().
    
        Signed-off-by: Geoffrey D. Bennett <g@b4.vu>
        Signed-off-by: Takashi Iwai <tiwai@suse.de>
        Link: https://lore.kernel.org/r/b21bacf4056366e10e01077e224d2b4970fdfe31.1703612638.git.g@b4.vu
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/usb/mixer_scarlett2.c
+++ sound/usb/mixer_scarlett2.c
@@ -3982,7 +3982,7 @@ static int scarlett2_direct_monitor_ctl_get(
 {
 	struct usb_mixer_elem_info *elem = kctl->private_data;
 	struct usb_mixer_interface *mixer = elem->head.mixer;
-	struct scarlett2_data *private = elem->head.mixer->private_data;
+	struct scarlett2_data *private = mixer->private_data;
 	int err = 0;
 
 	mutex_lock(&private->data_mutex);
@@ -4246,7 +4246,8 @@ static int scarlett2_meter_ctl_get(struct snd_kcontrol *kctl,
 				   struct snd_ctl_elem_value *ucontrol)
 {
 	struct usb_mixer_elem_info *elem = kctl->private_data;
-	struct scarlett2_data *private = elem->head.mixer->private_data;
+	struct usb_mixer_interface *mixer = elem->head.mixer;
+	struct scarlett2_data *private = mixer->private_data;
 	u8 *meter_level_map = private->meter_level_map;
 	u16 meter_levels[SCARLETT2_MAX_METERS];
 	int i, err;
@@ -4258,7 +4259,7 @@ static int scarlett2_meter_ctl_get(struct snd_kcontrol *kctl,
 		goto unlock;
 	}
 
-	err = scarlett2_usb_get_meter_levels(elem->head.mixer, elem->channels,
+	err = scarlett2_usb_get_meter_levels(mixer, elem->channels,
 					     meter_levels);
 	if (err < 0)
 		goto unlock;
