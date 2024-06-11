844e0ed599acf86518d55fb4c2025cc6f79af44f
Ivan Vecera <ivecera@redhat.com>
2024-06-11 11:22:10 +0200

    ip_tunnel: use a separate struct to store tunnel params in the kernel
    JIRA: https://issues.redhat.com/browse/RHEL-40130
    
    Conflicts:
    - context conflict due to __rh_deprecated_ndo_get_devlink_port() in
      net_device_ops that is RHEL specific
    
    commit 117aef12a7b1b797bce9f66b156c65eab850b5b5
    Author: Alexander Lobakin <aleksander.lobakin@intel.com>
    Date:   Wed Mar 27 16:23:52 2024 +0100
    
        ip_tunnel: use a separate struct to store tunnel params in the kernel
    
        Unlike IPv6 tunnels which use purely-kernel __ip6_tnl_parm structure
        to store params inside the kernel, IPv4 tunnel code uses the same
        ip_tunnel_parm which is being used to talk with the userspace.
        This makes it difficult to alter or add any fields or use a
        different format for whatever data.
        Define struct ip_tunnel_parm_kern, a 1:1 copy of ip_tunnel_parm for
        now, and use it throughout the code. Define the pieces, where the copy
        user <-> kernel happens, as standalone functions, and copy the data
        there field-by-field, so that the kernel-side structure could be easily
        modified later on and the users wouldn't have to care about this.
    
        Reviewed-by: Simon Horman <horms@kernel.org>
        Signed-off-by: Alexander Lobakin <aleksander.lobakin@intel.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- net/ipv4/ip_gre.c
+++ net/ipv4/ip_gre.c
@@ -791,7 +791,8 @@ static void ipgre_link_update(struct net_device *dev, bool set_mtu)
 	}
 }
 
-static int ipgre_tunnel_ctl(struct net_device *dev, struct ip_tunnel_parm *p,
+static int ipgre_tunnel_ctl(struct net_device *dev,
+			    struct ip_tunnel_parm_kern *p,
 			    int cmd)
 {
 	int err;
@@ -1138,7 +1139,7 @@ static int erspan_validate(struct nlattr *tb[], struct nlattr *data[],
 static int ipgre_netlink_parms(struct net_device *dev,
 				struct nlattr *data[],
 				struct nlattr *tb[],
-				struct ip_tunnel_parm *parms,
+				struct ip_tunnel_parm_kern *parms,
 				__u32 *fwmark)
 {
 	struct ip_tunnel *t = netdev_priv(dev);
@@ -1205,7 +1206,7 @@ static int ipgre_netlink_parms(struct net_device *dev,
 static int erspan_netlink_parms(struct net_device *dev,
 				struct nlattr *data[],
 				struct nlattr *tb[],
-				struct ip_tunnel_parm *parms,
+				struct ip_tunnel_parm_kern *parms,
 				__u32 *fwmark)
 {
 	struct ip_tunnel *t = netdev_priv(dev);
@@ -1364,7 +1365,7 @@ static int ipgre_newlink(struct net *src_net, struct net_device *dev,
 			 struct nlattr *tb[], struct nlattr *data[],
 			 struct netlink_ext_ack *extack)
 {
-	struct ip_tunnel_parm p;
+	struct ip_tunnel_parm_kern p;
 	__u32 fwmark = 0;
 	int err;
 
@@ -1382,7 +1383,7 @@ static int erspan_newlink(struct net *src_net, struct net_device *dev,
 			  struct nlattr *tb[], struct nlattr *data[],
 			  struct netlink_ext_ack *extack)
 {
-	struct ip_tunnel_parm p;
+	struct ip_tunnel_parm_kern p;
 	__u32 fwmark = 0;
 	int err;
 
@@ -1401,8 +1402,8 @@ static int ipgre_changelink(struct net_device *dev, struct nlattr *tb[],
 			    struct netlink_ext_ack *extack)
 {
 	struct ip_tunnel *t = netdev_priv(dev);
+	struct ip_tunnel_parm_kern p;
 	__u32 fwmark = t->fwmark;
-	struct ip_tunnel_parm p;
 	int err;
 
 	err = ipgre_newlink_encap_setup(dev, data);
@@ -1430,8 +1431,8 @@ static int erspan_changelink(struct net_device *dev, struct nlattr *tb[],
 			     struct netlink_ext_ack *extack)
 {
 	struct ip_tunnel *t = netdev_priv(dev);
+	struct ip_tunnel_parm_kern p;
 	__u32 fwmark = t->fwmark;
-	struct ip_tunnel_parm p;
 	int err;
 
 	err = ipgre_newlink_encap_setup(dev, data);
@@ -1503,7 +1504,7 @@ static size_t ipgre_get_size(const struct net_device *dev)
 static int ipgre_fill_info(struct sk_buff *skb, const struct net_device *dev)
 {
 	struct ip_tunnel *t = netdev_priv(dev);
-	struct ip_tunnel_parm *p = &t->parms;
+	struct ip_tunnel_parm_kern *p = &t->parms;
 	__be16 o_flags = p->o_flags;
 
 	if (nla_put_u32(skb, IFLA_GRE_LINK, p->link) ||
