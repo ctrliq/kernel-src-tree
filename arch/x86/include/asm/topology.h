26415ce0fe9e011f415210396604df7f523af967
David Arcari <darcari@redhat.com>
2024-06-17 07:49:17 -0400

    x86/cpu/topology: Use topology logical mapping mechanism
    JIRA: https://issues.redhat.com/browse/RHEL-43147
    Conflicts: RHEL commit 41f6b4aa7fa3 similarly modified topology.h
    
    commit 380414be78bf8dbe1c3ed98feb75e2579c4a1bae
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Tue Feb 13 22:06:09 2024 +0100
    
        x86/cpu/topology: Use topology logical mapping mechanism
    
        Replace the logical package and die management functionality and retrieve
        the logical IDs from the topology bitmaps.
    
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Tested-by: Michael Kelley <mhklinux@outlook.com>
        Tested-by: Sohil Mehta <sohil.mehta@intel.com>
        Link: https://lore.kernel.org/r/20240213210252.901865302@linutronix.de
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- arch/x86/include/asm/topology.h
+++ arch/x86/include/asm/topology.h
@@ -167,6 +167,13 @@ static inline int topology_get_logical_id(u32 apicid, enum x86_topology_domains 
 #define topology_core_cpumask(cpu)		(per_cpu(cpu_core_map, cpu))
 #define topology_sibling_cpumask(cpu)		(per_cpu(cpu_sibling_map, cpu))
 
+
+static inline int topology_phys_to_logical_pkg(unsigned int pkg)
+{
+	return topology_get_logical_id(pkg << x86_topo_system.dom_shifts[TOPO_PKG_DOMAIN],
+				       TOPO_PKG_DOMAIN);
+}
+
 extern int __max_smt_threads;
 
 static inline int topology_max_smt_threads(void)
@@ -175,10 +182,6 @@ static inline int topology_max_smt_threads(void)
 }
 
 #include <linux/cpu_smt.h>
-
-int topology_update_package_map(unsigned int apicid, unsigned int cpu);
-int topology_update_die_map(unsigned int dieid, unsigned int cpu);
-int topology_phys_to_logical_pkg(unsigned int pkg);
 
 extern unsigned int __amd_nodes_per_pkg;
 
