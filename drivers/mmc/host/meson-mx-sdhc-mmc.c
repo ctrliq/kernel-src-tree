0c07790416e58730434fa06bb815ab0cfab5a166
Lucas Karpinski <lkarpins@redhat.com>
2024-06-17 13:16:05 -0400

    mmc: meson-mx-sdhc: Convert to platform remove callback returning void
    JIRA: https://issues.redhat.com/browse/RHEL-41085
    
    commit 3372487a4c298843f257c9c30f7281092d1757bc
    Author: Yangtao Li <frank.li@vivo.com>
    Date:   Thu Jul 27 15:00:11 2023 +0800
    
        mmc: meson-mx-sdhc: Convert to platform remove callback returning void
    
        The .remove() callback for a platform driver returns an int which makes
        many driver authors wrongly assume it's possible to do error handling by
        returning an error code. However the value returned is (mostly) ignored
        and this typically results in resource leaks. To improve here there is a
        quest to make the remove callback return void. In the first step of this
        quest all drivers are converted to .remove_new() which already returns
        void.
    
        Trivially convert this driver from always returning zero in the remove
        callback to the void returning variant.
    
        Cc: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
        Signed-off-by: Yangtao Li <frank.li@vivo.com>
        Reviewed-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
        Link: https://lore.kernel.org/r/20230727070051.17778-22-frank.li@vivo.com
        Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    
    Signed-off-by: Lucas Karpinski <lkarpins@redhat.com>

--- drivers/mmc/host/meson-mx-sdhc-mmc.c
+++ drivers/mmc/host/meson-mx-sdhc-mmc.c
@@ -861,7 +861,7 @@ err_disable_pclk:
 	return ret;
 }
 
-static int meson_mx_sdhc_remove(struct platform_device *pdev)
+static void meson_mx_sdhc_remove(struct platform_device *pdev)
 {
 	struct meson_mx_sdhc_host *host = platform_get_drvdata(pdev);
 
@@ -870,8 +870,6 @@ static int meson_mx_sdhc_remove(struct platform_device *pdev)
 	meson_mx_sdhc_disable_clks(host->mmc);
 
 	clk_disable_unprepare(host->pclk);
-
-	return 0;
 }
 
 static const struct meson_mx_sdhc_data meson_mx_sdhc_data_meson8 = {
@@ -906,7 +904,7 @@ MODULE_DEVICE_TABLE(of, meson_mx_sdhc_of_match);
 
 static struct platform_driver meson_mx_sdhc_driver = {
 	.probe   = meson_mx_sdhc_probe,
-	.remove  = meson_mx_sdhc_remove,
+	.remove_new = meson_mx_sdhc_remove,
 	.driver  = {
 		.name = "meson-mx-sdhc",
 		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
