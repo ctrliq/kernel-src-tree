cba04e1cd5fc1d00ca326c2b59af34969659e8a2
Izabela Bakollari <ibakolla@redhat.com>
2024-06-24 19:00:18 +0200

    net: mdio: add mdiodev_c45_(read|write)
    JIRA: https://issues.redhat.com/browse/RHEL-33716
    
    commit f6479ea4e5990e17b9690d66474198dcdba1b2c1
    Author: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Date:   Tue Nov 8 14:25:56 2022 +0000
    
        net: mdio: add mdiodev_c45_(read|write)
    
        Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- include/linux/mdio.h
+++ include/linux/mdio.h
@@ -599,6 +599,19 @@ static inline int mdiodev_c45_modify_changed(struct mdio_device *mdiodev,
 					  regnum, mask, set);
 }
 
+static inline int mdiodev_c45_read(struct mdio_device *mdiodev, int devad,
+				   u16 regnum)
+{
+	return mdiobus_c45_read(mdiodev->bus, mdiodev->addr, devad, regnum);
+}
+
+static inline int mdiodev_c45_write(struct mdio_device *mdiodev, u32 devad,
+				    u16 regnum, u16 val)
+{
+	return mdiobus_c45_write(mdiodev->bus, mdiodev->addr, devad, regnum,
+				 val);
+}
+
 int mdiobus_register_device(struct mdio_device *mdiodev);
 int mdiobus_unregister_device(struct mdio_device *mdiodev);
 bool mdiobus_is_registered_device(struct mii_bus *bus, int addr);
