d19d3dc54b6acba25f74e92352bfdef96e9cc34f
Ming Lei <ming.lei@redhat.com>
2024-06-25 07:52:38 +0200

    xen-blkfront: fix sector_size propagation to the block layer
    JIRA: https://issues.redhat.com/browse/RHEL-56837
    
    commit 98d34c087249d39838874b83e17671e7d5eb1ca7
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Tue Jun 25 07:52:38 2024 +0200
    
        xen-blkfront: fix sector_size propagation to the block layer
    
        Ensure that info->sector_size and info->physical_sector_size are set
        before the call to blkif_set_queue_limits by doing away with the
        local variables and arguments that propagate them.
    
        Thanks to Marek Marczykowski-Górecki and Jürgen Groß for root causing
        the issue.
    
        Fixes: ba3f67c11638 ("xen-blkfront: atomically update queue limits")
        Reported-by: Rusty Bird <rustybird@net-c.com>
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
        Link: https://lore.kernel.org/r/20240625055238.7934-1-hch@lst.de
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/block/xen-blkfront.c
+++ drivers/block/xen-blkfront.c
@@ -1051,8 +1051,7 @@ static char *encode_disk_name(char *ptr, unsigned int n)
 }
 
 static int xlvbd_alloc_gendisk(blkif_sector_t capacity,
-		struct blkfront_info *info, u16 sector_size,
-		unsigned int physical_sector_size)
+		struct blkfront_info *info)
 {
 	struct queue_limits lim = {};
 	struct gendisk *gd;
@@ -1146,8 +1145,6 @@ static int xlvbd_alloc_gendisk(blkif_sector_t capacity,
 
 	info->rq = gd->queue;
 	info->gd = gd;
-	info->sector_size = sector_size;
-	info->physical_sector_size = physical_sector_size;
 
 	xlvbd_flush(info);
 
@@ -2264,8 +2261,6 @@ static void blkfront_gather_backend_features(struct blkfront_info *info)
 static void blkfront_connect(struct blkfront_info *info)
 {
 	unsigned long long sectors;
-	unsigned long sector_size;
-	unsigned int physical_sector_size;
 	int err, i;
 	struct blkfront_ring_info *rinfo;
 
@@ -2304,7 +2299,7 @@ static void blkfront_connect(struct blkfront_info *info)
 	err = xenbus_gather(XBT_NIL, info->xbdev->otherend,
 			    "sectors", "%llu", &sectors,
 			    "info", "%u", &info->vdisk_info,
-			    "sector-size", "%lu", &sector_size,
+			    "sector-size", "%lu", &info->sector_size,
 			    NULL);
 	if (err) {
 		xenbus_dev_fatal(info->xbdev, err,
@@ -2318,9 +2313,9 @@ static void blkfront_connect(struct blkfront_info *info)
 	 * provide this. Assume physical sector size to be the same as
 	 * sector_size in that case.
 	 */
-	physical_sector_size = xenbus_read_unsigned(info->xbdev->otherend,
+	info->physical_sector_size = xenbus_read_unsigned(info->xbdev->otherend,
 						    "physical-sector-size",
-						    sector_size);
+						    info->sector_size);
 	blkfront_gather_backend_features(info);
 	for_each_rinfo(info, rinfo, i) {
 		err = blkfront_setup_indirect(rinfo);
@@ -2332,8 +2327,7 @@ static void blkfront_connect(struct blkfront_info *info)
 		}
 	}
 
-	err = xlvbd_alloc_gendisk(sectors, info, sector_size,
-				  physical_sector_size);
+	err = xlvbd_alloc_gendisk(sectors, info);
 	if (err) {
 		xenbus_dev_fatal(info->xbdev, err, "xlvbd_add at %s",
 				 info->xbdev->otherend);
