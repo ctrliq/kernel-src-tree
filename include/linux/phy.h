38ff81cb8ef7019577e349e7b4e12d8dd4a8023a
Izabela Bakollari <ibakolla@redhat.com>
2024-06-25 11:48:07 +0200

    net: phy: provide whether link has changed in c37_read_status
    JIRA: https://issues.redhat.com/browse/RHEL-33716
    
    commit 9b1d5e055508393561e26bd1720f4c2639b03b1a
    Author: Christian Marangi <ansuelsmth@gmail.com>
    Date:   Tue Feb 6 18:31:09 2024 +0100
    
        net: phy: provide whether link has changed in c37_read_status
    
        Some PHY driver might require additional regs call after
        genphy_c37_read_status() is called.
    
        Expand genphy_c37_read_status to provide a bool wheather the link has
        changed or not to permit PHY driver to skip additional regs call if
        nothing has changed.
    
        Every user of genphy_c37_read_status() is updated with the new
        additional bool.
    
        Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -1884,7 +1884,7 @@ int genphy_write_mmd_unsupported(struct phy_device *phdev, int devnum,
 
 /* Clause 37 */
 int genphy_c37_config_aneg(struct phy_device *phydev);
-int genphy_c37_read_status(struct phy_device *phydev);
+int genphy_c37_read_status(struct phy_device *phydev, bool *changed);
 
 /* Clause 45 PHY */
 int genphy_c45_restart_aneg(struct phy_device *phydev);
