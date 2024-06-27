8520ae59d8ae8b0e7bc2d7c1daa96f460b7b1812
Benjamin Coddington <bcodding@redhat.com>
2024-06-27 08:14:24 -0400

    NFSv4: Add a parameter to limit the number of retries after NFS4ERR_DELAY
    JIRA: https://issues.redhat.com/browse/RHEL-34875
    
    commit 5b9d31ae1c925bb5f15975e31b31ff5ae3c81f8f
    Author: Trond Myklebust <trond.myklebust@hammerspace.com>
    Date:   Sat Sep 9 12:23:01 2023 -0400
    
        NFSv4: Add a parameter to limit the number of retries after NFS4ERR_DELAY
    
        When using a 'softerr' mount, the NFSv4 client can get stuck waiting
        forever while the server just returns NFS4ERR_DELAY. Among other things,
        this causes the knfsd server threads to busy wait.
        Add a parameter that tells the NFSv4 client how many times to retry
        before giving up.
    
        Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    
    Signed-off-by: Benjamin Coddington <bcodding@redhat.com>

--- fs/nfs/nfs4_fs.h
+++ fs/nfs/nfs4_fs.h
@@ -208,6 +208,7 @@ struct nfs4_exception {
 	struct inode *inode;
 	nfs4_stateid *stateid;
 	long timeout;
+	unsigned short retrans;
 	unsigned char task_is_privileged : 1;
 	unsigned char delay : 1,
 		      recovering : 1,
@@ -545,6 +546,7 @@ extern unsigned short max_session_slots;
 extern unsigned short max_session_cb_slots;
 extern unsigned short send_implementation_id;
 extern bool recover_lost_locks;
+extern short nfs_delay_retrans;
 
 #define NFS4_CLIENT_ID_UNIQ_LEN		(64)
 extern char nfs4_client_id_uniquifier[NFS4_CLIENT_ID_UNIQ_LEN];
