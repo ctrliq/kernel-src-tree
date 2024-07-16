c030ae3d68afe57e1d7069f9ad5fcb12ff8a9279
Antoine Tenart <atenart@redhat.com>
2024-07-16 10:31:27 +0200

    tcp: use drop reasons in cookie check for ipv4
    JIRA: https://issues.redhat.com/browse/RHEL-48648
    Upstream Status: linux.git
    Conflicts:\
    - Context difference due to missing upstream commits 8e7bab6b9652
      ("tcp: Factorise cookie-dependent fields initialisation in
      cookie_v[46]_check()") and 45c28509fee6 ("tcp: Cache sock_net(sk) in
      cookie_v[46]_check().") in c9s.
    
    commit a4a69a3719ec55f677b8481f58215b6338b781e5
    Author: Jason Xing <kernelxing@tencent.com>
    Date:   Mon Feb 26 11:22:20 2024 +0800
    
        tcp: use drop reasons in cookie check for ipv4
    
        Now it's time to use the prepared definitions to refine this part.
        Four reasons used might enough for now, I think.
    
        Signed-off-by: Jason Xing <kernelxing@tencent.com>
        Reviewed-by: Eric Dumazet <edumazet@google.com>
        Reviewed-by: David Ahern <dsahern@kernel.org>
        Reviewed-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv4/syncookies.c
+++ net/ipv4/syncookies.c
@@ -367,8 +367,10 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 
 	req = cookie_tcp_reqsk_alloc(&tcp_request_sock_ops,
 				     &tcp_request_sock_ipv4_ops, sk, skb);
-	if (!req)
+	if (!req) {
+		SKB_DR_SET(reason, NO_SOCKET);
 		goto out_drop;
+	}
 
 	ireq = inet_rsk(req);
 	treq = tcp_rsk(req);
@@ -400,8 +402,10 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 	 */
 	RCU_INIT_POINTER(ireq->ireq_opt, tcp_v4_save_options(sock_net(sk), skb));
 
-	if (security_inet_conn_request(sk, skb, req))
+	if (security_inet_conn_request(sk, skb, req)) {
+		SKB_DR_SET(reason, SECURITY_HOOK);
 		goto out_free;
+	}
 
 	req->num_retrans = 0;
 
@@ -418,8 +422,10 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 			   ireq->ir_loc_addr, th->source, th->dest, sk->sk_uid);
 	security_req_classify_flow(req, flowi4_to_flowi_common(&fl4));
 	rt = ip_route_output_key(sock_net(sk), &fl4);
-	if (IS_ERR(rt))
+	if (IS_ERR(rt)) {
+		SKB_DR_SET(reason, IP_OUTNOROUTES);
 		goto out_free;
+	}
 
 	/* Try to redo what tcp_v4_send_synack did. */
 	req->rsk_window_clamp = tp->window_clamp ? :dst_metric(&rt->dst, RTAX_WINDOW);
@@ -441,10 +447,11 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 	/* ip_queue_xmit() depends on our flow being setup
 	 * Normal sockets get it right from inet_csk_route_child_sock()
 	 */
-	if (ret)
-		inet_sk(ret)->cork.fl.u.ip4 = fl4;
-	else
+	if (!ret) {
+		SKB_DR_SET(reason, NO_SOCKET);
 		goto out_drop;
+	}
+	inet_sk(ret)->cork.fl.u.ip4 = fl4;
 out:
 	return ret;
 out_free:
