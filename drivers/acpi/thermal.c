2410a7a2e6e3de7134dc69bf70006922262b928b
Mark Langsdorf <mlangsdo@redhat.com>
2024-08-13 16:52:15 -0400

    ACPI: thermal: Register thermal zones without valid trip points
    JIRA: https://issues.redhat.com/browse/RHEL-54149
    Conflicts:
           drivers/acpi/thermal.c - in the first stanza, I passed
    tz->trip_table to thermal_zone_device_register_with_trips(),
    along with an additional 0 argument, to match the form of the
    function in the centos-9 codebase.
    	in the second stanza, I used tz->trip_table instead of
    trip_table
    
    commit 8a4ff5452dd0cdcc35940460bb777d836bece11c
    Author: Stephen Horvath <s.horvath@outlook.com.au>
    Date:   Sun Mar 31 18:37:06 2024 +1000
    
    Some laptops where the thermal control is handled by the EC may
    provide trip points that fail the kernels new validation, but still have
    working temperature sensors. An example of this is the Framework 13 AMD.
    
    This patch allows the thermal zone to still be registered without trip
    points if the trip points fail validation, allowing the temperature
    sensor to be viewed and used by the user.
    
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=218586
    Fixes: 9c8647224e9f ("ACPI: thermal: Use library functions to obtain trip point temperature values")
    Signed-off-by: Stephen Horvath <s.horvath@outlook.com.au>
    [ rjw: Subject edits, remove redundant braces ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/acpi/thermal.c
+++ drivers/acpi/thermal.c
@@ -667,14 +667,15 @@ static int acpi_thermal_register_thermal_zone(struct acpi_thermal *tz,
 {
 	int result;
 
-	tz->thermal_zone = thermal_zone_device_register_with_trips("acpitz",
-								   tz->trip_table,
-								   trip_count,
-								   0, tz,
-								   &acpi_thermal_zone_ops,
-								   NULL,
-								   passive_delay,
-								   tz->polling_frequency * 100);
+	if (trip_count)
+		tz->thermal_zone = thermal_zone_device_register_with_trips(
+					"acpitz", tz->trip_table, trip_count, 0, tz,
+					&acpi_thermal_zone_ops, NULL, passive_delay,
+					tz->polling_frequency * 100);
+	else
+		tz->thermal_zone = thermal_tripless_zone_device_register(
+					"acpitz", tz, &acpi_thermal_zone_ops, NULL);
+
 	if (IS_ERR(tz->thermal_zone))
 		return PTR_ERR(tz->thermal_zone);
 
@@ -922,6 +923,9 @@ static int acpi_thermal_add(struct acpi_device *device)
 		trip->priv = acpi_trip;
 		trip++;
 	}
+
+	if (trip == tz->trip_table)
+		pr_warn(FW_BUG "No valid trip points!\n");
 
 	result = acpi_thermal_register_thermal_zone(tz, trip_count, passive_delay);
 	if (result)
