5cd3a105f15eb38c7b5c4fb8a1edb43835919826
Myron Stowe <mstowe@redhat.com>
2024-08-14 19:31:04 -0600

    PCI: dwc: ep: Introduce dw_pcie_ep_cleanup() API for drivers supporting PERST#
    JIRA: https://issues.redhat.com/browse/RHEL-50255
    Upstream Status: 570d7715eed8a29ac5bd96c7694f060a991e5a31
    
    commit 570d7715eed8a29ac5bd96c7694f060a991e5a31
    Author: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Date:   Wed Mar 27 14:43:34 2024 +0530
    
        PCI: dwc: ep: Introduce dw_pcie_ep_cleanup() API for drivers supporting PERST#
    
        For DWC glue drivers supporting PERST# (currently Qcom and Tegra194), some
        of the DWC resources like eDMA should be cleaned up during the PERST#
        assert time.
    
        So let's introduce a dw_pcie_ep_cleanup() API that could be called by these
        drivers to cleanup the DWC specific resources. Currently, it just removes
        eDMA.
    
        Closes: https://lore.kernel.org/linux-pci/ZWYmX8Y%2F7Q9WMxES@x1-carbon
        Link: https://lore.kernel.org/linux-pci/20240327-pci-dbi-rework-v12-5-082625472414@linaro.org
        Reported-by: Niklas Cassel <cassel@kernel.org>
        Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
        Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
        Reviewed-by: Frank Li <Frank.Li@nxp.com>
        Reviewed-by: Niklas Cassel <cassel@kernel.org>
    
    Signed-off-by: Myron Stowe <mstowe@redhat.com>

--- drivers/pci/controller/dwc/pcie-designware.h
+++ drivers/pci/controller/dwc/pcie-designware.h
@@ -659,6 +659,7 @@ int dw_pcie_ep_init(struct dw_pcie_ep *ep);
 int dw_pcie_ep_init_complete(struct dw_pcie_ep *ep);
 void dw_pcie_ep_init_notify(struct dw_pcie_ep *ep);
 void dw_pcie_ep_deinit(struct dw_pcie_ep *ep);
+void dw_pcie_ep_cleanup(struct dw_pcie_ep *ep);
 int dw_pcie_ep_raise_intx_irq(struct dw_pcie_ep *ep, u8 func_no);
 int dw_pcie_ep_raise_msi_irq(struct dw_pcie_ep *ep, u8 func_no,
 			     u8 interrupt_num);
@@ -689,6 +690,10 @@ static inline void dw_pcie_ep_init_notify(struct dw_pcie_ep *ep)
 }
 
 static inline void dw_pcie_ep_deinit(struct dw_pcie_ep *ep)
+{
+}
+
+static inline void dw_pcie_ep_cleanup(struct dw_pcie_ep *ep)
 {
 }
 
