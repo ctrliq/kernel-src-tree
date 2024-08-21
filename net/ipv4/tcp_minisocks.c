e68e0d9b4005ecfd757df9eca694ecafdda58575
Florian Westphal <fwestpha@redhat.com>
2024-08-21 16:56:27 +0200

    net: tcp/dccp: prepare for tw_timer un-pinning
    JIRA: https://issues.redhat.com/browse/RHEL-9279
    Upstream Status: commit b334b924c9b7
    
    Conflicts: net/ipv4/tcp_minisocks.c
    
    We lack a "struct net *net" in this function, earlier
    conflict fixup used sock_net().  Resolve this by keeping
    sock_net() usage as-is.
    
    commit b334b924c9b709bc969644fb5c406f5c9d01dceb
    Author: Valentin Schneider <vschneid@redhat.com>
    Date:   Thu Jun 6 17:11:37 2024 +0200
    
        net: tcp/dccp: prepare for tw_timer un-pinning
    
        The TCP timewait timer is proving to be problematic for setups where
        scheduler CPU isolation is achieved at runtime via cpusets (as opposed to
        statically via isolcpus=domains).
    
        What happens there is a CPU goes through tcp_time_wait(), arming the
        time_wait timer, then gets isolated. TCP_TIMEWAIT_LEN later, the timer
        fires, causing interference for the now-isolated CPU. This is conceptually
        similar to the issue described in commit e02b93124855 ("workqueue: Unbind
        kworkers before sending them to exit()")
    
        Move inet_twsk_schedule() to within inet_twsk_hashdance(), with the ehash
        lock held. Expand the lock's critical section from inet_twsk_kill() to
        inet_twsk_deschedule_put(), serializing the scheduling vs descheduling of
        the timer. IOW, this prevents the following race:
    
                                     tcp_time_wait()
                                       inet_twsk_hashdance()
          inet_twsk_deschedule_put()
            del_timer_sync()
                                       inet_twsk_schedule()
    
        Thanks to Paolo Abeni for suggesting to leverage the ehash lock.
    
        This also restores a comment from commit ec94c2696f0b ("tcp/dccp: avoid
        one atomic operation for timewait hashdance") as inet_twsk_hashdance() had
        a "Step 1" and "Step 3" comment, but the "Step 2" had gone missing.
    
        inet_twsk_deschedule_put() now acquires the ehash spinlock to synchronize
        with inet_twsk_hashdance_schedule().
    
        To ease possible regression search, actual un-pin is done in next patch.
    
        Link: https://lore.kernel.org/all/ZPhpfMjSiHVjQkTk@localhost.localdomain/
        Reviewed-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: Valentin Schneider <vschneid@redhat.com>
        Co-developed-by: Florian Westphal <fw@strlen.de>
        Signed-off-by: Florian Westphal <fw@strlen.de>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>

--- net/ipv4/tcp_minisocks.c
+++ net/ipv4/tcp_minisocks.c
@@ -329,11 +329,10 @@ void tcp_time_wait(struct sock *sk, int state, int timeo)
 		 * we complete the initialization.
 		 */
 		local_bh_disable();
-		inet_twsk_schedule(tw, timeo);
 		/* Linkage updates.
 		 * Note that access to tw after this point is illegal.
 		 */
-		inet_twsk_hashdance(tw, sk, sock_net(sk)->ipv4.tcp_death_row.hashinfo);
+		inet_twsk_hashdance_schedule(tw, sk, sock_net(sk)->ipv4.tcp_death_row.hashinfo, timeo);
 		local_bh_enable();
 	} else {
 		/* Sorry, if we're out of memory, just CLOSE this
