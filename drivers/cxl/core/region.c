7d37d9330fca15bf62ea1f84ddbbdcd9fac7494c
John W. Linville <linville@redhat.com>
2024-09-03 17:11:51 -0700

    cxl: Calculate region bandwidth of targets with shared upstream link
    JIRA: https://issues.redhat.com/browse/RHEL-67033
    
    The current bandwidth calculation aggregates all the targets. This simple
    method does not take into account where multiple targets sharing under
    a switch or a root port where the aggregated bandwidth can be greater than
    the upstream link of the switch.
    
    To accurately account for the shared upstream uplink cases, a new update
    function is introduced by walking from the leaves to the root of the
    hierarchy and clamp the bandwidth in the process as needed. This process
    is done when all the targets for a region are present but before the
    final values are send to the HMAT handling code cached access_coordinate
    targets.
    
    The original perf calculation path was kept to calculate the latency
    performance data that does not require the shared link consideration.
    The shared upstream link calculation is done as a second pass when all
    the endpoints have arrived.
    
    Testing is done via qemu with CXL hierarchy. run_qemu[1] is modified to
    support several CXL hierarchy layouts. The following layouts are tested:
    
    HB: Host Bridge
    RP: Root Port
    SW: Switch
    EP: End Point
    
    2 HB 2 RP 2 EP: resulting bandwidth: 624
    1 HB 2 RP 2 EP: resulting bandwidth: 624
    2 HB 2 RP 2 SW 4 EP: resulting bandwidth: 624
    
    Current testing, perf number from SRAT/HMAT is hacked into the kernel
    code. However with new QEMU support of Generic Target Port that's
    incoming, the perf data injection is no longer needed.
    
    [1]: https://github.com/pmem/run_qemu
    
    Suggested-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Link: https://lore.kernel.org/linux-cxl/20240501152503.00002e60@Huawei.com/
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Acked-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://patch.msgid.link/20240904001316.1688225-3-dave.jiang@intel.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    (cherry picked from commit a5ab0de0ebaa65e0a75ec0761a2745c66a9d17dc)
    Signed-off-by: John W. Linville <linville@redhat.com>

--- drivers/cxl/core/region.c
+++ drivers/cxl/core/region.c
@@ -1989,6 +1989,7 @@ static int cxl_region_attach(struct cxl_region *cxlr,
 		 * then the region is already committed.
 		 */
 		p->state = CXL_CONFIG_COMMIT;
+		cxl_region_shared_upstream_bandwidth_update(cxlr);
 
 		return 0;
 	}
@@ -2010,6 +2011,7 @@ static int cxl_region_attach(struct cxl_region *cxlr,
 		if (rc)
 			return rc;
 		p->state = CXL_CONFIG_ACTIVE;
+		cxl_region_shared_upstream_bandwidth_update(cxlr);
 	}
 
 	cxled->cxld.interleave_ways = p->interleave_ways;
