0a988796554d537ff3333942d8c4956baeab0e2c
Rafael Aquini <raquini@redhat.com>
2024-09-05 20:32:06 -0400

    shmem: minor fixes to splice-read implementation
    JIRA: https://issues.redhat.com/browse/RHEL-27742
    
    This patch is a backport of the following upstream commit:
    commit fa598952fac059054316dccb2213478ccb81a0d1
    Author: Hugh Dickins <hughd@google.com>
    Date:   Sun Jul 23 14:05:54 2023 -0700
    
        shmem: minor fixes to splice-read implementation
    
        HWPoison: my reading of folio_test_hwpoison() is that it only tests the
        head page of a large folio, whereas splice_folio_into_pipe() will splice
        as much of the folio as it can: so for safety we should also check the
        has_hwpoisoned flag, set if any of the folio's pages are hwpoisoned.
        (Perhaps that ugliness can be improved at the mm end later.)
    
        The call to splice_zeropage_into_pipe() risked overrunning past EOF: ask
        it for "part" not "len".
    
        Link: https://lkml.kernel.org/r/32c72c9c-72a8-115f-407d-f0148f368@google.com
        Fixes: bd194b187115 ("shmem: Implement splice-read")
        Signed-off-by: Hugh Dickins <hughd@google.com>
        Reviewed-by: David Howells <dhowells@redhat.com>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: Jens Axboe <axboe@kernel.dk>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2920,7 +2920,8 @@ static ssize_t shmem_file_splice_read(struct file *in, loff_t *ppos,
 		if (*ppos >= i_size_read(inode))
 			break;
 
-		error = shmem_get_folio(inode, *ppos / PAGE_SIZE, &folio, SGP_READ);
+		error = shmem_get_folio(inode, *ppos / PAGE_SIZE, &folio,
+					SGP_READ);
 		if (error) {
 			if (error == -EINVAL)
 				error = 0;
@@ -2929,7 +2930,9 @@ static ssize_t shmem_file_splice_read(struct file *in, loff_t *ppos,
 		if (folio) {
 			folio_unlock(folio);
 
-			if (folio_test_hwpoison(folio)) {
+			if (folio_test_hwpoison(folio) ||
+			    (folio_test_large(folio) &&
+			     folio_test_has_hwpoisoned(folio))) {
 				error = -EIO;
 				break;
 			}
@@ -2965,7 +2968,7 @@ static ssize_t shmem_file_splice_read(struct file *in, loff_t *ppos,
 			folio_put(folio);
 			folio = NULL;
 		} else {
-			n = splice_zeropage_into_pipe(pipe, *ppos, len);
+			n = splice_zeropage_into_pipe(pipe, *ppos, part);
 		}
 
 		if (!n)
