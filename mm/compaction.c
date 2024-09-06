9dca1b8a236c1e1073aa8405b477c83bca2573a8
Rafael Aquini <raquini@redhat.com>
2024-09-05 20:32:03 -0400

    mm: compaction: remove unnecessary is_via_compact_memory() checks
    JIRA: https://issues.redhat.com/browse/RHEL-27742
    
    This patch is a backport of the following upstream commit:
    commit f98a497e1f16ee411df72629e32e31cba4cfa9cf
    Author: Johannes Weiner <hannes@cmpxchg.org>
    Date:   Fri May 19 14:39:58 2023 +0200
    
        mm: compaction: remove unnecessary is_via_compact_memory() checks
    
        Remove from all paths not reachable via /proc/sys/vm/compact_memory.
    
        Link: https://lkml.kernel.org/r/20230519123959.77335-5-hannes@cmpxchg.org
        Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
        Acked-by: Vlastimil Babka <vbabka@suse.cz>
        Cc: Mel Gorman <mgorman@techsingularity.net>
        Cc: Michal Hocko <mhocko@suse.com>
        Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -2313,9 +2313,6 @@ bool compaction_zonelist_suitable(struct alloc_context *ac, int order,
 		unsigned long available;
 		unsigned long watermark;
 
-		if (is_via_compact_memory(order))
-			return true;
-
 		/* Allocation can already succeed, nothing to do */
 		watermark = wmark_pages(zone, alloc_flags & ALLOC_WMARK_MASK);
 		if (zone_watermark_ok(zone, order, watermark,
@@ -2881,9 +2878,6 @@ static bool kcompactd_node_suitable(pg_data_t *pgdat)
 		if (!populated_zone(zone))
 			continue;
 
-		if (is_via_compact_memory(pgdat->kcompactd_max_order))
-			return true;
-
 		/* Allocation can already succeed, check other zones */
 		if (zone_watermark_ok(zone, pgdat->kcompactd_max_order,
 				      min_wmark_pages(zone),
@@ -2928,9 +2922,6 @@ static void kcompactd_do_work(pg_data_t *pgdat)
 		if (compaction_deferred(zone, cc.order))
 			continue;
 
-		if (is_via_compact_memory(cc.order))
-			goto compact;
-
 		/* Allocation can already succeed, nothing to do */
 		if (zone_watermark_ok(zone, cc.order,
 				      min_wmark_pages(zone), zoneid, 0))
@@ -2939,7 +2930,7 @@ static void kcompactd_do_work(pg_data_t *pgdat)
 		if (compaction_suitable(zone, cc.order,
 					zoneid) != COMPACT_CONTINUE)
 			continue;
-compact:
+
 		if (kthread_should_stop())
 			return;
 
