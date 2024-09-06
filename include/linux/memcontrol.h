96c3c057a9323b5f58042e7c0c177f849d892a8d
Rafael Aquini <raquini@redhat.com>
2024-09-05 20:31:47 -0400

    memcg: remove mem_cgroup_flush_stats_atomic()
    JIRA: https://issues.redhat.com/browse/RHEL-27742
    
    This patch is a backport of the following upstream commit:
    commit 35822fdae3bf509532b0954088070f17de51ff15
    Author: Yosry Ahmed <yosryahmed@google.com>
    Date:   Fri Apr 21 17:40:19 2023 +0000
    
        memcg: remove mem_cgroup_flush_stats_atomic()
    
        Previous patches removed all callers of mem_cgroup_flush_stats_atomic().
        Remove the function and simplify the code.
    
        Link: https://lkml.kernel.org/r/20230421174020.2994750-5-yosryahmed@google.com
        Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
        Acked-by: Shakeel Butt <shakeelb@google.com>
        Cc: Alexander Viro <viro@zeniv.linux.org.uk>
        Cc: Christian Brauner <brauner@kernel.org>
        Cc: Jan Kara <jack@suse.cz>
        Cc: Jens Axboe <axboe@kernel.dk>
        Cc: Johannes Weiner <hannes@cmpxchg.org>
        Cc: Michal Hocko <mhocko@kernel.org>
        Cc: Michal Koutný <mkoutny@suse.com>
        Cc: Muchun Song <songmuchun@bytedance.com>
        Cc: Roman Gushchin <roman.gushchin@linux.dev>
        Cc: Tejun Heo <tj@kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1078,7 +1078,6 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 }
 
 void mem_cgroup_flush_stats(void);
-void mem_cgroup_flush_stats_atomic(void);
 void mem_cgroup_flush_stats_ratelimited(void);
 
 void __mod_memcg_lruvec_state(struct lruvec *lruvec, enum node_stat_item idx,
@@ -1584,10 +1583,6 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 }
 
 static inline void mem_cgroup_flush_stats(void)
-{
-}
-
-static inline void mem_cgroup_flush_stats_atomic(void)
 {
 }
 
