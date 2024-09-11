41555d069029c470d30892fa1138351cdc05ff65
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:37:59 -0400

    selftests: bpf: Enable UDP sockmap reuseport tests
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Lorenz Bauer <lmb@cloudflare.com>
    commit 1f441b35ea5453e1dcc00fac03dbd5d7e6cd4f97
    
    Remove the guard that disables UDP tests now that sockmap
    has support for them.
    
    	Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    	Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    	Reviewed-by: Jakub Sitnicki <jakub@cloudflare.com>
    	Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/20200309111243.6982-12-lmb@cloudflare.com
    (cherry picked from commit 1f441b35ea5453e1dcc00fac03dbd5d7e6cd4f97)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/testing/selftests/bpf/prog_tests/select_reuseport.c
+++ tools/testing/selftests/bpf/prog_tests/select_reuseport.c
@@ -807,12 +807,6 @@ static void test_config(int sotype, sa_family_t family, bool inany)
 	char s[MAX_TEST_NAME];
 	const struct test *t;
 
-	/* SOCKMAP/SOCKHASH don't support UDP yet */
-	if (sotype == SOCK_DGRAM &&
-	    (inner_map_type == BPF_MAP_TYPE_SOCKMAP ||
-	     inner_map_type == BPF_MAP_TYPE_SOCKHASH))
-		return;
-
 	for (t = tests; t < tests + ARRAY_SIZE(tests); t++) {
 		snprintf(s, sizeof(s), "%s %s/%s %s %s",
 			 maptype_str(inner_map_type),
