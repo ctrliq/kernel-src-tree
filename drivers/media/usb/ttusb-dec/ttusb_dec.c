0852069a5def05e6ee5ec83c05133c552bd7768d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:21:40 -0400

    media: media/usb:Remove superfluous "breaks" in the ttusb_dec.c
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author dingsenjie <dingsenjie@yulong.com>
    commit c3bf67e80532829b886795a743c86f89b18e51d3
    
    Remove superfluous "breaks", as there is a "return" before them.
    
    	Signed-off-by: dingsenjie <dingsenjie@yulong.com>
    	Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    	Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    (cherry picked from commit c3bf67e80532829b886795a743c86f89b18e51d3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/media/usb/ttusb-dec/ttusb_dec.c
+++ drivers/media/usb/ttusb-dec/ttusb_dec.c
@@ -1101,11 +1101,9 @@ static int ttusb_dec_start_feed(struct dvb_demux_feed *dvbdmxfeed)
 
 	case DMX_TYPE_TS:
 		return ttusb_dec_start_ts_feed(dvbdmxfeed);
-		break;
 
 	case DMX_TYPE_SEC:
 		return ttusb_dec_start_sec_feed(dvbdmxfeed);
-		break;
 
 	default:
 		dprintk("  type: unknown (%d)\n", dvbdmxfeed->type);
@@ -1156,11 +1154,9 @@ static int ttusb_dec_stop_feed(struct dvb_demux_feed *dvbdmxfeed)
 	switch (dvbdmxfeed->type) {
 	case DMX_TYPE_TS:
 		return ttusb_dec_stop_ts_feed(dvbdmxfeed);
-		break;
 
 	case DMX_TYPE_SEC:
 		return ttusb_dec_stop_sec_feed(dvbdmxfeed);
-		break;
 	}
 
 	return 0;
