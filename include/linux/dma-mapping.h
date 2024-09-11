dbae5e7b4c7a587eff4f20f351a7768921eee90f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:17:15 -0400

    dma-mapping: remove a pointless empty line in dma_alloc_coherent
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Wang Qing <wangqing@vivo.com>
    commit 84fcfbdadbfdd86c9a43a52703203e05fe7efd92
    
    	Signed-off-by: Wang Qing <wangqing@vivo.com>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 84fcfbdadbfdd86c9a43a52703203e05fe7efd92)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -615,7 +615,6 @@ dma_common_get_sgtable(struct device *dev, struct sg_table *sgt, void *cpu_addr,
 static inline void *dma_alloc_coherent(struct device *dev, size_t size,
 		dma_addr_t *dma_handle, gfp_t gfp)
 {
-
 	return dma_alloc_attrs(dev, size, dma_handle, gfp,
 			(gfp & __GFP_NOWARN) ? DMA_ATTR_NO_WARN : 0);
 }
