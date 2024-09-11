74ff4996f179e32c8bf9743c8cd36eac443ab2ed
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:10:30 -0400

    net: stmmac: Use rtnl_lock/unlock on netif_set_real_num_rx_queues() call
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Wong Vee Khee <vee.khee.wong@intel.com>
    commit 8e5debed39017836a850c6c7bfacc93299d19bad
    
    Fix an issue where dump stack is printed on suspend resume flow due to
    netif_set_real_num_rx_queues() is not called with rtnl_lock held().
    
    Fixes: 686cff3d7022 ("net: stmmac: Fix incorrect location to set real_num_rx|tx_queues")
    	Reported-by: Christophe ROULLIER <christophe.roullier@st.com>
    	Tested-by: Christophe ROULLIER <christophe.roullier@st.com>
    	Cc: Alexandre TORGUE <alexandre.torgue@st.com>
    	Reviewed-by: Ong Boon Leong <boon.leong.ong@intel.com>
    	Signed-off-by: Wong Vee Khee <vee.khee.wong@intel.com>
    Link: https://lore.kernel.org/r/20201115074210.23605-1-vee.khee.wong@intel.com
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 8e5debed39017836a850c6c7bfacc93299d19bad)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -4995,6 +4995,7 @@ int stmmac_resume(struct device *dev)
 
 	netif_device_attach(ndev);
 
+	rtnl_lock();
 	mutex_lock(&priv->lock);
 
 	stmmac_reset_queues_param(priv);
@@ -5008,6 +5009,7 @@ int stmmac_resume(struct device *dev)
 	stmmac_enable_all_queues(priv);
 
 	mutex_unlock(&priv->lock);
+	rtnl_unlock();
 
 	if (!device_may_wakeup(priv->device) || !priv->plat->pmt) {
 		rtnl_lock();
