3b7ebafacd72c0d89ab9cdb93fd89899bbe26e8d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:23:54 -0400

    net: stmmac: update pci platform data to use phy_interface
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-331.el8
    commit-author Voon Weifeng <weifeng.voon@intel.com>
    commit 909c1dde67c433f1e4122f2619cbd8ac370fcf0a
    
    The recent patch to support passive mode converter did not take care the
    phy interface configuration in PCI platform data. Hence, converting all
    the PCI platform data from plat->interface to plat->phy_interface as the
    default mode is meant for PHY.
    
    Fixes: 0060c8783330 ("net: stmmac: implement support for passive mode converters via dt")
    	Signed-off-by: Voon Weifeng <weifeng.voon@intel.com>
    	Tested-by: Tan, Tee Min <tee.min.tan@intel.com>
    	Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 909c1dde67c433f1e4122f2619cbd8ac370fcf0a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
@@ -106,7 +106,7 @@ static int stmmac_default_data(struct pci_dev *pdev,
 
 	plat->bus_id = 1;
 	plat->phy_addr = 0;
-	plat->interface = PHY_INTERFACE_MODE_GMII;
+	plat->phy_interface = PHY_INTERFACE_MODE_GMII;
 
 	plat->dma_cfg->pbl = 32;
 	plat->dma_cfg->pblx8 = true;
@@ -230,7 +230,8 @@ static int ehl_sgmii_data(struct pci_dev *pdev,
 {
 	plat->bus_id = 1;
 	plat->phy_addr = 0;
-	plat->interface = PHY_INTERFACE_MODE_SGMII;
+	plat->phy_interface = PHY_INTERFACE_MODE_SGMII;
+
 	return ehl_common_data(pdev, plat);
 }
 
@@ -243,7 +244,8 @@ static int ehl_rgmii_data(struct pci_dev *pdev,
 {
 	plat->bus_id = 1;
 	plat->phy_addr = 0;
-	plat->interface = PHY_INTERFACE_MODE_RGMII;
+	plat->phy_interface = PHY_INTERFACE_MODE_RGMII;
+
 	return ehl_common_data(pdev, plat);
 }
 
@@ -271,7 +273,7 @@ static int tgl_sgmii_data(struct pci_dev *pdev,
 {
 	plat->bus_id = 1;
 	plat->phy_addr = 0;
-	plat->interface = PHY_INTERFACE_MODE_SGMII;
+	plat->phy_interface = PHY_INTERFACE_MODE_SGMII;
 	return tgl_common_data(pdev, plat);
 }
 
@@ -371,7 +373,7 @@ static int quark_default_data(struct pci_dev *pdev,
 
 	plat->bus_id = pci_dev_id(pdev);
 	plat->phy_addr = ret;
-	plat->interface = PHY_INTERFACE_MODE_RMII;
+	plat->phy_interface = PHY_INTERFACE_MODE_RMII;
 
 	plat->dma_cfg->pbl = 16;
 	plat->dma_cfg->pblx8 = true;
@@ -430,7 +432,7 @@ static int snps_gmac5_default_data(struct pci_dev *pdev,
 
 	plat->bus_id = 1;
 	plat->phy_addr = -1;
-	plat->interface = PHY_INTERFACE_MODE_GMII;
+	plat->phy_interface = PHY_INTERFACE_MODE_GMII;
 
 	plat->dma_cfg->pbl = 32;
 	plat->dma_cfg->pblx8 = true;
