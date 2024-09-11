ed7de5e8b201c1ff9c879c875e38db95d569c7de
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:28:17 -0400

    vdpa/mlx5: Add support for running with virtio_vdpa
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-331.el8
    commit-author Eli Cohen <elic@nvidia.com>
    commit 6f5312f801836e6af9bcbb0bdb44dc423e129206
    
    In order to support running vdpa using vritio_vdpa driver, we need  to
    create a different kind of MR, one that has 1:1 mapping, since the
    addresses referring to virtqueues are dma addresses.
    
    We create the 1:1 MR in mlx5_vdpa_dev_add() only in case firmware
    supports the general capability umem_uid_0. The reason for that is that
    1:1 MRs must be created with uid == 0 while virtqueue objects can be
    created with uid == 0 only when the firmware capability is on.
    
    If the set_map() callback is called with new translations provided
    through iotlb, the driver will destroy the 1:1 MR and create a regular
    one.
    
    	Signed-off-by: Eli Cohen <elic@nvidia.com>
    Link: https://lore.kernel.org/r/20210602085854.62690-1-elic@nvidia.com
    	Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    	Acked-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit 6f5312f801836e6af9bcbb0bdb44dc423e129206)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/vdpa/mlx5/net/mlx5_vnet.c
+++ drivers/vdpa/mlx5/net/mlx5_vnet.c
@@ -1777,6 +1777,10 @@ static void mlx5_vdpa_set_status(struct vdpa_device *vdev, u8 status)
 		ndev->mvdev.status = 0;
 		ndev->mvdev.mlx_features = 0;
 		++mvdev->generation;
+		if (MLX5_CAP_GEN(mvdev->mdev, umem_uid_0)) {
+			if (mlx5_vdpa_create_mr(mvdev, NULL))
+				mlx5_vdpa_warn(mvdev, "create MR failed\n");
+		}
 		return;
 	}
 
@@ -1857,6 +1861,7 @@ static void mlx5_vdpa_free(struct vdpa_device *vdev)
 	ndev = to_mlx5_vdpa_ndev(mvdev);
 
 	free_resources(ndev);
+	mlx5_vdpa_destroy_mr(mvdev);
 	if (!is_zero_ether_addr(ndev->config.mac)) {
 		pfmdev = pci_get_drvdata(pci_physfn(mvdev->mdev->pdev));
 		mlx5_mpfs_del_mac(pfmdev, ndev->config.mac);
@@ -2024,9 +2029,15 @@ static int mlx5v_probe(struct auxiliary_device *adev,
 	if (err)
 		goto err_mpfs;
 
+	if (MLX5_CAP_GEN(mvdev->mdev, umem_uid_0)) {
+		err = mlx5_vdpa_create_mr(mvdev, NULL);
+		if (err)
+			goto err_res;
+	}
+
 	err = alloc_resources(ndev);
 	if (err)
-		goto err_res;
+		goto err_mr;
 
 	err = vdpa_register_device(&mvdev->vdev);
 	if (err)
@@ -2037,6 +2048,8 @@ static int mlx5v_probe(struct auxiliary_device *adev,
 
 err_reg:
 	free_resources(ndev);
+err_mr:
+	mlx5_vdpa_destroy_mr(mvdev);
 err_res:
 	mlx5_vdpa_free_resources(&ndev->mvdev);
 err_mpfs:
