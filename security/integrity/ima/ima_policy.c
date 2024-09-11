e2ab367ae2842f1fe1c468a8f05701b1d23664ba
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:19:55 -0400

    ima: Add modsig appraise_type option for module-style appended signatures
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Thiago Jung Bauermann <bauerman@linux.ibm.com>
    commit 9044d627fd18f9fca49b62d4619ee14914b91464
    
    Introduce the modsig keyword to the IMA policy syntax to specify that
    a given hook should expect the file to have the IMA signature appended
    to it. Here is how it can be used in a rule:
    
    appraise func=KEXEC_KERNEL_CHECK appraise_type=imasig|modsig
    
    With this rule, IMA will accept either a signature stored in the extended
    attribute or an appended signature.
    
    For now, the rule above will behave exactly the same as if
    appraise_type=imasig was specified. The actual modsig implementation
    will be introduced separately.
    
    	Suggested-by: Mimi Zohar <zohar@linux.ibm.com>
    	Signed-off-by: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    	Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>
    (cherry picked from commit 9044d627fd18f9fca49b62d4619ee14914b91464)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- security/integrity/ima/ima_policy.c
+++ security/integrity/ima/ima_policy.c
@@ -1052,6 +1052,10 @@ static int ima_parse_rule(char *rule, struct ima_rule_entry *entry)
 			ima_log_string(ab, "appraise_type", args[0].from);
 			if ((strcmp(args[0].from, "imasig")) == 0)
 				entry->flags |= IMA_DIGSIG_REQUIRED;
+			else if (ima_hook_supports_modsig(entry->func) &&
+				 strcmp(args[0].from, "imasig|modsig") == 0)
+				entry->flags |= IMA_DIGSIG_REQUIRED |
+						IMA_MODSIG_ALLOWED;
 			else
 				result = -EINVAL;
 			break;
@@ -1371,8 +1375,12 @@ int ima_policy_show(struct seq_file *m, void *v)
 	}
 	if (entry->template)
 		seq_printf(m, "template=%s ", entry->template->name);
-	if (entry->flags & IMA_DIGSIG_REQUIRED)
-		seq_puts(m, "appraise_type=imasig ");
+	if (entry->flags & IMA_DIGSIG_REQUIRED) {
+		if (entry->flags & IMA_MODSIG_ALLOWED)
+			seq_puts(m, "appraise_type=imasig|modsig ");
+		else
+			seq_puts(m, "appraise_type=imasig ");
+	}
 	if (entry->flags & IMA_PERMIT_DIRECTIO)
 		seq_puts(m, "permit_directio ");
 	rcu_read_unlock();
