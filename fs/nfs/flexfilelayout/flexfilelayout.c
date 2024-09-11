01e1e50930b4f6c6a1e64038c9cfc00d3466c3e1
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:07:24 -0400

    pNFS/flexfiles: Record resend attempts on I/O failure
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Trond Myklebust <trondmy@gmail.com>
    commit 0722dc9fea58e8199577963bb7601ff9f3e00b0a
    
    If the attempt to do pNFS fails, then record what action we
    take to recover (resend, reset to pnfs or reset to mds).
    
    	Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    	Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    (cherry picked from commit 0722dc9fea58e8199577963bb7601ff9f3e00b0a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/nfs/flexfilelayout/flexfilelayout.c
+++ fs/nfs/flexfilelayout/flexfilelayout.c
@@ -1309,7 +1309,6 @@ static int ff_layout_read_done_cb(struct rpc_task *task,
 	int new_idx = hdr->pgio_mirror_idx;
 	int err;
 
-	trace_nfs4_pnfs_read(hdr, task->tk_status);
 	if (task->tk_status < 0)
 		ff_layout_io_track_ds_error(hdr->lseg, hdr->pgio_mirror_idx,
 					    hdr->args.offset, hdr->args.count,
@@ -1319,6 +1318,7 @@ static int ff_layout_read_done_cb(struct rpc_task *task,
 					   hdr->ds_clp, hdr->lseg,
 					   hdr->pgio_mirror_idx);
 
+	trace_nfs4_pnfs_read(hdr, err);
 	clear_bit(NFS_IOHDR_RESEND_PNFS, &hdr->flags);
 	clear_bit(NFS_IOHDR_RESEND_MDS, &hdr->flags);
 	switch (err) {
@@ -1482,7 +1482,6 @@ static int ff_layout_write_done_cb(struct rpc_task *task,
 	loff_t end_offs = 0;
 	int err;
 
-	trace_nfs4_pnfs_write(hdr, task->tk_status);
 	if (task->tk_status < 0)
 		ff_layout_io_track_ds_error(hdr->lseg, hdr->pgio_mirror_idx,
 					    hdr->args.offset, hdr->args.count,
@@ -1492,6 +1491,7 @@ static int ff_layout_write_done_cb(struct rpc_task *task,
 					   hdr->ds_clp, hdr->lseg,
 					   hdr->pgio_mirror_idx);
 
+	trace_nfs4_pnfs_write(hdr, err);
 	clear_bit(NFS_IOHDR_RESEND_PNFS, &hdr->flags);
 	clear_bit(NFS_IOHDR_RESEND_MDS, &hdr->flags);
 	switch (err) {
@@ -1525,7 +1525,6 @@ static int ff_layout_commit_done_cb(struct rpc_task *task,
 {
 	int err;
 
-	trace_nfs4_pnfs_commit_ds(data, task->tk_status);
 	if (task->tk_status < 0)
 		ff_layout_io_track_ds_error(data->lseg, data->ds_commit_index,
 					    data->args.offset, data->args.count,
@@ -1534,6 +1533,7 @@ static int ff_layout_commit_done_cb(struct rpc_task *task,
 	err = ff_layout_async_handle_error(task, NULL, data->ds_clp,
 					   data->lseg, data->ds_commit_index);
 
+	trace_nfs4_pnfs_commit_ds(data, err);
 	switch (err) {
 	case -NFS4ERR_RESET_TO_PNFS:
 		pnfs_generic_prepare_to_resend_writes(data);
