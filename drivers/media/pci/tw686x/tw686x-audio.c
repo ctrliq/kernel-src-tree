016c8cc0e234fcfad5992b152849db738b0cdf28
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:12:34 -0400

    media: tw686x: audio: Avoid non-standard macro usage
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    Rebuild_CHGLOG: - [sound] ALSA: media: tw686x: audio: Avoid non-standard macro usage (Jaroslav Kysela) [1797509]
    Rebuild_FUZZ: 94.55%
    commit-author Takashi Iwai <tiwai@suse.de>
    commit 3b316a8f6077a0003331aae43f02279ea62d54f9
    
    Pass the device pointer from the PCI pointer directly, instead of a
    non-standard macro.  The macro didn't give any better readability.
    
    	Acked-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 3b316a8f6077a0003331aae43f02279ea62d54f9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/media/pci/tw686x/tw686x-audio.c
+++ drivers/media/pci/tw686x/tw686x-audio.c
@@ -303,7 +303,7 @@ static int tw686x_snd_pcm_init(struct tw686x_dev *dev)
 
 	return snd_pcm_lib_preallocate_pages_for_all(pcm,
 				SNDRV_DMA_TYPE_DEV,
-				snd_dma_pci_data(dev->pci_dev),
+				&dev->pci_dev->dev,
 				TW686X_AUDIO_PAGE_MAX * AUDIO_DMA_SIZE_MAX,
 				TW686X_AUDIO_PAGE_MAX * AUDIO_DMA_SIZE_MAX);
 }
