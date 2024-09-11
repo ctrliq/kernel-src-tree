a201c904a03f48ec15b836085c78d24023776587
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:57:45 -0400

    bpf: Remove insn_buf[] declaration in inner block
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    Rebuild_CHGLOG: - bpf: Remove insn_buf[] declaration in inner block (Yauheni Kaliuta) [1944799]
    Rebuild_FUZZ: 96.84%
    commit-author Jianlin Lv <Jianlin.Lv@arm.com>
    commit 9ef05281e5d01dffdf75a4ae5c1aee7cbd7aaa73
    
    Two insn_buf[16] variables are declared in the function which acts on
    function scope and block scope respectively. The statement in the inner
    block is redundant, so remove it.
    
    	Signed-off-by: Jianlin Lv <Jianlin.Lv@arm.com>
    	Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20210318024851.49693-1-Jianlin.Lv@arm.com
    (cherry picked from commit 9ef05281e5d01dffdf75a4ae5c1aee7cbd7aaa73)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -11805,7 +11805,6 @@ static int do_misc_fixups(struct bpf_verifier_env *env)
 		    insn->code == (BPF_ALU64 | BPF_SUB | BPF_X)) {
 			const u8 code_add = BPF_ALU64 | BPF_ADD | BPF_X;
 			const u8 code_sub = BPF_ALU64 | BPF_SUB | BPF_X;
-			struct bpf_insn insn_buf[16];
 			struct bpf_insn *patch = &insn_buf[0];
 			bool issrc, isneg, isimm;
 			u32 off_reg;
