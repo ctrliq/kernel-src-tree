c6d613c44421a49811ef6fd4b3e74e5844859040
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:31:33 -0400

    ALSA: firewire-tascam: minor code refactoring to finish streaming session
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Takashi Sakamoto <o-takashi@sakamocchi.jp>
    commit ece92831f0fa49f6b7bab0843c096293eab1b113
    
    The operation to finish packet streaming corresponds to stopping
    isochronous contexts.
    
    This commit applies code refactoring to move codes to stop into
    a helper function to finish the session.
    
    	Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit ece92831f0fa49f6b7bab0843c096293eab1b113)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/firewire/tascam/tascam-stream.c
+++ sound/firewire/tascam/tascam-stream.c
@@ -180,6 +180,9 @@ static void finish_session(struct snd_tscm *tscm)
 {
 	__be32 reg;
 
+	amdtp_stream_stop(&tscm->rx_stream);
+	amdtp_stream_stop(&tscm->tx_stream);
+
 	reg = 0;
 	snd_fw_transaction(tscm->unit, TCODE_WRITE_QUADLET_REQUEST,
 			   TSCM_ADDR_BASE + TSCM_OFFSET_START_STREAMING,
@@ -353,9 +356,6 @@ int snd_tscm_stream_reserve_duplex(struct snd_tscm *tscm, unsigned int rate)
 		return err;
 
 	if (tscm->substreams_counter == 0 || rate != curr_rate) {
-		amdtp_stream_stop(&tscm->rx_stream);
-		amdtp_stream_stop(&tscm->tx_stream);
-
 		finish_session(tscm);
 
 		fw_iso_resources_free(&tscm->tx_resources);
@@ -396,12 +396,8 @@ int snd_tscm_stream_start_duplex(struct snd_tscm *tscm, unsigned int rate)
 		return 0;
 
 	if (amdtp_streaming_error(&tscm->rx_stream) ||
-	    amdtp_streaming_error(&tscm->tx_stream)) {
-		amdtp_stream_stop(&tscm->rx_stream);
-		amdtp_stream_stop(&tscm->tx_stream);
-
+	    amdtp_streaming_error(&tscm->tx_stream))
 		finish_session(tscm);
-	}
 
 	if (generation != fw_parent_device(tscm->unit)->card->generation) {
 		err = fw_iso_resources_update(&tscm->tx_resources);
@@ -451,9 +447,6 @@ int snd_tscm_stream_start_duplex(struct snd_tscm *tscm, unsigned int rate)
 
 	return 0;
 error:
-	amdtp_stream_stop(&tscm->rx_stream);
-	amdtp_stream_stop(&tscm->tx_stream);
-
 	finish_session(tscm);
 
 	return err;
@@ -461,12 +454,8 @@ error:
 
 void snd_tscm_stream_stop_duplex(struct snd_tscm *tscm)
 {
-	if (tscm->substreams_counter == 0) {
-		amdtp_stream_stop(&tscm->tx_stream);
-		amdtp_stream_stop(&tscm->rx_stream);
-
+	if (tscm->substreams_counter == 0)
 		finish_session(tscm);
-	}
 }
 
 void snd_tscm_stream_lock_changed(struct snd_tscm *tscm)
