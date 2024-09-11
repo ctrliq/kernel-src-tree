cf6e8a51c90b37e09a04e107c08e06023bfa4909
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:31:03 -0400

    i40e: Log error for oversized MTU on device
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-338.el8
    commit-author Eryk Rybak <eryk.roch.rybak@intel.com>
    commit 613142b0bb8840016c7a41b3b681f6130cc49c86
    
    When attempting to link XDP prog with MTU larger than supported,
    user is not informed why XDP linking fails. Adding proper
    error message: "MTU too large to enable XDP".
    
    	Signed-off-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    	Signed-off-by: Eryk Rybak <eryk.roch.rybak@intel.com>
    	Tested-by: Kiran Bhandare <kiranx.bhandare@intel.com>
    	Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit 613142b0bb8840016c7a41b3b681f6130cc49c86)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -12638,9 +12638,10 @@ out_err:
  * i40e_xdp_setup - add/remove an XDP program
  * @vsi: VSI to changed
  * @prog: XDP program
+ * @extack: netlink extended ack
  **/
-static int i40e_xdp_setup(struct i40e_vsi *vsi,
-			  struct bpf_prog *prog)
+static int i40e_xdp_setup(struct i40e_vsi *vsi, struct bpf_prog *prog,
+			  struct netlink_ext_ack *extack)
 {
 	int frame_size = vsi->netdev->mtu + ETH_HLEN + ETH_FCS_LEN + VLAN_HLEN;
 	struct i40e_pf *pf = vsi->back;
@@ -12649,8 +12650,10 @@ static int i40e_xdp_setup(struct i40e_vsi *vsi,
 	int i;
 
 	/* Don't allow frames that span over multiple buffers */
-	if (frame_size > vsi->rx_buf_len)
+	if (frame_size > vsi->rx_buf_len) {
+		NL_SET_ERR_MSG_MOD(extack, "MTU too large to enable XDP");
 		return -EINVAL;
+	}
 
 	if (!i40e_enabled_xdp_vsi(vsi) && !prog)
 		return 0;
@@ -12959,7 +12962,7 @@ static int i40e_xdp(struct net_device *dev,
 
 	switch (xdp->command) {
 	case XDP_SETUP_PROG:
-		return i40e_xdp_setup(vsi, xdp->prog);
+		return i40e_xdp_setup(vsi, xdp->prog, xdp->extack);
 	case XDP_SETUP_XSK_POOL:
 		return i40e_xsk_pool_setup(vsi, xdp->xsk.pool,
 					   xdp->xsk.queue_id);
