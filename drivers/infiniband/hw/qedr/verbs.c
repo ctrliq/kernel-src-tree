886593c92466f7dd49acc20c20a5aa3bf6c60d1a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:08:14 -0400

    RDMA/qedr: Improve error logs for rdma_alloc_tid error return
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Prabhakar Kushwaha <pkushwaha@marvell.com>
    commit 0050a57638ca4d681ff92bee55246bf64a6afe54
    
    Use -EINVAL return type to identify whether error is returned because of
    "Out of MR resources" or any other error types.
    
    Link: https://lore.kernel.org/r/20210729151732.30995-2-pkushwaha@marvell.com
    	Signed-off-by: Shai Malin <smalin@marvell.com>
    	Signed-off-by: Ariel Elior <aelior@marvell.com>
    	Signed-off-by: Prabhakar Kushwaha <pkushwaha@marvell.com>
    	Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    (cherry picked from commit 0050a57638ca4d681ff92bee55246bf64a6afe54)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/hw/qedr/verbs.c
+++ drivers/infiniband/hw/qedr/verbs.c
@@ -2999,7 +2999,11 @@ struct ib_mr *qedr_reg_user_mr(struct ib_pd *ibpd, u64 start, u64 len,
 
 	rc = dev->ops->rdma_alloc_tid(dev->rdma_ctx, &mr->hw_mr.itid);
 	if (rc) {
-		DP_ERR(dev, "roce alloc tid returned an error %d\n", rc);
+		if (rc == -EINVAL)
+			DP_ERR(dev, "Out of MR resources\n");
+		else
+			DP_ERR(dev, "roce alloc tid returned error %d\n", rc);
+
 		goto err1;
 	}
 
@@ -3094,7 +3098,11 @@ static struct qedr_mr *__qedr_alloc_mr(struct ib_pd *ibpd,
 
 	rc = dev->ops->rdma_alloc_tid(dev->rdma_ctx, &mr->hw_mr.itid);
 	if (rc) {
-		DP_ERR(dev, "roce alloc tid returned an error %d\n", rc);
+		if (rc == -EINVAL)
+			DP_ERR(dev, "Out of MR resources\n");
+		else
+			DP_ERR(dev, "roce alloc tid returned error %d\n", rc);
+
 		goto err0;
 	}
 
@@ -3224,7 +3232,11 @@ struct ib_mr *qedr_get_dma_mr(struct ib_pd *ibpd, int acc)
 
 	rc = dev->ops->rdma_alloc_tid(dev->rdma_ctx, &mr->hw_mr.itid);
 	if (rc) {
-		DP_ERR(dev, "roce alloc tid returned an error %d\n", rc);
+		if (rc == -EINVAL)
+			DP_ERR(dev, "Out of MR resources\n");
+		else
+			DP_ERR(dev, "roce alloc tid returned error %d\n", rc);
+
 		goto err1;
 	}
 
