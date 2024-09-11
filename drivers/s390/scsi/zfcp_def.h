cf638d00b6ef9442d036b9e97db64a1cfedf5b84
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:47:47 -0400

    scsi: zfcp: Fix indentation coding style issue
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Yevhen Viktorov <yevhen.viktorov@virginmedia.com>
    commit 8824db894dd1da48bad363612577ef410a5d1828
    
    Code indentation should use tabs where possible.
    
    Link: https://lore.kernel.org/r/e8a15a2f3d64e2e76a214647cfd4fe23d370b165.1618417667.git.bblock@linux.ibm.com
    	Signed-off-by: Yevhen Viktorov <yevhen.viktorov@virginmedia.com>
    	Signed-off-by: Steffen Maier <maier@linux.ibm.com>
    	Signed-off-by: Benjamin Block <bblock@linux.ibm.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 8824db894dd1da48bad363612577ef410a5d1828)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/s390/scsi/zfcp_def.h
+++ drivers/s390/scsi/zfcp_def.h
@@ -157,7 +157,7 @@ struct zfcp_adapter {
 	u32			fsf_lic_version;
 	u32			adapter_features;  /* FCP channel features */
 	u32			connection_features; /* host connection features */
-        u32			hardware_version;  /* of FCP channel */
+	u32			hardware_version;  /* of FCP channel */
 	u32			fc_security_algorithms; /* of FCP channel */
 	u32			fc_security_algorithms_old; /* of FCP channel */
 	u16			timer_ticks;       /* time int for a tick */
@@ -181,7 +181,7 @@ struct zfcp_adapter {
 	rwlock_t		erp_lock;
 	wait_queue_head_t	erp_done_wqh;
 	struct zfcp_erp_action	erp_action;	   /* pending error recovery */
-        atomic_t                erp_counter;
+	atomic_t		erp_counter;
 	u32			erp_total_count;   /* total nr of enqueued erp
 						      actions */
 	u32			erp_low_mem_count; /* nr of erp actions waiting
@@ -218,7 +218,7 @@ struct zfcp_port {
 	u32		       d_id;	       /* D_ID */
 	u32		       handle;	       /* handle assigned by FSF */
 	struct zfcp_erp_action erp_action;     /* pending error recovery */
-        atomic_t               erp_counter;
+	atomic_t	       erp_counter;
 	u32                    maxframe_size;
 	u32                    supported_classes;
 	u32                    connection_info;
