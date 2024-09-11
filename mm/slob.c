0ad06c7f9c3fa0a6318eff8aa88c975a7f54d907
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:45:01 -0400

    mm/sl[uo]b: export __kmalloc_track(_node)_caller
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    Rebuild_CHGLOG: - [mm] mm/slb: export __kmalloc_track(_node)_caller (Chris von Recklinghausen) [1885412]
    Rebuild_FUZZ: 95.65%
    commit-author Daniel Vetter <daniel.vetter@ffwll.ch>
    commit fd7cb5753ef49964ea9db5121c3fc9a4ec21ed8e
    
    slab does this already, and I want to use this in a memory allocation
    tracker in drm for stuff that's tied to the lifetime of a drm_device,
    not the underlying struct device. Kinda like devres, but for drm.
    
    	Acked-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    	Cc: Christoph Lameter <cl@linux.com>
    	Cc: Pekka Enberg <penberg@kernel.org>
    	Cc: David Rientjes <rientjes@google.com>
    	Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    	Cc: Andrew Morton <akpm@linux-foundation.org>
    	Cc: linux-mm@kvack.org
    Link: https://patchwork.freedesktop.org/patch/msgid/20200323144950.3018436-2-daniel.vetter@ffwll.ch
    (cherry picked from commit fd7cb5753ef49964ea9db5121c3fc9a4ec21ed8e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/slob.c
+++ mm/slob.c
@@ -517,6 +517,7 @@ void *__kmalloc_track_caller(size_t size, gfp_t gfp, unsigned long caller)
 {
 	return __do_kmalloc_node(size, gfp, NUMA_NO_NODE, caller);
 }
+EXPORT_SYMBOL(__kmalloc_track_caller);
 
 #ifdef CONFIG_NUMA
 void *__kmalloc_node_track_caller(size_t size, gfp_t gfp,
@@ -524,6 +525,7 @@ void *__kmalloc_node_track_caller(size_t size, gfp_t gfp,
 {
 	return __do_kmalloc_node(size, gfp, node, caller);
 }
+EXPORT_SYMBOL(__kmalloc_node_track_caller);
 #endif
 
 void kfree(const void *block)
