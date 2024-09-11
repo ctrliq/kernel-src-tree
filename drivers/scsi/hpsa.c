99fa1a06c00dabab6885dc3d3cfc03114f3a1c0a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:43:48 -0400

    scsi: hpsa: mark expected switch fall-throughs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Gustavo A. R. Silva <gustavo@embeddedor.com>
    commit 5dfdb0893596a837a95fa4013c122ba32aae9946
    
    In preparation to enabling -Wimplicit-fallthrough, mark switch cases where
    we are expecting to fall through.
    
    Addresses-Coverity-ID: 1195463 ("Missing break in switch")
    Addresses-Coverity-ID: 1195464 ("Missing break in switch")
    Addresses-Coverity-ID: 1195465 ("Missing break in switch")
    Addresses-Coverity-ID: 1195466 ("Missing break in switch")
    Addresses-Coverity-ID: 1357338 ("Missing break in switch")
    	Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    	Acked-by: Don Brace <don.brace@microsemi.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 5dfdb0893596a837a95fa4013c122ba32aae9946)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -4638,6 +4638,7 @@ static int fixup_ioaccel_cdb(u8 *cdb, int *cdb_len)
 	case WRITE_6:
 	case WRITE_12:
 		is_write = 1;
+		/* fall through */
 	case READ_6:
 	case READ_12:
 		if (*cdb_len == 6) {
@@ -5068,6 +5069,7 @@ static int hpsa_scsi_ioaccel_raid_map(struct ctlr_info *h,
 	switch (cmd->cmnd[0]) {
 	case WRITE_6:
 		is_write = 1;
+		/* fall through */
 	case READ_6:
 		first_block = (((cmd->cmnd[1] & 0x1F) << 16) |
 				(cmd->cmnd[2] << 8) |
@@ -5078,6 +5080,7 @@ static int hpsa_scsi_ioaccel_raid_map(struct ctlr_info *h,
 		break;
 	case WRITE_10:
 		is_write = 1;
+		/* fall through */
 	case READ_10:
 		first_block =
 			(((u64) cmd->cmnd[2]) << 24) |
@@ -5090,6 +5093,7 @@ static int hpsa_scsi_ioaccel_raid_map(struct ctlr_info *h,
 		break;
 	case WRITE_12:
 		is_write = 1;
+		/* fall through */
 	case READ_12:
 		first_block =
 			(((u64) cmd->cmnd[2]) << 24) |
@@ -5104,6 +5108,7 @@ static int hpsa_scsi_ioaccel_raid_map(struct ctlr_info *h,
 		break;
 	case WRITE_16:
 		is_write = 1;
+		/* fall through */
 	case READ_16:
 		first_block =
 			(((u64) cmd->cmnd[2]) << 56) |
