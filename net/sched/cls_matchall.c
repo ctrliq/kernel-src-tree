8eeb8927ce386e749ec98f885485ba3560ad54e2
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:15:07 -0400

    net: sched: matchall: verify that filter is not NULL in mall_walk()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    Rebuild_CHGLOG: - [net] sched: matchall: verify that filter is not NULL in mall_walk() (Ivan Vecera) [1688236 1687456]
    Rebuild_FUZZ: 96.12%
    commit-author Vlad Buslov <vladbu@mellanox.com>
    commit d66022cd1623edfd7b43e13c9e973403a811af02
    
    Check that filter is not NULL before passing it to tcf_walker->fn()
    callback. This can happen when mall_change() failed to offload filter to
    hardware.
    
    Fixes: ed76f5edccc9 ("net: sched: protect filter_chain list with filter_chain_lock mutex")
    	Reported-by: Ido Schimmel <idosch@mellanox.com>
    	Tested-by: Ido Schimmel <idosch@mellanox.com>
    	Signed-off-by: Vlad Buslov <vladbu@mellanox.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d66022cd1623edfd7b43e13c9e973403a811af02)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/sched/cls_matchall.c
+++ net/sched/cls_matchall.c
@@ -244,6 +244,9 @@ static void mall_walk(struct tcf_proto *tp, struct tcf_walker *arg)
 
 	if (arg->count < arg->skip)
 		goto skip;
+
+	if (!head)
+		return;
 	if (arg->fn(tp, head, arg) < 0)
 		arg->stop = 1;
 skip:
