837d95ed6981973cccd51b5a8a4477c5fd3bb9cf
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:42:41 -0400

    scsi: qla2xxx: Complain if sp->done() is not called from the completion path
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Bart Van Assche <bvanassche@acm.org>
    commit 88263208dd23327b56fbd7c2858c389c1153b366
    
    Not calling sp->done() from the command completion path is a severe bug.
    Hence complain loudly if that happens.
    
    	Cc: Himanshu Madhani <hmadhani@marvell.com>
    	Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    	Tested-by: Himanshu Madhani <hmadhani@marvell.com>
    	Reviewed-by: Himanshu Madhani <hmadhani@marvell.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 88263208dd23327b56fbd7c2858c389c1153b366)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/qla2xxx/qla_mr.c
+++ drivers/scsi/qla2xxx/qla_mr.c
@@ -2541,6 +2541,8 @@ check_scsi_status:
 
 	if (rsp->status_srb == NULL)
 		sp->done(sp, res);
+	else
+		WARN_ON_ONCE(true);
 }
 
 /**
@@ -2618,6 +2620,8 @@ qlafx00_status_cont_entry(struct rsp_que *rsp, sts_cont_entry_t *pkt)
 	if (sense_len == 0) {
 		rsp->status_srb = NULL;
 		sp->done(sp, cp->result);
+	} else {
+		WARN_ON_ONCE(true);
 	}
 }
 
