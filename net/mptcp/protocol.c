29f4ca2e1222994c5becb5e2b567447fc2d55e58
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:34:57 -0400

    mptcp: free acked data before waiting for more memory
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Florian Westphal <fw@strlen.de>
    commit 1cec170d458b1d18f6f1654ca84c0804a701c5ef
    
    After subflow lock is dropped, more wmem might have been made available.
    
    This fixes a deadlock in mptcp_connect.sh 'mmap' mode: wmem is exhausted.
    But as the mptcp socket holds on to already-acked data (for retransmit)
    no wakeup will occur.
    
    Using 'goto restart' calls mptcp_clean_una(sk) which will free pages
    that have been acked completely in the mean time.
    
    Fixes: fb529e62d3f3 ("mptcp: break and restart in case mptcp sndbuf is full")
    	Signed-off-by: Florian Westphal <fw@strlen.de>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1cec170d458b1d18f6f1654ca84c0804a701c5ef)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -920,7 +920,6 @@ restart:
 		goto out;
 	}
 
-wait_for_sndbuf:
 	__mptcp_flush_join_list(msk);
 	ssk = mptcp_subflow_get_send(msk);
 	while (!sk_stream_memory_free(sk) || !ssk) {
@@ -1007,7 +1006,7 @@ wait_for_sndbuf:
 				 */
 				mptcp_set_timeout(sk, ssk);
 				release_sock(ssk);
-				goto wait_for_sndbuf;
+				goto restart;
 			}
 		}
 	}
