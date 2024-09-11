295f3a31bacc1dcc5bcc8dc91162ee3879af0324
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:15:43 -0400

    mm/memory-failure.c: use page_shift() in add_to_kill()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Yunfeng Ye <yeyunfeng@huawei.com>
    commit 7506851837350e112685ddf4d13ba03a558f9e20
    
    page_shift() is supported after the commit 94ad9338109f ("mm: introduce
    page_shift()").
    
    So replace with page_shift() in add_to_kill() for readability.
    
    Link: http://lkml.kernel.org/r/543d8bc9-f2e7-3023-7c35-2e7ed67c0e82@huawei.com
    	Signed-off-by: Yunfeng Ye <yeyunfeng@huawei.com>
    	Reviewed-by: David Hildenbrand <david@redhat.com>
    	Acked-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 7506851837350e112685ddf4d13ba03a558f9e20)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -360,7 +360,7 @@ static void add_to_kill(struct task_struct *tsk, struct page *p,
 	if (is_zone_device_page(p))
 		tk->size_shift = dev_pagemap_mapping_shift(p, vma);
 	else
-		tk->size_shift = compound_order(compound_head(p)) + PAGE_SHIFT;
+		tk->size_shift = page_shift(compound_head(p));
 
 	/*
 	 * In theory we don't have to kill when the page was
