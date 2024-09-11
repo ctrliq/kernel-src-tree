475a569a6d1c33079914b286136d626b5c7a955a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:49:33 -0400

    powerpc/prom_init: Move ibm_arch_vec to __prombss
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    Rebuild_CHGLOG: - [powerpc] prom_init: Move ibm_arch_vec to __prombss (Michael Roth) [1856099]
    Rebuild_FUZZ: 91.11%
    commit-author Benjamin Herrenschmidt <benh@kernel.crashing.org>
    commit a614f52e75bd69b513707b4adc672149c4903995
    
    Make the existing initialized definition constant and copy
    it to a __prombss copy
    
    	Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    	Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    (cherry picked from commit a614f52e75bd69b513707b4adc672149c4903995)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/powerpc/kernel/prom_init.c
+++ arch/powerpc/kernel/prom_init.c
@@ -922,7 +922,7 @@ struct ibm_arch_vec {
 	struct option_vector6 vec6;
 } __packed;
 
-struct ibm_arch_vec __cacheline_aligned ibm_architecture_vec = {
+static const struct ibm_arch_vec ibm_architecture_vec_template = {
 	.pvrs = {
 		{
 			.mask = cpu_to_be32(0xfffe0000), /* POWER5/POWER5+ */
@@ -1059,6 +1059,8 @@ struct ibm_arch_vec __cacheline_aligned ibm_architecture_vec = {
 		.os_name = OV6_LINUX,
 	},
 };
+
+static struct ibm_arch_vec __prombss ibm_architecture_vec  ____cacheline_aligned;
 
 /* Old method - ELF header with PT_NOTE sections only works on BE */
 #ifdef __BIG_ENDIAN__
@@ -1276,6 +1278,10 @@ static void __init prom_check_platform_support(void)
 	};
 	int prop_len = prom_getproplen(prom.chosen,
 				       "ibm,arch-vec-5-platform-support");
+
+	/* First copy the architecture vec template */
+	ibm_architecture_vec = ibm_architecture_vec_template;
+
 	if (prop_len > 1) {
 		int i;
 		u8 vec[prop_len];
