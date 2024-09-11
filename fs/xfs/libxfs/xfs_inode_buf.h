4941f00c0917a0d7a749d3185d0174e7d2c5d105
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:15:27 -0400

    xfs: use the existing type definition for di_projid
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Kaixu Xia <kaixuxia@tencent.com>
    commit 9c0fce4c16fc8d4d119cc3a20f1e5ce870206706
    
    We have already defined the project ID type prid_t, so maybe should
    use it here.
    
    	Signed-off-by: Kaixu Xia <kaixuxia@tencent.com>
    	Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    	Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    	Reviewed-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 9c0fce4c16fc8d4d119cc3a20f1e5ce870206706)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/xfs/libxfs/xfs_inode_buf.h
+++ fs/xfs/libxfs/xfs_inode_buf.h
@@ -18,7 +18,7 @@ struct xfs_dinode;
 struct xfs_icdinode {
 	int8_t		di_format;	/* format of di_c data */
 	uint16_t	di_flushiter;	/* incremented on flush */
-	uint32_t	di_projid;	/* owner's project id */
+	prid_t		di_projid;	/* owner's project id */
 	xfs_fsize_t	di_size;	/* number of bytes in file */
 	xfs_rfsblock_t	di_nblocks;	/* # of direct & btree blocks used */
 	xfs_extlen_t	di_extsize;	/* basic/minimum extent size for file */
