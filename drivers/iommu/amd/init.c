6d5f4af5ff3dd3fee4f39397e5e97ca89276098b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:50:50 -0400

    iommu/amd: Use struct_group() for memcpy() region
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Kees Cook <keescook@chromium.org>
    commit 43d83af8a57a0500d452a2505b297e605012960d
    
    In preparation for FORTIFY_SOURCE performing compile-time and run-time
    field bounds checking for memcpy(), memmove(), and memset(), avoid
    intentionally writing across neighboring fields.
    
    Use struct_group() in struct ivhd_entry around members ext and hidh, so
    they can be referenced together. This will allow memcpy() and sizeof()
    to more easily reason about sizes, improve readability, and avoid future
    warnings about writing beyond the end of ext.
    
    "pahole" shows no size nor member offset changes to struct ivhd_entry.
    "objdump -d" shows no object code changes.
    
    	Cc: Will Deacon <will@kernel.org>
    	Cc: iommu@lists.linux-foundation.org
    	Acked-by: Joerg Roedel <jroedel@suse.de>
    	Signed-off-by: Kees Cook <keescook@chromium.org>
    (cherry picked from commit 43d83af8a57a0500d452a2505b297e605012960d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -133,8 +133,10 @@ struct ivhd_entry {
 	u8 type;
 	u16 devid;
 	u8 flags;
-	u32 ext;
-	u32 hidh;
+	struct_group(ext_hid,
+		u32 ext;
+		u32 hidh;
+	);
 	u64 cid;
 	u8 uidf;
 	u8 uidl;
@@ -1401,7 +1403,8 @@ static int __init init_iommu_from_acpi(struct amd_iommu *iommu,
 				break;
 			}
 
-			memcpy(hid, (u8 *)(&e->ext), ACPIHID_HID_LEN - 1);
+			BUILD_BUG_ON(sizeof(e->ext_hid) != ACPIHID_HID_LEN - 1);
+			memcpy(hid, &e->ext_hid, ACPIHID_HID_LEN - 1);
 			hid[ACPIHID_HID_LEN - 1] = '\0';
 
 			if (!(*hid)) {
