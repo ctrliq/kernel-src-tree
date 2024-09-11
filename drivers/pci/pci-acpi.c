78d73189748f3500283786311d66b9e53a737101
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:10:46 -0400

    PCI: PM: Simplify acpi_pci_power_manageable()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-408.el8
    commit-author Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    commit f09183712146909d56c9555e6901c10cc285339f
    
    Make acpi_pci_power_manageable() more straightforward.
    
    No functional impact.
    
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    	Tested-by: Ferry Toth <fntoth@gmail.com>
    (cherry picked from commit f09183712146909d56c9555e6901c10cc285339f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/pci/pci-acpi.c
+++ drivers/pci/pci-acpi.c
@@ -946,9 +946,7 @@ static bool acpi_pci_power_manageable(struct pci_dev *dev)
 {
 	struct acpi_device *adev = ACPI_COMPANION(&dev->dev);
 
-	if (!adev)
-		return false;
-	return acpi_device_power_manageable(adev);
+	return adev && acpi_device_power_manageable(adev);
 }
 
 static bool acpi_pci_bridge_d3(struct pci_dev *dev)
