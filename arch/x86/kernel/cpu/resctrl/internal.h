2ac303e0bbceb6d26d6bac52833af20d5b88d160
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:29:20 -0400

    x86/resctrl: Support wider MBM counters
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Reinette Chatre <reinette.chatre@intel.com>
    commit 0c4d5ba1b998e713815b7790d3db6ced0ae49489
    
    The original Memory Bandwidth Monitoring (MBM) architectural
    definition defines counters of up to 62 bits in the
    IA32_QM_CTR MSR while the first-generation MBM implementation
    uses statically defined 24 bit counters.
    
    The MBM CPUID enumeration properties have been expanded to include
    the MBM counter width, encoded as an offset from 24 bits.
    
    While eight bits are available for the counter width offset IA32_QM_CTR
    MSR only supports 62 bit counters. Add a sanity check, with warning
    printed when encountered, to ensure counters cannot exceed the 62 bit
    limit.
    
    	Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    	Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/69d52abd5b14794d3a0f05ba7c755ed1f4c0d5ed.1588715690.git.reinette.chatre@intel.com
    (cherry picked from commit 0c4d5ba1b998e713815b7790d3db6ced0ae49489)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kernel/cpu/resctrl/internal.h
+++ arch/x86/kernel/cpu/resctrl/internal.h
@@ -30,7 +30,7 @@
 
 #define CQM_LIMBOCHECK_INTERVAL	1000
 
-#define MBM_CNTR_WIDTH			24
+#define MBM_CNTR_WIDTH_BASE		24
 #define MBM_OVERFLOW_INTERVAL		1000
 #define MAX_MBA_BW			100u
 #define MBA_IS_LINEAR			0x4
@@ -39,6 +39,12 @@
 
 #define RMID_VAL_ERROR			BIT_ULL(63)
 #define RMID_VAL_UNAVAIL		BIT_ULL(62)
+/*
+ * With the above fields in use 62 bits remain in MSR_IA32_QM_CTR for
+ * data to be returned. The counter width is discovered from the hardware
+ * as an offset from MBM_CNTR_WIDTH_BASE.
+ */
+#define MBM_CNTR_WIDTH_OFFSET_MAX (62 - MBM_CNTR_WIDTH_BASE)
 
 DECLARE_STATIC_KEY_FALSE(rdt_enable_key);
 DECLARE_STATIC_KEY_FALSE(rdt_mon_enable_key);
