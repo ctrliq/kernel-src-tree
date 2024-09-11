c0931db28b5b51d6c215d788b1f0f1c844ccfc0a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:30:20 -0400

    net: thunderx: add nicvf_send_msg_to_pf result check for set_rx_mode_task
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Vadim Lomovtsev <vlomovtsev@marvell.com>
    commit 7db730d9d2f7b6af6aeac621b1890ea477a0cb8d
    
    The rx_set_mode invokes number of messages to be send to PF for receive
    mode configuration. In case if there any issues we need to stop sending
    messages and release allocated memory.
    
    This commit is to implement check of nicvf_msg_send_to_pf() result.
    
    	Signed-off-by: Vadim Lomovtsev <vlomovtsev@marvell.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7db730d9d2f7b6af6aeac621b1890ea477a0cb8d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/cavium/thunder/nicvf_main.c
+++ drivers/net/ethernet/cavium/thunder/nicvf_main.c
@@ -1948,7 +1948,8 @@ static void __nicvf_set_rx_mode_task(u8 mode, struct xcast_addr_list *mc_addrs,
 
 	/* flush DMAC filters and reset RX mode */
 	mbx.xcast.msg = NIC_MBOX_MSG_RESET_XCAST;
-	nicvf_send_msg_to_pf(nic, &mbx);
+	if (nicvf_send_msg_to_pf(nic, &mbx) < 0)
+		goto free_mc;
 
 	if (mode & BGX_XCAST_MCAST_FILTER) {
 		/* once enabling filtering, we need to signal to PF to add
@@ -1956,7 +1957,8 @@ static void __nicvf_set_rx_mode_task(u8 mode, struct xcast_addr_list *mc_addrs,
 		 */
 		mbx.xcast.msg = NIC_MBOX_MSG_ADD_MCAST;
 		mbx.xcast.data.mac = 0;
-		nicvf_send_msg_to_pf(nic, &mbx);
+		if (nicvf_send_msg_to_pf(nic, &mbx) < 0)
+			goto free_mc;
 	}
 
 	/* check if we have any specific MACs to be added to PF DMAC filter */
@@ -1965,9 +1967,9 @@ static void __nicvf_set_rx_mode_task(u8 mode, struct xcast_addr_list *mc_addrs,
 		for (idx = 0; idx < mc_addrs->count; idx++) {
 			mbx.xcast.msg = NIC_MBOX_MSG_ADD_MCAST;
 			mbx.xcast.data.mac = mc_addrs->mc[idx];
-			nicvf_send_msg_to_pf(nic, &mbx);
+			if (nicvf_send_msg_to_pf(nic, &mbx) < 0)
+				goto free_mc;
 		}
-		kfree(mc_addrs);
 	}
 
 	/* and finally set rx mode for PF accordingly */
@@ -1975,6 +1977,8 @@ static void __nicvf_set_rx_mode_task(u8 mode, struct xcast_addr_list *mc_addrs,
 	mbx.xcast.data.mode = mode;
 
 	nicvf_send_msg_to_pf(nic, &mbx);
+free_mc:
+	kfree(mc_addrs);
 }
 
 static void nicvf_set_rx_mode_task(struct work_struct *work_arg)
