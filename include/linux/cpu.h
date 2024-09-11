9702a526c9de07326a1927ef16f2b29264789eac
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:26:36 -0400

    cpu/hotplug: Create SMT sysfs interface for all arches
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Josh Poimboeuf <jpoimboe@redhat.com>
    commit de7b77e5bb9451417ca57f1b6501da654587c048
    
    Make the /sys/devices/system/cpu/smt/* files available on all arches, so
    user space has a consistent way to detect whether SMT is enabled.
    
    The 'control' file now shows 'notimplemented' for architectures which
    don't yet have CONFIG_HOTPLUG_SMT.
    
    [ tglx: Make notimplemented a real state ]
    
    	Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    	Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    	Cc: Andrea Arcangeli <aarcange@redhat.com>
    	Cc: Waiman Long <longman@redhat.com>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Jiri Kosina <jikos@kernel.org>
    Link: https://lkml.kernel.org/r/469c2b98055f2c41e75748e06447d592a64080c9.1553635520.git.jpoimboe@redhat.com
    
    (cherry picked from commit de7b77e5bb9451417ca57f1b6501da654587c048)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -173,6 +173,7 @@ enum cpuhp_smt_control {
 	CPU_SMT_DISABLED,
 	CPU_SMT_FORCE_DISABLED,
 	CPU_SMT_NOT_SUPPORTED,
+	CPU_SMT_NOT_IMPLEMENTED,
 };
 
 #if defined(CONFIG_SMP) && defined(CONFIG_HOTPLUG_SMT)
@@ -181,7 +182,7 @@ extern void cpu_smt_disable(bool force);
 extern void cpu_smt_check_topology_early(void);
 extern void cpu_smt_check_topology(void);
 #else
-# define cpu_smt_control		(CPU_SMT_ENABLED)
+# define cpu_smt_control		(CPU_SMT_NOT_IMPLEMENTED)
 static inline void cpu_smt_disable(bool force) { }
 static inline void cpu_smt_check_topology_early(void) { }
 static inline void cpu_smt_check_topology(void) { }
