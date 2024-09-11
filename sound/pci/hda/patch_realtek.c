8e879d1727c3df6bbdd826c1d4a91fe8e89a0bdc
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:32:07 -0400

    ALSA: hda/realtek - Check headset type by unplug and resume
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Kailang Yang <kailang@realtek.com>
    commit aeac1a0dadb4ab699a61136a5729d048abe6ee5f
    
    When system enable HDA power save mode.
    This issue will happen on new platform which DMIC connect to PCH.
    In Dell headset mode, it will recheck during runtime resume when
    headset was plugged.
    This patch will move check headset type on unplug and system resume.
    
    [ A few minor code cleanups by tiwai ]
    
    	Signed-off-by: Kailang Yang <kailang@realtek.com>
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit aeac1a0dadb4ab699a61136a5729d048abe6ee5f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -834,6 +834,8 @@ static void alc_pre_init(struct hda_codec *codec)
 	alc_fill_eapd_coef(codec);
 }
 
+#define is_s3_resume(codec) \
+	((codec)->core.dev.power.power_state.event == PM_EVENT_RESUME)
 #define is_s4_resume(codec) \
 	((codec)->core.dev.power.power_state.event == PM_EVENT_RESTORE)
 
@@ -4902,6 +4904,8 @@ static void alc_update_headset_mode(struct hda_codec *codec)
 	switch (new_headset_mode) {
 	case ALC_HEADSET_MODE_UNPLUGGED:
 		alc_headset_mode_unplugged(codec);
+		spec->current_headset_mode = ALC_HEADSET_MODE_UNKNOWN;
+		spec->current_headset_type = ALC_HEADSET_TYPE_UNKNOWN;
 		spec->gen.hp_jack_present = false;
 		break;
 	case ALC_HEADSET_MODE_HEADSET:
@@ -4944,8 +4948,6 @@ static void alc_update_headset_mode_hook(struct hda_codec *codec,
 static void alc_update_headset_jack_cb(struct hda_codec *codec,
 				       struct hda_jack_callback *jack)
 {
-	struct alc_spec *spec = codec->spec;
-	spec->current_headset_type = ALC_HEADSET_TYPE_UNKNOWN;
 	snd_hda_gen_hp_automute(codec, jack);
 }
 
@@ -4982,7 +4984,10 @@ static void alc_fixup_headset_mode(struct hda_codec *codec,
 		alc_probe_headset_mode(codec);
 		break;
 	case HDA_FIXUP_ACT_INIT:
-		spec->current_headset_mode = 0;
+		if (is_s3_resume(codec) || is_s4_resume(codec)) {
+			spec->current_headset_mode = ALC_HEADSET_MODE_UNKNOWN;
+			spec->current_headset_type = ALC_HEADSET_TYPE_UNKNOWN;
+		}
 		alc_update_headset_mode(codec);
 		break;
 	}
