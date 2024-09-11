3bc46d80aa2699ad2e820eb07de893e45c56f48c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:54:20 -0400

    KVM: Simplify kvm_free_memslot() and all its descendents
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Sean Christopherson <sean.j.christopherson@intel.com>
    commit e96c81ee89d80e1a0fe50a0e9be40c1b77e14aaa
    
    Now that all callers of kvm_free_memslot() pass NULL for @dont, remove
    the param from the top-level routine and all arch's implementations.
    
    No functional change intended.
    
    	Tested-by: Christoffer Dall <christoffer.dall@arm.com>
    	Reviewed-by: Peter Xu <peterx@redhat.com>
    	Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit e96c81ee89d80e1a0fe50a0e9be40c1b77e14aaa)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- virt/kvm/arm/mmu.c
+++ virt/kvm/arm/mmu.c
@@ -2365,8 +2365,7 @@ out:
 	return ret;
 }
 
-void kvm_arch_free_memslot(struct kvm *kvm, struct kvm_memory_slot *free,
-			   struct kvm_memory_slot *dont)
+void kvm_arch_free_memslot(struct kvm *kvm, struct kvm_memory_slot *slot)
 {
 }
 
