c85ff3d2452f393a61250cda9863b6abc1e55658
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:17:55 -0400

    efi/x86: Simplify 64-bit EFI firmware call wrapper
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Ard Biesheuvel <ardb@kernel.org>
    commit e5f930fe8dafd2055220c95958926af16ee20713
    
    The efi_call() wrapper used to invoke EFI runtime services serves
    a number of purposes:
    - realign the stack to 16 bytes
    - preserve FP and CR0 register state
    - translate from SysV to MS calling convention.
    
    Preserving CR0.TS is no longer necessary in Linux, and preserving the
    FP register state is also redundant in most cases, since efi_call() is
    almost always used from within the scope of a pair of kernel_fpu_begin()/
    kernel_fpu_end() calls, with the exception of the early call to
    SetVirtualAddressMap() and the SGI UV support code.
    
    So let's add a pair of kernel_fpu_begin()/_end() calls there as well,
    and remove the unnecessary code from the assembly implementation of
    efi_call(), and only keep the pieces that deal with the stack
    alignment and the ABI translation.
    
    	Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    	Cc: Andy Lutomirski <luto@kernel.org>
    	Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    	Cc: Arvind Sankar <nivedita@alum.mit.edu>
    	Cc: Matthew Garrett <mjg59@google.com>
    	Cc: linux-efi@vger.kernel.org
    Link: https://lkml.kernel.org/r/20200103113953.9571-10-ardb@kernel.org
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit e5f930fe8dafd2055220c95958926af16ee20713)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/platform/efi/efi_64.c
+++ arch/x86/platform/efi/efi_64.c
@@ -1023,6 +1023,8 @@ efi_status_t __init efi_set_virtual_address_map(unsigned long memory_map_size,
 		efi_switch_mm(&efi_mm);
 	}
 
+	kernel_fpu_begin();
+
 	/* Disable interrupts around EFI calls: */
 	local_irq_save(flags);
 	status = efi_call(efi.systab->runtime->set_virtual_address_map,
@@ -1030,6 +1032,7 @@ efi_status_t __init efi_set_virtual_address_map(unsigned long memory_map_size,
 			  descriptor_version, virtual_map);
 	local_irq_restore(flags);
 
+	kernel_fpu_end();
 
 	if (save_pgd)
 		efi_old_memmap_phys_epilog(save_pgd);
