e8f0bd9ea4252f23e28b5dda2975702119ba16bf
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:11:23 -0400

    net: usbnet: remove driver version
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit 6236239d1cc479b5cf49317822241e8a97fefb7b
    
    Obviously this driver version doesn't make sense. Go with the default
    and let ethtool display the kernel version.
    
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 6236239d1cc479b5cf49317822241e8a97fefb7b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -46,9 +46,6 @@
 #include <linux/kernel.h>
 #include <linux/pm_runtime.h>
 
-#define DRIVER_VERSION		"22-Aug-2005"
-
-
 /*-------------------------------------------------------------------------*/
 
 /*
@@ -1059,7 +1056,6 @@ void usbnet_get_drvinfo (struct net_device *net, struct ethtool_drvinfo *info)
 	struct usbnet *dev = netdev_priv(net);
 
 	strlcpy (info->driver, dev->driver_name, sizeof info->driver);
-	strlcpy (info->version, DRIVER_VERSION, sizeof info->version);
 	strlcpy (info->fw_version, dev->driver_info->description,
 		sizeof info->fw_version);
 	usb_make_path (dev->udev, info->bus_info, sizeof info->bus_info);
