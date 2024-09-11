9ed3191a329cacdd12ba2ad95484bb74a3469e49
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:36:45 -0400

    sfc: Use dev_get_drvdata where possible
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Chuhong Yuan <hslester96@gmail.com>
    commit 3e03a8ba8e1d6c9940a032992f4da2bfba391fdf
    
    Instead of using to_pci_dev + pci_get_drvdata,
    use dev_get_drvdata to make code simpler.
    
    	Signed-off-by: Chuhong Yuan <hslester96@gmail.com>
    	Acked-by: Edward Cree <ecree@solarflare.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 3e03a8ba8e1d6c9940a032992f4da2bfba391fdf)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/sfc/ef10.c
+++ drivers/net/ethernet/sfc/ef10.c
@@ -511,7 +511,7 @@ static ssize_t efx_ef10_show_link_control_flag(struct device *dev,
 					       struct device_attribute *attr,
 					       char *buf)
 {
-	struct efx_nic *efx = pci_get_drvdata(to_pci_dev(dev));
+	struct efx_nic *efx = dev_get_drvdata(dev);
 
 	return sprintf(buf, "%d\n",
 		       ((efx->mcdi->fn_flags) &
@@ -523,7 +523,7 @@ static ssize_t efx_ef10_show_primary_flag(struct device *dev,
 					  struct device_attribute *attr,
 					  char *buf)
 {
-	struct efx_nic *efx = pci_get_drvdata(to_pci_dev(dev));
+	struct efx_nic *efx = dev_get_drvdata(dev);
 
 	return sprintf(buf, "%d\n",
 		       ((efx->mcdi->fn_flags) &
