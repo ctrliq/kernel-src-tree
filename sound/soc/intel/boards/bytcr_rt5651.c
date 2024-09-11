47e9cc14c759bb8c2e3badd2617f767dbb0fdce4
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:33:32 -0400

    ASoC: Intel: boards: harden codec property handling
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-373.el8
    commit-author Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    commit c50f126b3c9ebb77585838726a3a490ad33b92cd
    
    In current ACPI-based devices, the DSDT does not include any of the
    properties required by the codec driver. This is not an ACPI
    limitation proper since the _DSD method could be used, as done for
    Camera and SoundWire in newer platforms. For legacy devices, there is
    unfortunately no other option than using a work-around: we add
    properties to the codec device from the machine driver.
    
    To avoid any issues with the codec driver being unbound, we need to
    keep a reference to the codec device until the card is removed.
    
    	Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Co-developed-by: Hans de Goede <hdegoede@redhat.com>
    	Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    	Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20210813151116.23931-2-pierre-louis.bossart@linux.intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit c50f126b3c9ebb77585838726a3a490ad33b92cd)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/intel/boards/bytcr_rt5651.c
+++ sound/soc/intel/boards/bytcr_rt5651.c
@@ -85,6 +85,7 @@ struct byt_rt5651_private {
 	struct gpio_desc *ext_amp_gpio;
 	struct gpio_desc *hp_detect;
 	struct snd_soc_jack jack;
+	struct device *codec_dev;
 };
 
 static const struct acpi_gpio_mapping *byt_rt5651_gpios;
@@ -993,12 +994,12 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 		byt_rt5651_quirk = quirk_override;
 	}
 
+	priv->codec_dev = codec_dev;
+
 	/* Must be called before register_card, also see declaration comment. */
 	ret_val = byt_rt5651_add_codec_device_props(codec_dev);
-	if (ret_val) {
-		put_device(codec_dev);
-		return ret_val;
-	}
+	if (ret_val)
+		goto err;
 
 	/* Cherry Trail devices use an external amplifier enable gpio */
 	if (soc_intel_is_cht() && !byt_rt5651_gpios)
@@ -1021,8 +1022,7 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 					ret_val);
 				/* fall through */
 			case -EPROBE_DEFER:
-				put_device(codec_dev);
-				return ret_val;
+				goto err;
 			}
 		}
 		priv->hp_detect = devm_fwnode_get_index_gpiod_from_child(
@@ -1040,13 +1040,10 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 					ret_val);
 				/* fall through */
 			case -EPROBE_DEFER:
-				put_device(codec_dev);
-				return ret_val;
+				goto err;
 			}
 		}
 	}
-
-	put_device(codec_dev);
 
 	log_quirks(&pdev->dev);
 
@@ -1071,7 +1068,7 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 			 * for all other errors, including -EPROBE_DEFER
 			 */
 			if (ret_val != -ENOENT)
-				return ret_val;
+				goto err;
 			byt_rt5651_quirk &= ~BYT_RT5651_MCLK_EN;
 		}
 	}
@@ -1100,7 +1097,7 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 	ret_val = snd_soc_fixup_dai_links_platform_name(&byt_rt5651_card,
 							platform_name);
 	if (ret_val)
-		return ret_val;
+		goto err;
 
 	sof_parent = snd_soc_acpi_sof_parent(&pdev->dev);
 
@@ -1122,10 +1119,23 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 	if (ret_val) {
 		dev_err(&pdev->dev, "devm_snd_soc_register_card failed %d\n",
 			ret_val);
-		return ret_val;
+		goto err;
 	}
 	platform_set_drvdata(pdev, &byt_rt5651_card);
 	return ret_val;
+
+err:
+	put_device(priv->codec_dev);
+	return ret_val;
+}
+
+static int snd_byt_rt5651_mc_remove(struct platform_device *pdev)
+{
+	struct snd_soc_card *card = platform_get_drvdata(pdev);
+	struct byt_rt5651_private *priv = snd_soc_card_get_drvdata(card);
+
+	put_device(priv->codec_dev);
+	return 0;
 }
 
 static struct platform_driver snd_byt_rt5651_mc_driver = {
@@ -1133,6 +1143,7 @@ static struct platform_driver snd_byt_rt5651_mc_driver = {
 		.name = "bytcr_rt5651",
 	},
 	.probe = snd_byt_rt5651_mc_probe,
+	.remove = snd_byt_rt5651_mc_remove,
 };
 
 module_platform_driver(snd_byt_rt5651_mc_driver);
