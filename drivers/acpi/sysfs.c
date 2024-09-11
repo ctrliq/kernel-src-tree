61f1bf0d7eed34a29ac2308cb00b1b1c21b243f9
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:10:41 -0400

    ACPI: sysfs: Unify pattern of memory allocations
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    commit b272c05984679c855ff2d08c9d54957bdcfd9b3b
    
    Use the form of foo = kmalloc(sizeof(*foo)) everywhere in order to
    unify pattern of memory allocations.
    
    	Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit b272c05984679c855ff2d08c9d54957bdcfd9b3b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/sysfs.c
+++ drivers/acpi/sysfs.c
@@ -395,8 +395,7 @@ acpi_status acpi_sysfs_table_handler(u32 event, void *table, void *context)
 
 	switch (event) {
 	case ACPI_TABLE_EVENT_INSTALL:
-		table_attr =
-		    kzalloc(sizeof(struct acpi_table_attr), GFP_KERNEL);
+		table_attr = kzalloc(sizeof(*table_attr), GFP_KERNEL);
 		if (!table_attr)
 			return AE_NO_MEMORY;
 
@@ -847,13 +846,11 @@ void acpi_irq_stats_init(void)
 	num_gpes = acpi_current_gpe_count;
 	num_counters = num_gpes + ACPI_NUM_FIXED_EVENTS + NUM_COUNTERS_EXTRA;
 
-	all_attrs = kcalloc(num_counters + 1, sizeof(struct attribute *),
-			    GFP_KERNEL);
+	all_attrs = kcalloc(num_counters + 1, sizeof(*all_attrs), GFP_KERNEL);
 	if (all_attrs == NULL)
 		return;
 
-	all_counters = kcalloc(num_counters, sizeof(struct event_counter),
-			       GFP_KERNEL);
+	all_counters = kcalloc(num_counters, sizeof(*all_counters), GFP_KERNEL);
 	if (all_counters == NULL)
 		goto fail;
 
@@ -861,8 +858,7 @@ void acpi_irq_stats_init(void)
 	if (ACPI_FAILURE(status))
 		goto fail;
 
-	counter_attrs = kcalloc(num_counters, sizeof(struct kobj_attribute),
-				GFP_KERNEL);
+	counter_attrs = kcalloc(num_counters, sizeof(*counter_attrs), GFP_KERNEL);
 	if (counter_attrs == NULL)
 		goto fail;
 
