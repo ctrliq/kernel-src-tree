e5eebc603a3df24bfca4e54e37c8367ba9dde782
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:51:29 -0400

    net: sched: fix possible refcount leak in tc_new_tfilter()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Hangyu Hua <hbh25y@gmail.com>
    commit c2e1cfefcac35e0eea229e148c8284088ce437b5
    
    tfilter_put need to be called to put the refount got by tp->ops->get to
    avoid possible refcount leak when chain->tmplt_ops != NULL and
    chain->tmplt_ops != tp->ops.
    
    Fixes: 7d5509fa0d3d ("net: sched: extend proto ops with 'put' callback")
    	Signed-off-by: Hangyu Hua <hbh25y@gmail.com>
    	Reviewed-by: Vlad Buslov <vladbu@nvidia.com>
    Link: https://lore.kernel.org/r/20220921092734.31700-1-hbh25y@gmail.com
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit c2e1cfefcac35e0eea229e148c8284088ce437b5)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -2140,6 +2140,7 @@ replay:
 	}
 
 	if (chain->tmplt_ops && chain->tmplt_ops != tp->ops) {
+		tfilter_put(tp, fh);
 		NL_SET_ERR_MSG(extack, "Chain template is set to a different filter kind");
 		err = -EINVAL;
 		goto errout;
