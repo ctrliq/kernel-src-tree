17b77e97ee34ec706534064f84a3d2f986d95776
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:18:47 -0400

    iommu: Take lock before reading iommu group default domain type
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Sai Praneeth Prakhya <sai.praneeth.prakhya@intel.com>
    commit 0b8a96a3120ffe4d3571d93902693c59f90c3d0c
    
    "/sys/kernel/iommu_groups/<grp_id>/type" file could be read to find out the
    default domain type of an iommu group. The default domain of an iommu group
    doesn't change after booting and hence could be read directly. But,
    after addding support to dynamically change iommu group default domain, the
    above assumption no longer stays valid.
    
    iommu group default domain type could be changed at any time by writing to
    "/sys/kernel/iommu_groups/<grp_id>/type". So, take group mutex before
    reading iommu group default domain type so that the user wouldn't see stale
    values or iommu_group_show_type() doesn't try to derefernce stale pointers.
    
    	Signed-off-by: Sai Praneeth Prakhya <sai.praneeth.prakhya@intel.com>
    	Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    	Cc: Christoph Hellwig <hch@lst.de>
    	Cc: Joerg Roedel <joro@8bytes.org>
    	Cc: Ashok Raj <ashok.raj@intel.com>
    	Cc: Will Deacon <will.deacon@arm.com>
    	Cc: Sohil Mehta <sohil.mehta@intel.com>
    	Cc: Robin Murphy <robin.murphy@arm.com>
    	Cc: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Link: https://lore.kernel.org/r/20201124130604.2912899-4-baolu.lu@linux.intel.com
    	Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 0b8a96a3120ffe4d3571d93902693c59f90c3d0c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -516,6 +516,7 @@ static ssize_t iommu_group_show_type(struct iommu_group *group,
 {
 	char *type = "unknown\n";
 
+	mutex_lock(&group->mutex);
 	if (group->default_domain) {
 		switch (group->default_domain->type) {
 		case IOMMU_DOMAIN_BLOCKED:
@@ -532,6 +533,7 @@ static ssize_t iommu_group_show_type(struct iommu_group *group,
 			break;
 		}
 	}
+	mutex_unlock(&group->mutex);
 	strcpy(buf, type);
 
 	return strlen(type);
