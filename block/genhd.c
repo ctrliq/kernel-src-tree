24531f1fe6dc37704f6760fda2aef4fc5160d667
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:36:13 -0400

    block: genhd: Use struct_size() helper
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Gustavo A. R. Silva <gustavo@embeddedor.com>
    commit 78b90a2ce8424eb4be4a6a1623dc7c07af8303aa
    
    Make use of the struct_size() helper instead of an open-coded version
    in order to avoid any potential type mistakes, in particular in the
    context in which this code is being used.
    
    So, replace the following form:
    
    sizeof(*new_ptbl) + target * sizeof(new_ptbl->part[0])
    
    with:
    
    struct_size(new_ptbl, part, target)
    
    Also, notice that variable size is unnecessary, hence it is removed.
    
    This code was detected with the help of Coccinelle.
    
    	Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 78b90a2ce8424eb4be4a6a1623dc7c07af8303aa)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- block/genhd.c
+++ block/genhd.c
@@ -1280,7 +1280,6 @@ int disk_expand_part_tbl(struct gendisk *disk, int partno)
 	struct disk_part_tbl *new_ptbl;
 	int len = old_ptbl ? old_ptbl->len : 0;
 	int i, target;
-	size_t size;
 
 	/*
 	 * check for int overflow, since we can get here from blkpg_ioctl()
@@ -1297,8 +1296,8 @@ int disk_expand_part_tbl(struct gendisk *disk, int partno)
 	if (target <= len)
 		return 0;
 
-	size = sizeof(*new_ptbl) + target * sizeof(new_ptbl->part[0]);
-	new_ptbl = kzalloc_node(size, GFP_KERNEL, disk->node_id);
+	new_ptbl = kzalloc_node(struct_size(new_ptbl, part, target), GFP_KERNEL,
+				disk->node_id);
 	if (!new_ptbl)
 		return -ENOMEM;
 
