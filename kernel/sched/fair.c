228ecd1c6e89ea3635842883cfc11a580c22b626
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:44:14 -0400

    sched/debug: Add new tracepoints to track PELT at rq level
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Qais Yousef <qais.yousef@arm.com>
    commit ba19f51fcb549c7ee6261da243eea55a47e98d78
    
    The new tracepoints allow tracking PELT signals at rq level for all
    scheduling classes + irq.
    
    	Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    	Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Pavankumar Kondeti <pkondeti@codeaurora.org>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Quentin Perret <quentin.perret@arm.com>
    	Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    	Cc: Steven Rostedt <rostedt@goodmis.org>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    	Cc: Uwe Kleine-Konig <u.kleine-koenig@pengutronix.de>
    Link: https://lkml.kernel.org/r/20190604111459.2862-4-qais.yousef@arm.com
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit ba19f51fcb549c7ee6261da243eea55a47e98d78)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3380,6 +3380,8 @@ static inline int propagate_entity_load_avg(struct sched_entity *se)
 	update_tg_cfs_util(cfs_rq, se, gcfs_rq);
 	update_tg_cfs_runnable(cfs_rq, se, gcfs_rq);
 
+	trace_pelt_cfs_tp(cfs_rq);
+
 	return 1;
 }
 
@@ -3528,6 +3530,8 @@ static void attach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 	add_tg_cfs_propagate(cfs_rq, se->avg.load_sum);
 
 	cfs_rq_util_change(cfs_rq, flags);
+
+	trace_pelt_cfs_tp(cfs_rq);
 }
 
 /**
@@ -3547,6 +3551,8 @@ static void detach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 	add_tg_cfs_propagate(cfs_rq, -se->avg.load_sum);
 
 	cfs_rq_util_change(cfs_rq, 0);
+
+	trace_pelt_cfs_tp(cfs_rq);
 }
 
 /*
