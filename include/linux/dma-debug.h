8292ce500b19d615fbf8d910758aa813d7075a2a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:25:50 -0400

    dma-mapping: simplify the dma_sync_single_range_for_{cpu,device} implementation
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Christoph Hellwig <hch@lst.de>
    commit 8d59b5f2a44611d7327a2a14b36090d692186f60
    
    We can just call the regular calls after adding offset the the address instead
    of reimplementing them.
    
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    	Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    	Tested-by: Jesper Dangaard Brouer <brouer@redhat.com>
    	Tested-by: Tony Luck <tony.luck@intel.com>
    (cherry picked from commit 8d59b5f2a44611d7327a2a14b36090d692186f60)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/dma-debug.h
+++ include/linux/dma-debug.h
@@ -72,17 +72,6 @@ extern void debug_dma_sync_single_for_device(struct device *dev,
 					     dma_addr_t dma_handle,
 					     size_t size, int direction);
 
-extern void debug_dma_sync_single_range_for_cpu(struct device *dev,
-						dma_addr_t dma_handle,
-						unsigned long offset,
-						size_t size,
-						int direction);
-
-extern void debug_dma_sync_single_range_for_device(struct device *dev,
-						   dma_addr_t dma_handle,
-						   unsigned long offset,
-						   size_t size, int direction);
-
 extern void debug_dma_sync_sg_for_cpu(struct device *dev,
 				      struct scatterlist *sg,
 				      int nelems, int direction);
@@ -171,22 +160,6 @@ static inline void debug_dma_sync_single_for_cpu(struct device *dev,
 static inline void debug_dma_sync_single_for_device(struct device *dev,
 						    dma_addr_t dma_handle,
 						    size_t size, int direction)
-{
-}
-
-static inline void debug_dma_sync_single_range_for_cpu(struct device *dev,
-						       dma_addr_t dma_handle,
-						       unsigned long offset,
-						       size_t size,
-						       int direction)
-{
-}
-
-static inline void debug_dma_sync_single_range_for_device(struct device *dev,
-							  dma_addr_t dma_handle,
-							  unsigned long offset,
-							  size_t size,
-							  int direction)
 {
 }
 
