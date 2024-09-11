f1cfdc6da301498bc12b313555cc2f22fa33a0da
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:08:28 -0400

    iommu/arm-smmu-v3: Don't issue CMD_SYNC for zero-length invalidations
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [iommu] arm-smmu-v3: Don't issue CMD_SYNC for zero-length invalidations (Jerry Snitselaar) [1729845]
    Rebuild_FUZZ: 95.45%
    commit-author Will Deacon <will@kernel.org>
    commit 7314ca8699e1defd6f2883f203c0e828a4f51f9f
    
    Calling arm_smmu_tlb_inv_range() with a size of zero, perhaps due to
    an empty 'iommu_iotlb_gather' structure, should be a NOP. Elide the
    CMD_SYNC when there is no invalidation to be performed.
    
    	Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 7314ca8699e1defd6f2883f203c0e828a4f51f9f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/iommu/arm-smmu-v3.c
+++ drivers/iommu/arm-smmu-v3.c
@@ -1970,6 +1970,9 @@ static void arm_smmu_tlb_inv_range_nosync(unsigned long iova, size_t size,
 		},
 	};
 
+	if (!size)
+		return;
+
 	if (smmu_domain->stage == ARM_SMMU_DOMAIN_S1) {
 		cmd.opcode	= CMDQ_OP_TLBI_NH_VA;
 		cmd.tlbi.asid	= smmu_domain->s1_cfg.cd.asid;
