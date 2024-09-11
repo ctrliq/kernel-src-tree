0dc5b1e07c86a6f83bf428ba2f8c169de4e11fc3
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:26:34 -0400

    dm: remove unnecessary current->bio_list check when submitting split bio
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Jeffle Xu <jefflexu@linux.alibaba.com>
    commit 985eabdcfe3aad1aea6fc195dafff503f303aa3a
    
    The depth-first splitting is introduced in commit 18a25da84354 ("dm:
    ensure bio submission follows a depth-first tree walk"), which is used
    to fix the potential deadlock in case of the misordering handling of
    bios caused by bio_list. There're two paths submitting split bios,
    dm_wq_work() from worker thread and submit_bio() from application. Back
    upon that time, dm_wq_work() thread calls __split_and_process_bio()
    directly and thus will not trigger this issue since bio_list doesn't
    exist here. So this issue will only be triggered from application
    calling submit_bio(), and the fix has to check if current->bio_list is
    non-NULL to distinguish this case.
    
    However since commit 0c2915b8c6db1 ("dm: fix missing imposition of
    queue_limits from dm_wq_work() thread"), dm_wq_work() thread calls
    submit_bio_noacct() and thus also uses bio_list. Since then all entries
    into __split_and_process_bio() are under protection of bio_list, and
    thus the checking of current->bio_list when determinning if the
    depth-first principle should be used, seems kind of nonsense. After all
    the checking always succeeds now.
    
    Fixes: 0c2915b8c6db1 ("dm: fix missing imposition of queue_limits from dm_wq_work() thread")
    	Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>
    	Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    (cherry picked from commit 985eabdcfe3aad1aea6fc195dafff503f303aa3a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1594,7 +1594,7 @@ static blk_qc_t __split_and_process_bio(struct mapped_device *md,
 		ci.sector_count = bio_sectors(bio);
 		while (ci.sector_count && !error) {
 			error = __split_and_process_non_flush(&ci);
-			if (current->bio_list && ci.sector_count && !error) {
+			if (ci.sector_count && !error) {
 				/*
 				 * Remainder must be passed to generic_make_request()
 				 * so that it gets handled *after* bios already submitted
