07d3d838951f33d9b5a138ef2d5fc0361d90bb0c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:13:21 -0400

    ASoC: Intel: Skylake: Shield against no-NHLT configurations
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    Rebuild_CHGLOG: - [sound] ALSA: ASoC: Intel: Skylake: Shield against no-NHLT configurations (Jaroslav Kysela) [1797509]
    Rebuild_FUZZ: 95.16%
    commit-author Cezary Rojewski <cezary.rojewski@intel.com>
    commit 9e6c382f5a6161eb55115fb56614b9827f2e7da3
    
    Some configurations expose no NHLT table at all within their
    /sys/firmware/acpi/tables. To prevent NULL-dereference errors from
    occurring, adjust probe flow and append additional safety checks in
    functions involved in NHLT lifecycle.
    
    	Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    	Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20200305145314.32579-5-cezary.rojewski@intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 9e6c382f5a6161eb55115fb56614b9827f2e7da3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/intel/skylake/skl.c
+++ sound/soc/intel/skylake/skl.c
@@ -636,6 +636,9 @@ static int skl_clock_device_register(struct skl_dev *skl)
 	struct platform_device_info pdevinfo = {NULL};
 	struct skl_clk_pdata *clk_pdata;
 
+	if (!skl->nhlt)
+		return 0;
+
 	clk_pdata = devm_kzalloc(&skl->pci->dev, sizeof(*clk_pdata),
 							GFP_KERNEL);
 	if (!clk_pdata)
@@ -1082,7 +1085,8 @@ out_dsp_free:
 out_clk_free:
 	skl_clock_device_unregister(skl);
 out_nhlt_free:
-	intel_nhlt_free(skl->nhlt);
+	if (skl->nhlt)
+		intel_nhlt_free(skl->nhlt);
 out_free:
 	skl_free(bus);
 
@@ -1131,7 +1135,8 @@ static void skl_remove(struct pci_dev *pci)
 	skl_dmic_device_unregister(skl);
 	skl_clock_device_unregister(skl);
 	skl_nhlt_remove_sysfs(skl);
-	intel_nhlt_free(skl->nhlt);
+	if (skl->nhlt)
+		intel_nhlt_free(skl->nhlt);
 	skl_free(bus);
 	dev_set_drvdata(&pci->dev, NULL);
 }
