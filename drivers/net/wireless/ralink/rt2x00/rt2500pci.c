8cbefd9600c6ad76228425e4a2d3cb7f970ef52d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:50:58 -0400

    rt2x00: pci: use generic power management
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Vaibhav Gupta <vaibhavgupta40@gmail.com>
    commit 560a218d1ce6ea36df6fb943a1065181df90a4d6
    
    Drivers using legacy PM have to manage PCI states and device's PM states
    themselves. They also need to take care of configuration registers.
    
    With improved and powerful support of generic PM, PCI Core takes care of
    above mentioned, device-independent, jobs.
    
    The callbacks make use of PCI helper functions like
    pci_save/restore_state(), pci_enable/disable_device() and
    pci_set_power_state() to do required operations. In generic mode, they are
    no longer needed.
    
    Change function parameter in both .suspend() and .resume() to
    "struct device*" type. Use dev_get_drvdata() to get drv data.
    
    The .suspend() callback is invoking rt2x00lib_suspend() which needs to be
    modified as generic rt2x00pci_suspend() has no pm_message_t type argument,
    passed to it, which is required by it according to its declaration.
    Although this variable remained unused in the function body. Hence, remove
    it from the function definition & declaration.
    
    rt2x00lib_suspend() is also invoked by rt2x00usb_suspend() and
    rt2x00soc_suspend(). Thus, modify the functional call accordingly in their
    function body.
    
    Earlier, .suspend() & .resume() were exported and were used by the
    following drivers:
        - drivers/net/wireless/ralink/rt2x00/rt2400pci.c
        - drivers/net/wireless/ralink/rt2x00/rt2500pci.c
        - drivers/net/wireless/ralink/rt2x00/rt2800pci.c
        - drivers/net/wireless/ralink/rt2x00/rt61pci.c
    
    Now, we only need to bind "struct dev_pm_ops" variable to
    "struct pci_driver". Thus, make the callbacks static. Declare an
    "extern const struct dev_pm_ops" variable and bind PM callbacks to it. Now,
    export the variable instead and use it in respective drivers.
    
    Compile-tested only.
    
    	Signed-off-by: Vaibhav Gupta <vaibhavgupta40@gmail.com>
    	Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200717110928.454867-1-vaibhavgupta40@gmail.com
    (cherry picked from commit 560a218d1ce6ea36df6fb943a1065181df90a4d6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/wireless/ralink/rt2x00/rt2500pci.c
+++ drivers/net/wireless/ralink/rt2x00/rt2500pci.c
@@ -2143,8 +2143,7 @@ static struct pci_driver rt2500pci_driver = {
 	.id_table	= rt2500pci_device_table,
 	.probe		= rt2500pci_probe,
 	.remove		= rt2x00pci_remove,
-	.suspend	= rt2x00pci_suspend,
-	.resume		= rt2x00pci_resume,
+	.driver.pm	= &rt2x00pci_pm_ops,
 };
 
 module_pci_driver(rt2500pci_driver);
