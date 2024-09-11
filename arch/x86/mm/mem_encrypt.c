0996ed026e52bd7141dc74c795b8c383624b9043
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:52:47 -0400

    x86/sev-es: Add SEV-ES Feature Detection
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    Rebuild_CHGLOG: - [x86] sev-es: Add SEV-ES Feature Detection (Vitaly Kuznetsov) [1868080]
    Rebuild_FUZZ: 94.74%
    commit-author Joerg Roedel <jroedel@suse.de>
    commit b57de6cd16395be1ebdaa9b489ffbf462bb585c4
    
    Add a sev_es_active() function for checking whether SEV-ES is enabled.
    Also cache the value of MSR_AMD64_SEV at boot to speed up the feature
    checking in the running code.
    
     [ bp: Remove "!!" in sev_active() too. ]
    
    	Signed-off-by: Joerg Roedel <jroedel@suse.de>
    	Signed-off-by: Borislav Petkov <bp@suse.de>
    	Reviewed-by: Kees Cook <keescook@chromium.org>
    Link: https://lkml.kernel.org/r/20200907131613.12703-37-joro@8bytes.org
    (cherry picked from commit b57de6cd16395be1ebdaa9b489ffbf462bb585c4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/mm/mem_encrypt.c
+++ arch/x86/mm/mem_encrypt.c
@@ -41,6 +41,7 @@
  * section is later cleared.
  */
 u64 sme_me_mask __section(.data) = 0;
+u64 sev_status __section(.data) = 0;
 EXPORT_SYMBOL(sme_me_mask);
 DEFINE_STATIC_KEY_FALSE(sev_enable_key);
 EXPORT_SYMBOL_GPL(sev_enable_key);
@@ -351,7 +352,13 @@ EXPORT_SYMBOL(sme_active);
 
 bool sev_active(void)
 {
-	return sme_me_mask && sev_enabled;
+	return sev_status & MSR_AMD64_SEV_ENABLED;
+}
+
+/* Needs to be called from non-instrumentable code */
+bool noinstr sev_es_active(void)
+{
+	return sev_status & MSR_AMD64_SEV_ES_ENABLED;
 }
 
 /* Override for DMA direct allocation check - ARCH_HAS_FORCE_DMA_UNENCRYPTED */
