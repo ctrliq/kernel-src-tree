39b09b44613faad8ae9e2293c3d2b9c74c0b8809
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:20:30 -0400

    fs: remove the unused SB_I_MULTIROOT flag
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Christoph Hellwig <hch@lst.de>
    commit 402dd2cf46b177be5bcb138b7d7fd8f38aa130e4
    
    The last user of SB_I_MULTIROOT is disappeared with commit f2aedb713c28
    ("NFS: Add fs_context support.")
    
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    	Reviewed-by: Jan Kara <jack@suse.cz>
    	Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 402dd2cf46b177be5bcb138b7d7fd8f38aa130e4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/namei.c
+++ fs/namei.c
@@ -564,8 +564,8 @@ static bool path_connected(const struct path *path)
 	struct vfsmount *mnt = path->mnt;
 	struct super_block *sb = mnt->mnt_sb;
 
-	/* Bind mounts and multi-root filesystems can have disconnected paths */
-	if (!(sb->s_iflags & SB_I_MULTIROOT) && (mnt->mnt_root == sb->s_root))
+	/* Bind mounts can have disconnected paths */
+	if (mnt->mnt_root == sb->s_root)
 		return true;
 
 	return is_subdir(path->dentry, mnt->mnt_root);
