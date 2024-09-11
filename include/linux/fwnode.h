0506f823f412047c12e572501ef113e3e9ff431e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:55:38 -0400

    device property: Add fwnode_get_name for returning the name of a node
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Sakari Ailus <sakari.ailus@linux.intel.com>
    commit bc0500c1e43d95cca5352d2345fb0769f314ba22
    
    The fwnode framework did not have means to obtain the name of a node. Add
    that now, in form of the fwnode_get_name() function and a corresponding
    get_name fwnode op. OF and ACPI support is included.
    
    	Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    	Acked-by: Rob Herring <robh@kernel.org> (for OF)
    	Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit bc0500c1e43d95cca5352d2345fb0769f314ba22)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/fwnode.h
+++ include/linux/fwnode.h
@@ -60,6 +60,7 @@ struct fwnode_reference_args {
  *				 otherwise.
  * @property_read_string_array: Read an array of string properties. Return zero
  *				on success, a negative error code otherwise.
+ * @get_name: Return the name of an fwnode.
  * @get_parent: Return the parent of an fwnode.
  * @get_next_child_node: Return the next child node in an iteration.
  * @get_named_child_node: Return a child node with a given name.
@@ -86,6 +87,7 @@ struct fwnode_operations {
 	(*property_read_string_array)(const struct fwnode_handle *fwnode_handle,
 				      const char *propname, const char **val,
 				      size_t nval);
+	const char *(*get_name)(const struct fwnode_handle *fwnode);
 	struct fwnode_handle *(*get_parent)(const struct fwnode_handle *fwnode);
 	struct fwnode_handle *
 	(*get_next_child_node)(const struct fwnode_handle *fwnode,
