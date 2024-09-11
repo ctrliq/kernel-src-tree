8d4004c3b47052be58145ff41890d138c57ae619
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:45:34 -0400

    b43: Use fallthrough pseudo-keyword
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-315.el8
    commit-author Gustavo A. R. Silva <gustavoars@kernel.org>
    commit 7b54281cb68575ca1c9d7e6d750b141d5c9ca98d
    
    Replace the existing /* fall through */ comments and its variants with
    the new pseudo-keyword macro fallthrough[1].
    
    [1] https://www.kernel.org/doc/html/v5.7/process/deprecated.html?highlight=fallthrough#implicit-switch-case-fall-through
    
    	Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    	Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200821064218.GA19502@embeddedor
    (cherry picked from commit 7b54281cb68575ca1c9d7e6d750b141d5c9ca98d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/wireless/broadcom/b43/main.c
+++ drivers/net/wireless/broadcom/b43/main.c
@@ -1887,7 +1887,7 @@ static void b43_handle_firmware_panic(struct b43_wldev *dev)
 	switch (reason) {
 	default:
 		b43dbg(dev->wl, "The panic reason is unknown.\n");
-		/* fallthrough */
+		fallthrough;
 	case B43_FWPANIC_DIE:
 		/* Do not restart the controller or firmware.
 		 * The device is nonfunctional from now on.
@@ -2281,7 +2281,7 @@ fw_ready:
 		size = be32_to_cpu(hdr->size);
 		if (size != ctx->blob->size - sizeof(struct b43_fw_header))
 			goto err_format;
-		/* fallthrough */
+		fallthrough;
 	case B43_FW_TYPE_IV:
 		if (hdr->ver != 1)
 			goto err_format;
@@ -3198,7 +3198,7 @@ static void b43_rate_memory_init(struct b43_wldev *dev)
 		b43_rate_memory_write(dev, B43_OFDM_RATE_36MB, 1);
 		b43_rate_memory_write(dev, B43_OFDM_RATE_48MB, 1);
 		b43_rate_memory_write(dev, B43_OFDM_RATE_54MB, 1);
-		/* fallthrough */
+		fallthrough;
 	case B43_PHYTYPE_B:
 		b43_rate_memory_write(dev, B43_CCK_RATE_1MB, 0);
 		b43_rate_memory_write(dev, B43_CCK_RATE_2MB, 0);
@@ -5349,7 +5349,7 @@ static void b43_supported_bands(struct b43_wldev *dev, bool *have_2ghz_phy,
 		/* There are 14e4:4321 PCI devs with 2.4 GHz BCM4321 (N-PHY) */
 		if (dev->phy.type != B43_PHYTYPE_G)
 			break;
-		/* fall through */
+		fallthrough;
 	case 0x4313: /* BCM4311 */
 	case 0x431a: /* BCM4318 */
 	case 0x432a: /* BCM4321 */
