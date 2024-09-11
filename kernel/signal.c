74950b277d1e840db18b00d2dcb5a7935a63bf5f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:01:33 -0400

    signal: make get_signal() return bool
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Christian Brauner <christian@brauner.io>
    commit 20ab7218d2507b2dbec9c053c2f1b96054e266b6
    
    make get_signal() already behaves like a boolean function.  Let's actually
    declare it as such too.
    
    Link: http://lkml.kernel.org/r/20180602103653.18181-18-christian@brauner.io
    	Signed-off-by: Christian Brauner <christian@brauner.io>
    	Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    	Cc: Al Viro <viro@zeniv.linux.org.uk>
    	Cc: Eric W. Biederman <ebiederm@xmission.com>
    	Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    	Cc: Ingo Molnar <mingo@kernel.org>
    	Cc: James Morris <james.morris@microsoft.com>
    	Cc: Kees Cook <keescook@chromium.org>
    	Cc: Oleg Nesterov <oleg@redhat.com>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Stephen Smalley <sds@tycho.nsa.gov>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 20ab7218d2507b2dbec9c053c2f1b96054e266b6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2333,7 +2333,7 @@ static int ptrace_signal(int signr, siginfo_t *info)
 	return signr;
 }
 
-int get_signal(struct ksignal *ksig)
+bool get_signal(struct ksignal *ksig)
 {
 	struct sighand_struct *sighand = current->sighand;
 	struct signal_struct *signal = current->signal;
@@ -2343,7 +2343,7 @@ int get_signal(struct ksignal *ksig)
 		task_work_run();
 
 	if (unlikely(uprobe_deny_signal()))
-		return 0;
+		return false;
 
 	/*
 	 * Do this once, we can't return to user-mode if freezing() == T.
