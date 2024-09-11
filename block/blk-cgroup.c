a3f6b60da848ea009c791b1ceae44c8b0e809311
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:43:24 -0400

    bfq: Fix bfq linkage error
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Pavel Begunkov <asml.silence@gmail.com>
    commit 89f3b6d62f2c7c1ed7b2e672be605016d9ff60f2
    
    Since commit 795fe54c2a828099e ("bfq: Add per-device weight"), bfq uses
    blkg_conf_prep() and blkg_conf_finish(), which are not exported. So, it
    causes linkage error if bfq compiled as a module.
    
    Fixes: 795fe54c2a828099e ("bfq: Add per-device weight")
    	Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 89f3b6d62f2c7c1ed7b2e672be605016d9ff60f2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -922,6 +922,7 @@ fail:
 	}
 	return ret;
 }
+EXPORT_SYMBOL_GPL(blkg_conf_prep);
 
 /**
  * blkg_conf_finish - finish up per-blkg config update
@@ -937,6 +938,7 @@ void blkg_conf_finish(struct blkg_conf_ctx *ctx)
 	rcu_read_unlock();
 	put_disk_and_module(ctx->disk);
 }
+EXPORT_SYMBOL_GPL(blkg_conf_finish);
 
 static int blkcg_print_stat(struct seq_file *sf, void *v)
 {
