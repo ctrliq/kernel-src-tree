04c28497b74cbf471051d7185115c065ff030977
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:32:49 -0400

    ASoC: Intel: sof_sdw: remove hdac-hdmi support
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-373.el8
    commit-author Kai Vehmanen <kai.vehmanen@linux.intel.com>
    commit b0cf3d3ccf31f31c9c415566968caf1405fc0893
    
    Remove support for using hdac_hdmi codec driver. No known products use
    this configuration and hdac_hdmi cannot support all the platforms
    sof_sdw does.
    
    This change also fixes a bug in Kconfig rules.
    SND_SOC_INTEL_SOUNDWIRE_SOF_MACH did not have a select SND_SOC_HDAC_HDMI
    and this could cause build failures.
    
    	Reported-by: Richard Fitzgerald <rf@opensource.cirrus.com>
    	Tested-by: Richard Fitzgerald <rf@opensource.cirrus.com>
    	Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    	Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20210621194057.21711-3-pierre-louis.bossart@linux.intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit b0cf3d3ccf31f31c9c415566968caf1405fc0893)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/intel/boards/sof_sdw.c
+++ sound/soc/intel/boards/sof_sdw.c
@@ -1258,8 +1258,6 @@ static int mc_probe(struct platform_device *pdev)
 	if (ret < 0)
 		return ret;
 
-	ctx->common_hdmi_codec_drv = mach->mach_params.common_hdmi_codec_drv;
-
 	/*
 	 * the default amp_num is zero for each codec and
 	 * amp_num will only be increased for active amp
