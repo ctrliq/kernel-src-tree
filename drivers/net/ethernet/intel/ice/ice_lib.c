175f64a29138c85e268617e9163c1db9c264e477
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:54:35 -0400

    ice: remove ring_active from ice_ring
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    commit e93d1c37a85b7276506c99ec964a4094d9c1bcf6
    
    This field is dead and driver is not making any use of it. Simply remove
    it.
    
    	Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    	Tested-by: Gurucharan G <gurucharanx.g@intel.com>
    	Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit e93d1c37a85b7276506c99ec964a4094d9c1bcf6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/ice/ice_lib.c
+++ drivers/net/ethernet/intel/ice/ice_lib.c
@@ -1285,7 +1285,6 @@ static int ice_vsi_alloc_rings(struct ice_vsi *vsi)
 
 		ring->q_index = i;
 		ring->reg_idx = vsi->txq_map[i];
-		ring->ring_active = false;
 		ring->vsi = vsi;
 		ring->tx_tstamps = &pf->ptp.port.tx;
 		ring->dev = dev;
@@ -1304,7 +1303,6 @@ static int ice_vsi_alloc_rings(struct ice_vsi *vsi)
 
 		ring->q_index = i;
 		ring->reg_idx = vsi->rxq_map[i];
-		ring->ring_active = false;
 		ring->vsi = vsi;
 		ring->netdev = vsi->netdev;
 		ring->dev = dev;
