b467e2998727e5763bb52dd55631aa5b0505d955
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:08:58 -0400

    blk-cgroup: move rcu locking from blkcg_bio_issue_check to blk_throtl_bio
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Christoph Hellwig <hch@lst.de>
    commit 93b8063804b62b55248e16499d853e1b20eff905
    
    The only thing in blkcg_bio_issue_check that needs to be under
    rcu_read_lock is blk_throtl_bio, so move the locking there.
    
    	Acked-by: Tejun Heo <tj@kernel.org>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 93b8063804b62b55248e16499d853e1b20eff905)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/blk-cgroup.h
+++ include/linux/blk-cgroup.h
@@ -554,7 +554,6 @@ static inline bool blkcg_bio_issue_check(struct request_queue *q,
 	struct blkcg_gq *blkg = bio->bi_blkg;
 	bool throtl = false;
 
-	rcu_read_lock();
 	throtl = blk_throtl_bio(q, blkg, bio);
 	if (!throtl) {
 		struct blkg_iostat_set *bis;
@@ -588,7 +587,6 @@ static inline bool blkcg_bio_issue_check(struct request_queue *q,
 
 	blkcg_bio_issue_init(bio);
 
-	rcu_read_unlock();
 	return !throtl;
 }
 
