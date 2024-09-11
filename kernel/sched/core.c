ee64f9b13bfe8b442533d641e1715dd922e5279d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:51:00 -0400

    sched: Avoid double preemption in __cond_resched_*lock*()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-394.el8
    commit-author Peter Zijlstra <peterz@infradead.org>
    commit 7e406d1ff39b8ee574036418a5043c86723170cf
    
    For PREEMPT/DYNAMIC_PREEMPT the *_unlock() will already trigger a
    preemption, no point in then calling preempt_schedule_common()
    *again*.
    
    Use _cond_resched() instead, since this is a NOP for the preemptible
    configs while it provide a preemption point for the others.
    
    	Reported-by: xuhaifeng <xuhaifeng@oppo.com>
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/YcGnvDEYBwOiV0cR@hirez.programming.kicks-ass.net
    (cherry picked from commit 7e406d1ff39b8ee574036418a5043c86723170cf)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6085,9 +6085,7 @@ int __cond_resched_lock(spinlock_t *lock)
 
 	if (spin_needbreak(lock) || resched) {
 		spin_unlock(lock);
-		if (resched)
-			preempt_schedule_common();
-		else
+		if (!_cond_resched())
 			cpu_relax();
 		ret = 1;
 		spin_lock(lock);
@@ -6105,9 +6103,7 @@ int __cond_resched_rwlock_read(rwlock_t *lock)
 
 	if (rwlock_needbreak(lock) || resched) {
 		read_unlock(lock);
-		if (resched)
-			preempt_schedule_common();
-		else
+		if (!_cond_resched())
 			cpu_relax();
 		ret = 1;
 		read_lock(lock);
@@ -6125,9 +6121,7 @@ int __cond_resched_rwlock_write(rwlock_t *lock)
 
 	if (rwlock_needbreak(lock) || resched) {
 		write_unlock(lock);
-		if (resched)
-			preempt_schedule_common();
-		else
+		if (!_cond_resched())
 			cpu_relax();
 		ret = 1;
 		write_lock(lock);
