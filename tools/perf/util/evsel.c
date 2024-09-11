8b0846ee9b242b04e85cd9dd3f80488bcba0597a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:30:59 -0400

    perf evsel: Rename perf_evsel__exit() to evsel__exit()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Arnaldo Carvalho de Melo <acme@redhat.com>
    commit 30f7c59124bb0ad42f74956613fc244bc4064840
    
    As it is a 'struct evsel' method, not part of tools/lib/perf/, aka
    libperf, to whom the perf_ prefix belongs.
    
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: Jiri Olsa <jolsa@kernel.org>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 30f7c59124bb0ad42f74956613fc244bc4064840)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -1260,7 +1260,7 @@ static void perf_evsel__free_config_terms(struct evsel *evsel)
 	}
 }
 
-void perf_evsel__exit(struct evsel *evsel)
+void evsel__exit(struct evsel *evsel)
 {
 	assert(list_empty(&evsel->core.node));
 	assert(evsel->evlist == NULL);
@@ -1280,7 +1280,7 @@ void perf_evsel__exit(struct evsel *evsel)
 
 void evsel__delete(struct evsel *evsel)
 {
-	perf_evsel__exit(evsel);
+	evsel__exit(evsel);
 	free(evsel);
 }
 
