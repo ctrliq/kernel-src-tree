4cfa203dba87430aa84d48ef0c5159bd035a5708
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:15:08 -0400

    xfs: Remove xfs_trans_roll in xfs_attr_node_removename
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Allison Collins <allison.henderson@oracle.com>
    commit a237f2ddae4b79797fcb4290116acda38323da16
    
    A transaction roll is not necessary immediately after setting the
    INCOMPLETE flag when removing a node xattr entry with remote value
    blocks. The remote block invalidation that immediately follows setting
    the flag is an in-core only change. The next step after that is to start
    unmapping the remote blocks from the attr fork, but the xattr remove
    transaction reservation includes reservation for full tree splits of the
    dabtree and bmap tree. The remote block unmap code will roll the
    transaction as extents are unmapped and freed.
    
    	Signed-off-by: Allison Collins <allison.henderson@oracle.com>
    	Reviewed-by: Brian Foster <bfoster@redhat.com>
    	Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    	Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    	Acked-by: Dave Chinner <dchinner@redhat.com>
    (cherry picked from commit a237f2ddae4b79797fcb4290116acda38323da16)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/xfs/libxfs/xfs_attr.c
+++ fs/xfs/libxfs/xfs_attr.c
@@ -1066,10 +1066,6 @@ xfs_attr_node_removename(
 		if (error)
 			goto out;
 
-		error = xfs_trans_roll_inode(&args->trans, args->dp);
-		if (error)
-			goto out;
-
 		error = xfs_attr_rmtval_invalidate(args);
 		if (error)
 			return error;
