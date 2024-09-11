44a8e50669d6941b8efef565c5da02da65056860
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:00:36 -0400

    NFS: Fix up incorrect documentation
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Trond Myklebust <trond.myklebust@hammerspace.com>
    commit 6453bcd0d8bd67fce9e0f566250caf864b5032f6
    
    	Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    (cherry picked from commit 6453bcd0d8bd67fce9e0f566250caf864b5032f6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/nfs/delegation.c
+++ fs/nfs/delegation.c
@@ -113,7 +113,7 @@ nfs4_do_check_delegation(struct inode *inode, fmode_t flags, bool mark)
 	return ret;
 }
 /**
- * nfs_have_delegation - check if inode has a delegation, mark it
+ * nfs4_have_delegation - check if inode has a delegation, mark it
  * NFS_DELEGATION_REFERENCED if there is one.
  * @inode: inode to check
  * @flags: delegation types to check for
@@ -673,7 +673,7 @@ void nfs_inode_evict_delegation(struct inode *inode)
 }
 
 /**
- * nfs_inode_return_delegation - synchronously return a delegation
+ * nfs4_inode_return_delegation - synchronously return a delegation
  * @inode: inode to process
  *
  * This routine will always flush any dirty data to disk on the
@@ -696,7 +696,7 @@ int nfs4_inode_return_delegation(struct inode *inode)
 }
 
 /**
- * nfs_inode_return_delegation_on_close - asynchronously return a delegation
+ * nfs4_inode_return_delegation_on_close - asynchronously return a delegation
  * @inode: inode to process
  *
  * This routine is called on file close in order to determine if the
@@ -810,7 +810,7 @@ void nfs_expire_all_delegations(struct nfs_client *clp)
 }
 
 /**
- * nfs_super_return_all_delegations - return delegations for one superblock
+ * nfs_server_return_all_delegations - return delegations for one superblock
  * @server: pointer to nfs_server to process
  *
  */
