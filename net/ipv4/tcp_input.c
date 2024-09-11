8d21f533d093eb560c0daf30333c00b90c6cdd65
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:51:55 -0400

    tcp: make tcp_rcv_state_process() drop monitor friendly
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Eric Dumazet <edumazet@google.com>
    commit 37fd4e842391a1b947789969ae8454f1596735c8
    
    tcp_rcv_state_process() incorrectly drops packets
    instead of consuming it, making drop monitor very noisy,
    if not unusable.
    
    Calling tcp_time_wait() or tcp_done() is part
    of standard behavior, packets triggering these actions
    were not dropped.
    
    	Signed-off-by: Eric Dumazet <edumazet@google.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 37fd4e842391a1b947789969ae8454f1596735c8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -6399,7 +6399,7 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 			inet_csk_reset_keepalive_timer(sk, tmo);
 		} else {
 			tcp_time_wait(sk, TCP_FIN_WAIT2, tmo);
-			goto discard;
+			goto consume;
 		}
 		break;
 	}
@@ -6407,7 +6407,7 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 	case TCP_CLOSING:
 		if (tp->snd_una == tp->write_seq) {
 			tcp_time_wait(sk, TCP_TIME_WAIT, 0);
-			goto discard;
+			goto consume;
 		}
 		break;
 
@@ -6415,7 +6415,7 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 		if (tp->snd_una == tp->write_seq) {
 			tcp_update_metrics(sk);
 			tcp_done(sk);
-			goto discard;
+			goto consume;
 		}
 		break;
 	}
@@ -6468,6 +6468,10 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 discard:
 		tcp_drop(sk, skb);
 	}
+	return 0;
+
+consume:
+	__kfree_skb(skb);
 	return 0;
 }
 EXPORT_SYMBOL(tcp_rcv_state_process);
