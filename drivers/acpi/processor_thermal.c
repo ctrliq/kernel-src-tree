73038a73bc4cfc387758b855ad0f29010a03837c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:10:25 -0400

    ACPI: processor_thermal: Remove unused PREFIX for printing
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Hanjun Guo <guohanjun@huawei.com>
    commit 673a0796b1237d1cbe4947e711daa196858a138a
    
    The PREFIX "ACPI: " is not used in this file, remove it.
    
    	Signed-off-by: Hanjun Guo <guohanjun@huawei.com>
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 673a0796b1237d1cbe4947e711daa196858a138a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/processor_thermal.c
+++ drivers/acpi/processor_thermal.c
@@ -30,8 +30,6 @@
 #include <acpi/processor.h>
 #include <linux/uaccess.h>
 
-#define PREFIX "ACPI: "
-
 #ifdef CONFIG_CPU_FREQ
 
 /* If a passive cooling situation is detected, primarily CPUfreq is used, as it
