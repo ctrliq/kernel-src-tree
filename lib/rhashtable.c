2301688db085a609de20e57a91411540eb9f42df
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:10:35 -0400

    rhashtable: detect when object movement between tables might have invalidated a lookup
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author NeilBrown <neilb@suse.com>
    commit 82208d0d54ab85d8fedbb1c9a1960bd401a4ca1a
    
    Some users of rhashtables might need to move an object from one table
    to another -  this appears to be the reason for the incomplete usage
    of NULLS markers.
    
    To support these, we store a unique NULLS_MARKER at the end of
    each chain, and when a search fails to find a match, we check
    if the NULLS marker found was the expected one.  If not, the search
    may not have examined all objects in the target bucket, so it is
    repeated.
    
    The unique NULLS_MARKER is derived from the address of the
    head of the chain.  As this cannot be derived at load-time the
    static rhnull in rht_bucket_nested() needs to be initialised
    at run time.
    
    Any caller of a lookup function must still be prepared for the
    possibility that the object returned is in a different table - it
    might have been there for some time.
    
    Note that this does NOT provide support for other uses of
    NULLS_MARKERs such as allocating with SLAB_TYPESAFE_BY_RCU or changing
    the key of an object and re-inserting it in the same table.
    These could only be done safely if new objects were inserted
    at the *start* of a hash chain, and that is not currently the case.
    
    	Signed-off-by: NeilBrown <neilb@suse.com>
    	Acked-by: Herbert Xu <herbert@gondor.apana.org.au>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 82208d0d54ab85d8fedbb1c9a1960bd401a4ca1a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- lib/rhashtable.c
+++ lib/rhashtable.c
@@ -1183,8 +1183,7 @@ struct rhash_head __rcu **rht_bucket_nested(const struct bucket_table *tbl,
 					    unsigned int hash)
 {
 	const unsigned int shift = PAGE_SHIFT - ilog2(sizeof(void *));
-	static struct rhash_head __rcu *rhnull =
-		(struct rhash_head __rcu *)NULLS_MARKER(0);
+	static struct rhash_head __rcu *rhnull;
 	unsigned int index = hash & ((1 << tbl->nest) - 1);
 	unsigned int size = tbl->size >> tbl->nest;
 	unsigned int subhash = hash;
@@ -1202,8 +1201,11 @@ struct rhash_head __rcu **rht_bucket_nested(const struct bucket_table *tbl,
 		subhash >>= shift;
 	}
 
-	if (!ntbl)
+	if (!ntbl) {
+		if (!rhnull)
+			INIT_RHT_NULLS_HEAD(rhnull);
 		return &rhnull;
+	}
 
 	return &ntbl[subhash].bucket;
 
