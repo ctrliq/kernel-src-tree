0d6376734374e36fc5749f97953620b718ff77bc
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:19:29 -0400

    net: socket: fix SIOCGIFNAME in compat
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    Rebuild_CHGLOG: - [net] socket: fix SIOCGIFNAME in compat (Paolo Abeni) [1738492]
    Rebuild_FUZZ: 92.96%
    commit-author Johannes Berg <johannes.berg@intel.com>
    commit c6c9fee35dc27362b7bac34b2fc9f5b8ace2e22c
    
    As reported by Robert O'Callahan in
    https://bugzilla.kernel.org/show_bug.cgi?id=202273
    reverting the previous changes in this area broke
    the SIOCGIFNAME ioctl in compat again (I'd previously
    fixed it after his previous report of breakage in
    https://bugzilla.kernel.org/show_bug.cgi?id=199469).
    
    This is obviously because I fixed SIOCGIFNAME more or
    less by accident.
    
    Fix it explicitly now by making it pass through the
    restored compat translation code.
    
    	Cc: stable@vger.kernel.org
    Fixes: 4cf808e7ac32 ("kill dev_ifname32()")
    	Reported-by: Robert O'Callahan <robert@ocallahan.org>
    	Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c6c9fee35dc27362b7bac34b2fc9f5b8ace2e22c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/socket.c
+++ net/socket.c
@@ -3147,6 +3147,7 @@ static int compat_ifreq_ioctl(struct net *net, struct socket *sock,
 		case SIOCGIFTXQLEN:
 		case SIOCGMIIPHY:
 		case SIOCGMIIREG:
+		case SIOCGIFNAME:
 			if (copy_in_user(uifr32, uifr, sizeof(*uifr32)))
 				err = -EFAULT;
 			break;
@@ -3370,6 +3371,7 @@ static int compat_sock_ioctl_trans(struct file *file, struct socket *sock,
 	case SIOCSIFTXQLEN:
 	case SIOCBRADDIF:
 	case SIOCBRDELIF:
+	case SIOCGIFNAME:
 	case SIOCSIFNAME:
 	case SIOCGMIIPHY:
 	case SIOCGMIIREG:
@@ -3384,7 +3386,6 @@ static int compat_sock_ioctl_trans(struct file *file, struct socket *sock,
 	case SIOCBONDRELEASE:
 	case SIOCBONDSETHWADDR:
 	case SIOCBONDCHANGEACTIVE:
-	case SIOCGIFNAME:
 		return sock_do_ioctl(net, sock, cmd, arg);
 	}
 
