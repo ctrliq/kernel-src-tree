528903e4732031d804095dfeb5e67d1eaf401b1c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:38:26 -0400

    SUNRPC: Move reset of TCP state variables into the reconnect code
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Trond Myklebust <trond.myklebust@hammerspace.com>
    commit e1806c7bfb803408df4dc53dfe502ffab0f46a67
    
    Rather than resetting state variables in socket state_change() callback,
    do it in the sunrpc TCP connect function itself.
    
    	Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    (cherry picked from commit e1806c7bfb803408df4dc53dfe502ffab0f46a67)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -1610,13 +1610,6 @@ static void xs_tcp_state_change(struct sock *sk)
 	case TCP_ESTABLISHED:
 		spin_lock(&xprt->transport_lock);
 		if (!xprt_test_and_set_connected(xprt)) {
-
-			/* Reset TCP record info */
-			transport->recv.offset = 0;
-			transport->recv.len = 0;
-			transport->recv.copied = 0;
-			transport->recv.flags =
-				TCP_RCV_COPY_FRAGHDR | TCP_RCV_COPY_XID;
 			xprt->connect_cookie++;
 			clear_bit(XPRT_SOCK_CONNECTING, &transport->sock_state);
 			xprt_clear_connecting(xprt);
@@ -2392,6 +2385,12 @@ static int xs_tcp_finish_connecting(struct rpc_xprt *xprt, struct socket *sock)
 		goto out;
 
 	xs_set_memalloc(xprt);
+
+	/* Reset TCP record info */
+	transport->recv.offset = 0;
+	transport->recv.len = 0;
+	transport->recv.copied = 0;
+	transport->recv.flags = TCP_RCV_COPY_FRAGHDR | TCP_RCV_COPY_XID;
 
 	/* Tell the socket layer to start connecting... */
 	set_bit(XPRT_SOCK_CONNECTING, &transport->sock_state);
