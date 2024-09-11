556b0a8cf9f62088fc2bb3beea5889813a28bcc1
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:12:48 -0400

    ALSA: hda: Honor PM disablement in PM freeze and thaw_noirq ops
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Takashi Iwai <tiwai@suse.de>
    commit 10db5bccc390e8e4bd9fcd1fbd4f1b23f271a405
    
    freeze_noirq and thaw_noirq need to check the PM availability like
    other PM ops.  There are cases where the device got disabled due to
    the error, and the PM operation should be ignored for that.
    
    Fixes: 3e6db33aaf1d ("ALSA: hda - Set SKL+ hda controller power at freeze() and thaw()")
    BugLink: https://bugzilla.kernel.org/show_bug.cgi?id=207043
    Link: https://lore.kernel.org/r/20200413082034.25166-3-tiwai@suse.de
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 10db5bccc390e8e4bd9fcd1fbd4f1b23f271a405)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1073,6 +1073,8 @@ static int azx_freeze_noirq(struct device *dev)
 	struct azx *chip = card->private_data;
 	struct pci_dev *pci = to_pci_dev(dev);
 
+	if (!azx_is_pm_ready(card))
+		return 0;
 	if (chip->driver_type == AZX_DRIVER_SKL)
 		pci_set_power_state(pci, PCI_D3hot);
 
@@ -1085,6 +1087,8 @@ static int azx_thaw_noirq(struct device *dev)
 	struct azx *chip = card->private_data;
 	struct pci_dev *pci = to_pci_dev(dev);
 
+	if (!azx_is_pm_ready(card))
+		return 0;
 	if (chip->driver_type == AZX_DRIVER_SKL)
 		pci_set_power_state(pci, PCI_D0);
 
