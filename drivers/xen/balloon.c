b2f0e2599691e7b5b18d56e3a401ef739c8406f8
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:17:40 -0400

    xen/balloon: Drop __balloon_append()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author David Hildenbrand <david@redhat.com>
    commit dde3285ffa1a75dbd67c6a61e5f3eba2267c6593
    
    Let's simply use balloon_append() directly.
    
    	Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    	Cc: Juergen Gross <jgross@suse.com>
    	Cc: Stefano Stabellini <sstabellini@kernel.org>
    	Signed-off-by: David Hildenbrand <david@redhat.com>
    	Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    	Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    (cherry picked from commit dde3285ffa1a75dbd67c6a61e5f3eba2267c6593)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/xen/balloon.c
+++ drivers/xen/balloon.c
@@ -162,7 +162,7 @@ static void scrub_page(struct page *page)
 }
 
 /* balloon_append: add the given page to the balloon. */
-static void __balloon_append(struct page *page)
+static void balloon_append(struct page *page)
 {
 	/* Lowmem is re-populated first, so highmem pages go at list tail. */
 	if (PageHighMem(page)) {
@@ -173,11 +173,6 @@ static void __balloon_append(struct page *page)
 		balloon_stats.balloon_low++;
 	}
 	wake_up(&balloon_wq);
-}
-
-static void balloon_append(struct page *page)
-{
-	__balloon_append(page);
 }
 
 /* balloon_retrieve: rescue a page from the balloon, if it is not empty. */
@@ -429,7 +424,7 @@ static void xen_online_page(struct page *page, unsigned int order)
 		p = pfn_to_page(start_pfn + i);
 		__online_page_set_limits(p);
 		__SetPageOffline(p);
-		__balloon_append(p);
+		balloon_append(p);
 	}
 	mutex_unlock(&balloon_mutex);
 }
@@ -787,7 +782,7 @@ static void __init balloon_add_region(unsigned long start_pfn,
 		   include the boot-time balloon extension, so
 		   don't subtract from it. */
 		__SetPageOffline(page);
-		__balloon_append(page);
+		balloon_append(page);
 	}
 
 	balloon_stats.total_pages += extra_pfn_end - start_pfn;
