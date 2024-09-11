8bbcb35913a7e56a6bc24411ae620c15f468e680
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:46:33 -0400

    NFS: Remove unnecessary inode locking in nfs_llseek_dir()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Trond Myklebust <trond.myklebust@hammerspace.com>
    commit 83f2c45e63935a325f73bde98b1609e0976a12e0
    
    Remove the contentious inode lock, and instead provide thread safety
    using the file->f_lock spinlock.
    
    	Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    	Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    (cherry picked from commit 83f2c45e63935a325f73bde98b1609e0976a12e0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -954,7 +954,6 @@ out:
 
 static loff_t nfs_llseek_dir(struct file *filp, loff_t offset, int whence)
 {
-	struct inode *inode = file_inode(filp);
 	struct nfs_open_dir_context *dir_ctx = filp->private_data;
 
 	dfprintk(FILE, "NFS: llseek dir(%pD2, %lld, %d)\n",
@@ -966,15 +965,15 @@ static loff_t nfs_llseek_dir(struct file *filp, loff_t offset, int whence)
 	case SEEK_SET:
 		if (offset < 0)
 			return -EINVAL;
-		inode_lock(inode);
+		spin_lock(&filp->f_lock);
 		break;
 	case SEEK_CUR:
 		if (offset == 0)
 			return filp->f_pos;
-		inode_lock(inode);
+		spin_lock(&filp->f_lock);
 		offset += filp->f_pos;
 		if (offset < 0) {
-			inode_unlock(inode);
+			spin_unlock(&filp->f_lock);
 			return -EINVAL;
 		}
 	}
@@ -986,7 +985,7 @@ static loff_t nfs_llseek_dir(struct file *filp, loff_t offset, int whence)
 			dir_ctx->dir_cookie = 0;
 		dir_ctx->duped = 0;
 	}
-	inode_unlock(inode);
+	spin_unlock(&filp->f_lock);
 	return offset;
 }
 
