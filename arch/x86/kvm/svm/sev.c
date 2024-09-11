66aa7a85cd2984fbdba0417c0d77887bb1f33ae3
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:23:33 -0400

    KVM: SVM: Return -EFAULT if copy_to_user() for SEV mig packet header fails
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-331.el8
    commit-author Sean Christopherson <seanjc@google.com>
    commit b4a693924aab93f3747465b2261add46c82c3220
    
    Return -EFAULT if copy_to_user() fails; if accessing user memory faults,
    copy_to_user() returns the number of bytes remaining, not an error code.
    
    	Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    	Cc: Steve Rutherford <srutherford@google.com>
    	Cc: Brijesh Singh <brijesh.singh@amd.com>
    	Cc: Ashish Kalra <ashish.kalra@amd.com>
    Fixes: d3d1af85e2c7 ("KVM: SVM: Add KVM_SEND_UPDATE_DATA command")
    	Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20210506175826.2166383-2-seanjc@google.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit b4a693924aab93f3747465b2261add46c82c3220)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -1250,8 +1250,9 @@ static int sev_send_update_data(struct kvm *kvm, struct kvm_sev_cmd *argp)
 	}
 
 	/* Copy packet header to userspace. */
-	ret = copy_to_user((void __user *)(uintptr_t)params.hdr_uaddr, hdr,
-				params.hdr_len);
+	if (copy_to_user((void __user *)(uintptr_t)params.hdr_uaddr, hdr,
+			 params.hdr_len))
+		ret = -EFAULT;
 
 e_free_trans_data:
 	kfree(trans_data);
