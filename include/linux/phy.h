d89c830b3444ff647c537cb2ac44b3c9db33ad5a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:05:51 -0400

    net: phy: remove state PHY_CHANGELINK
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [kernel] phy: remove state PHY_CHANGELINK (Petr Oros) [1772010]
    Rebuild_FUZZ: 92.75%
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit e302c2a5fe0ca63b8fcc93389917625f486e0670
    
    Since recent changes to the phylib state machine state PHY_CHANGELINK
    isn't used any longer. Therefore let's remove it.
    
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit e302c2a5fe0ca63b8fcc93389917625f486e0670)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -309,11 +309,6 @@ struct phy_device *mdiobus_scan(struct mii_bus *bus, int addr);
  * - irq or timer will set NOLINK if link goes down
  * - phy_stop moves to HALTED
  *
- * CHANGELINK: PHY experienced a change in link state
- * - timer moves to RUNNING if link
- * - timer moves to NOLINK if the link is down
- * - phy_stop moves to HALTED
- *
  * HALTED: PHY is up, but no polling or interrupts are done. Or
  * PHY is in an error state.
  *
@@ -332,7 +327,6 @@ enum phy_state {
 	PHY_RUNNING,
 	PHY_NOLINK,
 	PHY_FORCING,
-	PHY_CHANGELINK,
 	PHY_RESUMING
 };
 
