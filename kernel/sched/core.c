d1d62f7d724da1da6f99bce0814b92b5448e2fbc
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:25:07 -0400

    sched: Fix kernel-doc markup
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    commit 59a74b1544e1c07ffbfd1edff5fd73ce7d3d3146
    
    Kernel-doc requires that a kernel-doc markup to be immediately
    below the function prototype, as otherwise it will rename it.
    So, move sys_sched_yield() markup to the right place.
    
    Also fix the cpu_util() markup: Kernel-doc markups
    should use this format:
            identifier - description
    
    	Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    	Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/50cd6f460aeb872ebe518a8e9cfffda2df8bdb0a.1606823973.git.mchehab+huawei@kernel.org
    (cherry picked from commit 59a74b1544e1c07ffbfd1edff5fd73ce7d3d3146)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5362,14 +5362,6 @@ SYSCALL_DEFINE3(sched_getaffinity, pid_t, pid, unsigned int, len,
 	return ret;
 }
 
-/**
- * sys_sched_yield - yield the current processor to other threads.
- *
- * This function yields the current CPU to other tasks. If there are no
- * other threads running on this CPU then this function will return.
- *
- * Return: 0.
- */
 static void do_sched_yield(void)
 {
 	struct rq_flags rf;
@@ -5387,6 +5379,14 @@ static void do_sched_yield(void)
 	schedule();
 }
 
+/**
+ * sys_sched_yield - yield the current processor to other threads.
+ *
+ * This function yields the current CPU to other tasks. If there are no
+ * other threads running on this CPU then this function will return.
+ *
+ * Return: 0.
+ */
 SYSCALL_DEFINE0(sched_yield)
 {
 	do_sched_yield();
