150e429672fbe5e96c4342fb5348298950c6d51b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:09:23 -0400

    ALSA: pci: Constify snd_ac97_bus_ops definitions
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Takashi Iwai <tiwai@suse.de>
    commit 51055da51d1e92bb089c9b9e0ecb1be69396f808
    
    Now snd_ac97_bus() takes the const ops pointer, so we can define the
    snd_ac97_bus_ops locally as const as well for further optimization.
    
    There should be no functional changes by this patch.
    
    Link: https://lore.kernel.org/r/20200103081714.9560-28-tiwai@suse.de
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 51055da51d1e92bb089c9b9e0ecb1be69396f808)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/pci/atiixp_modem.c
+++ sound/pci/atiixp_modem.c
@@ -1048,7 +1048,7 @@ static int snd_atiixp_mixer_new(struct atiixp_modem *chip, int clock)
 	struct snd_ac97_template ac97;
 	int i, err;
 	int codec_count;
-	static struct snd_ac97_bus_ops ops = {
+	static const struct snd_ac97_bus_ops ops = {
 		.write = snd_atiixp_ac97_write,
 		.read = snd_atiixp_ac97_read,
 	};
