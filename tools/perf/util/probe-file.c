a6f3d46b090c116cbc3eabe40eb3c004e9277cb9
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:23:22 -0400

    perf probe: Support DW_AT_const_value constant value
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Masami Hiramatsu <mhiramat@kernel.org>
    commit 66f69b2197167cb99330c77a550da50f1f597abc
    
    Support DW_AT_const_value for variable assignment instead of location.
    Note that this requires ftrace supporting immediate value.
    
    	Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Cc: Ravi Bangoria <ravi.bangoria@linux.ibm.com>
    	Cc: Steven Rostedt (VMware) <rostedt@goodmis.org>
    	Cc: Tom Zanussi <tom.zanussi@linux.intel.com>
    Link: http://lore.kernel.org/lkml/157406476012.24476.16096289871757175775.stgit@devnote2
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 66f69b2197167cb99330c77a550da50f1f597abc)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/probe-file.c
+++ tools/perf/util/probe-file.c
@@ -1017,6 +1017,7 @@ enum ftrace_readme {
 	FTRACE_README_UPROBE_REF_CTR,
 	FTRACE_README_USER_ACCESS,
 	FTRACE_README_MULTIPROBE_EVENT,
+	FTRACE_README_IMMEDIATE_VALUE,
 	FTRACE_README_END,
 };
 
@@ -1031,6 +1032,7 @@ static struct {
 	DEFINE_TYPE(FTRACE_README_UPROBE_REF_CTR, "*ref_ctr_offset*"),
 	DEFINE_TYPE(FTRACE_README_USER_ACCESS, "*[u]<offset>*"),
 	DEFINE_TYPE(FTRACE_README_MULTIPROBE_EVENT, "*Create/append/*"),
+	DEFINE_TYPE(FTRACE_README_IMMEDIATE_VALUE, "*\\imm-value,*"),
 };
 
 static bool scan_ftrace_readme(enum ftrace_readme type)
@@ -1100,4 +1102,9 @@ bool user_access_is_supported(void)
 bool multiprobe_event_is_supported(void)
 {
 	return scan_ftrace_readme(FTRACE_README_MULTIPROBE_EVENT);
+}
+
+bool immediate_value_is_supported(void)
+{
+	return scan_ftrace_readme(FTRACE_README_IMMEDIATE_VALUE);
 }
