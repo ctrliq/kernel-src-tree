560e319010449863389ed2b822d43c87eca0c1df
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:16:00 -0400

    perf report/top: Add 'k' hotkey to zoom directly into the kernel map
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Arnaldo Carvalho de Melo <acme@redhat.com>
    commit 209f4e70a2f10bc6819eb20c5bc3988be31972c6
    
    As a convenience, equivalent to pressing Enter in a line with a kernel
    symbol and then selecting "Zoom" into the kernel DSO.
    
    	Reviewed-by: Jiri Olsa <jolsa@kernel.org>
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: Andi Kleen <ak@linux.intel.com>
    	Cc: Jin Yao <yao.jin@linux.intel.com>
    	Cc: Kan Liang <kan.liang@intel.com>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-vbnlnrpyfvz9deqoobtc3dz7@git.kernel.org
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 209f4e70a2f10bc6819eb20c5bc3988be31972c6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/ui/browsers/hists.c
+++ tools/perf/ui/browsers/hists.c
@@ -15,7 +15,9 @@
 #include "../../util/evsel.h"
 #include "../../util/evlist.h"
 #include "../../util/hist.h"
+#include "../../util/machine.h"
 #include "../../util/map.h"
+#include "../../util/maps.h"
 #include "../../util/symbol.h"
 #include "../../util/pstack.h"
 #include "../../util/sort.h"
@@ -2552,7 +2554,7 @@ add_dso_opt(struct hist_browser *browser, struct popup_action *act,
 	if (!hists__has(browser->hists, dso) || map == NULL)
 		return 0;
 
-	if (asprintf(optstr, "Zoom %s %s DSO",
+	if (asprintf(optstr, "Zoom %s %s DSO (use the 'k' hotkey to zoom directly into the kernel)",
 		     browser->hists->dso_filter ? "out of" : "into",
 		     __map__is_kernel(map) ? "the Kernel" : map->dso->short_name) < 0)
 		return 0;
@@ -2922,6 +2924,7 @@ static int perf_evsel__hists_browse(struct perf_evsel *evsel, int nr_events,
 	"E             Expand all callchains\n"				\
 	"F             Toggle percentage of filtered entries\n"		\
 	"H             Display column headers\n"			\
+	"k             Zoom into the kernel map\n"			\
 	"L             Change percent limit\n"				\
 	"m             Display context menu\n"				\
 	"S             Zoom into current Processor Socket\n"		\
@@ -3018,6 +3021,10 @@ static int perf_evsel__hists_browse(struct perf_evsel *evsel, int nr_events,
 		case 'd':
 			actions->ms.map = map;
 			do_zoom_dso(browser, actions);
+			continue;
+		case 'k':
+			if (browser->selection != NULL)
+				hists_browser__zoom_map(browser, browser->selection->maps->machine->vmlinux_map);
 			continue;
 		case 'V':
 			verbose = (verbose + 1) % 4;
