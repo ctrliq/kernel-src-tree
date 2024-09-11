0d278c755a5e4be1c607c51a4483cee9748b62e8
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:47:42 -0400

    bpftool: add peek command
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [tools] tool: add peek command (Yauheni Kaliuta) [1747341]
    Rebuild_FUZZ: 93.62%
    commit-author Stanislav Fomichev <sdf@google.com>
    commit 66cf6e0b1248e576468956cb33ad2dabc45163dc
    
    This is intended to be used with queues and stacks and be more
    user-friendly than 'lookup' without key/value.
    
    Example:
    bpftool map create /sys/fs/bpf/q type queue value 4 entries 10 name q
    bpftool map update pinned /sys/fs/bpf/q value 0 1 2 3
    bpftool map peek pinned /sys/fs/bpf/q
    value: 00 01 02 03
    
    	Signed-off-by: Stanislav Fomichev <sdf@google.com>
    	Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 66cf6e0b1248e576468956cb33ad2dabc45163dc)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/bpf/bpftool/map.c
+++ tools/bpf/bpftool/map.c
@@ -1188,6 +1188,7 @@ static int do_help(int argc, char **argv)
 		"       %s %s delete     MAP  key DATA\n"
 		"       %s %s pin        MAP  FILE\n"
 		"       %s %s event_pipe MAP [cpu N index M]\n"
+		"       %s %s peek       MAP\n"
 		"       %s %s help\n"
 		"\n"
 		"       " HELP_SPEC_MAP "\n"
@@ -1205,7 +1206,7 @@ static int do_help(int argc, char **argv)
 		bin_name, argv[-2], bin_name, argv[-2], bin_name, argv[-2],
 		bin_name, argv[-2], bin_name, argv[-2], bin_name, argv[-2],
 		bin_name, argv[-2], bin_name, argv[-2], bin_name, argv[-2],
-		bin_name, argv[-2]);
+		bin_name, argv[-2], bin_name, argv[-2]);
 
 	return 0;
 }
@@ -1222,6 +1223,7 @@ static const struct cmd cmds[] = {
 	{ "pin",	do_pin },
 	{ "event_pipe",	do_event_pipe },
 	{ "create",	do_create },
+	{ "peek",	do_lookup },
 	{ 0 }
 };
 
