a9d8a3b3e86533445fe97843711deea3eb1c260d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:22:08 -0400

    vmlinux.lds.h: Create section for protection against instrumentation
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Thomas Gleixner <tglx@linutronix.de>
    commit 6553896666433e7efec589838b400a2a652b3ffa
    
    Some code pathes, especially the low level entry code, must be protected
    against instrumentation for various reasons:
    
     - Low level entry code can be a fragile beast, especially on x86.
    
     - With NO_HZ_FULL RCU state needs to be established before using it.
    
    Having a dedicated section for such code allows to validate with tooling
    that no unsafe functions are invoked.
    
    Add the .noinstr.text section and the noinstr attribute to mark
    functions. noinstr implies notrace. Kprobes will gain a section check
    later.
    
    Provide also a set of markers: instrumentation_begin()/end()
    
    These are used to mark code inside a noinstr function which calls
    into regular instrumentable text section as safe.
    
    The instrumentation markers are only active when CONFIG_DEBUG_ENTRY is
    enabled as the end marker emits a NOP to prevent the compiler from merging
    the annotation points. This means the objtool verification requires a
    kernel compiled with this option.
    
    	Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    	Reviewed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    	Acked-by: Peter Zijlstra <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200505134100.075416272@linutronix.de
    (cherry picked from commit 6553896666433e7efec589838b400a2a652b3ffa)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -146,6 +146,10 @@ struct ftrace_likely_data {
 #define __malloc
 #endif
 
+/* Section for code which can't be instrumented at all */
+#define noinstr								\
+	noinline notrace __attribute((__section__(".noinstr.text")))
+
 /*
  * Allow us to avoid 'defined but not used' warnings on functions and data,
  * as well as force them to be emitted to the assembly file.
