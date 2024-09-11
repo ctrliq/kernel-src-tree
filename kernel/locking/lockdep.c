0bbbdfcf7e4680ddaca5189ca5b7deefa8165269
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:13:32 -0400

    locking/lockdep: Consolidate lock usage bit initialization
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Yuyang Du <duyuyang@gmail.com>
    commit 091806515124b20f8cff7927b4b7ff399483b109
    
    Lock usage bit initialization is consolidated into one function
    mark_usage(). Trivial readability improvement. No functional change.
    
    	Signed-off-by: Yuyang Du <duyuyang@gmail.com>
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    	Cc: bvanassche@acm.org
    	Cc: frederic@kernel.org
    	Cc: ming.lei@redhat.com
    	Cc: will.deacon@arm.com
    Link: https://lkml.kernel.org/r/20190506081939.74287-22-duyuyang@gmail.com
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 091806515124b20f8cff7927b4b7ff399483b109)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -3134,8 +3134,12 @@ void trace_softirqs_off(unsigned long ip)
 		debug_atomic_inc(redundant_softirqs_off);
 }
 
-static int mark_irqflags(struct task_struct *curr, struct held_lock *hlock)
+static int
+mark_usage(struct task_struct *curr, struct held_lock *hlock, int check)
 {
+	if (!check)
+		goto lock_used;
+
 	/*
 	 * If non-trylock use in a hardirq or softirq context, then
 	 * mark the lock as used in these contexts:
@@ -3179,6 +3183,11 @@ static int mark_irqflags(struct task_struct *curr, struct held_lock *hlock)
 		}
 	}
 
+lock_used:
+	/* mark it as used: */
+	if (!mark_lock(curr, hlock, LOCK_USED))
+		return 0;
+
 	return 1;
 }
 
@@ -3220,8 +3229,8 @@ int mark_lock_irq(struct task_struct *curr, struct held_lock *this,
 	return 1;
 }
 
-static inline int mark_irqflags(struct task_struct *curr,
-		struct held_lock *hlock)
+static inline int
+mark_usage(struct task_struct *curr, struct held_lock *hlock, int check)
 {
 	return 1;
 }
@@ -3508,11 +3517,8 @@ static int __lock_acquire(struct lockdep_map *lock, unsigned int subclass,
 #endif
 	hlock->pin_count = pin_count;
 
-	if (check && !mark_irqflags(curr, hlock))
-		return 0;
-
-	/* mark it as used: */
-	if (!mark_lock(curr, hlock, LOCK_USED))
+	/* Initialize the lock usage bit */
+	if (!mark_usage(curr, hlock, check))
 		return 0;
 
 	/*
