8f5afe61fd7ccaa2d68c533454bdba24011144aa
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:50:30 -0400

    netfilter: cttimeout: move ctnl_untimeout to nf_conntrack
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Harsha Sharma <harshasharmaiitr@gmail.com>
    commit 4e665afbd7bee29b44b5d22821b56207f8459e39
    
    As, ctnl_untimeout is required by nft_ct, so move ctnl_timeout from
    nfnetlink_cttimeout to nf_conntrack_timeout and rename as nf_ct_timeout.
    
    	Signed-off-by: Harsha Sharma <harshasharmaiitr@gmail.com>
    	Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (cherry picked from commit 4e665afbd7bee29b44b5d22821b56207f8459e39)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/net/netfilter/nf_conntrack_timeout.h
+++ include/net/netfilter/nf_conntrack_timeout.h
@@ -93,6 +93,7 @@ nf_ct_timeout_lookup(struct net *net, struct nf_conn *ct,
 #ifdef CONFIG_NF_CONNTRACK_TIMEOUT
 int nf_conntrack_timeout_init(void);
 void nf_conntrack_timeout_fini(void);
+void nf_ct_untimeout(struct net *net, struct ctnl_timeout *timeout);
 #else
 static inline int nf_conntrack_timeout_init(void)
 {
