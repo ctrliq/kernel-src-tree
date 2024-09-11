b94ef00cf67de1a6f38003085f596fe21762708c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:18:08 -0400

    drivers/net/usb/r8152: remove the unneeded variable "ret" in rtl8152_system_suspend
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author zhong jiang <zhongjiang@huawei.com>
    commit f741917e24e9de4cf5abe041ff1b4f28f9522387
    
    rtl8152_system_suspend defines the variable "ret", but it is not modified
    after initialization. So just remove it.
    
    	Signed-off-by: zhong jiang <zhongjiang@huawei.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f741917e24e9de4cf5abe041ff1b4f28f9522387)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -4427,7 +4427,6 @@ out1:
 static int rtl8152_system_suspend(struct r8152 *tp)
 {
 	struct net_device *netdev = tp->netdev;
-	int ret = 0;
 
 	netif_device_detach(netdev);
 
@@ -4442,7 +4441,7 @@ static int rtl8152_system_suspend(struct r8152 *tp)
 		napi_enable(napi);
 	}
 
-	return ret;
+	return 0;
 }
 
 static int rtl8152_suspend(struct usb_interface *intf, pm_message_t message)
