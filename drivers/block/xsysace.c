17349827094416d8ff03c04842b5e5863d07fdec
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:30:17 -0400

    xsysace: use bdev_check_media_change
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Christoph Hellwig <hch@lst.de>
    commit f094225bfcd6fc5c093e128ec0adb7a522b38b69
    
    Switch to use bdev_check_media_change instead of check_disk_change and
    call ace_revalidate_disk manually.  Given that ace_revalidate_disk only
    deals with media change events, the extra call into ->revalidate_disk
    from bdev_disk_changed is not required either, so stop wiring up the
    method.
    
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    	Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    	Reviewed-by: Hannes Reinecke <hare@suse.de>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit f094225bfcd6fc5c093e128ec0adb7a522b38b69)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/block/xsysace.c
+++ drivers/block/xsysace.c
@@ -925,7 +925,8 @@ static int ace_open(struct block_device *bdev, fmode_t mode)
 	ace->users++;
 	spin_unlock_irqrestore(&ace->lock, flags);
 
-	check_disk_change(bdev);
+	if (bdev_check_media_change(bdev))
+		ace_revalidate_disk(bdev->bd_disk);
 	mutex_unlock(&xsysace_mutex);
 
 	return 0;
@@ -969,7 +970,6 @@ static const struct block_device_operations ace_fops = {
 	.open = ace_open,
 	.release = ace_release,
 	.check_events = ace_check_events,
-	.revalidate_disk = ace_revalidate_disk,
 	.getgeo = ace_getgeo,
 };
 
