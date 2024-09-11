1b1f75accadcaac8ef7b5f0b8e04d2647fc547bb
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:09:48 -0400

    net: stmmac: Hold rtnl lock in suspend/resume callbacks
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Jose Abreu <Jose.Abreu@synopsys.com>
    commit 19e13cb27b998ff49f07e399b5871bfe5ba7e3f0
    
    We need to hold rnl lock in suspend and resume callbacks because phylink
    requires it. Otherwise we will get a WARN() in suspend and resume.
    
    Also, move phylink start and stop callbacks to inside device's internal
    lock so that we prevent concurrent HW accesses.
    
    Fixes: 74371272f97f ("net: stmmac: Convert to phylink and remove phylib logic")
    	Reported-by: Christophe ROULLIER <christophe.roullier@st.com>
    	Tested-by: Christophe ROULLIER <christophe.roullier@st.com>
    	Signed-off-by: Jose Abreu <joabreu@synopsys.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 19e13cb27b998ff49f07e399b5871bfe5ba7e3f0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -4725,9 +4725,11 @@ int stmmac_suspend(struct device *dev)
 	if (!ndev || !netif_running(ndev))
 		return 0;
 
-	phylink_stop(priv->phylink);
-
 	mutex_lock(&priv->lock);
+
+	rtnl_lock();
+	phylink_stop(priv->phylink);
+	rtnl_unlock();
 
 	netif_device_detach(ndev);
 	stmmac_stop_all_queues(priv);
@@ -4832,9 +4834,11 @@ int stmmac_resume(struct device *dev)
 
 	stmmac_start_all_queues(priv);
 
-	mutex_unlock(&priv->lock);
-
+	rtnl_lock();
 	phylink_start(priv->phylink);
+	rtnl_unlock();
+
+	mutex_unlock(&priv->lock);
 
 	return 0;
 }
