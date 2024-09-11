f4484306becb8d9737e1dc7e8f64ced5f78d4d5f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:49:46 -0400

    vgaarb: Add support for 64-bit frame buffer address
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Aaron Ma <aaron.ma@canonical.com>
    commit a81c9ab678802075b7942c41cf640d9d9866d2db
    
    EFI GOP uses 64-bit frame buffer address when some BIOS
    disabled CSM support. vgaarb only stores lfb_base,
    this will lead boot framebuffer to wrong device.
    
    Add ext_lfb_base support to use 64-bit fb address.
    
    	Signed-off-by: Aaron Ma <aaron.ma@canonical.com>
    	Acked-by: Alex Deucher <alexander.deucher@amd.com>
    	Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/1535739600-8842-1-git-send-email-aaron.ma@canonical.com
    (cherry picked from commit a81c9ab678802075b7942c41cf640d9d9866d2db)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/gpu/vga/vgaarb.c
+++ drivers/gpu/vga/vgaarb.c
@@ -1457,6 +1457,18 @@ static void __init vga_arb_select_default_device(void)
 	struct vga_device *vgadev;
 
 #if defined(CONFIG_X86) || defined(CONFIG_IA64)
+	u64 base = screen_info.lfb_base;
+	u64 size = screen_info.lfb_size;
+	u64 limit;
+	resource_size_t start, end;
+	unsigned long flags;
+	int i;
+
+	if (screen_info.capabilities & VIDEO_CAPABILITY_64BIT_BASE)
+		base |= (u64)screen_info.ext_lfb_base << 32;
+
+	limit = base + size;
+
 	list_for_each_entry(vgadev, &vga_list, list) {
 		struct device *dev = &vgadev->pdev->dev;
 		/*
@@ -1467,11 +1479,6 @@ static void __init vga_arb_select_default_device(void)
 		 * Select the device owning the boot framebuffer if there is
 		 * one.
 		 */
-		resource_size_t start, end, limit;
-		unsigned long flags;
-		int i;
-
-		limit = screen_info.lfb_base + screen_info.lfb_size;
 
 		/* Does firmware framebuffer belong to us? */
 		for (i = 0; i < DEVICE_COUNT_RESOURCE; i++) {
@@ -1486,7 +1493,7 @@ static void __init vga_arb_select_default_device(void)
 			if (!start || !end)
 				continue;
 
-			if (screen_info.lfb_base < start || limit >= end)
+			if (base < start || limit >= end)
 				continue;
 
 			if (!vga_default_device())
