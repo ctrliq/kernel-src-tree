3d08288f483af3d83eb5de6626cda7b06db15709
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:25:32 -0400

    netdevsim: dev: Fix memory leak in nsim_dev_take_snapshot_write
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Gustavo A. R. Silva <gustavo@embeddedor.com>
    commit 3902baf9abfa320b21e38fe206d66d5e6688e721
    
    In case memory resources for dummy_data were allocated, release them
    before return.
    
    Addresses-Coverity-ID: 1491997 ("Resource leak")
    Fixes: 7ef19d3b1d5e ("devlink: report error once U32_MAX snapshot ids have been used")
    	Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    	Reviewed-by: Jakub Kicinski <kuba@kernel.org>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 3902baf9abfa320b21e38fe206d66d5e6688e721)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/netdevsim/dev.c
+++ drivers/net/netdevsim/dev.c
@@ -56,6 +56,7 @@ static ssize_t nsim_dev_take_snapshot_write(struct file *file,
 	err = devlink_region_snapshot_id_get(priv_to_devlink(nsim_dev), &id);
 	if (err) {
 		pr_err("Failed to get snapshot id\n");
+		kfree(dummy_data);
 		return err;
 	}
 	err = devlink_region_snapshot_create(nsim_dev->dummy_region,
