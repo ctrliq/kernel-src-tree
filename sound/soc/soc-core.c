4cb279071cf9b3e0ac7c62a83e1afdc70b1a77b7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:14:51 -0400

    ASoC: soc-core: Fix regression causing sysfs entries to disappear
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    Rebuild_CHGLOG: - [sound] ALSA: ASoC: soc-core: Fix regression causing sysfs entries to disappear (Jaroslav Kysela) [1869536]
    Rebuild_FUZZ: 95.59%
    commit-author Charles Keepax <ckeepax@opensource.cirrus.com>
    commit 5c74c9d34aec1ac756de6979dd5580096aba8643
    
    The allocation order of things in soc_new_pcm_runtime was changed to
    move the device_register before the allocation of the rtd structure.
    This was to allow the rtd allocation to be managed by devm. However
    currently the sysfs entries are added by device_register and their
    visibility depends on variables within the rtd structure, this causes
    the pmdown_time and dapm_widgets sysfs entries to be missing for all
    rtds.
    
    Correct this issue by manually calling device_add_groups after the
    appropriate information is available.
    
    Fixes: d918a37610b1 ("ASoC: soc-core: tidyup soc_new_pcm_runtime() alloc order")
    	Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://lore.kernel.org/r/20200730120715.637-1-ckeepax@opensource.cirrus.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 5c74c9d34aec1ac756de6979dd5580096aba8643)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -471,7 +471,6 @@ static struct snd_soc_pcm_runtime *soc_new_pcm_runtime(
 
 	dev->parent	= card->dev;
 	dev->release	= soc_release_rtd_dev;
-	dev->groups	= soc_dev_attr_groups;
 
 	dev_set_name(dev, "%s", dai_link->name);
 
@@ -527,6 +526,10 @@ static struct snd_soc_pcm_runtime *soc_new_pcm_runtime(
 
 	/* see for_each_card_rtds */
 	list_add_tail(&rtd->list, &card->rtd_list);
+
+	ret = device_add_groups(dev, soc_dev_attr_groups);
+	if (ret < 0)
+		goto free_rtd;
 
 	return rtd;
 
