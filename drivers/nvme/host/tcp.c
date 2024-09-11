ed8a181ec09c04bc1e208967648ee2438625444c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:34:57 -0400

    nvme-tcp: fix wrong setting of request iov_iter
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Sagi Grimberg <sagi@grimberg.me>
    commit cb9b870fba3eba57cf3bcd7c6c4d4aa88bc5fe70
    
    We might set the iov_iter direction wrong, which is harmless for this
    use-case, but get it right. Also this makes the code slightly cleaner.
    
    	Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit cb9b870fba3eba57cf3bcd7c6c4d4aa88bc5fe70)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nvme/host/tcp.c
+++ drivers/nvme/host/tcp.c
@@ -979,7 +979,6 @@ static int nvme_tcp_try_send_cmd_pdu(struct nvme_tcp_request *req)
 			req->state = NVME_TCP_SEND_DATA;
 			if (queue->data_digest)
 				crypto_ahash_init(queue->snd_hash);
-			nvme_tcp_init_iter(req, WRITE);
 		} else {
 			nvme_tcp_done_send_req(queue);
 		}
@@ -1012,8 +1011,6 @@ static int nvme_tcp_try_send_data_pdu(struct nvme_tcp_request *req)
 		req->state = NVME_TCP_SEND_DATA;
 		if (queue->data_digest)
 			crypto_ahash_init(queue->snd_hash);
-		if (!req->data_sent)
-			nvme_tcp_init_iter(req, WRITE);
 		return 1;
 	}
 	req->offset += ret;
@@ -2264,12 +2261,12 @@ static blk_status_t nvme_tcp_setup_cmd_pdu(struct nvme_ns *ns,
 	req->data_len = blk_rq_nr_phys_segments(rq) ?
 				blk_rq_payload_bytes(rq) : 0;
 	req->curr_bio = rq->bio;
+	if (req->curr_bio)
+		nvme_tcp_init_iter(req, rq_data_dir(rq));
 
 	if (rq_data_dir(rq) == WRITE &&
 	    req->data_len <= nvme_tcp_inline_data_size(queue))
 		req->pdu_len = req->data_len;
-	else if (req->curr_bio)
-		nvme_tcp_init_iter(req, READ);
 
 	pdu->hdr.type = nvme_tcp_cmd;
 	pdu->hdr.flags = 0;
