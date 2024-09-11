c73b4d34ff0b9cd07f7976fc4265e36ad9cb59ad
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:00:03 -0400

    mm/vma: make vma_is_foreign() available for general use
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Anshuman Khandual <anshuman.khandual@arm.com>
    commit 7969f2264f92871b9879796f08b455f737373718
    
    Idea of a foreign VMA with respect to the present context is very generic.
    But currently there are two identical definitions for this in powerpc and
    x86 platforms.  Lets consolidate those redundant definitions while making
    vma_is_foreign() available for general use later.  This should not cause
    any functional change.
    
    	Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Acked-by: Vlastimil Babka <vbabka@suse.cz>
    	Cc: Paul Mackerras <paulus@samba.org>
    	Cc: Michael Ellerman <mpe@ellerman.id.au>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    	Cc: Ingo Molnar <mingo@redhat.com>
    Link: http://lkml.kernel.org/r/1582782965-3274-3-git-send-email-anshuman.khandual@arm.com
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 7969f2264f92871b9879796f08b455f737373718)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -28,6 +28,7 @@
 #include <linux/memremap.h>
 #include <linux/overflow.h>
 #include <linux/sizes.h>
+#include <linux/sched.h>
 
 #include <linux/rh_kabi.h>
 
@@ -577,6 +578,16 @@ static inline bool vma_is_anonymous(struct vm_area_struct *vma)
 	return !vma->vm_ops;
 }
 
+static inline bool vma_is_foreign(struct vm_area_struct *vma)
+{
+	if (!current->mm)
+		return true;
+
+	if (current->mm != vma->vm_mm)
+		return true;
+
+	return false;
+}
 #ifdef CONFIG_SHMEM
 /*
  * The vma_is_shmem is not inline because it is used only by slow
