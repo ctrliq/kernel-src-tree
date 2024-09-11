168700b965042aa33815ee4efd99c1dab9f4c444
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:20:36 -0400

    dma-buf: Add struct dma-buf-map for storing struct dma_buf.vaddr_ptr
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Thomas Zimmermann <tzimmermann@suse.de>
    commit 01fd30da0474d5528d96a154cb50ce3b0352fd19
    
    The new type struct dma_buf_map represents a mapping of dma-buf memory
    into kernel space. It contains a flag, is_iomem, that signals users to
    access the mapped memory with I/O operations instead of regular loads
    and stores.
    
    It was assumed that DMA buffer memory can be accessed with regular load
    and store operations. Some architectures, such as sparc64, require the
    use of I/O operations to access dma-map buffers that are located in I/O
    memory. Providing struct dma_buf_map allows drivers to implement this.
    This was specifically a problem when refreshing the graphics framebuffer
    on such systems. [1]
    
    As the first step, struct dma_buf stores an instance of struct dma_buf_map
    internally. Afterwards, dma-buf's vmap and vunmap interfaces are be
    converted. Finally, affected drivers can be fixed.
    
    v3:
    	* moved documentation into separate patch
    	* test for NULL pointers with !<ptr>
    
    [1] https://lore.kernel.org/dri-devel/20200725191012.GA434957@ravnborg.org/
    
    	Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    	Reviewed-by: Christian König <christian.koenig@amd.com>
    	Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    	Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    	Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200925115601.23955-2-tzimmermann@suse.de
    (cherry picked from commit 01fd30da0474d5528d96a154cb50ce3b0352fd19)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -13,6 +13,7 @@
 #ifndef __DMA_BUF_H__
 #define __DMA_BUF_H__
 
+#include <linux/dma-buf-map.h>
 #include <linux/file.h>
 #include <linux/err.h>
 #include <linux/scatterlist.h>
@@ -308,7 +309,7 @@ struct dma_buf {
 	const struct dma_buf_ops *ops;
 	struct mutex lock;
 	unsigned vmapping_counter;
-	void *vmap_ptr;
+	struct dma_buf_map vmap_ptr;
 	const char *exp_name;
 	const char *name;
 	spinlock_t name_lock; /* spinlock to protect name access */
