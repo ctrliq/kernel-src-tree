8dee76957e8033485363c12df082b1a038343d47
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:22:05 -0400

    nvmet-rdma: support max(16KB, PAGE_SIZE) inline data
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Steve Wise <swise@opengridcomputing.com>
    commit 0d5ee2b2ab4f6776c361bc975c2323bc8b5cf349
    
    The patch enables inline data sizes using up to 4 recv sges, and capping
    the size at 16KB or at least 1 page size.  So on a 4K page system, up to
    16KB is supported, and for a 64K page system 1 page of 64KB is supported.
    
    We avoid > 0 order page allocations for the inline buffers by using
    multiple recv sges, one for each page.  If the device cannot support
    the configured inline data size due to lack of enough recv sges, then
    log a warning and reduce the inline size.
    
    Add a new configfs port attribute, called param_inline_data_size,
    to allow configuring the size of inline data for a given nvmf port.
    The maximum size allowed is still enforced by nvmet-rdma with
    NVMET_RDMA_MAX_INLINE_DATA_SIZE, which is now max(16KB, PAGE_SIZE).
    And the default size, if not specified via configfs, is still PAGE_SIZE.
    This preserves the existing behavior, but allows larger inline sizes
    for small page systems.  If the configured inline data size exceeds
    NVMET_RDMA_MAX_INLINE_DATA_SIZE, a warning is logged and the size is
    reduced.  If param_inline_data_size is set to 0, then inline data is
    disabled for that nvmf port.
    
    	Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    	Reviewed-by: Max Gurtovoy <maxg@mellanox.com>
    	Signed-off-by: Steve Wise <swise@opengridcomputing.com>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 0d5ee2b2ab4f6776c361bc975c2323bc8b5cf349)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nvme/target/nvmet.h
+++ drivers/nvme/target/nvmet.h
@@ -116,6 +116,7 @@ struct nvmet_port {
 	struct list_head		referrals;
 	void				*priv;
 	bool				enabled;
+	int				inline_data_size;
 };
 
 static inline struct nvmet_port *to_nvmet_port(struct config_item *item)
@@ -225,7 +226,6 @@ struct nvmet_req;
 struct nvmet_fabrics_ops {
 	struct module *owner;
 	unsigned int type;
-	unsigned int sqe_inline_size;
 	unsigned int msdbd;
 	bool has_keyed_sgls : 1;
 	void (*queue_response)(struct nvmet_req *req);
