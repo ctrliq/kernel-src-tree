a42439b7c04bdf5dd56955b8c0a2af19424cb3c7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:32:01 -0400

    mptcp: avoid a WARN on bad input.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Paolo Abeni <pabeni@redhat.com>
    commit d6085fe19b8ec53d8ea2d8a10390b7ab6978ac44
    
    Syzcaller has found a way to trigger the WARN_ON_ONCE condition
    in check_fully_established().
    
    The root cause is a legit fallback to TCP scenario, so replace
    the WARN with a plain message on a more strict condition.
    
    Fixes: f296234c98a8 ("mptcp: Add handling of incoming MP_JOIN requests")
    	Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d6085fe19b8ec53d8ea2d8a10390b7ab6978ac44)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/mptcp/options.c
+++ net/mptcp/options.c
@@ -715,8 +715,6 @@ static bool check_fully_established(struct mptcp_sock *msk, struct sock *sk,
 		goto fully_established;
 	}
 
-	WARN_ON_ONCE(subflow->can_ack);
-
 	/* If the first established packet does not contain MP_CAPABLE + data
 	 * then fallback to TCP
 	 */
@@ -726,6 +724,8 @@ static bool check_fully_established(struct mptcp_sock *msk, struct sock *sk,
 		return false;
 	}
 
+	if (unlikely(!READ_ONCE(msk->pm.server_side)))
+		pr_warn_once("bogus mpc option on established client sk");
 	subflow->fully_established = 1;
 	subflow->remote_key = mp_opt->sndr_key;
 	subflow->can_ack = 1;
