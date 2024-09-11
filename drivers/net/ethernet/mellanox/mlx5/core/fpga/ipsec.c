12bb07a97ac6310cb97a338485e3b2439ad86503
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:21:40 -0400

    net/mlx5: Fix memory leak when setting fpga ipsec caps
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Talat Batheesh <talatb@mellanox.com>
    commit fd7e848077c1a466b9187537adce16658f7cb94b
    
    Allocated memory for context should be freed once
    finished working with it.
    
    Fixes: d6c4f0298cec ("net/mlx5: Refactor accel IPSec code")
    	Signed-off-by: Talat Batheesh <talatb@mellanox.com>
    	Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
    	Reviewed-by: Tariq Toukan <tariqt@mellanox.com>
    	Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    (cherry picked from commit fd7e848077c1a466b9187537adce16658f7cb94b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/fpga/ipsec.c
+++ drivers/net/ethernet/mellanox/mlx5/core/fpga/ipsec.c
@@ -251,7 +251,7 @@ static void *mlx5_fpga_ipsec_cmd_exec(struct mlx5_core_dev *mdev,
 		return ERR_PTR(res);
 	}
 
-	/* Context will be freed by wait func after completion */
+	/* Context should be freed by the caller after completion. */
 	return context;
 }
 
@@ -424,10 +424,8 @@ static int mlx5_fpga_ipsec_set_caps(struct mlx5_core_dev *mdev, u32 flags)
 	cmd.cmd = htonl(MLX5_FPGA_IPSEC_CMD_OP_SET_CAP);
 	cmd.flags = htonl(flags);
 	context = mlx5_fpga_ipsec_cmd_exec(mdev, &cmd, sizeof(cmd));
-	if (IS_ERR(context)) {
-		err = PTR_ERR(context);
-		goto out;
-	}
+	if (IS_ERR(context))
+		return PTR_ERR(context);
 
 	err = mlx5_fpga_ipsec_cmd_wait(context);
 	if (err)
@@ -441,6 +439,7 @@ static int mlx5_fpga_ipsec_set_caps(struct mlx5_core_dev *mdev, u32 flags)
 	}
 
 out:
+	kfree(context);
 	return err;
 }
 
