567cac36d4414f1965e0a445873cf54d976a7c06
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:30:10 -0400

    block: grant IOPRIO_CLASS_RT to CAP_SYS_NICE
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Khazhismel Kumykov <khazhy@google.com>
    commit 9d3a39a5f1e45827b008fff1ee9cf3cac3409665
    
    CAP_SYS_ADMIN is too broad, and ionice fits into CAP_SYS_NICE's grouping.
    
    Retain CAP_SYS_ADMIN permission for backwards compatibility.
    
    	Signed-off-by: Khazhismel Kumykov <khazhy@google.com>
    	Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    	Acked-by: Serge Hallyn <serge@hallyn.com>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 9d3a39a5f1e45827b008fff1ee9cf3cac3409665)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- block/ioprio.c
+++ block/ioprio.c
@@ -68,7 +68,7 @@ int ioprio_check_cap(int ioprio)
 
 	switch (class) {
 		case IOPRIO_CLASS_RT:
-			if (!capable(CAP_SYS_ADMIN))
+			if (!capable(CAP_SYS_NICE) && !capable(CAP_SYS_ADMIN))
 				return -EPERM;
 			/* fall through */
 			/* rt has prio field too */
