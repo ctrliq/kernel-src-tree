c4eee5ec6aac8c32810e03ccb73689950cb54983
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:36:13 -0400

    blkcg: perpcu_ref init/exit should be done from blkg_alloc/free()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Tejun Heo <tj@kernel.org>
    commit ef069b97feec11c2399bbc5f6f347b35482105dc
    
    blkg alloc is performed as a separate step from the rest of blkg
    creation so that GFP_KERNEL allocations can be used when creating
    blkgs from configuration file writes because otherwise user actions
    may fail due to failures of opportunistic GFP_NOWAIT allocations.
    
    While making blkgs use percpu_ref, 7fcf2b033b84 ("blkcg: change blkg
    reference counting to use percpu_ref") incorrectly added unconditional
    opportunistic percpu_ref_init() to blkg_create() breaking this
    guarantee.
    
    This patch moves percpu_ref_init() to blkg_alloc() so makes it use
    @gfp_mask that blkg_alloc() is called with.  Also, percpu_ref_exit()
    is moved to blkg_free() for consistency.
    
    	Signed-off-by: Tejun Heo <tj@kernel.org>
    Fixes: 7fcf2b033b84 ("blkcg: change blkg reference counting to use percpu_ref")
    	Cc: Dennis Zhou <dennis@kernel.org>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit ef069b97feec11c2399bbc5f6f347b35482105dc)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -78,14 +78,13 @@ static void blkg_free(struct blkcg_gq *blkg)
 
 	blkg_rwstat_exit(&blkg->stat_ios);
 	blkg_rwstat_exit(&blkg->stat_bytes);
+	percpu_ref_exit(&blkg->refcnt);
 	kfree(blkg);
 }
 
 static void __blkg_release(struct rcu_head *rcu)
 {
 	struct blkcg_gq *blkg = container_of(rcu, struct blkcg_gq, rcu_head);
-
-	percpu_ref_exit(&blkg->refcnt);
 
 	/* release the blkcg and parent blkg refs this blkg has been holding */
 	css_put(&blkg->blkcg->css);
@@ -130,6 +129,9 @@ static struct blkcg_gq *blkg_alloc(struct blkcg *blkcg, struct request_queue *q,
 	blkg = kzalloc_node(sizeof(*blkg), gfp_mask, q->node);
 	if (!blkg)
 		return NULL;
+
+	if (percpu_ref_init(&blkg->refcnt, blkg_release, 0, gfp_mask))
+		goto err_free;
 
 	if (blkg_rwstat_init(&blkg->stat_bytes, gfp_mask) ||
 	    blkg_rwstat_init(&blkg->stat_ios, gfp_mask))
@@ -243,11 +245,6 @@ static struct blkcg_gq *blkg_create(struct blkcg *blkcg,
 		blkg_get(blkg->parent);
 	}
 
-	ret = percpu_ref_init(&blkg->refcnt, blkg_release, 0,
-			      GFP_NOWAIT | __GFP_NOWARN);
-	if (ret)
-		goto err_cancel_ref;
-
 	/* invoke per-policy init */
 	for (i = 0; i < BLKCG_MAX_POLS; i++) {
 		struct blkcg_policy *pol = blkcg_policy[i];
@@ -280,8 +277,6 @@ static struct blkcg_gq *blkg_create(struct blkcg *blkcg,
 	blkg_put(blkg);
 	return ERR_PTR(ret);
 
-err_cancel_ref:
-	percpu_ref_exit(&blkg->refcnt);
 err_put_congested:
 	wb_congested_put(wb_congested);
 err_put_css:
