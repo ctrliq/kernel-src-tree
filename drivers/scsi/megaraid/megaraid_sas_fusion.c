cbf2c7a2455607661fc62af4fe01043e4ece5cb7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:08:07 -0400

    scsi: megaraid: make some symbols static in megaraid_sas_fusion.c
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Jason Yan <yanaijie@huawei.com>
    commit 057d1c0d1b03707bffb8c54c2ca104b049d1ce5d
    
    Fix the following sparse warning:
    
    drivers/scsi/megaraid/megaraid_sas_fusion.c:180:1: warning: symbol
    'megasas_enable_intr_fusion' was not declared. Should it be static?
    drivers/scsi/megaraid/megaraid_sas_fusion.c:202:1: warning: symbol
    'megasas_disable_intr_fusion' was not declared. Should it be static?
    drivers/scsi/megaraid/megaraid_sas_fusion.c:4233:6: warning: symbol
    'megasas_refire_mgmt_cmd' was not declared. Should it be static?
    
    Link: https://lore.kernel.org/r/20200407092827.18074-4-yanaijie@huawei.com
    	Reported-by: Hulk Robot <hulkci@huawei.com>
    	Signed-off-by: Jason Yan <yanaijie@huawei.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 057d1c0d1b03707bffb8c54c2ca104b049d1ce5d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/megaraid/megaraid_sas_fusion.c
+++ drivers/scsi/megaraid/megaraid_sas_fusion.c
@@ -188,7 +188,7 @@ static inline bool megasas_check_same_4gb_region
  * megasas_enable_intr_fusion -	Enables interrupts
  * @regs:			MFI register set
  */
-void
+static void
 megasas_enable_intr_fusion(struct megasas_instance *instance)
 {
 	struct megasas_register_set __iomem *regs;
@@ -210,7 +210,7 @@ megasas_enable_intr_fusion(struct megasas_instance *instance)
  * megasas_disable_intr_fusion - Disables interrupt
  * @regs:			 MFI register set
  */
-void
+static void
 megasas_disable_intr_fusion(struct megasas_instance *instance)
 {
 	u32 mask = 0xFFFFFFFF;
@@ -4242,7 +4242,7 @@ void  megasas_reset_reply_desc(struct megasas_instance *instance)
  * megasas_refire_mgmt_cmd :	Re-fire management commands
  * @instance:				Controller's soft instance
 */
-void megasas_refire_mgmt_cmd(struct megasas_instance *instance,
+static void megasas_refire_mgmt_cmd(struct megasas_instance *instance,
 			     bool return_ioctl)
 {
 	int j;
