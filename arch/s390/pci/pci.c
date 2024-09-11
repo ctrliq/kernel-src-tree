c61a0ce23d24df3c9c6b6bccb9a6497b549091ca
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:54:13 -0400

    s390/pci: rename get_zdev_by_bus() to zdev_from_bus()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Niklas Schnelle <schnelle@linux.ibm.com>
    commit 7dcfe50f58d28e0e2ba79e9e4333888bcf9442a4
    
    Getting a zpci_dev via get_zdev_by_bus() uses the long lived reference
    held in zbus->function[devfn]. This is accounted for in
    pcibios_add_device() and pcibios_release_device().
    
    Therefore there is no need to increment the reference count in
    get_zdev_by_bus() as is done for get_zdev_by_fid(). Instead callers must
    not access the device after pcibios_release_device() was called which is
    necessary for common PCI code anyway. With this though the very similar
    naming may be misleading so rename get_zdev_by_bus() to zdev_from_bus()
    emphasizing that we are directly referencing the zdev via the bus.
    
    	Reviewed-by: Matthew Rosato <mjrosato@linux.ibm.com>
    	Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>
    	Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    (cherry picked from commit 7dcfe50f58d28e0e2ba79e9e4333888bcf9442a4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/s390/pci/pci.c
+++ arch/s390/pci/pci.c
@@ -370,7 +370,7 @@ EXPORT_SYMBOL(pci_iounmap);
 static int pci_read(struct pci_bus *bus, unsigned int devfn, int where,
 		    int size, u32 *val)
 {
-	struct zpci_dev *zdev = get_zdev_by_bus(bus, devfn);
+	struct zpci_dev *zdev = zdev_from_bus(bus, devfn);
 
 	return (zdev) ? zpci_cfg_load(zdev, where, val, size) : -ENODEV;
 }
@@ -378,7 +378,7 @@ static int pci_read(struct pci_bus *bus, unsigned int devfn, int where,
 static int pci_write(struct pci_bus *bus, unsigned int devfn, int where,
 		     int size, u32 val)
 {
-	struct zpci_dev *zdev = get_zdev_by_bus(bus, devfn);
+	struct zpci_dev *zdev = zdev_from_bus(bus, devfn);
 
 	return (zdev) ? zpci_cfg_store(zdev, where, val, size) : -ENODEV;
 }
