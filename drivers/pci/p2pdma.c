f12068c34eaa6d5947d94da81e3a995f83f68293
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:43:49 -0400

    PCI: Use scnprintf(), not snprintf(), in sysfs "show" functions
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-315.el8
    commit-author Krzysztof Wilczyński <kw@linux.com>
    commit e7a7499d841bfc9c7a4443f0fd7a04ae49bdbdd3
    
    Sysfs "show" methods should return the number of bytes printed into the
    buffer.  This is the return value of scnprintf() [1].
    
    snprintf(buf, size, ...) prints at most "size" bytes into "buf", but
    returns the number of bytes that *would* be printed if "buf" were large
    enough.
    
    Replace use of snprintf() with scnprintf().  No functional change intended.
    
    Related:
      https://patchwork.kernel.org/patch/9946759/#20969333
      https://lwn.net/Articles/69419
    
    [1] Documentation/filesystems/sysfs.rst
    
    [bhelgaas: squashed, commit log]
    	Suggested-by: Bjorn Helgaas <bhelgaas@google.com>
    Link: https://lore.kernel.org/r/20200824233918.26306-2-kw@linux.com
    Link: https://lore.kernel.org/r/20200824233918.26306-3-kw@linux.com
    Link: https://lore.kernel.org/r/20200824233918.26306-4-kw@linux.com
    	Signed-off-by: Krzysztof Wilczyński <kw@linux.com>
    	Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    (cherry picked from commit e7a7499d841bfc9c7a4443f0fd7a04ae49bdbdd3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/pci/p2pdma.c
+++ drivers/pci/p2pdma.c
@@ -34,7 +34,7 @@ static ssize_t size_show(struct device *dev, struct device_attribute *attr,
 	if (pdev->p2pdma->pool)
 		size = gen_pool_size(pdev->p2pdma->pool);
 
-	return snprintf(buf, PAGE_SIZE, "%zd\n", size);
+	return scnprintf(buf, PAGE_SIZE, "%zd\n", size);
 }
 static DEVICE_ATTR_RO(size);
 
@@ -47,7 +47,7 @@ static ssize_t available_show(struct device *dev, struct device_attribute *attr,
 	if (pdev->p2pdma->pool)
 		avail = gen_pool_avail(pdev->p2pdma->pool);
 
-	return snprintf(buf, PAGE_SIZE, "%zd\n", avail);
+	return scnprintf(buf, PAGE_SIZE, "%zd\n", avail);
 }
 static DEVICE_ATTR_RO(available);
 
@@ -56,8 +56,8 @@ static ssize_t published_show(struct device *dev, struct device_attribute *attr,
 {
 	struct pci_dev *pdev = to_pci_dev(dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n",
-			pdev->p2pdma->p2pmem_published);
+	return scnprintf(buf, PAGE_SIZE, "%d\n",
+			 pdev->p2pdma->p2pmem_published);
 }
 static DEVICE_ATTR_RO(published);
 
