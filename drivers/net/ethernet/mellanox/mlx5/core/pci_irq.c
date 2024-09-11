099a63d9f8c49dfd1a0fe8d1ce55fdb6c85aa9d8
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:44:09 -0400

    net/mlx5: Move affinity assignment into irq_request
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Shay Drory <shayd@nvidia.com>
    commit 30c6afa735db60b4fb6ca81da568d11d19aa2c15
    
    Move affinity binding of the IRQ to irq_request function in order to
    bind the IRQ before inserting it to the xarray.
    
    After this change, the IRQ is ready for use when inserted to the xarray.
    
    	Signed-off-by: Shay Drory <shayd@nvidia.com>
    	Reviewed-by: Moshe Shemesh <moshe@nvidia.com>
    	Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    (cherry picked from commit 30c6afa735db60b4fb6ca81da568d11d19aa2c15)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/pci_irq.c
+++ drivers/net/ethernet/mellanox/mlx5/core/pci_irq.c
@@ -195,7 +195,8 @@ static bool irq_pool_is_sf_pool(struct mlx5_irq_pool *pool)
 	return !strncmp("mlx5_sf", pool->name, strlen("mlx5_sf"));
 }
 
-static struct mlx5_irq *irq_request(struct mlx5_irq_pool *pool, int i)
+static struct mlx5_irq *irq_request(struct mlx5_irq_pool *pool, int i,
+				    struct cpumask *affinity)
 {
 	struct mlx5_core_dev *dev = pool->dev;
 	char name[MLX5_MAX_IRQ_NAME];
@@ -224,6 +225,10 @@ static struct mlx5_irq *irq_request(struct mlx5_irq_pool *pool, int i)
 		err = -ENOMEM;
 		goto err_cpumask;
 	}
+	if (affinity) {
+		cpumask_copy(irq->mask, affinity);
+		irq_set_affinity_hint(irq->irqn, irq->mask);
+	}
 	irq->pool = pool;
 	kref_init(&irq->kref);
 	irq->index = i;
@@ -235,6 +240,7 @@ static struct mlx5_irq *irq_request(struct mlx5_irq_pool *pool, int i)
 	}
 	return irq;
 err_xa:
+	irq_set_affinity_hint(irq->irqn, NULL);
 	free_cpumask_var(irq->mask);
 err_cpumask:
 	free_irq(irq->irqn, &irq->nh);
@@ -284,7 +290,6 @@ int mlx5_irq_get_index(struct mlx5_irq *irq)
 static struct mlx5_irq *irq_pool_create_irq(struct mlx5_irq_pool *pool,
 					    struct cpumask *affinity)
 {
-	struct mlx5_irq *irq;
 	u32 irq_index;
 	int err;
 
@@ -292,12 +297,7 @@ static struct mlx5_irq *irq_pool_create_irq(struct mlx5_irq_pool *pool,
 		       GFP_KERNEL);
 	if (err)
 		return ERR_PTR(err);
-	irq = irq_request(pool, irq_index);
-	if (IS_ERR(irq))
-		return irq;
-	cpumask_copy(irq->mask, affinity);
-	irq_set_affinity_hint(irq->irqn, irq->mask);
-	return irq;
+	return irq_request(pool, irq_index, affinity);
 }
 
 /* looking for the irq with the smallest refcount and the same affinity */
@@ -373,11 +373,7 @@ irq_pool_request_vector(struct mlx5_irq_pool *pool, int vecidx,
 		kref_get(&irq->kref);
 		goto unlock;
 	}
-	irq = irq_request(pool, vecidx);
-	if (IS_ERR(irq) || !affinity)
-		goto unlock;
-	cpumask_copy(irq->mask, affinity);
-	irq_set_affinity_hint(irq->irqn, irq->mask);
+	irq = irq_request(pool, vecidx, affinity);
 unlock:
 	mutex_unlock(&pool->lock);
 	return irq;
