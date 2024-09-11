999ae6642e18fc5624739dc2d7e48bc2b9e021ea
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:49:42 -0400

    mm/huge_memory.c: fix "orig_pud" set but not used
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Qian Cai <cai@lca.pw>
    commit 70516b936bb0afac4fa2fc54a5e61870ece998da
    
    Commit a00cc7d9dd93 ("mm, x86: add support for PUD-sized transparent
    hugepages") introduced pudp_huge_get_and_clear_full() but no one uses
    its return code.
    
    In order to not diverge from pmdp_huge_get_and_clear_full(), just change
    zap_huge_pud() to not assign the return value from
    pudp_huge_get_and_clear_full().
    
      mm/huge_memory.c: In function 'zap_huge_pud':
      mm/huge_memory.c:1982:8: warning: variable 'orig_pud' set but not used [-Wunused-but-set-variable]
        pud_t orig_pud;
              ^~~~~~~~
    
    Link: http://lkml.kernel.org/r/20190301221956.97493-1-cai@lca.pw
    	Signed-off-by: Qian Cai <cai@lca.pw>
    	Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    	Cc: Matthew Wilcox <willy@infradead.org>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 70516b936bb0afac4fa2fc54a5e61870ece998da)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1988,7 +1988,6 @@ spinlock_t *__pud_trans_huge_lock(pud_t *pud, struct vm_area_struct *vma)
 int zap_huge_pud(struct mmu_gather *tlb, struct vm_area_struct *vma,
 		 pud_t *pud, unsigned long addr)
 {
-	pud_t orig_pud;
 	spinlock_t *ptl;
 
 	ptl = __pud_trans_huge_lock(pud, vma);
@@ -2000,8 +1999,7 @@ int zap_huge_pud(struct mmu_gather *tlb, struct vm_area_struct *vma,
 	 * pgtable_trans_huge_withdraw after finishing pudp related
 	 * operations.
 	 */
-	orig_pud = pudp_huge_get_and_clear_full(tlb->mm, addr, pud,
-			tlb->fullmm);
+	pudp_huge_get_and_clear_full(tlb->mm, addr, pud, tlb->fullmm);
 	tlb_remove_pud_tlb_entry(tlb, pud, addr);
 	if (vma_is_special_huge(vma)) {
 		spin_unlock(ptl);
