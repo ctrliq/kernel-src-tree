d2bda32f4dcaae7cf288ca565f632662bf998c04
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:06:43 -0400

    ASoC: don't indicate error message for snd_soc_[pcm_]dai_xxx()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    commit 62462e018220895267450155b188f5804f54c202
    
    All snd_soc_dai_xxx() and snd_soc_pcm_dai_xxx() itself
    indicate error message if failed.
    Its caller doesn't need to indicate duplicated error message.
    This patch removes it.
    
    	Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87a6r5utaa.wl-kuninori.morimoto.gx@renesas.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 62462e018220895267450155b188f5804f54c202)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -3851,11 +3851,9 @@ snd_soc_dai_link_event_pre_pmu(struct snd_soc_dapm_widget *w,
 		source = path->source->priv;
 
 		ret = snd_soc_dai_startup(source, substream);
-		if (ret < 0) {
-			dev_err(source->dev,
-				"ASoC: startup() failed: %d\n", ret);
+		if (ret < 0)
 			goto out;
-		}
+
 		snd_soc_dai_activate(source, substream->stream);
 	}
 
@@ -3864,11 +3862,9 @@ snd_soc_dai_link_event_pre_pmu(struct snd_soc_dapm_widget *w,
 		sink = path->sink->priv;
 
 		ret = snd_soc_dai_startup(sink, substream);
-		if (ret < 0) {
-			dev_err(sink->dev,
-				"ASoC: startup() failed: %d\n", ret);
+		if (ret < 0)
 			goto out;
-		}
+
 		snd_soc_dai_activate(sink, substream->stream);
 	}
 
@@ -3963,11 +3959,7 @@ static int snd_soc_dai_link_event(struct snd_soc_dapm_widget *w,
 		snd_soc_dapm_widget_for_each_sink_path(w, path) {
 			sink = path->sink->priv;
 
-			ret = snd_soc_dai_digital_mute(sink, 0,
-						       SNDRV_PCM_STREAM_PLAYBACK);
-			if (ret != 0 && ret != -ENOTSUPP)
-				dev_warn(sink->dev,
-					 "ASoC: Failed to unmute: %d\n", ret);
+			snd_soc_dai_digital_mute(sink, 0, SNDRV_PCM_STREAM_PLAYBACK);
 			ret = 0;
 		}
 		break;
@@ -3976,11 +3968,7 @@ static int snd_soc_dai_link_event(struct snd_soc_dapm_widget *w,
 		snd_soc_dapm_widget_for_each_sink_path(w, path) {
 			sink = path->sink->priv;
 
-			ret = snd_soc_dai_digital_mute(sink, 1,
-						       SNDRV_PCM_STREAM_PLAYBACK);
-			if (ret != 0 && ret != -ENOTSUPP)
-				dev_warn(sink->dev,
-					 "ASoC: Failed to mute: %d\n", ret);
+			snd_soc_dai_digital_mute(sink, 1, SNDRV_PCM_STREAM_PLAYBACK);
 			ret = 0;
 		}
 
