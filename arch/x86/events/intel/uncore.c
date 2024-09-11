0b3d5af51ffcbc932b932ff49c4dd4379344269d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:40:05 -0400

    perf/x86/intel/uncore: Add Rocket Lake support
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Kan Liang <kan.liang@linux.intel.com>
    commit 43bc103a8044b9f7963aa1684efbdc9bd60939de
    
    For Rocket Lake, the MSR uncore, e.g., CBOX, ARB and CLOCKBOX, are the
    same as Tiger Lake. Share the perf code with it.
    
    For Rocket Lake and Tiger Lake, the 8th CBOX is not mapped into a
    different MSR space anymore. Add rkl_uncore_msr_init_box() to replace
    skl_uncore_msr_init_box().
    
    The IMC uncore is the similar to Ice Lake. Add new PCIIDs of IMC for
    Rocket Lake.
    
    	Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20201019153528.13850-4-kan.liang@linux.intel.com
    (cherry picked from commit 43bc103a8044b9f7963aa1684efbdc9bd60939de)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/events/intel/uncore.c
+++ arch/x86/events/intel/uncore.c
@@ -1608,6 +1608,11 @@ static const struct intel_uncore_init_fun tgl_l_uncore_init __initconst = {
 	.mmio_init = tgl_l_uncore_mmio_init,
 };
 
+static const struct intel_uncore_init_fun rkl_uncore_init __initconst = {
+	.cpu_init = tgl_uncore_cpu_init,
+	.pci_init = skl_uncore_pci_init,
+};
+
 static const struct intel_uncore_init_fun icx_uncore_init __initconst = {
 	.cpu_init = icx_uncore_cpu_init,
 	.pci_init = icx_uncore_pci_init,
@@ -1655,6 +1660,7 @@ static const struct x86_cpu_id intel_uncore_match[] __initconst = {
 	X86_MATCH_INTEL_FAM6_MODEL(ICELAKE_X,		&icx_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(TIGERLAKE_L,		&tgl_l_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(TIGERLAKE,		&tgl_uncore_init),
+	X86_MATCH_INTEL_FAM6_MODEL(ROCKETLAKE,		&rkl_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(ATOM_TREMONT_D,	&snr_uncore_init),
 	{},
 };
