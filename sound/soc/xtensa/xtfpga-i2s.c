59bee597234171f6003de959e4351ecb7299beec
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:10:40 -0400

    ASoC: xtensa: Use managed buffer allocation
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    Rebuild_CHGLOG: - [sound] ALSA: ASoC: xtensa: Use managed buffer allocation (Jaroslav Kysela) [1797509]
    Rebuild_FUZZ: 93.48%
    commit-author Takashi Iwai <tiwai@suse.de>
    commit bfddcaffd8858973385ec11c6051bb04fa685e6c
    
    Clean up the driver with the new managed buffer allocation API.
    The superfluous snd_pcm_lib_malloc_pages() and
    snd_pcm_lib_free_pages() calls are dropped.
    
    	Cc: Max Filippov <jcmvbkbc@gmail.com>
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://lore.kernel.org/r/20191210142614.19405-17-tiwai@suse.de
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit bfddcaffd8858973385ec11c6051bb04fa685e6c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/xtensa/xtfpga-i2s.c
+++ sound/soc/xtensa/xtfpga-i2s.c
@@ -387,7 +387,6 @@ static int xtfpga_pcm_close(struct snd_pcm_substream *substream)
 static int xtfpga_pcm_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *hw_params)
 {
-	int ret;
 	struct snd_pcm_runtime *runtime = substream->runtime;
 	struct xtfpga_i2s *i2s = runtime->private_data;
 	unsigned channels = params_channels(hw_params);
@@ -419,9 +418,7 @@ static int xtfpga_pcm_hw_params(struct snd_pcm_substream *substream,
 		return -EINVAL;
 	}
 
-	ret = snd_pcm_lib_malloc_pages(substream,
-				       params_buffer_bytes(hw_params));
-	return ret;
+	return 0;
 }
 
 static int xtfpga_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
@@ -466,8 +463,8 @@ static int xtfpga_pcm_new(struct snd_soc_pcm_runtime *rtd)
 	struct snd_card *card = rtd->card->snd_card;
 	size_t size = xtfpga_pcm_hardware.buffer_bytes_max;
 
-	snd_pcm_lib_preallocate_pages_for_all(rtd->pcm, SNDRV_DMA_TYPE_DEV,
-					      card->dev, size, size);
+	snd_pcm_set_managed_buffer_all(rtd->pcm, SNDRV_DMA_TYPE_DEV,
+				       card->dev, size, size);
 	return 0;
 }
 
