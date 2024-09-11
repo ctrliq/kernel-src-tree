1816964d151b56d56ef308db7a325ed19389ced5
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:13:28 -0400

    snd/soc: correct trivial kernel-doc inconsistency
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    Rebuild_CHGLOG: - [sound] ALSA: snd/soc: correct trivial kernel-doc inconsistency (Jaroslav Kysela) [1869536]
    Rebuild_FUZZ: 94.23%
    commit-author Colton Lewis <colton.w.lewis@protonmail.com>
    commit b6d6e9ea8fb9d477f07e6e4ad8b8a38a47f6870e
    
    Silence documentation build warning by correcting kernel-doc comment
    for snd_soc_runtime_action.
    
    ./sound/soc/soc-pcm.c:220: warning: Function parameter or member 'action' not described in 'snd_soc_runtime_action'
    
    	Signed-off-by: Colton Lewis <colton.w.lewis@protonmail.com>
    Link: https://lore.kernel.org/r/20200626053953.68797-1-colton.w.lewis@protonmail.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit b6d6e9ea8fb9d477f07e6e4ad8b8a38a47f6870e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -87,6 +87,7 @@ static int soc_rtd_trigger(struct snd_soc_pcm_runtime *rtd,
  * PCM runtime components
  * @rtd: ASoC PCM runtime that is activated
  * @stream: Direction of the PCM stream
+ * @action: Activate stream if 1. Deactivate if -1.
  *
  * Increments/Decrements the active count for all the DAIs and components
  * attached to a PCM runtime.
