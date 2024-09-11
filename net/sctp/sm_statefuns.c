86d3c5d6715c76328ae07280d39fa4abc40ec1d7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:43:15 -0400

    security: pass asoc to sctp_assoc_request and sctp_sk_clone
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-373.el8
    commit-author Xin Long <lucien.xin@gmail.com>
    commit c081d53f97a1a90a38e4296dd3d6fda5e38dca2c
    
    This patch is to move secid and peer_secid from endpoint to association,
    and pass asoc to sctp_assoc_request and sctp_sk_clone instead of ep. As
    ep is the local endpoint and asoc represents a connection, and in SCTP
    one sk/ep could have multiple asoc/connection, saving secid/peer_secid
    for new asoc will overwrite the old asoc's.
    
    Note that since asoc can be passed as NULL, security_sctp_assoc_request()
    is moved to the place right after the new_asoc is created in
    sctp_sf_do_5_1B_init() and sctp_sf_do_unexpected_init().
    
    v1->v2:
      - fix the description of selinux_netlbl_skbuff_setsid(), as Jakub noticed.
      - fix the annotation in selinux_sctp_assoc_request(), as Richard Noticed.
    
    Fixes: 72e89f50084c ("security: Add support for SCTP security hooks")
    	Reported-by: Prashanth Prahlad <pprahlad@redhat.com>
    	Reviewed-by: Richard Haines <richard_c_haines@btinternet.com>
    	Tested-by: Richard Haines <richard_c_haines@btinternet.com>
    	Signed-off-by: Xin Long <lucien.xin@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c081d53f97a1a90a38e4296dd3d6fda5e38dca2c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/sctp/sm_statefuns.c
+++ net/sctp/sm_statefuns.c
@@ -341,11 +341,6 @@ enum sctp_disposition sctp_sf_do_5_1B_init(struct net *net,
 	struct sctp_packet *packet;
 	int len;
 
-	/* Update socket peer label if first association. */
-	if (security_sctp_assoc_request((struct sctp_endpoint *)ep,
-					chunk->skb))
-		return sctp_sf_pdiscard(net, ep, asoc, type, arg, commands);
-
 	/* 6.10 Bundling
 	 * An endpoint MUST NOT bundle INIT, INIT ACK or
 	 * SHUTDOWN COMPLETE with any other chunks.
@@ -429,6 +424,12 @@ enum sctp_disposition sctp_sf_do_5_1B_init(struct net *net,
 	new_asoc = sctp_make_temp_asoc(ep, chunk, GFP_ATOMIC);
 	if (!new_asoc)
 		goto nomem;
+
+	/* Update socket peer label if first association. */
+	if (security_sctp_assoc_request(new_asoc, chunk->skb)) {
+		sctp_association_free(new_asoc);
+		return sctp_sf_pdiscard(net, ep, asoc, type, arg, commands);
+	}
 
 	if (sctp_assoc_set_bind_addr_from_ep(new_asoc,
 					     sctp_scope(sctp_source(chunk)),
@@ -794,7 +795,6 @@ enum sctp_disposition sctp_sf_do_5_1D_ce(struct net *net,
 			return sctp_sf_pdiscard(net, ep, asoc, type, arg, commands);
 		}
 	}
-
 
 	/* Delay state machine commands until later.
 	 *
@@ -1531,11 +1531,6 @@ static enum sctp_disposition sctp_sf_do_unexpected_init(
 	struct sctp_packet *packet;
 	int len;
 
-	/* Update socket peer label if first association. */
-	if (security_sctp_assoc_request((struct sctp_endpoint *)ep,
-					chunk->skb))
-		return sctp_sf_pdiscard(net, ep, asoc, type, arg, commands);
-
 	/* 6.10 Bundling
 	 * An endpoint MUST NOT bundle INIT, INIT ACK or
 	 * SHUTDOWN COMPLETE with any other chunks.
@@ -1607,6 +1602,12 @@ static enum sctp_disposition sctp_sf_do_unexpected_init(
 	new_asoc = sctp_make_temp_asoc(ep, chunk, GFP_ATOMIC);
 	if (!new_asoc)
 		goto nomem;
+
+	/* Update socket peer label if first association. */
+	if (security_sctp_assoc_request(new_asoc, chunk->skb)) {
+		sctp_association_free(new_asoc);
+		return sctp_sf_pdiscard(net, ep, asoc, type, arg, commands);
+	}
 
 	if (sctp_assoc_set_bind_addr_from_ep(new_asoc,
 				sctp_scope(sctp_source(chunk)), GFP_ATOMIC) < 0)
@@ -2269,8 +2270,7 @@ enum sctp_disposition sctp_sf_do_5_2_4_dupcook(
 	}
 
 	/* Update socket peer label if first association. */
-	if (security_sctp_assoc_request((struct sctp_endpoint *)ep,
-					chunk->skb)) {
+	if (security_sctp_assoc_request(new_asoc, chunk->skb)) {
 		sctp_association_free(new_asoc);
 		return sctp_sf_pdiscard(net, ep, asoc, type, arg, commands);
 	}
