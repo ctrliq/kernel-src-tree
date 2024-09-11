ad04a151fff4ad6307dc427cdb44beb38e9b25dc
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:08:32 -0400

    ASoC: core: use list_del_init and move it back to soc_cleanup_component
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    Rebuild_CHGLOG: - [sound] ALSA: ASoC: core: use list_del_init and move it back to soc_cleanup_component (Jaroslav Kysela) [1797509]
    Rebuild_FUZZ: 95.95%
    commit-author Bard liao <yung-chuan.liao@linux.intel.com>
    commit 7a5d9815cc010b055c2a99ccf418c4629365fa43
    
    commit a0a4bf57a977 ("ASoC: core: delete component->card_list in soc_remove_component only")
    was trying to fix a kernel oops when list_del was called twice without
    re-init the list. Use list_del_init() can solve it, too. Besides, it
    will be more readable if we cleanup all component related resource at
    soc_cleanup_component().
    
    	Suggested-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    	Signed-off-by: Bard liao <yung-chuan.liao@linux.intel.com>
    Link: https://lore.kernel.org/r/20190918133131.15045-1-yung-chuan.liao@linux.intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 7a5d9815cc010b055c2a99ccf418c4629365fa43)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1005,6 +1005,7 @@ static void soc_cleanup_component(struct snd_soc_component *component)
 	/* For framework level robustness */
 	snd_soc_component_set_jack(component, NULL, NULL);
 
+	list_del_init(&component->card_list);
 	snd_soc_dapm_free(snd_soc_component_get_dapm(component));
 	soc_cleanup_component_debugfs(component);
 	component->card = NULL;
@@ -1017,7 +1018,7 @@ static void soc_remove_component(struct snd_soc_component *component)
 		return;
 
 	snd_soc_component_remove(component);
-	list_del(&component->card_list);
+
 	soc_cleanup_component(component);
 }
 
