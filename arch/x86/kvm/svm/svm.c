3f6a3214c4f87362552557c21e9de353247fa8bf
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:26:53 -0400

    KVM: LAPIC: Reduce world switch latency caused by timer_advance_ns
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Wanpeng Li <wanpengli@tencent.com>
    commit 010fd37fddf659ea7525b1191381e6f8c3ccab14
    
    All the checks in lapic_timer_int_injected(), __kvm_wait_lapic_expire(), and
    these function calls waste cpu cycles when the timer mode is not tscdeadline.
    We can observe ~1.3% world switch time overhead by kvm-unit-tests/vmexit.flat
    vmcall testing on AMD server. This patch reduces the world switch latency
    caused by timer_advance_ns feature when the timer mode is not tscdeadline by
    simpling move the check against apic->lapic_timer.expired_tscdeadline much
    earlier.
    
    	Signed-off-by: Wanpeng Li <wanpengli@tencent.com>
    Message-Id: <1599731444-3525-7-git-send-email-wanpengli@tencent.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 010fd37fddf659ea7525b1191381e6f8c3ccab14)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -3392,9 +3392,7 @@ static fastpath_t svm_vcpu_run(struct kvm_vcpu *vcpu)
 	clgi();
 	kvm_load_guest_xsave_state(vcpu);
 
-	if (lapic_in_kernel(vcpu) &&
-		vcpu->arch.apic->lapic_timer.timer_advance_ns)
-		kvm_wait_lapic_expire(vcpu);
+	kvm_wait_lapic_expire(vcpu);
 
 	/*
 	 * If this vCPU has touched SPEC_CTRL, restore the guest's value if
