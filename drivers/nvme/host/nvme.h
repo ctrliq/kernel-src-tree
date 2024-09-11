e56234657013bf3a3c4b89a01b3551991e0542c7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:52:40 -0400

    nvme: cleanup setting the disk name
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Christoph Hellwig <hch@lst.de>
    commit 9953ab0c5ae722dabbfa89a82c0d30a261125da0
    
    Return false from nvme_set_disk_name and let the caller set the
    non-multipath name instead of duplicating the naming information in two
    places.  Also remove the pointless local variables for the disk name
    and flags and the not needed ctrl argument.
    
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    	Reviewed-by: Keith Busch <kbusch@kernel.org>
    	Reviewed-by: Javier González <javier.gonz@samsung.com>
    (cherry picked from commit 9953ab0c5ae722dabbfa89a82c0d30a261125da0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -663,8 +663,7 @@ static inline bool nvme_ctrl_use_ana(struct nvme_ctrl *ctrl)
 void nvme_mpath_unfreeze(struct nvme_subsystem *subsys);
 void nvme_mpath_wait_freeze(struct nvme_subsystem *subsys);
 void nvme_mpath_start_freeze(struct nvme_subsystem *subsys);
-void nvme_set_disk_name(char *disk_name, struct nvme_ns *ns,
-			struct nvme_ctrl *ctrl, int *flags);
+bool nvme_mpath_set_disk_name(struct nvme_ns *ns, char *disk_name, int *flags);
 void nvme_failover_req(struct request *req);
 void nvme_update_ana(struct request *req);
 void nvme_kick_requeue_lists(struct nvme_ctrl *ctrl);
@@ -704,16 +703,11 @@ static inline bool nvme_ctrl_use_ana(struct nvme_ctrl *ctrl)
 {
 	return false;
 }
-/*
- * Without the multipath code enabled, multiple controller per subsystems are
- * visible as devices and thus we cannot use the subsystem instance.
- */
-static inline void nvme_set_disk_name(char *disk_name, struct nvme_ns *ns,
-				      struct nvme_ctrl *ctrl, int *flags)
+static inline bool nvme_mpath_set_disk_name(struct nvme_ns *ns, char *disk_name,
+		int *flags)
 {
-	sprintf(disk_name, "nvme%dn%d", ctrl->instance, ns->head->instance);
+	return false;
 }
-
 static inline void nvme_failover_req(struct request *req)
 {
 }
