f683b275d8c0594d464f2791adb52d6e512e819e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:42:10 -0400

    net: sched: Remove Qdisc::running sequence counter
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Ahmed S. Darwish <a.darwish@linutronix.de>
    commit 29cbcd85828372333aa87542c51f2b2b0fd4380c
    
    The Qdisc::running sequence counter has two uses:
    
      1. Reliably reading qdisc's tc statistics while the qdisc is running
         (a seqcount read/retry loop at gnet_stats_add_basic()).
    
      2. As a flag, indicating whether the qdisc in question is running
         (without any retry loops).
    
    For the first usage, the Qdisc::running sequence counter write section,
    qdisc_run_begin() => qdisc_run_end(), covers a much wider area than what
    is actually needed: the raw qdisc's bstats update. A u64_stats sync
    point was thus introduced (in previous commits) inside the bstats
    structure itself. A local u64_stats write section is then started and
    stopped for the bstats updates.
    
    Use that u64_stats sync point mechanism for the bstats read/retry loop
    at gnet_stats_add_basic().
    
    For the second qdisc->running usage, a __QDISC_STATE_RUNNING bit flag,
    accessed with atomic bitops, is sufficient. Using a bit flag instead of
    a sequence counter at qdisc_run_begin/end() and qdisc_is_running() leads
    to the SMP barriers implicitly added through raw_read_seqcount() and
    write_seqcount_begin/end() getting removed. All call sites have been
    surveyed though, and no required ordering was identified.
    
    Now that the qdisc->running sequence counter is no longer used, remove
    it.
    
    Note, using u64_stats implies no sequence counter protection for 64-bit
    architectures. This can lead to the qdisc tc statistics "packets" vs.
    "bytes" values getting out of sync on rare occasions. The individual
    values will still be valid.
    
    	Signed-off-by: Ahmed S. Darwish <a.darwish@linutronix.de>
    	Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 29cbcd85828372333aa87542c51f2b2b0fd4380c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/core/gen_estimator.c
+++ net/core/gen_estimator.c
@@ -46,7 +46,7 @@
 struct net_rate_estimator {
 	struct gnet_stats_basic_sync	*bstats;
 	spinlock_t		*stats_lock;
-	seqcount_t		*running;
+	bool			running;
 	struct gnet_stats_basic_sync __percpu *cpu_bstats;
 	u8			ewma_log;
 	u8			intvl_log; /* period : (250ms << intvl_log) */
@@ -70,7 +70,7 @@ static void est_fetch_counters(struct net_rate_estimator *e,
 	if (e->stats_lock)
 		spin_lock(e->stats_lock);
 
-	gnet_stats_add_basic(e->running, b, e->cpu_bstats, e->bstats);
+	gnet_stats_add_basic(b, e->cpu_bstats, e->bstats, e->running);
 
 	if (e->stats_lock)
 		spin_unlock(e->stats_lock);
@@ -117,7 +117,9 @@ static void est_timer(struct timer_list *t)
  * @cpu_bstats: bstats per cpu
  * @rate_est: rate estimator statistics
  * @lock: lock for statistics and control path
- * @running: qdisc running seqcount
+ * @running: true if @bstats represents a running qdisc, thus @bstats'
+ *           internal values might change during basic reads. Only used
+ *           if @bstats_cpu is NULL
  * @opt: rate estimator configuration TLV
  *
  * Creates a new rate estimator with &bstats as source and &rate_est
@@ -133,7 +135,7 @@ int gen_new_estimator(struct gnet_stats_basic_sync *bstats,
 		      struct gnet_stats_basic_sync __percpu *cpu_bstats,
 		      struct net_rate_estimator __rcu **rate_est,
 		      spinlock_t *lock,
-		      seqcount_t *running,
+		      bool running,
 		      struct nlattr *opt)
 {
 	struct gnet_estimator *parm = nla_data(opt);
@@ -222,7 +224,9 @@ EXPORT_SYMBOL(gen_kill_estimator);
  * @cpu_bstats: bstats per cpu
  * @rate_est: rate estimator statistics
  * @lock: lock for statistics and control path
- * @running: qdisc running seqcount (might be NULL)
+ * @running: true if @bstats represents a running qdisc, thus @bstats'
+ *           internal values might change during basic reads. Only used
+ *           if @cpu_bstats is NULL
  * @opt: rate estimator configuration TLV
  *
  * Replaces the configuration of a rate estimator by calling
@@ -234,7 +238,7 @@ int gen_replace_estimator(struct gnet_stats_basic_sync *bstats,
 			  struct gnet_stats_basic_sync __percpu *cpu_bstats,
 			  struct net_rate_estimator __rcu **rate_est,
 			  spinlock_t *lock,
-			  seqcount_t *running, struct nlattr *opt)
+			  bool running, struct nlattr *opt)
 {
 	return gen_new_estimator(bstats, cpu_bstats, rate_est,
 				 lock, running, opt);
