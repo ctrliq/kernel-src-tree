9229ce8218d1dd9d306c8555fd59f5763aa100f0
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:48:29 -0400

    Bluetooth: btbcm : Fix warning about missing blank lines after declarations
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Changqi Du <d.changqi@gmail.com>
    commit dde8010be0a0ed9823cc1ddf75ef8448910821a9
    
    This patches fixes two warnings of checkpatch.pl, both of the type
    WARNING: Missing a blank line after declarations
    
    	Signed-off-by: Changqi Du <d.changqi@gmail.com>
    	Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit dde8010be0a0ed9823cc1ddf75ef8448910821a9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/bluetooth/btbcm.c
+++ drivers/bluetooth/btbcm.c
@@ -45,6 +45,7 @@ int btbcm_check_bdaddr(struct hci_dev *hdev)
 			     HCI_INIT_TIMEOUT);
 	if (IS_ERR(skb)) {
 		int err = PTR_ERR(skb);
+
 		bt_dev_err(hdev, "BCM: Reading device address failed (%d)", err);
 		return err;
 	}
@@ -217,6 +218,7 @@ static int btbcm_reset(struct hci_dev *hdev)
 	skb = __hci_cmd_sync(hdev, HCI_OP_RESET, 0, NULL, HCI_INIT_TIMEOUT);
 	if (IS_ERR(skb)) {
 		int err = PTR_ERR(skb);
+
 		bt_dev_err(hdev, "BCM: Reset failed (%d)", err);
 		return err;
 	}
