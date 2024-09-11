1eacbc4d6bf4302e202b4ade2e07d9b91e49c09e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:38:20 -0400

    powerpc/security: Fix debugfs data leak on 32-bit
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Geert Uytterhoeven <geert+renesas@glider.be>
    commit 3b05a1e517e1a8cfda4866ec31d28b2bc4fee4c4
    
    "powerpc_security_features" is "unsigned long", i.e. 32-bit or 64-bit,
    depending on the platform (PPC_FSL_BOOK3E or PPC_BOOK3S_64).  Hence
    casting its address to "u64 *", and calling debugfs_create_x64() is
    wrong, and leaks 32-bit of nearby data to userspace on 32-bit platforms.
    
    While all currently defined SEC_FTR_* security feature flags fit in
    32-bit, they all have "ULL" suffixes to make them 64-bit constants.
    Hence fix the leak by changing the type of "powerpc_security_features"
    (and the parameter types of its accessors) to "u64".  This also allows
    to drop the cast.
    
    Fixes: 398af571128fe75f ("powerpc/security: Show powerpc_security_features in debugfs")
    	Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    	Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20191021142309.28105-1-geert+renesas@glider.be
    (cherry picked from commit 3b05a1e517e1a8cfda4866ec31d28b2bc4fee4c4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/powerpc/kernel/security.c
+++ arch/powerpc/kernel/security.c
@@ -19,7 +19,7 @@
 #include <asm/inst.h>
 
 
-unsigned long powerpc_security_features __read_mostly = SEC_FTR_DEFAULT;
+u64 powerpc_security_features __read_mostly = SEC_FTR_DEFAULT;
 
 enum branch_cache_flush_type {
 	BRANCH_CACHE_FLUSH_NONE	= 0x1,
@@ -112,7 +112,7 @@ device_initcall(barrier_nospec_debugfs_init);
 static __init int security_feature_debugfs_init(void)
 {
 	debugfs_create_x64("security_features", 0400, powerpc_debugfs_root,
-			   (u64 *)&powerpc_security_features);
+			   &powerpc_security_features);
 	return 0;
 }
 device_initcall(security_feature_debugfs_init);
