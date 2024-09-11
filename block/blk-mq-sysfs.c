c901086124bb2f6fdaa78112955a102ea15ba290
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:43:36 -0400

    block: Remove "dying" checks from sysfs callbacks
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Bart Van Assche <bvanassche@acm.org>
    commit bae85c156f619939ef6261f1bd4fabbe24361b50
    
    Block drivers must call del_gendisk() before blk_cleanup_queue().
    del_gendisk() calls kobject_del() and kobject_del() waits until any
    ongoing sysfs callback functions have finished. In other words, the
    sysfs callback functions won't be called for a queue in the dying
    state. Hence remove the "dying" checks from the sysfs callback
    functions.
    
    	Cc: Christoph Hellwig <hch@infradead.org>
    	Cc: Ming Lei <ming.lei@redhat.com>
    	Cc: Hannes Reinecke <hare@suse.com>
    	Cc: Johannes Thumshirn <jthumshirn@suse.de>
    	Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit bae85c156f619939ef6261f1bd4fabbe24361b50)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- block/blk-mq-sysfs.c
+++ block/blk-mq-sysfs.c
@@ -73,10 +73,8 @@ static ssize_t blk_mq_sysfs_show(struct kobject *kobj, struct attribute *attr,
 	if (!entry->show)
 		return -EIO;
 
-	res = -ENOENT;
 	mutex_lock(&q->sysfs_lock);
-	if (!blk_queue_dying(q))
-		res = entry->show(ctx, page);
+	res = entry->show(ctx, page);
 	mutex_unlock(&q->sysfs_lock);
 	return res;
 }
@@ -96,10 +94,8 @@ static ssize_t blk_mq_sysfs_store(struct kobject *kobj, struct attribute *attr,
 	if (!entry->store)
 		return -EIO;
 
-	res = -ENOENT;
 	mutex_lock(&q->sysfs_lock);
-	if (!blk_queue_dying(q))
-		res = entry->store(ctx, page, length);
+	res = entry->store(ctx, page, length);
 	mutex_unlock(&q->sysfs_lock);
 	return res;
 }
@@ -119,10 +115,8 @@ static ssize_t blk_mq_hw_sysfs_show(struct kobject *kobj,
 	if (!entry->show)
 		return -EIO;
 
-	res = -ENOENT;
 	mutex_lock(&q->sysfs_lock);
-	if (!blk_queue_dying(q))
-		res = entry->show(hctx, page);
+	res = entry->show(hctx, page);
 	mutex_unlock(&q->sysfs_lock);
 	return res;
 }
@@ -143,10 +137,8 @@ static ssize_t blk_mq_hw_sysfs_store(struct kobject *kobj,
 	if (!entry->store)
 		return -EIO;
 
-	res = -ENOENT;
 	mutex_lock(&q->sysfs_lock);
-	if (!blk_queue_dying(q))
-		res = entry->store(hctx, page, length);
+	res = entry->store(hctx, page, length);
 	mutex_unlock(&q->sysfs_lock);
 	return res;
 }
