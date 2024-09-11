598901e8cfcaccf43c516d312bb92df88d65f71a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:46:45 -0400

    mac80211: fix kernel-doc markups
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-315.el8
    commit-author Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    commit b1e8eb11fb9cf666d8ae36bbcf533233a504c921
    
    Some identifiers have different names between their prototypes
    and the kernel-doc markup.
    
    Others need to be fixed, as kernel-doc markups should use this format:
            identifier - description
    
    In the specific case of __sta_info_flush(), add a documentation
    for sta_info_flush(), as this one is the one used outside
    sta_info.c.
    
    	Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    	Reviewed-by: Johannes Berg <johannes@sipsolutions.net>
    Link: https://lore.kernel.org/r/978d35eef2dc76e21c81931804e4eaefbd6d635e.1603469755.git.mchehab+huawei@kernel.org
    	Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit b1e8eb11fb9cf666d8ae36bbcf533233a504c921)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -1407,7 +1407,7 @@ int cfg80211_check_station_change(struct wiphy *wiphy,
 				  enum cfg80211_station_type statype);
 
 /**
- * enum station_info_rate_flags - bitrate info flags
+ * enum rate_info_flags - bitrate info flags
  *
  * Used by the driver to indicate the specific rate transmission
  * type for 802.11n transmissions.
@@ -1480,7 +1480,7 @@ struct rate_info {
 };
 
 /**
- * enum station_info_rate_flags - bitrate info flags
+ * enum bss_param_flags - bitrate info flags
  *
  * Used by the driver to indicate the specific rate transmission
  * type for 802.11n transmissions.
@@ -6427,7 +6427,8 @@ void cfg80211_ibss_joined(struct net_device *dev, const u8 *bssid,
 			  struct ieee80211_channel *channel, gfp_t gfp);
 
 /**
- * cfg80211_notify_new_candidate - notify cfg80211 of a new mesh peer candidate
+ * cfg80211_notify_new_peer_candidate - notify cfg80211 of a new mesh peer
+ * 					candidate
  *
  * @dev: network device
  * @macaddr: the MAC address of the new candidate
@@ -7566,7 +7567,7 @@ u32 cfg80211_calculate_bitrate(struct rate_info *rate);
 void cfg80211_unregister_wdev(struct wireless_dev *wdev);
 
 /**
- * struct cfg80211_ft_event - FT Information Elements
+ * struct cfg80211_ft_event_params - FT Information Elements
  * @ies: FT IEs
  * @ies_len: length of the FT IE in bytes
  * @target_ap: target AP's MAC address
