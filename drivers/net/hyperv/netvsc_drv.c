617639ed9e9a62f0a7eafee45920fc8da7a7ad9c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:52:21 -0400

    hv_netvsc: Set needed_headroom according to VF
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Haiyang Zhang <haiyangz@microsoft.com>
    commit 536ba2e06d1aaaed8a11c30e1609281cd955082e
    
    Set needed_headroom according to VF if VF needs a bigger
    headroom.
    
    	Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 536ba2e06d1aaaed8a11c30e1609281cd955082e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -2410,6 +2410,9 @@ static int netvsc_register_vf(struct net_device *vf_netdev)
 	dev_hold(vf_netdev);
 	rcu_assign_pointer(net_device_ctx->vf_netdev, vf_netdev);
 
+	if (ndev->needed_headroom < vf_netdev->needed_headroom)
+		ndev->needed_headroom = vf_netdev->needed_headroom;
+
 	vf_netdev->wanted_features = ndev->features;
 	netdev_update_features(vf_netdev);
 
@@ -2487,6 +2490,8 @@ static int netvsc_unregister_vf(struct net_device *vf_netdev)
 	netdev_upper_dev_unlink(vf_netdev, ndev);
 	RCU_INIT_POINTER(net_device_ctx->vf_netdev, NULL);
 	dev_put(vf_netdev);
+
+	ndev->needed_headroom = RNDIS_AND_PPI_SIZE;
 
 	return NOTIFY_OK;
 }
