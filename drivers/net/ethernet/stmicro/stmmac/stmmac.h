73ab44ca40f41afb1613429670f93f27768a340f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:10:27 -0400

    net: stmmac: add ethtool support for get/set channels
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Ong Boon Leong <boon.leong.ong@intel.com>
    commit 0366f7e06a6bee7eace3946a6b67fb88b828bc5c
    
    Restructure NAPI add and delete process so that we can call them
    accordingly in open() and ethtool_set_channels() accordingly.
    
    Introduced stmmac_reinit_queues() to handle the transition needed
    for changing Rx & Tx channels accordingly.
    
    	Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0366f7e06a6bee7eace3946a6b67fb88b828bc5c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac.h
@@ -259,5 +259,6 @@ int stmmac_dvr_probe(struct device *device,
 		     struct stmmac_resources *res);
 void stmmac_disable_eee_mode(struct stmmac_priv *priv);
 bool stmmac_eee_init(struct stmmac_priv *priv);
+int stmmac_reinit_queues(struct net_device *dev, u32 rx_cnt, u32 tx_cnt);
 
 #endif /* __STMMAC_H__ */
