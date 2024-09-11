50cb05c6ff797a217535e5e7ae453bd2bcd87fbf
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:23:36 -0400

    devlink: double free in devlink_resource_fill()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Dan Carpenter <dan.carpenter@oracle.com>
    commit 83fe9a966111b51a34f10c35e568e45bff34de48
    
    Smatch reports that devlink_dpipe_send_and_alloc_skb() frees the skb
    on error so this is a double free.  We fixed a bunch of these bugs in
    commit 7fe4d6dcbcb4 ("devlink: Remove redundant free on error path") but
    we accidentally overlooked this one.
    
    Fixes: d9f9b9a4d05f ("devlink: Add support for resource abstraction")
    	Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 83fe9a966111b51a34f10c35e568e45bff34de48)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/core/devlink.c
+++ net/core/devlink.c
@@ -2594,7 +2594,7 @@ send_done:
 	if (!nlh) {
 		err = devlink_dpipe_send_and_alloc_skb(&skb, info);
 		if (err)
-			goto err_skb_send_alloc;
+			return err;
 		goto send_done;
 	}
 	return genlmsg_reply(skb, info);
@@ -2602,7 +2602,6 @@ send_done:
 nla_put_failure:
 	err = -EMSGSIZE;
 err_resource_put:
-err_skb_send_alloc:
 	nlmsg_free(skb);
 	return err;
 }
