1d8eb2af1a45bd3c4ac10e00c2cbbf1cb45e6255
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:07:38 -0400

    net: bridge: vlan: fix global vlan option range dumping
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Nikolay Aleksandrov <nikolay@nvidia.com>
    commit 6c4110d9f499e2170fbb36723b0a5f50a8116304
    
    When global vlan options are equal sequentially we compress them in a
    range to save space and reduce processing time. In order to have the
    proper range end id we need to update range_end if the options are equal
    otherwise we get ranges with the same end vlan id as the start.
    
    Fixes: 743a53d9636a ("net: bridge: vlan: add support for dumping global vlan options")
    	Signed-off-by: Nikolay Aleksandrov <nikolay@nvidia.com>
    Link: https://lore.kernel.org/r/20210810092139.11700-1-razor@blackwall.org
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 6c4110d9f499e2170fbb36723b0a5f50a8116304)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/bridge/br_vlan.c
+++ net/bridge/br_vlan.c
@@ -1852,7 +1852,7 @@ static int br_vlan_dump_dev(const struct net_device *dev,
 
 		if (dump_global) {
 			if (br_vlan_global_opts_can_enter_range(v, range_end))
-				continue;
+				goto update_end;
 			if (!br_vlan_global_opts_fill(skb, range_start->vid,
 						      range_end->vid,
 						      range_start)) {
@@ -1878,6 +1878,7 @@ static int br_vlan_dump_dev(const struct net_device *dev,
 
 			range_start = v;
 		}
+update_end:
 		range_end = v;
 	}
 
