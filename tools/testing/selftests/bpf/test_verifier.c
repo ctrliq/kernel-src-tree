58bfe1a8bdd763ffe5c7837e9b704b119763338f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:08:58 -0400

    selftests/bpf: add missing pointer dereference for map stacktrace fixup
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Stanislav Fomichev <sdf@google.com>
    commit c2a20a2731df11f9b7b7030f7ac3fc222c9ce39d
    
    I get a segfault without it, other fixups always do dereference, and
    without dereference I don't understand how it can ever work.
    
    Fixes: 7c85c448e7d74 ("selftests/bpf: test_verifier, check bpf_map_lookup_elem access in bpf prog")
    	Signed-off-by: Stanislav Fomichev <sdf@google.com>
    	Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit c2a20a2731df11f9b7b7030f7ac3fc222c9ce39d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/testing/selftests/bpf/test_verifier.c
+++ tools/testing/selftests/bpf/test_verifier.c
@@ -14289,7 +14289,7 @@ static void do_test_fixup(struct bpf_test *test, enum bpf_map_type prog_type,
 		do {
 			prog[*fixup_map_stacktrace].imm = map_fds[12];
 			fixup_map_stacktrace++;
-		} while (fixup_map_stacktrace);
+		} while (*fixup_map_stacktrace);
 	}
 }
 
