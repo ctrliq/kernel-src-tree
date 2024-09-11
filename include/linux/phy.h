7c6ca35decd5586482eaa1154c510bad256c3244
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:28:47 -0400

    net: phy: add genphy_read_abilities
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit 045925e3fe5b98e402337a176d154252c56cef2e
    
    Similar to genphy_c45_pma_read_abilities() add a function to dynamically
    detect the abilities of a Clause 22 PHY. This is mainly copied from
    genphy_config_init().
    
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 045925e3fe5b98e402337a176d154252c56cef2e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -1020,6 +1020,7 @@ void phy_attached_info(struct phy_device *phydev);
 
 /* Clause 22 PHY */
 int genphy_config_init(struct phy_device *phydev);
+int genphy_read_abilities(struct phy_device *phydev);
 int genphy_setup_forced(struct phy_device *phydev);
 int genphy_restart_aneg(struct phy_device *phydev);
 int genphy_config_aneg(struct phy_device *phydev);
