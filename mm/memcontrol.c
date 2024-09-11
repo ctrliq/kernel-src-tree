b0a0484b59cd524b038ebce7d332ec2819fdc332
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:01:03 -0400

    memcg, oom: no oom-kill for __GFP_RETRY_MAYFAIL
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Shakeel Butt <shakeelb@google.com>
    commit 38d384932ed1b58f0ed8dd17c66e0cd89acb354a
    
    The documentation of __GFP_RETRY_MAYFAIL clearly mentioned that the OOM
    killer will not be triggered and indeed the page alloc does not invoke OOM
    killer for such allocations.  However we do trigger memcg OOM killer for
    __GFP_RETRY_MAYFAIL.  Fix that.  This flag will used later to not trigger
    oom-killer in the charging path for fanotify and inotify event
    allocations.
    
    Link: http://lkml.kernel.org/r/20190514212259.156585-1-shakeelb@google.com
    	Signed-off-by: Shakeel Butt <shakeelb@google.com>
    	Acked-by: Michal Hocko <mhocko@suse.com>
    	Cc: Amir Goldstein <amir73il@gmail.com>
    	Cc: Jan Kara <jack@suse.cz>
    	Cc: Johannes Weiner <hannes@cmpxchg.org>
    	Cc: Roman Gushchin <guro@fb.com>
    	Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 38d384932ed1b58f0ed8dd17c66e0cd89acb354a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2441,7 +2441,6 @@ static int try_charge(struct mem_cgroup *memcg, gfp_t gfp_mask,
 	unsigned long nr_reclaimed;
 	bool may_swap = true;
 	bool drained = false;
-	bool oomed = false;
 	enum oom_status oom_status;
 
 	if (mem_cgroup_is_root(memcg))
@@ -2537,7 +2536,7 @@ retry:
 	if (nr_retries--)
 		goto retry;
 
-	if (gfp_mask & __GFP_RETRY_MAYFAIL && oomed)
+	if (gfp_mask & __GFP_RETRY_MAYFAIL)
 		goto nomem;
 
 	if (gfp_mask & __GFP_NOFAIL)
@@ -2556,7 +2555,6 @@ retry:
 	switch (oom_status) {
 	case OOM_SUCCESS:
 		nr_retries = MEM_CGROUP_RECLAIM_RETRIES;
-		oomed = true;
 		goto retry;
 	case OOM_FAILED:
 		goto force;
