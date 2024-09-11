ce97eb4e8865faf8143687d783514170994651de
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:32:06 -0400

    dmaengine: idxd: remove MSIX masking for interrupt handlers
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-338.el8
    commit-author Dave Jiang <dave.jiang@intel.com>
    commit a16104617d212d4b482568847b25172972b87e60
    
    Remove interrupt masking and just let the hard irq handler keep
    firing for new events. This is less of a performance impact vs
    the MMIO readback inside the pci_msi_{mask,unmas}_irq(). Especially
    with a loaded system those flushes can be stuck behind large amounts
    of MMIO writes to flush. When guest kernel is running on top of VFIO
    mdev, mask/unmask causes a vmexit each time and is not desirable.
    
    	Suggested-by: Dan Williams <dan.j.williams@intel.com>
    	Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    	Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://lore.kernel.org/r/161894523436.3210025.1834640110556139277.stgit@djiang5-desk3.ch.intel.com
    	Signed-off-by: Vinod Koul <vkoul@kernel.org>
    (cherry picked from commit a16104617d212d4b482568847b25172972b87e60)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/dma/idxd/idxd.h
+++ drivers/dma/idxd/idxd.h
@@ -359,7 +359,6 @@ struct device_type *idxd_get_device_type(struct idxd_device *idxd);
 /* device interrupt control */
 void idxd_msix_perm_setup(struct idxd_device *idxd);
 void idxd_msix_perm_clear(struct idxd_device *idxd);
-irqreturn_t idxd_irq_handler(int vec, void *data);
 irqreturn_t idxd_misc_thread(int vec, void *data);
 irqreturn_t idxd_wq_thread(int irq, void *data);
 void idxd_mask_error_interrupts(struct idxd_device *idxd);
