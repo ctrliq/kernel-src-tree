56876626d804ba58773c8cd4ee84444c5477c188
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:11:01 -0400

    net/mlx5e: Share the XDP SQ for XDP_TX between RQs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [netdrv] mlx5e: Share the XDP SQ for XDP_TX between RQs (Alaa Hleihel) [1724336]
    Rebuild_FUZZ: 95.83%
    commit-author Maxim Mikityanskiy <maximmi@mellanox.com>
    commit b9673cf5558c0ae1be787611884d4131633f31a8
    
    Put the XDP SQ that is used for XDP_TX into the channel. It used to be a
    part of the RQ, but with introduction of AF_XDP there will be one more
    RQ that could share the same XDP SQ. This patch is a preparation for
    that change.
    
    Separate XDP_TX statistics per RQ were implemented in one of the previous
    patches.
    
    	Signed-off-by: Maxim Mikityanskiy <maximmi@mellanox.com>
    	Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    	Acked-by: Saeed Mahameed <saeedm@mellanox.com>
    	Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit b9673cf5558c0ae1be787611884d4131633f31a8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -431,6 +431,7 @@ struct mlx5e_xdp_info {
 			dma_addr_t dma_addr;
 		} frame;
 		struct {
+			struct mlx5e_rq *rq;
 			struct mlx5e_dma_info di;
 		} page;
 	};
@@ -643,7 +644,7 @@ struct mlx5e_rq {
 
 	/* XDP */
 	struct bpf_prog       *xdp_prog;
-	struct mlx5e_xdpsq     xdpsq;
+	struct mlx5e_xdpsq    *xdpsq;
 	DECLARE_BITMAP(flags, 8);
 	struct page_pool      *page_pool;
 
@@ -662,6 +663,7 @@ struct mlx5e_rq {
 struct mlx5e_channel {
 	/* data path */
 	struct mlx5e_rq            rq;
+	struct mlx5e_xdpsq         rq_xdpsq;
 	struct mlx5e_txqsq         sq[MLX5E_MAX_NUM_TC];
 	struct mlx5e_icosq         icosq;   /* internal control operations */
 	bool                       xdp;
