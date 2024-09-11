50d17c5b13a8518dcda861a311e8aa86c3b04e6e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:51:34 -0400

    PM core: Use new async_schedule_dev command
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Alexander Duyck <alexander.h.duyck@linux.intel.com>
    commit 8b9ec6b732775849f506aa6c2649e626e82a297c
    
    Use the device specific version of the async_schedule commands to defer
    various tasks related to power management. By doing this we should see a
    slight improvement in performance as any device that is sensitive to
    latency/locality in the setup will now be initializing on the node closest
    to the device.
    
    	Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    	Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    	Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    	Signed-off-by: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 8b9ec6b732775849f506aa6c2649e626e82a297c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -726,7 +726,7 @@ void dpm_noirq_resume_devices(pm_message_t state)
 		reinit_completion(&dev->power.completion);
 		if (is_async(dev)) {
 			get_device(dev);
-			async_schedule(async_resume_noirq, dev);
+			async_schedule_dev(async_resume_noirq, dev);
 		}
 	}
 
@@ -883,7 +883,7 @@ void dpm_resume_early(pm_message_t state)
 		reinit_completion(&dev->power.completion);
 		if (is_async(dev)) {
 			get_device(dev);
-			async_schedule(async_resume_early, dev);
+			async_schedule_dev(async_resume_early, dev);
 		}
 	}
 
@@ -1047,7 +1047,7 @@ void dpm_resume(pm_message_t state)
 		reinit_completion(&dev->power.completion);
 		if (is_async(dev)) {
 			get_device(dev);
-			async_schedule(async_resume, dev);
+			async_schedule_dev(async_resume, dev);
 		}
 	}
 
@@ -1366,7 +1366,7 @@ static int device_suspend_noirq(struct device *dev)
 
 	if (is_async(dev)) {
 		get_device(dev);
-		async_schedule(async_suspend_noirq, dev);
+		async_schedule_dev(async_suspend_noirq, dev);
 		return 0;
 	}
 	return __device_suspend_noirq(dev, pm_transition, false);
@@ -1569,7 +1569,7 @@ static int device_suspend_late(struct device *dev)
 
 	if (is_async(dev)) {
 		get_device(dev);
-		async_schedule(async_suspend_late, dev);
+		async_schedule_dev(async_suspend_late, dev);
 		return 0;
 	}
 
@@ -1833,7 +1833,7 @@ static int device_suspend(struct device *dev)
 
 	if (is_async(dev)) {
 		get_device(dev);
-		async_schedule(async_suspend, dev);
+		async_schedule_dev(async_suspend, dev);
 		return 0;
 	}
 
