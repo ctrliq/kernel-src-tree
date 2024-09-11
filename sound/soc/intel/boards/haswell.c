7d6443937f6806117b8c994dcc9ac49baca07071
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:16:53 -0400

    ASoC: Intel: haswell: Mark FE DAIs as nonatomic
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    Rebuild_CHGLOG: - [sound] ALSA: ASoC: Intel: haswell: Mark FE DAIs as nonatomic (Jaroslav Kysela) [1869536]
    Rebuild_FUZZ: 94.00%
    commit-author Cezary Rojewski <cezary.rojewski@intel.com>
    commit dc155ad5fa6ef7d48fb3c3cc30497b492da0749e
    
    PCM operations for DAI links connected with DSP platform component
    involve communication with DSP firmware by IPCs. As IPC protocol may
    cause thread to sleep while waiting for a response from DSP, propagate
    that information to ALSA core by marking all FE DAIs as nonatomic.
    
    	Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Link: https://lore.kernel.org/r/20201004090609.29066-4-cezary.rojewski@intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit dc155ad5fa6ef7d48fb3c3cc30497b492da0749e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/intel/boards/haswell.c
+++ sound/soc/intel/boards/haswell.c
@@ -122,6 +122,7 @@ static struct snd_soc_dai_link haswell_rt5640_dais[] = {
 	{
 		.name = "System",
 		.stream_name = "System Playback/Capture",
+		.nonatomic = 1,
 		.dynamic = 1,
 		.init = haswell_rtd_init,
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
@@ -132,6 +133,7 @@ static struct snd_soc_dai_link haswell_rt5640_dais[] = {
 	{
 		.name = "Offload0",
 		.stream_name = "Offload0 Playback",
+		.nonatomic = 1,
 		.dynamic = 1,
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
 		.dpcm_playback = 1,
@@ -140,6 +142,7 @@ static struct snd_soc_dai_link haswell_rt5640_dais[] = {
 	{
 		.name = "Offload1",
 		.stream_name = "Offload1 Playback",
+		.nonatomic = 1,
 		.dynamic = 1,
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
 		.dpcm_playback = 1,
@@ -148,6 +151,7 @@ static struct snd_soc_dai_link haswell_rt5640_dais[] = {
 	{
 		.name = "Loopback",
 		.stream_name = "Loopback",
+		.nonatomic = 1,
 		.dynamic = 1,
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
 		.dpcm_capture = 1,
