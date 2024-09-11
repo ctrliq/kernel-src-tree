fa6e548ccfbff96cbe4c5356b67b2d8dc5b2d81d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:08:36 -0400

    mm/gup: don't permit users to call get_user_pages with FOLL_LONGTERM
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-408.el8
    commit-author Barry Song <song.bao.hua@hisilicon.com>
    commit 447f3e45c18a8f27018213bcb1b5a0076633f68a
    
    gup prohibits users from calling get_user_pages() with FOLL_PIN.  But it
    allows users to call get_user_pages() with FOLL_LONGTERM only.  It seems
    insensible.
    
    Since FOLL_LONGTERM is a stricter case of FOLL_PIN, we should prohibit
    users from calling get_user_pages() with FOLL_LONGTERM while not with
    FOLL_PIN.
    
    mm/gup_benchmark.c used to be the only user who did this improperly.
    But it has been fixed by moving to use pin_user_pages().
    
    [akpm@linux-foundation.org: fix CONFIG_MMU=n build]
      Link: https://lkml.kernel.org/r/CA+G9fYuNS3k0DVT62twfV746pfNhCSrk5sVMcOcQ1PGGnEseyw@mail.gmail.com
    
    	Signed-off-by: Barry Song <song.bao.hua@hisilicon.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    	Cc: John Hubbard <jhubbard@nvidia.com>
    	Cc: Jan Kara <jack@suse.cz>
    	Cc: Jérôme Glisse <jglisse@redhat.com>
    	Cc: "Matthew Wilcox (Oracle)" <willy@infradead.org>
    	Cc: Al Viro <viro@zeniv.linux.org.uk>
    	Cc: Christoph Hellwig <hch@infradead.org>
    	Cc: Dan Williams <dan.j.williams@intel.com>
    	Cc: Dave Chinner <david@fromorbit.com>
    	Cc: Jason Gunthorpe <jgg@ziepe.ca>
    	Cc: Jonathan Corbet <corbet@lwn.net>
    	Cc: Michal Hocko <mhocko@suse.com>
    	Cc: Mike Kravetz <mike.kravetz@oracle.com>
    	Cc: Shuah Khan <shuah@kernel.org>
    	Cc: Vlastimil Babka <vbabka@suse.cz>
    	Cc: Naresh Kamboju <naresh.kamboju@linaro.org>
    Link: http://lkml.kernel.org/r/20200819110100.23504-1-song.bao.hua@hisilicon.com
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 447f3e45c18a8f27018213bcb1b5a0076633f68a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/gup.c
+++ mm/gup.c
@@ -2006,6 +2006,25 @@ static __always_inline long __gup_longterm_locked(struct task_struct *tsk,
 }
 #endif /* CONFIG_FS_DAX || CONFIG_CMA */
 
+static bool is_valid_gup_flags(unsigned int gup_flags)
+{
+	/*
+	 * FOLL_PIN must only be set internally by the pin_user_pages*() APIs,
+	 * never directly by the caller, so enforce that with an assertion:
+	 */
+	if (WARN_ON_ONCE(gup_flags & FOLL_PIN))
+		return false;
+	/*
+	 * FOLL_PIN is a prerequisite to FOLL_LONGTERM. Another way of saying
+	 * that is, FOLL_LONGTERM is a specific case, more restrictive case of
+	 * FOLL_PIN.
+	 */
+	if (WARN_ON_ONCE(gup_flags & FOLL_LONGTERM))
+		return false;
+
+	return true;
+}
+
 #ifdef CONFIG_MMU
 static long __get_user_pages_remote(struct task_struct *tsk,
 				    struct mm_struct *mm,
@@ -2104,11 +2123,7 @@ long get_user_pages_remote(struct task_struct *tsk, struct mm_struct *mm,
 		unsigned int gup_flags, struct page **pages,
 		struct vm_area_struct **vmas, int *locked)
 {
-	/*
-	 * FOLL_PIN must only be set internally by the pin_user_pages*() APIs,
-	 * never directly by the caller, so enforce that with an assertion:
-	 */
-	if (WARN_ON_ONCE(gup_flags & FOLL_PIN))
+	if (!is_valid_gup_flags(gup_flags))
 		return -EINVAL;
 
 	return __get_user_pages_remote(tsk, mm, start, nr_pages, gup_flags,
@@ -2156,11 +2171,7 @@ long get_user_pages(unsigned long start, unsigned long nr_pages,
 		unsigned int gup_flags, struct page **pages,
 		struct vm_area_struct **vmas)
 {
-	/*
-	 * FOLL_PIN must only be set internally by the pin_user_pages*() APIs,
-	 * never directly by the caller, so enforce that with an assertion:
-	 */
-	if (WARN_ON_ONCE(gup_flags & FOLL_PIN))
+	if (!is_valid_gup_flags(gup_flags))
 		return -EINVAL;
 
 	return __gup_longterm_locked(current, current->mm, start, nr_pages,
@@ -3007,11 +3018,7 @@ EXPORT_SYMBOL_GPL(__get_user_pages_fast);
 int get_user_pages_fast(unsigned long start, int nr_pages,
 			unsigned int gup_flags, struct page **pages)
 {
-	/*
-	 * FOLL_PIN must only be set internally by the pin_user_pages*() APIs,
-	 * never directly by the caller, so enforce that:
-	 */
-	if (WARN_ON_ONCE(gup_flags & FOLL_PIN))
+	if (!is_valid_gup_flags(gup_flags))
 		return -EINVAL;
 
 	/*
