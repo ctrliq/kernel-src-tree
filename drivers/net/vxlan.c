9f46bf59abfcc83bc2cc1dbc73ae990c58ed9a4f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:21:13 -0400

    geneve, vxlan: Don't check skb_dst() twice
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Stefano Brivio <sbrivio@redhat.com>
    commit 7463e4f9b99c089cc962033b46349ff29f466e40
    
    Commit f15ca723c1eb ("net: don't call update_pmtu unconditionally") avoids
    that we try updating PMTU for a non-existent destination, but didn't clean
    up cases where the check was already explicit. Drop those redundant checks.
    
    	Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
    	Reviewed-by: Sabrina Dubroca <sd@queasysnail.net>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7463e4f9b99c089cc962033b46349ff29f466e40)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -2192,11 +2192,7 @@ static void vxlan_xmit_one(struct sk_buff *skb, struct net_device *dev,
 		}
 
 		ndst = &rt->dst;
-		if (skb_dst(skb)) {
-			int mtu = dst_mtu(ndst) - VXLAN_HEADROOM;
-
-			skb_dst_update_pmtu(skb, mtu);
-		}
+		skb_dst_update_pmtu(skb, dst_mtu(ndst) - VXLAN_HEADROOM);
 
 		tos = ip_tunnel_ecn_encap(tos, old_iph, skb);
 		ttl = ttl ? : ip4_dst_hoplimit(&rt->dst);
@@ -2233,11 +2229,7 @@ static void vxlan_xmit_one(struct sk_buff *skb, struct net_device *dev,
 				goto out_unlock;
 		}
 
-		if (skb_dst(skb)) {
-			int mtu = dst_mtu(ndst) - VXLAN6_HEADROOM;
-
-			skb_dst_update_pmtu(skb, mtu);
-		}
+		skb_dst_update_pmtu(skb, dst_mtu(ndst) - VXLAN6_HEADROOM);
 
 		tos = ip_tunnel_ecn_encap(tos, old_iph, skb);
 		ttl = ttl ? : ip6_dst_hoplimit(ndst);
