2884a2263940dc698e89e084dec5869170ea0017
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:26:56 -0400

    net: phy: ti: remove the use of .ack_interrupt()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-331.el8
    commit-author Ioana Ciornei <ioana.ciornei@nxp.com>
    commit aa2d603ac8c0a3070e78edd4c07606ae2587754f
    
    In preparation of removing the .ack_interrupt() callback, we must replace
    its occurrences (aka phy_clear_interrupt), from the 2 places where it is
    called from (phy_enable_interrupts and phy_disable_interrupts), with
    equivalent functionality.
    
    This means that clearing interrupts now becomes something that the PHY
    driver is responsible of doing, before enabling interrupts and after
    clearing them. Make this driver follow the new contract.
    
    	Cc: Dan Murphy <dmurphy@ti.com>
    	Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit aa2d603ac8c0a3070e78edd4c07606ae2587754f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/phy/dp83640.c
+++ drivers/net/phy/dp83640.c
@@ -1197,6 +1197,10 @@ static int dp83640_config_intr(struct phy_device *phydev)
 	int err;
 
 	if (phydev->interrupts == PHY_INTERRUPT_ENABLED) {
+		err = dp83640_ack_interrupt(phydev);
+		if (err)
+			return err;
+
 		misr = phy_read(phydev, MII_DP83640_MISR);
 		if (misr < 0)
 			return misr;
@@ -1235,7 +1239,11 @@ static int dp83640_config_intr(struct phy_device *phydev)
 			MII_DP83640_MISR_DUP_INT_EN |
 			MII_DP83640_MISR_SPD_INT_EN |
 			MII_DP83640_MISR_LINK_INT_EN);
-		return phy_write(phydev, MII_DP83640_MISR, misr);
+		err = phy_write(phydev, MII_DP83640_MISR, misr);
+		if (err)
+			return err;
+
+		return dp83640_ack_interrupt(phydev);
 	}
 }
 
@@ -1579,7 +1587,6 @@ static struct phy_driver dp83640_driver = {
 	.remove		= dp83640_remove,
 	.soft_reset	= dp83640_soft_reset,
 	.config_init	= dp83640_config_init,
-	.ack_interrupt  = dp83640_ack_interrupt,
 	.config_intr    = dp83640_config_intr,
 	.handle_interrupt = dp83640_handle_interrupt,
 };
