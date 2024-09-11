88b96a3ca781c5e325b0a5e1f402a1b547eb6c6e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:37:20 -0400

    tee: enable support to register kernel memory
    jira LE-1907
    cve CVE-2021-44733
    Rebuild_History Non-Buildable kernel-4.18.0-373.el8
    commit-author Sumit Garg <sumit.garg@linaro.org>
    commit 2a6ba3f794e892c37d67b8ebb19487ce105eabc2
    
    Enable support to register kernel memory reference with TEE. This change
    will allow TEE bus drivers to register memory references.
    
    	Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    	Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    (cherry picked from commit 2a6ba3f794e892c37d67b8ebb19487ce105eabc2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/tee/tee_shm.c
+++ drivers/tee/tee_shm.c
@@ -18,6 +18,7 @@
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/tee_drv.h>
+#include <linux/uio.h>
 #include "tee_private.h"
 
 static void tee_shm_release(struct tee_shm *shm)
@@ -193,14 +194,15 @@ struct tee_shm *tee_shm_register(struct tee_context *ctx, unsigned long addr,
 				 size_t length, u32 flags)
 {
 	struct tee_device *teedev = ctx->teedev;
-	const u32 req_flags = TEE_SHM_DMA_BUF | TEE_SHM_USER_MAPPED;
+	const u32 req_user_flags = TEE_SHM_DMA_BUF | TEE_SHM_USER_MAPPED;
+	const u32 req_kernel_flags = TEE_SHM_DMA_BUF | TEE_SHM_KERNEL_MAPPED;
 	struct tee_shm *shm;
 	void *ret;
 	int rc;
 	int num_pages;
 	unsigned long start;
 
-	if (flags != req_flags)
+	if (flags != req_user_flags && flags != req_kernel_flags)
 		return ERR_PTR(-ENOTSUPP);
 
 	if (!tee_device_get(teedev))
@@ -234,7 +236,27 @@ struct tee_shm *tee_shm_register(struct tee_context *ctx, unsigned long addr,
 		goto err;
 	}
 
-	rc = get_user_pages_fast(start, num_pages, FOLL_WRITE, shm->pages);
+	if (flags & TEE_SHM_USER_MAPPED) {
+		rc = get_user_pages_fast(start, num_pages, FOLL_WRITE,
+					 shm->pages);
+	} else {
+		struct kvec *kiov;
+		int i;
+
+		kiov = kcalloc(num_pages, sizeof(*kiov), GFP_KERNEL);
+		if (!kiov) {
+			ret = ERR_PTR(-ENOMEM);
+			goto err;
+		}
+
+		for (i = 0; i < num_pages; i++) {
+			kiov[i].iov_base = (void *)(start + i * PAGE_SIZE);
+			kiov[i].iov_len = PAGE_SIZE;
+		}
+
+		rc = get_kernel_pages(kiov, num_pages, 0, shm->pages);
+		kfree(kiov);
+	}
 	if (rc > 0)
 		shm->num_pages = rc;
 	if (rc != num_pages) {
