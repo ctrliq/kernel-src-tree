919e222cef3c9eba986b564527b0edeb7864b210
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:25:48 -0400

    net/tls: Fix return values to avoid ENOTSUPP
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [net] tls: Fix return values to avoid ENOTSUPP (Sabrina Dubroca) [1798179]
    Rebuild_FUZZ: 95.24%
    commit-author Valentin Vidic <vvidic@valentin-vidic.from.hr>
    commit 4a5cdc604b9cf645e6fa24d8d9f055955c3c8516
    
    ENOTSUPP is not available in userspace, for example:
    
      setsockopt failed, 524, Unknown error 524
    
    	Signed-off-by: Valentin Vidic <vvidic@valentin-vidic.from.hr>
    	Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 4a5cdc604b9cf645e6fa24d8d9f055955c3c8516)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/tls/tls_sw.c
+++ net/tls/tls_sw.c
@@ -901,7 +901,7 @@ int tls_sw_sendmsg(struct sock *sk, struct msghdr *msg, size_t size)
 	int ret = 0;
 
 	if (msg->msg_flags & ~(MSG_MORE | MSG_DONTWAIT | MSG_NOSIGNAL))
-		return -ENOTSUPP;
+		return -EOPNOTSUPP;
 
 	lock_sock(sk);
 
@@ -1231,7 +1231,7 @@ int tls_sw_sendpage_locked(struct sock *sk, struct page *page,
 	if (flags & ~(MSG_MORE | MSG_DONTWAIT | MSG_NOSIGNAL |
 		      MSG_SENDPAGE_NOTLAST | MSG_SENDPAGE_NOPOLICY |
 		      MSG_NO_SHARED_FRAGS))
-		return -ENOTSUPP;
+		return -EOPNOTSUPP;
 
 	return tls_sw_do_sendpage(sk, page, offset, size, flags);
 }
@@ -1243,7 +1243,7 @@ int tls_sw_sendpage(struct sock *sk, struct page *page,
 
 	if (flags & ~(MSG_MORE | MSG_DONTWAIT | MSG_NOSIGNAL |
 		      MSG_SENDPAGE_NOTLAST | MSG_SENDPAGE_NOPOLICY))
-		return -ENOTSUPP;
+		return -EOPNOTSUPP;
 
 	lock_sock(sk);
 	ret = tls_sw_do_sendpage(sk, page, offset, size, flags);
@@ -1941,7 +1941,7 @@ ssize_t tls_sw_splice_read(struct socket *sock,  loff_t *ppos,
 
 		/* splice does not support reading control messages */
 		if (ctx->control != TLS_RECORD_TYPE_DATA) {
-			err = -ENOTSUPP;
+			err = -EINVAL;
 			goto splice_read_end;
 		}
 
