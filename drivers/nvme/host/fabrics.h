81ecdb928eb1a78d95e684ed61cb2e2d89105162
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:38:28 -0400

    nvme-fabrics: allow user passing data digest
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Sagi Grimberg <sagi@lightbitslabs.com>
    commit 20d44e86321299f61bb782a39aaa30f579823f58
    
    Data digest is a nvme-tcp specific feature, but nothing prevents other
    transports reusing the concept so do not associate with tcp transport
    solely.
    
    	Signed-off-by: Sagi Grimberg <sagi@lightbitslabs.com>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 20d44e86321299f61bb782a39aaa30f579823f58)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nvme/host/fabrics.h
+++ drivers/nvme/host/fabrics.h
@@ -52,6 +52,7 @@ enum {
 	NVMF_OPT_DUP_CONNECT	= 1 << 13,
 	NVMF_OPT_DISABLE_SQFLOW = 1 << 14,
 	NVMF_OPT_HDR_DIGEST	= 1 << 15,
+	NVMF_OPT_DATA_DIGEST	= 1 << 16,
 };
 
 /**
@@ -97,6 +98,7 @@ struct nvmf_ctrl_options {
 	int			max_reconnects;
 	bool			disable_sqflow;
 	bool			hdr_digest;
+	bool			data_digest;
 };
 
 /*
