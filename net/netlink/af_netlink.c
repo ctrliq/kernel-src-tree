d0d20f3e3a4caec2fc075428b382c8f91a616054
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:55:19 -0400

    net: netlink: make netlink_walk_start() void return type
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [net] netlink: make netlink_walk_start() void return type (Petr Oros) [1751273]
    Rebuild_FUZZ: 95.33%
    commit-author Taehee Yoo <ap420073@gmail.com>
    commit abf9979f7ef34eea0d610885b7335ee4d6df05f9
    
    netlink_walk_start() needed to return an error code because of
    rhashtable_walk_init(). but that was converted to rhashtable_walk_enter()
    and it is a void type function. so now netlink_walk_start() doesn't need
    any return value.
    
    	Signed-off-by: Taehee Yoo <ap420073@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit abf9979f7ef34eea0d610885b7335ee4d6df05f9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -2549,12 +2549,10 @@ struct nl_seq_iter {
 	int link;
 };
 
-static int netlink_walk_start(struct nl_seq_iter *iter)
+static void netlink_walk_start(struct nl_seq_iter *iter)
 {
 	rhashtable_walk_enter(&nl_table[iter->link].hash, &iter->hti);
 	rhashtable_walk_start(&iter->hti);
-
-	return 0;
 }
 
 static void netlink_walk_stop(struct nl_seq_iter *iter)
@@ -2570,8 +2568,6 @@ static void *__netlink_seq_next(struct seq_file *seq)
 
 	do {
 		for (;;) {
-			int err;
-
 			nlk = rhashtable_walk_next(&iter->hti);
 
 			if (IS_ERR(nlk)) {
@@ -2588,9 +2584,7 @@ static void *__netlink_seq_next(struct seq_file *seq)
 			if (++iter->link >= MAX_LINKS)
 				return NULL;
 
-			err = netlink_walk_start(iter);
-			if (err)
-				return ERR_PTR(err);
+			netlink_walk_start(iter);
 		}
 	} while (sock_net(&nlk->sk) != seq_file_net(seq));
 
@@ -2602,13 +2596,10 @@ static void *netlink_seq_start(struct seq_file *seq, loff_t *posp)
 	struct nl_seq_iter *iter = seq->private;
 	void *obj = SEQ_START_TOKEN;
 	loff_t pos;
-	int err;
 
 	iter->link = 0;
 
-	err = netlink_walk_start(iter);
-	if (err)
-		return ERR_PTR(err);
+	netlink_walk_start(iter);
 
 	for (pos = *posp; pos && obj && !IS_ERR(obj); pos--)
 		obj = __netlink_seq_next(seq);
