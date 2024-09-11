43c7fa8e639cafef16daeb0c6c2b2869fdb0d015
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:03:55 -0400

    mm/gup/writeback: add callbacks for inaccessible pages
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Claudio Imbrenda <imbrenda@linux.ibm.com>
    commit f28d43636d6f940e60abef4f0131119836c8ebd4
    
    With the introduction of protected KVM guests on s390 there is now a
    concept of inaccessible pages.  These pages need to be made accessible
    before the host can access them.
    
    While cpu accesses will trigger a fault that can be resolved, I/O accesses
    will just fail.  We need to add a callback into architecture code for
    places that will do I/O, namely when writeback is started or when a page
    reference is taken.
    
    This is not only to enable paging, file backing etc, it is also necessary
    to protect the host against a malicious user space.  For example a bad
    QEMU could simply start direct I/O on such protected memory.  We do not
    want userspace to be able to trigger I/O errors and thus the logic is
    "whenever somebody accesses that page (gup) or does I/O, make sure that
    this page can be accessed".  When the guest tries to access that page we
    will wait in the page fault handler for writeback to have finished and for
    the page_ref to be the expected value.
    
    On s390x the function is not supposed to fail, so it is ok to use a
    WARN_ON on failure.  If we ever need some more finegrained handling we can
    tackle this when we know the details.
    
    	Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Reviewed-by: David Hildenbrand <david@redhat.com>
    	Reviewed-by: Christian Borntraeger <borntraeger@de.ibm.com>
    	Reviewed-by: John Hubbard <jhubbard@nvidia.com>
    	Acked-by: Will Deacon <will@kernel.org>
    	Cc: Jan Kara <jack@suse.cz>
    	Cc: Matthew Wilcox <willy@infradead.org>
    	Cc: Ira Weiny <ira.weiny@intel.com>
    	Cc: Jérôme Glisse <jglisse@redhat.com>
    	Cc: Al Viro <viro@zeniv.linux.org.uk>
    	Cc: Christoph Hellwig <hch@infradead.org>
    	Cc: Dan Williams <dan.j.williams@intel.com>
    	Cc: Dave Chinner <david@fromorbit.com>
    	Cc: Jason Gunthorpe <jgg@ziepe.ca>
    	Cc: Jonathan Corbet <corbet@lwn.net>
    	Cc: Michal Hocko <mhocko@suse.com>
    	Cc: Mike Kravetz <mike.kravetz@oracle.com>
    	Cc: Shuah Khan <shuah@kernel.org>
    	Cc: Vlastimil Babka <vbabka@suse.cz>
    Link: http://lkml.kernel.org/r/20200306132537.783769-3-imbrenda@linux.ibm.com
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit f28d43636d6f940e60abef4f0131119836c8ebd4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/gup.c
+++ mm/gup.c
@@ -191,6 +191,7 @@ static struct page *follow_page_pte(struct vm_area_struct *vma,
 	struct page *page;
 	spinlock_t *ptl;
 	pte_t *ptep, pte;
+	int ret;
 
 retry:
 	if (unlikely(pmd_bad(*pmd)))
@@ -244,8 +245,6 @@ retry:
 		if (is_zero_pfn(pte_pfn(pte))) {
 			page = pte_page(pte);
 		} else {
-			int ret;
-
 			ret = follow_pfn_pte(vma, address, ptep, flags);
 			page = ERR_PTR(ret);
 			goto out;
@@ -253,7 +252,6 @@ retry:
 	}
 
 	if (flags & FOLL_SPLIT && PageTransCompound(page)) {
-		int ret;
 		get_page(page);
 		pte_unmap_unlock(ptep, ptl);
 		lock_page(page);
@@ -268,6 +266,19 @@ retry:
 	if (flags & FOLL_GET) {
 		if (unlikely(!try_get_page(page))) {
 			page = ERR_PTR(-ENOMEM);
+			goto out;
+		}
+	}
+	/*
+	 * We need to make the page accessible if and only if we are going
+	 * to access its content (the FOLL_PIN case).  Please see
+	 * Documentation/core-api/pin_user_pages.rst for details.
+	 */
+	if (flags & FOLL_PIN) {
+		ret = arch_make_page_accessible(page);
+		if (ret) {
+			unpin_user_page(page);
+			page = ERR_PTR(ret);
 			goto out;
 		}
 	}
@@ -1761,6 +1772,19 @@ static int gup_pte_range(pmd_t pmd, unsigned long addr, unsigned long end,
 
 		VM_BUG_ON_PAGE(compound_head(page) != head, page);
 
+		/*
+		 * We need to make the page accessible if and only if we are
+		 * going to access its content (the FOLL_PIN case).  Please
+		 * see Documentation/core-api/pin_user_pages.rst for
+		 * details.
+		 */
+		if (flags & FOLL_PIN) {
+			ret = arch_make_page_accessible(page);
+			if (ret) {
+				unpin_user_page(page);
+				goto pte_unmap;
+			}
+		}
 		SetPageReferenced(page);
 		pages[*nr] = page;
 		(*nr)++;
