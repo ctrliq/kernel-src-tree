2fbbdab6acbd4df5ef96ec6f4c63737caa535499
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:37:03 -0400

    tools/power/x86/intel-speed-select: Drop __DATE__ and __TIME__ macros
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-373.el8
    commit-author Antonio Terceiro <antonio.terceiro@linaro.org>
    commit 17de9a5654f577356f7384a5676e82e544bb2ca1
    
    These macros introduce nondeterminism in builds, and break reproducible
    builds.
    
    	Signed-off-by: Antonio Terceiro <antonio.terceiro@linaro.org>
    	Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    	Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    	Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    (cherry picked from commit 17de9a5654f577356f7384a5676e82e544bb2ca1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/power/x86/intel-speed-select/isst-config.c
+++ tools/power/x86/intel-speed-select/isst-config.c
@@ -2795,7 +2795,6 @@ static void usage(void)
 static void print_version(void)
 {
 	fprintf(outf, "Version %s\n", version_str);
-	fprintf(outf, "Build date %s time %s\n", __DATE__, __TIME__);
 	exit(0);
 }
 
