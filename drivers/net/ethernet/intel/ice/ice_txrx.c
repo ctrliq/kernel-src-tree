f660437a4a2ad978a95cba988f1014c67f732681
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:44:17 -0400

    ice: fix numeric overflow warning
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Bruce Allan <bruce.w.allan@intel.com>
    commit 32a64994dbc3e79250f99802faddc074fa8ce667
    
    When compiling and analyzing the driver on newer kernels, a static
    analyzer warns about the following "numeric overflow" issues:
    
      "The result of expression: 'budget-1' generates 4-byte type while casting
       to a bigger size of 8-byte".
    
      "The result of expression: '*words-words_read' generates 4-byte type
       while casting to a bigger size of 8-byte".
    
    Fix them both.
    
    	Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    	Signed-off-by: Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
    	Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    	Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 32a64994dbc3e79250f99802faddc074fa8ce667)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/ice/ice_txrx.c
+++ drivers/net/ethernet/intel/ice/ice_txrx.c
@@ -1104,7 +1104,7 @@ int ice_napi_poll(struct napi_struct *napi, int budget)
 	if (test_bit(ICE_FLAG_MSIX_ENA, pf->flags))
 		ice_irq_dynamic_ena(&vsi->back->hw, vsi, q_vector);
 
-	return min(work_done, budget - 1);
+	return min_t(int, work_done, budget - 1);
 }
 
 /* helper function for building cmd/type/offset */
