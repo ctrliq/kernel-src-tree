161feca755d724631a501677db0c6d3bb0ad5092
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:17:53 -0400

    x86/entry/64: Use the TSS sp2 slot for SYSCALL/SYSRET scratch space
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Andy Lutomirski <luto@kernel.org>
    commit 98f05b5138f0a9b56022295cc1387e635b25635d
    
    In the non-trampoline SYSCALL64 path, a percpu variable is used to
    temporarily store the user RSP value.
    
    Instead of a separate variable, use the otherwise unused sp2 slot in the
    TSS.  This will improve cache locality, as the sp1 slot is already used in
    the same code to find the kernel stack.  It will also simplify a future
    change to make the non-trampoline path work in PTI mode.
    
    	Signed-off-by: Andy Lutomirski <luto@kernel.org>
    	Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    	Reviewed-by: Borislav Petkov <bp@suse.de>
    	Cc: Borislav Petkov <bp@alien8.de>
    	Cc: Dave Hansen <dave.hansen@linux.intel.com>
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    	Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    	Cc: Joerg Roedel <joro@8bytes.org>
    	Cc: Jiri Olsa <jolsa@redhat.com>
    	Cc: Andi Kleen <ak@linux.intel.com>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/08e769a0023dbad4bac6f34f3631dbaf8ad59f4f.1536015544.git.luto@kernel.org
    
    (cherry picked from commit 98f05b5138f0a9b56022295cc1387e635b25635d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kernel/asm-offsets.c
+++ arch/x86/kernel/asm-offsets.c
@@ -104,7 +104,8 @@ void common(void) {
 	OFFSET(CPU_ENTRY_AREA_entry_stack, cpu_entry_area, entry_stack_page);
 	DEFINE(SIZEOF_entry_stack, sizeof(struct entry_stack));
 
-	/* Offset for sp0 and sp1 into the tss_struct */
+	/* Offset for fields in tss_struct */
 	OFFSET(TSS_sp0, tss_struct, x86_tss.sp0);
 	OFFSET(TSS_sp1, tss_struct, x86_tss.sp1);
+	OFFSET(TSS_sp2, tss_struct, x86_tss.sp2);
 }
