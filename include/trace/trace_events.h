b835637e3bc729f7558707ac942f2a7f621b8402
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:03:00 -0400

    tracing: Add trace_event helper macros __string_len() and __assign_str_len()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-394.el8
    commit-author Steven Rostedt (VMware) <rostedt@goodmis.org>
    commit 883b4aee4dec64bc807a7dda4651c6a5efe9a74d
    
    There's a few cases that a string that is to be recorded in a trace event,
    does not have a terminating 'nul' character, and instead, the tracepoint
    passes in the length of the string to record.
    
    Add two helper macros to the trace event code that lets this work easier,
    than tricks with "%.*s" logic.
    
      __string_len() which is similar to __string() for declaration, but takes a
                     length argument.
    
      __assign_str_len() which is similar to __assign_str() for assiging the
                     string, but it too takes a length argument.
    
    Note, the TRACE_EVENT() macro will allocate the location on the ring
    buffer to 'len + 1', that will be used to store the string into. It is a
    requirement that the 'len' used for this is a most the length of the
    string being recorded.
    
    This string can still use __get_str() just like strings created with
    __string() can use to retrieve the string.
    
    Link: https://lore.kernel.org/linux-nfs/20210513105018.7539996a@gandalf.local.home/
    
    	Tested-by: Chuck Lever <chuck.lever@oracle.com>
    	Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    	Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    (cherry picked from commit 883b4aee4dec64bc807a7dda4651c6a5efe9a74d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/trace/trace_events.h
+++ include/trace/trace_events.h
@@ -101,6 +101,9 @@ TRACE_MAKE_SYSTEM_STR();
 #undef __string
 #define __string(item, src) __dynamic_array(char, item, -1)
 
+#undef __string_len
+#define __string_len(item, src, len) __dynamic_array(char, item, -1)
+
 #undef __bitmask
 #define __bitmask(item, nr_bits) __dynamic_array(char, item, -1)
 
@@ -195,6 +198,9 @@ TRACE_MAKE_SYSTEM_STR();
 
 #undef __string
 #define __string(item, src) __dynamic_array(char, item, -1)
+
+#undef __string_len
+#define __string_len(item, src, len) __dynamic_array(char, item, -1)
 
 #undef __bitmask
 #define __bitmask(item, nr_bits) __dynamic_array(unsigned long, item, -1)
@@ -462,6 +468,9 @@ static struct trace_event_functions trace_event_type_funcs_##call = {	\
 #undef __string
 #define __string(item, src) __dynamic_array(char, item, -1)
 
+#undef __string_len
+#define __string_len(item, src, len) __dynamic_array(char, item, -1)
+
 #undef __bitmask
 #define __bitmask(item, nr_bits) __dynamic_array(unsigned long, item, -1)
 
@@ -520,6 +529,9 @@ trace_event_define_fields_##call(struct trace_event_call *event_call)	\
 #undef __string
 #define __string(item, src) __dynamic_array(char, item,			\
 		    strlen((src) ? (const char *)(src) : "(null)") + 1)
+
+#undef __string_len
+#define __string_len(item, src, len) __dynamic_array(char, item, (len) + 1)
 
 /*
  * __bitmask_size_in_bytes_raw is the number of bytes needed to hold
@@ -690,9 +702,19 @@ static inline notrace int trace_event_get_offsets_##call(		\
 #undef __string
 #define __string(item, src) __dynamic_array(char, item, -1)
 
+#undef __string_len
+#define __string_len(item, src, len) __dynamic_array(char, item, -1)
+
 #undef __assign_str
 #define __assign_str(dst, src)						\
 	strcpy(__get_str(dst), (src) ? (const char *)(src) : "(null)");
+
+#undef __assign_str_len
+#define __assign_str_len(dst, src, len)					\
+	do {								\
+		memcpy(__get_str(dst), (src), (len));			\
+		__get_str(dst)[len] = '\0';				\
+	} while(0)
 
 #undef __bitmask
 #define __bitmask(item, nr_bits) __dynamic_array(unsigned long, item, -1)
