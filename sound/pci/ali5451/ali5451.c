0c9a2916b2d6a430c9c329e92d2338ba187bee4f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:09:08 -0400

    ALSA: ali5451: Support PCM sync_stop
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Takashi Iwai <tiwai@suse.de>
    commit 5110bc701b4aa839f461aa5a1a6259bf91b07036
    
    The driver invokes snd_pcm_period_elapsed() simply from the interrupt
    handler.  Set card->sync_irq for enabling the missing sync_stop PCM
    operation, as well as removing the superfluous synchronize_irq()
    call.
    
    Link: https://lore.kernel.org/r/20191210063454.31603-3-tiwai@suse.de
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 5110bc701b4aa839f461aa5a1a6259bf91b07036)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/pci/ali5451/ali5451.c
+++ sound/pci/ali5451/ali5451.c
@@ -2030,6 +2030,7 @@ static int snd_ali_resources(struct snd_ali *codec)
 		return -EBUSY;
 	}
 	codec->irq = codec->pci->irq;
+	codec->card->sync_irq = codec->irq;
 	dev_dbg(codec->card->dev, "resources allocated.\n");
 	return 0;
 }
@@ -2102,8 +2103,6 @@ static int snd_ali_create(struct snd_card *card,
 		snd_ali_free(codec);
 		return -EBUSY;
 	}
-
-	synchronize_irq(pci->irq);
 
 	codec->synth.chmap = 0;
 	codec->synth.chcnt = 0;
