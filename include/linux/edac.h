60df13e2183a4bcbd2355d8fbc824871f261a955
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:43:23 -0400

    EDAC: Add RDDR5 and LRDDR5 memory types
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-373.el8
    commit-author Yazen Ghannam <yazen.ghannam@amd.com>
    commit f95711242390d759f69fd67ad46b31491fe904d6
    
    Include Registered-DDR5 and Load-Reduced DDR5 in the list of memory
    types.
    
    	Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>
    	Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lore.kernel.org/r/20211208174356.1997855-2-yazen.ghannam@amd.com
    (cherry picked from commit f95711242390d759f69fd67ad46b31491fe904d6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/edac.h
+++ include/linux/edac.h
@@ -190,6 +190,8 @@ static inline char *mc_event_error_type(const unsigned int err_type)
  * @MEM_LRDDR4:		Load-Reduced DDR4 memory.
  * @MEM_LPDDR4:		Low-Power DDR4 memory.
  * @MEM_DDR5:		Unbuffered DDR5 RAM
+ * @MEM_RDDR5:		Registered DDR5 RAM
+ * @MEM_LRDDR5:		Load-Reduced DDR5 memory.
  * @MEM_NVDIMM:		Non-volatile RAM
  * @MEM_WIO2:		Wide I/O 2.
  */
@@ -218,6 +220,8 @@ enum mem_type {
 	MEM_LRDDR4,
 	MEM_LPDDR4,
 	MEM_DDR5,
+	MEM_RDDR5,
+	MEM_LRDDR5,
 	MEM_NVDIMM,
 	MEM_WIO2,
 };
@@ -245,6 +249,8 @@ enum mem_type {
 #define MEM_FLAG_LRDDR4         BIT(MEM_LRDDR4)
 #define MEM_FLAG_LPDDR4         BIT(MEM_LPDDR4)
 #define MEM_FLAG_DDR5           BIT(MEM_DDR5)
+#define MEM_FLAG_RDDR5          BIT(MEM_RDDR5)
+#define MEM_FLAG_LRDDR5         BIT(MEM_LRDDR5)
 #define MEM_FLAG_NVDIMM         BIT(MEM_NVDIMM)
 #define MEM_FLAG_WIO2		BIT(MEM_WIO2)
 
