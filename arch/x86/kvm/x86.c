51c75244e4f39d87e3027faec2d7cc32449dd1d4
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:47:42 -0400

    x86/kvm/fpu: Remove kvm_vcpu_arch.guest_supported_xcr0
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-383.el8
    commit-author Leonardo Bras <leobras@redhat.com>
    commit 988896bb61827345c6d074dd5f2af1b7b008193f
    
    kvm_vcpu_arch currently contains the guest supported features in both
    guest_supported_xcr0 and guest_fpu.fpstate->user_xfeatures field.
    
    Currently both fields are set to the same value in
    kvm_vcpu_after_set_cpuid() and are not changed anywhere else after that.
    
    Since it's not good to keep duplicated data, remove guest_supported_xcr0.
    
    To keep the code more readable, introduce kvm_guest_supported_xcr()
    and kvm_guest_supported_xfd() to replace the previous usages of
    guest_supported_xcr0.
    
    	Signed-off-by: Leonardo Bras <leobras@redhat.com>
    Message-Id: <20220217053028.96432-3-leobras@redhat.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 988896bb61827345c6d074dd5f2af1b7b008193f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -973,6 +973,16 @@ void kvm_load_host_xsave_state(struct kvm_vcpu *vcpu)
 }
 EXPORT_SYMBOL_GPL(kvm_load_host_xsave_state);
 
+static inline u64 kvm_guest_supported_xcr0(struct kvm_vcpu *vcpu)
+{
+	return vcpu->arch.guest_fpu.fpstate->user_xfeatures;
+}
+
+static inline u64 kvm_guest_supported_xfd(struct kvm_vcpu *vcpu)
+{
+	return kvm_guest_supported_xcr0(vcpu) & XFEATURE_MASK_USER_DYNAMIC;
+}
+
 static int __kvm_set_xcr(struct kvm_vcpu *vcpu, u32 index, u64 xcr)
 {
 	u64 xcr0 = xcr;
@@ -992,7 +1002,7 @@ static int __kvm_set_xcr(struct kvm_vcpu *vcpu, u32 index, u64 xcr)
 	 * saving.  However, xcr0 bit 0 is always set, even if the
 	 * emulated CPU does not support XSAVE (see fx_init).
 	 */
-	valid_bits = vcpu->arch.guest_supported_xcr0 | XFEATURE_MASK_FP;
+	valid_bits = kvm_guest_supported_xcr0(vcpu) | XFEATURE_MASK_FP;
 	if (xcr0 & ~valid_bits)
 		return 1;
 
@@ -3587,8 +3597,7 @@ int kvm_set_msr_common(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		    !guest_cpuid_has(vcpu, X86_FEATURE_XFD))
 			return 1;
 
-		if (data & ~(XFEATURE_MASK_USER_DYNAMIC &
-			     vcpu->arch.guest_supported_xcr0))
+		if (data & ~kvm_guest_supported_xfd(vcpu))
 			return 1;
 
 		fpu_update_guest_xfd(&vcpu->arch.guest_fpu, data);
@@ -3598,8 +3607,7 @@ int kvm_set_msr_common(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		    !guest_cpuid_has(vcpu, X86_FEATURE_XFD))
 			return 1;
 
-		if (data & ~(XFEATURE_MASK_USER_DYNAMIC &
-			     vcpu->arch.guest_supported_xcr0))
+		if (data & ~kvm_guest_supported_xfd(vcpu))
 			return 1;
 
 		vcpu->arch.guest_fpu.xfd_err = data;
