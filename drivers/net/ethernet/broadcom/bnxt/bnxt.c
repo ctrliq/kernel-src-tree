a8f8ae614a4a8dcc26237922cf942fb76a1186d1
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:17:01 -0400

    bnxt_en: Call bnxt_ulp_stop()/bnxt_ulp_start() during suspend/resume.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [netdrv] bnxt_en: Call bnxt_ulp_stop()/bnxt_ulp_start() during suspend/resume (Jonathan Toppins) [1773724]
    Rebuild_FUZZ: 99.27%
    commit-author Pavan Chebbi <pavan.chebbi@broadcom.com>
    commit 6a68749dbd777b832e1d84265bd6d8b39d1843ac
    
    Inform the RDMA driver to stop/start during suspend/resume.  The
    RDMA driver needs to stop and start just like error recovery.
    
    	Signed-off-by: Pavan Chebbi <pavan.chebbi@broadcom.com>
    	Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 6a68749dbd777b832e1d84265bd6d8b39d1843ac)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -10986,6 +10986,7 @@ static int bnxt_suspend(struct device *device)
 	int rc = 0;
 
 	rtnl_lock();
+	bnxt_ulp_stop(bp);
 	if (netif_running(dev)) {
 		netif_device_detach(dev);
 		rc = bnxt_close(dev);
@@ -11019,6 +11020,7 @@ static int bnxt_resume(struct device *device)
 	}
 
 resume_exit:
+	bnxt_ulp_start(bp, rc);
 	rtnl_unlock();
 	return rc;
 }
