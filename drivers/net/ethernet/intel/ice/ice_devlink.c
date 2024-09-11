5d9b882f8860236b600cc4fa842ef458cf2e4988
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:26:46 -0400

    net: devlink: region: Pass the region ops to the snapshot function
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Andrew Lunn <andrew@lunn.ch>
    commit d4602a9f47196dd62deba66ec361b5897f1ae62b
    
    Pass the region to be snapshotted to the function performing the
    snapshot. This allows one function to operate on numerous regions.
    
    v4:
    Add missing kerneldoc for ICE
    
    	Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    	Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d4602a9f47196dd62deba66ec361b5897f1ae62b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/ice/ice_devlink.c
+++ drivers/net/ethernet/intel/ice/ice_devlink.c
@@ -411,6 +411,7 @@ void ice_devlink_destroy_port(struct ice_vsi *vsi)
 /**
  * ice_devlink_nvm_snapshot - Capture a snapshot of the Shadow RAM contents
  * @devlink: the devlink instance
+ * @ops: the devlink region being snapshotted
  * @extack: extended ACK response structure
  * @data: on exit points to snapshot data buffer
  *
@@ -423,6 +424,7 @@ void ice_devlink_destroy_port(struct ice_vsi *vsi)
  * error code on failure.
  */
 static int ice_devlink_nvm_snapshot(struct devlink *devlink,
+				    const struct devlink_region_ops *ops,
 				    struct netlink_ext_ack *extack, u8 **data)
 {
 	struct ice_pf *pf = devlink_priv(devlink);
@@ -466,6 +468,7 @@ static int ice_devlink_nvm_snapshot(struct devlink *devlink,
 /**
  * ice_devlink_devcaps_snapshot - Capture snapshot of device capabilities
  * @devlink: the devlink instance
+ * @ops: the devlink region being snapshotted
  * @extack: extended ACK response structure
  * @data: on exit points to snapshot data buffer
  *
@@ -478,6 +481,7 @@ static int ice_devlink_nvm_snapshot(struct devlink *devlink,
  */
 static int
 ice_devlink_devcaps_snapshot(struct devlink *devlink,
+			     const struct devlink_region_ops *ops,
 			     struct netlink_ext_ack *extack, u8 **data)
 {
 	struct ice_pf *pf = devlink_priv(devlink);
