d878d3147b1235d5015d00fb6df42c02e2754808
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:43:13 -0400

    net/mlx5: Warn for devlink reload when there are VFs alive
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-373.el8
    commit-author Lama Kayal <lkayal@nvidia.com>
    commit 2b0247e220975fd0a9902eef1643f5003d323ef8
    
    When performing PF reload, VF can't communicate with FW until
    it recovers and reloads as well.
    
    Add a warning message when performing devlink reload while
    VFs are still present. Thus, giving a notice of an unfavorable
    behavior that might occur as a result of a consequential reloads
    and cause interruption of VF recovery.
    
    	Signed-off-by: Lama Kayal <lkayal@nvidia.com>
    	Reviewed-by: Moshe Shemesh <moshe@nvidia.com>
    	Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    (cherry picked from commit 2b0247e220975fd0a9902eef1643f5003d323ef8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/devlink.c
+++ drivers/net/ethernet/mellanox/mlx5/core/devlink.c
@@ -135,6 +135,7 @@ static int mlx5_devlink_reload_down(struct devlink *devlink, bool netns_change,
 				    struct netlink_ext_ack *extack)
 {
 	struct mlx5_core_dev *dev = devlink_priv(devlink);
+	struct pci_dev *pdev = dev->pdev;
 	bool sf_dev_allocated;
 
 	sf_dev_allocated = mlx5_sf_dev_allocated(dev);
@@ -150,6 +151,10 @@ static int mlx5_devlink_reload_down(struct devlink *devlink, bool netns_change,
 	if (mlx5_lag_is_active(dev)) {
 		NL_SET_ERR_MSG_MOD(extack, "reload is unsupported in Lag mode");
 		return -EOPNOTSUPP;
+	}
+
+	if (pci_num_vf(pdev)) {
+		NL_SET_ERR_MSG_MOD(extack, "reload while VFs are present is unfavorable");
 	}
 
 	switch (action) {
