b6cbb50b3bb7ad2ea087c79c49bd705d5163f364
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:02:58 -0400

    gpiolib: Fix missing updates of bitmap index
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Janusz Krzysztofik <jmkrzyszt@gmail.com>
    commit 35ae7f9694e3c059a9d6a6049c7c171c8233c0c9
    
    In new code introduced by commit b17566a6b08b ("gpiolib: Implement fast
    processing path in get/set array"), bitmap index is not updated with
    next found zero bit position as it should while skipping over pins
    already processed via fast bitmap path, possibly resulting in an
    infinite loop.  Fix it.
    
    	Signed-off-by: Janusz Krzysztofik <jmkrzyszt@gmail.com>
    	Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    	Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    (cherry picked from commit 35ae7f9694e3c059a9d6a6049c7c171c8233c0c9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -2923,8 +2923,8 @@ int gpiod_get_array_value_complex(bool raw, bool can_sleep,
 			__set_bit(hwgpio, mask);
 
 			if (array_info)
-				find_next_zero_bit(array_info->get_mask,
-						   array_size, i);
+				i = find_next_zero_bit(array_info->get_mask,
+						       array_size, i);
 			else
 				i++;
 		} while ((i < array_size) &&
@@ -2948,7 +2948,8 @@ int gpiod_get_array_value_complex(bool raw, bool can_sleep,
 			trace_gpio_value(desc_to_gpio(desc), 1, value);
 
 			if (array_info)
-				find_next_zero_bit(array_info->get_mask, i, j);
+				j = find_next_zero_bit(array_info->get_mask, i,
+						       j);
 			else
 				j++;
 		}
@@ -3235,8 +3236,8 @@ int gpiod_set_array_value_complex(bool raw, bool can_sleep,
 			}
 
 			if (array_info)
-				find_next_zero_bit(array_info->set_mask,
-						   array_size, i);
+				i = find_next_zero_bit(array_info->set_mask,
+						       array_size, i);
 			else
 				i++;
 		} while ((i < array_size) &&
