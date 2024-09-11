18358b4bf9ef7a86f30cba556a8f1f98d2c8d268
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:10:16 -0400

    net: hso: Fix -Wcast-function-type
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Phong Tran <tranmanphong@gmail.com>
    commit 1d4a09dadbb9ea7baaf6be897d69020c729e7440
    
    correct usage prototype of callback in tasklet_init().
    Report by https://github.com/KSPP/linux/issues/20
    
    	Signed-off-by: Phong Tran <tranmanphong@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1d4a09dadbb9ea7baaf6be897d69020c729e7440)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/usb/hso.c
+++ drivers/net/usb/hso.c
@@ -1228,8 +1228,9 @@ static void hso_std_serial_read_bulk_callback(struct urb *urb)
  * This needs to be a tasklet otherwise we will
  * end up recursively calling this function.
  */
-static void hso_unthrottle_tasklet(struct hso_serial *serial)
+static void hso_unthrottle_tasklet(unsigned long data)
 {
+	struct hso_serial *serial = (struct hso_serial *)data;
 	unsigned long flags;
 
 	spin_lock_irqsave(&serial->serial_lock, flags);
@@ -1279,7 +1280,7 @@ static int hso_serial_open(struct tty_struct *tty, struct file *filp)
 		/* Force default termio settings */
 		_hso_serial_set_termios(tty, NULL);
 		tasklet_init(&serial->unthrottle_tasklet,
-			     (void (*)(unsigned long))hso_unthrottle_tasklet,
+			     hso_unthrottle_tasklet,
 			     (unsigned long)serial);
 		result = hso_start_serial_device(serial->parent, GFP_KERNEL);
 		if (result) {
