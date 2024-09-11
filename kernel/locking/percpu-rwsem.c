9f30fea99bae085043345f2b7f97c754c042a21e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:45:54 -0400

    locking/percpu-rwsem: Move __this_cpu_inc() into the slowpath
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Peter Zijlstra <peterz@infradead.org>
    commit 71365d40232110f7b029befc9033ea311d680611
    
    As preparation to rework __percpu_down_read() move the
    __this_cpu_inc() into it.
    
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    	Reviewed-by: Davidlohr Bueso <dbueso@suse.de>
    	Acked-by: Will Deacon <will@kernel.org>
    	Acked-by: Waiman Long <longman@redhat.com>
    	Tested-by: Juri Lelli <juri.lelli@redhat.com>
    Link: https://lkml.kernel.org/r/20200131151540.041600199@infradead.org
    (cherry picked from commit 71365d40232110f7b029befc9033ea311d680611)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/locking/percpu-rwsem.c
+++ kernel/locking/percpu-rwsem.c
@@ -42,6 +42,8 @@ EXPORT_SYMBOL_GPL(percpu_free_rwsem);
 
 int __percpu_down_read(struct percpu_rw_semaphore *sem, int try)
 {
+	__this_cpu_inc(*sem->read_count);
+
 	/*
 	 * Due to having preemption disabled the decrement happens on
 	 * the same CPU as the increment, avoiding the
