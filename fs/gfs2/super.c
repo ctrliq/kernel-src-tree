d5ffefb37d39fa58e362d54094e5e44965e08e0d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:25:39 -0400

    gfs2: Remove sb_start_write from gfs2_statfs_sync
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-331.el8
    commit-author Bob Peterson <rpeterso@redhat.com>
    commit dd64fe8167ba4cbda343f0a40c5a7c2143d48866
    
    Before this patch, function gfs2_statfs_sync called sb_start_write and
    sb_end_write. This is completely unnecessary because, aside from grabbing
    glocks, gfs2_statfs_sync does all its updates to statfs with a transaction:
    gfs2_trans_begin and _end. And transactions always do sb_start_intwrite in
    gfs2_trans_begin and sb_end_intwrite in gfs2_trans_end.
    
    This patch simply removes the call to sb_start_write.
    
    	Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    	Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit dd64fe8167ba4cbda343f0a40c5a7c2143d48866)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/gfs2/super.c
+++ fs/gfs2/super.c
@@ -345,7 +345,6 @@ int gfs2_statfs_sync(struct super_block *sb, int type)
 	struct buffer_head *m_bh, *l_bh;
 	int error;
 
-	sb_start_write(sb);
 	error = gfs2_glock_nq_init(m_ip->i_gl, LM_ST_EXCLUSIVE, GL_NOCACHE,
 				   &gh);
 	if (error)
@@ -384,7 +383,6 @@ out_bh:
 out_unlock:
 	gfs2_glock_dq_uninit(&gh);
 out:
-	sb_end_write(sb);
 	return error;
 }
 
