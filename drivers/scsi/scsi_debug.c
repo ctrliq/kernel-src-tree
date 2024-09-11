567a300b70647466babea1080473537538a3606b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:10:40 -0400

    scsi: scsi_debug: Make sdebug_build_parts() respect virtual_gb
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author John Pittman <jpittman@redhat.com>
    commit 8c6572356646f700e1679d8f7eb2ab3a4eab678c
    
    If virtual_gb is passed while using num_parts, when creating the
    partitions, virtual_gb is not respected.  Set num_sectors using
    get_sdebug_capacity() to pull virtual_gb if set.
    
    Link: https://lore.kernel.org/r/20200902211434.9979-3-jpittman@redhat.com
    	Acked-by: Douglas Gilbert <dgilbert@interlog.com>
    	Signed-off-by: John Pittman <jpittman@redhat.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 8c6572356646f700e1679d8f7eb2ab3a4eab678c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -4196,7 +4196,7 @@ static void __init sdebug_build_parts(unsigned char *ramp,
 		sdebug_num_parts = SDEBUG_MAX_PARTS;
 		pr_warn("reducing partitions to %d\n", SDEBUG_MAX_PARTS);
 	}
-	num_sectors = (int)sdebug_store_sectors;
+	num_sectors = (int)get_sdebug_capacity();
 	sectors_per_part = (num_sectors - sdebug_sectors_per)
 			   / sdebug_num_parts;
 	heads_by_sects = sdebug_heads * sdebug_sectors_per;
