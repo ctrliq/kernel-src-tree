bdd1a1d598971de999a812b2c8d803d7a8e211b0
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:10:35 -0400

    ACPI: sysfs: Drop four redundant return statements
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Clayton Casciato <majortomtosourcecontrol@gmail.com>
    commit 85c1ad47d2c7b9df335511b72b0fb6fdb230811d
    
    	Signed-off-by: Clayton Casciato <majortomtosourcecontrol@gmail.com>
    [ rjw: Subject and changelog edits ]
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 85c1ad47d2c7b9df335511b72b0fb6fdb230811d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/sysfs.c
+++ drivers/acpi/sysfs.c
@@ -588,8 +588,6 @@ static void delete_gpe_attr_array(void)
 		kfree(counter_attrs);
 	}
 	kfree(all_attrs);
-
-	return;
 }
 
 static void gpe_count(u32 gpe_number)
@@ -604,8 +602,6 @@ static void gpe_count(u32 gpe_number)
 	else
 		all_counters[num_gpes + ACPI_NUM_FIXED_EVENTS +
 			     COUNT_ERROR].count++;
-
-	return;
 }
 
 static void fixed_event_count(u32 event_number)
@@ -618,8 +614,6 @@ static void fixed_event_count(u32 event_number)
 	else
 		all_counters[num_gpes + ACPI_NUM_FIXED_EVENTS +
 			     COUNT_ERROR].count++;
-
-	return;
 }
 
 static void acpi_global_event_handler(u32 event_type, acpi_handle device,
@@ -919,8 +913,6 @@ static void __exit interrupt_stats_exit(void)
 	sysfs_remove_group(acpi_kobj, &interrupt_stats_attr_group);
 
 	delete_gpe_attr_array();
-
-	return;
 }
 
 static ssize_t
