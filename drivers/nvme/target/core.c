fb04f14e5c443393e8bd013ce38a8353eaa13257
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:38:13 -0400

    nvmet: free workqueue object if module init fails
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    commit 04db0e5ec58167364a80fd33ddb4f3b67434eb85
    
    	Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 04db0e5ec58167364a80fd33ddb4f3b67434eb85)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nvme/target/core.c
+++ drivers/nvme/target/core.c
@@ -1296,7 +1296,7 @@ static int __init nvmet_init(void)
 
 	error = nvmet_init_discovery();
 	if (error)
-		goto out;
+		goto out_free_work_queue;
 
 	error = nvmet_init_configfs();
 	if (error)
@@ -1305,6 +1305,8 @@ static int __init nvmet_init(void)
 
 out_exit_discovery:
 	nvmet_exit_discovery();
+out_free_work_queue:
+	destroy_workqueue(buffered_io_wq);
 out:
 	return error;
 }
