c4b3faa7285e50fea2ea531ae7418bb1ee278ee5
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:58:30 -0400

    scsi: smartpqi: Add new controller PCI IDs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Mike McGowen <mike.mcgowen@microchip.com>
    commit 0b93cf2a9097b1c3d75642ef878ba87f15f03043
    
    All PCI ID entries in Hex.
    Add PCI IDs for ByteDance controllers:
                                                VID  / DID  / SVID / SDID
                                                ----   ----   ----   ----
        ByteHBA JGH43024-8                      9005 / 028f / 1e93 / 1000
        ByteHBA JGH43034-8                      9005 / 028f / 1e93 / 1001
        ByteHBA JGH44014-8                      9005 / 028f / 1e93 / 1002
    
    Add PCI IDs for new Inspur controllers:
                                                VID  / DID  / SVID / SDID
                                                ----   ----   ----   ----
        INSPUR RT0800M7E                        9005 / 028f / 1bd4 / 0086
        INSPUR RT0800M7H                        9005 / 028f / 1bd4 / 0087
        INSPUR RT0804M7R                        9005 / 028f / 1bd4 / 0088
        INSPUR RT0808M7R                        9005 / 028f / 1bd4 / 0089
    
    Add PCI IDs for new FAB A controllers:
                                                VID  / DID  / SVID / SDID
                                                ----   ----   ----   ----
        Adaptec SmartRAID 3254-16e /e           9005 / 028f / 9005 / 1475
        Adaptec HBA 1200-16e                    9005 / 028f / 9005 / 14c3
        Adaptec HBA 1200-8e                     9005 / 028f / 9005 / 14c4
    
    Add H3C controller PCI IDs:
                                                VID  / DID  / SVID / SDID
                                                ----   ----   ----   ----
        H3C H4508-Mf-8i                         9005 / 028f / 193d / 110b
    
    	Reviewed-by: Scott Benesh <scott.benesh@microchip.com>
    	Reviewed-by: Scott Teel <scott.teel@microchip.com>
    	Signed-off-by: Mike McGowen <mike.mcgowen@microchip.com>
    	Signed-off-by: Don Brace <don.brace@microchip.com>
    Link: https://lore.kernel.org/r/166793530327.322537.6056884426657539311.stgit@brunhilda
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 0b93cf2a9097b1c3d75642ef878ba87f15f03043)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/smartpqi/smartpqi_init.c
+++ drivers/scsi/smartpqi/smartpqi_init.c
@@ -9299,6 +9299,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x193d, 0x110b)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x193d, 0x8460)
 	},
 	{
@@ -9396,6 +9400,22 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1bd4, 0x0072)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x0086)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x0087)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x0088)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x0089)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
@@ -9647,6 +9667,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x1475)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x1480)
 	},
 	{
@@ -9700,6 +9724,14 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x14c2)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x14c3)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x14c4)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
@@ -9936,6 +9968,18 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_LENOVO, 0x0623)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+				0x1e93, 0x1000)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+				0x1e93, 0x1001)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+				0x1e93, 0x1002)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
