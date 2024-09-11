8f7061cb51af85f793ef0b3dbbd09248ef891af6
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:10:01 -0400

    net/mlx5: Rename total_vfs to total_vports
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [netdrv] mlx5: Rename total_vfs to total_vports (Alaa Hleihel) [1724335]
    Rebuild_FUZZ: 95.00%
    commit-author Parav Pandit <parav@mellanox.com>
    commit 2aca178760288936b8b3030ca83fa53082948736
    
    Macro MLX5_TOTAL_VPORTS() returns total number of vports. Therefore,
    rename variable total_vfs to total_vports to improve code readability.
    
    	Signed-off-by: Parav Pandit <parav@mellanox.com>
    	Reviewed-by: Bodong Wang <bodong@mellanox.com>
    	Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    (cherry picked from commit 2aca178760288936b8b3030ca83fa53082948736)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
@@ -1211,14 +1211,14 @@ void esw_offloads_cleanup_reps(struct mlx5_eswitch *esw)
 
 int esw_offloads_init_reps(struct mlx5_eswitch *esw)
 {
-	int total_vfs = MLX5_TOTAL_VPORTS(esw->dev);
+	int total_vports = MLX5_TOTAL_VPORTS(esw->dev);
 	struct mlx5_core_dev *dev = esw->dev;
 	struct mlx5_esw_offload *offloads;
 	struct mlx5_eswitch_rep *rep;
 	u8 hw_id[ETH_ALEN];
 	int vport;
 
-	esw->offloads.vport_reps = kcalloc(total_vfs,
+	esw->offloads.vport_reps = kcalloc(total_vports,
 					   sizeof(struct mlx5_eswitch_rep),
 					   GFP_KERNEL);
 	if (!esw->offloads.vport_reps)
