23d283536e7027a34662a79714f2d9ae8fec58d3
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:53:14 -0400

    KVM: arm: Drop kvm_arch_vcpu_free()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Sean Christopherson <sean.j.christopherson@intel.com>
    commit 4b8fff780b07570993a966475b7500b6964cd91e
    
    Remove the superfluous kvm_arch_vcpu_free() as it is no longer called
    from commmon KVM code.  Note, kvm_arch_vcpu_destroy() *is* called from
    common code, i.e. choosing which function to whack is not completely
    arbitrary.
    
    	Acked-by: Christoffer Dall <christoffer.dall@arm.com>
    	Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 4b8fff780b07570993a966475b7500b6964cd91e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- virt/kvm/arm/arm.c
+++ virt/kvm/arm/arm.c
@@ -182,7 +182,7 @@ void kvm_arch_destroy_vm(struct kvm *kvm)
 
 	for (i = 0; i < KVM_MAX_VCPUS; ++i) {
 		if (kvm->vcpus[i]) {
-			kvm_arch_vcpu_free(kvm->vcpus[i]);
+			kvm_arch_vcpu_destroy(kvm->vcpus[i]);
 			kvm->vcpus[i] = NULL;
 		}
 	}
@@ -307,7 +307,7 @@ void kvm_arch_vcpu_postcreate(struct kvm_vcpu *vcpu)
 {
 }
 
-void kvm_arch_vcpu_free(struct kvm_vcpu *vcpu)
+void kvm_arch_vcpu_destroy(struct kvm_vcpu *vcpu)
 {
 	if (vcpu->arch.has_run_once && unlikely(!irqchip_in_kernel(vcpu->kvm)))
 		static_branch_dec(&userspace_irqchip_in_use);
@@ -317,11 +317,6 @@ void kvm_arch_vcpu_free(struct kvm_vcpu *vcpu)
 	kvm_pmu_vcpu_destroy(vcpu);
 	kvm_vcpu_uninit(vcpu);
 	kmem_cache_free(kvm_vcpu_cache, vcpu);
-}
-
-void kvm_arch_vcpu_destroy(struct kvm_vcpu *vcpu)
-{
-	kvm_arch_vcpu_free(vcpu);
 }
 
 int kvm_cpu_has_pending_timer(struct kvm_vcpu *vcpu)
