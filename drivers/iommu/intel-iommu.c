c50e2bf290d153f3d015ef549f5f1b44dbd54a64
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:33:44 -0400

    iommu/vt-d: Use pci_real_dma_dev() for mapping
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Jon Derrick <jonathan.derrick@intel.com>
    commit 2b0140c69637e6f3cf9f8a0b7629567de9645680
    
    The PCI device may have a DMA requester on another bus, such as VMD
    subdevices needing to use the VMD endpoint.  This case requires the real
    DMA device for the IOMMU mapping, so use pci_real_dma_dev() to find that
    device.
    
    Link: https://lore.kernel.org/r/1579613871-301529-5-git-send-email-jonathan.derrick@intel.com
    	Signed-off-by: Jon Derrick <jonathan.derrick@intel.com>
    	Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    	Acked-by: Lu Baolu <baolu.lu@linux.intel.com>
    (cherry picked from commit 2b0140c69637e6f3cf9f8a0b7629567de9645680)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -790,6 +790,8 @@ static struct intel_iommu *device_to_iommu(struct device *dev, u8 *bus, u8 *devf
 			return NULL;
 #endif
 
+		pdev = pci_real_dma_dev(pdev);
+
 		/* VFs aren't listed in scope tables; we need to look up
 		 * the PF instead to find the IOMMU. */
 		pf_pdev = pci_physfn(pdev);
@@ -2434,6 +2436,9 @@ static struct dmar_domain *find_domain(struct device *dev)
 
 	if (unlikely(attach_deferred(dev) || iommu_dummy(dev)))
 		return NULL;
+
+	if (dev_is_pci(dev))
+		dev = &pci_real_dma_dev(to_pci_dev(dev))->dev;
 
 	/* No lock here, assumes no domain exit in normal case */
 	info = dev->archdata.iommu;
