dfec58ca34a747201d4120bf9aef4677040f74a4
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:44:58 -0400

    PCI/ASPM: Add sysfs attributes for controlling ASPM link states
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit 72ea91afbfb08619696ccde610ee4d0d29cf4a1d
    
    Add sysfs attributes to Endpoints and other Upstream Ports to control ASPM,
    Clock PM, and L1 PM Substates.  The new attributes are:
    
      /sys/devices/pci*/.../link/clkpm
      /sys/devices/pci*/.../link/l0s_aspm
      /sys/devices/pci*/.../link/l1_aspm
      /sys/devices/pci*/.../link/l1_1_aspm
      /sys/devices/pci*/.../link/l1_2_aspm
      /sys/devices/pci*/.../link/l1_1_pcipm
      /sys/devices/pci*/.../link/l1_2_pcipm
    
    An attribute is only visible if both ends of the Link leading to the device
    support the state.  Writing y/1/on to the file enables the state; n/0/off
    disables it.
    
    These attributes can be used to tune the power/performance tradeoff for
    individual devices.
    
    [bhelgaas: commit log, rename directory to "link"]
    Link: https://lore.kernel.org/r/b1c83f8a-9bf6-eac5-82d0-cf5b90128fbf@gmail.com
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    (cherry picked from commit 72ea91afbfb08619696ccde610ee4d0d29cf4a1d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/pci/pci-sysfs.c
+++ drivers/pci/pci-sysfs.c
@@ -1757,6 +1757,9 @@ static const struct attribute_group *pci_dev_attr_groups[] = {
 #ifdef CONFIG_PCIEAER
 	&aer_stats_attr_group,
 #endif
+#ifdef CONFIG_PCIEASPM
+	&aspm_ctrl_attr_group,
+#endif
 	NULL,
 };
 
