8aa4f258c878284113a829b6d88b4d37fd881224
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:12:09 -0400

    soundwire: bus_type: introduce sdw_slave_type and sdw_master_type
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    Rebuild_CHGLOG: - [sound] ALSA: soundwire: bus_type: introduce sdw_slave_type and sdw_master_type (Jaroslav Kysela) [1869536]
    Rebuild_FUZZ: 95.59%
    commit-author Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    commit 90acca1d54ad566b4af5f1030b4a4a2420ce2ef0
    
    this is a preparatory patch before the introduction of the
    sdw_master_type. The SoundWire slave support is slightly modified with
    the use of a sdw_slave_type, and the uevent handling move to
    slave.c (since it's not necessary for the master).
    
    No functionality change other than moving code around.
    
    	Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    	Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    	Acked-by: Jaroslav Kysela <perex@perex.cz>
    Link: https://lore.kernel.org/r/20200518174322.31561-3-yung-chuan.liao@linux.intel.com
    	Signed-off-by: Vinod Koul <vkoul@kernel.org>
    (cherry picked from commit 90acca1d54ad566b4af5f1030b4a4a2420ce2ef0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/soundwire/sdw_type.h
+++ include/linux/soundwire/sdw_type.h
@@ -5,6 +5,13 @@
 #define __SOUNDWIRE_TYPES_H
 
 extern struct bus_type sdw_bus_type;
+extern struct device_type sdw_slave_type;
+extern struct device_type sdw_master_type;
+
+static inline int is_sdw_slave(const struct device *dev)
+{
+	return dev->type == &sdw_slave_type;
+}
 
 #define drv_to_sdw_driver(_drv) container_of(_drv, struct sdw_driver, driver)
 
@@ -14,6 +21,6 @@ extern struct bus_type sdw_bus_type;
 int __sdw_register_driver(struct sdw_driver *drv, struct module *);
 void sdw_unregister_driver(struct sdw_driver *drv);
 
-int sdw_slave_modalias(const struct sdw_slave *slave, char *buf, size_t size);
+int sdw_slave_uevent(struct device *dev, struct kobj_uevent_env *env);
 
 #endif /* __SOUNDWIRE_TYPES_H */
