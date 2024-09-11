a6cd578052b1726a2ad952378d1d55ee69aa9690
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:33:30 -0400

    cifs: remove unused variable 'server'
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Wei Yongjun <weiyongjun1@huawei.com>
    commit 66a4bbc327e740c65b19a471fb9911f7f4b876fb
    
    Fix build warning by removing unused variable 'server':
    
    fs/cifs/inode.c:1089:26: warning:
     variable server set but not used [-Wunused-but-set-variable]
     1089 |  struct TCP_Server_Info *server;
          |                          ^~~~~~
    
    	Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    	Signed-off-by: Steve French <stfrench@microsoft.com>
    	Reviewed-by: Aurelien Aptel <aaptel@suse.com>
    (cherry picked from commit 66a4bbc327e740c65b19a471fb9911f7f4b876fb)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/cifs/inode.c
+++ fs/cifs/inode.c
@@ -1089,7 +1089,6 @@ smb311_posix_get_inode_info(struct inode **inode,
 		    struct super_block *sb, unsigned int xid)
 {
 	struct cifs_tcon *tcon;
-	struct TCP_Server_Info *server;
 	struct tcon_link *tlink;
 	struct cifs_sb_info *cifs_sb = CIFS_SB(sb);
 	bool adjust_tz = false;
@@ -1103,7 +1102,6 @@ smb311_posix_get_inode_info(struct inode **inode,
 	if (IS_ERR(tlink))
 		return PTR_ERR(tlink);
 	tcon = tlink_tcon(tlink);
-	server = tcon->ses->server;
 
 	/*
 	 * 1. Fetch file metadata
