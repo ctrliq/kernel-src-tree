c33197d5ee531d1131d5cd4e8cd6764d3242c3de
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:56:40 -0400

    RDMA/irdma: Use auxiliary_device driver data helpers
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-394.el8
    commit-author David E. Box <david.e.box@linux.intel.com>
    commit 27963d3da4d2e0602948cf5f346a7035e694c3b4
    
    Use auxiliary_get_drvdata and auxiliary_set_drvdata helpers.
    
    	Reviewed-by: Cezary Rojewski <cezary.rojewski@intel.com>
    	Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Link: https://lore.kernel.org/r/20211221235852.323752-2-david.e.box@linux.intel.com
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 27963d3da4d2e0602948cf5f346a7035e694c3b4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -4453,7 +4453,7 @@ static int mlx5r_mp_probe(struct auxiliary_device *adev,
 	}
 	mutex_unlock(&mlx5_ib_multiport_mutex);
 
-	dev_set_drvdata(&adev->dev, mpi);
+	auxiliary_set_drvdata(adev, mpi);
 	return 0;
 }
 
@@ -4461,7 +4461,7 @@ static void mlx5r_mp_remove(struct auxiliary_device *adev)
 {
 	struct mlx5_ib_multiport_info *mpi;
 
-	mpi = dev_get_drvdata(&adev->dev);
+	mpi = auxiliary_get_drvdata(adev);
 	mutex_lock(&mlx5_ib_multiport_mutex);
 	if (mpi->ibdev)
 		mlx5_ib_unbind_slave_port(mpi->ibdev, mpi);
@@ -4511,7 +4511,7 @@ static int mlx5r_probe(struct auxiliary_device *adev,
 		return ret;
 	}
 
-	dev_set_drvdata(&adev->dev, dev);
+	auxiliary_set_drvdata(adev, dev);
 	return 0;
 }
 
@@ -4519,7 +4519,7 @@ static void mlx5r_remove(struct auxiliary_device *adev)
 {
 	struct mlx5_ib_dev *dev;
 
-	dev = dev_get_drvdata(&adev->dev);
+	dev = auxiliary_get_drvdata(adev);
 	__mlx5_ib_remove(dev, dev->profile, MLX5_IB_STAGE_MAX);
 }
 
