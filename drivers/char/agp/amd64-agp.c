0c73210af1ca39a25803438a3fb5ae2bcd85a1df
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:39:13 -0400

    x86/amd_nb, char/amd64-agp: Use amd_nb_num() accessor
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Borislav Petkov <bp@suse.de>
    commit 19d33357ecdf6f4d591b4c17f119bacd6ae834eb
    
    ... to find whether there are northbridges present on the
    system. Convert the last forgotten user and therefore, unexport
    amd_nb_misc_ids[] too.
    
    	Signed-off-by: Borislav Petkov <bp@suse.de>
    	Cc: Michal Kubecek <mkubecek@suse.cz>
    	Cc: Yazen Ghannam <yazen.ghannam@amd.com>
    Link: https://lkml.kernel.org/r/20200316150725.925-1-bp@alien8.de
    (cherry picked from commit 19d33357ecdf6f4d591b4c17f119bacd6ae834eb)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/char/agp/amd64-agp.c
+++ drivers/char/agp/amd64-agp.c
@@ -774,7 +774,7 @@ int __init agp_amd64_init(void)
 		}
 
 		/* First check that we have at least one AMD64 NB */
-		if (!pci_dev_present(amd_nb_misc_ids)) {
+		if (!amd_nb_num()) {
 			pci_unregister_driver(&agp_amd64_pci_driver);
 			return -ENODEV;
 		}
