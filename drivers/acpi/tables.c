89d61025acf156560a320969ba4602beed6aecc8
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:26:31 -0400

    acpi: Disable ACPI table override if the kernel is locked down
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Linn Crosetto <lcrosetto@gmail.com>
    commit 6ea0e815fc5e18597724169caa6e4d46dd8e693d
    
    >From the kernel documentation (initrd_table_override.txt):
    
      If the ACPI_INITRD_TABLE_OVERRIDE compile option is true, it is possible
      to override nearly any ACPI table provided by the BIOS with an
      instrumented, modified one.
    
    When lockdown is enabled, the kernel should disallow any unauthenticated
    changes to kernel space.  ACPI tables contain code invoked by the kernel,
    so do not allow ACPI tables to be overridden if the kernel is locked down.
    
    	Signed-off-by: Linn Crosetto <lcrosetto@gmail.com>
    	Signed-off-by: David Howells <dhowells@redhat.com>
    	Signed-off-by: Matthew Garrett <mjg59@google.com>
    	Reviewed-by: Kees Cook <keescook@chromium.org>
    cc: linux-acpi@vger.kernel.org
    	Signed-off-by: James Morris <jmorris@namei.org>
    (cherry picked from commit 6ea0e815fc5e18597724169caa6e4d46dd8e693d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/tables.c
+++ drivers/acpi/tables.c
@@ -35,6 +35,7 @@
 #include <linux/earlycpio.h>
 #include <linux/memblock.h>
 #include <linux/initrd.h>
+#include <linux/security.h>
 #include "internal.h"
 
 #ifdef CONFIG_ACPI_CUSTOM_DSDT
@@ -531,6 +532,11 @@ void __init acpi_table_upgrade(void)
 	}
 	if (table_nr == 0)
 		return;
+
+	if (security_locked_down(LOCKDOWN_ACPI_TABLES)) {
+		pr_notice("kernel is locked down, ignoring table override\n");
+		return;
+	}
 
 	acpi_tables_addr =
 		memblock_find_in_range(0, ACPI_TABLE_UPGRADE_MAX_PHYS,
