7b78cd7e422fc86fd0fbbf013e356408164d8481
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:30:13 -0400

    KVM: pass through CPUID(0x80000006)
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Eric Northup <digitaleric@gmail.com>
    commit 43d05de2bee7561b7958628b7c27c89b779de990
    
    Return the host's L2 cache and TLB information for CPUID.0x80000006
    instead of zeroing out the entry as part of KVM_GET_SUPPORTED_CPUID.
    This allows a userspace VMM to feed KVM_GET_SUPPORTED_CPUID's output
    directly into KVM_SET_CPUID2 (without breaking the guest).
    
    	Signed-off-by: Eric Northup (Google) <digitaleric@gmail.com>
    	Signed-off-by: Jim Mattson <jmattson@google.com>
    	Signed-off-by: Jon Cargille <jcargill@google.com>
    Message-Id: <20200415012320.236065-1-jcargill@google.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 43d05de2bee7561b7958628b7c27c89b779de990)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -752,6 +752,9 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		entry->ecx &= kvm_cpuid_8000_0001_ecx_x86_features;
 		cpuid_mask(&entry->ecx, CPUID_8000_0001_ECX);
 		break;
+	case 0x80000006:
+		/* L2 cache and TLB: pass through host info. */
+		break;
 	case 0x80000007: /* Advanced power management */
 		/* invariant TSC is CPUID.80000007H:EDX[8] */
 		entry->edx &= (1 << 8);
