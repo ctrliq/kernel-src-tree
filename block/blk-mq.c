2320edc2684e8ebc864157485d43d796fc97fba6
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:43:38 -0400

    blk-mq: remove needless goto from blk_mq_get_driver_tag
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author André Almeida <andrealmeid@collabora.com>
    commit 1fead7182f381ab0ebab5eaf1a060a15550da994
    
    The only usage of the label "done" is when (rq->tag != -1) at the
    beginning of the function. Rather than jumping to label, we can just
    remove this label and execute the code at the "if". Besides that, the
    code that would be executed after the label "done" is the return of the
    logical expression (rq->tag != -1) but since we are already inside the
    if, we now that this is true. Remove the label and replace the goto with
    the proper result of the label.
    
    	Signed-off-by: André Almeida <andrealmeid@collabora.com>
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 1fead7182f381ab0ebab5eaf1a060a15550da994)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1032,7 +1032,7 @@ bool blk_mq_get_driver_tag(struct request *rq)
 	bool shared;
 
 	if (rq->tag != -1)
-		goto done;
+		return true;
 
 	if (blk_mq_tag_is_reserved(data.hctx->sched_tags, rq->internal_tag))
 		data.flags |= BLK_MQ_REQ_RESERVED;
@@ -1047,7 +1047,6 @@ bool blk_mq_get_driver_tag(struct request *rq)
 		data.hctx->tags->rqs[rq->tag] = rq;
 	}
 
-done:
 	return rq->tag != -1;
 }
 
