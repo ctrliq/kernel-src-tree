4e69a25889814328b215164b012165d993fdb0bf
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:01:07 -0400

    PM-runtime: Fix __pm_runtime_set_status() race with runtime resume
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    commit c1567f813a9992a64f8d0f6cfb912c3922812c35
    
    Commit 4080ab083000 ("PM-runtime: Take suppliers into account in
    __pm_runtime_set_status()") introduced a race condition that may
    trigger if __pm_runtime_set_status() is used incorrectly (that is,
    if it is called when PM-runtime is enabled for the target device
    and working).
    
    In that case, if the original PM-runtime status of the device is
    RPM_SUSPENDED, a runtime resume of the device may occur after
    __pm_runtime_set_status() has dropped its power.lock spinlock
    and before deactivating its suppliers, so the suppliers may be
    deactivated while the device is PM-runtime-active which may lead
    to functional issues.
    
    To avoid that, modify __pm_runtime_set_status() to check whether
    or not PM-runtime is enabled for the device before activating its
    suppliers (if the new status is RPM_ACTIVE) and either return an
    error if that's the case or increment the device's disable_depth
    counter to prevent PM-runtime from being enabled for it while
    the remaining part of the function is running (disable_depth is
    then decremented on the way out).
    
    Fixes: 4080ab083000 ("PM-runtime: Take suppliers into account in __pm_runtime_set_status()")
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    	Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit c1567f813a9992a64f8d0f6cfb912c3922812c35)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/base/power/runtime.c
+++ drivers/base/power/runtime.c
@@ -1114,6 +1114,22 @@ int __pm_runtime_set_status(struct device *dev, unsigned int status)
 	if (status != RPM_ACTIVE && status != RPM_SUSPENDED)
 		return -EINVAL;
 
+	spin_lock_irq(&dev->power.lock);
+
+	/*
+	 * Prevent PM-runtime from being enabled for the device or return an
+	 * error if it is enabled already and working.
+	 */
+	if (dev->power.runtime_error || dev->power.disable_depth)
+		dev->power.disable_depth++;
+	else
+		error = -EAGAIN;
+
+	spin_unlock_irq(&dev->power.lock);
+
+	if (error)
+		return error;
+
 	/*
 	 * If the new status is RPM_ACTIVE, the suppliers can be activated
 	 * upfront regardless of the current status, because next time
@@ -1131,12 +1147,6 @@ int __pm_runtime_set_status(struct device *dev, unsigned int status)
 	}
 
 	spin_lock_irq(&dev->power.lock);
-
-	if (!dev->power.runtime_error && !dev->power.disable_depth) {
-		status = dev->power.runtime_status;
-		error = -EAGAIN;
-		goto out;
-	}
 
 	if (dev->power.runtime_status == status || !parent)
 		goto out_set;
@@ -1189,6 +1199,8 @@ int __pm_runtime_set_status(struct device *dev, unsigned int status)
 
 		device_links_read_unlock(idx);
 	}
+
+	pm_runtime_enable(dev);
 
 	return error;
 }
