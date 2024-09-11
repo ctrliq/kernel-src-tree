6b21a3c83abc841e1520eeb2b53626a5da905eda
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:35:00 -0400

    scsi: qla2xxx: remove redundant null check on pointer sess
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Colin Ian King <colin.king@canonical.com>
    commit bb6abdd453e123bebdf949a03df5e846ecb5866b
    
    The null check on pointer sess and the subsequent call is redundant as sess
    is null on all the the paths that lead to the out_term2 label.  Hence the
    null check and the call can be removed.  Also remove the redundant setting
    of sess to NULL as this is not required now.
    
    Detected by CoverityScan, CID#1420663 ("Logically dead code")
    
    	Signed-off-by: Colin Ian King <colin.king@canonical.com>
    	Acked-by: Himanshu Madhani <hmadhani@marvell.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit bb6abdd453e123bebdf949a03df5e846ecb5866b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/qla2xxx/qla_target.c
+++ drivers/scsi/qla2xxx/qla_target.c
@@ -6192,7 +6192,7 @@ static void qlt_tmr_work(struct qla_tgt *tgt,
 	struct atio_from_isp *a = &prm->tm_iocb2;
 	struct scsi_qla_host *vha = tgt->vha;
 	struct qla_hw_data *ha = vha->hw;
-	struct fc_port *sess = NULL;
+	struct fc_port *sess;
 	unsigned long flags;
 	uint8_t *s_id = NULL; /* to hide compiler warnings */
 	int rc;
@@ -6218,7 +6218,6 @@ static void qlt_tmr_work(struct qla_tgt *tgt,
 			goto out_term2;
 	} else {
 		if (sess->deleted) {
-			sess = NULL;
 			goto out_term2;
 		}
 
@@ -6226,7 +6225,6 @@ static void qlt_tmr_work(struct qla_tgt *tgt,
 			ql_dbg(ql_dbg_tgt_tmr, vha, 0xf020,
 			    "%s: kref_get fail %8phC\n",
 			     __func__, sess->port_name);
-			sess = NULL;
 			goto out_term2;
 		}
 	}
@@ -6245,8 +6243,6 @@ static void qlt_tmr_work(struct qla_tgt *tgt,
 	return;
 
 out_term2:
-	if (sess)
-		ha->tgt.tgt_ops->put_sess(sess);
 	spin_unlock_irqrestore(&ha->tgt.sess_lock, flags);
 out_term:
 	qlt_send_term_exchange(ha->base_qpair, NULL, &prm->tm_iocb2, 1, 0);
