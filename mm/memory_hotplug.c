8bd9485adca7db473b62e632d049e42a8078d45b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:17:14 -0400

    mm, memory_hotplug: print reason for the offlining failure
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Michal Hocko <mhocko@suse.com>
    commit 7960509329c24a2bf0bc4929636614a1b7bb4443
    
    The memory offlining failure reporting is inconsistent and insufficient.
    Some error paths simply do not report the failure to the log at all.  When
    we do report there are no details about the reason of the failure and
    there are several of them which makes memory offlining failures hard to
    debug.
    
    Make sure that the
    	memory offlining [mem %#010llx-%#010llx] failed
    message is printed for all failures and also provide a short textual
    reason for the failure e.g.
    
    [ 1984.506184] rac1 kernel: memory offlining [mem 0x82600000000-0x8267fffffff] failed due to signal backoff
    
    this tells us that the offlining has failed because of a signal pending
    aka user intervention.
    
    [akpm@linux-foundation.org: tweak messages a bit]
    Link: http://lkml.kernel.org/r/20181107101830.17405-5-mhocko@kernel.org
    	Signed-off-by: Michal Hocko <mhocko@suse.com>
    	Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    	Reviewed-by: Oscar Salvador <osalvador@suse.de>
    	Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    	Cc: Baoquan He <bhe@redhat.com>
    	Cc: Oscar Salvador <OSalvador@suse.com>
    	Cc: William Kucharski <william.kucharski@oracle.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 7960509329c24a2bf0bc4929636614a1b7bb4443)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1523,6 +1523,7 @@ static int __ref __offline_pages(unsigned long start_pfn,
 	unsigned long valid_start, valid_end;
 	struct zone *zone;
 	struct memory_notify arg;
+	char *reason;
 
 	mem_hotplug_begin();
 
@@ -1531,7 +1532,9 @@ static int __ref __offline_pages(unsigned long start_pfn,
 	if (!test_pages_in_a_zone(start_pfn, end_pfn, &valid_start,
 				  &valid_end)) {
 		mem_hotplug_done();
-		return -EINVAL;
+		ret = -EINVAL;
+		reason = "multizone range";
+		goto failed_removal;
 	}
 
 	zone = page_zone(pfn_to_page(valid_start));
@@ -1543,7 +1546,8 @@ static int __ref __offline_pages(unsigned long start_pfn,
 				       MIGRATE_MOVABLE, true);
 	if (ret < 0) {
 		mem_hotplug_done();
-		return ret;
+		reason = "failure to isolate range";
+		goto failed_removal;
 	}
 
 	nr_isolate_pageblock = ret;
@@ -1554,15 +1558,19 @@ static int __ref __offline_pages(unsigned long start_pfn,
 
 	ret = memory_notify(MEM_GOING_OFFLINE, &arg);
 	ret = notifier_to_errno(ret);
-	if (ret)
-		goto failed_removal;
+	if (ret) {
+		reason = "notifier failure";
+		goto failed_removal_isolated;
+	}
 
 	pfn = start_pfn;
 repeat:
 	/* start memory hot removal */
 	ret = -EINTR;
-	if (signal_pending(current))
-		goto failed_removal;
+	if (signal_pending(current)) {
+		reason = "signal backoff";
+		goto failed_removal_isolated;
+	}
 
 	cond_resched();
 	lru_add_drain_all();
@@ -1579,8 +1587,10 @@ repeat:
 	 * actually in order to make hugetlbfs's object counting consistent.
 	 */
 	ret = dissolve_free_huge_pages(start_pfn, end_pfn);
-	if (ret)
-		goto failed_removal;
+	if (ret) {
+		reason = "failure to dissolve huge pages";
+		goto failed_removal_isolated;
+	}
 	/* check again */
 	offlined_pages = check_pages_isolated(start_pfn, end_pfn);
 	if (offlined_pages < 0)
@@ -1629,13 +1639,15 @@ repeat:
 	mem_hotplug_done();
 	return 0;
 
+failed_removal_isolated:
+	undo_isolate_page_range(start_pfn, end_pfn, MIGRATE_MOVABLE);
 failed_removal:
-	pr_debug("memory offlining [mem %#010llx-%#010llx] failed\n",
+	pr_debug("memory offlining [mem %#010llx-%#010llx] failed due to %s\n",
 		 (unsigned long long) start_pfn << PAGE_SHIFT,
-		 ((unsigned long long) end_pfn << PAGE_SHIFT) - 1);
+		 ((unsigned long long) end_pfn << PAGE_SHIFT) - 1,
+		 reason);
 	memory_notify(MEM_CANCEL_OFFLINE, &arg);
 	/* pushback to free area */
-	undo_isolate_page_range(start_pfn, end_pfn, MIGRATE_MOVABLE);
 	mem_hotplug_done();
 	return ret;
 }
