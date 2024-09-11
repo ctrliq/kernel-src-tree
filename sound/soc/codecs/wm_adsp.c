47b91728aff099ce8a3de8b054a4fa8e610a1bf6
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:56:44 -0400

    ASoC: wm_adsp: Make use of local variables
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    Rebuild_CHGLOG: - [sound] ALSA: ASoC: wm_adsp: Make use of local variables (Jaroslav Kysela) [1683051]
    Rebuild_FUZZ: 93.33%
    commit-author Charles Keepax <ckeepax@opensource.cirrus.com>
    commit 789b930a8f0de609fd2ef9f2ebf73726b7087fea
    
    Tidy up some instances of dereferencing to obtain things that are
    already stored in local variables.
    
    	Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 789b930a8f0de609fd2ef9f2ebf73726b7087fea)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -3374,7 +3374,7 @@ static int wm_adsp_compr_attach(struct wm_adsp_compr *compr)
 		return -EINVAL;
 
 	compr->buf = buf;
-	compr->buf->compr = compr;
+	buf->compr = compr;
 
 	return 0;
 }
@@ -4082,7 +4082,7 @@ int wm_adsp_compr_pointer(struct snd_compr_stream *stream,
 
 	buf = compr->buf;
 
-	if (!compr->buf || compr->buf->error) {
+	if (!buf || buf->error) {
 		snd_compr_stop_error(stream, SNDRV_PCM_STATE_XRUN);
 		ret = -EIO;
 		goto out;
@@ -4102,7 +4102,7 @@ int wm_adsp_compr_pointer(struct snd_compr_stream *stream,
 		if (buf->avail < wm_adsp_compr_frag_words(compr)) {
 			ret = wm_adsp_buffer_get_error(buf);
 			if (ret < 0) {
-				if (compr->buf->error)
+				if (buf->error)
 					snd_compr_stop_error(stream,
 							SNDRV_PCM_STATE_XRUN);
 				goto out;
