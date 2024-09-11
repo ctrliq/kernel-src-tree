f01037e453e7ba95609bc767e4ff52da483a996e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:35:03 -0400

    mptcp: allow creating non-backup subflows
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Paolo Abeni <pabeni@redhat.com>
    commit 4596a2c1b7f55b8dac9ef4d8724cbef6e11eb74c
    
    Currently the 'backup' attribute of local endpoint
    is ignored. Let's use it for the MP_JOIN handshake
    
    	Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    	Reviewed-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 4596a2c1b7f55b8dac9ef4d8724cbef6e11eb74c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/mptcp/subflow.c
+++ net/mptcp/subflow.c
@@ -20,6 +20,7 @@
 #include <net/ip6_route.h>
 #endif
 #include <net/mptcp.h>
+#include <uapi/linux/mptcp.h>
 #include "protocol.h"
 #include "mib.h"
 
@@ -1059,7 +1060,7 @@ int __mptcp_subflow_connect(struct sock *sk, int ifindex,
 	subflow->remote_token = remote_token;
 	subflow->local_id = loc->id;
 	subflow->request_join = 1;
-	subflow->request_bkup = 1;
+	subflow->request_bkup = !!(loc->flags & MPTCP_PM_ADDR_FLAG_BACKUP);
 	mptcp_info2sockaddr(remote, &addr);
 
 	err = kernel_connect(sf, (struct sockaddr *)&addr, addrlen, O_NONBLOCK);
