d4e73947152e0609f9b3707a101a5c67c838b2d6
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:44:37 -0400

    libnvdimm: Move nvdimm_attribute_group to device_type
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Dan Williams <dan.j.williams@intel.com>
    commit 360eba7ebdf716194ed2ede1ebc3ce0f9790a91c
    
    A 'struct device_type' instance can carry default attributes for the
    device. Use this facility to remove the export of
    nvdimm_attribute_group and put the responsibility on the core rather
    than leaf implementations to define this attribute.
    
    	Cc: Ira Weiny <ira.weiny@intel.com>
    	Cc: Michael Ellerman <mpe@ellerman.id.au>
    	Cc: "Oliver O'Halloran" <oohall@gmail.com>
    	Cc: Vishal Verma <vishal.l.verma@intel.com>
    	Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    	Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    	Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Link: https://lore.kernel.org/r/157309903201.1582359.10966209746585062329.stgit@dwillia2-desk3.amr.corp.intel.com
    (cherry picked from commit 360eba7ebdf716194ed2ede1ebc3ce0f9790a91c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/nfit/core.c
+++ drivers/acpi/nfit/core.c
@@ -1706,7 +1706,6 @@ static const struct attribute_group acpi_nfit_dimm_attribute_group = {
 };
 
 static const struct attribute_group *acpi_nfit_dimm_attribute_groups[] = {
-	&nvdimm_attribute_group,
 	&acpi_nfit_dimm_attribute_group,
 	NULL,
 };
