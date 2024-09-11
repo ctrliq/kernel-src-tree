8906033ea34def46e7f6edb875e189c7ed9e4177
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:31:47 -0400

    tcp: remove SOCK_QUEUE_SHRUNK
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Eric Dumazet <edumazet@google.com>
    commit 0cbe6a8f089e5912a577537c97833546d558c357
    
    SOCK_QUEUE_SHRUNK is currently used by TCP as a temporary state
    that remembers if some room has been made in the rtx queue
    by an incoming ACK packet.
    
    This is later used from tcp_check_space() before
    considering to send EPOLLOUT.
    
    Problem is: If we receive SACK packets, and no packet
    is removed from RTX queue, we can send fresh packets, thus
    moving them from write queue to rtx queue and eventually
    empty the write queue.
    
    This stall can happen if TCP_NOTSENT_LOWAT is used.
    
    With this fix, we no longer risk stalling sends while holes
    are repaired, and we can fully use socket sndbuf.
    
    This also removes a cache line dirtying for typical RPC
    workloads.
    
    Fixes: c9bee3b7fdec ("tcp: TCP_NOTSENT_LOWAT socket option")
    	Signed-off-by: Eric Dumazet <edumazet@google.com>
    	Cc: Soheil Hassas Yeganeh <soheil@google.com>
    	Acked-by: Neal Cardwell <ncardwell@google.com>
    	Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0cbe6a8f089e5912a577537c97833546d558c357)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -846,7 +846,6 @@ enum sock_flags {
 	SOCK_RCVTSTAMP, /* %SO_TIMESTAMP setting */
 	SOCK_RCVTSTAMPNS, /* %SO_TIMESTAMPNS setting */
 	SOCK_LOCALROUTE, /* route locally only, %SO_DONTROUTE setting */
-	SOCK_QUEUE_SHRUNK, /* write queue has been shrunk recently */
 	SOCK_MEMALLOC, /* VM depends on this socket for swapping */
 	SOCK_TIMESTAMPING_RX_SOFTWARE,  /* %SOF_TIMESTAMPING_RX_SOFTWARE */
 	SOCK_FASYNC, /* fasync() active */
@@ -1529,7 +1528,6 @@ static inline void sk_mem_uncharge(struct sock *sk, int size)
 
 static inline void sk_wmem_free_skb(struct sock *sk, struct sk_buff *skb)
 {
-	sock_set_flag(sk, SOCK_QUEUE_SHRUNK);
 	sk_wmem_queued_add(sk, -skb->truesize);
 	sk_mem_uncharge(sk, skb->truesize);
 	__kfree_skb(skb);
