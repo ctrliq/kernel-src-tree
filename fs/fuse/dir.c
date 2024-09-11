0ddcffff782019ab23890563e1980d387e7ca1e3
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:49:58 -0400

    fuse: kmemcg account fs data
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Khazhismel Kumykov <khazhy@google.com>
    commit dc69e98c241e1456e37d73b862f7b8b8900ba50f
    
    account per-file, dentry, and inode data
    
    blockdev/superblock and temporary per-request data was left alone, as
    this usually isn't accounted
    
    	Reviewed-by: Shakeel Butt <shakeelb@google.com>
    	Signed-off-by: Khazhismel Kumykov <khazhy@google.com>
    	Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit dc69e98c241e1456e37d73b862f7b8b8900ba50f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -237,7 +237,8 @@ invalid:
 
 static int fuse_dentry_init(struct dentry *dentry)
 {
-	dentry->d_fsdata = kzalloc(sizeof(union fuse_dentry), GFP_KERNEL);
+	dentry->d_fsdata = kzalloc(sizeof(union fuse_dentry),
+				   GFP_KERNEL_ACCOUNT | __GFP_RECLAIMABLE);
 
 	return dentry->d_fsdata ? 0 : -ENOMEM;
 }
