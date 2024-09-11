69ab098351b4874836b1d73d14623b4d95d6a58a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:15:43 -0400

    xfs: fix scrub and remount-ro protection when running scrub
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-326.el8
    commit-author Darrick J. Wong <djwong@kernel.org>
    commit 71bddbccab436a261a22afe5d90de269941d0fe7
    
    While running a new fstest that races a readonly remount with scrub
    running in repair mode, I observed the kernel tripping over debugging
    assertions in the log quiesce code that were checking that the CIL was
    empty.  When the sysadmin runs scrub in repair mode, the scrub code
    allocates real transactions (with reservations) to change things, but
    doesn't increment the superblock writers count to block a readonly
    remount attempt while it is running.
    
    We don't require the userspace caller to have a writable file descriptor
    to run repairs, so we have to call mnt_want_write_file to obtain freeze
    protection and increment the writers count.  It's ok to remove the call
    to sb_start_write for the dry-run case because commit 8321ddb2fa29
    removed the behavior where scrub and fsfreeze fight over the buffer LRU.
    
    	Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    	Reviewed-by: Chandan Babu R <chandanrlinux@gmail.com>
    	Reviewed-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 71bddbccab436a261a22afe5d90de269941d0fe7)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -1856,7 +1856,7 @@ out_free:
 
 STATIC int
 xfs_ioc_scrub_metadata(
-	struct xfs_inode		*ip,
+	struct file			*file,
 	void				__user *arg)
 {
 	struct xfs_scrub_metadata	scrub;
@@ -1868,7 +1868,7 @@ xfs_ioc_scrub_metadata(
 	if (copy_from_user(&scrub, arg, sizeof(scrub)))
 		return -EFAULT;
 
-	error = xfs_scrub_metadata(ip, &scrub);
+	error = xfs_scrub_metadata(file, &scrub);
 	if (error)
 		return error;
 
@@ -2167,7 +2167,7 @@ xfs_file_ioctl(
 		return xfs_ioc_getfsmap(ip, arg);
 
 	case XFS_IOC_SCRUB_METADATA:
-		return xfs_ioc_scrub_metadata(ip, arg);
+		return xfs_ioc_scrub_metadata(filp, arg);
 
 	case XFS_IOC_FD_TO_HANDLE:
 	case XFS_IOC_PATH_TO_HANDLE:
