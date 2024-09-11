838d3a6a0942a22e76321412a439a199b86c3dc8
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:33:07 -0400

    ASoC: SOF: Intel: hda: use SOF defined init chip in resume
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [sound] ALSA: ASoC: SOF: Intel: hda: use SOF defined init chip in resume (Jaroslav Kysela) [1738610]
    Rebuild_FUZZ: 95.08%
    commit-author Zhu Yingjiang <yingjiang.zhu@linux.intel.com>
    commit d06973515f95ac74d68fb3e1f1fb4b055531296c
    
    Unify resume code by using SOF common function hda_dsp_ctrl_init_chip()
    which can handle both HDA and non-HDA cases. Move code to reset
    stream-to-link mapping into hda_dsp_ctrl_init_chip().
    
    	Signed-off-by: Zhu Yingjiang <yingjiang.zhu@linux.intel.com>
    	Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20190722141402.7194-15-pierre-louis.bossart@linux.intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit d06973515f95ac74d68fb3e1f1fb4b055531296c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/sof/intel/hda-ctrl.c
+++ sound/soc/sof/intel/hda-ctrl.c
@@ -164,6 +164,9 @@ int hda_dsp_ctrl_clock_power_gating(struct snd_sof_dev *sdev, bool enable)
 int hda_dsp_ctrl_init_chip(struct snd_sof_dev *sdev, bool full_reset)
 {
 	struct hdac_bus *bus = sof_to_bus(sdev);
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_HDA)
+	struct hdac_ext_link *hlink;
+#endif
 	struct hdac_stream *stream;
 	int sd_offset, ret = 0;
 
@@ -255,6 +258,12 @@ int hda_dsp_ctrl_init_chip(struct snd_sof_dev *sdev, bool full_reset)
 		snd_hdac_chip_writel(bus, DPUBASE,
 				     upper_32_bits(bus->posbuf.addr));
 	}
+#endif
+
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_HDA)
+	/* Reset stream-to-link mapping */
+	list_for_each_entry(hlink, &bus->hlink_list, list)
+		bus->io_ops->reg_writel(0, hlink->ml_addr + AZX_REG_ML_LOSIDV);
 #endif
 
 	bus->chip_init = true;
