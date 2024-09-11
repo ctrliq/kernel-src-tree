cfa13688686b31ffb2a3b146d9a8aff525fc0538
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:05:46 -0400

    phy: Revert toggling reset changes.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    Rebuild_CHGLOG: - [netdrv] phy: Revert toggling reset changes (Petr Oros) [1772010]
    Rebuild_FUZZ: 98.55%
    commit-author David S. Miller <davem@davemloft.net>
    commit 7b566f70e1bf65b189b66eb3de6f431c30f7dff2
    
    This reverts:
    
    ef1b5bf506b1 ("net: phy: Fix not to call phy_resume() if PHY is not attached")
    8c85f4b81296 ("net: phy: micrel: add toggling phy reset if PHY is not  attached")
    
    Andrew Lunn informs me that there are alternative efforts
    underway to fix this more properly.
    
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7b566f70e1bf65b189b66eb3de6f431c30f7dff2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -259,7 +259,7 @@ static bool phy_may_suspend(struct phy_device *phydev)
 }
 
 #ifdef CONFIG_PM
-static bool mdio_bus_phy_may_suspend(struct phy_device *phydev, bool suspend)
+static bool mdio_bus_phy_may_suspend(struct phy_device *phydev)
 {
 	struct device_driver *drv = phydev->mdio.dev.driver;
 	struct phy_driver *phydrv = to_phy_driver(drv);
@@ -271,11 +271,10 @@ static bool mdio_bus_phy_may_suspend(struct phy_device *phydev, bool suspend)
 	/* PHY not attached? May suspend if the PHY has not already been
 	 * suspended as part of a prior call to phy_disconnect() ->
 	 * phy_detach() -> phy_suspend() because the parent netdev might be the
-	 * MDIO bus driver and clock gated at this point. Also may resume if
-	 * PHY is not attached.
+	 * MDIO bus driver and clock gated at this point.
 	 */
 	if (!netdev)
-		return suspend ? !phydev->suspended : phydev->suspended;
+		return !phydev->suspended;
 
 	return phy_may_suspend(phydev);
 }
@@ -292,7 +291,7 @@ static int mdio_bus_phy_suspend(struct device *dev)
 	if (phydev->attached_dev && phydev->adjust_link)
 		phy_stop_machine(phydev);
 
-	if (!mdio_bus_phy_may_suspend(phydev, true))
+	if (!mdio_bus_phy_may_suspend(phydev))
 		return 0;
 
 	return phy_suspend(phydev);
@@ -303,7 +302,7 @@ static int mdio_bus_phy_resume(struct device *dev)
 	struct phy_device *phydev = to_phy_device(dev);
 	int ret;
 
-	if (!mdio_bus_phy_may_suspend(phydev, false))
+	if (!mdio_bus_phy_may_suspend(phydev))
 		goto no_resume;
 
 	ret = phy_resume(phydev);
