a185a1dc30a147fbdac01714dd03cb2ec7b3b7bb
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:54:41 -0400

    mlxsw: spectrum: Add packet traps for BFD packets
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Ido Schimmel <idosch@mellanox.com>
    commit 9785b92b4443f2862495c9aa0ee8caed6f43523d
    
    Bidirectional Forwarding Detection (BFD) provides "low-overhead,
    short-duration detection of failures in the path between adjacent
    forwarding engines" (RFC 5880).
    
    This is accomplished by exchanging BFD packets between the two
    forwarding engines. Up until now these packets were trapped via the
    general local delivery (i.e., IP2ME) trap which also traps a lot of
    other packets that are not as time-sensitive as BFD packets.
    
    Expose dedicated traps for BFD packets so that user space could
    configure a dedicated policer for them.
    
    	Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    	Reviewed-by: Jiri Pirko <jiri@mellanox.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 9785b92b4443f2862495c9aa0ee8caed6f43523d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.c
@@ -4084,6 +4084,8 @@ static const struct mlxsw_listener mlxsw_sp_listener[] = {
 	MLXSW_SP_RXL_MARK(ROUTER_ALERT_IPV6, TRAP_TO_CPU, IP2ME, false),
 	MLXSW_SP_RXL_MARK(IPV4_VRRP, TRAP_TO_CPU, VRRP, false),
 	MLXSW_SP_RXL_MARK(IPV6_VRRP, TRAP_TO_CPU, VRRP, false),
+	MLXSW_SP_RXL_MARK(IPV4_BFD, TRAP_TO_CPU, BFD, false),
+	MLXSW_SP_RXL_MARK(IPV6_BFD, TRAP_TO_CPU, BFD, false),
 	MLXSW_SP_RXL_NO_MARK(DISCARD_ING_ROUTER_SIP_CLASS_E, FORWARD,
 			     ROUTER_EXP, false),
 	MLXSW_SP_RXL_NO_MARK(DISCARD_ING_ROUTER_MC_DMAC, FORWARD,
@@ -4176,6 +4178,10 @@ static int mlxsw_sp_cpu_policers_set(struct mlxsw_core *mlxsw_core)
 			rate = 360;
 			burst_size = 7;
 			break;
+		case MLXSW_REG_HTGT_TRAP_GROUP_SP_BFD:
+			rate = 20 * 1024;
+			burst_size = 10;
+			break;
 		default:
 			continue;
 		}
@@ -4217,6 +4223,7 @@ static int mlxsw_sp_trap_groups_set(struct mlxsw_core *mlxsw_core)
 		case MLXSW_REG_HTGT_TRAP_GROUP_SP_PIM:
 		case MLXSW_REG_HTGT_TRAP_GROUP_SP_PTP0:
 		case MLXSW_REG_HTGT_TRAP_GROUP_SP_VRRP:
+		case MLXSW_REG_HTGT_TRAP_GROUP_SP_BFD:
 			priority = 5;
 			tc = 5;
 			break;
