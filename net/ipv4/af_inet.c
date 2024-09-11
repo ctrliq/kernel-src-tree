5fc7b88590e6d5844e7f3ed375777e48f8426ff7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:11:18 -0400

    udp: move gro declarations to net/udp.h
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Eric Dumazet <edumazet@google.com>
    commit 6db693285cd109e566c553694002dea769612b24
    
    This removes following warnings :
      CC      net/ipv4/udp_offload.o
    net/ipv4/udp_offload.c:504:17: warning: no previous prototype for 'udp4_gro_receive' [-Wmissing-prototypes]
      504 | struct sk_buff *udp4_gro_receive(struct list_head *head, struct sk_buff *skb)
          |                 ^~~~~~~~~~~~~~~~
    net/ipv4/udp_offload.c:584:29: warning: no previous prototype for 'udp4_gro_complete' [-Wmissing-prototypes]
      584 | INDIRECT_CALLABLE_SCOPE int udp4_gro_complete(struct sk_buff *skb, int nhoff)
          |                             ^~~~~~~~~~~~~~~~~
    
      CHECK   net/ipv6/udp_offload.c
    net/ipv6/udp_offload.c:115:16: warning: symbol 'udp6_gro_receive' was not declared. Should it be static?
    net/ipv6/udp_offload.c:148:29: warning: symbol 'udp6_gro_complete' was not declared. Should it be static?
      CC      net/ipv6/udp_offload.o
    net/ipv6/udp_offload.c:115:17: warning: no previous prototype for 'udp6_gro_receive' [-Wmissing-prototypes]
      115 | struct sk_buff *udp6_gro_receive(struct list_head *head, struct sk_buff *skb)
          |                 ^~~~~~~~~~~~~~~~
    net/ipv6/udp_offload.c:148:29: warning: no previous prototype for 'udp6_gro_complete' [-Wmissing-prototypes]
      148 | INDIRECT_CALLABLE_SCOPE int udp6_gro_complete(struct sk_buff *skb, int nhoff)
          |                             ^~~~~~~~~~~~~~~~~
    	Signed-off-by: Eric Dumazet <edumazet@google.com>
    
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 6db693285cd109e566c553694002dea769612b24)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -1418,8 +1418,6 @@ static struct sk_buff *ipip_gso_segment(struct sk_buff *skb,
 	return inet_gso_segment(skb, features);
 }
 
-INDIRECT_CALLABLE_DECLARE(struct sk_buff *udp4_gro_receive(struct list_head *,
-							   struct sk_buff *));
 struct sk_buff *inet_gro_receive(struct list_head *head, struct sk_buff *skb)
 {
 	const struct net_offload *ops;
@@ -1592,7 +1590,6 @@ int inet_recv_error(struct sock *sk, struct msghdr *msg, int len, int *addr_len)
 	return -EINVAL;
 }
 
-INDIRECT_CALLABLE_DECLARE(int udp4_gro_complete(struct sk_buff *, int));
 int inet_gro_complete(struct sk_buff *skb, int nhoff)
 {
 	__be16 newlen = htons(skb->len - nhoff);
