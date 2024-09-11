3a0f69fda3c42db0a08f17c816f1cafe19cb70ba
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:29:13 -0400

    KVM: x86: to track if L1 is running L2 VM
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-310.el8
    commit-author Dongli Zhang <dongli.zhang@oracle.com>
    commit 43c11d91fb1e4c41309db3d233ac1e048cdc8dd0
    
    The new per-cpu stat 'nested_run' is introduced in order to track if L1 VM
    is running or used to run L2 VM.
    
    An example of the usage of 'nested_run' is to help the host administrator
    to easily track if any L1 VM is used to run L2 VM. Suppose there is issue
    that may happen with nested virtualization, the administrator will be able
    to easily narrow down and confirm if the issue is due to nested
    virtualization via 'nested_run'. For example, whether the fix like
    commit 88dddc11a8d6 ("KVM: nVMX: do not use dangling shadow VMCS after
    guest reset") is required.
    
    	Cc: Joe Jin <joe.jin@oracle.com>
    	Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>
    Message-Id: <20210305225747.7682-1-dongli.zhang@oracle.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 43c11d91fb1e4c41309db3d233ac1e048cdc8dd0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kvm/vmx/nested.c
+++ arch/x86/kvm/vmx/nested.c
@@ -3479,6 +3479,8 @@ static int nested_vmx_run(struct kvm_vcpu *vcpu, bool launch)
 	u32 interrupt_shadow = vmx_get_interrupt_shadow(vcpu);
 	enum nested_evmptrld_status evmptrld_status;
 
+	++vcpu->stat.nested_run;
+
 	if (!nested_vmx_check_permission(vcpu))
 		return 1;
 
