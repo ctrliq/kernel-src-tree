ca39611463c4c7d4f38bc8fc9dd3c69d7c7167c4
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:59:03 -0400

    scsi: qla2xxx: Fix N2N inconsistent PLOGI
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-394.el8
    commit-author Quinn Tran <qutran@marvell.com>
    commit c13ce47c64ea8f14e77eecb40d1e7c2ac667f898
    
    For N2N topology, ELS Passthrough is used to send PLOGI. On failure of ELS
    pass through PLOGI, driver flipped over to using LLIOCB PLOGI for N2N. This
    is not consistent. Delete the session to restart the connection where ELS
    pass through PLOGI would be used consistently.
    
    Link: https://lore.kernel.org/r/20220310092604.22950-7-njavali@marvell.com
    Fixes: c76ae845ea83 ("scsi: qla2xxx: Add error handling for PLOGI ELS passthrough")
    	Cc: stable@vger.kernel.org
    	Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    	Signed-off-by: Quinn Tran <qutran@marvell.com>
    	Signed-off-by: Nilesh Javali <njavali@marvell.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit c13ce47c64ea8f14e77eecb40d1e7c2ac667f898)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/qla2xxx/qla_iocb.c
+++ drivers/scsi/qla2xxx/qla_iocb.c
@@ -3012,6 +3012,7 @@ static void qla2x00_els_dcmd2_sp_done(srb_t *sp, int res)
 					set_bit(ISP_ABORT_NEEDED,
 					    &vha->dpc_flags);
 					qla2xxx_wake_dpc(vha);
+					break;
 				}
 				/* fall through */
 			default:
@@ -3021,9 +3022,7 @@ static void qla2x00_els_dcmd2_sp_done(srb_t *sp, int res)
 				    fw_status[0], fw_status[1], fw_status[2]);
 
 				fcport->flags &= ~FCF_ASYNC_SENT;
-				qla2x00_set_fcport_disc_state(fcport,
-				    DSC_LOGIN_FAILED);
-				set_bit(RELOGIN_NEEDED, &vha->dpc_flags);
+				qlt_schedule_sess_for_deletion(fcport);
 				break;
 			}
 			break;
@@ -3035,8 +3034,7 @@ static void qla2x00_els_dcmd2_sp_done(srb_t *sp, int res)
 			    fw_status[0], fw_status[1], fw_status[2]);
 
 			sp->fcport->flags &= ~FCF_ASYNC_SENT;
-			qla2x00_set_fcport_disc_state(fcport, DSC_LOGIN_FAILED);
-			set_bit(RELOGIN_NEEDED, &vha->dpc_flags);
+			qlt_schedule_sess_for_deletion(fcport);
 			break;
 		}
 
