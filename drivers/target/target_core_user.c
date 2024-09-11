b846bd70f43f78698e6dd9cec325598837b4172f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:04:03 -0400

    scsi: target: tcmu: Switch to bitmap_zalloc()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    commit 98effe4746c6e64b76703bbf5fd64e304e3e5396
    
    Switch to bitmap_zalloc() to show clearly what we are allocating.  Besides
    that it returns pointer of bitmap type instead of opaque void *.
    
    	Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    	Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    	Acked-by: Mike Christie <mchristi@redhat.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 98effe4746c6e64b76703bbf5fd64e304e3e5396)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/target/target_core_user.c
+++ drivers/target/target_core_user.c
@@ -1663,7 +1663,7 @@ static void tcmu_dev_kref_release(struct kref *kref)
 	WARN_ON(!all_expired);
 
 	tcmu_blocks_release(&udev->data_blocks, 0, udev->dbi_max + 1);
-	kfree(udev->data_bitmap);
+	bitmap_free(udev->data_bitmap);
 	mutex_unlock(&udev->cmdr_lock);
 
 	call_rcu(&dev->rcu_head, tcmu_dev_call_rcu);
@@ -1871,9 +1871,7 @@ static int tcmu_configure_device(struct se_device *dev)
 	info = &udev->uio_info;
 
 	mutex_lock(&udev->cmdr_lock);
-	udev->data_bitmap = kcalloc(BITS_TO_LONGS(udev->max_blocks),
-				    sizeof(unsigned long),
-				    GFP_KERNEL);
+	udev->data_bitmap = bitmap_zalloc(udev->max_blocks, GFP_KERNEL);
 	mutex_unlock(&udev->cmdr_lock);
 	if (!udev->data_bitmap) {
 		ret = -ENOMEM;
@@ -1960,7 +1958,7 @@ err_register:
 	vfree(udev->mb_addr);
 	udev->mb_addr = NULL;
 err_vzalloc:
-	kfree(udev->data_bitmap);
+	bitmap_free(udev->data_bitmap);
 	udev->data_bitmap = NULL;
 err_bitmap_alloc:
 	kfree(info->name);
