dfca3d13b846ad8821f7741dc67fa33501914a2e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:03:45 -0400

    scsi: target: Rename transport_init_session() into transport_alloc_session()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Bart Van Assche <bart.vanassche@wdc.com>
    commit 317f89712d7aa4acc2d93df27b753d4489826cc5
    
    	Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    	Reviewed-by: Mike Christie <mchristi@redhat.com>
    	Cc: Mike Christie <mchristi@redhat.com>
    	Cc: Hannes Reinecke <hare@suse.com>
    	Cc: Christoph Hellwig <hch@lst.de>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 317f89712d7aa4acc2d93df27b753d4489826cc5)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/target/target_core_fabric.h
+++ include/target/target_core_fabric.h
@@ -116,7 +116,7 @@ struct se_session *target_setup_session(struct se_portal_group *,
 				struct se_session *, void *));
 void target_remove_session(struct se_session *);
 
-struct se_session *transport_init_session(enum target_prot_op);
+struct se_session *transport_alloc_session(enum target_prot_op);
 int transport_alloc_session_tags(struct se_session *, unsigned int,
 		unsigned int);
 struct se_session *transport_init_session_tags(unsigned int, unsigned int,
