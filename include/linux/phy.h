876a510ba5384755b6cf89962ce0871229d70313
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:21:48 -0400

    net: phy: add interface mode PHY_INTERFACE_MODE_USXGMII
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit 4618d6719743b60f1da4b8112c4518ee46110b94
    
    Add support for interface mode PHY_INTERFACE_MODE_USXGMII.
    
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 4618d6719743b60f1da4b8112c4518ee46110b94)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -108,6 +108,7 @@ typedef enum {
 	PHY_INTERFACE_MODE_XAUI,
 	/* 10GBASE-KR, XFI, SFI - single lane 10G Serdes */
 	PHY_INTERFACE_MODE_10GKR,
+	PHY_INTERFACE_MODE_USXGMII,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -183,6 +184,8 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "xaui";
 	case PHY_INTERFACE_MODE_10GKR:
 		return "10gbase-kr";
+	case PHY_INTERFACE_MODE_USXGMII:
+		return "usxgmii";
 	default:
 		return "unknown";
 	}
