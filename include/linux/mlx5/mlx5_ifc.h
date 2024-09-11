6c86fb9486408e4c88da5e08074bbb1574cb5184
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:04:54 -0400

    net/mlx5: Set uid as part of SQ commands
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Yishai Hadas <yishaih@mellanox.com>
    commit 430ae0d5a3ce1350375690cb6ab29ab6fae4a9ac
    
    Set uid as part of SQ commands so that the firmware can manage the
    SQ object in a secured way.
    
    That will enable using an SQ that was created by verbs application
    to be used by the DEVX flow in case the uid is equal.
    
    	Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
    	Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    (cherry picked from commit 430ae0d5a3ce1350375690cb6ab29ab6fae4a9ac)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -5364,7 +5364,7 @@ struct mlx5_ifc_modify_sq_out_bits {
 
 struct mlx5_ifc_modify_sq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
@@ -6079,7 +6079,7 @@ struct mlx5_ifc_destroy_sq_out_bits {
 
 struct mlx5_ifc_destroy_sq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
@@ -6752,7 +6752,7 @@ struct mlx5_ifc_create_sq_out_bits {
 
 struct mlx5_ifc_create_sq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
