2f1f10a20ef7797606b8cbb84001134881869a52
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:39:35 -0400

    x86: Clean up 'sizeof x' => 'sizeof(x)'
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Jordan Borgner <mail@jordan-borgner.de>
    commit 0e96f31ea4249b1e94e266fe4dff908c2983a9b3
    
    "sizeof(x)" is the canonical coding style used in arch/x86 most of the time.
    Fix the few places that didn't follow the convention.
    
    (Also do some whitespace cleanups in a few places while at it.)
    
    [ mingo: Rewrote the changelog. ]
    
    	Signed-off-by: Jordan Borgner <mail@jordan-borgner.de>
    	Cc: Borislav Petkov <bp@alien8.de>
    	Cc: H. Peter Anvin <hpa@zytor.com>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20181028125828.7rgammkgzep2wpam@JordanDesktop
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 0e96f31ea4249b1e94e266fe4dff908c2983a9b3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1056,7 +1056,7 @@ static void __init early_identify_cpu(struct cpuinfo_x86 *c)
 #endif
 	c->x86_cache_alignment = c->x86_clflush_size;
 
-	memset(&c->x86_capability, 0, sizeof c->x86_capability);
+	memset(&c->x86_capability, 0, sizeof(c->x86_capability));
 	c->extended_cpuid_level = 0;
 
 	/* cyrix could have cpuid enabled via c_identify()*/
@@ -1316,7 +1316,7 @@ static void identify_cpu(struct cpuinfo_x86 *c)
 	c->x86_virt_bits = 32;
 #endif
 	c->x86_cache_alignment = c->x86_clflush_size;
-	memset(&c->x86_capability, 0, sizeof c->x86_capability);
+	memset(&c->x86_capability, 0, sizeof(c->x86_capability));
 
 	generic_identify(c);
 
