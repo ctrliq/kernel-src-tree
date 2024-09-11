8fc6b8e7e614f19334db05a404936dc30560c6a3
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:55:15 -0400

    mfd: cpcap: Fix interrupt regression with regmap clear_ack
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Tony Lindgren <tony@atomide.com>
    commit 14639a22de657eabbb776f503a816594393cc935
    
    With commit 3a6f0fb7b8eb ("regmap: irq: Add support to clear ack
    registers"), the cpcap interrupts are no longer getting acked properly
    leading to a very unresponsive device with CPUs fully loaded spinning
    in the threaded IRQ handlers.
    
    To me it looks like the clear_ack commit above actually fixed a long
    standing bug in regmap_irq_thread() where we unconditionally acked the
    interrupts earlier without considering ack_invert. And the issue with
    cpcap started happening as we now also consider ack_invert.
    
    Tim Harvey <tharvey@gateworks.com> tried to fix this issue earlier with
    "[PATCH v2] regmap: irq: fix ack-invert", but the reading of the ack
    register was considered unnecessary for just ack_invert, and we did not
    have clear_ack available yet. As the cpcap irqs worked both with and
    without ack_invert earlier because of the unconditional ack, the
    problem remained hidden until now.
    
    Also, looks like the earlier v3.0.8 based Motorola Android Linux kernel
    does clear_ack style read-clear-write with "ireg_val & ~mreg_val" instead
    of just ack_invert style write. So let's switch cpcap to use clear_ack
    to fix the issue.
    
    Fixes: 3a6f0fb7b8eb ("regmap: irq: Add support to clear ack registers")
    	Cc: Carl Philipp Klemm <philipp@uvos.xyz>
    	Cc: Laxminath Kasam <lkasam@codeaurora.org>
    	Cc: Merlijn Wajer <merlijn@wizzup.org>
    	Cc: Mark Brown <broonie@kernel.org>
    	Cc: Pavel Machek <pavel@ucw.cz>
    	Cc: Sebastian Reichel <sre@kernel.org>
    	Cc: Tim Harvey <tharvey@gateworks.com>
    	Signed-off-by: Tony Lindgren <tony@atomide.com>
    	Tested-by: Pavel Machek <pavel@ucw.cz>
    Reviewed-By: Tim Harvey <tharvey@gateworks.com>
    	Signed-off-by: Lee Jones <lee.jones@linaro.org>
    (cherry picked from commit 14639a22de657eabbb776f503a816594393cc935)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mfd/motorola-cpcap.c
+++ drivers/mfd/motorola-cpcap.c
@@ -99,7 +99,7 @@ static struct regmap_irq_chip cpcap_irq_chip[CPCAP_NR_IRQ_CHIPS] = {
 		.ack_base = CPCAP_REG_MI1,
 		.mask_base = CPCAP_REG_MIM1,
 		.use_ack = true,
-		.ack_invert = true,
+		.clear_ack = true,
 	},
 	{
 		.name = "cpcap-m2",
@@ -108,7 +108,7 @@ static struct regmap_irq_chip cpcap_irq_chip[CPCAP_NR_IRQ_CHIPS] = {
 		.ack_base = CPCAP_REG_MI2,
 		.mask_base = CPCAP_REG_MIM2,
 		.use_ack = true,
-		.ack_invert = true,
+		.clear_ack = true,
 	},
 	{
 		.name = "cpcap1-4",
@@ -117,7 +117,7 @@ static struct regmap_irq_chip cpcap_irq_chip[CPCAP_NR_IRQ_CHIPS] = {
 		.ack_base = CPCAP_REG_INT1,
 		.mask_base = CPCAP_REG_INTM1,
 		.use_ack = true,
-		.ack_invert = true,
+		.clear_ack = true,
 	},
 };
 
