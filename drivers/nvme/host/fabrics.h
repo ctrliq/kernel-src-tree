35127772f02ecff417a03aa9a2b8818699ddbaaf
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:38:36 -0400

    nvme-fabrics: allow user to pass in nr_poll_queues
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Sagi Grimberg <sagi@grimberg.me>
    commit 89d43802b0e7298bc0b464bdb5fff864074e2218
    
    This argument will specify how many polling I/O queues to connect when
    creating the controller. These I/O queues will host I/O that is set with
    REQ_HIPRI.
    
    	Reviewed-by: Steve Wise <swise@opengridcomputing.com>
    	Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 89d43802b0e7298bc0b464bdb5fff864074e2218)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nvme/host/fabrics.h
+++ drivers/nvme/host/fabrics.h
@@ -54,6 +54,7 @@ enum {
 	NVMF_OPT_HDR_DIGEST	= 1 << 15,
 	NVMF_OPT_DATA_DIGEST	= 1 << 16,
 	NVMF_OPT_NR_WRITE_QUEUES = 1 << 17,
+	NVMF_OPT_NR_POLL_QUEUES = 1 << 18,
 };
 
 /**
@@ -85,6 +86,7 @@ enum {
  * @hdr_digest: generate/verify header digest (TCP)
  * @data_digest: generate/verify data digest (TCP)
  * @nr_write_queues: number of queues for write I/O
+ * @nr_poll_queues: number of queues for polling I/O
  */
 struct nvmf_ctrl_options {
 	unsigned		mask;
@@ -105,6 +107,7 @@ struct nvmf_ctrl_options {
 	bool			hdr_digest;
 	bool			data_digest;
 	unsigned int		nr_write_queues;
+	unsigned int		nr_poll_queues;
 };
 
 /*
