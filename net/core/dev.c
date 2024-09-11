a0f56a2500f910c5f495e28c608959c312aab2d6
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:36:27 -0400

    net: export netdev_next_lower_dev_rcu()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    Rebuild_CHGLOG: - [net] export netdev_next_lower_dev_rcu() (Ivan Vecera) [1818074]
    Rebuild_FUZZ: 93.15%
    commit-author Taehee Yoo <ap420073@gmail.com>
    commit 7151affeef8d527f50b4b68a871fd28bd660023f
    
    netdev_next_lower_dev_rcu() will be used to implement a function,
    which is to walk all lower interfaces.
    There are already functions that they walk their lower interface.
    (netdev_walk_all_lower_dev_rcu, netdev_walk_all_lower_dev()).
    But, there would be cases that couldn't be covered by given
    netdev_walk_all_lower_dev_{rcu}() function.
    So, some modules would want to implement own function,
    which is to walk all lower interfaces.
    
    In the next patch, netdev_next_lower_dev_rcu() will be used.
    In addition, this patch removes two unused prototypes in netdevice.h.
    
    	Signed-off-by: Taehee Yoo <ap420073@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7151affeef8d527f50b4b68a871fd28bd660023f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -154,7 +154,6 @@
 
 /* Instead of increasing this, you should create a hash table. */
 #define MAX_GRO_SKBS 8
-#define MAX_NEST_DEV 8
 
 /* This should be increased if a protocol with a bigger head is added. */
 #define GRO_MAX_HEAD (MAX_HEADER + 128)
@@ -6687,8 +6686,8 @@ static int __netdev_walk_all_lower_dev(struct net_device *dev,
 	return 0;
 }
 
-static struct net_device *netdev_next_lower_dev_rcu(struct net_device *dev,
-						    struct list_head **iter)
+struct net_device *netdev_next_lower_dev_rcu(struct net_device *dev,
+					     struct list_head **iter)
 {
 	struct netdev_adjacent *lower;
 
@@ -6700,6 +6699,7 @@ static struct net_device *netdev_next_lower_dev_rcu(struct net_device *dev,
 
 	return lower->dev;
 }
+EXPORT_SYMBOL(netdev_next_lower_dev_rcu);
 
 static u8 __netdev_upper_depth(struct net_device *dev)
 {
