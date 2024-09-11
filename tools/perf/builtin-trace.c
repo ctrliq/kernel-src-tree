c0d14467e0f1be4d06716b5552ba8529d848d46a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 16:06:33 -0400

    perf evlist: Use the right prefix for 'struct evlist' 'find' methods
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Arnaldo Carvalho de Melo <acme@redhat.com>
    commit b02736f776d5f50bb13ff85eb34efaed0c3f5ffa
    
    perf_evlist__ is for 'struct perf_evlist' methods, in tools/lib/perf/,
    go on completing this split.
    
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: Ian Rogers <irogers@google.com>
    	Cc: Jiri Olsa <jolsa@kernel.org>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit b02736f776d5f50bb13ff85eb34efaed0c3f5ffa)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/builtin-trace.c
+++ tools/perf/builtin-trace.c
@@ -4230,12 +4230,10 @@ static int trace__replay(struct trace *trace)
 	if (err)
 		goto out;
 
-	evsel = perf_evlist__find_tracepoint_by_name(session->evlist,
-						     "raw_syscalls:sys_enter");
+	evsel = evlist__find_tracepoint_by_name(session->evlist, "raw_syscalls:sys_enter");
 	/* older kernels have syscalls tp versus raw_syscalls */
 	if (evsel == NULL)
-		evsel = perf_evlist__find_tracepoint_by_name(session->evlist,
-							     "syscalls:sys_enter");
+		evsel = evlist__find_tracepoint_by_name(session->evlist, "syscalls:sys_enter");
 
 	if (evsel &&
 	    (evsel__init_raw_syscall_tp(evsel, trace__sys_enter) < 0 ||
@@ -4244,11 +4242,9 @@ static int trace__replay(struct trace *trace)
 		goto out;
 	}
 
-	evsel = perf_evlist__find_tracepoint_by_name(session->evlist,
-						     "raw_syscalls:sys_exit");
+	evsel = evlist__find_tracepoint_by_name(session->evlist, "raw_syscalls:sys_exit");
 	if (evsel == NULL)
-		evsel = perf_evlist__find_tracepoint_by_name(session->evlist,
-							     "syscalls:sys_exit");
+		evsel = evlist__find_tracepoint_by_name(session->evlist, "syscalls:sys_exit");
 	if (evsel &&
 	    (evsel__init_raw_syscall_tp(evsel, trace__sys_exit) < 0 ||
 	    perf_evsel__init_sc_tp_uint_field(evsel, ret))) {
@@ -4908,7 +4904,7 @@ int cmd_trace(int argc, const char **argv)
 	if (evsel) {
 		trace.syscalls.events.augmented = evsel;
 
-		evsel = perf_evlist__find_tracepoint_by_name(trace.evlist, "raw_syscalls:sys_enter");
+		evsel = evlist__find_tracepoint_by_name(trace.evlist, "raw_syscalls:sys_enter");
 		if (evsel == NULL) {
 			pr_err("ERROR: raw_syscalls:sys_enter not found in the augmented BPF object\n");
 			goto out;
