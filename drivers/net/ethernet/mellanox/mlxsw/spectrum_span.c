5d7395e2d0b0e36937b2e83ae93e7c7dd6c4a666
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:54:51 -0400

    mlxsw: spectrum_span: Add driver private info to parms_set() callback
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Ido Schimmel <idosch@mellanox.com>
    commit f4a626e2ca09aeffd30509349759fb30379fe83f
    
    The parms_set() callback is supposed to fill in the parameters for the
    SPAN agent, such as the destination port and encapsulation info, if any.
    
    When mirroring to the CPU port we cannot resolve the destination port
    (the CPU port) without access to the driver private info.
    
    Pass the driver private info to parms_set() callback so that it could be
    used later on to resolve the CPU port.
    
    	Reviewed-by: Jiri Pirko <jiri@mellanox.com>
    	Signed-off-by: Petr Machata <petrm@mellanox.com>
    	Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f4a626e2ca09aeffd30509349759fb30379fe83f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_span.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_span.c
@@ -129,7 +129,8 @@ void mlxsw_sp_span_fini(struct mlxsw_sp *mlxsw_sp)
 }
 
 static int
-mlxsw_sp_span_entry_phys_parms(const struct net_device *to_dev,
+mlxsw_sp_span_entry_phys_parms(struct mlxsw_sp *mlxsw_sp,
+			       const struct net_device *to_dev,
 			       struct mlxsw_sp_span_parms *sparmsp)
 {
 	sparmsp->dest_port = netdev_priv(to_dev);
@@ -401,7 +402,8 @@ out:
 }
 
 static int
-mlxsw_sp_span_entry_gretap4_parms(const struct net_device *to_dev,
+mlxsw_sp_span_entry_gretap4_parms(struct mlxsw_sp *mlxsw_sp,
+				  const struct net_device *to_dev,
 				  struct mlxsw_sp_span_parms *sparmsp)
 {
 	struct ip_tunnel_parm tparm = mlxsw_sp_ipip_netdev_parms4(to_dev);
@@ -502,7 +504,8 @@ out:
 }
 
 static int
-mlxsw_sp_span_entry_gretap6_parms(const struct net_device *to_dev,
+mlxsw_sp_span_entry_gretap6_parms(struct mlxsw_sp *mlxsw_sp,
+				  const struct net_device *to_dev,
 				  struct mlxsw_sp_span_parms *sparmsp)
 {
 	struct __ip6_tnl_parm tparm = mlxsw_sp_ipip_netdev_parms6(to_dev);
@@ -576,7 +579,8 @@ mlxsw_sp_span_vlan_can_handle(const struct net_device *dev)
 }
 
 static int
-mlxsw_sp_span_entry_vlan_parms(const struct net_device *to_dev,
+mlxsw_sp_span_entry_vlan_parms(struct mlxsw_sp *mlxsw_sp,
+			       const struct net_device *to_dev,
 			       struct mlxsw_sp_span_parms *sparmsp)
 {
 	struct net_device *real_dev;
@@ -648,7 +652,8 @@ struct mlxsw_sp_span_entry_ops *mlxsw_sp2_span_entry_ops_arr[] = {
 };
 
 static int
-mlxsw_sp_span_entry_nop_parms(const struct net_device *to_dev,
+mlxsw_sp_span_entry_nop_parms(struct mlxsw_sp *mlxsw_sp,
+			      const struct net_device *to_dev,
 			      struct mlxsw_sp_span_parms *sparmsp)
 {
 	return mlxsw_sp_span_entry_unoffloadable(sparmsp);
@@ -931,7 +936,7 @@ static void mlxsw_sp_span_respin_work(struct work_struct *work)
 		if (!refcount_read(&curr->ref_count))
 			continue;
 
-		err = curr->ops->parms_set(curr->to_dev, &sparms);
+		err = curr->ops->parms_set(mlxsw_sp, curr->to_dev, &sparms);
 		if (err)
 			continue;
 
@@ -967,7 +972,7 @@ int mlxsw_sp_span_agent_get(struct mlxsw_sp *mlxsw_sp,
 	}
 
 	memset(&sparms, 0, sizeof(sparms));
-	err = ops->parms_set(to_dev, &sparms);
+	err = ops->parms_set(mlxsw_sp, to_dev, &sparms);
 	if (err)
 		return err;
 
