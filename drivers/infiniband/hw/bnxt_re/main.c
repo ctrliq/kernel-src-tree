cddd96a49c73e0a2d08db5b4c6c9b03ba392516e
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:17:24 -0400

    RDMA/bnxt_re: Remove unnecessary sched count
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Selvin Xavier <selvin.xavier@broadcom.com>
    commit 4e88cef11d1913d1875c7870df4facbd096a062b
    
    Since the lifetime of bnxt_re_task is controlled by the kref of device,
    sched_count is no longer required.  Remove it.
    
    Link: https://lore.kernel.org/r/1584117207-2664-4-git-send-email-selvin.xavier@broadcom.com
    	Signed-off-by: Selvin Xavier <selvin.xavier@broadcom.com>
    	Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    (cherry picked from commit 4e88cef11d1913d1875c7870df4facbd096a062b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/hw/bnxt_re/main.c
+++ drivers/infiniband/hw/bnxt_re/main.c
@@ -1666,8 +1666,6 @@ static void bnxt_re_task(struct work_struct *work)
 		break;
 	}
 	ib_device_put(&rdev->ibdev);
-	smp_mb__before_atomic();
-	atomic_dec(&rdev->sched_count);
 exit:
 	put_device(&rdev->ibdev.dev);
 	kfree(re_work);
@@ -1719,11 +1717,6 @@ static int bnxt_re_netdev_event(struct notifier_block *notifier,
 		break;
 
 	case NETDEV_UNREGISTER:
-		/* netdev notifier will call NETDEV_UNREGISTER again later since
-		 * we are still holding the reference to the netdev
-		 */
-		if (atomic_read(&rdev->sched_count) > 0)
-			goto exit;
 		ib_unregister_device_queued(&rdev->ibdev);
 		break;
 
@@ -1741,7 +1734,6 @@ static int bnxt_re_netdev_event(struct notifier_block *notifier,
 			re_work->vlan_dev = (real_dev == netdev ?
 					     NULL : netdev);
 			INIT_WORK(&re_work->work, bnxt_re_task);
-			atomic_inc(&rdev->sched_count);
 			queue_work(bnxt_re_wq, &re_work->work);
 		}
 	}
