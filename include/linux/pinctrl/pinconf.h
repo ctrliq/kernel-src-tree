4de98a0e8142267251cd7d6fed88288bfe66894a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 17:46:04 -0400

    pinctrl: Document pin_config_group_get() return codes like pin_config_get()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-358.el8
    commit-author Douglas Anderson <dianders@chromium.org>
    commit 63f3fb8d7ccb813e0a1f2ceedb9a3ebe2685b620
    
    The pinconf_generic_dump_one() function makes the assumption that
    pin_config_group_get() should return -EINVAL and -ENOTSUPP just like
    pin_config_get() does.  Document that so it's more obvious.
    
    	Signed-off-by: Douglas Anderson <dianders@chromium.org>
    	Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    (cherry picked from commit 63f3fb8d7ccb813e0a1f2ceedb9a3ebe2685b620)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/pinctrl/pinconf.h
+++ include/linux/pinctrl/pinconf.h
@@ -26,7 +26,8 @@ struct seq_file;
  *	is not available on this controller this should return -ENOTSUPP
  *	and if it is available but disabled it should return -EINVAL
  * @pin_config_set: configure an individual pin
- * @pin_config_group_get: get configurations for an entire pin group
+ * @pin_config_group_get: get configurations for an entire pin group; should
+ *	return -ENOTSUPP and -EINVAL using the same rules as pin_config_get.
  * @pin_config_group_set: configure all pins in a group
  * @pin_config_dbg_show: optional debugfs display hook that will provide
  *	per-device info for a certain pin in debugfs
