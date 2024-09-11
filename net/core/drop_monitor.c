0a8321724ff67425bd53bd6fcc061fef32fe1af7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:36:08 -0400

    drop_monitor: Avoid multiple blank lines
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Ido Schimmel <idosch@mellanox.com>
    commit ff3818ca39c9f9ce07c4d50db594b9673dfa422c
    
    Remove multiple blank lines which are visually annoying and useless.
    
    This suppresses the "Please don't use multiple blank lines" checkpatch
    messages.
    
    	Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ff3818ca39c9f9ce07c4d50db594b9673dfa422c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/core/drop_monitor.c
+++ net/core/drop_monitor.c
@@ -299,7 +299,6 @@ out_unlock:
 	return rc;
 }
 
-
 static int net_dm_cmd_config(struct sk_buff *skb,
 			struct genl_info *info)
 {
@@ -425,7 +424,6 @@ static int __init init_net_drop_monitor(void)
 		spin_lock_init(&data->lock);
 		reset_per_cpu_data(data);
 	}
-
 
 	goto out;
 
