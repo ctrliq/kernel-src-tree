f7c05238ba385c9bfecea5c6cd2efdbdb5859a4c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:36:38 -0400

    net: openvswitch: remove unused keep_flows
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    Rebuild_CHGLOG: - [net] openvswitch: remove unused keep_flows (Eelco Chaudron) [1895862]
    Rebuild_FUZZ: 93.67%
    commit-author Tonghao Zhang <xiangxia.m.yue@gmail.com>
    commit e6896163b20cc2dc1c169fa5e007243a4f9650a4
    
    keep_flows was introduced by [1], which used as flag to delete flows or not.
    When rehashing or expanding the table instance, we will not flush the flows.
    Now don't use it anymore, remove it.
    
    [1] - https://github.com/openvswitch/ovs/commit/acd051f1761569205827dc9b037e15568a8d59f8
    	Cc: Pravin B Shelar <pshelar@ovn.org>
    	Signed-off-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>
    	Acked-by: Pravin B Shelar <pshelar@ovn.org>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit e6896163b20cc2dc1c169fa5e007243a4f9650a4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/openvswitch/flow_table.c
+++ net/openvswitch/flow_table.c
@@ -177,7 +177,6 @@ static struct table_instance *table_instance_alloc(int new_size)
 
 	ti->n_buckets = new_size;
 	ti->node_ver = 0;
-	ti->keep_flows = false;
 	get_random_bytes(&ti->hash_seed, sizeof(u32));
 
 	return ti;
@@ -493,9 +492,6 @@ void table_instance_flow_flush(struct flow_table *table,
 {
 	int i;
 
-	if (ti->keep_flows)
-		return;
-
 	for (i = 0; i < ti->n_buckets; i++) {
 		struct sw_flow *flow;
 		struct hlist_head *head = &ti->buckets[i];
@@ -609,8 +605,6 @@ static void flow_table_copy_flows(struct table_instance *old,
 						 lockdep_ovsl_is_held())
 				table_instance_insert(new, flow);
 	}
-
-	old->keep_flows = true;
 }
 
 static struct table_instance *table_instance_rehash(struct table_instance *ti,
