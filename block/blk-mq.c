cb47bc8cbad4e79f6503e8432735cdb80773384b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 11:22:24 -0400

    blk-mq: fix failure to decrement plug count on single rq removal
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-80.el8
    commit-author Jens Axboe <axboe@kernel.dk>
    commit 4711b57317f0ff5ca9fbd5e2df6c73b2c07ddc53
    
    If we yank a 'same_queue_rq' request off the plug list, we should
    also decrement the cached request count.
    
    Fixes: 5f0ed774ed29 ("block: sum requests in the plug structure")
    	Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 4711b57317f0ff5ca9fbd5e2df6c73b2c07ddc53)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1942,8 +1942,10 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 		 */
 		if (list_empty(&plug->mq_list))
 			same_queue_rq = NULL;
-		if (same_queue_rq)
+		if (same_queue_rq) {
 			list_del_init(&same_queue_rq->queuelist);
+			plug->rq_count--;
+		}
 		list_add_tail(&rq->queuelist, &plug->mq_list);
 
 		blk_mq_put_ctx(data.ctx);
