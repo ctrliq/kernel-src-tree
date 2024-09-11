27bb2526286bd6086097837732ccb8e7766da065
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:53:55 -0400

    perf auxtrace: Add itrace 'q' option for quicker, less detailed decoding
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Adrian Hunter <adrian.hunter@intel.com>
    commit 51971536ef53478ec92cd9e8249048743169df22
    
    The 'q' option is for modes of decoding that are quicker because they
    skip or omit decoding some aspects of trace data.
    
    If supported, the 'q' option may be repeated to increase the effect.
    
    	Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    	Reviewed-by: Andi Kleen <ak@linux.intel.com>
    	Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lore.kernel.org/lkml/20200710151104.15137-11-adrian.hunter@intel.com
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 51971536ef53478ec92cd9e8249048743169df22)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/auxtrace.c
+++ tools/perf/util/auxtrace.c
@@ -1563,6 +1563,9 @@ int itrace_parse_synth_opts(const struct option *opt, const char *str,
 		case 'a':
 			synth_opts->remote_access = true;
 			break;
+		case 'q':
+			synth_opts->quick += 1;
+			break;
 		case ' ':
 		case ',':
 			break;
