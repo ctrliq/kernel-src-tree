405a4a944c07a9bf0ea5a82121a25e106611cb71
Jonathan Maple <jmaple@ciq.com>
2024-09-11 13:06:41 -0400

    r8169: fix MAC address being lost in PCI D3
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit 59715171fbd0172a579576f46821031800a63bc5
    
    (At least) RTL8168e forgets its MAC address in PCI D3. To fix this set
    the MAC address when resuming. For resuming from runtime-suspend we
    had this in place already, for resuming from S3/S5 it was missing.
    
    The commit referenced as being fixed isn't wrong, it's just the first
    one where the patch applies cleanly.
    
    Fixes: 0f07bd850d36 ("r8169: use dev_get_drvdata where possible")
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Reported-by: Albert Astals Cid <aacid@kde.org>
    	Tested-by: Albert Astals Cid <aacid@kde.org>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 59715171fbd0172a579576f46821031800a63bc5)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/realtek/r8169.c
+++ drivers/net/ethernet/realtek/r8169.c
@@ -6659,6 +6659,8 @@ static int rtl8169_resume(struct device *device)
 	struct net_device *dev = dev_get_drvdata(device);
 	struct rtl8169_private *tp = netdev_priv(dev);
 
+	rtl_rar_set(tp, dev->dev_addr);
+
 	clk_prepare_enable(tp->clk);
 
 	if (netif_running(dev))
@@ -6692,6 +6694,7 @@ static int rtl8169_runtime_resume(struct device *device)
 {
 	struct net_device *dev = dev_get_drvdata(device);
 	struct rtl8169_private *tp = netdev_priv(dev);
+
 	rtl_rar_set(tp, dev->dev_addr);
 
 	if (!tp->TxDescArray)
