884a12a6601d320bf426b4a745d64ee9a5b86ca9
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:10:49 -0400

    net: switchdev: Add PORT_PRE_BRIDGE_FLAGS
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    commit-author Florian Fainelli <f.fainelli@gmail.com>
    commit 746dc184ba59aa2c843b9e888be3f1302d227b74
    
    In preparation for removing switchdev_port_attr_get(), introduce
    PORT_PRE_BRIDGE_FLAGS which will be called through
    switchdev_port_attr_set(), in the caller's context (possibly atomic) and
    which must be checked by the switchdev driver in order to return whether
    the operation is supported or not.
    
    This is entirely analoguous to how the BRIDGE_FLAGS_SUPPORT works,
    except it goes through a set() instead of get().
    
    	Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    	Reviewed-by: Ido Schimmel <idosch@mellanox.com>
    	Acked-by: Jiri Pirko <jiri@mellanox.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 746dc184ba59aa2c843b9e888be3f1302d227b74)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/net/switchdev.h
+++ include/net/switchdev.h
@@ -49,6 +49,7 @@ enum switchdev_attr_id {
 	SWITCHDEV_ATTR_ID_PORT_STP_STATE,
 	SWITCHDEV_ATTR_ID_PORT_BRIDGE_FLAGS,
 	SWITCHDEV_ATTR_ID_PORT_BRIDGE_FLAGS_SUPPORT,
+	SWITCHDEV_ATTR_ID_PORT_PRE_BRIDGE_FLAGS,
 	SWITCHDEV_ATTR_ID_PORT_MROUTER,
 	SWITCHDEV_ATTR_ID_BRIDGE_AGEING_TIME,
 	SWITCHDEV_ATTR_ID_BRIDGE_VLAN_FILTERING,
@@ -65,7 +66,7 @@ struct switchdev_attr {
 	union {
 		struct netdev_phys_item_id ppid;	/* PORT_PARENT_ID */
 		u8 stp_state;				/* PORT_STP_STATE */
-		unsigned long brport_flags;		/* PORT_BRIDGE_FLAGS */
+		unsigned long brport_flags;		/* PORT_{PRE}_BRIDGE_FLAGS */
 		unsigned long brport_flags_support;	/* PORT_BRIDGE_FLAGS_SUPPORT */
 		bool mrouter;				/* PORT_MROUTER */
 		clock_t ageing_time;			/* BRIDGE_AGEING_TIME */
