ff2c96f2776090707f55cdd52f4c8a06f92b94bf
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:51:21 -0400

    mmc: sdhci-pci: Add support for Intel EHL
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Adrian Hunter <adrian.hunter@intel.com>
    commit cb3a7d4a0aec4e56043d6433724e0606d6b6efba
    
    Add PCI Ids for Intel EHL.
    
    	Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit cb3a7d4a0aec4e56043d6433724e0606d6b6efba)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/host/sdhci-pci-core.c
+++ drivers/mmc/host/sdhci-pci-core.c
@@ -1502,6 +1502,8 @@ static const struct pci_device_id pci_ids[] = {
 	SDHCI_PCI_DEVICE(INTEL, CNPH_SD,   intel_byt_sd),
 	SDHCI_PCI_DEVICE(INTEL, ICP_EMMC,  intel_glk_emmc),
 	SDHCI_PCI_DEVICE(INTEL, ICP_SD,    intel_byt_sd),
+	SDHCI_PCI_DEVICE(INTEL, EHL_EMMC,  intel_glk_emmc),
+	SDHCI_PCI_DEVICE(INTEL, EHL_SD,    intel_byt_sd),
 	SDHCI_PCI_DEVICE(INTEL, CML_EMMC,  intel_glk_emmc),
 	SDHCI_PCI_DEVICE(INTEL, CML_SD,    intel_byt_sd),
 	SDHCI_PCI_DEVICE(INTEL, CMLH_SD,   intel_byt_sd),
