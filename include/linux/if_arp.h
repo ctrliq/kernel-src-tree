4ce1075424bd7078a3975fc43455a839d470bb9a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 18:11:19 -0400

    net: dev_is_mac_header_xmit() true for ARPHRD_RAWIP
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-365.el8
    commit-author Maciej Żenczykowski <maze@google.com>
    commit 3b707c3008cad04604c1f50e39f456621821c414
    
    __bpf_redirect() and act_mirred checks this boolean
    to determine whether to prefix an ethernet header.
    
    	Signed-off-by: Maciej Żenczykowski <maze@google.com>
    	Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 3b707c3008cad04604c1f50e39f456621821c414)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/if_arp.h
+++ include/linux/if_arp.h
@@ -55,6 +55,7 @@ static inline bool dev_is_mac_header_xmit(const struct net_device *dev)
 	case ARPHRD_IP6GRE:
 	case ARPHRD_VOID:
 	case ARPHRD_NONE:
+	case ARPHRD_RAWIP:
 		return false;
 	default:
 		return true;
