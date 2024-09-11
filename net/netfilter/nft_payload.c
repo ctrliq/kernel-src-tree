a00e850d9aa66834b736b5dc73c1160a1d9e3282
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:19:38 -0400

    netfilter: nftables: allow re-computing sctp CRC-32C in 'payload' statements
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Davide Caratti <dcaratti@redhat.com>
    commit 346e320cb2103edef709c4466a29140c4a8e527a
    
    nftables payload statements are used to mangle SCTP headers, but they can
    only replace the Internet Checksum. As a consequence, nftables rules that
    mangle sport/dport/vtag in SCTP headers potentially generate packets that
    are discarded by the receiver, unless the CRC-32C is "offloaded" (e.g the
    rule mangles a skb having 'ip_summed' equal to 'CHECKSUM_PARTIAL'.
    
    Fix this extending uAPI definitions and L4 checksum update function, in a
    way that userspace programs (e.g. nft) can instruct the kernel to compute
    CRC-32C in SCTP headers. Also ensure that LIBCRC32C is built if NF_TABLES
    is 'y' or 'm' in the kernel build configuration.
    
    	Signed-off-by: Davide Caratti <dcaratti@redhat.com>
    	Signed-off-by: Florian Westphal <fw@strlen.de>
    	Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 346e320cb2103edef709c4466a29140c4a8e527a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/netfilter/nft_payload.c
+++ net/netfilter/nft_payload.c
@@ -25,6 +25,7 @@
 #include <linux/icmpv6.h>
 #include <linux/ip.h>
 #include <linux/ipv6.h>
+#include <net/sctp/checksum.h>
 
 /* add vlan header into the user buffer for if tag was removed by offloads */
 static bool
@@ -439,6 +440,19 @@ static int nft_payload_l4csum_offset(const struct nft_pktinfo *pkt,
 	return 0;
 }
 
+static int nft_payload_csum_sctp(struct sk_buff *skb, int offset)
+{
+	struct sctphdr *sh;
+
+	if (skb_ensure_writable(skb, offset + sizeof(*sh)))
+		return -1;
+
+	sh = (struct sctphdr *)(skb->data + offset);
+	sh->checksum = sctp_compute_cksum(skb, offset);
+	skb->ip_summed = CHECKSUM_UNNECESSARY;
+	return 0;
+}
+
 static int nft_payload_l4csum_update(const struct nft_pktinfo *pkt,
 				     struct sk_buff *skb,
 				     __wsum fsum, __wsum tsum)
@@ -542,6 +556,13 @@ static void nft_payload_set_eval(const struct nft_expr *expr,
 	    skb_store_bits(skb, offset, src, priv->len) < 0)
 		goto err;
 
+	if (priv->csum_type == NFT_PAYLOAD_CSUM_SCTP &&
+	    pkt->tprot == IPPROTO_SCTP &&
+	    skb->ip_summed != CHECKSUM_PARTIAL) {
+		if (nft_payload_csum_sctp(skb, pkt->xt.thoff))
+			goto err;
+	}
+
 	return;
 err:
 	regs->verdict.code = NFT_BREAK;
@@ -577,6 +598,13 @@ static int nft_payload_set_init(const struct nft_ctx *ctx,
 	switch (priv->csum_type) {
 	case NFT_PAYLOAD_CSUM_NONE:
 	case NFT_PAYLOAD_CSUM_INET:
+		break;
+	case NFT_PAYLOAD_CSUM_SCTP:
+		if (priv->base != NFT_PAYLOAD_TRANSPORT_HEADER)
+			return -EINVAL;
+
+		if (priv->csum_offset != offsetof(struct sctphdr, checksum))
+			return -EINVAL;
 		break;
 	default:
 		return -EOPNOTSUPP;
