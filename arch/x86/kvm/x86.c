59e9b7a0431b31530666a3d7a9079722686774cd
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:40:06 -0400

    KVM: LAPIC: Optimize timer latency further
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-193.el8
    commit-author Wanpeng Li <wanpengli@tencent.com>
    commit b6c4bc659c6f3b7f2b6c3a330ae36f1cfd69d73e
    
    Advance lapic timer tries to hidden the hypervisor overhead between the
    host emulated timer fires and the guest awares the timer is fired. However,
    it just hidden the time between apic_timer_fn/handle_preemption_timer ->
    wait_lapic_expire, instead of the real position of vmentry which is
    mentioned in the orignial commit d0659d946be0 ("KVM: x86: add option to
    advance tscdeadline hrtimer expiration"). There is 700+ cpu cycles between
    the end of wait_lapic_expire and before world switch on my haswell desktop.
    
    This patch tries to narrow the last gap(wait_lapic_expire -> world switch),
    it takes the real overhead time between apic_timer_fn/handle_preemption_timer
    and before world switch into consideration when adaptively tuning timer
    advancement. The patch can reduce 40% latency (~1600+ cycles to ~1000+ cycles
    on a haswell desktop) for kvm-unit-tests/tscdeadline_latency when testing
    busy waits.
    
    	Cc: Paolo Bonzini <pbonzini@redhat.com>
    	Cc: Radim Krčmář <rkrcmar@redhat.com>
    	Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    	Cc: Liran Alon <liran.alon@oracle.com>
    	Reviewed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    	Signed-off-by: Wanpeng Li <wanpengli@tencent.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit b6c4bc659c6f3b7f2b6c3a330ae36f1cfd69d73e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -7910,9 +7910,6 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 	}
 
 	trace_kvm_entry(vcpu->vcpu_id);
-	if (lapic_in_kernel(vcpu) &&
-	    vcpu->arch.apic->lapic_timer.timer_advance_ns)
-		wait_lapic_expire(vcpu);
 	guest_enter_irqoff();
 
 	if (unlikely(vcpu->arch.switch_db_regs)) {
