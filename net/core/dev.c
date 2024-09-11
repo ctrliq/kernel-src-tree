12a2d32ce048a1c5ee6cf43f6a2d07452acc3de2
Jonathan Maple <jmaple@ciq.com>
2024-09-11 12:14:53 -0400

    net/core: use __vlan_hwaccel helpers
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-147.el8
    Rebuild_CHGLOG: - [net] core: use __vlan_hwaccel helpers (Ivan Vecera) [1688236 1687456]
    Rebuild_FUZZ: 94.12%
    commit-author Michał Mirosław <mirq-linux@rere.qmqm.pl>
    commit b1817524c028a5a5284f21358185c74790001e0e
    
    This removes assumptions about VLAN_TAG_PRESENT bit.
    
    	Signed-off-by: Michał Mirosław <mirq-linux@rere.qmqm.pl>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit b1817524c028a5a5284f21358185c74790001e0e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4896,7 +4896,7 @@ skip_classify:
 		 * and set skb->priority like in vlan_do_receive()
 		 * For the time being, just ignore Priority Code Point
 		 */
-		skb->vlan_tci = 0;
+		__vlan_hwaccel_clear_tag(skb);
 	}
 
 	type = skb->protocol;
@@ -5215,7 +5215,9 @@ static void gro_list_prepare(struct napi_struct *napi, struct sk_buff *skb)
 		}
 
 		diffs = (unsigned long)p->dev ^ (unsigned long)skb->dev;
-		diffs |= p->vlan_tci ^ skb->vlan_tci;
+		diffs |= skb_vlan_tag_present(p) ^ skb_vlan_tag_present(skb);
+		if (skb_vlan_tag_present(p))
+			diffs |= p->vlan_tci ^ skb->vlan_tci;
 		diffs |= skb_metadata_dst_cmp(p, skb);
 		diffs |= skb_metadata_differs(p, skb);
 		if (maclen == ETH_HLEN)
@@ -5464,7 +5466,7 @@ static void napi_reuse_skb(struct napi_struct *napi, struct sk_buff *skb)
 	__skb_pull(skb, skb_headlen(skb));
 	/* restore the reserve we had after netdev_alloc_skb_ip_align() */
 	skb_reserve(skb, NET_SKB_PAD + NET_IP_ALIGN - skb_headroom(skb));
-	skb->vlan_tci = 0;
+	__vlan_hwaccel_clear_tag(skb);
 	skb->dev = napi->dev;
 	skb->skb_iif = 0;
 	skb->encapsulation = 0;
