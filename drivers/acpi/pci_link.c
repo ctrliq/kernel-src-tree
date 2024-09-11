1e3d01cf36770b429d5c2135a7608113d905234b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 19:47:38 -0400

    ACPI/PCI: Remove useless NULL pointer checks
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Andrey Strachuk <strochuk@ispras.ru>
    commit 2c65e312bc6bb6e175c16a17db8ec86d2dcd1608
    
    Local variable 'p' is initialized by an address of field of acpi_resource,
    so it does not make sense to compare 'p' with NULL.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    	Signed-off-by: Andrey Strachuk <strochuk@ispras.ru>
    	Reviewed-by: Bjorn Helgaas <bhelgaas@google.com>
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 2c65e312bc6bb6e175c16a17db8ec86d2dcd1608)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/pci_link.c
+++ drivers/acpi/pci_link.c
@@ -108,7 +108,7 @@ static acpi_status acpi_pci_link_check_possible(struct acpi_resource *resource,
 	case ACPI_RESOURCE_TYPE_IRQ:
 		{
 			struct acpi_resource_irq *p = &resource->data.irq;
-			if (!p || !p->interrupt_count) {
+			if (!p->interrupt_count) {
 				acpi_handle_debug(handle,
 						  "Blank _PRS IRQ resource\n");
 				return AE_OK;
@@ -134,7 +134,7 @@ static acpi_status acpi_pci_link_check_possible(struct acpi_resource *resource,
 		{
 			struct acpi_resource_extended_irq *p =
 			    &resource->data.extended_irq;
-			if (!p || !p->interrupt_count) {
+			if (!p->interrupt_count) {
 				acpi_handle_debug(handle,
 						  "Blank _PRS EXT IRQ resource\n");
 				return AE_OK;
@@ -195,7 +195,7 @@ static acpi_status acpi_pci_link_check_current(struct acpi_resource *resource,
 	case ACPI_RESOURCE_TYPE_IRQ:
 		{
 			struct acpi_resource_irq *p = &resource->data.irq;
-			if (!p || !p->interrupt_count) {
+			if (!p->interrupt_count) {
 				/*
 				 * IRQ descriptors may have no IRQ# bits set,
 				 * particularly those w/ _STA disabled
@@ -210,7 +210,7 @@ static acpi_status acpi_pci_link_check_current(struct acpi_resource *resource,
 		{
 			struct acpi_resource_extended_irq *p =
 			    &resource->data.extended_irq;
-			if (!p || !p->interrupt_count) {
+			if (!p->interrupt_count) {
 				/*
 				 * extended IRQ descriptors must
 				 * return at least 1 IRQ
