c10f948da4242638fa4787b6d0ba33dd54e45c34
Jonathan Maple <jmaple@ciq.com>
2024-09-11 15:06:59 -0400

    xfs: avoid redundant checks when options is empty
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-294.el8
    commit-author Ian Kent <raven@themaw.net>
    commit 846410ccd104c7294d18e61c665bf7c0c7e6d7d1
    
    When options passed to xfs_parseargs() is NULL the checks performed
    after taking the branch are made with the initial values of dsunit,
    dswidth and iosizelog. But all the checks do nothing in this case
    so return immediately instead.
    
    	Signed-off-by: Ian Kent <raven@themaw.net>
    	Reviewed-by: Christoph Hellwig <hch@lst.de>
    	Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    	Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    (cherry picked from commit 846410ccd104c7294d18e61c665bf7c0c7e6d7d1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -180,7 +180,7 @@ xfs_parseargs(
 	mp->m_logbsize = -1;
 
 	if (!options)
-		goto done;
+		return 0;
 
 	while ((p = strsep(&options, ",")) != NULL) {
 		int		token;
@@ -360,7 +360,6 @@ xfs_parseargs(
 		return -EINVAL;
 	}
 
-done:
 	if (mp->m_logbufs != -1 &&
 	    mp->m_logbufs != 0 &&
 	    (mp->m_logbufs < XLOG_MIN_ICLOGS ||
