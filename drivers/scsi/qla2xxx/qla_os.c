d38f82114f16b677f9a1489a048fc48d2d1fe7cb
Jonathan Maple <jmaple@ciq.com>
2024-09-11 14:15:24 -0400

    scsi: qla2xxx: Fix I/Os being passed down when FC device is being deleted
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-240.el8
    commit-author Arun Easi <aeasi@marvell.com>
    commit 78c3e5e6cde8193467427ed9e5f90a0d585625a1
    
    I/Os could be passed down while the device FC SCSI device is being deleted.
    This would result in unnecessary delay of I/O and driver messages (when
    extended logging is set).
    
    [mkp: fixed commit hash and added SoB for Nilesh]
    
    Link: https://lore.kernel.org/r/20200313085001.3781-1-njavali@marvell.com
    Fixes: 3c75ad1d87c7 ("scsi: qla2xxx: Remove defer flag to indicate immeadiate port loss") # v5.6-rc1+
    	Tested-by: Laurence Oberman <loberman@redhat.com>
    	Reviewed-by: Laurence Oberman <loberman@redhat.com>
    	Reviewed-by: Ewan D. Milne <emilne@redhat.com>
    	Reviewed-by: Roman Bolshakov <r.bolshakov@yadro.com>
    	Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    	Signed-off-by: Arun Easi <aeasi@marvell.com>
    	Signed-off-by: Nilesh Javali <njavali@marvell.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 78c3e5e6cde8193467427ed9e5f90a0d585625a1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -862,7 +862,7 @@ qla2xxx_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 		goto qc24_fail_command;
 	}
 
-	if (atomic_read(&fcport->state) != FCS_ONLINE) {
+	if (atomic_read(&fcport->state) != FCS_ONLINE || fcport->deleted) {
 		if (atomic_read(&fcport->state) == FCS_DEVICE_DEAD ||
 			atomic_read(&base_vha->loop_state) == LOOP_DEAD) {
 			ql_dbg(ql_dbg_io, vha, 0x3005,
@@ -944,7 +944,7 @@ qla2xxx_mqueuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd,
 		goto qc24_fail_command;
 	}
 
-	if (atomic_read(&fcport->state) != FCS_ONLINE) {
+	if (atomic_read(&fcport->state) != FCS_ONLINE || fcport->deleted) {
 		if (atomic_read(&fcport->state) == FCS_DEVICE_DEAD ||
 			atomic_read(&base_vha->loop_state) == LOOP_DEAD) {
 			ql_dbg(ql_dbg_io, vha, 0x3077,
