7c7adf563021199e70d423ca267cdbc54fe87a92
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:54:25 -0400

    Bluetooth: hci_request: Remove bg_scan_update work
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-541.el8
    commit-author Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    commit 9482c5074a7d0bec682cc35a12d687f185684f31
    
    This work is no longer necessary since all the code using it has been
    converted to use hci_passive_scan/hci_passive_scan_sync.
    
    	Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    	Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit 9482c5074a7d0bec682cc35a12d687f185684f31)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -496,7 +496,6 @@ struct hci_dev {
 	struct work_struct	tx_work;
 
 	struct work_struct	discov_update;
-	struct work_struct	bg_scan_update;
 	struct work_struct	scan_update;
 	struct work_struct	connectable_update;
 	struct work_struct	discoverable_update;
