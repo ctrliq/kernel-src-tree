10bee86de3dbc3b6248f89e5bbc73a414be7efb7
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:55:02 -0400

    RDMA/efa: Add rdma write capability to device caps
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-541.el8
    commit-author Yonatan Nachum <ynachum@amazon.com>
    commit 531094dc7164718d28ebb581d729807d7e846363
    
    Add rdma write capability that is propagated from the device to rdma-core.
    Enable MR creation with remote write permissions according to this device
    capability.
    
    Link: https://lore.kernel.org/r/20230404154313.35194-1-ynachum@amazon.com
    	Reviewed-by: Firas Jahjah <firasj@amazon.com>
    	Reviewed-by: Michael Margolin <mrgolin@amazon.com>
    	Signed-off-by: Yonatan Nachum <ynachum@amazon.com>
    	Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    (cherry picked from commit 531094dc7164718d28ebb581d729807d7e846363)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/hw/efa/efa_verbs.c
+++ drivers/infiniband/hw/efa/efa_verbs.c
@@ -251,6 +251,9 @@ int efa_query_device(struct ib_device *ibdev,
 		if (EFA_DEV_CAP(dev, DATA_POLLING_128))
 			resp.device_caps |= EFA_QUERY_DEVICE_CAPS_DATA_POLLING_128;
 
+		if (EFA_DEV_CAP(dev, RDMA_WRITE))
+			resp.device_caps |= EFA_QUERY_DEVICE_CAPS_RDMA_WRITE;
+
 		if (dev->neqs)
 			resp.device_caps |= EFA_QUERY_DEVICE_CAPS_CQ_NOTIFICATIONS;
 
@@ -1578,7 +1581,8 @@ struct ib_mr *efa_reg_mr(struct ib_pd *ibpd, u64 start, u64 length,
 
 	supp_access_flags =
 		IB_ACCESS_LOCAL_WRITE |
-		(EFA_DEV_CAP(dev, RDMA_READ) ? IB_ACCESS_REMOTE_READ : 0);
+		(EFA_DEV_CAP(dev, RDMA_READ) ? IB_ACCESS_REMOTE_READ : 0) |
+		(EFA_DEV_CAP(dev, RDMA_WRITE) ? IB_ACCESS_REMOTE_WRITE : 0);
 
 	access_flags &= ~IB_ACCESS_OPTIONAL;
 	if (access_flags & ~supp_access_flags) {
