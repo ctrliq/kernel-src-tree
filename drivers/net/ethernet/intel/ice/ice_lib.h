898142aacc1dee7e07cba0dae3d1fdf669519cf8
Jonathan Maple <jmaple@ciq.com>
2024-09-12 18:44:37 -0400

    ice: Implement support for SMA and U.FL on E810-T
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-372.9.1.el8
    commit-author Maciej Machnikowski <maciej.machnikowski@intel.com>
    commit 325b2064d00a88d10c511c96adea4e8274778ee1
    
    Expose SMA and U.FL connectors as ptp_pins on E810-T based adapters and
    allow controlling them.
    
    E810-T adapters are equipped with:
    - 2 external bidirectional SMA connectors
    - 1 internal TX U.FL
    - 1 internal RX U.FL
    
    U.FL connectors share signal lines with the SMA connectors. The TX U.FL1
    share the line with the SMA1 and the RX U.FL2 share line with the SMA2.
    This dependence is controlled by the ice_verify_pin_e810t.
    
    Additionally add support for the E810-T-based  devices which don't use the
    SMA/U.FL controller. If the IO expander is not detected don't expose pins
    and use 2 predefined 1PPS input and output pins.
    
    	Signed-off-by: Maciej Machnikowski <maciej.machnikowski@intel.com>
    	Tested-by: Sunitha Mekala <sunithax.d.mekala@intel.com>
    	Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit 325b2064d00a88d10c511c96adea4e8274778ee1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/ice/ice_lib.h
+++ drivers/net/ethernet/intel/ice/ice_lib.h
@@ -118,6 +118,7 @@ int ice_set_dflt_vsi(struct ice_sw *sw, struct ice_vsi *vsi);
 
 int ice_clear_dflt_vsi(struct ice_sw *sw);
 bool ice_is_feature_supported(struct ice_pf *pf, enum ice_feature f);
+void ice_clear_feature_support(struct ice_pf *pf, enum ice_feature f);
 void ice_init_feature_support(struct ice_pf *pf);
 int ice_set_min_bw_limit(struct ice_vsi *vsi, u64 min_tx_rate);
 int ice_set_max_bw_limit(struct ice_vsi *vsi, u64 max_tx_rate);
