598b37263da1eeeb26e340016af983722a3abc8b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:43:25 -0400

    ice: always return IRQ_WAKE_THREAD in ice_misc_intr()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-535.el8
    commit-author Karol Kolacinski <karol.kolacinski@intel.com>
    commit d578e618f192f453baf4fd7e32fec88ed7e678b8
    
    Refactor the ice_misc_intr() function to always return IRQ_WAKE_THREAD, and
    schedule the service task during the soft IRQ thread function instead of at
    the end of the hard IRQ handler.
    
    Remove the duplicate call to ice_service_task_schedule() that happened when
    we got a PCI exception.
    
    	Signed-off-by: Karol Kolacinski <karol.kolacinski@intel.com>
    	Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    	Tested-by: Arpana Arland <arpanax.arland@intel.com> (A Contingent worker at Intel)
    	Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit d578e618f192f453baf4fd7e32fec88ed7e678b8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -3060,7 +3060,6 @@ static irqreturn_t ice_misc_intr(int __always_unused irq, void *data)
 {
 	struct ice_pf *pf = (struct ice_pf *)data;
 	struct ice_hw *hw = &pf->hw;
-	irqreturn_t ret = IRQ_NONE;
 	struct device *dev;
 	u32 oicr, ena_mask;
 
@@ -3141,10 +3140,8 @@ static irqreturn_t ice_misc_intr(int __always_unused irq, void *data)
 
 	if (oicr & PFINT_OICR_TSYN_TX_M) {
 		ena_mask &= ~PFINT_OICR_TSYN_TX_M;
-		if (!hw->reset_ongoing) {
+		if (!hw->reset_ongoing)
 			set_bit(ICE_MISC_THREAD_TX_TSTAMP, pf->misc_thread);
-			ret = IRQ_WAKE_THREAD;
-		}
 	}
 
 	if (oicr & PFINT_OICR_TSYN_EVNT_M) {
@@ -3161,7 +3158,6 @@ static irqreturn_t ice_misc_intr(int __always_unused irq, void *data)
 					       GLTSYN_STAT_EVENT2_M);
 
 			set_bit(ICE_MISC_THREAD_EXTTS_EVENT, pf->misc_thread);
-			ret = IRQ_WAKE_THREAD;
 		}
 	}
 
@@ -3182,16 +3178,12 @@ static irqreturn_t ice_misc_intr(int __always_unused irq, void *data)
 		if (oicr & (PFINT_OICR_PCI_EXCEPTION_M |
 			    PFINT_OICR_ECC_ERR_M)) {
 			set_bit(ICE_PFR_REQ, pf->state);
-			ice_service_task_schedule(pf);
 		}
 	}
-	if (!ret)
-		ret = IRQ_HANDLED;
 
-	ice_service_task_schedule(pf);
 	ice_irq_dynamic_ena(hw, NULL, NULL);
 
-	return ret;
+	return IRQ_WAKE_THREAD;
 }
 
 /**
@@ -3205,6 +3197,8 @@ static irqreturn_t ice_misc_intr_thread_fn(int __always_unused irq, void *data)
 
 	if (ice_is_reset_in_progress(pf->state))
 		return IRQ_HANDLED;
+
+	ice_service_task_schedule(pf);
 
 	if (test_and_clear_bit(ICE_MISC_THREAD_EXTTS_EVENT, pf->misc_thread))
 		ice_ptp_extts_event(pf);
