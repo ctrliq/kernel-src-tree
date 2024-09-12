ad59291398bd59dcfc8bdd3ff00dfb1ee32609f8
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:36:36 -0400

    ACPICA: Check that EBDA pointer is in valid memory
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-529.el8
    commit-author Vit Kabele <vit@kabele.me>
    commit 4fe54f509304ea6cc80a40620cac860c57edf7f3
    
    ACPICA commit cc9e7763ceb2e2649fe3422130416d84a3c6854a
    
    If the memory at 0x40e is uninitialized, the retrieved physical_memory
    address of EBDA may be beyond the low memory (i.e. above 640K).
    
    If so, the kernel may unintentionally access the VGA memory, that
    might not be decoded or even present in case of virtualization.
    
    Link: https://github.com/acpica/acpica/commit/cc9e7763
    	Signed-off-by: Vit Kabele <vit@kabele.me>
    	Signed-off-by: Bob Moore <robert.moore@intel.com>
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 4fe54f509304ea6cc80a40620cac860c57edf7f3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/acpica/tbxfroot.c
+++ drivers/acpi/acpica/tbxfroot.c
@@ -140,7 +140,11 @@ acpi_find_root_pointer(acpi_physical_address *table_address)
 
 	/* EBDA present? */
 
-	if (physical_address > 0x400) {
+	/*
+	 * Check that the EBDA pointer from memory is sane and does not point
+	 * above valid low memory
+	 */
+	if (physical_address > 0x400 && physical_address < 0xA0000) {
 		/*
 		 * Calculate the scan window size
 		 * The EBDA is not guaranteed to be larger than a ki_b and in case
