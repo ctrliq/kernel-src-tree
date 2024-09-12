abe2a936f072396c7ee4e5d1b576c57907d5ab25
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:28:31 -0400

    gfs2: Add new go_held glock operation
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-483.el8
    commit-author Andreas Gruenbacher <agruenba@redhat.com>
    commit 86c30a01f5da411d0d090f14f7aeadd8c20b2d05
    
    Right now, inode_go_instantiate() contains functionality that relates to
    how a glock is held rather than the glock itself, like waiting for
    pending direct I/O to complete and completing interrupted truncates.
    This code is meant to be run each time a holder is acquired, but
    go_instantiate is actually only called once, when the glock is
    instantiated.
    
    To fix that, introduce a new go_held glock operation that is called each
    time a glock holder is acquired.  Move the holder specific code in
    inode_go_instantiate() over to inode_go_held().
    
    	Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit 86c30a01f5da411d0d090f14f7aeadd8c20b2d05)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/gfs2/glops.c
+++ fs/gfs2/glops.c
@@ -485,14 +485,21 @@ static int inode_go_instantiate(struct gfs2_holder *gh)
 	struct gfs2_glock *gl = gh->gh_gl;
 	struct gfs2_sbd *sdp = gl->gl_name.ln_sbd;
 	struct gfs2_inode *ip = gl->gl_object;
+
+	if (!ip) /* no inode to populate - read it in later */
+		return 0;
+
+	return gfs2_inode_refresh(ip);
+}
+
+static int inode_go_held(struct gfs2_holder *gh)
+{
+	struct gfs2_glock *gl = gh->gh_gl;
+	struct gfs2_inode *ip = gl->gl_object;
 	int error = 0;
 
 	if (!ip) /* no inode to populate - read it in later */
-		goto out;
-
-	error = gfs2_inode_refresh(ip);
-	if (error)
-		goto out;
+		return 0;
 
 	if (gh->gh_state != LM_ST_DEFERRED)
 		inode_dio_wait(&ip->i_inode);
@@ -508,7 +515,6 @@ static int inode_go_instantiate(struct gfs2_holder *gh)
 		gfs2_wait_truncate(ip);
 	}
 
-out:
 	return error;
 }
 
@@ -731,6 +737,7 @@ const struct gfs2_glock_operations gfs2_inode_glops = {
 	.go_inval = inode_go_inval,
 	.go_demote_ok = inode_go_demote_ok,
 	.go_instantiate = inode_go_instantiate,
+	.go_held = inode_go_held,
 	.go_dump = inode_go_dump,
 	.go_type = LM_TYPE_INODE,
 	.go_flags = GLOF_ASPACE | GLOF_LRU | GLOF_LVB,
