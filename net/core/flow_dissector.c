7455080d2a445c0a735814dc0d3a5de2b3531c2a
Jonathan Maple <jmaple@ciq.com>
2024-09-12 17:03:38 -0400

    cls_flower: Fix inability to match GRE/IPIP packets
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-425.3.1.el8
    commit-author Yoshiki Komachi <komachi.yoshiki@gmail.com>
    commit 6de6e46d27ef386feecdbea56b3bfd6c3b3bc1f9
    
    When a packet of a new flow arrives in openvswitch kernel module, it dissects
    the packet and passes the extracted flow key to ovs-vswtichd daemon. If hw-
    offload configuration is enabled, the daemon creates a new TC flower entry to
    bypass openvswitch kernel module for the flow (TC flower can also offload flows
    to NICs but this time that does not matter).
    
    In this processing flow, I found the following issue in cases of GRE/IPIP
    packets.
    
    When ovs_flow_key_extract() in openvswitch module parses a packet of a new
    GRE (or IPIP) flow received on non-tunneling vports, it extracts information
    of the outer IP header for ip_proto/src_ip/dst_ip match keys.
    
    This means ovs-vswitchd creates a TC flower entry with IP protocol/addresses
    match keys whose values are those of the outer IP header. OTOH, TC flower,
    which uses flow_dissector (different parser from openvswitch module), extracts
    information of the inner IP header.
    
    The following flow is an example to describe the issue in more detail.
    
       <----------- Outer IP -----------------> <---------- Inner IP ---------->
      +----------+--------------+--------------+----------+----------+----------+
      | ip_proto | src_ip       | dst_ip       | ip_proto | src_ip   | dst_ip   |
      | 47 (GRE) | 192.168.10.1 | 192.168.10.2 | 6 (TCP)  | 10.0.0.1 | 10.0.0.2 |
      +----------+--------------+--------------+----------+----------+----------+
    
    In this case, TC flower entry and extracted information are shown as below:
    
      - ovs-vswitchd creates TC flower entry with:
          - ip_proto: 47
          - src_ip: 192.168.10.1
          - dst_ip: 192.168.10.2
    
      - TC flower extracts below for IP header matches:
          - ip_proto: 6
          - src_ip: 10.0.0.1
          - dst_ip: 10.0.0.2
    
    Thus, GRE or IPIP packets never match the TC flower entry, as each
    dissector behaves differently.
    
    IMHO, the behavior of TC flower (flow dissector) does not look correct,
    as ip_proto/src_ip/dst_ip in TC flower match means the outermost IP
    header information except for GRE/IPIP cases. This patch adds a new
    flow_dissector flag FLOW_DISSECTOR_F_STOP_BEFORE_ENCAP which skips
    dissection of the encapsulated inner GRE/IPIP header in TC flower
    classifier.
    
    	Signed-off-by: Yoshiki Komachi <komachi.yoshiki@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 6de6e46d27ef386feecdbea56b3bfd6c3b3bc1f9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/core/flow_dissector.c
+++ net/core/flow_dissector.c
@@ -1299,6 +1299,11 @@ ip_proto_again:
 
 	switch (ip_proto) {
 	case IPPROTO_GRE:
+		if (flags & FLOW_DISSECTOR_F_STOP_BEFORE_ENCAP) {
+			fdret = FLOW_DISSECT_RET_OUT_GOOD;
+			break;
+		}
+
 		fdret = __skb_flow_dissect_gre(skb, key_control, flow_dissector,
 					       target_container, data,
 					       &proto, &nhoff, &hlen, flags);
@@ -1356,6 +1361,11 @@ ip_proto_again:
 		break;
 	}
 	case IPPROTO_IPIP:
+		if (flags & FLOW_DISSECTOR_F_STOP_BEFORE_ENCAP) {
+			fdret = FLOW_DISSECT_RET_OUT_GOOD;
+			break;
+		}
+
 		proto = htons(ETH_P_IP);
 
 		key_control->flags |= FLOW_DIS_ENCAPSULATION;
@@ -1368,6 +1378,11 @@ ip_proto_again:
 		break;
 
 	case IPPROTO_IPV6:
+		if (flags & FLOW_DISSECTOR_F_STOP_BEFORE_ENCAP) {
+			fdret = FLOW_DISSECT_RET_OUT_GOOD;
+			break;
+		}
+
 		proto = htons(ETH_P_IPV6);
 
 		key_control->flags |= FLOW_DIS_ENCAPSULATION;
