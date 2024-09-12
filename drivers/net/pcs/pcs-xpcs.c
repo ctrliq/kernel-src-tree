5e093bd5ea5adc6a56756275988c826f0373a601
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:19:09 -0400

    net: pcs: pcs-xpcs: Convert to mdiobus_c45_read
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-481.el8
    commit-author Andrew Lunn <andrew@lunn.ch>
    commit d18af067c98eb187b8d9fe1f4e2e59ba3ad0a1d8
    
    Stop using the helpers to construct a special mdio address which
    indicates C45. Instead use the C45 accessors, which will call the
    busses C45 specific read/write API.
    
    	Reviewed-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    	Tested-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    	Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    	Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    (cherry picked from commit d18af067c98eb187b8d9fe1f4e2e59ba3ad0a1d8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/pcs/pcs-xpcs.c
+++ drivers/net/pcs/pcs-xpcs.c
@@ -240,20 +240,18 @@ static bool __xpcs_linkmode_supported(const struct xpcs_compat *compat,
 
 static int xpcs_read(struct mdio_xpcs_args *xpcs, int dev, u32 reg)
 {
-	u32 reg_addr = mdiobus_c45_addr(dev, reg);
 	struct mii_bus *bus = xpcs->mdiodev->bus;
 	int addr = xpcs->mdiodev->addr;
 
-	return mdiobus_read(bus, addr, reg_addr);
+	return mdiobus_c45_read(bus, addr, dev, reg);
 }
 
 static int xpcs_write(struct mdio_xpcs_args *xpcs, int dev, u32 reg, u16 val)
 {
-	u32 reg_addr = mdiobus_c45_addr(dev, reg);
 	struct mii_bus *bus = xpcs->mdiodev->bus;
 	int addr = xpcs->mdiodev->addr;
 
-	return mdiobus_write(bus, addr, reg_addr, val);
+	return mdiobus_c45_write(bus, addr, dev, reg, val);
 }
 
 static int xpcs_read_vendor(struct mdio_xpcs_args *xpcs, int dev, u32 reg)
