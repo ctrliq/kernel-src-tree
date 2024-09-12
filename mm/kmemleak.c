526e793eb5bbe12e1bfcdd38175ed645f57c0d53
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:30:02 -0400

    mm/kmemleak: move up cond_resched() call in page scanning loop
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-524.el8
    commit-author Waiman Long <longman@redhat.com>
    commit e68d343d2720779362cb7160cb7f4bd24979b2b4
    
    Commit bde5f6bc68db ("kmemleak: add scheduling point to kmemleak_scan()")
    added a cond_resched() call to the struct page scanning loop to prevent
    soft lockup from happening.  However, soft lockup can still happen in that
    loop in some corner cases when the pages that satisfy the "!(pfn & 63)"
    check are skipped for some reasons.
    
    Fix this corner case by moving up the cond_resched() check so that it will
    be called every 64 pages unconditionally.
    
    Link: https://lkml.kernel.org/r/20230825164947.1317981-1-longman@redhat.com
    Fixes: bde5f6bc68db ("kmemleak: add scheduling point to kmemleak_scan()")
    	Signed-off-by: Waiman Long <longman@redhat.com>
    	Cc: Catalin Marinas <catalin.marinas@arm.com>
    	Cc: Yisheng Xie <xieyisheng1@huawei.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit e68d343d2720779362cb7160cb7f4bd24979b2b4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -1506,6 +1506,9 @@ static void kmemleak_scan(void)
 		for (pfn = start_pfn; pfn < end_pfn; pfn++) {
 			struct page *page = pfn_to_online_page(pfn);
 
+			if (!(pfn & 63))
+				cond_resched();
+
 			if (!page)
 				continue;
 
@@ -1516,8 +1519,6 @@ static void kmemleak_scan(void)
 			if (page_count(page) == 0)
 				continue;
 			scan_block(page, page + 1, NULL);
-			if (!(pfn & 63))
-				cond_resched();
 		}
 	}
 	put_online_mems();
