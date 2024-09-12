d29cbbe8bbd5b04cb5c00dc9f4c66bf5e2416a79
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:32:07 -0400

    ptp: Add adjphase function to support phase offset control.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-487.el8
    commit-author Vincent Cheng <vincent.cheng.xh@renesas.com>
    commit 184ecc9eb260d5a3bcdddc5bebd18f285ac004e9
    
    Adds adjust phase function to take advantage of a PHC
    clock's hardware filtering capability that uses phase offset
    control word instead of frequency offset control word.
    
    	Signed-off-by: Vincent Cheng <vincent.cheng.xh@renesas.com>
    	Reviewed-by: Richard Cochran <richardcochran@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 184ecc9eb260d5a3bcdddc5bebd18f285ac004e9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/ptp_clock_kernel.h
+++ include/linux/ptp_clock_kernel.h
@@ -51,7 +51,7 @@ struct ptp_system_timestamp {
 };
 
 /**
- * struct ptp_clock_info - decribes a PTP hardware clock
+ * struct ptp_clock_info - describes a PTP hardware clock
  *
  * @owner:     The clock driver should set to THIS_MODULE.
  * @name:      A short "friendly name" to identify the clock and to
@@ -79,6 +79,9 @@ struct ptp_system_timestamp {
  *            the @adjfine method instead.
  *            parameter delta: Desired frequency offset from nominal frequency
  *            in parts per billion
+ *
+ * @adjphase:  Adjusts the phase offset of the hardware clock.
+ *             parameter delta: Desired change in nanoseconds.
  *
  * @adjtime:  Shifts the time of the hardware clock.
  *            parameter delta: Desired change in nanoseconds.
@@ -143,6 +146,7 @@ struct ptp_clock_info {
 	struct ptp_pin_desc *pin_config;
 	int (*adjfine)(struct ptp_clock_info *ptp, long scaled_ppm);
 	int (*adjfreq)(struct ptp_clock_info *ptp, s32 delta);
+	int (*adjphase)(struct ptp_clock_info *ptp, s32 phase);
 	int (*adjtime)(struct ptp_clock_info *ptp, s64 delta);
 	int (*gettime64)(struct ptp_clock_info *ptp, struct timespec64 *ts);
 	int (*gettimex64)(struct ptp_clock_info *ptp, struct timespec64 *ts,
