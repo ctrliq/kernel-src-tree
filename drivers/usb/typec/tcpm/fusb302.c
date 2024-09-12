247423a760beebbc4384a5f840c90252cb38824a
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:31:38 -0400

    usb: typec: fusb302: mark OF related data as maybe unused
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-526.el8
    commit-author Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    commit b16de6a5c70b53bee495a42ebe74ccca076a3e26
    
    The driver can be compile tested with !CONFIG_OF making certain data
    unused:
    
      drivers/usb/typec/tcpm/fusb302.c:1816:34: error: ‘fusb302_dt_match’ defined but not used [-Werror=unused-const-variable=]
    
    	Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    	Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    	Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Link: https://lore.kernel.org/r/20230310222937.315773-2-krzysztof.kozlowski@linaro.org
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit b16de6a5c70b53bee495a42ebe74ccca076a3e26)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/usb/typec/tcpm/fusb302.c
+++ drivers/usb/typec/tcpm/fusb302.c
@@ -1816,7 +1816,7 @@ static int fusb302_pm_resume(struct device *dev)
 	return 0;
 }
 
-static const struct of_device_id fusb302_dt_match[] = {
+static const struct of_device_id fusb302_dt_match[] __maybe_unused = {
 	{.compatible = "fcs,fusb302"},
 	{},
 };
