003d7bb49fe21ad2482c230a543f5cb28f548dac
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:36:40 -0400

    ACPI/IORT: Update SMMUv3 DeviceID support
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-529.el8
    commit-author Robin Murphy <robin.murphy@arm.com>
    commit 05da178ce0aa152f0592e10fa874054187f3621b
    
    IORT E.e now allows SMMUv3 nodes to describe the DeviceID for MSIs
    independently of wired GSIVs, where the previous oddly-restrictive
    definition meant that an SMMU without PRI support had to provide a
    DeviceID even if it didn't support MSIs either. Support this, with
    the usual temporary flag definition while the real one is making
    its way through ACPICA.
    
    	Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    	Tested-by: Nicolin Chen <nicolinc@nvidia.com>
    	Reviewed-by: Nicolin Chen <nicolinc@nvidia.com>
    Link: https://lore.kernel.org/r/4b3e2ead4f392d1a47a7528da119d57918e5d806.1664392886.git.robin.murphy@arm.com
    	Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 05da178ce0aa152f0592e10fa874054187f3621b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/arm64/iort.c
+++ drivers/acpi/arm64/iort.c
@@ -410,6 +410,10 @@ static struct acpi_iort_node *iort_node_get_id(struct acpi_iort_node *node,
 	return NULL;
 }
 
+#ifndef ACPI_IORT_SMMU_V3_DEVICEID_VALID
+#define ACPI_IORT_SMMU_V3_DEVICEID_VALID (1 << 4)
+#endif
+
 static int iort_get_id_mapping_index(struct acpi_iort_node *node)
 {
 	struct acpi_iort_smmu_v3 *smmu;
@@ -426,12 +430,16 @@ static int iort_get_id_mapping_index(struct acpi_iort_node *node)
 
 		smmu = (struct acpi_iort_smmu_v3 *)node->node_data;
 		/*
-		 * ID mapping index is only ignored if all interrupts are
-		 * GSIV based
+		 * Until IORT E.e (node rev. 5), the ID mapping index was
+		 * defined to be valid unless all interrupts are GSIV-based.
 		 */
-		if (smmu->event_gsiv && smmu->pri_gsiv && smmu->gerr_gsiv
-		    && smmu->sync_gsiv)
+		if (node->revision < 5) {
+			if (smmu->event_gsiv && smmu->pri_gsiv &&
+			    smmu->gerr_gsiv && smmu->sync_gsiv)
+				return -EINVAL;
+		} else if (!(smmu->flags & ACPI_IORT_SMMU_V3_DEVICEID_VALID)) {
 			return -EINVAL;
+		}
 
 		if (smmu->id_mapping_index >= node->mapping_count) {
 			pr_err(FW_BUG "[node %p type %d] ID mapping index overflows valid mappings\n",
