ea968baab15d6a05cf9285110981387d87190ff2
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:32:44 -0400

    futex: Remove unneeded gotos
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-487.el8
    commit-author Jangwoong Kim <6812skiii@gmail.com>
    commit 0f9438503ea1312ef49be4d9762e0f0006546364
    
    Get rid of gotos that do not contain any cleanup. These were not removed in
    commit 9180bd467f9a ("futex: Remove put_futex_key()").
    
    	Signed-off-by: Jangwoong Kim <6812skiii@gmail.com>
    	Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20201230122953.10473-1-6812skiii@gmail.com
    
    (cherry picked from commit 0f9438503ea1312ef49be4d9762e0f0006546364)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/futex.c
+++ kernel/futex.c
@@ -3252,7 +3252,7 @@ retry:
 		 * Success, we're done! No tricky corner cases.
 		 */
 		if (!ret)
-			goto out_putkey;
+			return ret;
 		/*
 		 * The atomic access to the futex value generated a
 		 * pagefault, so retry the user-access and the wakeup:
@@ -3269,7 +3269,7 @@ retry:
 		 * wake_futex_pi has detected invalid state. Tell user
 		 * space.
 		 */
-		goto out_putkey;
+		return ret;
 	}
 
 	/*
@@ -3290,7 +3290,7 @@ retry:
 
 		default:
 			WARN_ON_ONCE(1);
-			goto out_putkey;
+			return ret;
 		}
 	}
 
@@ -3301,7 +3301,6 @@ retry:
 
 out_unlock:
 	spin_unlock(&hb->lock);
-out_putkey:
 	return ret;
 
 pi_retry:
