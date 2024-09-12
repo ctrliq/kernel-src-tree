317c1bb8ed93ac4dee525c3d23f6a4be174028f5
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:37:23 -0400

    iommu: Remove apply_resv_region
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-492.el8
    commit-author Lu Baolu <baolu.lu@linux.intel.com>
    commit 71fe30698dc3be6fd277c49c0c7b220c7ae92ffd
    
    The apply_resv_region callback in iommu_ops was introduced to reserve an
    IOVA range in the given DMA domain when the IOMMU driver manages the IOVA
    by itself. As all drivers converted to use dma-iommu in the core, there's
    no driver using this anymore. Remove it to avoid dead code.
    
    	Suggested-by: Robin Murphy <robin.murphy@arm.com>
    	Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    	Reviewed-by: Christoph Hellwig <hch@lst.de>
    	Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20220216025249.3459465-6-baolu.lu@linux.intel.com
    	Signed-off-by: Joerg Roedel <jroedel@suse.de>
    (cherry picked from commit 71fe30698dc3be6fd277c49c0c7b220c7ae92ffd)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -253,7 +253,6 @@ struct iommu_iotlb_gather {
  * @set_pgtable_quirks: Set io page table quirks (IO_PGTABLE_QUIRK_*)
  * @get_resv_regions: Request list of reserved regions for a device
  * @put_resv_regions: Free list of reserved regions for a device
- * @apply_resv_region: Temporary helper call-back for iova reserved ranges
  * @of_xlate: add OF master IDs to iommu grouping
  * @is_attach_deferred: Check if domain attach should be deferred from iommu
  *                      driver init to device driver init (default no)
@@ -318,9 +317,6 @@ struct iommu_ops {
 	/* Request/Free a list of reserved regions for a device */
 	void (*get_resv_regions)(struct device *dev, struct list_head *list);
 	void (*put_resv_regions)(struct device *dev, struct list_head *list);
-	void (*apply_resv_region)(struct device *dev,
-				  struct iommu_domain *domain,
-				  struct iommu_resv_region *region);
 
 	RH_KABI_BROKEN_REMOVE_BLOCK(
 	int (*domain_window_enable)(struct iommu_domain *domain, u32 wnd_nr,
