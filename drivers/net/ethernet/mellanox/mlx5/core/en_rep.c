a233ee06c6866e9adb3e8629dd93d0f0d97fef6b
Jonathan Maple <jmaple@ciq.com>
2024-09-12 17:05:04 -0400

    net/mlx5e: Support ethtool cq mode
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-425.3.1.el8
    commit-author Saeed Mahameed <saeedm@nvidia.com>
    commit bc541621f8ba18cc8aeed4c48b63aec181d6c20f
    
    Add support for ethtool coalesce cq mode set and get.
    
    	Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    	Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
    (cherry picked from commit bc541621f8ba18cc8aeed4c48b63aec181d6c20f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
@@ -264,7 +264,7 @@ static int mlx5e_rep_get_coalesce(struct net_device *netdev,
 {
 	struct mlx5e_priv *priv = netdev_priv(netdev);
 
-	return mlx5e_ethtool_get_coalesce(priv, coal);
+	return mlx5e_ethtool_get_coalesce(priv, coal, kernel_coal);
 }
 
 static int mlx5e_rep_set_coalesce(struct net_device *netdev,
@@ -274,7 +274,7 @@ static int mlx5e_rep_set_coalesce(struct net_device *netdev,
 {
 	struct mlx5e_priv *priv = netdev_priv(netdev);
 
-	return mlx5e_ethtool_set_coalesce(priv, coal);
+	return mlx5e_ethtool_set_coalesce(priv, coal, kernel_coal, extack);
 }
 
 static u32 mlx5e_rep_get_rxfh_key_size(struct net_device *netdev)
