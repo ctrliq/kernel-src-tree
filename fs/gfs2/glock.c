54b91747152e3e217415e87cf41aca0eb0af346f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:23:13 -0400

    gfs2: Make use of list_is_first
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-516.el8
    commit-author Andreas Gruenbacher <agruenba@redhat.com>
    commit 5a27a43efd1d5cd55dd5988cac596bdea7294d73
    
    Use list_is_first() instead of open-coding it.
    
    	Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit 5a27a43efd1d5cd55dd5988cac596bdea7294d73)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -482,7 +482,7 @@ static int do_promote(struct gfs2_glock *gl)
 			 * some reason. If this holder is the head of the list, it
 			 * means we have a blocked holder at the head, so return 1.
 			 */
-			if (gh->gh_list.prev == &gl->gl_holders)
+			if (list_is_first(&gh->gh_list, &gl->gl_holders))
 				return 1;
 			do_error(gl, 0);
 			break;
