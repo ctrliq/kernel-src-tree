641ace086e35a25063c37fd2f4a11c91c565dc1b
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:03:30 -0400

    x86/microcode/intel: Use appropriate type in microcode_sanity_check()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-502.el8
    commit-author Jithu Joseph <jithu.joseph@intel.com>
    commit 2e13ab0158dd4a033d61a5baa8f9b5b7c9ea8431
    
    The data type of the @print_err parameter used by microcode_sanity_check()
    is int. In preparation for exporting this function to be used by
    the IFS driver convert it to a more appropriate bool type for readability.
    
    No functional change intended.
    
    	Suggested-by: Tony Luck <tony.luck@intel.com>
    	Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    	Signed-off-by: Borislav Petkov <bp@suse.de>
    	Reviewed-by: Tony Luck <tony.luck@intel.com>
    	Reviewed-by: Ashok Raj <ashok.raj@intel.com>
    	Reviewed-by: Sohil Mehta <sohil.mehta@intel.com>
    Link: https://lore.kernel.org/r/20221117035935.4136738-7-jithu.joseph@intel.com
    (cherry picked from commit 2e13ab0158dd4a033d61a5baa8f9b5b7c9ea8431)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kernel/cpu/microcode/intel.c
+++ arch/x86/kernel/cpu/microcode/intel.c
@@ -177,7 +177,7 @@ static void save_microcode_patch(struct ucode_cpu_info *uci, void *data, unsigne
 		intel_ucode_patch = p->data;
 }
 
-static int microcode_sanity_check(void *mc, int print_err)
+static int microcode_sanity_check(void *mc, bool print_err)
 {
 	unsigned long total_size, data_size, ext_table_size;
 	struct microcode_header_intel *mc_header = mc;
@@ -295,7 +295,7 @@ scan_microcode(void *data, size_t size, struct ucode_cpu_info *uci, bool save)
 		mc_size = get_totalsize(mc_header);
 		if (!mc_size ||
 		    mc_size > size ||
-		    microcode_sanity_check(data, 0) < 0)
+		    microcode_sanity_check(data, false) < 0)
 			break;
 
 		size -= mc_size;
@@ -858,7 +858,7 @@ static enum ucode_state generic_load_microcode(int cpu, struct iov_iter *iter)
 		memcpy(mc, &mc_header, sizeof(mc_header));
 		data = mc + sizeof(mc_header);
 		if (!copy_from_iter_full(data, data_size, iter) ||
-		    microcode_sanity_check(mc, 1) < 0) {
+		    microcode_sanity_check(mc, true) < 0) {
 			break;
 		}
 
