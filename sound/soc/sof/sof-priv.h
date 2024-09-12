784a572df631e7bfae59c5bcae35f28e51c9d0a7
Jonathan Maple <jmaple@ciq.com>
2024-09-12 18:36:29 -0400

    ASoC: SOF: Remove struct sof_ops_table and sof_get_ops() macro
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-372.9.1.el8
    commit-author Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    commit 600e0ae9aa7175d777cbac16d0d3bbbebe63e2a5
    
    sof_get_ops() is not used and the struct sof_ops_table is only used by that
    macro.
    
    	Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    	Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    	Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    	Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20210920064156.4763-1-peter.ujfalusi@linux.intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 600e0ae9aa7175d777cbac16d0d3bbbebe63e2a5)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/sof/sof-priv.h
+++ sound/soc/sof/sof-priv.h
@@ -294,12 +294,6 @@ struct sof_arch_ops {
 
 #define sof_arch_ops(sdev) ((sdev)->pdata->desc->ops->arch_ops)
 
-/* DSP device HW descriptor mapping between bus ID and ops */
-struct sof_ops_table {
-	const struct sof_dev_desc *desc;
-	const struct snd_sof_dsp_ops *ops;
-};
-
 /* FS entry for debug files that can expose DSP memories, registers */
 struct snd_sof_dfsentry {
 	struct dentry *dfsentry;
