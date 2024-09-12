ac59aa5e877baf00cc537d1a31b31418a8d716e3
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:20:15 -0400

    wifi: rtw89: coex: Enable Wi-Fi RX gain control for free run solution
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-512.el8
    commit-author Ching-Te Ku <ku920601@realtek.com>
    commit 36ef71db559f6a0dc2d7a9af8edb16c387d18ce2
    
    When Wi-Fi & Bluetooth are both busy at the same time, Wi-Fi need to
    enable RX gain to protect Wi-Fi RX RF ability. Without this configure
    the interference from Bluetooth will bring a big impact to Wi-Fi RX.
    
    	Signed-off-by: Ching-Te Ku <ku920601@realtek.com>
    	Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    	Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20230412012831.10519-2-pkshih@realtek.com
    (cherry picked from commit 36ef71db559f6a0dc2d7a9af8edb16c387d18ce2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/wireless/realtek/rtw89/rtw8852c.c
+++ drivers/net/wireless/realtek/rtw89/rtw8852c.c
@@ -2537,7 +2537,7 @@ static const struct rtw89_btc_rf_trx_para rtw89_btc_8852c_rf_ul[] = {
 	{255, 0, 0, 7}, /* 2 ->reserved for shared-antenna */
 	{255, 0, 0, 7}, /* 3- >reserved for shared-antenna */
 	{255, 0, 0, 7}, /* 4 ->reserved for shared-antenna */
-	{255, 0, 0, 7}, /* the below id is for non-shared-antenna free-run */
+	{255, 1, 0, 7}, /* the below id is for non-shared-antenna free-run */
 	{6, 1, 0, 7},
 	{13, 1, 0, 7},
 	{13, 1, 0, 7}
@@ -2549,7 +2549,7 @@ static const struct rtw89_btc_rf_trx_para rtw89_btc_8852c_rf_dl[] = {
 	{255, 0, 0, 7}, /* 2 ->reserved for shared-antenna */
 	{255, 0, 0, 7}, /* 3- >reserved for shared-antenna */
 	{255, 0, 0, 7}, /* 4 ->reserved for shared-antenna */
-	{255, 0, 0, 7}, /* the below id is for non-shared-antenna free-run */
+	{255, 1, 0, 7}, /* the below id is for non-shared-antenna free-run */
 	{255, 1, 0, 7},
 	{255, 1, 0, 7},
 	{255, 1, 0, 7}
