ea912caa97119e13d58f25925ce7715fa261148c
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:56:14 -0400

    selftests/vm: skip 128TBswitch on unsupported arch
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-542.el8
    commit-author Adam Sindelar <adam@wowsignal.io>
    commit ac3ced5fc12fb3d7268054485cbc36441c05cf24
    
    The test va_128TBswitch.c exercises a feature only supported on PPC and
    x86_64, but it's run on other 64-bit archs as well.  Before this patch,
    the test did nothing and returned 0 for KSFT_PASS.  This patch makes it
    return the KSFT codes from kselftest.h, including KSFT_SKIP when
    appropriate.
    
    Verified on arm64 and x86_64.
    
    Link: https://lkml.kernel.org/r/20220704123813.427625-1-adam@wowsignal.io
    	Signed-off-by: Adam Sindelar <adam@wowsignal.io>
    	Cc: David Vernet <void@manifault.com>
    	Cc: Shuah Khan <shuah@kernel.org>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit ac3ced5fc12fb3d7268054485cbc36441c05cf24)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/testing/selftests/vm/va_128TBswitch.c
+++ tools/testing/selftests/vm/va_128TBswitch.c
@@ -239,7 +239,7 @@ static struct testcase hugetlb_testcases[] = {
 static int run_test(struct testcase *test, int count)
 {
 	void *p;
-	int i, ret = 0;
+	int i, ret = KSFT_PASS;
 
 	for (i = 0; i < count; i++) {
 		struct testcase *t = test + i;
@@ -250,13 +250,13 @@ static int run_test(struct testcase *test, int count)
 
 		if (p == MAP_FAILED) {
 			printf("FAILED\n");
-			ret = 1;
+			ret = KSFT_FAIL;
 			continue;
 		}
 
 		if (t->low_addr_required && p >= (void *)(ADDR_SWITCH_HINT)) {
 			printf("FAILED\n");
-			ret = 1;
+			ret = KSFT_FAIL;
 		} else {
 			/*
 			 * Do a dereference of the address returned so that we catch
@@ -288,7 +288,7 @@ int main(int argc, char **argv)
 	int ret;
 
 	if (!supported_arch())
-		return 0;
+		return KSFT_SKIP;
 
 	ret = run_test(testcases, ARRAY_SIZE(testcases));
 	if (argc == 2 && !strcmp(argv[1], "--run-hugetlb"))
