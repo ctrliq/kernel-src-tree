e7167c9820f4f8253f7cc3e5eee0d6ddcdea4e99
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:07:32 -0400

    regmap: mmio: Remove mmio_relaxed member from context
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-504.el8
    commit-author Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    commit ada79bca380009a85d1e643e5a4da0c079f28225
    
    There is no need to keep mmio_relaxed member in the context, it's
    onetime used during generation of the context. Remove it.
    
    	Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    	Acked-by: William Breathitt Gray <william.gray@linaro.org>
    Link: https://lore.kernel.org/r/20220808203401.35153-2-andriy.shevchenko@linux.intel.com
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit ada79bca380009a85d1e643e5a4da0c079f28225)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/base/regmap/regmap-mmio.c
+++ drivers/base/regmap/regmap-mmio.c
@@ -28,7 +28,6 @@
 struct regmap_mmio_context {
 	void __iomem *regs;
 	unsigned int val_bytes;
-	bool relaxed_mmio;
 
 	bool attached_clk;
 	struct clk *clk;
@@ -302,7 +301,6 @@ static struct regmap_mmio_context *regmap_mmio_gen_context(struct device *dev,
 
 	ctx->regs = regs;
 	ctx->val_bytes = config->val_bits / 8;
-	ctx->relaxed_mmio = config->use_relaxed_mmio;
 	ctx->clk = ERR_PTR(-ENODEV);
 
 	switch (regmap_get_val_endian(dev, &regmap_mmio, config)) {
@@ -313,7 +311,7 @@ static struct regmap_mmio_context *regmap_mmio_gen_context(struct device *dev,
 #endif
 		switch (config->val_bits) {
 		case 8:
-			if (ctx->relaxed_mmio) {
+			if (config->use_relaxed_mmio) {
 				ctx->reg_read = regmap_mmio_read8_relaxed;
 				ctx->reg_write = regmap_mmio_write8_relaxed;
 			} else {
@@ -322,7 +320,7 @@ static struct regmap_mmio_context *regmap_mmio_gen_context(struct device *dev,
 			}
 			break;
 		case 16:
-			if (ctx->relaxed_mmio) {
+			if (config->use_relaxed_mmio) {
 				ctx->reg_read = regmap_mmio_read16le_relaxed;
 				ctx->reg_write = regmap_mmio_write16le_relaxed;
 			} else {
@@ -331,7 +329,7 @@ static struct regmap_mmio_context *regmap_mmio_gen_context(struct device *dev,
 			}
 			break;
 		case 32:
-			if (ctx->relaxed_mmio) {
+			if (config->use_relaxed_mmio) {
 				ctx->reg_read = regmap_mmio_read32le_relaxed;
 				ctx->reg_write = regmap_mmio_write32le_relaxed;
 			} else {
@@ -341,7 +339,7 @@ static struct regmap_mmio_context *regmap_mmio_gen_context(struct device *dev,
 			break;
 #ifdef CONFIG_64BIT
 		case 64:
-			if (ctx->relaxed_mmio) {
+			if (config->use_relaxed_mmio) {
 				ctx->reg_read = regmap_mmio_read64le_relaxed;
 				ctx->reg_write = regmap_mmio_write64le_relaxed;
 			} else {
