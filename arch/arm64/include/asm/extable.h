e25d556ba909de5cfd24083b886faab7168e75be
Jonathan Maple <jmaple@ciq.com>
2024-09-12 15:58:29 -0400

    arm64: extable: make fixup_exception() return bool
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-477.10.1.el8_8
    commit-author Mark Rutland <mark.rutland@arm.com>
    commit e8c328d7de03bf4d7a18e38ff87a7c12fdf8afb1
    
    The return values of fixup_exception() and arm64_bpf_fixup_exception()
    represent a boolean condition rather than an error code, so for clarity
    it would be better to return `bool` rather than `int`.
    
    This patch adjusts the code accordingly. While we're modifying the
    prototype, we also remove the unnecessary `extern` keyword, so that this
    won't look out of place when we make subsequent additions to the header.
    
    There should be no functional change as a result of this patch.
    
    	Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    	Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    	Cc: Alexei Starovoitov <ast@kernel.org>
    	Cc: Andrii Nakryiko <andrii@kernel.org>
    	Cc: Catalin Marinas <catalin.marinas@arm.com>
    	Cc: Daniel Borkmann <daniel@iogearbox.net>
    	Cc: James Morse <james.morse@arm.com>
    	Cc: Jean-Philippe Brucker <jean-philippe@linaro.org>
    	Cc: Robin Murphy <robin.murphy@arm.com>
    	Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20211019160219.5202-9-mark.rutland@arm.com
    	Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit e8c328d7de03bf4d7a18e38ff87a7c12fdf8afb1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/arm64/include/asm/extable.h
+++ arch/arm64/include/asm/extable.h
@@ -23,16 +23,16 @@ struct exception_table_entry
 #define ARCH_HAS_RELATIVE_EXTABLE
 
 #ifdef CONFIG_BPF_JIT
-int arm64_bpf_fixup_exception(const struct exception_table_entry *ex,
+bool arm64_bpf_fixup_exception(const struct exception_table_entry *ex,
 			      struct pt_regs *regs);
 #else /* !CONFIG_BPF_JIT */
 static inline
-int arm64_bpf_fixup_exception(const struct exception_table_entry *ex,
-			      struct pt_regs *regs)
+bool arm64_bpf_fixup_exception(const struct exception_table_entry *ex,
+			       struct pt_regs *regs)
 {
-	return 0;
+	return false;
 }
 #endif /* !CONFIG_BPF_JIT */
 
-extern int fixup_exception(struct pt_regs *regs);
+bool fixup_exception(struct pt_regs *regs);
 #endif
