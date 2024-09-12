a8695e0f9ff09efbcce4042e2d2618473107eb3f
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:06:36 -0400

    media: uvcvideo: Fix missing newline after declarations
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-504.el8
    commit-author Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
    commit 2a8c1952ed4bc85f64174def8c00c71c264dc3a2
    
    Fixes 'Missing a blank line after declarations' warning issued by
    scripts/checkpatch.pl on drivers/media/usb/uvc/uvc_driver.c
    
    	Signed-off-by: Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
    	Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    	Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    (cherry picked from commit 2a8c1952ed4bc85f64174def8c00c71c264dc3a2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -694,6 +694,7 @@ static int uvc_parse_streaming(struct uvc_device *dev,
 	/* Parse the alternate settings to find the maximum bandwidth. */
 	for (i = 0; i < intf->num_altsetting; ++i) {
 		struct usb_host_endpoint *ep;
+
 		alts = &intf->altsetting[i];
 		ep = uvc_find_endpoint(alts,
 				streaming->header.bEndpointAddress);
@@ -1810,12 +1811,14 @@ static void uvc_delete(struct kref *kref)
 
 	list_for_each_safe(p, n, &dev->chains) {
 		struct uvc_video_chain *chain;
+
 		chain = list_entry(p, struct uvc_video_chain, list);
 		kfree(chain);
 	}
 
 	list_for_each_safe(p, n, &dev->entities) {
 		struct uvc_entity *entity;
+
 		entity = list_entry(p, struct uvc_entity, list);
 #ifdef CONFIG_MEDIA_CONTROLLER
 		uvc_mc_cleanup_entity(entity);
@@ -1825,6 +1828,7 @@ static void uvc_delete(struct kref *kref)
 
 	list_for_each_safe(p, n, &dev->streams) {
 		struct uvc_streaming *streaming;
+
 		streaming = list_entry(p, struct uvc_streaming, list);
 		usb_driver_release_interface(&uvc_driver.driver,
 			streaming->intf);
