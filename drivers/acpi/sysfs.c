856248e9d0a50ed8d0d5705efdb0d24e0cb971db
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:36:39 -0400

    ACPI: sysfs: Use kstrtobool() instead of strtobool()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-529.el8
    commit-author Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    commit 4612c7f1387df2f9f20a28b2a271e6cb154eb2b9
    
    strtobool() is the same as kstrtobool().
    However, the latter is more used within the kernel.
    
    In order to remove strtobool() and slightly simplify kstrtox.h, switch to
    the other function name.
    
    While at it, include the corresponding header file (<linux/kstrtox.h>)
    
    	Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    	Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    	Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 4612c7f1387df2f9f20a28b2a271e6cb154eb2b9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/sysfs.c
+++ drivers/acpi/sysfs.c
@@ -9,6 +9,7 @@
 #include <linux/bitmap.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/kstrtox.h>
 #include <linux/moduleparam.h>
 
 #include "internal.h"
@@ -999,7 +1000,7 @@ static ssize_t force_remove_store(struct kobject *kobj,
 	bool val;
 	int ret;
 
-	ret = strtobool(buf, &val);
+	ret = kstrtobool(buf, &val);
 	if (ret < 0)
 		return ret;
 
