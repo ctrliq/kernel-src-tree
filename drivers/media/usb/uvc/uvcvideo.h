fb6c0b35bfdedd485e663cb3064ca8bc1ebea5dc
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:06:42 -0400

    media: uvcvideo: Undup use uvc_endpoint_max_bpi() code
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-504.el8
    commit-author Ricardo Ribalda <ribalda@chromium.org>
    commit 5b9c75c794ce041e6e00789efef75d71915c4f4c
    
    Replace manual decoding of psize in uvc_parse_streaming(), with the code
    from uvc_endpoint_max_bpi(). It also handles usb3 devices.
    
    	Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    	Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    	Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    (cherry picked from commit 5b9c75c794ce041e6e00789efef75d71915c4f4c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/media/usb/uvc/uvcvideo.h
+++ drivers/media/usb/uvc/uvcvideo.h
@@ -784,6 +784,7 @@ int uvc_xu_ctrl_query(struct uvc_video_chain *chain,
 /* Utility functions */
 struct usb_host_endpoint *uvc_find_endpoint(struct usb_host_interface *alts,
 					    u8 epaddr);
+u16 uvc_endpoint_max_bpi(struct usb_device *dev, struct usb_host_endpoint *ep);
 
 /* Quirks support */
 void uvc_video_decode_isight(struct uvc_urb *uvc_urb,
