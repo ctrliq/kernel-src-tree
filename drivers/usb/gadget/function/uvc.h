8b0b8a62077957f759e106ad37dd5912539d02a3
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:07:10 -0400

    usb: gadget: uvc: fix multiple opens
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-504.el8
    commit-author Thomas Haemmerle <thomas.haemmerle@wolfvision.net>
    commit 72ee48ee8925446eaeda8e4ef3f2eb16b4a93d2a
    
    Currently, the UVC function is activated when open on the corresponding
    v4l2 device is called.  On another open the activation of the function
    fails since the deactivation counter in `usb_function_activate` equals
    0. However the error is not returned to userspace since the open of the
    v4l2 device is successful.
    
    On a close the function is deactivated (since deactivation counter still
    equals 0) and the video is disabled in `uvc_v4l2_release`, although the
    UVC application potentially is streaming.
    
    Move activation of UVC function to subscription on UVC_EVENT_SETUP
    because there we can guarantee for a userspace application utilizing
    UVC.  Block subscription on UVC_EVENT_SETUP while another application
    already is subscribed to it, indicated by `bool func_connected` in
    `struct uvc_device`.  Extend the `struct uvc_file_handle` with member
    `bool is_uvc_app_handle` to tag it as the handle used by the userspace
    UVC application.
    
    With this a process is able to check capabilities of the v4l2 device
    without deactivating the function for the actual UVC application.
    
    Reviewed-By: Michael Tretter <m.tretter@pengutronix.de>
    	Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    	Signed-off-by: Thomas Haemmerle <thomas.haemmerle@wolfvision.net>
    	Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>
    	Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    	Acked-by: Felipe Balbi <balbi@kernel.org>
    Link: https://lore.kernel.org/r/20211003201355.24081-1-m.grzeschik@pengutronix.de
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 72ee48ee8925446eaeda8e4ef3f2eb16b4a93d2a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/usb/gadget/function/uvc.h
+++ drivers/usb/gadget/function/uvc.h
@@ -115,6 +115,7 @@ struct uvc_device {
 	enum uvc_state state;
 	struct usb_function func;
 	struct uvc_video video;
+	bool func_connected;
 
 	/* Descriptors */
 	struct {
@@ -145,6 +146,7 @@ static inline struct uvc_device *to_uvc(struct usb_function *f)
 struct uvc_file_handle {
 	struct v4l2_fh vfh;
 	struct uvc_video *device;
+	bool is_uvc_app_handle;
 };
 
 #define to_uvc_file_handle(handle) \
