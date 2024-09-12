ec5d913dbcbafb405d1cfbf259e65835c1012da6
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:50:08 -0400

    firewire: add kernel API to access CYCLE_TIME register
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-499.el8
    commit-author Takashi Sakamoto <o-takashi@sakamocchi.jp>
    commit baa914cd81f51f4e4f3bae5bb59764b32ad8c353
    
    1394 OHCI specification defined Isochronous Cycle Timer Register to get
    value of CYCLE_TIME register defined by IEEE 1394 for CSR architecture
    defined by ISO/IEC 13213. Unit driver can calculate packet time by
    compute with the value of CYCLE_TIME and timeStamp field in descriptor
    of each isochronous and asynchronous context. The resolution of CYCLE_TIME
    is 49.576 MHz, while the one of timeStamp is 8,000 Hz.
    
    Current implementation of Linux FireWire subsystem allows the driver to
    get the value of CYCLE_TIMER CSR register by transaction service. The
    transaction service has overhead in regard of access to MMIO register.
    
    This commit adds kernel API for unit driver to access the register
    directly.
    
    	Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Link: https://lore.kernel.org/r/20220405072221.226217-3-o-takashi@sakamocchi.jp
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit baa914cd81f51f4e4f3bae5bb59764b32ad8c353)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/firewire/core-cdev.c
+++ drivers/firewire/core-cdev.c
@@ -1231,7 +1231,9 @@ static int ioctl_get_cycle_timer2(struct client *client, union ioctl_arg *arg)
 
 	local_irq_disable();
 
-	cycle_time = card->driver->read_csr(card, CSR_CYCLE_TIME);
+	ret = fw_card_read_cycle_time(card, &cycle_time);
+	if (ret < 0)
+		goto end;
 
 	switch (a->clk_id) {
 	case CLOCK_REALTIME:      getnstimeofday(&ts);	break;
@@ -1240,7 +1242,7 @@ static int ioctl_get_cycle_timer2(struct client *client, union ioctl_arg *arg)
 	default:
 		ret = -EINVAL;
 	}
-
+end:
 	local_irq_enable();
 
 	a->tv_sec      = ts.tv_sec;
