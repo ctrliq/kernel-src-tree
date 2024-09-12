c8efef2cc004989f218b7d3ea2a026af7fe72757
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:35:46 -0400

    scsi: qla2xxx: Make qla_trim_buf() and __qla_adjust_buf() static
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-490.el8
    commit-author Tom Rix <trix@redhat.com>
    commit 54c51253b3d5544943fc3ec072cd0194915bac30
    
    Smatch reports:
    
    drivers/scsi/qla2xxx/qla_mid.c:1189:6: warning: symbol 'qla_trim_buf' was not declared. Should it be static?
    drivers/scsi/qla2xxx/qla_mid.c:1221:6: warning: symbol '__qla_adjust_buf' was not declared. Should it be static?
    
    These functions are only used in qla_mid.c, so they should be static.
    
    Fixes: 1f8f9c34127e ("scsi: qla2xxx: edif: Reduce memory usage during low I/O")
    	Signed-off-by: Tom Rix <trix@redhat.com>
    Link: https://lore.kernel.org/r/20230114013724.3943580-1-trix@redhat.com
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 54c51253b3d5544943fc3ec072cd0194915bac30)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/qla2xxx/qla_mid.c
+++ drivers/scsi/qla2xxx/qla_mid.c
@@ -1196,7 +1196,7 @@ again:
 	return 0;
 }
 
-void qla_trim_buf(struct qla_qpair *qp, u16 trim)
+static void qla_trim_buf(struct qla_qpair *qp, u16 trim)
 {
 	int i, j;
 	struct qla_hw_data *ha = qp->vha->hw;
@@ -1228,7 +1228,7 @@ void qla_trim_buf(struct qla_qpair *qp, u16 trim)
 	       qp->id, trim, qp->buf_pool.num_alloc);
 }
 
-void __qla_adjust_buf(struct qla_qpair *qp)
+static void __qla_adjust_buf(struct qla_qpair *qp)
 {
 	u32 trim;
 
