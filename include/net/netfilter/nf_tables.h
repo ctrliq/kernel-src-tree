03620584a15be9bab0b413bf85f44316f1a9c605
Jonathan Maple <jmaple@ciq.com>
2024-09-12 13:28:19 -0400

    netfilter: nf_tables: restrict tunnel object to NFPROTO_NETDEV
    jira LE-1907
    Rebuild_History Non-Buildable kernel-5.14.0-427.33.1.el9_4
    commit-author Pablo Neira Ayuso <pablo@netfilter.org>
    commit 776d451648443f9884be4a1b4e38e8faf1c621f9
    
    Bail out on using the tunnel dst template from other than netdev family.
    Add the infrastructure to check for the family in objects.
    
    Fixes: af308b94a2a4 ("netfilter: nf_tables: add tunnel support")
    	Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (cherry picked from commit 776d451648443f9884be4a1b4e38e8faf1c621f9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -1265,6 +1265,7 @@ void nft_obj_notify(struct net *net, const struct nft_table *table,
  *	@type: stateful object numeric type
  *	@owner: module owner
  *	@maxattr: maximum netlink attribute
+ *	@family: address family for AF-specific object types
  *	@policy: netlink attribute policy
  */
 struct nft_object_type {
@@ -1274,6 +1275,7 @@ struct nft_object_type {
 	struct list_head		list;
 	u32				type;
 	unsigned int                    maxattr;
+	u8				family;
 	struct module			*owner;
 	const struct nla_policy		*policy;
 };
