e92ba10f71cf01443c70c4f43c83918642ec6d29
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:04:34 -0400

    ASoC: simple-card-utils: Make asoc_simple_clean_reference() return void
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-448.el8
    commit-author Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    commit e6f08af6340eaf88e9eeff71bd4533eee9a04119
    
    asoc_simple_clean_reference() returns zero unconditionally. Letting it
    return void instead makes it easier to see in the caller that there is no
    error to handle.
    
    This is a preparation for making platform remove callbacks return void.
    
    	Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Link: https://lore.kernel.org/r/20220605153537.26591-1-u.kleine-koenig@pengutronix.de
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit e6f08af6340eaf88e9eeff71bd4533eee9a04119)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/generic/simple-card-utils.c
+++ sound/soc/generic/simple-card-utils.c
@@ -614,7 +614,7 @@ void asoc_simple_canonicalize_cpu(struct snd_soc_dai_link_component *cpus,
 }
 EXPORT_SYMBOL_GPL(asoc_simple_canonicalize_cpu);
 
-int asoc_simple_clean_reference(struct snd_soc_card *card)
+void asoc_simple_clean_reference(struct snd_soc_card *card)
 {
 	struct snd_soc_dai_link *dai_link;
 	struct snd_soc_dai_link_component *cpu;
@@ -627,7 +627,6 @@ int asoc_simple_clean_reference(struct snd_soc_card *card)
 		for_each_link_codecs(dai_link, j, codec)
 			of_node_put(codec->of_node);
 	}
-	return 0;
 }
 EXPORT_SYMBOL_GPL(asoc_simple_clean_reference);
 
@@ -867,7 +866,9 @@ int asoc_simple_remove(struct platform_device *pdev)
 {
 	struct snd_soc_card *card = platform_get_drvdata(pdev);
 
-	return asoc_simple_clean_reference(card);
+	asoc_simple_clean_reference(card);
+
+	return 0;
 }
 EXPORT_SYMBOL_GPL(asoc_simple_remove);
 
