8a5d2795c67d4713f91f1faf7f61e2cba60faa86
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:49:56 -0400

    swiotlb: move slot allocation explanation comment where it belongs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-537.el8
    commit-author Petr Tesarik <petr.tesarik.ext@huawei.com>
    commit f94cb36e760d2a4d359ad64f5fafc62ca755fd72
    
    Move the comment down in front of the loop that actually sets the list
    member of struct io_tlb_slot to zero.
    
    Fixes: 26a7e094783d ("swiotlb: refactor swiotlb_tbl_map_single")
    	Signed-off-by: Petr Tesarik <petr.tesarik.ext@huawei.com>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit f94cb36e760d2a4d359ad64f5fafc62ca755fd72)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/dma/swiotlb.c
+++ kernel/dma/swiotlb.c
@@ -617,11 +617,6 @@ static int swiotlb_do_find_slots(struct io_tlb_mem *mem,
 			continue;
 		}
 
-		/*
-		 * If we find a slot that indicates we have 'nslots' number of
-		 * contiguous buffers, we allocate the buffers from that slot
-		 * and mark the entries as '0' indicating unavailable.
-		 */
 		if (!iommu_is_span_boundary(slot_index, nslots,
 					    nr_slots(tbl_dma_addr),
 					    max_slots)) {
@@ -637,6 +632,11 @@ not_found:
 	return -1;
 
 found:
+	/*
+	 * If we find a slot that indicates we have 'nslots' number of
+	 * contiguous buffers, we allocate the buffers from that slot onwards
+	 * and set the list of free entries to '0' indicating unavailable.
+	 */
 	for (i = slot_index; i < slot_index + nslots; i++) {
 		mem->slots[i].list = 0;
 		mem->slots[i].alloc_size = alloc_size - (offset +
