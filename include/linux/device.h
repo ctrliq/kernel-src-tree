f08c31fc040c3088f5fe04c96cf98e0e11a86b40
Jonathan Maple <jmaple@ciq.com>
2024-09-11 20:18:22 -0400

    driver core: Add missing kernel doc for device::msi_lock
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-481.el8
    commit-author Thomas Gleixner <tglx@linutronix.de>
    commit 7a3dc4f35bf8e1a07e5c3f8ecc8ac923f48493fe
    
    Fixes: 77e89afc25f3 ("PCI/MSI: Protect msi_desc::masked for multi-MSI")
    	Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    	Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    (cherry picked from commit 7a3dc4f35bf8e1a07e5c3f8ecc8ac923f48493fe)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/device.h
+++ include/linux/device.h
@@ -1154,6 +1154,7 @@ struct device_extended_rh {
  * 		along with subsystem-level and driver-level callbacks.
  * @pins:	For device pin management.
  *		See Documentation/driver-api/pin-control.rst for details.
+ * @msi_lock:	Lock to protect MSI mask cache and mask register
  * @msi_list:	Hosts MSI descriptors
  * @msi_domain: The generic MSI domain this device is using.
  * @numa_node:	NUMA node this device is close to.
