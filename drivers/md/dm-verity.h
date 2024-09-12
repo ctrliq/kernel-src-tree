bb20735fe647370c3f1619d5148768c087e2539d
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:59:19 -0400

    dm-verity: recheck the hash after a failure
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-546.el8
    commit-author Mikulas Patocka <mpatocka@redhat.com>
    commit 9177f3c0dea6143d05cac1bbd28668fd0e216d11
    
    If a userspace process reads (with O_DIRECT) multiple blocks into the same
    buffer, dm-verity reports an error [1].
    
    This commit fixes dm-verity, so that if hash verification fails, the data
    is read again into a kernel buffer (where userspace can't modify it) and
    the hash is rechecked. If the recheck succeeds, the content of the kernel
    buffer is copied into the user buffer; if the recheck fails, an error is
    reported.
    
    [1] https://people.redhat.com/~mpatocka/testcases/blk-auth-modify/read2.c
    
    	Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    	Cc: stable@vger.kernel.org
    	Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    (cherry picked from commit 9177f3c0dea6143d05cac1bbd28668fd0e216d11)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/md/dm-verity.h
+++ drivers/md/dm-verity.h
@@ -12,6 +12,7 @@
 #ifndef DM_VERITY_H
 #define DM_VERITY_H
 
+#include <linux/dm-io.h>
 #include <linux/dm-bufio.h>
 #include <linux/device-mapper.h>
 #include <linux/interrupt.h>
@@ -69,6 +70,9 @@ struct dm_verity {
 	unsigned long *validated_blocks; /* bitset blocks validated */
 
 	char *signature_key_desc; /* signature keyring reference */
+
+	struct dm_io_client *io;
+	mempool_t recheck_pool;
 };
 
 struct dm_verity_io {
@@ -84,6 +88,8 @@ struct dm_verity_io {
 	struct bvec_iter iter;
 
 	struct work_struct work;
+
+	char *recheck_buffer;
 
 	/*
 	 * Three variably-size fields follow this struct:
