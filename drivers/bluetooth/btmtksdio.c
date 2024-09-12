d6641c35cc6d932243a4312298b06b9409baaf32
Jonathan Maple <jmaple@ciq.com>
2024-09-11 21:14:19 -0400

    Bluetooth: btmtksdio: fix the reset takes too long
    jira LE-1907
    Rebuild_History Non-Buildable kernel-4.18.0-509.el8
    commit-author Sean Wang <sean.wang@mediatek.com>
    commit baabb7f530e8a3f0085d12f4ea0bada4115515d3
    
    Sending WMT command during the reset in progress is invalid and would get
    no response from firmware until the reset is complete, so we ignore the WMT
    command here to resolve the issue which causes the whole reset process
    taking too long.
    
    Fixes: 8fafe702253d ("Bluetooth: mt7921s: support bluetooth reset mechanism")
    Co-developed-by: Yake Yang <yake.yang@mediatek.com>
    	Signed-off-by: Yake Yang <yake.yang@mediatek.com>
    	Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    	Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit baabb7f530e8a3f0085d12f4ea0bada4115515d3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/bluetooth/btmtksdio.c
+++ drivers/bluetooth/btmtksdio.c
@@ -932,6 +932,10 @@ static int btmtksdio_shutdown(struct hci_dev *hdev)
 	 */
 	pm_runtime_get_sync(bdev->dev);
 
+	/* wmt command only works until the reset is complete */
+	if (test_bit(BTMTKSDIO_HW_RESET_ACTIVE, &bdev->tx_state))
+		goto ignore_wmt_cmd;
+
 	/* Disable the device */
 	wmt_params.op = MTK_WMT_FUNC_CTRL;
 	wmt_params.flag = 0;
@@ -945,6 +949,7 @@ static int btmtksdio_shutdown(struct hci_dev *hdev)
 		return err;
 	}
 
+ignore_wmt_cmd:
 	pm_runtime_put_noidle(bdev->dev);
 	pm_runtime_disable(bdev->dev);
 
