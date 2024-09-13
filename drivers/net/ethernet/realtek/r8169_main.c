9683fd692fe1c50e0cd18ae4b69af6e3552e8e28
Jonathan Maple <jmaple@ciq.com>
2024-09-13 13:37:39 -0400

    r8169: Fix fall-through warning for Clang
    jira LE-1907
    Rebuild_History Non-Buildable kernel-rt-4.18.0-372.9.1.rt7.166.el8
    commit-author Gustavo A. R. Silva <gustavoars@kernel.org>
    commit ffb35c679842b471d8bc42c6986daa3b373f57b2
    
    In preparation to enable -Wimplicit-fallthrough for Clang, fix a warning
    by explicitly adding a break statement instead of letting the code fall
    through to the next case.
    
    Link: https://github.com/KSPP/linux/issues/115
    	Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    	Acked-by: Heiner Kallweit <hkallweit1@gmail.com>
    Link: https://lore.kernel.org/r/20210528202327.GA39994@embeddedor
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit ffb35c679842b471d8bc42c6986daa3b373f57b2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -4117,6 +4117,7 @@ static unsigned int rtl_quirk_packet_padto(struct rtl8169_private *tp,
 	case RTL_GIGA_MAC_VER_61:
 	case RTL_GIGA_MAC_VER_63:
 		padto = max_t(unsigned int, padto, ETH_ZLEN);
+		break;
 	default:
 		break;
 	}
