e2fe8284ded1dd8263a7133e215a877219471a43
Jonathan Maple <jmaple@ciq.com>
2024-09-13 12:26:13 -0400

    perf: replace bitmap_weight with bitmap_empty where appropriate
    jira LE-1907
    Rebuild_History Non-Buildable kernel-rt-4.18.0-477.10.1.rt7.274.el8_8
    commit-author Yury Norov <yury.norov@gmail.com>
    commit 95ed57c73bbcffa02cbb1d61c19484c2ec6de094
    
    In some places, drivers/perf code calls bitmap_weight() to check if any
    bit of a given bitmap is set. It's better to use bitmap_empty() in that
    case because bitmap_empty() stops traversing the bitmap as soon as it
    finds first set bit, while bitmap_weight() counts all bits unconditionally.
    
    	Signed-off-by: Yury Norov <yury.norov@gmail.com>
    	Acked-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20220210224933.379149-13-yury.norov@gmail.com
    	Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 95ed57c73bbcffa02cbb1d61c19484c2ec6de094)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/perf/hisilicon/hisi_uncore_pmu.c
+++ drivers/perf/hisilicon/hisi_uncore_pmu.c
@@ -323,7 +323,7 @@ void hisi_uncore_pmu_read(struct perf_event *event)
 void hisi_uncore_pmu_enable(struct pmu *pmu)
 {
 	struct hisi_pmu *hisi_pmu = to_hisi_pmu(pmu);
-	int enabled = bitmap_weight(hisi_pmu->pmu_events.used_mask,
+	bool enabled = !bitmap_empty(hisi_pmu->pmu_events.used_mask,
 				    hisi_pmu->num_counters);
 
 	if (!enabled)
