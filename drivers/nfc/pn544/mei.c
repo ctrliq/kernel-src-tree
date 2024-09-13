5498957a67555c1011eb90c2eb88a2d2503814aa
Jonathan Maple <jmaple@ciq.com>
2024-09-13 12:17:22 -0400

    mei: bus: change remove callback to return void
    jira LE-1907
    Rebuild_History Non-Buildable kernel-rt-4.18.0-477.10.1.rt7.274.el8_8
    commit-author Uwe Kleine-König <uwe@kleine-koenig.org>
    commit bf5c9cc8ad7fffd1f72df3baa5870449e4c16d1b
    
    The driver core ignores the return value of mei_cl_device_remove() so
    passing an error value doesn't solve any problem. As most mei drivers'
    remove callbacks return 0 unconditionally and returning a different value
    doesn't have any effect, change this prototype to return void and return 0
    unconditionally in mei_cl_device_remove(). The only driver that could
    return an error value is modified to emit an explicit warning in the error
    case.
    
    	Acked-by: Guenter Roeck <linux@roeck-us.net>
    	Signed-off-by: Uwe Kleine-König <uwe@kleine-koenig.org>
    Link: https://lore.kernel.org/r/20210208073705.428185-3-uwe@kleine-koenig.org
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit bf5c9cc8ad7fffd1f72df3baa5870449e4c16d1b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nfc/pn544/mei.c
+++ drivers/nfc/pn544/mei.c
@@ -53,7 +53,7 @@ static int pn544_mei_probe(struct mei_cl_device *cldev,
 	return 0;
 }
 
-static int pn544_mei_remove(struct mei_cl_device *cldev)
+static void pn544_mei_remove(struct mei_cl_device *cldev)
 {
 	struct nfc_mei_phy *phy = mei_cldev_get_drvdata(cldev);
 
@@ -62,8 +62,6 @@ static int pn544_mei_remove(struct mei_cl_device *cldev)
 	pn544_hci_remove(phy->hdev);
 
 	nfc_mei_phy_free(phy);
-
-	return 0;
 }
 
 static struct mei_cl_device_id pn544_mei_tbl[] = {
