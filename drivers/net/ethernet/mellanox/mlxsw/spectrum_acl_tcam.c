593079cfdc320b087c557e6dcbf059e78349aa37
Jonathan Maple <jmaple@ciq.com>
2024-09-13 11:55:09 -0400

    mlxsw: spectrum_acl_tcam: Fix incorrect list API usage
    jira LE-1907
    cve CVE-2024-36006
    Rebuild_History Non-Buildable kernel-rt-4.18.0-553.16.1.rt7.357.el8_10
    commit-author Ido Schimmel <idosch@nvidia.com>
    commit b377add0f0117409c418ddd6504bd682ebe0bf79
    
    Both the function that migrates all the chunks within a region and the
    function that migrates all the entries within a chunk call
    list_first_entry() on the respective lists without checking that the
    lists are not empty. This is incorrect usage of the API, which leads to
    the following warning [1].
    
    Fix by returning if the lists are empty as there is nothing to migrate
    in this case.
    
    [1]
    WARNING: CPU: 0 PID: 6437 at drivers/net/ethernet/mellanox/mlxsw/spectrum_acl_tcam.c:1266 mlxsw_sp_acl_tcam_vchunk_migrate_all+0x1f1/0>
    Modules linked in:
    CPU: 0 PID: 6437 Comm: kworker/0:37 Not tainted 6.9.0-rc3-custom-00883-g94a65f079ef6 #39
    Hardware name: Mellanox Technologies Ltd. MSN3700/VMOD0005, BIOS 5.11 01/06/2019
    Workqueue: mlxsw_core mlxsw_sp_acl_tcam_vregion_rehash_work
    RIP: 0010:mlxsw_sp_acl_tcam_vchunk_migrate_all+0x1f1/0x2c0
    [...]
    Call Trace:
     <TASK>
     mlxsw_sp_acl_tcam_vregion_rehash_work+0x6c/0x4a0
     process_one_work+0x151/0x370
     worker_thread+0x2cb/0x3e0
     kthread+0xd0/0x100
     ret_from_fork+0x34/0x50
     ret_from_fork_asm+0x1a/0x30
     </TASK>
    
    Fixes: 6f9579d4e302 ("mlxsw: spectrum_acl: Remember where to continue rehash migration")
    	Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    	Tested-by: Alexander Zubkov <green@qrator.net>
    	Reviewed-by: Petr Machata <petrm@nvidia.com>
    	Signed-off-by: Petr Machata <petrm@nvidia.com>
    	Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://lore.kernel.org/r/4628e9a22d1d84818e28310abbbc498e7bc31bc9.1713797103.git.petrm@nvidia.com
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit b377add0f0117409c418ddd6504bd682ebe0bf79)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_acl_tcam.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_acl_tcam.c
@@ -1350,6 +1350,9 @@ mlxsw_sp_acl_tcam_vchunk_migrate_one(struct mlxsw_sp *mlxsw_sp,
 		return 0;
 	}
 
+	if (list_empty(&vchunk->ventry_list))
+		goto out;
+
 	/* If the migration got interrupted, we have the ventry to start from
 	 * stored in context.
 	 */
@@ -1401,6 +1404,7 @@ mlxsw_sp_acl_tcam_vchunk_migrate_one(struct mlxsw_sp *mlxsw_sp,
 		}
 	}
 
+out:
 	mlxsw_sp_acl_tcam_vchunk_migrate_end(mlxsw_sp, vchunk, ctx);
 	return 0;
 }
@@ -1413,6 +1417,9 @@ mlxsw_sp_acl_tcam_vchunk_migrate_all(struct mlxsw_sp *mlxsw_sp,
 {
 	struct mlxsw_sp_acl_tcam_vchunk *vchunk;
 	int err;
+
+	if (list_empty(&vregion->vchunk_list))
+		return 0;
 
 	/* If the migration got interrupted, we have the vchunk
 	 * we are working on stored in context.
