40ecf98bee11dad05cbeb99fb4d99f87c4e8c34a
Jonathan Maple <jmaple@ciq.com>
2024-09-13 12:33:20 -0400

    hvcs: Get reference to tty in remove
    jira LE-1907
    Rebuild_History Non-Buildable kernel-rt-4.18.0-477.21.1.rt7.284.el8_8
    commit-author Brian King <brking@linux.vnet.ibm.com>
    commit 3a8d3b366ce47024bf274eac783f8af5df2780f5
    
    Grab a reference to the tty when removing the hvcs to ensure
    it does not get freed unexpectedly.
    
    	Signed-off-by: Brian King <brking@linux.vnet.ibm.com>
    Link: https://lore.kernel.org/r/20230203155802.404324-4-brking@linux.vnet.ibm.com
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 3a8d3b366ce47024bf274eac783f8af5df2780f5)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/tty/hvc/hvcs.c
+++ drivers/tty/hvc/hvcs.c
@@ -825,7 +825,7 @@ static void hvcs_remove(struct vio_dev *dev)
 
 	spin_lock_irqsave(&hvcsd->lock, flags);
 
-	tty = hvcsd->port.tty;
+	tty = tty_port_tty_get(&hvcsd->port);
 
 	spin_unlock_irqrestore(&hvcsd->lock, flags);
 
@@ -840,8 +840,10 @@ static void hvcs_remove(struct vio_dev *dev)
 	 * hvcs_hangup.  The tty should always be valid at this time unless a
 	 * simultaneous tty close already cleaned up the hvcs_struct.
 	 */
-	if (tty)
+	if (tty) {
 		tty_hangup(tty);
+		tty_kref_put(tty);
+	}
 
 	printk(KERN_INFO "HVCS: vty-server@%X removed from the"
 			" vio bus.\n", dev->unit_address);
