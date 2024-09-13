5f8fa39e19481841a96c1964d5f460a26f1f6537
Jonathan Maple <jmaple@ciq.com>
2024-09-13 13:38:09 -0400

    mlx5: implement ethtool::get_fec_stats
    jira LE-1907
    Rebuild_History Non-Buildable kernel-rt-4.18.0-372.9.1.rt7.166.el8
    commit-author Jakub Kicinski <kuba@kernel.org>
    commit 1703bb50df0e4248b8def88fc6dd43a505479c66
    
    Report corrected bits.
    
    v2: catch reg access errors (Saeed)
    
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    	Acked-by: Saeed Mahameed <saeedm@nvidia.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1703bb50df0e4248b8def88fc6dd43a505479c66)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
@@ -1605,6 +1605,14 @@ static int mlx5e_set_wol(struct net_device *netdev, struct ethtool_wolinfo *wol)
 	return mlx5_set_port_wol(mdev, mlx5_wol_mode);
 }
 
+static void mlx5e_get_fec_stats(struct net_device *netdev,
+				struct ethtool_fec_stats *fec_stats)
+{
+	struct mlx5e_priv *priv = netdev_priv(netdev);
+
+	mlx5e_stats_fec_get(priv, fec_stats);
+}
+
 static int mlx5e_get_fecparam(struct net_device *netdev,
 			      struct ethtool_fecparam *fecparam)
 {
@@ -2212,6 +2220,7 @@ const struct ethtool_ops mlx5e_ethtool_ops = {
 	.self_test         = mlx5e_self_test,
 	.get_msglevel      = mlx5e_get_msglevel,
 	.set_msglevel      = mlx5e_set_msglevel,
+	.get_fec_stats     = mlx5e_get_fec_stats,
 	.get_fecparam      = mlx5e_get_fecparam,
 	.set_fecparam      = mlx5e_set_fecparam,
 };
