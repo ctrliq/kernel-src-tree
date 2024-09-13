d6874d3c03f4de0af4605d315acf29a3911310a2
Jonathan Maple <jmaple@ciq.com>
2024-09-13 13:47:05 -0400

    powerpc/signal64: Don't opencode page prefaulting
    jira LE-1907
    Rebuild_History Non-Buildable kernel-rt-4.18.0-372.16.1.rt7.173.el8_6
    commit-author Christophe Leroy <christophe.leroy@csgroup.eu>
    commit 96032f983ca32ad1d43c73da922dbc7022754c3c
    
    Instead of doing a __get_user() from the first and last location
    into a tmp var which won't be used, use fault_in_pages_readable()
    
    	Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    	Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/810bd8840ef990a200f58c9dea9abe767ca02a3a.1594146723.git.christophe.leroy@csgroup.eu
    (cherry picked from commit 96032f983ca32ad1d43c73da922dbc7022754c3c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/powerpc/kernel/signal_64.c
+++ arch/powerpc/kernel/signal_64.c
@@ -25,6 +25,7 @@
 #include <linux/ptrace.h>
 #include <linux/ratelimit.h>
 #include <linux/syscalls.h>
+#include <linux/pagemap.h>
 
 #include <asm/sigcontext.h>
 #include <asm/ucontext.h>
@@ -636,7 +637,6 @@ static long setup_trampoline(unsigned int syscall, unsigned int __user *tramp)
 SYSCALL_DEFINE3(swapcontext, struct ucontext __user *, old_ctx,
 		struct ucontext __user *, new_ctx, long, ctx_size)
 {
-	unsigned char tmp;
 	sigset_t set;
 	unsigned long new_msr = 0;
 	int ctx_has_vsx_region = 0;
@@ -671,9 +671,8 @@ SYSCALL_DEFINE3(swapcontext, struct ucontext __user *, old_ctx,
 	}
 	if (new_ctx == NULL)
 		return 0;
-	if (!access_ok(new_ctx, ctx_size)
-	    || __get_user(tmp, (u8 __user *) new_ctx)
-	    || __get_user(tmp, (u8 __user *) new_ctx + ctx_size - 1))
+	if (!access_ok(new_ctx, ctx_size) ||
+	    fault_in_pages_readable((u8 __user *)new_ctx, ctx_size))
 		return -EFAULT;
 
 	/*
