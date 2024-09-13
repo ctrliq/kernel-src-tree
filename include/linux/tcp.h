7101735f5a8e255e6d61f62fb25e5e8e8c505fe9
Jonathan Maple <jmaple@ciq.com>
2024-09-13 13:43:00 -0400

    tcp: Add TCP_INFO counter for packets received out-of-order
    jira LE-1907
    Rebuild_History Non-Buildable kernel-rt-4.18.0-372.9.1.rt7.166.el8
    commit-author Thomas Higdon <tph@fb.com>
    commit f9af2dbbfe01def62765a58af7fbc488351893c3
    
    For receive-heavy cases on the server-side, we want to track the
    connection quality for individual client IPs. This counter, similar to
    the existing system-wide TCPOFOQueue counter in /proc/net/netstat,
    tracks out-of-order packet reception. By providing this counter in
    TCP_INFO, it will allow understanding to what degree receive-heavy
    sockets are experiencing out-of-order delivery and packet drops
    indicating congestion.
    
    Please note that this is similar to the counter in NetBSD TCP_INFO, and
    has the same name.
    
    Also note that we avoid increasing the size of the tcp_sock struct by
    taking advantage of a hole.
    
    	Signed-off-by: Thomas Higdon <tph@fb.com>
    	Acked-by: Neal Cardwell <ncardwell@google.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f9af2dbbfe01def62765a58af7fbc488351893c3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -367,6 +367,8 @@ struct tcp_sock {
 #define BPF_SOCK_OPS_TEST_FLAG(TP, ARG) 0
 #endif
 
+	u32 rcv_ooopack; /* Received out-of-order packets, for tcpinfo */
+
 /* Receiver side RTT estimation */
 	struct {
 		u32	rtt_us;
