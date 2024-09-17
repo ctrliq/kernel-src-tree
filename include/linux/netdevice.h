86f0a122f79c64b95bb3d717272920eed3184f0c
Michal Schmidt <mschmidt@redhat.com>
2024-09-17 13:04:25 +0200

    ethtool: move firmware flashing flag to struct ethtool_netdev_state
    JIRA: https://issues.redhat.com/browse/RHEL-57750
    
    commit caa93b7c25945d302689de07bd404655db93ae6e
    Author: Edward Cree <ecree.xilinx@gmail.com>
    Date:   Wed Jul 3 13:18:49 2024 +0100
    
        ethtool: move firmware flashing flag to struct ethtool_netdev_state
    
        Commit 31e0aa99dc02 ("ethtool: Veto some operations during firmware flashing process")
         added a flag module_fw_flash_in_progress to struct net_device.  As
         this is ethtool related state, move it to the recently created
         struct ethtool_netdev_state, accessed via the 'ethtool' member of
         struct net_device.
    
        Suggested-by: Jakub Kicinski <kuba@kernel.org>
        Signed-off-by: Edward Cree <ecree.xilinx@gmail.com>
        Reviewed-by: Michal Kubiak <michal.kubiak@intel.com>
        Reviewed-by: Ido Schimmel <idosch@nvidia.com>
        Link: https://patch.msgid.link/20240703121849.652893-1-edward.cree@amd.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2030,8 +2030,6 @@ enum netdev_reg_state {
  *
  *	@threaded:	napi threaded mode is enabled
  *
- *	@module_fw_flash_in_progress:	Module firmware flashing is in progress.
- *
  *	@net_notifier_list:	List of per-net netdev notifier block
  *				that follow this device when it is moved
  *				to another network namespace.
@@ -2419,7 +2417,6 @@ struct net_device {
 	bool			proto_down;
 	bool			threaded;
 
-	unsigned		module_fw_flash_in_progress:1;
 	struct list_head	net_notifier_list;
 
 #if IS_ENABLED(CONFIG_MACSEC)
