49e6dbc901e2a986cbadb2aee5106e04ba65a069
Scott Mayhew <smayhew@redhat.com>
2024-09-23 16:33:44 -0400

    nfsd: make svc_stat per-network namespace instead of global
    JIRA: https://issues.redhat.com/browse/RHEL-59704
    
    commit 16fb9808ab2c99979f081987752abcbc5b092eac
    Author: Josef Bacik <josef@toxicpanda.com>
    Date:   Fri Jan 26 10:39:49 2024 -0500
    
        nfsd: make svc_stat per-network namespace instead of global
    
        The final bit of stats that is global is the rpc svc_stat.  Move this
        into the nfsd_net struct and use that everywhere instead of the global
        struct.  Remove the unused global struct.
    
        Signed-off-by: Josef Bacik <josef@toxicpanda.com>
        Reviewed-by: Jeff Layton <jlayton@kernel.org>
        Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    
    Signed-off-by: Scott Mayhew <smayhew@redhat.com>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -1648,6 +1648,8 @@ static __net_init int nfsd_net_init(struct net *net)
 	retval = nfsd_stat_counters_init(nn);
 	if (retval)
 		goto out_repcache_error;
+	memset(&nn->nfsd_svcstats, 0, sizeof(nn->nfsd_svcstats));
+	nn->nfsd_svcstats.program = &nfsd_program;
 	nn->nfsd_versions = NULL;
 	nn->nfsd4_minorversions = NULL;
 	nn->nfsd_info.mutex = &nfsd_mutex;
