aaa814d7e125607c97d54ca68dbc70d67240256c
Rafael Aquini <raquini@redhat.com>
2024-09-27 10:45:15 -0400

    mm: remove checks for pte_index
    JIRA: https://issues.redhat.com/browse/RHEL-27743
    
    This patch is a backport of the following upstream commit:
    commit bb7dbaafff3f582d18028a5b99a8faa789842678
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Sat Aug 19 04:18:37 2023 +0100
    
        mm: remove checks for pte_index
    
        Since pte_index is always defined, we don't need to check whether it's
        defined or not.  Delete the slow version that doesn't depend on it and
        remove the #define since nobody needs to test for it.
    
        Link: https://lkml.kernel.org/r/20230819031837.3160096-1-willy@infradead.org
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Reviewed-by: Mike Rapoport (IBM) <rppt@kernel.org>
        Cc: Christian Dietrich <stettberger@dokucode.de>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1872,7 +1872,6 @@ out:
 	return retval;
 }
 
-#ifdef pte_index
 static int insert_page_in_batch_locked(struct vm_area_struct *vma, pte_t *pte,
 			unsigned long addr, struct page *page, pgprot_t prot)
 {
@@ -1887,7 +1886,7 @@ static int insert_page_in_batch_locked(struct vm_area_struct *vma, pte_t *pte,
 }
 
 /* insert_pages() amortizes the cost of spinlock operations
- * when inserting pages in a loop. Arch *must* define pte_index.
+ * when inserting pages in a loop.
  */
 static int insert_pages(struct vm_area_struct *vma, unsigned long addr,
 			struct page **pages, unsigned long *num, pgprot_t prot)
@@ -1946,7 +1945,6 @@ out:
 	*num = remaining_pages_total;
 	return ret;
 }
-#endif  /* ifdef pte_index */
 
 /**
  * vm_insert_pages - insert multiple pages into user vma, batching the pmd lock.
@@ -1966,7 +1964,6 @@ out:
 int vm_insert_pages(struct vm_area_struct *vma, unsigned long addr,
 			struct page **pages, unsigned long *num)
 {
-#ifdef pte_index
 	const unsigned long end_addr = addr + (*num * PAGE_SIZE) - 1;
 
 	if (addr < vma->vm_start || end_addr >= vma->vm_end)
@@ -1978,18 +1975,6 @@ int vm_insert_pages(struct vm_area_struct *vma, unsigned long addr,
 	}
 	/* Defer page refcount checking till we're about to map that page. */
 	return insert_pages(vma, addr, pages, num, vma->vm_page_prot);
-#else
-	unsigned long idx = 0, pgcount = *num;
-	int err = -EINVAL;
-
-	for (; idx < pgcount; ++idx) {
-		err = vm_insert_page(vma, addr + (PAGE_SIZE * idx), pages[idx]);
-		if (err)
-			break;
-	}
-	*num = pgcount - idx;
-	return err;
-#endif  /* ifdef pte_index */
 }
 EXPORT_SYMBOL(vm_insert_pages);
 
