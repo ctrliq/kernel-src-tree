c36de5dd7c5f30abbfdbcf14c6e76bd74f90c70e
Rafael Aquini <raquini@redhat.com>
2024-09-27 10:43:43 -0400

    mm/page_table_check: remove unused parameter in [__]page_table_check_pmd_set
    JIRA: https://issues.redhat.com/browse/RHEL-27743
    Conflicts:
      * arch/riscv/include/asm/pgtable.h: hunk dropped (unsupported arch)
    
    This patch is a backport of the following upstream commit:
    commit a3b837130b5865521fa8662aceaa6ebc8d29389a
    Author: Kemeng Shi <shikemeng@huaweicloud.com>
    Date:   Fri Jul 14 01:26:35 2023 +0800
    
        mm/page_table_check: remove unused parameter in [__]page_table_check_pmd_set
    
        Remove unused addr in __page_table_check_pmd_set and
        page_table_check_pmd_set.
    
        Link: https://lkml.kernel.org/r/20230713172636.1705415-8-shikemeng@huaweicloud.com
        Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
        Cc: Pavel Tatashin <pasha.tatashin@soleen.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -1242,7 +1242,7 @@ static inline void set_pte_at(struct mm_struct *mm, unsigned long addr,
 static inline void set_pmd_at(struct mm_struct *mm, unsigned long addr,
 			      pmd_t *pmdp, pmd_t pmd)
 {
-	page_table_check_pmd_set(mm, addr, pmdp, pmd);
+	page_table_check_pmd_set(mm, pmdp, pmd);
 	set_pmd(pmdp, pmd);
 }
 
@@ -1387,7 +1387,7 @@ static inline void pmdp_set_wrprotect(struct mm_struct *mm,
 static inline pmd_t pmdp_establish(struct vm_area_struct *vma,
 		unsigned long address, pmd_t *pmdp, pmd_t pmd)
 {
-	page_table_check_pmd_set(vma->vm_mm, address, pmdp, pmd);
+	page_table_check_pmd_set(vma->vm_mm, pmdp, pmd);
 	if (IS_ENABLED(CONFIG_SMP)) {
 		return xchg(pmdp, pmd);
 	} else {
