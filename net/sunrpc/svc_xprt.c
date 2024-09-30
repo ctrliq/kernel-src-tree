196111a579eb1dc964ba24a904ac2731e6ccbe0c
Scott Mayhew <smayhew@redhat.com>
2024-09-30 16:29:03 -0400

    SUNRPC: change how svc threads are asked to exit.
    JIRA: https://issues.redhat.com/browse/RHEL-59704
    Conflicts:
    	include/linux/sunrpc/svc.h: Context diff due to out of order backport
    	of 3587b5c75376 ("SUNRPC: Remove RQ_SPLICE_OK").
    	net/sunrpc/svc.c: Context and code diff due to out of order backport
    	of 1e3577a4521e ("SUNRPC: discard sv_refcnt, and svc_get/svc_put").
    	net/sunrpc/svc_xprt.c: Context diff due to RHEL-only commit
    	d654df46f8db ("sunrpc: have svc tasks sleep in TASK_INTERRUPTIBLE
    	instead of TASK_IDLE").
    
    commit fa341560ca7458f4396d5a0771cb5f2358d8535d
    Author: NeilBrown <neilb@suse.de>
    Date:   Mon Sep 11 10:39:04 2023 -0400
    
        SUNRPC: change how svc threads are asked to exit.
    
        svc threads are currently stopped using kthread_stop().  This requires
        identifying a specific thread.  However we don't care which thread
        stops, just as long as one does.
    
        So instead, set a flag in the svc_pool to say that a thread needs to
        die, and have each thread check this flag instead of calling
        kthread_should_stop().  The first thread to find and clear this flag
        then moves towards exiting.
    
        This removes an explicit dependency on sp_all_threads which will make a
        future patch simpler.
    
        Signed-off-by: NeilBrown <neilb@suse.de>
        Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    
    Signed-off-by: Scott Mayhew <smayhew@redhat.com>

--- net/sunrpc/svc_xprt.c
+++ net/sunrpc/svc_xprt.c
@@ -9,7 +9,6 @@
 #include <linux/sched/mm.h>
 #include <linux/errno.h>
 #include <linux/freezer.h>
-#include <linux/kthread.h>
 #include <linux/slab.h>
 #include <net/sock.h>
 #include <linux/sunrpc/addr.h>
@@ -674,7 +673,7 @@ static bool svc_alloc_arg(struct svc_rqst *rqstp)
 			continue;
 
 		set_current_state(TASK_INTERRUPTIBLE);
-		if (kthread_should_stop()) {
+		if (svc_thread_should_stop(rqstp)) {
 			set_current_state(TASK_RUNNING);
 			return false;
 		}
@@ -712,7 +711,7 @@ rqst_should_sleep(struct svc_rqst *rqstp)
 		return false;
 
 	/* are we shutting down? */
-	if (kthread_should_stop())
+	if (svc_thread_should_stop(rqstp))
 		return false;
 
 	/* are we freezing? */
@@ -857,7 +856,7 @@ void svc_recv(struct svc_rqst *rqstp)
 
 	clear_bit(SP_TASK_PENDING, &pool->sp_flags);
 
-	if (kthread_should_stop())
+	if (svc_thread_should_stop(rqstp))
 		return;
 
 	rqstp->rq_xprt = svc_xprt_dequeue(pool);
