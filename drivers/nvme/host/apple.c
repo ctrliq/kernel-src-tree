81cf20a18f7646c77f9f8b4044c58295bab038da
Maurizio Lombardi <mlombard@redhat.com>
2024-10-01 15:18:03 +0200

    nvme: split device add from initialization
    JIRA: https://issues.redhat.com/browse/RHEL-51888
    
    Combining both creates an ambiguous cleanup scenario for the caller if
    an error is returned: does the device reference need to be dropped or
    did the error occur before the device was initialized? If an error
    occurs after the device is added, then the existing cleanup routines
    will leak memory.
    
    Furthermore, the nvme core is taking it upon itself to free the device's
    kobj name under certain conditions rather than go through the core
    device API. We shouldn't be peaking into these implementation details.
    
    Split the device initialization from the addition to make it easier to
    know the error handling actions, fix the existing memory leaks, and stop
    the device layering violations.
    
    Link: https://lore.kernel.org/linux-nvme/c4050a37-ecc9-462c-9772-65e25166f439@grimberg.me/
    Tested-by: Yi Zhang <yi.zhang@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Chaitanya Kulkarni <kch@nvidia.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    (cherry picked from commit 1a9e218195a55d0a31e8cbe263aa2f618580ef1d)
    Signed-off-by: Maurizio Lombardi <mlombard@redhat.com>

--- drivers/nvme/host/apple.c
+++ drivers/nvme/host/apple.c
@@ -1532,6 +1532,10 @@ static int apple_nvme_probe(struct platform_device *pdev)
 	if (IS_ERR(anv))
 		return PTR_ERR(anv);
 
+	ret = nvme_add_ctrl(&anv->ctrl);
+	if (ret)
+		goto out_put_ctrl;
+
 	anv->ctrl.admin_q = blk_mq_alloc_queue(&anv->admin_tagset, NULL, NULL);
 	if (IS_ERR(anv->ctrl.admin_q)) {
 		ret = -ENOMEM;
@@ -1546,6 +1550,7 @@ static int apple_nvme_probe(struct platform_device *pdev)
 
 out_uninit_ctrl:
 	nvme_uninit_ctrl(&anv->ctrl);
+out_put_ctrl:
 	nvme_put_ctrl(&anv->ctrl);
 	return ret;
 }
