8a6a8c0ac5f91f3c692c78bb422bfb638b6582af
Ivan Vecera <ivecera@redhat.com>
2024-10-07 15:20:35 +0200

    treewide: Remove system_counterval_t.cs, which is never read
    JIRA: https://issues.redhat.com/browse/RHEL-61639
    
    commit b152688c91313ab4073cff4a5e63ff4cc491c358
    Author: Peter Hilber <peter.hilber@opensynergy.com>
    Date:   Thu Feb 1 02:04:52 2024 +0100
    
        treewide: Remove system_counterval_t.cs, which is never read
    
        The clocksource pointer in struct system_counterval_t is not evaluated any
        more. Remove the code setting the member, and the member itself.
    
        Signed-off-by: Peter Hilber <peter.hilber@opensynergy.com>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Link: https://lore.kernel.org/r/20240201010453.2212371-8-peter.hilber@opensynergy.com
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/linux/ptp_kvm.h
+++ include/linux/ptp_kvm.h
@@ -12,12 +12,10 @@
 #include <linux/types.h>
 
 struct timespec64;
-struct clocksource;
 
 int kvm_arch_ptp_init(void);
 int kvm_arch_ptp_get_clock(struct timespec64 *ts);
 int kvm_arch_ptp_get_crosststamp(u64 *cycle,
-		struct timespec64 *tspec, struct clocksource **cs,
-		enum clocksource_ids *cs_id);
+		struct timespec64 *tspec, enum clocksource_ids *cs_id);
 
 #endif /* _PTP_KVM_H_ */
