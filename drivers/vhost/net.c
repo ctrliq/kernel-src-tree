722c5020a2d7505a0b29c965802eb51cc241e61c
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:32:23 -0400

    vhost: Remove custom vhost rcu usage
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [vhost] Remove custom vhost rcu usage (Igor Mammedov) [1152962]
    Rebuild_FUZZ: 89.23%
    commit-author Asias He <asias@redhat.com>
    commit 22fa90c7fb479694d6affebc049d21f06b714be6
    
    Now, vq->private_data is always accessed under vq mutex. No need to play
    the vhost rcu trick.
    
    	Signed-off-by: Asias He <asias@redhat.com>
    	Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit 22fa90c7fb479694d6affebc049d21f06b714be6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/vhost/net.c
+++ drivers/vhost/net.c
@@ -15,7 +15,6 @@
 #include <linux/moduleparam.h>
 #include <linux/mutex.h>
 #include <linux/workqueue.h>
-#include <linux/rcupdate.h>
 #include <linux/file.h>
 #include <linux/slab.h>
 
@@ -770,8 +769,7 @@ static int vhost_net_enable_vq(struct vhost_net *n,
 	struct vhost_poll *poll = n->poll + (nvq - n->vqs);
 	struct socket *sock;
 
-	sock = rcu_dereference_protected(vq->private_data,
-					 lockdep_is_held(&vq->mutex));
+	sock = vq->private_data;
 	if (!sock)
 		return 0;
 
@@ -784,10 +782,9 @@ static struct socket *vhost_net_stop_vq(struct vhost_net *n,
 	struct socket *sock;
 
 	mutex_lock(&vq->mutex);
-	sock = rcu_dereference_protected(vq->private_data,
-					 lockdep_is_held(&vq->mutex));
+	sock = vq->private_data;
 	vhost_net_disable_vq(n, vq);
-	rcu_assign_pointer(vq->private_data, NULL);
+	vq->private_data = NULL;
 	mutex_unlock(&vq->mutex);
 	return sock;
 }
@@ -945,8 +942,7 @@ static long vhost_net_set_backend(struct vhost_net *n, unsigned index, int fd)
 	}
 
 	/* start polling new socket */
-	oldsock = rcu_dereference_protected(vq->private_data,
-					    lockdep_is_held(&vq->mutex));
+	oldsock = vq->private_data;
 	if (sock != oldsock) {
 		ubufs = vhost_net_ubuf_alloc(vq,
 					     sock && vhost_sock_zcopy(sock));
@@ -956,7 +952,7 @@ static long vhost_net_set_backend(struct vhost_net *n, unsigned index, int fd)
 		}
 
 		vhost_net_disable_vq(n, vq);
-		rcu_assign_pointer(vq->private_data, sock);
+		vq->private_data = sock;
 		r = vhost_init_used(vq);
 		if (r)
 			goto err_used;
@@ -990,7 +986,7 @@ static long vhost_net_set_backend(struct vhost_net *n, unsigned index, int fd)
 	return 0;
 
 err_used:
-	rcu_assign_pointer(vq->private_data, oldsock);
+	vq->private_data = oldsock;
 	vhost_net_enable_vq(n, vq);
 	if (ubufs)
 		vhost_net_ubuf_put_wait_and_free(ubufs);
