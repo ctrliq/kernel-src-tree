43c4b2a64dbca53b7902eb8bd07f0eb9ab64140e
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:58:14 -0400

    netfilter: bridge: query conntrack about skb dnat
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Florian Westphal <fw@strlen.de>
    commit c055d5b03bb4cb69d349d787c9787c0383abd8b2
    
    ask conntrack instead of storing ipv4 address in nf_bridge_info->data.
    
    Ths avoids the need to use ->data during NF_PRE_ROUTING.
    Only two functions that need ->data remain.
    
    These will be addressed in followup patches.
    
    	Signed-off-by: Florian Westphal <fw@strlen.de>
    	Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (cherry picked from commit c055d5b03bb4cb69d349d787c9787c0383abd8b2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/bridge/br_netfilter.c
+++ net/bridge/br_netfilter.c
@@ -36,16 +36,15 @@
 #include <net/ipv6.h>
 #include <net/route.h>
 
+#if IS_ENABLED(CONFIG_NF_CONNTRACK)
+#include <net/netfilter/nf_conntrack.h>
+#endif
+
 #include <asm/uaccess.h>
 #include "br_private.h"
 #ifdef CONFIG_SYSCTL
 #include <linux/sysctl.h>
 #endif
-
-#define skb_origaddr(skb)	 (((struct bridge_skb_cb *) \
-				 (skb->nf_bridge->data))->daddr.ipv4)
-#define store_orig_dstaddr(skb)	 (skb_origaddr(skb) = ip_hdr(skb)->daddr)
-#define dnat_took_place(skb)	 (skb_origaddr(skb) != ip_hdr(skb)->daddr)
 
 #ifdef CONFIG_SYSCTL
 static struct ctl_table_header *brnf_sysctl_header;
@@ -373,6 +372,22 @@ free_skb:
 	return 0;
 }
 
+static bool dnat_took_place(const struct sk_buff *skb)
+{
+#if IS_ENABLED(CONFIG_NF_CONNTRACK)
+	enum ip_conntrack_info ctinfo;
+	struct nf_conn *ct;
+
+	ct = nf_ct_get(skb, &ctinfo);
+	if (!ct || nf_ct_is_untracked(ct))
+		return false;
+
+	return test_bit(IPS_DST_NAT_BIT, &ct->status);
+#else
+	return false;
+#endif
+}
+
 /* This requires some explaining. If DNAT has taken place,
  * we will need to fix up the destination Ethernet address.
  *
@@ -674,7 +689,7 @@ static unsigned int br_nf_pre_routing(const struct nf_hook_ops *ops,
 		return NF_DROP;
 	if (!setup_pre_routing(skb))
 		return NF_DROP;
-	store_orig_dstaddr(skb);
+
 	skb->protocol = htons(ETH_P_IP);
 
 	NF_HOOK(NFPROTO_IPV4, NF_INET_PRE_ROUTING, state->sk, skb,
