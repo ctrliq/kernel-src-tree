ec0a65e9b9e511717fe1adae86108c6c43ab0fcd
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:58:33 -0400

    perf/x86/intel: Add lockdep assert
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Peter Zijlstra <peterz@infradead.org>
    commit b32ed7f5de262b10633bb6c6dbb7f8ba46598cf4
    
    Lockdep is very good at finding incorrect IRQ state while locking and
    is far better at telling us if we hold a lock than the _is_locked()
    API. It also generates less code for !DEBUG kernels.
    
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Stephane Eranian <eranian@google.com>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    	Cc: Vince Weaver <vincent.weaver@maine.edu>
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit b32ed7f5de262b10633bb6c6dbb7f8ba46598cf4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kernel/cpu/perf_event_intel.c
+++ arch/x86/kernel/cpu/perf_event_intel.c
@@ -1897,7 +1897,6 @@ intel_start_scheduling(struct cpu_hw_events *cpuc)
 	 * in stop_event_scheduling()
 	 * makes scheduling appear as a transaction
 	 */
-	WARN_ON_ONCE(!irqs_disabled());
 	raw_spin_lock(&excl_cntrs->lock);
 
 	/*
@@ -2179,7 +2178,7 @@ static void intel_commit_scheduling(struct cpu_hw_events *cpuc, int idx, int cnt
 
 	xl = &excl_cntrs->states[tid];
 
-	WARN_ON_ONCE(!raw_spin_is_locked(&excl_cntrs->lock));
+	lockdep_assert_held(&excl_cntrs->lock);
 
 	if (cntr >= 0) {
 		if (c->flags & PERF_X86_EVENT_EXCL)
