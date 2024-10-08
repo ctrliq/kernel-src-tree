9fcbcf9d66f78429b76ecf7399c67c32af05de4e
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:29:17 -0400

    perf tools: Move mem_bswap32/64 to util.c
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    Rebuild_CHGLOG: - [tools] perf: Move mem_bswap32/64 to util.c (Jiri Olsa) [1133083]
    Rebuild_FUZZ: 92.11%
    commit-author Adrian Hunter <adrian.hunter@intel.com>
    commit 71db07b12eace6a3619335d03eaf3cbe2de131ed
    
    Move functions mem_bswap_32() and mem_bswap_64() so they can be reused.
    
    	Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: Andi Kleen <ak@linux.intel.com>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Frederic Weisbecker <fweisbec@gmail.com>
    	Cc: Ingo Molnar <mingo@redhat.com>
    	Cc: Jiri Olsa <jolsa@redhat.com>
    	Cc: Mike Galbraith <efault@gmx.de>
    	Cc: Namhyung Kim <namhyung@gmail.com>
    	Cc: Paul Mackerras <paulus@samba.org>
    	Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    	Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/r/1386765443-26966-21-git-send-email-alexander.shishkin@linux.intel.com
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 71db07b12eace6a3619335d03eaf3cbe2de131ed)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/session.h
+++ tools/perf/util/session.h
@@ -73,8 +73,6 @@ int perf_session__resolve_callchain(struct perf_session *self, struct perf_evsel
 
 bool perf_session__has_traces(struct perf_session *self, const char *msg);
 
-void mem_bswap_64(void *src, int byte_size);
-void mem_bswap_32(void *src, int byte_size);
 void perf_event__attr_swap(struct perf_event_attr *attr);
 
 int perf_session__create_kernel_maps(struct perf_session *self);
