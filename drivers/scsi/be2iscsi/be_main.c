28038d9637a1966a0d63a582458c6b639775ceea
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:09:05 -0400

    be2iscsi : Fix memory leak in the unload path
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [scsi] be2iscsi: Fix memory leak in the unload path (Rob Evers) [1183800]
    Rebuild_FUZZ: 98.88%
    commit-author John Soni Jose <sony.john-n@emulex.com>
    commit 4e2bdf7a2b841422f207bb6d74819955e386a175
    
     Driver was not freeing the DMA memory allocated for EQ/CQ in the
     unload path. This patch frees the DMA memory during the driver unload.
    
    	Signed-off-by: John Soni Jose <sony.john-n@emulex.com>
    	Signed-off-by: Jayamohan Kallickal <jayamohan.kallickal@emulex.com>
    	Reviewed-by: Mike Christie <michaelc@cs.wisc.edu>
    	Signed-off-by: James Bottomley <JBottomley@Odin.com>
    (cherry picked from commit 4e2bdf7a2b841422f207bb6d74819955e386a175)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/be2iscsi/be_main.c
+++ drivers/scsi/be2iscsi/be_main.c
@@ -3742,14 +3742,16 @@ static void be_mcc_queues_destroy(struct beiscsi_hba *phba)
 	struct be_ctrl_info *ctrl = &phba->ctrl;
 
 	q = &phba->ctrl.mcc_obj.q;
-	if (q->created)
+	if (q->created) {
 		beiscsi_cmd_q_destroy(ctrl, q, QTYPE_MCCQ);
-	be_queue_free(phba, q);
+		be_queue_free(phba, q);
+	}
 
 	q = &phba->ctrl.mcc_obj.cq;
-	if (q->created)
+	if (q->created) {
 		beiscsi_cmd_q_destroy(ctrl, q, QTYPE_CQ);
-	be_queue_free(phba, q);
+		be_queue_free(phba, q);
+	}
 }
 
 static void hwi_cleanup(struct beiscsi_hba *phba)
@@ -3793,8 +3795,10 @@ static void hwi_cleanup(struct beiscsi_hba *phba)
 
 	for (i = 0; i < (phba->num_cpus); i++) {
 		q = &phwi_context->be_cq[i];
-		if (q->created)
+		if (q->created) {
+			be_queue_free(phba, q);
 			beiscsi_cmd_q_destroy(ctrl, q, QTYPE_CQ);
+		}
 	}
 
 	be_mcc_queues_destroy(phba);
@@ -3804,8 +3808,10 @@ static void hwi_cleanup(struct beiscsi_hba *phba)
 		eq_for_mcc = 0;
 	for (i = 0; i < (phba->num_cpus + eq_for_mcc); i++) {
 		q = &phwi_context->be_eq[i].q;
-		if (q->created)
+		if (q->created) {
+			be_queue_free(phba, q);
 			beiscsi_cmd_q_destroy(ctrl, q, QTYPE_EQ);
+		}
 	}
 	be_cmd_fw_uninit(ctrl);
 }
