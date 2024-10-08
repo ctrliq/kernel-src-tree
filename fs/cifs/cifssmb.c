9addc562194f319dceb933aa598003adcea33f27
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:23:48 -0400

    CIFS: Use separate var for the number of bytes got in async read
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    commit-author Pavel Shilovsky <pshilovsky@samba.org>
    commit 34a54d617785e5ecafe1605df7aa689ec193964c
    
    and don't mix it with the number of bytes that was requested.
    
    	Signed-off-by: Pavel Shilovsky <pshilovsky@samba.org>
    	Signed-off-by: Steve French <smfrench@gmail.com>
    (cherry picked from commit 34a54d617785e5ecafe1605df7aa689ec193964c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/cifs/cifssmb.c
+++ fs/cifs/cifssmb.c
@@ -1517,7 +1517,7 @@ cifs_readv_receive(struct TCP_Server_Info *server, struct mid_q_entry *mid)
 		return length;
 
 	server->total_read += length;
-	rdata->bytes = length;
+	rdata->got_bytes = length;
 
 	cifs_dbg(FYI, "total_read=%u buflen=%u remaining=%u\n",
 		 server->total_read, buflen, data_len);
@@ -1560,8 +1560,8 @@ cifs_readv_callback(struct mid_q_entry *mid)
 					 rc);
 		}
 		/* FIXME: should this be counted toward the initiating task? */
-		task_io_account_read(rdata->bytes);
-		cifs_stats_bytes_read(tcon, rdata->bytes);
+		task_io_account_read(rdata->got_bytes);
+		cifs_stats_bytes_read(tcon, rdata->got_bytes);
 		break;
 	case MID_REQUEST_SUBMITTED:
 	case MID_RETRY_NEEDED:
