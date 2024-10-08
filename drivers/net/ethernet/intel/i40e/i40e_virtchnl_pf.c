dfd22ff49fa8c5a8a9dfc7e1c76bfdbaad186991
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:36:09 -0400

    i40e: use QOS field consistently
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    commit-author Mitch Williams <mitch.a.williams@intel.com>
    commit f7fc2f2eb11fe808df330df42a9429aa5b804388
    
    In i40e_ndo_set_vf_port_vlan, we were using the QOS value
    inconsistently, sometimes shifting it, sometimes not. Do the shift-and-
    or operation correctly, once, and use the result consistently everywhere
    in the function.
    
    Change-ID: I46f062f3edc90a8a017ecec9137f4d1ab0ab9e41
    	Signed-off-by: Mitch Williams <mitch.a.williams@intel.com>
    	Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    	Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit f7fc2f2eb11fe808df330df42a9429aa5b804388)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/i40e/i40e_virtchnl_pf.c
+++ drivers/net/ethernet/intel/i40e/i40e_virtchnl_pf.c
@@ -2068,6 +2068,7 @@ error_param:
 int i40e_ndo_set_vf_port_vlan(struct net_device *netdev,
 			      int vf_id, u16 vlan_id, u8 qos)
 {
+	u16 vlanprio = vlan_id | (qos << I40E_VLAN_PRIORITY_SHIFT);
 	struct i40e_netdev_priv *np = netdev_priv(netdev);
 	struct i40e_pf *pf = np->vsi->back;
 	struct i40e_vsi *vsi;
@@ -2095,8 +2096,7 @@ int i40e_ndo_set_vf_port_vlan(struct net_device *netdev,
 		goto error_pvid;
 	}
 
-	if (le16_to_cpu(vsi->info.pvid) ==
-	    (vlan_id | (qos << I40E_VLAN_PRIORITY_SHIFT)))
+	if (le16_to_cpu(vsi->info.pvid) == vlanprio)
 		/* duplicate request, so just return success */
 		goto error_pvid;
 
@@ -2120,7 +2120,7 @@ int i40e_ndo_set_vf_port_vlan(struct net_device *netdev,
 	 * MAC addresses deleted.
 	 */
 	if ((!(vlan_id || qos) ||
-	    (vlan_id | qos) != le16_to_cpu(vsi->info.pvid)) &&
+	    vlanprio != le16_to_cpu(vsi->info.pvid)) &&
 	    vsi->info.pvid)
 		ret = i40e_vsi_add_vlan(vsi, I40E_VLAN_ANY);
 
@@ -2135,8 +2135,7 @@ int i40e_ndo_set_vf_port_vlan(struct net_device *netdev,
 		}
 	}
 	if (vlan_id || qos)
-		ret = i40e_vsi_add_pvid(vsi,
-				vlan_id | (qos << I40E_VLAN_PRIORITY_SHIFT));
+		ret = i40e_vsi_add_pvid(vsi, vlanprio);
 	else
 		i40e_vsi_remove_pvid(vsi);
 
