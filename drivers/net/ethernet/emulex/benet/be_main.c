b978a80c4b0c1147c8cdd631d738203ac17a25e6
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:31:39 -0400

    be2net: query FW to check if EVB is enabled
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    commit-author Kalesh Purayil <kalesh.purayil@avagotech.com>
    commit ff9ed19d263d9678394b6ac079abd68efb3c55c0
    
    The current code assumes that bridge functionality (EVB) in the adapter
    is enabled only when SR-IOV is enabled. This is not always true.
    This patch uses the GET_HSW_CONFIG FW cmd to query this from the FW.
    
    	Signed-off-by: Kalesh AP <kalesh.purayil@avagotech.com>
    	Signed-off-by: Sathya Perla <sathya.perla@avagotech.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ff9ed19d263d9678394b6ac079abd68efb3c55c0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/emulex/benet/be_main.c
+++ drivers/net/ethernet/emulex/benet/be_main.c
@@ -4589,9 +4589,6 @@ static int be_ndo_bridge_getlink(struct sk_buff *skb, u32 pid, u32 seq,
 	int status = 0;
 	u8 hsw_mode;
 
-	if (!sriov_enabled(adapter))
-		return 0;
-
 	/* BE and Lancer chips support VEB mode only */
 	if (BEx_chip(adapter) || lancer_chip(adapter)) {
 		hsw_mode = PORT_FWD_TYPE_VEB;
@@ -4600,6 +4597,9 @@ static int be_ndo_bridge_getlink(struct sk_buff *skb, u32 pid, u32 seq,
 					       adapter->if_handle, &hsw_mode,
 					       NULL);
 		if (status)
+			return 0;
+
+		if (hsw_mode == PORT_FWD_TYPE_PASSTHRU)
 			return 0;
 	}
 
