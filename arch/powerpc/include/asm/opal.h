ea2af3d9401d18c983bcad57d960b4b516da56eb
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:28:43 -0400

    powerpc/opal: Add PHB to cxl mode call
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [powerpc] opal: Add PHB to cxl mode call (Gustavo Duarte) [1182022]
    Rebuild_FUZZ: 88.24%
    commit-author Ian Munsie <imunsie@au1.ibm.com>
    commit 09521736011e83003ca3cf7d9a31c43148306a20
    
    This adds the OPAL call to change a PHB into cxl mode.
    
    	Signed-off-by: Ian Munsie <imunsie@au1.ibm.com>
    	Signed-off-by: Michael Neuling <mikey@neuling.org>
    	Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    (cherry picked from commit 09521736011e83003ca3cf7d9a31c43148306a20)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/powerpc/include/asm/opal.h
+++ arch/powerpc/include/asm/opal.h
@@ -147,6 +147,7 @@ struct opal_sg_list {
 #define OPAL_GET_PARAM				89
 #define OPAL_SET_PARAM				90
 #define OPAL_DUMP_RESEND			91
+#define OPAL_PCI_SET_PHB_CXL_MODE		93
 #define OPAL_DUMP_INFO2				94
 #define OPAL_REGISTER_DUMP_REGION		101
 #define OPAL_UNREGISTER_DUMP_REGION		102
@@ -866,6 +867,7 @@ int64_t opal_set_param(uint64_t token, uint32_t param_id, uint64_t buffer,
 int64_t opal_sensor_read(uint32_t sensor_hndl, int token, __be32 *sensor_data);
 int64_t opal_register_dump_region(uint32_t id, uint64_t start, uint64_t end);
 int64_t opal_unregister_dump_region(uint32_t id);
+int64_t opal_pci_set_phb_cxl_mode(uint64_t phb_id, uint64_t mode, uint64_t pe_number);
 
 /* Internal functions */
 extern int early_init_dt_scan_opal(unsigned long node, const char *uname,
