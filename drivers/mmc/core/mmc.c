cd52cad2f5859eba6609d967faf38a0d1c0cbc42
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:22:12 -0400

    mmc: Add support for marking hpi as broken through devicetree
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    Rebuild_CHGLOG: - [mmc] Add support for marking hpi as broken through devicetree (Don Zickus) [1127975 1277866 1280133 1286932 1297039]
    Rebuild_FUZZ: 95.73%
    commit-author Hans de Goede <hdegoede@redhat.com>
    commit 81f8a7be6642b4c26ab681b2e0f4c4120a6de1b0
    
    The eMMC on a tablet I've will stop working / communicating as soon as
    the kernel executes:
    
    		mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
     				EXT_CSD_HPI_MGMT, 1,
     				card->ext_csd.generic_cmd6_time);
    
    There seems to be no way to reliable identify eMMC-s which have a broken
    hpi implementation, but at least for eMMC's which are soldered onto a board
    we can work around this by specifying that hpi is broken in devicetree.
    
    	Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit 81f8a7be6642b4c26ab681b2e0f4c4120a6de1b0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -11,6 +11,7 @@
  */
 
 #include <linux/err.h>
+#include <linux/of.h>
 #include <linux/slab.h>
 #include <linux/stat.h>
 #include <linux/pm_runtime.h>
@@ -363,6 +364,8 @@ static int mmc_decode_ext_csd(struct mmc_card *card, u8 *ext_csd)
 {
 	int err = 0, idx;
 	unsigned int part_size;
+	struct device_node *np;
+	bool broken_hpi = false;
 
 	BUG_ON(!card);
 
@@ -380,6 +383,11 @@ static int mmc_decode_ext_csd(struct mmc_card *card, u8 *ext_csd)
 			goto out;
 		}
 	}
+
+	np = mmc_of_find_child_device(card->host, 0);
+	if (np && of_device_is_compatible(np, "mmc-card"))
+		broken_hpi = of_property_read_bool(np, "broken-hpi");
+	of_node_put(np);
 
 	/*
 	 * The EXT_CSD format is meant to be forward compatible. As long
@@ -524,7 +532,7 @@ static int mmc_decode_ext_csd(struct mmc_card *card, u8 *ext_csd)
 		}
 
 		/* check whether the eMMC card supports HPI */
-		if (ext_csd[EXT_CSD_HPI_FEATURES] & 0x1) {
+		if (!broken_hpi && (ext_csd[EXT_CSD_HPI_FEATURES] & 0x1)) {
 			card->ext_csd.hpi = 1;
 			if (ext_csd[EXT_CSD_HPI_FEATURES] & 0x2)
 				card->ext_csd.hpi_cmd =	MMC_STOP_TRANSMISSION;
