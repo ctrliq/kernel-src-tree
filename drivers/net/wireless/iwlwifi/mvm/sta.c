2aa401a8066a31eda6d36cfff9c643bfe0388680
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:56:29 -0400

    iwlwifi: mvm: set the encryption type of an IGTK key
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Ayala Beker <ayala.beker@intel.com>
    commit aa950524d501afa28869b7f56e539fd9e744dd9f
    
    The FW expect the driver to set the encryption algorithm type when
    installing the IGTK key in the HW.
    Currently when installing CMAC IGTK key we don't set the algorithm type
    and as a result the FW fails to calculate the MIC of multicast management
    frames.
    Fix it.
    
    	Signed-off-by: Ayala Beker <ayala.beker@intel.com>
    	Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    (cherry picked from commit aa950524d501afa28869b7f56e539fd9e744dd9f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/wireless/iwlwifi/mvm/sta.c
+++ drivers/net/wireless/iwlwifi/mvm/sta.c
@@ -1276,6 +1276,14 @@ static int iwl_mvm_send_sta_igtk(struct iwl_mvm *mvm,
 		struct ieee80211_key_seq seq;
 		const u8 *pn;
 
+		switch (keyconf->cipher) {
+		case WLAN_CIPHER_SUITE_AES_CMAC:
+			igtk_cmd.ctrl_flags |= cpu_to_le32(STA_KEY_FLG_CCM);
+			break;
+		default:
+			return -EINVAL;
+		}
+
 		memcpy(igtk_cmd.IGTK, keyconf->key, keyconf->keylen);
 		ieee80211_aes_cmac_calculate_k1_k2(keyconf,
 						   igtk_cmd.K1, igtk_cmd.K2);
