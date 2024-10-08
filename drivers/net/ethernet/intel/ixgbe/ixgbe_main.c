a7bc0ccbe7d860342a5da14eba3bc41462add042
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:33:10 -0400

    ixgbe: avoid duplicate code in suspend and stop paths
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    commit-author Jacob Keller <jacob.e.keller@intel.com>
    commit a0cccce2cec76568190090f7bb028916d8b3210e
    
    Resume path calls .open but suspend path cannot call .stop because
    fdirs should not be freed and control over hardware should not be
    released until WoL is configured.  To avoid having to duplicate all
    changes made in .stop on suspend path split out part of .stop that
    is relevant during suspend and call it from .stop and during suspend.
    
    This fix also ensures that ixgbe_ptp_suspend is called during the
    suspend path, and helps avoid similar errors. We can't call
    ixgbe_ptp_stop, since it will free the PTP clock device, which we
    shouldn't be doing during a suspend path.
    
    	Signed-off-by: Jakub Kicinski <kubakici@wp.pl>
    	Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    	Tested-by: Phil Schmitt <phillip.j.schmitt@intel.com>
    	Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit a0cccce2cec76568190090f7bb028916d8b3210e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -5357,6 +5357,17 @@ err_setup_tx:
 	return err;
 }
 
+static void ixgbe_close_suspend(struct ixgbe_adapter *adapter)
+{
+	ixgbe_ptp_suspend(adapter);
+
+	ixgbe_down(adapter);
+	ixgbe_free_irq(adapter);
+
+	ixgbe_free_all_tx_resources(adapter);
+	ixgbe_free_all_rx_resources(adapter);
+}
+
 /**
  * ixgbe_close - Disables a network interface
  * @netdev: network interface device structure
@@ -5374,13 +5385,9 @@ static int ixgbe_close(struct net_device *netdev)
 
 	ixgbe_ptp_stop(adapter);
 
-	ixgbe_down(adapter);
-	ixgbe_free_irq(adapter);
+	ixgbe_close_suspend(adapter);
 
 	ixgbe_fdir_filter_exit(adapter);
-
-	ixgbe_free_all_tx_resources(adapter);
-	ixgbe_free_all_rx_resources(adapter);
 
 	ixgbe_release_hw_control(adapter);
 
@@ -5448,12 +5455,8 @@ static int __ixgbe_shutdown(struct pci_dev *pdev, bool *enable_wake)
 	netif_device_detach(netdev);
 
 	rtnl_lock();
-	if (netif_running(netdev)) {
-		ixgbe_down(adapter);
-		ixgbe_free_irq(adapter);
-		ixgbe_free_all_tx_resources(adapter);
-		ixgbe_free_all_rx_resources(adapter);
-	}
+	if (netif_running(netdev))
+		ixgbe_close_suspend(adapter);
 	rtnl_unlock();
 
 	ixgbe_clear_interrupt_scheme(adapter);
