20ba88e89e5498731e332a0636b6de45b4bf90bd
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:28:59 -0400

    perf evsel: Skip ignored symbols while printing callchain
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    Rebuild_CHGLOG: - [tools] perf/evsel: Skip ignored symbols while printing callchain (Jiri Olsa) [1133083]
    Rebuild_FUZZ: 98.25%
    commit-author David Ahern <dsahern@gmail.com>
    commit d2ff1b1499c8e0ad2fc79376a4215ba37771823f
    
    Allows a command to have a symbol_filter controlled by the user to skip
    certain functions in a backtrace. One example is to allow the user to
    reduce repeating patterns like:
    
        do_select  core_sys_select  sys_select
    
    to just sys_select when dumping callchains, consuming less real estate
    on the screen while still conveying the essential message - the process
    is in a select call.
    
    This option is leveraged by the upcoming timehist command.
    
    	Signed-off-by: David Ahern <dsahern@gmail.com>
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: Frederic Weisbecker <fweisbec@gmail.com>
    	Cc: Ingo Molnar <mingo@kernel.org>
    	Cc: Jiri Olsa <jolsa@redhat.com>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/r/1384806771-2945-2-git-send-email-dsahern@gmail.com
    [ Checked if al.sym is NULL before touching al.sym->ignored, as noted by Adrian Hunter ]
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit d2ff1b1499c8e0ad2fc79376a4215ba37771823f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -1528,6 +1528,9 @@ void perf_evsel__print_ip(struct perf_evsel *evsel, union perf_event *event,
 			if (!node)
 				break;
 
+			if (node->sym && node->sym->ignore)
+				goto next;
+
 			if (print_ip)
 				printf("%c%16" PRIx64, s, node->ip);
 
@@ -1550,12 +1553,15 @@ void perf_evsel__print_ip(struct perf_evsel *evsel, union perf_event *event,
 			if (!print_oneline)
 				printf("\n");
 
-			callchain_cursor_advance(&callchain_cursor);
-
 			stack_depth--;
+next:
+			callchain_cursor_advance(&callchain_cursor);
 		}
 
 	} else {
+		if (al.sym && al.sym->ignore)
+			return;
+
 		if (print_ip)
 			printf("%16" PRIx64, sample->ip);
 
