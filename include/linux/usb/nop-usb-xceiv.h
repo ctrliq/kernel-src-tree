f27184a7789067acd5d54e55fe08f557ad577eff
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:02:15 -0400

    usb: phy: generic: switch over to IS_ENABLED()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Felipe Balbi <balbi@ti.com>
    commit dca769bd5a76e9e634cc36987760306846153cac
    
    when checking if our generic PHY is enabled,
    it's a lot easier to use IS_ENABLED() instead
    of manually checking for it. While at that, also
    remove the bogus defined(MODULE) at the end of
    the line.
    
    	Signed-off-by: Felipe Balbi <balbi@ti.com>
    (cherry picked from commit dca769bd5a76e9e634cc36987760306846153cac)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/usb/nop-usb-xceiv.h
+++ include/linux/usb/nop-usb-xceiv.h
@@ -13,7 +13,7 @@ struct nop_usb_xceiv_platform_data {
 	int gpio_reset;
 };
 
-#if defined(CONFIG_NOP_USB_XCEIV) || (defined(CONFIG_NOP_USB_XCEIV_MODULE) && defined(MODULE))
+#if IS_ENABLED(CONFIG_NOP_USB_XCEIV)
 /* sometimes transceivers are accessed only through e.g. ULPI */
 extern void usb_nop_xceiv_register(void);
 extern void usb_nop_xceiv_unregister(void);
