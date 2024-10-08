f47087e8955c6d2ac4dbd03651b5f19a6b83fd79
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:10:51 -0400

    PCI: Whitespace cleanup in pci-acpi.c
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    Rebuild_CHGLOG: - [pci] Whitespace cleanup in pci-acpi.c (Myron Stowe) [1325402]
    Rebuild_FUZZ: 92.75%
    commit-author Bjorn Helgaas <bhelgaas@google.com>
    commit abbfec34e1df3073429cd6b0fad1c26635597799
    
    Whitespace fixes only; no functional change.
    
    	Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    (cherry picked from commit abbfec34e1df3073429cd6b0fad1c26635597799)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/pci/pci-acpi.c
+++ drivers/pci/pci-acpi.c
@@ -122,8 +122,8 @@ phys_addr_t acpi_pci_root_get_mcfg_addr(acpi_handle handle)
 	return (phys_addr_t)mcfg_addr;
 }
 
-static acpi_status
-decode_type0_hpx_record(union acpi_object *record, struct hotplug_params *hpx)
+static acpi_status decode_type0_hpx_record(union acpi_object *record,
+					   struct hotplug_params *hpx)
 {
 	int i;
 	union acpi_object *fields = record->package.elements;
@@ -152,8 +152,8 @@ decode_type0_hpx_record(union acpi_object *record, struct hotplug_params *hpx)
 	return AE_OK;
 }
 
-static acpi_status
-decode_type1_hpx_record(union acpi_object *record, struct hotplug_params *hpx)
+static acpi_status decode_type1_hpx_record(union acpi_object *record,
+					   struct hotplug_params *hpx)
 {
 	int i;
 	union acpi_object *fields = record->package.elements;
@@ -181,8 +181,8 @@ decode_type1_hpx_record(union acpi_object *record, struct hotplug_params *hpx)
 	return AE_OK;
 }
 
-static acpi_status
-decode_type2_hpx_record(union acpi_object *record, struct hotplug_params *hpx)
+static acpi_status decode_type2_hpx_record(union acpi_object *record,
+					   struct hotplug_params *hpx)
 {
 	int i;
 	union acpi_object *fields = record->package.elements;
@@ -223,8 +223,7 @@ decode_type2_hpx_record(union acpi_object *record, struct hotplug_params *hpx)
 	return AE_OK;
 }
 
-static acpi_status
-acpi_run_hpx(acpi_handle handle, struct hotplug_params *hpx)
+static acpi_status acpi_run_hpx(acpi_handle handle, struct hotplug_params *hpx)
 {
 	acpi_status status;
 	struct acpi_buffer buffer = {ACPI_ALLOCATE_BUFFER, NULL};
@@ -288,8 +287,7 @@ acpi_run_hpx(acpi_handle handle, struct hotplug_params *hpx)
 	return status;
 }
 
-static acpi_status
-acpi_run_hpp(acpi_handle handle, struct hotplug_params *hpp)
+static acpi_status acpi_run_hpp(acpi_handle handle, struct hotplug_params *hpp)
 {
 	acpi_status status;
 	struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
