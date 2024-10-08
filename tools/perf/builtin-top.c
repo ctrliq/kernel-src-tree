3cf6122efbfcdee5f0165ad6a123b5ea4193da89
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:00:43 -0400

    perf top: Register idle thread
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Namhyung Kim <namhyung@kernel.org>
    commit c53d138d41a7f33cf085762c64b4b61e8d223e1c
    
    The perf top didn't add the idle/swapper thread to the machine's thread
    list and its comm was displayed as ':0'.  Fix it.
    
    	Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Jiri Olsa <jolsa@redhat.com>
    	Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1443577526-3240-3-git-send-email-namhyung@kernel.org
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit c53d138d41a7f33cf085762c64b4b61e8d223e1c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/builtin-top.c
+++ tools/perf/builtin-top.c
@@ -961,6 +961,9 @@ static int __cmd_top(struct perf_top *top)
 	if (ret)
 		goto out_delete;
 
+	if (perf_session__register_idle_thread(top->session) == NULL)
+		goto out_delete;
+
 	machine__synthesize_threads(&top->session->machines.host, &opts->target,
 				    top->evlist->threads, false);
 	ret = perf_top__start_counters(top);
