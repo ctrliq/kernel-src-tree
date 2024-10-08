5287d68a53e2b5edf21e833241d19a09ab5de125
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:23:55 -0400

    efi: Export more EFI table variables to sysfs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    commit-author Dave Young <dyoung@redhat.com>
    commit a0998eb15afeffbf52a2c2829318f67df9ac57b8
    
    Export fw_vendor, runtime and config table physical addresses to
    /sys/firmware/efi/{fw_vendor,runtime,config_table} because kexec kernels
    need them.
    
    From EFI spec these 3 variables will be updated to virtual address after
    entering virtual mode. But kernel startup code will need the physical
    address.
    
    	Signed-off-by: Dave Young <dyoung@redhat.com>
    	Tested-by: Toshi Kani <toshi.kani@hp.com>
    	Signed-off-by: Matt Fleming <matt.fleming@intel.com>
    (cherry picked from commit a0998eb15afeffbf52a2c2829318f67df9ac57b8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/platform/efi/efi.c
+++ arch/x86/platform/efi/efi.c
@@ -651,6 +651,10 @@ void __init efi_init(void)
 
 	set_bit(EFI_SYSTEM_TABLES, &x86_efi_facility);
 
+	efi.config_table = (unsigned long)efi.systab->tables;
+	efi.fw_vendor	 = (unsigned long)efi.systab->fw_vendor;
+	efi.runtime	 = (unsigned long)efi.systab->runtime;
+
 	/*
 	 * Show what we know for posterity
 	 */
