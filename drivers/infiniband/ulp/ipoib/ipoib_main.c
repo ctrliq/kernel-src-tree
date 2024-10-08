62d852d293d7109e4fb491c23dd0efdae185d43c
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:28:51 -0400

    IB/Verbs: Reform IB-ulp ipoib
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [infiniband] verbs: Reform IB-ulp ipoib (Honggang Li) [1229265]
    Rebuild_FUZZ: 94.55%
    commit-author Michael Wang <yun.wang@profitbricks.com>
    commit 8e37ab68fe4d9ee47f2d7c5f45e04216ff68b2d7
    
    Use raw management helpers to reform IB-ulp ipoib.
    
    	Signed-off-by: Michael Wang <yun.wang@profitbricks.com>
    	Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    	Tested-by: Ira Weiny <ira.weiny@intel.com>
    	Reviewed-by: Sean Hefty <sean.hefty@intel.com>
    	Reviewed-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    	Tested-by: Doug Ledford <dledford@redhat.com>
    	Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from commit 8e37ab68fe4d9ee47f2d7c5f45e04216ff68b2d7)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/ulp/ipoib/ipoib_main.c
+++ drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -1661,9 +1661,7 @@ static void ipoib_add_one(struct ib_device *device)
 	struct net_device *dev;
 	struct ipoib_dev_priv *priv;
 	int s, e, p;
-
-	if (rdma_node_get_transport(device->node_type) != RDMA_TRANSPORT_IB)
-		return;
+	int count = 0;
 
 	dev_list = kmalloc(sizeof *dev_list, GFP_KERNEL);
 	if (!dev_list)
@@ -1680,13 +1678,19 @@ static void ipoib_add_one(struct ib_device *device)
 	}
 
 	for (p = s; p <= e; ++p) {
-		if (rdma_port_get_link_layer(device, p) != IB_LINK_LAYER_INFINIBAND)
+		if (!rdma_protocol_ib(device, p))
 			continue;
 		dev = ipoib_add_port("ib%d", device, p);
 		if (!IS_ERR(dev)) {
 			priv = netdev_priv(dev);
 			list_add_tail(&priv->list, dev_list);
+			count++;
 		}
+	}
+
+	if (!count) {
+		kfree(dev_list);
+		return;
 	}
 
 	ib_set_client_data(device, &ipoib_client, dev_list);
@@ -1696,9 +1700,6 @@ static void ipoib_remove_one(struct ib_device *device)
 {
 	struct ipoib_dev_priv *priv, *tmp;
 	struct list_head *dev_list;
-
-	if (rdma_node_get_transport(device->node_type) != RDMA_TRANSPORT_IB)
-		return;
 
 	dev_list = ib_get_client_data(device, &ipoib_client);
 	if (!dev_list)
