223815f2ad4f4d0c4cbff2b58906b8b5a11755e0
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:02:23 -0400

    bonding: fix bond_arp_rcv setting and arp validate desync state
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    commit-author nikolay@redhat.com <nikolay@redhat.com>
    commit 5bb9e0b50d2188d8fac481742d9f801436e2c5ab
    
    We make bond_arp_rcv global so it can be used in bond_sysfs if the bond
    interface is up and arp_interval is being changed to a positive value
    and cleared otherwise as per Jay's suggestion.
    This also fixes a problem where bond_arp_rcv was set even though
    arp_validate was disabled while the bond was up by unsetting recv_probe
    in bond_store_arp_validate and respectively setting it if enabled.
    
    	Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    	Signed-off-by: Marcelo Ricardo Leitner <mleitner@redhat.com>
    	Acked-by: Veaceslav Falico <vfalico@redhat.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 5bb9e0b50d2188d8fac481742d9f801436e2c5ab)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -414,6 +414,7 @@ static inline __be32 bond_confirm_addr(struct net_device *dev, __be32 dst, __be3
 
 struct bond_net;
 
+int bond_arp_rcv(const struct sk_buff *skb, struct bonding *bond, struct slave *slave);
 struct vlan_entry *bond_next_vlan(struct bonding *bond, struct vlan_entry *curr);
 int bond_dev_queue_xmit(struct bonding *bond, struct sk_buff *skb, struct net_device *slave_dev);
 int bond_create(struct net *net, const char *name);
