b6e89551c841ca095274ebf3e23b2f11eb46bb3e
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:17:10 -0400

    KVM: x86: pass the whole hflags field to emulator and back
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [kvm] x86: pass the whole hflags field to emulator and back (Paolo Bonzini) [1202825]
    Rebuild_FUZZ: 95.50%
    commit-author Paolo Bonzini <pbonzini@redhat.com>
    commit a584539b24b87dc8be83a713006396cabec47833
    
    The hflags field will contain information about system management mode
    and will be useful for the emulator.  Pass the entire field rather than
    just the guest-mode information.
    
    	Reviewed-by: Radim Krčmář <rkrcmar@redhat.com>
    	Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit a584539b24b87dc8be83a713006396cabec47833)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -4830,7 +4830,7 @@ int x86_emulate_insn(struct x86_emulate_ctxt *ctxt)
 				fetch_possible_mmx_operand(ctxt, &ctxt->dst);
 		}
 
-		if (unlikely(ctxt->guest_mode) && (ctxt->d & Intercept)) {
+		if (unlikely(ctxt->emul_flags & X86EMUL_GUEST_MASK) && ctxt->intercept) {
 			rc = emulator_check_intercept(ctxt, ctxt->intercept,
 						      X86_ICPT_PRE_EXCEPT);
 			if (rc != X86EMUL_CONTINUE)
@@ -4859,7 +4859,7 @@ int x86_emulate_insn(struct x86_emulate_ctxt *ctxt)
 				goto done;
 		}
 
-		if (unlikely(ctxt->guest_mode) && (ctxt->d & Intercept)) {
+		if (unlikely(ctxt->emul_flags & X86EMUL_GUEST_MASK) && (ctxt->d & Intercept)) {
 			rc = emulator_check_intercept(ctxt, ctxt->intercept,
 						      X86_ICPT_POST_EXCEPT);
 			if (rc != X86EMUL_CONTINUE)
@@ -4908,7 +4908,7 @@ int x86_emulate_insn(struct x86_emulate_ctxt *ctxt)
 
 special_insn:
 
-	if (unlikely(ctxt->guest_mode) && (ctxt->d & Intercept)) {
+	if (unlikely(ctxt->emul_flags & X86EMUL_GUEST_MASK) && (ctxt->d & Intercept)) {
 		rc = emulator_check_intercept(ctxt, ctxt->intercept,
 					      X86_ICPT_POST_MEMACCESS);
 		if (rc != X86EMUL_CONTINUE)
