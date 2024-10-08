cb299dcdf297a2eb6c089f5882c746815c4c03a4
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:44:21 -0400

    qlcnic: dcb: a couple off by one bugs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    Rebuild_CHGLOG: - [ethernet] qlcnic: dcb, a couple off by one bugs (Chad Dupuis) [1089352]
    Rebuild_FUZZ: 97.30%
    commit-author Dan Carpenter <dan.carpenter@oracle.com>
    commit e842b068bf9a9eb7b38e0e1875357440a1fd2169
    
    The ->tc_cfg[] array has QLC_DCB_MAX_TC (8) elements so the check is
    off by one.  These functions are always called with valid values though
    so it doesn't affect how the code works.
    
    	Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    	Acked-by: Sucheta Chakraborty <sucheta.chakraborty@qlogic.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit e842b068bf9a9eb7b38e0e1875357440a1fd2169)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/qlogic/qlcnic/qlcnic_dcb.c
+++ drivers/net/ethernet/qlogic/qlcnic/qlcnic_dcb.c
@@ -804,7 +804,7 @@ qlcnic_dcb_get_pg_tc_cfg_tx(struct net_device *netdev, int tc, u8 *prio,
 	    !type->tc_param_valid)
 		return;
 
-	if (tc < 0 || (tc > QLC_DCB_MAX_TC))
+	if (tc < 0 || (tc >= QLC_DCB_MAX_TC))
 		return;
 
 	tc_cfg = &type->tc_cfg[tc];
@@ -840,7 +840,7 @@ static void qlcnic_dcb_get_pg_bwg_cfg_tx(struct net_device *netdev, int pgid,
 	    !type->tc_param_valid)
 		return;
 
-	if (pgid < 0 || pgid > QLC_DCB_MAX_PG)
+	if (pgid < 0 || pgid >= QLC_DCB_MAX_PG)
 		return;
 
 	pgcfg = &type->pg_cfg[pgid];
