a434dcd3714aef1cf9a4188b37fc400623e6364e
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:20:10 -0400

    tun: Report "persist" flag to userspace
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    commit-author Pavel Emelyanov <xemul@parallels.com>
    commit 274038f8c94c493e2977983e2aecb5f5f0778479
    
    The TUN_PERSIST flag is not reported at all -- both TUNGETIFF, and sysfs
    "flags" attribute skip one. Knowing whether a device is persistent or not
    is critical for checkpoint-restore, thus I propose to add the read-only
    IFF_PERSIST one for this.
    
    Setting this new IFF_PERSIST is hardly possible, as TUNSETIFF doesn't check
    for unknown flags being zero and thus there can be trash.
    
    	Signed-off-by: Pavel Emelyanov <xemul@parallels.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 274038f8c94c493e2977983e2aecb5f5f0778479)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1596,6 +1596,9 @@ static int tun_flags(struct tun_struct *tun)
 	if (tun->flags & TUN_TAP_MQ)
 		flags |= IFF_MULTI_QUEUE;
 
+	if (tun->flags & TUN_PERSIST)
+		flags |= IFF_PERSIST;
+
 	return flags;
 }
 
