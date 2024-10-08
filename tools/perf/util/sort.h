573efb29ab0d0e45f216c4f3d4cacb276442bf36
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:19:32 -0400

    perf tools: Make cl_address global
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Jiri Olsa <jolsa@kernel.org>
    commit e95cf700b1b267d912ee779c3ab36e582111a52d
    
    It'll be used in following patches.
    
    	Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    	Cc: Andi Kleen <ak@linux.intel.com>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    	Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/r/1455525293-8671-3-git-send-email-jolsa@kernel.org
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit e95cf700b1b267d912ee779c3ab36e582111a52d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/sort.h
+++ tools/perf/util/sort.h
@@ -159,6 +159,11 @@ static inline float hist_entry__get_percent_limit(struct hist_entry *he)
 	return period * 100.0 / total_period;
 }
 
+static inline u64 cl_address(u64 address)
+{
+	/* return the cacheline of the address */
+	return (address & ~(cacheline_size - 1));
+}
 
 enum sort_mode {
 	SORT_MODE__NORMAL,
