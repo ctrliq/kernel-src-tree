8d2334bb5798e83617165c42bf1df5c4538c3307
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:22:22 -0400

    mmc: sdhci: Add a callback to select drive strength
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    Rebuild_CHGLOG: - [mmc] sdhci: Add a callback to select drive strength (Don Zickus) [1127975 1277866 1280133 1286932 1297039]
    Rebuild_FUZZ: 94.85%
    commit-author Adrian Hunter <adrian.hunter@intel.com>
    commit cb8496482e8886bbd6bd79462d8062db572b0473
    
    Add a callbak to let host drivers select drive
    strength.
    
    	Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit cb8496482e8886bbd6bd79462d8062db572b0473)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2057,6 +2057,18 @@ out:
 	return err;
 }
 
+static int sdhci_select_drive_strength(struct mmc_card *card,
+				       unsigned int max_dtr, int host_drv,
+				       int card_drv, int *drv_type)
+{
+	struct sdhci_host *host = mmc_priv(card->host);
+
+	if (!host->ops->select_drive_strength)
+		return 0;
+
+	return host->ops->select_drive_strength(host, card, max_dtr, host_drv,
+						card_drv, drv_type);
+}
 
 static void sdhci_enable_preset_value(struct sdhci_host *host, bool enable)
 {
@@ -2200,6 +2212,7 @@ static const struct mmc_host_ops sdhci_ops = {
 	.enable_sdio_irq = sdhci_enable_sdio_irq,
 	.start_signal_voltage_switch	= sdhci_start_signal_voltage_switch,
 	.execute_tuning			= sdhci_execute_tuning,
+	.select_drive_strength		= sdhci_select_drive_strength,
 	.card_event			= sdhci_card_event,
 	.card_busy	= sdhci_card_busy,
 };
