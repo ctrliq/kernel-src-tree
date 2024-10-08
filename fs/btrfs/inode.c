0a02e7421f13ea5a20b9df3a50ef49a4d113632f
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:24:30 -0400

    Btrfs: balance delayed inode updates
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    commit-author Josef Bacik <jbacik@fb.com>
    commit c581afc8db4e9aaa8af2246bb72c1bf72825014d
    
    While trying to reproduce a delayed ref problem I noticed the box kept falling
    over using all 80gb of my ram with btrfs_inode's and btrfs_delayed_node's.
    Turns out this is because we only throttle delayed inode updates in
    btrfs_dirty_inode, which doesn't actually get called that often, especially when
    all you are doing is creating a bunch of files.  So balance delayed inode
    updates everytime we create a new inode.  With this patch we no longer use up
    all of our ram with delayed inode updates.  Thanks,
    
    	Signed-off-by: Josef Bacik <jbacik@fb.com>
    (cherry picked from commit c581afc8db4e9aaa8af2246bb72c1bf72825014d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -5752,6 +5752,7 @@ static int btrfs_mknod(struct inode *dir, struct dentry *dentry,
 	}
 out_unlock:
 	btrfs_end_transaction(trans, root);
+	btrfs_balance_delayed_items(root);
 	btrfs_btree_balance_dirty(root);
 	if (drop_inode) {
 		inode_dec_link_count(inode);
@@ -5825,6 +5826,7 @@ out_unlock:
 		inode_dec_link_count(inode);
 		iput(inode);
 	}
+	btrfs_balance_delayed_items(root);
 	btrfs_btree_balance_dirty(root);
 	return err;
 }
@@ -5883,6 +5885,7 @@ static int btrfs_link(struct dentry *old_dentry, struct inode *dir,
 	}
 
 	btrfs_end_transaction(trans, root);
+	btrfs_balance_delayed_items(root);
 fail:
 	if (drop_inode) {
 		inode_dec_link_count(inode);
@@ -5949,6 +5952,7 @@ out_fail:
 	btrfs_end_transaction(trans, root);
 	if (drop_on_err)
 		iput(inode);
+	btrfs_balance_delayed_items(root);
 	btrfs_btree_balance_dirty(root);
 	return err;
 }
