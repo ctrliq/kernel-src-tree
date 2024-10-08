1e67688ff8ed166fc9695f55834fc570512d3c90
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:14:41 -0400

    HID: multitouch: Synchronize MT frame on reset_resume
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    Rebuild_CHGLOG: - [hid] multitouch: Synchronize MT frame on reset_resume (Benjamin Tissoires) [1311883]
    Rebuild_FUZZ: 95.05%
    commit-author Gabriele Mazzotta <gabriele.mzt@gmail.com>
    commit 0a0f5b7e5b56829b91d3637cf6999e0ade791ddb
    
    input_mt_get_slot_by_key() requires input_mt_sync_frame() to be called
    at each frame. Do it when releasing the touches, or else we won't get
    a proper slot number after mt_reset_resume().
    
    	Signed-off-by: Gabriele Mazzotta <gabriele.mzt@gmail.com>
    	Reviewed-by: Benson Leung <bleung@chromium.org>
    	Reviewed-by: Benjamin TIssoires <benjamin.tissoires@redhat.com>
    	Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    (cherry picked from commit 0a0f5b7e5b56829b91d3637cf6999e0ade791ddb)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1038,6 +1038,7 @@ static void mt_release_contacts(struct hid_device *hid)
 							   MT_TOOL_FINGER,
 							   false);
 			}
+			input_mt_sync_frame(input_dev);
 			input_sync(input_dev);
 		}
 	}
