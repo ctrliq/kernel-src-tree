cc06176ed10d51491d101f449cd30c8120cd0f1d
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:22:42 -0400

    ALSA: hda - Remove superfluous memory allocation error messages
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [alsa] hda - Remove superfluous memory allocation error messages (Jaroslav Kysela) [1197064]
    Rebuild_FUZZ: 95.00%
    commit-author Takashi Iwai <tiwai@suse.de>
    commit f4de8fe6cffb449a779dff61f071bd1af9e18e0f
    
    The memory allocators should have already given the kernel warning
    messages, thus we don't have to annoy again.
    
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit f4de8fe6cffb449a779dff61f071bd1af9e18e0f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/pci/hda/hda_codec.c
+++ sound/pci/hda/hda_codec.c
@@ -790,10 +790,8 @@ int snd_hda_bus_new(struct snd_card *card,
 		*busp = NULL;
 
 	bus = kzalloc(sizeof(*bus), GFP_KERNEL);
-	if (bus == NULL) {
-		dev_err(card->dev, "can't allocate struct hda_bus\n");
+	if (!bus)
 		return -ENOMEM;
-	}
 
 	bus->card = card;
 	mutex_init(&bus->cmd_mutex);
@@ -1237,10 +1235,8 @@ int snd_hda_codec_new(struct hda_bus *bus, struct snd_card *card,
 	}
 
 	codec = kzalloc(sizeof(*codec), GFP_KERNEL);
-	if (codec == NULL) {
-		dev_err(card->dev, "can't allocate struct hda_codec\n");
+	if (!codec)
 		return -ENOMEM;
-	}
 
 	dev = hda_codec_dev(codec);
 	device_initialize(dev);
@@ -1322,10 +1318,8 @@ int snd_hda_codec_new(struct hda_bus *bus, struct snd_card *card,
 
 	fg = codec->afg ? codec->afg : codec->mfg;
 	err = read_widget_caps(codec, fg);
-	if (err < 0) {
-		dev_err(card->dev, "cannot malloc\n");
+	if (err < 0)
 		goto error;
-	}
 	err = read_pin_defaults(codec);
 	if (err < 0)
 		goto error;
@@ -1391,10 +1385,8 @@ int snd_hda_codec_update_widgets(struct hda_codec *codec)
 	kfree(codec->wcaps);
 	fg = codec->afg ? codec->afg : codec->mfg;
 	err = read_widget_caps(codec, fg);
-	if (err < 0) {
-		codec_err(codec, "cannot malloc\n");
+	if (err < 0)
 		return err;
-	}
 
 	snd_array_free(&codec->init_pins);
 	err = read_pin_defaults(codec);
