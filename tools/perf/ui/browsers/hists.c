2011529e2fb6812fa8edb8fa3661a30b409e8dae
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:19:06 -0400

    perf hists browser: Offer non-symbol specific menu options for --sort without 'sym'
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Namhyung Kim <namhyung@kernel.org>
    commit 4056132e1072f02bbad77f2071770271cc5b58fc
    
    Now that we check more strictly what each of the menu entries need, we
    can stop bailing out when 'sym' is not in the --sort order, instead we
    let each option be added if what it needs is present.
    
    This way, for instance, we can run scripts on all samples, see DSO map
    details when 'dso' is in the --sort provided, etc.
    
    	Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    	Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    	Cc: Andi Kleen <andi@firstfloor.org>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Jiri Olsa <jolsa@kernel.org>,
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Stephane Eranian <eranian@google.com>
    	Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/r/1452960197-5323-9-git-send-email-namhyung@kernel.org
    [ Carved out from a  larger patch ]
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 4056132e1072f02bbad77f2071770271cc5b58fc)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/ui/browsers/hists.c
+++ tools/perf/ui/browsers/hists.c
@@ -2208,10 +2208,7 @@ static int perf_evsel__hists_browse(struct perf_evsel *evsel, int nr_events,
 			continue;
 		}
 
-		if (!sort__has_sym)
-			goto add_exit_option;
-
-		if (browser->selection == NULL)
+		if (!sort__has_sym || browser->selection == NULL)
 			goto skip_annotation;
 
 		if (sort__mode == SORT_MODE__BRANCH) {
@@ -2276,7 +2273,6 @@ skip_annotation:
 					     &options[nr_options], NULL, NULL);
 		nr_options += add_switch_opt(browser, &actions[nr_options],
 					     &options[nr_options]);
-add_exit_option:
 		nr_options += add_exit_opt(browser, &actions[nr_options],
 					   &options[nr_options]);
 
