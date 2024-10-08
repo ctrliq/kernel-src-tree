235ac0acd21a4f5f09b46936842c1a38b57b3f33
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:21:40 -0400

    mmc: core: Remove mmc_free_ext_csd()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    Rebuild_CHGLOG: - [mmc] core: Remove mmc_free_ext_csd() (Don Zickus) [1127975 1277866 1280133 1286932 1297039]
    Rebuild_FUZZ: 92.54%
    commit-author Ulf Hansson <ulf.hansson@linaro.org>
    commit 00b41b58cdb115ffc28de1adad63a59a61428db8
    
    Let callers of mmc_free_ext_csd() do kfree() directly instead.
    
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit 00b41b58cdb115ffc28de1adad63a59a61428db8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -601,12 +601,6 @@ out:
 	return err;
 }
 
-static inline void mmc_free_ext_csd(u8 *ext_csd)
-{
-	kfree(ext_csd);
-}
-
-
 static int mmc_compare_ext_csds(struct mmc_card *card, unsigned bus_width)
 {
 	u8 *bw_ext_csd;
@@ -677,7 +671,7 @@ static int mmc_compare_ext_csds(struct mmc_card *card, unsigned bus_width)
 		err = -EINVAL;
 
 out:
-	mmc_free_ext_csd(bw_ext_csd);
+	kfree(bw_ext_csd);
 	return err;
 }
 
@@ -1376,14 +1370,14 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 	if (!oldcard)
 		host->card = card;
 
-	mmc_free_ext_csd(ext_csd);
+	kfree(ext_csd);
 	return 0;
 
 free_card:
 	if (!oldcard)
 		mmc_remove_card(card);
 err:
-	mmc_free_ext_csd(ext_csd);
+	kfree(ext_csd);
 
 	return err;
 }
