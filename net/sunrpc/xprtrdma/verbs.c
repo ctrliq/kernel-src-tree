4c336912a756c2ac75719727c48d66400fe6cbe0
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:10:13 -0400

    xprtrdma: Don't fall back to PHYSICAL memory registration
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Chuck Lever <chuck.lever@oracle.com>
    commit d23109302390d61d83675a86453674446eccb776
    
    PHYSICAL memory registration uses a single rkey for all of the
    client's memory, thus is insecure. It is still useful in some cases
    for testing.
    
    Retain the ability to select PHYSICAL memory registration capability
    via /proc/sys/sunrpc/rdma_memreg_strategy, but don't fall back to it
    if the HCA does not support FRWR or FMR.
    
    This means amso1100 no longer works out of the box with NFS/RDMA.
    When using amso1100 HCAs, set the memreg_strategy sysctl to 6 before
    performing NFS/RDMA mounts.
    
    	Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    	Tested-by: Devesh Sharma <devesh.sharma@avagotech.com>
    	Reviewed-by: Christoph Hellwig <hch@lst.de>
    	Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    (cherry picked from commit d23109302390d61d83675a86453674446eccb776)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/sunrpc/xprtrdma/verbs.c
+++ net/sunrpc/xprtrdma/verbs.c
@@ -620,7 +620,7 @@ rpcrdma_ia_open(struct rpcrdma_xprt *xprt, struct sockaddr *addr, int memreg)
 		if (!ia->ri_id->device->alloc_fmr) {
 			dprintk("RPC:       %s: MTHCAFMR registration "
 				"not supported by HCA\n", __func__);
-			memreg = RPCRDMA_ALLPHYSICAL;
+			goto out3;
 		}
 	}
 
