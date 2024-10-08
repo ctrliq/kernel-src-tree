a1bd20cf249b1900824673ad71a3fe99fb418f8c
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:18:54 -0400

    perf/core: Fix perf_sched_count derailment
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Alexander Shishkin <alexander.shishkin@linux.intel.com>
    commit 927a5570855836e5d5859a80ce7e91e963545e8f
    
    The error path in perf_event_open() is such that asking for a sampling
    event on a PMU that doesn't generate interrupts will end up in dropping
    the perf_sched_count even though it hasn't been incremented for this
    event yet.
    
    Given a sufficient amount of these calls, we'll end up disabling
    scheduler's jump label even though we'd still have active events in the
    system, thereby facilitating the arrival of the infernal regions upon us.
    
    I'm fixing this by moving account_event() inside perf_event_alloc().
    
    	Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    	Cc: <stable@vger.kernel.org>
    	Cc: Arnaldo Carvalho de Melo <acme@infradead.org>
    	Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    	Cc: Jiri Olsa <jolsa@redhat.com>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Stephane Eranian <eranian@google.com>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    	Cc: Vince Weaver <vincent.weaver@maine.edu>
    	Cc: vince@deater.net
    Link: http://lkml.kernel.org/r/1456917854-29427-1-git-send-email-alexander.shishkin@linux.intel.com
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 927a5570855836e5d5859a80ce7e91e963545e8f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7411,6 +7411,9 @@ perf_event_alloc(struct perf_event_attr *attr, int cpu,
 		}
 	}
 
+	/* symmetric to unaccount_event() in _free_event() */
+	account_event(event);
+
 	return event;
 
 err_pmu:
@@ -7739,8 +7742,6 @@ SYSCALL_DEFINE5(perf_event_open,
 		}
 	}
 
-	account_event(event);
-
 	/*
 	 * Special case software events and allow them to be part of
 	 * any hardware group.
@@ -7979,8 +7980,6 @@ perf_event_create_kernel_counter(struct perf_event_attr *attr, int cpu,
 
 	/* Mark owner so we could distinguish it from user events. */
 	event->owner = EVENT_OWNER_KERNEL;
-
-	account_event(event);
 
 	ctx = find_get_context(event->pmu, task, event);
 	if (IS_ERR(ctx)) {
