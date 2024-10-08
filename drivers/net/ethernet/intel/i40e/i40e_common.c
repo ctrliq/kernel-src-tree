adf5d305f28641afc0a04898f99da145342e286c
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:24:25 -0400

    i40e: add VEB stat control and remove L2 cloud filter
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Shannon Nelson <shannon.nelson@intel.com>
    commit 8a187f448ede55147dfa2fdcacfacd795747972d
    
    With the latest firmware, statistics gathering can now be enabled and
    disabled in the HW switch, so we need to add a parameter to allow the
    driver to set it as desired.  At the same time, the L2 cloud filtering
    parameter has been removed as it was never used.
    
    Older drivers working with the newer firmware and newer drivers working
    with older firmware will not run into problems with these bits as the
    defaults are reasonable and there is no overlap in the bit definitions.
    Also, newer drivers will be forced to update because of the change in
    function call parameters, a reminder that the functionality exists.
    
    Also update copyright year.
    
    Change-ID: I9acb9160b892ca3146f2f11a88fdcd86be3cadcc
    	Signed-off-by: Shannon Nelson <shannon.nelson@intel.com>
    	Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    	Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 8a187f448ede55147dfa2fdcacfacd795747972d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/i40e/i40e_common.c
+++ drivers/net/ethernet/intel/i40e/i40e_common.c
@@ -2302,8 +2302,8 @@ i40e_status i40e_get_link_status(struct i40e_hw *hw, bool *link_up)
  * @downlink_seid: the VSI SEID
  * @enabled_tc: bitmap of TCs to be enabled
  * @default_port: true for default port VSI, false for control port
- * @enable_l2_filtering: true to add L2 filter table rules to regular forwarding rules for cloud support
  * @veb_seid: pointer to where to put the resulting VEB SEID
+ * @enable_stats: true to turn on VEB stats
  * @cmd_details: pointer to command details structure or NULL
  *
  * This asks the FW to add a VEB between the uplink and downlink
@@ -2311,8 +2311,8 @@ i40e_status i40e_get_link_status(struct i40e_hw *hw, bool *link_up)
  **/
 i40e_status i40e_aq_add_veb(struct i40e_hw *hw, u16 uplink_seid,
 				u16 downlink_seid, u8 enabled_tc,
-				bool default_port, bool enable_l2_filtering,
-				u16 *veb_seid,
+				bool default_port, u16 *veb_seid,
+				bool enable_stats,
 				struct i40e_asq_cmd_details *cmd_details)
 {
 	struct i40e_aq_desc desc;
@@ -2339,8 +2339,9 @@ i40e_status i40e_aq_add_veb(struct i40e_hw *hw, u16 uplink_seid,
 	else
 		veb_flags |= I40E_AQC_ADD_VEB_PORT_TYPE_DATA;
 
-	if (enable_l2_filtering)
-		veb_flags |= I40E_AQC_ADD_VEB_ENABLE_L2_FILTER;
+	/* reverse logic here: set the bitflag to disable the stats */
+	if (!enable_stats)
+		veb_flags |= I40E_AQC_ADD_VEB_ENABLE_DISABLE_STATS;
 
 	cmd->veb_flags = cpu_to_le16(veb_flags);
 
