d67b492ac2cb0996d28a37108636be804564c80b
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:14:42 -0400

    s390/zcrypt: Add support for new crypto express (CEX5S) adapter.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [s390] zcrypt: Add support for new crypto express (CEX5S) adapter (Hendrik Brueckner) [1182171]
    Rebuild_FUZZ: 95.08%
    commit-author Ingo Tuchscherer <ingo.tuchscherer@de.ibm.com>
    commit bdea1f1bb273383312f0eca56241794b06ed4205
    
    Extends the generic cryptographic device driver (zcrypt)
    to support the Crypto Express 5S adapter.
    
    	Signed-off-by: Ingo Tuchscherer <ingo.tuchscherer@de.ibm.com>
    	Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    (cherry picked from commit bdea1f1bb273383312f0eca56241794b06ed4205)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/s390/crypto/ap_bus.c
+++ drivers/s390/crypto/ap_bus.c
@@ -1492,9 +1492,6 @@ static void ap_scan_bus(struct work_struct *unused)
 				continue;
 			}
 			break;
-		case 11:
-			ap_dev->device_type = 10;
-			break;
 		default:
 			ap_dev->device_type = device_type;
 		}
