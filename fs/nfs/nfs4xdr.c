0bb778a33db318a1dd3fe139ac635fab2913f738
Jonathan Maple <jmaple@ciq.com>
2024-10-08 16:53:12 -0400

    NFSv4: Fix a regression against the FreeBSD server
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-123.el7
    Rebuild_CHGLOG: - [fs] nfs: Fix a regression against the FreeBSD server (Steve Dickson) [987090]
    Rebuild_FUZZ: 97.96%
    commit-author Trond Myklebust <Trond.Myklebust@netapp.com>
    commit b4a2cf76ab7c08628c62b2062dacefa496b59dfd
    
    Technically, the Linux client is allowed by the NFSv4 spec to send
    3 word bitmaps as part of an OPEN request. However, this causes the
    current FreeBSD server to return NFS4ERR_ATTRNOTSUPP errors.
    
    Fix the regression by making the Linux client use a 2 word bitmap unless
    doing NFSv4.2 with labeled NFS.
    
    	Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    (cherry picked from commit b4a2cf76ab7c08628c62b2062dacefa496b59dfd)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/nfs/nfs4xdr.c
+++ fs/nfs/nfs4xdr.c
@@ -983,6 +983,7 @@ static void encode_attrs(struct xdr_stream *xdr, const struct iattr *iap, const 
 	__be32 *p;
 	__be32 *q;
 	int len;
+	uint32_t bmval_len = 2;
 	uint32_t bmval0 = 0;
 	uint32_t bmval1 = 0;
 	uint32_t bmval2 = 0;
@@ -994,7 +995,7 @@ static void encode_attrs(struct xdr_stream *xdr, const struct iattr *iap, const 
 	 * = 40 bytes, plus any contribution from variable-length fields
 	 *            such as owner/group.
 	 */
-	len = 20;
+	len = 8;
 
 	/* Sigh */
 	if (iap->ia_valid & ATTR_SIZE)
@@ -1032,15 +1033,22 @@ static void encode_attrs(struct xdr_stream *xdr, const struct iattr *iap, const 
 		len += 16;
 	else if (iap->ia_valid & ATTR_MTIME)
 		len += 4;
+	if (label) {
+		len += 4 + 4 + 4 + (XDR_QUADLEN(label->len) << 2);
+		bmval_len = 3;
+	}
+
+	len += bmval_len << 2;
 	p = reserve_space(xdr, len);
 
 	/*
 	 * We write the bitmap length now, but leave the bitmap and the attribute
 	 * buffer length to be backfilled at the end of this routine.
 	 */
-	*p++ = cpu_to_be32(3);
+	*p++ = cpu_to_be32(bmval_len);
 	q = p;
-	p += 4;
+	/* Skip bitmap entries + attrlen */
+	p += bmval_len + 1;
 
 	if (iap->ia_valid & ATTR_SIZE) {
 		bmval0 |= FATTR4_WORD0_SIZE;
@@ -1087,10 +1095,11 @@ static void encode_attrs(struct xdr_stream *xdr, const struct iattr *iap, const 
 				len, ((char *)p - (char *)q) + 4);
 		BUG();
 	}
-	len = (char *)p - (char *)q - 16;
+	len = (char *)p - (char *)q - (bmval_len << 2);
 	*q++ = htonl(bmval0);
 	*q++ = htonl(bmval1);
-	*q++ = htonl(bmval2);
+	if (bmval_len == 3)
+		*q++ = htonl(bmval2);
 	*q = htonl(len);
 
 /* out: */
