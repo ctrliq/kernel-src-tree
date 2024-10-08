a31666f7d47f581d56ec60e5232c49d767d218db
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:00:59 -0400

    ACPI / fan: use acpi_device_xxx_power instead of acpi_bus equivelant
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    Rebuild_CHGLOG: - [acpi] fan: use acpi_device_xxx_power instead of acpi_bus equivelant (Prarit Bhargava) [1174059]
    Rebuild_FUZZ: 94.57%
    commit-author Aaron Lu <aaron.lu@intel.com>
    commit 2bb3a2bf9939f3361e25045f4ef7b136b864c3b8
    
    When we have the acpi_device pointer, there is no need to pass the
    device's handle to the acpi_bus_xxx_power functions to get/set/update
    the device's power state, instead, use the acpi_device_xxx_power
    functions directly.
    
    To make this happen for fan module, export acpi_device_update_power.
    
    	Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    	Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    (cherry picked from commit 2bb3a2bf9939f3361e25045f4ef7b136b864c3b8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/acpi/fan.c
+++ drivers/acpi/fan.c
@@ -97,7 +97,7 @@ static int fan_get_cur_state(struct thermal_cooling_device *cdev, unsigned long
 	if (!device)
 		return -EINVAL;
 
-	result = acpi_bus_update_power(device->handle, &acpi_state);
+	result = acpi_device_update_power(device, &acpi_state);
 	if (result)
 		return result;
 
@@ -115,7 +115,7 @@ fan_set_cur_state(struct thermal_cooling_device *cdev, unsigned long state)
 	if (!device || (state != 0 && state != 1))
 		return -EINVAL;
 
-	result = acpi_bus_set_power(device->handle,
+	result = acpi_device_set_power(device,
 				state ? ACPI_STATE_D0 : ACPI_STATE_D3_COLD);
 
 	return result;
@@ -139,7 +139,7 @@ static int acpi_fan_add(struct acpi_device *device)
 	strcpy(acpi_device_name(device), "Fan");
 	strcpy(acpi_device_class(device), ACPI_FAN_CLASS);
 
-	result = acpi_bus_update_power(device->handle, NULL);
+	result = acpi_device_update_power(device, NULL);
 	if (result) {
 		printk(KERN_ERR PREFIX "Setting initial power state\n");
 		goto end;
@@ -191,7 +191,7 @@ static int acpi_fan_remove(struct acpi_device *device)
 #ifdef CONFIG_PM_SLEEP
 static int acpi_fan_suspend(struct device *dev)
 {
-	acpi_bus_set_power(to_acpi_device(dev)->handle, ACPI_STATE_D0);
+	acpi_device_set_power(to_acpi_device(dev), ACPI_STATE_D0);
 
 	return AE_OK;
 }
@@ -200,7 +200,7 @@ static int acpi_fan_resume(struct device *dev)
 {
 	int result;
 
-	result = acpi_bus_update_power(to_acpi_device(dev)->handle, NULL);
+	result = acpi_device_update_power(to_acpi_device(dev), NULL);
 	if (result)
 		printk(KERN_ERR PREFIX "Error updating fan power state\n");
 
