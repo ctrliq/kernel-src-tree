49cfed9d98f7511e14fb86935604bf8dfebd64fb
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:47:18 -0400

    pm8001: clean bitmap management functions
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    commit-author Tomas Henzl <thenzl@redhat.com>
    commit ef300544723b3be25c877e520b23831eaf7830e8
    
    In the driver two different functions are used to free the same resource,
    this patch makes the code easier to read. In addittion to that, some
    minor optimisations were made too.
    
    	Signed-off-by: Tomas Henzl <thenzl@redhat.com>
    	Acked-by: Suresh Thiagarajan <Suresh.Thiagarajan@pmcs.com>
    	Acked-by: Jack Wang <xjtuwjp@gmail.com>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit ef300544723b3be25c877e520b23831eaf7830e8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/pm8001/pm8001_hwi.c
+++ drivers/scsi/pm8001/pm8001_hwi.c
@@ -3146,7 +3146,7 @@ void pm8001_mpi_set_dev_state_resp(struct pm8001_hba_info *pm8001_ha,
 	complete(pm8001_dev->setds_completion);
 	ccb->task = NULL;
 	ccb->ccb_tag = 0xFFFFFFFF;
-	pm8001_ccb_free(pm8001_ha, tag);
+	pm8001_tag_free(pm8001_ha, tag);
 }
 
 void pm8001_mpi_set_nvmd_resp(struct pm8001_hba_info *pm8001_ha, void *piomb)
@@ -3165,7 +3165,7 @@ void pm8001_mpi_set_nvmd_resp(struct pm8001_hba_info *pm8001_ha, void *piomb)
 	}
 	ccb->task = NULL;
 	ccb->ccb_tag = 0xFFFFFFFF;
-	pm8001_ccb_free(pm8001_ha, tag);
+	pm8001_tag_free(pm8001_ha, tag);
 }
 
 void
@@ -3227,7 +3227,7 @@ pm8001_mpi_get_nvmd_resp(struct pm8001_hba_info *pm8001_ha, void *piomb)
 	complete(pm8001_ha->nvmd_completion);
 	ccb->task = NULL;
 	ccb->ccb_tag = 0xFFFFFFFF;
-	pm8001_ccb_free(pm8001_ha, tag);
+	pm8001_tag_free(pm8001_ha, tag);
 }
 
 int pm8001_mpi_local_phy_ctl(struct pm8001_hba_info *pm8001_ha, void *piomb)
@@ -3634,7 +3634,7 @@ int pm8001_mpi_reg_resp(struct pm8001_hba_info *pm8001_ha, void *piomb)
 	complete(pm8001_dev->dcompletion);
 	ccb->task = NULL;
 	ccb->ccb_tag = 0xFFFFFFFF;
-	pm8001_ccb_free(pm8001_ha, htag);
+	pm8001_tag_free(pm8001_ha, htag);
 	return 0;
 }
 
@@ -3718,7 +3718,7 @@ int pm8001_mpi_fw_flash_update_resp(struct pm8001_hba_info *pm8001_ha,
 	complete(pm8001_ha->nvmd_completion);
 	ccb->task = NULL;
 	ccb->ccb_tag = 0xFFFFFFFF;
-	pm8001_ccb_free(pm8001_ha, tag);
+	pm8001_tag_free(pm8001_ha, tag);
 	return 0;
 }
 
