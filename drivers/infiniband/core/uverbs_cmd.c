b7ef64441e353e33b8b244ad2921b0bf396f36b4
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:42:02 -0400

    IB/core: Replace setting the zero values in ib_uverbs_ex_query_device
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Leon Romanovsky <leonro@mellanox.com>
    commit 2953f42513225e2cc90a7a7e3d2d5ce9a7abc799
    
    The setting to zero during variable initialization eliminates
    the need to explicitly set to zero variables and structures.
    
    	Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    	Reviewed-by: Matan Barak <matanb@mellanox.com>
    	Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from commit 2953f42513225e2cc90a7a7e3d2d5ce9a7abc799)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -3574,9 +3574,9 @@ int ib_uverbs_ex_query_device(struct ib_uverbs_file *file,
 			      struct ib_udata *ucore,
 			      struct ib_udata *uhw)
 {
-	struct ib_uverbs_ex_query_device_resp resp;
+	struct ib_uverbs_ex_query_device_resp resp = { {0} };
 	struct ib_uverbs_ex_query_device  cmd;
-	struct ib_device_attr attr;
+	struct ib_device_attr attr = {0};
 	int err;
 
 	if (ucore->inlen < sizeof(cmd))
@@ -3597,14 +3597,11 @@ int ib_uverbs_ex_query_device(struct ib_uverbs_file *file,
 	if (ucore->outlen < resp.response_length)
 		return -ENOSPC;
 
-	memset(&attr, 0, sizeof(attr));
-
 	err = ib_dev->query_device(ib_dev, &attr, uhw);
 	if (err)
 		return err;
 
 	copy_query_dev_fields(file, ib_dev, &resp.base, &attr);
-	resp.comp_mask = 0;
 
 	if (ucore->outlen < resp.response_length + sizeof(resp.odp_caps))
 		goto end;
@@ -3617,9 +3614,6 @@ int ib_uverbs_ex_query_device(struct ib_uverbs_file *file,
 		attr.odp_caps.per_transport_caps.uc_odp_caps;
 	resp.odp_caps.per_transport_caps.ud_odp_caps =
 		attr.odp_caps.per_transport_caps.ud_odp_caps;
-	resp.odp_caps.reserved = 0;
-#else
-	memset(&resp.odp_caps, 0, sizeof(resp.odp_caps));
 #endif
 	resp.response_length += sizeof(resp.odp_caps);
 
@@ -3637,8 +3631,5 @@ int ib_uverbs_ex_query_device(struct ib_uverbs_file *file,
 
 end:
 	err = ib_copy_to_udata(ucore, &resp, resp.response_length);
-	if (err)
-		return err;
-
-	return 0;
+	return err;
 }
