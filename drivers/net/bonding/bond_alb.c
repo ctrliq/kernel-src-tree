fcafc6bca12a67e14b59345c620e9ea0b2d03723
Jonathan Maple <jmaple@ciq.com>
2024-10-08 18:02:33 -0400

    bonding: remove bond read lock for bond_alb_monitor()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-327.el7
    commit-author dingtianhong <dingtianhong@huawei.com>
    commit 2d0dafb0152a6ac61cd31d38c3ef3d49463b6a57
    
    The bond slave list may change when the monitor is running, the slave list is no longer
    protected by bond->lock, only protected by rtnl lock(), so we have 3 ways to modify it:
    1.add bond_master_upper_dev_link() and bond_upper_dev_unlink() in bond->lock, but it is unsafe
    to call call_netdevice_notifiers() in write lock.
    2.remove unused bond->lock for monitor function, only use the existing rtnl lock().
    3.use rcu_read_lock() to protect it, of course, it will transform bond_for_each_slave to
    bond_for_each_slave_rcu() and performance is better, but in slow path, it is ignored.
    so I remove the bond->lock and move the rtnl lock to protect the whole monitor function.
    
    	Signed-off-by: Ding Tianhong <dingtianhong@huawei.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 2d0dafb0152a6ac61cd31d38c3ef3d49463b6a57)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/bonding/bond_alb.c
+++ drivers/net/bonding/bond_alb.c
@@ -1464,11 +1464,13 @@ void bond_alb_monitor(struct work_struct *work)
 	struct slave *slave;
 	int i;
 
-	read_lock(&bond->lock);
+	if (!rtnl_trylock())
+		goto re_arm;
 
 	if (bond->slave_cnt == 0) {
 		bond_info->tx_rebalance_counter = 0;
 		bond_info->lp_counter = 0;
+		rtnl_unlock();
 		goto re_arm;
 	}
 
@@ -1518,16 +1520,6 @@ void bond_alb_monitor(struct work_struct *work)
 		if (bond_info->primary_is_promisc &&
 		    (++bond_info->rlb_promisc_timeout_counter >= RLB_PROMISC_TIMEOUT)) {
 
-			/*
-			 * dev_set_promiscuity requires rtnl and
-			 * nothing else.  Avoid race with bond_close.
-			 */
-			read_unlock(&bond->lock);
-			if (!rtnl_trylock()) {
-				read_lock(&bond->lock);
-				goto re_arm;
-			}
-
 			bond_info->rlb_promisc_timeout_counter = 0;
 
 			/* If the primary was set to promiscuous mode
@@ -1536,9 +1528,6 @@ void bond_alb_monitor(struct work_struct *work)
 			 */
 			dev_set_promiscuity(bond->curr_active_slave->dev, -1);
 			bond_info->primary_is_promisc = 0;
-
-			rtnl_unlock();
-			read_lock(&bond->lock);
 		}
 
 		if (bond_info->rlb_rebalance) {
@@ -1561,10 +1550,9 @@ void bond_alb_monitor(struct work_struct *work)
 		}
 	}
 
+	rtnl_unlock();
 re_arm:
 	queue_delayed_work(bond->wq, &bond->alb_work, alb_delta_in_ticks);
-
-	read_unlock(&bond->lock);
 }
 
 /* assumption: called before the slave is attached to the bond
