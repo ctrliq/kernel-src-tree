43942ae4c476ff3cb9f9ebf68fa82563a3b0c799
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:29:13 -0400

    Revert "igb: Fix a deadlock in igb_sriov_reinit"
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Arika Chen <arika.chen@huawei.com>
    commit d99e366fc90c9b6e6197584ecd3a185441452b0c
    
    This reverts commit 3eb14ea8d958 ("igb: Fix a deadlock in
    igb_sriov_reinit")
    It is the same as commit f468adc944ef ("igb: missing rtnl_unlock in
    igb_sriov_reinit()")
    There is no rtnl_lock() in igb_resume before, rtnl_unlock will cause a
    deadlock.
    
    	Signed-off-by: Arika Chen <arika.chen@huawei.com>
    	Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    	Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit d99e366fc90c9b6e6197584ecd3a185441452b0c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -7582,7 +7582,6 @@ static int igb_resume(struct device *dev)
 
 	if (igb_init_interrupt_scheme(adapter, true)) {
 		dev_err(&pdev->dev, "Unable to allocate memory for queues\n");
-		rtnl_unlock();
 		return -ENOMEM;
 	}
 
