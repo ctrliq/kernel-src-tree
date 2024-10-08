1420a472b5fa3289344423d827c3c7d7e82e2ab4
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:07:34 -0400

    Drivers: hv: vmbus: fix the building warning with hyperv-keyboard
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    Rebuild_CHGLOG: - [hv] vmbus: fix the building warning with hyperv-keyboard (Vitaly Kuznetsov) [1321073]
    Rebuild_FUZZ: 88.89%
    commit-author Dexuan Cui <decui@microsoft.com>
    commit 2048157ad02e65f6327118dd4a7b9c9f1fd12f77
    
    With the recent change af3ff643ea91ba64dd8d0b1cbed54d44512f96cd
    (Drivers: hv: vmbus: Use uuid_le type consistently), we always get this
    warning:
    
      CC [M]  drivers/input/serio/hyperv-keyboard.o
    drivers/input/serio/hyperv-keyboard.c:427:2: warning: missing braces around
    	initializer [-Wmissing-braces]
      { HV_KBD_GUID, },
      ^
    drivers/input/serio/hyperv-keyboard.c:427:2: warning: (near initialization
    	for .id_table[0].guid.b.) [-Wmissing-braces]
    
    The patch fixes the warning.
    
    	Signed-off-by: Dexuan Cui <decui@microsoft.com>
    	Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 2048157ad02e65f6327118dd4a7b9c9f1fd12f77)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -1066,6 +1066,14 @@ void vmbus_driver_unregister(struct hv_driver *hv_driver);
 		}
 
 /*
+ * Keyboard GUID
+ * {f912ad6d-2b17-48ea-bd65-f927a61c7684}
+ */
+#define HV_KBD_GUID \
+	.guid = UUID_LE(0xf912ad6d, 0x2b17, 0x48ea, 0xbd, 0x65, \
+			0xf9, 0x27, 0xa6, 0x1c, 0x76, 0x84)
+
+/*
  * VSS (Backup/Restore) GUID
  */
 #define HV_VSS_GUID \
