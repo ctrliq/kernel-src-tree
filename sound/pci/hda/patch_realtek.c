4d5e550054a25b1d7f036b731bddc785972c88bf
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:04:48 -0400

    ALSA: hda - Add support of ALC255 codecs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-123.el7
    Rebuild_CHGLOG: - [alsa] hda: Add support of ALC255 codecs (Jaroslav Kysela) [1044022]
    Rebuild_FUZZ: 87.67%
    commit-author Kailang Yang <kailang@realtek.com>
    commit 1d04c9de5c76df113e4af7120feb53c628b5efcc
    
    It's just another variant of ALC269 & co.
    
    	Signed-off-by: Kailang Yang <kailang@realtek.com>
    	Cc: <stable@vger.kernel.org>
    	Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 1d04c9de5c76df113e4af7120feb53c628b5efcc)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -2526,6 +2526,7 @@ enum {
 	ALC269_TYPE_ALC282,
 	ALC269_TYPE_ALC284,
 	ALC269_TYPE_ALC286,
+	ALC269_TYPE_ALC255,
 };
 
 /*
@@ -2550,6 +2551,7 @@ static int alc269_parse_auto_config(struct hda_codec *codec)
 	case ALC269_TYPE_ALC269VD:
 	case ALC269_TYPE_ALC282:
 	case ALC269_TYPE_ALC286:
+	case ALC269_TYPE_ALC255:
 		ssids = alc269_ssids;
 		break;
 	default:
@@ -3968,6 +3970,9 @@ static int patch_alc269(struct hda_codec *codec)
 	case 0x10ec0286:
 		spec->codec_variant = ALC269_TYPE_ALC286;
 		break;
+	case 0x10ec0255:
+		spec->codec_variant = ALC269_TYPE_ALC255;
+		break;
 	}
 
 	if (snd_hda_codec_read(codec, 0x51, 0, AC_VERB_PARAMETERS, 0) == 0x10ec5505) {
@@ -4677,6 +4682,7 @@ static int patch_alc680(struct hda_codec *codec)
 static const struct hda_codec_preset snd_hda_preset_realtek[] = {
 	{ .id = 0x10ec0221, .name = "ALC221", .patch = patch_alc269 },
 	{ .id = 0x10ec0233, .name = "ALC233", .patch = patch_alc269 },
+	{ .id = 0x10ec0255, .name = "ALC255", .patch = patch_alc269 },
 	{ .id = 0x10ec0260, .name = "ALC260", .patch = patch_alc260 },
 	{ .id = 0x10ec0262, .name = "ALC262", .patch = patch_alc262 },
 	{ .id = 0x10ec0267, .name = "ALC267", .patch = patch_alc268 },
