2d23f1515ceb539180f6d006efe121072d58120d
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:13:30 -0400

    hv_netvsc: Restore needed_headroom request
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    commit-author Vitaly Kuznetsov <vkuznets@redhat.com>
    commit 14a03cf80edf3e19953bb744938e48bc9d496d30
    
    Commit c0eb454034aa ("hv_netvsc: Don't ask for additional head room in the
    skb") got rid of needed_headroom setting for the driver. With the change I
    hit the following issue trying to use ptkgen module:
    
    [   57.522021] kernel BUG at net/core/skbuff.c:1128!
    [   57.522021] invalid opcode: 0000 [#1] SMP DEBUG_PAGEALLOC
    ...
    [   58.721068] Call Trace:
    [   58.721068]  [<ffffffffa0144e86>] netvsc_start_xmit+0x4c6/0x8e0 [hv_netvsc]
    ...
    [   58.721068]  [<ffffffffa02f87fc>] ? pktgen_finalize_skb+0x25c/0x2a0 [pktgen]
    [   58.721068]  [<ffffffff814f5760>] ? __netdev_alloc_skb+0xc0/0x100
    [   58.721068]  [<ffffffffa02f9907>] pktgen_thread_worker+0x257/0x1920 [pktgen]
    
    Basically, we're calling skb_cow_head(skb, RNDIS_AND_PPI_SIZE) and crash on
        if (skb_shared(skb))
            BUG();
    
    We probably need to restore needed_headroom setting (but shrunk to
    RNDIS_AND_PPI_SIZE as we don't need more) to request the required headroom
    space. In theory, it should not give us performance penalty.
    
    	Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 14a03cf80edf3e19953bb744938e48bc9d496d30)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -892,6 +892,9 @@ static int netvsc_probe(struct hv_device *dev,
 	SET_ETHTOOL_OPS(net, &ethtool_ops);
 	SET_NETDEV_DEV(net, &dev->device);
 
+	/* We always need headroom for rndis header */
+	net->needed_headroom = RNDIS_AND_PPI_SIZE;
+
 	/* Notify the netvsc driver of the new device */
 	device_info.ring_size = ring_size;
 	ret = rndis_filter_device_add(dev, &device_info);
