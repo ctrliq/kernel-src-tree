7082860606c5b64db98f74da07d4d7b9d0720681
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:40:46 -0400

    md/raid5: close recently introduced race in stripe_head management.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    Rebuild_CHGLOG: - [md] raid5: close recently introduced race in stripe_head management (Jes Sorensen) [1085530]
    Rebuild_FUZZ: 96.92%
    commit-author NeilBrown <neilb@suse.de>
    commit 7da9d450ab2843bf1db378c156acc6304dbc1c2b
    
    As release_stripe and __release_stripe decrement ->count and then
    manipulate ->lru both under ->device_lock, it is important that
    get_active_stripe() increments ->count and clears ->lru also under
    ->device_lock.
    
    However we currently list_del_init ->lru under the lock, but increment
    the ->count outside the lock.  This can lead to races and list
    corruption.
    
    So move the atomic_inc(&sh->count) up inside the ->device_lock
    protected region.
    
    Note that we still increment ->count without device lock in the case
    where get_free_stripe() was called, and in fact don't take
    ->device_lock at all in that path.
    This is safe because if the stripe_head can be found by
    get_free_stripe, then the hash lock assures us the no-one else could
    possibly be calling release_stripe() at the same time.
    
    Fixes: 566c09c53455d7c4f1130928ef8071da1a24ea65
    	Cc: stable@vger.kernel.org (3.13)
    Reported-and-tested-by: Ian Kumlien <ian.kumlien@gmail.com>
    	Signed-off-by: NeilBrown <neilb@suse.de>
    (cherry picked from commit 7da9d450ab2843bf1db378c156acc6304dbc1c2b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -684,8 +684,10 @@ get_active_stripe(struct r5conf *conf, sector_t sector,
 					 || !conf->inactive_blocked),
 					*(conf->hash_locks + hash));
 				conf->inactive_blocked = 0;
-			} else
+			} else {
 				init_stripe(sh, sector, previous);
+				atomic_inc(&sh->count);
+			}
 		} else {
 			spin_lock(&conf->device_lock);
 			if (atomic_read(&sh->count)) {
@@ -704,12 +706,10 @@ get_active_stripe(struct r5conf *conf, sector_t sector,
 					sh->group = NULL;
 				}
 			}
+			atomic_inc(&sh->count);
 			spin_unlock(&conf->device_lock);
 		}
 	} while (sh == NULL);
-
-	if (sh)
-		atomic_inc(&sh->count);
 
 	spin_unlock_irq(conf->hash_locks + hash);
 	return sh;
