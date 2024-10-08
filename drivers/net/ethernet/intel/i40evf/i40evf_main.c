561a6edaff12240e24a36892186e8d1c20bf9c22
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:27:37 -0400

    i40evf: remove unnecessary break after goto
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-229.el7
    commit-author Fabian Frederick <fabf@skynet.be>
    commit 11029d03bf5ddea9726513bd499dc35e398bffa3
    
    	Signed-off-by: Fabian Frederick <fabf@skynet.be>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 11029d03bf5ddea9726513bd499dc35e398bffa3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/i40evf/i40evf_main.c
+++ drivers/net/ethernet/intel/i40evf/i40evf_main.c
@@ -2019,7 +2019,6 @@ static void i40evf_init_task(struct work_struct *work)
 		}
 		adapter->state = __I40EVF_INIT_VERSION_CHECK;
 		goto restart;
-		break;
 	case __I40EVF_INIT_VERSION_CHECK:
 		if (!i40evf_asq_done(hw)) {
 			dev_err(&pdev->dev, "Admin queue command never completed\n");
@@ -2045,7 +2044,6 @@ static void i40evf_init_task(struct work_struct *work)
 		}
 		adapter->state = __I40EVF_INIT_GET_RESOURCES;
 		goto restart;
-		break;
 	case __I40EVF_INIT_GET_RESOURCES:
 		/* aq msg sent, awaiting reply */
 		if (!adapter->vf_res) {
