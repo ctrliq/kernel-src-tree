6fd84f079cfee9ad5d1ee11aa5fbfe60063a3ee7
Jonathan Maple <jmaple@ciq.com>
2024-10-08 19:22:18 -0400

    mmc: host: sdhci: Use BUG_ON()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-514.el7
    Rebuild_CHGLOG: - [mmc] host: sdhci: Use BUG_ON() (Don Zickus) [1127975 1277866 1280133 1286932 1297039]
    Rebuild_FUZZ: 90.91%
    commit-author Fabio Estevam <fabio.estevam@freescale.com>
    commit bf3a35ac8038d6f3104c4b9fd8a6970c6b42b56f
    
    Use BUG_ON() instead of an 'if' condition followed by BUG().
    
    The semantic patch that makes this change is available
    in scripts/coccinelle/misc/bugon.cocci.
    
    More information about semantic patching is available at
    http://coccinelle.lip6.fr/
    
    	Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit bf3a35ac8038d6f3104c4b9fd8a6970c6b42b56f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -350,8 +350,7 @@ static void sdhci_read_block_pio(struct sdhci_host *host)
 	local_irq_save(flags);
 
 	while (blksize) {
-		if (!sg_miter_next(&host->sg_miter))
-			BUG();
+		BUG_ON(!sg_miter_next(&host->sg_miter));
 
 		len = min(host->sg_miter.length, blksize);
 
@@ -396,8 +395,7 @@ static void sdhci_write_block_pio(struct sdhci_host *host)
 	local_irq_save(flags);
 
 	while (blksize) {
-		if (!sg_miter_next(&host->sg_miter))
-			BUG();
+		BUG_ON(!sg_miter_next(&host->sg_miter));
 
 		len = min(host->sg_miter.length, blksize);
 
