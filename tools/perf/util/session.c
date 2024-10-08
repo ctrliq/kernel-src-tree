a8ba5d857b14d5a677bf125f7ef317827a1fd0b8
Jonathan Maple <jmaple@ciq.com>
2024-10-08 17:02:21 -0400

    perf tools: Fix symbol offset computation for some dsos
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-123.el7
    Rebuild_CHGLOG: - [tools] perf: Fix symbol offset computation for some dsos (Jiri Olsa) [1036665]
    Rebuild_FUZZ: 94.23%
    commit-author David Ahern <dsahern@gmail.com>
    commit 0b8c25d949e010274c6697c570b772797ebc7c27
    
    For some dsos (e.g., libc, libpthread, kernel modules) the symbol offset
    is huge. e.g.,
    
    qemu-kvm 17238/17242 [007] 762235.640311:
        ffffffff816288a1 __schedule+0x451 ([kernel.kallsyms])
        ffffffff81629609 schedule+0x29 ([kernel.kallsyms])
        ffffffffa00a6ded kvm_vcpu_block+0xffffffffa00a106d (/lib/modules/3.11.0-rc1+/kernel/arch/x86/kvm/kvm.ko)
        ffffffffa00bae6b kvm_arch_vcpu_ioctl_run+0xffffffffa00a118b (/lib/modules/3.11.0-rc1+/kernel/arch/x86/kvm/kvm.ko)
        ffffffffa00a4d7a kvm_vcpu_ioctl+0xffffffffa00a141a (/lib/modules/3.11.0-rc1+/kernel/arch/x86/kvm/kvm.ko)
        ffffffff811a7bdb do_vfs_ioctl+0x8b ([kernel.kallsyms])
        ffffffff811a80c1 sys_ioctl+0x91 ([kernel.kallsyms])
        ffffffff81633182 system_call+0x72 ([kernel.kallsyms])
            7f882a97af27 __GI___ioctl+0x7f882a891007 (/lib64/libc-2.14.90.so)
               100000002 [unknown] ([unknown])
    
    It seems to be maps with a non-0 start. Taking that into account the
    offsets are correct:
    
    qemu-kvm 17238/17242 [007] 762235.640311:
        ffffffff816288a1 __schedule+0x451 ([kernel.kallsyms])
        ffffffff81629609 schedule+0x29 ([kernel.kallsyms])
        ffffffffa00a6ded kvm_vcpu_block+0x6d (/lib/modules/3.11.0-rc1+/kernel/arch/x86/kvm/kvm.ko)
        ffffffffa00bae6b kvm_arch_vcpu_ioctl_run+0x18b (/lib/modules/3.11.0-rc1+/kernel/arch/x86/kvm/kvm.ko)
        ffffffffa00a4d7a kvm_vcpu_ioctl+0x41a (/lib/modules/3.11.0-rc1+/kernel/arch/x86/kvm/kvm.ko)
        ffffffff811a7bdb do_vfs_ioctl+0x8b ([kernel.kallsyms])
        ffffffff811a80c1 sys_ioctl+0x91 ([kernel.kallsyms])
        ffffffff81633182 system_call+0x72 ([kernel.kallsyms])
            7f882a97af27 __GI___ioctl+0x7 (/lib64/libc-2.14.90.so)
               100000002 [unknown] ([unknown])
    
    	Signed-off-by: David Ahern <dsahern@gmail.com>
    Link: http://lkml.kernel.org/r/1375026512-45826-1-git-send-email-dsahern@gmail.com
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 0b8c25d949e010274c6697c570b772797ebc7c27)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -1511,6 +1511,7 @@ void perf_evsel__print_ip(struct perf_evsel *evsel, union perf_event *event,
 				printf(" ");
 				if (print_symoffset) {
 					al.addr = node->ip;
+					al.map  = node->map;
 					symbol__fprintf_symname_offs(node->sym, &al, stdout);
 				} else
 					symbol__fprintf_symname(node->sym, stdout);
