4e1b411d073b076f8ca5c298b49a0e7db057f53f
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:55:34 -0400

    nvme-fc: correct io timeout behavior
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author James Smart <jsmart2021@gmail.com>
    commit 134aedc9c157d49069e9a98636b0a917678586ee
    
    The transport io timeout behavior wasn't quite correct. It ignored
    that the io error handler is supposed to be synchronous so it possibly
    allowed the blk request to be restarted while the io associated was
    still aborting. Timeouts on reserved commands, those used for
    association create, were never timing out thus they hung out forever.
    
    To correct:
    If an io is times out while a remoteport is not connected, just
    restart the io timer. The lack of connectivity will simultaneously
    be resetting the controller, so the reset path will abort and terminate
    the io.
    
    If an io is times out while it was marked for transport abort, just
    reset the io timer. The abort process is underway and will complete
    the io.
    
    Otherwise, if an io times out, abort the io. If the abort was
    unsuccessful (unlikely) give up and return not handled.
    
    If the abort was successful, as the abort process is underway it will
    terminate the io, so rather than synchronously waiting, just restart
    the io timer.
    
    	Signed-off-by: James Smart <james.smart@broadcom.com>
    	Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 134aedc9c157d49069e9a98636b0a917678586ee)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/nvme/host/fc.c
+++ drivers/nvme/host/fc.c
@@ -1748,13 +1748,14 @@ nvme_fc_timeout(struct request *rq, bool reserved)
 	struct nvme_fc_ctrl *ctrl = op->ctrl;
 	int ret;
 
-	if (reserved)
+	if (ctrl->rport->remoteport.port_state != FC_OBJSTATE_ONLINE ||
+			atomic_read(&op->state) == FCPOP_STATE_ABORTED)
 		return BLK_EH_RESET_TIMER;
 
 	ret = __nvme_fc_abort_op(ctrl, op);
 	if (ret)
-		/* io wasn't active to abort consider it done */
-		return BLK_EH_HANDLED;
+		/* io wasn't active to abort */
+		return BLK_EH_NOT_HANDLED;
 
 	/*
 	 * TODO: force a controller reset
@@ -1763,7 +1764,12 @@ nvme_fc_timeout(struct request *rq, bool reserved)
 	 *   will no longer be needed.
 	 */
 
-	return BLK_EH_HANDLED;
+	/*
+	 * the io abort has been initiated. Have the reset timer
+	 * restarted and the abort completion will complete the io
+	 * shortly. Avoids a synchronous wait while the abort finishes.
+	 */
+	return BLK_EH_RESET_TIMER;
 }
 
 static int
