f62b2c439e3280687c057dfbc59f241cb127244d
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:02:11 -0400

    nfp: bpf: byte swap the instructions
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Jakub Kicinski <jakub.kicinski@netronome.com>
    commit 2e85d3884f25a0419a941676d1a7c25779884be2
    
    Device expects the instructions in little endian.  Make sure we
    byte swap on big endian hosts.
    
    	Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    	Reviewed-by: Simon Horman <simon.horman@netronome.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 2e85d3884f25a0419a941676d1a7c25779884be2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/netronome/nfp/bpf/jit.c
+++ drivers/net/ethernet/netronome/nfp/bpf/jit.c
@@ -1703,7 +1703,7 @@ static int nfp_bpf_optimize(struct nfp_prog *nfp_prog)
 	return 0;
 }
 
-static int nfp_bpf_ustore_calc(struct nfp_prog *nfp_prog)
+static int nfp_bpf_ustore_calc(struct nfp_prog *nfp_prog, __le64 *ustore)
 {
 	int i;
 
@@ -1715,6 +1715,8 @@ static int nfp_bpf_ustore_calc(struct nfp_prog *nfp_prog)
 			return err;
 
 		nfp_prog->prog[i] = nfp_ustore_calc_ecc_insn(nfp_prog->prog[i]);
+
+		ustore[i] = cpu_to_le64(nfp_prog->prog[i]);
 	}
 
 	return 0;
@@ -1774,7 +1776,7 @@ nfp_bpf_jit(struct bpf_prog *filter, void *prog_mem,
 		goto out;
 	}
 
-	ret = nfp_bpf_ustore_calc(nfp_prog);
+	ret = nfp_bpf_ustore_calc(nfp_prog, (__force __le64 *)prog_mem);
 
 	res->n_instr = nfp_prog->prog_len;
 	res->dense_mode = false;
