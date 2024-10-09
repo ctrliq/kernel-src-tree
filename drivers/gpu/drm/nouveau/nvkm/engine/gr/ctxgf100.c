8f058d25f4b519c76898b56e575270b5d5f15f33
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:16:27 -0400

    drm/nouveau/gr/gf100-: virtualise r4060a8 + apply fixes from traces
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Ben Skeggs <bskeggs@redhat.com>
    commit 9d8a80df73b58c700e36a0051b2fb44f252693e2
    
    Also fixes some GPUs where we write too many registers.
    
    	Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    (cherry picked from commit 9d8a80df73b58c700e36a0051b2fb44f252693e2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/ctxgf100.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/ctxgf100.c
@@ -1086,7 +1086,9 @@ void
 gf100_grctx_generate_r4060a8(struct gf100_gr *gr)
 {
 	struct nvkm_device *device = gr->base.engine.subdev.device;
-	u8  tpcnr[GPC_MAX], data[TPC_MAX];
+	const u8 gpcmax = nvkm_rd32(device, 0x022430);
+	const u8 tpcmax = nvkm_rd32(device, 0x022434) * gpcmax;
+	u8 tpcnr[GPC_MAX], data[TPC_MAX];
 	int gpc, tpc, i;
 
 	memcpy(tpcnr, gr->tpc_nr, sizeof(gr->tpc_nr));
@@ -1101,7 +1103,7 @@ gf100_grctx_generate_r4060a8(struct gf100_gr *gr)
 		data[tpc] = gpc;
 	}
 
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < DIV_ROUND_UP(tpcmax, 4); i++)
 		nvkm_wr32(device, 0x4060a8 + (i * 4), ((u32 *)data)[i]);
 }
 
@@ -1239,6 +1241,9 @@ gf100_grctx_generate_floorsweep(struct gf100_gr *gr)
 		nvkm_wr32(device, 0x406028 + (i * 4), data);
 		nvkm_wr32(device, 0x405870 + (i * 4), data);
 	}
+
+	if (func->r4060a8)
+		func->r4060a8(gr);
 }
 
 void
@@ -1270,7 +1275,6 @@ gf100_grctx_generate_main(struct gf100_gr *gr, struct gf100_grctx *info)
 	grctx->unkn(gr);
 
 	gf100_grctx_generate_floorsweep(gr);
-	gf100_grctx_generate_r4060a8(gr);
 	gf100_grctx_generate_r418bb8(gr);
 	gf100_grctx_generate_r406800(gr);
 
@@ -1416,4 +1420,5 @@ gf100_grctx = {
 	.attrib_nr = 0x218,
 	.sm_id = gf100_grctx_generate_sm_id,
 	.tpc_nr = gf100_grctx_generate_tpc_nr,
+	.r4060a8 = gf100_grctx_generate_r4060a8,
 };
