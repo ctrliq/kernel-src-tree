9661ba77bfbc5905eb3db0f7374ad0f0da20e7b8
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:19:10 -0400

    nfp: fix nfp_cpp_read()/nfp_cpp_write() error paths
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Jakub Kicinski <jakub.kicinski@netronome.com>
    commit 7d2da6038218ea08f3df74046f32bdff3589cf00
    
    When acquiring an area fails we can't call function doing both
    release and free.
    
    	Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7d2da6038218ea08f3df74046f32bdff3589cf00)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/netronome/nfp/nfpcore/nfp_cppcore.c
+++ drivers/net/ethernet/netronome/nfp/nfpcore/nfp_cppcore.c
@@ -952,12 +952,14 @@ int nfp_cpp_read(struct nfp_cpp *cpp, u32 destination,
 			return -ENOMEM;
 
 		err = nfp_cpp_area_acquire(area);
-		if (err)
-			goto out;
+		if (err) {
+			nfp_cpp_area_free(area);
+			return err;
+		}
 	}
 
 	err = nfp_cpp_area_read(area, offset, kernel_vaddr, length);
-out:
+
 	if (cache)
 		area_cache_put(cpp, cache);
 	else
@@ -994,13 +996,14 @@ int nfp_cpp_write(struct nfp_cpp *cpp, u32 destination,
 			return -ENOMEM;
 
 		err = nfp_cpp_area_acquire(area);
-		if (err)
-			goto out;
+		if (err) {
+			nfp_cpp_area_free(area);
+			return err;
+		}
 	}
 
 	err = nfp_cpp_area_write(area, offset, kernel_vaddr, length);
 
-out:
 	if (cache)
 		area_cache_put(cpp, cache);
 	else
