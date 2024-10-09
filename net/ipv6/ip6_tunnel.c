adc434bf7a4f498e9c6612669520c8c26423b176
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:32:27 -0400

    ip6_tunnel: use the right value for ipv4 min mtu check in ip6_tnl_xmit
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    commit-author Xin Long <lucien.xin@gmail.com>
    commit 82a40777de12728dedf4075453b694f0d1baee80
    
    According to RFC791, 68 bytes is the minimum size of IPv4 datagram every
    device must be able to forward without further fragmentation while 576
    bytes is the minimum size of IPv4 datagram every device has to be able
    to receive, so in ip6_tnl_xmit(), 68(IPV4_MIN_MTU) should be the right
    value for the ipv4 min mtu check in ip6_tnl_xmit.
    
    While at it, change to use max() instead of if statement.
    
    Fixes: c9fefa08190f ("ip6_tunnel: get the min mtu properly in ip6_tnl_xmit")
    	Reported-by: Sabrina Dubroca <sd@queasysnail.net>
    	Signed-off-by: Xin Long <lucien.xin@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 82a40777de12728dedf4075453b694f0d1baee80)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/ipv6/ip6_tunnel.c
+++ net/ipv6/ip6_tunnel.c
@@ -1079,12 +1079,8 @@ int ip6_tnl_xmit(struct sk_buff *skb, struct net_device *dev, __u8 dsfield,
 		max_headroom += 8;
 		mtu -= 8;
 	}
-	if (skb->protocol == htons(ETH_P_IPV6)) {
-		if (mtu < IPV6_MIN_MTU)
-			mtu = IPV6_MIN_MTU;
-	} else if (mtu < 576) {
-		mtu = 576;
-	}
+	mtu = max(mtu, skb->protocol == htons(ETH_P_IPV6) ?
+		       IPV6_MIN_MTU : IPV4_MIN_MTU);
 
 	if (skb_dst(skb))
 		skb_dst(skb)->ops->update_pmtu(skb_dst(skb), NULL, skb, mtu);
