2f0ba60f8dfce3f19f057c6d5788c2c4e3bc2458
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:04:53 -0400

    usb: xhci: ASMedia ASM1042A chipset need shorts TX quirk
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [usb] xhci: ASMedia ASM1042A chipset need shorts TX quirk (Torez Smith) [1540721]
    Rebuild_FUZZ: 95.33%
    commit-author Corentin Labbe <clabbe.montjoie@gmail.com>
    commit d2f48f05cd2a2a0a708fbfa45f1a00a87660d937
    
    When plugging an USB webcam I see the following message:
    [106385.615559] xhci_hcd 0000:04:00.0: WARN Successful completion on short TX: needs XHCI_TRUST_TX_LENGTH quirk?
    [106390.583860] handle_tx_event: 913 callbacks suppressed
    
    With this patch applied, I get no more printing of this message.
    
    	Cc: <stable@vger.kernel.org>
    	Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    	Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit d2f48f05cd2a2a0a708fbfa45f1a00a87660d937)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -199,6 +199,9 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 	if (pdev->vendor == PCI_VENDOR_ID_ASMEDIA &&
 			pdev->device == 0x1042)
 		xhci->quirks |= XHCI_BROKEN_STREAMS;
+	if (pdev->vendor == PCI_VENDOR_ID_ASMEDIA &&
+			pdev->device == 0x1142)
+		xhci->quirks |= XHCI_TRUST_TX_LENGTH;
 
 	if (pdev->vendor == PCI_VENDOR_ID_ASMEDIA &&
 		pdev->device == PCI_DEVICE_ID_ASMEDIA_1042A_XHCI)
