abb27b617f8eb8be9214dbcb0e453d99dd092c0a
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:18:40 -0400

    tools/power turbostat: remove blank lines
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [tools] power turbostat: remove blank lines (Prarit Bhargava) [1454489]
    Rebuild_FUZZ: 92.11%
    commit-author Len Brown <len.brown@intel.com>
    commit 94d6ab4b1171ae58c11cf892fc008dc4e967de37
    
    When the user reuests to collect and show columns
    that are not present on every row (eg. for every CPU)
    turbostat still prints an (empty) line for every CPU.
    Update so no blank lines are printed.
    
    old:
    	# turbostat --quiet --show Pkg%pc6
    	Pkg%pc6
    	9.12
    	9.12
    
    	Pkg%pc6
    	9.12
    	9.12
    
    new:
    	# turbostat --quiet --show Pkg%pc6
    	Pkg%pc6
    	9.12
    	9.12
    	Pkg%pc6
    	9.12
    	9.12
    
    	Reported-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    	Signed-off-by: Len Brown <len.brown@intel.com>
    (cherry picked from commit 94d6ab4b1171ae58c11cf892fc008dc4e967de37)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -1009,7 +1009,8 @@ int format_counters(struct thread_data *t, struct core_data *c,
 	}
 
 done:
-	outp += sprintf(outp, "\n");
+	if (*(outp - 1) != '\n')
+		outp += sprintf(outp, "\n");
 
 	return 0;
 }
