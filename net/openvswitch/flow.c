e8e0b2159b76ce55b344bccdddd9b6db742c17bc
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:16:30 -0400

    openvswitch: Optimize updating for OvS flow_stats.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    Rebuild_CHGLOG: - [net] openvswitch: Optimize updating for OvS flow_stats (Jiri Benc) [1497774]
    Rebuild_FUZZ: 98.99%
    commit-author Tonghao Zhang <xiangxia.m.yue@gmail.com>
    commit c57c054eb5b1ccf230c49f736f7a018fcbc3e952
    
    In the ovs_flow_stats_update(), we only use the node
    var to alloc flow_stats struct. But this is not a
    common case, it is unnecessary to call the numa_node_id()
    everytime. This patch is not a bugfix, but there maybe
    a small increase.
    
    	Signed-off-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c57c054eb5b1ccf230c49f736f7a018fcbc3e952)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/openvswitch/flow.c
+++ net/openvswitch/flow.c
@@ -72,7 +72,6 @@ void ovs_flow_stats_update(struct sw_flow *flow, __be16 tcp_flags,
 			   const struct sk_buff *skb)
 {
 	struct flow_stats *stats;
-	int node = numa_node_id();
 	int cpu = smp_processor_id();
 	int len = skb->len + (skb_vlan_tag_present(skb) ? VLAN_HLEN : 0);
 
@@ -106,7 +105,7 @@ void ovs_flow_stats_update(struct sw_flow *flow, __be16 tcp_flags,
 					kmem_cache_alloc_node(flow_stats_cache,
 							      GFP_THISNODE |
 							      __GFP_NOMEMALLOC,
-							      node);
+							      numa_node_id());
 				if (likely(new_stats)) {
 					new_stats->used = jiffies;
 					new_stats->packet_count = 1;
