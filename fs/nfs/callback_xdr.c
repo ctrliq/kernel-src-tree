59f15a89a4a55a894a308adeef1b9d8ab3131c8a
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:42:16 -0400

    sunrpc: turn bitfield flags in svc_version into bools
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1127.el7
    commit-author Jeff Layton <jlayton@redhat.com>
    commit 05a45a2db42543c5f1a32e08f545aebbd7cb4790
    
    It's just simpler to read this way, IMO. Also, no need to explicitly
    set vs_hidden to false in the nfsacl ones.
    
    	Signed-off-by: Jeff Layton <jlayton@redhat.com>
    	Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    (cherry picked from commit 05a45a2db42543c5f1a32e08f545aebbd7cb4790)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/nfs/callback_xdr.c
+++ fs/nfs/callback_xdr.c
@@ -1088,7 +1088,7 @@ struct svc_version nfs4_callback_version1 = {
 	.vs_proc = nfs4_callback_procedures1,
 	.vs_xdrsize = NFS4_CALLBACK_XDRSIZE,
 	.vs_dispatch = NULL,
-	.vs_hidden = 1,
+	.vs_hidden = true,
 };
 
 struct svc_version nfs4_callback_version4 = {
@@ -1097,5 +1097,5 @@ struct svc_version nfs4_callback_version4 = {
 	.vs_proc = nfs4_callback_procedures1,
 	.vs_xdrsize = NFS4_CALLBACK_XDRSIZE,
 	.vs_dispatch = NULL,
-	.vs_hidden = 1,
+	.vs_hidden = true,
 };
