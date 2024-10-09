4d800c6ee504d5029ec94a8c3ecb45b9d5d662d2
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:34:37 -0400

    crypto: chelsio - Check error code with IS_ERR macro
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    Rebuild_CHGLOG: - [crypto] chelsio - Check error code with IS_ERR macro (Arjun Vynipadath) [1542351]
    Rebuild_FUZZ: 91.67%
    commit-author Yeshaswi M R Gowda <yeshaswi@chelsio.com>
    commit 40cdbe1a1bd989b608f86dbdc5e83c6ba777961b
    
    Check and return proper error code.
    
    	Signed-off-by: Jitendra Lulla <jlulla@chelsio.com>
    	Signed-off-by: Harsh Jain <harsh@chelsio.com>
    	Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit 40cdbe1a1bd989b608f86dbdc5e83c6ba777961b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/crypto/chelsio/chcr_algo.c
+++ drivers/crypto/chelsio/chcr_algo.c
@@ -899,8 +899,8 @@ static int chcr_ahash_update(struct ahash_request *req)
 	req_ctx->result = 0;
 	req_ctx->data_len += params.sg_len + params.bfr_len;
 	skb = create_hash_wr(req, &params);
-	if (!skb)
-		return -ENOMEM;
+	if (IS_ERR(skb))
+		return PTR_ERR(skb);
 
 	if (remainder) {
 		u8 *temp;
@@ -963,8 +963,8 @@ static int chcr_ahash_final(struct ahash_request *req)
 		params.more = 0;
 	}
 	skb = create_hash_wr(req, &params);
-	if (!skb)
-		return -ENOMEM;
+	if (IS_ERR(skb))
+		return PTR_ERR(skb);
 
 	skb->dev = u_ctx->lldi.ports[0];
 	set_wr_txq(skb, CPL_PRIORITY_DATA, ctx->tx_qidx);
@@ -1014,8 +1014,8 @@ static int chcr_ahash_finup(struct ahash_request *req)
 	}
 
 	skb = create_hash_wr(req, &params);
-	if (!skb)
-		return -ENOMEM;
+	if (IS_ERR(skb))
+		return PTR_ERR(skb);
 
 	skb->dev = u_ctx->lldi.ports[0];
 	set_wr_txq(skb, CPL_PRIORITY_DATA, ctx->tx_qidx);
@@ -1065,8 +1065,8 @@ static int chcr_ahash_digest(struct ahash_request *req)
 	}
 
 	skb = create_hash_wr(req, &params);
-	if (!skb)
-		return -ENOMEM;
+	if (IS_ERR(skb))
+		return PTR_ERR(skb);
 
 	skb->dev = u_ctx->lldi.ports[0];
 	set_wr_txq(skb, CPL_PRIORITY_DATA, ctx->tx_qidx);
