54251ddad1cac607fc6d5aeae9a804d7e3d37160
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:28:03 -0400

    IB/core, RDMA RW API: Do not exceed QP SGE send limit
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    commit-author Bart Van Assche <bart.vanassche@sandisk.com>
    commit 632bc3f65081dd1e2e5394a9161580a0f78e8839
    
    Compute the SGE limit for RDMA READ and WRITE requests in
    ib_create_qp(). Use that limit in the RDMA RW API implementation.
    
    	Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
    	Cc: Christoph Hellwig <hch@lst.de>
    	Cc: Sagi Grimberg <sagi@grimberg.me>
    	Cc: Steve Wise <swise@opengridcomputing.com>
    	Cc: Parav Pandit <pandit.parav@gmail.com>
    	Cc: Nicholas Bellinger <nab@linux-iscsi.org>
    	Cc: Laurence Oberman <loberman@redhat.com>
    	Cc: <stable@vger.kernel.org> #v4.7+
    	Reviewed-by: Christoph Hellwig <hch@lst.de>
    	Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from commit 632bc3f65081dd1e2e5394a9161580a0f78e8839)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -1433,6 +1433,10 @@ struct ib_srq {
 	} ext;
 };
 
+/*
+ * @max_write_sge: Maximum SGE elements per RDMA WRITE request.
+ * @max_read_sge:  Maximum SGE elements per RDMA READ request.
+ */
 struct ib_qp {
 	struct ib_device       *device;
 	struct ib_pd	       *pd;
@@ -1454,6 +1458,8 @@ struct ib_qp {
 	void                  (*event_handler)(struct ib_event *, void *);
 	void		       *qp_context;
 	u32			qp_num;
+	u32			max_write_sge;
+	u32			max_read_sge;
 	enum ib_qp_type		qp_type;
 	struct ib_rwq_ind_table *rwq_ind_tbl;
 };
