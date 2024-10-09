11afa6f625bf5905db207c3078c569d8ed2ba7be
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:31:14 -0400

    bnxt_en: Add support for ethtool -p.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    Rebuild_CHGLOG: - [netdrv] bnxt_en: Add support for ethtool -p (Jonathan Toppins) [1382378]
    Rebuild_FUZZ: 98.59%
    commit-author Michael Chan <michael.chan@broadcom.com>
    commit 5ad2cbeed74bd1e89ac4ba14288158ec7eb167da
    
    Add LED blinking code to support ethtool -p on the PF.
    
    	Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 5ad2cbeed74bd1e89ac4ba14288158ec7eb167da)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -874,6 +874,20 @@ struct bnxt_queue_info {
 	u8	queue_profile;
 };
 
+#define BNXT_MAX_LED			4
+
+struct bnxt_led_info {
+	u8	led_id;
+	u8	led_type;
+	u8	led_group_id;
+	u8	unused;
+	__le16	led_state_caps;
+#define BNXT_LED_ALT_BLINK_CAP(x)	((x) &	\
+	cpu_to_le16(PORT_LED_QCAPS_RESP_LED0_STATE_CAPS_BLINK_ALT_SUPPORTED))
+
+	__le16	led_color_caps;
+};
+
 #define BNXT_GRCPF_REG_WINDOW_BASE_OUT	0x400
 #define BNXT_CAG_REG_LEGACY_INT_STATUS	0x4014
 #define BNXT_CAG_REG_BASE		0x300000
@@ -1113,6 +1127,9 @@ struct bnxt {
 	struct ethtool_eee	eee;
 	u32			lpi_tmr_lo;
 	u32			lpi_tmr_hi;
+
+	u8			num_leds;
+	struct bnxt_led_info	leds[BNXT_MAX_LED];
 };
 
 #ifdef CONFIG_NET_RX_BUSY_POLL
