ecdde397a21cf49e431e12ec9c2585619e060489
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:11:32 -0400

    scsi: mpt3sas: Avoid sleeping in interrupt context
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    Rebuild_CHGLOG: - [scsi] mpt3sas: Avoid sleeping in interrupt context (Tomas Henzl) [1450206]
    Rebuild_FUZZ: 93.62%
    commit-author Bart Van Assche <bart.vanassche@sandisk.com>
    commit 8893cf6cb1cf56334c05120e23092dbfc9423ebb
    
    Commit 669f044170d8 ("scsi: srp_transport: Move queuecommand() wait code
    to SCSI core") can make scsi_internal_device_block() sleep.  However,
    the mpt3sas driver can call this function from an interrupt
    handler. Hence add a second argument to scsi_internal_device_block()
    that restores the old behavior of this function for the mpt3sas handler.
    
    The call chain that triggered an "IRQ handler enabled interrupts"
    complaint is as follows:
    
    _base_interrupt()
    -> _base_async_event()
       -> mpt3sas_scsih_event_callback()
          -> _scsih_check_topo_delete_events()
             -> _scsih_block_io_to_children_attached_directly()
                -> _scsih_block_io_device()
                   -> _scsih_internal_device_block()
                      -> scsi_internal_device_block()
    
    	Reported-by: Omar Sandoval <osandov@osandov.com>
    	Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
    	Cc: Omar Sandoval <osandov@osandov.com>
    	Cc: Hannes Reinecke <hare@suse.com>
    	Cc: Sagi Grimberg <sagi@grimberg.me>
    	Cc: Christoph Hellwig <hch@lst.de>
    	Cc: Sathya Prakash <sathya.prakash@broadcom.com>
    	Cc: Chaitra P B <chaitra.basappa@broadcom.com>
    	Cc: Suganath Prabu Subramani <suganath-prabu.subramani@broadcom.com>
    	Cc: Sreekanth Reddy <Sreekanth.Reddy@broadcom.com>
    	Cc: <stable@vger.kernel.org> # v4.10+
    	Tested-by: Omar Sandoval <osandov@fb.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 8893cf6cb1cf56334c05120e23092dbfc9423ebb)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -2901,6 +2901,8 @@ EXPORT_SYMBOL(scsi_target_resume);
 /**
  * scsi_internal_device_block - internal function to put a device temporarily into the SDEV_BLOCK state
  * @sdev:	device to block
+ * @wait:	Whether or not to wait until ongoing .queuecommand() /
+ *		.queue_rq() calls have finished.
  *
  * Block request made by scsi lld's to temporarily stop all
  * scsi commands on the specified device. May sleep.
@@ -2918,7 +2920,7 @@ EXPORT_SYMBOL(scsi_target_resume);
  * remove the rport mutex lock and unlock calls from srp_queuecommand().
  */
 int
-scsi_internal_device_block(struct scsi_device *sdev)
+scsi_internal_device_block(struct scsi_device *sdev, bool wait)
 {
 	struct request_queue *q = sdev->request_queue;
 	unsigned long flags;
@@ -2938,12 +2940,16 @@ scsi_internal_device_block(struct scsi_device *sdev)
 	 * request queue. 
 	 */
 	if (q->mq_ops) {
-		blk_mq_quiesce_queue(q);
+		if (wait)
+			blk_mq_quiesce_queue(q);
+		else
+			blk_mq_stop_hw_queues(q);
 	} else {
 		spin_lock_irqsave(q->queue_lock, flags);
 		blk_stop_queue(q);
 		spin_unlock_irqrestore(q->queue_lock, flags);
-		scsi_wait_for_queuecommand(sdev);
+		if (wait)
+			scsi_wait_for_queuecommand(sdev);
 	}
 
 	return 0;
@@ -3005,7 +3011,7 @@ EXPORT_SYMBOL_GPL(scsi_internal_device_unblock);
 static void
 device_block(struct scsi_device *sdev, void *data)
 {
-	scsi_internal_device_block(sdev);
+	scsi_internal_device_block(sdev, true);
 }
 
 static int
