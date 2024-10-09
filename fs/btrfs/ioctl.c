ce9a107394b65f8d04b86449e69ee8b11859a91f
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:49:06 -0400

    btrfs: make file clone aware of fatal signals
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    commit-author Wang Xiaoguang <wangxg.fnst@cn.fujitsu.com>
    commit 69ae5e4459e43e56f03d0987e865fbac2b05af2a
    
    Indeed this just make the behavior similar to xfs when process has
    fatal signals pending, and it'll make fstests/generic/298 happy.
    
    	Signed-off-by: Wang Xiaoguang <wangxg.fnst@cn.fujitsu.com>
    	Reviewed-by: David Sterba <dsterba@suse.com>
    	Signed-off-by: David Sterba <dsterba@suse.com>
    (cherry picked from commit 69ae5e4459e43e56f03d0987e865fbac2b05af2a)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -3905,6 +3905,11 @@ process_slot:
 		}
 		btrfs_release_path(path);
 		key.offset = next_key_min_offset;
+
+		if (fatal_signal_pending(current)) {
+			ret = -EINTR;
+			goto out;
+		}
 	}
 	ret = 0;
 
