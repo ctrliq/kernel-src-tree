b35812b6f3d8e4eaeb7b13e2e154dd409bf181d0
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:29:20 -0400

    md: MD_CLOSING needs to be cleared after called md_set_readonly or do_md_stop
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [md] MD_CLOSING needs to be cleared after called md_set_readonly or do_md_stop (Nigel Croxon) [1494474]
    Rebuild_FUZZ: 97.33%
    commit-author NeilBrown <neilb@suse.com>
    commit 065e519e71b2c1f41936cce75b46b5ab34adb588
    
    if called md_set_readonly and set MD_CLOSING bit, the mddev cannot
    be opened any more due to the MD_CLOING bit wasn't cleared. Thus it
    needs to be cleared in md_ioctl after any call to md_set_readonly()
    or do_md_stop().
    
    	Signed-off-by: NeilBrown <neilb@suse.com>
    Fixes: af8d8e6f0315 ("md: changes for MD_STILL_CLOSED flag")
    	Cc: stable@vger.kernel.org (v4.9+)
    	Signed-off-by: Zhilong Liu <zlliu@suse.com>
    	Signed-off-by: Shaohua Li <shli@fb.com>
    (cherry picked from commit 065e519e71b2c1f41936cce75b46b5ab34adb588)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -6853,6 +6853,7 @@ static int md_ioctl(struct block_device *bdev, fmode_t mode,
 	void __user *argp = (void __user *)arg;
 	struct mddev *mddev = NULL;
 	int ro;
+	bool did_set_md_closing = false;
 
 	if (!md_ioctl_valid(cmd))
 		return -ENOTTY;
@@ -6942,7 +6943,9 @@ static int md_ioctl(struct block_device *bdev, fmode_t mode,
 			err = -EBUSY;
 			goto out;
 		}
+		WARN_ON_ONCE(test_bit(MD_CLOSING, &mddev->flags));
 		set_bit(MD_CLOSING, &mddev->flags);
+		did_set_md_closing = true;
 		mutex_unlock(&mddev->open_mutex);
 		sync_blockdev(bdev);
 	}
@@ -7128,6 +7131,8 @@ unlock:
 		mddev->hold_active = 0;
 	mddev_unlock(mddev);
 out:
+	if(did_set_md_closing)
+		clear_bit(MD_CLOSING, &mddev->flags);
 	return err;
 }
 #ifdef CONFIG_COMPAT
