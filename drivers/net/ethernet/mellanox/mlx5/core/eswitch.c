a398a86c93b03e3cf35a2764fa01c5cf3fae8fc8
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:58:27 -0400

    net/mlx5: Prevent setting multicast macs for VFs
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    Rebuild_CHGLOG: - [netdrv] mlx5: Prevent setting multicast macs for VFs (Don Dutile) [1385330 1417286]
    Rebuild_FUZZ: 95.65%
    commit-author Mohamad Haj Yahia <mohamad@mellanox.com>
    commit ccce1700263d8b5b219359d04180492a726cea16
    
    Need to check that VF mac address entered by the admin user is either
    zero or unicast mac.
    Multicast mac addresses are prohibited.
    
    Fixes: 77256579c6b4 ('net/mlx5: E-Switch, Introduce Vport administration functions')
    	Signed-off-by: Mohamad Haj Yahia <mohamad@mellanox.com>
    	Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ccce1700263d8b5b219359d04180492a726cea16)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -1450,7 +1450,7 @@ int mlx5_eswitch_set_vport_mac(struct mlx5_eswitch *esw,
 
 	if (!ESW_ALLOWED(esw))
 		return -EPERM;
-	if (!LEGAL_VPORT(esw, vport))
+	if (!LEGAL_VPORT(esw, vport) || is_multicast_ether_addr(mac))
 		return -EINVAL;
 
 	err = mlx5_modify_nic_vport_mac_address(esw->dev, vport, mac);
