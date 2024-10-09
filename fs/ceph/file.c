50e317c1fe9eb719901c18293b6293fb1fef7b46
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:39:07 -0400

    ceph: nuke startsync op
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Yanhu Cao <gmayyyha@gmail.com>
    commit 3fb99d483e614bc3834784c7a686572c7970bb92
    
    startsync is a no-op, has been for years.  Remove it.
    
    Link: http://tracker.ceph.com/issues/20604
    	Signed-off-by: Yanhu Cao <gmayyyha@gmail.com>
    	Reviewed-by: "Yan, Zheng" <zyan@redhat.com>
    	Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit 3fb99d483e614bc3834784c7a686572c7970bb92)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/ceph/file.c
+++ fs/ceph/file.c
@@ -773,7 +773,6 @@ static void ceph_aio_retry_work(struct work_struct *work)
 	}
 
 	req->r_ops[0] = orig_req->r_ops[0];
-	osd_req_op_init(req, 1, CEPH_OSD_OP_STARTSYNC, 0);
 
 	req->r_mtime = aio_req->mtime;
 	req->r_data_offset = req->r_ops[0].extent.offset;
@@ -849,8 +848,7 @@ ceph_direct_read_write(struct kiocb *iocb, struct iov_iter *iter,
 		vino = ceph_vino(inode);
 		req = ceph_osdc_new_request(&fsc->client->osdc, &ci->i_layout,
 					    vino, pos, &size, 0,
-					    /*include a 'startsync' command*/
-					    write ? 2 : 1,
+					    1,
 					    write ? CEPH_OSD_OP_WRITE :
 						    CEPH_OSD_OP_READ,
 					    flags, snapc,
@@ -898,7 +896,6 @@ ceph_direct_read_write(struct kiocb *iocb, struct iov_iter *iter,
 			truncate_inode_pages_range(inode->i_mapping, pos,
 					(pos+len) | (PAGE_CACHE_SIZE - 1));
 
-			osd_req_op_init(req, 1, CEPH_OSD_OP_STARTSYNC, 0);
 			req->r_mtime = mtime;
 		}
 
