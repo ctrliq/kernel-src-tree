6d0c188da6421ee22e44f680d247d65d6565c0cf
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:25:59 -0400

    cls_basic: use tcf_exts_get_net() before call_rcu()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Cong Wang <xiyou.wangcong@gmail.com>
    commit 0b2a59894b7657fab46b50f176bd772aa495044f
    
    Hold netns refcnt before call_rcu() and release it after
    the tcf_exts_destroy() is done.
    
    Note, on ->destroy() path we have to respect the return value
    of tcf_exts_get_net(), on other paths it should always return
    true, so we don't need to care.
    
    	Cc: Lucas Bates <lucasb@mojatatu.com>
    	Cc: Jamal Hadi Salim <jhs@mojatatu.com>
    	Cc: Jiri Pirko <jiri@resnulli.us>
    	Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0b2a59894b7657fab46b50f176bd772aa495044f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/sched/cls_basic.c
+++ net/sched/cls_basic.c
@@ -84,16 +84,21 @@ static int basic_init(struct tcf_proto *tp)
 	return 0;
 }
 
+static void __basic_delete_filter(struct basic_filter *f)
+{
+	tcf_exts_destroy(&f->exts);
+	tcf_em_tree_destroy(&f->ematches);
+	tcf_exts_put_net(&f->exts);
+	kfree(f);
+}
+
 static void basic_delete_filter_work(struct work_struct *work)
 {
 	struct basic_filter *f = container_of(work, struct basic_filter, work);
 
 	rtnl_lock();
-	tcf_exts_destroy(&f->exts);
-	tcf_em_tree_destroy(&f->ematches);
+	__basic_delete_filter(f);
 	rtnl_unlock();
-
-	kfree(f);
 }
 
 static void basic_delete_filter(struct rcu_head *head)
@@ -115,7 +120,10 @@ static bool basic_destroy(struct tcf_proto *tp, bool force)
 	list_for_each_entry_safe(f, n, &head->flist, link) {
 		list_del_rcu(&f->link);
 		tcf_unbind_filter(tp, &f->res);
-		call_rcu(&f->rcu, basic_delete_filter);
+		if (tcf_exts_get_net(&f->exts))
+			call_rcu(&f->rcu, basic_delete_filter);
+		else
+			__basic_delete_filter(f);
 	}
 	kfree_rcu(head, rcu);
 	return true;
@@ -127,6 +135,7 @@ static int basic_delete(struct tcf_proto *tp, unsigned long arg)
 
 	list_del_rcu(&f->link);
 	tcf_unbind_filter(tp, &f->res);
+	tcf_exts_get_net(&f->exts);
 	call_rcu(&f->rcu, basic_delete_filter);
 	return 0;
 }
@@ -226,6 +235,7 @@ static int basic_change(struct net *net, struct sk_buff *in_skb,
 	if (fold) {
 		list_replace_rcu(&fold->link, &fnew->link);
 		tcf_unbind_filter(tp, &fold->res);
+		tcf_exts_get_net(&fold->exts);
 		call_rcu(&fold->rcu, basic_delete_filter);
 	} else {
 		list_add_rcu(&fnew->link, &head->flist);
