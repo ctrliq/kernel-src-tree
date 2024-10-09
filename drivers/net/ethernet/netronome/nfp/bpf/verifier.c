542dc707c1c6f5391a30a7e6092be2038235dfa0
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:02:34 -0400

    nfp: bpf: prepare for call support
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Jakub Kicinski <jakub.kicinski@netronome.com>
    commit 2cb230bdedb1b422507359ebc39d8f134300bf01
    
    Add skeleton of verifier checks and translation handler
    for call instructions.  Make sure jump target resolution
    will not treat them as jumps.
    
    	Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    	Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 2cb230bdedb1b422507359ebc39d8f134300bf01)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/netronome/nfp/bpf/verifier.c
+++ drivers/net/ethernet/netronome/nfp/bpf/verifier.c
@@ -75,6 +75,20 @@ nfp_bpf_goto_meta(struct nfp_prog *nfp_prog, struct nfp_insn_meta *meta,
 }
 
 static int
+nfp_bpf_check_call(struct nfp_prog *nfp_prog, struct nfp_insn_meta *meta)
+{
+	u32 func_id = meta->insn.imm;
+
+	switch (func_id) {
+	default:
+		pr_warn("unsupported function id: %d\n", func_id);
+		return -EOPNOTSUPP;
+	}
+
+	return 0;
+}
+
+static int
 nfp_bpf_check_exit(struct nfp_prog *nfp_prog,
 		   const struct bpf_verifier_env *env)
 {
@@ -178,6 +192,8 @@ nfp_verify_insn(struct bpf_verifier_env *env, int insn_idx, int prev_insn_idx)
 		return -EINVAL;
 	}
 
+	if (meta->insn.code == (BPF_JMP | BPF_CALL))
+		return nfp_bpf_check_call(nfp_prog, meta);
 	if (meta->insn.code == (BPF_JMP | BPF_EXIT))
 		return nfp_bpf_check_exit(priv->prog, env);
 
