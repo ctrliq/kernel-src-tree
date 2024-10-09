bccfe8e0d2cbfbae4de6dd3d918fb5e57ad32a9e
Jonathan Maple <jmaple@ciq.com>
2024-10-09 14:14:46 -0400

    sched/numa: Simplify task_numa_compare()
    jira LE-1907
    cve CVE-2019-20934
    Rebuild_History Non-Buildable kernel-3.10.0-1160.36.2.el7
    commit-author Rik van Riel <riel@redhat.com>
    commit 1c5d3eb3759013bc7ee4197aa0a9f245bdb6eb90
    
    When a task is part of a numa_group, the comparison should always use
    the group weight, in order to make workloads converge.
    
    	Signed-off-by: Rik van Riel <riel@redhat.com>
    	Cc: chegu_vinod@hp.com
    	Cc: mgorman@suse.de
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1403538378-31571-4-git-send-email-riel@redhat.com
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 1c5d3eb3759013bc7ee4197aa0a9f245bdb6eb90)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1157,7 +1157,7 @@ static void task_numa_compare(struct task_numa_env *env,
 	struct task_struct *cur;
 	long dst_load, src_load;
 	long load;
-	long imp = (groupimp > 0) ? groupimp : taskimp;
+	long imp = env->p->numa_group ? groupimp : taskimp;
 
 	rcu_read_lock();
 	cur = ACCESS_ONCE(dst_rq->curr);
@@ -1195,11 +1195,6 @@ static void task_numa_compare(struct task_numa_env *env,
 			 * itself (not part of a group), use the task weight
 			 * instead.
 			 */
-			if (env->p->numa_group)
-				imp = groupimp;
-			else
-				imp = taskimp;
-
 			if (cur->numa_group)
 				imp += group_weight(cur, env->src_nid) -
 				       group_weight(cur, env->dst_nid);
