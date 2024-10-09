8f7f34f07fc54820ce9f77b730b369a24aa6608b
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:56:52 -0400

    scsi: megaraid: Use dma_pool_zalloc()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [scsi] megaraid: Use dma_pool_zalloc() (Tomas Henzl) [1513838]
    Rebuild_FUZZ: 91.18%
    commit-author Souptick Joarder <jrdr.linux@gmail.com>
    commit 61b142afb2e28d4f06e158399409d181ad91949c
    
    Use dma_pool_zalloc() instead of dma_pool_alloc + memset
    
    	Signed-off-by: Souptick Joarder <jrdr.linux@gmail.com>
    	Acked-by: Sumit Saxena <sumit.saxena@broadcom.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 61b142afb2e28d4f06e158399409d181ad91949c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/megaraid/megaraid_sas_base.c
+++ drivers/scsi/megaraid/megaraid_sas_base.c
@@ -4002,7 +4002,7 @@ static int megasas_create_frame_pool(struct megasas_instance *instance)
 
 		cmd = instance->cmd_list[i];
 
-		cmd->frame = dma_pool_alloc(instance->frame_dma_pool,
+		cmd->frame = dma_pool_zalloc(instance->frame_dma_pool,
 					    GFP_KERNEL, &cmd->frame_phys_addr);
 
 		cmd->sense = dma_pool_alloc(instance->sense_dma_pool,
@@ -4018,7 +4018,6 @@ static int megasas_create_frame_pool(struct megasas_instance *instance)
 			return -ENOMEM;
 		}
 
-		memset(cmd->frame, 0, instance->mfi_frame_size);
 		cmd->frame->io.context = cpu_to_le32(cmd->index);
 		cmd->frame->io.pad_0 = 0;
 		if ((instance->adapter_type == MFI_SERIES) && reset_devices)
