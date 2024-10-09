9ef2dbfade9411de32b1ec673c4db8c4e2c40ff8
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:52:31 -0400

    scsi: qla2xxx: Move GPSC and GFPNID out of session management
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [scsi] qla2xxx: Move GPSC and GFPNID out of session management (Himanshu Madhani) [1628301]
    Rebuild_FUZZ: 94.83%
    commit-author Quinn Tran <quinn.tran@cavium.com>
    commit cc28e0ace97c5615cc2333d98827751cd0d794d1
    
    Move GPSC & GFPNID commands out of session management to reduce time lag
    in reporting the session state to remote port. These commands are not
    essential when it comes to maintaining the rport state. Delay sending
    these commands after rport state is set to Online.
    
    	Signed-off-by: Quinn Tran <quinn.tran@cavium.com>
    	Signed-off-by: Himanshu Madhani <himanshu.madhani@cavium.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit cc28e0ace97c5615cc2333d98827751cd0d794d1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/qla2xxx/qla_target.c
+++ drivers/scsi/qla2xxx/qla_target.c
@@ -4832,7 +4832,6 @@ static int qlt_24xx_handle_els(struct scsi_qla_host *vha,
 			switch (sess->disc_state) {
 			case DSC_LOGIN_PEND:
 			case DSC_GPDB:
-			case DSC_GPSC:
 			case DSC_UPD_FCPORT:
 			case DSC_LOGIN_COMPLETE:
 			case DSC_ADISC:
