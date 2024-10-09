0f405e57e88cfe23fdc4dba6272ee451920661a6
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:57:20 -0400

    tools include uapi: Grab a copy of linux/prctl.h
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Arnaldo Carvalho de Melo <acme@redhat.com>
    commit 433727948904301b01f1d5ebf39893c96cd4bab7
    
    We will use it to generate tables for beautifying prctl's 'option' arg
    and some of the others eventually.
    
    	Cc: Andy Lutomirski <luto@kernel.org>
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Jiri Olsa <jolsa@kernel.org>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-cg8mpmz4hk9nfih685emnbk9@git.kernel.org
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 433727948904301b01f1d5ebf39893c96cd4bab7)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/check-headers.sh
+++ tools/perf/check-headers.sh
@@ -4,6 +4,7 @@ HEADERS='
 include/uapi/linux/fcntl.h
 include/uapi/linux/kvm.h
 include/uapi/linux/perf_event.h
+include/uapi/linux/prctl.h
 include/uapi/linux/sched.h
 include/uapi/linux/stat.h
 include/uapi/linux/vhost.h
