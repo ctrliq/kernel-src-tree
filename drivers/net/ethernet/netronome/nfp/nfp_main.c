d8ebd160e6f3b5aab4904ae41ac1a1c9045bd5a2
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:19:35 -0400

    nfp: set driver VF limit
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Jakub Kicinski <jakub.kicinski@netronome.com>
    commit 651e1f2f19995c7585f34688331cda4f88e8df47
    
    PCI subsystem has support for drivers limiting the number of VFs
    available below what the IOV capability claims.  Make use of it.
    
    While at it remove the #ifdef/#endif on CONFIG_PCI_IOV, it was
    there to avoid unnecessary warnings in case device read failed
    but kernel doesn't have SR-IOV support anyway.  Device reads
    should not fail.
    
    Note that we still need the driver-internal check for the case
    where max VFs is 0 since PCI subsystem treats 0 as limit not set.
    
    	Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 651e1f2f19995c7585f34688331cda4f88e8df47)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/netronome/nfp/nfp_main.c
+++ drivers/net/ethernet/netronome/nfp/nfp_main.c
@@ -70,20 +70,22 @@ static const struct pci_device_id nfp_pci_device_ids[] = {
 	{ 0, } /* Required last entry. */
 };
 
-static void nfp_pcie_sriov_read_nfd_limit(struct nfp_pf *pf)
+static int nfp_pcie_sriov_read_nfd_limit(struct nfp_pf *pf)
 {
-#ifdef CONFIG_PCI_IOV
 	int err;
 
 	pf->limit_vfs = nfp_rtsym_read_le(pf->cpp, "nfd_vf_cfg_max_vfs", &err);
 	if (!err)
-		return;
+		return pci_sriov_set_totalvfs(pf->pdev, pf->limit_vfs);
 
 	pf->limit_vfs = ~0;
+	pci_sriov_set_totalvfs(pf->pdev, 0); /* 0 is unset */
 	/* Allow any setting for backwards compatibility if symbol not found */
-	if (err != -ENOENT)
-		nfp_warn(pf->cpp, "Warning: VF limit read failed: %d\n", err);
-#endif
+	if (err == -ENOENT)
+		return 0;
+
+	nfp_warn(pf->cpp, "Warning: VF limit read failed: %d\n", err);
+	return err;
 }
 
 static int nfp_pcie_sriov_enable(struct pci_dev *pdev, int num_vfs)
@@ -362,14 +364,18 @@ static int nfp_pci_probe(struct pci_dev *pdev,
 	if (err)
 		goto err_cpp_free;
 
-	nfp_pcie_sriov_read_nfd_limit(pf);
-
-	err = nfp_net_pci_probe(pf);
+	err = nfp_pcie_sriov_read_nfd_limit(pf);
 	if (err)
 		goto err_fw_unload;
 
+	err = nfp_net_pci_probe(pf);
+	if (err)
+		goto err_sriov_unlimit;
+
 	return 0;
 
+err_sriov_unlimit:
+	pci_sriov_set_totalvfs(pf->pdev, 0);
 err_fw_unload:
 	if (pf->fw_loaded)
 		nfp_fw_unload(pf);
@@ -394,6 +400,7 @@ static void nfp_pci_remove(struct pci_dev *pdev)
 	nfp_net_pci_remove(pf);
 
 	nfp_pcie_sriov_disable(pdev);
+	pci_sriov_set_totalvfs(pf->pdev, 0);
 
 	if (pf->fw_loaded)
 		nfp_fw_unload(pf);
