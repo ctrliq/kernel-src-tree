083eddd4a0d14f7976ae226cd1fd11177e26c2e8
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:33:45 -0400

    net: phy: realtek: add support for RTL8211
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [netdrv] phy: realtek: add support for RTL8211 (Corinna Vinschen) [1706531]
    Rebuild_FUZZ: 93.67%
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit d241d4aac93f25a48ed42c246163b6c28354b1e5
    
    In preparation of adding phylib support to the r8169 driver we need
    PHY drivers for all chip-internal PHY types. Fortunately almost all
    of them are either supported by the Realtek PHY driver already or work
    with the genphy driver.
    Still missing is support for the PHY of RTL8169s, it requires a quirk
    to properly support 100Mbit-fixed mode. The quirk was copied from
    r8169 driver which copied it from the vendor driver.
    Based on the PHYID the internal PHY seems to be a RTL8211.
    
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d241d4aac93f25a48ed42c246163b6c28354b1e5)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -151,6 +151,28 @@ static int rtl8211f_config_intr(struct phy_device *phydev)
 	return rtl8211x_page_write(phydev, 0xa42, RTL821x_INER, val);
 }
 
+static int rtl8211_config_aneg(struct phy_device *phydev)
+{
+	int ret;
+
+	ret = genphy_config_aneg(phydev);
+	if (ret < 0)
+		return ret;
+
+	/* Quirk was copied from vendor driver. Unfortunately it includes no
+	 * description of the magic numbers.
+	 */
+	if (phydev->speed == SPEED_100 && phydev->autoneg == AUTONEG_DISABLE) {
+		phy_write(phydev, 0x17, 0x2138);
+		phy_write(phydev, 0x0e, 0x0260);
+	} else {
+		phy_write(phydev, 0x17, 0x2108);
+		phy_write(phydev, 0x0e, 0x0000);
+	}
+
+	return 0;
+}
+
 static int rtl8211f_config_init(struct phy_device *phydev)
 {
 	int ret;
@@ -202,6 +224,14 @@ static struct phy_driver realtek_drvs[] = {
 		.config_intr	= &rtl8201_config_intr,
 		.suspend	= genphy_suspend,
 		.resume		= genphy_resume,
+	}, {
+		.phy_id		= 0x001cc910,
+		.name		= "RTL8211 Gigabit Ethernet",
+		.phy_id_mask	= 0x001fffff,
+		.features	= PHY_GBIT_FEATURES,
+		.config_aneg	= rtl8211_config_aneg,
+		.read_mmd	= &genphy_read_mmd_unsupported,
+		.write_mmd	= &genphy_write_mmd_unsupported,
 	}, {
 		.phy_id		= 0x001cc912,
 		.name		= "RTL8211B Gigabit Ethernet",
