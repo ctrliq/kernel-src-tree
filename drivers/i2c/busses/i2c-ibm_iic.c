6daeee425b37244dd84b57bf0a030288e145eadd
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:11:29 -0400

    i2c: Delete error messages for failed memory allocations
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [target] Delete error messages for failed memory allocations (Maurizio Lombardi) [1585081]
    Rebuild_FUZZ: 95.33%
    commit-author Markus Elfring <elfring@users.sourceforge.net>
    commit 6b3b21a8542fd2fb6ffc61bc13b9419f0c58ebad
    
    These issues were detected by using the Coccinelle software.
    
    	Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    	Signed-off-by: Wolfram Sang <wsa@kernel.org>
    (cherry picked from commit 6b3b21a8542fd2fb6ffc61bc13b9419f0c58ebad)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/i2c/busses/i2c-ibm_iic.c
+++ drivers/i2c/busses/i2c-ibm_iic.c
@@ -699,10 +699,8 @@ static int iic_probe(struct platform_device *ofdev)
 	int ret;
 
 	dev = kzalloc(sizeof(*dev), GFP_KERNEL);
-	if (!dev) {
-		dev_err(&ofdev->dev, "failed to allocate device data\n");
+	if (!dev)
 		return -ENOMEM;
-	}
 
 	dev_set_drvdata(&ofdev->dev, dev);
 
