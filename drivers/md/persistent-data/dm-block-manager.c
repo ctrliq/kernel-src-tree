ce7dee700fe369fe22cbfe86f9dc315e7875ae64
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:29:10 -0400

    dm block manager: remove redundant unlikely annotation
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    commit-author Chengguang Xu <cgxu519@gmx.com>
    commit 5941c621dc9eb61e3b22f934675577d559d1fa27
    
    unlikely has already included in IS_ERR(),
    so just remove redundant unlikely annotation.
    
    	Signed-off-by: Chengguang Xu <cgxu519@gmx.com>
    	Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    (cherry picked from commit 5941c621dc9eb61e3b22f934675577d559d1fa27)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/md/persistent-data/dm-block-manager.c
+++ drivers/md/persistent-data/dm-block-manager.c
@@ -461,7 +461,7 @@ int dm_bm_read_lock(struct dm_block_manager *bm, dm_block_t b,
 	int r;
 
 	p = dm_bufio_read(bm->bufio, b, (struct dm_buffer **) result);
-	if (unlikely(IS_ERR(p)))
+	if (IS_ERR(p))
 		return PTR_ERR(p);
 
 	aux = dm_bufio_get_aux_data(to_buffer(*result));
@@ -497,7 +497,7 @@ int dm_bm_write_lock(struct dm_block_manager *bm,
 		return -EPERM;
 
 	p = dm_bufio_read(bm->bufio, b, (struct dm_buffer **) result);
-	if (unlikely(IS_ERR(p)))
+	if (IS_ERR(p))
 		return PTR_ERR(p);
 
 	aux = dm_bufio_get_aux_data(to_buffer(*result));
@@ -530,7 +530,7 @@ int dm_bm_read_try_lock(struct dm_block_manager *bm,
 	int r;
 
 	p = dm_bufio_get(bm->bufio, b, (struct dm_buffer **) result);
-	if (unlikely(IS_ERR(p)))
+	if (IS_ERR(p))
 		return PTR_ERR(p);
 	if (unlikely(!p))
 		return -EWOULDBLOCK;
@@ -566,7 +566,7 @@ int dm_bm_write_lock_zero(struct dm_block_manager *bm,
 		return -EPERM;
 
 	p = dm_bufio_new(bm->bufio, b, (struct dm_buffer **) result);
-	if (unlikely(IS_ERR(p)))
+	if (IS_ERR(p))
 		return PTR_ERR(p);
 
 	memset(p, 0, dm_bm_block_size(bm));
