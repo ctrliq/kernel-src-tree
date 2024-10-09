682ef281356eb218d8633ce1f1d961dfda4a61bd
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:08:22 -0400

    net/mlx5e: Add counter for MPWQE filler strides
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [netdrv] mlx5e: Add counter for MPWQE filler strides (Alaa Hleihel) [1642498]
    Rebuild_FUZZ: 95.56%
    commit-author Tariq Toukan <tariqt@mellanox.com>
    commit b71ba6b46ffeeb99f4b5f52892e9029b700a9068
    
    Add ethtool counter to indicate the number of strides consumed
    by filler CQEs.
    
    	Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    	Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    (cherry picked from commit b71ba6b46ffeeb99f4b5f52892e9029b700a9068)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_stats.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_stats.h
@@ -81,7 +81,8 @@ struct mlx5e_sw_stats {
 	u64 tx_xmit_more;
 	u64 tx_cqe_err;
 	u64 rx_wqe_err;
-	u64 rx_mpwqe_filler;
+	u64 rx_mpwqe_filler_cqes;
+	u64 rx_mpwqe_filler_strides;
 	u64 rx_buff_alloc_err;
 	u64 rx_cqe_compress_blks;
 	u64 rx_cqe_compress_pkts;
@@ -170,7 +171,8 @@ struct mlx5e_rq_stats {
 	u64 xdp_tx_cqe;
 	u64 xdp_tx_full;
 	u64 wqe_err;
-	u64 mpwqe_filler;
+	u64 mpwqe_filler_cqes;
+	u64 mpwqe_filler_strides;
 	u64 buff_alloc_err;
 	u64 cqe_compress_blks;
 	u64 cqe_compress_pkts;
