b51b6e7c7ff3a434bb95280cbd770a45a02b6812
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:18:43 -0400

    perf tools: Fallback to reading sysfs to get cacheline size
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    commit-author Arnaldo Carvalho de Melo <acme@redhat.com>
    commit 9a3dc28bb015fc730f7ffc49b7b0abf830b56b61
    
    On systems where sysconf(_SC_LEVEL1_DCACHE_LINESIZE) is not available,
    such as musl LIBC and Android's bionic libc.
    
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: Chris Phlipot <cphlipot0@gmail.com>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Jiri Olsa <jolsa@kernel.org>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-772obxzby758g7m2wmzcejxz@git.kernel.org
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 9a3dc28bb015fc730f7ffc49b7b0abf830b56b61)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -494,6 +494,16 @@ void pthread__unblock_sigwinch(void)
 	pthread_sigmask(SIG_UNBLOCK, &set, NULL);
 }
 
+#ifdef _SC_LEVEL1_DCACHE_LINESIZE
+#define cache_line_size(cacheline_sizep) *cacheline_sizep = sysconf(_SC_LEVEL1_DCACHE_LINESIZE)
+#else
+static void cache_line_size(int *cacheline_sizep)
+{
+	if (sysfs__read_int("devices/system/cpu/cpu0/cache/index0/coherency_line_size", cacheline_sizep))
+		perror("cannot determine cache line size");
+}
+#endif
+
 int main(int argc, const char **argv)
 {
 	const char *cmd;
@@ -505,7 +515,7 @@ int main(int argc, const char **argv)
 
 	/* The page_size is placed in util object. */
 	page_size = sysconf(_SC_PAGE_SIZE);
-	cacheline_size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+	cache_line_size(&cacheline_size);
 
 	cmd = extract_argv0_path(argv[0]);
 	if (!cmd)
