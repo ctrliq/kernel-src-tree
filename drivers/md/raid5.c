a31150caeb0a4690fb5cd24e6e292e58838ff10c
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:29:24 -0400

    md: report sector of stripes with check mismatches
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [md] report sector of stripes with check mismatches (Nigel Croxon) [1494474]
    Rebuild_FUZZ: 95.83%
    commit-author Nix <nix@esperi.org.uk>
    commit e153903686deac9ea8c499b49516126f1a844fdb
    
    This makes it possible, with appropriate filesystem support, for a
    sysadmin to tell what is affected by the mismatch, and whether
    it should be ignored (if it's inside a swap partition, for
    instance).
    
    We ratelimit to prevent log flooding: if there are so many
    mismatches that ratelimiting is necessary, the individual messages
    are relatively unlikely to be important (either the machine is
    swapping like crazy or something is very wrong with the disk).
    
    	Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    	Signed-off-by: Shaohua Li <shli@fb.com>
    (cherry picked from commit e153903686deac9ea8c499b49516126f1a844fdb)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -4002,10 +4002,15 @@ static void handle_parity_checks5(struct r5conf *conf, struct stripe_head *sh,
 			set_bit(STRIPE_INSYNC, &sh->state);
 		else {
 			atomic64_add(STRIPE_SECTORS, &conf->mddev->resync_mismatches);
-			if (test_bit(MD_RECOVERY_CHECK, &conf->mddev->recovery))
+			if (test_bit(MD_RECOVERY_CHECK, &conf->mddev->recovery)) {
 				/* don't try to repair!! */
 				set_bit(STRIPE_INSYNC, &sh->state);
-			else {
+				pr_warn_ratelimited("%s: mismatch sector in range "
+						    "%llu-%llu\n", mdname(conf->mddev),
+						    (unsigned long long) sh->sector,
+						    (unsigned long long) sh->sector +
+						    STRIPE_SECTORS);
+			} else {
 				sh->check_state = check_state_compute_run;
 				set_bit(STRIPE_COMPUTE_RUN, &sh->state);
 				set_bit(STRIPE_OP_COMPUTE_BLK, &s->ops_request);
@@ -4154,10 +4159,15 @@ static void handle_parity_checks6(struct r5conf *conf, struct stripe_head *sh,
 			}
 		} else {
 			atomic64_add(STRIPE_SECTORS, &conf->mddev->resync_mismatches);
-			if (test_bit(MD_RECOVERY_CHECK, &conf->mddev->recovery))
+			if (test_bit(MD_RECOVERY_CHECK, &conf->mddev->recovery)) {
 				/* don't try to repair!! */
 				set_bit(STRIPE_INSYNC, &sh->state);
-			else {
+				pr_warn_ratelimited("%s: mismatch sector in range "
+						    "%llu-%llu\n", mdname(conf->mddev),
+						    (unsigned long long) sh->sector,
+						    (unsigned long long) sh->sector +
+						    STRIPE_SECTORS);
+			} else {
 				int *target = &sh->ops.target;
 
 				sh->ops.target = -1;
