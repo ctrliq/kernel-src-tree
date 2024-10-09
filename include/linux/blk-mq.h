1b4e7cce00590e8e800bed5626cde51572c170ae
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:51:26 -0400

    blk-mq: only allocate a single mq_map per tag_set
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    commit-author Christoph Hellwig <hch@lst.de>
    commit bdd17e75cd97c5c39feee409890a91d0396640fe
    
    The mapping is identical for all queues in a tag_set, so stop wasting
    memory for building multiple.  Note that for now I've kept the mq_map
    pointer in the request_queue, but we'll need to investigate if we can
    remove it without suffering too much from the additional pointer chasing.
    The same would apply to the mq_ops pointer as well.
    
    	Signed-off-by: Christoph Hellwig <hch@lst.de>
    	Reviewed-by: Keith Busch <keith.busch@intel.com>
    	Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit bdd17e75cd97c5c39feee409890a91d0396640fe)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/blk-mq.h
+++ include/linux/blk-mq.h
@@ -92,6 +92,7 @@ struct blk_mq_reg {
 };
 #else
 struct blk_mq_tag_set {
+	unsigned int		*mq_map;
 	struct blk_mq_ops	*ops;
 	unsigned int		nr_hw_queues;
 	unsigned int		queue_depth;	/* max hw supported */
