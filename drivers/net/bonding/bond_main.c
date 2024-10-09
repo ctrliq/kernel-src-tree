06b4bd8b100e7aa682df1fb6cfc03242edaa951c
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:34:27 -0400

    bonding: initialize work-queues during creation of bond
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Mahesh Bandewar <maheshb@google.com>
    commit 4493b81bea24269df898339dee638d7c5cb2b2df
    
    Initializing work-queues every time ifup operation performed is unnecessary
    and can be performed only once when the port is created.
    
    	Signed-off-by: Mahesh Bandewar <maheshb@google.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 4493b81bea24269df898339dee638d7c5cb2b2df)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -3308,8 +3308,6 @@ static int bond_open(struct net_device *bond_dev)
 		}
 	}
 
-	bond_work_init_all(bond);
-
 	if (bond_is_lb(bond)) {
 		/* bond_alb_initialize must be called before the timer
 		 * is started.
@@ -4753,6 +4751,8 @@ int bond_create(struct net *net, const char *name)
 	res = register_netdevice(bond_dev);
 
 	netif_carrier_off(bond_dev);
+
+	bond_work_init_all(bond);
 
 	rtnl_unlock();
 	if (res < 0)
