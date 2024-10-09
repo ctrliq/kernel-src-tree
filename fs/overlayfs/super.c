0f21964471045608d19e88a36f346a928e6e98a4
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:29:59 -0400

    ovl: use path_put_init() in error paths for ovl_fill_super()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    commit-author Miklos Szeredi <mszeredi@redhat.com>
    commit 8aafcb593d25e81c13be49310550e80d8788b995
    
    This allows simplifying the error cleanup later.
    
    	Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit 8aafcb593d25e81c13be49310550e80d8788b995)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -624,7 +624,7 @@ static int ovl_mount_dir_noesc(const char *name, struct path *path)
 	return 0;
 
 out_put:
-	path_put(path);
+	path_put_init(path);
 out:
 	return err;
 }
@@ -642,7 +642,7 @@ static int ovl_mount_dir(const char *name, struct path *path)
 			if (ovl_dentry_remote(path->dentry)) {
 				pr_err("overlayfs: filesystem on '%s' not supported as upperdir\n",
 				       tmp);
-				path_put(path);
+				path_put_init(path);
 				err = -EINVAL;
 			}
 		kfree(tmp);
@@ -702,7 +702,7 @@ static int ovl_lower_dir(const char *name, struct path *path,
 	return 0;
 
 out_put:
-	path_put(path);
+	path_put_init(path);
 out:
 	return err;
 }
