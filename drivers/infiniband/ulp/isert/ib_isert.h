019768c3f5f3e27c8422d586cf9699c4e2d326e5
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:11:33 -0400

    iser-target: avoid posting a recv buffer twice
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    commit-author Sagi Grimberg <sagi@grimberg.me>
    commit 7a56dc8888be23f44158a85b92da45d545cbf548
    
    We pre-allocate our send-queues and might overflow them
    in case we have multi work-request operations which tend
    to occur for large RDMA transfers over devices with limited
    allowed sg elements. When we get to a queue-full condition
    we might retry again later, so track our receive buffers
    so we don't repost them for a retry case.
    
    	Reported-by: Potnuri Bharat Teja <bharat@chelsio.com>
    	Tested-by: Potnuri Bharat Teja <bharat@chelsio.com>
    	Reviewed-by: Potnuri Bharat Teja <bharat@chelsio.com>
    	Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    	Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>
    (cherry picked from commit 7a56dc8888be23f44158a85b92da45d545cbf548)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/ulp/isert/ib_isert.h
+++ drivers/infiniband/ulp/isert/ib_isert.h
@@ -62,7 +62,7 @@
 
 #define ISER_RX_PAD_SIZE	(ISCSI_DEF_MAX_RECV_SEG_LEN + 4096 - \
 		(ISER_RX_PAYLOAD_SIZE + sizeof(u64) + sizeof(struct ib_sge) + \
-		 sizeof(struct ib_cqe)))
+		 sizeof(struct ib_cqe) + sizeof(bool)))
 
 #define ISCSI_ISER_SG_TABLESIZE		256
 
@@ -94,6 +94,7 @@ struct iser_rx_desc {
 	u64		dma_addr;
 	struct ib_sge	rx_sg;
 	struct ib_cqe	rx_cqe;
+	bool		in_use;
 	char		pad[ISER_RX_PAD_SIZE];
 } __packed;
 
