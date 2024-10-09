735ba54abc93af03941979780fa09c0fcd4605e9
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:14:27 -0400

    pagewalk: add walk_page_vma()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    commit 900fc5f197b05253ae9433fb9a066c3f37d08f69
    
    Introduce walk_page_vma(), which is useful for the callers which want to
    walk over a given vma.  It's used by later patches.
    
    	Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    	Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    	Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    	Cc: Andrea Arcangeli <aarcange@redhat.com>
    	Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    	Cc: Dave Hansen <dave.hansen@intel.com>
    	Cc: Pavel Emelyanov <xemul@parallels.com>
    	Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 900fc5f197b05253ae9433fb9a066c3f37d08f69)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/pagewalk.c
+++ mm/pagewalk.c
@@ -266,3 +266,21 @@ int walk_page_range(unsigned long addr, unsigned long end,
 
 	return err;
 }
+
+int walk_page_vma(struct vm_area_struct *vma, struct mm_walk *walk)
+{
+	int err;
+
+	if (!walk->mm)
+		return -EINVAL;
+
+	VM_BUG_ON(!rwsem_is_locked(&walk->mm->mmap_sem));
+	VM_BUG_ON(!vma);
+	walk->vma = vma;
+	err = walk_page_test(vma->vm_start, vma->vm_end, walk);
+	if (err > 0)
+		return 0;
+	if (err < 0)
+		return err;
+	return __walk_page_range(vma->vm_start, vma->vm_end, walk);
+}
