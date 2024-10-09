5463bd7fd6ca38964c2025439b660b8ee1dfc857
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:46:39 -0400

    ASoC: topology: Dont free template strings whilst they are in use.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    Rebuild_CHGLOG: - [sound] alsa: asoc: topology: Dont free template strings whilst they are in use (Jaroslav Kysela) [1463624]
    Rebuild_FUZZ: 94.89%
    commit-author Liam Girdwood <liam.r.girdwood@linux.intel.com>
    commit c3421a6a65abc636b067eb15a4c5e9cb59e91c95
    
    Template name pointers are copied when creating new widgets and are freed
    in widget destroy.
    
    	Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit c3421a6a65abc636b067eb15a4c5e9cb59e91c95)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- sound/soc/soc-topology.c
+++ sound/soc/soc-topology.c
@@ -1465,6 +1465,7 @@ static int soc_tplg_dapm_widget_create(struct soc_tplg *tplg,
 	if (template.id < 0)
 		return template.id;
 
+	/* strings are allocated here, but used and freed by the widget */
 	template.name = kstrdup(w->name, GFP_KERNEL);
 	if (!template.name)
 		return -ENOMEM;
@@ -1577,8 +1578,6 @@ widget:
 	widget->dobj.widget.kcontrol_type = kcontrol_type;
 	widget->dobj.ops = tplg->ops;
 	widget->dobj.index = tplg->index;
-	kfree(template.sname);
-	kfree(template.name);
 	list_add(&widget->dobj.list, &tplg->comp->dobj_list);
 	return 0;
 
