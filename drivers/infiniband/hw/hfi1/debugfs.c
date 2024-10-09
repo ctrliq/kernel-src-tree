1af4ba636c1b574a850793d967efc2719f1269d1
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:13:56 -0400

    IB/hfi1: Convert qp_stats debugfs interface to use new iterator API
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Mike Marciniszyn <mike.marciniszyn@intel.com>
    commit e5c197ac35950659a44b2032d6b31defbe631bf9
    
    Continue moving copy/paste code into rdmavt.
    
    	Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    	Signed-off-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    	Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    	Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from commit e5c197ac35950659a44b2032d6b31defbe631bf9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/hw/hfi1/debugfs.c
+++ drivers/infiniband/hw/hfi1/debugfs.c
@@ -1,4 +1,3 @@
-#ifdef CONFIG_DEBUG_FS
 /*
  * Copyright(c) 2015-2017 Intel Corporation.
  *
@@ -308,10 +307,10 @@ DEBUGFS_FILE_OPS(ctx_stats);
 static void *_qp_stats_seq_start(struct seq_file *s, loff_t *pos)
 	__acquires(RCU)
 {
-	struct qp_iter *iter;
+	struct rvt_qp_iter *iter;
 	loff_t n = *pos;
 
-	iter = qp_iter_init(s->private);
+	iter = rvt_qp_iter_init(s->private, 0, NULL);
 
 	/* stop calls rcu_read_unlock */
 	rcu_read_lock();
@@ -320,7 +319,7 @@ static void *_qp_stats_seq_start(struct seq_file *s, loff_t *pos)
 		return NULL;
 
 	do {
-		if (qp_iter_next(iter)) {
+		if (rvt_qp_iter_next(iter)) {
 			kfree(iter);
 			return NULL;
 		}
@@ -333,11 +332,11 @@ static void *_qp_stats_seq_next(struct seq_file *s, void *iter_ptr,
 				loff_t *pos)
 	__must_hold(RCU)
 {
-	struct qp_iter *iter = iter_ptr;
+	struct rvt_qp_iter *iter = iter_ptr;
 
 	(*pos)++;
 
-	if (qp_iter_next(iter)) {
+	if (rvt_qp_iter_next(iter)) {
 		kfree(iter);
 		return NULL;
 	}
@@ -353,7 +352,7 @@ static void _qp_stats_seq_stop(struct seq_file *s, void *iter_ptr)
 
 static int _qp_stats_seq_show(struct seq_file *s, void *iter_ptr)
 {
-	struct qp_iter *iter = iter_ptr;
+	struct rvt_qp_iter *iter = iter_ptr;
 
 	if (!iter)
 		return 0;
@@ -1573,5 +1572,3 @@ void hfi1_dbg_exit(void)
 	debugfs_remove_recursive(hfi1_dbg_root);
 	hfi1_dbg_root = NULL;
 }
-
-#endif
