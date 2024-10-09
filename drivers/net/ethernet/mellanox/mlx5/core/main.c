1bd0188c7ba5a58564e44d0041c0b99652a8d43d
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:41:29 -0400

    net/mlx5: Fix invalid pointer reference when prof_sel parameter is invalid
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    Rebuild_CHGLOG: - [netdrv] mlx5: Fix invalid pointer reference when prof_sel parameter is invalid (Don Dutile) [1385330 1417285]
    Rebuild_FUZZ: 97.22%
    commit-author Huy Nguyen <huyn@mellanox.com>
    commit 0e97a34083a03c931d4e9b34ba7899c29a09dce6
    
    When prof_sel is invalid, mlx5_core_warn is called but the
    mlx5_core_dev is not initialized yet. Solution is moving the prof_sel code
    after dev->pdev assignment
    
    Fixes: 2974ab6e8bd8 ('net/mlx5: Improve driver log messages')
    	Signed-off-by: Huy Nguyen <huyn@mellanox.com>
    	Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0e97a34083a03c931d4e9b34ba7899c29a09dce6)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1323,14 +1323,15 @@ static int init_one(struct pci_dev *pdev,
 
 	pci_set_drvdata(pdev, dev);
 
+	dev->pdev = pdev;
+	dev->event = mlx5_core_event;
+
 	if (prof_sel < 0 || prof_sel >= ARRAY_SIZE(profile)) {
 		pr_warn("selected profile out of range, selecting default (%d)\n",
 			MLX5_DEFAULT_PROF);
 		prof_sel = MLX5_DEFAULT_PROF;
 	}
 	dev->profile = &profile[prof_sel];
-	dev->pdev = pdev;
-	dev->event = mlx5_core_event;
 
 	INIT_LIST_HEAD(&priv->ctx_list);
 	spin_lock_init(&priv->ctx_lock);
