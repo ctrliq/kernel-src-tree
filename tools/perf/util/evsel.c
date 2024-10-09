48a80e2c0ab2f8d0e925404b67d5cf1335373813
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:15:44 -0400

    perf evsel: Centralize perf_sample initialization
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Jiri Olsa <jolsa@kernel.org>
    commit 3ad31d8a0df257c3f18c989119359c1f25cd009d
    
    Move the initialization bits into common place at the beginning of the
    function.
    
    Also removing some superfluous zero initialization for addr and
    transaction, because we zero all the data at the top.
    
    	Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    	Cc: Adrian Hunter <adrian.hunter@intel.com>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-1gv5t6fvv735t1rt3mxpy1h9@git.kernel.org
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 3ad31d8a0df257c3f18c989119359c1f25cd009d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -1936,6 +1936,8 @@ int perf_evsel__parse_sample(struct perf_evsel *evsel, union perf_event *event,
 	data->stream_id = data->id = data->time = -1ULL;
 	data->period = evsel->attr.sample_period;
 	data->cpumode = event->header.misc & PERF_RECORD_MISC_CPUMODE_MASK;
+	data->id = -1ULL;
+	data->data_src = PERF_MEM_DATA_SRC_NONE;
 
 	if (event->header.type != PERF_RECORD_SAMPLE) {
 		if (!evsel->attr.sample_id_all)
@@ -1953,7 +1955,6 @@ int perf_evsel__parse_sample(struct perf_evsel *evsel, union perf_event *event,
 	if (evsel->sample_size + sizeof(event->header) > event->header.size)
 		return -EFAULT;
 
-	data->id = -1ULL;
 	if (type & PERF_SAMPLE_IDENTIFIER) {
 		data->id = *array;
 		array++;
@@ -1983,7 +1984,6 @@ int perf_evsel__parse_sample(struct perf_evsel *evsel, union perf_event *event,
 		array++;
 	}
 
-	data->addr = 0;
 	if (type & PERF_SAMPLE_ADDR) {
 		data->addr = *array;
 		array++;
@@ -2147,14 +2147,12 @@ int perf_evsel__parse_sample(struct perf_evsel *evsel, union perf_event *event,
 		array++;
 	}
 
-	data->data_src = PERF_MEM_DATA_SRC_NONE;
 	if (type & PERF_SAMPLE_DATA_SRC) {
 		OVERFLOW_CHECK_u64(array);
 		data->data_src = *array;
 		array++;
 	}
 
-	data->transaction = 0;
 	if (type & PERF_SAMPLE_TRANSACTION) {
 		OVERFLOW_CHECK_u64(array);
 		data->transaction = *array;
