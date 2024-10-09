aa8b987a09ec582a0a968ad78e2db50b46abf688
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:24:33 -0400

    usb: ldusb: use irqsave() in USB's complete callback
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [usb] ldusb: use irqsave() in USB's complete callback (Torez Smith) [1657401]
    Rebuild_FUZZ: 94.95%
    commit-author Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    commit d7cdbdd024d978b9002a20223cc384d11a0a03ea
    
    The USB completion callback does not disable interrupts while acquiring
    the lock. We want to remove the local_irq_disable() invocation from
    __usb_hcd_giveback_urb() and therefore it is required for the callback
    handler to disable the interrupts while acquiring the lock.
    The callback may be invoked either in IRQ or BH context depending on the
    USB host controller.
    Use the _irqsave() variant of the locking primitives.
    
    	Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit d7cdbdd024d978b9002a20223cc384d11a0a03ea)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/usb/misc/ldusb.c
+++ drivers/usb/misc/ldusb.c
@@ -219,6 +219,7 @@ static void ld_usb_interrupt_in_callback(struct urb *urb)
 	size_t *actual_buffer;
 	unsigned int next_ring_head;
 	int status = urb->status;
+	unsigned long flags;
 	int retval;
 
 	if (status) {
@@ -230,12 +231,12 @@ static void ld_usb_interrupt_in_callback(struct urb *urb)
 			dev_dbg(&dev->intf->dev,
 				"%s: nonzero status received: %d\n", __func__,
 				status);
-			spin_lock(&dev->rbsl);
+			spin_lock_irqsave(&dev->rbsl, flags);
 			goto resubmit; /* maybe we can recover */
 		}
 	}
 
-	spin_lock(&dev->rbsl);
+	spin_lock_irqsave(&dev->rbsl, flags);
 	if (urb->actual_length > 0) {
 		next_ring_head = (dev->ring_head+1) % ring_buffer_size;
 		if (next_ring_head != dev->ring_tail) {
@@ -264,7 +265,7 @@ resubmit:
 			dev->buffer_overflow = 1;
 		}
 	}
-	spin_unlock(&dev->rbsl);
+	spin_unlock_irqrestore(&dev->rbsl, flags);
 exit:
 	dev->interrupt_in_done = 1;
 	wake_up_interruptible(&dev->read_wait);
