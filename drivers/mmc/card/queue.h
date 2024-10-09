b931cd73c204c24340306361e678ce75807b93cd
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:47:16 -0400

    mmc: queue: Introduce queue depth and use it to allocate and free
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    Rebuild_CHGLOG: - [mmc] queue: Introduce queue depth and use it to allocate and free (Don Zickus) [1430497]
    Rebuild_FUZZ: 96.00%
    commit-author Adrian Hunter <adrian.hunter@intel.com>
    commit c5bda0ca6fab8e040c8ea3c71fdd16deee0f132f
    
    Add a mmc_queue member to record the size of the queue, which currently
    supports 2 requests on-the-go at a time. Instead of allocating resources
    for 2 slots in the queue, allow for an arbitrary number.
    
    	Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit c5bda0ca6fab8e040c8ea3c71fdd16deee0f132f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/card/queue.h
+++ drivers/mmc/card/queue.h
@@ -55,9 +55,10 @@ struct mmc_queue {
 	int			(*issue_fn)(struct mmc_queue *, struct request *);
 	void			*data;
 	struct request_queue	*queue;
-	struct mmc_queue_req	mqrq[2];
+	struct mmc_queue_req	*mqrq;
 	struct mmc_queue_req	*mqrq_cur;
 	struct mmc_queue_req	*mqrq_prev;
+	int			qdepth;
 };
 
 extern int mmc_init_queue(struct mmc_queue *, struct mmc_card *, spinlock_t *,
