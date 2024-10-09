6619bc3118a44f539676f95f1d8746162c64a30e
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:14:49 -0400

    mm: export follow_pte()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    Rebuild_CHGLOG: - [mm] export follow_pte() (Larry Woodman) [1457569 1383493 1457572]
    Rebuild_FUZZ: 90.48%
    commit-author Jan Kara <jack@suse.cz>
    commit cae1240257d9ba4b40eb240124c530de8ee349bc
    
    DAX will need to implement its own version of page_check_address().  To
    avoid duplicating page table walking code, export follow_pte() which
    does what we need.
    
    Link: http://lkml.kernel.org/r/1479460644-25076-18-git-send-email-jack@suse.cz
    	Signed-off-by: Jan Kara <jack@suse.cz>
    	Reviewed-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    	Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    	Cc: Dan Williams <dan.j.williams@intel.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit cae1240257d9ba4b40eb240124c530de8ee349bc)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3518,8 +3518,8 @@ out:
 	return -EINVAL;
 }
 
-static inline int follow_pte(struct mm_struct *mm, unsigned long address,
-			     pte_t **ptepp, spinlock_t **ptlp)
+int follow_pte(struct mm_struct *mm, unsigned long address, pte_t **ptepp,
+	       spinlock_t **ptlp)
 {
 	int res;
 
