5085f8d96c92a79934f97f87d534aac1f363c7c5
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:50:43 -0400

    scsi: aacraid: Fix potential null access
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    Rebuild_CHGLOG: - [scsi] aacraid: Fix potential null access (Scott Benesh) [1384213]
    Rebuild_FUZZ: 91.89%
    commit-author Raghava Aditya Renukunta <RaghavaAditya.Renukunta@microsemi.com>
    commit e498520edec6655e93ac5e768b04f4fd2299fe4d
    
    Currently, command threads fails to return ioctls commands for older
    controller versions, since it returns when all the fibs have been
    allocated. Another issue is even all the fibs have not been allocated,
    the correct allocated fibs is not updated nor freed.
    
    Fixes: 113156bcea9ef1e6 (scsi: aacraid: Reworked aac_command_thread)
    	Reported-by: Tomas Henzl <thenzl@redhat.com>
    	Signed-off-by: Raghava Aditya Renukunta <RaghavaAditya.Renukunta@microsemi.com>
    	Reviewed-by: Dave Carroll <david.carroll@microsemi.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit e498520edec6655e93ac5e768b04f4fd2299fe4d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/aacraid/commsup.c
+++ drivers/scsi/aacraid/commsup.c
@@ -1907,7 +1907,6 @@ static int fillup_pools(struct aac_dev *dev, struct hw_fib **hw_fib_pool,
 {
 	struct hw_fib **hw_fib_p;
 	struct fib **fib_p;
-	int rcode = 1;
 
 	hw_fib_p = hw_fib_pool;
 	fib_p = fib_pool;
@@ -1925,11 +1924,11 @@ static int fillup_pools(struct aac_dev *dev, struct hw_fib **hw_fib_pool,
 		}
 	}
 
+	/*
+	 * Get the actual number of allocated fibs
+	 */
 	num = hw_fib_p - hw_fib_pool;
-	if (!num)
-		rcode = 0;
-
-	return rcode;
+	return num;
 }
 
 static void wakeup_fibctx_threads(struct aac_dev *dev,
@@ -2037,7 +2036,6 @@ static void aac_process_events(struct aac_dev *dev)
 	struct fib *fib;
 	unsigned long flags;
 	spinlock_t *t_lock;
-	unsigned int rcode;
 
 	t_lock = dev->queues->queue[HostNormCmdQueue].lock;
 	spin_lock_irqsave(t_lock, flags);
@@ -2120,8 +2118,8 @@ static void aac_process_events(struct aac_dev *dev)
 		 * Fill up fib pointer pools with actual fibs
 		 * and hw_fibs
 		 */
-		rcode = fillup_pools(dev, hw_fib_pool, fib_pool, num);
-		if (!rcode)
+		num = fillup_pools(dev, hw_fib_pool, fib_pool, num);
+		if (!num)
 			goto free_mem;
 
 		/*
