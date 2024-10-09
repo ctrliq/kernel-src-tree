b65a979639050d014faa120fad0b19919f731715
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:29:20 -0400

    perf/x86/intel/uncore: Fix memory leaks on allocation failures
    jira LE-1907
    cve CVE-2017-5754
    cve CVE-2017-5753
    cve CVE-2017-5715
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Colin Ian King <colin.king@canonical.com>
    commit 629eb703d3e46aa570c6c91235d38fd09ed8c58b
    
    Currently if an allocation fails then the error return paths
    don't free up any currently allocated pmus[].boxes and pmus causing
    a memory leak.  Add an error clean up exit path that frees these
    objects.
    
    Detected by CoverityScan, CID#711632 ("Resource Leak")
    
    	Signed-off-by: Colin Ian King <colin.king@canonical.com>
    	Acked-by: Peter Zijlstra <peterz@infradead.org>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    	Cc: kernel-janitors@vger.kernel.org
    Fixes: 087bfbb03269 ("perf/x86: Add generic Intel uncore PMU support")
    Link: http://lkml.kernel.org/r/20171009172655.6132-1-colin.king@canonical.com
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 629eb703d3e46aa570c6c91235d38fd09ed8c58b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/events/intel/uncore.c
+++ arch/x86/events/intel/uncore.c
@@ -840,7 +840,7 @@ static int __init uncore_type_init(struct intel_uncore_type *type, bool setid)
 		pmus[i].type	= type;
 		pmus[i].boxes	= kzalloc(size, GFP_KERNEL);
 		if (!pmus[i].boxes)
-			return -ENOMEM;
+			goto err;
 	}
 
 	type->pmus = pmus;
@@ -854,7 +854,7 @@ static int __init uncore_type_init(struct intel_uncore_type *type, bool setid)
 		attr_group = kzalloc(sizeof(struct attribute *) * (i + 1) +
 					sizeof(*attr_group), GFP_KERNEL);
 		if (!attr_group)
-			return -ENOMEM;
+			goto err;
 
 		attrs = (struct attribute **)(attr_group + 1);
 		attr_group->name = "events";
@@ -867,7 +867,15 @@ static int __init uncore_type_init(struct intel_uncore_type *type, bool setid)
 	}
 
 	type->pmu_group = &uncore_pmu_attr_group;
+
 	return 0;
+
+err:
+	for (i = 0; i < type->num_boxes; i++)
+		kfree(pmus[i].boxes);
+	kfree(pmus);
+
+	return -ENOMEM;
 }
 
 static int __init
