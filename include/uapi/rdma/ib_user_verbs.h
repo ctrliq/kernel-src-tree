15fc90a2d882d6d9781ff4aeef0d27ef8726ca01
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:20:35 -0400

    RDMA/uverbs: Add missing driver_data
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    commit-author Jason Gunthorpe <jgg@mellanox.com>
    commit 1b09577bef3ac135ed02115943c9ab53f2129555
    
    If the struct is used with a driver_udata it should have a trailing
    driver_data flex array to mark it as having udata.
    
    In most cases this forces the end of the struct to be aligned to u64 which
    is needed to make the trailing driver_data naturally aligned.
    
    Unfortunately We have a few cases where the base struct is not aligned to
    8 bytes, these are marked with a u32 driver_data and userspace will check
    for alignment issues when it compiles the driver.
    
    Also remove the empty ib_uverbs_modify_qp_resp as nothing uses this.
    
    pahole says there is no change to any struct sizes by this change.
    
    	Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    	Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    (cherry picked from commit 1b09577bef3ac135ed02115943c9ab53f2129555)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/uapi/rdma/ib_user_verbs.h
+++ include/uapi/rdma/ib_user_verbs.h
@@ -163,6 +163,7 @@ struct ib_uverbs_get_context {
 struct ib_uverbs_get_context_resp {
 	__u32 async_fd;
 	__u32 num_comp_vectors;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_query_device {
@@ -308,6 +309,7 @@ struct ib_uverbs_alloc_pd {
 
 struct ib_uverbs_alloc_pd_resp {
 	__u32 pd_handle;
+	__u32 driver_data[0];
 };
 
 struct ib_uverbs_dealloc_pd {
@@ -323,6 +325,7 @@ struct ib_uverbs_open_xrcd {
 
 struct ib_uverbs_open_xrcd_resp {
 	__u32 xrcd_handle;
+	__u32 driver_data[0];
 };
 
 struct ib_uverbs_close_xrcd {
@@ -343,6 +346,7 @@ struct ib_uverbs_reg_mr_resp {
 	__u32 mr_handle;
 	__u32 lkey;
 	__u32 rkey;
+	__u32 driver_data[0];
 };
 
 struct ib_uverbs_rereg_mr {
@@ -354,11 +358,13 @@ struct ib_uverbs_rereg_mr {
 	__aligned_u64 hca_va;
 	__u32 pd_handle;
 	__u32 access_flags;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_rereg_mr_resp {
 	__u32 lkey;
 	__u32 rkey;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_dereg_mr {
@@ -370,11 +376,13 @@ struct ib_uverbs_alloc_mw {
 	__u32 pd_handle;
 	__u8  mw_type;
 	__u8  reserved[3];
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_alloc_mw_resp {
 	__u32 mw_handle;
 	__u32 rkey;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_dealloc_mw {
@@ -417,6 +425,7 @@ struct ib_uverbs_ex_create_cq {
 struct ib_uverbs_create_cq_resp {
 	__u32 cq_handle;
 	__u32 cqe;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_ex_create_cq_resp {
@@ -627,6 +636,7 @@ struct ib_uverbs_create_qp_resp {
 	__u32 max_recv_sge;
 	__u32 max_inline_data;
 	__u32 reserved;
+	__u32 driver_data[0];
 };
 
 struct ib_uverbs_ex_create_qp_resp {
@@ -729,9 +739,6 @@ struct ib_uverbs_ex_modify_qp {
 	struct ib_uverbs_modify_qp base;
 	__u32	rate_limit;
 	__u32	reserved;
-};
-
-struct ib_uverbs_modify_qp_resp {
 };
 
 struct ib_uverbs_ex_modify_qp_resp {
@@ -861,10 +868,12 @@ struct ib_uverbs_create_ah {
 	__u32 pd_handle;
 	__u32 reserved;
 	struct ib_uverbs_ah_attr attr;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_create_ah_resp {
 	__u32 ah_handle;
+	__u32 driver_data[0];
 };
 
 struct ib_uverbs_destroy_ah {
@@ -1173,6 +1182,7 @@ struct ib_uverbs_create_srq_resp {
 	__u32 max_wr;
 	__u32 max_sge;
 	__u32 srqn;
+	__u32 driver_data[0];
 };
 
 struct ib_uverbs_modify_srq {
