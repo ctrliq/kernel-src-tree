eab33a92465aeeff1e79df0fcee63e28869e8c88
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:47:46 -0400

    mmc: core: guard dereference of optional parameter
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [mmc] core: guard dereference of optional parameter (Gopal Tiwari) [1549495]
    Rebuild_FUZZ: 94.74%
    commit-author tcharding <me@tobin.cc>
    commit 9b980d950e5bcdce6dbc15a9c00ff960beeea28b
    
    Various functions take as parameter an optional pointer. Pointer
    should be guarded with non-NULL check before dereferencing.
    
    Add non-NULL check before dereference of pointer.
    
    	Signed-off-by: Tobin C. Harding <me@tobin.cc>
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit 9b980d950e5bcdce6dbc15a9c00ff960beeea28b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/core/sdio_io.c
+++ drivers/mmc/core/sdio_io.c
@@ -371,7 +371,8 @@ u8 sdio_readb(struct sdio_func *func, unsigned int addr, int *err_ret)
 	u8 val;
 
 	if (!func) {
-		*err_ret = -EINVAL;
+		if (err_ret)
+			*err_ret = -EINVAL;
 		return 0xFF;
 	}
 
@@ -405,7 +406,8 @@ void sdio_writeb(struct sdio_func *func, u8 b, unsigned int addr, int *err_ret)
 	int ret;
 
 	if (!func) {
-		*err_ret = -EINVAL;
+		if (err_ret)
+			*err_ret = -EINVAL;
 		return;
 	}
 
@@ -633,7 +635,8 @@ unsigned char sdio_f0_readb(struct sdio_func *func, unsigned int addr,
 	unsigned char val;
 
 	if (!func) {
-		*err_ret = -EINVAL;
+		if (err_ret)
+			*err_ret = -EINVAL;
 		return 0xFF;
 	}
 
@@ -671,7 +674,8 @@ void sdio_f0_writeb(struct sdio_func *func, unsigned char b, unsigned int addr,
 	int ret;
 
 	if (!func) {
-		*err_ret = -EINVAL;
+		if (err_ret)
+			*err_ret = -EINVAL;
 		return;
 	}
 
