008b726e80988deb24aacf5c9f68d157e3bb81fa
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:32:53 -0400

    netvsc: remove unused variables
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author stephen hemminger <stephen@networkplumber.org>
    commit ceaaea0483cf82a2c817781f3cbacf29f2c5cc97
    
    Fixes set but never used warnings
    
    	Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ceaaea0483cf82a2c817781f3cbacf29f2c5cc97)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -1444,7 +1444,6 @@ static int netvsc_vf_down(struct net_device *vf_netdev)
 static int netvsc_unregister_vf(struct net_device *vf_netdev)
 {
 	struct net_device *ndev;
-	struct netvsc_device *netvsc_dev;
 	struct net_device_context *net_device_ctx;
 
 	ndev = get_netvsc_byref(vf_netdev);
@@ -1452,7 +1451,6 @@ static int netvsc_unregister_vf(struct net_device *vf_netdev)
 		return NOTIFY_DONE;
 
 	net_device_ctx = netdev_priv(ndev);
-	netvsc_dev = net_device_ctx->nvdev;
 
 	netdev_info(ndev, "VF unregistering: %s\n", vf_netdev->name);
 
@@ -1552,7 +1550,6 @@ static int netvsc_remove(struct hv_device *dev)
 {
 	struct net_device *net;
 	struct net_device_context *ndev_ctx;
-	struct netvsc_device *net_device;
 
 	net = hv_get_drvdata(dev);
 
@@ -1562,7 +1559,6 @@ static int netvsc_remove(struct hv_device *dev)
 	}
 
 	ndev_ctx = netdev_priv(net);
-	net_device = ndev_ctx->nvdev;
 
 	/* Avoid racing with netvsc_change_mtu()/netvsc_set_channels()
 	 * removing the device.
