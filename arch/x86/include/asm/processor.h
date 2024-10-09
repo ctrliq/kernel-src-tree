939db726ecbac3fe6eafa441bc47cc4afc69b940
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:31:22 -0400

    x86/CPU: Modify detect_extended_topology() to return result
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [x86] cpu: Modify detect_extended_topology() to return result (Suravee Suthikulpanit) [1543815]
    Rebuild_FUZZ: 96.49%
    commit-author Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    commit 6c4f5abaf3566dbf5b26e7b14f4392be400f12e3
    
    Current implementation does not communicate whether it can successfully
    detect CPUID function 0xB information. Therefore, modify the function to
    return success or error codes. This will be used by subsequent patches.
    
    	Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    	Signed-off-by: Borislav Petkov <bp@suse.de>
    	Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    	Reviewed-by: Borislav Petkov <bp@suse.de>
    Link: http://lkml.kernel.org/r/1524865681-112110-2-git-send-email-suravee.suthikulpanit@amd.com
    
    (cherry picked from commit 6c4f5abaf3566dbf5b26e7b14f4392be400f12e3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -203,7 +203,7 @@ extern u32 get_scattered_cpuid_leaf(unsigned int level,
 extern unsigned int init_intel_cacheinfo(struct cpuinfo_x86 *c);
 extern void init_amd_cacheinfo(struct cpuinfo_x86 *c);
 
-extern void detect_extended_topology(struct cpuinfo_x86 *c);
+extern int detect_extended_topology(struct cpuinfo_x86 *c);
 extern void detect_ht(struct cpuinfo_x86 *c);
 
 extern int get_cpu_cache_id(int cpu, int level);
