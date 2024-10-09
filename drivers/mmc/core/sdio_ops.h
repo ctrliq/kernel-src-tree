7a39dee254283a50038983a1a6fa6789ea8d5d68
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:47:59 -0400

    mmc: sdio: Fix sdio wait busy implement limitation
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [mmc] sdio: Fix sdio wait busy implement limitation (Gopal Tiwari) [1549495]
    Rebuild_FUZZ: 94.74%
    commit-author jiajie.hao@mediatek.com <jiajie.hao@mediatek.com>
    commit f328c76e568d13c9fee21b5cf7411c96285f0f89
    
    The host may issue an I/O abort by writing to the CCCR at any time
    during I/O read operation via CMD52. And host may need suspend
    transcation during write busy stage in SDIO suspend/resume scenario.
    >From other side, a card may accept CMD52 during data transfer phase.
    
    Previous implement would block issuing above command in busy stage.
    It cause function driver can't implement as proper way and has no
    opportunity to do some coverage in error case via I/O abort etc.
    
    We need bypass some necessary operation during busy check stage.
    
    	Signed-off-by: Jiajie Hao <jiajie.hao@mediatek.com>
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit f328c76e568d13c9fee21b5cf7411c96285f0f89)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/core/sdio_ops.h
+++ drivers/mmc/core/sdio_ops.h
@@ -21,9 +21,15 @@ int mmc_io_rw_extended(struct mmc_card *card, int write, unsigned fn,
 	unsigned addr, int incr_addr, u8 *buf, unsigned blocks, unsigned blksz);
 int sdio_reset(struct mmc_host *host);
 
-static inline bool mmc_is_io_op(u32 opcode)
+static inline bool sdio_is_io_busy(u32 opcode, u32 arg)
 {
-	return opcode == SD_IO_RW_DIRECT || opcode == SD_IO_RW_EXTENDED;
+	u32 addr;
+
+	addr = (arg >> 9) & 0x1FFFF;
+
+	return (opcode == SD_IO_RW_EXTENDED ||
+		(opcode == SD_IO_RW_DIRECT &&
+		!(addr == SDIO_CCCR_ABORT || addr == SDIO_CCCR_SUSPEND)));
 }
 
 #endif
