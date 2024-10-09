c5d70e62f34447913b103ec61de217fdd9892a71
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:54:38 -0400

    mm/hugetlb: filter out hugetlb pages if HUGEPAGE migration is not supported.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [mm] hugetlb: filter out hugetlb pages if HUGEPAGE migration is not supported (Baoquan He) [1601867]
    Rebuild_FUZZ: 97.30%
    commit-author Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    commit 464c7ffbcb164b2e5cebfa406b7fc6cdb7945344
    
    When scanning for movable pages, filter out Hugetlb pages if hugepage
    migration is not supported.  Without this we hit infinte loop in
    __offline_pages() where we do
    
    	pfn = scan_movable_pages(start_pfn, end_pfn);
    	if (pfn) { /* We have movable pages */
    		ret = do_migrate_range(pfn, end_pfn);
    		goto repeat;
    	}
    
    Fix this by checking hugepage_migration_supported both in
    has_unmovable_pages which is the primary backoff mechanism for page
    offlining and for consistency reasons also into scan_movable_pages
    because it doesn't make any sense to return a pfn to non-migrateable
    huge page.
    
    This issue was revealed by, but not caused by 72b39cfc4d75 ("mm,
    memory_hotplug: do not fail offlining too early").
    
    Link: http://lkml.kernel.org/r/20180824063314.21981-1-aneesh.kumar@linux.ibm.com
    Fixes: 72b39cfc4d75 ("mm, memory_hotplug: do not fail offlining too early")
    	Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    	Reported-by: Haren Myneni <haren@linux.vnet.ibm.com>
    	Acked-by: Michal Hocko <mhocko@suse.com>
    	Reviewed-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    	Cc: <stable@vger.kernel.org>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 464c7ffbcb164b2e5cebfa406b7fc6cdb7945344)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1450,7 +1450,8 @@ static unsigned long scan_movable_pages(unsigned long start, unsigned long end)
 			if (PageLRU(page))
 				return pfn;
 			if (PageHuge(page)) {
-				if (page_huge_active(page))
+				if (hugepage_migration_supported(page_hstate(page)) &&
+				    page_huge_active(page))
 					return pfn;
 				else
 					pfn = round_up(pfn + 1,
