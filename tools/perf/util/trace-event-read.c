341c20fca3beb667db03bc2c5015fc81839fa6cf
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:26:34 -0400

    perf tools: Avoid double free in read_event_file()
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    commit-author Sanskriti Sharma <sansharm@redhat.com>
    commit 470c8f7c88de013d266e1b61044efe8937728b7f
    
    The temporary 'buf' buffer allocated in read_event_file() may be freed
    twice.  Move the free() call to the common function exit point.
    
    Fixes the following coverity complaints:
    
      Error: USE_AFTER_FREE (CWE-825):
      tools/perf/util/trace-event-read.c:309: double_free: Calling "free"
      frees pointer "buf" which has already been freed.
    
    	Signed-off-by: Sanskriti Sharma <sansharm@redhat.com>
    	Reviewed-by: Jiri Olsa <jolsa@kernel.org>
    	Cc: Joe Lawrence <joe.lawrence@redhat.com>
    Link: http://lkml.kernel.org/r/1538490554-8161-5-git-send-email-sansharm@redhat.com
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 470c8f7c88de013d266e1b61044efe8937728b7f)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/trace-event-read.c
+++ tools/perf/util/trace-event-read.c
@@ -297,10 +297,8 @@ static int read_event_file(struct pevent *pevent, char *sys,
 	}
 
 	ret = do_read(buf, size);
-	if (ret < 0) {
-		free(buf);
+	if (ret < 0)
 		goto out;
-	}
 
 	ret = parse_event_file(pevent, buf, size, sys);
 	if (ret < 0)
