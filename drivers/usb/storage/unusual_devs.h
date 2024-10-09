7da871696268dd773d1ca930c5036140f6e73848
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:07:06 -0400

    USB: storage: remove old wording about how to submit a change
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [usb] storage: remove old wording about how to submit a change (Torez Smith) [1540721]
    Rebuild_FUZZ: 95.73%
    commit-author Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    commit 43618257fc1fa98bbd71d4ff10a61ddecdeb7c5e
    
    It's best to just send new updates to the usb-storage quirk table to the
    linux-usb@vger.kernel.org mailing list, no need to bother Phil and Alan
    or the almost defunct usb-storage list.
    
    	Reported-by: Alan Stern <stern@rowland.harvard.edu>
    	Cc: Matthew Dharm <mdharm-usb@one-eyed-alien.net>
    	Cc: Phil Dibowitz <phil@ipom.com>
    	Acked-by: Alan Stern <stern@rowland.harvard.edu>
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 43618257fc1fa98bbd71d4ff10a61ddecdeb7c5e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/usb/storage/unusual_devs.h
+++ drivers/usb/storage/unusual_devs.h
@@ -44,10 +44,7 @@
  *	  explanation of the reason for the entry),
  *	- a copy of /proc/bus/usb/devices with your device plugged in
  *	  running with this patch.
- * Send your submission to either Phil Dibowitz <phil@ipom.com> or
- * Alan Stern <stern@rowland.harvard.edu>, and don't forget to CC: the
- * USB development list <linux-usb@vger.kernel.org> and the USB storage list
- * <usb-storage@lists.one-eyed-alien.net>
+ * Send your submission to the USB development list <linux-usb@vger.kernel.org>
  */
 
 /*
