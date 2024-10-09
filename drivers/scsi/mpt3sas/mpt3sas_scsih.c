b78d79775f0e8fa798c4ee44efc371174076e9ef
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:59:51 -0400

    scsi: mpt3sas: Introduce flag for aero based controllers
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [scsi] mpt3sas: Introduce flag for aero based controllers (Tomas Henzl) [1660791]
    Rebuild_FUZZ: 94.34%
    commit-author Suganath Prabu <suganath-prabu.subramani@broadcom.com>
    commit cc68e6077bbf09de3e792716a1d38588bdb7a785
    
    Adding flag "is_aero_ioc" to differentiate aero based controllers from
    other gen35 controllers.
    
    	Signed-off-by: Suganath Prabu <suganath-prabu.subramani@broadcom.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit cc68e6077bbf09de3e792716a1d38588bdb7a785)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/mpt3sas/mpt3sas_scsih.c
+++ drivers/scsi/mpt3sas/mpt3sas_scsih.c
@@ -10712,8 +10712,16 @@ _scsih_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 		case MPI26_MFGPAGE_DEVID_SAS3616:
 			ioc->is_gen35_ioc = 1;
 			break;
+		case MPI26_MFGPAGE_DEVID_CFG_SEC_3816:
+		case MPI26_MFGPAGE_DEVID_CFG_SEC_3916:
+			dev_info(&pdev->dev,
+			    "HBA is in Configurable Secure mode\n");
+		case MPI26_MFGPAGE_DEVID_HARD_SEC_3816:
+		case MPI26_MFGPAGE_DEVID_HARD_SEC_3916:
+			ioc->is_aero_ioc = ioc->is_gen35_ioc = 1;
+			break;
 		default:
-			ioc->is_gen35_ioc = 0;
+			ioc->is_gen35_ioc = ioc->is_aero_ioc = 0;
 		}
 		if ((ioc->hba_mpi_version_belonged == MPI25_VERSION &&
 			pdev->revision >= SAS3_PCI_DEVICE_C0_REVISION) ||
