8fe3c921b1bca6f803f2ddc9c35d3e56626cbb09
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:55:20 -0400

    hyper-v: Use fast hypercall for HVCALL_SIGNAL_EVENT
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Vitaly Kuznetsov <vkuznets@redhat.com>
    commit 057841713cfff62b4485cdd2b245f05b7ea3ba16
    
    We need to pass only 8 bytes of input for HvSignalEvent which makes it a
    perfect fit for fast hypercall. hv_input_signal_event_buffer is not needed
    any more and hv_input_signal_event is converted to union for convenience.
    
    	Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    	Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    	Reviewed-by: Stephen Hemminger <sthemmin@microsoft.com>
    	Cc: Andy Lutomirski <luto@kernel.org>
    	Cc: Haiyang Zhang <haiyangz@microsoft.com>
    	Cc: Jork Loeser <Jork.Loeser@microsoft.com>
    	Cc: K. Y. Srinivasan <kys@microsoft.com>
    	Cc: Linus Torvalds <torvalds@linux-foundation.org>
    	Cc: Peter Zijlstra <peterz@infradead.org>
    	Cc: Simon Xiao <sixiao@microsoft.com>
    	Cc: Steven Rostedt <rostedt@goodmis.org>
    	Cc: Thomas Gleixner <tglx@linutronix.de>
    	Cc: devel@linuxdriverproject.org
    Link: http://lkml.kernel.org/r/20170802160921.21791-5-vkuznets@redhat.com
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 057841713cfff62b4485cdd2b245f05b7ea3ba16)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/hv/connection.c
+++ drivers/hv/connection.c
@@ -501,6 +501,6 @@ void vmbus_set_event(struct vmbus_channel *channel)
 			(child_relid >> 5));
 	}
 
-	hv_do_hypercall(HVCALL_SIGNAL_EVENT, channel->sig_event, NULL);
+	hv_do_fast_hypercall8(HVCALL_SIGNAL_EVENT, channel->sig_event);
 }
 EXPORT_SYMBOL_GPL(vmbus_set_event);
