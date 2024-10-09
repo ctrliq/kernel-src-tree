7b2fd84191f5051b898c1b8db69a3eaf99331cec
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:26:35 -0400

    perf annotate: Add Sparc support
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    commit-author David Miller <davem@davemloft.net>
    commit 0ab41886648bb75b951bd41d8b5cecaca8e0ad66
    
    E.g.:
    
      $ perf annotate --stdio2
      Samples: 7K of event 'cycles:ppp', 4000 Hz, Event count (approx.): 3086733887
      __gettimeofday  /lib32/libc-2.27.so [Percent: local period]
      Percent│
             │
             │
             │    Disassembly of section .text:
             │
             │    000a6fa0 <__gettimeofday@@GLIBC_2.0>:
        0.47 │      save   %sp, -96, %sp
        0.73 │      sethi  %hi(0xe9000), %l7
             │    → call   __frame_state_for@@GLIBC_2.0+0x480
        0.30 │      add    %l7, 0x58, %l7     ! e9058 <nftw64@@GLIBC_2.3.3+0x818>
        1.33 │      mov    %i0, %o0
             │      mov    %i1, %o1
        0.43 │      mov    0x74, %g1
             │      ta     0x10
       88.92 │    ↓ bcc    30
        2.95 │      clr    %g1
             │      neg    %o0
             │      mov    1, %g1
        0.31 │30:   cmp    %g1, 0
             │      bne,pn %icc, a6fe4 <__gettimeofday@@GLIBC_2.0+0x44>
             │      mov    %o0, %i0
        1.96 │    ← return %i7 + 8
        2.62 │      nop
             │      sethi  %hi(0), %g1
             │      neg    %o0, %g2
             │      add    %g1, 0x160, %g1
             │      ld     [ %l7 + %g1 ], %g1
             │      st     %g2, [ %g7 + %g1 ]
             │    ← return %i7 + 8
             │      mov    -1, %o0
    
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    Link: http://lkml.kernel.org/r/20181016.205555.1070918198627611771.davem@davemloft.net
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 0ab41886648bb75b951bd41d8b5cecaca8e0ad66)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/util/annotate.c
+++ tools/perf/util/annotate.c
@@ -140,6 +140,7 @@ static int arch__associate_ins_ops(struct arch* arch, const char *name, struct i
 #include "arch/x86/annotate/instructions.c"
 #include "arch/powerpc/annotate/instructions.c"
 #include "arch/s390/annotate/instructions.c"
+#include "arch/sparc/annotate/instructions.c"
 
 static struct arch architectures[] = {
 	{
@@ -168,6 +169,13 @@ static struct arch architectures[] = {
 		.name = "s390",
 		.init = s390__annotate_init,
 		.objdump =  {
+			.comment_char = '#',
+		},
+	},
+	{
+		.name = "sparc",
+		.init = sparc__annotate_init,
+		.objdump = {
 			.comment_char = '#',
 		},
 	},
