927ab21a034f419e37b53ff5af1a91f995a8e532
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:14:55 -0400

    net: ipv6: Always leave anycast and multicast groups on link down
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [net] ipv6: Always leave anycast and multicast groups on link down (Ivan Vecera) [1500871]
    Rebuild_FUZZ: 96.00%
    commit-author Mike Manning <mmanning@brocade.com>
    commit ea06f7176413e2538d13bb85b65387d0917943d9
    
    Default kernel behavior is to delete IPv6 addresses on link
    down, which entails deletion of the multicast and the
    subnet-router anycast addresses. These deletions do not
    happen with sysctl setting to keep global IPv6 addresses on
    link down, so every link down/up causes an increment of the
    anycast and multicast refcounts. These bogus refcounts may
    stop these addrs from being removed on subsequent calls to
    delete them. The solution is to leave the groups for the
    multicast and subnet anycast on link down for the callflow
    when global IPv6 addresses are kept.
    
    Fixes: f1705ec197e7 ("net: ipv6: Make address flushing on ifdown optional")
    	Signed-off-by: Mike Manning <mmanning@brocade.com>
    	Acked-by: David Ahern <dsa@cumulusnetworks.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ea06f7176413e2538d13bb85b65387d0917943d9)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -3347,6 +3347,10 @@ static int addrconf_ifdown(struct net_device *dev, int how)
 		if (state != INET6_IFADDR_STATE_DEAD) {
 			__ipv6_ifa_notify(RTM_DELADDR, ifa);
 			inet6addr_notifier_call_chain(NETDEV_DOWN, ifa);
+		} else {
+			if (idev->cnf.forwarding)
+				addrconf_leave_anycast(ifa);
+			addrconf_leave_solict(ifa->idev, &ifa->addr);
 		}
 		in6_ifa_put(ifa);
 
