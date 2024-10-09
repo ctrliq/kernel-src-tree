153a5cc793f1a2da0d10af51fdf42e95127998a0
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:51:26 -0400

    igb: Remove useless argument
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Benjamin Poirier <bpoirier@suse.com>
    commit 81e3f64a9b2d837717a58606d9f22420a47fdf68
    
    Given that all callers of igb_update_stats() pass the same two arguments:
    (adapter, &adapter->stats64), the second argument can be removed.
    
    	Signed-off-by: Benjamin Poirier <bpoirier@suse.com>
    	Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    	Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 81e3f64a9b2d837717a58606d9f22420a47fdf68)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/intel/igb/igb_ethtool.c
+++ drivers/net/ethernet/intel/igb/igb_ethtool.c
@@ -2308,7 +2308,7 @@ static void igb_get_ethtool_stats(struct net_device *netdev,
 	char *p;
 
 	spin_lock(&adapter->stats64_lock);
-	igb_update_stats(adapter, net_stats);
+	igb_update_stats(adapter);
 
 	for (i = 0; i < IGB_GLOBAL_STATS_LEN; i++) {
 		p = (char *)adapter + igb_gstrings_stats[i].stat_offset;
