41c756397ceccc69f5c176fb091ad04d5e8918b1
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:16:17 -0400

    netfilter: conntrack: remove unused netns_ct member
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Florian Westphal <fw@strlen.de>
    commit 7e416ad7416307e22871a0ef0f0f14e2bb66a0d1
    
    since 23014011ba420 ('netfilter: conntrack: support a fixed size of 128 distinct labels')
    this isn't needed anymore.
    
    	Signed-off-by: Florian Westphal <fw@strlen.de>
    	Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (cherry picked from commit 7e416ad7416307e22871a0ef0f0f14e2bb66a0d1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/net/netns/conntrack.h
+++ include/net/netns/conntrack.h
@@ -144,7 +144,6 @@ struct netns_ct {
 	struct nf_ip_net	nf_ct_proto;
 #if defined(CONFIG_NF_CONNTRACK_LABELS)
 	unsigned int		labels_used;
-	u8			label_words;
 #endif
 #ifdef CONFIG_NF_NAT_NEEDED
 	struct hlist_head	*nat_bysource;
