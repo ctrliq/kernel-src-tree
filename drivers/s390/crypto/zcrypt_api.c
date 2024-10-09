cba30d1d01b94f4ae1dc60e19dec1e897e22bb8a
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:50:27 -0400

    s390/zcrypt: Add some debug messages on failure.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    Rebuild_CHGLOG: - [s390] zcrypt: Add some debug messages on failure (Hendrik Brueckner) [1380349]
    Rebuild_FUZZ: 93.33%
    commit-author Harald Freudenberger <freude@linux.vnet.ibm.com>
    commit dbed23dba0fb153c4566af59da7dfa06b780b0af
    
    Added some dbf debug messages on failure of the most important
    ioctl calls. These messages are only enabled with dbf level
    6 (debug) and so do not affect the normal operating mode which
    uses level 3 (errors and higher).
    
    	Signed-off-by: Harald Freudenberger <freude@linux.vnet.ibm.com>
    	Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    (cherry picked from commit dbed23dba0fb153c4566af59da7dfa06b780b0af)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/s390/crypto/zcrypt_api.c
+++ drivers/s390/crypto/zcrypt_api.c
@@ -854,8 +854,10 @@ static long zcrypt_unlocked_ioctl(struct file *filp, unsigned int cmd,
 			do {
 				rc = zcrypt_rsa_modexpo(&mex);
 			} while (rc == -EAGAIN);
-		if (rc)
+		if (rc) {
+			ZCRYPT_DBF(DBF_DEBUG, "ioctl ICARSAMODEXPO rc=%d", rc);
 			return rc;
+		}
 		return put_user(mex.outputdatalength, &umex->outputdatalength);
 	}
 	case ICARSACRT: {
@@ -871,8 +873,10 @@ static long zcrypt_unlocked_ioctl(struct file *filp, unsigned int cmd,
 			do {
 				rc = zcrypt_rsa_crt(&crt);
 			} while (rc == -EAGAIN);
-		if (rc)
+		if (rc) {
+			ZCRYPT_DBF(DBF_DEBUG, "ioctl ICARSACRT rc=%d", rc);
 			return rc;
+		}
 		return put_user(crt.outputdatalength, &ucrt->outputdatalength);
 	}
 	case ZSECSENDCPRB: {
@@ -888,6 +892,8 @@ static long zcrypt_unlocked_ioctl(struct file *filp, unsigned int cmd,
 			do {
 				rc = zcrypt_send_cprb(&xcRB);
 			} while (rc == -EAGAIN);
+		if (rc)
+			ZCRYPT_DBF(DBF_DEBUG, "ioctl ZSENDCPRB rc=%d", rc);
 		if (copy_to_user(uxcRB, &xcRB, sizeof(xcRB)))
 			return -EFAULT;
 		return rc;
@@ -905,6 +911,8 @@ static long zcrypt_unlocked_ioctl(struct file *filp, unsigned int cmd,
 			do {
 				rc = zcrypt_send_ep11_cprb(&xcrb);
 			} while (rc == -EAGAIN);
+		if (rc)
+			ZCRYPT_DBF(DBF_DEBUG, "ioctl ZSENDEP11CPRB rc=%d", rc);
 		if (copy_to_user(uxcrb, &xcrb, sizeof(xcrb)))
 			return -EFAULT;
 		return rc;
