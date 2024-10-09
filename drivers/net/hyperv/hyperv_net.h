e9b75ebfc539671495355ebb17653bf3e0aa96ce
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:33:09 -0400

    hv_netvsc: Fix the carrier state error when data path is off
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Haiyang Zhang <haiyangz@microsoft.com>
    commit 53fa1a6f33520f01f9dbee48369074b34d77616b
    
    When the VF NIC is opened, the synthetic NIC's carrier state is set to
    off. This tells the host to transitions data path to the VF device. But
    if startup script or user manipulates the admin state of the netvsc
    device directly for example:
            # ifconfig eth0 down
    	# ifconfig eth0 up
    Then the carrier state of the synthetic NIC would be on, even though the
    data path was still over the VF NIC. This patch sets the carrier state
    of synthetic NIC with consideration of the related VF state.
    
    	Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    	Reviewed-by: Stephen Hemminger <sthemmin@microsoft.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 53fa1a6f33520f01f9dbee48369074b34d77616b)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/hyperv/hyperv_net.h
+++ drivers/net/hyperv/hyperv_net.h
@@ -713,6 +713,8 @@ struct net_device_context {
 	u32 vf_alloc;
 	/* Serial number of the VF to team with */
 	u32 vf_serial;
+
+	bool datapath;	/* 0 - synthetic, 1 - VF nic */
 };
 
 /* Per netvsc device */
