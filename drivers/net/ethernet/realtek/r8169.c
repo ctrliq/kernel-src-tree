54c7a82d6eccb7d79153e4ce411e48559da72c9b
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:26:39 -0400

    r8169: remove unneeded rpm ops in rtl_shutdown
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit b9aa1c75e694618e4e2ba32667b89134c7fe2e7c
    
    This patch reverts commit 2a15cd2ff488 "r8169: runtime resume before
    shutdown" from 2012. Few months after this change the underlying issue
    was solved in the PCI core with commit 3ff2de9ba1a2 "PCI/PM: Resume
    device before shutdown".
    
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit b9aa1c75e694618e4e2ba32667b89134c7fe2e7c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/realtek/r8169.c
+++ drivers/net/ethernet/realtek/r8169.c
@@ -8198,9 +8198,6 @@ static void rtl_shutdown(struct pci_dev *pdev)
 {
 	struct net_device *dev = pci_get_drvdata(pdev);
 	struct rtl8169_private *tp = netdev_priv(dev);
-	struct device *d = &pdev->dev;
-
-	pm_runtime_get_sync(d);
 
 	rtl8169_net_suspend(dev);
 
@@ -8218,8 +8215,6 @@ static void rtl_shutdown(struct pci_dev *pdev)
 		pci_wake_from_d3(pdev, true);
 		pci_set_power_state(pdev, PCI_D3hot);
 	}
-
-	pm_runtime_put_noidle(d);
 }
 
 static void rtl_remove_one(struct pci_dev *pdev)
