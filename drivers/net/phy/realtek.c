ca04a380d38989841d1d45660afdcbba86484e32
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:33:45 -0400

    net: phy: realtek: add missing entry for RTL8211 to mdio_device_id table
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [netdrv] phy: realtek: add missing entry for RTL8211 to mdio_device_id table (Corinna Vinschen) [1706531]
    Rebuild_FUZZ: 96.40%
    commit-author Heiner Kallweit <hkallweit1@gmail.com>
    commit 100ec4bf09679583d45316fcf13611f66a8d0f88
    
    When adding support for RTL8211 I forgot to update the mdio_device_id
    table.
    
    	Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Fixes: d241d4aac93f ("net: phy: realtek: add support for RTL8211")
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 100ec4bf09679583d45316fcf13611f66a8d0f88)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -301,6 +301,7 @@ module_exit(realtek_exit);
 
 static struct mdio_device_id __maybe_unused realtek_tbl[] = {
 	{ 0x001cc816, 0x001fffff },
+	{ 0x001cc910, 0x001fffff },
 	{ 0x001cc912, 0x001fffff },
 	{ 0x001cc914, 0x001fffff },
 	{ 0x001cc915, 0x001fffff },
