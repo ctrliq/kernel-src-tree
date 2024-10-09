7266ebd0d2c099f169f834941bfa52abf931de2a
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:37:01 -0400

    scsi: fcoe: hold disc_mutex when traversing rport lists
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [scsi] fcoe: hold disc_mutex when traversing rport lists (Chris Leech) [1608481]
    Rebuild_FUZZ: 94.23%
    commit-author Hannes Reinecke <hare@suse.de>
    commit 0993ed922a3e62a66fcad41633985b127ecf71b0
    
    When calling either fc_rport_logon() or fc_rport_logoff() during rport list
    traversal we cannot use the RCU list traversal, as either of these
    functions will be taking a mutex.  So we need to partially revert commit
    a407c593398c to take the disc mutex during traversal.  We should, however,
    continue to use krefs to ensure that the rport object will not be freed
    from under us.
    
    Fixes: a407c593398c ("scsi: libfc: Fixup disc_mutex handling")
    	Signed-off-by: Hannes Reinecke <hare@suse.com>
    	Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 0993ed922a3e62a66fcad41633985b127ecf71b0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/fcoe/fcoe_ctlr.c
+++ drivers/scsi/fcoe/fcoe_ctlr.c
@@ -2135,15 +2135,13 @@ static void fcoe_ctlr_disc_stop_locked(struct fc_lport *lport)
 {
 	struct fc_rport_priv *rdata;
 
-	rcu_read_lock();
+	mutex_lock(&lport->disc.disc_mutex);
 	list_for_each_entry_rcu(rdata, &lport->disc.rports, peers) {
 		if (kref_get_unless_zero(&rdata->kref)) {
 			lport->tt.rport_logoff(rdata);
 			kref_put(&rdata->kref, lport->tt.rport_destroy);
 		}
 	}
-	rcu_read_unlock();
-	mutex_lock(&lport->disc.disc_mutex);
 	lport->disc.disc_callback = NULL;
 	mutex_unlock(&lport->disc.disc_mutex);
 }
@@ -2639,7 +2637,7 @@ static unsigned long fcoe_ctlr_vn_age(struct fcoe_ctlr *fip)
 	unsigned long deadline;
 
 	next_time = jiffies + msecs_to_jiffies(FIP_VN_BEACON_INT * 10);
-	rcu_read_lock();
+	mutex_lock(&lport->disc.disc_mutex);
 	list_for_each_entry_rcu(rdata, &lport->disc.rports, peers) {
 		if (!kref_get_unless_zero(&rdata->kref))
 			continue;
@@ -2660,7 +2658,7 @@ static unsigned long fcoe_ctlr_vn_age(struct fcoe_ctlr *fip)
 			next_time = deadline;
 		kref_put(&rdata->kref, lport->tt.rport_destroy);
 	}
-	rcu_read_unlock();
+	mutex_unlock(&lport->disc.disc_mutex);
 	return next_time;
 }
 
@@ -2783,8 +2781,6 @@ static void fcoe_ctlr_vn_disc(struct fcoe_ctlr *fip)
 	mutex_lock(&disc->disc_mutex);
 	callback = disc->pending ? disc->disc_callback : NULL;
 	disc->pending = 0;
-	mutex_unlock(&disc->disc_mutex);
-	rcu_read_lock();
 	list_for_each_entry_rcu(rdata, &disc->rports, peers) {
 		if (!kref_get_unless_zero(&rdata->kref))
 			continue;
@@ -2793,7 +2789,7 @@ static void fcoe_ctlr_vn_disc(struct fcoe_ctlr *fip)
 			lport->tt.rport_login(rdata);
 		kref_put(&rdata->kref, lport->tt.rport_destroy);
 	}
-	rcu_read_unlock();
+	mutex_unlock(&disc->disc_mutex);
 	if (callback)
 		callback(lport, DISC_EV_SUCCESS);
 }
