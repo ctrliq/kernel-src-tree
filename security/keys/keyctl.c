6c6e8a60854d9d70386011f1e4c4b3aee578befc
Jonathan Maple <jmaple@ciq.com>
2024-10-08 22:23:05 -0400

    KEYS: use kvfree() in add_key
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Geliang Tang <geliangtang@163.com>
    commit d0e0eba043c78b1d6ce3d29367abb41446d83747
    
    There is no need to make a flag to tell that this memory is allocated by
    kmalloc or vmalloc. Just use kvfree to free the memory.
    
    	Signed-off-by: Geliang Tang <geliangtang@163.com>
    	Signed-off-by: David Howells <dhowells@redhat.com>
    (cherry picked from commit d0e0eba043c78b1d6ce3d29367abb41446d83747)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- security/keys/keyctl.c
+++ security/keys/keyctl.c
@@ -65,7 +65,6 @@ SYSCALL_DEFINE5(add_key, const char __user *, _type,
 	char type[32], *description;
 	void *payload;
 	long ret;
-	bool vm;
 
 	ret = -EINVAL;
 	if (plen > 1024 * 1024 - 1)
@@ -96,14 +95,12 @@ SYSCALL_DEFINE5(add_key, const char __user *, _type,
 	/* pull the payload in if one was supplied */
 	payload = NULL;
 
-	vm = false;
 	if (_payload) {
 		ret = -ENOMEM;
 		payload = kmalloc(plen, GFP_KERNEL | __GFP_NOWARN);
 		if (!payload) {
 			if (plen <= PAGE_SIZE)
 				goto error2;
-			vm = true;
 			payload = vmalloc(plen);
 			if (!payload)
 				goto error2;
@@ -136,10 +133,7 @@ SYSCALL_DEFINE5(add_key, const char __user *, _type,
 
 	key_ref_put(keyring_ref);
  error3:
-	if (!vm)
-		kfree(payload);
-	else
-		vfree(payload);
+	kvfree(payload);
  error2:
 	kfree(description);
  error:
