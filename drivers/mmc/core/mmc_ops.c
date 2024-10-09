ba2f4dc980e527044ef56f46e7eb3efdd576a0e8
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:13:40 -0400

    mmc: core: Re-factor code for sending CID
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [mmc] core: Re-factor code for sending CID (Gopal Tiwari) [1456570]
    Rebuild_FUZZ: 93.51%
    commit-author Ulf Hansson <ulf.hansson@linaro.org>
    commit a1473732507499ec4002ad8cd33e645579d61a26
    
    Instead of having the caller to check for SPI mode, let's leave that to
    internals of mmc_send_cid(). In this way the code gets cleaner and it
    becomes clear what is specific to SPI and non-SPI mode.
    
    	Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    	Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    	Reviewed-by: Shawn Lin <shawn.lin@rock-chips.com>
    (cherry picked from commit a1473732507499ec4002ad8cd33e645579d61a26)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -206,7 +206,7 @@ int mmc_send_op_cond(struct mmc_host *host, u32 ocr, u32 *rocr)
 	return err;
 }
 
-int mmc_all_send_cid(struct mmc_host *host, u32 *cid)
+static int mmc_all_send_cid(struct mmc_host *host, u32 *cid)
 {
 	int err;
 	struct mmc_command cmd = {};
@@ -333,7 +333,7 @@ err:
 	return ret;
 }
 
-int mmc_send_cid(struct mmc_host *host, u32 *cid)
+static int mmc_spi_send_cid(struct mmc_host *host, u32 *cid)
 {
 	int ret, i;
 	__be32 *cid_tmp;
@@ -352,6 +352,14 @@ int mmc_send_cid(struct mmc_host *host, u32 *cid)
 err:
 	kfree(cid_tmp);
 	return ret;
+}
+
+int mmc_send_cid(struct mmc_host *host, u32 *cid)
+{
+	if (mmc_host_is_spi(host))
+		return mmc_spi_send_cid(host, cid);
+
+	return mmc_all_send_cid(host, cid);
 }
 
 int mmc_get_ext_csd(struct mmc_card *card, u8 **new_ext_csd)
