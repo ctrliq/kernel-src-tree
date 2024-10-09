7d7cfa3a253f8cc591197afb14eb79f6a454103c
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:22:14 -0400

    vfio iommu type1: Fix size argument to vfio_find_dma() during DMA UNMAP.
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    Rebuild_CHGLOG: - [vfio] vfio iommu type1: Fix size argument to vfio_find_dma() during DMA UNMAP (Tarun Gupta) [1116064]
    Rebuild_FUZZ: 99.30%
    commit-author Kirti Wankhede <kwankhede@nvidia.com>
    commit 7c03f428464333589f0c374b833af95777eb9dd8
    
    Passing zero for the size to vfio_find_dma() isn't compatible with
    matching the start address of an existing vfio_dma. Doing so triggers a
    corner case. In vfio_find_dma(), when the start address is equal to
    dma->iova and size is 0, check for the end of search range makes it to
    take wrong side of RB-tree. That fails the search even though the address
    is present in mapped dma ranges. Due to this, in vfio_dma_do_unmap(),
    while checking boundary conditions, size should be set to 1 for verifying
    start address of unmap range.
    vfio_find_dma() is also used to verify last address in unmap range with
    size = 0, but in that case address to be searched is calculated with
    start + size - 1 and so it works correctly.
    
    	Signed-off-by: Kirti Wankhede <kwankhede@nvidia.com>
    	Signed-off-by: Neo Jia <cjia@nvidia.com>
    [aw: changelog tweak]
    	Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    (cherry picked from commit 7c03f428464333589f0c374b833af95777eb9dd8)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/vfio/vfio_iommu_type1.c
+++ drivers/vfio/vfio_iommu_type1.c
@@ -809,7 +809,7 @@ static int vfio_dma_do_unmap(struct vfio_iommu *iommu,
 	 * mappings within the range.
 	 */
 	if (iommu->v2) {
-		dma = vfio_find_dma(iommu, unmap->iova, 0);
+		dma = vfio_find_dma(iommu, unmap->iova, 1);
 		if (dma && dma->iova != unmap->iova) {
 			ret = -EINVAL;
 			goto unlock;
