6b575540924e37c3b1f9194c299fcc6411426770
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:57:16 -0400

    perf tests attr: Add test_attr__ready function
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    commit-author Jiri Olsa <jolsa@kernel.org>
    commit 10213e2ff2e0453324689eb50e73df3df1ee6163
    
    We create many test events before the real ones just to test specific
    features. But there's no way for attr tests to separate those test
    events from those it needs to check.
    
    Adding 'ready' call from the events open interface to trigger/start
    events collection for attr test.
    
    	Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    	Cc: David Ahern <dsahern@gmail.com>
    	Cc: Namhyung Kim <namhyung@kernel.org>
    	Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    	Cc: Thomas Richter <tmricht@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/20170703145030.12903-4-jolsa@kernel.org
    	Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit 10213e2ff2e0453324689eb50e73df3df1ee6163)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- tools/perf/tests/attr.c
+++ tools/perf/tests/attr.c
@@ -32,6 +32,7 @@
 extern int verbose;
 
 static char *dir;
+static bool ready;
 
 void test_attr__init(void)
 {
@@ -62,6 +63,9 @@ static int store_event(struct perf_event_attr *attr, pid_t pid, int cpu,
 {
 	FILE *file;
 	char path[PATH_MAX];
+
+	if (!ready)
+		return 0;
 
 	snprintf(path, PATH_MAX, "%s/event-%d-%llu-%d", dir,
 		 attr->type, attr->config, fd);
@@ -136,6 +140,12 @@ void test_attr__open(struct perf_event_attr *attr, pid_t pid, int cpu,
 		die("test attr FAILED");
 
 	errno = errno_saved;
+}
+
+void test_attr__ready(void)
+{
+	if (unlikely(test_attr__enabled) && !ready)
+		ready = true;
 }
 
 static int run_dir(const char *d, const char *perf)
