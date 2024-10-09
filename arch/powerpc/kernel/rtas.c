5c9d3bb0b874a22d9f1552467956fc2c81fb8bde
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:27:08 -0400

    powerpc/pseries/mobility: Extend start/stop topology update scope
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [powerpc] pseries/mobility: Extend start/stop topology update scope (Desnes Augusto Nunes do Rosario) [1671799]
    Rebuild_FUZZ: 93.44%
    commit-author Michael Bringmann <mwb@linux.vnet.ibm.com>
    commit 65b9fdadfc4d87e2577b791fb3495cd39c93d8c0
    
    The powerpc mobility code may receive RTAS requests to perform PRRN
    (Platform Resource Reassignment Notification) topology changes at any
    time, including during LPAR migration operations.
    
    In some configurations where the affinity of CPUs or memory is being
    changed on that platform, the PRRN requests may apply or refer to
    outdated information prior to the complete update of the device-tree.
    
    This patch changes the duration for which topology updates are
    suppressed during LPAR migrations from just the rtas_ibm_suspend_me()
    / 'ibm,suspend-me' call(s) to cover the entire migration_store()
    operation to allow all changes to the device-tree to be applied prior
    to accepting and applying any PRRN requests.
    
    For tracking purposes, pr_info notices are added to the functions
    start_topology_update() and stop_topology_update() of 'numa.c'.
    
    	Signed-off-by: Michael Bringmann <mwb@linux.vnet.ibm.com>
    	Reviewed-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>
    	Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    (cherry picked from commit 65b9fdadfc4d87e2577b791fb3495cd39c93d8c0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/powerpc/kernel/rtas.c
+++ arch/powerpc/kernel/rtas.c
@@ -962,7 +962,6 @@ int rtas_ibm_suspend_me(u64 handle)
 	}
 
 	cpu_hotplug_disable();
-	stop_topology_update();
 
 	/* Call function on all CPUs.  One of us will make the
 	 * rtas call
@@ -975,7 +974,6 @@ int rtas_ibm_suspend_me(u64 handle)
 	if (atomic_read(&data.error) != 0)
 		printk(KERN_ERR "Error doing global join\n");
 
-	start_topology_update();
 	cpu_hotplug_enable();
 
 	/* Take down CPUs not online prior to suspend */
