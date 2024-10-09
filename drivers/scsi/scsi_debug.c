f82d333688f4665fa2b603e06912edac3bd87b0e
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:53:39 -0400

    scsi: scsi_debug: Fix memory leak if LBP enabled and module is unloaded
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1160.el7
    commit-author Ewan D. Milne <emilne@redhat.com>
    commit 4d2b496f19f3c2cfaca1e8fa0710688b5ff3811d
    
    map_storep was not being vfree()'d in the module_exit call.
    
    	Cc: <stable@vger.kernel.org>
    	Signed-off-by: Ewan D. Milne <emilne@redhat.com>
    	Reviewed-by: Laurence Oberman <loberman@redhat.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 4d2b496f19f3c2cfaca1e8fa0710688b5ff3811d)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -3472,6 +3472,7 @@ static void __exit scsi_debug_exit(void)
 	bus_unregister(&pseudo_lld_bus);
 	root_device_unregister(pseudo_primary);
 
+	vfree(map_storep);
 	vfree(dif_storep);
 	vfree(fake_storep);
 }
