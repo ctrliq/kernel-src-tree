ca13148bd108a9210699f8db28baab0381bd63aa
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:38:24 -0400

    mm, thp: replace smp_mb after atomic_add by smp_mb__after_atomic
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1127.el7
    commit-author Waiman Long <Waiman.Long@hp.com>
    commit 3a79d52aa3c63c939f5a1f86e80e634f84e987c4
    
    In some architectures like x86, atomic_add() is a full memory barrier.
    In that case, an additional smp_mb() is just a waste of time.  This
    patch replaces that smp_mb() by smp_mb__after_atomic() which will avoid
    the redundant memory barrier in some architectures.
    
    With a 3.16-rc1 based kernel, this patch reduced the execution time of
    breaking 1000 transparent huge pages from 38,245us to 30,964us.  A
    reduction of 19% which is quite sizeable.  It also reduces the %cpu time
    of the __split_huge_page_refcount function in the perf profile from
    2.18% to 1.15%.
    
    	Signed-off-by: Waiman Long <Waiman.Long@hp.com>
    	Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    	Cc: Andrea Arcangeli <aarcange@redhat.com>
    	Cc: Mel Gorman <mgorman@suse.de>
    	Cc: Rik van Riel <riel@redhat.com>
    	Cc: Scott J Norton <scott.norton@hp.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 3a79d52aa3c63c939f5a1f86e80e634f84e987c4)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2174,7 +2174,7 @@ static void __split_huge_page_refcount(struct page *page,
 			   mmu_gather_count + 1, &page_tail->_count);
 
 		/* after clearing PageTail the gup refcount can be released */
-		smp_mb();
+		smp_mb__after_atomic();
 
 		/*
 		 * retain hwpoison flag of the poisoned tail page:
