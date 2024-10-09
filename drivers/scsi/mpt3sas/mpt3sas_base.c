53bf39e2401b24d38f171b0c42d7ea8aa82eb250
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:51:16 -0400

    scsi: mpt3sas: Convert logging uses with MPT3SAS_FMT and reply_q_name to %s:
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [scsi] mpt3sas: Convert logging uses with MPT3SAS_FMT and reply_q_name to s: (Tomas Henzl) [1642370]
    Rebuild_FUZZ: 95.17%
    commit-author Joe Perches <joe@perches.com>
    commit fc7d510ec4c8ff8801cb4ff5ba1a9841d80edb7c
    
    Convert the existing 2 uses to make the format and arguments matching more
    obvious.
    
    Miscellanea:
    
    o Move the word "enabled" into the format to trivially reduce object size
    o Remove unnecessary parentheses
    
    	Signed-off-by: Joe Perches <joe@perches.com>
    	Acked-by: Suganath Prabu <suganath-prabu.subramani@broadcom.com>
    	Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit fc7d510ec4c8ff8801cb4ff5ba1a9841d80edb7c)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/mpt3sas/mpt3sas_base.c
+++ drivers/scsi/mpt3sas/mpt3sas_base.c
@@ -2035,7 +2035,7 @@ _base_request_irq(struct MPT3SAS_ADAPTER *ioc, u8 index)
 	r = request_irq(pci_irq_vector(pdev, index), _base_interrupt,
 			IRQF_SHARED, reply_q->name, reply_q);
 	if (r) {
-		pr_err(MPT3SAS_FMT "unable to allocate interrupt %d!\n",
+		pr_err("%s: unable to allocate interrupt %d!\n",
 		       reply_q->name, pci_irq_vector(pdev, index));
 		kfree(reply_q);
 		return -EBUSY;
@@ -2369,10 +2369,10 @@ mpt3sas_base_map_resources(struct MPT3SAS_ADAPTER *ioc)
 	}
 
 	list_for_each_entry(reply_q, &ioc->reply_queue_list, list)
-		pr_info(MPT3SAS_FMT "%s: IRQ %d\n",
-		    reply_q->name,  ((ioc->msix_enable) ? "PCI-MSI-X enabled" :
-		    "IO-APIC enabled"),
-		    pci_irq_vector(ioc->pdev, reply_q->msix_index));
+		pr_info("%s: %s enabled: IRQ %d\n",
+			reply_q->name,
+			ioc->msix_enable ? "PCI-MSI-X" : "IO-APIC",
+			pci_irq_vector(ioc->pdev, reply_q->msix_index));
 
 	pr_info(MPT3SAS_FMT "iomem(0x%016llx), mapped(0x%p), size(%d)\n",
 	    ioc->name, (unsigned long long)chip_phys, ioc->chip, memap_sz);
