c6b9ef03ff5f8086f4e80ed32ff3686d12013ba1
Jonathan Maple <jmaple@ciq.com>
2024-10-09 00:28:26 -0400

    dax: Use dax_write_cache* helpers
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-1062.el7
    Rebuild_CHGLOG: - [dax] Use dax_write_cache* helpers (Jeff Moyer) [1689002]
    Rebuild_FUZZ: 91.80%
    commit-author Ross Zwisler <ross.zwisler@linux.intel.com>
    commit 808c340be17dc77131fcdf9ad1eb34452d650da1
    
    Use dax_write_cache() and dax_write_cache_enabled() instead of open coding
    the bit operations.
    
    	Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    	Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    (cherry picked from commit 808c340be17dc77131fcdf9ad1eb34452d650da1)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/dax/super.c
+++ drivers/dax/super.c
@@ -201,8 +201,7 @@ static ssize_t write_cache_show(struct device *dev,
 	if (!dax_dev)
 		return -ENXIO;
 
-	rc = sprintf(buf, "%d\n", !!test_bit(DAXDEV_WRITE_CACHE,
-				&dax_dev->flags));
+	rc = sprintf(buf, "%d\n", !!dax_write_cache_enabled(dax_dev));
 	put_dax(dax_dev);
 	return rc;
 }
@@ -220,10 +219,8 @@ static ssize_t write_cache_store(struct device *dev,
 
 	if (rc)
 		len = rc;
-	else if (write_cache)
-		set_bit(DAXDEV_WRITE_CACHE, &dax_dev->flags);
 	else
-		clear_bit(DAXDEV_WRITE_CACHE, &dax_dev->flags);
+		dax_write_cache(dax_dev, write_cache);
 
 	put_dax(dax_dev);
 	return len;
@@ -317,7 +314,7 @@ EXPORT_SYMBOL_GPL(dax_memcpy_toiovecend);
 void arch_wb_cache_pmem(void *addr, size_t size);
 void dax_flush(struct dax_device *dax_dev, void *addr, size_t size)
 {
-	if (unlikely(!test_bit(DAXDEV_WRITE_CACHE, &dax_dev->flags)))
+	if (unlikely(!dax_write_cache_enabled(dax_dev)))
 		return;
 
 	arch_wb_cache_pmem(addr, size);
