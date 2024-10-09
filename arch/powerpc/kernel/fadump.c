e878f619eaa2223b98baa3124459ed98f7bb2a6a
Jonathan Maple <jmaple@ciq.com>
2024-10-08 23:37:17 -0400

    powerpc/fadump: cleanup crash memory ranges support
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-957.el7
    Rebuild_CHGLOG: - [powerpc] fadump: cleanup crash memory ranges support (Gustavo Duarte) [1621969]
    Rebuild_FUZZ: 91.49%
    commit-author Hari Bathini <hbathini@linux.ibm.com>
    commit a58183138cb72059a0c278f8370a47f41dae9afa
    
    Commit 1bd6a1c4b80a ("powerpc/fadump: handle crash memory ranges array
    index overflow") changed crash memory ranges to a dynamic array that
    is reallocated on-demand with krealloc(). The relevant header for this
    call was not included. The kernel compiles though. But be cautious and
    add the header anyway.
    
    Also, memory allocation logic in fadump_add_crash_memory() takes care
    of memory allocation for crash memory ranges in all scenarios. Drop
    unnecessary memory allocation in fadump_setup_crash_memory_ranges().
    
    Fixes: 1bd6a1c4b80a ("powerpc/fadump: handle crash memory ranges array index overflow")
    	Cc: Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
    	Signed-off-by: Hari Bathini <hbathini@linux.ibm.com>
    	Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    (cherry picked from commit a58183138cb72059a0c278f8370a47f41dae9afa)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/powerpc/kernel/fadump.c
+++ arch/powerpc/kernel/fadump.c
@@ -35,6 +35,7 @@
 #include <linux/crash_dump.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
+#include <linux/slab.h>
 
 #include <asm/page.h>
 #include <asm/prom.h>
@@ -986,13 +987,6 @@ static int fadump_setup_crash_memory_ranges(void)
 
 	pr_debug("Setup crash memory ranges.\n");
 	crash_mem_ranges = 0;
-
-	/* allocate memory for crash memory ranges for the first time */
-	if (!max_crash_mem_ranges) {
-		ret = allocate_crash_memory_ranges();
-		if (ret)
-			return ret;
-	}
 
 	/*
 	 * add the first memory chunk (RMA_START through boot_memory_size) as
