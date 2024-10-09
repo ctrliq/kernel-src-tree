0f9458872b3d04e71d437372d6dc49d182097f2a
Jonathan Maple <jmaple@ciq.com>
2024-10-08 21:59:17 -0400

    x86/platform/iosf_mbi: Source cleanup
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-862.el7
    Rebuild_CHGLOG: - [x86] platform/iosf_mbi: Source cleanup (Rob Clark) [1470097]
    Rebuild_FUZZ: 94.29%
    commit-author Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    commit 7e1ff15b699bcb2bce1e8086323d227788960044
    
     - Move the static variables to one place
     - Fix indentations in the header
     - Correct comments
    
    No functional change.
    
    [ tglx: Massaged changelog ]
    
    	Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    	Cc: David E . Box <david.e.box@linux.intel.com>
    Link: http://lkml.kernel.org/r/1436366709-17683-5-git-send-email-andriy.shevchenko@linux.intel.com
    	Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    (cherry picked from commit 7e1ff15b699bcb2bce1e8086323d227788960044)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- arch/x86/kernel/iosf_mbi.c
+++ arch/x86/kernel/iosf_mbi.c
@@ -30,14 +30,13 @@
 #define PCI_DEVICE_ID_BAYTRAIL		0x0F00
 #define PCI_DEVICE_ID_QUARK_X1000	0x0958
 
+static struct pci_dev *mbi_pdev;
 static DEFINE_SPINLOCK(iosf_mbi_lock);
 
 static inline u32 iosf_mbi_form_mcr(u8 op, u8 port, u8 offset)
 {
 	return (op << 24) | (port << 16) | (offset << 8) | MBI_ENABLE;
 }
-
-static struct pci_dev *mbi_pdev;	/* one mbi device */
 
 static int iosf_mbi_pci_read_mdr(u32 mcrx, u32 mcr, u32 *mdr)
 {
@@ -103,7 +102,7 @@ int iosf_mbi_read(u8 port, u8 opcode, u32 offset, u32 *mdr)
 	unsigned long flags;
 	int ret;
 
-	/*Access to the GFX unit is handled by GPU code */
+	/* Access to the GFX unit is handled by GPU code */
 	if (port == BT_MBI_UNIT_GFX) {
 		WARN_ON(1);
 		return -EPERM;
@@ -126,7 +125,7 @@ int iosf_mbi_write(u8 port, u8 opcode, u32 offset, u32 mdr)
 	unsigned long flags;
 	int ret;
 
-	/*Access to the GFX unit is handled by GPU code */
+	/* Access to the GFX unit is handled by GPU code */
 	if (port == BT_MBI_UNIT_GFX) {
 		WARN_ON(1);
 		return -EPERM;
@@ -150,7 +149,7 @@ int iosf_mbi_modify(u8 port, u8 opcode, u32 offset, u32 mdr, u32 mask)
 	unsigned long flags;
 	int ret;
 
-	/*Access to the GFX unit is handled by GPU code */
+	/* Access to the GFX unit is handled by GPU code */
 	if (port == BT_MBI_UNIT_GFX) {
 		WARN_ON(1);
 		return -EPERM;
