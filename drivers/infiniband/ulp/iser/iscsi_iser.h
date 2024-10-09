3d16301ee97c554208410cc3bd1847e29bc279fe
Jonathan Maple <jmaple@ciq.com>
2024-10-08 20:58:35 -0400

    IB/iser: remove unused variable from iser_conn struct
    jira LE-1907
    Rebuild_History Non-Buildable kernel-3.10.0-693.el7
    commit-author Max Gurtovoy <maxg@mellanox.com>
    commit 83236f0157feec0f01bf688a1474b889bdcc5ad0
    
    max_sectors calculation was fixed in commit:
    9c674815d346 ("IB/iser: Fix max_sectors calculation").
    Thus, iser_conn variable scsi_max_sectors is not needed anymore.
    
    	Signed-off-by: Max Gurtovoy <maxg@mellanox.com>
    	Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    	Tested-by: Raju Rangoju <rajur@chelsio.com>
    	Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from commit 83236f0157feec0f01bf688a1474b889bdcc5ad0)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/infiniband/ulp/iser/iscsi_iser.h
+++ drivers/infiniband/ulp/iser/iscsi_iser.h
@@ -497,7 +497,6 @@ struct ib_conn {
  * @rx_descs:         rx buffers array (cyclic buffer)
  * @num_rx_descs:     number of rx descriptors
  * @scsi_sg_tablesize: scsi host sg_tablesize
- * @scsi_max_sectors: scsi host max sectors
  */
 struct iser_conn {
 	struct ib_conn		     ib_conn;
@@ -520,7 +519,6 @@ struct iser_conn {
 	struct iser_rx_desc	     *rx_descs;
 	u32                          num_rx_descs;
 	unsigned short               scsi_sg_tablesize;
-	unsigned int                 scsi_max_sectors;
 	bool			     snd_w_inv;
 };
 
