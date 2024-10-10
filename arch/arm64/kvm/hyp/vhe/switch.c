7333ee5577e4607e4470137076771a154f6055d3
Shaoqin Huang <shahuang@redhat.com>
2024-10-10 04:54:58 -0400

    KVM: arm64: nv: Handle CPACR_EL1 traps
    JIRA: https://issues.redhat.com/browse/RHEL-57113
    
    Conflicts:
    - arch/arm64/kvm/hyp/vhe/switch.c
    Conflicts due to merge commit
    8c2899e7706e ("Merge branch kvm-arm64/nv-sve into kvmarm/next")
    fixes the merge conflicts. Resolve the conflicts by merging the
    kvm_hyp_handle_cpacr_el1() function into kvm_hyp_handle_sysreg_vhe()
    function.
    
    commit 493da2b1c49ac86c0eb0dde9e42b79333272d1f9
    Author: Marc Zyngier <maz@kernel.org>
    Date:   Thu Jun 20 16:46:46 2024 +0000
    
        KVM: arm64: nv: Handle CPACR_EL1 traps
    
        Handle CPACR_EL1 accesses when running a VHE guest. In order to
        limit the cost of the emulation, implement it ass a shallow exit.
    
        In the other cases:
    
        - this is a nVHE L1 which will write to memory, and we don't trap
    
        - this is a L2 guest:
    
          * the L1 has CPTR_EL2.TCPAC==0, and the L2 has direct register
           access
    
          * the L1 has CPTR_EL2.TCPAC==1, and the L2 will trap, but the
            handling is defered to the general handling for forwarding
    
        Signed-off-by: Marc Zyngier <maz@kernel.org>
        Link: https://lore.kernel.org/r/20240620164653.1130714-10-oliver.upton@linux.dev
        Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    
    Signed-off-by: Shaoqin Huang <shahuang@redhat.com>

--- arch/arm64/kvm/hyp/vhe/switch.c
+++ arch/arm64/kvm/hyp/vhe/switch.c
@@ -306,9 +306,34 @@ static bool kvm_hyp_handle_tlbi_el2(struct kvm_vcpu *vcpu, u64 *exit_code)
 	return true;
 }
 
+static bool kvm_hyp_handle_cpacr_el1(struct kvm_vcpu *vcpu, u64 *exit_code)
+{
+	u64 esr = kvm_vcpu_get_esr(vcpu);
+	int rt;
+
+	if (!is_hyp_ctxt(vcpu) || esr_sys64_to_sysreg(esr) != SYS_CPACR_EL1)
+		return false;
+
+	rt = kvm_vcpu_sys_get_rt(vcpu);
+
+	if ((esr & ESR_ELx_SYS64_ISS_DIR_MASK) == ESR_ELx_SYS64_ISS_DIR_READ) {
+		vcpu_set_reg(vcpu, rt, __vcpu_sys_reg(vcpu, CPTR_EL2));
+	} else {
+		vcpu_write_sys_reg(vcpu, vcpu_get_reg(vcpu, rt), CPTR_EL2);
+		__activate_cptr_traps(vcpu);
+	}
+
+	__kvm_skip_instr(vcpu);
+
+	return true;
+}
+
 static bool kvm_hyp_handle_sysreg_vhe(struct kvm_vcpu *vcpu, u64 *exit_code)
 {
 	if (kvm_hyp_handle_tlbi_el2(vcpu, exit_code))
+		return true;
+
+	if (kvm_hyp_handle_cpacr_el1(vcpu, exit_code))
 		return true;
 
 	return kvm_hyp_handle_sysreg(vcpu, exit_code);
