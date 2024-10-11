f5608fe2bcb79ccf6415ea9de913f6213f996d09
Mark Salter <msalter@redhat.com>
2024-10-11 10:13:48 -0400

    coresight: etm4x: Fix unbalanced pm_runtime_enable()
    JIRA: https://issues.redhat.com/browse/RHEL-21456
    
    commit caa41c47dab7e1054f587e592ab21296e3a6781c
    Author: Anshuman Khandual <anshuman.khandual@arm.com>
    Date: Thu, 14 Mar 2024 11:28:33 +0530
    
        There is an unbalanced pm_runtime_enable() in etm4_probe_platform_dev()
        when etm4_probe() fails. This problem can be observed via the coresight
        etm4 module's (load -> unload -> load) sequence when etm4_probe() fails
        in etm4_probe_platform_dev().
    
        [   63.379943] coresight-etm4x 7040000.etm: Unbalanced pm_runtime_enable!
        [   63.393630] coresight-etm4x 7140000.etm: Unbalanced pm_runtime_enable!
        [   63.407455] coresight-etm4x 7240000.etm: Unbalanced pm_runtime_enable!
        [   63.420983] coresight-etm4x 7340000.etm: Unbalanced pm_runtime_enable!
        [   63.420999] coresight-etm4x 7440000.etm: Unbalanced pm_runtime_enable!
        [   63.441209] coresight-etm4x 7540000.etm: Unbalanced pm_runtime_enable!
        [   63.454689] coresight-etm4x 7640000.etm: Unbalanced pm_runtime_enable!
        [   63.474982] coresight-etm4x 7740000.etm: Unbalanced pm_runtime_enable!
    
        This fixes the above problem - with an explicit pm_runtime_disable() call
        when etm4_probe() fails during etm4_probe_platform_dev().
    
        Cc: Lorenzo Pieralisi <lpieralisi@kernel.org>
        Cc: Hanjun Guo <guohanjun@huawei.com>
        Cc: Sudeep Holla <sudeep.holla@arm.com>
        Cc: "Rafael J. Wysocki" <rafael@kernel.org>
        Cc: Len Brown <lenb@kernel.org>
        Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
        Cc: Mike Leach <mike.leach@linaro.org>
        Cc: James Clark <james.clark@arm.com>
        Cc: Leo Yan <leo.yan@linaro.org>
        Cc: linux-acpi@vger.kernel.org
        Cc: linux-arm-kernel@lists.infradead.org
        Cc: linux-kernel@vger.kernel.org
        Cc: coresight@lists.linaro.org
        Fixes: 5214b563588e ("coresight: etm4x: Add support for sysreg only devices")
        Reviewed-by: James Clark <james.clark@arm.com>
        Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
        Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
        Link: https://lore.kernel.org/r/20240314055843.2625883-2-anshuman.khandual@arm.com
    
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -2214,6 +2214,9 @@ static int etm4_probe_platform_dev(struct platform_device *pdev)
 	ret = etm4_probe(&pdev->dev);
 
 	pm_runtime_put(&pdev->dev);
+	if (ret)
+		pm_runtime_disable(&pdev->dev);
+
 	return ret;
 }
 
