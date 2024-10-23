90de66448926ed9125003c0a8558246bd30af77e
Viktor Malik <vmalik@redhat.com>
2024-10-23 12:44:03 +0200

    bpf: Replace deprecated strncpy with strscpy
    JIRA: https://issues.redhat.com/browse/RHEL-30773
    
    commit 2e114248e086fb376405ed3f89b220f8586a2541
    Author: Justin Stitt <justinstitt@google.com>
    Date:   Tue Apr 2 23:52:50 2024 +0000
    
        bpf: Replace deprecated strncpy with strscpy
        
        strncpy() is deprecated for use on NUL-terminated destination strings
        [1] and as such we should prefer more robust and less ambiguous string
        interfaces.
        
        bpf sym names get looked up and compared/cleaned with various string
        apis. This suggests they need to be NUL-terminated (strncpy() suggests
        this but does not guarantee it).
        
        |	static int compare_symbol_name(const char *name, char *namebuf)
        |	{
        |		cleanup_symbol_name(namebuf);
        |		return strcmp(name, namebuf);
        |	}
        
        |	static void cleanup_symbol_name(char *s)
        |	{
        |		...
        |		res = strstr(s, ".llvm.");
        |		...
        |	}
        
        Use strscpy() as this method guarantees NUL-termination on the
        destination buffer.
        
        This patch also replaces two uses of strncpy() used in log.c. These are
        simple replacements as postfix has been zero-initialized on the stack
        and has source arguments with a size less than the destination's size.
        
        Note that this patch uses the new 2-argument version of strscpy
        introduced in commit e6584c3964f2f ("string: Allow 2-argument strscpy()").
        
        Signed-off-by: Justin Stitt <justinstitt@google.com>
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Acked-by: Daniel Borkmann <daniel@iogearbox.net>
        Link: https://www.kernel.org/doc/html/latest/process/deprecated.html#strncpy-on-nul-terminated-strings [1]
        Link: https://manpages.debian.org/testing/linux-manual-4.8/strscpy.9.en.html [2]
        Link: https://github.com/KSPP/linux/issues/90
        Link: https://lore.kernel.org/bpf/20240402-strncpy-kernel-bpf-core-c-v1-1-7cb07a426e78@google.com
    
    Signed-off-by: Viktor Malik <vmalik@redhat.com>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -748,7 +748,7 @@ const char *__bpf_address_lookup(unsigned long addr, unsigned long *size,
 		unsigned long symbol_start = ksym->start;
 		unsigned long symbol_end = ksym->end;
 
-		strncpy(sym, ksym->name, KSYM_NAME_LEN);
+		strscpy(sym, ksym->name, KSYM_NAME_LEN);
 
 		ret = sym;
 		if (size)
@@ -814,7 +814,7 @@ int bpf_get_kallsym(unsigned int symnum, unsigned long *value, char *type,
 		if (it++ != symnum)
 			continue;
 
-		strncpy(sym, ksym->name, KSYM_NAME_LEN);
+		strscpy(sym, ksym->name, KSYM_NAME_LEN);
 
 		*value = ksym->start;
 		*type  = BPF_SYM_ELF_TYPE;
