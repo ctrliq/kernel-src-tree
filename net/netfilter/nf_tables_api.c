ea628a4485cc479352d02199f9d47dc8c9eaa879
Greg Rose <g.v.rose@ciq.com>
2024-10-23 11:11:26 -0700

    netfilter: nf_tables: adapt set backend to use GC transaction API
    jira VULN-429
    cve CVE-2023-4244
    commit-author Pablo Neira Ayuso <pablo@netfilter.org>'
    commit f6c383b8c31a93752a52697f8430a71dcbc46adf
    upstream-diff - Upstream fuzz and conflicts.  Resolved by pointing
    to the rocky8_10 branch as the source of truth.
    
    Use the GC transaction API to replace the old and buggy gc API and the
    busy mark approach.
    
    No set elements are removed from async garbage collection anymore,
    instead the _DEAD bit is set on so the set element is not visible from
    lookup path anymore. Async GC enqueues transaction work that might be
    aborted and retried later.
    
    rbtree and pipapo set backends does not set on the _DEAD bit from the
    sync GC path since this runs in control plane path where mutex is held.
    In this case, set elements are deactivated, removed and then released
    via RCU callback, sync GC never fails.
    
    Fixes: 3c4287f62044 ("nf_tables: Add set type for arbitrary concatenation of ranges")
    Fixes: 8d8540c4f5e0 ("netfilter: nft_set_rbtree: add timeout support")
    Fixes: 9d0982927e79 ("netfilter: nft_hash: add support for timeouts")
    	Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (cherry picked from commit f6c383b8c31a93752a52697f8430a71dcbc46adf)
    	Signed-off-by: Greg Rose <g.v.rose@ciq.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -5778,6 +5778,7 @@ void nft_setelem_data_deactivate(const struct net *net,
 	if (nft_set_ext_exists(ext, NFT_SET_EXT_OBJREF))
 		nft_use_dec(&(*nft_set_ext_obj(ext))->use);
 }
+EXPORT_SYMBOL_GPL(nft_setelem_data_deactivate);
 
 static int nft_del_setelem(struct nft_ctx *ctx, struct nft_set *set,
 			   const struct nlattr *attr)
@@ -7846,6 +7847,7 @@ void nft_trans_gc_destroy(struct nft_trans_gc *trans)
 	put_net(trans->net);
 	kfree(trans);
 }
+EXPORT_SYMBOL_GPL(nft_trans_gc_destroy);
 
 static void nft_trans_gc_trans_free(struct rcu_head *rcu)
 {
@@ -7933,11 +7935,13 @@ struct nft_trans_gc *nft_trans_gc_alloc(struct nft_set *set,
 
 	return trans;
 }
+EXPORT_SYMBOL_GPL(nft_trans_gc_alloc);
 
 void nft_trans_gc_elem_add(struct nft_trans_gc *trans, void *priv)
 {
 	trans->priv[trans->count++] = priv;
 }
+EXPORT_SYMBOL_GPL(nft_trans_gc_elem_add);
 
 static void nft_trans_gc_queue_work(struct nft_trans_gc *trans)
 {
@@ -7963,6 +7967,7 @@ struct nft_trans_gc *nft_trans_gc_queue_async(struct nft_trans_gc *gc,
 
 	return nft_trans_gc_alloc(gc->set, gc_seq, gfp);
 }
+EXPORT_SYMBOL_GPL(nft_trans_gc_queue_async);
 
 void nft_trans_gc_queue_async_done(struct nft_trans_gc *trans)
 {
@@ -7973,6 +7978,7 @@ void nft_trans_gc_queue_async_done(struct nft_trans_gc *trans)
 
 	nft_trans_gc_queue_work(trans);
 }
+EXPORT_SYMBOL_GPL(nft_trans_gc_queue_async_done);
 
 struct nft_trans_gc *nft_trans_gc_queue_sync(struct nft_trans_gc *gc, gfp_t gfp)
 {
@@ -7986,6 +7992,7 @@ struct nft_trans_gc *nft_trans_gc_queue_sync(struct nft_trans_gc *gc, gfp_t gfp)
 
 	return nft_trans_gc_alloc(gc->set, 0, gfp);
 }
+EXPORT_SYMBOL_GPL(nft_trans_gc_queue_sync);
 
 void nft_trans_gc_queue_sync_done(struct nft_trans_gc *trans)
 {
@@ -7998,6 +8005,7 @@ void nft_trans_gc_queue_sync_done(struct nft_trans_gc *trans)
 
 	call_rcu(&trans->rcu, nft_trans_gc_trans_free);
 }
+EXPORT_SYMBOL_GPL(nft_trans_gc_queue_sync_done);
 
 static void nf_tables_module_autoload_cleanup(struct net *net)
 {
