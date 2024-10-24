6ef8d82285847de8b62882a1abbbb425854c9c0d
Viktor Malik <vmalik@redhat.com>
2024-10-24 15:17:21 +0200

    powerpc/bpf: enable kfunc call
    JIRA: https://issues.redhat.com/browse/RHEL-30773
    
    Conflicts: Omitting the part for PCREL which is not present in RHEL
               (upstream commit 7e3a68be42e1 ("powerpc/64: vmlinux support
               building with PCREL addresing"))
    
    commit 61688a82e047a4166436bf2665716cc070572ffa
    Author: Hari Bathini <hbathini@linux.ibm.com>
    Date:   Thu May 2 23:02:05 2024 +0530
    
        powerpc/bpf: enable kfunc call
    
        Currently, bpf jit code on powerpc assumes all the bpf functions and
        helpers to be part of core kernel text. This is false for kfunc case,
        as function addresses may not be part of core kernel text area. So,
        add support for addresses that are not within core kernel text area
        too, to enable kfunc support. Emit instructions based on whether the
        function address is within core kernel text address or not, to retain
        optimized instruction sequence where possible.
    
        In case of PCREL, as a bpf function that is not within core kernel
        text area is likely to go out of range with relative addressing on
        kernel base, use PC relative addressing. If that goes out of range,
        load the full address with PPC_LI64().
    
        With addresses that are not within core kernel text area supported,
        override bpf_jit_supports_kfunc_call() to enable kfunc support. Also,
        override bpf_jit_supports_far_kfunc_call() to enable 64-bit pointers,
        as an address offset can be more than 32-bit long on PPC64.
    
        Signed-off-by: Hari Bathini <hbathini@linux.ibm.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
        Link: https://msgid.link/20240502173205.142794-2-hbathini@linux.ibm.com
    
    Signed-off-by: Viktor Malik <vmalik@redhat.com>

--- arch/powerpc/net/bpf_jit_comp64.c
+++ arch/powerpc/net/bpf_jit_comp64.c
@@ -205,19 +205,45 @@ static int bpf_jit_emit_func_call_hlp(u32 *image, struct codegen_context *ctx, u
 	unsigned long func_addr = func ? ppc_function_entry((void *)func) : 0;
 	long reladdr;
 
-	if (WARN_ON_ONCE(!core_kernel_text(func_addr)))
+	if (WARN_ON_ONCE(!kernel_text_address(func_addr)))
 		return -EINVAL;
 
-	reladdr = func_addr - kernel_toc_addr();
-	if (reladdr > 0x7FFFFFFF || reladdr < -(0x80000000L)) {
-		pr_err("eBPF: address of %ps out of range of kernel_toc.\n", (void *)func);
-		return -ERANGE;
-	}
+	if (core_kernel_text(func_addr)) {
+		reladdr = func_addr - kernel_toc_addr();
+		if (reladdr > 0x7FFFFFFF || reladdr < -(0x80000000L)) {
+			pr_err("eBPF: address of %ps out of range of kernel_toc.\n", (void *)func);
+			return -ERANGE;
+		}
 
-	EMIT(PPC_RAW_ADDIS(_R12, _R2, PPC_HA(reladdr)));
-	EMIT(PPC_RAW_ADDI(_R12, _R12, PPC_LO(reladdr)));
-	EMIT(PPC_RAW_MTCTR(_R12));
-	EMIT(PPC_RAW_BCTRL());
+		EMIT(PPC_RAW_ADDIS(_R12, _R2, PPC_HA(reladdr)));
+		EMIT(PPC_RAW_ADDI(_R12, _R12, PPC_LO(reladdr)));
+		EMIT(PPC_RAW_MTCTR(_R12));
+		EMIT(PPC_RAW_BCTRL());
+	} else {
+		if (IS_ENABLED(CONFIG_PPC64_ELF_ABI_V1)) {
+			/* func points to the function descriptor */
+			PPC_LI64(bpf_to_ppc(TMP_REG_2), func);
+			/* Load actual entry point from function descriptor */
+			EMIT(PPC_RAW_LD(bpf_to_ppc(TMP_REG_1), bpf_to_ppc(TMP_REG_2), 0));
+			/* ... and move it to CTR */
+			EMIT(PPC_RAW_MTCTR(bpf_to_ppc(TMP_REG_1)));
+			/*
+			 * Load TOC from function descriptor at offset 8.
+			 * We can clobber r2 since we get called through a
+			 * function pointer (so caller will save/restore r2).
+			 */
+			EMIT(PPC_RAW_LD(_R2, bpf_to_ppc(TMP_REG_2), 8));
+		} else {
+			PPC_LI64(_R12, func);
+			EMIT(PPC_RAW_MTCTR(_R12));
+		}
+		EMIT(PPC_RAW_BCTRL());
+		/*
+		 * Load r2 with kernel TOC as kernel TOC is used if function address falls
+		 * within core kernel text.
+		 */
+		EMIT(PPC_RAW_LD(_R2, _R13, offsetof(struct paca_struct, kernel_toc)));
+	}
 
 	return 0;
 }
