8a3a08d0154cf77bd7c727ddedd4df42a69fd932
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2024-10-25 12:00:30 +0200

    timer: Update the ktimersd series.
    This is an all-in-one update of the ktimersd updating it to the latest
    posting as of v2.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -616,14 +616,27 @@ extern void __raise_softirq_irqoff(unsigned int nr);
 extern void raise_softirq_irqoff(unsigned int nr);
 extern void raise_softirq(unsigned int nr);
 
+/*
+ * Handle timers in a dedicated thread at a low SCHED_FIFO priority instead in
+ * ksoftirqd as to be prefred over SCHED_NORMAL tasks.
+ */
 #ifdef CONFIG_PREEMPT_RT
 DECLARE_PER_CPU(struct task_struct *, timersd);
 DECLARE_PER_CPU(unsigned long, pending_timer_softirq);
 
-extern void raise_timer_softirq(void);
-extern void raise_hrtimer_softirq(void);
+void raise_ktimers_thread(unsigned int nr);
 
-static inline unsigned int local_pending_timers(void)
+static inline void raise_timer_softirq(void)
+{
+	raise_ktimers_thread(TIMER_SOFTIRQ);
+}
+
+static inline void raise_hrtimer_softirq(void)
+{
+	raise_ktimers_thread(HRTIMER_SOFTIRQ);
+}
+
+static inline unsigned int local_timers_pending(void)
 {
 	return __this_cpu_read(pending_timer_softirq);
 }
@@ -631,15 +644,17 @@ static inline unsigned int local_pending_timers(void)
 #else
 static inline void raise_timer_softirq(void)
 {
-	raise_softirq(TIMER_SOFTIRQ);
+	lockdep_assert_in_irq();
+	__raise_softirq_irqoff(TIMER_SOFTIRQ);
 }
 
 static inline void raise_hrtimer_softirq(void)
 {
-	raise_softirq_irqoff(HRTIMER_SOFTIRQ);
+	lockdep_assert_in_irq();
+	__raise_softirq_irqoff(HRTIMER_SOFTIRQ);
 }
 
-static inline unsigned int local_pending_timers(void)
+static inline unsigned int local_timers_pending(void)
 {
 	return local_softirq_pending();
 }
