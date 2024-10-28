ef09c13fd10457064acd426534f529d9a59bc6d5
Antoine Tenart <atenart@redhat.com>
2024-10-28 16:49:53 +0100

    inet: fix inet_fill_ifaddr() flags truncation
    JIRA: https://issues.redhat.com/browse/RHEL-62204
    Upstream Status: linux.git
    Conflicts:\
    - Context difference due to missing upstream commit 47f0bd503210 ("net:
      Add new protocol attribute to IP addresses") in c9s.
    
    commit 1af7f88af269c4e06a4dc3bc920ff6cdf7471124
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Fri May 10 07:29:32 2024 +0000
    
        inet: fix inet_fill_ifaddr() flags truncation
    
        I missed that (struct ifaddrmsg)->ifa_flags was only 8bits,
        while (struct in_ifaddr)->ifa_flags is 32bits.
    
        Use a temporary 32bit variable as I did in set_ifa_lifetime()
        and check_lifetime().
    
        Fixes: 3ddc2231c810 ("inet: annotate data-races around ifa->ifa_flags")
        Reported-by: Yu Watanabe <watanabe.yu@gmail.com>
        Dianosed-by: Yu Watanabe <watanabe.yu@gmail.com>
        Closes: https://github.com/systemd/systemd/pull/32666#issuecomment-2103977928
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reviewed-by: Larysa Zaremba <larysa.zaremba@intel.com>
        Reviewed-by: David Ahern <dsahern@kernel.org>
        Link: https://lore.kernel.org/r/20240510072932.2678952-1-edumazet@google.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -1672,6 +1672,7 @@ static int inet_fill_ifaddr(struct sk_buff *skb, const struct in_ifaddr *ifa,
 	struct nlmsghdr  *nlh;
 	unsigned long tstamp;
 	u32 preferred, valid;
+	u32 flags;
 
 	nlh = nlmsg_put(skb, args->portid, args->seq, args->event, sizeof(*ifm),
 			args->flags);
@@ -1681,7 +1682,13 @@ static int inet_fill_ifaddr(struct sk_buff *skb, const struct in_ifaddr *ifa,
 	ifm = nlmsg_data(nlh);
 	ifm->ifa_family = AF_INET;
 	ifm->ifa_prefixlen = ifa->ifa_prefixlen;
-	ifm->ifa_flags = READ_ONCE(ifa->ifa_flags);
+
+	flags = READ_ONCE(ifa->ifa_flags);
+	/* Warning : ifm->ifa_flags is an __u8, it holds only 8 bits.
+	 * The 32bit value is given in IFA_FLAGS attribute.
+	 */
+	ifm->ifa_flags = (__u8)flags;
+
 	ifm->ifa_scope = ifa->ifa_scope;
 	ifm->ifa_index = ifa->ifa_dev->dev->ifindex;
 
@@ -1690,7 +1697,7 @@ static int inet_fill_ifaddr(struct sk_buff *skb, const struct in_ifaddr *ifa,
 		goto nla_put_failure;
 
 	tstamp = READ_ONCE(ifa->ifa_tstamp);
-	if (!(ifm->ifa_flags & IFA_F_PERMANENT)) {
+	if (!(flags & IFA_F_PERMANENT)) {
 		preferred = READ_ONCE(ifa->ifa_preferred_lft);
 		valid = READ_ONCE(ifa->ifa_valid_lft);
 		if (preferred != INFINITY_LIFE_TIME) {
@@ -1719,7 +1726,7 @@ static int inet_fill_ifaddr(struct sk_buff *skb, const struct in_ifaddr *ifa,
 	     nla_put_in_addr(skb, IFA_BROADCAST, ifa->ifa_broadcast)) ||
 	    (ifa->ifa_label[0] &&
 	     nla_put_string(skb, IFA_LABEL, ifa->ifa_label)) ||
-	    nla_put_u32(skb, IFA_FLAGS, ifm->ifa_flags) ||
+	    nla_put_u32(skb, IFA_FLAGS, flags) ||
 	    (ifa->ifa_rt_priority &&
 	     nla_put_u32(skb, IFA_RT_PRIORITY, ifa->ifa_rt_priority)) ||
 	    put_cacheinfo(skb, READ_ONCE(ifa->ifa_cstamp), tstamp,
