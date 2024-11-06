05b6ce4daeabb43d34983870fd250fc2a54cc868
Waiman Long <longman@redhat.com>
2024-11-05 23:28:53 -0500

    vfs: dcache: move hashlen_hash() from callers into d_hash()
    JIRA: https://issues.redhat.com/browse/RHEL-65965
    Conflicts: Also include partial backport of the fs/dcache.c hunks from
    	   commit 68279f9c9f59 ("treewide: mark stuff as __ro_after_init").
    
    commit e60cc61153e61e4e38bd983492df9959e82ae4dc
    Author: Linus Torvalds <torvalds@linux-foundation.org>
    Date:   Mon, 10 Jun 2024 11:47:06 -0700
    
        vfs: dcache: move hashlen_hash() from callers into d_hash()
    
        Both __d_lookup_rcu() and __d_lookup_rcu_op_compare() have the full
        'name_hash' value of the qstr that they want to look up, and mask it off
        to just the low 32-bit hash before calling down to d_hash().
    
        Other callers just load the 32-bit hash and pass it as the argument.
    
        If we move the masking into d_hash() itself, it simplifies the two
        callers that currently do the masking, and is a no-op for the other
        cases.  It doesn't actually change the generated code since the compiler
        will inline d_hash() and see that the end result is the same.
    
        [ Technically, since the parse tree changes, the code generation may not
          be 100% the same, and for me on x86-64, this does result in gcc
          switching the operands around for one 'cmpl' instruction. So not
          necessarily the exact same code generation, but equivalent ]
    
        However, this does encapsulate the 'd_hash()' operation more, and makes
        the shift operation in particular be a "shift 32 bits right, return full
        word".  Which matches the instruction semantics on both x86-64 and arm64
        better, since a 32-bit shift will clear the upper bits.
    
        That makes the next step of introducing a "shift by runtime constant"
        more obvious and generates the shift with no extraneous type masking.
    
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -78,7 +78,7 @@ __cacheline_aligned_in_smp DEFINE_SEQLOCK(rename_lock);
 
 EXPORT_SYMBOL(rename_lock);
 
-static struct kmem_cache *dentry_cache __read_mostly;
+static struct kmem_cache *dentry_cache __ro_after_init;
 
 const struct qstr empty_name = QSTR_INIT("", 0);
 EXPORT_SYMBOL(empty_name);
@@ -96,13 +96,13 @@ EXPORT_SYMBOL(dotdot_name);
  * information, yet avoid using a prime hash-size or similar.
  */
 
-static unsigned int d_hash_shift __read_mostly;
+static unsigned int d_hash_shift __ro_after_init;
 
-static struct hlist_bl_head *dentry_hashtable __read_mostly;
+static struct hlist_bl_head *dentry_hashtable __ro_after_init;
 
-static inline struct hlist_bl_head *d_hash(unsigned int hash)
+static inline struct hlist_bl_head *d_hash(unsigned long hashlen)
 {
-	return dentry_hashtable + (hash >> d_hash_shift);
+	return dentry_hashtable + ((u32)hashlen >> d_hash_shift);
 }
 
 #define IN_LOOKUP_SHIFT 10
@@ -2112,7 +2112,7 @@ static noinline struct dentry *__d_lookup_rcu_op_compare(
 	unsigned *seqp)
 {
 	u64 hashlen = name->hash_len;
-	struct hlist_bl_head *b = d_hash(hashlen_hash(hashlen));
+	struct hlist_bl_head *b = d_hash(hashlen);
 	struct hlist_bl_node *node;
 	struct dentry *dentry;
 
@@ -2179,7 +2179,7 @@ struct dentry *__d_lookup_rcu(const struct dentry *parent,
 {
 	u64 hashlen = name->hash_len;
 	const unsigned char *str = name->name;
-	struct hlist_bl_head *b = d_hash(hashlen_hash(hashlen));
+	struct hlist_bl_head *b = d_hash(hashlen);
 	struct hlist_bl_node *node;
 	struct dentry *dentry;
 
@@ -3162,7 +3162,7 @@ static void __init dcache_init(void)
 }
 
 /* SLAB cache for __getname() consumers */
-struct kmem_cache *names_cachep __read_mostly;
+struct kmem_cache *names_cachep __ro_after_init;
 EXPORT_SYMBOL(names_cachep);
 
 void __init vfs_caches_init_early(void)
