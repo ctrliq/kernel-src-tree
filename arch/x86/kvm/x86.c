a70eb52b55e94310becc02832e79681da8d5bfb9
Maxim Levitsky <mlevitsk@redhat.com>
2024-11-12 16:50:16 -0500

    KVM: X86: Remove unnecessary GFP_KERNEL_ACCOUNT for temporary variables
    JIRA: https://issues.redhat.com/browse/RHEL-64444
    
    commit dd103407ca315b467074d74b3580abe210c4c695
    Author: Peng Hao <flyingpeng@tencent.com>
    Date:   Mon Jun 24 09:20:16 2024 +0800
    
        KVM: X86: Remove unnecessary GFP_KERNEL_ACCOUNT for temporary variables
    
        Some variables allocated in kvm_arch_vcpu_ioctl are released when
        the function exits, so there is no need to set GFP_KERNEL_ACCOUNT.
    
        Signed-off-by: Peng Hao <flyingpeng@tencent.com>
        Link: https://lore.kernel.org/r/20240624012016.46133-1-flyingpeng@tencent.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -5887,8 +5887,7 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 		r = -EINVAL;
 		if (!lapic_in_kernel(vcpu))
 			goto out;
-		u.lapic = kzalloc(sizeof(struct kvm_lapic_state),
-				GFP_KERNEL_ACCOUNT);
+		u.lapic = kzalloc(sizeof(struct kvm_lapic_state), GFP_KERNEL);
 
 		r = -ENOMEM;
 		if (!u.lapic)
@@ -6083,7 +6082,7 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 		if (vcpu->arch.guest_fpu.uabi_size > sizeof(struct kvm_xsave))
 			break;
 
-		u.xsave = kzalloc(sizeof(struct kvm_xsave), GFP_KERNEL_ACCOUNT);
+		u.xsave = kzalloc(sizeof(struct kvm_xsave), GFP_KERNEL);
 		r = -ENOMEM;
 		if (!u.xsave)
 			break;
@@ -6114,7 +6113,7 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 	case KVM_GET_XSAVE2: {
 		int size = vcpu->arch.guest_fpu.uabi_size;
 
-		u.xsave = kzalloc(size, GFP_KERNEL_ACCOUNT);
+		u.xsave = kzalloc(size, GFP_KERNEL);
 		r = -ENOMEM;
 		if (!u.xsave)
 			break;
@@ -6132,7 +6131,7 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 	}
 
 	case KVM_GET_XCRS: {
-		u.xcrs = kzalloc(sizeof(struct kvm_xcrs), GFP_KERNEL_ACCOUNT);
+		u.xcrs = kzalloc(sizeof(struct kvm_xcrs), GFP_KERNEL);
 		r = -ENOMEM;
 		if (!u.xcrs)
 			break;
