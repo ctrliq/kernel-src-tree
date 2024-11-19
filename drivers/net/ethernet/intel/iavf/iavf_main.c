eb1bdb9890e5e078a76422eaa4ff4795c2b540ff
Petr Oros <poros@redhat.com>
2024-11-19 14:51:28 +0100

    net: intel: Remove MODULE_AUTHORs
    JIRA: https://issues.redhat.com/browse/RHEL-59099
    
    Conflicts:
    - omitted i40e chunk because it is already in the tree
    
    Upstream commit(s):
    commit bf130ed3aaa37bbd9c7f4b305c42e02128187cd4
    Author: Tony Nguyen <anthony.l.nguyen@intel.com>
    Date:   Tue Jul 2 14:38:46 2024 -0700
    
        net: intel: Remove MODULE_AUTHORs
    
        We are moving away from the Sourceforge email address. Rather than
        removing or updating the email for the affected entries, remove the
        MODULE_AUTHOR altogether as its usage is incorrect [1].
    
        Link: https://lore.kernel.org/netdev/20200626115236.7f36d379@kicinski-fedora-pc1c0hjn.dhcp.thefacebook.com/ [1]
        Reviewed-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
        Acked-by: Alexander Lobakin <aleksander.lobakin@intel.com> # libeth, libie
        Reviewed-by: Simon Horman <horms@kernel.org>
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    
    Signed-off-by: Petr Oros <poros@redhat.com>

--- drivers/net/ethernet/intel/iavf/iavf_main.c
+++ drivers/net/ethernet/intel/iavf/iavf_main.c
@@ -43,7 +43,6 @@ static const struct pci_device_id iavf_pci_tbl[] = {
 MODULE_DEVICE_TABLE(pci, iavf_pci_tbl);
 
 MODULE_ALIAS("i40evf");
-MODULE_AUTHOR("Intel Corporation, <linux.nics@intel.com>");
 MODULE_DESCRIPTION("Intel(R) Ethernet Adaptive Virtual Function Network Driver");
 MODULE_IMPORT_NS(LIBIE);
 MODULE_LICENSE("GPL v2");
