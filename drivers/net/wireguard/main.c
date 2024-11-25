ab39cead6a6b3dc37f15c3b21175fc0dcaf154b1
Petr Oros <poros@redhat.com>
2024-11-25 10:55:01 +0100

    genetlink: remove linux/genetlink.h
    JIRA: https://issues.redhat.com/browse/RHEL-57756
    
    Upstream commit(s):
    commit cd7209628cdb2a7edd7656c126d2455e7102e949
    Author: Jakub Kicinski <kuba@kernel.org>
    Date:   Fri Mar 29 10:57:10 2024 -0700
    
        genetlink: remove linux/genetlink.h
    
        genetlink.h is a shell of what used to be a combined uAPI
        and kernel header over a decade ago. It has fewer than
        10 lines of code. Merge it into net/genetlink.h.
        In some ways it'd be better to keep the combined header
        under linux/ but it would make looking through git history
        harder.
    
        Acked-by: Sven Eckelmann <sven@narfation.org>
        Link: https://lore.kernel.org/r/20240329175710.291749-4-kuba@kernel.org
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Petr Oros <poros@redhat.com>

--- drivers/net/wireguard/main.c
+++ drivers/net/wireguard/main.c
@@ -15,7 +15,7 @@
 #include <linux/fips.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/genetlink.h>
+#include <net/genetlink.h>
 #include <net/rtnetlink.h>
 
 static int __init wg_mod_init(void)
