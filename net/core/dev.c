96cea0886511788d765d4d1deb40bfc586b5ff49
Petr Oros <poros@redhat.com>
2024-11-25 10:55:06 +0100

    net-core: use netdev_* calls for kernel messages
    JIRA: https://issues.redhat.com/browse/RHEL-57756
    
    Upstream commit(s):
    commit 5b92be649605504e1019a1ad0c95b0d74a4e2be1
    Author: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Date:   Tue Oct 19 09:42:28 2021 -0700
    
        net-core: use netdev_* calls for kernel messages
    
        While loading a driver and changing the number of queues, I noticed this
        message in the kernel log:
    
        "[253489.070080] Number of in use tx queues changed invalidating tc
        mappings. Priority traffic classification disabled!"
    
        But I had no idea what interface was being talked about because this
        message used pr_warn().
    
        After investigating, it appears we can use the netdev_* helpers already
        defined to create predictably formatted messages, and that already handle
        <unknown netdev> cases, in more of the messages in dev.c.
    
        After this change, this message (and others) will look like this:
        "[  170.181093] ice 0000:3b:00.0 ens785f0: Number of in use tx queues
        changed invalidating tc mappings. Priority traffic classification
        disabled!"
    
        One goal here was not to change the message significantly from the
        original format so as to not break user's expectations, so I just
        changed messages that used pr_* and generally started with %s ==
        dev->name.
    
        Signed-off-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Petr Oros <poros@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1304,8 +1304,8 @@ rollback:
 			old_assign_type = NET_NAME_RENAMED;
 			goto rollback;
 		} else {
-			pr_err("%s: name change rollback failed: %d\n",
-			       dev->name, ret);
+			netdev_err(dev, "name change rollback failed: %d\n",
+				   ret);
 		}
 	}
 
@@ -2400,7 +2400,7 @@ static void netif_setup_tc(struct net_device *dev, unsigned int txq)
 
 	/* If TC0 is invalidated disable TC mapping */
 	if (tc->offset + tc->count > txq) {
-		pr_warn("Number of in use tx queues changed invalidating tc mappings. Priority traffic classification disabled!\n");
+		netdev_warn(dev, "Number of in use tx queues changed invalidating tc mappings. Priority traffic classification disabled!\n");
 		dev->num_tc = 0;
 		return;
 	}
@@ -2411,8 +2411,8 @@ static void netif_setup_tc(struct net_device *dev, unsigned int txq)
 
 		tc = &dev->tc_to_txq[q];
 		if (tc->offset + tc->count > txq) {
-			pr_warn("Number of in use tx queues changed. Priority %i to tc mapping %i is no longer valid. Setting map to 0\n",
-				i, q);
+			netdev_warn(dev, "Number of in use tx queues changed. Priority %i to tc mapping %i is no longer valid. Setting map to 0\n",
+				    i, q);
 			netdev_set_prio_tc_map(dev, i, 0);
 		}
 	}
@@ -3417,7 +3417,7 @@ __be16 skb_network_protocol(struct sk_buff *skb, int *depth)
 #ifdef CONFIG_BUG
 static void do_netdev_rx_csum_fault(struct net_device *dev, struct sk_buff *skb)
 {
-	pr_err("%s: hw csum failure\n", dev ? dev->name : "<unknown>");
+	netdev_err(dev, "hw csum failure\n");
 	skb_dump(KERN_ERR, skb, true);
 	dump_stack();
 }
@@ -6665,8 +6665,8 @@ static int __napi_poll(struct napi_struct *n, bool *repoll)
 	}
 
 	if (unlikely(work > weight))
-		pr_err_once("NAPI poll function %pS returned %d, exceeding its budget of %d.\n",
-			    n->poll, work, weight);
+		netdev_err_once(n->dev, "NAPI poll function %pS returned %d, exceeding its budget of %d.\n",
+				n->poll, work, weight);
 
 	if (likely(work < weight))
 		return work;
@@ -8487,8 +8487,7 @@ static int __dev_set_promiscuity(struct net_device *dev, int inc, bool notify)
 			dev->flags &= ~IFF_PROMISC;
 		else {
 			dev->promiscuity -= inc;
-			pr_warn("%s: promiscuity touches roof, set promiscuity failed. promiscuity feature of device might be broken.\n",
-				dev->name);
+			netdev_warn(dev, "promiscuity touches roof, set promiscuity failed. promiscuity feature of device might be broken.\n");
 			return -EOVERFLOW;
 		}
 	}
@@ -8558,8 +8557,7 @@ static int __dev_set_allmulti(struct net_device *dev, int inc, bool notify)
 			dev->flags &= ~IFF_ALLMULTI;
 		else {
 			dev->allmulti -= inc;
-			pr_warn("%s: allmulti touches roof, set allmulti failed. allmulti feature of device might be broken.\n",
-				dev->name);
+			netdev_warn(dev, "allmulti touches roof, set allmulti failed. allmulti feature of device might be broken.\n");
 			return -EOVERFLOW;
 		}
 	}
