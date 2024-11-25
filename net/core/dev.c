ce54afa357ed687b76c1b4ca3abd85edecbb8d75
Petr Oros <poros@redhat.com>
2024-11-25 10:55:05 +0100

    rtnetlink: do not depend on RTNL for IFLA_TXQLEN output
    JIRA: https://issues.redhat.com/browse/RHEL-57756
    
    Upstream commit(s):
    commit ad13b5b0d1f9eb8e048394919e6393e520b14552
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Fri May 3 19:20:54 2024 +0000
    
        rtnetlink: do not depend on RTNL for IFLA_TXQLEN output
    
        rtnl_fill_ifinfo() can read dev->tx_queue_len locklessly,
        granted we add corresponding READ_ONCE()/WRITE_ONCE() annotations.
    
        Add missing READ_ONCE(dev->tx_queue_len) in teql_enqueue()
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Petr Oros <poros@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -8887,7 +8887,7 @@ int dev_change_tx_queue_len(struct net_device *dev, unsigned long new_len)
 		return -ERANGE;
 
 	if (new_len != orig_len) {
-		dev->tx_queue_len = new_len;
+		WRITE_ONCE(dev->tx_queue_len, new_len);
 		res = call_netdevice_notifiers(NETDEV_CHANGE_TX_QUEUE_LEN, dev);
 		res = notifier_to_errno(res);
 		if (res)
@@ -8901,7 +8901,7 @@ int dev_change_tx_queue_len(struct net_device *dev, unsigned long new_len)
 
 err_rollback:
 	netdev_err(dev, "refused to change device tx_queue_len\n");
-	dev->tx_queue_len = orig_len;
+	WRITE_ONCE(dev->tx_queue_len, orig_len);
 	return res;
 }
 
