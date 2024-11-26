7fee3cc5299566da4032bb6d6317971acdaa1a37
Izabela Bakollari <ibakolla@redhat.com>
2024-11-26 13:06:41 +0100

    net: phy: Use for_each_available_child_of_node_scoped()
    JIRA: https://issues.redhat.com/browse/RHEL-57766
    
    commit 1dce520abd461efcaf4a5a942d8eb833eef52d0b
    Author: Jinjie Ruan <ruanjinjie@huawei.com>
    Date:   Fri Aug 30 11:13:20 2024 +0800
    
        net: phy: Use for_each_available_child_of_node_scoped()
    
        Avoid need to manually handle of_node_put() by using
        for_each_available_child_of_node_scoped(), which can simplfy code.
    
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
        Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
        Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -3379,7 +3379,7 @@ static int of_phy_led(struct phy_device *phydev,
 static int of_phy_leds(struct phy_device *phydev)
 {
 	struct device_node *node = phydev->mdio.dev.of_node;
-	struct device_node *leds, *led;
+	struct device_node *leds;
 	int err;
 
 	if (!IS_ENABLED(CONFIG_OF_MDIO))
@@ -3392,10 +3392,9 @@ static int of_phy_leds(struct phy_device *phydev)
 	if (!leds)
 		return 0;
 
-	for_each_available_child_of_node(leds, led) {
+	for_each_available_child_of_node_scoped(leds, led) {
 		err = of_phy_led(phydev, led);
 		if (err) {
-			of_node_put(led);
 			of_node_put(leds);
 			phy_leds_unregister(phydev);
 			return err;
