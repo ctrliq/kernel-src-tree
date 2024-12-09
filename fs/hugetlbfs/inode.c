38dc40218d5632b5ddc61d0ae654109051872d64
Rafael Aquini <raquini@redhat.com>
2024-12-09 12:16:06 -0500

    fs: hugetlbfs: set vma policy only when needed for allocating folio
    JIRA: https://issues.redhat.com/browse/RHEL-27745
    Conflicts:
      * minor context difference on the 2nd hunk due to out-of-order backport
        of upstream commit fd4aed8d985a ("hugetlb: revert use of page_cache_next_miss()")
    
    This patch is a backport of the following upstream commit:
    commit adef080382637f207c68e8ad633481d9b9332b63
    Author: Ackerley Tng <ackerleytng@google.com>
    Date:   Tue May 2 23:56:22 2023 +0000
    
        fs: hugetlbfs: set vma policy only when needed for allocating folio
    
        Calling hugetlb_set_vma_policy() later avoids setting the vma policy
        and then dropping it on a page cache hit.
    
        Link: https://lkml.kernel.org/r/20230502235622.3652586-1-ackerleytng@google.com
        Signed-off-by: Ackerley Tng <ackerleytng@google.com>
        Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
        Cc: Erdem Aktas <erdemaktas@google.com>
        Cc: John Hubbard <jhubbard@nvidia.com>
        Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
        Cc: Muchun Song <muchun.song@linux.dev>
        Cc: Sidhartha Kumar <sidhartha.kumar@oracle.com>
        Cc: Vishal Annapurve <vannapurve@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- fs/hugetlbfs/inode.c
+++ fs/hugetlbfs/inode.c
@@ -882,9 +882,6 @@ static long hugetlbfs_fallocate(struct file *file, int mode, loff_t offset,
 			break;
 		}
 
-		/* Set numa allocation policy based on index */
-		hugetlb_set_vma_policy(&pseudo_vma, inode, index);
-
 		/* addr is the offset within the file (zero based) */
 		addr = index * hpage_size;
 
@@ -897,7 +894,6 @@ static long hugetlbfs_fallocate(struct file *file, int mode, loff_t offset,
 		if (!IS_ERR(folio)) {
 			folio_put(folio);
 			mutex_unlock(&hugetlb_fault_mutex_table[hash]);
-			hugetlb_drop_vma_policy(&pseudo_vma);
 			continue;
 		}
 
@@ -909,6 +905,7 @@ static long hugetlbfs_fallocate(struct file *file, int mode, loff_t offset,
 		 * folios in these areas, we need to consume the reserves
 		 * to keep reservation accounting consistent.
 		 */
+		hugetlb_set_vma_policy(&pseudo_vma, inode, index);
 		folio = alloc_hugetlb_folio(&pseudo_vma, addr, 0);
 		hugetlb_drop_vma_policy(&pseudo_vma);
 		if (IS_ERR(folio)) {
