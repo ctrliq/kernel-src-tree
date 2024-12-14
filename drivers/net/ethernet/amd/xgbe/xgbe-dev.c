b186d772e492d5355f66a25ab0a7a10a163e0abb
Michal Schmidt <mschmidt@redhat.com>
2024-12-14 22:19:12 +0100

    amd-xgbe: Replace MII_ADDR_C45 with XGBE_ADDR_C45
    JIRA: https://issues.redhat.com/browse/RHEL-62004
    
    commit 47e61593f367060c83f07a6b46aa7a4f77ffb4dd
    Author: Andrew Lunn <andrew@lunn.ch>
    Date:   Tue Jan 17 00:52:24 2023 +0100
    
        amd-xgbe: Replace MII_ADDR_C45 with XGBE_ADDR_C45
    
        The xgbe driver reuses MII_ADDR_C45 for its own purpose. The values
        derived with it are never passed to phylib or a linux MDIO bus driver.
        In order that MII_ADDR_C45 can be removed, add an XGBE specific
    
        Signed-off-by: Andrew Lunn <andrew@lunn.ch>
        Signed-off-by: Michael Walle <michael@walle.cc>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/amd/xgbe/xgbe-dev.c
+++ drivers/net/ethernet/amd/xgbe/xgbe-dev.c
@@ -1157,8 +1157,8 @@ static int xgbe_read_mmd_regs_v2(struct xgbe_prv_data *pdata, int prtad,
 	unsigned int mmd_address, index, offset;
 	int mmd_data;
 
-	if (mmd_reg & MII_ADDR_C45)
-		mmd_address = mmd_reg & ~MII_ADDR_C45;
+	if (mmd_reg & XGBE_ADDR_C45)
+		mmd_address = mmd_reg & ~XGBE_ADDR_C45;
 	else
 		mmd_address = (pdata->mdio_mmd << 16) | (mmd_reg & 0xffff);
 
@@ -1189,8 +1189,8 @@ static void xgbe_write_mmd_regs_v2(struct xgbe_prv_data *pdata, int prtad,
 	unsigned long flags;
 	unsigned int mmd_address, index, offset;
 
-	if (mmd_reg & MII_ADDR_C45)
-		mmd_address = mmd_reg & ~MII_ADDR_C45;
+	if (mmd_reg & XGBE_ADDR_C45)
+		mmd_address = mmd_reg & ~XGBE_ADDR_C45;
 	else
 		mmd_address = (pdata->mdio_mmd << 16) | (mmd_reg & 0xffff);
 
@@ -1220,8 +1220,8 @@ static int xgbe_read_mmd_regs_v1(struct xgbe_prv_data *pdata, int prtad,
 	unsigned int mmd_address;
 	int mmd_data;
 
-	if (mmd_reg & MII_ADDR_C45)
-		mmd_address = mmd_reg & ~MII_ADDR_C45;
+	if (mmd_reg & XGBE_ADDR_C45)
+		mmd_address = mmd_reg & ~XGBE_ADDR_C45;
 	else
 		mmd_address = (pdata->mdio_mmd << 16) | (mmd_reg & 0xffff);
 
@@ -1248,8 +1248,8 @@ static void xgbe_write_mmd_regs_v1(struct xgbe_prv_data *pdata, int prtad,
 	unsigned int mmd_address;
 	unsigned long flags;
 
-	if (mmd_reg & MII_ADDR_C45)
-		mmd_address = mmd_reg & ~MII_ADDR_C45;
+	if (mmd_reg & XGBE_ADDR_C45)
+		mmd_address = mmd_reg & ~XGBE_ADDR_C45;
 	else
 		mmd_address = (pdata->mdio_mmd << 16) | (mmd_reg & 0xffff);
 
