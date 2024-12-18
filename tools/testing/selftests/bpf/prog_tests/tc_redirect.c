20673b026a1a09b713aa76ffcba01e2847494f93
Jerome Marchand <jmarchan@redhat.com>
2024-12-18 10:37:45 +0100

    selftests/bpf: Fix error compiling tc_redirect.c with musl libc
    JIRA: https://issues.redhat.com/browse/RHEL-63880
    
    Conflicts: Some code not present due to missing commit ce6f6cffaeaa
    ("selftests/bpf: Wait for the netstamp_needed_key static key to be
    turned on")
    
    commit 21c5f4f55da759c7444a1ef13e90b6e6f674eeeb
    Author: Tony Ambardar <tony.ambardar@gmail.com>
    Date:   Mon Jul 29 02:24:24 2024 -0700
    
        selftests/bpf: Fix error compiling tc_redirect.c with musl libc
    
        Linux 5.1 implemented 64-bit time types and related syscalls to address the
        Y2038 problem generally across archs. Userspace handling of Y2038 varies
        with the libc however. While musl libc uses 64-bit time across all 32-bit
        and 64-bit platforms, GNU glibc uses 64-bit time on 64-bit platforms but
        defaults to 32-bit time on 32-bit platforms unless they "opt-in" to 64-bit
        time or explicitly use 64-bit syscalls and time structures.
    
        One specific area is the standard setsockopt() call, SO_TIMESTAMPNS option
        used for timestamping, and the related output 'struct timespec'. GNU glibc
        defaults as above, also exposing the SO_TIMESTAMPNS_NEW flag to explicitly
        use a 64-bit call and 'struct __kernel_timespec'. Since these are not
        exposed or needed with musl libc, their use in tc_redirect.c leads to
        compile errors building for mips64el/musl:
    
          tc_redirect.c: In function 'rcv_tstamp':
          tc_redirect.c:425:32: error: 'SO_TIMESTAMPNS_NEW' undeclared (first use in this function); did you mean 'SO_TIMESTAMPNS'?
            425 |             cmsg->cmsg_type == SO_TIMESTAMPNS_NEW)
                |                                ^~~~~~~~~~~~~~~~~~
                |                                SO_TIMESTAMPNS
          tc_redirect.c:425:32: note: each undeclared identifier is reported only once for each function it appears in
          tc_redirect.c: In function 'test_inet_dtime':
          tc_redirect.c:491:49: error: 'SO_TIMESTAMPNS_NEW' undeclared (first use in this function); did you mean 'SO_TIMESTAMPNS'?
            491 |         err = setsockopt(listen_fd, SOL_SOCKET, SO_TIMESTAMPNS_NEW,
                |                                                 ^~~~~~~~~~~~~~~~~~
                |                                                 SO_TIMESTAMPNS
    
        However, using SO_TIMESTAMPNS_NEW isn't strictly needed, nor is Y2038 being
        explicitly tested. The timestamp checks in tc_redirect.c are simple: the
        packet receive timestamp is non-zero and processed/handled in less than 5
        seconds.
    
        Switch to using the standard setsockopt() call and SO_TIMESTAMPNS option to
        ensure compatibility across glibc and musl libc. In the worst-case, there
        is a 5-second window 14 years from now where tc_redirect tests may fail on
        32-bit systems. However, we should reasonably expect glibc to adopt a
        64-bit mandate rather than the current "opt-in" policy before the Y2038
        roll-over.
    
        Fixes: ce6f6cffaeaa ("selftests/bpf: Wait for the netstamp_needed_key static key to be turned on")
        Fixes: c803475fd8dd ("bpf: selftests: test skb->tstamp in redirect_neigh")
        Signed-off-by: Tony Ambardar <tony.ambardar@gmail.com>
        Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
        Link: https://lore.kernel.org/bpf/031d656c058b4e55ceae56ef49c4e1729b5090f3.1722244708.git.tony.ambardar@gmail.com
    
    Signed-off-by: Jerome Marchand <jmarchan@redhat.com>

--- tools/testing/selftests/bpf/prog_tests/tc_redirect.c
+++ tools/testing/selftests/bpf/prog_tests/tc_redirect.c
@@ -398,7 +398,7 @@ static int set_forwarding(bool enable)
 
 static void rcv_tstamp(int fd, const char *expected, size_t s)
 {
-	struct __kernel_timespec pkt_ts = {};
+	struct timespec pkt_ts = {};
 	char ctl[CMSG_SPACE(sizeof(pkt_ts))];
 	struct timespec now_ts;
 	struct msghdr msg = {};
@@ -422,7 +422,7 @@ static void rcv_tstamp(int fd, const char *expected, size_t s)
 
 	cmsg = CMSG_FIRSTHDR(&msg);
 	if (cmsg && cmsg->cmsg_level == SOL_SOCKET &&
-	    cmsg->cmsg_type == SO_TIMESTAMPNS_NEW)
+	    cmsg->cmsg_type == SO_TIMESTAMPNS)
 		memcpy(&pkt_ts, CMSG_DATA(cmsg), sizeof(pkt_ts));
 
 	pkt_ns = pkt_ts.tv_sec * NSEC_PER_SEC + pkt_ts.tv_nsec;
@@ -488,9 +488,9 @@ static void test_inet_dtime(int family, int type, const char *addr, __u16 port)
 		return;
 
 	/* Ensure the kernel puts the (rcv) timestamp for all skb */
-	err = setsockopt(listen_fd, SOL_SOCKET, SO_TIMESTAMPNS_NEW,
+	err = setsockopt(listen_fd, SOL_SOCKET, SO_TIMESTAMPNS,
 			 &opt, sizeof(opt));
-	if (!ASSERT_OK(err, "setsockopt(SO_TIMESTAMPNS_NEW)"))
+	if (!ASSERT_OK(err, "setsockopt(SO_TIMESTAMPNS)"))
 		goto done;
 
 	if (type == SOCK_STREAM) {
