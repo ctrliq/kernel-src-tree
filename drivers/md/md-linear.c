17cbda34c3c7034cc3bb9ea1564f5b5a0aaf1eed
Nigel Croxon <ncroxon@redhat.com>
2025-01-29 14:56:35 -0800

    md: Fix linear_set_limits()
    JIRA: https://issues.redhat.com/browse/RHEL-73514
    
    commit a572593ac80e51eb69ecede7e614289fcccdbf8d
    Author: Bart Van Assche <bvanassche@acm.org>
    Date:   Wed Jan 29 14:56:35 2025 -0800
    
    md: Fix linear_set_limits()
    
    queue_limits_cancel_update() must only be called if
    queue_limits_start_update() is called first. Remove the
    queue_limits_cancel_update() call from linear_set_limits() because
    there is no corresponding queue_limits_start_update() call.
    
    This bug was discovered by annotating all mutex operations with clang
    thread-safety attributes and by building the kernel with clang and
    -Wthread-safety.
    
    Cc: Yu Kuai <yukuai3@huawei.com>
    Cc: Coly Li <colyli@kernel.org>
    Cc: Mike Snitzer <snitzer@kernel.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Fixes: 127186cfb184 ("md: reintroduce md-linear")
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20250129225636.2667932-1-bvanassche@acm.org
    Signed-off-by: Song Liu <song@kernel.org>
    (cherry picked from commit a572593ac80e51eb69ecede7e614289fcccdbf8d)
    Signed-off-by: Nigel Croxon <ncroxon@redhat.com>

--- drivers/md/md-linear.c
+++ drivers/md/md-linear.c
@@ -66,10 +66,9 @@ static int linear_set_limits(struct mddev *mddev)
 
 	md_init_stacking_limits(&lim);
 	err = mddev_stack_rdev_limits(mddev, &lim, MDDEV_STACK_INTEGRITY);
-	if (err) {
-		queue_limits_cancel_update(mddev->gendisk->queue);
+	if (err)
 		return err;
-	}
+
 	return queue_limits_set(mddev->gendisk->queue, &lim);
 }
 
