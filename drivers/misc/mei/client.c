6a711fede6f267ede2ae2b4d2631b7e0df24fe18
David Arcari <darcari@redhat.com>
2025-01-30 13:12:05 -0500

    misc: mei: client.c: return negative error code in mei_cl_write
    JIRA: https://issues.redhat.com/browse/RHEL-77170
    
    commit 8f06aee8089cf42fd99a20184501bd1347ce61b9
    Author: Su Hui <suhui@nfschina.com>
    Date:   Mon Nov 20 17:55:23 2023 +0800
    
        misc: mei: client.c: return negative error code in mei_cl_write
    
        mei_msg_hdr_init() return negative error code, rets should be
        'PTR_ERR(mei_hdr)' rather than '-PTR_ERR(mei_hdr)'.
    
        Fixes: 0cd7c01a60f8 ("mei: add support for mei extended header.")
        Signed-off-by: Su Hui <suhui@nfschina.com>
        Link: https://lore.kernel.org/r/20231120095523.178385-1-suhui@nfschina.com
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- drivers/misc/mei/client.c
+++ drivers/misc/mei/client.c
@@ -2009,7 +2009,7 @@ ssize_t mei_cl_write(struct mei_cl *cl, struct mei_cl_cb *cb, unsigned long time
 
 	mei_hdr = mei_msg_hdr_init(cb);
 	if (IS_ERR(mei_hdr)) {
-		rets = -PTR_ERR(mei_hdr);
+		rets = PTR_ERR(mei_hdr);
 		mei_hdr = NULL;
 		goto err;
 	}
