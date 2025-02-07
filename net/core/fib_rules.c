73208d73d7b9d2277b17a4e8051a5710da6f7da5
Antoine Tenart <atenart@redhat.com>
2025-02-07 16:25:02 +0900

    net: fib_rules: Convert RTM_DELRULE to per-netns RTNL.
    JIRA: https://issues.redhat.com/browse/RHEL-150155
    Upstream Status: linux.git
    
    commit 88b9cfca8d7735b0e8c809241365577f07c52cb2
    Author: Kuniyuki Iwashima <kuniyu@amazon.com>
    Date:   Fri Feb 7 16:25:02 2025 +0900
    
        net: fib_rules: Convert RTM_DELRULE to per-netns RTNL.
    
        fib_nl_delrule() is the doit() handler for RTM_DELRULE but also called
        from vrf_newlink() in case something fails in vrf_add_fib_rules().
    
        In the latter case, RTNL is already held and the 4th arg is true.
    
        Let's hold per-netns RTNL in fib_delrule() if rtnl_held is false.
    
        Now we can place ASSERT_RTNL_NET() in call_fib_rule_notifiers().
    
        Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Reviewed-by: Eric Dumazet <edumazet@google.com>
        Reviewed-by: Ido Schimmel <idosch@nvidia.com>
        Tested-by: Ido Schimmel <idosch@nvidia.com>
        Link: https://patch.msgid.link/20250207072502.87775-9-kuniyu@amazon.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -391,7 +391,8 @@ static int call_fib_rule_notifiers(struct net *net,
 		.rule = rule,
 	};
 
-	ASSERT_RTNL();
+	ASSERT_RTNL_NET(net);
+
 	/* Paired with READ_ONCE() in fib_rules_seq() */
 	WRITE_ONCE(ops->fib_rules_seq, ops->fib_rules_seq + 1);
 	return call_fib_notifiers(net, event_type, &info.info);
@@ -967,6 +968,9 @@ int fib_delrule(struct net *net, struct sk_buff *skb, struct nlmsghdr *nlh,
 	if (err)
 		goto errout;
 
+	if (!rtnl_held)
+		rtnl_net_lock(net);
+
 	err = fib_nl2rule_rtnl(nlrule, ops, tb, extack);
 	if (err)
 		goto errout_free;
@@ -1021,10 +1025,12 @@ int fib_delrule(struct net *net, struct sk_buff *skb, struct nlmsghdr *nlh,
 		}
 	}
 
-	call_fib_rule_notifiers(net, FIB_EVENT_RULE_DEL, rule, ops,
-				NULL);
-	notify_rule_change(RTM_DELRULE, rule, ops, nlh,
-			   NETLINK_CB(skb).portid);
+	call_fib_rule_notifiers(net, FIB_EVENT_RULE_DEL, rule, ops, NULL);
+
+	if (!rtnl_held)
+		rtnl_net_unlock(net);
+
+	notify_rule_change(RTM_DELRULE, rule, ops, nlh, NETLINK_CB(skb).portid);
 	fib_rule_put(rule);
 	flush_route_cache(ops);
 	rules_ops_put(ops);
@@ -1032,6 +1038,8 @@ int fib_delrule(struct net *net, struct sk_buff *skb, struct nlmsghdr *nlh,
 	return 0;
 
 errout_free:
+	if (!rtnl_held)
+		rtnl_net_unlock(net);
 	kfree(nlrule);
 errout:
 	rules_ops_put(ops);
@@ -1042,7 +1050,7 @@ EXPORT_SYMBOL_GPL(fib_delrule);
 static int fib_nl_delrule(struct sk_buff *skb, struct nlmsghdr *nlh,
 			  struct netlink_ext_ack *extack)
 {
-	return fib_delrule(sock_net(skb->sk), skb, nlh, extack, true);
+	return fib_delrule(sock_net(skb->sk), skb, nlh, extack, false);
 }
 
 static inline size_t fib_rule_nlmsg_size(struct fib_rules_ops *ops,
@@ -1367,7 +1375,8 @@ static struct pernet_operations fib_rules_net_ops = {
 static const struct rtnl_msg_handler fib_rules_rtnl_msg_handlers[] __initconst = {
 	{.msgtype = RTM_NEWRULE, .doit = fib_nl_newrule,
 	 .flags = RTNL_FLAG_DOIT_PERNET},
-	{.msgtype = RTM_DELRULE, .doit = fib_nl_delrule},
+	{.msgtype = RTM_DELRULE, .doit = fib_nl_delrule,
+	 .flags = RTNL_FLAG_DOIT_PERNET},
 	{.msgtype = RTM_GETRULE, .dumpit = fib_nl_dumprule,
 	 .flags = RTNL_FLAG_DUMP_UNLOCKED},
 };
