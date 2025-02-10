ce6d94aaaf4ea0ce0f2d9159f1287554c6306448
Gregory Bell <grbell@redhat.com>
2025-02-10 12:16:56 -0500

    selftests/bpf: Add test that uses fsverity and xattr to sign a file
    JIRA: https://issues.redhat.com/browse/RHEL-33903
    
    Conflicts: Includes changes to ‎tools/testing/selftests/bpf/progs/test_sig_in_xattr.c
    from 2b23b6c0f03c ("selftests/bpf: Add return value checks for failed tests")
    that were omitted in previous backport
    
    commit 1030e9154258b54e3c7dc07c39e7b6dcf24bc3d2
    Author: Song Liu <song@kernel.org>
    Date:   Wed Nov 29 15:44:17 2023 -0800
    
        selftests/bpf: Add test that uses fsverity and xattr to sign a file
    
        This selftests shows a proof of concept method to use BPF LSM to enforce
        file signature. This test is added to verify_pkcs7_sig, so that some
        existing logic can be reused.
    
        This file signature method uses fsverity, which provides reliable and
        efficient hash (known as digest) of the file. The file digest is signed
        with asymmetic key, and the signature is stored in xattr. At the run time,
        BPF LSM reads file digest and the signature, and then checks them against
        the public key.
    
        Note that this solution does NOT require FS_VERITY_BUILTIN_SIGNATURES.
        fsverity is only used to provide file digest. The signature verification
        and access control is all implemented in BPF LSM.
    
        Signed-off-by: Song Liu <song@kernel.org>
        Link: https://lore.kernel.org/r/20231129234417.856536-7-song@kernel.org
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Gregory Bell <grbell@redhat.com>

--- tools/testing/selftests/bpf/bpf_kfuncs.h
+++ tools/testing/selftests/bpf/bpf_kfuncs.h
@@ -49,6 +49,14 @@ extern int bpf_get_file_xattr(struct file *file, const char *name,
 			      struct bpf_dynptr *value_ptr) __ksym;
 extern int bpf_get_fsverity_digest(struct file *file, struct bpf_dynptr *digest_ptr) __ksym;
 
+extern struct bpf_key *bpf_lookup_user_key(__u32 serial, __u64 flags) __ksym;
+extern struct bpf_key *bpf_lookup_system_key(__u64 id) __ksym;
+extern void bpf_key_put(struct bpf_key *key) __ksym;
+extern int bpf_verify_pkcs7_signature(struct bpf_dynptr *data_ptr,
+				      struct bpf_dynptr *sig_ptr,
+				      struct bpf_key *trusted_keyring) __ksym;
+
 extern bool bpf_session_is_return(void) __ksym __weak;
 extern __u64 *bpf_session_cookie(void) __ksym __weak;
+
 #endif
