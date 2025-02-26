fbc5281d4ecc549d00441ec42a1533b77cca01c7
Eder Zulian <ezulian@redhat.com>
2025-02-26 20:21:08 +0100

    iommu: Restore iommu_flush_iotlb_all()
    JIRA: https://issues.redhat.com/browse/RHEL-78701
    
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    commit a33bf8d8ce7e06bf0f033865b0cea5887cd2ac8c
    Author: Joerg Roedel <jroedel@suse.de>
    Date:   Tue Oct 29 12:11:49 2024 +0100
    
        iommu: Restore iommu_flush_iotlb_all()
    
        This patch restores the iommu_flush_iotlb_all() function.
        Commit
    
                69e5a17511f6 ("iommu: Remove useless flush from iommu_create_device_direct_mappings()")
    
        claims it removed the last call-site, except it did not. There is still
        at least one caller in
    
                drivers/gpu/drm/msm/msm_iommu.c
    
        so keep the function around until all call-sites are updated.
    
        Cc: Jason Gunthorpe <jgg@ziepe.ca>
        Fixes: 69e5a17511f6 ("iommu: Remove useless flush from iommu_create_device_direct_mappings()")
        Acked-by: Will Deacon <will@kernel.org>
        Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Signed-off-by: Eder Zulian <ezulian@redhat.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -909,6 +909,12 @@ void iommu_set_dma_strict(void);
 extern int report_iommu_fault(struct iommu_domain *domain, struct device *dev,
 			      unsigned long iova, int flags);
 
+static inline void iommu_flush_iotlb_all(struct iommu_domain *domain)
+{
+	if (domain->ops->flush_iotlb_all)
+		domain->ops->flush_iotlb_all(domain);
+}
+
 static inline void iommu_iotlb_sync(struct iommu_domain *domain,
 				  struct iommu_iotlb_gather *iotlb_gather)
 {
@@ -1193,6 +1199,10 @@ static inline ssize_t iommu_map_sg(struct iommu_domain *domain,
 				   unsigned int nents, int prot, gfp_t gfp)
 {
 	return -ENODEV;
+}
+
+static inline void iommu_flush_iotlb_all(struct iommu_domain *domain)
+{
 }
 
 static inline void iommu_iotlb_sync(struct iommu_domain *domain,
