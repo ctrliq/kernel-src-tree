18ac5c27efd3ed860673399914933f032ca4dda0
Miklos Szeredi <mszeredi@redhat.com>
2025-03-14 16:48:27 +0100

    ovl: check type and offset of struct vfsmount in ovl_entry
    JIRA: https://issues.redhat.com/browse/RHEL-83562
    
    commit f723edb8a532cd26e1ff0a2b271d73762d48f762
    Author: Christian Brauner <brauner@kernel.org>
    Date:   Tue Jun 13 10:13:37 2023 +0200
    
        ovl: check type and offset of struct vfsmount in ovl_entry
    
        Porting overlayfs to the new amount api I started experiencing random
        crashes that couldn't be explained easily. So after much debugging and
        reasoning it became clear that struct ovl_entry requires the point to
        struct vfsmount to be the first member and of type struct vfsmount.
    
        During the port I added a new member at the beginning of struct
        ovl_entry which broke all over the place in the form of random crashes
        and cache corruptions. While there's a comment in ovl_free_fs() to the
        effect of "Hack! Reuse ofs->layers as a vfsmount array before freeing
        it" there's no such comment on struct ovl_entry which makes this easy to
        trip over.
    
        Add a comment and two static asserts for both the offset and the type of
        pointer in struct ovl_entry.
    
        Signed-off-by: Christian Brauner <brauner@kernel.org>
        Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/ovl_entry.h
+++ fs/overlayfs/ovl_entry.h
@@ -32,6 +32,7 @@ struct ovl_sb {
 };
 
 struct ovl_layer {
+	/* ovl_free_fs() relies on @mnt being the first member! */
 	struct vfsmount *mnt;
 	/* Trap in ovl inode cache */
 	struct inode *trap;
@@ -43,6 +44,14 @@ struct ovl_layer {
 	/* xwhiteouts were found on this layer */
 	bool has_xwhiteouts;
 };
+
+/*
+ * ovl_free_fs() relies on @mnt being the first member when unmounting
+ * the private mounts created for each layer. Let's check both the
+ * offset and type.
+ */
+static_assert(offsetof(struct ovl_layer, mnt) == 0);
+static_assert(__same_type(typeof_member(struct ovl_layer, mnt), struct vfsmount *));
 
 struct ovl_path {
 	const struct ovl_layer *layer;
