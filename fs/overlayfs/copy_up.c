c65df314182bbba8ed9d5498397c118fcec2a4a9
Miklos Szeredi <mszeredi@redhat.com>
2025-03-14 16:48:24 +0100

    ovl: use kvalloc in xattr copy-up
    JIRA: https://issues.redhat.com/browse/RHEL-83562
    
    commit f945ca1963c8bd29471020d7c58c594ee7007006
    Author: Miklos Szeredi <mszeredi@redhat.com>
    Date:   Thu Jul 22 14:18:14 2021 +0200
    
        ovl: use kvalloc in xattr copy-up
    
        Extended attributes are usually small, but could be up to 64k in size, so
        use the most efficient method for doing the allocation.
    
        Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/copy_up.c
+++ fs/overlayfs/copy_up.c
@@ -92,7 +92,7 @@ int ovl_copy_xattr(struct super_block *sb, const struct path *oldpath, struct de
 		return list_size;
 	}
 
-	buf = kzalloc(list_size, GFP_KERNEL);
+	buf = kvzalloc(list_size, GFP_KERNEL);
 	if (!buf)
 		return -ENOMEM;
 
@@ -144,11 +144,12 @@ retry:
 		if (size > value_size) {
 			void *new;
 
-			new = krealloc(value, size, GFP_KERNEL);
+			new = kvmalloc(size, GFP_KERNEL);
 			if (!new) {
 				error = -ENOMEM;
 				break;
 			}
+			kvfree(value);
 			value = new;
 			value_size = size;
 			goto retry;
@@ -163,9 +164,9 @@ retry:
 			error = 0;
 		}
 	}
-	kfree(value);
+	kvfree(value);
 out:
-	kfree(buf);
+	kvfree(buf);
 	return error;
 }
 
