70729cbf33676a07c024384dc701ecd6c5a85672
Jaroslav Kysela <jkysela@redhat.com>
2025-03-17 13:42:46 +0100

    ASoC: remove rtd->num
    JIRA: https://issues.redhat.com/browse/RHEL-80681
    
    commit c59db5ed233a19f6aadd086fb89149ec5f6fa855
    Author: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Date: Thu Oct 24 01:29:45 2024 +0000
    
        ASoC: remove rtd->num
    
        No one is using rtd->num. Let's remove it.
    
        Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
        Link: https://patch.msgid.link/87sesmb852.wl-kuninori.morimoto.gx@renesas.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1185,7 +1185,6 @@ struct snd_soc_pcm_runtime {
 	struct dentry *debugfs_dpcm_root;
 #endif
 
-	unsigned int num; /* REMOVE ME */
 	unsigned int id; /* 0-based and monotonic increasing */
 	struct list_head list; /* rtd list of the soc card */
 
