9ec7b8b1699d6509ba4eabcb7cf61cb19558f43a
David Arcari <darcari@redhat.com>
2025-03-21 10:12:19 -0400

    i2c: i801: Add lis3lv02d for Dell Precision 3540
    JIRA: https://issues.redhat.com/browse/RHEL-47426
    
    commit 6d9450464ce1825d7d0e7ef5d33a9d2701f41c76
    Author: Paul Menzel <pmenzel@molgen.mpg.de>
    Date:   Wed Dec 20 17:10:01 2023 +0100
    
        i2c: i801: Add lis3lv02d for Dell Precision 3540
    
        On the Dell Precision 3540/0M14W7, BIOS 1.7.4 05/12/2020, Linux prints
        the warning below.
    
            i801_smbus 0000:00:1f.4: Accelerometer lis3lv02d is present on SMBus but its address is unknown, skipping registration
    
        With the help of Wolfram Sang, the test to probe it on I2C bus 6 at address
        0x29 was successful.
    
            $ echo lis3lv02d 0x29 | sudo tee /sys/bus/i2c/devices/i2c-6/new_device
            [ 2110.787000] i2c i2c-6: new_device: Instantiated device lis3lv02d at 0x29
            [ 2110.791932] lis3lv02d_i2c 6-0029: supply Vdd not found, using dummy regulator
            [ 2110.791981] lis3lv02d_i2c 6-0029: supply Vdd_IO not found, using dummy regulator
            [ 2110.809233] lis3lv02d: 8 bits 3DC sensor found
            [ 2110.900668] input: ST LIS3LV02DL Accelerometer as /devices/platform/lis3lv02d/input/input23
    
        So, the device has that accelerometer. Add the I2C address to the
        mapping list.
    
        Link: https://lore.kernel.org/linux-i2c/97708c11-ac85-fb62-2c8e-d37739ca826f@molgen.mpg.de/
        Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
        Acked-by: Pali Rohár <pali@kernel.org>
        Reviewed-by: Andi Shyti <andi.shyti@kernel.org>
        [wsa: shortened commit message a little]
        Signed-off-by: Wolfram Sang <wsa@kernel.org>
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- drivers/i2c/busses/i2c-i801.c
+++ drivers/i2c/busses/i2c-i801.c
@@ -1234,6 +1234,7 @@ static const struct {
 	 * Additional individual entries were added after verification.
 	 */
 	{ "Latitude 5480",      0x29 },
+	{ "Precision 3540",     0x29 },
 	{ "Vostro V131",        0x1d },
 	{ "Vostro 5568",        0x29 },
 };
