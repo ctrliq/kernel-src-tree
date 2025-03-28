ec94e5c6502e81517d3b9140bae8c1f7511b2d53
Eric Chanudet <echanude@redhat.com>
2025-03-28 16:52:20 -0400

    mm/mm_init.c: print mem_init info after defer_init is done
    JIRA: https://issues.redhat.com/browse/RHEL-85565
    
    commit 4f66da89d31ca56d4c41de01dd663f79d697904b
    Author: Wei Yang <richard.weiyang@gmail.com>
    Date:   Tue Jun 11 14:52:23 2024 +0000
    
        mm/mm_init.c: print mem_init info after defer_init is done
    
        Current call flow looks like this:
    
        start_kernel
          mm_core_init
            mem_init
            mem_init_print_info
          rest_init
            kernel_init
              kernel_init_freeable
                page_alloc_init_late
                  deferred_init_memmap
    
        If CONFIG_DEFERRED_STRUCT_PAGE_INIT, the time mem_init_print_info()
        calls, pages are not totally initialized and freed to buddy.
    
        This has one issue
    
          * nr_free_pages() just contains partial free pages in the system,
            which is not we expect.
    
        Let's print the mem info after defer_init is done.
    
        Also this would help changing totalram_pages accounting, since we plan
        to move the accounting into __free_pages_core().
    
        Link: https://lkml.kernel.org/r/20240611145223.16872-1-richard.weiyang@gmail.com
        Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
        Acked-by: David Hildenbrand <david@redhat.com>
        Cc: Mike Rapoport (IBM) <rppt@kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Eric Chanudet <echanude@redhat.com>

--- mm/mm_init.c
+++ mm/mm_init.c
@@ -2368,6 +2368,7 @@ void set_zone_contiguous(struct zone *zone)
 	zone->contiguous = true;
 }
 
+static void __init mem_init_print_info(void);
 void __init page_alloc_init_late(void)
 {
 	struct zone *zone;
@@ -2394,6 +2395,8 @@ void __init page_alloc_init_late(void)
 	files_maxfiles_init();
 #endif
 
+	/* Accounting of total+free memory is stable at this point. */
+	mem_init_print_info();
 	buffer_init();
 
 	/* Discard memblock private memory */
@@ -2772,7 +2775,6 @@ void __init mm_core_init(void)
 	kmsan_init_shadow();
 	stack_depot_early_init();
 	mem_init();
-	mem_init_print_info();
 	kmem_cache_init();
 	/*
 	 * page_owner must be initialized after buddy is ready, and also after
