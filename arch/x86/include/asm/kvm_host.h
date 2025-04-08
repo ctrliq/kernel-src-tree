7b72b44b2555eab8c937b8030939ba8d2541fe20
Paolo Bonzini <pbonzini@redhat.com>
2025-04-08 13:48:46 +0200

    KVM: x86: Add infrastructure for secure TSC
    JIRA: https://issues.redhat.com/browse/RHEL-15711
    
    Add guest_tsc_protected member to struct kvm_arch_vcpu and prohibit
    changing TSC offset/multiplier when guest_tsc_protected is true.
    
    X86 confidential computing technology defines protected guest TSC so that
    the VMM can't change the TSC offset/multiplier once vCPU is initialized.
    SEV-SNP defines Secure TSC as optional, whereas TDX mandates it.
    
    KVM has common logic on x86 that tries to guess or adjust TSC
    offset/multiplier for better guest TSC and TSC interrupt latency
    at KVM vCPU creation (kvm_arch_vcpu_postcreate()), vCPU migration
    over pCPU (kvm_arch_vcpu_load()), vCPU TSC device attributes
    (kvm_arch_tsc_set_attr()) and guest/host writing to TSC or TSC adjust MSR
    (kvm_set_msr_common()).
    
    The current x86 KVM implementation conflicts with protected TSC because the
    VMM can't change the TSC offset/multiplier.
    Because KVM emulates the TSC timer or the TSC deadline timer with the TSC
    offset/multiplier, the TSC timer interrupts is injected to the guest at the
    wrong time if the KVM TSC offset is different from what the TDX module
    determined.
    
    Originally this issue was found by cyclic test of rt-test [1] as the
    latency in TDX case is worse than VMX value + TDX SEAMCALL overhead.  It
    turned out that the KVM TSC offset is different from what the TDX module
    determines.
    
    Disable or ignore the KVM logic to change/adjust the TSC offset/multiplier
    somehow, thus keeping the KVM TSC offset/multiplier the same as the
    value of the TDX module.  Writes to MSR_IA32_TSC are also blocked as
    they amount to a change in the TSC offset.
    
    [1] https://git.kernel.org/pub/scm/utils/rt-tests/rt-tests.git
    
    Reported-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Message-ID: <3a7444aec08042fe205666864b6858910e86aa98.1728719037.git.isaku.yamahata@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit adafea1106004dba26ea12d3193f4f132b1f0065)
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1038,6 +1038,7 @@ struct kvm_vcpu_arch {
 
 	/* Protected Guests */
 	bool guest_state_protected;
+	bool guest_tsc_protected;
 
 	/*
 	 * Set when PDPTS were loaded directly by the userspace without
