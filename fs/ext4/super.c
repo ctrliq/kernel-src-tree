db6ed9f8ed5b8e86087c653ef2f08bcb2abaf54b
Brian Foster <bfoster@redhat.com>
2025-04-08 09:20:52 -0400

    ext4: warn if delalloc counters are not zero on inactive
    JIRA: https://issues.redhat.com/browse/RHEL-86510
    
    commit b37c907073e80016333b442c845c3acc198e570f
    Author: Zhang Yi <yi.zhang@huawei.com>
    Date:   Fri May 17 20:39:58 2024 +0800
    
        ext4: warn if delalloc counters are not zero on inactive
    
        The per-inode i_reserved_data_blocks count the reserved delalloc blocks
        in a regular file, it should be zero when destroying the file. The
        per-fs s_dirtyclusters_counter count all reserved delalloc blocks in a
        filesystem, it also should be zero when umounting the filesystem. Now we
        have only an error message if the i_reserved_data_blocks is not zero,
        which is unable to be simply captured, so add WARN_ON_ONCE to make it
        more visable.
    
        Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
        Reviewed-by: Jan Kara <jack@suse.cz>
        Link: https://patch.msgid.link/20240517124005.347221-4-yi.zhang@huaweicloud.com
        Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1275,6 +1275,9 @@ static void ext4_put_super(struct super_block *sb)
 		kvfree(flex_groups);
 	}
 	rcu_read_unlock();
+
+	WARN_ON_ONCE(!(sbi->s_mount_state & EXT4_ERROR_FS) &&
+		     percpu_counter_sum(&sbi->s_dirtyclusters_counter));
 	percpu_counter_destroy(&sbi->s_freeclusters_counter);
 	percpu_counter_destroy(&sbi->s_freeinodes_counter);
 	percpu_counter_destroy(&sbi->s_dirs_counter);
@@ -1410,7 +1413,8 @@ static void ext4_destroy_inode(struct inode *inode)
 		dump_stack();
 	}
 
-	if (EXT4_I(inode)->i_reserved_data_blocks)
+	if (!(EXT4_SB(inode->i_sb)->s_mount_state & EXT4_ERROR_FS) &&
+	    WARN_ON_ONCE(EXT4_I(inode)->i_reserved_data_blocks))
 		ext4_msg(inode->i_sb, KERN_ERR,
 			 "Inode %lu (%p): i_reserved_data_blocks (%u) not cleared!",
 			 inode->i_ino, EXT4_I(inode),
