62a6436a79e754eab9b785f172c16af66e10e74d
Myron Stowe <mstowe@redhat.com>
2025-04-10 09:53:55 -0600

    PCI: Check BAR index for validity
    JIRA: https://issues.redhat.com/browse/RHEL-86521
    Upstream Status: b1a7f99967fc0c052db8e65b449c7b32b1e9177f
    
    commit b1a7f99967fc0c052db8e65b449c7b32b1e9177f
    Author: Philipp Stanner <phasta@kernel.org>
    Date:   Wed Mar 12 09:06:35 2025 +0100
    
        PCI: Check BAR index for validity
    
        Many functions in PCI use accessor macros such as pci_resource_len(),
        which take a BAR index. That index, however, is never checked for
        validity, potentially resulting in undefined behavior by overflowing the
        array pci_dev.resource in the macro pci_resource_n().
    
        Since many users of those macros directly assign the accessed value to
        an unsigned integer, the macros cannot be changed easily anymore to
        return -EINVAL for invalid indexes. Consequently, the problem has to be
        mitigated in higher layers.
    
        Add pci_bar_index_valid(). Use it where appropriate.
    
        Link: https://lore.kernel.org/r/20250312080634.13731-4-phasta@kernel.org
        Closes: https://lore.kernel.org/all/adb53b1f-29e1-3d14-0e61-351fd2d3ff0d@linux.intel.com/
        Reported-by: Bingbu Cao <bingbu.cao@linux.intel.com>
        Signed-off-by: Philipp Stanner <phasta@kernel.org>
        [kwilczynski: correct if-statement condition the pci_bar_index_is_valid()
        helper function uses, tidy up code comments]
        Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
        [bhelgaas: fix typo]
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    Signed-off-by: Myron Stowe <mstowe@redhat.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -3937,6 +3937,9 @@ EXPORT_SYMBOL(pci_enable_atomic_ops_to_root);
  */
 void pci_release_region(struct pci_dev *pdev, int bar)
 {
+	if (!pci_bar_index_is_valid(bar))
+		return;
+
 	/*
 	 * This is done for backwards compatibility, because the old PCI devres
 	 * API had a mode in which the function became managed if it had been
@@ -3981,6 +3984,9 @@ EXPORT_SYMBOL(pci_release_region);
 static int __pci_request_region(struct pci_dev *pdev, int bar,
 				const char *name, int exclusive)
 {
+	if (!pci_bar_index_is_valid(bar))
+		return -EINVAL;
+
 	if (pci_is_managed(pdev)) {
 		if (exclusive == IORESOURCE_EXCLUSIVE)
 			return pcim_request_region_exclusive(pdev, bar, name);
