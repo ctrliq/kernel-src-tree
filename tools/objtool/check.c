b1039941e7db58241562a1103c4233da470fa6d8
Joe Lawrence <joe.lawrence@redhat.com>
2025-04-16 15:07:10 -0400

    objtool: Fix error handling inconsistencies in check()
    JIRA: https://issues.redhat.com/browse/RHEL-85302
    Conflicts:
    	tools/objtool/check.c
    	- cs-9 is missing upstream bd456a1bedd2 ("objtool: Separate
    	  prefix code from stack validation code"), so no need to handle
    	  opts.prefix case
    
    commit b745962cb97569aad026806bb0740663cf813147
    Author: Josh Poimboeuf <jpoimboe@kernel.org>
    Date:   Fri Mar 14 12:29:00 2025 -0700
    
        objtool: Fix error handling inconsistencies in check()
    
        Make sure all fatal errors are funneled through the 'out' label with a
        negative ret.
    
        Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Reviewed-by: Brendan Jackman <jackmanb@google.com>
        Link: https://lore.kernel.org/r/0f49d6a27a080b4012e84e6df1e23097f44cc082.1741975349.git.jpoimboe@kernel.org
    
    Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -4508,8 +4508,10 @@ int check(struct objtool_file *file)
 	init_cfi_state(&force_undefined_cfi);
 	force_undefined_cfi.force_undefined = true;
 
-	if (!cfi_hash_alloc(1UL << (file->elf->symbol_bits - 3)))
+	if (!cfi_hash_alloc(1UL << (file->elf->symbol_bits - 3))) {
+		ret = -1;
 		goto out;
+	}
 
 	cfi_hash_add(&init_cfi);
 	cfi_hash_add(&func_cfi);
@@ -4526,7 +4528,7 @@ int check(struct objtool_file *file)
 	if (opts.retpoline) {
 		ret = validate_retpoline(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
@@ -4562,7 +4564,7 @@ int check(struct objtool_file *file)
 		 */
 		ret = validate_unrets(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
