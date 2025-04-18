02f69b11aa5fc5dd0979f1c8d08824173b5ec169
Mark Langsdorf <mlangsdo@redhat.com>
2025-04-18 13:14:44 -0500

    powerpc/machdep: Drop include of dma-mapping.h
    JIRA: https://issues.redhat.com/browse/RHEL-87863
    Conflicts:
    	arch/powerpc/include/asm/machdep.h - minor
    context differences
    
    commit b23b9edf64b6387334aa2f8687cca6792b0d9d6c
    Author: Michael Ellerman <mpe@ellerman.id.au>
    Date: Tue, 29 Oct 2024 23:01:05 +0000
    
    Drop the include of dma-mapping.h in machdep.h, replace it with forward
    declarations of struct device and struct pci_dev, and include time64.h
    and page.h which are required for time64_t and pgprot_t respectively.
    
    Add direct includes of some other headers to some files that were
    getting them via machdep.h.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://patch.msgid.link/20241009051826.132805-2-mpe@ellerman.id.au
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- arch/powerpc/include/asm/machdep.h
+++ arch/powerpc/include/asm/machdep.h
@@ -5,8 +5,10 @@
 
 #include <linux/seq_file.h>
 #include <linux/init.h>
-#include <linux/dma-mapping.h>
 #include <linux/export.h>
+#include <linux/time64.h>
+
+#include <asm/page.h>
 
 #include <asm/setup.h>
 
@@ -17,10 +19,12 @@
 
 struct pt_regs;
 struct pci_bus;	
+struct device;
 struct device_node;
 struct iommu_table;
 struct rtc_time;
 struct file;
+struct pci_dev;
 struct pci_controller;
 struct kimage;
 struct pci_host_bridge;
