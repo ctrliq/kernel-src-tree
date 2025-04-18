eaa0c202515e0d462bfd4b063895546d11d06b21
Antoine Tenart <atenart@redhat.com>
2025-04-17 17:03:45 -0700

    ipv6: Check GATEWAY in rtm_to_fib6_multipath_config().
    JIRA: https://issues.redhat.com/browse/RHEL-150155
    Upstream Status: linux.git
    
    commit e6f497955fb6a072999db491a01dd3a203d5bcea
    Author: Kuniyuki Iwashima <kuniyu@amazon.com>
    Date:   Thu Apr 17 17:03:45 2025 -0700
    
        ipv6: Check GATEWAY in rtm_to_fib6_multipath_config().
    
        In ip6_route_multipath_add(), we call rt6_qualify_for_ecmp() for each
        entry.  If it returns false, the request fails.
    
        rt6_qualify_for_ecmp() returns false if either of the conditions below
        is true:
    
          1. f6i->fib6_flags has RTF_ADDRCONF
          2. f6i->nh is not NULL
          3. f6i->fib6_nh->fib_nh_gw_family is AF_UNSPEC
    
        1 is unnecessary because rtm_to_fib6_config() never sets RTF_ADDRCONF
        to cfg->fc_flags.
    
        2. is equivalent with cfg->fc_nh_id.
    
        3. can be replaced by checking RTF_GATEWAY in the base and each multipath
        entry because AF_INET6 is set to f6i->fib6_nh->fib_nh_gw_family only when
        cfg.fc_is_fdb is true or RTF_GATEWAY is set, but the former is always
        false.
    
        These checks do not require RCU and can be done earlier.
    
        Let's perform the equivalent checks in rtm_to_fib6_multipath_config().
    
        Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Link: https://patch.msgid.link/20250418000443.43734-5-kuniyu@amazon.com
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -5040,6 +5040,7 @@ static int rtm_to_fib6_multipath_config(struct fib6_config *cfg,
 	}
 
 	do {
+		bool has_gateway = cfg->fc_flags & RTF_GATEWAY;
 		int attrlen = rtnh_attrlen(rtnh);
 
 		if (attrlen > 0) {
@@ -5053,7 +5054,15 @@ static int rtm_to_fib6_multipath_config(struct fib6_config *cfg,
 						       "Invalid IPv6 address in RTA_GATEWAY");
 					return -EINVAL;
 				}
+
+				has_gateway = true;
 			}
+		}
+
+		if (newroute && (cfg->fc_nh_id || !has_gateway)) {
+			NL_SET_ERR_MSG(extack,
+				       "Device only routes can not be added for IPv6 using the multipath API.");
+			return -EINVAL;
 		}
 
 		rtnh = rtnh_next(rtnh, &remaining);
@@ -5390,13 +5399,6 @@ static int ip6_route_multipath_add(struct fib6_config *cfg,
 		if (IS_ERR(rt)) {
 			err = PTR_ERR(rt);
 			rt = NULL;
-			goto cleanup;
-		}
-		if (!rt6_qualify_for_ecmp(rt)) {
-			err = -EINVAL;
-			NL_SET_ERR_MSG(extack,
-				       "Device only routes can not be added for IPv6 using the multipath API.");
-			fib6_info_release(rt);
 			goto cleanup;
 		}
 
