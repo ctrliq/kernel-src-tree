ef80b5cbbe3099ad1f7284888e366c5b774b461b
Waiman Long <longman@redhat.com>
2025-04-20 19:52:54 -0400

    x86/alternatives: Use a temporary buffer when optimizing NOPs
    JIRA: https://issues.redhat.com/browse/RHEL-68940
    
    commit f796c75837623058db1ff93252b9f1681306b83d
    Author: Borislav Petkov (AMD) <bp@alien8.de>
    Date:   Tue, 30 Jan 2024 11:59:38 +0100
    
        x86/alternatives: Use a temporary buffer when optimizing NOPs
    
        Instead of optimizing NOPs in-place, use a temporary buffer like the
        usual alternatives patching flow does. This obviates the need to grab
        locks when patching, see
    
          6778977590da ("x86/alternatives: Disable interrupts and sync when optimizing NOPs in place")
    
        While at it, add nomenclature definitions clarifying and simplifying the
        naming of function-local variables in the alternatives code.
    
        Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
        Link: https://lore.kernel.org/r/20240130105941.19707-2-bp@alien8.de
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- arch/x86/kernel/callthunks.c
+++ arch/x86/kernel/callthunks.c
@@ -180,8 +180,7 @@ static void *patch_dest(void *dest, bool direct)
 	u8 *pad = dest - tsize;
 
 	memcpy(insn_buff, skl_call_thunk_template, tsize);
-	apply_relocation(insn_buff, tsize, pad,
-			 skl_call_thunk_template, tsize);
+	apply_relocation(insn_buff, pad, tsize, skl_call_thunk_template, tsize);
 
 	/* Already patched? */
 	if (!bcmp(pad, insn_buff, tsize))
@@ -303,8 +302,7 @@ bool is_callthunk(void *addr)
 	pad = (void *)(dest - tmpl_size);
 
 	memcpy(insn_buff, skl_call_thunk_template, tmpl_size);
-	apply_relocation(insn_buff, tmpl_size, pad,
-			 skl_call_thunk_template, tmpl_size);
+	apply_relocation(insn_buff, pad, tmpl_size, skl_call_thunk_template, tmpl_size);
 
 	return !bcmp(pad, insn_buff, tmpl_size);
 }
@@ -322,8 +320,7 @@ int x86_call_depth_emit_accounting(u8 **pprog, void *func, void *ip)
 		return 0;
 
 	memcpy(insn_buff, skl_call_thunk_template, tmpl_size);
-	apply_relocation(insn_buff, tmpl_size, ip,
-			 skl_call_thunk_template, tmpl_size);
+	apply_relocation(insn_buff, ip, tmpl_size, skl_call_thunk_template, tmpl_size);
 
 	memcpy(*pprog, insn_buff, tmpl_size);
 	*pprog += tmpl_size;
