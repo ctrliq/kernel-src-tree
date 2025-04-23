857aa6e639e22b8146fa6910d764e869c0d0c001
Ryan Sullivan <rysulliv@redhat.com>
2025-04-23 16:07:33 -0400

    objtool: Handle various symbol types of rodata
    JIRA: https://issues.redhat.com/browse/RHEL-85292
    
    In the relocation section ".rela.rodata" of each .o file compiled with
    LoongArch toolchain, there are various symbol types such as STT_NOTYPE,
    STT_OBJECT, STT_FUNC in addition to the usual STT_SECTION, it needs to
    use reloc symbol offset instead of reloc addend to find the destination
    instruction in find_jump_table() and add_jump_table().
    
    For the most part, an absolute relocation type is used for rodata. In the
    case of STT_SECTION, reloc->sym->offset is always zero, and for the other
    symbol types, reloc_addend(reloc) is always zero, thus it can use a simple
    statement "reloc->sym->offset + reloc_addend(reloc)" to obtain the symbol
    offset for various symbol types.
    
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Link: https://lore.kernel.org/r/20250211115016.26913-2-yangtiezhu@loongson.cn
    Acked-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    (cherry picked from commit ab6ce22b789622ca732e91cbb3a5cb5ba370cbd0)
    Signed-off-by: Ryan Sullivan <rysulliv@redhat.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -1946,6 +1946,7 @@ static int add_jump_table(struct objtool_file *file, struct instruction *insn,
 	unsigned int prev_offset = 0;
 	struct reloc *reloc = table;
 	struct alternative *alt;
+	unsigned long sym_offset;
 
 	/*
 	 * Each @reloc is a switch table relocation which points to the target
@@ -1963,9 +1964,10 @@ static int add_jump_table(struct objtool_file *file, struct instruction *insn,
 		if (prev_offset && reloc_offset(reloc) != prev_offset + 8)
 			break;
 
+		sym_offset = reloc->sym->offset + reloc_addend(reloc);
+
 		/* Detect function pointers from contiguous objects: */
-		if (reloc->sym->sec == pfunc->sec &&
-		    reloc_addend(reloc) == pfunc->offset)
+		if (reloc->sym->sec == pfunc->sec && sym_offset == pfunc->offset)
 			break;
 
 		/*
@@ -1973,10 +1975,10 @@ static int add_jump_table(struct objtool_file *file, struct instruction *insn,
 		 * which point to the end of the function.  Ignore them.
 		 */
 		if (reloc->sym->sec == pfunc->sec &&
-		    reloc_addend(reloc) == pfunc->offset + pfunc->len)
+		    sym_offset == pfunc->offset + pfunc->len)
 			goto next;
 
-		dest_insn = find_insn(file, reloc->sym->sec, reloc_addend(reloc));
+		dest_insn = find_insn(file, reloc->sym->sec, sym_offset);
 		if (!dest_insn)
 			break;
 
@@ -2015,6 +2017,7 @@ static void find_jump_table(struct objtool_file *file, struct symbol *func,
 	struct reloc *table_reloc;
 	struct instruction *dest_insn, *orig_insn = insn;
 	unsigned long table_size;
+	unsigned long sym_offset;
 
 	/*
 	 * Backward search using the @first_jump_src links, these help avoid
@@ -2038,7 +2041,10 @@ static void find_jump_table(struct objtool_file *file, struct symbol *func,
 		table_reloc = arch_find_switch_table(file, insn, &table_size);
 		if (!table_reloc)
 			continue;
-		dest_insn = find_insn(file, table_reloc->sym->sec, reloc_addend(table_reloc));
+
+		sym_offset = table_reloc->sym->offset + reloc_addend(table_reloc);
+
+		dest_insn = find_insn(file, table_reloc->sym->sec, sym_offset);
 		if (!dest_insn || !insn_func(dest_insn) || insn_func(dest_insn)->pfunc != func)
 			continue;
 
