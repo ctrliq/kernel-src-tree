bf078b22e4830a982a280db4aafdc9f37ce68ec5
Anubhav Shelat <ashelat@redhat.com>
2025-04-25 09:34:17 -0400

    Revert "objtool: Exclude __tracepoints data from ENDBR checks"
    JIRA: https://issues.redhat.com/browse/RHEL-87959
    
    Upstream Status: RHEL-only
    
    This reverts commit d5ae7668de669177ebb6bcdf2ecef1520807792e.
    
    When tracepoints are enabled, tracepoint_add_func() does an indirect call
    to syscall_regfunc(). Since tracepoint data is excluded from ENDBR
    checks, syscall_regfunc() is missing an ENDBR instruction
    which causes Intel CET/IBT to throw a control protection exemption.
    Therefore, revert commit d5ae7668de66 to ensure the ENDBR instruction is
    included.
    
    Signed-off-by: Anubhav Shelat <ashelat@redhat.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -4578,7 +4578,6 @@ static int validate_ibt(struct objtool_file *file)
 		    !strcmp(sec->name, ".kcfi_traps")			||
 		    !strcmp(sec->name, ".llvm.call-graph-profile")	||
 		    !strcmp(sec->name, ".llvm_bb_addr_map")		||
-		    !strcmp(sec->name, "__tracepoints")			||
 		    strstr(sec->name, "__patchable_function_entries"))
 			continue;
 
