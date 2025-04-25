b5c82d82fbaeaf7d5b460ec8ff9ea5884ec07cff
Jaroslav Kysela <jkysela@redhat.com>
2025-04-25 12:32:23 +0200

    soundwire: mipi_disco: read lane mapping properties from ACPI
    JIRA: https://issues.redhat.com/browse/RHEL-80682
    
    commit 7533d0df69452c3e7b69c727c1e8e1a7e1afc83c
    Author: Bard Liao <yung-chuan.liao@linux.intel.com>
    Date: Wed Dec 18 16:01:43 2024 +0800
    
        soundwire: mipi_disco: read lane mapping properties from ACPI
    
        The DisCo for SoundWire 2.0 added support for the
        'mipi-sdw-lane-<n>-mapping' property.
    
        Co-developed-by: Chao Song <chao.song@linux.intel.com>
        Signed-off-by: Chao Song <chao.song@linux.intel.com>
        Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
        Link: https://lore.kernel.org/r/20241218080155.102405-3-yung-chuan.liao@linux.intel.com
        Signed-off-by: Vinod Koul <vkoul@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- include/linux/soundwire/sdw.h
+++ include/linux/soundwire/sdw.h
@@ -55,6 +55,8 @@ struct sdw_slave;
 #define SDW_MAX_PORTS			15
 #define SDW_VALID_PORT_RANGE(n)		((n) < SDW_MAX_PORTS && (n) >= 1)
 
+#define SDW_MAX_LANES		8
+
 enum {
 	SDW_PORT_DIRN_SINK = 0,
 	SDW_PORT_DIRN_SOURCE,
@@ -357,6 +359,7 @@ struct sdw_dpn_prop {
  * and masks are supported
  * @commit_register_supported: is PCP_Commit register supported
  * @scp_int1_mask: SCP_INT1_MASK desired settings
+ * @lane_maps: Lane mapping for the slave, only valid if lane_control_support is set
  * @clock_reg_supported: the Peripheral implements the clock base and scale
  * registers introduced with the SoundWire 1.2 specification. SDCA devices
  * do not need to set this boolean property as the registers are required.
@@ -386,6 +389,7 @@ struct sdw_slave_prop {
 	u32 sdca_interrupt_register_list;
 	u8 commit_register_supported;
 	u8 scp_int1_mask;
+	u8 lane_maps[SDW_MAX_LANES];
 	bool clock_reg_supported;
 	bool use_domain_irq;
 };
@@ -451,6 +455,7 @@ struct sdw_master_prop {
 
 int sdw_master_read_prop(struct sdw_bus *bus);
 int sdw_slave_read_prop(struct sdw_slave *slave);
+int sdw_slave_read_lane_mapping(struct sdw_slave *slave);
 
 /*
  * SDW Slave Structures and APIs
