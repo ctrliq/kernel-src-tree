b63dba44fe69e0a26c96d4050e5ef0ce8a04ae54
Jocelyn Falempe <jfalempe@redhat.com>
2025-04-29 10:04:28 +0200

    fbcon: Add an option to disable fbcon in panic
    JIRA: https://issues.redhat.com/browse/RHEL-75953
    Upstream Status: v6.12-rc1
    
    commit d20a9f568f99591886ec73b80ff7283486710643
    Author:     Jocelyn Falempe <jfalempe@redhat.com>
    AuthorDate: Wed Jul 17 10:48:40 2024 +0200
    Commit:     Jocelyn Falempe <jfalempe@redhat.com>
    CommitDate: Fri Jul 19 11:38:23 2024 +0200
    
        This is required to avoid conflict between DRM_PANIC, and fbcon. If
        a drm device already handle panic with drm_panic, it should set
        the skip_panic field in fb_info, so that fbcon will stay quiet, and
        not overwrite the panic_screen.
    
        Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>
        Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
        Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
        Link: https://patchwork.freedesktop.org/patch/msgid/20240717090102.968152-3-jfalempe@redhat.com
    
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -508,6 +508,7 @@ struct fb_info {
 	void *par;
 
 	bool skip_vt_switch; /* no VT switch on suspend/resume required */
+	bool skip_panic; /* Do not write to the fb after a panic */
 };
 
 /* This will go away
