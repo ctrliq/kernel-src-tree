7c3e58985fa6f7cd5e6391b3384daa5fabe95858
Maxim Levitsky <mlevitsk@redhat.com>
2025-05-06 18:14:14 -0400

    iommu/amd: WARN if KVM attempts to set vCPU affinity without posted intrrupts
    JIRA: https://issues.redhat.com/browse/RHEL-82917
    
    commit aae251a380fe4741594368e0d7836a082b17ae3e
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Fri Apr 4 12:38:21 2025 -0700
    
        iommu/amd: WARN if KVM attempts to set vCPU affinity without posted intrrupts
    
        WARN if KVM attempts to set vCPU affinity when posted interrupts aren't
        enabled, as KVM shouldn't try to enable posting when they're unsupported,
        and the IOMMU driver darn well should only advertise posting support when
        AMD_IOMMU_GUEST_IR_VAPIC() is true.
    
        Note, KVM consumes is_guest_mode only on success.
    
        Signed-off-by: Sean Christopherson <seanjc@google.com>
        Message-ID: <20250404193923.1413163-7-seanjc@google.com>
        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -3659,6 +3659,9 @@ static int amd_ir_set_vcpu_affinity(struct irq_data *data, void *vcpu_info)
 	struct irq_2_irte *irte_info = &ir_data->irq_2_irte;
 	struct iommu_dev_data *dev_data;
 
+	if (WARN_ON_ONCE(!AMD_IOMMU_GUEST_IR_VAPIC(amd_iommu_guest_ir)))
+		return -EINVAL;
+
 	if (ir_data->iommu == NULL)
 		return -EINVAL;
 
@@ -3673,16 +3676,6 @@ static int amd_ir_set_vcpu_affinity(struct irq_data *data, void *vcpu_info)
 
 	ir_data->cfg = irqd_cfg(data);
 	pi_data->ir_data = ir_data;
-
-	/* Note:
-	 * SVM tries to set up for VAPIC mode, but we are in
-	 * legacy mode. So, we force legacy mode instead.
-	 */
-	if (!AMD_IOMMU_GUEST_IR_VAPIC(amd_iommu_guest_ir)) {
-		pr_debug("%s: Fall back to using intr legacy remap\n",
-			 __func__);
-		pi_data->is_guest_mode = false;
-	}
 
 	pi_data->prev_ga_tag = ir_data->cached_ga_tag;
 	if (pi_data->is_guest_mode) {
