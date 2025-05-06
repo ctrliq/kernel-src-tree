ad5869e403412bce4802ea0354a8a456b7587ffb
Maxim Levitsky <mlevitsk@redhat.com>
2025-05-06 16:03:44 -0400

    KVM: x86: Setup Hyper-V TSC page before Xen PV clocks (during clock update)
    JIRA: https://issues.redhat.com/browse/RHEL-82917
    
    commit 847d68abf10c7dc930551318ea41ec96144b8fd6
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Fri Jan 31 17:38:26 2025 -0800
    
        KVM: x86: Setup Hyper-V TSC page before Xen PV clocks (during clock update)
    
        When updating paravirtual clocks, setup the Hyper-V TSC page before
        Xen PV clocks.  This will allow dropping xen_pvclock_tsc_unstable in favor
        of simply clearing PVCLOCK_TSC_STABLE_BIT in the reference flags.
    
        Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
        Reviewed-by: Paul Durrant <paul@xen.org>
        Link: https://lore.kernel.org/r/20250201013827.680235-11-seanjc@google.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -3289,6 +3289,8 @@ int kvm_guest_time_update(struct kvm_vcpu *v)
 		hv_clock.flags &= ~PVCLOCK_GUEST_STOPPED;
 	}
 
+	kvm_hv_setup_tsc_page(v->kvm, &hv_clock);
+
 #ifdef CONFIG_KVM_XEN
 	if (vcpu->xen.vcpu_info_cache.active)
 		kvm_setup_guest_pvclock(&hv_clock, v, &vcpu->xen.vcpu_info_cache,
@@ -3298,7 +3300,6 @@ int kvm_guest_time_update(struct kvm_vcpu *v)
 		kvm_setup_guest_pvclock(&hv_clock, v, &vcpu->xen.vcpu_time_info_cache, 0,
 					xen_pvclock_tsc_unstable);
 #endif
-	kvm_hv_setup_tsc_page(v->kvm, &hv_clock);
 	return 0;
 }
 
