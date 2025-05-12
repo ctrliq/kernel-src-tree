06fb53e33c50675a6ad77cb37c354e61b4ed1a1f
Antoine Tenart <atenart@redhat.com>
2025-05-12 16:03:19 +0200

    net: ip: make ip_route_input() return drop reasons
    JIRA: https://issues.redhat.com/browse/RHEL-88890
    Upstream Status: linux.git
    Conflicts:\
    - Minor code differences as the tos to dscp conversion wasn't done in
      c9s yet.
    - Context difference due to missing upstream commit 7e863e5db618 ("ipv4:
      Convert ip_route_input() to dscp_t.") in c9s.
    
    commit 50038bf38e6577a15d52b890d82c197cf3b163a0
    Author: Menglong Dong <menglong8.dong@gmail.com>
    Date:   Thu Nov 7 20:55:59 2024 +0800
    
        net: ip: make ip_route_input() return drop reasons
    
        In this commit, we make ip_route_input() return skb drop reasons that come
        from ip_route_input_noref().
    
        Meanwhile, adjust all the call to it.
    
        Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv4/icmp.c
+++ net/ipv4/icmp.c
@@ -552,7 +552,7 @@ static struct rtable *icmp_route_lookup(struct net *net,
 		orefdst = skb_in->_skb_refdst; /* save old refdst */
 		skb_dst_set(skb_in, NULL);
 		err = ip_route_input(skb_in, fl4_dec.daddr, fl4_dec.saddr,
-				     RT_TOS(tos), rt2->dst.dev);
+				     RT_TOS(tos), rt2->dst.dev) ? -EINVAL : 0;
 
 		dst_release(&rt2->dst);
 		rt2 = skb_rtable(skb_in);
