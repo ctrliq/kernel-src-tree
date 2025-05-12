4d055a10d948c0aeafa3367ab00a4a69f5bb690b
Antoine Tenart <atenart@redhat.com>
2025-05-12 15:51:02 +0200

    net: ip: make ip_route_input_rcu() return drop reasons
    JIRA: https://issues.redhat.com/browse/RHEL-88891
    Upstream Status: linux.git
    Conflicts:\
    - Minor code differences as the tos to dscp conversion wasn't done in
      c10s yet.
    
    commit 61b95c70f3449c1c0bd1415c8ef37e2959cf1c41
    Author: Menglong Dong <menglong8.dong@gmail.com>
    Date:   Thu Nov 7 20:55:57 2024 +0800
    
        net: ip: make ip_route_input_rcu() return drop reasons
    
        In this commit, we make ip_route_input_rcu() return drop reasons, which
        come from ip_route_input_mc() and ip_route_input_slow().
    
        The only caller of ip_route_input_rcu() is ip_route_input_noref(). We
        adjust it by making it return -EINVAL on error and ignore the reasons that
        ip_route_input_rcu() returns. In the following patch, we will make
        ip_route_input_noref() returns the drop reasons.
    
        Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -2450,8 +2450,10 @@ martian_source:
 }
 
 /* called with rcu_read_lock held */
-static int ip_route_input_rcu(struct sk_buff *skb, __be32 daddr, __be32 saddr,
-			      u8 tos, struct net_device *dev, struct fib_result *res)
+static enum skb_drop_reason
+ip_route_input_rcu(struct sk_buff *skb, __be32 daddr, __be32 saddr,
+		   u8 tos, struct net_device *dev,
+		   struct fib_result *res)
 {
 	/* Multicast recognition logic is moved from route cache to here.
 	 * The problem was that too many Ethernet cards have broken/missing
@@ -2494,24 +2496,24 @@ static int ip_route_input_rcu(struct sk_buff *skb, __be32 daddr, __be32 saddr,
 			reason = ip_route_input_mc(skb, daddr, saddr, tos,
 						   dev, our);
 		}
-		return reason ? -EINVAL : 0;
+		return reason;
 	}
 
-	return ip_route_input_slow(skb, daddr, saddr, tos, dev, res) ? -EINVAL : 0;
+	return ip_route_input_slow(skb, daddr, saddr, tos, dev, res);
 }
 
 int ip_route_input_noref(struct sk_buff *skb, __be32 daddr, __be32 saddr,
 			 u8 tos, struct net_device *dev)
 {
+	enum skb_drop_reason reason;
 	struct fib_result res;
-	int err;
 
 	tos &= INET_DSCP_MASK;
 	rcu_read_lock();
-	err = ip_route_input_rcu(skb, daddr, saddr, tos, dev, &res);
+	reason = ip_route_input_rcu(skb, daddr, saddr, tos, dev, &res);
 	rcu_read_unlock();
 
-	return err;
+	return reason ? -EINVAL : 0;
 }
 EXPORT_SYMBOL(ip_route_input_noref);
 
@@ -3322,8 +3324,8 @@ static int inet_rtm_getroute(struct sk_buff *in_skb, struct nlmsghdr *nlh,
 		skb->dev	= dev;
 		skb->mark	= mark;
 		err = ip_route_input_rcu(skb, dst, src,
-					 rtm->rtm_tos & INET_DSCP_MASK, dev,
-					 &res);
+					 rtm->rtm_tos & INET_DSCP_MASK,
+					 dev, &res) ? -EINVAL : 0;
 
 		rt = skb_rtable(skb);
 		if (err == 0 && rt->dst.error)
