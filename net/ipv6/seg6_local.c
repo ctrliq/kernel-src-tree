d1767d22e622148a43a5eeb4e60319c78a0bbd2f
Antoine Tenart <atenart@redhat.com>
2025-05-15 19:27:20 -0700

    Revert "ipv6: sr: switch to GFP_ATOMIC flag to allocate memory during seg6local LWT setup"
    JIRA: https://issues.redhat.com/browse/RHEL-150155
    Upstream Status: linux.git
    
    commit cefe6e131cc4f032110efe1687295e133f3d5964
    Author: Kuniyuki Iwashima <kuniyu@amazon.com>
    Date:   Thu May 15 19:27:20 2025 -0700
    
        Revert "ipv6: sr: switch to GFP_ATOMIC flag to allocate memory during seg6local LWT setup"
    
        The previous patch fixed the same issue mentioned in
        commit 14a0087e7236 ("ipv6: sr: switch to GFP_ATOMIC
        flag to allocate memory during seg6local LWT setup").
    
        Let's revert it.
    
        Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Reviewed-by: Andrea Mayer <andrea.mayer@uniroma2.it>
        Link: https://patch.msgid.link/20250516022759.44392-5-kuniyu@amazon.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv6/seg6_local.c
+++ net/ipv6/seg6_local.c
@@ -1669,7 +1669,7 @@ static int parse_nla_srh(struct nlattr **attrs, struct seg6_local_lwt *slwt,
 	if (!seg6_validate_srh(srh, len, false))
 		return -EINVAL;
 
-	slwt->srh = kmemdup(srh, len, GFP_ATOMIC);
+	slwt->srh = kmemdup(srh, len, GFP_KERNEL);
 	if (!slwt->srh)
 		return -ENOMEM;
 
@@ -1909,7 +1909,7 @@ static int parse_nla_bpf(struct nlattr **attrs, struct seg6_local_lwt *slwt,
 	if (!tb[SEG6_LOCAL_BPF_PROG] || !tb[SEG6_LOCAL_BPF_PROG_NAME])
 		return -EINVAL;
 
-	slwt->bpf.name = nla_memdup(tb[SEG6_LOCAL_BPF_PROG_NAME], GFP_ATOMIC);
+	slwt->bpf.name = nla_memdup(tb[SEG6_LOCAL_BPF_PROG_NAME], GFP_KERNEL);
 	if (!slwt->bpf.name)
 		return -ENOMEM;
 
@@ -1992,7 +1992,7 @@ static int parse_nla_counters(struct nlattr **attrs,
 		return -EINVAL;
 
 	/* counters are always zero initialized */
-	pcounters = seg6_local_alloc_pcpu_counters(GFP_ATOMIC);
+	pcounters = seg6_local_alloc_pcpu_counters(GFP_KERNEL);
 	if (!pcounters)
 		return -ENOMEM;
 
