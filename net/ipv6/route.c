4dc89770e2ef5c887be98a524f0bc2e527d20925
Antoine Tenart <atenart@redhat.com>
2025-05-15 19:27:18 -0700

    inet: Remove rtnl_is_held arg of lwtunnel_valid_encap_type(_attr)?().
    JIRA: https://issues.redhat.com/browse/RHEL-150155
    Upstream Status: linux.git
    
    commit f0a56c17e64bb5e7cdb9295df2b5fc21e4949005
    Author: Kuniyuki Iwashima <kuniyu@amazon.com>
    Date:   Thu May 15 19:27:18 2025 -0700
    
        inet: Remove rtnl_is_held arg of lwtunnel_valid_encap_type(_attr)?().
    
        Commit f130a0cc1b4f ("inet: fix lwtunnel_valid_encap_type() lock
        imbalance") added the rtnl_is_held argument as a temporary fix while
        I'm converting nexthop and IPv6 routing table to per-netns RTNL or RCU.
    
        Now all callers of lwtunnel_valid_encap_type() do not hold RTNL.
    
        Let's remove the argument.
    
        Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Link: https://patch.msgid.link/20250516022759.44392-3-kuniyu@amazon.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -5174,8 +5174,7 @@ static int rtm_to_fib6_multipath_config(struct fib6_config *cfg,
 		rtnh = rtnh_next(rtnh, &remaining);
 	} while (rtnh_ok(rtnh, remaining));
 
-	return lwtunnel_valid_encap_type_attr(cfg->fc_mp, cfg->fc_mp_len,
-					      extack, false);
+	return lwtunnel_valid_encap_type_attr(cfg->fc_mp, cfg->fc_mp_len, extack);
 }
 
 static int rtm_to_fib6_config(struct sk_buff *skb, struct nlmsghdr *nlh,
@@ -5306,8 +5305,7 @@ static int rtm_to_fib6_config(struct sk_buff *skb, struct nlmsghdr *nlh,
 	if (tb[RTA_ENCAP_TYPE]) {
 		cfg->fc_encap_type = nla_get_u16(tb[RTA_ENCAP_TYPE]);
 
-		err = lwtunnel_valid_encap_type(cfg->fc_encap_type,
-						extack, false);
+		err = lwtunnel_valid_encap_type(cfg->fc_encap_type, extack);
 		if (err < 0)
 			goto errout;
 	}
