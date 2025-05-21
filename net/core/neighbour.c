efdc89b748eec7ec3033bc4038b038e9af1dc9ca
Antoine Tenart <atenart@redhat.com>
2025-05-21 10:14:08 +0800

    net: neigh: use kfree_skb_reason() in neigh_resolve_output() and neigh_connected_output()
    JIRA: https://issues.redhat.com/browse/RHEL-139236
    Upstream Status: linux.git
    
    commit e45b7196df60a4aef86c3998611c91fcc93d21f3
    Author: Qiu Yutan <qiu.yutan@zte.com.cn>
    Date:   Wed May 21 10:14:08 2025 +0800
    
        net: neigh: use kfree_skb_reason() in neigh_resolve_output() and neigh_connected_output()
    
        Replace kfree_skb() used in neigh_resolve_output() and
        neigh_connected_output() with kfree_skb_reason().
    
        Following new skb drop reason is added:
        /* failed to fill the device hard header */
        SKB_DROP_REASON_NEIGH_HH_FILLFAIL
    
        Signed-off-by: Qiu Yutan <qiu.yutan@zte.com.cn>
        Signed-off-by: Jiang Kun <jiang.kun2@zte.com.cn>
        Reviewed-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Reviewed-by: Xu Xin <xu.xin16@zte.com.cn>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -1547,7 +1547,7 @@ out:
 	return rc;
 out_kfree_skb:
 	rc = -EINVAL;
-	kfree_skb(skb);
+	kfree_skb_reason(skb, SKB_DROP_REASON_NEIGH_HH_FILLFAIL);
 	goto out;
 }
 EXPORT_SYMBOL(neigh_resolve_output);
@@ -1571,7 +1571,7 @@ int neigh_connected_output(struct neighbour *neigh, struct sk_buff *skb)
 		err = dev_queue_xmit(skb);
 	else {
 		err = -EINVAL;
-		kfree_skb(skb);
+		kfree_skb_reason(skb, SKB_DROP_REASON_NEIGH_HH_FILLFAIL);
 	}
 	return err;
 }
