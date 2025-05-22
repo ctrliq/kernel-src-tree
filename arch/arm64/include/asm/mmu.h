664d1f27b00128678d714d346f4449ecc7a7efd0
Sultan Alsawaf <sultan@ciq.com>
2025-05-22 15:52:58 -0700

    arm64: mm: Pin down ASIDs for sharing mm with devices
    jira LE-2580
    commit-author Jean-Philippe Brucker <jean-philippe@linaro.org>
    commit 48118151d8cc7a457f61d91df0c053473d4b31b1
    upstream-diff The context.c conflict was trivial. For mmu_context.h, the
                  initialization of nr_active_mm had to be preserved in
                  init_new_context(). This field originates from a non-upstream
                  RHEL patchset picked from LKML [1][2].
                  [1] https://lore.kernel.org/all/20200223192520.20808-1-aarcange@redhat.com/
                  [2] https://lore.kernel.org/all/20200401003203.GA12536@redhat.com/
    
    To enable address space sharing with the IOMMU, introduce
    arm64_mm_context_get() and arm64_mm_context_put(), that pin down a
    context and ensure that it will keep its ASID after a rollover. Export
    the symbols to let the modular SMMUv3 driver use them.
    
    Pinning is necessary because a device constantly needs a valid ASID,
    unlike tasks that only require one when running. Without pinning, we would
    need to notify the IOMMU when we're about to use a new ASID for a task,
    and it would get complicated when a new task is assigned a shared ASID.
    Consider the following scenario with no ASID pinned:
    
    1. Task t1 is running on CPUx with shared ASID (gen=1, asid=1)
    2. Task t2 is scheduled on CPUx, gets ASID (1, 2)
    3. Task tn is scheduled on CPUy, a rollover occurs, tn gets ASID (2, 1)
       We would now have to immediately generate a new ASID for t1, notify
       the IOMMU, and finally enable task tn. We are holding the lock during
       all that time, since we can't afford having another CPU trigger a
       rollover. The IOMMU issues invalidation commands that can take tens of
       milliseconds.
    
    It gets needlessly complicated. All we wanted to do was schedule task tn,
    that has no business with the IOMMU. By letting the IOMMU pin tasks when
    needed, we avoid stalling the slow path, and let the pinning fail when
    we're out of shareable ASIDs.
    
    After a rollover, the allocator expects at least one ASID to be available
    in addition to the reserved ones (one per CPU). So (NR_ASIDS - NR_CPUS -
    1) is the maximum number of ASIDs that can be shared with the IOMMU.
    
    	Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    	Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Link: https://lore.kernel.org/r/20200918101852.582559-5-jean-philippe@linaro.org
    	Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 48118151d8cc7a457f61d91df0c053473d4b31b1)
    	Signed-off-by: Sultan Alsawaf <sultan@ciq.com>
    
    # Conflicts:
    #       arch/arm64/include/asm/mmu_context.h
    #       arch/arm64/mm/context.c

--- arch/arm64/include/asm/mmu.h
+++ arch/arm64/include/asm/mmu.h
@@ -28,11 +28,14 @@
 
 #ifndef __ASSEMBLY__
 
+#include <linux/refcount.h>
+
 typedef struct {
 	atomic64_t	id;
 #ifdef CONFIG_COMPAT
 	void		*sigpage;
 #endif
+	refcount_t	pinned;
 	void		*vdso;
 	unsigned long	flags;
 	atomic_t	nr_active_mm;
