63fdca00ad03489f28fffa76009166523142d843
Jaroslav Kysela <jkysela@redhat.com>
2025-06-09 13:36:16 +0200

    ASoC: cs42l42: Convert to RUNTIME_PM_OPS() & co
    JIRA: https://issues.redhat.com/browse/RHEL-80657
    
    commit 227b132349825b52ded464e71271751aceb4b4c9
    Author: Takashi Iwai <tiwai@suse.de>
    Date: Mon Mar 17 10:54:31 2025 +0100
    
        ASoC: cs42l42: Convert to RUNTIME_PM_OPS() & co
    
        Use the newer RUNTIME_PM_OPS() and SYSTEM_SLEEP_PM_OPS() macros
        instead of SET_RUNTIME_PM_OPS() and SET_SYSTEM_SLEEP_PM_OPS() together
        with pm_ptr(), which allows us dropping ugly __maybe_unused
        attributes.
    
        This optimizes slightly when CONFIG_PM is disabled, too.
    
        Cc: patches@opensource.cirrus.com
        Reviewed-by: Charles Keepax <ckeepax@opensource.cirrus.com>
        Signed-off-by: Takashi Iwai <tiwai@suse.de>
        Link: https://patch.msgid.link/20250317095603.20073-10-tiwai@suse.de
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/soc/codecs/cs42l42-i2c.c
+++ sound/soc/codecs/cs42l42-i2c.c
@@ -50,7 +50,7 @@ static int cs42l42_i2c_remove(struct i2c_client *i2c_client)
 	return 0;
 }
 
-static int __maybe_unused cs42l42_i2c_resume(struct device *dev)
+static int cs42l42_i2c_resume(struct device *dev)
 {
 	int ret;
 
@@ -64,7 +64,7 @@ static int __maybe_unused cs42l42_i2c_resume(struct device *dev)
 }
 
 static const struct dev_pm_ops cs42l42_i2c_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(cs42l42_suspend, cs42l42_i2c_resume)
+	SYSTEM_SLEEP_PM_OPS(cs42l42_suspend, cs42l42_i2c_resume)
 };
 
 static const struct of_device_id __maybe_unused cs42l42_of_match[] = {
@@ -89,7 +89,7 @@ MODULE_DEVICE_TABLE(i2c, cs42l42_id);
 static struct i2c_driver cs42l42_i2c_driver = {
 	.driver = {
 		.name = "cs42l42",
-		.pm = &cs42l42_i2c_pm_ops,
+		.pm = pm_ptr(&cs42l42_i2c_pm_ops),
 		.of_match_table = of_match_ptr(cs42l42_of_match),
 		.acpi_match_table = ACPI_PTR(cs42l42_acpi_match),
 		},
