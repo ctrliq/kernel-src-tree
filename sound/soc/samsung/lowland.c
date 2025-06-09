f56b28105d6117c417d357cb1433565e79913305
Jaroslav Kysela <jkysela@redhat.com>
2025-06-09 13:01:52 +0200

    ASoC: samsung: lowland: use inclusive language for SND_SOC_DAIFMT_CBx_CFx
    JIRA: https://issues.redhat.com/browse/RHEL-80657
    
    commit 9ef6a439bc987753b7e5af5a926f05debe82bd1c
    Author: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Date: Wed Feb 26 01:38:04 2025 +0000
    
        ASoC: samsung: lowland: use inclusive language for SND_SOC_DAIFMT_CBx_CFx
    
        In SND_SOC_DAIFMT_CBx_CFx, M/S are no longer used. use P/C.
    
        Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
        Link: https://patch.msgid.link/87bjupa3k3.wl-kuninori.morimoto.gx@renesas.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/soc/samsung/lowland.c
+++ sound/soc/samsung/lowland.c
@@ -101,7 +101,7 @@ static struct snd_soc_dai_link lowland_dai[] = {
 		.name = "CPU",
 		.stream_name = "CPU",
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
-				SND_SOC_DAIFMT_CBM_CFM,
+				SND_SOC_DAIFMT_CBP_CFP,
 		.init = lowland_wm5100_init,
 		SND_SOC_DAILINK_REG(cpu),
 	},
@@ -109,7 +109,7 @@ static struct snd_soc_dai_link lowland_dai[] = {
 		.name = "Baseband",
 		.stream_name = "Baseband",
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
-				SND_SOC_DAIFMT_CBM_CFM,
+				SND_SOC_DAIFMT_CBP_CFP,
 		.ignore_suspend = 1,
 		SND_SOC_DAILINK_REG(baseband),
 	},
@@ -117,7 +117,7 @@ static struct snd_soc_dai_link lowland_dai[] = {
 		.name = "Sub Speaker",
 		.stream_name = "Sub Speaker",
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
-				SND_SOC_DAIFMT_CBM_CFM,
+				SND_SOC_DAIFMT_CBP_CFP,
 		.ignore_suspend = 1,
 		.c2c_params = &sub_params,
 		.num_c2c_params = 1,
