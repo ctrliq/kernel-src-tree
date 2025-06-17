40ac0155ca0c3075507c218b07dbf52dee87cfa8
Ming Lei <ming.lei@redhat.com>
2025-06-17 15:43:23 +0200

    lib/group_cpus: Let group_cpu_evenly() return the number of initialized masks
    JIRA: https://issues.redhat.com/browse/RHEL-144763
    
    commit b6139a6abf673029008f80d42abd3848d80a9108
    Author: Daniel Wagner <wagi@kernel.org>
    Date:   Tue Jun 17 15:43:23 2025 +0200
    
        lib/group_cpus: Let group_cpu_evenly() return the number of initialized masks
    
        group_cpu_evenly() might have allocated less groups then requested:
    
        group_cpu_evenly()
          __group_cpus_evenly()
            alloc_nodes_groups()
              # allocated total groups may be less than numgrps when
              # active total CPU number is less then numgrps
    
        In this case, the caller will do an out of bound access because the
        caller assumes the masks returned has numgrps.
    
        Return the number of groups created so the caller can limit the access
        range accordingly.
    
        Acked-by: Thomas Gleixner <tglx@linutronix.de>
        Reviewed-by: Hannes Reinecke <hare@suse.de>
        Reviewed-by: Ming Lei <ming.lei@redhat.com>
        Signed-off-by: Daniel Wagner <wagi@kernel.org>
        Reviewed-by: Chaitanya Kulkarni <kch@nvidia.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Link: https://lore.kernel.org/r/20250617-isolcpus-queue-counters-v1-1-13923686b54b@kernel.org
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- block/blk-mq-cpumap.c
+++ block/blk-mq-cpumap.c
@@ -59,9 +59,9 @@ EXPORT_SYMBOL_GPL(blk_mq_num_online_queues);
 void blk_mq_map_queues(struct blk_mq_queue_map *qmap)
 {
 	const struct cpumask *masks;
-	unsigned int queue, cpu;
+	unsigned int queue, cpu, nr_masks;
 
-	masks = group_cpus_evenly(qmap->nr_queues);
+	masks = group_cpus_evenly(qmap->nr_queues, &nr_masks);
 	if (!masks) {
 		for_each_possible_cpu(cpu)
 			qmap->mq_map[cpu] = qmap->queue_offset;
@@ -69,7 +69,7 @@ void blk_mq_map_queues(struct blk_mq_queue_map *qmap)
 	}
 
 	for (queue = 0; queue < qmap->nr_queues; queue++) {
-		for_each_cpu(cpu, &masks[queue])
+		for_each_cpu(cpu, &masks[queue % nr_masks])
 			qmap->mq_map[cpu] = qmap->queue_offset + queue;
 	}
 	kfree(masks);
