1e79e76324ba67a52363e808dcb0360399728598
Mark Langsdorf <mlangsdo@redhat.com>
2025-06-30 11:15:43 -0500

    cpufreq/amd-pstate: Show a warning when a CPU fails to setup
    JIRA: https://issues.redhat.com/browse/RHEL-83800
    
    commit a9ba0fd452d82ca0da170eb6291aac01075a17d5
    Author: Mario Limonciello <mario.limonciello@amd.com>
    Date: Thu, 06 Mar 2025 13:01:24 +0000
    
    I came across a system that MSR_AMD_CPPC_CAP1 for some CPUs isn't
    populated.  This is an unexpected behavior that is most likely a
    BIOS bug. In the event it happens I'd like users to report bugs
    to properly root cause and get this fixed.
    
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Reviewed-by: Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -1027,6 +1027,7 @@ static int amd_pstate_cpu_init(struct cpufreq_policy *policy)
 free_cpudata2:
 	freq_qos_remove_request(&cpudata->req[0]);
 free_cpudata1:
+	pr_warn("Failed to initialize CPU %d: %d\n", policy->cpu, ret);
 	kfree(cpudata);
 	return ret;
 }
@@ -1520,6 +1521,7 @@ static int amd_pstate_epp_cpu_init(struct cpufreq_policy *policy)
 	return 0;
 
 free_cpudata1:
+	pr_warn("Failed to initialize CPU %d: %d\n", policy->cpu, ret);
 	kfree(cpudata);
 	return ret;
 }
