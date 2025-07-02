c7c385a7c6080df684104af5ef05402a79c524b6
Waiman Long <longman@redhat.com>
2025-07-02 17:33:32 -0400

    mm: fix (harmless) type confusion in lock_vma_under_rcu()
    JIRA: https://issues.redhat.com/browse/RHEL-101190
    Conflicts:
      A context diff in the lock_vma_under_rcu() hunk of mm/memory.c due to
      missing upstream commit a373baed5a9d ("mm: delay the check for a NULL
      anon_vma").
    
    commit 17fe833b0de08a78bfb51388e0615969d73ea8ad
    Author: Jann Horn <jannh@google.com>
    Date:   Mon, 5 Aug 2024 14:52:03 +0200
    
        mm: fix (harmless) type confusion in lock_vma_under_rcu()
    
        There is a (harmless) type confusion in lock_vma_under_rcu(): After
        vma_start_read(), we have taken the VMA lock but don't know yet whether
        the VMA has already been detached and scheduled for RCU freeing.  At this
        point, ->vm_start and ->vm_end are accessed.
    
        vm_area_struct contains a union such that ->vm_rcu uses the same memory as
        ->vm_start and ->vm_end; so accessing ->vm_start and ->vm_end of a
        detached VMA is illegal and leads to type confusion between union members.
    
        Fix it by reordering the vma->detached check above the address checks, and
        document the rules for RCU readers accessing VMAs.
    
        This will probably change the number of observed VMA_LOCK_MISS events
        (since previously, trying to access a detached VMA whose ->vm_rcu has been
        scheduled would bail out when checking the fault address against the
        rcu_head members reinterpreted as VMA bounds).
    
        Link: https://lkml.kernel.org/r/20240805-fix-vma-lock-type-confusion-v1-1-9f25443a9a71@google.com
        Fixes: 50ee32537206 ("mm: introduce lock_vma_under_rcu to be used from arch-specific code")
        Signed-off-by: Jann Horn <jannh@google.com>
        Acked-by: Suren Baghdasaryan <surenb@google.com>
        Cc: Matthew Wilcox <willy@infradead.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -617,6 +617,9 @@ struct vma_numab_state {
  * per VM-area/task. A VM area is any part of the process virtual memory
  * space that has a special rule for the page-fault handlers (ie a shared
  * library, the executable area etc).
+ *
+ * Only explicitly marked struct members may be accessed by RCU readers before
+ * getting a stable reference.
  */
 struct vm_area_struct {
 	/* The first cache line has the info for VMA tree walking. */
@@ -632,7 +635,11 @@ struct vm_area_struct {
 #endif
 	};
 
-	struct mm_struct *vm_mm;	/* The address space we belong to. */
+	/*
+	 * The address space we belong to.
+	 * Unstable RCU readers are allowed to read this.
+	 */
+	struct mm_struct *vm_mm;
 	pgprot_t vm_page_prot;          /* Access permissions of this VMA. */
 
 	/*
@@ -645,7 +652,10 @@ struct vm_area_struct {
 	};
 
 #ifdef CONFIG_PER_VMA_LOCK
-	/* Flag to indicate areas detached from the mm->mm_mt tree */
+	/*
+	 * Flag to indicate areas detached from the mm->mm_mt tree.
+	 * Unstable RCU readers are allowed to read this.
+	 */
 	bool detached;
 
 	/*
@@ -663,6 +673,7 @@ struct vm_area_struct {
 	 * slowpath.
 	 */
 	int vm_lock_seq;
+	/* Unstable RCU readers are allowed to read this. */
 	struct vma_lock *vm_lock;
 #endif
 
