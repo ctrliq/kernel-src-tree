941760ae94ffd0060cd768c926eb1d1cd047da4c
Miklos Szeredi <mszeredi@redhat.com>
2025-07-07 17:15:03 +0200

    fanotify: report old and/or new parent+name in FAN_RENAME event
    JIRA: https://issues.redhat.com/browse/RHEL-102134
    
    commit 7326e382c21e9c23c89c88369afdc90b82a14da8
    Author: Amir Goldstein <amir73il@gmail.com>
    Date:   Mon Nov 29 22:15:36 2021 +0200
    
        fanotify: report old and/or new parent+name in FAN_RENAME event
    
        In the special case of FAN_RENAME event, we report old or new or both
        old and new parent+name.
    
        A single info record will be reported if either the old or new dir
        is watched and two records will be reported if both old and new dir
        (or their filesystem) are watched.
    
        The old and new parent+name are reported using new info record types
        FAN_EVENT_INFO_TYPE_{OLD,NEW}_DFID_NAME, so if a single info record
        is reported, it is clear to the application, to which dir entry the
        fid+name info is referring to.
    
        Link: https://lore.kernel.org/r/20211129201537.1932819-11-amir73il@gmail.com
        Signed-off-by: Amir Goldstein <amir73il@gmail.com>
        Signed-off-by: Jan Kara <jack@suse.cz>
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- include/uapi/linux/fanotify.h
+++ include/uapi/linux/fanotify.h
@@ -132,6 +132,12 @@ struct fanotify_event_metadata {
 #define FAN_EVENT_INFO_TYPE_DFID	3
 #define FAN_EVENT_INFO_TYPE_ERROR	5
 
+/* Special info types for FAN_RENAME */
+#define FAN_EVENT_INFO_TYPE_OLD_DFID_NAME	10
+/* Reserved for FAN_EVENT_INFO_TYPE_OLD_DFID	11 */
+#define FAN_EVENT_INFO_TYPE_NEW_DFID_NAME	12
+/* Reserved for FAN_EVENT_INFO_TYPE_NEW_DFID	13 */
+
 /* Variable length info record following event metadata */
 struct fanotify_event_info_header {
 	__u8 info_type;
