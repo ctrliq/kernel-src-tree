ca951dd5ef4e99e2823d0e06317cfeb5dad1510c
Andreas Gruenbacher <agruenba@redhat.com>
2025-07-08 22:07:17 +0200

    gfs2: Partially revert "gfs2: do_xmote fixes"
    JIRA: https://issues.redhat.com/browse/RHEL-116886
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/gfs2/linux-gfs2.git
    
    When the lm_lock hook which calls dlm_lock() returns an error,
    do_xmote() previously reported the error to the syslog ("lm_lock ret
    %d\n") but otherwise ignored it during withdraws.  Commit 9947a06d29c0
    ("gfs2: do_xmote fixes") changed that to pass the error on to the glock
    layer, but the error would then only result in an unconditional BUG() in
    finish_xmote(), which doesn't help.  Instead, revert to the previous
    behavior.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit 418c854759341cdf687240bd064de09ce38b92c5)

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -834,7 +834,8 @@ skip_inval:
 			 */
 		} else {
 			fs_err(sdp, "lm_lock ret %d\n", ret);
-			target = gl->gl_state | LM_OUT_ERROR;
+			GLOCK_BUG_ON(gl, !gfs2_withdrawing_or_withdrawn(sdp));
+			return;
 		}
 	}
 
