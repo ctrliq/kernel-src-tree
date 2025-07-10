aacd9375ec1ef9ffefddd0c8f7d8c56ba436117a
Čestmír Kalina <ckalina@redhat.com>
2025-07-10 06:54:23 +0200

    rcu: Let dump_cpu_task() be used without preemption disabled
    JIRA: https://issues.redhat.com/browse/RHEL-79879
    
    commit 51b739990cc81316768b85db519eac3999ba92a9
    Author: Ryo Takakura <takakura@valinux.co.jp>
    Date:   Fri Jun 28 13:18:26 2024 +0900
    
    	The commit 2d7f00b2f0130 ("rcu: Suppress smp_processor_id() complaint
    	in synchronize_rcu_expedited_wait()") disabled preemption around
    	dump_cpu_task() to suppress warning on its usage within preemtible context.
    
    	Calling dump_cpu_task() doesn't required to be in non-preemptible context
    	except for suppressing the smp_processor_id() warning.
    	As the smp_processor_id() is evaluated along with in_hardirq()
    	to check if it's in interrupt context, this patch removes the need
    	for its preemtion disablement by reordering the condition so that
    	smp_processor_id() only gets evaluated when it's in interrupt context.
    
    	Signed-off-by: Ryo Takakura <takakura@valinux.co.jp>
    	Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    	Signed-off-by: Neeraj Upadhyay <neeraj.upadhyay@kernel.org>
    
    Signed-off-by: Čestmír Kalina <ckalina@redhat.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9837,7 +9837,7 @@ struct cgroup_subsys cpu_cgrp_subsys = {
 
 void dump_cpu_task(int cpu)
 {
-	if (cpu == smp_processor_id() && in_hardirq()) {
+	if (in_hardirq() && cpu == smp_processor_id()) {
 		struct pt_regs *regs;
 
 		regs = get_irq_regs();
