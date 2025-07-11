39fe12ce82695bd85fde019b3ea621052b4ade93
Bill O'Donnell <bodonnel@redhat.com>
2025-07-10 21:50:17 -0500

    vfs: export remap and write check helpers
    JIRA: https://issues.redhat.com/browse/RHEL-85589
    
    commit 5b9932f6001c70b984e8c9c2fe09e443beb4baba
    Author: Darrick J. Wong <djwong@kernel.org>
    Date:   Mon Apr 15 14:54:13 2024 -0700
    
        vfs: export remap and write check helpers
    
        Export these functions so that the next patch can use them to check the
        file ranges being passed to the XFS_IOC_EXCHANGE_RANGE operation.
    
        Cc: linux-fsdevel@vger.kernel.org
        Signed-off-by: Darrick J. Wong <djwong@kernel.org>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
    
    Conflicts:
    - fs/read_write.c: 1 hunk modified
    - fs/remap_range.c: 2 hunks modified
    - include/linux/fs.h: 1 hunk modified
    
    Assisted-by: Patchpal AI
    Signed-off-by: Bill O'Donnell <bodonnel@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2308,6 +2308,7 @@ extern ssize_t vfs_read(struct file *, char __user *, size_t, loff_t *);
 extern ssize_t vfs_write(struct file *, const char __user *, size_t, loff_t *);
 extern ssize_t vfs_copy_file_range(struct file *, loff_t , struct file *,
 				   loff_t, size_t, unsigned int);
+int remap_verify_area(struct file *file, loff_t pos, loff_t len, bool write);
 extern ssize_t generic_copy_file_range(struct file *file_in, loff_t pos_in,
 				       struct file *file_out, loff_t pos_out,
 				       size_t len, unsigned int flags);
