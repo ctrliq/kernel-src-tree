24068d5c32fea40e00d430012f6ed45f4e6f2bb2
Bill O'Donnell <bodonnel@redhat.com>
2025-07-10 21:50:35 -0500

    xfs: reinstate delalloc for RT inodes (if sb_rextsize == 1)
    JIRA: https://issues.redhat.com/browse/RHEL-85589
    
    commit 6a94b1acda7e7262418e23f906c12a2b08b69d12
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Mon Apr 22 13:20:19 2024 +0200
    
        xfs: reinstate delalloc for RT inodes (if sb_rextsize == 1)
    
        Commit aff3a9edb708 ("xfs: Use preallocation for inodes with extsz
        hints") disabled delayed allocation for all inodes with extent size
        hints due a data exposure problem.  It turns out we fixed this data
        exposure problem since by always creating unwritten extents for
        delalloc conversions due to more data exposure problems, but the
        writeback path doesn't actually support extent size hints when
        converting delalloc these days, which probably isn't a problem given
        that people using the hints know what they get.
    
        However due to the way how xfs_get_extsz_hint is implemented, it
        always claims an extent size hint for RT inodes even if the RT
        extent size is a single FSB.  Due to that the above commit effectively
        disabled delalloc support for RT inodes.
    
        Switch xfs_get_extsz_hint to return 0 for this case and work around
        that in a few places to reinstate delalloc support for RT inodes on
        file systems with an sb_rextsize of 1.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Dave Chinner <dchinner@redhat.com>
        Reviewed-by: "Darrick J. Wong" <djwong@kernel.org>
        Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>
    
    Signed-off-by: Bill O'Donnell <bodonnel@redhat.com>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1339,6 +1339,8 @@ xfs_bmap_rtalloc(
 	int			error;
 
 	align = xfs_get_extsz_hint(ap->ip);
+	if (!align)
+		align = 1;
 retry:
 	error = xfs_bmap_extsize_align(mp, &ap->got, &ap->prev,
 					align, 1, ap->eof, 0,
