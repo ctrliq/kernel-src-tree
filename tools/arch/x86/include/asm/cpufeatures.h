3e5a6dd61f4f443d78f2bc98bc7a2fa5b923f996
Waiman Long <longman@redhat.com>
2025-07-11 12:19:36 -0400

    Revert "mm/execmem: Unify early execmem_cache behaviour"
    JIRA: https://issues.redhat.com/browse/RHEL-91960
    
    commit 7cd9a11dd0c3d1dd225795ed1b5b53132888e7b5
    Author: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Date:   Tue, 3 Jun 2025 14:14:45 +0300
    
        Revert "mm/execmem: Unify early execmem_cache behaviour"
    
        The commit d6d1e3e6580c ("mm/execmem: Unify early execmem_cache
        behaviour") changed early behaviour of execemem ROX cache to allow its
        usage in early x86 code that allocates text pages when
        CONFIG_MITGATION_ITS is enabled.
    
        The permission management of the pages allocated from execmem for ITS
        mitigation is now completely contained in arch/x86/kernel/alternatives.c
        and therefore there is no need to special case early allocations in
        execmem.
    
        This reverts commit d6d1e3e6580ca35071ad474381f053cbf1fb6414.
    
        Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: stable@vger.kernel.org
        Link: https://lkml.kernel.org/r/20250603111446.2609381-6-rppt@kernel.org
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- tools/arch/x86/include/asm/cpufeatures.h
+++ tools/arch/x86/include/asm/cpufeatures.h
@@ -386,6 +386,7 @@
 #define X86_FEATURE_V_SPEC_CTRL		(15*32+20) /* "v_spec_ctrl" Virtual SPEC_CTRL */
 #define X86_FEATURE_VNMI		(15*32+25) /* "vnmi" Virtual NMI */
 #define X86_FEATURE_SVME_ADDR_CHK	(15*32+28) /* SVME addr check */
+#define X86_FEATURE_BUS_LOCK_THRESHOLD	(15*32+29) /* Bus lock threshold */
 #define X86_FEATURE_IDLE_HLT		(15*32+30) /* IDLE HLT intercept */
 
 /* Intel-defined CPU features, CPUID level 0x00000007:0 (ECX), word 16 */
@@ -454,6 +455,7 @@
 #define X86_FEATURE_DEBUG_SWAP		(19*32+14) /* "debug_swap" SEV-ES full debug state swap support */
 #define X86_FEATURE_RMPREAD		(19*32+21) /* RMPREAD instruction */
 #define X86_FEATURE_SEGMENTED_RMP	(19*32+23) /* Segmented RMP support */
+#define X86_FEATURE_ALLOWED_SEV_FEATURES (19*32+27) /* Allowed SEV Features */
 #define X86_FEATURE_SVSM		(19*32+28) /* "svsm" SVSM present */
 #define X86_FEATURE_HV_INUSE_WR_ALLOWED	(19*32+30) /* Allow Write to in-use hypervisor-owned pages */
 
@@ -465,6 +467,7 @@
 #define X86_FEATURE_AUTOIBRS		(20*32+ 8) /* Automatic IBRS */
 #define X86_FEATURE_NO_SMM_CTL_MSR	(20*32+ 9) /* SMM_CTL MSR is not present */
 
+#define X86_FEATURE_PREFETCHI		(20*32+20) /* Prefetch Data/Instruction to Cache Level */
 #define X86_FEATURE_SBPB		(20*32+27) /* Selective Branch Prediction Barrier */
 #define X86_FEATURE_IBPB_BRTYPE		(20*32+28) /* MSR_PRED_CMD[IBPB] flushes all branch type predictions */
 #define X86_FEATURE_SRSO_NO		(20*32+29) /* CPU is not affected by SRSO */
