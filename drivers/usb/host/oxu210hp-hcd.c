134c71a0747592085e1903d8e2e8054ea0119bf4
Desnes Nunes <desnesn@redhat.com>
2025-07-14 16:42:24 -0300

    treewide: Switch/rename to timer_delete[_sync]()
    JIRA: https://issues.redhat.com/browse/RHEL-78839
    Conflicts:
    * Partial backport only including usb, memstick and cardreader changes; in
      order to follow new code standard regarding the usage of pending timers
    * Avoiding commit 8e0285ab95a9 ("ARM/musb: omap2: Remove global GPIO
      numbers from TUSB6010") on tusb_musb_exit() in tusb6010.c adding *glue
    * Avoiding commit 21acc656a06e ("usb: musb: Add and use inline functions
      musb_{get,set}_state") on musb_core.c to update the otg state
    
    commit 8fa7292fee5c5240402371ea89ab285ec856c916
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date: Sat, 5 Apr 2025 10:17:26 +0200
    
      timer_delete[_sync]() replaces del_timer[_sync](). Convert the whole tree
      over and remove the historical wrapper inlines.
    
      Conversion was done with coccinelle plus manual fixups where necessary.
    
      Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
      Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Desnes Nunes <desnesn@redhat.com>

--- drivers/usb/host/oxu210hp-hcd.c
+++ drivers/usb/host/oxu210hp-hcd.c
@@ -1152,7 +1152,7 @@ static void ehci_mem_cleanup(struct oxu_hcd *oxu)
 		qh_put(oxu->async);
 	oxu->async = NULL;
 
-	del_timer(&oxu->urb_timer);
+	timer_delete(&oxu->urb_timer);
 
 	oxu->periodic = NULL;
 
@@ -3179,7 +3179,7 @@ static void oxu_stop(struct usb_hcd *hcd)
 	ehci_port_power(oxu, 0);
 
 	/* no more interrupts ... */
-	del_timer_sync(&oxu->watchdog);
+	timer_delete_sync(&oxu->watchdog);
 
 	spin_lock_irq(&oxu->lock);
 	if (HC_IS_RUNNING(hcd->state))
@@ -3912,7 +3912,7 @@ static int oxu_bus_suspend(struct usb_hcd *hcd)
 
 	spin_unlock_irq(&oxu->lock);
 	/* turn off now-idle HC */
-	del_timer_sync(&oxu->watchdog);
+	timer_delete_sync(&oxu->watchdog);
 	spin_lock_irq(&oxu->lock);
 	ehci_halt(oxu);
 	hcd->state = HC_STATE_SUSPENDED;
