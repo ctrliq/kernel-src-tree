86b610e454b2442a114f5cf945e9a201bc0368dc
Mark Langsdorf <mlangsdo@redhat.com>
2025-07-14 16:40:58 -0400

    ACPI: platform_profile: Remove platform_profile_handler from callbacks
    JIRA: https://issues.redhat.com/browse/RHEL-89362
    Conflicts:
           drivers/platform/x86/dell/alienware-wmi.c - centos-stream-10
    does not support thermal_profiles for alienware yet, so this part of
    the commit was dropped
    
    commit cf3ea098dd3af415f079bc0b999055f213dd4a83
    Author: Kurt Borja <kuurtb@gmail.com>
    Date:   Wed Jan 15 19:27:05 2025 -0500
    
    Devices can now set drvdata to the class device, thus passing the
    platform_profile_handler to callbacks is unnecessary. Instead pass the
    class device.
    
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Kurt Borja <kuurtb@gmail.com>
    Reviewed-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Tested-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Link: https://lore.kernel.org/r/20250116002721.75592-4-kuurtb@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/platform/x86/acer-wmi.c
+++ drivers/platform/x86/acer-wmi.c
@@ -1914,7 +1914,7 @@ static int acer_toggle_turbo(void)
 }
 
 static int
-acer_predator_v4_platform_profile_get(struct platform_profile_handler *pprof,
+acer_predator_v4_platform_profile_get(struct device *dev,
 				      enum platform_profile_option *profile)
 {
 	u8 tp;
@@ -1948,7 +1948,7 @@ acer_predator_v4_platform_profile_get(struct platform_profile_handler *pprof,
 }
 
 static int
-acer_predator_v4_platform_profile_set(struct platform_profile_handler *pprof,
+acer_predator_v4_platform_profile_set(struct device *dev,
 				      enum platform_profile_option profile)
 {
 	int err, tp;
