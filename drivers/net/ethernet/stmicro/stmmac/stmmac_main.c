03eb58d1094bbd5ca04fa545816968e7552138e6
Izabela Bakollari <ibakolla@redhat.com>
2025-07-15 13:17:52 +0200

    net: stmmac: check priv->eee_sw_timer_en in suspend path
    JIRA: https://issues.redhat.com/browse/RHEL-100501
    
    commit c920e6402523ab43b035c913e14d7a580d815d83
    Author: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Date:   Mon Jan 13 11:45:55 2025 +0000
    
        net: stmmac: check priv->eee_sw_timer_en in suspend path
    
        The suspend path uses priv->eee_enabled when cleaning up the software
        timed LPI mode. Use priv->eee_sw_timer_en instead so we're consistently
        using a single control for software-based timer handling.
    
        Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
        Link: https://patch.msgid.link/E1tXItH-000MBC-8i@rmk-PC.armlinux.org.uk
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7713,7 +7713,7 @@ int stmmac_suspend(struct device *dev)
 	for (chan = 0; chan < priv->plat->tx_queues_to_use; chan++)
 		hrtimer_cancel(&priv->dma_conf.tx_queue[chan].txtimer);
 
-	if (priv->eee_enabled) {
+	if (priv->eee_sw_timer_en) {
 		priv->tx_path_in_lpi_mode = false;
 		timer_delete_sync(&priv->eee_ctrl_timer);
 	}
