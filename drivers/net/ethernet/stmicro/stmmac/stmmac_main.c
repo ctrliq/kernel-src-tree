b6143baf2162029bdc2cb32e976eb12a4ba486ec
Izabela Bakollari <ibakolla@redhat.com>
2025-07-15 13:18:25 +0200

    net: stmmac: Drop redundant skb_mark_for_recycle() for SKB frags
    JIRA: https://issues.redhat.com/browse/RHEL-100501
    
    commit ba5f78505fb4e3feb73d8e5249a13b3244dac4d9
    Author: Furong Xu <0x1207@gmail.com>
    Date:   Fri Jan 17 14:28:05 2025 +0800
    
        net: stmmac: Drop redundant skb_mark_for_recycle() for SKB frags
    
        After commit df542f669307 ("net: stmmac: Switch to zero-copy in
        non-XDP RX path"), SKBs are always marked for recycle, it is redundant
        to mark SKBs more than once when new frags are appended.
    
        Signed-off-by: Furong Xu <0x1207@gmail.com>
        Link: https://patch.msgid.link/20250117062805.192393-1-0x1207@gmail.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -5643,9 +5643,6 @@ read_again:
 			skb_add_rx_frag(skb, skb_shinfo(skb)->nr_frags,
 					buf->page, buf->page_offset, buf1_len,
 					priv->dma_conf.dma_buf_sz);
-
-			/* Data payload appended into SKB */
-			skb_mark_for_recycle(skb);
 			buf->page = NULL;
 		}
 
@@ -5655,9 +5652,6 @@ read_again:
 			skb_add_rx_frag(skb, skb_shinfo(skb)->nr_frags,
 					buf->sec_page, 0, buf2_len,
 					priv->dma_conf.dma_buf_sz);
-
-			/* Data payload appended into SKB */
-			skb_mark_for_recycle(skb);
 			buf->sec_page = NULL;
 		}
 
