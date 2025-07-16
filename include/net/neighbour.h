866d68abaafb6a6dd1aec8ec7000c1dd5608f85a
Antoine Tenart <atenart@redhat.com>
2025-07-16 22:08:11 +0000

    neighbour: Annotate neigh_table.phash_buckets and pneigh_entry.next with __rcu.
    JIRA: https://issues.redhat.com/browse/RHEL-150155
    Upstream Status: linux.git
    Conflicts:\
    - Context difference due to missing upstream commit c7d78566bbd3 ("neighbour:
      add support for NUD_PERMANENT proxy entries") in c10s.
    
    commit d63382aea70aa4ecb516126e00930bc8ab5e55ef
    Author: Kuniyuki Iwashima <kuniyu@google.com>
    Date:   Wed Jul 16 22:08:11 2025 +0000
    
        neighbour: Annotate neigh_table.phash_buckets and pneigh_entry.next with __rcu.
    
        The next patch will free pneigh_entry with call_rcu().
    
        Then, we need to annotate neigh_table.phash_buckets[] and
        pneigh_entry.next with __rcu.
    
        To make the next patch cleaner, let's annotate the fields in advance.
    
        Currently, all accesses to the fields are under the neigh table lock,
        so rcu_dereference_protected() is used with 1 for now, but most of them
        (except in pneigh_delete() and pneigh_ifdown_and_unlock()) will be
        replaced with rcu_dereference() and rcu_dereference_check().
    
        Note that pneigh_ifdown_and_unlock() changes pneigh_entry.next to a
        local list, which is illegal because the RCU iterator could be moved
        to another list.  This part will be fixed in the next patch.
    
        Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
        Link: https://patch.msgid.link/20250716221221.442239-7-kuniyu@google.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- include/net/neighbour.h
+++ include/net/neighbour.h
@@ -174,7 +174,7 @@ struct neigh_ops {
 };
 
 struct pneigh_entry {
-	struct pneigh_entry	*next;
+	struct pneigh_entry	__rcu *next;
 	possible_net_t		net;
 	struct net_device	*dev;
 	netdevice_tracker	dev_tracker;
@@ -233,7 +233,7 @@ struct neigh_table {
 	unsigned long		last_rand;
 	struct neigh_statistics	__percpu *stats;
 	struct neigh_hash_table __rcu *nht;
-	struct pneigh_entry	**phash_buckets;
+	struct pneigh_entry	__rcu **phash_buckets;
 };
 
 enum {
