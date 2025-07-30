20c3688d49602aa948cae3933e115231696eeb25
Izabela Bakollari <ibakolla@redhat.com>
2025-07-30 08:46:43 +0200

    net: dwmac-imx: Use helper rgmii_clock
    JIRA: https://issues.redhat.com/browse/RHEL-100498
    
    commit 839b75ea4d940f810650a2ce11c91d94c5f01aa3
    Author: Jan Petrous (OSS) <jan.petrous@oss.nxp.com>
    Date:   Thu Dec 5 17:43:03 2024 +0100
    
        net: dwmac-imx: Use helper rgmii_clock
    
        Utilize a new helper function rgmii_clock().
    
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Reviewed-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
        Signed-off-by: Jan Petrous (OSS) <jan.petrous@oss.nxp.com>
        Link: https://patch.msgid.link/20241205-upstream_s32cc_gmac-v8-6-ec1d180df815@oss.nxp.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-imx.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-imx.c
@@ -187,7 +187,7 @@ static void imx_dwmac_fix_speed(void *priv, unsigned int speed, unsigned int mod
 {
 	struct plat_stmmacenet_data *plat_dat;
 	struct imx_priv_data *dwmac = priv;
-	unsigned long rate;
+	long rate;
 	int err;
 
 	plat_dat = dwmac->plat_dat;
@@ -197,17 +197,8 @@ static void imx_dwmac_fix_speed(void *priv, unsigned int speed, unsigned int mod
 	    (plat_dat->mac_interface == PHY_INTERFACE_MODE_MII))
 		return;
 
-	switch (speed) {
-	case SPEED_1000:
-		rate = 125000000;
-		break;
-	case SPEED_100:
-		rate = 25000000;
-		break;
-	case SPEED_10:
-		rate = 2500000;
-		break;
-	default:
+	rate = rgmii_clock(speed);
+	if (rate < 0) {
 		dev_err(dwmac->dev, "invalid speed %u\n", speed);
 		return;
 	}
