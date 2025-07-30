b63c60d23a595b5581c1532bc34c12d3cffd3e34
Izabela Bakollari <ibakolla@redhat.com>
2025-07-30 10:31:11 +0200

    net: stmmac: generate software timestamp just before the doorbell
    JIRA: https://issues.redhat.com/browse/RHEL-100498
    
    commit 33d4cc81fcd930fdbcca7ac9e8959225cbec0a5e
    Author: Jason Xing <kernelxing@tencent.com>
    Date:   Sat May 10 21:48:12 2025 +0800
    
        net: stmmac: generate software timestamp just before the doorbell
    
        Make sure the call of skb_tx_timestamp is as close as possbile to the
        doorbell.
    
        The patch also adjusts the order of setting SKBTX_IN_PROGRESS and
        generate software timestamp so that without SOF_TIMESTAMPING_OPT_TX_SWHW
        being set the software and hardware timestamps will not appear in the
        error queue of socket nearly at the same time (Please see __skb_tstamp_tx()).
    
        Signed-off-by: Jason Xing <kernelxing@tencent.com>
        Link: https://patch.msgid.link/20250510134812.48199-4-kerneljasonxing@gmail.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -4498,8 +4498,6 @@ static netdev_tx_t stmmac_tso_xmit(struct sk_buff *skb, struct net_device *dev)
 	if (priv->sarc_type)
 		stmmac_set_desc_sarc(priv, first, priv->sarc_type);
 
-	skb_tx_timestamp(skb);
-
 	if (unlikely((skb_shinfo(skb)->tx_flags & SKBTX_HW_TSTAMP) &&
 		     priv->hwts_tx_en)) {
 		/* declare that device is doing timestamping */
@@ -4532,6 +4530,7 @@ static netdev_tx_t stmmac_tso_xmit(struct sk_buff *skb, struct net_device *dev)
 	}
 
 	netdev_tx_sent_queue(netdev_get_tx_queue(dev, queue), skb->len);
+	skb_tx_timestamp(skb);
 
 	stmmac_flush_tx_descriptors(priv, queue);
 	stmmac_tx_timer_arm(priv, queue);
@@ -4775,8 +4774,6 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 	if (priv->sarc_type)
 		stmmac_set_desc_sarc(priv, first, priv->sarc_type);
 
-	skb_tx_timestamp(skb);
-
 	/* Ready to fill the first descriptor and set the OWN bit w/o any
 	 * problems because all the descriptors are actually ready to be
 	 * passed to the DMA engine.
@@ -4823,7 +4820,7 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 	netdev_tx_sent_queue(netdev_get_tx_queue(dev, queue), skb->len);
 
 	stmmac_enable_dma_transmission(priv, priv->ioaddr, queue);
-
+	skb_tx_timestamp(skb);
 	stmmac_flush_tx_descriptors(priv, queue);
 	stmmac_tx_timer_arm(priv, queue);
 
