e3abd265af2a3f4b136ef2e99282fc82725589d5
Pratham Patel <ppatel@ciq.com>
2025-07-31 22:34:08 +0530

    driver: base: fix UAF when driver_attach failed
    jira VULN-51189
    cve CVE-2022-49385
    commit-author Schspa Shi <schspa@gmail.com>
    commit 310862e574001a97ad02272bac0fd13f75f42a27
    
    When driver_attach(drv); failed, the driver_private will be freed.
    But it has been added to the bus, which caused a UAF.
    
    To fix it, we need to delete it from the bus when failed.
    
    Fixes: 190888ac01d0 ("driver core: fix possible missing of device probe")
    	Signed-off-by: Schspa Shi <schspa@gmail.com>
    Link: https://lore.kernel.org/r/20220513112444.45112-1-schspa@gmail.com
    	Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 310862e574001a97ad02272bac0fd13f75f42a27)
    	Signed-off-by: Pratham Patel <ppatel@ciq.com>

--- drivers/base/bus.c
+++ drivers/base/bus.c
@@ -712,7 +712,7 @@ int bus_add_driver(struct device_driver *drv)
 	if (drv->bus->p->drivers_autoprobe) {
 		error = driver_attach(drv);
 		if (error)
-			goto out_unregister;
+			goto out_del_list;
 	}
 	module_add_driver(drv->owner, drv);
 
@@ -743,6 +743,8 @@ int bus_add_driver(struct device_driver *drv)
 
 	return 0;
 
+out_del_list:
+	klist_del(&priv->knode_bus);
 out_unregister:
 	kobject_put(&priv->kobj);
 	kfree(drv->p);
