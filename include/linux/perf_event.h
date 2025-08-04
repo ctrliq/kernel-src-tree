de0ad3b736b5a2cc2d8dfd6500f3ae5d5581b1f1
Marcin Wcisło <marcin.wcislo@conclusive.pl>
2025-08-04 21:54:14 +0200

    perf: Disallow mis-matched inherited group reads
    jira VULN-4127
    cve CVE-2023-5717
    commit-author Peter Zijlstra <peterz@infradead.org>
    commit 32671e3799ca2e4590773fd0e63aaa4229e50c06
    upstream-diff The mainline fix 32671e3799ca2e4590773fd0e63aaa4229e50c06
      adds a new `group_generation' field to the `perf_event' struct. This
      breaks LTS 8.6 kABI. The new field was preserved, but moved to the end
      of the struct and wrapped in the `RH_KABI_EXTEND' macro. The kABI in
      this particular case is preserved, as the `perf_event' struct is always
      dynamically allocated through `perf_event_alloc()' and used indirectly
      through a pointer. It's not used as a field in any other struct nor
      as an array element.
    
    Because group consistency is non-atomic between parent (filedesc) and children
    (inherited) events, it is possible for PERF_FORMAT_GROUP read() to try and sum
    non-matching counter groups -- with non-sensical results.
    
    Add group_generation to distinguish the case where a parent group removes and
    adds an event and thus has the same number, but a different configuration of
    events as inherited groups.
    
    This became a problem when commit fa8c269353d5 ("perf/core: Invert
    perf_read_group() loops") flipped the order of child_list and sibling_list.
    Previously it would iterate the group (sibling_list) first, and for each
    sibling traverse the child_list. In this order, only the group composition of
    the parent is relevant. By flipping the order the group composition of the
    child (inherited) events becomes an issue and the mis-match in group
    composition becomes evident.
    
    That said; even prior to this commit, while reading of a group that is not
    equally inherited was not broken, it still made no sense.
    
    (Ab)use ECHILD as error return to indicate issues with child process group
    composition.
    
    Fixes: fa8c269353d5 ("perf/core: Invert perf_read_group() loops")
    	Reported-by: Budimir Markovic <markovicbudimir@gmail.com>
    	Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20231018115654.GK33217@noisy.programming.kicks-ass.net
    (cherry picked from commit 32671e3799ca2e4590773fd0e63aaa4229e50c06)
    	Signed-off-by: Marcin Wcisło <marcin.wcislo@conclusive.pl>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -57,6 +57,7 @@ struct perf_guest_info_callbacks {
 #include <linux/cgroup.h>
 #include <linux/refcount.h>
 #include <linux/security.h>
+#include <linux/rh_kabi.h>
 #include <asm/local.h>
 
 struct perf_callchain_entry {
@@ -800,6 +801,8 @@ struct perf_event {
 	RH_KABI_BROKEN_INSERT(void *security)
 #endif
 	struct list_head		sb_list;
+
+	RH_KABI_EXTEND(unsigned int	group_generation)
 #endif /* CONFIG_PERF_EVENTS */
 };
 
