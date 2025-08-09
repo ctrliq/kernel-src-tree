51e7a19238ca5202a5683d2b4b7930dcd23a65ca
Andreas Gruenbacher <agruenba@redhat.com>
2025-08-09 22:04:00 +0200

    Revert "gfs2: fix infinite loop when checking ail item count before go_inval"
    JIRA: https://issues.redhat.com/browse/RHEL-65700
    Upstream Status: v6.19-rc1
    
    The current withdraw code duplicates the journal recovery code gfs2
    already has for dealing with node failures, and it does so poorly.  That
    code was added because when releasing a lockspace, we didn't have a way
    to indicate that the lockspace needs recovery.  We now do have this
    feature, so the current withdraw code can be removed almost entirely.
    This is one of several steps towards that.
    
    Reverts commit 33dbd1e41a1d ("gfs2: fix infinite loop when checking ail
    item count before go_inval").
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit 655531c95be333cb979fb58f6825ddae40455c13)

--- fs/gfs2/glops.c
+++ fs/gfs2/glops.c
@@ -228,6 +228,7 @@ static void rgrp_go_inval(struct gfs2_glock *gl, int flags)
 	end = PAGE_ALIGN((rgd->rd_addr + rgd->rd_length) * bsize) - 1;
 	gfs2_rgrp_brelse(rgd);
 	WARN_ON_ONCE(!(flags & DIO_METADATA));
+	gfs2_assert_withdraw(sdp, !atomic_read(&gl->gl_ail_count));
 	truncate_inode_pages_range(mapping, start, end);
 	set_bit(GLF_INSTANTIATE_NEEDED, &gl->gl_flags);
 }
@@ -352,6 +353,8 @@ out:
 static void inode_go_inval(struct gfs2_glock *gl, int flags)
 {
 	struct gfs2_inode *ip = gfs2_glock2inode(gl);
+
+	gfs2_assert_withdraw(gl->gl_name.ln_sbd, !atomic_read(&gl->gl_ail_count));
 
 	if (flags & DIO_METADATA) {
 		struct address_space *mapping = gfs2_glock2aspace(gl);
