dd5176f3cfb524c1530011576a14e5a220db05ed
Mark Langsdorf <mlangsdo@redhat.com>
2025-08-18 10:05:03 -0500

    PM: sleep: Update power.completion for all devices on errors
    JIRA: https://issues.redhat.com/browse/RHEL-109251
    
    commit ebd6884167eac94bae9f92793fcd84069d9e4415
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date: Tue, 15 Jul 2025 14:55:11 +0000
    
    After commit aa7a9275ab81 ("PM: sleep: Suspend async parents after
    suspending children"), the following scenario is possible:
    
     1. Device A is async and it depends on device B that is sync.
     2. Async suspend is scheduled for A before the processing of B is
        started.
     3. A is waiting for B.
     4. In the meantime, an unrelated device fails to suspend and returns
        an error.
     5. The processing of B doesn't start at all and its power.completion is
        not updated.
     6. A is still waiting for B when async_synchronize_full() is called.
     7. Deadlock ensues.
    
    To prevent this from happening, update power.completion for all devices
    on errors in all suspend phases, but do not do it directly for devices
    that are already being processed or are waiting for the processing to
    start because in those cases it may be necessary to wait for the
    processing to actually complete before updating power.completion for
    the device.
    
    Fixes: aa7a9275ab81 ("PM: sleep: Suspend async parents after suspending children")
    Fixes: 443046d1ad66 ("PM: sleep: Make suspend of devices more asynchronous")
    Closes: https://lore.kernel.org/linux-pm/e13740a0-88f3-4a6f-920f-15805071a7d6@linaro.org/
    Reported-and-tested-by: Tudor Ambarus <tudor.ambarus@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Link: https://patch.msgid.link/6191258.lOV4Wx5bFT@rjwysocki.net
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1336,6 +1336,22 @@ static void dpm_async_suspend_superior(struct device *dev, async_func_t func)
 	device_links_read_unlock(idx);
 }
 
+static void dpm_async_suspend_complete_all(struct list_head *device_list)
+{
+	struct device *dev;
+
+	guard(mutex)(&async_wip_mtx);
+
+	list_for_each_entry_reverse(dev, device_list, power.entry) {
+		/*
+		 * In case the device is being waited for and async processing
+		 * has not started for it yet, let the waiters make progress.
+		 */
+		if (!dev->power.work_in_progress)
+			complete_all(&dev->power.completion);
+	}
+}
+
 /**
  * resume_event - Return a "resume" message for given "suspend" sleep state.
  * @sleep_state: PM message representing a sleep state.
@@ -1512,6 +1528,7 @@ static int dpm_noirq_suspend_devices(pm_message_t state)
 		mutex_lock(&dpm_list_mtx);
 
 		if (error || async_error) {
+			dpm_async_suspend_complete_all(&dpm_late_early_list);
 			/*
 			 * Move all devices to the target list to resume them
 			 * properly.
@@ -1714,6 +1731,7 @@ int dpm_suspend_late(pm_message_t state)
 		mutex_lock(&dpm_list_mtx);
 
 		if (error || async_error) {
+			dpm_async_suspend_complete_all(&dpm_suspended_list);
 			/*
 			 * Move all devices to the target list to resume them
 			 * properly.
@@ -2007,6 +2025,7 @@ int dpm_suspend(pm_message_t state)
 		mutex_lock(&dpm_list_mtx);
 
 		if (error || async_error) {
+			dpm_async_suspend_complete_all(&dpm_prepared_list);
 			/*
 			 * Move all devices to the target list to resume them
 			 * properly.
