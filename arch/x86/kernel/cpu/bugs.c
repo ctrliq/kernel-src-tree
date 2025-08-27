318a21e6e9d74e0be3634f317ff318d7f89f8382
Waiman Long <longman@redhat.com>
2025-08-27 15:26:12 -0400

    x86/bugs: Remove X86_BUG_MMIO_UNKNOWN
    JIRA: https://issues.redhat.com/browse/RHEL-83893
    Conflicts:
      The md_clear_update_mitigation() hunk is dropped due to upstream
      merge conflict with commit 6f0960a760eb ("x86/bugs: Remove
      md_clear_*_mitigation()").
    
    commit dd86a1d013e0c94fedd060514b9e7be2988ef320
    Author: Borislav Petkov (AMD) <bp@alien8.de>
    Date:   Mon, 14 Apr 2025 17:09:51 +0200
    
        x86/bugs: Remove X86_BUG_MMIO_UNKNOWN
    
        Whack this thing because:
    
        - the "unknown" handling is done only for this vuln and not for the
          others
    
        - it doesn't do anything besides reporting things differently. It
          doesn't apply any mitigations - it is simply causing unnecessary
          complications to the code which don't bring anything besides
          maintenance overhead to what is already a very nasty spaghetti pile
    
        - all the currently unaffected CPUs can also be in "unknown" status so
          there's no need for special handling here
    
        so get rid of it.
    
        Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
        Cc: Andrew Cooper <andrew.cooper3@citrix.com>
        Cc: David Kaplan <david.kaplan@amd.com>
        Cc: H. Peter Anvin <hpa@zytor.com>
        Cc: Josh Poimboeuf <jpoimboe@redhat.com>
        Cc: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
        Link: https://lore.kernel.org/r/20250414150951.5345-1-bp@kernel.org
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -561,7 +561,6 @@ static const char * const mmio_strings[] = {
 static void __init mmio_select_mitigation(void)
 {
 	if (!boot_cpu_has_bug(X86_BUG_MMIO_STALE_DATA) ||
-	     boot_cpu_has_bug(X86_BUG_MMIO_UNKNOWN) ||
 	     cpu_mitigations_off()) {
 		mmio_mitigation = MMIO_MITIGATION_OFF;
 		return;
@@ -3096,9 +3095,6 @@ static ssize_t tsx_async_abort_show_state(char *buf)
 
 static ssize_t mmio_stale_data_show_state(char *buf)
 {
-	if (boot_cpu_has_bug(X86_BUG_MMIO_UNKNOWN))
-		return sysfs_emit(buf, "Unknown: No mitigations\n");
-
 	if (mmio_mitigation == MMIO_MITIGATION_OFF)
 		return sysfs_emit(buf, "%s\n", mmio_strings[mmio_mitigation]);
 
@@ -3288,7 +3284,6 @@ static ssize_t cpu_show_common(struct device *dev, struct device_attribute *attr
 		return srbds_show_state(buf);
 
 	case X86_BUG_MMIO_STALE_DATA:
-	case X86_BUG_MMIO_UNKNOWN:
 		return mmio_stale_data_show_state(buf);
 
 	case X86_BUG_RETBLEED:
@@ -3360,10 +3355,7 @@ ssize_t cpu_show_srbds(struct device *dev, struct device_attribute *attr, char *
 
 ssize_t cpu_show_mmio_stale_data(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	if (boot_cpu_has_bug(X86_BUG_MMIO_UNKNOWN))
-		return cpu_show_common(dev, attr, buf, X86_BUG_MMIO_UNKNOWN);
-	else
-		return cpu_show_common(dev, attr, buf, X86_BUG_MMIO_STALE_DATA);
+	return cpu_show_common(dev, attr, buf, X86_BUG_MMIO_STALE_DATA);
 }
 
 ssize_t cpu_show_retbleed(struct device *dev, struct device_attribute *attr, char *buf)
