9aab74e198e34a93950ffc371326f3c65d2eca9a
Ming Lei <ming.lei@redhat.com>
2025-08-30 10:18:21 +0800

    blk-mq: Defer freeing of tags page_list to SRCU callback
    JIRA: https://issues.redhat.com/browse/RHEL-123480
    
    commit ad0d05dbddc1bf86e92220fea873176de6b12f78
    Author: Ming Lei <ming.lei@redhat.com>
    Date:   Sat Aug 30 10:18:21 2025 +0800
    
        blk-mq: Defer freeing of tags page_list to SRCU callback
    
        Tag iterators can race with the freeing of the request pages(tags->page_list),
        potentially leading to use-after-free issues.
    
        Defer the freeing of the page list and the tags structure itself until
        after an SRCU grace period has passed. This ensures that any concurrent
        tag iterators have completed before the memory is released. With this
        way, we can replace the big tags->lock in tags iterator code path with
        srcu for solving the issue.
    
        This is achieved by:
        - Adding a new `srcu_struct tags_srcu` to `blk_mq_tag_set` to protect
          tag map iteration.
        - Adding an `rcu_head` to `struct blk_mq_tags` to be used with
          `call_srcu`.
        - Moving the page list freeing logic and the `kfree(tags)` call into a
          new callback function, `blk_mq_free_tags_callback`.
        - In `blk_mq_free_tags`, invoking `call_srcu` to schedule the new
          callback for deferred execution.
    
        The read-side protection for the tag iterators will be added in a
        subsequent patch.
    
        Reviewed-by: Hannes Reinecke <hare@suse.de>
        Reviewed-by: Yu Kuai <yukuai3@huawei.com>
        Signed-off-by: Ming Lei <ming.lei@redhat.com>
        Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- include/linux/blk-mq.h
+++ include/linux/blk-mq.h
@@ -542,6 +542,7 @@ struct blk_mq_tag_set {
 	struct mutex		tag_list_lock;
 	struct list_head	tag_list;
 	struct srcu_struct	*srcu;
+	struct srcu_struct	tags_srcu;
 
 	struct rw_semaphore	update_nr_hwq_lock;
 	RH_KABI_RESERVE(1)
@@ -797,6 +798,7 @@ struct blk_mq_tags {
 	 * request pool
 	 */
 	spinlock_t lock;
+	struct rcu_head rcu_head;
 };
 
 static inline struct request *blk_mq_tag_to_rq(struct blk_mq_tags *tags,
