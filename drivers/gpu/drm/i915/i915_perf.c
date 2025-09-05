a0d770f962c110fd17b4279eb9d2966ce4e8ae7f
José Expósito <jexposit@redhat.com>
2025-09-05 15:07:14 +0200

    Revert "drm/i915: Avoid open-coded use of ratelimit_state structure's ->missed field"
    JIRA: https://issues.redhat.com/browse/RHEL-110619
    Upstream Status: RHEL only
    
    This reverts commit 25228c60999f848fdeb291031858de49c115b64e.
    
    This commit was	backported automatically as part of the	DRM backport.
    
    Upstream, using the ratelimit_state_get_miss() function is
    prefered over accesing the ratelimit_state structure's ->missed field.
    Since this kernel doesn't have that function, revert this commit.
    
    NOTE: This commit does not compile due to missing RHEL-specific fix-ups,
    which are split out into separate Git commits following this one.
    
    If git bisect selected this commit, run "git bisect skip" and try again.
    Repeat this until it selects a commit which compiles.
    
    Signed-off-by: José Expósito <jexposit@redhat.com>

--- drivers/gpu/drm/i915/i915_perf.c
+++ drivers/gpu/drm/i915/i915_perf.c
@@ -1666,7 +1666,6 @@ static void i915_oa_stream_destroy(struct i915_perf_stream *stream)
 	struct i915_perf *perf = stream->perf;
 	struct intel_gt *gt = stream->engine->gt;
 	struct i915_perf_group *g = stream->engine->oa_group;
-	int m;
 
 	if (WARN_ON(stream != g->exclusive_stream))
 		return;
@@ -1691,9 +1690,10 @@ static void i915_oa_stream_destroy(struct i915_perf_stream *stream)
 	free_oa_configs(stream);
 	free_noa_wait(stream);
 
-	m = ratelimit_state_get_miss(&perf->spurious_report_rs);
-	if (m)
-		gt_notice(gt, "%d spurious OA report notices suppressed due to ratelimiting\n", m);
+	if (perf->spurious_report_rs.missed) {
+		gt_notice(gt, "%d spurious OA report notices suppressed due to ratelimiting\n",
+			  perf->spurious_report_rs.missed);
+	}
 }
 
 static void gen7_init_oa_buffer(struct i915_perf_stream *stream)
