f6c9daa3e6741ffd6556220ff6d75593508c958a
Phil Auld <pauld@redhat.com>
2025-09-08 13:48:51 +0000

    sched/smp: Use the SMP version of the deadline scheduling class
    JIRA: https://issues.redhat.com/browse/RHEL-112445
    
    commit 6324dce8f6262ec2049494af311e5418bc733341
    Author: Ingo Molnar <mingo@kernel.org>
    Date:   Wed May 28 10:09:10 2025 +0200
    
        sched/smp: Use the SMP version of the deadline scheduling class
    
        Simplify the scheduler by making CONFIG_SMP=y code
        in prio_changed_dl() unconditional.
    
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
        Acked-by: Peter Zijlstra <peterz@infradead.org>
        Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
        Cc: Juri Lelli <juri.lelli@redhat.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Mel Gorman <mgorman@suse.de>
        Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
        Cc: Shrikanth Hegde <sshegde@linux.ibm.com>
        Cc: Steven Rostedt <rostedt@goodmis.org>
        Cc: Valentin Schneider <vschneid@redhat.com>
        Cc: Vincent Guittot <vincent.guittot@linaro.org>
        Link: https://lore.kernel.org/r/20250528080924.2273858-30-mingo@kernel.org
    
    Signed-off-by: Phil Auld <pauld@redhat.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -3043,7 +3043,6 @@ static void prio_changed_dl(struct rq *rq, struct task_struct *p,
 	if (!task_on_rq_queued(p))
 		return;
 
-#ifdef CONFIG_SMP
 	/*
 	 * This might be too much, but unfortunately
 	 * we don't have the old deadline value, and
@@ -3072,13 +3071,6 @@ static void prio_changed_dl(struct rq *rq, struct task_struct *p,
 		    dl_time_before(p->dl.deadline, rq->curr->dl.deadline))
 			resched_curr(rq);
 	}
-#else /* !CONFIG_SMP: */
-	/*
-	 * We don't know if p has a earlier or later deadline, so let's blindly
-	 * set a (maybe not needed) rescheduling point.
-	 */
-	resched_curr(rq);
-#endif /* !CONFIG_SMP */
 }
 
 #ifdef CONFIG_SCHED_CORE
