89d5a145b66cdbfe16339e12dccc0fd57c324756
Phil Auld <pauld@redhat.com>
2025-09-09 14:27:21 +0000

    Revert "sched/numa: add statistics of numa balance task"
    JIRA: https://issues.redhat.com/browse/RHEL-110301
    
    commit db6cc3f4ac2e6cdc898fc9cbc8b32ae1bf56bdad
    Author: Chen Yu <yu.c.chen@intel.com>
    Date:   Fri Jul 4 21:56:20 2025 +0800
    
        Revert "sched/numa: add statistics of numa balance task"
    
        This reverts commit ad6b26b6a0a79166b53209df2ca1cf8636296382.
    
        This commit introduces per-memcg/task NUMA balance statistics, but
        unfortunately it introduced a NULL pointer exception due to the following
        race condition: After a swap task candidate was chosen, its mm_struct
        pointer was set to NULL due to task exit.  Later, when performing the
        actual task swapping, the p->mm caused the problem.
    
        CPU0                                   CPU1
        :
        ...
        task_numa_migrate
             task_numa_find_cpu
              task_numa_compare
                # a normal task p is chosen
                env->best_task = p
    
                                                  # p exit:
                                                  exit_signals(p);
                                                     p->flags |= PF_EXITING
                                                  exit_mm
                                                     p->mm = NULL;
    
              migrate_swap_stop
                __migrate_swap_task((arg->src_task, arg->dst_cpu)
                 count_memcg_event_mm(p->mm, NUMA_TASK_SWAP)# p->mm is NULL
    
        task_lock() should be held and the PF_EXITING flag needs to be checked to
        prevent this from happening.  After discussion, the conclusion was that
        adding a lock is not worthwhile for some statistics calculations.  Revert
        the change and rely on the tracepoint for this purpose.
    
        Link: https://lkml.kernel.org/r/20250704135620.685752-1-yu.c.chen@intel.com
        Link: https://lkml.kernel.org/r/20250708064917.BBD13C4CEED@smtp.kernel.org
        Fixes: ad6b26b6a0a7 ("sched/numa: add statistics of numa balance task")
        Signed-off-by: Chen Yu <yu.c.chen@intel.com>
        Reported-by: Jirka Hladky <jhladky@redhat.com>
        Closes: https://lore.kernel.org/all/CAE4VaGBLJxpd=NeRJXpSCuw=REhC5LWJpC29kDy-Zh2ZDyzQZA@mail.gmail.com/
        Reported-by: Srikanth Aithal <Srikanth.Aithal@amd.com>
        Reported-by: Suneeth D <Suneeth.D@amd.com>
        Acked-by: Michal Hocko <mhocko@suse.com>
        Cc: Borislav Petkov <bp@alien8.de>
        Cc: Ingo Molnar <mingo@redhat.com>
        Cc: Jiri Hladky <jhladky@redhat.com>
        Cc: Libo Chen <libo.chen@oracle.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Phil Auld <pauld@redhat.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1340,8 +1340,6 @@ const char * const vmstat_text[] = {
 	"numa_hint_faults",
 	"numa_hint_faults_local",
 	"numa_pages_migrated",
-	"numa_task_migrated",
-	"numa_task_swapped",
 #endif
 #ifdef CONFIG_MIGRATION
 	"pgmigrate_success",
