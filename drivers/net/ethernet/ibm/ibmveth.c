71e4dcafec08e17a06f09e36d9400b4136185384
Jonathan Maple <jmaple@ciq.com>
2025-09-16 18:29:06 -0400

    ibmveth: Always stop tx queues during close
    jira LE-4159
    Rebuild_History Non-Buildable kernel-5.14.0-570.41.1.el9_6
    commit-author Nick Child <nnac123@linux.ibm.com>
    commit 127b7218bfdd60205cfe2fa3f06d95e85a2650ed
    
    netif_stop_all_queues must be called before calling H_FREE_LOGICAL_LAN.
    As a result, we can remove the pool_config field from the ibmveth
    adapter structure.
    
    Some device configuration changes call ibmveth_close in order to free
    the current resources held by the device. These functions then make
    their changes and call ibmveth_open to reallocate and reserve resources
    for the device.
    
    Prior to this commit, the flag pool_config was used to tell ibmveth_close
    that it should not halt the transmit queue. pool_config was introduced in
    commit 860f242eb534 ("[PATCH] ibmveth change buffer pools dynamically")
    to avoid interrupting the tx flow when making rx config changes. Since
    then, other commits adopted this approach, even if making tx config
    changes.
    
    The issue with this approach was that the hypervisor freed all of
    the devices control structures after the hcall H_FREE_LOGICAL_LAN
    was performed but the transmit queues were never stopped. So the higher
    layers in the network stack would continue transmission but any
    H_SEND_LOGICAL_LAN hcall would fail with H_PARAMETER until the
    hypervisor's structures for the device were allocated with the
    H_REGISTER_LOGICAL_LAN hcall in ibmveth_open. This resulted in
    no real networking harm but did cause several of these error
    messages to be logged: "h_send_logical_lan failed with rc=-4"
    
    So, instead of trying to keep the transmit queues alive during network
    configuration changes, just stop the queues, make necessary changes then
    restart the queues.
    
    	Signed-off-by: Nick Child <nnac123@linux.ibm.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 127b7218bfdd60205cfe2fa3f06d95e85a2650ed)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/net/ethernet/ibm/ibmveth.c
+++ drivers/net/ethernet/ibm/ibmveth.c
@@ -667,8 +667,7 @@ static int ibmveth_close(struct net_device *netdev)
 
 	napi_disable(&adapter->napi);
 
-	if (!adapter->pool_config)
-		netif_tx_stop_all_queues(netdev);
+	netif_tx_stop_all_queues(netdev);
 
 	h_vio_signal(adapter->vdev->unit_address, VIO_IRQ_DISABLE);
 
@@ -776,9 +775,7 @@ static int ibmveth_set_csum_offload(struct net_device *dev, u32 data)
 
 	if (netif_running(dev)) {
 		restart = 1;
-		adapter->pool_config = 1;
 		ibmveth_close(dev);
-		adapter->pool_config = 0;
 	}
 
 	set_attr = 0;
@@ -860,9 +857,7 @@ static int ibmveth_set_tso(struct net_device *dev, u32 data)
 
 	if (netif_running(dev)) {
 		restart = 1;
-		adapter->pool_config = 1;
 		ibmveth_close(dev);
-		adapter->pool_config = 0;
 	}
 
 	set_attr = 0;
@@ -1512,9 +1507,7 @@ static int ibmveth_change_mtu(struct net_device *dev, int new_mtu)
 	   only the buffer pools necessary to hold the new MTU */
 	if (netif_running(adapter->netdev)) {
 		need_restart = 1;
-		adapter->pool_config = 1;
 		ibmveth_close(adapter->netdev);
-		adapter->pool_config = 0;
 	}
 
 	/* Look for an active buffer pool that can hold the new MTU */
@@ -1678,7 +1671,6 @@ static int ibmveth_probe(struct vio_dev *dev, const struct vio_device_id *id)
 	adapter->vdev = dev;
 	adapter->netdev = netdev;
 	adapter->mcastFilterSize = be32_to_cpu(*mcastFilterSize_p);
-	adapter->pool_config = 0;
 	ibmveth_init_link_settings(netdev);
 
 	netif_napi_add_weight(netdev, &adapter->napi, ibmveth_poll, 16);
@@ -1819,9 +1811,7 @@ static ssize_t veth_pool_store(struct kobject *kobj, struct attribute *attr,
 					return -ENOMEM;
 				}
 				pool->active = 1;
-				adapter->pool_config = 1;
 				ibmveth_close(netdev);
-				adapter->pool_config = 0;
 				if ((rc = ibmveth_open(netdev)))
 					return rc;
 			} else {
@@ -1847,10 +1837,8 @@ static ssize_t veth_pool_store(struct kobject *kobj, struct attribute *attr,
 			}
 
 			if (netif_running(netdev)) {
-				adapter->pool_config = 1;
 				ibmveth_close(netdev);
 				pool->active = 0;
-				adapter->pool_config = 0;
 				if ((rc = ibmveth_open(netdev)))
 					return rc;
 			}
@@ -1861,9 +1849,7 @@ static ssize_t veth_pool_store(struct kobject *kobj, struct attribute *attr,
 			return -EINVAL;
 		} else {
 			if (netif_running(netdev)) {
-				adapter->pool_config = 1;
 				ibmveth_close(netdev);
-				adapter->pool_config = 0;
 				pool->size = value;
 				if ((rc = ibmveth_open(netdev)))
 					return rc;
@@ -1876,9 +1862,7 @@ static ssize_t veth_pool_store(struct kobject *kobj, struct attribute *attr,
 			return -EINVAL;
 		} else {
 			if (netif_running(netdev)) {
-				adapter->pool_config = 1;
 				ibmveth_close(netdev);
-				adapter->pool_config = 0;
 				pool->buff_size = value;
 				if ((rc = ibmveth_open(netdev)))
 					return rc;
