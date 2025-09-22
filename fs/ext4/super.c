0e2c8c19c76ea2d949706f9bf99a3ef3e99b2d19
Brett Mastbergen <bmastbergen@ciq.com>
2025-09-22 16:02:31 -0400

    ext4: avoid journaling sb update on error if journal is destroying
    jira VULN-65357
    cve CVE-2025-22113
    commit-author Ojaswin Mujoo <ojaswin@linux.ibm.com>
    commit ce2f26e73783b4a7c46a86e3af5b5c8de0971790
    
    Presently we always BUG_ON if trying to start a transaction on a journal marked
    with JBD2_UNMOUNT, since this should never happen. However, while ltp running
    stress tests, it was observed that in case of some error handling paths, it is
    possible for update_super_work to start a transaction after the journal is
    destroyed eg:
    
    (umount)
    ext4_kill_sb
      kill_block_super
        generic_shutdown_super
          sync_filesystem /* commits all txns */
          evict_inodes
            /* might start a new txn */
          ext4_put_super
    	flush_work(&sbi->s_sb_upd_work) /* flush the workqueue */
            jbd2_journal_destroy
              journal_kill_thread
                journal->j_flags |= JBD2_UNMOUNT;
              jbd2_journal_commit_transaction
                jbd2_journal_get_descriptor_buffer
                  jbd2_journal_bmap
                    ext4_journal_bmap
                      ext4_map_blocks
                        ...
                        ext4_inode_error
                          ext4_handle_error
                            schedule_work(&sbi->s_sb_upd_work)
    
                                                   /* work queue kicks in */
                                                   update_super_work
                                                     jbd2_journal_start
                                                       start_this_handle
                                                         BUG_ON(journal->j_flags &
                                                                JBD2_UNMOUNT)
    
    Hence, introduce a new mount flag to indicate journal is destroying and only do
    a journaled (and deferred) update of sb if this flag is not set. Otherwise, just
    fallback to an un-journaled commit.
    
    Further, in the journal destroy path, we have the following sequence:
    
      1. Set mount flag indicating journal is destroying
      2. force a commit and wait for it
      3. flush pending sb updates
    
    This sequence is important as it ensures that, after this point, there is no sb
    update that might be journaled so it is safe to update the sb outside the
    journal. (To avoid race discussed in 2d01ddc86606)
    
    Also, we don't need a similar check in ext4_grp_locked_error since it is only
    called from mballoc and AFAICT it would be always valid to schedule work here.
    
    Fixes: 2d01ddc86606 ("ext4: save error info to sb through journal if available")
    	Reported-by: Mahesh Kumar <maheshkumar657g@gmail.com>
    	Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    	Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/9613c465d6ff00cd315602f99283d5f24018c3f7.1742279837.git.ojaswin@linux.ibm.com
    	Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    (cherry picked from commit ce2f26e73783b4a7c46a86e3af5b5c8de0971790)
    	Signed-off-by: Brett Mastbergen <bmastbergen@ciq.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -668,9 +668,13 @@ static void ext4_handle_error(struct super_block *sb, bool force_ro, int error,
 		 * In case the fs should keep running, we need to writeout
 		 * superblock through the journal. Due to lock ordering
 		 * constraints, it may not be safe to do it right here so we
-		 * defer superblock flushing to a workqueue.
+		 * defer superblock flushing to a workqueue. We just need to be
+		 * careful when the journal is already shutting down. If we get
+		 * here in that case, just update the sb directly as the last
+		 * transaction won't commit anyway.
 		 */
-		if (continue_fs && journal)
+		if (continue_fs && journal &&
+		    !ext4_test_mount_flag(sb, EXT4_MF_JOURNAL_DESTROY))
 			schedule_work(&EXT4_SB(sb)->s_sb_upd_work);
 		else
 			ext4_commit_super(sb);
@@ -1203,7 +1207,6 @@ static void ext4_put_super(struct super_block *sb)
 	ext4_unregister_li_request(sb);
 	ext4_quota_off_umount(sb);
 
-	flush_work(&sbi->s_sb_upd_work);
 	destroy_workqueue(sbi->rsv_conversion_wq);
 	ext4_release_orphan_info(sb);
 
@@ -1213,7 +1216,8 @@ static void ext4_put_super(struct super_block *sb)
 		if ((err < 0) && !aborted) {
 			ext4_abort(sb, -err, "Couldn't clean up the journal");
 		}
-	}
+	} else
+		flush_work(&sbi->s_sb_upd_work);
 
 	ext4_es_unregister_shrinker(sbi);
 	del_timer_sync(&sbi->s_err_report);
@@ -4891,8 +4895,6 @@ static int ext4_load_and_init_journal(struct super_block *sb,
 	return 0;
 
 out:
-	/* flush s_sb_upd_work before destroying the journal. */
-	flush_work(&sbi->s_sb_upd_work);
 	ext4_journal_destroy(sbi, sbi->s_journal);
 	return -EINVAL;
 }
@@ -5599,8 +5601,6 @@ failed_mount_wq:
 	sbi->s_ea_block_cache = NULL;
 
 	if (sbi->s_journal) {
-		/* flush s_sb_upd_work before journal destroy. */
-		flush_work(&sbi->s_sb_upd_work);
 		ext4_journal_destroy(sbi, sbi->s_journal);
 	}
 failed_mount3a:
