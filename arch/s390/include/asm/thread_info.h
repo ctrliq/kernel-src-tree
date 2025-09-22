0da1424f937a66cbcc61584532c9b83621db86af
Thomas Huth <thuth@redhat.com>
2025-09-22 17:37:39 +0200

    s390: Remove 2k vs 4k page table leftovers
    JIRA: https://issues.redhat.com/browse/RHEL-113452
    
    commit 174cb82a5761f15f2c469672452b720d9f51cbd7
    Author: Heiko Carstens <hca@linux.ibm.com>
    Date:   Mon Mar 10 12:43:00 2025 +0100
    
        s390: Remove 2k vs 4k page table leftovers
    
        Since commit d08d4e7cd6bf ("s390/mm: use full 4KB page for 2KB PTE") always
        4k page tables are allocated, however there is still some (now) obsolete
        code left which deals with switching from 2k to 4k page tables for qemu/kvm
        processes.
    
        Reviewed-by: Alexander Gordeev <agordeev@linux.ibm.com>
        Remove the not needed code.
    
        Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
        Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- arch/s390/include/asm/thread_info.h
+++ arch/s390/include/asm/thread_info.h
@@ -65,7 +65,6 @@ void arch_setup_new_exec(void);
 #define TIF_UPROBE		3	/* breakpointed or single-stepping */
 #define TIF_GUARDED_STORAGE	4	/* load guarded storage control block */
 #define TIF_PATCH_PENDING	5	/* pending live patching update */
-#define TIF_PGSTE		6	/* New mm's will use 4K page tables */
 #define TIF_NOTIFY_SIGNAL	7	/* signal notifications exist */
 #define TIF_ISOLATE_BP_GUEST	9	/* Run KVM guests with isolated BP */
 #define TIF_PER_TRAP		10	/* Need to handle PER trap on exit to usermode */
@@ -86,7 +85,6 @@ void arch_setup_new_exec(void);
 #define _TIF_UPROBE		BIT(TIF_UPROBE)
 #define _TIF_GUARDED_STORAGE	BIT(TIF_GUARDED_STORAGE)
 #define _TIF_PATCH_PENDING	BIT(TIF_PATCH_PENDING)
-#define _TIF_PGSTE		BIT(TIF_PGSTE)
 #define _TIF_NOTIFY_SIGNAL	BIT(TIF_NOTIFY_SIGNAL)
 #define _TIF_ISOLATE_BP_GUEST	BIT(TIF_ISOLATE_BP_GUEST)
 #define _TIF_PER_TRAP		BIT(TIF_PER_TRAP)
