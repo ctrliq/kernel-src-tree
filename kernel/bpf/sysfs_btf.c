695ebb2527fc96f852ac9d55af832582c7e64754
Viktor Malik <vmalik@redhat.com>
2025-09-24 11:35:52 +0200

    btf: Allow mmap of vmlinux btf
    JIRA: https://issues.redhat.com/browse/RHEL-78203
    
    Conflicts: Took one chunk (setting of `bin_attr_btf_vmlinux.private`)
               from 42369b9a1ecf ("btf: Switch vmlinux BTF attribute to
               sysfs_bin_attr_simple_read()") since it is required for this
               patch. Not taking the entire 42369b9a1ecf since it is
               accompanied by a number of tree-wide changes in sysfs. Also a
               context change due to missing 42369b9a1ecf.
    
    commit a539e2a6d51d1c12d89eec149ccc72ec561639bc
    Author: Lorenz Bauer <lmb@isovalent.com>
    Date:   Tue May 20 14:01:17 2025 +0100
    
        btf: Allow mmap of vmlinux btf
    
        User space needs access to kernel BTF for many modern features of BPF.
        Right now each process needs to read the BTF blob either in pieces or
        as a whole. Allow mmaping the sysfs file so that processes can directly
        access the memory allocated for it in the kernel.
    
        remap_pfn_range is used instead of vm_insert_page due to aarch64
        compatibility issues.
    
        Signed-off-by: Lorenz Bauer <lmb@isovalent.com>
        Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
        Tested-by: Alan Maguire <alan.maguire@oracle.com>
        Reviewed-by: Shakeel Butt <shakeel.butt@linux.dev>
        Link: https://lore.kernel.org/bpf/20250520-vmlinux-mmap-v5-1-e8c941acc414@isovalent.com
    
    Signed-off-by: Viktor Malik <vmalik@redhat.com>

--- kernel/bpf/sysfs_btf.c
+++ kernel/bpf/sysfs_btf.c
@@ -7,6 +7,9 @@
 #include <linux/kobject.h>
 #include <linux/init.h>
 #include <linux/sysfs.h>
+#include <linux/mm.h>
+#include <linux/io.h>
+#include <linux/btf.h>
 
 /* See scripts/link-vmlinux.sh, gen_btf() func for details */
 extern char __start_BTF[];
@@ -21,15 +24,46 @@ btf_vmlinux_read(struct file *file, struct kobject *kobj,
 	return len;
 }
 
+static int btf_sysfs_vmlinux_mmap(struct file *filp, struct kobject *kobj,
+				  const struct bin_attribute *attr,
+				  struct vm_area_struct *vma)
+{
+	unsigned long pages = PAGE_ALIGN(attr->size) >> PAGE_SHIFT;
+	size_t vm_size = vma->vm_end - vma->vm_start;
+	phys_addr_t addr = virt_to_phys(__start_BTF);
+	unsigned long pfn = addr >> PAGE_SHIFT;
+
+	if (attr->private != __start_BTF || !PAGE_ALIGNED(addr))
+		return -EINVAL;
+
+	if (vma->vm_pgoff)
+		return -EINVAL;
+
+	if (vma->vm_flags & (VM_WRITE | VM_EXEC | VM_MAYSHARE))
+		return -EACCES;
+
+	if (pfn + pages < pfn)
+		return -EINVAL;
+
+	if ((vm_size >> PAGE_SHIFT) > pages)
+		return -EINVAL;
+
+	vm_flags_mod(vma, VM_DONTDUMP, VM_MAYEXEC | VM_MAYWRITE);
+	return remap_pfn_range(vma, vma->vm_start, pfn, vm_size, vma->vm_page_prot);
+}
+
+
 static struct bin_attribute bin_attr_btf_vmlinux __ro_after_init = {
 	.attr = { .name = "vmlinux", .mode = 0444, },
 	.read = btf_vmlinux_read,
+	.mmap = btf_sysfs_vmlinux_mmap,
 };
 
 struct kobject *btf_kobj;
 
 static int __init btf_vmlinux_init(void)
 {
+	bin_attr_btf_vmlinux.private = __start_BTF;
 	bin_attr_btf_vmlinux.size = __stop_BTF - __start_BTF;
 
 	if (bin_attr_btf_vmlinux.size == 0)
