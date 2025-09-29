021246e37ca7fa30481751c32fee1d52f19fba91
Jerry Snitselaar <jsnitsel@redhat.com>
2025-09-28 21:48:53 -0700

    dmaengine: ti: k3-udma: Enable second resource range for BCDMA and PKTDMA
    JIRA: https://issues.redhat.com/browse/RHEL-114133
    Upstream-Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    commit 566beb347eded7a860511164a7a163bc882dc4d0
    Author: Siddharth Vadapalli <s-vadapalli@ti.com>
    Date: Wed Feb 5 17:48:01 2025 +0530
    
        dmaengine: ti: k3-udma: Enable second resource range for BCDMA and PKTDMA
    
        The SoC DMA resources for UDMA, BCDMA and PKTDMA can be described via a
        combination of up to two resource ranges. The first resource range handles
        the default partitioning wherein all resources belonging to that range are
        allocated to a single entity and form a continuous range. For use-cases
        where the resources are shared across multiple entities and require to be
        described via discontinuous ranges, a second resource range is required.
    
        Currently, udma_setup_resources() supports handling resources that belong
        to the second range. Extend bcdma_setup_resources() and
        pktdma_setup_resources() to support the same.
    
        Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>
        Acked-by: Peter Ujfalusi <peter.ujfalusi@gmail.com>
        Link: https://lore.kernel.org/r/20250205121805.316792-1-s-vadapalli@ti.com
        Signed-off-by: Vinod Koul <vkoul@kernel.org>
    
    (cherry picked from commit 566beb347eded7a860511164a7a163bc882dc4d0)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- drivers/dma/ti/k3-udma.c
+++ drivers/dma/ti/k3-udma.c
@@ -4885,6 +4885,12 @@ static int bcdma_setup_resources(struct udma_dev *ud)
 				irq_res.desc[i].start = rm_res->desc[i].start +
 							oes->bcdma_bchan_ring;
 				irq_res.desc[i].num = rm_res->desc[i].num;
+
+				if (rm_res->desc[i].num_sec) {
+					irq_res.desc[i].start_sec = rm_res->desc[i].start_sec +
+									oes->bcdma_bchan_ring;
+					irq_res.desc[i].num_sec = rm_res->desc[i].num_sec;
+				}
 			}
 		}
 	} else {
@@ -4908,6 +4914,15 @@ static int bcdma_setup_resources(struct udma_dev *ud)
 				irq_res.desc[i + 1].start = rm_res->desc[j].start +
 							oes->bcdma_tchan_ring;
 				irq_res.desc[i + 1].num = rm_res->desc[j].num;
+
+				if (rm_res->desc[j].num_sec) {
+					irq_res.desc[i].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_tchan_data;
+					irq_res.desc[i].num_sec = rm_res->desc[j].num_sec;
+					irq_res.desc[i + 1].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_tchan_ring;
+					irq_res.desc[i + 1].num_sec = rm_res->desc[j].num_sec;
+				}
 			}
 		}
 	}
@@ -4928,6 +4943,15 @@ static int bcdma_setup_resources(struct udma_dev *ud)
 				irq_res.desc[i + 1].start = rm_res->desc[j].start +
 							oes->bcdma_rchan_ring;
 				irq_res.desc[i + 1].num = rm_res->desc[j].num;
+
+				if (rm_res->desc[j].num_sec) {
+					irq_res.desc[i].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_rchan_data;
+					irq_res.desc[i].num_sec = rm_res->desc[j].num_sec;
+					irq_res.desc[i + 1].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_rchan_ring;
+					irq_res.desc[i + 1].num_sec = rm_res->desc[j].num_sec;
+				}
 			}
 		}
 	}
@@ -5062,6 +5086,12 @@ static int pktdma_setup_resources(struct udma_dev *ud)
 			irq_res.desc[i].start = rm_res->desc[i].start +
 						oes->pktdma_tchan_flow;
 			irq_res.desc[i].num = rm_res->desc[i].num;
+
+			if (rm_res->desc[i].num_sec) {
+				irq_res.desc[i].start_sec = rm_res->desc[i].start_sec +
+								oes->pktdma_tchan_flow;
+				irq_res.desc[i].num_sec = rm_res->desc[i].num_sec;
+			}
 		}
 	}
 	rm_res = tisci_rm->rm_ranges[RM_RANGE_RFLOW];
@@ -5073,6 +5103,12 @@ static int pktdma_setup_resources(struct udma_dev *ud)
 			irq_res.desc[i].start = rm_res->desc[j].start +
 						oes->pktdma_rchan_flow;
 			irq_res.desc[i].num = rm_res->desc[j].num;
+
+			if (rm_res->desc[j].num_sec) {
+				irq_res.desc[i].start_sec = rm_res->desc[j].start_sec +
+								oes->pktdma_rchan_flow;
+				irq_res.desc[i].num_sec = rm_res->desc[j].num_sec;
+			}
 		}
 	}
 	ret = ti_sci_inta_msi_domain_alloc_irqs(ud->dev, &irq_res);
