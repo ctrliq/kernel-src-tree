e7653b58e605a9e09a6d27f23302a2741ce92284
Eder Zulian <ezulian@redhat.com>
2025-10-02 10:34:19 +0200

    iommu/pages: Remove the order argument to iommu_free_pages()
    JIRA: https://issues.redhat.com/browse/RHEL-116573
    
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    Conflicts:
     - Modify/delete conflict in drivers/iommu/riscv/iommu.c. The file does
       not exist in the downstream repository, but it is modified by
       upstream commit 4316ba4a5033 ("iommu/pages: Remove the order argument
       to iommu_free_pages()")
    
    commit 4316ba4a50331a963a8c76c7716cd7a4646aa385
    Author: Jason Gunthorpe <jgg@ziepe.ca>
    Date:   Tue Apr 8 13:53:53 2025 -0300
    
        iommu/pages: Remove the order argument to iommu_free_pages()
    
        Now that we have a folio under the allocation iommu_free_pages() can know
        the order of the original allocation and do the correct thing to free it.
    
        The next patch will rename iommu_free_page() to iommu_free_pages() so we
        have naming consistency with iommu_alloc_pages_node().
    
        Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
        Reviewed-by: Mostafa Saleh <smostafa@google.com>
        Tested-by: Nicolin Chen <nicolinc@nvidia.com>
        Tested-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
        Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
        Link: https://lore.kernel.org/r/5-v4-c8663abbb606+3f7-iommu_pages_jgg@nvidia.com
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Signed-off-by: Eder Zulian <ezulian@redhat.com>

--- drivers/iommu/io-pgtable-arm.c
+++ drivers/iommu/io-pgtable-arm.c
@@ -299,7 +299,7 @@ out_free:
 	if (cfg->free)
 		cfg->free(cookie, pages, size);
 	else
-		iommu_free_pages(pages, order);
+		iommu_free_pages(pages);
 
 	return NULL;
 }
@@ -315,7 +315,7 @@ static void __arm_lpae_free_pages(void *pages, size_t size,
 	if (cfg->free)
 		cfg->free(cookie, pages, size);
 	else
-		iommu_free_pages(pages, get_order(size));
+		iommu_free_pages(pages);
 }
 
 static void __arm_lpae_sync_pte(arm_lpae_iopte *ptep, int num_entries,
