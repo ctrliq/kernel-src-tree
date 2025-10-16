ffaf54e62ab52c064f1dc2ab9aac75fd201da27c
Jaroslav Kysela <jkysela@redhat.com>
2025-10-16 09:58:28 +0200

    Revert "ASoC: amd: yc: Add DMI quirk for HP Laptop 17 cp-2033dx"
    JIRA: https://issues.redhat.com/browse/RHEL-118909
    Upstream-status: RHEL-only
    
    This change will be reapplied to renamed file (sound/hda/codecs).
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -602,13 +602,6 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 		.driver_data = &acp6x_card,
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "HP"),
-			DMI_MATCH(DMI_BOARD_NAME, "8A81"),
-		}
-	},
-	{
-		.driver_data = &acp6x_card,
-		.matches = {
-			DMI_MATCH(DMI_BOARD_VENDOR, "HP"),
 			DMI_MATCH(DMI_BOARD_NAME, "8B27"),
 		}
 	},
