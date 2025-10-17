90fd511ec9f259177e68681042919fff30718c19
Maxim Levitsky <mlevitsk@redhat.com>
2025-10-17 16:02:30 -0400

    KVM: SVM: Tag MSR bitmap initialization helpers with __init
    JIRA: https://issues.redhat.com/browse/RHEL-120168
    
    commit f886515f9ba23a4cc65e1bc7afeb5177e92c8936
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Tue Jun 10 15:57:09 2025 -0700
    
        KVM: SVM: Tag MSR bitmap initialization helpers with __init
    
        Tag init_msrpm_offsets() and add_msr_offset() with __init, as they're used
        only during hardware setup to map potential passthrough MSRs to offsets in
        the bitmap.
    
        Reviewed-by: Chao Gao <chao.gao@intel.com>
        Link: https://lore.kernel.org/r/20250610225737.156318-5-seanjc@google.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -945,7 +945,7 @@ static void svm_msr_filter_changed(struct kvm_vcpu *vcpu)
 	}
 }
 
-static int add_msr_offset(u32 offset)
+static __init int add_msr_offset(u32 offset)
 {
 	int i;
 
@@ -968,7 +968,7 @@ static int add_msr_offset(u32 offset)
 	return -ENOSPC;
 }
 
-static int init_msrpm_offsets(void)
+static __init int init_msrpm_offsets(void)
 {
 	int i;
 
