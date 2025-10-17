b00cd97d5f509a7dd745d55361566a3b3cd9ef80
Maxim Levitsky <mlevitsk@redhat.com>
2025-10-17 16:07:55 -0400

    KVM: SVM: Rename init_vmcb_after_set_cpuid() to make it intercepts specific
    JIRA: https://issues.redhat.com/browse/RHEL-120168
    
    commit 049dff172b6dde91649d65dbdad62e62f4855a97
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Tue Jun 10 15:57:26 2025 -0700
    
        KVM: SVM: Rename init_vmcb_after_set_cpuid() to make it intercepts specific
    
        Rename init_vmcb_after_set_cpuid() to svm_recalc_intercepts_after_set_cpuid()
        to more precisely describe its role.  Strictly speaking, the name isn't
        perfect as toggling virtual VM{LOAD,SAVE} is arguably not recalculating an
        intercept, but practically speaking it's close enough.
    
        No functional change intended.
    
        Link: https://lore.kernel.org/r/20250610225737.156318-22-seanjc@google.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -1103,7 +1103,7 @@ static void svm_recalc_instruction_intercepts(struct kvm_vcpu *vcpu,
 	}
 }
 
-static inline void init_vmcb_after_set_cpuid(struct kvm_vcpu *vcpu)
+static void svm_recalc_intercepts_after_set_cpuid(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_svm *svm = to_svm(vcpu);
 
@@ -1269,7 +1269,8 @@ static void init_vmcb(struct kvm_vcpu *vcpu)
 		sev_init_vmcb(svm);
 
 	svm_hv_init_vmcb(vmcb);
-	init_vmcb_after_set_cpuid(vcpu);
+
+	svm_recalc_intercepts_after_set_cpuid(vcpu);
 
 	vmcb_mark_all_dirty(vmcb);
 
@@ -4521,7 +4522,7 @@ static void svm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
 	if (sev_guest(vcpu->kvm))
 		sev_vcpu_after_set_cpuid(svm);
 
-	init_vmcb_after_set_cpuid(vcpu);
+	svm_recalc_intercepts_after_set_cpuid(vcpu);
 }
 
 static bool svm_has_wbinvd_exit(void)
