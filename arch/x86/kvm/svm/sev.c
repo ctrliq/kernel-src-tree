c1e451ef45c92d4b5fad0b874bb6c65793034577
Maxim Levitsky <mlevitsk@redhat.com>
2025-10-22 18:59:11 -0400

    KVM: SEV: Save the SEV policy if and only if LAUNCH_START succeeds
    JIRA: https://issues.redhat.com/browse/RHEL-120168
    
    commit 2f5f8fb9de095e9b255a89269827f1761c714690
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Thu Aug 21 14:38:41 2025 -0700
    
        KVM: SEV: Save the SEV policy if and only if LAUNCH_START succeeds
    
        Wait until LAUNCH_START fully succeeds to set a VM's SEV/SNP policy so
        that KVM doesn't keep a potentially stale policy.  In practice, the issue
        is benign as the policy is only used to detect if the VMSA can be
        decrypted, and the VMSA only needs to be decrypted if LAUNCH_UPDATE and
        thus LAUNCH_START succeeded.
    
        Fixes: 962e2b6152ef ("KVM: SVM: Decrypt SEV VMSA in dump_vmcb() if debugging is enabled")
        Cc: Tom Lendacky <thomas.lendacky@amd.com>
        Cc: Kim Phillips <kim.phillips@amd.com>
        Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
        Link: https://lore.kernel.org/r/20250821213841.3462339-1-seanjc@google.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Conflicts:
        context conflict due to missing '[PATCH v11 0/8] KVM: SVM: Enable Secure TSC for SEV-SNP'
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -573,8 +573,6 @@ static int sev_launch_start(struct kvm *kvm, struct kvm_sev_cmd *argp)
 	if (copy_from_user(&params, u64_to_user_ptr(argp->data), sizeof(params)))
 		return -EFAULT;
 
-	sev->policy = params.policy;
-
 	memset(&start, 0, sizeof(start));
 
 	dh_blob = NULL;
@@ -622,6 +620,7 @@ static int sev_launch_start(struct kvm *kvm, struct kvm_sev_cmd *argp)
 		goto e_free_session;
 	}
 
+	sev->policy = params.policy;
 	sev->handle = start.handle;
 	sev->fd = argp->sev_fd;
 
@@ -2188,8 +2187,6 @@ static int snp_launch_start(struct kvm *kvm, struct kvm_sev_cmd *argp)
 	if (!(params.policy & SNP_POLICY_MASK_RSVD_MBO))
 		return -EINVAL;
 
-	sev->policy = params.policy;
-
 	sev->snp_context = snp_context_create(kvm, argp);
 	if (!sev->snp_context)
 		return -ENOTTY;
@@ -2204,6 +2201,7 @@ static int snp_launch_start(struct kvm *kvm, struct kvm_sev_cmd *argp)
 		goto e_free_context;
 	}
 
+	sev->policy = params.policy;
 	sev->fd = argp->sev_fd;
 	rc = snp_bind_asid(kvm, &argp->error);
 	if (rc) {
