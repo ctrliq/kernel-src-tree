0e9481f86f4d220cf62c2e63bf3623d67d25cf0f
Mark Salter <msalter@redhat.com>
2025-10-22 16:48:29 -0400

    arm64: remove CONFIG_VMAP_STACK checks from entry code
    JIRA: https://issues.redhat.com/browse/RHEL-101059
    
    commit 9d1869f0f537d26005a521a141dde759fc3303f5
    Author: Breno Leitao <leitao@debian.org>
    Date: Mon, 7 Jul 2025 09:01:08 -0700
    
        With VMAP_STACK now always enabled on arm64, remove all CONFIG_VMAP_STACK
        conditionals from entry handling in arch/arm64/kernel/entry-common.c and
        arch/arm64/kernel/entry.S.
    
        This change unconditionally includes the bad stack handling and overflow
        detection logic, simplifying the code and reflecting the mandatory use of
        VMAP_STACK for all arm64 kernel builds.
    
        Signed-off-by: Breno Leitao <leitao@debian.org>
        Acked-by: Ard Biesheuvel <ardb@kernel.org>
        Acked-by: Mark Rutland <mark.rutland@arm.com>
        Link: https://lore.kernel.org/r/20250707-arm64_vmap-v1-8-8de98ca0f91c@debian.org
        Signed-off-by: Will Deacon <will@kernel.org>
    
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- arch/arm64/kernel/entry-common.c
+++ arch/arm64/kernel/entry-common.c
@@ -1105,7 +1105,6 @@ UNHANDLED(el0t, 32, fiq)
 UNHANDLED(el0t, 32, error)
 #endif /* CONFIG_COMPAT */
 
-#ifdef CONFIG_VMAP_STACK
 asmlinkage void noinstr __noreturn handle_bad_stack(struct pt_regs *regs)
 {
 	unsigned long esr = read_sysreg(esr_el1);
@@ -1114,7 +1113,6 @@ asmlinkage void noinstr __noreturn handle_bad_stack(struct pt_regs *regs)
 	arm64_enter_nmi(regs);
 	panic_bad_stack(regs, esr, far);
 }
-#endif /* CONFIG_VMAP_STACK */
 
 #ifdef CONFIG_ARM_SDE_INTERFACE
 asmlinkage noinstr unsigned long
