bddccb4b7a3d9739d2ece0958e21e1951362f249
Mamatha Inamdar <minamdar@redhat.com>
2025-10-29 09:57:06 -0400

    powerpc/pseries: Add ibm,get-dynamic-sensor-state RTAS call support
    JIRA: https://issues.redhat.com/browse/RHEL-101838
    
    commit 496c7524dcec16d3993382db35b960f8c3955083
    Author: Haren Myneni <haren@linux.ibm.com>
    Date:   Wed Apr 16 15:57:40 2025 -0700
    
        powerpc/pseries: Add ibm,get-dynamic-sensor-state RTAS call support
    
        The RTAS call ibm,get-dynamic-sensor-state is used to get the
        sensor state identified by the location code and the sensor
        token. The librtas library provides an API
        rtas_get_dynamic_sensor() which uses /dev/mem access for work
        area allocation but is restricted under system lockdown.
    
        This patch provides an interface with new ioctl
         PAPR_DYNAMIC_SENSOR_IOC_GET to the papr-indices character
        driver which executes this HCALL and copies the sensor state
        in the user specified ioctl buffer.
    
        Refer PAPR 7.3.19 ibm,get-dynamic-sensor-state for more
        information on this RTAS call.
        - User input parameters to the RTAS call: location code string
          and the sensor token
    
        Expose these interfaces to user space with a /dev/papr-indices
        character device using the following programming model:
         int fd = open("/dev/papr-indices", O_RDWR);
         int ret = ioctl(fd, PAPR_DYNAMIC_SENSOR_IOC_GET,
                        struct papr_indices_io_block)
          - The user space specifies input parameters in
            papr_indices_io_block struct
          - Returned state for the specified sensor is copied to
            papr_indices_io_block.dynamic_param.state
    
        Signed-off-by: Haren Myneni <haren@linux.ibm.com>
        Tested-by: Sathvika Vasireddy <sv@linux.ibm.com>
        Signed-off-by: Madhavan Srinivasan <maddy@linux.ibm.com>
        Link: https://patch.msgid.link/20250416225743.596462-6-haren@linux.ibm.com
    
    Signed-off-by: Mamatha Inamdar <minamdar@redhat.com>

--- arch/powerpc/kernel/rtas.c
+++ arch/powerpc/kernel/rtas.c
@@ -79,6 +79,7 @@ EXPORT_SYMBOL(rtas_flash_term_hook);
 DEFINE_MUTEX(rtas_ibm_get_vpd_lock);
 DEFINE_MUTEX(rtas_ibm_get_indices_lock);
 DEFINE_MUTEX(rtas_ibm_set_dynamic_indicator_lock);
+DEFINE_MUTEX(rtas_ibm_get_dynamic_sensor_state_lock);
 
 /* RTAS use home made raw locking instead of spin_lock_irqsave
  * because those can be called from within really nasty contexts
@@ -1125,6 +1126,9 @@ static struct mutex *find_rtas_mutex(int token)
 
 	if (token == rtas_token("ibm,set-dynamic-indicator"))
 		return &rtas_ibm_set_dynamic_indicator_lock;
+
+	if (token == rtas_token("ibm,get-dynamic-sensor-state"))
+		return &rtas_ibm_get_dynamic_sensor_state_lock;
 
 	return NULL;
 }
