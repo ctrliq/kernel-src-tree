bb06d2fe543fbe4af682faa8fc5cbe7ef63fcc6b
Jonathan Maple <jmaple@ciq.com>
2025-10-30 13:29:49 -0400

    mm/shmem: fix potential dead loop in shmem_unuse()
    jira LE-4623
    Rebuild_History Non-Buildable kernel-4.18.0-553.81.1.el8_10
    commit-author Kemeng Shi <shikemeng@huaweicloud.com>
    commit 3f778ab1b52450d467109321e2abfc4b36ec2d3e
    
    If multi shmem_unuse() for different swap type is called concurrently, a
    dead loop could occur as following:
    
    shmem_unuse(typeA)               shmem_unuse(typeB)
     mutex_lock(&shmem_swaplist_mutex)
     list_for_each_entry_safe(info, next, ...)
      ...
      mutex_unlock(&shmem_swaplist_mutex)
      /* info->swapped may drop to 0 */
      shmem_unuse_inode(&info->vfs_inode, type)
    
                                      mutex_lock(&shmem_swaplist_mutex)
                                      list_for_each_entry(info, next, ...)
                                       if (!info->swapped)
                                        list_del_init(&info->swaplist)
    
                                      ...
                                      mutex_unlock(&shmem_swaplist_mutex)
    
      mutex_lock(&shmem_swaplist_mutex)
      /* iterate with offlist entry and encounter a dead loop */
      next = list_next_entry(info, swaplist);
      ...
    
    Restart the iteration if the inode is already off shmem_swaplist list to
    fix the issue.
    
    Link: https://lkml.kernel.org/r/20250516170939.965736-4-shikemeng@huaweicloud.com
    Fixes: b56a2d8af914 ("mm: rid swapoff of quadratic complexity")
    	Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    	Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    	Cc: Hugh Dickins <hughd@google.com>
    	Cc: Kairui Song <kasong@tencent.com>
    	Cc: kernel test robot <oliver.sang@intel.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 3f778ab1b52450d467109321e2abfc4b36ec2d3e)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -1231,6 +1231,7 @@ int shmem_unuse(unsigned int type, bool frontswap,
 		return 0;
 
 	mutex_lock(&shmem_swaplist_mutex);
+start_over:
 	list_for_each_entry_safe(info, next, &shmem_swaplist, swaplist) {
 		if (!info->swapped) {
 			list_del_init(&info->swaplist);
@@ -1250,13 +1251,15 @@ int shmem_unuse(unsigned int type, bool frontswap,
 		cond_resched();
 
 		mutex_lock(&shmem_swaplist_mutex);
-		next = list_next_entry(info, swaplist);
-		if (!info->swapped)
-			list_del_init(&info->swaplist);
 		if (atomic_dec_and_test(&info->stop_eviction))
 			wake_up_var(&info->stop_eviction);
 		if (error)
 			break;
+		if (list_empty(&info->swaplist))
+			goto start_over;
+		next = list_next_entry(info, swaplist);
+		if (!info->swapped)
+			list_del_init(&info->swaplist);
 	}
 	mutex_unlock(&shmem_swaplist_mutex);
 
