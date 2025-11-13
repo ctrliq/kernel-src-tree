bac3d268a9af5ea69b7ce3860fc7d6d162878654
Jose Ignacio Tornos Martinez <jtornosm@redhat.com>
2025-11-13 13:31:43 +0100

    treewide: Switch/rename to timer_delete[_sync]() [partial]
    JIRA: https://issues.redhat.com/browse/RHEL-122977
    
    Conflicts:
        Include only wireless hunks, except not suppoprted drivers:
        wilc1000, purelifi, libertas and ti.
    
    commit 8fa7292fee5c5240402371ea89ab285ec856c916
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Sat Apr 5 10:17:26 2025 +0200
    
        treewide: Switch/rename to timer_delete[_sync]()
        
        timer_delete[_sync]() replaces del_timer[_sync](). Convert the whole tree
        over and remove the historical wrapper inlines.
        
        Conversion was done with coccinelle plus manual fixups where necessary.
        
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Jose Ignacio Tornos Martinez <jtornosm@redhat.com>

--- drivers/net/wireless/ath/ath6kl/txrx.c
+++ drivers/net/wireless/ath/ath6kl/txrx.c
@@ -1828,7 +1828,7 @@ void aggr_reset_state(struct aggr_info_conn *aggr_conn)
 		return;
 
 	if (aggr_conn->timer_scheduled) {
-		del_timer(&aggr_conn->timer);
+		timer_delete(&aggr_conn->timer);
 		aggr_conn->timer_scheduled = false;
 	}
 
