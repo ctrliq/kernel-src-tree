b3ba039c5662f5bf44c75b5aea8ebf3e2971759c
Donald Dutile <ddutile@redhat.com>
2025-11-13 15:46:09 -0500

    KVM: arm64: Enforce the sorting of the GICv3 system register table
    JIRA: https://issues.redhat.com/browse/RHEL-115972
    
    commit 8af3e8ab09d0ba9b19fc3ea96ef1fa14660fef21
    Author: Marc Zyngier <maz@kernel.org>
    Date:   Fri Jul 18 12:11:53 2025 +0100
    
        KVM: arm64: Enforce the sorting of the GICv3 system register table
    
        In order to avoid further embarassing bugs, enforce that the GICv3
        sysreg table is actually sorted, just like all the other tables.
    
        Signed-off-by: Marc Zyngier <maz@kernel.org>
        Reviewed-by: Sebastian Ott <sebott@redhat.com>
        Link: https://lore.kernel.org/r/20250718111154.104029-4-maz@kernel.org
        Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -5322,8 +5322,9 @@ int kvm_finalize_sys_regs(struct kvm_vcpu *vcpu)
 
 int __init kvm_sys_reg_table_init(void)
 {
+	const struct sys_reg_desc *gicv3_regs;
 	bool valid = true;
-	unsigned int i;
+	unsigned int i, sz;
 	int ret = 0;
 
 	/* Make sure tables are unique and in order. */
@@ -5333,6 +5334,9 @@ int __init kvm_sys_reg_table_init(void)
 	valid &= check_sysreg_table(cp15_regs, ARRAY_SIZE(cp15_regs), false);
 	valid &= check_sysreg_table(cp15_64_regs, ARRAY_SIZE(cp15_64_regs), false);
 	valid &= check_sysreg_table(sys_insn_descs, ARRAY_SIZE(sys_insn_descs), false);
+
+	gicv3_regs = vgic_v3_get_sysreg_table(&sz);
+	valid &= check_sysreg_table(gicv3_regs, sz, false);
 
 	if (!valid)
 		return -EINVAL;
