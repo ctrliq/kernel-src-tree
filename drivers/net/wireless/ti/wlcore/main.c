4ed6f84f43df6448db903c51ace7a0c705ce5d67
Jose Ignacio Tornos Martinez <jtornosm@redhat.com>
2025-11-13 13:49:51 +0100

    wifi: cfg80211/mac80211: Add support to get radio index
    JIRA: https://issues.redhat.com/browse/RHEL-114891
    
    Conflicts:
      - drivers/net/wireless/purelifi/plfxlc/mac.c
      - drivers/net/wireless/silabs/wfx/sta.c
      - drivers/net/wireless/silabs/wfx/sta.h
      - drivers/staging/rtl8723bs/os_dep/ioctl_cfg80211.c
        remove hunk with conflicts for unsupported drivers: purelifi, silabs and
        staging. 
    
    commit b74947b4f6ff7c122a1bb6eb38bb7ecfbb1d3820
    Author: Roopni Devanathan <quic_rdevanat@quicinc.com>
    Date:   Sun Jun 15 13:53:09 2025 +0530
    
        wifi: cfg80211/mac80211: Add support to get radio index
        
        Currently, per-radio attributes are set on per-phy basis, i.e., all the
        radios present in a wiphy will take attributes values sent from user. But
        each radio in a wiphy can get different values from userspace based on
        its requirement.
        
        To extend support to set per-radio attributes, add support to get radio
        index from userspace. Add an NL attribute - NL80211_ATTR_WIPHY_RADIO_INDEX,
        to get user specified radio index for which attributes should be changed.
        Pass this to individual drivers, so that the drivers can use this radio
        index to change per-radio attributes when necessary. Currently, per-radio
        attributes identified are:
        NL80211_ATTR_WIPHY_TX_POWER_LEVEL
        NL80211_ATTR_WIPHY_ANTENNA_TX
        NL80211_ATTR_WIPHY_ANTENNA_RX
        NL80211_ATTR_WIPHY_RETRY_SHORT
        NL80211_ATTR_WIPHY_RETRY_LONG
        NL80211_ATTR_WIPHY_FRAG_THRESHOLD
        NL80211_ATTR_WIPHY_RTS_THRESHOLD
        NL80211_ATTR_WIPHY_COVERAGE_CLASS
        NL80211_ATTR_TXQ_LIMIT
        NL80211_ATTR_TXQ_MEMORY_LIMIT
        NL80211_ATTR_TXQ_QUANTUM
        
        By default, the radio index is set to -1. This means the attribute should
        be treated as a global configuration. If the user has not specified any
        index, then the radio index passed to individual drivers would be -1. This
        would indicate that the attribute applies to all radios in that wiphy.
        
        Signed-off-by: Roopni Devanathan <quic_rdevanat@quicinc.com>
        Link: https://patch.msgid.link/20250615082312.619639-2-quic_rdevanat@quicinc.com
        Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    Signed-off-by: Jose Ignacio Tornos Martinez <jtornosm@redhat.com>

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -3110,7 +3110,7 @@ static int wl12xx_config_vif(struct wl1271 *wl, struct wl12xx_vif *wlvif,
 	return 0;
 }
 
-static int wl1271_op_config(struct ieee80211_hw *hw, u32 changed)
+static int wl1271_op_config(struct ieee80211_hw *hw, int radio_idx, u32 changed)
 {
 	struct wl1271 *wl = hw->priv;
 	struct wl12xx_vif *wlvif;
@@ -3853,7 +3853,8 @@ out:
 	return 0;
 }
 
-static int wl1271_op_set_frag_threshold(struct ieee80211_hw *hw, u32 value)
+static int wl1271_op_set_frag_threshold(struct ieee80211_hw *hw,
+					int radio_idx, u32 value)
 {
 	struct wl1271 *wl = hw->priv;
 	int ret = 0;
@@ -3884,7 +3885,8 @@ out:
 	return ret;
 }
 
-static int wl1271_op_set_rts_threshold(struct ieee80211_hw *hw, u32 value)
+static int wl1271_op_set_rts_threshold(struct ieee80211_hw *hw, int radio_idx,
+				       u32 value)
 {
 	struct wl1271 *wl = hw->priv;
 	struct wl12xx_vif *wlvif;
