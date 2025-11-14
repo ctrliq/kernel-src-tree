4168c4b0334352d41fa100a353491ccba3382547
Paulo Alcantara <paalcant@redhat.com>
2025-11-14 11:32:27 -0300

    smb: client: Fix refcount leak for cifs_sb_tlink
    JIRA: https://issues.redhat.com/browse/RHEL-128582
    
    commit c2b77f42205ef485a647f62082c442c1cd69d3fc
    Author: Shuhao Fu <sfual@cse.ust.hk>
    Date:   Thu Oct 16 02:52:55 2025 +0000
    
        smb: client: Fix refcount leak for cifs_sb_tlink
        
        Fix three refcount inconsistency issues related to `cifs_sb_tlink`.
        
        Comments for `cifs_sb_tlink` state that `cifs_put_tlink()` needs to be
        called after successful calls to `cifs_sb_tlink()`. Three calls fail to
        update refcount accordingly, leading to possible resource leaks.
        
        Fixes: 8ceb98437946 ("CIFS: Move rename to ops struct")
        Fixes: 2f1afe25997f ("cifs: Use smb 2 - 3 and cifsacl mount options getacl functions")
        Fixes: 366ed846df60 ("cifs: Use smb 2 - 3 and cifsacl mount options setacl function")
        Cc: stable@vger.kernel.org
        Signed-off-by: Shuhao Fu <sfual@cse.ust.hk>
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/smb2ops.c
+++ fs/smb/client/smb2ops.c
@@ -3117,8 +3117,7 @@ get_smb2_acl_by_path(struct cifs_sb_info *cifs_sb,
 	utf16_path = cifs_convert_path_to_utf16(path, cifs_sb);
 	if (!utf16_path) {
 		rc = -ENOMEM;
-		free_xid(xid);
-		return ERR_PTR(rc);
+		goto put_tlink;
 	}
 
 	oparms = (struct cifs_open_parms) {
@@ -3150,6 +3149,7 @@ get_smb2_acl_by_path(struct cifs_sb_info *cifs_sb,
 		SMB2_close(xid, tcon, fid.persistent_fid, fid.volatile_fid);
 	}
 
+put_tlink:
 	cifs_put_tlink(tlink);
 	free_xid(xid);
 
@@ -3190,8 +3190,7 @@ set_smb2_acl(struct smb_ntsd *pnntsd, __u32 acllen,
 	utf16_path = cifs_convert_path_to_utf16(path, cifs_sb);
 	if (!utf16_path) {
 		rc = -ENOMEM;
-		free_xid(xid);
-		return rc;
+		goto put_tlink;
 	}
 
 	oparms = (struct cifs_open_parms) {
@@ -3212,6 +3211,7 @@ set_smb2_acl(struct smb_ntsd *pnntsd, __u32 acllen,
 		SMB2_close(xid, tcon, fid.persistent_fid, fid.volatile_fid);
 	}
 
+put_tlink:
 	cifs_put_tlink(tlink);
 	free_xid(xid);
 	return rc;
