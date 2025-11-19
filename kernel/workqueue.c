5e450728ba1d8847b0f61cfeac6763829cd1cccc
Waiman Long <longman@redhat.com>
2025-11-18 21:27:14 -0500

    workqueue: Initialize wq_isolated_cpumask in workqueue_init_early()
    JIRA: https://issues.redhat.com/browse/RHEL-120441
    Conflicts: Minor context diff due to missing blank line.
    
    commit 261dce3d64021e7ec828a17b4975ce9182e54ceb
    Author: Chuyi Zhou <zhouchuyi@bytedance.com>
    Date:   Tue, 17 Jun 2025 12:42:16 +0800
    
        workqueue: Initialize wq_isolated_cpumask in workqueue_init_early()
    
        Now when isolcpus is enabled via the cmdline, wq_isolated_cpumask does
        not include these isolated CPUs, even wq_unbound_cpumask has already
        excluded them. It is only when we successfully configure an isolate cpuset
        partition that wq_isolated_cpumask gets overwritten by
        workqueue_unbound_exclude_cpumask(), including both the cmdline-specified
        isolated CPUs and the isolated CPUs within the cpuset partitions.
    
        Fix this issue by initializing wq_isolated_cpumask properly in
        workqueue_init_early().
    
        Fixes: fe28f631fa94 ("workqueue: Add workqueue_unbound_exclude_cpumask() to exclude CPUs from wq_unbound_cpumask")
        Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>
        Reviewed-by: Waiman Long <longman@redhat.com>
        Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -7814,8 +7814,10 @@ void __init workqueue_init_early(void)
 	restrict_unbound_cpumask("HK_TYPE_DOMAIN", housekeeping_cpumask(HK_TYPE_DOMAIN));
 	if (!cpumask_empty(&wq_cmdline_cpumask))
 		restrict_unbound_cpumask("workqueue.unbound_cpus", &wq_cmdline_cpumask);
-	cpumask_copy(wq_requested_unbound_cpumask, wq_unbound_cpumask);
 
+	cpumask_copy(wq_requested_unbound_cpumask, wq_unbound_cpumask);
+	cpumask_andnot(wq_isolated_cpumask, cpu_possible_mask,
+						housekeeping_cpumask(HK_TYPE_DOMAIN));
 	pwq_cache = KMEM_CACHE(pool_workqueue, SLAB_PANIC);
 
 	wq_update_pod_attrs_buf = alloc_workqueue_attrs();
