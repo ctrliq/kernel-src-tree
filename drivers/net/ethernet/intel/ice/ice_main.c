884ba3544d33d0092b56bacc86e730080d32e655
Michal Schmidt <mschmidt@redhat.com>
2025-11-20 18:22:55 +0100

    iidc/ice/irdma: Update IDC to support multiple consumers
    JIRA: https://issues.redhat.com/browse/RHEL-129902
    
    commit c24a65b6a27c78d8540409800886b6622ea86ebf
    Author: Dave Ertman <david.m.ertman@intel.com>
    Date:   Tue Apr 15 21:15:49 2025 -0500
    
        iidc/ice/irdma: Update IDC to support multiple consumers
    
        In preparation of supporting more than a single core PCI driver
        for RDMA, move ice specific structs like qset_params, qos_info
        and qos_params from iidc_rdma.h to iidc_rdma_ice.h.
    
        Previously, the ice driver was just exporting its entire PF struct
        to the auxiliary driver, but since each core driver will have its own
        different PF struct, implement a universal struct that all core drivers
        can provide to the auxiliary driver through the probe call.
    
        Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
        Signed-off-by: Dave Ertman <david.m.ertman@intel.com>
        Co-developed-by: Mustafa Ismail <mustafa.ismail@intel.com>
        Signed-off-by: Mustafa Ismail <mustafa.ismail@intel.com>
        Co-developed-by: Shiraz Saleem <shiraz.saleem@intel.com>
        Signed-off-by: Shiraz Saleem <shiraz.saleem@intel.com>
        Co-developed-by: Tatyana Nikolova <tatyana.e.nikolova@intel.com>
        Signed-off-by: Tatyana Nikolova <tatyana.e.nikolova@intel.com>
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    
    (cherry picked from commit c24a65b6a27c78d8540409800886b6622ea86ebf)
    Assisted-by: Patchpal
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -9392,6 +9392,7 @@ ice_setup_tc(struct net_device *netdev, enum tc_setup_type type,
 	     void *type_data)
 {
 	struct ice_netdev_priv *np = netdev_priv(netdev);
+	struct iidc_rdma_core_dev_info *cdev;
 	struct ice_pf *pf = np->vsi->back;
 	bool locked = false;
 	int err;
@@ -9408,11 +9409,12 @@ ice_setup_tc(struct net_device *netdev, enum tc_setup_type type,
 			return -EOPNOTSUPP;
 		}
 
-		if (pf->adev) {
+		cdev = pf->cdev_info;
+		if (cdev && cdev->adev) {
 			mutex_lock(&pf->adev_mutex);
-			device_lock(&pf->adev->dev);
+			device_lock(&cdev->adev->dev);
 			locked = true;
-			if (pf->adev->dev.driver) {
+			if (cdev->adev->dev.driver) {
 				netdev_err(netdev, "Cannot change qdisc when RDMA is active\n");
 				err = -EBUSY;
 				goto adev_unlock;
@@ -9426,7 +9428,7 @@ ice_setup_tc(struct net_device *netdev, enum tc_setup_type type,
 
 adev_unlock:
 		if (locked) {
-			device_unlock(&pf->adev->dev);
+			device_unlock(&cdev->adev->dev);
 			mutex_unlock(&pf->adev_mutex);
 		}
 		return err;
