667c97bdcb77c5efc6fe0188806eb180680e552a
Marcin Wcisło <marcin.wcislo@conclusive.pl>
2025-12-03 20:21:48 +0100

    netfilter: nf_tables: expose opaque set element as struct nft_elem_priv
    jira VULN-430
    subsystem-update netfilter: centos-stream-9 cfd9694caf4378958e4f2314024f50a4009015f3
    commit-author Pablo Neira Ayuso <pablo@netfilter.org>
    commit 9dad402b89e81a0516bad5e0ac009b7a0a80898f
    upstream-diff Context conflict with the cve fix
      5d4bb57cb9e7703d390f48a0d8dc69cbd45a5804 (wrong application order).
    
    Add placeholder structure and place it at the beginning of each struct
    nft_*_elem for each existing set backend, instead of exposing elements
    as void type to the frontend which defeats compiler type checks. Use
    this pointer to this new type to replace void *.
    
    This patch updates the following set backend API to use this new struct
    nft_elem_priv placeholder structure:
    
    - update
    - deactivate
    - flush
    - get
    
    as well as the following helper functions:
    
    - nft_set_elem_ext()
    - nft_set_elem_init()
    - nft_set_elem_destroy()
    - nf_tables_set_elem_destroy()
    
    This patch adds nft_elem_priv_cast() to cast struct nft_elem_priv to
    native element representation from the corresponding set backend.
    BUILD_BUG_ON() makes sure this .priv placeholder is always at the top
    of the opaque set element representation.
    
    	Suggested-by: Florian Westphal <fw@strlen.de>
    	Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (cherry picked from commit 9dad402b89e81a0516bad5e0ac009b7a0a80898f)
    	Signed-off-by: Marcin Wcisło <marcin.wcislo@conclusive.pl>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -601,7 +601,7 @@ static int nft_mapelem_deactivate(const struct nft_ctx *ctx,
 struct nft_set_elem_catchall {
 	struct list_head	list;
 	struct rcu_head		rcu;
-	void			*elem;
+	struct nft_elem_priv	*elem;
 };
 
 static void nft_map_catchall_deactivate(const struct nft_ctx *ctx,
@@ -5855,10 +5855,11 @@ static int nft_set_ext_memcpy(const struct nft_set_ext_tmpl *tmpl, u8 id,
 	return 0;
 }
 
-void *nft_set_elem_init(const struct nft_set *set,
-			const struct nft_set_ext_tmpl *tmpl,
-			const u32 *key, const u32 *key_end,
-			const u32 *data, u64 timeout, u64 expiration, gfp_t gfp)
+struct nft_elem_priv *nft_set_elem_init(const struct nft_set *set,
+					const struct nft_set_ext_tmpl *tmpl,
+					const u32 *key, const u32 *key_end,
+					const u32 *data,
+					u64 timeout, u64 expiration, gfp_t gfp)
 {
 	struct nft_set_ext *ext;
 	void *elem;
@@ -5923,10 +5924,11 @@ static void nft_set_elem_expr_destroy(const struct nft_ctx *ctx,
 }
 
 /* Drop references and destroy. Called from gc, dynset and abort path. */
-void nft_set_elem_destroy(const struct nft_set *set, void *elem,
+void nft_set_elem_destroy(const struct nft_set *set,
+			  const struct nft_elem_priv *elem_priv,
 			  bool destroy_expr)
 {
-	struct nft_set_ext *ext = nft_set_elem_ext(set, elem);
+	struct nft_set_ext *ext = nft_set_elem_ext(set, elem_priv);
 	struct nft_ctx ctx = {
 		.net	= read_pnet(&set->net),
 		.family	= set->table->family,
@@ -5937,10 +5939,10 @@ void nft_set_elem_destroy(const struct nft_set *set, void *elem,
 		nft_data_release(nft_set_ext_data(ext), set->dtype);
 	if (destroy_expr && nft_set_ext_exists(ext, NFT_SET_EXT_EXPRESSIONS))
 		nft_set_elem_expr_destroy(&ctx, nft_set_ext_expr(ext));
-
 	if (nft_set_ext_exists(ext, NFT_SET_EXT_OBJREF))
 		nft_use_dec(&(*nft_set_ext_obj(ext))->use);
-	kfree(elem);
+
+	kfree(elem_priv);
 }
 EXPORT_SYMBOL_GPL(nft_set_elem_destroy);
 
@@ -5948,14 +5950,15 @@ EXPORT_SYMBOL_GPL(nft_set_elem_destroy);
  * path via nft_setelem_data_deactivate().
  */
 void nf_tables_set_elem_destroy(const struct nft_ctx *ctx,
-				const struct nft_set *set, void *elem)
+				const struct nft_set *set,
+				const struct nft_elem_priv *elem_priv)
 {
-	struct nft_set_ext *ext = nft_set_elem_ext(set, elem);
+	struct nft_set_ext *ext = nft_set_elem_ext(set, elem_priv);
 
 	if (nft_set_ext_exists(ext, NFT_SET_EXT_EXPRESSIONS))
 		nft_set_elem_expr_destroy(ctx, nft_set_ext_expr(ext));
 
-	kfree(elem);
+	kfree(elem_priv);
 }
 
 int nft_set_elem_expr_clone(const struct nft_ctx *ctx, struct nft_set *set,
