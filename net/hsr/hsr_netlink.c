71ba9c253e15dd868269bcbf1be959e5b9f1d6a7
Felix Maurer <fmaurer@redhat.com>
2025-12-04 13:52:11 +0100

    net: hsr: prevent creation of HSR device with slaves from another netns
    JIRA: https://issues.redhat.com/browse/RHEL-127793
    Conflicts: Code difference due to missing 69c7be1b903f ("rtnetlink: Pack
               newlink() params into struct") and cf517ac16ad9 ("net: Use
               link/peer netns in newlink() of rtnl_link_ops"). The first one
               just replaced src_net with params->net without a functional
               change. The second one switched to a helper to get the link_net
               with a fallback to src_net. This is exactly how the src_net
               parameter is filled by rtnl_newlink_create() at the moment
               (link_net ? : net). Therefore, we continue to use src_net
               instead of link_net in hsr_newlink().
    
    commit c0178eec8884231a5ae0592b9fce827bccb77e86
    Author: Fernando Fernandez Mancera <fmancera@suse.de>
    Date:   Mon Oct 20 15:55:33 2025 +0200
    
        net: hsr: prevent creation of HSR device with slaves from another netns
    
        HSR/PRP driver does not handle correctly having slaves/interlink devices
        in a different net namespace. Currently, it is possible to create a HSR
        link in a different net namespace than the slaves/interlink with the
        following command:
    
         ip link add hsr0 netns hsr-ns type hsr slave1 eth1 slave2 eth2
    
        As there is no use-case on supporting this scenario, enforce that HSR
        device link matches netns defined by IFLA_LINK_NETNSID.
    
        The iproute2 command mentioned above will throw the following error:
    
         Error: hsr: HSR slaves/interlink must be on the same net namespace than HSR link.
    
        Fixes: f421436a591d ("net/hsr: Add support for the High-availability Seamless Redundancy protocol (HSRv0)")
        Signed-off-by: Fernando Fernandez Mancera <fmancera@suse.de>
        Link: https://patch.msgid.link/20251020135533.9373-1-fmancera@suse.de
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Felix Maurer <fmaurer@redhat.com>

--- net/hsr/hsr_netlink.c
+++ net/hsr/hsr_netlink.c
@@ -33,11 +33,17 @@ static int hsr_newlink(struct net *src_net, struct net_device *dev,
 		       struct nlattr *tb[], struct nlattr *data[],
 		       struct netlink_ext_ack *extack)
 {
+	struct net_device *link[2], *interlink = NULL;
 	enum hsr_version proto_version;
 	unsigned char multicast_spec;
 	u8 proto = HSR_PROTOCOL_HSR;
 
-	struct net_device *link[2], *interlink = NULL;
+	if (!net_eq(src_net, dev_net(dev))) {
+		NL_SET_ERR_MSG_MOD(extack,
+				   "HSR slaves/interlink must be on the same net namespace than HSR link");
+		return -EINVAL;
+	}
+
 	if (!data) {
 		NL_SET_ERR_MSG_MOD(extack, "No slave devices specified");
 		return -EINVAL;
