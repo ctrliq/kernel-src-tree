9cb3838ba63f3b7b28a79d239219c8725dd5e743
Donald Dutile <ddutile@redhat.com>
2025-12-08 15:34:12 -0500

    KVM: arm64: Hide ID_AA64MMFR2_EL1.NV from guest and userspace
    JIRA: https://issues.redhat.com/browse/RHEL-134393
    
    Backporting the following commit into 10.2, and all 10.y.z's
    to fix NV migration mitigation between the supported RHEL releases.
    Need to be input to 10.2/cs10 in order to clone to z-streams.
    
    Testing: Built 10.2/cs10 and 9.8/cs9 test kernels and verified
             that migration of a VM saved in one kernel can be restored
    	 on the other kernel, emulating cross-server migration
    	 (using RHEL9 qemu-kvm).
    
    commit 9d6745572899599966fb76a868acca1cae9518af
    Author: Marc Zyngier <maz@kernel.org>
    Date:   Thu Feb 20 13:48:55 2025 +0000
    
        KVM: arm64: Hide ID_AA64MMFR2_EL1.NV from guest and userspace
    
        Since our take on FEAT_NV is to only support FEAT_NV2, we should
        never expose ID_AA64MMFR2_EL1.NV to a guest nor userspace.
    
        Make sure we mask this field for good.
    
        Signed-off-by: Marc Zyngier <maz@kernel.org>
        Reviewed-by: Joey Gouly <joey.gouly@arm.com>
        Link: https://lore.kernel.org/r/20250220134907.554085-3-maz@kernel.org
        [oliver: squash diff for NV field]
        Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -1761,6 +1761,7 @@ static u64 __kvm_read_sanitised_id_reg(const struct kvm_vcpu *vcpu,
 		break;
 	case SYS_ID_AA64MMFR2_EL1:
 		val &= ~ID_AA64MMFR2_EL1_CCIDX_MASK;
+		val &= ~ID_AA64MMFR2_EL1_NV;
 		break;
 	case SYS_ID_AA64MMFR3_EL1:
 		val &= ID_AA64MMFR3_EL1_TCRX |
@@ -2150,6 +2151,22 @@ static int set_id_aa64mmfr0_el1(struct kvm_vcpu *vcpu,
 	if (vcpu_has_nv(vcpu) &&
 	    ((sanitized_val & tgran2_mask) != (user_val & tgran2_mask)))
 		return -EINVAL;
+
+	return set_id_reg(vcpu, rd, user_val);
+}
+
+static int set_id_aa64mmfr2_el1(struct kvm_vcpu *vcpu,
+				const struct sys_reg_desc *rd, u64 user_val)
+{
+	u64 hw_val = read_sanitised_ftr_reg(SYS_ID_AA64MMFR2_EL1);
+	u64 nv_mask = ID_AA64MMFR2_EL1_NV_MASK;
+
+	/*
+	 * We made the mistake to expose the now deprecated NV field,
+	 * so allow userspace to write it, but silently ignore it.
+	 */
+	if ((hw_val & nv_mask) == (user_val & nv_mask))
+		user_val &= ~nv_mask;
 
 	return set_id_reg(vcpu, rd, user_val);
 }
@@ -3132,7 +3149,8 @@ static const struct sys_reg_desc sys_reg_descs[] = {
 					ID_AA64MMFR1_EL1_XNX |
 					ID_AA64MMFR1_EL1_VH |
 					ID_AA64MMFR1_EL1_VMIDBits)),
-	ID_WRITABLE(ID_AA64MMFR2_EL1, ~(ID_AA64MMFR2_EL1_RES0 |
+	ID_FILTERED(ID_AA64MMFR2_EL1,
+		    id_aa64mmfr2_el1, ~(ID_AA64MMFR2_EL1_RES0 |
 					ID_AA64MMFR2_EL1_EVT |
 					ID_AA64MMFR2_EL1_FWB |
 					ID_AA64MMFR2_EL1_IDS |
