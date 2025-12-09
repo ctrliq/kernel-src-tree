baa34230b75837bd6edbdd8d574c677ad9123b01
Mohammad Heib <mheib@redhat.com>
2025-12-09 18:43:29 +0200

    net: protect rxq->mp_params with the instance lock
    JIRA: https://issues.redhat.com/browse/RHEL-89816
    
    Conflicts:
     - conflict in dev.c due to missing:
       f8350a4358fc ("net: page_pool: add a mp hook to
       unregister_netdevice*").
       Move dev_dmabuf_uninstall under the lock instaed of
       dev_memory_provider_uninstall.
    
    commit b52458652eca5a551ddb55605201b136f091b04d
    Author: Jakub Kicinski <kuba@kernel.org>
    Date:   Mon Mar 24 15:45:33 2025 -0700
    
        net: protect rxq->mp_params with the instance lock
    
        Ensure that all accesses to mp_params are under the netdev
        instance lock. The only change we need is to move
        dev_memory_provider_uninstall() under the lock.
    
        Appropriately swap the asserts.
    
        Reviewed-by: Mina Almasry <almasrymina@google.com>
        Acked-by: Stanislav Fomichev <sdf@fomichev.me>
        Link: https://patch.msgid.link/20250324224537.248800-8-kuba@kernel.org
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Mohammad Heib <mheib@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -10290,7 +10290,7 @@ u32 dev_get_min_mp_channel_count(const struct net_device *dev)
 {
 	int i;
 
-	ASSERT_RTNL();
+	netdev_ops_assert_locked(dev);
 
 	for (i = dev->real_num_rx_queues - 1; i >= 0; i--)
 		if (dev->_rx[i].mp_params.mp_priv)
@@ -11855,9 +11855,9 @@ void unregister_netdevice_many_notify(struct list_head *head,
 		dev_tcx_uninstall(dev);
 		netdev_lock_ops(dev);
 		dev_xdp_uninstall(dev);
+		dev_dmabuf_uninstall(dev);
 		netdev_unlock_ops(dev);
 		bpf_dev_bound_netdev_unregister(dev);
-		dev_dmabuf_uninstall(dev);
 
 		netdev_offload_xstats_disable_all(dev);
 
