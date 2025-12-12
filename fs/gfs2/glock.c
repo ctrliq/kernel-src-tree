3063047605dfa187eb7c8a579776ef27b5a44c3e
Andreas Gruenbacher <agruenba@redhat.com>
2025-12-12 16:02:05 +0000

    gfs2: glock cancelation flag fix
    JIRA: https://issues.redhat.com/browse/RHEL-135362
    Upstream Status: https://git.kernel.org/pub/scm/linux/kernel/git/gfs2/linux-gfs2.git/ retry-rename
    
    When an asynchronous glock holder is dequeued that hasn't been granted
    yet (HIF_HOLDER not set) and no dlm operation is in progress on behalf
    of that holder (GLF_LOCK not set), the dequeuing takes place in
    __gfs2_glock_dq().  There, we are not clearing the HIF_WAIT flag.  This
    prevents the same holder from being enqueued later (gfs2_glock_nq())
    without first reinitializing it (gfs2_holder_reinit()).  Fix that by
    always clearing the HIF_WAIT flag in __gfs2_glock_dq().
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit de958017e4fed4f3f5aa115128da73f8d63ac975)

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -1611,6 +1611,7 @@ static void __gfs2_glock_dq(struct gfs2_holder *gh)
 	list_del_init(&gh->gh_list);
 	clear_bit(HIF_HOLDER, &gh->gh_iflags);
 	trace_gfs2_glock_queue(gh, 0);
+	clear_bit(HIF_WAIT, &gh->gh_iflags);
 
 	/*
 	 * If there hasn't been a demote request we are done.
