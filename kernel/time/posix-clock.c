a4e9d6da43808adb25b4269dcf0fb7e94ccc4871
Waiman Long <longman@redhat.com>
2025-12-12 00:26:49 -0500

    posix-clock: Store file pointer in struct posix_clock_context
    JIRA: https://issues.redhat.com/browse/RHEL-114122
    
    commit e859d375d1694488015e6804bfeea527a0b25b9f
    Author: Wojtek Wasko <wwasko@nvidia.com>
    Date:   Mon, 3 Mar 2025 18:13:43 +0200
    
        posix-clock: Store file pointer in struct posix_clock_context
    
        File descriptor based pc_clock_*() operations of dynamic posix clocks
        have access to the file pointer and implement permission checks in the
        generic code before invoking the relevant dynamic clock callback.
    
        Character device operations (open, read, poll, ioctl) do not implement a
        generic permission control and the dynamic clock callbacks have no
        access to the file pointer to implement them.
    
        Extend struct posix_clock_context with a struct file pointer and
        initialize it in posix_clock_open(), so that all dynamic clock callbacks
        can access it.
    
        Acked-by: Richard Cochran <richardcochran@gmail.com>
        Reviewed-by: Vadim Fedorenko <vadim.fedorenko@linux.dev>
        Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
        Signed-off-by: Wojtek Wasko <wwasko@nvidia.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- kernel/time/posix-clock.c
+++ kernel/time/posix-clock.c
@@ -109,6 +109,7 @@ static int posix_clock_open(struct inode *inode, struct file *fp)
 		goto out;
 	}
 	pccontext->clk = clk;
+	pccontext->fp = fp;
 	if (clk->ops.open) {
 		err = clk->ops.open(pccontext, fp->f_mode);
 		if (err) {
