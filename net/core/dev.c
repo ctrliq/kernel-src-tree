d012be82879c1a029377d91f8a226f8056d34195
Xin Long <lxin@redhat.com>
2025-12-12 20:40:54 -0500

    net: s/dev_set_threaded/netif_set_threaded/
    JIRA: https://issues.redhat.com/browse/RHEL-130765
    Tested: compile only
    
    Conflicts:
    - the dashes that represent empty cells are not removed due to missing
      commit 54b771e6c6759.
    
    commit 5d4d84618e1aa2c9531afa3a6323f56e1db4dcf7
    Author: Stanislav Fomichev <sdf@fomichev.me>
    Date:   Thu Jul 17 10:23:32 2025 -0700
    
        net: s/dev_set_threaded/netif_set_threaded/
    
        Commit cc34acd577f1 ("docs: net: document new locking reality")
        introduced netif_ vs dev_ function semantics: the former expects locked
        netdev, the latter takes care of the locking. We don't strictly
        follow this semantics on either side, but there are more dev_xxx handlers
        now that don't fit. Rename them to netif_xxx where appropriate.
    
        Note that one dev_set_threaded call still remains in mt76 for debugfs file.
    
        Signed-off-by: Stanislav Fomichev <sdf@fomichev.me>
        Link: https://patch.msgid.link/20250717172333.1288349-7-sdf@fomichev.me
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Xin Long <lxin@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4726,7 +4726,7 @@ static inline void ____napi_schedule(struct softnet_data *sd,
 
 	if (test_bit(NAPI_STATE_THREADED, &napi->state)) {
 		/* Paired with smp_mb__before_atomic() in
-		 * napi_enable()/dev_set_threaded().
+		 * napi_enable()/netif_set_threaded().
 		 * Use READ_ONCE() to guarantee a complete
 		 * read on napi->thread. Only call
 		 * wake_up_process() when it's not NULL.
@@ -6879,7 +6879,7 @@ int napi_set_threaded(struct napi_struct *napi, bool threaded)
 	return 0;
 }
 
-int dev_set_threaded(struct net_device *dev, bool threaded)
+int netif_set_threaded(struct net_device *dev, bool threaded)
 {
 	struct napi_struct *napi;
 	int err = 0;
@@ -6920,7 +6920,7 @@ int dev_set_threaded(struct net_device *dev, bool threaded)
 
 	return err;
 }
-EXPORT_SYMBOL(dev_set_threaded);
+EXPORT_SYMBOL(netif_set_threaded);
 
 /**
  * netif_queue_set_napi - Associate queue with the napi
