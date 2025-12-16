663f2f5cfd72ae1fe0910f6a3a323053a94dd3cc
Paulo Alcantara <paalcant@redhat.com>
2025-12-16 19:37:16 -0300

    cifs: Do some preparation prior to organising the function declarations
    JIRA: https://issues.redhat.com/browse/RHEL-116054
    Conflicts:
    	- Code difference due to upstream commits e05b3115e753 ("smb:
              client: Use HMAC-SHA256 library for SMB2 signature
              calculation"), c04e55b257b4 ("smb: client: Use MD5 library
              for SMB1 signature calculation").
    
    commit 32a60868093aed5ccaa5e57f3b57f791f22ac133
    Author: David Howells <dhowells@redhat.com>
    Date:   Mon Dec 1 08:40:25 2025 +0000
    
        cifs: Do some preparation prior to organising the function declarations
    
        Make some preparatory cleanups prior to running a script to organise the
        function declarations within the fs/smb/client/ headers.  These include:
    
         (1) Remove "inline" from the dummy cifs_proc_init/clean() functions as
             they are in a .c file.
    
         (2) Move should_compress()'s kdoc comment to the .c file and remove kdoc
             markers from the comments.
    
         (3) Rename CIFS_ALLOW_INSECURE_LEGACY in #endif comments to have CONFIG_
             on the front to allow the script to recognise it.
    
         (4) Don't let comments have bare words at the left margin as that confused
             the simplistic function detection code in the script.
    
         (5) Adjust some argument lists so that when and if the cleanup script is
             run they don't end up over 100 chars.
    
         (6) Fix a few comments to have missing '*' added or the "*/" moved to
             their own lines so that checkpatch doesn't moan over the cleanup
             script patch.
    
         (7) Move struct cifs_calc_sig_ctx to cifsglob.h.
    
         (8) Remove some __KERNEL__ conditionals.
    
        Signed-off-by: David Howells <dhowells@redhat.com>
        Reviewed-by: Paulo Alcantara (Red Hat) <pc@manguebit.org>
        cc: linux-cifs@vger.kernel.org
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/cifsproto.h
+++ fs/smb/client/cifsproto.h
@@ -594,7 +594,7 @@ extern int cifs_do_set_acl(const unsigned int xid, struct cifs_tcon *tcon,
 			   const struct nls_table *nls_codepage, int remap);
 extern int CIFSGetExtAttr(const unsigned int xid, struct cifs_tcon *tcon,
 			const int netfid, __u64 *pExtAttrBits, __u64 *pMask);
-#endif /* CIFS_ALLOW_INSECURE_LEGACY */
+#endif /* CONFIG_CIFS_ALLOW_INSECURE_LEGACY */
 extern void cifs_autodisable_serverino(struct cifs_sb_info *cifs_sb);
 extern bool couldbe_mf_symlink(const struct cifs_fattr *fattr);
 extern int check_mf_symlink(unsigned int xid, struct cifs_tcon *tcon,
