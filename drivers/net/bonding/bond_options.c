90a3aa95e2f9eb3206b52506fcfa06eda83039e4
Hangbin Liu <haliu@redhat.com>
2026-01-05 14:53:48 +0800

    net: bonding: add broadcast_neighbor option for 802.3ad
    JIRA: https://issues.redhat.com/browse/RHEL-131711
    Upstream Status: net.git commit ce7a381697cb
    
    Conflicts: we haven't backported coupled_control option yet.
    
    commit ce7a381697cb3958ffe0b45e5028ac69444e9288
    Author: Tonghao Zhang <tonghao@bamaicloud.com>
    Date:   Fri Jun 27 21:49:28 2025 +0800
    
        net: bonding: add broadcast_neighbor option for 802.3ad
    
        Stacking technology is a type of technology used to expand ports on
        Ethernet switches. It is widely used as a common access method in
        large-scale Internet data center architectures. Years of practice
        have proved that stacking technology has advantages and disadvantages
        in high-reliability network architecture scenarios. For instance,
        in stacking networking arch, conventional switch system upgrades
        require multiple stacked devices to restart at the same time.
        Therefore, it is inevitable that the business will be interrupted
        for a while. It is for this reason that "no-stacking" in data centers
        has become a trend. Additionally, when the stacking link connecting
        the switches fails or is abnormal, the stack will split. Although it is
        not common, it still happens in actual operation. The problem is that
        after the split, it is equivalent to two switches with the same
        configuration appearing in the network, causing network configuration
        conflicts and ultimately interrupting the services carried by the
        stacking system.
    
        To improve network stability, "non-stacking" solutions have been
        increasingly adopted, particularly by public cloud providers and
        tech companies like Alibaba, Tencent, and Didi. "non-stacking" is
        a method of mimicing switch stacking that convinces a LACP peer,
        bonding in this case, connected to a set of "non-stacked" switches
        that all of its ports are connected to a single switch
        (i.e., LACP aggregator), as if those switches were stacked. This
        enables the LACP peer's ports to aggregate together, and requires
        (a) special switch configuration, described in the linked article,
        and (b) modifications to the bonding 802.3ad (LACP) mode to send
        all ARP/ND packets across all ports of the active aggregator.
    
        Note that, with multiple aggregators, the current broadcast mode
        logic will send only packets to the selected aggregator(s).
    
         +-----------+   +-----------+
         |  switch1  |   |  switch2  |
         +-----------+   +-----------+
                 ^           ^
                 |           |
              +-----------------+
              |   bond4 lacp    |
              +-----------------+
                 |           |
                 | NIC1      | NIC2
              +-----------------+
              |     server      |
              +-----------------+
    
        - https://www.ruijie.com/fr-fr/support/tech-gallery/de-stack-data-center-network-architecture/
    
        Cc: Jay Vosburgh <jv@jvosburgh.net>
        Cc: "David S. Miller" <davem@davemloft.net>
        Cc: Eric Dumazet <edumazet@google.com>
        Cc: Jakub Kicinski <kuba@kernel.org>
        Cc: Paolo Abeni <pabeni@redhat.com>
        Cc: Simon Horman <horms@kernel.org>
        Cc: Jonathan Corbet <corbet@lwn.net>
        Cc: Andrew Lunn <andrew+netdev@lunn.ch>
        Cc: Steven Rostedt <rostedt@goodmis.org>
        Cc: Masami Hiramatsu <mhiramat@kernel.org>
        Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
        Cc: Nikolay Aleksandrov <razor@blackwall.org>
        Signed-off-by: Tonghao Zhang <tonghao@bamaicloud.com>
        Signed-off-by: Zengbing Tu <tuzengbing@didiglobal.com>
        Link: https://patch.msgid.link/84d0a044514157bb856a10b6d03a1028c4883561.1751031306.git.tonghao@bamaicloud.com
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Hangbin Liu <haliu@redhat.com>

--- drivers/net/bonding/bond_options.c
+++ drivers/net/bonding/bond_options.c
@@ -85,7 +85,8 @@ static int bond_option_ad_user_port_key_set(struct bonding *bond,
 					    const struct bond_opt_value *newval);
 static int bond_option_missed_max_set(struct bonding *bond,
 				      const struct bond_opt_value *newval);
-
+static int bond_option_broadcast_neigh_set(struct bonding *bond,
+					   const struct bond_opt_value *newval);
 
 static const struct bond_opt_value bond_mode_tbl[] = {
 	{ "balance-rr",    BOND_MODE_ROUNDROBIN,   BOND_VALFLAG_DEFAULT},
@@ -231,6 +232,12 @@ static const struct bond_opt_value bond_missed_max_tbl[] = {
 	{ "maxval",	255,	BOND_VALFLAG_MAX},
 	{ "default",	2,	BOND_VALFLAG_DEFAULT},
 	{ NULL,		-1,	0},
+};
+
+static const struct bond_opt_value bond_broadcast_neigh_tbl[] = {
+	{ "off", 0, BOND_VALFLAG_DEFAULT},
+	{ "on",	 1, 0},
+	{ NULL,  -1, 0}
 };
 
 static const struct bond_option bond_opts[BOND_OPT_LAST] = {
@@ -497,6 +504,14 @@ static const struct bond_option bond_opts[BOND_OPT_LAST] = {
 		.desc = "Delay between each peer notification on failover event, in milliseconds",
 		.values = bond_peer_notif_delay_tbl,
 		.set = bond_option_peer_notif_delay_set
+	},
+	[BOND_OPT_BROADCAST_NEIGH] = {
+		.id = BOND_OPT_BROADCAST_NEIGH,
+		.name = "broadcast_neighbor",
+		.desc = "Broadcast neighbor packets to all active slaves",
+		.unsuppmodes = BOND_MODE_ALL_EX(BIT(BOND_MODE_8023AD)),
+		.values = bond_broadcast_neigh_tbl,
+		.set = bond_option_broadcast_neigh_set,
 	}
 };
 
@@ -890,6 +905,13 @@ static int bond_option_mode_set(struct bonding *bond,
 	/* don't cache arp_validate between modes */
 	bond->params.arp_validate = BOND_ARP_VALIDATE_NONE;
 	bond->params.mode = newval->value;
+
+	/* When changing mode, the bond device is down, we may reduce
+	 * the bond_bcast_neigh_enabled in bond_close() if broadcast_neighbor
+	 * enabled in 8023ad mode. Therefore, only clear broadcast_neighbor
+	 * to 0.
+	 */
+	bond->params.broadcast_neighbor = 0;
 
 	if (bond->dev->reg_state == NETREG_REGISTERED) {
 		bool update = false;
@@ -1830,5 +1852,24 @@ static int bond_option_ad_user_port_key_set(struct bonding *bond,
 		   newval->value);
 
 	bond->params.ad_user_port_key = newval->value;
+	return 0;
+}
+
+static int bond_option_broadcast_neigh_set(struct bonding *bond,
+					   const struct bond_opt_value *newval)
+{
+	if (bond->params.broadcast_neighbor == newval->value)
+		return 0;
+
+	bond->params.broadcast_neighbor = newval->value;
+	if (bond->dev->flags & IFF_UP) {
+		if (bond->params.broadcast_neighbor)
+			static_branch_inc(&bond_bcast_neigh_enabled);
+		else
+			static_branch_dec(&bond_bcast_neigh_enabled);
+	}
+
+	netdev_dbg(bond->dev, "Setting broadcast_neighbor to %s (%llu)\n",
+		   newval->string, newval->value);
 	return 0;
 }
