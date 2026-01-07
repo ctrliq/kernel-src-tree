7ed8bf29ef15dfdb9bba91a0638ec0638bf44a9f
Joe Lawrence <joe.lawrence@redhat.com>
2026-01-07 12:54:59 -0500

    objtool: Mark .cold subfunctions
    JIRA: https://issues.redhat.com/browse/RHEL-114916
    
    Conflicts:
    	tools/objtool/include/objtool/elf.h
    	- Diff context: cs-10 is missing upstream 894af4a1cde6
    	  ("objtool: Validate kCFI calls") which added symbol.nocfi.
    
    commit 4ea029389bf0cc44da6d3a24a520200e060ce6bf
    Author: Josh Poimboeuf <jpoimboe@kernel.org>
    Date:   Wed Sep 17 09:03:37 2025 -0700
    
        objtool: Mark .cold subfunctions
    
        Introduce a flag to identify .cold subfunctions so they can be detected
        easier and faster.
    
        Acked-by: Petr Mladek <pmladek@suse.com>
        Tested-by: Joe Lawrence <joe.lawrence@redhat.com>
        Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    
    Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

--- tools/objtool/include/objtool/elf.h
+++ tools/objtool/include/objtool/elf.h
@@ -71,6 +71,7 @@ struct symbol {
 	u8 local_label       : 1;
 	u8 frame_pointer     : 1;
 	u8 ignore	     : 1;
+	u8 cold		     : 1;
 	struct list_head pv_target;
 	struct reloc *relocs;
 	struct section *group_sec;
