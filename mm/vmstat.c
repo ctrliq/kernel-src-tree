8f812d0ef9e74b0fd8caed04f2a621ecc6690801
Jonathan Maple <jmaple@ciq.com>
2026-01-16 16:21:45 -0500

    Rebuild rocky10_1 with kernel-6.12.0-124.27.1.el10_1
    Rebuild_History BUILDABLE
    Rebuilding Kernel from rpm changelog with Fuzz Limit: 87.50%
    Number of commits in upstream range v6.12~1..kernel-mainline: 93416
    Number of commits in rpm: 37
    Number of commits matched with upstream: 32 (86.49%)
    Number of commits in upstream but not in rpm: 93384
    Number of commits NOT found in upstream: 5 (13.51%)
    
    Rebuilding Kernel on Branch rocky10_1_rebuild_kernel-6.12.0-124.27.1.el10_1 for kernel-6.12.0-124.27.1.el10_1
    Clean Cherry Picks: 27 (84.38%)
    Empty Cherry Picks: 5 (15.62%)
    _______________________________
    
    Full Details Located here:
    ciq/ciq_backports/kernel-6.12.0-124.27.1.el10_1/rebuild.details.txt
    
    Includes:
    * git commit header above
    * Empty Commits with upstream SHA
    * RPM ChangeLog Entries that could not be matched
    
    Individual Empty Commit failures contained in the same containing directory.
    The git message for empty commits will have the path for the failed commit.
    File names are the first 8 characters of the upstream SHA

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1269,6 +1269,7 @@ const char * const vmstat_text[] = {
 #ifdef CONFIG_NUMA_BALANCING
 	"pgpromote_success",
 	"pgpromote_candidate",
+	"pgpromote_candidate_nrl",
 #endif
 	"pgdemote_kswapd",
 	"pgdemote_direct",
