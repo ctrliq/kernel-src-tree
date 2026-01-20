a8f9b18bb4ec61abd7f838a6fc592a47d0532393
Michal Schmidt <mschmidt@redhat.com>
2026-01-20 08:03:48 +0100

    idpf: correct queue index in Rx allocation error messages
    JIRA: https://issues.redhat.com/browse/RHEL-139535
    JIRA: https://issues.redhat.com/browse/RHEL-99644
    
    commit 79bb84758f2c6dda7133c5ac391acd61ae4326fd
    Author: Alok Tiwari <alok.a.tiwari@oracle.com>
    Date:   Tue Nov 25 14:36:28 2025 -0800
    
        idpf: correct queue index in Rx allocation error messages
    
        The error messages in idpf_rx_desc_alloc_all() used the group index i
        when reporting memory allocation failures for individual Rx and Rx buffer
        queues. This is incorrect.
    
        Update the messages to use the correct queue index j and include the
        queue group index i for clearer identification of the affected Rx and Rx
        buffer queues.
    
        Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>
        Reviewed-by: Simon Horman <horms@kernel.org>
        Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
        Link: https://patch.msgid.link/20251125223632.1857532-10-anthony.l.nguyen@intel.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/intel/idpf/idpf_txrx.c
+++ drivers/net/ethernet/intel/idpf/idpf_txrx.c
@@ -878,8 +878,8 @@ static int idpf_rx_desc_alloc_all(struct idpf_vport *vport)
 			err = idpf_rx_desc_alloc(vport, q);
 			if (err) {
 				pci_err(vport->adapter->pdev,
-					"Memory allocation for Rx Queue %u failed\n",
-					i);
+					"Memory allocation for Rx queue %u from queue group %u failed\n",
+					j, i);
 				goto err_out;
 			}
 		}
@@ -895,8 +895,8 @@ static int idpf_rx_desc_alloc_all(struct idpf_vport *vport)
 			err = idpf_bufq_desc_alloc(vport, q);
 			if (err) {
 				pci_err(vport->adapter->pdev,
-					"Memory allocation for Rx Buffer Queue %u failed\n",
-					i);
+					"Memory allocation for Rx Buffer Queue %u from queue group %u failed\n",
+					j, i);
 				goto err_out;
 			}
 		}
