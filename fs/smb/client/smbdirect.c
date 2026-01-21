ceb13f1cda92645cda1445149c21fc5d98344bf8
Paulo Alcantara <paalcant@redhat.com>
2026-01-21 17:03:27 -0300

    smb: client: pass struct smbdirect_socket to smbd_qp_async_error_upcall()
    JIRA: https://issues.redhat.com/browse/RHEL-116058
    
    commit 94a0e794b77f3300c99fcac65d1a4fc83fd4684f
    Author: Stefan Metzmacher <metze@samba.org>
    Date:   Fri Aug 22 11:08:13 2025 +0200
    
        smb: client: pass struct smbdirect_socket to smbd_qp_async_error_upcall()
    
        This will make it easier to move function to the common code
        in future.
    
        Cc: Steve French <smfrench@gmail.com>
        Cc: Tom Talpey <tom@talpey.com>
        Cc: Long Li <longli@microsoft.com>
        Cc: linux-cifs@vger.kernel.org
        Cc: samba-technical@lists.samba.org
        Acked-by: Namjae Jeon <linkinjeon@kernel.org>
        Signed-off-by: Stefan Metzmacher <metze@samba.org>
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/smbdirect.c
+++ fs/smb/client/smbdirect.c
@@ -380,11 +380,10 @@ static int smbd_conn_upcall(
 static void
 smbd_qp_async_error_upcall(struct ib_event *event, void *context)
 {
-	struct smbd_connection *info = context;
-	struct smbdirect_socket *sc = &info->socket;
+	struct smbdirect_socket *sc = context;
 
-	log_rdma_event(ERR, "%s on device %s info %p\n",
-		ib_event_msg(event->event), event->device->name, info);
+	log_rdma_event(ERR, "%s on device %s socket %p\n",
+		ib_event_msg(event->event), event->device->name, sc);
 
 	switch (event->event) {
 	case IB_EVENT_CQ_ERR:
@@ -1781,7 +1780,7 @@ static struct smbd_connection *_smbd_get_connection(
 	}
 
 	sc->ib.send_cq =
-		ib_alloc_cq_any(sc->ib.dev, info,
+		ib_alloc_cq_any(sc->ib.dev, sc,
 				sp->send_credit_target, IB_POLL_SOFTIRQ);
 	if (IS_ERR(sc->ib.send_cq)) {
 		sc->ib.send_cq = NULL;
@@ -1789,7 +1788,7 @@ static struct smbd_connection *_smbd_get_connection(
 	}
 
 	sc->ib.recv_cq =
-		ib_alloc_cq_any(sc->ib.dev, info,
+		ib_alloc_cq_any(sc->ib.dev, sc,
 				sp->recv_credit_max, IB_POLL_SOFTIRQ);
 	if (IS_ERR(sc->ib.recv_cq)) {
 		sc->ib.recv_cq = NULL;
@@ -1798,7 +1797,7 @@ static struct smbd_connection *_smbd_get_connection(
 
 	memset(&qp_attr, 0, sizeof(qp_attr));
 	qp_attr.event_handler = smbd_qp_async_error_upcall;
-	qp_attr.qp_context = info;
+	qp_attr.qp_context = sc;
 	qp_attr.cap.max_send_wr = sp->send_credit_target;
 	qp_attr.cap.max_recv_wr = sp->recv_credit_max;
 	qp_attr.cap.max_send_sge = SMBDIRECT_SEND_IO_MAX_SGE;
