0ff5f782c03fb24198a392b50405f7e12606e1a5
Mamatha Inamdar <minamdar@redhat.com>
2026-01-22 06:48:50 -0500

    powerpc/ftrace: Add support for DYNAMIC_FTRACE_WITH_CALL_OPS
    JIRA: https://issues.redhat.com/browse/RHEL-14156
    
    Conflicts:
    	Backport of upstream commit e717754f0bb5. arch/powerpc/Kconfig file
    	slightly modified and hunk in arch/powerpc/Makefile dropped as the
    	dependent PPC32 changes to enable DYNAMIC_FTRACE_WITH_CALL_OPS are
    	not fully backported. Also, hunk#1 & hunk#2 for ftrace_entry.S file
    	applied to ftrace_64_mprofile.S due to missing refactoring and ppc32
    	& ppc64 consolidation commits like 41315494beed, 228216716cb5 &
    	bad90aa52d9a and hunk#3 applied to ftrace_64.S file. Additionally,
    	with commit e6b8940e7e80 that introduced  patch_ulong() not being
    	part of this build, using patch_instructions() instead.
    
    commit e717754f0bb5c5347aac82232691340955735ce1
    Author: Naveen N Rao <naveen@kernel.org>
    Date:   Wed Oct 30 12:38:47 2024 +0530
    
        powerpc/ftrace: Add support for DYNAMIC_FTRACE_WITH_CALL_OPS
    
        Implement support for DYNAMIC_FTRACE_WITH_CALL_OPS similar to the
        arm64 implementation.
    
        This works by patching-in a pointer to an associated ftrace_ops
        structure before each traceable function. If multiple ftrace_ops are
        associated with a call site, then a special ftrace_list_ops is used to
        enable iterating over all the registered ftrace_ops. If no ftrace_ops
        are associated with a call site, then a special ftrace_nop_ops structure
        is used to render the ftrace call as a no-op. ftrace trampoline can then
        read the associated ftrace_ops for a call site by loading from an offset
        from the LR, and branch directly to the associated function.
    
        The primary advantage with this approach is that we don't have to
        iterate over all the registered ftrace_ops for call sites that have a
        single ftrace_ops registered. This is the equivalent of implementing
        support for dynamic ftrace trampolines, which set up a special ftrace
        trampoline for each registered ftrace_ops and have individual call sites
        branch into those directly.
    
        A secondary advantage is that this gives us a way to add support for
        direct ftrace callers without having to resort to using stubs. The
        address of the direct call trampoline can be loaded from the ftrace_ops
        structure.
    
        To support this, we reserve a nop before each function on 32-bit
        powerpc. For 64-bit powerpc, two nops are reserved before each
        out-of-line stub. During ftrace activation, we update this location with
        the associated ftrace_ops pointer. Then, on ftrace entry, we load from
        this location and call into ftrace_ops->func().
    
        For 64-bit powerpc, we ensure that the out-of-line stub area is
        doubleword aligned so that ftrace_ops address can be updated atomically.
    
        Signed-off-by: Naveen N Rao <naveen@kernel.org>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
        Link: https://patch.msgid.link/20241030070850.1361304-15-hbathini@linux.ibm.com
    
    Signed-off-by: Mamatha Inamdar <minamdar@redhat.com>

--- arch/powerpc/include/asm/ftrace.h
+++ arch/powerpc/include/asm/ftrace.h
@@ -169,8 +169,11 @@ static inline u8 this_cpu_get_ftrace_enabled(void)
 
 #ifdef CONFIG_PPC_FTRACE_OUT_OF_LINE
 struct ftrace_ool_stub {
+#ifdef CONFIG_DYNAMIC_FTRACE_WITH_CALL_OPS
+	struct ftrace_ops *ftrace_op;
+#endif
 	u32	insn[4];
-};
+} __aligned(sizeof(unsigned long));
 extern struct ftrace_ool_stub ftrace_ool_stub_text_end[], ftrace_ool_stub_text[],
 			      ftrace_ool_stub_inittext[];
 extern unsigned int ftrace_ool_stub_text_end_count, ftrace_ool_stub_text_count,
