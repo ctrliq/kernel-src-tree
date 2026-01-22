5ef5037f55e7ea3d3582987b9d7d57d6a7330151
Herton R. Krzesinski <herton@redhat.com>
2026-01-22 17:05:29 -0300

    printk: Add is_printk_legacy_deferred()
    JIRA: https://issues.redhat.com/browse/RHEL-141481
    Upstream Status: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/
    
    commit 70411bf8d22ab3d2f794f199d81d70b62d3a85fa
    Author: John Ogness <john.ogness@linutronix.de>
    Date:   Tue Aug 20 08:35:49 2024 +0206
    
        printk: Add is_printk_legacy_deferred()
    
        If printk has been explicitly deferred or is called from NMI
        context, legacy console printing must be deferred to an irq_work
        context. Introduce a helper function is_printk_legacy_deferred()
        for a CPU to query if it must defer legacy console printing.
    
        In follow-up commits this helper will be needed at other call
        sites as well.
    
        Signed-off-by: John Ogness <john.ogness@linutronix.de>
        Reviewed-by: Petr Mladek <pmladek@suse.com>
        Link: https://lore.kernel.org/r/20240820063001.36405-24-john.ogness@linutronix.de
        Signed-off-by: Petr Mladek <pmladek@suse.com>
    
    (cherry picked from commit 70411bf8d22ab3d2f794f199d81d70b62d3a85fa)
    Assisted-by: Patchpal
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- kernel/printk/internal.h
+++ kernel/printk/internal.h
@@ -78,6 +78,7 @@ bool printk_percpu_data_ready(void);
 	} while (0)
 
 void defer_console_output(void);
+bool is_printk_legacy_deferred(void);
 
 u16 printk_parse_prefix(const char *text, int *level,
 			enum printk_info_flags *flags);
@@ -174,6 +175,7 @@ static inline void nbcon_kthread_create(struct console *con) { }
 #define printk_safe_exit_irqrestore(flags) local_irq_restore(flags)
 
 static inline bool printk_percpu_data_ready(void) { return false; }
+static inline bool is_printk_legacy_deferred(void) { return false; }
 static inline u64 nbcon_seq_read(struct console *con) { return 0; }
 static inline void nbcon_seq_force(struct console *con, u64 seq) { }
 static inline bool nbcon_alloc(struct console *con) { return false; }
