b1df405ee9f52c6d4d4853298d2b814741860f0f
Izabela Bakollari <ibakolla@redhat.com>
2026-01-27 10:57:08 +0100

    net: phy: micrel: always set shared->phydev for LAN8814
    JIRA: https://issues.redhat.com/browse/RHEL-121401
    
    commit 399d10934740ae8cdaa4e3245f7c5f6c332da844
    Author: Robert Marko <robert.marko@sartura.hr>
    Date:   Tue Oct 21 15:20:26 2025 +0200
    
        net: phy: micrel: always set shared->phydev for LAN8814
    
        Currently, during the LAN8814 PTP probe shared->phydev is only set if PTP
        clock gets actually set, otherwise the function will return before setting
        it.
    
        This is an issue as shared->phydev is unconditionally being used when IRQ
        is being handled, especially in lan8814_gpio_process_cap and since it was
        not set it will cause a NULL pointer exception and crash the kernel.
    
        So, simply always set shared->phydev to avoid the NULL pointer exception.
    
        Fixes: b3f1a08fcf0d ("net: phy: micrel: Add support for PTP_PF_EXTTS for lan8814")
        Signed-off-by: Robert Marko <robert.marko@sartura.hr>
        Tested-by: Horatiu Vultur <horatiu.vultur@microchip.com>
        Link: https://patch.msgid.link/20251021132034.983936-1-robert.marko@sartura.hr
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/phy/micrel.c
+++ drivers/net/phy/micrel.c
@@ -3914,6 +3914,8 @@ static int __lan8814_ptp_probe_once(struct phy_device *phydev, char *pin_name,
 {
 	struct lan8814_shared_priv *shared = phy_package_get_priv(phydev);
 
+	shared->phydev = phydev;
+
 	/* Initialise shared lock for clock*/
 	mutex_init(&shared->shared_lock);
 
@@ -3968,8 +3970,6 @@ static int __lan8814_ptp_probe_once(struct phy_device *phydev, char *pin_name,
 		return 0;
 
 	phydev_dbg(phydev, "successfully registered ptp clock\n");
-
-	shared->phydev = phydev;
 
 	/* The EP.4 is shared between all the PHYs in the package and also it
 	 * can be accessed by any of the PHYs
