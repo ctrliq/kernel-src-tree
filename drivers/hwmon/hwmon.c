1a85095b3e58c17147ff4c6c7b031112ffaef727
Ivan Vecera <ivecera@redhat.com>
2026-01-27 10:51:53 +0100

    hwmon: Add static visibility member to struct hwmon_ops
    JIRA: https://issues.redhat.com/browse/RHEL-144397
    
    Conflicts:
    -  Context conflict due to absence of 4ce6e8a859f05 ("hwmon: Add PEC
       attribute support to hardware monitoring core")
    
    commit 79bc0af904db647979c735563299c9b0d820e432
    Author: Heiner Kallweit <hkallweit1@gmail.com>
    Date:   Thu Oct 10 21:35:42 2024 +0200
    
        hwmon: Add static visibility member to struct hwmon_ops
    
        Several drivers return the same static value in their is_visible
        callback, what results in code duplication. Therefore add an option
        for drivers to specify a static visibility directly.
    
        Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
        Message-ID: <89690b81-2c73-47ae-9ae9-45c77b45ca0c@gmail.com>
        groeck: Renamed hwmon_ops_is_visible -> hwmon_is_visible
        Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/hwmon/hwmon.c
+++ drivers/hwmon/hwmon.c
@@ -128,6 +128,17 @@ static struct class hwmon_class = {
 
 static DEFINE_IDA(hwmon_ida);
 
+static umode_t hwmon_is_visible(const struct hwmon_ops *ops,
+				const void *drvdata,
+				enum hwmon_sensor_types type,
+				u32 attr, int channel)
+{
+	if (ops->visible)
+		return ops->visible;
+
+	return ops->is_visible(drvdata, type, attr, channel);
+}
+
 /* Thermal zone handling */
 
 /*
@@ -250,8 +261,8 @@ static int hwmon_thermal_register_sensors(struct device *dev)
 			int err;
 
 			if (!(info[i]->config[j] & HWMON_T_INPUT) ||
-			    !chip->ops->is_visible(drvdata, hwmon_temp,
-						   hwmon_temp_input, j))
+			    !hwmon_is_visible(chip->ops, drvdata, hwmon_temp,
+					      hwmon_temp_input, j))
 				continue;
 
 			err = hwmon_thermal_add_sensor(dev, j);
@@ -385,7 +396,7 @@ static struct attribute *hwmon_genattr(const void *drvdata,
 	if (!template)
 		return ERR_PTR(-ENOENT);
 
-	mode = ops->is_visible(drvdata, type, attr, index);
+	mode = hwmon_is_visible(ops, drvdata, type, attr, index);
 	if (!mode)
 		return ERR_PTR(-ENOENT);
 
@@ -872,7 +883,7 @@ hwmon_device_register_with_info(struct device *dev, const char *name,
 	if (!dev || !name || !chip)
 		return ERR_PTR(-EINVAL);
 
-	if (!chip->ops || !chip->ops->is_visible || !chip->info)
+	if (!chip->ops || !(chip->ops->visible || chip->ops->is_visible) || !chip->info)
 		return ERR_PTR(-EINVAL);
 
 	return __hwmon_device_register(dev, name, drvdata, chip, extra_groups);
