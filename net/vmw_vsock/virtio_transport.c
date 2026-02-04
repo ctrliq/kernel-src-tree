ce8af3b25b8f2094570414f023b858bd8001906e
Stefano Garzarella <sgarzare@redhat.com>
2026-02-04 17:03:17 +0100

    vsock: add netns to vsock core
    JIRA: https://issues.redhat.com/browse/RHEL-117126
    
    Conflicts:
    - register_net_sysctl_sz() is not available downstream since commit
      95d4977876d66 ("sysctl: Add size to register_net_sysctl function") is
      not backported, so replace with register_net_sysctl() and add the
      sentinel to `struct ctl_table`.
    
    - commit 78eb4ea25cd5f ("sysctl: treewide: constify the ctl_table argument
      of proc_handlers") is not backported, so remove the const qualify of the
      struct ctl_table argument in the proc_handler function
    
    commit eafb64f40ca49c79f0769aab25d0fae5c9d3becb
    Author: Bobby Eshleman <bobbyeshleman@meta.com>
    Date:   Wed Jan 21 14:11:41 2026 -0800
    
        vsock: add netns to vsock core
    
        Add netns logic to vsock core. Additionally, modify transport hook
        prototypes to be used by later transport-specific patches (e.g.,
        *_seqpacket_allow()).
    
        Namespaces are supported primarily by changing socket lookup functions
        (e.g., vsock_find_connected_socket()) to take into account the socket
        namespace and the namespace mode before considering a candidate socket a
        "match".
    
        This patch also introduces the sysctl /proc/sys/net/vsock/ns_mode to
        report the mode and /proc/sys/net/vsock/child_ns_mode to set the mode
        for new namespaces.
    
        Add netns functionality (initialization, passing to transports, procfs,
        etc...) to the af_vsock socket layer. Later patches that add netns
        support to transports depend on this patch.
    
        This patch changes the allocation of random ports for connectible vsocks
        in order to avoid leaking the random port range starting point to other
        namespaces.
    
        dgram_allow(), stream_allow(), and seqpacket_allow() callbacks are
        modified to take a vsk in order to perform logic on namespace modes. In
        future patches, the net will also be used for socket
        lookups in these functions.
    
        Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
        Signed-off-by: Bobby Eshleman <bobbyeshleman@meta.com>
        Link: https://patch.msgid.link/20260121-vsock-vmtest-v16-1-2859a7512097@meta.com
        Acked-by: Michael S. Tsirkin <mst@redhat.com>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>

--- net/vmw_vsock/virtio_transport.c
+++ net/vmw_vsock/virtio_transport.c
@@ -496,7 +496,8 @@ static bool virtio_transport_msgzerocopy_allow(void)
 	return true;
 }
 
-static bool virtio_transport_seqpacket_allow(u32 remote_cid);
+static bool virtio_transport_seqpacket_allow(struct vsock_sock *vsk,
+					     u32 remote_cid);
 
 static struct virtio_transport virtio_transport = {
 	.transport = {
@@ -551,10 +552,14 @@ static struct virtio_transport virtio_transport = {
 	.can_msgzerocopy = virtio_transport_can_msgzerocopy,
 };
 
-static bool virtio_transport_seqpacket_allow(u32 remote_cid)
+static bool
+virtio_transport_seqpacket_allow(struct vsock_sock *vsk, u32 remote_cid)
 {
 	struct virtio_vsock *vsock;
 	bool seqpacket_allow;
+
+	if (!vsock_net_mode_global(vsk))
+		return false;
 
 	seqpacket_allow = false;
 	rcu_read_lock();
