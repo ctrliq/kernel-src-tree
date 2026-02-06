57298654622e90fdc3d96deab51a5edfecbfd04e
David Marlin <dmarlin@redhat.com>
2026-02-06 10:56:24 -0600

    Bluetooth: btusb: revert use of devm_kzalloc in btusb
    JIRA: https://issues.redhat.com/browse/RHEL-147490
    
    commit 252714f1e8bdd542025b16321c790458014d6880
    Author: Raphael Pinsonneault-Thibeault <rpthibeault@gmail.com>
    Date:   Wed Dec 10 11:02:28 2025 -0500
    
        Bluetooth: btusb: revert use of devm_kzalloc in btusb
    
        This reverts commit 98921dbd00c4e ("Bluetooth: Use devm_kzalloc in
        btusb.c file").
    
        In btusb_probe(), we use devm_kzalloc() to allocate the btusb data. This
        ties the lifetime of all the btusb data to the binding of a driver to
        one interface, INTF. In a driver that binds to other interfaces, ISOC
        and DIAG, this is an accident waiting to happen.
    
        The issue is revealed in btusb_disconnect(), where calling
        usb_driver_release_interface(&btusb_driver, data->intf) will have devm
        free the data that is also being used by the other interfaces of the
        driver that may not be released yet.
    
        To fix this, revert the use of devm and go back to freeing memory
        explicitly.
    
        Fixes: 98921dbd00c4e ("Bluetooth: Use devm_kzalloc in btusb.c file")
        Signed-off-by: Raphael Pinsonneault-Thibeault <rpthibeault@gmail.com>
        Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Signed-off-by: David Marlin <dmarlin@redhat.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -4006,7 +4006,7 @@ static int btusb_probe(struct usb_interface *intf,
 			return -ENODEV;
 	}
 
-	data = devm_kzalloc(&intf->dev, sizeof(*data), GFP_KERNEL);
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
 	if (!data)
 		return -ENOMEM;
 
@@ -4029,8 +4029,10 @@ static int btusb_probe(struct usb_interface *intf,
 		}
 	}
 
-	if (!data->intr_ep || !data->bulk_tx_ep || !data->bulk_rx_ep)
+	if (!data->intr_ep || !data->bulk_tx_ep || !data->bulk_rx_ep) {
+		kfree(data);
 		return -ENODEV;
+	}
 
 	if (id->driver_info & BTUSB_AMP) {
 		data->cmdreq_type = USB_TYPE_CLASS | 0x01;
@@ -4085,8 +4087,10 @@ static int btusb_probe(struct usb_interface *intf,
 	data->recv_acl = hci_recv_frame;
 
 	hdev = hci_alloc_dev_priv(priv_size);
-	if (!hdev)
+	if (!hdev) {
+		kfree(data);
 		return -ENOMEM;
+	}
 
 	hdev->bus = HCI_USB;
 	hci_set_drvdata(hdev, data);
@@ -4359,6 +4363,7 @@ out_free_dev:
 	if (data->reset_gpio)
 		gpiod_put(data->reset_gpio);
 	hci_free_dev(hdev);
+	kfree(data);
 	return err;
 }
 
@@ -4407,6 +4412,7 @@ static void btusb_disconnect(struct usb_interface *intf)
 	}
 
 	hci_free_dev(hdev);
+	kfree(data);
 }
 
 #ifdef CONFIG_PM
