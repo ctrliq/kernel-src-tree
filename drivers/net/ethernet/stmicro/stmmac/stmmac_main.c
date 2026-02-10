6de188db81705c12847ef3dde834194f71bcbba8
Izabela Bakollari <ibakolla@redhat.com>
2026-02-10 01:10:13 +0100

    net: stmmac: add suspend()/resume() platform ops
    JIRA: https://issues.redhat.com/browse/RHEL-128151
    
    commit 07bbbfe7addf5b032e04f3c38f0b183d067a3f0d
    Author: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Date:   Mon Aug 11 19:50:43 2025 +0100
    
        net: stmmac: add suspend()/resume() platform ops
    
        Add suspend/resume platform operations, which, when populated, override
        the init/exit platform operations when we suspend and resume. These
        suspend()/resume() methods are called by core code, and thus are
        designed to support any struct device, not just platform devices. This
        allows them to be used by the PCI drivers we have.
    
        Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
        Reviewed-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
        Link: https://patch.msgid.link/E1ulXbX-008gqZ-Bb@rmk-PC.armlinux.org.uk
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7879,6 +7879,9 @@ int stmmac_suspend(struct device *dev)
 	if (stmmac_fpe_supported(priv))
 		timer_shutdown_sync(&priv->fpe_cfg.verify_timer);
 
+	if (priv->plat->suspend)
+		return priv->plat->suspend(dev, priv->plat->bsp_priv);
+
 	return 0;
 }
 EXPORT_SYMBOL_GPL(stmmac_suspend);
@@ -7930,6 +7933,12 @@ int stmmac_resume(struct device *dev)
 	struct net_device *ndev = dev_get_drvdata(dev);
 	struct stmmac_priv *priv = netdev_priv(ndev);
 	int ret;
+
+	if (priv->plat->resume) {
+		ret = priv->plat->resume(dev, priv->plat->bsp_priv);
+		if (ret)
+			return ret;
+	}
 
 	if (!netif_running(ndev))
 		return 0;
