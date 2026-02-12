cc0a28aa553e1a89350fc2a9ec9c3ca1c2c918d5
Izabela Bakollari <ibakolla@redhat.com>
2026-02-12 15:41:37 +0100

    net: ethtool: mm: extract stmmac verification logic into common library
    Conflicts: Only the stmmac driver changes were applies, as the rest of
    the patch is aalready present in RHEl
    
    JIRA: https://issues.redhat.com/browse/RHEL-128151
    
    commit 9ff2aa4206eff40a202e425f232036bc84ad4c0e
    Author: Vladimir Oltean <vladimir.oltean@nxp.com>
    Date:   Mon Mar 17 23:07:30 2025 -0400
    
        net: ethtool: mm: extract stmmac verification logic into common library
    
        It appears that stmmac is not the only hardware which requires a
        software-driven verification state machine for the MAC Merge layer.
    
        While on the one hand it's good to encourage hardware implementations,
        on the other hand it's quite difficult to tolerate multiple drivers
        implementing independently fairly non-trivial logic.
    
        Extract the hardware-independent logic from stmmac into library code and
        put it in ethtool. Name the state structure "mmsv" for MAC Merge
        Software Verification. Let this expose an operations structure for
        executing the hardware stuff: sync hardware with the tx_active boolean
        (result of verification process), enable/disable the pMAC, send mPackets,
        notify library of external events (reception of mPackets), as well as
        link state changes.
    
        Note that it is assumed that the external events are received in hardirq
        context. If they are not, it is probably a good idea to disable hardirqs
        when calling ethtool_mmsv_event_handle(), because the library does not
        do so.
    
        Also, the MM software verification process has no business with the
        tx_min_frag_size, that is all the driver's to handle.
    
        Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
        Co-developed-by: Choong Yong Liang <yong.liang.choong@linux.intel.com>
        Signed-off-by: Choong Yong Liang <yong.liang.choong@linux.intel.com>
        Tested-by: Choong Yong Liang <yong.liang.choong@linux.intel.com>
        Tested-by: Furong Xu <0x1207@gmail.com>
        Reviewed-by: Vladimir Oltean <vladimir.oltean@nxp.com>
        Signed-off-by: Faizal Rahim <faizal.abdul.rahim@linux.intel.com>
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -897,7 +897,7 @@ static void stmmac_mac_link_down(struct phylink_config *config,
 		stmmac_set_eee_pls(priv, priv->hw, false);
 
 	if (stmmac_fpe_supported(priv))
-		stmmac_fpe_link_state_handle(priv, false);
+		ethtool_mmsv_link_state_handle(&priv->fpe_cfg.mmsv, false);
 }
 
 static void stmmac_mac_link_up(struct phylink_config *config,
@@ -1015,7 +1015,7 @@ static void stmmac_mac_link_up(struct phylink_config *config,
 		stmmac_set_eee_pls(priv, priv->hw, true);
 
 	if (stmmac_fpe_supported(priv))
-		stmmac_fpe_link_state_handle(priv, true);
+		ethtool_mmsv_link_state_handle(&priv->fpe_cfg.mmsv, true);
 
 	if (priv->plat->flags & STMMAC_FLAG_HWTSTAMP_CORRECT_LATENCY)
 		stmmac_hwtstamp_correct_latency(priv, priv);
@@ -4143,7 +4143,7 @@ static void __stmmac_release(struct net_device *dev)
 	stmmac_release_ptp(priv);
 
 	if (stmmac_fpe_supported(priv))
-		timer_shutdown_sync(&priv->fpe_cfg.verify_timer);
+		ethtool_mmsv_stop(&priv->fpe_cfg.mmsv);
 }
 
 /**
@@ -7856,7 +7856,7 @@ int stmmac_suspend(struct device *dev)
 	rtnl_unlock();
 
 	if (stmmac_fpe_supported(priv))
-		timer_shutdown_sync(&priv->fpe_cfg.verify_timer);
+		ethtool_mmsv_stop(&priv->fpe_cfg.mmsv);
 
 suspend_bsp:
 	if (priv->plat->suspend)
