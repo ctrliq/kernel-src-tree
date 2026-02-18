04763adfcc4ed4739ccff944d6615ba151c02004
Jerry Snitselaar <jsnitsel@redhat.com>
2026-02-18 14:11:14 -0700

    iommu/amd: Make amd_iommu_make_clear_dte() non-static inline
    JIRA: https://issues.redhat.com/browse/RHEL-147902
    Upstream-Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    commit 11cfa782f01b2d988175011c19906180a62dd893
    Author: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Date: Thu Jan 15 06:08:04 2026 +0000
    
        iommu/amd: Make amd_iommu_make_clear_dte() non-static inline
    
        This will be reused in a new nested.c file for nested translation.
    
        Also, remove unused function parameter ptr.
    
        Reviewed-by: Nicolin Chen <nicolinc@nvidia.com>
        Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
        Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
        Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
        Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    
    (cherry picked from commit 11cfa782f01b2d988175011c19906180a62dd893)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -2010,14 +2010,6 @@ int amd_iommu_clear_gcr3(struct iommu_dev_data *dev_data, ioasid_t pasid)
 	return ret;
 }
 
-static void make_clear_dte(struct iommu_dev_data *dev_data, struct dev_table_entry *ptr,
-			   struct dev_table_entry *new)
-{
-	/* All existing DTE must have V bit set */
-	new->data128[0] = DTE_FLAG_V;
-	new->data128[1] = 0;
-}
-
 /*
  * Note:
  * The old value for GCR3 table and GPT have been cleared from caller.
@@ -2067,7 +2059,7 @@ static void set_dte_entry(struct amd_iommu *iommu,
 	struct dev_table_entry *dte = &get_dev_table(iommu)[dev_data->devid];
 	struct pt_iommu_amdv1_hw_info pt_info;
 
-	make_clear_dte(dev_data, dte, &new);
+	amd_iommu_make_clear_dte(dev_data, &new);
 
 	if (gcr3_info && gcr3_info->gcr3_tbl)
 		domid = dev_data->gcr3_info.domid;
@@ -2148,9 +2140,8 @@ static void set_dte_entry(struct amd_iommu *iommu,
 static void clear_dte_entry(struct amd_iommu *iommu, struct iommu_dev_data *dev_data)
 {
 	struct dev_table_entry new = {};
-	struct dev_table_entry *dte = &get_dev_table(iommu)[dev_data->devid];
 
-	make_clear_dte(dev_data, dte, &new);
+	amd_iommu_make_clear_dte(dev_data, &new);
 	update_dte256(iommu, dev_data, &new);
 }
 
