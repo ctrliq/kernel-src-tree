65af5dd610055f34aa7a673db4fb28626a091f51
Jerry Snitselaar <jsnitsel@redhat.com>
2026-02-18 12:19:14 -0700

    iommu: Pass in old domain to attach_dev callback functions
    JIRA: https://issues.redhat.com/browse/RHEL-147902
    Upstream-Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Conflicts: Dropped changes to unsupported drivers.
    
    commit fd714986e4e46effa6697b13d32918fc59608ccb
    Author: Nicolin Chen <nicolinc@nvidia.com>
    Date: Wed Oct 22 19:21:09 2025 -0700
    
        iommu: Pass in old domain to attach_dev callback functions
    
        The IOMMU core attaches each device to a default domain on probe(). Then,
        every new "attach" operation has a fundamental meaning of two-fold:
         - detach from its currently attached (old) domain
         - attach to a given new domain
    
        Modern IOMMU drivers following this pattern usually want to clean up the
        things related to the old domain, so they call iommu_get_domain_for_dev()
        to fetch the old domain.
    
        Pass in the old domain pointer from the core to drivers, aligning with the
        set_dev_pasid op that does so already.
    
        Ensure all low-level attach fcuntions in the core can forward the correct
        old domain pointer. Thus, rework those functions as well.
    
        Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
        Reviewed-by: Kevin Tian <kevin.tian@intel.com>
        Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
        Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
        Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
        Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    
    (cherry picked from commit fd714986e4e46effa6697b13d32918fc59608ccb)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -101,7 +101,7 @@ static int iommu_bus_notifier(struct notifier_block *nb,
 			      unsigned long action, void *data);
 static void iommu_release_device(struct device *dev);
 static int __iommu_attach_device(struct iommu_domain *domain,
-				 struct device *dev);
+				 struct device *dev, struct iommu_domain *old);
 static int __iommu_attach_group(struct iommu_domain *domain,
 				struct iommu_group *group);
 static struct iommu_domain *__iommu_paging_domain_alloc_flags(struct device *dev,
@@ -115,6 +115,7 @@ enum {
 static int __iommu_device_set_domain(struct iommu_group *group,
 				     struct device *dev,
 				     struct iommu_domain *new_domain,
+				     struct iommu_domain *old_domain,
 				     unsigned int flags);
 static int __iommu_group_set_domain_internal(struct iommu_group *group,
 					     struct iommu_domain *new_domain,
@@ -555,7 +556,8 @@ static void iommu_deinit_device(struct device *dev)
 		    release_domain == ops->blocked_domain)
 			release_domain = ops->identity_domain;
 
-		release_domain->ops->attach_dev(release_domain, dev);
+		release_domain->ops->attach_dev(release_domain, dev,
+						group->domain);
 	}
 
 	if (ops->release_device)
@@ -641,7 +643,8 @@ static int __iommu_probe_device(struct device *dev, struct list_head *group_list
 	if (group->default_domain)
 		iommu_create_device_direct_mappings(group->default_domain, dev);
 	if (group->domain) {
-		ret = __iommu_device_set_domain(group, dev, group->domain, 0);
+		ret = __iommu_device_set_domain(group, dev, group->domain, NULL,
+						0);
 		if (ret)
 			goto err_remove_gdev;
 	} else if (!group->default_domain && !group_list) {
@@ -2128,14 +2131,14 @@ static void __iommu_group_set_core_domain(struct iommu_group *group)
 }
 
 static int __iommu_attach_device(struct iommu_domain *domain,
-				 struct device *dev)
+				 struct device *dev, struct iommu_domain *old)
 {
 	int ret;
 
 	if (unlikely(domain->ops->attach_dev == NULL))
 		return -ENODEV;
 
-	ret = domain->ops->attach_dev(domain, dev);
+	ret = domain->ops->attach_dev(domain, dev, old);
 	if (ret)
 		return ret;
 	dev->iommu->attach_deferred = 0;
@@ -2184,7 +2187,7 @@ EXPORT_SYMBOL_GPL(iommu_attach_device);
 int iommu_deferred_attach(struct device *dev, struct iommu_domain *domain)
 {
 	if (dev->iommu && dev->iommu->attach_deferred)
-		return __iommu_attach_device(domain, dev);
+		return __iommu_attach_device(domain, dev, NULL);
 
 	return 0;
 }
@@ -2297,6 +2300,7 @@ EXPORT_SYMBOL_GPL(iommu_attach_group);
 static int __iommu_device_set_domain(struct iommu_group *group,
 				     struct device *dev,
 				     struct iommu_domain *new_domain,
+				     struct iommu_domain *old_domain,
 				     unsigned int flags)
 {
 	int ret;
@@ -2322,7 +2326,7 @@ static int __iommu_device_set_domain(struct iommu_group *group,
 		dev->iommu->attach_deferred = 0;
 	}
 
-	ret = __iommu_attach_device(new_domain, dev);
+	ret = __iommu_attach_device(new_domain, dev, old_domain);
 	if (ret) {
 		/*
 		 * If we have a blocking domain then try to attach that in hopes
@@ -2332,7 +2336,8 @@ static int __iommu_device_set_domain(struct iommu_group *group,
 		if ((flags & IOMMU_SET_DOMAIN_MUST_SUCCEED) &&
 		    group->blocking_domain &&
 		    group->blocking_domain != new_domain)
-			__iommu_attach_device(group->blocking_domain, dev);
+			__iommu_attach_device(group->blocking_domain, dev,
+					      old_domain);
 		return ret;
 	}
 	return 0;
@@ -2379,7 +2384,7 @@ static int __iommu_group_set_domain_internal(struct iommu_group *group,
 	result = 0;
 	for_each_group_device(group, gdev) {
 		ret = __iommu_device_set_domain(group, gdev->dev, new_domain,
-						flags);
+						group->domain, flags);
 		if (ret) {
 			result = ret;
 			/*
@@ -2414,7 +2419,7 @@ err_revert:
 		 */
 		if (group->domain)
 			WARN_ON(__iommu_device_set_domain(
-				group, gdev->dev, group->domain,
+				group, gdev->dev, group->domain, new_domain,
 				IOMMU_SET_DOMAIN_MUST_SUCCEED));
 	}
 	return ret;
