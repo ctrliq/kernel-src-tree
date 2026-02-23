d628bcf2f52d16155a313ed75b03184b8888aa9b
Thomas Huth <thuth@redhat.com>
2026-02-23 20:33:18 +0100

    s390/uv: Convert PG_arch_1 users to only work on small folios
    JIRA: https://issues.redhat.com/browse/RHEL-143730
    
    commit 036c0e104bd2cf45304a3795fd8ec985ea01bfe8
    Author: David Hildenbrand <david@redhat.com>
    Date:   Wed May 8 20:29:49 2024 +0200
    
        s390/uv: Convert PG_arch_1 users to only work on small folios
    
        Now that make_folio_secure() may only set PG_arch_1 for small folios,
        let's convert relevant remaining UV code to only work on (small) folios
        and simply reject large folios early. This way, we'll never end up
        touching PG_arch_1 on tail pages of a large folio in UV code.
    
        The folio_get()/folio_put() for functions that are documented to already
        hold a folio reference look weird; likely they are required to make
        concurrent gmap_make_secure() back off because the caller might only hold
        an implicit reference due to the page mapping. So leave that alone for now.
    
        Reviewed-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
        Signed-off-by: David Hildenbrand <david@redhat.com>
        Link: https://lore.kernel.org/r/20240508182955.358628-5-david@redhat.com
        Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
        Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>
    
    Conflicts:
        arch/s390/include/asm/page.h
        (contextual conflict due to out-of-order backport of
         commit c5c3238d9b8cee58cd4b08bbbe9347a94a566390)
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- arch/s390/include/asm/page.h
+++ arch/s390/include/asm/page.h
@@ -251,6 +251,9 @@ static inline unsigned long __phys_addr(unsigned long x, bool is_31bit)
 #define phys_to_pfn(phys)	((phys) >> PAGE_SHIFT)
 #define pfn_to_phys(pfn)	((pfn) << PAGE_SHIFT)
 
+#define phys_to_folio(phys)	page_folio(phys_to_page(phys))
+#define folio_to_phys(page)	pfn_to_phys(folio_pfn(folio))
+
 static inline void *pfn_to_virt(unsigned long pfn)
 {
 	return __va(pfn_to_phys(pfn));
