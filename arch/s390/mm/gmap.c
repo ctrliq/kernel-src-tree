e930a8f83cdcc35f38fbe7781d331b72998bb22e
Thomas Huth <thuth@redhat.com>
2026-03-03 19:08:25 +0100

    s390/cpufeature: Convert MACHINE_HAS_IDTE to cpu_has_idte()
    JIRA: https://issues.redhat.com/browse/RHEL-143730
    
    commit 949b73c9902613bddb789d9375981afbffe596fc
    Author: Heiko Carstens <hca@linux.ibm.com>
    Date:   Fri Feb 7 15:48:55 2025 +0100
    
        s390/cpufeature: Convert MACHINE_HAS_IDTE to cpu_has_idte()
    
        Convert MACHINE_HAS_... to cpu_has_...() which uses test_facility() instead
        of testing the machine_flags lowcore member if the feature is present.
    
        test_facility() generates better code since it results in a static branch
        without accessing memory. The branch is patched via alternatives by the
        decompressor depending on the availability of the required facility.
    
        Reviewed-by: Vasily Gorbik <gor@linux.ibm.com>
        Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
        Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- arch/s390/mm/gmap.c
+++ arch/s390/mm/gmap.c
@@ -8,6 +8,7 @@
  *		 Janosch Frank <frankja@linux.vnet.ibm.com>
  */
 
+#include <linux/cpufeature.h>
 #include <linux/kernel.h>
 #include <linux/pagewalk.h>
 #include <linux/swap.h>
@@ -128,7 +129,7 @@ EXPORT_SYMBOL_GPL(gmap_create);
 
 static void gmap_flush_tlb(struct gmap *gmap)
 {
-	if (MACHINE_HAS_IDTE)
+	if (cpu_has_idte())
 		__tlb_flush_idte(gmap->asce);
 	else
 		__tlb_flush_global();
@@ -2372,7 +2373,7 @@ static void gmap_pmdp_xchg(struct gmap *gmap, pmd_t *pmdp, pmd_t new,
 	if (MACHINE_HAS_TLB_GUEST)
 		__pmdp_idte(gaddr, (pmd_t *)pmdp, IDTE_GUEST_ASCE, gmap->asce,
 			    IDTE_GLOBAL);
-	else if (MACHINE_HAS_IDTE)
+	else if (cpu_has_idte())
 		__pmdp_idte(gaddr, (pmd_t *)pmdp, 0, 0, IDTE_GLOBAL);
 	else
 		__pmdp_csp(pmdp);
@@ -2455,7 +2456,7 @@ void gmap_pmdp_idte_local(struct mm_struct *mm, unsigned long vmaddr)
 			if (MACHINE_HAS_TLB_GUEST)
 				__pmdp_idte(gaddr, pmdp, IDTE_GUEST_ASCE,
 					    gmap->asce, IDTE_LOCAL);
-			else if (MACHINE_HAS_IDTE)
+			else if (cpu_has_idte())
 				__pmdp_idte(gaddr, pmdp, 0, 0, IDTE_LOCAL);
 			*entry = _SEGMENT_ENTRY_EMPTY;
 		}
@@ -2491,7 +2492,7 @@ void gmap_pmdp_idte_global(struct mm_struct *mm, unsigned long vmaddr)
 			if (MACHINE_HAS_TLB_GUEST)
 				__pmdp_idte(gaddr, pmdp, IDTE_GUEST_ASCE,
 					    gmap->asce, IDTE_GLOBAL);
-			else if (MACHINE_HAS_IDTE)
+			else if (cpu_has_idte())
 				__pmdp_idte(gaddr, pmdp, 0, 0, IDTE_GLOBAL);
 			else
 				__pmdp_csp(pmdp);
