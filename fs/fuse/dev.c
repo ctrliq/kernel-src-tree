0793de75d55daad32a6a9ae2f4acd0c514e64955
Jonathan Maple <jmaple@ciq.com>
2026-03-05 14:50:16 -0500

    fuse: rename to fuse_dev_end_requests and make non-static
    jira SECO-478
    RFE: FUSE_IO_URING
    commit-author Bernd Schubert <bschubert@ddn.com>
    commit 92270d076115ae81b83f0605703602d2d5a866e2
    
    This function is needed by fuse_uring.c to clean ring queues,
    so make it non static. Especially in non-static mode the function
    name 'end_requests' should be prefixed with fuse_
    
    	Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    	Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    	Reviewed-by: Joanne Koong <joannelkoong@gmail.com>
    	Reviewed-by: Luis Henriques <luis@igalia.com>
    	Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit 92270d076115ae81b83f0605703602d2d5a866e2)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -7,6 +7,7 @@
 */
 
 #include "fuse_i.h"
+#include "fuse_dev_i.h"
 
 #include <linux/init.h>
 #include <linux/module.h>
@@ -34,8 +35,6 @@ MODULE_ALIAS("devname:fuse");
 #define FUSE_REQ_ID_STEP (1ULL << 1)
 
 static struct kmem_cache *fuse_req_cachep;
-
-static void end_requests(struct list_head *head);
 
 static struct fuse_dev *fuse_get_dev(struct file *file)
 {
@@ -1874,7 +1873,7 @@ static void fuse_resend(struct fuse_conn *fc)
 		spin_unlock(&fiq->lock);
 		list_for_each_entry(req, &to_queue, list)
 			clear_bit(FR_PENDING, &req->flags);
-		end_requests(&to_queue);
+		fuse_dev_end_requests(&to_queue);
 		return;
 	}
 	/* iq and pq requests are both oldest to newest */
@@ -2209,7 +2208,7 @@ static __poll_t fuse_dev_poll(struct file *file, poll_table *wait)
 }
 
 /* Abort all requests on the given list (pending or processing) */
-static void end_requests(struct list_head *head)
+void fuse_dev_end_requests(struct list_head *head)
 {
 	while (!list_empty(head)) {
 		struct fuse_req *req;
@@ -2312,7 +2311,7 @@ void fuse_abort_conn(struct fuse_conn *fc)
 		wake_up_all(&fc->blocked_waitq);
 		spin_unlock(&fc->lock);
 
-		end_requests(&to_end);
+		fuse_dev_end_requests(&to_end);
 	} else {
 		spin_unlock(&fc->lock);
 	}
@@ -2342,7 +2341,7 @@ int fuse_dev_release(struct inode *inode, struct file *file)
 			list_splice_init(&fpq->processing[i], &to_end);
 		spin_unlock(&fpq->lock);
 
-		end_requests(&to_end);
+		fuse_dev_end_requests(&to_end);
 
 		/* Are we the last open device? */
 		if (atomic_dec_and_test(&fc->dev_count)) {
