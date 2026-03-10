9d404d5910f6e6aa81a29bfcaf6045d4923807dc
Phil Auld <pauld@redhat.com>
2026-03-10 20:23:40 +0000

    sched: Start blocked_on chain processing in find_proxy_task()
    JIRA: https://issues.redhat.com/browse/RHEL-147186
    Conflicts: Minor fuzz due to already having 0d4eaf8caf8c ("sched/fair:
    Do not balance task to a throttled cfs_rq").
    
    commit 7de9d4f946383f48ec393b6e9ad0c20e49e174e7
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Sat Jul 12 03:33:49 2025 +0000
    
        sched: Start blocked_on chain processing in find_proxy_task()
    
        Start to flesh out the real find_proxy_task() implementation,
        but avoid the migration cases for now, in those cases just
        deactivate the donor task and pick again.
    
        To ensure the donor task or other blocked tasks in the chain
        aren't migrated away while we're running the proxy, also tweak
        the fair class logic to avoid migrating donor or mutex blocked
        tasks.
    
        [jstultz: This change was split out from the larger proxy patch]
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
        Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
        Signed-off-by: Connor O'Brien <connoro@google.com>
        Signed-off-by: John Stultz <jstultz@google.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
        Link: https://lkml.kernel.org/r/20250712033407.2383110-9-jstultz@google.com
    
    Signed-off-by: Phil Auld <pauld@redhat.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9382,7 +9382,8 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	 * 2) target cfs_rq is in throttled hierarchy, or
 	 * 3) cannot be migrated to this CPU due to cpus_ptr, or
 	 * 4) running (obviously), or
-	 * 5) are cache-hot on their current CPU.
+	 * 5) are cache-hot on their current CPU, or
+	 * 6) are blocked on mutexes (if SCHED_PROXY_EXEC is enabled)
 	 */
 	if ((p->se.sched_delayed) && (env->migration_type != migrate_load))
 		return 0;
@@ -9402,6 +9403,9 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 
 	/* Disregard percpu kthreads; they are where they need to be. */
 	if (kthread_is_per_cpu(p))
+		return 0;
+
+	if (task_is_blocked(p))
 		return 0;
 
 	if (!cpumask_test_cpu(env->dst_cpu, p->cpus_ptr)) {
@@ -9439,7 +9443,8 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	/* Record that we found at least one task that could run on dst_cpu */
 	env->flags &= ~LBF_ALL_PINNED;
 
-	if (task_on_cpu(env->src_rq, p)) {
+	if (task_on_cpu(env->src_rq, p) ||
+	    task_current_donor(env->src_rq, p)) {
 		schedstat_inc(p->stats.nr_failed_migrations_running);
 		return 0;
 	}
@@ -9482,6 +9487,9 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 		schedstat_inc(env->sd->lb_hot_gained[env->idle]);
 		schedstat_inc(p->stats.nr_forced_migrations);
 	}
+
+	WARN_ON(task_current(env->src_rq, p));
+	WARN_ON(task_current_donor(env->src_rq, p));
 
 	deactivate_task(env->src_rq, p, DEQUEUE_NOCLOCK);
 	set_task_cpu(p, env->dst_cpu);
