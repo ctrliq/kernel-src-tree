1f2acb016c3cb1a8a44b899be26e1e93732d6fe1
Jaroslav Kysela <jkysela@redhat.com>
2026-03-11 12:21:08 +0100

    soundwire: cadence_master: make frame index trace more readable
    JIRA: https://issues.redhat.com/browse/RHEL-152847
    
    commit 4be4ac36f6ca5d3854049581be3aca20345bbe01
    Author: Bard Liao <yung-chuan.liao@linux.intel.com>
    Date: Tue Oct 14 11:14:44 2025 +0800
    
        soundwire: cadence_master: make frame index trace more readable
    
        The existing code uses i as the frame index which is 0 based. And
        "bad frame %d/%d", i, num_frames where num_frames is 1 based. Change the
        frame index to i + 1 to align with num_frames and more readable.
    
        Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
        Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
        Link: https://patch.msgid.link/20251014031450.3781789-2-yung-chuan.liao@linux.intel.com
        Signed-off-by: Vinod Koul <vkoul@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- drivers/soundwire/cadence_master.c
+++ drivers/soundwire/cadence_master.c
@@ -2562,14 +2562,14 @@ int sdw_cdns_check_write_response(struct device *dev, u8 *dma_buffer,
 		ret = check_frame_start(header, counter);
 		if (ret < 0) {
 			dev_err(dev, "%s: bad frame %d/%d start header %x\n",
-				__func__, i, num_frames, header);
+				__func__, i + 1, num_frames, header);
 			return ret;
 		}
 
 		ret = check_frame_end(footer);
 		if (ret < 0) {
 			dev_err(dev, "%s: bad frame %d/%d end footer %x\n",
-				__func__, i, num_frames, footer);
+				__func__, i + 1, num_frames, footer);
 			return ret;
 		}
 
@@ -2640,7 +2640,7 @@ int sdw_cdns_check_read_response(struct device *dev, u8 *dma_buffer, int dma_buf
 		ret = check_frame_start(header, counter);
 		if (ret < 0) {
 			dev_err(dev, "%s: bad frame %d/%d start header %x\n",
-				__func__, i, num_frames, header);
+				__func__, i + 1, num_frames, header);
 			return ret;
 		}
 
@@ -2655,7 +2655,7 @@ int sdw_cdns_check_read_response(struct device *dev, u8 *dma_buffer, int dma_buf
 
 		if (crc != expected_crc) {
 			dev_err(dev, "%s: bad frame %d/%d crc %#x expected %#x\n",
-				__func__, i, num_frames, crc, expected_crc);
+				__func__, i + 1, num_frames, crc, expected_crc);
 			return -EIO;
 		}
 
@@ -2666,7 +2666,7 @@ int sdw_cdns_check_read_response(struct device *dev, u8 *dma_buffer, int dma_buf
 		ret = check_frame_end(footer);
 		if (ret < 0) {
 			dev_err(dev, "%s: bad frame %d/%d end footer %x\n",
-				__func__, i, num_frames, footer);
+				__func__, i + 1, num_frames, footer);
 			return ret;
 		}
 
