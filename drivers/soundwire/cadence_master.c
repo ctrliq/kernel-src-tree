a6390ab2c66e768b0b3cf66d50c8820f2a44e1aa
Jaroslav Kysela <jkysela@redhat.com>
2026-03-11 12:21:08 +0100

    soundwire: cadence_master: set data_per_frame as frame capability
    JIRA: https://issues.redhat.com/browse/RHEL-152847
    
    commit 5a838e010c64b794ac990e9b19bfb0bf7083a1f2
    Author: Bard Liao <yung-chuan.liao@linux.intel.com>
    Date: Tue Oct 14 11:14:46 2025 +0800
    
        soundwire: cadence_master: set data_per_frame as frame capability
    
        data_per_frame will be used for preparing the TX buffer and we may add
        some fake frames to ensure the data in the buffer will be flushed.
        So that it should indicate the frame capability even if the required
        data bytes are less than the frame capability.
    
        Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
        Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
        Link: https://patch.msgid.link/20251014031450.3781789-4-yung-chuan.liao@linux.intel.com
        Signed-off-by: Vinod Koul <vkoul@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- drivers/soundwire/cadence_master.c
+++ drivers/soundwire/cadence_master.c
@@ -2144,9 +2144,6 @@ int sdw_cdns_bpt_find_buffer_sizes(int command, /* 0: write, 1: read */
 	if (!actual_bpt_bytes)
 		return -EINVAL;
 
-	if (data_bytes < actual_bpt_bytes)
-		actual_bpt_bytes = data_bytes;
-
 	/*
 	 * the caller may want to set the number of bytes per frame,
 	 * allow when possible
@@ -2155,6 +2152,9 @@ int sdw_cdns_bpt_find_buffer_sizes(int command, /* 0: write, 1: read */
 		actual_bpt_bytes = requested_bytes_per_frame;
 
 	*data_per_frame = actual_bpt_bytes;
+
+	if (data_bytes < actual_bpt_bytes)
+		actual_bpt_bytes = data_bytes;
 
 	if (command == 0) {
 		/*
