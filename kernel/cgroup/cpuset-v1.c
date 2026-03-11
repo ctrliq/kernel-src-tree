e6994dbff5d6f24f9a5a871b3579faf02bebd361
Waiman Long <longman@redhat.com>
2026-03-10 23:11:06 -0400

    cpuset: add helpers for cpus read and cpuset_mutex locks
    JIRA: https://issues.redhat.com/browse/RHEL-154974
    
    commit 2c98144fc832b35c4e9293a3bfc518608d6f5145
    Author: Chen Ridong <chenridong@huawei.com>
    Date:   Mon, 25 Aug 2025 03:23:52 +0000
    
        cpuset: add helpers for cpus read and cpuset_mutex locks
    
        cpuset: add helpers for cpus_read_lock and cpuset_mutex locks.
    
        Replace repetitive locking patterns with new helpers:
        - cpuset_full_lock()
        - cpuset_full_unlock()
    
        This makes the code cleaner and ensures consistent lock ordering.
    
        Signed-off-by: Chen Ridong <chenridong@huawei.com>
        Reviewed-by: Waiman Long <longman@redhat.com>
        Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- kernel/cgroup/cpuset-v1.c
+++ kernel/cgroup/cpuset-v1.c
@@ -169,8 +169,7 @@ static int cpuset_write_s64(struct cgroup_subsys_state *css, struct cftype *cft,
 	cpuset_filetype_t type = cft->private;
 	int retval = -ENODEV;
 
-	cpus_read_lock();
-	cpuset_lock();
+	cpuset_full_lock();
 	if (!is_cpuset_online(cs))
 		goto out_unlock;
 
@@ -183,8 +182,7 @@ static int cpuset_write_s64(struct cgroup_subsys_state *css, struct cftype *cft,
 		break;
 	}
 out_unlock:
-	cpuset_unlock();
-	cpus_read_unlock();
+	cpuset_full_unlock();
 	return retval;
 }
 
@@ -453,8 +451,7 @@ static int cpuset_write_u64(struct cgroup_subsys_state *css, struct cftype *cft,
 	cpuset_filetype_t type = cft->private;
 	int retval = 0;
 
-	cpus_read_lock();
-	cpuset_lock();
+	cpuset_full_lock();
 	if (!is_cpuset_online(cs)) {
 		retval = -ENODEV;
 		goto out_unlock;
@@ -490,8 +487,7 @@ static int cpuset_write_u64(struct cgroup_subsys_state *css, struct cftype *cft,
 		break;
 	}
 out_unlock:
-	cpuset_unlock();
-	cpus_read_unlock();
+	cpuset_full_unlock();
 	return retval;
 }
 
