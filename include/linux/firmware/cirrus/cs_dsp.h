2d937bc0d18c546deae406c00b25e1c1a3fb0034
Jaroslav Kysela <jkysela@redhat.com>
2026-03-11 12:18:45 +0100

    firmware: cs_dsp: Store control length as 32-bit
    JIRA: https://issues.redhat.com/browse/RHEL-152847
    
    Assisted-by: Patchpal AI
    
    commit 7584edf15892e29190b2145294cc1680aa142586
    Author: Richard Fitzgerald <rf@opensource.cirrus.com>
    Date: Mon Nov 24 17:15:35 2025 +0000
    
        firmware: cs_dsp: Store control length as 32-bit
    
        The architectures supported by this driver have a maximum of 32-bits
        of address, so we don't need more than 32-bits to store the length of
        control data. Change the length in struct cs_dsp_coeff_ctl to an
        unsigned int instead of a size_t. Also make a corresponding trivial
        change to wm_adsp.c to prevent a compiler warning.
    
        Tested on x86_64 builds this saves at least 4 bytes per control
        (another 4 bytes might be saved if the compiler was inserting padding
        to align the size_t).
    
        Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>
        Link: https://patch.msgid.link/20251124171536.78962-1-rf@opensource.cirrus.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- include/linux/firmware/cirrus/cs_dsp.h
+++ include/linux/firmware/cirrus/cs_dsp.h
@@ -104,7 +104,7 @@ struct cs_dsp_coeff_ctl {
 	struct list_head list;
 	void *cache;
 	unsigned int offset;
-	size_t len;
+	unsigned int len;
 	unsigned int set:1;
 	unsigned int flags;
 	unsigned int type;
