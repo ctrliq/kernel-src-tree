51173f423cfc69d2b291efb5c6b81687d3655e3e
Mark Langsdorf <mlangsdo@redhat.com>
2026-03-16 14:36:14 -0500

    driver core: Check drivers_autoprobe for all added devices
    JIRA: https://issues.redhat.com/browse/RHEL-156429
    
    commit ea34511aaf755349999a1067b2984a541bee1492
    Author: Vincent Liu <vincent.liu@nutanix.com>
    Date: Wed, 26 Nov 2025 15:22:19 +0000
    
    When a device is hot-plugged, the drivers_autoprobe sysfs attribute is
    not checked (at least for PCI devices). This means that
    drivers_autoprobe is not working as intended, e.g. hot-plugged PCI
    devices will still be autoprobed and bound to drivers even with
    drivers_autoprobe disabled.
    
    The problem likely started when device_add() was removed from
    pci_bus_add_device() in commit 4f535093cf8f ("PCI: Put pci_dev in device
    tree as early as possible") which means that the check for
    drivers_autoprobe which used to happen in bus_probe_device() is no
    longer present (previously bus_add_device() calls bus_probe_device()).
    Conveniently, in commit 91703041697c ("PCI: Allow built-in drivers to
    use async initial probing") device_attach() was replaced with
    device_initial_probe() which faciliates this change to push the check
    for drivers_autoprobe into device_initial_probe().
    
    Make sure all devices check drivers_autoprobe by pushing the
    drivers_autoprobe check into device_initial_probe(). This will only
    affect devices on the PCI bus for now as device_initial_probe() is only
    called by pci_bus_add_device() and bus_probe_device(), but
    bus_probe_device() already checks for autoprobe, so callers of
    bus_probe_device() should not observe changes on autoprobing.
    Note also that pushing this check into device_initial_probe() rather
    than device_attach() makes it only affect automatic probing of
    drivers (e.g. when a device is hot-plugged), userspace can still choose
    to manually bind a driver by writing to drivers_probe sysfs attribute,
    even with autoprobe disabled.
    
    Any future callers of device_initial_probe() will respect the
    drivers_autoprobe sysfs attribute, which is the intended purpose of
    drivers_autoprobe.
    
    Signed-off-by: Vincent Liu <vincent.liu@nutanix.com>
    Link: https://patch.msgid.link/20251022120740.2476482-1-vincent.liu@nutanix.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -1073,7 +1073,15 @@ EXPORT_SYMBOL_GPL(device_attach);
 
 void device_initial_probe(struct device *dev)
 {
-	__device_attach(dev, true);
+	struct subsys_private *sp = bus_to_subsys(dev->bus);
+
+	if (!sp)
+		return;
+
+	if (sp->drivers_autoprobe)
+		__device_attach(dev, true);
+
+	subsys_put(sp);
 }
 
 /*
