07cab20c67783f26be3c02f565fc52fe4baa4982
Tomas Glozar <tglozar@redhat.com>
2026-03-16 16:30:08 +0100

    rtla: Set stop threshold after all instances are enabled
    JIRA: https://redhat.atlassian.net/browse/RHEL-151369
    
    Conflicts: Context around moving setting stop threshold included loading
    BPF action program.
    
    commit c219d4ee1d63b772d5fa8ed453b9cec18a9e2f6a
    Author: Crystal Wood <crwood@redhat.com>
    Date:   Wed Nov 12 09:25:29 2025 -0600
    
        rtla: Set stop threshold after all instances are enabled
    
        This avoids startup races where one of the instances hit a threshold
        before all instances were enabled, and thus tracing stops without
        the relevant event.  In particular, this is not uncommon with the
        tests that set a very tight threshold and then complain if there's
        no analysis.
    
        This also ensures that we don't stop tracing during a warmup.
    
        The downside is a small chance of having an event over the threshold
        early in the output, without stopping on it, which could cause user
        confusion.  This should be less likely if the warmup feature is used, but
        that doesn't eliminate the race window, just the odds of an unusual spike
        right at that moment.
    
        Signed-off-by: Crystal Wood <crwood@redhat.com>
        Link: https://lore.kernel.org/r/20251112152529.956778-6-crwood@redhat.com
        Signed-off-by: Tomas Glozar <tglozar@redhat.com>
    
    Signed-off-by: Tomas Glozar <tglozar@redhat.com>

--- tools/tracing/rtla/src/timerlat.c
+++ tools/tracing/rtla/src/timerlat.c
@@ -48,24 +48,6 @@ timerlat_apply_config(struct osnoise_tool *tool, struct timerlat_params *params)
 		}
 	}
 
-	if (params->mode != TRACING_MODE_BPF) {
-		/*
-		 * In tracefs and mixed mode, timerlat tracer handles stopping
-		 * on threshold
-		 */
-		retval = osnoise_set_stop_us(tool->context, params->common.stop_us);
-		if (retval) {
-			err_msg("Failed to set stop us\n");
-			goto out_err;
-		}
-
-		retval = osnoise_set_stop_total_us(tool->context, params->common.stop_total_us);
-		if (retval) {
-			err_msg("Failed to set stop total us\n");
-			goto out_err;
-		}
-	}
-
 	/* Check if BPF action program is requested but BPF is not available */
 	if (params->bpf_action_program) {
 		if (params->mode == TRACING_MODE_TRACEFS) {
@@ -192,6 +174,16 @@ int timerlat_enable(struct osnoise_tool *tool)
 			err_msg("Error attaching BPF program\n");
 			return retval;
 		}
+	}
+
+	/*
+	 * In tracefs and mixed mode, timerlat tracer handles stopping
+	 * on threshold
+	 */
+	if (params->mode != TRACING_MODE_BPF) {
+		retval = osn_set_stop(tool);
+		if (retval)
+			return retval;
 	}
 
 	return 0;
