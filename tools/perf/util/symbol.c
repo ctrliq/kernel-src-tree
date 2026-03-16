f80f37a578ff53f853fbab095ca78afdd524037a
Trevor Allison <tallison@redhat.com>
2026-03-16 12:39:57 -0400

    perf tools: Use machine->root_dir to find /proc/kallsyms
    JIRA: https://redhat.atlassian.net/browse/RHEL-140195
    
    upstream
    ========
    commit 4fba95fc383f9231ba486512ee1c9d60b8f4a9a4
    Author: Namhyung Kim <namhyung@kernel.org>
    Date: Tue Dec 2 15:57:17 2025 -0800
    
    description
    ===========
    This is for test functions to find the kallsyms correctly.  It can find
    the machine from the kernel maps and use its root_dir.  This is helpful
    to setup fake /proc directory for testing.
    
        Reviewed-by: Ian Rogers <irogers@google.com>
        Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    
    Signed-off-by: Trevor Allison <tallison@redhat.com>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -2007,6 +2007,7 @@ static char *dso__find_kallsyms(struct dso *dso, struct map *map)
 	char sbuild_id[SBUILD_ID_SIZE];
 	bool is_host = false;
 	char path[PATH_MAX];
+	struct maps *kmaps = map__kmaps(map);
 
 	if (!dso__has_build_id(dso)) {
 		/*
@@ -2043,8 +2044,13 @@ static char *dso__find_kallsyms(struct dso *dso, struct map *map)
 		return strdup(path);
 
 	/* Use current /proc/kallsyms if possible */
-	if (is_host) {
 proc_kallsyms:
+	if (kmaps) {
+		struct machine *machine = maps__machine(kmaps);
+
+		scnprintf(path, sizeof(path), "%s/proc/kallsyms", machine->root_dir);
+		return strdup(path);
+	} else if (is_host) {
 		return strdup("/proc/kallsyms");
 	}
 
