75af892372d5c407efec46c58a66ef71e231ddb5
Phil Auld <pauld@redhat.com>
2026-03-18 13:38:22 +0000

    sched_ext: Use kvfree_rcu() to release per-cpu ksyncs object
    JIRA: https://issues.redhat.com/browse/RHEL-151616
    
    commit 348d3c587ac6fa5aaa24ab5c71b7b5635c47e765
    Author: Zqiang <qiang.zhang@linux.dev>
    Date:   Mon Nov 17 20:53:11 2025 +0800
    
        sched_ext: Use kvfree_rcu() to release per-cpu ksyncs object
    
        The free_kick_syncs_rcu() rcu-callback only invoke kvfree() to
        release per-cpu ksyncs object, this can use kvfree_rcu() replace
        call_rcu() to release per-cpu ksyncs object in the free_kick_syncs().
    
        Signed-off-by: Zqiang <qiang.zhang@linux.dev>
        Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Signed-off-by: Phil Auld <pauld@redhat.com>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -4194,13 +4194,6 @@ static const char *scx_exit_reason(enum scx_exit_kind kind)
 	}
 }
 
-static void free_kick_syncs_rcu(struct rcu_head *rcu)
-{
-	struct scx_kick_syncs *ksyncs = container_of(rcu, struct scx_kick_syncs, rcu);
-
-	kvfree(ksyncs);
-}
-
 static void free_kick_syncs(void)
 {
 	int cpu;
@@ -4211,7 +4204,7 @@ static void free_kick_syncs(void)
 
 		to_free = rcu_replace_pointer(*ksyncs, NULL, true);
 		if (to_free)
-			call_rcu(&to_free->rcu, free_kick_syncs_rcu);
+			kvfree_rcu(to_free, rcu);
 	}
 }
 
