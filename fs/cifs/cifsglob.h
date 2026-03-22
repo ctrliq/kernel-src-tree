9c4a232038075c4cca24809b0136e0c0d2f35bc0
Marcin Wcisło <marcin.wcislo@conclusive.pl>
2026-03-23 00:26:48 +0100

    cifs: fix potential use-after-free bugs in TCP_Server_Info::hostname
    jira VULN-169227
    cve CVE-2023-53751
    commit-author Paulo Alcantara <pc@manguebit.com>
    commit 90c49fce1c43e1cc152695e20363ff5087897c09
    upstream-diff |
      fs/cifs/cifsglob.h
            Added `srv_lock' to the `TCP_Server_Info' struct as it was done in
            the non-backported commit d7d7a66aacd6fd8ca57baf08a7bac5421282f6f8
            ("cifs: avoid use of global locks for high contention data").
      fs/cifs/cifs_debug.c
            Ignored changes in `cifs_debug_data_proc_show()' because the code
            using `hostname' was only added in the non-backported commit
            40f077a02bf9d70719128d2a807e28a3503711eb ("cifs: clarify hostname
            vs ip address in /proc/fs/cifs/DebugData").
      fs/cifs/connect.c
            - Changed the `reconn_set_next_dfs_target()' function instead of
              `__reconnect_target_unlocked()' which doesn't exist in LTS
              8.6. The `__reconnect_target_unlocked()' is where the `hostname'
              is "updated once or many times during reconnect" (see original
              commit message). The "reconnect" refers to the function
              `cifs_reconnect()', which calls `__reconnect_target_unlocked()'
              on a third level down the call tree.  Relative to LTS 8.6 the
              `cifs_reconnect()' underwent major rewrites in the commits
              bbcce368044572d0802c3bbb8ef3fe98f581d803 ("cifs: split out dfs
              code from cifs_reconnect()") and
              c88f7dcd6d6429197fc2fd87b54a894ffcd48e8e ("cifs: support nested
              dfs links over reconnect"). In LTS 8.6 the updating of
              `hostname' can be tracked down to the
              `reconn_set_next_dfs_target()' function called by
              `cifs_reconnect()'. Apart from guarding the `server->hostname'
              value change some elements of the commit bbcce3680 were included
              as well:
              - Used helper local variable `hostname' to store the value of
                `extract_hostname()' to avoid calling it while locked, which
                could lead to a schedule while atomic.
              - Avoided assigning to `server->hostname' the result of
                `extract_hostname()' call in case it ended with
                error. Otherwise `server->hostname' would contain
                rubbish. Added a debug message informing about that case.
            - Added initialization of `TCP_Server_Info::srv_lock' in
              `cifs_get_tcp_session()' as it's done in d7d7a66aa.
            - In `match_server()' sandwiched the `strcasecmp(server->hostname,
              ...)' call in the lock/unlock pair of `server->srv_lock',
              because in LTS 8.6 the `match_server()' is not called with the
              `server->srv_lock' held and yet this is the lock chosen to
              protect the `server->hostname' field.
      fs/cifs/sess.c
            Ignored changes in `cifs_try_adding_channels()' because the code
            using `hostname' was only added in the non-backported commit
            9c2dc11df50d1c8537075ff6b98472198e24438e ("smb3: do not attempt
            multichannel to server which does not support it"). As a result no
            changes to the file have been made.
    
    TCP_Server_Info::hostname may be updated once or many times during
    reconnect, so protect its access outside reconnect path as well and
    then prevent any potential use-after-free bugs.
    
    	Cc: stable@vger.kernel.org
    	Signed-off-by: Paulo Alcantara (SUSE) <pc@manguebit.com>
    	Signed-off-by: Steve French <stfrench@microsoft.com>
    (cherry picked from commit 90c49fce1c43e1cc152695e20363ff5087897c09)
    	Signed-off-by: Marcin Wcisło <marcin.wcislo@conclusive.pl>

--- fs/cifs/cifsglob.h
+++ fs/cifs/cifsglob.h
@@ -580,6 +580,7 @@ inc_rfc1001_len(void *buf, int count)
 struct TCP_Server_Info {
 	struct list_head tcp_ses_list;
 	struct list_head smb_ses_list;
+	spinlock_t srv_lock;  /* protect anything here that is not protected */
 	int srv_count; /* reference counter */
 	/* 15 character server name + 0x20 16th byte indicating type = srv */
 	char server_RFC1001_name[RFC1001_NAME_LEN_WITH_NULL];
