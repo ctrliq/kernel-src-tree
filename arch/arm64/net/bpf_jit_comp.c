0a166b454f30afa1d8ddd06d76f4236aa993915c
Ines Qian <iqian@redhat.com>
2026-03-23 17:55:11 -0400

    bpf, arm64: Remove duplicated bpf_flush_icache()
    JIRA: https://issues.redhat.com/browse/RHEL-78291
    
    commit 929adf8838f7765f0bd57235f0e6f365ff628a0d
    Author: Hengqi Chen <hengqi.chen@gmail.com>
    Date:   Thu Sep 4 07:57:03 2025 +0000
    
        bpf, arm64: Remove duplicated bpf_flush_icache()
        
        The bpf_flush_icache() is done by bpf_arch_text_copy() already.
        Remove the duplicated one in arch_prepare_bpf_trampoline().
        
        Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>
        Acked-by: Puranjay Mohan <puranjay@kernel.org>
        Link: https://lore.kernel.org/r/20250904075703.49404-1-hengqi.chen@gmail.com
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Ines Qian <iqian@redhat.com>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -2751,7 +2751,6 @@ int arch_prepare_bpf_trampoline(struct bpf_tramp_image *im, void *ro_image,
 		goto out;
 	}
 
-	bpf_flush_icache(ro_image, ro_image + size);
 out:
 	kvfree(image);
 	return ret;
