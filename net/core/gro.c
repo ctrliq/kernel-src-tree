a044a85adaf51fc3c4a1cbaad0b8f8e831bddac5
Paolo Abeni <pabeni@redhat.com>
2026-03-31 18:48:13 +0200

    net: gro: clear skb_shinfo(skb)->hwtstamps in napi_reuse_skb()
    JIRA: https://issues.redhat.com/browse/RHEL-152734
    
    Upstream commit:
    commit d0d3e9c2867b32c9c70e39e74b9425871cf0042a
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Wed Oct 15 06:32:21 2025 +0000
    
        net: gro: clear skb_shinfo(skb)->hwtstamps in napi_reuse_skb()
    
        Some network drivers assume this field is zero after napi_get_frags().
    
        We must clear it in napi_reuse_skb() otherwise the following can happen:
    
        1) A packet is received, and skb_shinfo(skb)->hwtstamps is populated
           because a bit in the receive descriptor announced hwtstamp
           availability for this packet.
    
        2) Packet is given to gro layer via napi_gro_frags().
    
        3) Packet is merged to a prior one held in GRO queues.
    
        4) skb is saved after some cleanup in napi->skb via a call
           to napi_reuse_skb().
    
        5) Next packet is received 10 seconds later, gets the recycled skb
           from napi_get_frags().
    
        6) The receive descriptor does not announce hwtstamp availability.
           Driver does not clear shinfo->hwtstamps.
    
        7) We have in shinfo->hwtstamps an old timestamp.
    
        Fixes: ac45f602ee3d ("net: infrastructure for hardware time stamping")
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reviewed-by: Alexander Lobakin <aleksander.lobakin@intel.com>
        Link: https://patch.msgid.link/20251015063221.4171986-1-edumazet@google.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/core/gro.c
+++ net/core/gro.c
@@ -637,6 +637,8 @@ EXPORT_SYMBOL(gro_receive_skb);
 
 static void napi_reuse_skb(struct napi_struct *napi, struct sk_buff *skb)
 {
+	struct skb_shared_info *shinfo;
+
 	if (unlikely(skb->pfmemalloc)) {
 		consume_skb(skb);
 		return;
@@ -653,8 +655,12 @@ static void napi_reuse_skb(struct napi_struct *napi, struct sk_buff *skb)
 
 	skb->encapsulation = 0;
 	skb->ip_summed = CHECKSUM_NONE;
-	skb_shinfo(skb)->gso_type = 0;
-	skb_shinfo(skb)->gso_size = 0;
+
+	shinfo = skb_shinfo(skb);
+	shinfo->gso_type = 0;
+	shinfo->gso_size = 0;
+	shinfo->hwtstamps.hwtstamp = 0;
+
 	if (unlikely(skb->slow_gro)) {
 		skb_orphan(skb);
 		skb_ext_reset(skb);
