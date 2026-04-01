8e1fd74da2cc961b49ebc3683f74fbd3b3bd0be4
Mark Langsdorf <mlangsdo@redhat.com>
2026-04-01 10:02:47 -0400

    ACPI: EC: Register a platform device for ECDT EC
    JIRA: https://issues.redhat.com/browse/RHEL-156421
    
    commit fe9542b8b53cd72cd6304278052a3d7db3f6c824
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date: Mon, 29 Dec 2025 21:04:23 +0000
    
    To facilitate converting the ACPI EC driver into a platform one,
    modify acpi_bus_register_early_device(), used by acpi_ec_ecdt_start()
    for creating a struct acpi_device to represent the "early" EC based
    on the ECDT ACPI table, to carry out the default ACPI enumeration for
    the given device which will cause a platform device to be registered
    for it.
    
    No intentional functional impact.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/2397353.ElGaqSPkdT@rafael.j.wysocki
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -2750,6 +2750,8 @@ int acpi_bus_register_early_device(int type)
 	if (result)
 		return result;
 
+	acpi_default_enumeration(device);
+
 	device->flags.match_driver = true;
 	return device_attach(&device->dev);
 }
