5df5e8d2b5eed2537501d30c8ec1ace718ca4256
Steve Best <sbest@redhat.com>
2026-04-02 11:34:05 -0400

    cpufreq: intel_pstate: Rearrange variable declaration involving __free()
    JIRA: https://issues.redhat.com/browse/RHEL-133082
    
    commit 5f2ec3536d7933a804d28d2541e4e918ba32587b
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Wed Sep 3 16:59:47 2025 +0200
    
        cpufreq: intel_pstate: Rearrange variable declaration involving __free()
    
        Follow cleanup.h recommendations and define and assign a variable
        in one statement when __free() is used.
    
        No intentional functional impact.
    
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
        Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
        Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
        Reviewed-by: Zihuan Zhang <zhangzihuan@kylinos.cn>
        Link: https://patch.msgid.link/2251447.irdbgypaU6@rafael.j.wysocki
    
    Signed-off-by: Steve Best <sbest@redhat.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -1485,9 +1485,7 @@ static void intel_pstate_update_policies(void)
 
 static bool intel_pstate_update_max_freq(struct cpudata *cpudata)
 {
-	struct cpufreq_policy *policy __free(put_cpufreq_policy);
-
-	policy = cpufreq_cpu_get(cpudata->cpu);
+	struct cpufreq_policy *policy __free(put_cpufreq_policy) = cpufreq_cpu_get(cpudata->cpu);
 	if (!policy)
 		return false;
 
