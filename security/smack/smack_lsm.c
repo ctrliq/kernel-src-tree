0c0289393f51c3e3d6754ffc23b9176774d5937b
Ricardo Robaina <rrobaina@redhat.com>
2026-04-07 11:31:53 -0300

    audit: add record for multiple object contexts
    JIRA: https://issues.redhat.com/browse/RHEL-145403
    Conflicts:
        include/uapi/linux/audit.h:
          Context conflict due to missing upstream commits:
          33e65b0d3add ("landlock: Add AUDIT_LANDLOCK_ACCESS and log ptrace denials")
          1d636984e088 ("landlock: Add AUDIT_LANDLOCK_DOMAIN and log domain status")
    
    commit 0ffbc876d03c80b83d70aeefac7bbb94a9f4e135
    Author: Casey Schaufler <casey@schaufler-ca.com>
    Date:   Sat Aug 16 10:28:59 2025 -0700
    
        audit: add record for multiple object contexts
    
        Create a new audit record AUDIT_MAC_OBJ_CONTEXTS.
        An example of the MAC_OBJ_CONTEXTS record is:
    
            type=MAC_OBJ_CONTEXTS
              msg=audit(1601152467.009:1050):
              obj_selinux=unconfined_u:object_r:user_home_t:s0
    
        When an audit event includes a AUDIT_MAC_OBJ_CONTEXTS record
        the "obj=" field in other records in the event will be "obj=?".
        An AUDIT_MAC_OBJ_CONTEXTS record is supplied when the system has
        multiple security modules that may make access decisions based
        on an object security context.
    
        Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
        [PM: subj tweak, audit example readability indents]
        Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    Signed-off-by: Ricardo Robaina <rrobaina@redhat.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -5306,7 +5306,9 @@ static __init int smack_init(void)
 	init_smack_known_list();
 
 	/* Inform the audit system that secctx is used */
-	audit_cfg_lsm(&smack_lsmid, AUDIT_CFG_LSM_SECCTX_SUBJECT);
+	audit_cfg_lsm(&smack_lsmid,
+		      AUDIT_CFG_LSM_SECCTX_SUBJECT |
+		      AUDIT_CFG_LSM_SECCTX_OBJECT);
 
 	return 0;
 }
