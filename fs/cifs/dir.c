e9dfd907f082a1567f429396a2b80cdc38cb6047
Jonathan Maple <jmaple@ciq.com>
2026-04-07 15:20:33 -0400

    smb: client: get rid of dfs naming in automount code
    jira KERNEL-814
    Rebuild_History Non-Buildable kernel-4.18.0-553.115.1.el8_10
    commit-author Paulo Alcantara <pc@manguebit.com>
    commit 0a049935e47e30c62df165e4d6ff563ff9c002d5
    
    Automount code will handle both DFS links and reparse mount points.
    
    Also, get rid of BUG_ON() in cifs_release_automount_timer() while
    we're at it.
    
    	Signed-off-by: Paulo Alcantara (SUSE) <pc@manguebit.com>
    	Signed-off-by: Steve French <stfrench@microsoft.com>
    (cherry picked from commit 0a049935e47e30c62df165e4d6ff563ff9c002d5)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- fs/cifs/dir.c
+++ fs/cifs/dir.c
@@ -772,7 +772,7 @@ cifs_d_revalidate(struct dentry *direntry, unsigned int flags)
 
 const struct dentry_operations cifs_dentry_ops = {
 	.d_revalidate = cifs_d_revalidate,
-	.d_automount = cifs_dfs_d_automount,
+	.d_automount = cifs_d_automount,
 /* d_delete:       cifs_d_delete,      */ /* not needed except for debugging */
 };
 
@@ -847,5 +847,5 @@ const struct dentry_operations cifs_ci_dentry_ops = {
 	.d_revalidate = cifs_d_revalidate,
 	.d_hash = cifs_ci_hash,
 	.d_compare = cifs_ci_compare,
-	.d_automount = cifs_dfs_d_automount,
+	.d_automount = cifs_d_automount,
 };
