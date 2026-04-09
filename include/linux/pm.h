36b1925fbc176c2b3a8b1f0a18ab4102ed039c4c
Mark Langsdorf <mlangsdo@redhat.com>
2026-04-09 13:56:02 -0400

    PM: wakeup: Add out-of-band system wakeup support for devices
    JIRA: https://issues.redhat.com/browse/RHEL-156429
    Conflcits:
    	include/linux/pm.h - minor context differences
    
    commit 4acbfb6c116be5989d5a0e38a48deca2d5b8bb92
    Author: Peng Fan <peng.fan@nxp.com>
    Date: Wed, 19 Nov 2025 18:06:49 +0000
    
    Some devices can wake up the system from suspend even when their power
    domains are turned off. This is possible because their system-wakeup logic
    resides in an always-on power domain - indicating that they support
    out-of-band system wakeup.
    
    Currently, PM domain core doesn't power off such devices if they are marked
    as system wakeup sources. To better represent devices with out-of-band
    wakeup capability, this patch introduces a new flag out_band_wakeup in
    'struct dev_pm_info'.
    
    Two helper APIs are added:
     - device_set_out_band_wakeup() - to mark a device as having out-of-band
       wakeup capability.
     - device_out_band_wakeup() - to query the flag.
    
    Allow the PM core and drivers to distinguish between regular and
    out-of-band wakeup sources, enable more accurate power management decision.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Dhruva Gole <d-gole@ti.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -672,6 +672,7 @@ struct dev_pm_info {
 	bool			smart_suspend:1;	/* Owned by the PM core */
 	unsigned int		must_resume:1;	/* Owned by the PM core */
 	unsigned int		may_skip_resume:1;	/* Set by subsystems */
+	bool			out_band_wakeup:1;
 	bool			strict_midlayer:1;
 #else
 	unsigned int		should_wakeup:1;
