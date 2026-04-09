f89f1dfd5e83441dcb1cc251c124533a94830450
Mark Langsdorf <mlangsdo@redhat.com>
2026-04-09 14:12:00 -0400

    driver core: attribute_container: change return type to void
    JIRA: https://issues.redhat.com/browse/RHEL-156429
    
    commit 7a96ccc82c106b763dd561cb87f9c7261dff4f0d
    Author: Daniel Gomez <da.gomez@samsung.com>
    Date: Fri, 16 Jan 2026 16:46:14 +0000
    
    attribute_container_register() has always returned 0 since its
    introduction in commit 06ff5a987e ("Add attribute container to generic
    device model") in the historical Linux tree [1]. Convert the return type
    to void and update all callers.
    
    This removes dead code where callers checked for errors that could never
    occur.
    
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/tglx/history.git [1]
    
    Signed-off-by: Daniel Gomez <da.gomez@samsung.com>
    Link: https://patch.msgid.link/20251220-dev-attribute-container-linux-scsi-v1-1-d58fcd03bf21@samsung.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- include/linux/transport_class.h
+++ include/linux/transport_class.h
@@ -81,9 +81,9 @@ transport_unregister_device(struct device *dev)
 	transport_destroy_device(dev);
 }
 
-static inline int transport_container_register(struct transport_container *tc)
+static inline void transport_container_register(struct transport_container *tc)
 {
-	return attribute_container_register(&tc->ac);
+	attribute_container_register(&tc->ac);
 }
 
 static inline void transport_container_unregister(struct transport_container *tc)
@@ -93,7 +93,7 @@ static inline void transport_container_unregister(struct transport_container *tc
 }
 
 int transport_class_register(struct transport_class *);
-int anon_transport_class_register(struct anon_transport_class *);
+void anon_transport_class_register(struct anon_transport_class *);
 void transport_class_unregister(struct transport_class *);
 void anon_transport_class_unregister(struct anon_transport_class *);
 
