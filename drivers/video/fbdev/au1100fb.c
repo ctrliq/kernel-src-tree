3b27c7bb1a6ad4b28e2ffd477e53acc01654c216
Mika Penttilä <mpenttil@redhat.com>
2026-04-20 09:29:42 +0300

    fbdev: au1100fb: Check return value of clk_enable() in .resume()
    JIRA: https://issues.redhat.com/browse/RHEL-135262
    Upstream Status: v7.0-rc1
    
    Conflicts:
            drivers/video/fbdev/au1100fb.c
    
    
    commit cbfb9c715f33a07a46d577fe8d62869eb6363064
    Author:     Chen Ni <nichen@iscas.ac.cn>
    AuthorDate: Thu Jan 29 12:07:14 2026 +0800
    Commit:     Helge Deller <deller@gmx.de>
    CommitDate: Sat Feb 14 11:09:47 2026 +0100
    
        Check the return value of clk_enable() in au1100fb_drv_resume() and
        return the error on failure.
        This ensures the system is aware of the resume failure and can track
        its state accurately.
    
        Signed-off-by: Chen Ni <nichen@iscas.ac.cn>
        Acked-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
        Signed-off-by: Helge Deller <deller@gmx.de>
    
    Signed-off-by: Mika Penttilä <mpenttil@redhat.com>

--- drivers/video/fbdev/au1100fb.c
+++ drivers/video/fbdev/au1100fb.c
@@ -571,6 +571,7 @@ int au1100fb_drv_suspend(struct platform_device *dev, pm_message_t state)
 int au1100fb_drv_resume(struct platform_device *dev)
 {
 	struct au1100fb_device *fbdev = platform_get_drvdata(dev);
+	int ret = 0;
 
 	if (!fbdev)
 		return 0;
@@ -578,7 +579,9 @@ int au1100fb_drv_resume(struct platform_device *dev)
 	memcpy(fbdev->regs, &fbregs, sizeof(struct au1100fb_regs));
 
 	if (fbdev->lcdclk)
-		clk_enable(fbdev->lcdclk);
+		ret = clk_enable(fbdev->lcdclk);
+	if (ret)
+		return ret;
 
 	/* Unblank the LCD */
 	au1100fb_fb_blank(VESA_NO_BLANKING, &fbdev->info);
