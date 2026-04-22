f9c79f4967ffd8ae09d79de857ed2e85ec9fa0dd
Myron Stowe <mstowe@redhat.com>
2026-04-22 11:20:43 -0600

    PCI/pwrctrl: Switch to pwrctrl create, power on/off, destroy APIs
    JIRA: https://issues.redhat.com/browse/RHEL-151449
    Upstream Status: b921aa3f8decae440009134d65399ee9b2425300
    
    Conflicts(s):
      Patching file drivers/pci/bus.c; Hunk #2 FAILED at 360.  This is a
      context only conflict due to out-of-order patch application (with
      upstream commit 51c0996dadae "PCI/PM: Prevent runtime suspend until
      devices are fully initialized").  This is the same situation that
      occurred upstream and was handled within upstream merge commit
      bf37448d9b77 "Merge branch 'pci/pwrctrl'".
    
      File drivers/pci/pwrctrl/pci-pwrctrl-tc9563.c:
      RHEL does not have the file.  As such, its 4 out of 4 hunks were ignored.
    
    
    commit b921aa3f8decae440009134d65399ee9b2425300
    Author: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
    Date:   Thu Jan 15 12:59:04 2026 +0530
    
        PCI/pwrctrl: Switch to pwrctrl create, power on/off, destroy APIs
    
        Adopt pwrctrl APIs to create, power on/off, and destroy pwrctrl devices.
    
        In qcom_pcie_host_init(), call pci_pwrctrl_create_devices() to create
        devices, then pci_pwrctrl_power_on_devices() to power them on, both after
        controller resource initialization. Once successful, deassert PERST# for
        all devices.
    
        In qcom_pcie_host_deinit(), call pci_pwrctrl_power_off_devices() after
        asserting PERST#. Note that pci_pwrctrl_destroy_devices() is not called
        here, as deinit is only invoked during system suspend where device
        destruction is unnecessary. If the driver becomes removable in future,
        pci_pwrctrl_destroy_devices() should be called in the remove() handler.
    
        Remove the old pwrctrl framework code from the PCI core (including
        devlinks) as the new APIs are now the sole consumer of pwrctrl
        functionality. And also do not power on the pwrctrl drivers during probe()
        as this is now handled by the APIs.
    
        Co-developed-by: Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>
        Signed-off-by: Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>
        Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
        Tested-by: Chen-Yu Tsai <wenst@chromium.org>
        Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
        Link: https://patch.msgid.link/20260115-pci-pwrctrl-rework-v5-12-9d26da3ce903@oss.qualcomm.com
    
    Signed-off-by: Myron Stowe <mstowe@redhat.com>

--- drivers/pci/controller/dwc/pcie-qcom.c
+++ drivers/pci/controller/dwc/pcie-qcom.c
@@ -24,6 +24,7 @@
 #include <linux/of_pci.h>
 #include <linux/pci.h>
 #include <linux/pci-ecam.h>
+#include <linux/pci-pwrctrl.h>
 #include <linux/pm_opp.h>
 #include <linux/pm_runtime.h>
 #include <linux/platform_device.h>
@@ -1309,10 +1310,18 @@ static int qcom_pcie_host_init(struct dw_pcie_rp *pp)
 	if (ret)
 		goto err_deinit;
 
+	ret = pci_pwrctrl_create_devices(pci->dev);
+	if (ret)
+		goto err_disable_phy;
+
+	ret = pci_pwrctrl_power_on_devices(pci->dev);
+	if (ret)
+		goto err_pwrctrl_destroy;
+
 	if (pcie->cfg->ops->post_init) {
 		ret = pcie->cfg->ops->post_init(pcie);
 		if (ret)
-			goto err_disable_phy;
+			goto err_pwrctrl_power_off;
 	}
 
 	qcom_pcie_clear_aspm_l0s(pcie->pci);
@@ -1329,6 +1338,11 @@ static int qcom_pcie_host_init(struct dw_pcie_rp *pp)
 
 err_assert_reset:
 	qcom_ep_reset_assert(pcie);
+err_pwrctrl_power_off:
+	pci_pwrctrl_power_off_devices(pci->dev);
+err_pwrctrl_destroy:
+	if (ret != -EPROBE_DEFER)
+		pci_pwrctrl_destroy_devices(pci->dev);
 err_disable_phy:
 	qcom_pcie_phy_power_off(pcie);
 err_deinit:
@@ -1343,6 +1357,12 @@ static void qcom_pcie_host_deinit(struct dw_pcie_rp *pp)
 	struct qcom_pcie *pcie = to_qcom_pcie(pci);
 
 	qcom_ep_reset_assert(pcie);
+
+	/*
+	 * No need to destroy pwrctrl devices as this function only gets called
+	 * during system suspend as of now.
+	 */
+	pci_pwrctrl_power_off_devices(pci->dev);
 	qcom_pcie_phy_power_off(pcie);
 	pcie->cfg->ops->deinit(pcie);
 }
@@ -1950,7 +1970,7 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 
 	ret = dw_pcie_host_init(pp);
 	if (ret) {
-		dev_err(dev, "cannot initialize host\n");
+		dev_err_probe(dev, ret, "cannot initialize host\n");
 		goto err_phy_exit;
 	}
 
