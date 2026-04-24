4d2d440b6cd564d62d38ea754997e9b064a88168
Maxim Levitsky <mlevitsk@redhat.com>
2026-04-24 11:39:55 -0400

    KVM: x86: Rename kvm_{g,s}et_msr()* to show that they emulate guest accesses
    JIRA: https://issues.redhat.com/browse/RHEL-151869
    
    commit d2dcf25a4cf2d9058a866c2237884287209b8d19
    Author: Yang Weijiang <weijiang.yang@intel.com>
    Date:   Mon Aug 11 19:55:09 2025 -0700
    
        KVM: x86: Rename kvm_{g,s}et_msr()* to show that they emulate guest accesses
    
        Rename
                kvm_{g,s}et_msr_with_filter()
                kvm_{g,s}et_msr()
        to
                kvm_emulate_msr_{read,write}
                __kvm_emulate_msr_{read,write}
    
        to make it more obvious that KVM uses these helpers to emulate guest
        behaviors, i.e., host_initiated == false in these helpers.
    
        Suggested-by: Sean Christopherson <seanjc@google.com>
        Suggested-by: Chao Gao <chao.gao@intel.com>
        Signed-off-by: Yang Weijiang <weijiang.yang@intel.com>
        Reviewed-by: Maxim Levitsky <mlevitsk@redhat.com>
        Reviewed-by: Chao Gao <chao.gao@intel.com>
        Tested-by: Mathias Krause <minipli@grsecurity.net>
        Tested-by: John Allen <john.allen@amd.com>
        Signed-off-by: Chao Gao <chao.gao@intel.com>
        Tested-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
        Link: https://lore.kernel.org/r/20250812025606.74625-2-chao.gao@intel.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1947,33 +1947,33 @@ static int kvm_get_msr_ignored_check(struct kvm_vcpu *vcpu,
 				 __kvm_get_msr);
 }
 
-int kvm_get_msr_with_filter(struct kvm_vcpu *vcpu, u32 index, u64 *data)
+int kvm_emulate_msr_read(struct kvm_vcpu *vcpu, u32 index, u64 *data)
 {
 	if (!kvm_msr_allowed(vcpu, index, KVM_MSR_FILTER_READ))
 		return KVM_MSR_RET_FILTERED;
 	return kvm_get_msr_ignored_check(vcpu, index, data, false);
 }
-EXPORT_SYMBOL_GPL(kvm_get_msr_with_filter);
+EXPORT_SYMBOL_GPL(kvm_emulate_msr_read);
 
-int kvm_set_msr_with_filter(struct kvm_vcpu *vcpu, u32 index, u64 data)
+int kvm_emulate_msr_write(struct kvm_vcpu *vcpu, u32 index, u64 data)
 {
 	if (!kvm_msr_allowed(vcpu, index, KVM_MSR_FILTER_WRITE))
 		return KVM_MSR_RET_FILTERED;
 	return kvm_set_msr_ignored_check(vcpu, index, data, false);
 }
-EXPORT_SYMBOL_GPL(kvm_set_msr_with_filter);
+EXPORT_SYMBOL_GPL(kvm_emulate_msr_write);
 
-int kvm_get_msr(struct kvm_vcpu *vcpu, u32 index, u64 *data)
+int __kvm_emulate_msr_read(struct kvm_vcpu *vcpu, u32 index, u64 *data)
 {
 	return kvm_get_msr_ignored_check(vcpu, index, data, false);
 }
-EXPORT_SYMBOL_GPL(kvm_get_msr);
+EXPORT_SYMBOL_GPL(__kvm_emulate_msr_read);
 
-int kvm_set_msr(struct kvm_vcpu *vcpu, u32 index, u64 data)
+int __kvm_emulate_msr_write(struct kvm_vcpu *vcpu, u32 index, u64 data)
 {
 	return kvm_set_msr_ignored_check(vcpu, index, data, false);
 }
-EXPORT_SYMBOL_GPL(kvm_set_msr);
+EXPORT_SYMBOL_GPL(__kvm_emulate_msr_write);
 
 static void complete_userspace_rdmsr(struct kvm_vcpu *vcpu)
 {
@@ -2054,7 +2054,8 @@ static int __kvm_emulate_rdmsr(struct kvm_vcpu *vcpu, u32 msr, int reg,
 	u64 data;
 	int r;
 
-	r = kvm_get_msr_with_filter(vcpu, msr, &data);
+	r = kvm_emulate_msr_read(vcpu, msr, &data);
+
 	if (!r) {
 		trace_kvm_msr_read(msr, data);
 
@@ -2094,7 +2095,7 @@ static int __kvm_emulate_wrmsr(struct kvm_vcpu *vcpu, u32 msr, u64 data)
 {
 	int r;
 
-	r = kvm_set_msr_with_filter(vcpu, msr, data);
+	r = kvm_emulate_msr_write(vcpu, msr, data);
 	if (!r) {
 		trace_kvm_msr_write(msr, data);
 	} else {
@@ -8396,7 +8397,7 @@ static int emulator_get_msr_with_filter(struct x86_emulate_ctxt *ctxt,
 	struct kvm_vcpu *vcpu = emul_to_vcpu(ctxt);
 	int r;
 
-	r = kvm_get_msr_with_filter(vcpu, msr_index, pdata);
+	r = kvm_emulate_msr_read(vcpu, msr_index, pdata);
 	if (r < 0)
 		return X86EMUL_UNHANDLEABLE;
 
@@ -8419,7 +8420,7 @@ static int emulator_set_msr_with_filter(struct x86_emulate_ctxt *ctxt,
 	struct kvm_vcpu *vcpu = emul_to_vcpu(ctxt);
 	int r;
 
-	r = kvm_set_msr_with_filter(vcpu, msr_index, data);
+	r = kvm_emulate_msr_write(vcpu, msr_index, data);
 	if (r < 0)
 		return X86EMUL_UNHANDLEABLE;
 
@@ -8439,7 +8440,7 @@ static int emulator_set_msr_with_filter(struct x86_emulate_ctxt *ctxt,
 static int emulator_get_msr(struct x86_emulate_ctxt *ctxt,
 			    u32 msr_index, u64 *pdata)
 {
-	return kvm_get_msr(emul_to_vcpu(ctxt), msr_index, pdata);
+	return __kvm_emulate_msr_read(emul_to_vcpu(ctxt), msr_index, pdata);
 }
 
 static int emulator_check_rdpmc_early(struct x86_emulate_ctxt *ctxt, u32 pmc)
