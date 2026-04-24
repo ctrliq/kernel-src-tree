5c56fde45624960893dd6c4d546db4d2237034d2
Davide Caratti <dcaratti@redhat.com>
2026-04-24 11:31:56 +0200

    net/tcp: Wire up l3index to TCP-AO
    JIRA: https://redhat.atlassian.net/browse/RHEL-142637
    Upstream Status: net.git commit 248411b8cb8974a1e1c8e43123c1e682fbd64969
    Conflicts:
      - net/ipv4/syncookies.c:
      - net/ipv6/syncookies.c:
        context mismatch because of ed43e76cdcc4 ("tcp: directly drop skb in
        cookie check for ipv6") already backported
    
    commit 248411b8cb8974a1e1c8e43123c1e682fbd64969
    Author: Dmitry Safonov <0x7f454c46@gmail.com>
    Date:   Mon Oct 23 20:22:13 2023 +0100
    
        net/tcp: Wire up l3index to TCP-AO
    
        Similarly how TCP_MD5SIG_FLAG_IFINDEX works for TCP-MD5,
        TCP_AO_KEYF_IFINDEX is an AO-key flag that binds that MKT to a specified
        by L3 ifinndex. Similarly, without this flag the key will work in
        the default VRF l3index = 0 for connections.
        To prevent AO-keys from overlapping, it's restricted to add key B for a
        socket that has key A, which have the same sndid/rcvid and one of
        the following is true:
        - !(A.keyflags & TCP_AO_KEYF_IFINDEX) or !(B.keyflags & TCP_AO_KEYF_IFINDEX)
          so that any key is non-bound to a VRF
        - A.l3index == B.l3index
          both want to work for the same VRF
    
        Additionally, it's restricted to match TCP-MD5 keys for the same peer
        the following way:
        |--------------|--------------------|----------------|---------------|
        |              | MD5 key without    |     MD5 key    |    MD5 key    |
        |              |     l3index        |    l3index=0   |   l3index=N   |
        |--------------|--------------------|----------------|---------------|
        |  TCP-AO key  |                    |                |               |
        |  without     |       reject       |    reject      |   reject      |
        |  l3index     |                    |                |               |
        |--------------|--------------------|----------------|---------------|
        |  TCP-AO key  |                    |                |               |
        |  l3index=0   |       reject       |    reject      |   allow       |
        |--------------|--------------------|----------------|---------------|
        |  TCP-AO key  |                    |                |               |
        |  l3index=N   |       reject       |    allow       |   reject      |
        |--------------|--------------------|----------------|---------------|
    
        This is done with the help of tcp_md5_do_lookup_any_l3index() to reject
        adding AO key without TCP_AO_KEYF_IFINDEX if there's TCP-MD5 in any VRF.
        This is important for case where sysctl_tcp_l3mdev_accept = 1
        Similarly, for TCP-AO lookups tcp_ao_do_lookup() may be used with
        l3index < 0, so that __tcp_ao_key_cmp() will match TCP-AO key in any VRF.
    
        Signed-off-by: Dmitry Safonov <dima@arista.com>
        Acked-by: David Ahern <dsahern@kernel.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>

--- net/ipv6/syncookies.c
+++ net/ipv6/syncookies.c
@@ -141,6 +141,7 @@ struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 	__u8 rcv_wscale;
 	u32 tsoff = 0;
 	SKB_DR(reason);
+	int l3index;
 
 	if (!READ_ONCE(sock_net(sk)->ipv4.sysctl_tcp_syncookies) ||
 	    !th->ack || th->rst)
@@ -223,7 +224,9 @@ struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 	treq->snt_isn = cookie;
 	treq->ts_off = 0;
 	treq->txhash = net_tx_rndhash();
-	tcp_ao_syncookie(sk, skb, treq, AF_INET6);
+
+	l3index = l3mdev_master_ifindex_by_index(sock_net(sk), ireq->ir_iif);
+	tcp_ao_syncookie(sk, skb, treq, AF_INET6, l3index);
 
 	if (IS_ENABLED(CONFIG_SMC))
 		ireq->smc_ok = 0;
