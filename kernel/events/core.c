b3f09b5eae2ed79653f7a5b0e3f6377f31e98776
Maxim Levitsky <mlevitsk@redhat.com>
2026-04-24 11:42:46 -0400

    perf: Clean up mediated vPMU accounting
    JIRA: https://issues.redhat.com/browse/RHEL-151869
    
    commit 3cb3c2f6886f9489df13de8efe7a1e803a3f21ea
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Wed Dec 17 12:08:01 2025 +0100
    
        perf: Clean up mediated vPMU accounting
    
        The mediated_pmu_account_event() and perf_create_mediated_pmu()
        functions implement the exclusion between '!exclude_guest' counters
        and mediated vPMUs. Their implementation is basically identical,
        except mirrored in what they count/check.
    
        Make sure the actual implementations reflect this similarity.
    
        Notably:
         - while perf_release_mediated_pmu() has an underflow check;
           mediated_pmu_unaccount_event() did not.
         - while perf_create_mediated_pmu() has an inc_not_zero() path;
           mediated_pmu_account_event() did not.
    
        Also, the inc_not_zero() path can be outsite of
        perf_mediated_pmu_mutex. The mutex must guard the 0->1 (of either
        nr_include_guest_events or nr_mediated_pmu_vms) transition, but once a
        counter is already non-zero, it can safely be incremented further.
    
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Link: https://patch.msgid.link/20251208115156.GE3707891@noisy.programming.kicks-ass.net
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -6354,8 +6354,10 @@ static int mediated_pmu_account_event(struct perf_event *event)
 	if (!is_include_guest_event(event))
 		return 0;
 
-	guard(mutex)(&perf_mediated_pmu_mutex);
+	if (atomic_inc_not_zero(&nr_include_guest_events))
+		return 0;
 
+	guard(mutex)(&perf_mediated_pmu_mutex);
 	if (atomic_read(&nr_mediated_pmu_vms))
 		return -EOPNOTSUPP;
 
@@ -6366,6 +6368,9 @@ static int mediated_pmu_account_event(struct perf_event *event)
 static void mediated_pmu_unaccount_event(struct perf_event *event)
 {
 	if (!is_include_guest_event(event))
+		return;
+
+	if (WARN_ON_ONCE(!atomic_read(&nr_include_guest_events)))
 		return;
 
 	atomic_dec(&nr_include_guest_events);
@@ -6383,10 +6388,10 @@ static void mediated_pmu_unaccount_event(struct perf_event *event)
  */
 int perf_create_mediated_pmu(void)
 {
-	guard(mutex)(&perf_mediated_pmu_mutex);
 	if (atomic_inc_not_zero(&nr_mediated_pmu_vms))
 		return 0;
 
+	guard(mutex)(&perf_mediated_pmu_mutex);
 	if (atomic_read(&nr_include_guest_events))
 		return -EBUSY;
 
