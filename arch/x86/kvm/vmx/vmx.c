a407d66c1049c676981f986bc5bc68b5a5119e22
Maxim Levitsky <mlevitsk@redhat.com>
2026-04-24 11:40:18 -0400

    KVM: x86: Rework KVM_REQ_MSR_FILTER_CHANGED into a generic RECALC_INTERCEPTS
    JIRA: https://issues.redhat.com/browse/RHEL-151869
    
    commit 6057497336bbfabd3a2f632bba2cd2bfbcb7b304
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Wed Aug 6 12:56:46 2025 -0700
    
        KVM: x86: Rework KVM_REQ_MSR_FILTER_CHANGED into a generic RECALC_INTERCEPTS
    
        Rework the MSR_FILTER_CHANGED request into a more generic RECALC_INTERCEPTS
        request, and expand the responsibilities of vendor code to recalculate all
        intercepts that vary based on userspace input, e.g. instruction intercepts
        that are tied to guest CPUID.
    
        Providing a generic recalc request will allow the upcoming mediated PMU
        support to trigger a recalc when PMU features, e.g. PERF_CAPABILITIES, are
        set by userspace, without having to make multiple calls to/from PMU code.
        As a bonus, using a request will effectively coalesce recalcs, e.g. will
        reduce the number of recalcs for normal usage from 3+ to 1 (vCPU create,
        set CPUID, set PERF_CAPABILITIES (Intel only), set filter).
    
        The downside is that MSR filter changes that are done in isolation will do
        a small amount of unnecessary work, but that's already a relatively slow
        path, and the cost of recalculating instruction intercepts is negligible.
    
        Tested-by: Xudong Hao <xudong.hao@intel.com>
        Link: https://lore.kernel.org/r/20250806195706.1650976-25-seanjc@google.com
        Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -4079,7 +4079,7 @@ void pt_update_intercept_for_msr(struct kvm_vcpu *vcpu)
 	}
 }
 
-void vmx_recalc_msr_intercepts(struct kvm_vcpu *vcpu)
+static void vmx_recalc_msr_intercepts(struct kvm_vcpu *vcpu)
 {
 	if (!cpu_has_vmx_msr_bitmap())
 		return;
@@ -4130,6 +4130,11 @@ void vmx_recalc_msr_intercepts(struct kvm_vcpu *vcpu)
 	 * x2APIC and LBR MSR intercepts are modified on-demand and cannot be
 	 * filtered by userspace.
 	 */
+}
+
+void vmx_recalc_intercepts(struct kvm_vcpu *vcpu)
+{
+	vmx_recalc_msr_intercepts(vcpu);
 }
 
 static int vmx_deliver_nested_posted_interrupt(struct kvm_vcpu *vcpu,
@@ -7813,7 +7818,7 @@ void vmx_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
 			~FEAT_CTL_SGX_LC_ENABLED;
 
 	/* Recalc MSR interception to account for feature changes. */
-	vmx_recalc_msr_intercepts(vcpu);
+	vmx_recalc_intercepts(vcpu);
 
 	/* Refresh #PF interception to account for MAXPHYADDR changes. */
 	vmx_update_exception_bitmap(vcpu);
