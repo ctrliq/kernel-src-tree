38230efc794220a8a4e8647bf4a6b87c813a6a6d
Davide Caratti <dcaratti@redhat.com>
2026-04-24 11:45:28 +0200

    net/tcp: Use static_branch_tcp_{md5,ao} to drop ifdefs
    JIRA: https://redhat.atlassian.net/browse/RHEL-142637
    Upstream Status: net.git commit 3966a668bfeef49e265e4975a2cdc55fb931036d
    
    commit 3966a668bfeef49e265e4975a2cdc55fb931036d
    Author: Dmitry Safonov <0x7f454c46@gmail.com>
    Date:   Fri Jun 7 00:25:55 2024 +0100
    
        net/tcp: Use static_branch_tcp_{md5,ao} to drop ifdefs
    
        It's possible to clean-up some ifdefs by hiding that
        tcp_{md5,ao}_needed static branch is defined and compiled only
        under related configs, since commit 4c8530dc7d7d ("net/tcp: Only produce
        AO/MD5 logs if there are any keys").
    
        Reviewed-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: Dmitry Safonov <0x7f454c46@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1053,12 +1053,10 @@ static void tcp_v4_timewait_ack(struct sock *sk, struct sk_buff *skb)
 #else
 	if (0) {
 #endif
-#ifdef CONFIG_TCP_MD5SIG
-	} else if (static_branch_unlikely(&tcp_md5_needed.key)) {
+	} else if (static_branch_tcp_md5()) {
 		key.md5_key = tcp_twsk_md5_key(tcptw);
 		if (key.md5_key)
 			key.type = TCP_KEY_MD5;
-#endif
 	}
 
 	tcp_v4_send_ack(sk, skb,
@@ -1127,8 +1125,7 @@ static void tcp_v4_reqsk_send_ack(const struct sock *sk, struct sk_buff *skb,
 #else
 	if (0) {
 #endif
-#ifdef CONFIG_TCP_MD5SIG
-	} else if (static_branch_unlikely(&tcp_md5_needed.key)) {
+	} else if (static_branch_tcp_md5()) {
 		const union tcp_md5_addr *addr;
 		int l3index;
 
@@ -1137,7 +1134,6 @@ static void tcp_v4_reqsk_send_ack(const struct sock *sk, struct sk_buff *skb,
 		key.md5_key = tcp_md5_do_lookup(sk, l3index, addr, AF_INET);
 		if (key.md5_key)
 			key.type = TCP_KEY_MD5;
-#endif
 	}
 
 	/* RFC 7323 2.3
