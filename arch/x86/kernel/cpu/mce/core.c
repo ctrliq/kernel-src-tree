71f915d2fb43d47c793eeb9f3486874853255869
Steve Best <sbest@redhat.com>
2026-04-28 09:27:01 -0400

    x86/msr: Rename 'mce_wrmsrl()' to 'mce_wrmsrq()'
    JIRA: https://issues.redhat.com/browse/RHEL-130026
    
    commit 8e44e83f57c3289a41507eb79a315400629978ae
    Author: Ingo Molnar <mingo@kernel.org>
    Date:   Wed Apr 9 22:29:03 2025 +0200
    
        x86/msr: Rename 'mce_wrmsrl()' to 'mce_wrmsrq()'
    
        Suggested-by: "H. Peter Anvin" <hpa@zytor.com>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
        Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Juergen Gross <jgross@suse.com>
        Cc: Dave Hansen <dave.hansen@intel.com>
        Cc: Xin Li <xin@zytor.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit 8e44e83f57c3289a41507eb79a315400629978ae)
    Assisted-by: Patchpal
    Signed-off-by: Steve Best <sbest@redhat.com>

--- arch/x86/kernel/cpu/mce/core.c
+++ arch/x86/kernel/cpu/mce/core.c
@@ -419,7 +419,7 @@ noinstr u64 mce_rdmsrq(u32 msr)
 	return EAX_EDX_VAL(val, low, high);
 }
 
-static noinstr void mce_wrmsrl(u32 msr, u64 v)
+static noinstr void mce_wrmsrq(u32 msr, u64 v)
 {
 	u32 low, high;
 
@@ -805,7 +805,7 @@ clear_it:
 		/*
 		 * Clear state for this bank.
 		 */
-		mce_wrmsrl(mca_msr_reg(i, MCA_STATUS), 0);
+		mce_wrmsrq(mca_msr_reg(i, MCA_STATUS), 0);
 	}
 
 	/*
@@ -886,8 +886,8 @@ static noinstr bool quirk_skylake_repmov(void)
 	      MCI_STATUS_ADDRV | MCI_STATUS_MISCV |
 	      MCI_STATUS_AR | MCI_STATUS_S)) {
 		misc_enable &= ~MSR_IA32_MISC_ENABLE_FAST_STRING;
-		mce_wrmsrl(MSR_IA32_MISC_ENABLE, misc_enable);
-		mce_wrmsrl(MSR_IA32_MCx_STATUS(1), 0);
+		mce_wrmsrq(MSR_IA32_MISC_ENABLE, misc_enable);
+		mce_wrmsrq(MSR_IA32_MCx_STATUS(1), 0);
 
 		instrumentation_begin();
 		pr_err_once("Erratum detected, disable fast string copy instructions.\n");
@@ -1250,7 +1250,7 @@ static __always_inline void mce_clear_state(unsigned long *toclear)
 
 	for (i = 0; i < this_cpu_read(mce_num_banks); i++) {
 		if (arch_test_bit(i, toclear))
-			mce_wrmsrl(mca_msr_reg(i, MCA_STATUS), 0);
+			mce_wrmsrq(mca_msr_reg(i, MCA_STATUS), 0);
 	}
 }
 
@@ -1669,7 +1669,7 @@ out:
 	instrumentation_end();
 
 clear:
-	mce_wrmsrl(MSR_IA32_MCG_STATUS, 0);
+	mce_wrmsrq(MSR_IA32_MCG_STATUS, 0);
 }
 EXPORT_SYMBOL_GPL(do_machine_check);
 
