4a81ea3fdd6e8bc4c9405d6d36a72f67f15209be
David Arcari <darcari@redhat.com>
2026-05-04 07:32:04 -0400

    powercap: intel_rapl: Remove incorrect CPU check in PMU context
    JIRA: https://issues.redhat.com/browse/RHEL-128568
    
    commit 7537bae8b6eb635583e0e6260f61d13ddbd52087
    Author: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Date:   Mon Feb 9 15:43:09 2026 -0800
    
        powercap: intel_rapl: Remove incorrect CPU check in PMU context
    
        The RAPL MSR read path incorrectly validates CPU context when called
        from the PMU subsystem:
    
            if (atomic) {
                if (unlikely(smp_processor_id() != cpu))
                    return -EIO;
                rdmsrq(ra->reg.msr, ra->value);
            }
    
        This check fails for package-scoped MSRs like RAPL energy counters,
        which are readable from any CPU within the package.
    
        The perf tool avoids hitting this check by validating against
        /sys/bus/event_source/devices/power/cpumask before opening events.
        However, turbostat does not perform this validation and may attempt
        reads from non-lead CPUs, causing the check to fail and return zero
        power values.
    
        Since package-scoped MSRs are architecturally accessible from any CPU
        in the package, remove the CPU matching check.
    
        Also rename 'atomic' to 'pmu_ctx' to clarify this indicates PMU context
        where rdmsrq() can be used directly instead of rdmsrl_safe_on_cpu().
    
        Fixes: 748d6ba43afd ("powercap: intel_rapl: Enable MSR-based RAPL PMU support")
        Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
        Tested-by: Furquim Ulisses <ulisses.furquim@intel.com>
        Reviewed-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
        Link: https://patch.msgid.link/20260209234310.1440722-2-sathyanarayanan.kuppuswamy@linux.intel.com
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    (cherry picked from commit 7537bae8b6eb635583e0e6260f61d13ddbd52087)
    Assisted-by: Patchpal
    Signed-off-by: David Arcari <darcari@redhat.com>

--- drivers/powercap/intel_rapl_common.c
+++ drivers/powercap/intel_rapl_common.c
@@ -253,7 +253,7 @@ static void rapl_init_domains(struct rapl_package *rp);
 static int rapl_read_data_raw(struct rapl_domain *rd,
 			      enum rapl_primitives prim,
 			      bool xlate, u64 *data,
-			      bool atomic);
+			      bool pmu_ctx);
 static int rapl_write_data_raw(struct rapl_domain *rd,
 			       enum rapl_primitives prim,
 			       unsigned long long value);
@@ -831,7 +831,7 @@ prim_fixups(struct rapl_domain *rd, enum rapl_primitives prim)
  */
 static int rapl_read_data_raw(struct rapl_domain *rd,
 			      enum rapl_primitives prim, bool xlate, u64 *data,
-			      bool atomic)
+			      bool pmu_ctx)
 {
 	u64 value;
 	enum rapl_primitives prim_fixed = prim_fixups(rd, prim);
@@ -853,7 +853,7 @@ static int rapl_read_data_raw(struct rapl_domain *rd,
 
 	ra.mask = rpi->mask;
 
-	if (rd->rp->priv->read_raw(get_rid(rd->rp), &ra, atomic)) {
+	if (rd->rp->priv->read_raw(get_rid(rd->rp), &ra, pmu_ctx)) {
 		pr_debug("failed to read reg 0x%llx for %s:%s\n", ra.reg.val, rd->rp->name, rd->name);
 		return -EIO;
 	}
