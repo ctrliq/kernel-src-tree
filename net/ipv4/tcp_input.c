90d01f24791d1981af697b4e7e05cec28c7605c2
Davide Caratti <dcaratti@redhat.com>
2026-05-04 19:17:08 +0200

    tcp: Don't flag tcp_sk(sk)->rx_opt.saw_unknown for TCP AO.
    JIRA: https://redhat.atlassian.net/browse/RHEL-142637
    Upstream Status: net.git commit 4b74726c01b7a0b5e1029e1e9247fd81590da726
    
    commit 4b74726c01b7a0b5e1029e1e9247fd81590da726
    Author: Kuniyuki Iwashima <kuniyu@amazon.com>
    Date:   Tue Jul 2 20:35:08 2024 -0700
    
        tcp: Don't flag tcp_sk(sk)->rx_opt.saw_unknown for TCP AO.
    
        When we process segments with TCP AO, we don't check it in
        tcp_parse_options().  Thus, opt_rx->saw_unknown is set to 1,
        which unconditionally triggers the BPF TCP option parser.
    
        Let's avoid the unnecessary BPF invocation.
    
        Fixes: 0a3a809089eb ("net/tcp: Verify inbound TCP-AO signed segments")
        Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Reviewed-by: Eric Dumazet <edumazet@google.com>
        Acked-by: Dmitry Safonov <0x7f454c46@gmail.com>
        Link: https://patch.msgid.link/20240703033508.6321-1-kuniyu@amazon.com
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4209,6 +4209,13 @@ void tcp_parse_options(const struct net *net,
 				 */
 				break;
 #endif
+#ifdef CONFIG_TCP_AO
+			case TCPOPT_AO:
+				/* TCP AO has already been checked
+				 * (see tcp_inbound_ao_hash()).
+				 */
+				break;
+#endif
 			case TCPOPT_FASTOPEN:
 				tcp_parse_fastopen_option(
 					opsize - TCPOLEN_FASTOPEN_BASE,
