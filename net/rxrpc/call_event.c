bed5c6d16075d1a7c708278ac235083e6cc22957
Sultan Alsawaf <sultan@ciq.com>
2026-05-07 20:48:15 -0700

    rxrpc: Fix potential UAF after skb_unshare() failure
    cve-pre CVE-2026-43500
    commit-author David Howells <dhowells@redhat.com>
    commit 1f2740150f904bfa60e4bad74d65add3ccb5e7f8
    upstream-diff |
    	Upstream's rxrpc_input_call_event() pulls skbs from a per-call queue
    	in a loop, so the unshare block sits inside that loop alongside
    	rxrpc_free_skb() and saw_ack bookkeeping. This kernel's version
    	receives a single skb as a function parameter and doesn't free
    	it (the caller in rxrpc_io_thread() does). As such, the adapted
    	unshare block wraps the existing rxrpc_input_call_packet() call
    	site, creates and frees a copy when needed, and drops the loop
    	and queue-drain logic that doesn't exist here.
    
    	Upstream already had rxrpc_skb_put_call_rx and rxrpc_skb_get_call_rx
    	trace constants from a prior refactor not present in this tree.
    	rxrpc_skb_put_call_rx is added here since the new skb_copy path
    	needs it; rxrpc_skb_get_call_rx is omitted since nothing uses it.
    
    	Several trace constants added by upstream (rxrpc_skb_put_purge_oob,
    	rxrpc_skb_put_response, rxrpc_skb_put_response_copy) don't apply
    	since the code they trace doesn't exist in this kernel.
    
    If skb_unshare() fails to unshare a packet due to allocation failure in
    rxrpc_input_packet(), the skb pointer in the parent (rxrpc_io_thread())
    will be NULL'd out.  This will likely cause the call to
    trace_rxrpc_rx_done() to oops.
    
    Fix this by moving the unsharing down to where rxrpc_input_call_event()
    calls rxrpc_input_call_packet().  There are a number of places prior to
    that where we ignore DATA packets for a variety of reasons (such as the
    call already being complete) for which an unshare is then avoided.
    
    And with that, rxrpc_input_packet() doesn't need to take a pointer to the
    pointer to the packet, so change that to just a pointer.
    
    Fixes: 2d1faf7a0ca3 ("rxrpc: Simplify skbuff accounting in receive path")
    Closes: https://sashiko.dev/#/patchset/20260408121252.2249051-1-dhowells%40redhat.com
    	Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Marc Dionne <marc.dionne@auristor.com>
    cc: Jeffrey Altman <jaltman@auristor.com>
    cc: Simon Horman <horms@kernel.org>
    cc: linux-afs@lists.infradead.org
    cc: stable@kernel.org
    Link: https://patch.msgid.link/20260422161438.2593376-4-dhowells@redhat.com
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 1f2740150f904bfa60e4bad74d65add3ccb5e7f8)
    	Signed-off-by: Sultan Alsawaf <sultan@ciq.com>
    	Signed-off-by: Shreeya Patel <spatel@ciq.com>

--- net/rxrpc/call_event.c
+++ net/rxrpc/call_event.c
@@ -398,6 +398,28 @@ bool rxrpc_input_call_event(struct rxrpc_call *call, struct sk_buff *skb)
 	if (skb && skb->mark == RXRPC_SKB_MARK_ERROR)
 		goto out;
 
+	if (skb) {
+		struct rxrpc_skb_priv *sp = rxrpc_skb(skb);
+
+		if (sp->hdr.securityIndex != 0 &&
+		    skb_cloned(skb)) {
+			/* Unshare the packet so that it can be
+			 * modified by in-place decryption.
+			 */
+			struct sk_buff *nskb = skb_copy(skb, GFP_ATOMIC);
+
+			if (nskb) {
+				rxrpc_new_skb(nskb, rxrpc_skb_new_unshared);
+				rxrpc_input_call_packet(call, nskb);
+				rxrpc_free_skb(nskb, rxrpc_skb_put_call_rx);
+			} else {
+				rxrpc_see_skb(skb, rxrpc_skb_see_unshare_nomem);
+			}
+		} else {
+			rxrpc_input_call_packet(call, skb);
+		}
+	}
+
 	/* If we see our async-event poke, check for timeout trippage. */
 	now = jiffies;
 	t = READ_ONCE(call->expect_rx_by);
