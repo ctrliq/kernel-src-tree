69ff34f69f3d903fc025727b9664bdf289bf0934
Mark Langsdorf <mlangsdo@redhat.com>
2026-05-11 13:32:23 -0500

    cpufreq: dt-platdev: Blacklist ti,am62d2 SoC
    JIRA: https://redhat.atlassian.net/browse/RHEL-176382
    
    commit fa40cbe1c86b6626d548cf2eb555bee4eea7566c
    Author: Paresh Bhagat <p-bhagat@ti.com>
    Date: Wed, 20 Aug 2025 14:06:06 +0000
    
    Add ti,am62d2 SoC to the blacklist as the ti-cpufreq driver will handle
    creating the cpufreq-dt platform device after it completes and ensure
    it is not created twice.
    
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -180,6 +180,7 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "ti,dra7", },
 	{ .compatible = "ti,omap3", },
 	{ .compatible = "ti,am62a7", },
+	{ .compatible = "ti,am62d2", },
 	{ .compatible = "ti,am62p5", },
 
 	{ .compatible = "qcom,ipq5332", },
