f8becac4c7309f55c8943a5d33fcc2f2a9d16cb4
Guillaume Nault <gnault@redhat.com>
2026-05-13 14:43:10 +0200

    net: ipv6: Use link netns in newlink() of rtnl_link_ops
    JIRA: https://redhat.atlassian.net/browse/RHEL-144808
    Upstream Status: linux.git
    
    commit 5e72ce3e39800791700cf3aae4348a1727d3548d
    Author: Xiao Liang <shaw.leon@gmail.com>
    Date:   Wed Feb 19 20:50:34 2025 +0800
    
        net: ipv6: Use link netns in newlink() of rtnl_link_ops
    
        When link_net is set, use it as link netns instead of dev_net(). This
        prepares for rtnetlink core to create device in target netns directly,
        in which case the two namespaces may be different.
    
        Signed-off-by: Xiao Liang <shaw.leon@gmail.com>
        Reviewed-by: Kuniyuki Iwashima <kuniyu@amazon.com>
        Link: https://patch.msgid.link/20250219125039.18024-9-shaw.leon@gmail.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Guillaume Nault <gnault@redhat.com>

--- net/ipv6/ip6_gre.c
+++ net/ipv6/ip6_gre.c
@@ -1962,7 +1962,7 @@ static bool ip6gre_netlink_encap_parms(struct nlattr *data[],
 	return ret;
 }
 
-static int ip6gre_newlink_common(struct net *src_net, struct net_device *dev,
+static int ip6gre_newlink_common(struct net *link_net, struct net_device *dev,
 				 struct nlattr *tb[], struct nlattr *data[],
 				 struct netlink_ext_ack *extack)
 {
@@ -1983,7 +1983,7 @@ static int ip6gre_newlink_common(struct net *src_net, struct net_device *dev,
 		eth_hw_addr_random(dev);
 
 	nt->dev = dev;
-	nt->net = dev_net(dev);
+	nt->net = link_net;
 
 	err = register_netdevice(dev);
 	if (err)
@@ -2000,11 +2000,10 @@ static int ip6gre_newlink(struct net_device *dev,
 			  struct rtnl_newlink_params *params,
 			  struct netlink_ext_ack *extack)
 {
+	struct net *net = params->link_net ? : dev_net(dev);
 	struct ip6_tnl *nt = netdev_priv(dev);
 	struct nlattr **data = params->data;
-	struct net *src_net = params->net;
 	struct nlattr **tb = params->tb;
-	struct net *net = dev_net(dev);
 	struct ip6gre_net *ign;
 	int err;
 
@@ -2019,7 +2018,7 @@ static int ip6gre_newlink(struct net_device *dev,
 			return -EEXIST;
 	}
 
-	err = ip6gre_newlink_common(src_net, dev, tb, data, extack);
+	err = ip6gre_newlink_common(net, dev, tb, data, extack);
 	if (!err) {
 		ip6gre_tnl_link_config(nt, !tb[IFLA_MTU]);
 		ip6gre_tunnel_link_md(ign, nt);
@@ -2239,11 +2238,10 @@ static int ip6erspan_newlink(struct net_device *dev,
 			     struct rtnl_newlink_params *params,
 			     struct netlink_ext_ack *extack)
 {
+	struct net *net = params->link_net ? : dev_net(dev);
 	struct ip6_tnl *nt = netdev_priv(dev);
 	struct nlattr **data = params->data;
-	struct net *src_net = params->net;
 	struct nlattr **tb = params->tb;
-	struct net *net = dev_net(dev);
 	struct ip6gre_net *ign;
 	int err;
 
@@ -2259,7 +2257,7 @@ static int ip6erspan_newlink(struct net_device *dev,
 			return -EEXIST;
 	}
 
-	err = ip6gre_newlink_common(src_net, dev, tb, data, extack);
+	err = ip6gre_newlink_common(net, dev, tb, data, extack);
 	if (!err) {
 		ip6erspan_tnl_link_config(nt, !tb[IFLA_MTU]);
 		ip6erspan_tunnel_link_md(ign, nt);
