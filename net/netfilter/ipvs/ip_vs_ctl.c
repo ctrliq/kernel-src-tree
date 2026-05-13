a7f3af873633fba688c872bfde78325517ace5ca
Florian Westphal <fwestpha@redhat.com>
2026-05-13 17:10:29 +0200

    ipvs: do not keep dest_dst after dest is removed
    JIRA: https://redhat.atlassian.net/browse/RHEL-168848
    Upstream Status: commit 40fb72209fd8
    
    Conflict: lack of 3de0ec2873ea ("ipvs: some service readers can use RCU").
    
    commit 40fb72209fd83edaf3632dba644b1fcf81143cfc
    Author: Julian Anastasov <ja@ssi.bg>
    Date:   Tue Feb 24 21:50:43 2026 +0100
    
        ipvs: do not keep dest_dst after dest is removed
    
        Before now dest->dest_dst is not released when server is moved into
        dest_trash list after removal. As result, we can keep dst/dev
        references for long time without actively using them.
    
        It is better to avoid walking the dest_trash list when
        ip_vs_dst_event() receives dev events. So, make sure we do not
        hold dev references in dest_trash list. As packets can be flying
        while server is being removed, check the IP_VS_DEST_F_AVAILABLE
        flag in slow path to ensure we do not save new dev references to
        removed servers.
    
        Signed-off-by: Julian Anastasov <ja@ssi.bg>
        Signed-off-by: Florian Westphal <fw@strlen.de>
        Link: https://patch.msgid.link/20260224205048.4718-5-fw@strlen.de
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Assisted-by: Patchpal AI
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>

--- net/netfilter/ipvs/ip_vs_ctl.c
+++ net/netfilter/ipvs/ip_vs_ctl.c
@@ -830,7 +830,6 @@ static void ip_vs_dest_free(struct ip_vs_dest *dest)
 {
 	struct ip_vs_service *svc = rcu_dereference_protected(dest->svc, 1);
 
-	__ip_vs_dst_cache_reset(dest);
 	__ip_vs_svc_put(svc);
 	call_rcu(&dest->rcu_head, ip_vs_dest_rcu_free);
 }
@@ -1033,10 +1032,6 @@ __ip_vs_update_dest(struct ip_vs_service *svc, struct ip_vs_dest *dest,
 
 	dest->af = udest->af;
 
-	spin_lock_bh(&dest->dst_lock);
-	__ip_vs_dst_cache_reset(dest);
-	spin_unlock_bh(&dest->dst_lock);
-
 	if (add) {
 		list_add_rcu(&dest->n_list, &svc->destinations);
 		svc->num_dests++;
@@ -1044,6 +1039,10 @@ __ip_vs_update_dest(struct ip_vs_service *svc, struct ip_vs_dest *dest,
 		if (sched && sched->add_dest)
 			sched->add_dest(svc, dest);
 	} else {
+		spin_lock_bh(&dest->dst_lock);
+		__ip_vs_dst_cache_reset(dest);
+		spin_unlock_bh(&dest->dst_lock);
+
 		sched = rcu_dereference_protected(svc->scheduler, 1);
 		if (sched && sched->upd_dest)
 			sched->upd_dest(svc, dest);
@@ -1277,6 +1276,10 @@ static void __ip_vs_unlink_dest(struct ip_vs_service *svc,
 				int svcupd)
 {
 	dest->flags &= ~IP_VS_DEST_F_AVAILABLE;
+
+	spin_lock_bh(&dest->dst_lock);
+	__ip_vs_dst_cache_reset(dest);
+	spin_unlock_bh(&dest->dst_lock);
 
 	/*
 	 *  Remove it from the d-linked destination list.
@@ -1797,12 +1800,6 @@ static int ip_vs_dst_event(struct notifier_block *this, unsigned long event,
 
 		}
 	}
-
-	spin_lock_bh(&ipvs->dest_trash_lock);
-	list_for_each_entry(dest, &ipvs->dest_trash, t_list) {
-		ip_vs_forget_dev(dest, dev);
-	}
-	spin_unlock_bh(&ipvs->dest_trash_lock);
 	mutex_unlock(&__ip_vs_mutex);
 	return NOTIFY_DONE;
 }
