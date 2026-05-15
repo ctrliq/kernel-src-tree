77b7f9abab71df96ddb1f66c82481575463866a9
Jerry Snitselaar <jsnitsel@redhat.com>
2026-05-14 17:19:02 -0700

    dma-mapping: benchmark: Restore padding to ensure uABI remained consistent
    JIRA: https://issues.redhat.com/browse/RHEL-213791
    Upstream-Status: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    commit 23ee8a2563a0f24cf4964685ced23c32be444ab8
    Author: Qinxin Xia <xiaqinxin@huawei.com>
    Date: Tue Oct 28 20:08:59 2025 +0800
    
        dma-mapping: benchmark: Restore padding to ensure uABI remained consistent
    
        The padding field in the structure was previously reserved to
        maintain a stable interface for potential new fields, ensuring
        compatibility with user-space shared data structures.
        However,it was accidentally removed by tiantao in a prior commit,
        which may lead to incompatibility between user space and the kernel.
    
        This patch reinstates the padding to restore the original structure
        layout and preserve compatibility.
    
        Fixes: 8ddde07a3d28 ("dma-mapping: benchmark: extract a common header file for map_benchmark definition")
        Cc: stable@vger.kernel.org
        Acked-by: Barry Song <baohua@kernel.org>
        Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>
        Reported-by: Barry Song <baohua@kernel.org>
        Closes: https://lore.kernel.org/lkml/CAGsJ_4waiZ2+NBJG+SCnbNk+nQ_ZF13_Q5FHJqZyxyJTcEop2A@mail.gmail.com/
        Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
        Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
        Link: https://lore.kernel.org/r/20251028120900.2265511-2-xiaqinxin@huawei.com
    
    (cherry picked from commit 23ee8a2563a0f24cf4964685ced23c32be444ab8)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- include/uapi/linux/map_benchmark.h
+++ include/uapi/linux/map_benchmark.h
@@ -29,6 +29,7 @@ struct map_benchmark {
 	__u32 dma_dir; /* DMA data direction */
 	__u32 dma_trans_ns; /* time for DMA transmission in ns */
 	__u32 granule;  /* how many PAGE_SIZE will do map/unmap once a time */
+	__u8 expansion[76]; /* For future use */
 };
 
 #endif /* _UAPI_DMA_BENCHMARK_H */
