947f6edfae1a8f462039953441030e78dfad8a6b
Jan Polensky <jpolensk@redhat.com>
2026-05-15 18:11:00 +0200

    dibs: Define dibs_client_ops and dibs_dev_ops
    JIRA: https://redhat.atlassian.net/browse/RHEL-136143
    
    commit 69baaac9361edd169713562f088829a1be9c51a9
    Author: Alexandra Winter <wintera@linux.ibm.com>
    Date:   Thu Sep 18 13:04:53 2025 +0200
    
        dibs: Define dibs_client_ops and dibs_dev_ops
    
        Move the device add() and remove() functions from ism_client to
        dibs_client_ops and call add_dev()/del_dev() for ism devices and
        dibs_loopback devices. dibs_client_ops->add_dev() = smcd_register_dev() for
        the smc_dibs_client. This is the first step to handle ism and loopback
        devices alike (as dibs devices) in the smc dibs client.
    
        Define dibs_dev->ops and move smcd_ops->get_chid to
        dibs_dev_ops->get_fabric_id() for ism and loopback devices. See below for
        why this needs to be in the same patch as dibs_client_ops->add_dev().
    
        The following changes contain intermediate steps, that will be obsoleted by
        follow-on patches, once more functionality has been moved to dibs:
    
        Use different smcd_ops and max_dmbs for ism and loopback. Follow-on patches
        will change SMC-D to directly use dibs_ops instead of smcd_ops.
    
        In smcd_register_dev() it is now necessary to identify a dibs_loopback
        device before smcd_dev and smcd_ops->get_chid() are available. So provide
        dibs_dev_ops->get_fabric_id() in this patch and evaluate it in
        smc_ism_is_loopback().
    
        Call smc_loopback_init() in smcd_register_dev() and call
        smc_loopback_exit() in smcd_unregister_dev() to handle the functionality
        that is still in smc_loopback. Follow-on patches will move all smc_loopback
        code to dibs_loopback.
    
        In smcd_[un]register_dev() use only ism device name, this will be replaced
        by dibs device name by a follow-on patch.
    
        End of changes with intermediate parts.
    
        Allocate an smcd event workqueue for all dibs devices, although
        dibs_loopback does not generate events.
    
        Use kernel memory instead of devres memory for smcd_dev and smcd->conn.
        Since commit a72178cfe855 ("net/smc: Fix dependency of SMC on ISM") an ism
        device and its driver can have a longer lifetime than the smc module, so
        smc should not rely on devres to free its resources [1]. It is now the
        responsibility of the smc client to free smcd and smcd->conn for all dibs
        devices, ism devices as well as loopback. Call client->ops->del_dev() for
        all existing dibs devices in dibs_unregister_client(), so all device
        related structures can be freed in the client.
    
        When dibs_unregister_client() is called in the context of smc_exit() or
        smc_core_reboot_event(), these functions have already called
        smc_lgrs_shutdown() which calls smc_smcd_terminate_all(smcd) and sets
        going_away. This is done a second time in smcd_unregister_dev(). This is
        analogous to how smcr is handled in these functions, by calling first
        smc_lgrs_shutdown() and then smc_ib_unregister_client() >
        smc_ib_remove_dev(), so leave it that way. It may be worth investigating,
        whether smc_lgrs_shutdown() is still required or useful.
    
        Remove CONFIG_SMC_LO. CONFIG_DIBS_LO now controls whether a dibs loopback
        device exists or not.
    
        Link: https://www.kernel.org/doc/Documentation/driver-model/devres.txt [1]
        Signed-off-by: Alexandra Winter <wintera@linux.ibm.com>
        Reviewed-by: Mahanta Jambigi <mjambigi@linux.ibm.com>
        Link: https://patch.msgid.link/20250918110500.1731261-8-wintera@linux.ibm.com
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Jan Polensky <jpolensk@redhat.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -56,7 +56,6 @@
 #include "smc_stats.h"
 #include "smc_tracepoint.h"
 #include "smc_sysctl.h"
-#include "smc_loopback.h"
 #include "smc_inet.h"
 
 static DEFINE_MUTEX(smc_server_lgr_pending);	/* serialize link group
@@ -3614,16 +3613,10 @@ static int __init smc_init(void)
 		goto out_sock;
 	}
 
-	rc = smc_loopback_init();
-	if (rc) {
-		pr_err("%s: smc_loopback_init fails with %d\n", __func__, rc);
-		goto out_ib;
-	}
-
 	rc = tcp_register_ulp(&smc_ulp_ops);
 	if (rc) {
 		pr_err("%s: tcp_ulp_register fails with %d\n", __func__, rc);
-		goto out_lo;
+		goto out_ib;
 	}
 	rc = smc_inet_init();
 	if (rc) {
@@ -3634,8 +3627,6 @@ static int __init smc_init(void)
 	return 0;
 out_ulp:
 	tcp_unregister_ulp(&smc_ulp_ops);
-out_lo:
-	smc_loopback_exit();
 out_ib:
 	smc_ib_unregister_client();
 out_sock:
@@ -3674,7 +3665,6 @@ static void __exit smc_exit(void)
 	tcp_unregister_ulp(&smc_ulp_ops);
 	sock_unregister(PF_SMC);
 	smc_core_exit();
-	smc_loopback_exit();
 	smc_ib_unregister_client();
 	smc_ism_exit();
 	destroy_workqueue(smc_close_wq);
