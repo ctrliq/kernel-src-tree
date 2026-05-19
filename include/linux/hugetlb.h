414e0535c54eeb24782d9763b217dc805342fc0a
Jonathan Maple <jmaple@ciq.com>
2026-05-19 18:43:10 -0400

    mm/hugetlb: fix hugetlb_pmd_shared()
    jira KERNEL-1035
    cve CVE-2026-23100
    Rebuild_History Non-Buildable kernel-5.14.0-687.5.1.el9_8
    commit-author David Hildenbrand (Red Hat) <david@kernel.org>
    commit ca1a47cd3f5f4c46ca188b1c9a27af87d1ab2216
    
    Patch series "mm/hugetlb: fixes for PMD table sharing (incl.  using
    mmu_gather)", v3.
    
    One functional fix, one performance regression fix, and two related
    comment fixes.
    
    I cleaned up my prototype I recently shared [1] for the performance fix,
    deferring most of the cleanups I had in the prototype to a later point.
    While doing that I identified the other things.
    
    The goal of this patch set is to be backported to stable trees "fairly"
    easily. At least patch #1 and #4.
    
    Patch #1 fixes hugetlb_pmd_shared() not detecting any sharing
    Patch #2 + #3 are simple comment fixes that patch #4 interacts with.
    Patch #4 is a fix for the reported performance regression due to excessive
    IPI broadcasts during fork()+exit().
    
    The last patch is all about TLB flushes, IPIs and mmu_gather.
    Read: complicated
    
    There are plenty of cleanups in the future to be had + one reasonable
    optimization on x86. But that's all out of scope for this series.
    
    Runtime tested, with a focus on fixing the performance regression using
    the original reproducer [2] on x86.
    
    This patch (of 4):
    
    We switched from (wrongly) using the page count to an independent shared
    count.  Now, shared page tables have a refcount of 1 (excluding
    speculative references) and instead use ptdesc->pt_share_count to identify
    sharing.
    
    We didn't convert hugetlb_pmd_shared(), so right now, we would never
    detect a shared PMD table as such, because sharing/unsharing no longer
    touches the refcount of a PMD table.
    
    Page migration, like mbind() or migrate_pages() would allow for migrating
    folios mapped into such shared PMD tables, even though the folios are not
    exclusive.  In smaps we would account them as "private" although they are
    "shared", and we would be wrongly setting the PM_MMAP_EXCLUSIVE in the
    pagemap interface.
    
    Fix it by properly using ptdesc_pmd_is_shared() in hugetlb_pmd_shared().
    
    Link: https://lkml.kernel.org/r/20251223214037.580860-1-david@kernel.org
    Link: https://lkml.kernel.org/r/20251223214037.580860-2-david@kernel.org
    Link: https://lore.kernel.org/all/8cab934d-4a56-44aa-b641-bfd7e23bd673@kernel.org/ [1]
    Link: https://lore.kernel.org/all/8cab934d-4a56-44aa-b641-bfd7e23bd673@kernel.org/ [2]
    Fixes: 59d9094df3d7 ("mm: hugetlb: independent PMD page table shared count")
    	Signed-off-by: David Hildenbrand (Red Hat) <david@kernel.org>
    	Reviewed-by: Rik van Riel <riel@surriel.com>
    	Reviewed-by: Lance Yang <lance.yang@linux.dev>
    	Tested-by: Lance Yang <lance.yang@linux.dev>
    	Reviewed-by: Harry Yoo <harry.yoo@oracle.com>
    	Tested-by: Laurence Oberman <loberman@redhat.com>
    	Reviewed-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    	Acked-by: Oscar Salvador <osalvador@suse.de>
    	Cc: Liu Shixin <liushixin2@huawei.com>
    	Cc: Uschakow, Stanislav" <suschako@amazon.de>
    	Cc: <stable@vger.kernel.org>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit ca1a47cd3f5f4c46ca188b1c9a27af87d1ab2216)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -1301,7 +1301,7 @@ static inline __init void hugetlb_cma_reserve(int order)
 #ifdef CONFIG_ARCH_WANT_HUGE_PMD_SHARE
 static inline bool hugetlb_pmd_shared(pte_t *pte)
 {
-	return page_count(virt_to_page(pte)) > 1;
+	return ptdesc_pmd_is_shared(virt_to_ptdesc(pte));
 }
 #else
 static inline bool hugetlb_pmd_shared(pte_t *pte)
