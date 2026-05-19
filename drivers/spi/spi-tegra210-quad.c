7f96deef0597764ad8e0a623d6a9d8a28b4ce310
Jonathan Maple <jmaple@ciq.com>
2026-05-19 18:43:19 -0400

    spi: tegra210-quad: Move curr_xfer read inside spinlock
    jira KERNEL-1035
    Rebuild_History Non-Buildable kernel-5.14.0-687.5.1.el9_8
    commit-author Breno Leitao <leitao@debian.org>
    commit ef13ba357656451d6371940d8414e3e271df97e3
    
    Move the assignment of the transfer pointer from curr_xfer inside the
    spinlock critical section in both handle_cpu_based_xfer() and
    handle_dma_based_xfer().
    
    Previously, curr_xfer was read before acquiring the lock, creating a
    window where the timeout path could clear curr_xfer between reading it
    and using it. By moving the read inside the lock, the handlers are
    guaranteed to see a consistent value that cannot be modified by the
    timeout path.
    
    Fixes: 921fc1838fb0 ("spi: tegra210-quad: Add support for Tegra210 QSPI controller")
    	Signed-off-by: Breno Leitao <leitao@debian.org>
    	Acked-by: Thierry Reding <treding@nvidia.com>
    	Tested-by: Jon Hunter <jonathanh@nvidia.com>
    	Acked-by: Jon Hunter <jonathanh@nvidia.com>
    Link: https://patch.msgid.link/20260126-tegra_xfer-v2-2-6d2115e4f387@debian.org
    	Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit ef13ba357656451d6371940d8414e3e271df97e3)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/spi/spi-tegra210-quad.c
+++ drivers/spi/spi-tegra210-quad.c
@@ -1449,10 +1449,11 @@ static int tegra_qspi_transfer_one_message(struct spi_master *master,
 
 static irqreturn_t handle_cpu_based_xfer(struct tegra_qspi *tqspi)
 {
-	struct spi_transfer *t = tqspi->curr_xfer;
+	struct spi_transfer *t;
 	unsigned long flags;
 
 	spin_lock_irqsave(&tqspi->lock, flags);
+	t = tqspi->curr_xfer;
 
 	if (tqspi->tx_status ||  tqspi->rx_status) {
 		tegra_qspi_handle_error(tqspi);
@@ -1483,7 +1484,7 @@ exit:
 
 static irqreturn_t handle_dma_based_xfer(struct tegra_qspi *tqspi)
 {
-	struct spi_transfer *t = tqspi->curr_xfer;
+	struct spi_transfer *t;
 	unsigned int total_fifo_words;
 	unsigned long flags;
 	long wait_status;
@@ -1522,6 +1523,7 @@ static irqreturn_t handle_dma_based_xfer(struct tegra_qspi *tqspi)
 	}
 
 	spin_lock_irqsave(&tqspi->lock, flags);
+	t = tqspi->curr_xfer;
 
 	if (num_errors) {
 		tegra_qspi_dma_unmap_xfer(tqspi, t);
