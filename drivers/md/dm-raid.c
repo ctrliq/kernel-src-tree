8ef662ee06cea3d192e9c05087f21b5c6583f9e6
Nigel Croxon <ncroxon@redhat.com>
2026-05-27 06:49:50 -0400

    dm-raid: only requeue bios when dm is suspending
    JIRA: https://issues.redhat.com/browse/RHEL-129337
    
    commit 97cb8be0fd4c50d310988e0822f7e91d1711e6f8
    Author: Benjamin Marzinski <bmarzins@redhat.com>
    Date:   Tue Apr 28 19:20:10 2026 -0400
    
    dm-raid: only requeue bios when dm is suspending
    
    returning DM_MAPIO_REQUEUE from the target map() function only requeues
    the bio during noflush suspends. During regular operations or during
    flushing suspends, it fails the bio. Failing the bio during flushing
    suspends is the correct behavior here. We cannot handle the bio, and we
    cannot suspends while it is outstanding. But during normal operations,
    we should not push the bio back to dm. Instead, wait for the reshape
    to be resumed.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    (cherry picked from commit 97cb8be0fd4c50d310988e0822f7e91d1711e6f8)
    Signed-off-by: Nigel Croxon <ncroxon@redhat.com>

--- drivers/md/dm-raid.c
+++ drivers/md/dm-raid.c
@@ -3831,6 +3831,7 @@ static void raid_presuspend(struct dm_target *ti)
 	 * resume, raid_postsuspend() is too late.
 	 */
 	set_bit(RT_FLAG_RS_FROZEN, &rs->runtime_flags);
+	set_bit(MD_DM_SUSPENDING, &mddev->flags);
 
 	if (!reshape_interrupted(mddev))
 		return;
@@ -3847,13 +3848,16 @@ static void raid_presuspend(struct dm_target *ti)
 static void raid_presuspend_undo(struct dm_target *ti)
 {
 	struct raid_set *rs = ti->private;
+	struct mddev *mddev = &rs->md;
 
+	clear_bit(MD_DM_SUSPENDING, &mddev->flags);
 	clear_bit(RT_FLAG_RS_FROZEN, &rs->runtime_flags);
 }
 
 static void raid_postsuspend(struct dm_target *ti)
 {
 	struct raid_set *rs = ti->private;
+	struct mddev *mddev = &rs->md;
 
 	if (!test_and_set_bit(RT_FLAG_RS_SUSPENDED, &rs->runtime_flags)) {
 		/*
@@ -3863,6 +3867,8 @@ static void raid_postsuspend(struct dm_target *ti)
 		md_stop_writes(&rs->md);
 		mddev_suspend(&rs->md, false);
 	}
+	clear_bit(MD_DM_SUSPENDING, &mddev->flags);
+
 }
 
 static void attempt_restore_of_faulty_devices(struct raid_set *rs)
