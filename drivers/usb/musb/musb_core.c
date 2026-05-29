30666df22af6230cbaa3db1cbb735079e27877a2
Desnes Nunes <desnesn@redhat.com>
2026-05-29 11:56:58 -0300

    usb: Remove redundant pm_runtime_mark_last_busy() calls
    JIRA: https://issues.redhat.com/browse/RHEL-147793
    Conflicts:
    * Avoiding commit 67a59f82196c ("usb: musb: fix gadget state on discon-
      nect") that changed the usb_gadget state and commit a6d45ea063f0 ("usb:
      musb: Allow running without CONFIG_USB_PHY") that changed how
      musb->xceiv was deferenced.
    
    commit 8d3c283ef80950532500f017a80a00a83e8cce0f
    Author: Sakari Ailus <sakari.ailus@linux.intel.com>
    Date: Tue, 11 Nov 2025 11:51:17 +0200
    
      pm_runtime_put_autosuspend(), pm_runtime_put_sync_autosuspend(),
      pm_runtime_autosuspend() and pm_request_autosuspend() now include a call
      to pm_runtime_mark_last_busy(). Remove the now-reduntant explicit call to
      pm_runtime_mark_last_busy().
    
      Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
      Acked-by: Thierry Reding <treding@nvidia.com>
      Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
      Acked-by: Peter Chen <peter.chen@kernel.org>
      Link: https://patch.msgid.link/20251111095117.95023-1-sakari.ailus@linux.intel.com
      Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Desnes Nunes <desnesn@redhat.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2036,7 +2036,6 @@ static void musb_pm_runtime_check_session(struct musb *musb)
 		if (!musb->session)
 			break;
 		trace_musb_state(musb, devctl, "Allow PM on possible host mode disconnect");
-		pm_runtime_mark_last_busy(musb->controller);
 		pm_runtime_put_autosuspend(musb->controller);
 		musb->session = false;
 		return;
@@ -2068,7 +2067,6 @@ static void musb_pm_runtime_check_session(struct musb *musb)
 					      msecs_to_jiffies(3000));
 	} else {
 		trace_musb_state(musb, devctl, "Allow PM with no session");
-		pm_runtime_mark_last_busy(musb->controller);
 		pm_runtime_put_autosuspend(musb->controller);
 	}
 
@@ -2095,7 +2093,6 @@ static void musb_irq_work(struct work_struct *data)
 		sysfs_notify(&musb->controller->kobj, NULL, "mode");
 	}
 
-	pm_runtime_mark_last_busy(musb->controller);
 	pm_runtime_put_autosuspend(musb->controller);
 }
 
@@ -2570,7 +2567,6 @@ musb_init_controller(struct device *dev, int nIrq, void __iomem *ctrl)
 	musb_init_debugfs(musb);
 
 	musb->is_initialized = 1;
-	pm_runtime_mark_last_busy(musb->controller);
 	pm_runtime_put_autosuspend(musb->controller);
 
 	return 0;
@@ -2896,7 +2892,6 @@ static int musb_resume(struct device *dev)
 			error);
 	spin_unlock_irqrestore(&musb->lock, flags);
 
-	pm_runtime_mark_last_busy(dev);
 	pm_runtime_put_autosuspend(dev);
 
 	return 0;
