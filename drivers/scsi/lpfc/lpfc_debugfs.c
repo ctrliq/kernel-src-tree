cfbcb1581a8d15c1c061e4acda2ff20cc729801a
Jonathan Maple <jmaple@ciq.com>
2026-06-02 15:46:55 -0400

    Revert "scsi: lpfc: Use switch case statements in DIF debugfs handlers"
    jira KERNEL-1029
    Rebuild_History Non-Buildable kernel-6.12.0-211.18.1.el10_2
    Rebuild_CHGLOG: - Revert "scsi: lpfc: Use switch case statements in DIF debugfs handlers" (Ewan D. Milne) [RHEL-169786]
    Rebuild_FUZZ: 93.23%
    commit-author Justin Tee <justin.tee@broadcom.com>
    commit d19023a336a31f49111b96a266610a4d923f9cad
    Vendor revert of upstream commit 5d7ef44d8ae3b94565e8212d68c06bcee6f6ef90
    
    This reverts commit d19023a336a31f49111b96a266610a4d923f9cad.
    
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- drivers/scsi/lpfc/lpfc_debugfs.c
+++ drivers/scsi/lpfc/lpfc_debugfs.c
@@ -2373,117 +2373,93 @@ out:
 
 static ssize_t
 lpfc_debugfs_dif_err_read(struct file *file, char __user *buf,
-			  size_t nbytes, loff_t *ppos)
+	size_t nbytes, loff_t *ppos)
 {
 	struct lpfc_hba *phba = file->private_data;
 	int kind = debugfs_get_aux_num(file);
-	char cbuf[32] = {0};
+	char cbuf[32];
+	uint64_t tmp = 0;
 	int cnt = 0;
 
-	switch (kind) {
-	case writeGuard:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "%u\n",
-				phba->lpfc_injerr_wgrd_cnt);
-		break;
-	case writeApp:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "%u\n",
-				phba->lpfc_injerr_wapp_cnt);
-		break;
-	case writeRef:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "%u\n",
-				phba->lpfc_injerr_wref_cnt);
-		break;
-	case readGuard:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "%u\n",
-				phba->lpfc_injerr_rgrd_cnt);
-		break;
-	case readApp:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "%u\n",
-				phba->lpfc_injerr_rapp_cnt);
-		break;
-	case readRef:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "%u\n",
-				phba->lpfc_injerr_rref_cnt);
-		break;
-	case InjErrNPortID:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "0x%06x\n",
+	if (kind == writeGuard)
+		cnt = scnprintf(cbuf, 32, "%u\n", phba->lpfc_injerr_wgrd_cnt);
+	else if (kind == writeApp)
+		cnt = scnprintf(cbuf, 32, "%u\n", phba->lpfc_injerr_wapp_cnt);
+	else if (kind == writeRef)
+		cnt = scnprintf(cbuf, 32, "%u\n", phba->lpfc_injerr_wref_cnt);
+	else if (kind == readGuard)
+		cnt = scnprintf(cbuf, 32, "%u\n", phba->lpfc_injerr_rgrd_cnt);
+	else if (kind == readApp)
+		cnt = scnprintf(cbuf, 32, "%u\n", phba->lpfc_injerr_rapp_cnt);
+	else if (kind == readRef)
+		cnt = scnprintf(cbuf, 32, "%u\n", phba->lpfc_injerr_rref_cnt);
+	else if (kind == InjErrNPortID)
+		cnt = scnprintf(cbuf, 32, "0x%06x\n",
 				phba->lpfc_injerr_nportid);
-		break;
-	case InjErrWWPN:
-		cnt = scnprintf(cbuf, sizeof(cbuf), "0x%016llx\n",
-				be64_to_cpu(phba->lpfc_injerr_wwpn.u.wwn_be));
-		break;
-	case InjErrLBA:
-		if (phba->lpfc_injerr_lba == LPFC_INJERR_LBA_OFF)
-			cnt = scnprintf(cbuf, sizeof(cbuf), "off\n");
+	else if (kind == InjErrWWPN) {
+		memcpy(&tmp, &phba->lpfc_injerr_wwpn, sizeof(struct lpfc_name));
+		tmp = cpu_to_be64(tmp);
+		cnt = scnprintf(cbuf, 32, "0x%016llx\n", tmp);
+	} else if (kind == InjErrLBA) {
+		if (phba->lpfc_injerr_lba == (sector_t)(-1))
+			cnt = scnprintf(cbuf, 32, "off\n");
 		else
-			cnt = scnprintf(cbuf, sizeof(cbuf), "0x%llx\n",
-					(uint64_t)phba->lpfc_injerr_lba);
-		break;
-	default:
-		lpfc_log_msg(phba, KERN_WARNING, LOG_INIT,
-			     "0547 Unknown debugfs error injection entry\n");
-		break;
-	}
+			cnt = scnprintf(cbuf, 32, "0x%llx\n",
+				 (uint64_t) phba->lpfc_injerr_lba);
+	} else
+		lpfc_printf_log(phba, KERN_ERR, LOG_INIT,
+			 "0547 Unknown debugfs error injection entry\n");
 
 	return simple_read_from_buffer(buf, nbytes, ppos, &cbuf, cnt);
 }
 
 static ssize_t
 lpfc_debugfs_dif_err_write(struct file *file, const char __user *buf,
-			   size_t nbytes, loff_t *ppos)
+	size_t nbytes, loff_t *ppos)
 {
 	struct lpfc_hba *phba = file->private_data;
 	int kind = debugfs_get_aux_num(file);
-	char dstbuf[33] = {0};
-	unsigned long long tmp;
-	unsigned long size;
+	char dstbuf[33];
+	uint64_t tmp = 0;
+	int size;
 
-	size = (nbytes < (sizeof(dstbuf) - 1)) ? nbytes : (sizeof(dstbuf) - 1);
+	memset(dstbuf, 0, 33);
+	size = (nbytes < 32) ? nbytes : 32;
 	if (copy_from_user(dstbuf, buf, size))
 		return -EFAULT;
 
-	if (kstrtoull(dstbuf, 0, &tmp)) {
-		if (kind != InjErrLBA || !strstr(dstbuf, "off"))
-			return -EINVAL;
+	if (kind == InjErrLBA) {
+		if ((dstbuf[0] == 'o') && (dstbuf[1] == 'f') &&
+		    (dstbuf[2] == 'f'))
+			tmp = (uint64_t)(-1);
 	}
 
-	switch (kind) {
-	case writeGuard:
+	if ((tmp == 0) && (kstrtoull(dstbuf, 0, &tmp)))
+		return -EINVAL;
+
+	if (kind == writeGuard)
 		phba->lpfc_injerr_wgrd_cnt = (uint32_t)tmp;
-		break;
-	case writeApp:
+	else if (kind == writeApp)
 		phba->lpfc_injerr_wapp_cnt = (uint32_t)tmp;
-		break;
-	case writeRef:
+	else if (kind == writeRef)
 		phba->lpfc_injerr_wref_cnt = (uint32_t)tmp;
-		break;
-	case readGuard:
+	else if (kind == readGuard)
 		phba->lpfc_injerr_rgrd_cnt = (uint32_t)tmp;
-		break;
-	case readApp:
+	else if (kind == readApp)
 		phba->lpfc_injerr_rapp_cnt = (uint32_t)tmp;
-		break;
-	case readRef:
+	else if (kind == readRef)
 		phba->lpfc_injerr_rref_cnt = (uint32_t)tmp;
-		break;
-	case InjErrLBA:
-		if (strstr(dstbuf, "off"))
-			phba->lpfc_injerr_lba = LPFC_INJERR_LBA_OFF;
-		else
-			phba->lpfc_injerr_lba = (sector_t)tmp;
-		break;
-	case InjErrNPortID:
+	else if (kind == InjErrLBA)
+		phba->lpfc_injerr_lba = (sector_t)tmp;
+	else if (kind == InjErrNPortID)
 		phba->lpfc_injerr_nportid = (uint32_t)(tmp & Mask_DID);
-		break;
-	case InjErrWWPN:
-		phba->lpfc_injerr_wwpn.u.wwn_be = cpu_to_be64(tmp);
-		break;
-	default:
-		lpfc_log_msg(phba, KERN_WARNING, LOG_INIT,
-			     "0548 Unknown debugfs error injection entry\n");
-		break;
-	}
+	else if (kind == InjErrWWPN) {
+		tmp = cpu_to_be64(tmp);
+		memcpy(&phba->lpfc_injerr_wwpn, &tmp, sizeof(struct lpfc_name));
+	} else
+		lpfc_printf_log(phba, KERN_ERR, LOG_INIT,
+			 "0548 Unknown debugfs error injection entry\n");
+
 	return nbytes;
 }
 
--- drivers/scsi/lpfc/lpfc_hw.h
+++ drivers/scsi/lpfc/lpfc_hw.h
@@ -366,7 +366,6 @@ struct lpfc_name {
 		} s;
 		uint8_t wwn[8];
 		uint64_t name __packed __aligned(4);
-		__be64 wwn_be __packed __aligned(4);
 	} u;
 };
 
