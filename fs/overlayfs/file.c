c2685f8b7b4392f68d8492a331de2f544cc80a8b
Ondrej Mosnacek <omosnace@redhat.com>
2026-06-02 10:36:28 +0200

    lsm: add backing_file LSM hooks
    JIRA: https://issues.redhat.com/browse/RHEL-179440
    CVE: CVE-2026-46054
    Conflicts:
      - fs/backing-file.c: adjust backing_file_mmap() to downstream state (missing
        scoped guards)
      - fs/erofs/ishare.c: hunk dropped, files not in RHEL10
      - fs/overlayfs/dir.c: adjust to different indentation
      - fs/overlayfs/file.c: adjust to different indentation
    
    commit 6af36aeb147a06dea47c49859cd6ca5659aeb987
    Author: Paul Moore <paul@paul-moore.com>
    Date:   Fri Dec 19 13:18:22 2025 -0500
    
        lsm: add backing_file LSM hooks
    
        Stacked filesystems such as overlayfs do not currently provide the
        necessary mechanisms for LSMs to properly enforce access controls on the
        mmap() and mprotect() operations.  In order to resolve this gap, a LSM
        security blob is being added to the backing_file struct and the following
        new LSM hooks are being created:
    
         security_backing_file_alloc()
         security_backing_file_free()
         security_mmap_backing_file()
    
        The first two hooks are to manage the lifecycle of the LSM security blob
        in the backing_file struct, while the third provides a new mmap() access
        control point for the underlying backing file.  It is also expected that
        LSMs will likely want to update their security_file_mprotect() callback
        to address issues with their mprotect() controls, but that does not
        require a change to the security_file_mprotect() LSM hook.
    
        There are a three other small changes to support these new LSM hooks:
        * Pass the user file associated with a backing file down to
        alloc_empty_backing_file() so it can be included in the
        security_backing_file_alloc() hook.
        * Add getter and setter functions for the backing_file struct LSM blob
        as the backing_file struct remains private to fs/file_table.c.
        * Constify the file struct field in the LSM common_audit_data struct to
        better support LSMs that need to pass a const file struct pointer into
        the common LSM audit code.
    
        Thanks to Arnd Bergmann for identifying the missing EXPORT_SYMBOL_GPL()
        and supplying a fixup.
    
        Cc: stable@vger.kernel.org
        Cc: linux-fsdevel@vger.kernel.org
        Cc: linux-unionfs@vger.kernel.org
        Cc: linux-erofs@lists.ozlabs.org
        Reviewed-by: Amir Goldstein <amir73il@gmail.com>
        Reviewed-by: Serge Hallyn <serge@hallyn.com>
        Reviewed-by: Christian Brauner <brauner@kernel.org>
        Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>

--- fs/overlayfs/file.c
+++ fs/overlayfs/file.c
@@ -48,7 +48,7 @@ static struct file *ovl_open_realfile(const struct file *file,
 		if (!inode_owner_or_capable(real_idmap, realinode))
 			flags &= ~O_NOATIME;
 
-		realfile = backing_file_open(&file->f_path, flags, realpath,
+		realfile = backing_file_open(file, flags, realpath,
 					     current_cred());
 	}
 	revert_creds(old_cred);
