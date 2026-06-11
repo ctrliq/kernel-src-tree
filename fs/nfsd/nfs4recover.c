fcc792167acded3b1f40b8171ec80a38a23213a3
Olga Kornievskaia <okorniev@redhat.com>
2026-06-11 15:09:03 -0400

    nfsd: switch purge_old() to use start_removing_noperm()
    JIRA: https://issues.redhat.com/browse/RHEL-171581
    Conflicts: context difference due to merge fix in b0319c464263 and due
    to missing 8ad9248471138e ("nfsd: Use lookup_one() rather than lookup_one_len()")
    
    commit 6cb3411962c884c628ec198c654bab79a5500ccc
    Author: NeilBrown <neil@brown.name>
    Date: Wed, 25 Feb 2026 09:16:52 +1100
    
        nfsd: switch purge_old() to use start_removing_noperm()
    
        Rather than explicit locking, use the start_removing_noperm() and
        end_removing() wrappers.
        This was not done with other start_removing changes due to conflicting
        in-flight patches.
    
        Reviewed-by: Jeff Layton <jlayton@kernel.org>
        Signed-off-by: NeilBrown <neil@brown.name>
        Link: https://patch.msgid.link/20260224222542.3458677-8-neilb@ownmail.net
        Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    Signed-off-by: Olga Kornievskaia <okorniev@redhat.com>

--- fs/nfsd/nfs4recover.c
+++ fs/nfsd/nfs4recover.c
@@ -361,16 +361,14 @@ purge_old(struct dentry *parent, char *cname, struct nfsd_net *nn)
 	if (nfs4_has_reclaimed_state(name, nn))
 		goto out_free;
 
-	inode_lock_nested(d_inode(parent), I_MUTEX_PARENT);
-	child = lookup_one_len(cname, parent, HEXDIR_LEN-1);
+	child = start_removing_noperm(parent, &QSTR(cname));
 	if (!IS_ERR(child)) {
 		status = vfs_rmdir(&nop_mnt_idmap, d_inode(parent), child);
 		if (status)
 			printk("failed to remove client recovery directory %pd\n",
 			       child);
-		dput(child);
 	}
-	inode_unlock(d_inode(parent));
+	end_removing(child);
 
 out_free:
 	kfree(name.data);
