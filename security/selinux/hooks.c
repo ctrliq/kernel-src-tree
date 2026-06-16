7afc16c3e0cf718250da44444c47d04836225bb4
Ondrej Mosnacek <omosnace@redhat.com>
2026-06-16 10:06:13 +0200

    selinux: RHEL-only hotfix for execmem regression
    JIRA: https://redhat.atlassian.net/browse/RHEL-185115
    Upstream Status: RHEL10-only
    
    As discovered by image-mode/CoreOS testing, the fixes for CVE-2026-46054
    caused a regression that results in unexpected execmem denials in
    specific scenarios involving overlayfs (or another stacked filesystem).
    
    Specifically in case of image mode / CoreOS there is often (always?) an
    overlayfs filesystem mounted during early boot (before SELinux policy is
    loaded), which means that overlayfs captures the kernel’s SELinux
    context as part of the mounter credentials, which are later used by
    overlayfs+SELinux to verify that file accesses through the overlay mount
    don’t give the mounter a way to access underlying files it otherwise
    wouldn’t have access to. This verification would normally pass, as the
    policy grants the kernel context almost unrestricted access to the
    filesystem. However, the new checks added to fix CVE-2026-46054
    erroneously include the execmem check for the mounter and in the policy
    kernel_t doesn’t have the execmem permission, so mmapping an overlay
    file with MAP_PRIVATE and PROT_WRITE|PROT_EXEC would now result in a
    SELinux denial.
    
    Fix this by passing a boolean through the helper functions that allows
    to distinguish the direct permission check from the mounter check and
    skipping the execmem check in the mounter case.
    
    This is a transient RHEL-only fix to allow the CVE fix to go through
    without breaking image mode/CoreOS deployments. Once an optimal solution
    is figured out and applied upstream, this commit will be reverted and
    replaced with the upstream fix (at least in Y-streams). I expect the
    upstream solution to be functionally equivalent, though probably
    cosmetically different.
    
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3809,7 +3809,7 @@ static int default_noexec __ro_after_init;
 
 static int __file_map_prot_check(const struct cred *cred,
 				 const struct file *file, unsigned long prot,
-				 bool shared, bool bf_user_file)
+				 bool shared, bool mounter, bool bf_user_file)
 {
 	struct inode *inode = NULL;
 	bool prot_exec = prot & PROT_EXEC;
@@ -3823,7 +3823,7 @@ static int __file_map_prot_check(const struct cred *cred,
 	}
 
 	if (default_noexec && prot_exec &&
-	    (!file || IS_PRIVATE(inode) || (!shared && prot_write))) {
+	    (!file || IS_PRIVATE(inode) || (!shared && prot_write)) && !mounter) {
 		int rc;
 		u32 sid = cred_sid(cred);
 
@@ -3853,9 +3853,9 @@ static int __file_map_prot_check(const struct cred *cred,
 
 static inline int file_map_prot_check(const struct cred *cred,
 				      const struct file *file,
-				      unsigned long prot, bool shared)
+				      unsigned long prot, bool shared, bool mounter)
 {
-	return __file_map_prot_check(cred, file, prot, shared, false);
+	return __file_map_prot_check(cred, file, prot, shared, mounter, false);
 }
 
 static int selinux_mmap_addr(unsigned long addr)
@@ -3872,7 +3872,7 @@ static int selinux_mmap_addr(unsigned long addr)
 }
 
 static int selinux_mmap_file_common(const struct cred *cred, struct file *file,
-				    unsigned long prot, bool shared)
+				    unsigned long prot, bool shared, bool mounter)
 {
 	if (file) {
 		int rc;
@@ -3885,7 +3885,7 @@ static int selinux_mmap_file_common(const struct cred *cred, struct file *file,
 			return rc;
 	}
 
-	return file_map_prot_check(cred, file, prot, shared);
+	return file_map_prot_check(cred, file, prot, shared, mounter);
 }
 
 static int selinux_mmap_file(struct file *file,
@@ -3893,7 +3893,8 @@ static int selinux_mmap_file(struct file *file,
 			     unsigned long prot, unsigned long flags)
 {
 	return selinux_mmap_file_common(current_cred(), file, prot,
-					(flags & MAP_TYPE) == MAP_SHARED);
+					(flags & MAP_TYPE) == MAP_SHARED,
+					false);
 }
 
 /**
@@ -3926,7 +3927,8 @@ static int selinux_mmap_backing_file(struct vm_area_struct *vma,
 		prot |= PROT_EXEC;
 
 	return selinux_mmap_file_common(backing_file->f_cred, backing_file,
-					prot, vma->vm_flags & VM_SHARED);
+					prot, vma->vm_flags & VM_SHARED,
+					true);
 }
 
 static int selinux_file_mprotect(struct vm_area_struct *vma,
@@ -3987,11 +3989,11 @@ static int selinux_file_mprotect(struct vm_area_struct *vma,
 		}
 	}
 
-	rc = __file_map_prot_check(cred, file, prot, shared, backing_file);
+	rc = __file_map_prot_check(cred, file, prot, shared, false, backing_file);
 	if (rc)
 		return rc;
 	if (backing_file) {
-		rc = file_map_prot_check(file->f_cred, file, prot, shared);
+		rc = file_map_prot_check(file->f_cred, file, prot, shared, true);
 		if (rc)
 			return rc;
 	}
