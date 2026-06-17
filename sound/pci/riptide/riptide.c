56e3b32e96372cdb3edbaf15fcc175dd65fd52b1
Jaroslav Kysela <jkysela@redhat.com>
2026-06-17 15:54:19 +0200

    sound/soundwire: Convert 'alloc_obj' family to use the new default GFP_KERNEL argument
    JIRA: https://issues.redhat.com/browse/RHEL-183295
    
    Conflicts: only sound/, drivers/soundwire and drivers/firmware/cirrus trees
    
    commit bf4afc53b77aeaa48b5409da5c8da6bb4eff7f43
    Author: Linus Torvalds <torvalds@linux-foundation.org>
    Date: Sat Feb 21 16:37:42 2026 -0800
    
        Convert 'alloc_obj' family to use the new default GFP_KERNEL argument
    
        This was done entirely with mindless brute force, using
    
            git grep -l '\<k[vmz]*alloc_objs*(.*, GFP_KERNEL)' |
                xargs sed -i 's/\(alloc_objs*(.*\), GFP_KERNEL)/\1)/'
    
        to convert the new alloc_obj() users that had a simple GFP_KERNEL
        argument to just drop that argument.
    
        Note that due to the extreme simplicity of the scripting, any slightly
        more complex cases spread over multiple lines would not be triggered:
        they definitely exist, but this covers the vast bulk of the cases, and
        the resulting diff is also then easier to check automatically.
    
        For the same reason the 'flex' versions will be done as a separate
        conversion.
    
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/pci/riptide/riptide.c
+++ sound/pci/riptide/riptide.c
@@ -1594,7 +1594,7 @@ static int snd_riptide_playback_open(struct snd_pcm_substream *substream)
 	chip->playback_substream[sub_num] = substream;
 	runtime->hw = snd_riptide_playback;
 
-	data = kzalloc_obj(struct pcmhw, GFP_KERNEL);
+	data = kzalloc_obj(struct pcmhw);
 	if (data == NULL)
 		return -ENOMEM;
 	data->paths = lbus_play_paths[sub_num];
@@ -1617,7 +1617,7 @@ static int snd_riptide_capture_open(struct snd_pcm_substream *substream)
 	chip->capture_substream = substream;
 	runtime->hw = snd_riptide_capture;
 
-	data = kzalloc_obj(struct pcmhw, GFP_KERNEL);
+	data = kzalloc_obj(struct pcmhw);
 	if (data == NULL)
 		return -ENOMEM;
 	data->paths = lbus_rec_path;
@@ -1767,7 +1767,7 @@ static int snd_riptide_initialize(struct snd_riptide *chip)
 
 	cif = chip->cif;
 	if (!cif) {
-		cif = kzalloc_obj(struct cmdif, GFP_KERNEL);
+		cif = kzalloc_obj(struct cmdif);
 		if (!cif)
 			return -ENOMEM;
 		cif->dev = chip->card->dev;
--- sound/soc/codecs/tas2781-fmwlib.c
+++ sound/soc/codecs/tas2781-fmwlib.c
@@ -186,7 +186,7 @@ static struct tasdevice_config_info *tasdevice_add_config(
 	 * of audio cases, flexible configs have been introduced in the
 	 * dsp firmware.
 	 */
-	cfg_info = kzalloc_obj(struct tasdevice_config_info, GFP_KERNEL);
+	cfg_info = kzalloc_obj(struct tasdevice_config_info);
 	if (!cfg_info) {
 		*status = -ENOMEM;
 		goto out;
@@ -242,7 +242,7 @@ static struct tasdevice_config_info *tasdevice_add_config(
 				__func__, i, cfg_info->nblocks);
 			break;
 		}
-		bk_da[i] = kzalloc_obj(struct tasdev_blk_data, GFP_KERNEL);
+		bk_da[i] = kzalloc_obj(struct tasdev_blk_data);
 		if (!bk_da[i]) {
 			*status = -ENOMEM;
 			break;
@@ -389,7 +389,7 @@ int tasdevice_rca_parser(void *context, const struct firmware *fmw)
 		goto out;
 	}
 
-	cfg_info = kzalloc_objs(*cfg_info, fw_hdr->nconfig, GFP_KERNEL);
+	cfg_info = kzalloc_objs(*cfg_info, fw_hdr->nconfig);
 	if (!cfg_info) {
 		ret = -ENOMEM;
 		tas_priv->fw_state = TASDEVICE_DSP_FW_FAIL;
@@ -2278,7 +2278,7 @@ static int tasdevice_dspfw_ready(const struct firmware *fmw,
 		return -EINVAL;
 	}
 
-	tas_priv->fmw = kzalloc_obj(struct tasdevice_fw, GFP_KERNEL);
+	tas_priv->fmw = kzalloc_obj(struct tasdevice_fw);
 	if (!tas_priv->fmw)
 		return -ENOMEM;
 
