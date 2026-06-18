da2ddcfd0a26490005f12985314c62d89764ce29
Michal Schmidt <mschmidt@redhat.com>
2026-06-18 13:37:57 +0200

    treewide: Replace kmalloc with kmalloc_obj for non-scalar types [idpf]
    JIRA: https://redhat.atlassian.net/browse/RHEL-132835
    
    Conflicts: partial backport. Only idpf hunks.
    
    commit 69050f8d6d075dc01af7a5f2f550a8067510366f
    Author: Kees Cook <kees@kernel.org>
    Date:   Fri Feb 20 23:49:23 2026 -0800
    
        treewide: Replace kmalloc with kmalloc_obj for non-scalar types
    
        This is the result of running the Coccinelle script from
        scripts/coccinelle/api/kmalloc_objs.cocci. The script is designed to
        avoid scalar types (which need careful case-by-case checking), and
        instead replace kmalloc-family calls that allocate struct or union
        object instances:
    
        Single allocations:     kmalloc(sizeof(TYPE), ...)
        are replaced with:      kmalloc_obj(TYPE, ...)
    
        Array allocations:      kmalloc_array(COUNT, sizeof(TYPE), ...)
        are replaced with:      kmalloc_objs(TYPE, COUNT, ...)
    
        Flex array allocations: kmalloc(struct_size(PTR, FAM, COUNT), ...)
        are replaced with:      kmalloc_flex(*PTR, FAM, COUNT, ...)
    
        (where TYPE may also be *VAR)
    
        The resulting allocations no longer return "void *", instead returning
        "TYPE *".
    
        Signed-off-by: Kees Cook <kees@kernel.org>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/intel/idpf/idpf_ethtool.c
+++ drivers/net/ethernet/intel/idpf/idpf_ethtool.c
@@ -200,11 +200,11 @@ static int idpf_add_flow_steer(struct net_device *netdev,
 	if (q_index >= num_rxq)
 		return -EINVAL;
 
-	rule = kzalloc(struct_size(rule, rule_info, 1), GFP_KERNEL);
+	rule = kzalloc_flex(*rule, rule_info, 1, GFP_KERNEL);
 	if (!rule)
 		return -ENOMEM;
 
-	fltr = kzalloc(sizeof(*fltr), GFP_KERNEL);
+	fltr = kzalloc_obj(*fltr, GFP_KERNEL);
 	if (!fltr) {
 		err = -ENOMEM;
 		goto out_free_rule;
@@ -307,7 +307,7 @@ static int idpf_del_flow_steer(struct net_device *netdev,
 	vport_config = vport->adapter->vport_config[np->vport_idx];
 	user_config = &vport_config->user_config;
 
-	rule = kzalloc(struct_size(rule, rule_info, 1), GFP_KERNEL);
+	rule = kzalloc_flex(*rule, rule_info, 1, GFP_KERNEL);
 	if (!rule)
 		return -ENOMEM;
 
