db1425aeace9a35c073eb40a8db6e5d069aca072
Jaroslav Kysela <jkysela@redhat.com>
2026-06-19 15:02:15 +0200

    ASoC: rt5677: Reset the codec during probing
    JIRA: https://issues.redhat.com/browse/RHEL-183296
    
    commit 904e71d42505ca2af19b0031ffffefc8c1fefb48
    Author: Yauhen Kharuzhy <jekhor@gmail.com>
    Date: Wed Feb 11 02:18:38 2026 +0200
    
        ASoC: rt5677: Reset the codec during probing
    
        To ensure that the codec is in a known state, reset it during driver
        initialization.
    
        Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>
        Link: https://patch.msgid.link/20260211002255.4090440-7-jekhor@gmail.com
        Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>

--- sound/soc/codecs/rt5677.c
+++ sound/soc/codecs/rt5677.c
@@ -5580,12 +5580,19 @@ static int rt5677_i2c_probe(struct i2c_client *i2c)
 		dev_err(&i2c->dev, "Failed to request POW_LDO2: %d\n", ret);
 		return ret;
 	}
+
 	rt5677->reset_pin = devm_gpiod_get_optional(&i2c->dev,
-			"realtek,reset", GPIOD_OUT_LOW);
+			"realtek,reset", GPIOD_OUT_HIGH);
+
 	if (IS_ERR(rt5677->reset_pin)) {
 		ret = PTR_ERR(rt5677->reset_pin);
 		dev_err(&i2c->dev, "Failed to request RESET: %d\n", ret);
 		return ret;
+	}
+
+	if (rt5677->reset_pin) {
+		msleep(1);
+		gpiod_set_value_cansleep(rt5677->reset_pin, 0);
 	}
 
 	if (rt5677->pow_ldo2 || rt5677->reset_pin) {
