2699567f321af4beb1847c3c0c5aeda1efd69123
Michal Schmidt <mschmidt@redhat.com>
2026-06-19 13:53:10 +0200

    Convert 'alloc_obj' family to use the new default GFP_KERNEL argument [iavf]
    JIRA: https://redhat.atlassian.net/browse/RHEL-118530
    
    Conflicts: partial backport. Only iavf.
    
    commit bf4afc53b77aeaa48b5409da5c8da6bb4eff7f43
    Author: Linus Torvalds <torvalds@linux-foundation.org>
    Date:   Sat Feb 21 16:37:42 2026 -0800
    
        Convert 'alloc_obj' family to use the new default GFP_KERNEL argument
    
        This was done entirely with mindless brute force, using
    
            git grep -l '\<k[vmz]*alloc_objs*(.*, GFP_KERNEL)' |
                xargs sed -i 's/\(alloc_objs*(.*\), GFP_KERNEL)/\1)/'
    
        to convert the new alloc_obj() users that had a simple GFP_KERNEL
        argument to just drop that argument.
    
        Note that due to the extreme simplicity of the scripting, any slightly
        more complex cases spread over multiple lines would not be triggered:
        they definitely exist, but this covers the vast bulk of the cases, and
        the resulting diff is also then easier to check automatically.
    
        For the same reason the 'flex' versions will be done as a separate
        conversion.
    
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/intel/iavf/iavf_main.c
+++ drivers/net/ethernet/intel/iavf/iavf_main.c
@@ -1787,7 +1787,7 @@ static int iavf_alloc_q_vectors(struct iavf_adapter *adapter)
 	struct iavf_q_vector *q_vector;
 
 	num_q_vectors = adapter->num_msix_vectors - NONQ_VECS;
-	adapter->q_vectors = kzalloc_objs(*q_vector, num_q_vectors, GFP_KERNEL);
+	adapter->q_vectors = kzalloc_objs(*q_vector, num_q_vectors);
 	if (!adapter->q_vectors)
 		return -ENOMEM;
 
@@ -4101,7 +4101,7 @@ static int iavf_configure_clsflower(struct iavf_adapter *adapter,
 		return -EINVAL;
 	}
 
-	filter = kzalloc_obj(*filter, GFP_KERNEL);
+	filter = kzalloc_obj(*filter);
 	if (!filter)
 		return -ENOMEM;
 	filter->cookie = cls_flower->cookie;
@@ -4216,7 +4216,7 @@ static int iavf_add_cls_u32(struct iavf_adapter *adapter,
 		return -EOPNOTSUPP;
 	}
 
-	fltr = kzalloc_obj(*fltr, GFP_KERNEL);
+	fltr = kzalloc_obj(*fltr);
 	if (!fltr)
 		return -ENOMEM;
 
