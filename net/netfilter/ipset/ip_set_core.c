6c81f7969a584a6bd72bbb6cb3aa83118e8b60b2
Florian Westphal <fwestpha@redhat.com>
2026-06-25 20:50:59 +0200

    netfilter: ipset: fix order of kfree_rcu() and rcu_assign_pointer()
    JIRA: https://redhat.atlassian.net/browse/RHEL-186601
    Upstream Status: commit 3ca9982a8882
    
    commit 3ca9982a8882470aa0ac4e8bb9a552b181d1efcd
    Author: Jozsef Kadlecsik <kadlec@netfilter.org>
    Date:   Wed Jun 17 10:41:24 2026 +0200
    
        netfilter: ipset: fix order of kfree_rcu() and rcu_assign_pointer()
    
        Sashiko pointed out that kfree_rcu() was called before
        rcu_assign_pointer() in handling the comment extension.
        Fix the order so that rcu_assign_pointer() called first.
    
        Fixes: b57b2d1fa53f ("netfilter: ipset: Prepare the ipset core to use RCU at set level")
        Signed-off-by: Jozsef Kadlecsik <kadlec@netfilter.org>
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>

--- net/netfilter/ipset/ip_set_core.c
+++ net/netfilter/ipset/ip_set_core.c
@@ -353,8 +353,8 @@ ip_set_init_comment(struct ip_set *set, struct ip_set_comment *comment,
 
 	if (unlikely(c)) {
 		set->ext_size -= sizeof(*c) + strlen(c->str) + 1;
-		kfree_rcu(c, rcu);
 		rcu_assign_pointer(comment->c, NULL);
+		kfree_rcu(c, rcu);
 	}
 	if (!len)
 		return;
@@ -395,8 +395,8 @@ ip_set_comment_free(struct ip_set *set, void *ptr)
 	if (unlikely(!c))
 		return;
 	set->ext_size -= sizeof(*c) + strlen(c->str) + 1;
-	kfree_rcu(c, rcu);
 	rcu_assign_pointer(comment->c, NULL);
+	kfree_rcu(c, rcu);
 }
 
 typedef void (*destroyer)(struct ip_set *, void *);
