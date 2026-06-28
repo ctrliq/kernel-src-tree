9420505213543be9ef239253eda54cada7612ff8
Alex Markuze <amarkuze@redhat.com>
2026-06-28 15:37:38 +0000

    ceph: check the cephx mds auth access for async dirop
    JIRA: https://redhat.atlassian.net/browse/RHEL-180666
    
    ```
    commit 2827badaf8162157271027ea6cc13056890f3e93
    Author: Xiubo Li <xiubli@redhat.com>
    Date:   Wed Nov 8 11:06:05 2023 +0800
    
        ceph: check the cephx mds auth access for async dirop
    
        Before doing the op locally we need to check the cephx access.
    
        Link: https://tracker.ceph.com/issues/61333
        Signed-off-by: Xiubo Li <xiubli@redhat.com>
        Reviewed-by: Milind Changire <mchangir@redhat.com>
        Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    ```
    
    [Conflicts]
    fs/ceph/dir.c:
     - ceph_mdsc_build_path(mdsc, dn, &pathlen, &pathbase, 0) ->
       ceph_mdsc_build_path(dn, &path_info, 0): RHEL9 uses struct
       ceph_path_info wrapper instead of individual out-parameters
     - ceph_mdsc_free_path(path, pathlen) -> ceph_mdsc_free_path_info(&path_info):
       corresponding cleanup helper change
     - Replaced char *path / int pathlen / u64 pathbase locals with
       struct ceph_path_info path_info and char *path
    fs/ceph/file.c:
     - Same ceph_mdsc_build_path / ceph_mdsc_free_path adaptations as dir.c
    
    Signed-off-by: Alex Markuze <amarkuze@redhat.com>

--- fs/ceph/file.c
+++ fs/ceph/file.c
@@ -771,6 +771,8 @@ int ceph_atomic_open(struct inode *dir, struct dentry *dentry,
 	bool try_async = ceph_test_mount_opt(fsc, ASYNC_DIROPS);
 	int mask;
 	int err;
+	struct ceph_path_info path_info = {};
+	char *path;
 
 	dout("atomic_open %p dentry %p '%pd' %s flags %d mode 0%o\n",
 	     dir, dentry, dentry,
@@ -787,6 +789,34 @@ int ceph_atomic_open(struct inode *dir, struct dentry *dentry,
 	 * permission check. The caller will do the truncation afterward.
 	 */
 	flags &= ~O_TRUNC;
+
+	dn = d_find_alias(dir);
+	if (!dn) {
+		try_async = false;
+	} else {
+		path = ceph_mdsc_build_path(dn, &path_info, 0);
+		if (IS_ERR(path)) {
+			try_async = false;
+			err = 0;
+		} else {
+			int fmode = ceph_flags_to_mode(flags);
+
+			mask = MAY_READ;
+			if (fmode & CEPH_FILE_MODE_WR)
+				mask |= MAY_WRITE;
+			err = ceph_mds_check_access(mdsc, path, mask);
+		}
+		ceph_mdsc_free_path_info(&path_info);
+		dput(dn);
+
+		/* For none EACCES cases will let the MDS do the mds auth check */
+		if (err == -EACCES) {
+			return err;
+		} else if (err < 0) {
+			try_async = false;
+			err = 0;
+		}
+	}
 
 retry:
 	if (flags & O_CREAT) {
