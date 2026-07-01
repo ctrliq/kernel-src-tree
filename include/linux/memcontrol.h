739b5e0fafc6df0c883677375cc31898f38f1590
Rafael Aquini <raquini@redhat.com>
2026-07-01 16:05:52 -0400

    mm: separate folio_split_memcg_refs() from split_page_memcg()
    JIRA: https://redhat.atlassian.net/browse/RHEL-145694
    
    commit fa23a338de93aa03eb0b6146a0440f5762309f85
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Fri Mar 14 13:36:11 2025 +0000
    
        mm: separate folio_split_memcg_refs() from split_page_memcg()
    
        Patch series "Minor memcg cleanups & prep for memdescs", v2.
    
        Separate the handling of accounted folios and GFP_ACCOUNT pages for easier
        to understand code.  For more detail, see
        https://lore.kernel.org/linux-mm/Z9LwTOudOlCGny3f@casper.infradead.org/
    
        This patch (of 5):
    
        Folios always use memcg_data to refer to the mem_cgroup while pages
        allocated with GFP_ACCOUNT have a pointer to the obj_cgroup.  Since the
        caller already knows what it has, split the function into two and then we
        don't need to check.
    
        Move the assignment of split folio memcg_data to the point where we set up
        the other parts of the new folio.  That leaves folio_split_memcg_refs()
        just handling the memcg accounting.
    
        Link: https://lkml.kernel.org/r/20250314133617.138071-1-willy@infradead.org
        Link: https://lkml.kernel.org/r/20250314133617.138071-2-willy@infradead.org
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Acked-by: Johannes Weiner <hannes@cmpxchg.org>
        Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
        Acked-by: Zi Yan <ziy@nvidia.com>
        Acked-by: Roman Gushchin <roman.gushchin@linux.dev>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: Matthew Wilcow (Oracle) <willy@infradead.org>
        Cc: Michal Hocko <mhocko@kernel.org>
        Cc: Muchun Song <muchun.song@linux.dev>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1047,6 +1047,8 @@ static inline void memcg_memory_event_mm(struct mm_struct *mm,
 }
 
 void split_page_memcg(struct page *head, int old_order, int new_order);
+void folio_split_memcg_refs(struct folio *folio, unsigned old_order,
+		unsigned new_order);
 
 static inline u64 cgroup_id_from_mm(struct mm_struct *mm)
 {
@@ -1468,6 +1470,11 @@ void count_memcg_event_mm(struct mm_struct *mm, enum vm_event_item idx)
 }
 
 static inline void split_page_memcg(struct page *head, int old_order, int new_order)
+{
+}
+
+static inline void folio_split_memcg_refs(struct folio *folio,
+		unsigned old_order, unsigned new_order)
 {
 }
 
