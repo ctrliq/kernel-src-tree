4dd223c83474573cc78aa400a575382a94ed6dcd
Rafael Aquini <raquini@redhat.com>
2026-07-01 16:08:59 -0400

    mm: remove devmap related functions and page table bits
    JIRA: https://redhat.atlassian.net/browse/RHEL-145694
    Conflicts:
      * with the removal of ARCH_HAS_PTE_DEVMAP as a dependency for
        CONFIG_ZONE_DEVICE s390x builds can finally have the latter
        enabled properly, and as we already have CONFIG_ZONE_DEVICE
        being set to Y under "redhat/configs/rhel/generic/" there is
        nothing else to be done in this front. However, we now need
        to include s390x's CONFIG_DEVICE_PRIVATE selection and for
        that matter, we are following kernel-ark and consolidating
        all of the individual arch selections (all se to Y) under a
        single entry "redhat/configs/common/generic/CONFIG_DEVICE_PRIVATE"
    
    commit d438d273417055241ebaaf1ba3be23459fc27cba
    Author: Alistair Popple <apopple@nvidia.com>
    Date:   Thu Jun 19 18:58:03 2025 +1000
    
        mm: remove devmap related functions and page table bits
    
        Now that DAX and all other reference counts to ZONE_DEVICE pages are
        managed normally there is no need for the special devmap PTE/PMD/PUD page
        table bits.  So drop all references to these, freeing up a software
        defined page table bit on architectures supporting it.
    
        Link: https://lkml.kernel.org/r/6389398c32cc9daa3dfcaa9f79c7972525d310ce.1750323463.git-series.apopple@nvidia.com
        Signed-off-by: Alistair Popple <apopple@nvidia.com>
        Acked-by: Will Deacon <will@kernel.org> # arm64
        Acked-by: David Hildenbrand <david@redhat.com>
        Suggested-by: Chunyan Zhang <zhang.lyra@gmail.com>
        Reviewed-by: Björn Töpel <bjorn@rivosinc.com>
        Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
        Cc: Balbir Singh <balbirs@nvidia.com>
        Cc: Björn Töpel <bjorn@kernel.org>
        Cc: Christoph Hellwig <hch@lst.de>
        Cc: Dan Williams <dan.j.williams@intel.com>
        Cc: Deepak Gupta <debug@rivosinc.com>
        Cc: Gerald Schaefer <gerald.schaefer@linux.ibm.com>
        Cc: Inki Dae <m.szyprowski@samsung.com>
        Cc: John Groves <john@groves.net>
        Cc: John Hubbard <jhubbard@nvidia.com>
        Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
        Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- mm/madvise.c
+++ mm/madvise.c
@@ -1075,7 +1075,7 @@ static int guard_install_pud_entry(pud_t *pud, unsigned long addr,
 	pud_t pudval = pudp_get(pud);
 
 	/* If huge return >0 so we abort the operation + zap. */
-	return pud_trans_huge(pudval) || pud_devmap(pudval);
+	return pud_trans_huge(pudval);
 }
 
 static int guard_install_pmd_entry(pmd_t *pmd, unsigned long addr,
@@ -1084,7 +1084,7 @@ static int guard_install_pmd_entry(pmd_t *pmd, unsigned long addr,
 	pmd_t pmdval = pmdp_get(pmd);
 
 	/* If huge return >0 so we abort the operation + zap. */
-	return pmd_trans_huge(pmdval) || pmd_devmap(pmdval);
+	return pmd_trans_huge(pmdval);
 }
 
 static int guard_install_pte_entry(pte_t *pte, unsigned long addr,
@@ -1195,7 +1195,7 @@ static int guard_remove_pud_entry(pud_t *pud, unsigned long addr,
 	pud_t pudval = pudp_get(pud);
 
 	/* If huge, cannot have guard pages present, so no-op - skip. */
-	if (pud_trans_huge(pudval) || pud_devmap(pudval))
+	if (pud_trans_huge(pudval))
 		walk->action = ACTION_CONTINUE;
 
 	return 0;
@@ -1207,7 +1207,7 @@ static int guard_remove_pmd_entry(pmd_t *pmd, unsigned long addr,
 	pmd_t pmdval = pmdp_get(pmd);
 
 	/* If huge, cannot have guard pages present, so no-op - skip. */
-	if (pmd_trans_huge(pmdval) || pmd_devmap(pmdval))
+	if (pmd_trans_huge(pmdval))
 		walk->action = ACTION_CONTINUE;
 
 	return 0;
