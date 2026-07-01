8d670b8c3bed029c7af90e163f5d48592628d716
Rafael Aquini <raquini@redhat.com>
2026-07-01 16:02:25 -0400

    mm: vmstat: move sysctls to mm/vmstat.c
    JIRA: https://redhat.atlassian.net/browse/RHEL-145694
    
    commit b8974b89603c601f64b302d80129b0faf4879199
    Author: Kaixiong Yu <yukaixiong@huawei.com>
    Date:   Sat Jan 11 15:07:36 2025 +0800
    
        mm: vmstat: move sysctls to mm/vmstat.c
    
        This moves all vmstat related sysctls to its own file, removes useless
        extern variable declarations, and do some related clean-ups. To avoid
        compiler warnings when CONFIG_PROC_FS is not defined, add the macro
        definition CONFIG_PROC_FS ahead CONFIG_NUMA in vmstat.c.
    
        Signed-off-by: Kaixiong Yu <yukaixiong@huawei.com>
        Reviewed-by: Kees Cook <kees@kernel.org>
        Reviewed-by: Jeff Layton <jlayton@kernel.org>
        Signed-off-by: Joel Granados <joel.granados@kernel.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -48,7 +48,6 @@
 #include <linux/limits.h>
 #include <linux/dcache.h>
 #include <linux/syscalls.h>
-#include <linux/vmstat.h>
 #include <linux/nfs_fs.h>
 #include <linux/acpi.h>
 #include <linux/reboot.h>
@@ -1851,17 +1850,6 @@ static const struct ctl_table vm_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_TWO_HUNDRED,
 	},
-#ifdef CONFIG_NUMA
-	{
-		.procname	= "numa_stat",
-		.data		= &sysctl_vm_numa_stat,
-		.maxlen		= sizeof(int),
-		.mode		= 0644,
-		.proc_handler	= sysctl_vm_numa_stat_handler,
-		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_ONE,
-	},
-#endif
 	{
 		.procname	= "drop_caches",
 		.data		= &sysctl_drop_caches,
@@ -1925,22 +1913,6 @@ static const struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
-	},
-#endif
-#ifdef CONFIG_SMP
-	{
-		.procname	= "stat_interval",
-		.data		= &sysctl_stat_interval,
-		.maxlen		= sizeof(sysctl_stat_interval),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec_jiffies,
-	},
-	{
-		.procname	= "stat_refresh",
-		.data		= NULL,
-		.maxlen		= 0,
-		.mode		= 0600,
-		.proc_handler	= vmstat_refresh,
 	},
 #endif
 #ifdef CONFIG_MMU
