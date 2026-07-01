3dad77a305c8022d180ad85c69012724a6b12451
Jonathan Maple <jmaple@ciq.com>
2026-07-01 00:03:06 -0400

    net/mlx5: Add software system image GUID infrastructure
    jira KERNEL-1233
    Rebuild_History Non-Buildable kernel-5.14.0-687.20.1.el9_8
    commit-author Mark Bloch <mbloch@nvidia.com>
    commit 7718f2a8b87af7363d60819ac0ac0da8b2f8ff00
    
    Replace direct hardware system image GUID usage with a new software
    system image GUID function that supports variable-length identifiers.
    
    Key changes:
    - Add mlx5_query_nic_sw_system_image_guid() function with length parameter.
    - Update all callsites to use the new function and buffer/length approach.
    - Modify mapping contexts to use byte arrays instead of u64 keys.
    - Update devcom matching to support variable-length keys.
    - Change mlx5_same_hw_devs() to use buffer comparison instead of u64.
    
    This refactoring prepares the infrastructure for balance ID support,
    which requires extending the system image GUID with additional data.
    The change maintains backward compatibility while enabling future
    enhancements.
    
    	Signed-off-by: Mark Bloch <mbloch@nvidia.com>
    	Reviewed-by: Shay Drori <shayd@nvidia.com>
    	Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Link: https://patch.msgid.link/1761211020-925651-3-git-send-email-tariqt@nvidia.com
    	Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    (cherry picked from commit 7718f2a8b87af7363d60819ac0ac0da8b2f8ff00)
    	Signed-off-by: Jonathan Maple <jmaple@ciq.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -1356,4 +1356,7 @@ static inline struct net *mlx5_core_net(struct mlx5_core_dev *dev)
 {
 	return devlink_net(priv_to_devlink(dev));
 }
+
+#define MLX5_SW_IMAGE_GUID_MAX_BYTES 8
+
 #endif /* MLX5_DRIVER_H */
