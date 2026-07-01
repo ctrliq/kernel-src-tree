2c5c37ce5db4013c7814f3abff7d0ed6f1f93697
Rafael Aquini <raquini@redhat.com>
2026-07-01 16:05:52 -0400

    mm: simplify split_page_memcg()
    JIRA: https://redhat.atlassian.net/browse/RHEL-145694
    
    commit 1506c25508acd740ced5e92c539ed3d12f622c5b
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Fri Mar 14 13:36:12 2025 +0000
    
        mm: simplify split_page_memcg()
    
        The last argument to split_page_memcg() is now always 0, so remove it,
        effectively reverting commit b8791381d7ed.
    
        Link: https://lkml.kernel.org/r/20250314133617.138071-3-willy@infradead.org
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Acked-by: Johannes Weiner <hannes@cmpxchg.org>
        Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
        Acked-by: Zi Yan <ziy@nvidia.com>
        Acked-by: Roman Gushchin <roman.gushchin@linux.dev>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: Michal Hocko <mhocko@kernel.org>
        Cc: Muchun Song <muchun.song@linux.dev>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1046,7 +1046,7 @@ static inline void memcg_memory_event_mm(struct mm_struct *mm,
 	rcu_read_unlock();
 }
 
-void split_page_memcg(struct page *head, int old_order, int new_order);
+void split_page_memcg(struct page *first, unsigned order);
 void folio_split_memcg_refs(struct folio *folio, unsigned old_order,
 		unsigned new_order);
 
@@ -1469,7 +1469,7 @@ void count_memcg_event_mm(struct mm_struct *mm, enum vm_event_item idx)
 {
 }
 
-static inline void split_page_memcg(struct page *head, int old_order, int new_order)
+static inline void split_page_memcg(struct page *first, unsigned order)
 {
 }
 
