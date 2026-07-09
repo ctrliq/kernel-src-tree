ef519f54cecb8734eb25b998faef60f4e509d966
Michal Schmidt <mschmidt@redhat.com>
2026-07-09 13:13:13 +0200

    ice: remove ice_q_stats struct and use struct_group
    JIRA: https://redhat.atlassian.net/browse/RHEL-177450
    
    commit b470944ee6e8f51c1d561899c045286b6c416b46
    Author: Jacob Keller <jacob.e.keller@intel.com>
    Date:   Thu Nov 20 12:20:43 2025 -0800
    
        ice: remove ice_q_stats struct and use struct_group
    
        The ice_qp_reset_stats function resets the stats for all rings on a VSI. It
        currently behaves differently for Tx and Rx rings. For Rx rings, it only
        clears the rx_stats which do not include the pkt and byte counts. For Tx
        rings and XDP rings, it clears only the pkt and byte counts.
    
        We could add extra memset calls to cover both the stats and relevant
        tx/rx stats fields. Instead, lets convert stats into a struct_group which
        contains both the pkts and bytes fields as well as the Tx or Rx stats, and
        remove the ice_q_stats structure entirely.
    
        The only remaining user of ice_q_stats is the ice_q_stats_len function in
        ice_ethtool.c, which just counts the number of fields. Replace this with a
        simple multiplication by 2. I find this to be simpler to reason about than
        relying on knowing the layout of the ice_q_stats structure.
    
        Now that the stats field of the ice_ring_stats covers all of the statistic
        values, the ice_qp_reset_stats function will properly zero out all of the
        fields.
    
        Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
        Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
        Reviewed-by: Simon Horman <horms@kernel.org>
        Tested-by: Rinitha S <sx.rinitha@intel.com> (A Contingent worker at Intel)
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Assisted-by: Patchpal
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/intel/ice/ice_txrx.h
+++ drivers/net/ethernet/intel/ice/ice_txrx.h
@@ -206,11 +206,6 @@ struct ice_rx_buf {
 	bool has_data;
 };
 
-struct ice_q_stats {
-	u64 pkts;
-	u64 bytes;
-};
-
 struct ice_txq_stats {
 	u64 restart_q;
 	u64 tx_busy;
@@ -226,12 +221,15 @@ struct ice_rxq_stats {
 
 struct ice_ring_stats {
 	struct rcu_head rcu;	/* to avoid race on free */
-	struct ice_q_stats stats;
 	struct u64_stats_sync syncp;
-	union {
-		struct ice_txq_stats tx_stats;
-		struct ice_rxq_stats rx_stats;
-	};
+	struct_group(stats,
+		u64 pkts;
+		u64 bytes;
+		union {
+			struct ice_txq_stats tx_stats;
+			struct ice_rxq_stats rx_stats;
+		};
+	);
 };
 
 enum ice_ring_state_t {
