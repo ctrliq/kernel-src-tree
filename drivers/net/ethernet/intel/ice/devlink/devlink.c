1c81a14e5f3bdc1baeb11ed5800569d12183f6ff
Michal Schmidt <mschmidt@redhat.com>
2026-07-09 13:13:22 +0200

    drivers: net: ice: fix devlink parameters get without irdma
    JIRA: https://redhat.atlassian.net/browse/RHEL-177450
    
    commit bd98c6204d1195973b1760fe45860863deb6200c
    Author: Nikolay Aleksandrov <nikolay@nvidia.com>
    Date:   Fri Feb 13 10:48:41 2026 +0200
    
        drivers: net: ice: fix devlink parameters get without irdma
    
        If CONFIG_IRDMA isn't enabled but there are ice NICs in the system, the
        driver will prevent full devlink dev param show dump because its rdma get
        callbacks return ENODEV and stop the dump. For example:
         $ devlink dev param show
         pci/0000:82:00.0:
           name msix_vec_per_pf_max type generic
             values:
               cmode driverinit value 2
           name msix_vec_per_pf_min type generic
             values:
               cmode driverinit value 2
         kernel answers: No such device
    
        Returning EOPNOTSUPP allows the dump to continue so we can see all devices'
        devlink parameters.
    
        Fixes: c24a65b6a27c ("iidc/ice/irdma: Update IDC to support multiple consumers")
        Signed-off-by: Nikolay Aleksandrov <nikolay@nvidia.com>
        Tested-by: Rinitha S <sx.rinitha@intel.com> (A Contingent worker at Intel)
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    
    Signed-off-by: Michal Schmidt <mschmidt@redhat.com>

--- drivers/net/ethernet/intel/ice/devlink/devlink.c
+++ drivers/net/ethernet/intel/ice/devlink/devlink.c
@@ -1359,7 +1359,7 @@ ice_devlink_enable_roce_get(struct devlink *devlink, u32 id,
 
 	cdev = pf->cdev_info;
 	if (!cdev)
-		return -ENODEV;
+		return -EOPNOTSUPP;
 
 	ctx->val.vbool = !!(cdev->rdma_protocol & IIDC_RDMA_PROTOCOL_ROCEV2);
 
@@ -1425,7 +1425,7 @@ ice_devlink_enable_iw_get(struct devlink *devlink, u32 id,
 
 	cdev = pf->cdev_info;
 	if (!cdev)
-		return -ENODEV;
+		return -EOPNOTSUPP;
 
 	ctx->val.vbool = !!(cdev->rdma_protocol & IIDC_RDMA_PROTOCOL_IWARP);
 
