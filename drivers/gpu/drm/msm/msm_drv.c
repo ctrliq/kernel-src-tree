db166fea0a1d1b94fac1635c40f68ae3d038a81c
Karol Herbst <kherbst@redhat.com>
2026-07-12 22:51:38 +0200

    drm/gem: Make the GEM LRU lock part of drm_device
    JIRA: https://redhat.atlassian.net/browse/RHEL-180328
    
    Conflicts:
     * We do not enable the msm driver, so changes in the msm_gem.c,
       msm_gem_shrinker.c, msm_gem_vma.c and msm_ringbuffer.c files were
       omitted.
    
    commit 6e13c85cac3bc92f13535fd18a96e206f9f9df19
    Author: Boris Brezillon <boris.brezillon@collabora.com>
    Date:   Mon May 18 13:41:45 2026 +0200
    
        drm/gem: Make the GEM LRU lock part of drm_device
    
        [ Upstream commit 379e8f1ca5e919b130b40d8115d92a536e5f8d7a ]
    
        Recently, a few races have been discovered in the GEM LRU logic, all
        of them caused by the fact the LRU lock is accessed through
        gem->lru->lock, and that very same lock also protects changes to
        gem->lru, leading to situations where gem->lru needs to first be
        accessed without the lock held, to then get the lru to access the lock
        through and finally take the lock and do the expected operation.
    
        Currently, the only driver making use of this API (MSM) declares a
        device-wide lock, and the user we're about to add (panthor) will
        do the same. There's no evidence that we will ever have a driver
        that wants different pools of LRUs protected by different locks under
        the same drm_device. So we're better off moving this lock to drm_device
        and always locking it through obj->dev->gem_lru_mutex, or directly
        through dev->gem_lru_mutex.
    
        If anyone ever needs more fine-grained locking, this can be revisited
        to pass some drm_gem_lru_pool object representing the pool of LRUs
        under a specific lock, but for now, the per-device lock seems to be
        enough.
    
        Fixes: e7c2af13f811 ("drm/gem: Add LRU/shrinker helper")
        Reported-by: Chia-I Wu <olvaffe@gmail.com>
        Closes: https://gitlab.freedesktop.org/panfrost/linux/-/work_items/86
        Reviewed-by: Rob Clark <rob.clark@oss.qualcomm.com>
        Reviewed-by: Liviu Dudau <liviu.dudau@arm.com>
        Reviewed-by: Steven Price <steven.price@arm.com>
        Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
        Link: https://patch.msgid.link/20260518-panthor-shrinker-fixes-v4-1-1920234470d5@collabora.com
        Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
        Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    Signed-off-by: Karol Herbst <kherbst@redhat.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -238,15 +238,14 @@ static int msm_drm_init(struct device *dev, const struct drm_driver *drv)
 	/*
 	 * Initialize the LRUs:
 	 */
-	mutex_init(&priv->lru.lock);
-	drm_gem_lru_init(&priv->lru.unbacked, &priv->lru.lock);
-	drm_gem_lru_init(&priv->lru.pinned,   &priv->lru.lock);
-	drm_gem_lru_init(&priv->lru.willneed, &priv->lru.lock);
-	drm_gem_lru_init(&priv->lru.dontneed, &priv->lru.lock);
+	drm_gem_lru_init(&priv->lru.unbacked);
+	drm_gem_lru_init(&priv->lru.pinned);
+	drm_gem_lru_init(&priv->lru.willneed);
+	drm_gem_lru_init(&priv->lru.dontneed);
 
 	/* Teach lockdep about lock ordering wrt. shrinker: */
 	fs_reclaim_acquire(GFP_KERNEL);
-	might_lock(&priv->lru.lock);
+	might_lock(&ddev->gem_lru_mutex);
 	fs_reclaim_release(GFP_KERNEL);
 
 	if (priv->kms_init) {
