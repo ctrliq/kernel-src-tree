ecd2969027d811d65dea6a72cd85f1d2201f8014
Anubhav Shelat <ashelat@redhat.com>
2026-07-15 15:12:40 -0400

    perf/x86/intel: Initialize architectural PEBS
    JIRA: https://issues.redhat.com/browse/RHEL-45125
    
    Conflict: Missing 78255eb (x86/msr: Rename 'wrmsrl()' to 'wrmsrq()')
    
    upstream
    ========
    commit d243d0bb64af1e90ec18ac2fa6e7cadfe8895913
    Author: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Date: Wed Oct 29 18:21:29 2025 +0800
    
    description
    ===========
    arch-PEBS leverages CPUID.23H.4/5 sub-leaves enumerate arch-PEBS
    supported capabilities and counters bitmap. This patch parses these 2
    sub-leaves and initializes arch-PEBS capabilities and corresponding
    structures.
    
    Since IA32_PEBS_ENABLE and MSR_PEBS_DATA_CFG MSRs are no longer existed
    for arch-PEBS, arch-PEBS doesn't need to manipulate these MSRs. Thus add
    a simple pair of __intel_pmu_pebs_enable/disable() callbacks for
    arch-PEBS.
    
        Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Link: https://patch.msgid.link/20251029102136.61364-6-dapeng1.mi@linux.intel.com
    
    Signed-off-by: Anubhav Shelat <ashelat@redhat.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -724,6 +724,12 @@ enum atom_native_id {
 	skt_native_id           = 0x3,  /* Skymont */
 };
 
+struct arch_pebs_cap {
+	u64 caps;
+	u64 counters;
+	u64 pdists;
+};
+
 struct x86_hybrid_pmu {
 	struct pmu			pmu;
 	const char			*name;
@@ -767,6 +773,8 @@ struct x86_hybrid_pmu {
 	unsigned int			late_ack	:1,
 					mid_ack		:1,
 					enabled_ack	:1;
+
+	struct arch_pebs_cap		arch_pebs_cap;
 
 	u64				pebs_data_source[PERF_PEBS_DATA_SOURCE_MAX];
 };
@@ -922,7 +930,7 @@ struct x86_pmu {
 	union perf_capabilities intel_cap;
 
 	/*
-	 * Intel DebugStore bits
+	 * Intel DebugStore and PEBS bits
 	 */
 	unsigned int	bts			:1,
 			bts_active		:1,
@@ -933,7 +941,8 @@ struct x86_pmu {
 			pebs_no_tlb		:1,
 			pebs_no_isolation	:1,
 			pebs_block		:1,
-			pebs_ept		:1;
+			pebs_ept		:1,
+			arch_pebs		:1;
 	int		pebs_record_size;
 	int		pebs_buffer_size;
 	u64		pebs_events_mask;
@@ -944,6 +953,11 @@ struct x86_pmu {
 	unsigned long	large_pebs_flags;
 	u64		rtm_abort_event;
 	u64		pebs_capable;
+
+	/*
+	 * Intel Architectural PEBS
+	 */
+	struct arch_pebs_cap arch_pebs_cap;
 
 	/*
 	 * Intel LBR
@@ -1240,7 +1254,7 @@ int x86_reserve_hardware(void);
 
 void x86_release_hardware(void);
 
-int x86_pmu_max_precise(void);
+int x86_pmu_max_precise(struct pmu *pmu);
 
 void hw_perf_lbr_event_destroy(struct perf_event *event);
 
@@ -1812,6 +1826,11 @@ static inline int intel_pmu_max_num_pebs(struct pmu *pmu)
 {
 	static_assert(MAX_PEBS_EVENTS == 32);
 	return fls((u32)hybrid(pmu, pebs_events_mask));
+}
+
+static inline bool intel_pmu_has_pebs(void)
+{
+	return x86_pmu.ds_pebs || x86_pmu.arch_pebs;
 }
 
 #else /* CONFIG_CPU_SUP_INTEL */
