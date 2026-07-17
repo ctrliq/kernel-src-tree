d66412224fcff99c1adc68b4b95405e90ad061e9
Waiman Long <longman@redhat.com>
2026-07-16 22:25:59 -0400

    mm/mempolicy: fix weighted interleave auto sysfs name
    JIRA: https://redhat.atlassian.net/browse/RHEL-193649
    Conflicts:
      A context diff due to missing upstream commit 1ec8a6e30e9c ("mm/mempolicy:
      skip unnecessary synchronize_rcu()").
    
    commit 8fedac321fb0fb368d4c14674e2a64852b4f225e
    Author: Joshua Hahn <joshua.hahnjy@gmail.com>
    Date:   Tue, 7 Apr 2026 07:14:14 -0700
    
        mm/mempolicy: fix weighted interleave auto sysfs name
    
        The __ATTR macro is a utility that makes defining kobj_attributes easier
        by stringfying the name, verifying the mode, and setting the show/store
        fields in a single initializer.  It takes a raw token as the first value,
        rather than a string, so that __ATTR family macros like __ATTR_RW can
        token-paste it for inferring the _show / _store function names.
    
        Commit e341f9c3c841 ("mm/mempolicy: Weighted Interleave Auto-tuning") used
        the __ATTR macro to define the "auto" sysfs for weighted interleave.  A
        few months later, commit 2fb6915fa22d ("compiler_types.h: add "auto" as a
        macro for "__auto_type"") introduced a #define macro which expanded auto
        into __auto_type.
    
        This led to the "auto" token passed into __ATTR to be expanded out into
        __auto_type, and the sysfs entry to be displayed as __auto_type as well.
    
        Expand out the __ATTR macro and directly pass a string "auto" instead of
        the raw token 'auto' to prevent it from being expanded out.  Also bypass
        the VERIFY_OCTAL_PERMISSIONS check by triple checking that 0664 is indeed
        the intended permissions for this sysfs file.
    
        Before:
        $ ls /sys/kernel/mm/mempolicy/weighted_interleave
        __auto_type  node0
    
        After:
        $ ls /sys/kernel/mm/mempolicy/weighted_interleave/
        auto  node0
    
        Link: https://lore.kernel.org/20260407141415.3080960-1-joshua.hahnjy@gmail.com
        Fixes: 2fb6915fa22d ("compiler_types.h: add "auto" as a macro for "__auto_type"")
        Signed-off-by: Joshua Hahn <joshua.hahnjy@gmail.com>
        Reviewed-by: Gregory Price <gourry@gourry.net>
        Reviewed-by: Rakie Kim <rakie.kim@sk.com>
        Acked-by: David Hildenbrand (Arm) <david@kernel.org>
        Acked-by: Zi Yan <ziy@nvidia.com>
        Cc: Alistair Popple <apopple@nvidia.com>
        Cc: Byungchul Park <byungchul@sk.com>
        Cc: "Huang, Ying" <ying.huang@linux.alibaba.com>
        Cc: Matthew Brost <matthew.brost@intel.com>
        Cc: Rakie Kim <rakie.kim@sk.com>
        Cc: Ying Huang <ying.huang@linux.alibaba.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -3724,9 +3724,11 @@ static void wi_state_free(void)
 	kfree(old_wi_state);
 }
 
-static struct kobj_attribute wi_auto_attr =
-	__ATTR(auto, 0664, weighted_interleave_auto_show,
-			   weighted_interleave_auto_store);
+static struct kobj_attribute wi_auto_attr = {
+	.attr = { .name = "auto", .mode = 0664 },
+	.show = weighted_interleave_auto_show,
+	.store = weighted_interleave_auto_store,
+};
 
 static void wi_cleanup(void) {
 	sysfs_remove_file(&wi_group->wi_kobj, &wi_auto_attr.attr);
