9c5e0b8dbba1260575e6e1409d39e999c3f738b6
Paulo Alcantara <paalcant@redhat.com>
2026-07-28 16:39:06 -0300

    smb: client: Remove unnecessary selection of CRYPTO_ECB
    JIRA: https://issues.redhat.com/browse/RHEL-216926
    
    commit 314ef7f7249b87cdee368ab6b3e89774ac00e567
    Author: Eric Biggers <ebiggers@kernel.org>
    Date:   Sat Apr 4 12:42:10 2026 -0700
    
        smb: client: Remove unnecessary selection of CRYPTO_ECB
        
        Since the SMB client never uses any ecb(...) algorithm from the
        crypto_skcipher API, selecting CRYPTO_ECB is unnecessary.
        
        Specifically, it has been unnecessary since commit 06deeec77a5a ("cifs:
        Fix smbencrypt() to stop pointing a scatterlist at the stack") in 2016.
        
        Signed-off-by: Eric Biggers <ebiggers@kernel.org>
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/cifsfs.c
+++ fs/smb/client/cifsfs.c
@@ -2151,7 +2151,6 @@ MODULE_DESCRIPTION
 	("VFS to access SMB3 servers e.g. Samba, Macs, Azure and Windows (and "
 	"also older servers complying with the SNIA CIFS Specification)");
 MODULE_VERSION(CIFS_VERSION);
-MODULE_SOFTDEP("ecb");
 MODULE_SOFTDEP("hmac");
 MODULE_SOFTDEP("md5");
 MODULE_SOFTDEP("nls");
