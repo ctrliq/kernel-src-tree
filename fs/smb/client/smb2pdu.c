6f4610b2540cd8fd7d95a15220392fae29a63df5
Paulo Alcantara <paalcant@redhat.com>
2026-07-28 17:49:05 -0300

    smb: client: add tracepoints for lock operations
    JIRA: https://issues.redhat.com/browse/RHEL-216926
    
    commit eb4d3691fc9bf3687026daa8ec2cf32cfd962a11
    Author: Bharath SM <bharathsm@microsoft.com>
    Date:   Tue Apr 14 21:48:02 2026 +0530
    
        smb: client: add tracepoints for lock operations
        
        Add tracepoints when lock operations are sent to the
        server with details including lock offset, length, and flags.
        
        smb3_lock_enter: before sending lock request
        smb3_lock_done: lock acquired successfully
        smb3_lock_err: lock request failed
        smb3_lock_cached: lock granted from local cache (no server roundtrip)
        
        Signed-off-by: Bharath SM <bharathsm@microsoft.com>
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/smb2pdu.c
+++ fs/smb/client/smb2pdu.c
@@ -6092,6 +6092,11 @@ replay_again:
 		smb2_set_replay(server, &rqst);
 	}
 
+	trace_smb3_lock_enter(xid, persist_fid, tcon->tid, tcon->ses->Suid,
+			      le64_to_cpu(buf[0].Offset),
+			      le64_to_cpu(buf[0].Length),
+			      le32_to_cpu(buf[0].Flags), num_lock, 0);
+
 	rc = cifs_send_recv(xid, tcon->ses, server,
 			    &rqst, &resp_buf_type, flags,
 			    &rsp_iov);
@@ -6100,7 +6105,15 @@ replay_again:
 		cifs_dbg(FYI, "Send error in smb2_lockv = %d\n", rc);
 		cifs_stats_fail_inc(tcon, SMB2_LOCK_HE);
 		trace_smb3_lock_err(xid, persist_fid, tcon->tid,
-				    tcon->ses->Suid, rc);
+				    tcon->ses->Suid,
+				    le64_to_cpu(buf[0].Offset),
+				    le64_to_cpu(buf[0].Length),
+				    le32_to_cpu(buf[0].Flags), num_lock, rc);
+	} else {
+		trace_smb3_lock_done(xid, persist_fid, tcon->tid, tcon->ses->Suid,
+				     le64_to_cpu(buf[0].Offset),
+				     le64_to_cpu(buf[0].Length),
+				     le32_to_cpu(buf[0].Flags), num_lock, 0);
 	}
 
 	if (is_replayable_error(rc) &&
