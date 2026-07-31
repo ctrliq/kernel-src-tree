03dde8bdd2b1bc00f040a8f010ad5c90d1a84d28
Rafael Aquini <raquini@redhat.com>
2026-07-31 14:48:20 -0400

    mm/hugetlb: remove prepare_hugepage_range()
    JIRA: https://redhat.atlassian.net/browse/RHEL-145695
    
    commit eff41389d8249a1a5a67faa440255ed8e526803a
    Author: Peter Xu <peterx@redhat.com>
    Date:   Fri Jun 27 12:07:07 2025 -0400
    
        mm/hugetlb: remove prepare_hugepage_range()
    
        Only mips and loongarch implemented this API, however what it does was
        checking against stack overflow for either len or addr.  That's already
        done in arch's arch_get_unmapped_area*() functions, even though it may not
        be 100% identical checks.
    
        For example, for both of the architectures, there will be a trivial
        difference on how stack top was defined.  The old code uses STACK_TOP
        which may be slightly smaller than TASK_SIZE on either of them, but the
        hope is that shouldn't be a problem.
    
        It means the whole API is pretty much obsolete at least now, remove it
        completely.
    
        Link: https://lkml.kernel.org/r/20250627160707.2124580-1-peterx@redhat.com
        Signed-off-by: Peter Xu <peterx@redhat.com>
        Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
        Reviewed-by: Oscar Salvador <osalvador@suse.de>
        Acked-by: David Hildenbrand <david@redhat.com>
        Reviewed-by: Liam R. Howlett <Liam.Howlett@oracle.com>
        Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
        Cc: Huacai Chen <chenhuacai@kernel.org>
        Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
        Cc: Muchun Song <muchun.song@linux.dev>
        Cc: Jann Horn <jannh@google.com>
        Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
        Cc: Pedro Falcato <pfalcato@suse.de>
        Cc: Vlastimil Babka <vbabka@suse.cz>
        Cc: Zi Yan <ziy@nvidia.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <raquini@redhat.com>

--- arch/loongarch/include/asm/hugetlb.h
+++ arch/loongarch/include/asm/hugetlb.h
@@ -10,20 +10,6 @@
 
 uint64_t pmd_to_entrylo(unsigned long pmd_val);
 
-#define __HAVE_ARCH_PREPARE_HUGEPAGE_RANGE
-static inline int prepare_hugepage_range(struct file *file,
-					 unsigned long addr,
-					 unsigned long len)
-{
-	unsigned long task_size = STACK_TOP;
-
-	if (len > task_size)
-		return -ENOMEM;
-	if (task_size - len < addr)
-		return -EINVAL;
-	return 0;
-}
-
 #define __HAVE_ARCH_HUGE_PTEP_GET_AND_CLEAR
 static inline pte_t huge_ptep_get_and_clear(struct mm_struct *mm,
 					    unsigned long addr, pte_t *ptep,
