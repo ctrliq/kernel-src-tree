6f70f7c6387e4309643eb1f9e617d465b2ca7c77
Florian Westphal <fwestpha@redhat.com>
2026-08-03 11:17:50 +0200

    ipvs: clear the nfct flag under lock
    JIRA: https://redhat.atlassian.net/browse/RHEL-185609
    Upstream Status: commit da7d894c41d5
    
    commit da7d894c41d5910daae2b8ffa024c52ff0a4df6a
    Author: Julian Anastasov <ja@ssi.bg>
    Date:   Wed Jul 22 13:25:39 2026 +0300
    
        ipvs: clear the nfct flag under lock
    
        Sashiko warns that cp->flags should be changed under cp->lock
    
        Fixes: 35dfb013149f ("ipvs: queue delayed work to expire no destination connections if expire_nodest_conn=1")
        Fixes: f0a5e4d7a594 ("ipvs: allow connection reuse for unconfirmed conntrack")
        Link: https://sashiko.dev/#/patchset/CALMqdkR704S2BG_QD_bgHTFp2%2B1QCi7n0T4zoZyTo8mDZevYSA%40mail.gmail.com
        Signed-off-by: Julian Anastasov <ja@ssi.bg>
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>

--- net/netfilter/ipvs/ip_vs_core.c
+++ net/netfilter/ipvs/ip_vs_core.c
@@ -2016,8 +2016,11 @@ ip_vs_in_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state
 		}
 
 		if (resched) {
-			if (!old_ct)
+			if (!old_ct) {
+				spin_lock_bh(&cp->lock);
 				cp->flags &= ~IP_VS_CONN_F_NFCT;
+				spin_unlock_bh(&cp->lock);
+			}
 			if (!atomic_read(&cp->n_control))
 				ip_vs_conn_expire_now(cp);
 			__ip_vs_conn_put(cp);
@@ -2033,8 +2036,11 @@ ip_vs_in_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state
 		if (sysctl_expire_nodest_conn(ipvs)) {
 			bool old_ct = ip_vs_conn_uses_old_conntrack(cp, skb);
 
-			if (!old_ct)
+			if (!old_ct) {
+				spin_lock_bh(&cp->lock);
 				cp->flags &= ~IP_VS_CONN_F_NFCT;
+				spin_unlock_bh(&cp->lock);
+			}
 
 			ip_vs_conn_expire_now(cp);
 			__ip_vs_conn_put(cp);
