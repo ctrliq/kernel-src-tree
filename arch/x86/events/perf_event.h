b36e2ce29b44801cbabb398abb95203875921123
Michael Petlan <mpetlan@redhat.com>
2026-08-07 15:29:28 +0200

    perf/x86/intel: Add core PMU support for Novalake
    JIRA: https://redhat.atlassian.net/browse/RHEL-117336
    
    upstream
    ========
    commit c847a208f43bfeb56943f2ca6fe2baf1db9dee7a
    Author: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Date: Wed Jan 14 09:17:48 2026 +0800
    
    description
    ===========
    This patch enables core PMU support for Novalake, covering both P-core
    and E-core. It includes Arctic Wolf-specific counters and PEBS
    constraints, and the model-specific OMR extra registers table.
    
    Since Coyote Cove shares the same PMU capabilities as Panther Cove, the
    existing Panther Cove PMU enabling functions are reused for Coyote Cove.
    
    For detailed information about counter constraints, please refer to
    section 16.3 "COUNTER RESTRICTIONS" in the ISE documentation.
    
        Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Link: https://patch.msgid.link/20260114011750.350569-6-dapeng1.mi@linux.intel.com
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -1711,6 +1711,8 @@ extern struct event_constraint intel_glp_pebs_event_constraints[];
 
 extern struct event_constraint intel_grt_pebs_event_constraints[];
 
+extern struct event_constraint intel_arw_pebs_event_constraints[];
+
 extern struct event_constraint intel_nehalem_pebs_event_constraints[];
 
 extern struct event_constraint intel_westmere_pebs_event_constraints[];
