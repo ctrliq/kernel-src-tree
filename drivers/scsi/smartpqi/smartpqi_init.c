bc95bd4a2c4aee1666bf90ba472985df71beb713
Don Brace <dbrace@redhat.com>
2026-08-07 15:32:21 -0500

    scsi: smartpqi: Add new PCI device-ids
    JIRA: https://redhat.atlassian.net/browse/RHEL-224339
    
    commit 1a220e6e87a31c2572cd07ff47023e5cd92ac80f
    Author: David Strahan <David.Strahan@microchip.com>
    Date:   Wed Jul 22 17:04:00 2026 -0500
    
        scsi: smartpqi: Add new PCI device-ids
    
        All PCI ID entries in Hex.
    
        Add PCI IDs for Hurray Data controllers:
                                                    VID  / DID  / SVID / SDID
                                                    ----   ----   ----   ----
                                                    9005 / 028f / 207d / 4246
                                                    9005 / 028f / 207d / 4256
                                                    9005 / 028f / 207d / 4356
                                                    9005 / 028f / 207d / 4940
                                                    9005 / 028f / 207d / 4a46
    
        Add PCI IDs for ZTE controllers:
                                                    VID  / DID  / SVID / SDID
                                                    ----   ----   ----   ----
                                                    9005 / 028f / 1cf2 / 5451
                                                    9005 / 028f / 1cf2 / 5452
                                                    9005 / 028f / 1cf2 / 5453
    
        Add PCI ID for Ramaxel controller:
                                                    VID  / DID  / SVID / SDID
                                                    ----   ----   ----   ----
                                                    9005 / 028f / 1f3f / 0670
    
        Signed-off-by: David Strahan <david.strahan@microchip.com>
        Reviewed-by: Don Brace <don.brace@microchip.com>
        Link: https://lore.kernel.org/linux-scsi/20260722220401.6357-1-david.strahan@microchip.com/
        Link: https://patch.msgid.link/20260722220401.6357-4-david.strahan@microchip.com
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Don Brace <dbrace@redhat.com>

--- drivers/scsi/smartpqi/smartpqi_init.c
+++ drivers/scsi/smartpqi/smartpqi_init.c
@@ -10311,7 +10311,27 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4246)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4256)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4356)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x207d, 0x4840)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4940)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4a46)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
@@ -10499,6 +10519,18 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x5451)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x5452)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x5453)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1cf2, 0x54da)
 	},
 	{
@@ -10536,6 +10568,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1f3f, 0x0610)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1f3f, 0x0670)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
