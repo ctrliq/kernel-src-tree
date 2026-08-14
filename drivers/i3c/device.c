bc2a536e8d983b48689779a9b8c22e19fe2d7001
Jennifer Berringer <jberring@redhat.com>
2026-08-14 02:08:34 -0400

    i3c: master: Introduce optional Runtime PM support
    JIRA: https://issues.redhat.com/browse/RHEL-143335
    
    commit 990c149c61ee45da4fb6372e6b2fdd9808414e7a
    Author: Adrian Hunter <adrian.hunter@intel.com>
    Date:   Tue, 13 Jan 2026 09:27:00 +0200
    
        i3c: master: Introduce optional Runtime PM support
    
        Master drivers currently manage Runtime PM individually, but all require
        runtime resume for bus operations.  This can be centralized in common code.
    
        Add optional Runtime PM support to ensure the parent device is runtime
        resumed before bus operations and auto-suspended afterward.
    
        Notably, do not call ->bus_cleanup() if runtime resume fails.  Master
        drivers that opt-in to core runtime PM support must take that into account.
    
        Also provide an option to allow IBIs and hot-joins while runtime suspended.
    
        Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
        Reviewed-by: Frank Li <Frank.Li@nxp.com>
        Link: https://patch.msgid.link/20260113072702.16268-20-adrian.hunter@intel.com
        Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    
    Signed-off-by: Jennifer Berringer <jberring@redhat.com>

--- drivers/i3c/device.c
+++ drivers/i3c/device.c
@@ -45,9 +45,15 @@ int i3c_device_do_xfers(struct i3c_device *dev, struct i3c_xfer *xfers,
 			return -EINVAL;
 	}
 
+	ret = i3c_bus_rpm_get(dev->bus);
+	if (ret)
+		return ret;
+
 	i3c_bus_normaluse_lock(dev->bus);
 	ret = i3c_dev_do_xfers_locked(dev->desc, xfers, nxfers, mode);
 	i3c_bus_normaluse_unlock(dev->bus);
+
+	i3c_bus_rpm_put(dev->bus);
 
 	return ret;
 }
@@ -65,9 +71,15 @@ int i3c_device_do_setdasa(struct i3c_device *dev)
 {
 	int ret;
 
+	ret = i3c_bus_rpm_get(dev->bus);
+	if (ret)
+		return ret;
+
 	i3c_bus_normaluse_lock(dev->bus);
 	ret = i3c_dev_setdasa_locked(dev->desc);
 	i3c_bus_normaluse_unlock(dev->bus);
+
+	i3c_bus_rpm_put(dev->bus);
 
 	return ret;
 }
@@ -105,15 +117,26 @@ EXPORT_SYMBOL_GPL(i3c_device_get_info);
  */
 int i3c_device_disable_ibi(struct i3c_device *dev)
 {
-	int ret = -ENOENT;
+	int ret;
+
+	if (i3c_bus_rpm_ibi_allowed(dev->bus)) {
+		ret = i3c_bus_rpm_get(dev->bus);
+		if (ret)
+			return ret;
+	}
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (dev->desc) {
 		mutex_lock(&dev->desc->ibi_lock);
 		ret = i3c_dev_disable_ibi_locked(dev->desc);
 		mutex_unlock(&dev->desc->ibi_lock);
+	} else {
+		ret = -ENOENT;
 	}
 	i3c_bus_normaluse_unlock(dev->bus);
+
+	if (!ret || i3c_bus_rpm_ibi_allowed(dev->bus))
+		i3c_bus_rpm_put(dev->bus);
 
 	return ret;
 }
@@ -134,15 +157,24 @@ EXPORT_SYMBOL_GPL(i3c_device_disable_ibi);
  */
 int i3c_device_enable_ibi(struct i3c_device *dev)
 {
-	int ret = -ENOENT;
+	int ret;
+
+	ret = i3c_bus_rpm_get(dev->bus);
+	if (ret)
+		return ret;
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (dev->desc) {
 		mutex_lock(&dev->desc->ibi_lock);
 		ret = i3c_dev_enable_ibi_locked(dev->desc);
 		mutex_unlock(&dev->desc->ibi_lock);
+	} else {
+		ret = -ENOENT;
 	}
 	i3c_bus_normaluse_unlock(dev->bus);
+
+	if (ret || i3c_bus_rpm_ibi_allowed(dev->bus))
+		i3c_bus_rpm_put(dev->bus);
 
 	return ret;
 }
@@ -162,18 +194,26 @@ EXPORT_SYMBOL_GPL(i3c_device_enable_ibi);
 int i3c_device_request_ibi(struct i3c_device *dev,
 			   const struct i3c_ibi_setup *req)
 {
-	int ret = -ENOENT;
+	int ret;
 
 	if (!req->handler || !req->num_slots)
 		return -EINVAL;
+
+	ret = i3c_bus_rpm_get(dev->bus);
+	if (ret)
+		return ret;
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (dev->desc) {
 		mutex_lock(&dev->desc->ibi_lock);
 		ret = i3c_dev_request_ibi_locked(dev->desc, req);
 		mutex_unlock(&dev->desc->ibi_lock);
+	} else {
+		ret = -ENOENT;
 	}
 	i3c_bus_normaluse_unlock(dev->bus);
+
+	i3c_bus_rpm_put(dev->bus);
 
 	return ret;
 }
