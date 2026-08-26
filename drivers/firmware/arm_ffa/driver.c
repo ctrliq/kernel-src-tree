237abbdd821bad1452759841bd996f203dedb5cf
Jennifer Berringer <jberring@redhat.com>
2026-08-26 06:04:48 -0400

    Revert "firmware: arm_ffa: Set the core device as FF-A device parent"
    JIRA: https://redhat.atlassian.net/browse/RHEL-249190
    Upstream Status: RHEL Only
    
    Revert this patch and the others in its patch series in order to avoid
    depending on IMA_INIT_LATE_SYNC, which has not yet been backported.
    
    This reverts commit 24c8cf92527f198a8cc959308b8d8389869960c8.
    
    Signed-off-by: Jennifer Berringer <jberring@redhat.com>

--- drivers/firmware/arm_ffa/driver.c
+++ drivers/firmware/arm_ffa/driver.c
@@ -1755,7 +1755,7 @@ static int ffa_setup_host_partition(int vm_id)
 	int ret;
 
 	buf.id = vm_id;
-	ffa_dev = ffa_device_register(&buf, &ffa_drv_ops, &ffa_pdev->dev);
+	ffa_dev = ffa_device_register(&buf, &ffa_drv_ops);
 	if (!ffa_dev) {
 		pr_err("%s: failed to register host partition ID 0x%x\n",
 		       __func__, vm_id);
@@ -1830,8 +1830,7 @@ static int ffa_setup_partitions(void)
 		 * provides UUID here for each partition as part of the
 		 * discovery API and the same is passed.
 		 */
-		ffa_dev = ffa_device_register(tpbuf, &ffa_drv_ops,
-					      &ffa_pdev->dev);
+		ffa_dev = ffa_device_register(tpbuf, &ffa_drv_ops);
 		if (!ffa_dev) {
 			pr_err("%s: failed to register partition ID 0x%x\n",
 			       __func__, tpbuf->id);
