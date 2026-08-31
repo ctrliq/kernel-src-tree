1a362aeff2b7eb5919e9c04078179e29b40d98f8
Brett Mastbergen <bmastbergen@ciq.com>
2026-08-31 10:55:18 -0400

    af_unix: Don't call wait_for_unix_gc() on every sendmsg().
    cve-pre CVE-2026-53361
    commit-author Kuniyuki Iwashima <kuniyu@google.com>
    commit 384900542dc85
    
    We have been calling wait_for_unix_gc() on every sendmsg() in case
    there are too many inflight AF_UNIX sockets.
    
    This is also because the old GC implementation had poor knowledge
    of the inflight sockets and had to suspect every sendmsg().
    
    This was improved by commit d9f21b361333 ("af_unix: Try to run GC
    async."), but we do not even need to call wait_for_unix_gc() if the
    process is not sending AF_UNIX sockets.
    
    The wait_for_unix_gc() call only helps when a malicious process
    continues to create cyclic references, and we can detect that
    in a better place and slow it down.
    
    Let's move wait_for_unix_gc() to unix_prepare_fpl() that is called
    only when AF_UNIX socket fd is passed via SCM_RIGHTS.
    
    	Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://patch.msgid.link/20251115020935.2643121-5-kuniyu@google.com
    	Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 384900542dc85f3aac7918fea8e7ef62141e3ea6)
    	Signed-off-by: Brett Mastbergen <bmastbergen@ciq.com>

--- net/unix/garbage.c
+++ net/unix/garbage.c
@@ -290,6 +290,8 @@ void unix_update_edges(struct unix_sock *receiver)
 	}
 }
 
+static void wait_for_unix_gc(struct scm_fp_list *fpl);
+
 int unix_prepare_fpl(struct scm_fp_list *fpl)
 {
 	struct unix_vertex *vertex;
@@ -310,6 +312,8 @@ int unix_prepare_fpl(struct scm_fp_list *fpl)
 				    GFP_KERNEL_ACCOUNT);
 	if (!fpl->edges)
 		goto err;
+
+	wait_for_unix_gc(fpl);
 
 	return 0;
 
@@ -636,7 +640,7 @@ void unix_schedule_gc(void)
 #define UNIX_INFLIGHT_TRIGGER_GC 16000
 #define UNIX_INFLIGHT_SANE_USER (SCM_MAX_FD * 8)
 
-void wait_for_unix_gc(struct scm_fp_list *fpl)
+static void wait_for_unix_gc(struct scm_fp_list *fpl)
 {
 	/* If number of inflight sockets is insane,
 	 * force a garbage collect right now.
@@ -650,8 +654,7 @@ void wait_for_unix_gc(struct scm_fp_list *fpl)
 	/* Penalise users who want to send AF_UNIX sockets
 	 * but whose sockets have not been received yet.
 	 */
-	if (!fpl || !fpl->count_unix ||
-	    READ_ONCE(fpl->user->unix_inflight) < UNIX_INFLIGHT_SANE_USER)
+	if (READ_ONCE(fpl->user->unix_inflight) < UNIX_INFLIGHT_SANE_USER)
 		return;
 
 	if (READ_ONCE(gc_in_progress))
