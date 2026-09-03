1372170cf50b6818cd76bafee5cbb6ab26de0820
Dennis Chen <dechen@redhat.com>
2026-09-03 16:07:29 -0400

    platform/x86: int3472: Rename pled to led in LED registration code
    JIRA: https://redhat.atlassian.net/browse/RHEL-248696
    
    commit 39237e3208209d1bb35d939d6fee1f36b642f562
    Author: Marco Nenciarini <mnencia@kcore.it>
    Date:   Wed Apr 1 22:36:36 2026 +0200
    
        platform/x86: int3472: Rename pled to led in LED registration code
    
        Rename the privacy LED type, struct member, and functions from "pled"
        to "led" in preparation for supporting additional LED types beyond
        just the privacy LED.
    
        No functional change.
    
        Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
        Reviewed-by: Hans de Goede <johannes.goede@oss.qualcomm.com>
        Signed-off-by: Marco Nenciarini <mnencia@kcore.it>
        Link: https://patch.msgid.link/20260401203638.1601661-3-mnencia@kcore.it
        Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
        Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    
    Signed-off-by: Dennis Chen <dechen@redhat.com>

--- include/linux/platform_data/x86/int3472.h
+++ include/linux/platform_data/x86/int3472.h
@@ -124,12 +124,12 @@ struct int3472_discrete_device {
 		u8 imgclk_index;
 	} clock;
 
-	struct int3472_pled {
+	struct int3472_led {
 		struct led_classdev classdev;
 		struct led_lookup_data lookup;
 		char name[INT3472_LED_MAX_NAME_LEN];
 		struct gpio_desc *gpio;
-	} pled;
+	} led;
 
 	struct int3472_discrete_quirks quirks;
 
@@ -163,7 +163,7 @@ int skl_int3472_register_regulator(struct int3472_discrete_device *int3472,
 				   const char *second_sensor);
 void skl_int3472_unregister_regulator(struct int3472_discrete_device *int3472);
 
-int skl_int3472_register_pled(struct int3472_discrete_device *int3472, struct gpio_desc *gpio);
-void skl_int3472_unregister_pled(struct int3472_discrete_device *int3472);
+int skl_int3472_register_led(struct int3472_discrete_device *int3472, struct gpio_desc *gpio);
+void skl_int3472_unregister_led(struct int3472_discrete_device *int3472);
 
 #endif
