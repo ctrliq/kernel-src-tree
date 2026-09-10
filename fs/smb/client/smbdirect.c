91e2bad95d2cc4e815a65209076cb174b7b483da
Paulo Alcantara <paalcant@redhat.com>
2026-09-10 15:59:42 -0300

    smb: client: make use of smbdirect_socket.send_io.bcredits
    JIRA: https://issues.redhat.com/browse/RHEL-231162
    
    commit 21538121efe6c8c5b51c742fa02cbe820bc48714
    Author: Stefan Metzmacher <metze@samba.org>
    Date:   Thu Jan 22 18:16:57 2026 +0100
    
        smb: client: make use of smbdirect_socket.send_io.bcredits
    
        It turns out that our code will corrupt the stream of
        reassabled data transfer messages when we trigger an
        immendiate (empty) send.
    
        In order to fix this we'll have a single 'batch' credit per
        connection. And code getting that credit is free to use
        as much messages until remaining_length reaches 0, then
        the batch credit it given back and the next logical send can
        happen.
    
        Cc: <stable@vger.kernel.org> # 6.18.x
        Cc: Steve French <smfrench@gmail.com>
        Cc: Tom Talpey <tom@talpey.com>
        Cc: Long Li <longli@microsoft.com>
        Cc: Namjae Jeon <linkinjeon@kernel.org>
        Cc: linux-cifs@vger.kernel.org
        Cc: samba-technical@lists.samba.org
        Signed-off-by: Stefan Metzmacher <metze@samba.org>
        Signed-off-by: Steve French <stfrench@microsoft.com>
    
    Signed-off-by: Paulo Alcantara <paalcant@redhat.com>

--- fs/smb/client/smbdirect.c
+++ fs/smb/client/smbdirect.c
@@ -669,6 +669,7 @@ static bool process_negotiation_response(
 			sp->max_frmr_depth * PAGE_SIZE);
 	sp->max_frmr_depth = sp->max_read_write_size / PAGE_SIZE;
 
+	atomic_set(&sc->send_io.bcredits.count, 1);
 	sc->recv_io.expected = SMBDIRECT_EXPECT_DATA_TRANSFER;
 	return true;
 }
@@ -1226,6 +1227,7 @@ static void smbd_send_batch_init(struct smbdirect_send_batch *batch,
 	batch->wr_cnt = 0;
 	batch->need_invalidate_rkey = need_invalidate_rkey;
 	batch->remote_key = remote_key;
+	batch->credit = 0;
 }
 
 static int smbd_send_batch_flush(struct smbdirect_socket *sc,
@@ -1236,7 +1238,7 @@ static int smbd_send_batch_flush(struct smbdirect_socket *sc,
 	int ret = 0;
 
 	if (list_empty(&batch->msg_list))
-		return 0;
+		goto release_credit;
 
 	first = list_first_entry(&batch->msg_list,
 				 struct smbdirect_send_io,
@@ -1278,6 +1280,13 @@ static int smbd_send_batch_flush(struct smbdirect_socket *sc,
 		smbd_free_send_io(last);
 	}
 
+release_credit:
+	if (is_last && !ret && batch->credit) {
+		atomic_add(batch->credit, &sc->send_io.bcredits.count);
+		batch->credit = 0;
+		wake_up(&sc->send_io.bcredits.wait_queue);
+	}
+
 	return ret;
 }
 
@@ -1301,6 +1310,25 @@ static int wait_for_credits(struct smbdirect_socket *sc,
 		else if (ret < 0)
 			return ret;
 	} while (true);
+}
+
+static int wait_for_send_bcredit(struct smbdirect_socket *sc,
+				 struct smbdirect_send_batch *batch)
+{
+	int ret;
+
+	if (batch->credit)
+		return 0;
+
+	ret = wait_for_credits(sc,
+			       &sc->send_io.bcredits.wait_queue,
+			       &sc->send_io.bcredits.count,
+			       1);
+	if (ret)
+		return ret;
+
+	batch->credit = 1;
+	return 0;
 }
 
 static int wait_for_send_lcredit(struct smbdirect_socket *sc,
@@ -1351,6 +1379,19 @@ static int smbd_post_send_sgl(struct smbdirect_socket *sc,
 	struct smbdirect_data_transfer *packet;
 	int new_credits;
 	struct scatterlist *sg;
+	struct smbdirect_send_batch _batch;
+
+	if (!batch) {
+		smbd_send_batch_init(&_batch, false, 0);
+		batch = &_batch;
+	}
+
+	rc = wait_for_send_bcredit(sc, batch);
+	if (rc) {
+		log_outgoing(ERR, "disconnected not sending on wait_bcredit\n");
+		rc = -EAGAIN;
+		goto err_wait_bcredit;
+	}
 
 	rc = wait_for_send_lcredit(sc, batch);
 	if (rc) {
@@ -1439,8 +1480,14 @@ static int smbd_post_send_sgl(struct smbdirect_socket *sc,
 	}
 
 	rc = smbd_post_send(sc, batch, request);
-	if (!rc)
-		return 0;
+	if (!rc) {
+		if (batch != &_batch)
+			return 0;
+
+		rc = smbd_send_batch_flush(sc, batch, true);
+		if (!rc)
+			return 0;
+	}
 
 err_dma:
 	smbd_free_send_io(request);
@@ -1454,6 +1501,11 @@ err_wait_credit:
 	wake_up(&sc->send_io.lcredits.wait_queue);
 
 err_wait_lcredit:
+	atomic_add(batch->credit, &sc->send_io.bcredits.count);
+	batch->credit = 0;
+	wake_up(&sc->send_io.bcredits.wait_queue);
+
+err_wait_bcredit:
 	return rc;
 }
 
