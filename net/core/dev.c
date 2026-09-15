328c63bb788e0babe83a5817a61c29df70da1c2b
Jonathan Maple <jmaple@ciq.com>
2026-09-15 00:02:21 -0400

    Rebuild rocky9_8 with kernel-5.14.0-687.47.1.el9_8
    Rebuild_History BUILDABLE
    Rebuilding Kernel from rpm changelog with Fuzz Limit: 87.50%
    Number of commits in upstream range v5.14~1..kernel-mainline: 394115
    Number of commits in rpm: 84
    Number of commits matched with upstream: 67 (79.76%)
    Number of commits in upstream but not in rpm: 394049
    Number of commits NOT found in upstream: 17 (20.24%)
    
    Rebuilding Kernel on Branch rocky9_8_rebuild_kernel-5.14.0-687.47.1.el9_8 for kernel-5.14.0-687.47.1.el9_8
    Clean Cherry Picks: 51 (76.12%)
    Empty Cherry Picks: 14 (20.90%)
    _______________________________
    
    Full Details Located here:
    ciq/ciq_backports/kernel-5.14.0-687.47.1.el9_8/rebuild.details.txt
    
    Includes:
    * git commit header above
    * Empty Commits with upstream SHA
    * RPM ChangeLog Entries that could not be matched
    
    Individual Empty Commit failures contained in the same containing directory.
    The git message for empty commits will have the path for the failed commit.
    File names are the first 8 characters of the upstream SHA

--- net/core/dev.c
+++ net/core/dev.c
@@ -4529,6 +4529,7 @@ int __dev_queue_xmit(struct sk_buff *skb, struct net_device *sb_dev)
 {
 	struct net_device *dev = skb->dev;
 	struct netdev_queue *txq = NULL;
+	enum skb_drop_reason reason;
 	struct Qdisc *q;
 	int rc = -ENOMEM;
 	bool again = false;
