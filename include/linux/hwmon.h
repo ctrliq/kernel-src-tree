6f820739bb743a11777e8de5d4d3fa5ca8ab09be
Jonathan Maple <jmaple@ciq.com>
2026-09-18 03:02:06 -0400

    Rebuild rocky10_2 with kernel-6.12.0-211.56.1.el10_2
    Rebuild_History BUILDABLE
    Rebuilding Kernel from rpm changelog with Fuzz Limit: 87.50%
    Number of commits in upstream range v6.12~1..kernel-mainline: 138299
    Number of commits in rpm: 67
    Number of commits matched with upstream: 53 (79.10%)
    Number of commits in upstream but not in rpm: 138246
    Number of commits NOT found in upstream: 14 (20.90%)
    
    Rebuilding Kernel on Branch rocky10_2_rebuild_kernel-6.12.0-211.56.1.el10_2 for kernel-6.12.0-211.56.1.el10_2
    Clean Cherry Picks: 48 (90.57%)
    Empty Cherry Picks: 5 (9.43%)
    _______________________________
    
    Full Details Located here:
    ciq/ciq_backports/kernel-6.12.0-211.56.1.el10_2/rebuild.details.txt
    
    Includes:
    * git commit header above
    * Empty Commits with upstream SHA
    * RPM ChangeLog Entries that could not be matched
    
    Individual Empty Commit failures contained in the same containing directory.
    The git message for empty commits will have the path for the failed commit.
    File names are the first 8 characters of the upstream SHA

--- include/linux/hwmon.h
+++ include/linux/hwmon.h
@@ -13,6 +13,7 @@
 #define _HWMON_H_
 
 #include <linux/bitops.h>
+#include <linux/cleanup.h>
 
 struct device;
 struct attribute_group;
@@ -490,6 +491,11 @@ int hwmon_notify_event(struct device *dev, enum hwmon_sensor_types type,
 
 char *hwmon_sanitize_name(const char *name);
 char *devm_hwmon_sanitize_name(struct device *dev, const char *name);
+
+void hwmon_lock(struct device *dev);
+void hwmon_unlock(struct device *dev);
+
+DEFINE_GUARD(hwmon_lock, struct device *, hwmon_lock(_T), hwmon_unlock(_T))
 
 /**
  * hwmon_is_bad_char - Is the char invalid in a hwmon name
